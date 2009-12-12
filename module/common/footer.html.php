0d9adcf8ce190dac16ab46c359ac851f8e0fbae3
wangchunsheng <chunsheng@cnezsoft.com>
2009-12-12 11:29:42 +0000

    * fix the error when no position.

--- module/common/footer.html.php
+++ module/common/footer.html.php
@@ -2,10 +2,10 @@
 <iframe frameborder='0' name='hiddenwin' id='hiddenwin' class='<?php $config->debug ? print("debugwin") : print('hiddenwin')?>' src='<?php echo $this->createLink('index', 'ping');?>'></iframe>
 <div id='footer' class='yui-d0 yui-t7'>
   <div class='yui-g'>
-    <div class='yui-g first' id='bread'> <?php common::printBreadMenu($this->moduleName, $position); ?></div>
+    <div class='yui-g first' id='crumbs'><?php common::printBreadMenu($this->moduleName, isset($position) ? $position : ''); ?></div>
     <div class='yui-g'>
-      <div class='yui-u first'></div>
-      <div class='yui-u a-right' id='footer-right'>
+      <div class='yui-u first'> </div>
+      <div class='yui-u a-right'>
         <span id='poweredby'>powered by <a href='http://www.zentao.cn' target='_blank'>ZenTaoPMS</a> (<?php echo $config->version;?>)</span>
       </div>
     </div>
