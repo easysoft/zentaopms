86878971cc7f3a87d7e3d2e5bd4ba64d0ba518f7
wangchunsheng <chunsheng@cnezsoft.com>
2009-12-13 15:02:33 +0000

    * reduce the code.

--- module/common/tablesorter.html.php
+++ module/common/tablesorter.html.php
@@ -1,9 +1,3 @@
 <link rel="stylesheet" href="<?php echo $clientTheme;?>tablesorter.css" />
 <script src="<?php echo $jsRoot;?>jquery/tablesorter/min.js" type="text/javascript"></script>
-<script language='javascript'>
-$(function() 
-{ 
-    $(".tablesorter").tablesorter(); 
-} 
-); 
-</script>
+<script language='javascript'> $(function() { $(".tablesorter").tablesorter(); } );</script>
