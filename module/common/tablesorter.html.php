f8a9a55685147cc02379514a068166f9fe405892
wangchunsheng <chunsheng@cnezsoft.com>
2010-01-18 08:15:13 +0000

    * remove the line of include colorbox.

--- module/common/tablesorter.html.php
+++ module/common/tablesorter.html.php
@@ -1,6 +1,5 @@
 <link rel='stylesheet' href='<?php echo $clientTheme;?>tablesorter.css' />
 <script src='<?php echo $jsRoot;?>jquery/tablesorter/min.js' type='text/javascript'></script>
-<script src='<?php echo $jsRoot;?>jquery/colorize/full.js' type='text/javascript'></script>
 <script language='javascript'>
 $(function() {
 
