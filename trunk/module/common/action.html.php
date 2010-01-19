f12e8bee924c912f1aaa15d95af764c9c8314588
wangchunsheng <chunsheng@cnezsoft.com>
2010-01-19 09:24:31 +0000

    * fix the bug of substr.

--- trunk/module/common/action.html.php
+++ trunk/module/common/action.html.php
@@ -5,7 +5,7 @@
     <li>
       <?php
       if(isset($users[$action->actor])) $action->actor = $users[$action->actor];
-      if($pos = strpos($action->actor, ':') !== false) $action->actor = substr($action->actor, $pos + 1);
+      if(strpos($action->actor, ':') !== false) $action->actor = substr($action->actor, strpos($action->actor, ':') + 1);
       ?>
       <span><?php echo "$action->date, <strong>$action->action</strong> by <strong>$action->actor</strong>"; ?></span>
       <?php if(!empty($action->comment) or !empty($action->history)):?>
