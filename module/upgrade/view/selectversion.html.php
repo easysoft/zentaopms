e00c2163bc3d2272d6925f5945a1ee9d85448557
wangchunsheng <chunsheng@cnezsoft.com>
2010-01-25 06:18:03 +0000

    + add upgrade module.

---
+++ module/upgrade/view/selectversion.html.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The html template file of select version method of upgrade module of ZenTaoMS.
+ *
+ * ZenTaoMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ZenTaoMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ZenTaoMS.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @copyright   Copyright: 2009 Chunsheng Wang
+ * @author      Chunsheng Wang <wwccss@263.net>
+ * @package     upgrade
+ * @version     $Id$
+ */
+?>
+<?php include '../../common/header.html.php';?>
+<div class='yui-d0'>
+  <form method='post' action='<?php echo inlink('execute');?>'>
+  <table align='center' class='table-5 f-14px'>
+    <caption><?php echo $lang->upgrade->selectVersion;?></caption>
+    <tr>
+      <th class='w-p20 rowhead'><?php echo $lang->upgrade->fromVersion;?></th>
+      <td><?php echo html::select('fromVersion', $lang->upgrade->fromVersions, '', 'class=select-3');?></td>
+    </tr>
+    <tr>
+      <td colspan='2' class='a-center'><?php echo html::submitButton($lang->upgrade->common);?></td>
+    </tr>
+  </table>
+  </form>
+</div>
+<?php include '../../common/footer.html.php';?>
