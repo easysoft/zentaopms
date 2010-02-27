993919565cc9d2594ad0b402568b2458a6a3c4a4
wangchunsheng <chunsheng@cnezsoft.com>
2010-02-27 01:20:32 +0000

    * adjust the config.

--- trunk/config/config.php
+++ trunk/config/config.php
@@ -21,12 +21,14 @@
  * @version     $Id$
  * @link        http://www.zentao.cn
  */
-$config->version     = '0.5 beta';        // 版本号，切勿修改。
-$config->helpRoot    = 'http://doc.zentao.cn/help/';
-$config->encoding    = 'UTF-8';           // 网站的编码。
-$config->cookiePath  = '/';               // cookie的有效路径。
-$config->cookieLife  = time() + 2592000;  // cookie的生命周期。
+/* 基本参数设定。*/
+$config->version     = '0.5 beta';                      // 版本号，切勿修改。
+$config->helpRoot    = 'http://doc.zentao.cn/help/';    // 在线帮助的URI地址。
+$config->encoding    = 'UTF-8';                         // 网站的编码。
+$config->cookiePath  = '/';                             // cookie的有效路径。
+$config->cookieLife  = time() + 2592000;                // cookie的生命周期。
 
+/* 请求方式设置。*/
 $config->requestType = 'PATH_INFO';       // 如何获取当前请求的信息，可选值：PATH_INFO|GET
 $config->pathType    = 'clean';           // requestType=PATH_INFO: 请求url的格式，可选值为full|clean，full格式会带有参数名称，clean则只有取值。
 $config->requestFix  = '-';               // requestType=PATH_INFO: 请求url的分隔符，可选值为斜线、下划线、减号。后面两种形式有助于SEO。
@@ -34,28 +36,33 @@ $config->moduleVar   = 'm';               // requestType=GET: 模块变量名。
 $config->methodVar   = 'f';               // requestType=GET: 方法变量名。
 $config->viewVar     = 't';               // requestType=GET: 模板变量名。
 
+/* 视图和主题。*/
 $config->views       = ',html,';          // 支持的视图列表。
 $config->themes      = 'default';         // 支持的主题列表。
 
-$config->langs['en']    = 'English';      
+/* 支持的语言列表。*/
 $config->langs['zh-cn'] = 'Chinese Simplified';
 
-$config->super2OBJ   = true;              // 是否通过对象来访问全局变量。
-
+/* 默认参数设定。*/
 $config->default->view   = 'html';             // 默认的视图格式。
 $config->default->lang   = 'zh-cn';            // 默认的语言。
 $config->default->theme  = 'default';          // 默认的主题。
 $config->default->module = 'index';            // 默认的模块。当请求中没有指定模块时，加载该模块。
 $config->default->method = 'index';            // 默认的方法。当请求中没有指定方法或者指定的方法不存在时，调用该方法。
 
+/* 上传附件参数设定。*/
 $config->file->dangers = 'php,jsp,py,rb,asp,'; // 不允许上传的文件类型列表。
 $config->file->maxSize = 1024 * 1024;          // 允许上传的文件大小，单位为字节。
 
+/* 数据库参数设定。*/
 $config->db->persistant = false;               // 是否打开持久连接。
 $config->db->driver     = 'mysql';             // pdo的驱动类型，目前暂时只支持mysql。
 $config->db->dao        = true;                // 是否使用DAO。
 $config->db->encoding   = 'UTF8';              // 数据库的编码。
 $config->db->strictMode = false;               // 关闭MySQL的严格模式。
+
+/* 通过对象引用全局变量。*/
+$config->super2OBJ = true;
 
 /* 包含自定义配置文件。*/
 $myConfig = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'my.php';
