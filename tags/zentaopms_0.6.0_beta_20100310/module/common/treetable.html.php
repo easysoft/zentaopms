7839701a1766a702f32f2730d1ab30bc972de511
wangchunsheng <chunsheng@cnezsoft.com>
2010-03-10 15:19:31 +0000

    - drop it.

--- tags/zentaopms_0.6.0_beta_20100310/module/common/treetable.html.php
+++
@@ -1,3 +0,0 @@
-<link rel='stylesheet' href='<?php echo $clientTheme;?>treetable.css' type='text/css' />
-<script src='<?php echo $jsRoot;?>jquery/treetable/min.js' type='text/javascript'></script>
-<script language='javascript'>$(function() { $("#treetable").treeTable({clickableNodeNames:true,initialState:"expanded"})})</script>
