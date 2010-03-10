c8c6dd4227f66901f31c2e5c321faf78e1708708
wangchunsheng <chunsheng@cnezsoft.com>
2010-03-10 13:26:51 +0000

    + add the build tools

---
+++ trunk/bin/build.php
@@ -0,0 +1,12 @@
+1. 修改zentaophp中的version number，打tag。
+2. 修改zentaoms中的version
+    config.php中的version.
+    install中的version。
+3. 修改升级程序。
+4. 打包zentaoms。
+5. 合并目录。
+6. 修改www/index.php中的包含路径。
+8. zip包。
+9. windows包。
+10. 上传文件。
+11. 撰写升级声明。
