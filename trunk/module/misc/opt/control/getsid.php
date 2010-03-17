034c376aba7ce1b0bb197e48ea5ef5f269fc6a26
wangchunsheng <chunsheng@cnezsoft.com>
2010-03-17 14:46:20 +0000

    + add the example of model extension.

--- trunk/module/misc/opt/control/getsid.php
+++ trunk/module/misc/opt/control/getsid.php
@@ -5,7 +5,8 @@ class misc extends control
     public function getsid()
     {
         $this->view->header->title = 'getsid';
-        $this->view->sid = session_id();
+        $this->view->sid  = session_id();
+        $this->view->test = $this->misc->test();
         $this->display();
     }
 }
