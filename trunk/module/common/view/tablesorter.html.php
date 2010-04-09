f7395f60bb91cf98a7b8bd52a9a40f56e89e5ca2
wangchunsheng <chunsheng@cnezsoft.com>
2010-04-09 09:38:21 +0000

    * merge the tablesorter css to style.css.

--- trunk/module/common/view/tablesorter.html.php
+++ trunk/module/common/view/tablesorter.html.php
@@ -1,4 +1,3 @@
-<link rel='stylesheet' href='<?php echo $clientTheme;?>tablesorter.css' />
 <script src='<?php echo $jsRoot;?>jquery/tablesorter/min.js' type='text/javascript'></script>
 <script src='<?php echo $jsRoot;?>jquery/tablesorter/metadata.js' type='text/javascript'></script>
 <script language='javascript'>
