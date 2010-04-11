3488d63e920cc464cd365d6b5050c7e8c0e42abc
wangchunsheng <chunsheng@cnezsoft.com>
2010-04-11 16:05:34 +0000

    * adjust the UI of productplan.  * log action when link and unlink a story to or from a plan.

--- trunk/module/productplan/view/browse.html.php
+++ trunk/module/productplan/view/browse.html.php
@@ -25,28 +25,30 @@
 <?php include '../../common/view/header.html.php';?>
 <?php include '../../common/view/tablesorter.html.php';?>
 <div class='yui-d0'>
-  <table class='table-1 tablesorter'>
-    <caption>
+  <table class='table-1 tablesorter fixed'>
+    <caption class='caption-tr'>
       <div class='f-left'><?php echo $lang->productplan->browse;?></div>
       <div class='f-right'><?php common::printLink('productplan', 'create', "productID=$product->id", $lang->productplan->create);?></div>
     </caption>
     <thead>
-    <tr>
+    <tr class='colhead'>
+      <th class='w-id'><?php echo $lang->idAB;?></th>
+      <th class='w-100px'><?php echo $lang->productplan->begin;?></th>
+      <th class='w-100px'><?php echo $lang->productplan->end;?></th>
       <th><?php echo $lang->productplan->title;?></th>
-      <th><?php echo $lang->productplan->begin;?></th>
-      <th><?php echo $lang->productplan->end;?></th>
-      <th><?php echo $lang->productplan->desc;?></th>
-      <th class="{sorter: false}"><?php echo $lang->actions;?></th>
+      <th class='w-p50'><?php echo $lang->productplan->desc;?></th>
+      <th class="w-120px {sorter: false}"><?php echo $lang->actions;?></th>
     </tr>
     </thead>
     <tbody>
     <?php foreach($plans as $plan):?>
     <tr class='a-center'>
-      <td><?php echo html::a(inlink('view', "id=$plan->id"), $plan->title);?></td>
+      <td><?php echo html::a(inlink('view', "id=$plan->id"), $plan->id);?></td>
       <td><?php echo $plan->begin;?></td>
       <td><?php echo $plan->end;?></td>
-      <td class='a-left'><?php echo nl2br($plan->desc);?></td>
-      <td class='nobr'>
+      <td class='a-left nobr'><?php echo html::a(inlink('view', "id=$plan->id"), $plan->title);?></td>
+      <td class='a-left nobr'><?php echo nl2br($plan->desc);?></td>
+      <td>
         <?php
         common::printLink('productplan', 'edit', "planID=$plan->id", $lang->edit);
         common::printLink('productplan', 'linkstory', "planID=$plan->id", $lang->productplan->linkStory);
