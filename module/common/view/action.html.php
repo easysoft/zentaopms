f1f47154e0d69defe195348697626ce7dd09a3cc
wangchunsheng <chunsheng@cnezsoft.com>
2010-04-13 02:28:52 +0000

    * add table theme to actions.

--- module/common/view/action.html.php
+++ module/common/view/action.html.php
@@ -1,7 +1,15 @@
 <script src='<?php echo $jsRoot;?>jquery/reverseorder/raw.js' type='text/javascript'></script>
+<?php if(!isset($actionTheme)) $actionTheme = 'fieldset';?>
+<?php if($actionTheme == 'fieldset'):?>
 <div>
 <fieldset>
   <legend onclick='$("#historyItem li").reverseOrder();' class='hand'><?php echo $lang->history . $lang->reverse;?></legend>
+<?php else:?>
+<table class='table-1'>
+  <caption onclick='$("#historyItem li").reverseOrder();' class='hand'><?php echo $lang->history . $lang->reverse;?></caption>
+  <tr><td>
+<?php endif;?>
+
   <ol id='historyItem'>
     <?php $i = 1; ?>
     <?php foreach($actions as $action):?>
@@ -20,5 +28,10 @@
     </li>
     <?php endforeach;?>
   </ol>
+
+<?php if($actionTheme == 'fieldset'):?>
 </fieldset>
+<?php else:?>
+</td></tr></table>
+<?php endif;?>
 </div>
