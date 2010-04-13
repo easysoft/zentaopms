0c2c8eb7a959af852c18e52a3bdde203b536f199
wangchunsheng <chunsheng@cnezsoft.com>
2010-04-13 03:38:52 +0000

    * adjust the my-bugs page.

--- trunk/module/my/view/bug.html.php
+++ trunk/module/my/view/bug.html.php
@@ -25,28 +25,41 @@
 <?php include '../../common/view/header.html.php';?>
 <?php include '../../common/view/tablesorter.html.php';?>
 <div class='yui-d0'>
-  <table class='table-1 tablesorter'>
+  <table class='table-1 fixed tablesorter'>
     <thead>
     <tr class='colhead'>
-      <th><?php echo $lang->bug->id;?></th>
-      <th><?php echo $lang->bug->severity;?></th>
+      <th class='w-id'><?php echo $lang->idAB;?></th>
+      <th class='w-severity'><?php echo $lang->bug->severityAB;?></th>
+      <th class='w-pri'><?php echo $lang->priAB;?></th>
+      <th class='w-type'><?php echo $lang->typeAB;?></th>
       <th><?php echo $lang->bug->title;?></th>
-      <th><?php echo $lang->bug->openedBy;?></th>
-      <th><?php echo $lang->bug->assignedTo;?></th>
-      <th><?php echo $lang->bug->resolvedBy;?></th>
-      <th><?php echo $lang->bug->resolution;?></th>
+      <th class='w-user'><?php echo $lang->openedByAB;?></th>
+      <th class='w-user'><?php echo $lang->assignedToAB;?></th>
+      <th class='w-user'><?php echo $lang->bug->resolvedBy;?></th>
+      <th class='w-resolution'><?php echo $lang->bug->resolutionAB;?></th>
+      <th class='w-100px {sorter:false}'><?php echo $lang->actions;?></th>
     </tr>
     </thead>
     <tbody>
     <?php foreach($bugs as $bug):?>
     <tr class='a-center'>
       <td><?php echo html::a($this->createLink('bug', 'view', "bugID=$bug->id"), $bug->id, '_blank');?></td>
-      <td><?php echo $bug->severity?></td>
-      <td width='50%' class='a-left'><?php echo $bug->title;?></td>
-      <td><?php echo $bug->openedBy;?></td>
-      <td><?php echo $bug->assignedTo;?></td>
-      <td><?php echo $bug->resolvedBy;?></td>
-      <td><?php echo $bug->resolution;?></td>
+      <td><?php echo $lang->bug->severityList[$bug->severity]?></td>
+      <td><?php echo $lang->bug->priList[$bug->pri]?></td>
+      <td><?php echo $lang->bug->typeList[$bug->type]?></td>
+      <td class='a-left nobr'><?php echo $bug->title;?></td>
+      <td><?php echo $users[$bug->openedBy];?></td>
+      <td><?php echo $users[$bug->assignedTo];?></td>
+      <td><?php echo $users[$bug->resolvedBy];?></td>
+      <td><?php echo $lang->bug->resolutionList[$bug->resolution];?></td>
+      <td>
+        <?php
+        $params = "bugID=$bug->id";
+        if(!($bug->status == 'active'   and common::printLink('bug', 'resolve', $params, $lang->bug->buttonResolve))) echo $lang->bug->buttonResolve . ' ';
+        if(!($bug->status == 'resolved' and common::printLink('bug', 'close', $params, $lang->bug->buttonClose)))     echo $lang->bug->buttonClose . ' ';
+        common::printLink('bug', 'edit', $params, $lang->bug->buttonEdit);
+        ?>
+      </td>
     </tr>
     <?php endforeach;?>
     </tbody>
