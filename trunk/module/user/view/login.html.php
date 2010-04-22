08e89f22eca1d91a58364ca084e844a2a1a6925f
wangchunsheng <chunsheng@cnezsoft.com>
2010-04-22 03:31:26 +0000

    * adjust the login form.

--- trunk/module/user/view/login.html.php
+++ trunk/module/user/view/login.html.php
@@ -43,40 +43,50 @@ $themeRoot   = $webRoot . "theme/";
 <link rel='icon'          href='<?php echo $webRoot;?>favicon.ico' type="image/x-icon" />
 <link rel='shortcut icon' href='<?php echo $webRoot;?>favicon.ico' type='image/x-icon' />
 <script language='Javascript'>loadFixedCSS();</script>
-<style>#debugbar, .helplink {display:none}</style>
-<body onload="document.getElementById('account').focus();">
-<div class='yui-d0'>
-  <form method='post' style='margin-top:100px' target='hiddenwin'>
+<style>
+html{background-color:#06294e;}
+body{background-image:url(theme/default/images/main/loginbg.png); background-position:center top; background-repeat:no-repeat;}
+table, tr, td, th, input{ border:none;}
+.rowhead{width:280px; font-weight:normal; font-size:14px; text-align:right; color:#fff;}
+.text-2 {width:160px; height:22px; background-color:#a4c5e0; border:1px solid #035793; font-size:16px; font-weight:bold}
+.pt-20px {padding-top:20px}
+.pt-200px{padding-top:200px}
+.pt-25px {padding-top:25px}
+#debugbar, .helplink{display:none}
+#welcome{background:none; border:none; color:#FFF; padding-top:8px;}
+#poweredby{color:#fff; margin-top:35px; text-align:center; line-height:1}
+#poweredby a {color:#fff}
+.button-s, .button-c {padding:3px 5px 3px 5px; width:80px; font-size:14px; font-weight:bold}
+</style>
+<body onLoad="document.getElementById('account').focus();">
+<div class='yui-d0 pt-200px'>
+  <form method='post' target='hiddenwin'>
     <table align='center' class='table-4'> 
-      <caption><?php printf($lang->welcome, $app->company->name);?></caption>
+      <caption id='welcome'><?php printf($lang->welcome, $app->company->name);?></caption>
       <tr>
-        <th><?php echo $lang->user->account;?></th>  
-        <td><input type='text' name='account' id='account' /></td>
+        <td class='rowhead pt-25px'><?php echo $lang->user->account;?>：</td>  
+        <td class='pt-25px'><input class='text-2' type='text' name='account' id='account' /></td>
       </tr>  
       <tr>
-        <th><?php echo $lang->user->password;?></th>  
-        <td><input type='password' name='password' /></td>
+        <td class='rowhead'><?php echo $lang->user->password;?>：</td>  
+        <td><input class='text-2' type='password' name='password' /></td>
       </tr>
       <tr>
-        <td colspan='2' class='a-center'>
-          <?php 
-          echo html::submitButton($lang->login);
-          echo html::resetButton(); 
-          $onclick = "onclick='location.href=\"" . $this->createLink($config->default->module) . "\"'";
-          if($app->company->guest) echo html::commonButton($lang->user->asGuest, $onclick);
-          echo html::hidden('referer', $referer);
-          ?>
+        <td colspan='2' class='a-center pt-20px'>
+        <?php 
+        echo html::submitButton($lang->login);
+        if($app->company->guest) echo html::linkButton($lang->user->asGuest, $this->createLink($config->default->module));
+        echo html::hidden('referer', $referer);
+        ?>
         </td>
       </tr>  
     </table>
-    <div class='a-center'>
+    <div id='poweredby'>
     powered by <a href='http://www.zentao.cn' target='_blank'>ZenTaoPMS</a>(<?php echo $config->version;?>). <br />
     <script src='http://www.zentao.cn/check.php?v=<?php echo $config->version;?>'></script>
     </div>
   </form>
 </div>  
-<div class='yui-d0' id='hiddenbar'>
-  <iframe frameborder='0' name='hiddenwin' id='hiddenwin' class='hidden'></iframe>
-</div>
+<div class='yui-d0' id='hiddenbar'><iframe frameborder='0' name='hiddenwin' id='hiddenwin' class='hidden'></iframe></div>
 </body>
 </html>
