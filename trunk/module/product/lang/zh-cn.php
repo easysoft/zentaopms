8d54fb21215b9b1922ef1bb2781cfadf438d00fa
wangchunsheng <chunsheng@cnezsoft.com>
2010-04-27 14:34:05 +0000

    * add the empty status.

--- trunk/module/product/lang/zh-cn.php
+++ trunk/module/product/lang/zh-cn.php
@@ -55,5 +55,6 @@ $lang->product->moduleStory = '按模块浏览';
 $lang->product->searchStory = '搜索';
 $lang->product->allStory    = '全部需求';
 
+$lang->product->statusList['']       = '';
 $lang->product->statusList['normal'] = '正常';
 $lang->product->statusList['closed'] = '结束';
