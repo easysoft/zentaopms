0d3a73e425182e730f46edec449f91173dce9346
wangchunsheng <chunsheng@cnezsoft.com>
2010-05-03 07:43:42 +0000

    - remove it.

--- tags/zentaopms_1.0.0_stable_20100503/module/common/view/treeview.html.php
+++
@@ -1,3 +0,0 @@
-<link rel='stylesheet' href='<?php echo $clientTheme;?>treeview.css' type='text/css' />
-<script src='<?php echo $jsRoot;?>jquery/treeview/min.js' type='text/javascript'></script>
-<script language='javascript'>$(function() { $("#tree").treeview({ persist: "cookie", collapsed: true, unique: true }) })</script>
--- tags/zentaopms_1.0.0_stable_20100503/module/todo/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-$config->todo->create->requiredFields = 'name';
-$config->todo->edit->requiredFields   = 'name';
