2bb998ad249b68d97c739776ad14ccaa1489e13f
wangchunsheng <chunsheng@cnezsoft.com>
2010-07-22 02:08:10 +0000

    + add doc module.

---
+++ trunk/module/doc/control.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The control file of doc module of ZenTaoMS.
+ *
+ * ZenTaoMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *                                                                             
+ * ZenTaoMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ZenTaoMS.  If not, see <http://www.gnu.org/licenses/>.  
+ *
+ * @copyright   Copyright 2009-2010 青岛易软天创网络科技有限公司(www.cnezsoft.com)
+ * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
+ * @package     doc
+ * @version     $Id: control.php 933 2010-07-06 06:53:40Z wwccss $
+ * @link        http://www.zentaoms.com
+ */
+class doc extends control
+{
+}
---
+++ trunk/module/doc/model.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The model file of doc module of ZenTaoMS.
+ *
+ * ZenTaoMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *                                                                             
+ * ZenTaoMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ZenTaoMS.  If not, see <http://www.gnu.org/licenses/>.  
+ *
+ * @copyright   Copyright 2009-2010 青岛易软天创网络科技有限公司(www.cnezsoft.com)
+ * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
+ * @package     doc
+ * @version     $Id: model.php 881 2010-06-22 06:50:32Z chencongzhi520 $
+ * @link        http://www.zentaoms.com
+ */
+?>
+<?php
+class docModel extends model
+{
+}
