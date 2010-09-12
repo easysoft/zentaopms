2c4cdcd4140163097fbc690d6807e160e3cc682c
wangchunsheng <chunsheng@cnezsoft.com>
2010-09-12 03:38:55 +0000

    * adjus the date picker.

--- module/common/view/datepicker.html.php
+++ module/common/view/datepicker.html.php
@@ -17,13 +17,8 @@ Date.dayNames       = ['日', '一', '二', '三', '四', '五', '六'];
 Date.abbrDayNames   = ['星期天', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
 Date.monthNames     = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
  
-$(function() {$(".date")
-    .datePicker({createButton:false, startDate:'1970-1-1'})
-    .bind('click', function() {
-        $(this).dpDisplay();
-        this.blur();
-        return false;
-    }
-    );
+$(function() {
+    $(".date").datePicker({createButton:true, startDate:'2000-1-1'})
+    .dpSetPosition($.dpConst.POS_TOP, $.dpConst.POS_RIGHT)
 });
 </script>
