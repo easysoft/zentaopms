2b0f64b4755f63e6a73566db6974d41a80357a3b
wangchunsheng <chunsheng@cnezsoft.com>
2010-09-12 12:51:56 +0000

    + add the keywords field.

--- db/update1.2.sql
+++ db/update1.2.sql
@@ -1,2 +1,3 @@
 -- 2010-09-12 doc table.
-UPDATE `zt_doc` SET `type` = 'file' WHERE `type` = '';
+UPDATE `zt_doc` SET `type` = 'file';
+ALTER TABLE `zt_doc` ADD `keywords` VARCHAR( 255 ) NOT NULL AFTER `digest`;
