7687bf5559e40c489ba3598b32dcea984cbcfa7b
fujia <yuren_@126.com>
2010-11-06 05:51:17 +0000

    + complete task #344:add extension to downloadfile.

--- module/file/view/printfiles.html.php
+++ module/file/view/printfiles.html.php
@@ -6,7 +6,7 @@
   <?php
   foreach($files as $file)
   {
-      if(common::hasPriv('file' , 'download')) echo html::a($this->createLink('file', 'download', "fileID=$file->id"), $file->title, '_blank', "onclick='return downloadFile($file->id)'");
+      if(common::hasPriv('file' , 'download')) echo html::a($this->createLink('file', 'download', "fileID=$file->id"), $file->title .'.' . $file->extension, '_blank', "onclick='return downloadFile($file->id)'");
       if(common::hasPriv('file', 'delete'))    echo html::commonButton(' x ', "onclick=deleteFile($file->id)");
   }
   ?>
