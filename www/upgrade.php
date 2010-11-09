50ec0bb2891634868402a9245fbc9902a08f79db
wangchunsheng <chunsheng@cnezsoft.com>
2010-11-09 06:28:36 +0000

    - remove the long license.

--- www/upgrade.php
+++ www/upgrade.php
@@ -2,19 +2,6 @@
 /**
  * The upgrade router file of ZenTaoMS.
  *
- * ZenTaoMS is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
-
- * ZenTaoMS is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with ZenTaoMS.  If not, see <http://www.gnu.org/licenses/>.
- *
  * @copyright   Copyright 2009-2010 QingDao Nature Easy Soft Network Technology Co,LTD (www.cnezsoft.com)
  * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
  * @package     ZenTaoMS
