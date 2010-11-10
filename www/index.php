b37cae0e8ed768e3b0caa722837bb1542f884d74
wangchunsheng <chunsheng@cnezsoft.com>
2010-11-10 08:08:42 +0000

    * change the routers.

--- www/index.php
+++ www/index.php
@@ -13,85 +13,25 @@
  */
 error_reporting(0);
 
-/* 记录最开始的时间。*/
-$timeStart = _getTime();
-
-/* 包含必须的类文件。*/
+/* Load the framework. */
 include '../framework/router.class.php';
 include '../framework/control.class.php';
 include '../framework/model.class.php';
 include '../framework/helper.class.php';
 
-/* 实例化路由对象，加载配置，设置时区。*/
+/* Log the time and define the run mode. */
+$startTime = getTime();
+
+/* Instance the app. */
 $app    = router::createApp('pms', dirname(dirname(__FILE__)));
-$config = $app->loadConfig('common');
-$app->setDebug();
-$app->setTimezone();
-
-/* 判断是否有GET变量mode=getconfig。*/
-if(isset($_GET['mode']) and $_GET['mode'] == 'getconfig') die($app->exportConfig());
-
-/* 检查是否已经安装。*/
-if(!isset($config->installed) or !$config->installed) die(header('location: install.php'));
-
-/* 连接到数据库。*/
-$dbh = $app->connectDB();
-
-/* 如果是debug模式，记录sql查询。*/
-if($config->debug) register_shutdown_function('_saveSQL');
-
-/* 设置客户端所使用的语言、风格。*/
-$app->setClientLang();
-$app->setClientTheme();
-
-/* 加载语言文件，加载公共模块。*/
-$lang   = $app->loadLang('common');
 $common = $app->loadCommon();
 
-/* 加载相应的lib文件，并设置超全局变量的引用。*/
-$app->loadClass('front',  $static = true);
-$app->loadClass('filter', $static = true);
-$app->setSuperVars();
+/* Check the reqeust is getconfig or not. Check installed or not. */
+if(isset($_GET['mode']) and $_GET['mode'] == 'getconfig') die($app->exportConfig());  // 
+if(!isset($config->installed) or !$config->installed) die(header('location: install.php'));
 
-/* 处理请求，验证权限，加载相应的模块。*/
+/* Run the app. */
 $app->parseRequest();
 $common->checkPriv();
 $app->loadModule();
-
-/* Debug信息，监控页面的执行时间和内存占用。*/
-if($config->debug)
-{
-    $timeUsed = round(_getTime() - $timeStart, 4) * 1000;
-    $memory   = round(memory_get_peak_usage() / 1024, 1);
-    $querys   = count(dao::$querys);
-    echo "<div id='debugbar'>TIME: $timeUsed ms, MEM: $memory KB, SQL: $querys.  </div>";
-    echo '<style>body{padding-bottom:50px}</style>';
-}
-
-/* 获取系统时间，微秒为单位。*/
-function _getTime()
-{
-    list($usec, $sec) = explode(" ", microtime());
-    return ((float)$usec + (float)$sec);
-}
-
-/* 保存query记录。*/
-function _saveSQL()
-{
-    global $app;
-    $sqlLog = $app->getLogRoot() . 'sql.' . date('Ymd') . '.log';
-    $fh = @fopen($sqlLog, 'a');
-    if(!$fh) return false;
-    fwrite($fh, date('Ymd H:i:s') . ": " . $app->getURI() . "\n");
-    foreach(dao::$querys as $query) fwrite($fh, "  $query\n");
-    fwrite($fh, "\n");
-    fclose($fh);
-}
-
-/* print_r。*/
-function a($var)
-{
-    echo "<xmp class='a-left'>";
-    print_r($var);
-    echo "</xmp>";
-}
+$common->printRunInfo($startTime);
--- www/upgrade.php
+++ www/upgrade.php
@@ -11,42 +11,26 @@
  */
 error_reporting(0);
 
-/* 包含必须的类文件。*/
+/* Load the framework. */
 include '../framework/router.class.php';
 include '../framework/control.class.php';
 include '../framework/model.class.php';
 include '../framework/helper.class.php';
 
-/* 实例化路由对象，加载配置，连接到数据库。*/
-$app    = router::createApp('pms', dirname(dirname(__FILE__)));
-$config = $app->loadConfig('common');
+/* Instance the app. */
+$app = router::createApp('pms', dirname(dirname(__FILE__)));
+$common = $app->loadCommon();
 
-/* 重新设置config参数，进行升级。*/
+/* Reset the config params to make sure the install program will be lauched. */
 $config->set('requestType', 'GET');
 $config->set('debug', true);
 $config->set('default.module', 'upgrade');
 $app->setDebug();
 
-/* 连接到数据库。*/
-$dbh = $app->connectDB();
-
-/* 设置客户端所使用的语言、风格。*/
-$app->setClientLang();
-$app->setClientTheme();
-
-/* 加载语言文件，加载公共模块。*/
-$lang   = $app->loadLang('common');
-$common = $app->loadCommon();
-
-/* 加载相应的lib文件，并设置超全局变量的引用。*/
-$app->loadClass('front',  $static = true);
-$app->loadClass('filter', $static = true);
-$app->setSuperVars();
-
-/* 检查是否已经是最新的版本。*/
+/* Check the installed version is the latest or not. */
 $config->installedVersion = $common->loadModel('setting')->getVersion();
 if(version_compare($config->version, $config->installedVersion) <= 0) die(header('location: index.php'));
 
-/* 处理请求，验证权限，加载相应的模块。*/
+/* Run it. */
 $app->parseRequest();
 $app->loadModule();
