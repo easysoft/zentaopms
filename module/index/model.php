ddb3aa78c62f0a54ad537dc8631798db0648ed48
wangchunsheng <chunsheng@cnezsoft.com>
2010-11-16 07:36:59 +0000

    * rewrite the index model.

--- module/index/model.php
+++ module/index/model.php
@@ -5,20 +5,31 @@
  * @copyright   Copyright 2009-2010 QingDao Nature Easy Soft Network Technology Co,LTD (www.cnezsoft.com)
  * @license     LGPL (http://www.gnu.org/licenses/lgpl.html)
  * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
- * @package     ZenTaoPMS
+ * @package     index
  * @version     $Id$
  */
 ?>
 <?php
 class indexModel extends model
 {
-    public function __construct()
+    /**
+     * Print stats.
+     * 
+     * @param  string $statusList 
+     * @param  string $stats 
+     * @access public
+     * @return void
+     */
+    public function printStats($statusList, $stats)
     {
-        parent::__construct();
-    }
-
-    function stat()
-    {
-        return get_included_files();
+        global $lang;
+        $sum = array_sum($stats);
+        $string = sprintf($lang->index->total, $sum);
+        foreach($stats as $status => $value)
+        {
+            $percent = round($value / $sum, 2) * 100 . '%';
+            $string .= strtolower($statusList[$status]) . " <strong>$value<small><i>($percent)</i></small></strong>$lang->comma ";
+        }
+        echo rtrim($string, $lang->comma) . $lang->dot;
     }
 }
