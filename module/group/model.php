96048e30e0c117383793496140133d2f44773b0e
chencongzhi520@gmail.com <azhi@cnezsoft.com>
2010-11-21 06:34:12 +0000

    * fix the bug #167.

--- module/group/model.php
+++ module/group/model.php
@@ -158,6 +158,7 @@ class groupModel extends model
             ->from(TABLE_USERGROUP)->alias('t1')
             ->leftJoin(TABLE_USER)->alias('t2')->on('t1.account = t2.account')
             ->where('`group`')->eq((int)$groupID)
+            ->andWhere('t2.deleted')->eq(0)
             ->andWhere('t2.company')->eq($this->app->company->id)
             ->fetchPairs();
     }
