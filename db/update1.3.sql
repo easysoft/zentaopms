46bd5228960ebe3c0231f4b8e70ec20b3151f9f5
wangchunsheng <chunsheng@cnezsoft.com>
2010-11-24 15:37:54 +0000

    * adjust the task table.

--- db/update1.3.sql
+++ db/update1.3.sql
@@ -17,3 +17,20 @@ ALTER TABLE `zt_dept` CHANGE `name` `name` CHAR( 60 ) CHARACTER SET utf8 COLLATE
 ALTER TABLE `zt_doc` CHANGE `title` `title` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
 ALTER TABLE `zt_story` CHANGE `title` `title` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
 ALTER TABLE `zt_task` CHANGE `name` `name` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+
+-- task table.
+ALTER TABLE `zt_task` ADD `openedBy` VARCHAR( 30 ) NOT NULL AFTER `desc` ,
+ADD `openedDate` DATETIME NOT NULL AFTER `openedBy` ,
+ADD `assignedTo` VARCHAR( 30 ) NOT NULL AFTER `openedDate` ,
+ADD `assignedDate` DATETIME NOT NULL AFTER `assignedTo` ,
+ADD `finishedBy` VARCHAR( 30 ) NOT NULL AFTER `assignedDate` ,
+ADD `finishedDate` DATETIME NOT NULL AFTER `finishedBy` ,
+ADD `canceledBy` VARCHAR( 30 ) NOT NULL AFTER `finishedDate` ,
+ADD `canceledDate` DATETIME NOT NULL AFTER `canceledBy` ,
+ADD `closedBy` VARCHAR( 30 ) NOT NULL AFTER `canceledDate` ,
+ADD `closedDate` DATETIME NOT NULL AFTER `closedBy` ;
+ALTER TABLE `zt_task` ADD `lastEditedBy` VARCHAR( 30 ) NOT NULL AFTER `closedDate` ,
+ADD `lastEditedDate` DATETIME NOT NULL AFTER `lastEditedBy` ;
+UPDATE zt_task SET assignedTo = owner ;
+ALTER TABLE `zt_task` DROP `owner`;
+ALTER TABLE `zt_task` CHANGE `status` `status` ENUM( 'wait', 'doing', 'done', 'cancel', 'closed' ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'wait';
