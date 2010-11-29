2cdd5e35dc882f1af9be30e36398d46f96b39753
chencongzhi520@gmail.com <azhi@cnezsoft.com>
2010-11-29 06:27:14 +0000

    *fix the bug#.153

--- module/doc/lang/zh-cn.php
+++ module/doc/lang/zh-cn.php
@@ -51,5 +51,6 @@ $lang->doc->types['file'] = '文件';
 $lang->doc->types['url']  = '链接';
 $lang->doc->types['text'] = '网页';
 
+$lang->doc->confirmDelete      = "您确定删除该文档吗？";
 $lang->doc->confirmDeleteLib   = " 您确定删除该文档库吗？";
 $lang->doc->errorEditSystemDoc = "系统文档库无需修改。";
