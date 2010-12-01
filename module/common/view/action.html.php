572bbe068fa19cee60d93d70554a287c1baa1ba8
wangchunsheng <chunsheng@cnezsoft.com>
2010-12-01 13:02:02 +0000

    * fix the error of switchDisplay.

--- module/common/view/action.html.php
+++ module/common/view/action.html.php
@@ -1,20 +1,23 @@
 <script language='Javascript'>
 var fold   = '<?php echo $lang->fold;?>';
 var unfold = '<?php echo $lang->unfold;?>';
-function switchHistoryDisplay(historyID,type)
+function switchChange(historyID,type)
 {
-    if(type == unfold){
+    if(type == unfold)
+    {
         $('#switchButton' + historyID).val(fold);
-        $('#historyBox' + historyID).show();
+        $('#changeBox' + historyID).show();
     }
-    else{
+    else
+    {
         $('#switchButton' + historyID).val(unfold);
-        $('#historyBox' + historyID).hide();
+        $('#changeBox' + historyID).hide();
     }
 }
 </script>
 <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
 <script src='<?php echo $jsRoot;?>jquery/reverseorder/raw.js' type='text/javascript'></script>
+
 <?php if(!isset($actionTheme)) $actionTheme = 'fieldset';?>
 <?php if($actionTheme == 'fieldset'):?>
 <div id='actionbox'>
@@ -40,16 +43,16 @@ function switchHistoryDisplay(historyID,type)
       if(isset($users[$action->actor])) $action->actor = $users[$action->actor];
       if(strpos($action->actor, ':') !== false) $action->actor = substr($action->actor, strpos($action->actor, ':') + 1);
       ?>
-      <span><?php $this->action->printAction($action);?>
-      <?php if(!empty($action->history)) echo html::commonButton($lang->unfold, "id=switchButton$i onclick=switchHistoryDisplay($i,this.value)");?>
+      <span>
+        <?php $this->action->printAction($action);?>
+        <?php if(!empty($action->history)) echo html::commonButton($lang->unfold, "id=switchButton$i onclick=switchChange($i,this.value)");?>
+      </span>
       <?php if(!empty($action->comment) or !empty($action->history)):?>
       <?php if(!empty($action->comment)) echo "<div class='history'>";?>
-        <?php echo "<span id='historyBox$i' class='hidden'>";?>
-          <div class='changes'>
-          <?php echo $this->action->printChanges($action->objectType, $action->history);?>
-          <?php if($action->comment) echo '<br />';?>
-          </div>
-        </span>
+        <div class='changes hidden' id='changeBox<?php echo $i;?>'>
+        <?php echo $this->action->printChanges($action->objectType, $action->history);?>
+        <?php if($action->comment) echo '<br />';?>
+        </div>
         <?php if($action->comment) echo nl2br($action->comment);?>
       <?php if(!empty($action->comment)) echo "</div>";?>
       <?php endif;?>
