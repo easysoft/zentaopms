11761daf19ff24e9db954a47d3b400d7610bf9af
wangchunsheng <chunsheng@cnezsoft.com>
2010-12-02 06:12:27 +0000

    * move the config code to the beginning.

--- bin/computeburn.php
+++ bin/computeburn.php
@@ -2,6 +2,10 @@
 <?php
 include dirname(dirname(__FILE__)) . '/lib/api/api.class.php';
 
+$config->zentao->root     = "";    // 禅道访问的完整路径，包括后面的斜线。比如http://pms.zentao.net/
+$config->zentao->account  = "";    // 可以访问禅道的帐号，需要有超级model调用接口的访问权限。
+$config->zentao->password = "";    // 密码。
+
 class computeburn
 {
     public $config;    // the config var.
@@ -39,10 +43,6 @@ class computeburn
         $this->zentao = new ztclient($this->config->zentao->root, $this->config->zentao->account, $this->config->zentao->password);
     }
 }
-
-$config->zentao->root     = "";    // 禅道访问的完整路径，包括后面的斜线。比如http://pms.zentao.net/
-$config->zentao->account  = "";    // 可以访问禅道的帐号，需要有超级model调用接口的访问权限。
-$config->zentao->password = "";    // 密码。
 
 $computeburn = new computeburn($config);
 $computeburn->run();
