1625cfe73c77474af0a7a13202789cd9b9c3571a
wangchunsheng <chunsheng@cnezsoft.com>
2010-12-02 06:48:17 +0000

    * fix the bug #137.

--- module/user/model.php
+++ module/user/model.php
@@ -156,9 +156,11 @@ class userModel extends model
     {
         if(!$this->checkPassword()) return;
 
+        $oldUser = $this->getById($userID);
+
         $userID = (int)$userID;
         $user = fixer::input('post')
-            ->setIF(isset($_POST['join']) and $this->post->join == '', 'join', '0000-00-00')
+            ->setDefault('join', '0000-00-00')
             ->setIF($this->post->password1 != false, 'password', md5($this->post->password1))
             ->remove('password1, password2')
             ->specialChars('msn,qq,yahoo,gtalk,wangwang,mobile,phone,address,zipcode')
@@ -172,6 +174,17 @@ class userModel extends model
             ->checkIF($this->post->email != false, 'email', 'email')
             ->where('id')->eq((int)$userID)
             ->exec();
+
+        /* If account changed, update the privilege. */
+        if($this->post->account != $oldUser->account)
+        {
+            $this->dao->update(TABLE_USERGROUP)->set('account')->eq($this->post->account)->where('account')->eq($oldUser->account)->exec();
+            if(strpos($this->app->company->admins, ',' . $oldUser->account . ',') !== false)
+            {
+                $admins = ',' . $this->post->account . ',';
+                $this->dao->update(TABLE_COMPANY)->set('admins')->eq($admins)->where('id')->eq($this->app->company->id)->exec(false);
+            }
+        }
     }
 
     /**
