582e36172d3fce505284ecddad222c02b6a33998
wangchunsheng <chunsheng@cnezsoft.com>
2011-02-17 14:38:51 +0000

    * change copyright time.

--- www/index.php
+++ www/index.php
@@ -4,7 +4,7 @@
  *
  * All request should be routed by this router.
  *
- * @copyright   Copyright 2009-2010 QingDao Nature Easy Soft Network Technology Co,LTD (www.cnezsoft.com)
+ * @copyright   Copyright 2009-2011 QingDao Nature Easy Soft Network Technology Co,LTD (www.cnezsoft.com)
  * @license     LGPL (http://www.gnu.org/licenses/lgpl.html)
  * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
  * @package     ZenTaoPMS
