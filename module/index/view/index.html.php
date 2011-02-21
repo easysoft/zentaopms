e3a2f4686af71f56dc63d6cc977a8c5272517a49
wangchunsheng <chunsheng@cnezsoft.com>
2011-02-21 13:27:48 +0000

    * rewrite the index page useing table layout.

--- module/index/view/index.html.php
+++ module/index/view/index.html.php
@@ -11,68 +11,61 @@
 ?>
 <?php include '../../common/view/header.html.php';?>
 <?php include '../../common/view/jquerytools.html.php';?>
-<script language='Javascript'>
-$(function() 
-{ 
-    $("#projectbox").tabs("#projectbox .panes > div", {tabs: '.tabs a'});
-    $("#mybox").tabs("#mybox .panes > div", {tabs: '.tabs a'});
-});
-</script>
-<div class="yui-d0 yui-t6">  
-  <div class='yui-main'>
-    <div class='yui-b'>
-      <div class='yui-g'>
-        <table class='table-1' id='projectbox' height='240'>
-          <caption><?php echo $lang->index->projects;?></caption>
-          <tr>
-            <td class='tabs' width='220'><?php foreach($projects as $project) echo "<a href='#' title='$project->name'>$project->name</a>";?></td>
-            <td class='panes'>
-              <?php foreach($projects as $key => $project):?>
-              <div class='a-center'>
-                <?php
-                echo $burns[$project->id];
-                echo html::a($this->createLink('project', 'browse', "projectid=$project->id"), $project->name);
-                common::printLink('project', 'burn', "projectID=$project->id", $lang->project->largeBurnChart);
-                common::printLink('project', 'computeBurn', 'reload=yes', $lang->project->computeBurn, 'hiddenwin');
-                ?>
-              </div>
-              <?php endforeach;?>
-            </td>
-          </tr>
-        </table>
-      </div>
-      <div class='yui-g'>
-        <div class='yui-u first'>
-          <table class='table-1' height='200'>
-            <caption><?php echo $lang->index->stats;?></caption>
-            <tr>
-              <td><?php echo $lang->index->products;?></td>
-              <td><?php $this->index->printStats($lang->product->statusList, $stats['product']);?></td>
-            </tr>
-            <tr>
-              <td><?php echo $lang->index->projects;?></td>
-              <td><?php $this->index->printStats($lang->project->statusList, $stats['project']);?></td>
-            </tr>
-            <tr>
-              <td><?php echo $lang->index->tasks;?></td>
-              <td><?php $this->index->printStats($lang->task->statusList, $stats['task']);?></td>
-            </tr>
-            <tr>
-              <td><?php echo $lang->index->stories;?></td>
-              <td><?php $this->index->printStats($lang->story->statusList, $stats['story']);?></td>
-            </tr>
-            <tr>
-              <td><?php echo $lang->index->bugs;?></td>
-              <td><?php $this->index->printStats($lang->bug->statusList, $stats['bug']);?></td>
-            </tr>
-            <tr>
-              <td><?php echo $lang->index->todos;?></td>
-              <td><?php $this->index->printStats($lang->todo->statusList, $stats['todo']);?></td>
-            </tr>
-          </table>
-        </div>
-        <div class='yui-u'>
-          <table class='table-1 fixed' id='mybox' height='200'>
+<div class='outer' style='margin:10px; text-align:center'>
+<table class='cont' align='center'>
+  <tr valign='top'>
+    <td>
+      <table class='table-1' id='projectbox' height='240'>
+        <caption><?php echo $lang->index->projects;?></caption>
+        <tr>
+          <td class='tabs' width='220'><?php foreach($projects as $project) echo "<a href='#' title='$project->name'>$project->name</a>";?></td>
+          <td class='panes'>
+            <?php foreach($projects as $key => $project):?>
+            <div class='a-center'>
+              <?php
+              echo $burns[$project->id];
+              echo html::a($this->createLink('project', 'browse', "projectid=$project->id"), $project->name);
+              common::printLink('project', 'burn', "projectID=$project->id", $lang->project->largeBurnChart);
+              common::printLink('project', 'computeBurn', 'reload=yes', $lang->project->computeBurn, 'hiddenwin');
+              ?>
+            </div>
+            <?php endforeach;?>
+          </td>
+        </tr>
+      </table>
+      <table>
+        <tr>
+          <td width='50%'>
+            <table class='table-1' height='200'>
+              <caption><?php echo $lang->index->stats;?></caption>
+              <tr>
+                <td><?php echo $lang->index->products;?></td>
+                <td><?php $this->index->printStats($lang->product->statusList, $stats['product']);?></td>
+              </tr>
+              <tr>
+                <td><?php echo $lang->index->projects;?></td>
+                <td><?php $this->index->printStats($lang->project->statusList, $stats['project']);?></td>
+              </tr>
+              <tr>
+                <td><?php echo $lang->index->tasks;?></td>
+                <td><?php $this->index->printStats($lang->task->statusList, $stats['task']);?></td>
+              </tr>
+              <tr>
+                <td><?php echo $lang->index->stories;?></td>
+                <td><?php $this->index->printStats($lang->story->statusList, $stats['story']);?></td>
+              </tr>
+              <tr>
+                <td><?php echo $lang->index->bugs;?></td>
+                <td><?php $this->index->printStats($lang->bug->statusList, $stats['bug']);?></td>
+              </tr>
+              <tr>
+                <td><?php echo $lang->index->todos;?></td>
+                <td><?php $this->index->printStats($lang->todo->statusList, $stats['todo']);?></td>
+              </tr>
+            </table>
+          </td>
+          <td>
+            <table class='table-1 fixed' id='mybox' height='200'>
             <caption><?php echo $lang->index->my;?></caption>
             <tr>
               <td class='tabs' width='100'>
@@ -112,24 +105,26 @@ $(function()
               <td width='10' style='border-left:none'></td>
             </tr>
           </table>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class='yui-b'>
-    <table class='table-1 colored'>
-      <caption><?php echo $lang->index->latest;?></caption>
-      <?php 
-      foreach($actions as $action)
-      {
-          $user = isset($users[$action->actor]) ? $users[$action->actor] : $action->actor;
-          echo "<tr><td>";
-          printf($lang->index->action, $action->date, $user, $action->actionLabel, $action->objectLabel, $action->objectLink);
-          echo "</td></tr>";
-      }
-      ?>
-    </table>
-  </div>
+        </td>
+      </tr>
+      </table>
+    </td>
+    <td width='300'>
+      <table class='table-1 colored'>
+        <caption><?php echo $lang->index->latest;?></caption>
+        <?php 
+        foreach($actions as $action)
+        {
+            $user = isset($users[$action->actor]) ? $users[$action->actor] : $action->actor;
+            echo "<tr><td>";
+            printf($lang->index->action, $action->date, $user, $action->actionLabel, $action->objectLabel, $action->objectLink);
+            echo "</td></tr>";
+        }
+        ?>
+      </table>
+    </td>
+  </tr>
+</table>
 </div>
 <script language='Javascript'><?php for($i = 1; $i <= count($projects); $i ++) echo "createChart$i();"; ?></script>
 <?php include '../../common/view/footer.html.php';?>
