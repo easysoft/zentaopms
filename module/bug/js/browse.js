5812a7e6d79bfbcca2a426296e818874f425fd67
wangchunsheng <chunsheng@cnezsoft.com>
2011-02-24 14:01:06 +0000

    * fix the error when click the search link.  * slite the js from browseCustom.html.php.

--- module/bug/js/browse.js
+++ module/bug/js/browse.js
@@ -2,6 +2,7 @@
 function browseByModule(active)
 {
     $('#treebox').removeClass('hidden');
+    $('.divider').removeClass('hidden');
     $('#bymoduleTab').addClass('active');
     $('#querybox').addClass('hidden');
     $('#' + active + 'Tab').removeClass('active');
@@ -10,6 +11,7 @@ function browseByModule(active)
 function browseBySearch(active)
 {
     $('#treebox').addClass('hidden');
+    $('.divider').addClass('hidden');
     $('#querybox').removeClass('hidden');
     $('#bymoduleTab').removeClass('active');
     $('#' + active + 'Tab').removeClass('active');
@@ -21,4 +23,10 @@ $(document).ready(function()
     $("a.iframe").colorbox({width:640, height:420, iframe:true, transition:'none'});
     $('#' + browseType + 'Tab').addClass('active'); 
     $('#module' + moduleID).addClass('active'); 
+
+    /* If customed and the browse is ie6, remove the ie6.css. */
+    if(customed && $.browser.msie && Math.floor(parseInt($.browser.version)) == 6)
+    {
+        $("#browsecss").attr('href', '');
+    }
 });
