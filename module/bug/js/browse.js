29f715e34e62194ddba8a858c0e285639cfb0983
wangchunsheng <chunsheng@cnezsoft.com>
2011-02-25 08:11:15 +0000

    * rewrite the customFields page code.

--- module/bug/js/browse.js
+++ module/bug/js/browse.js
@@ -20,7 +20,7 @@ function browseBySearch(active)
 
 $(document).ready(function()
 {
-    $("a.iframe").colorbox({width:640, height:420, iframe:true, transition:'none'});
+    $("a.iframe").colorbox({width:600, height:340, iframe:true, transition:'none'});
     $('#' + browseType + 'Tab').addClass('active'); 
     $('#module' + moduleID).addClass('active'); 
 
--- module/bug/view/customfields.html.php
+++ module/bug/view/customfields.html.php
@@ -11,50 +11,38 @@
  */
 ?>
 <?php include '../../common/view/header.lite.html.php';?>
-<style>
-body{background:white}
-.button-c {width:60px}
-</style>
-<script language='Javascript'>
-function restoreDefault()
-{
-    $('#customFields option').remove();
-    $('#defaultFields option').clone().appendTo('#customFields');
-}
-</script>
-<div id='yui-d0' style='margin-top:20px'>
-  <form method='post'>
-    <table class='table-1'> 
-      <caption class='caption-tl'><?php echo $lang->bug->customFields;?></caption>
-      <tr class='colhead'>
-        <th><?php echo $lang->bug->lblAllFields;?></th>
-        <th></th>
-        <th><?php echo $lang->bug->lblCustomFields;?></th>
-        <th></th>
-      </tr>  
-      <tr>
-        <td>
-          <?php 
-          echo html::select('allFields[]', $allFields, '', 'class=select-2 size=10 multiple');
-          echo html::select('defaultFields[]', $defaultFields, '', 'class=hidden');
-          ?>
-        </td>
-        <td>
-          <?php
-          echo html::commonButton('>', "onclick=\"addItem('allFields', 'customFields')\"") . '<br />';
-          echo html::commonButton('<', "onclick=delItem('customFields')")  . '<br />';
-          ?>
-        </td>
-        <td><?php echo html::select('customFields[]', $customFields, '', 'class=select-2 size=10 multiple');?></td>
-        <td>
-          <?php
-          echo html::commonButton('+', "onclick=upItem('customFields')")  . '<br />';
-          echo html::commonButton('-', "onclick=downItem('customFields')")  . '<br />';
-          echo html::commonButton($lang->bug->restoreDefault, "onclick=restoreDefault()")  . '<br />';
-          ?>
-        </td>
-      </tr>  
-      <tr><td colspan='4' class='a-center'><?php echo html::submitButton('', 'onclick=selectItem("customFields")');?></td></tr>
-    </table>
-  </form>
-</div>  
+<form method='post' class='mt-20px'>
+  <table class='table-4' align='center'> 
+    <caption class='caption-tl'><?php echo $lang->bug->customFields;?></caption>
+    <tr class='colhead'>
+      <th><?php echo $lang->bug->lblAllFields;?></th>
+      <th></th>
+      <th><?php echo $lang->bug->lblCustomFields;?></th>
+      <th></th>
+    </tr>  
+    <tr>
+      <td>
+        <?php 
+        echo html::select('allFields[]', $allFields, '', 'class=select-2 size=10 multiple');
+        echo html::select('defaultFields[]', $defaultFields, '', 'class=hidden');
+        ?>
+      </td>
+      <td>
+        <?php
+        echo html::commonButton('>', "onclick=\"addItem('allFields', 'customFields')\"") . '<br />';
+        echo html::commonButton('<', "onclick=delItem('customFields')")  . '<br />';
+        ?>
+      </td>
+      <td><?php echo html::select('customFields[]', $customFields, '', 'class=select-2 size=10 multiple');?></td>
+      <td>
+        <?php
+        echo html::commonButton('+', "onclick=upItem('customFields')")  . '<br />';
+        echo html::commonButton('-', "onclick=downItem('customFields')")  . '<br />';
+        echo html::commonButton($lang->bug->restoreDefault, "onclick=restoreDefault()")  . '<br />';
+        ?>
+      </td>
+    </tr>  
+    <tr><td colspan='4' class='a-center'><?php echo html::submitButton('', 'onclick=selectItem("customFields")');?></td></tr>
+  </table>
+</form>
+<?php include '../../common/view/footer.lite.html.php';?>
