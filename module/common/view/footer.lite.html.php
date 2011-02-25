7dd258469cc8c6cf0bd2297f40b3792d9bcf80d8
wangchunsheng <chunsheng@cnezsoft.com>
2011-02-25 08:09:51 +0000

    * remove the divider in footer.lite.

--- module/common/view/footer.lite.html.php
+++ module/common/view/footer.lite.html.php
@@ -1,6 +1,5 @@
 </div>
 <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
-<div id='divider'></div>
 <iframe frameborder='0' name='hiddenwin' scrolling='no' class='<?php $config->debug ? print("debugwin") : print('hidden')?>'></iframe>
 <script laguage='Javascript'>
 $().ready(function(){setDebugWin('white')})
