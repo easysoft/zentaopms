3b9edd50aa29a500d576553703e48dd00b6bdc90
wangyidong <wangyidong@cnezsoft.com>
2011-03-09 05:55:30 +0000

    * change for task #359

--- module/project/js/task.js
+++ module/project/js/task.js
@@ -3,5 +3,8 @@ function browseBySearch(active)
 {
     $('.divider').addClass('hidden');
     $('#querybox').removeClass('hidden');
+    $('#querybox').removeClass('hidden');
+    $('#bymoduleTab').removeClass('active');
     $('#' + active + 'Tab').removeClass('active');
+    $('#bysearchTab').addClass('active');
 }
