8c13b1f476e3a5b11836b795da14aee918c7ea87
wangyidong <wangyidong@cnezsoft.com>
2011-03-13 00:59:00 +0000

    * change for search form

--- module/doc/control.php
+++ module/doc/control.php
@@ -125,6 +125,7 @@ class doc extends control
         $this->config->doc->search['params']['product']['values']       = array(''=>'') + $this->product->getPairs() + array('all'=>$this->lang->doc->allProduct);
         $this->config->doc->search['params']['project']['values']       = array(''=>'') + $this->project->getPairs() + array('all'=>$this->lang->doc->allProject);
         $this->config->doc->search['params']['lib']['values']           = array(''=>'') + $this->libs;
+        $this->config->doc->search['params']['type']['values']           = array(''=>'') + $this->config->doc->search['params']['type']['values'];
                 /* Get the modules. */
         if($libID == 'product' or $libID == 'project')
         {
