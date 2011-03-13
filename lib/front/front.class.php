27a94efe3da9a057008b07232e6558bece8f1280
wangchunsheng <chunsheng@cnezsoft.com>
2011-03-13 06:26:23 +0000

    * revert to the prev version.

--- lib/front/front.class.php
+++ lib/front/front.class.php
@@ -703,7 +703,7 @@ EOT;
     static public function reload($window = 'self')
     {
         $js  = self::start();
-        $js .= "$window.location.reload()";
+        $js .=  "$window.location.href=$window.location.href";
         $js .= self::end();
         return $js;
     }
