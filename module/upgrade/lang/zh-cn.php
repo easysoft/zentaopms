74d8115807cd277cc5fc683d61dcd23b03a5ed79
wangyidong <wangyidong@cnezsoft.com>
2011-03-13 01:40:10 +0000

    * change for upgade

--- module/upgrade/lang/zh-cn.php
+++ module/upgrade/lang/zh-cn.php
@@ -47,3 +47,4 @@ $lang->upgrade->fromVersions['1_0_1']     = '1.0.1';
 $lang->upgrade->fromVersions['1_1']       = '1.1';
 $lang->upgrade->fromVersions['1_2']       = '1.2';
 $lang->upgrade->fromVersions['1_3']       = '1.3';
+$lang->upgrade->fromVersions['1_4']       = '1.4';
