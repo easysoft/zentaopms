a52be35d05bcdce5d06d36254c9cd9058022a4fd
wenjie@cnezsoft.com <wenjie@cnezsoft.com>
2011-04-10 01:37:23 +0000

    * add a logic to include footer.hook.*

--- module/common/view/footer.html.php
+++ module/common/view/footer.html.php
@@ -1,5 +1,17 @@
 </div>
 <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
+<?php 
+    $extPath     = dirname(dirname(dirname(realpath($viewFile)))) . '/common/ext/view/';
+    $extViewFile = $extPath . 'footer.hook.*';
+    $names = glob($extViewFile);
+    if(!empty($names))
+    {
+        foreach($names as $name)
+        {
+            include $name;
+        }
+    }
+?>
 <div id='divider'></div>
 <iframe frameborder='0' name='hiddenwin' id='hiddenwin' scrolling='no' class='<?php $config->debug ? print("debugwin") : print('hidden')?>'></iframe>
 <div id='footer'>
