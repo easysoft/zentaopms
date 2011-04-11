17d01b82ce099c2482a0217744b2cbe00493be98
shiyangyangwork@yahoo.cn <shiyangyang@cnezsoft.com>
2011-04-11 09:02:22 +0000

    * UpDate it

--- module/bug/view/create.html.php
+++ module/bug/view/create.html.php
@@ -26,47 +26,16 @@ userList = "<?php echo join(',', array_keys($users));?>".split(',');
         <?php echo html::select('product', $products, $productID, "onchange='loadAll(this.value)' class='select-2'");?>
         <span id='moduleIdBox'><?php echo html::select('module', $moduleOptionMenu, $moduleID, "onchange='setAssignedTo()' class='select-3'");?></span>
       </td>
-    </tr>  
-    <tr>
-      <th class='rowhead'><?php echo $lang->bug->lblProjectAndTask;?></th>
-      <td>
-        <span id='projectIdBox'><?php echo html::select('project', $projects, $projectID, 'onchange=loadProjectRelated(this.value)');?></span>
-        <span id='taskIdBox'><?php echo html::select('task', $tasks, $taskID);?></span>
-      </td>
-    </tr>
-    <tr>
-      <th class='rowhead'><?php echo $lang->bug->lblStory;?></th>
-      <td>
-        <span id='storyIdBox'><?php echo html::select('story', $stories, $storyID);?></span>
-      </td>
-    </tr>  
-    <tr>
+     </tr>  
+     <tr>
       <th class='rowhead'><?php echo $lang->bug->openedBuild;?></th>
       <td>
         <span id='buildBox'><?php echo html::select('openedBuild[]', $builds, $buildID, 'size=3 multiple=multiple class=select-3');?></span>
       </td>
     </tr>
-    <tr>
-      <th class='rowhead'><?php echo $lang->bug->lblTypeAndSeverity;?></th>
-      <td> 
-        <?php echo html::select('type', $lang->bug->typeList, $type, 'class=select-2');?> 
-        <?php echo html::select('severity', $lang->bug->severityList, $severity, 'class=select-2');?>
-      </td>
-    </tr>
-    <tr>
-      <th class='rowhead'><nobr><?php echo $lang->bug->lblSystemBrowserAndHardware;?></nobr></th>
-      <td>
-        <?php echo html::select('os', $lang->bug->osList, $os, 'class=select-2');?>
-        <?php echo html::select('browser', $lang->bug->browserList, $browser, 'class=select-2');?>
-      </td>
-    </tr>
-    <tr>
+      <tr>
       <th class='rowhead'><nobr><?php echo $lang->bug->lblAssignedTo;?></nobr></th>
       <td> <?php echo html::select('assignedTo', $users, $assignedTo, 'class=select-3');?></td>
-    </tr>
-    <tr>
-      <th class='rowhead'><nobr><?php echo $lang->bug->lblMailto;?></nobr></th>
-      <td> <?php echo html::input('mailto', $mailto, 'class=text-1');?> </td>
     </tr>
     <tr>
       <th class='rowhead'><?php echo $lang->bug->title;?></th>
@@ -84,10 +53,41 @@ userList = "<?php echo join(',', array_keys($users));?>".split(',');
       </td>
     </tr>  
     <tr>
+      <th class='rowhead'><?php echo $lang->bug->lblProjectAndTask;?></th>
+      <td>
+        <span id='projectIdBox'><?php echo html::select('project', $projects, $projectID, 'onchange=loadProjectRelated(this.value)');?></span>
+        <span id='taskIdBox'><?php echo html::select('task', $tasks, $taskID);?></span>
+      </td>
+     </tr>
+
+      <tr>
+      <th class='rowhead'><?php echo $lang->bug->lblStory;?></th>
+      <td>
+        <span id='storyIdBox'><?php echo html::select('story', $stories, $storyID);?></span>
+      </td>
+    </tr>  
+      <tr>
+      <th class='rowhead'><?php echo $lang->bug->lblTypeAndSeverity;?></th>
+      <td> 
+        <?php echo html::select('type', $lang->bug->typeList, $type, 'class=select-2');?> 
+        <?php echo html::select('severity', $lang->bug->severityList, $severity, 'class=select-2');?>
+      </td>
+    </tr>
+    <tr>
+      <th class='rowhead'><nobr><?php echo $lang->bug->lblSystemBrowserAndHardware;?></nobr></th>
+      <td>
+        <?php echo html::select('os', $lang->bug->osList, $os, 'class=select-2');?>
+        <?php echo html::select('browser', $lang->bug->browserList, $browser, 'class=select-2');?>
+      </td>
+    </tr>
+       <tr>
+      <th class='rowhead'><nobr><?php echo $lang->bug->lblMailto;?></nobr></th>
+      <td> <?php echo html::input('mailto', $mailto, 'class=text-1');?> </td>
+    </tr>
+    <tr>
       <th class='rowhead'><?php echo $lang->bug->keywords;?></th>
       <td><?php echo html::input('keywords', $keywords, "class='text-1'");?></td>
-    </tr>  
-    <tr>
+    </tr>    <tr>
       <th class='rowhead'><?php echo $lang->bug->files;?></th>
       <td><?php echo $this->fetch('file', 'buildform', 'fileCount=2&percent=0.85');?></td>
     </tr>  
