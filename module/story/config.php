5ee2a83d57e2c3fa47c0e34e2383e350acae418e
shiyangyangwork@yahoo.cn <shiyangyang@cnezsoft.com>
2011-04-11 10:07:15 +0000

    * UpDate it.

--- module/story/config.php
+++ module/story/config.php
@@ -1,6 +1,6 @@
 <?php
-$config->story->create->requiredFields = 'estimate,title';
-$config->story->edit->requiredFields   = 'estimate,title';
+$config->story->create->requiredFields = 'title';
+$config->story->edit->requiredFields   = 'title';
 $config->story->change->requiredFields = 'title';
 $config->story->close->requiredFields  = 'closedReason';
 $config->story->review->requiredFields = 'assignedTo,reviewedBy,result';
