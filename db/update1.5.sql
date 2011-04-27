bc72e2f4a4dafb61c6728cfad550d017edf27159
wangchunsheng <chunsheng@cnezsoft.com>
2011-04-27 05:20:39 +0000

    + add the extension table.

--- db/update1.5.sql
+++ db/update1.5.sql
@@ -1,1 +1,23 @@
 ALTER TABLE `zt_storySpec` ADD `verify` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+
+CREATE TABLE IF NOT EXISTS `zt_extension` (
+  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `company` mediumint(8) unsigned NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `code` varchar(30) NOT NULL,
+  `version` varchar(50) NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `desc` text NOT NULL,
+  `license` text NOT NULL,
+  `site` varchar(150) NOT NULL,
+  `zentaoVersion` varchar(100) NOT NULL,
+  `installedTime` datetime NOT NULL,
+  `dirs` text NOT NULL,
+  `files` text NOT NULL,
+  `status` varchar(20) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `code` (`code`),
+  KEY `name` (`name`),
+  KEY `addedTime` (`installedTime`),
+  KEY `company` (`company`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
