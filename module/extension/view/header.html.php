b2507509b25832cb63bc53205a7efb0722ab57ff
wangchunsheng <chunsheng@cnezsoft.com>
2011-04-30 05:55:58 +0000

    * add the div of f-left.

--- module/extension/view/header.html.php
+++ module/extension/view/header.html.php
@@ -1,12 +1,14 @@
 <?php include '../../common/view/header.html.php';?>
 <?php include '../../common/view/tablesorter.html.php';?>
 <div id='featurebar'>
+  <div class='f-left'>
   <?php 
+  echo '<span id="obtain">  '   . html::a(inlink('obtain'), $lang->extension->obtain) . '</span>';
+  echo '<span id="upload" >'    . html::a(inlink('upload'),   $lang->extension->upload, '', "class='iframe'") . '</span>';
   echo '<span id="installed">'  . html::a(inlink('browse', "type=installed"),   $lang->extension->installed)   . '</span>';
   echo '<span id="deactivated">'. html::a(inlink('browse', "type=deactivated"), $lang->extension->deactivated) . '</span>';
   echo '<span id="available">'  . html::a(inlink('browse', "type=available"),   $lang->extension->available )  . '</span>';
-  echo '<span id="obtain">  '   . html::a(inlink('obtain'), $lang->extension->obtain) . '</span>';
-  echo '<span id="upload" >'    . html::a(inlink('upload'),   $lang->extension->upload, '', "class='iframe'") . '</span>';
   ?>
+  </div>
   <script>$('#<?php echo $tab;?>').addClass('active')</script>
 </div>
