9f89da9211c9831c144bc916d0f1d1f4f68ce455
wangchunsheng <chunsheng@cnezsoft.com>
2011-05-02 06:07:19 +0000

    - remove the css and event of tfoot.

--- module/common/view/colorize.html.php
+++ module/common/view/colorize.html.php
@@ -1,3 +1,10 @@
 <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
 <script src='<?php echo $jsRoot;?>jquery/colorize/full.js' type='text/javascript'></script>
-<script language='javascript'>$(function(){$('.colored').colorize();});</script>
+<script language='javascript'>
+$(function()
+{
+    $('.colored').colorize();
+    $('tfoot td').css('background', 'white').unbind('click').unbind('hover');
+}
+);
+</script>
