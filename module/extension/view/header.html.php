d4ca83e416599468cdbd82163a36abbd52117a19
wangchunsheng <chunsheng@cnezsoft.com>
2011-05-02 01:43:55 +0000

    * adjust the default page.

--- module/extension/view/header.html.php
+++ module/extension/view/header.html.php
@@ -3,11 +3,11 @@
 <div id='featurebar'>
   <div class='f-left'>
   <?php 
-  echo '<span id="obtain">  '   . html::a(inlink('obtain'), $lang->extension->obtain) . '</span>';
-  echo '<span id="upload" >'    . html::a(inlink('upload'),   $lang->extension->upload, '', "class='iframe'") . '</span>';
   echo '<span id="installed">'  . html::a(inlink('browse', "type=installed"),   $lang->extension->installed)   . '</span>';
   echo '<span id="deactivated">'. html::a(inlink('browse', "type=deactivated"), $lang->extension->deactivated) . '</span>';
   echo '<span id="available">'  . html::a(inlink('browse', "type=available"),   $lang->extension->available )  . '</span>';
+  echo '<span id="obtain">  '   . html::a(inlink('obtain'), $lang->extension->obtain) . '</span>';
+  echo '<span id="upload" >'    . html::a(inlink('upload'), $lang->extension->upload, '', "class='iframe'") . '</span>';
   ?>
   </div>
   <script>$('#<?php echo $tab;?>').addClass('active')</script>
