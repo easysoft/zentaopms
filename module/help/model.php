554ff32e2b01178fd3f0212b3e06854bb2601a38
wangyidong <wangyidong@cnezsoft.com>
2011-05-16 04:20:43 +0000

    + add model.php for no model of module.

---
+++ module/help/model.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The model file of help module of ZenTaoCMS.
+ *
+ * @copyright   Copyright 2009-2010 QingDao Nature Easy Soft Network Technology Co,LTD (www.cnezsoft.com)
+ * @license     LGPL (http://www.gnu.org/licenses/lgpl.html)
+ * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
+ * @package     help
+ * @version     $Id$
+ * @link        http://www.zentao.net
+ */
+class helpModel extends model
+{
+}
+
