18992836c574048bdc789eebbfda151e2653313b
wangchunsheng <chunsheng@cnezsoft.com>
2011-05-20 06:22:37 +0000

    * keep the same with the zentaophp framework.

--- lib/front/front.class.php
+++ lib/front/front.class.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * The front class file of ZenTaoPMS.
+ * The helper class file of ZenTaoPHP framework.
  *
- * @copyright   Copyright 2009-2011 QingDao Nature Easy Soft Network Technology Co,LTD (www.cnezsoft.com)
- * @license     LGPL (http://www.gnu.org/licenses/lgpl.html)
- * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
- * @package     framework
- * @version     $Id$
- * @link        http://www.zentao.net
+ * The author disclaims copyright to this source code.  In place of
+ * a legal notice, here is a blessing:
+ * 
+ *  May you do good and not evil.
+ *  May you find forgiveness for yourself and forgive others.
+ *  May you share freely, never taking more than you give.
  */
 
 /**
