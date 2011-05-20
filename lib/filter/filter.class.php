8cd2c3b54140d4f3234486d69e71018dd3a1c1f5
wangchunsheng <chunsheng@cnezsoft.com>
2011-05-20 06:28:24 +0000

    * change the copyright.

--- lib/filter/filter.class.php
+++ lib/filter/filter.class.php
@@ -1,14 +1,15 @@
 <?php
 /**
- * The validater and fixer class file of ZenTaoPMS.
+ * The validater and fixer class file of ZenTaoPHP framework.
  *
- * @copyright   Copyright 2009-2011 QingDao Nature Easy Soft Network Technology Co,LTD (www.cnezsoft.com)
- * @license     LGPL (http://www.gnu.org/licenses/lgpl.html)
- * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
- * @package     framework
- * @version     $Id: filter.class.php 134 2010-09-11 07:24:27Z wwccss $
- * @link        http://www.zentao.net
+ * The author disclaims copyright to this source code.  In place of
+ * a legal notice, here is a blessing:
+ * 
+ *  May you do good and not evil.
+ *  May you find forgiveness for yourself and forgive others.
+ *  May you share freely, never taking more than you give.
  */
+
 /**
  * The valida clas, checking datas by rules.
  * 
--- lib/pager/pager.class.php
+++ lib/pager/pager.class.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * The pager class file of ZenTaoPMS.
+ * The pager class file of ZenTaoPHP framework.
  *
- * @copyright   Copyright 2009-2011 QingDao Nature Easy Soft Network Technology Co,LTD (www.cnezsoft.com)
- * @license     LGPL (http://www.gnu.org/licenses/lgpl.html)
- * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
- * @package     framework
- * @version     $Id: pager.class.php 134 2010-09-11 07:24:27Z wwccss $
- * @link        http://www.zentao.net
+ * The author disclaims copyright to this source code.  In place of
+ * a legal notice, here is a blessing:
+ * 
+ *  May you do good and not evil.
+ *  May you find forgiveness for yourself and forgive others.
+ *  May you share freely, never taking more than you give.
  */
 /**
  * Pager class.
