e8e725ef98ff03b59c7c014107fa26671f5def04
wangchunsheng <chunsheng@cnezsoft.com>
2011-06-21 06:27:31 +0000

    + add the tests example of bug.

---
+++ module/bug/tests/setmenu.m.php
@@ -0,0 +1,11 @@
+#!/usr/bin/env php
+<?php
+/**
+ * 测试setMenu.
+ */
+include '../../../test/init.php';
+include '../model.php';
+$app->user->account = 'test';
+$bug = new bugModel();
+$bug->setMenu(array(1, 2, 3), 1);
+print_r($lang->bug->menu->product);
