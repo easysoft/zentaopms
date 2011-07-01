9e8ac1972969e90acd2829d64a6cc4a2d45fb9a8
wangchunsheng <chunsheng@cnezsoft.com>
2011-07-01 05:47:12 +0000

    * add more note for email errors.

--- lib/phpmailer/language/phpmailer.lang-zh-cn.php
+++ lib/phpmailer/language/phpmailer.lang-zh-cn.php
@@ -5,8 +5,8 @@
 * @author liqwei <liqwei@liqwei.com>
 */
 
-$PHPMAILER_LANG['authenticate'] = 'SMTP 错误：登录失败。';
-$PHPMAILER_LANG['connect_host'] = 'SMTP 错误：无法连接到 SMTP 主机。';
+$PHPMAILER_LANG['authenticate'] = 'SMTP 错误：登录失败。\n请检查您设置的用户名密码是否正确。\n有的系统登录用户名是完整的邮箱地址。';
+$PHPMAILER_LANG['connect_host'] = 'SMTP 错误：无法连接到 SMTP 主机。\n请确认禅道所在的机器能够访问smtp服务器，\n确认域名解析是否正常，端口号是否正确，防火墙是否正常。';
 $PHPMAILER_LANG['data_not_accepted'] = 'SMTP 错误：数据不被接受。';
 //$P$PHPMAILER_LANG['empty_message']        = 'Message body empty';
 $PHPMAILER_LANG['encoding'] = '未知编码: ';
@@ -17,7 +17,7 @@ $PHPMAILER_LANG['from_failed'] = '发送地址错误：';
 $PHPMAILER_LANG['instantiate'] = '未知函数调用。';
 //$PHPMAILER_LANG['invalid_email']        = 'Not sending, email address is invalid: ';
 $PHPMAILER_LANG['mailer_not_supported'] = '发信客户端不被支持。';
-$PHPMAILER_LANG['provide_address'] = '必须提供至少一个收件人地址。';
+$PHPMAILER_LANG['provide_address'] = '收件人没有设置邮箱，请到组织视图中检查下相应用户的email设置。';
 $PHPMAILER_LANG['recipients_failed'] = 'SMTP 错误：收件人地址错误：';
 //$PHPMAILER_LANG['signing']              = 'Signing Error: ';
 //$PHPMAILER_LANG['smtp_connect_failed']  = 'SMTP Connect() failed.';
