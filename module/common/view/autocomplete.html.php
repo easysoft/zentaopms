1da1daea36dac1b865dc27aef1b2395231ff3fab
wangchunsheng <chunsheng@cnezsoft.com>
2011-07-02 01:54:34 +0000

    - remove the bgiframe for autocomplete, because there's the latest bgiframe in lib.js yet.

--- module/common/view/autocomplete.html.php
+++ module/common/view/autocomplete.html.php
@@ -1,4 +1,3 @@
 <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
 <link rel='stylesheet' href='<?php echo $defaultTheme;?>autocomplete.css' />
-<script src='<?php echo $jsRoot;?>jquery/autocomplete/bgiframe.min.js' type='text/javascript'></script>
 <script src='<?php echo $jsRoot;?>jquery/autocomplete/autocomplete.min.js' type='text/javascript'></script>
