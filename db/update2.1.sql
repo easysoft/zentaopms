57aae7e92bf33d77faded8156880d162fbd2957f
wangchunsheng <chunsheng@cnezsoft.com>
2011-07-19 01:14:19 +0000

    * change the type of desc for build and plan table.

---
+++ db/update2.1.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `zt_build` CHANGE `desc` `desc` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `zt_productPlan` CHANGE `desc` `desc` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
