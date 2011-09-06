05c7294867251246da85938e64f915977078753f
wangchunsheng <chunsheng@cnezsoft.com>
2011-09-06 03:01:19 +0000

    * when print download link, append the session to it.

--- module/file/view/printfiles.html.php
+++ module/file/view/printfiles.html.php
@@ -1,3 +1,7 @@
+<?php
+$sessionString  = $config->requestType == 'PATH_INFO' ? '?' : '&';
+$sessionString .= session_name() . '=' . session_id();
+?>
 <style>.button-c {padding:1px}</style>
 <script language='Javascript'>
 /* Delete a file. */
@@ -10,8 +14,9 @@ function deleteFile(fileID)
 function downloadFile(fileID)
 {
     if(!fileID) return;
-    var URL = createLink('file', 'download', 'fileID=' + fileID + '&mouse=left');
-    window.open(URL, '_blank');
+    var sessionString = '<?php echo $sessionString;?>';
+    var url = createLink('file', 'download', 'fileID=' + fileID + '&mouse=left') + sessionString;
+    window.open(url, '_blank');
     return false;
 }
 </script>
@@ -23,7 +28,7 @@ function downloadFile(fileID)
   <?php
   foreach($files as $file)
   {
-      if(common::hasPriv('file' , 'download')) echo html::a($this->createLink('file', 'download', "fileID=$file->id"), $file->title .'.' . $file->extension, '_blank', "onclick='return downloadFile($file->id)'");
+      if(common::hasPriv('file' , 'download')) echo html::a($this->createLink('file', 'download', "fileID=$file->id") . $sessionString, $file->title .'.' . $file->extension, '_blank', "onclick='return downloadFile($file->id)'");
       if(common::hasPriv('file', 'delete'))    echo html::commonButton(' x ', "onclick='deleteFile($file->id)'");
   }
   ?>
