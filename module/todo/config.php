543cccc91308fff1f22b9b6b2ebf4ce246b25961
wangchunsheng <chunsheng@cnezsoft.com>
2011-09-07 07:48:01 +0000

    * adjust the begin and end time.

--- module/todo/config.php
+++ module/todo/config.php
@@ -2,9 +2,9 @@
 $config->todo->create->requiredFields = 'name';
 $config->todo->edit->requiredFields   = 'name';
 $config->todo->dates->end             = 15;
-$config->todo->times->begin           = 9;
-$config->todo->times->end             = 22;
-$config->todo->times->delta           = 15;
+$config->todo->times->begin           = 6;
+$config->todo->times->end             = 23;
+$config->todo->times->delta           = 10;
 
 $config->todo->editor->create = array('id' => 'desc', 'tools' => 'simpleTools');
 $config->todo->editor->edit   = array('id' => 'desc', 'tools' => 'simpleTools');
