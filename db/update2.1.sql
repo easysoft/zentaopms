64686c942acffe45d053c084c6c45e3d4b4f75fa
chencongzhi520@gmail.com <azhi@cnezsoft.com>
2011-09-17 08:25:07 +0000

    * fix bug#249

--- db/update2.1.sql
+++ db/update2.1.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `zt_build` CHANGE `desc` `desc` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
 ALTER TABLE `zt_productPlan` CHANGE `desc` `desc` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+
+ -- 2011-9-17 change length of name field in zt_module
+ALTER TABLE `zt_module` CHANGE `name` `name` CHAR( 60 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
