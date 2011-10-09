3d7bdf603a483b9b2da9afcdda56e78fe3c6f1ec
wangchunsheng <chunsheng@cnezsoft.com>
2011-10-09 13:19:15 +0000

    - remove the table2csv extension.

--- module/common/view/table2csv.html.php
+++
@@ -1,6 +0,0 @@
-<?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
-<?php
-css::import($defaultTheme . 'alert.css');
-js::import($jsRoot . 'jquery/alert/raw.js');
-js::import($jsRoot . 'jquery/table2csv/raw.js');
-?>
