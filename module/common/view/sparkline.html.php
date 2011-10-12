7eabfbd3c3463e43233ee7a3e5b3067d772f8986
wangchunsheng <chunsheng@cnezsoft.com>
2011-10-12 08:34:07 +0000

    * use js::import instenad.

--- module/common/view/sparkline.html.php
+++ module/common/view/sparkline.html.php
@@ -1,2 +1,2 @@
 <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
-<script src="<?php echo $jsRoot;?>jquery/sparkline/min.js" type="text/javascript"></script>
+<?php js::import($jsRoot . 'jquery/sparkline/min.js');?>
