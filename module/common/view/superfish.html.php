810f6a09ae49f2ca48e9ea179ed3a9d5996e693e
wangchunsheng <chunsheng@cnezsoft.com>
2011-10-12 08:36:41 +0000

    + import the superfish #516.

---
+++ module/common/view/superfish.html.php
@@ -0,0 +1,5 @@
+<?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
+<?php 
+css::import($defaultTheme . 'superfish.css');
+js::import($jsRoot . 'jquery/superfish/min.js');
+?>
