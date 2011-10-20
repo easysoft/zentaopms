d6b77a433682c521ced67237ea705e0e4c580363
wangchunsheng <chunsheng@cnezsoft.com>
2011-10-20 09:18:59 +0000

    * adjust the style of batch create story page.

--- module/story/view/batchcreate.html.php
+++ module/story/view/batchcreate.html.php
@@ -22,19 +22,19 @@
       <th><?php echo $lang->story->spec;?></th>
       <th><?php echo $lang->story->pri;?></th>
       <th><?php echo $lang->story->estimate;?></th>
-      <th class='w-40px'><?php echo $lang->story->needReview;?></th>
+      <th class='w-40px'><?php echo $lang->story->review;?></th>
     </tr>
     <?php for($i = 0; $i < $config->story->batchCreate; $i++):?>
     <?php $moduleID = $i == 0 ? 0 : 'same';?>
     <?php $planID   = $i == 0 ? '' : 'same';?>
-    <tr>
+    <tr class='a-center'>
       <td><?php echo html::select("use[$i]", $lang->story->useList, 0);?>
       <td><span id='moduleIdBox'><?php echo html::select("module[$i]", $moduleOptionMenu, $moduleID);?></span></td>
       <td><span id='planIdBox'><?php echo html::select("plan[$i]", $plans, $planID, 'class=select-2');?></span></td>
-      <td><?php echo html::input("title[$i]", $title, "class='text-1'"); echo '*';?></td>
+      <td><?php echo html::input("title[$i]", $title, "class='text-1'"); echo "<span class='star'>*</span>";?></td>
       <td><?php echo html::textarea("spec[$i]", $spec, "rows='1' class='text-1'");?></td>
-      <td><?php echo html::select("pri[$i]", (array)$lang->story->priList, $pri, 'class=select-2');?></td>
-      <td><?php echo html::input("estimate[$i]", $estimate, "class='text-2'");?></td>
+      <td><?php echo html::select("pri[$i]", (array)$lang->story->priList, $pri, 'class=w-50px');?></td>
+      <td><?php echo html::input("estimate[$i]", $estimate, "class='w-50px'");?></td>
       <td><?php echo html::select("needReview[$i]", $lang->story->reviewList, 0, "id='needNotReview'");?></td>
     </tr>  
     <?php endfor;?>
