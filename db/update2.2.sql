5b58f8126e8203bd977dcff55ea86368b0a13b1e
wangchunsheng <chunsheng@cnezsoft.com>
2011-10-28 05:56:19 +0000

    * adjust the field of confirmed.

--- db/update2.2.sql
+++ db/update2.2.sql
@@ -1,7 +1,7 @@
 ALTER TABLE `zt_story` ADD `source` VARCHAR( 20 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `plan` ;
-ALTER TABLE `zt_bug` ADD `activatedCount` SMALLINT( 6 ) NOT NULL AFTER `status` ;
-ALTER TABLE `zt_bug` ADD `confirm` BOOL NOT NULL DEFAULT '0' AFTER `activatedCount` ;
-ALTER TABLE `zt_bug` ADD `confirmedBy` VARCHAR( 30 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `closedDate` , ADD `confirmedDate` DATETIME NOT NULL AFTER `confirmedBy` ;
+ALTER TABLE `zt_bug` ADD `confirmed` BOOL NOT NULL DEFAULT '0' AFTER `status` ;
+ALTER TABLE `zt_bug` ADD `activatedCount` SMALLINT( 6 ) NOT NULL AFTER `confirmed` ;
+UPDATE `zt_bug` SET `confirmed` = 1 WHERE status in('closed', 'resolved');
 
  -- 2011-10-16 add precondition field in zt_case
 ALTER TABLE `zt_case` ADD `precondition` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `title`;
