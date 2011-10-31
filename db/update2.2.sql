89a08f296820d8fc650bbf31d0012d6749c536d1
wangchunsheng <chunsheng@cnezsoft.com>
2011-10-31 01:07:32 +0000

    * move the update2.1 here because the update sql of 2.1 version not saved into the zentao.sql.

--- db/update2.2.sql
+++ db/update2.2.sql
@@ -1,3 +1,9 @@
+ALTER TABLE `zt_build` CHANGE `desc` `desc` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `zt_productPlan` CHANGE `desc` `desc` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+
+ -- 2011-9-17 change length of name field in zt_module
+ALTER TABLE `zt_module` CHANGE `name` `name` CHAR( 60 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
+
 ALTER TABLE `zt_story` ADD `source` VARCHAR( 20 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `plan` ;
 ALTER TABLE `zt_bug` ADD `confirmed` BOOL NOT NULL DEFAULT '0' AFTER `status` ;
 ALTER TABLE `zt_bug` ADD `activatedCount` SMALLINT( 6 ) NOT NULL AFTER `confirmed` ;
