bd98ab60afaac14a74ed7d1842f02d65ba44d93b
wangchunsheng <chunsheng@cnezsoft.com>
2011-12-07 00:08:30 +0000

    * adjust the link of import and create story.

--- module/project/view/story.html.php
+++ module/project/view/story.html.php
@@ -16,10 +16,10 @@
   <caption class='caption-tl'>
     <div class='f-left'><?php echo $lang->project->story;?></div>
     <div class='f-right'>
-    <?php 
-        if(common::hasPriv('project', 'linkstory')) echo html::a($this->createLink('project', 'linkstory', "project=$project->id"), $lang->project->linkStory);
-        common::printLink('story', 'create', "productID=0&moduleID=0&story=0&project=$project->id", $lang->story->create);
-    ?>
+      <?php 
+      common::printLink('story', 'create', "productID=0&moduleID=0&story=0&project=$project->id", $lang->story->create);
+      if(common::hasPriv('project', 'linkstory')) echo html::a($this->createLink('project', 'linkstory', "project=$project->id"), $lang->project->linkStory);
+      ?>
     </div>
   </caption>
   <thead>
