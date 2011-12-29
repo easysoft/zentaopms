a789f30aa7215f848bb2aabb6b7e9b89fce5c8d5
shiyangyangwork@yahoo.cn <shiyangyang@cnezsoft.com>
2011-12-29 08:33:42 +0000

    * adjust code for task #664.

--- module/build/view/create.html.php
+++ module/build/view/create.html.php
@@ -43,47 +43,49 @@
     <tr>
       <th class='rowhead'><?php echo $lang->build->linkStoriesAndBugs;?></th>
       <td>
-        <div class='half-left' style="height:300px; overflow-y:auto">
-          <table class='table-1'>
-            <caption>story</caption>
-            <tr class='colhead'>
-            <?php $vars = "projectID=$projectID&orderBy=%s"; ?>
-              <th class='w-id {sorter:false}'>    <?php common::printOrderLink('id',     $orderBy, $vars, $lang->idAB);?></th>
-              <th class= '{sorter:false}'>        <?php common::printOrderLink('title',  $orderBy, $vars, $lang->story->title);?></th>
-              <th class='w-100px {sorter:false}'> <?php common::printOrderLink('stage',  $orderBy, $vars, $lang->story->stageAB);?></th>
-            </tr>
-            <?php foreach($stories as $key => $story):?>
-            <?php
-            $storyLink = $this->createLink('story', 'view', "storyID=$story->id");
-            ?>
-            <tr class='a-center'>
-              <td><input type='checkbox' name='stories[]' value="<?php echo $story->id;?>"> <?php echo sprintf('%03d', $story->id);?></td>
-              <td class='a-left nobr'><?php echo html::a($storyLink,$story->title);?></td>
-              <td><?php echo $lang->story->stageList[$story->stage];?></td>
-            </tr>
-            <?php endforeach;?>
-          </table>
-        </div>
-        <div class='half-right' style="height:300px; overflow-y:auto">
-          <table class='table-1'>
-            <caption>bug</caption>
-            <tr class='colhead'>
-              <th class='w-id'>       <?php echo $lang->idAB;?></th>
-              <th><?php echo $lang->bug->title;?></th>
-            </tr>
-            <?php foreach($bugs as $bug):?>
-            <tr class='a-center'>
-              <td><input type='checkbox' name='bugs[]' value="<?php echo $bug->id;?>"> <?php echo sprintf('%03d', $bug->id);?></td>
-              <td class='a-left nobr'><?php common::printLink('bug', 'view', "bugID=$bug->id", $bug->title, '', "class='preview'");?></td>
-            </tr>
-            <?php endforeach;?>
-          </table>
+        <div class="w-p90">
+          <div class='half-left' style="height:225px; overflow-y:auto">
+            <table class='table-1'>
+              <caption>story</caption>
+              <tr class='colhead'>
+              <?php $vars = "projectID=$projectID&orderBy=%s"; ?>
+                <th class='w-id {sorter:false}'>    <?php common::printOrderLink('id',     $orderBy, $vars, $lang->idAB);?></th>
+                <th class= '{sorter:false}'>        <?php common::printOrderLink('title',  $orderBy, $vars, $lang->story->title);?></th>
+                <th class='w-100px {sorter:false}'> <?php common::printOrderLink('stage',  $orderBy, $vars, $lang->story->stageAB);?></th>
+              </tr>
+              <?php foreach($stories as $key => $story):?>
+              <?php
+              $storyLink = $this->createLink('story', 'view', "storyID=$story->id");
+              ?>
+              <tr class='a-center'>
+                <td><input type='checkbox' name='stories[]' value="<?php echo $story->id;?>" <?php if($story->stage == 'developed') echo 'checked';?>> <?php echo sprintf('%03d', $story->id);?></td>
+                <td class='a-left nobr'><?php echo html::a($storyLink,$story->title);?></td>
+                <td><?php echo $lang->story->stageList[$story->stage];?></td>
+              </tr>
+              <?php endforeach;?>
+            </table>
+          </div>
+          <div class='half-right' style="height:225px; overflow-y:auto">
+            <table class='table-1'>
+              <caption>bug</caption>
+              <tr class='colhead'>
+                <th class='w-id'>       <?php echo $lang->idAB;?></th>
+                <th><?php echo $lang->bug->title;?></th>
+              </tr>
+              <?php foreach($bugs as $bug):?>
+              <tr class='a-center'>
+                <td><input type='checkbox' name='bugs[]' value="<?php echo $bug->id;?>" checked> <?php echo sprintf('%03d', $bug->id);?></td>
+                <td class='a-left nobr'><?php common::printLink('bug', 'view', "bugID=$bug->id", $bug->title, '', "class='preview'");?></td>
+              </tr>
+              <?php endforeach;?>
+            </table>
+          </div>
         </div>
       </td>
     </tr>
     <tr>
       <th class='rowhead'><?php echo $lang->build->desc;?></th>
-      <td><?php echo html::textarea('desc', '', "rows='15' class='area-1'");?></td>
+      <td><?php echo html::textarea('desc', '', "rows='10' class='area-1'");?></td>
     </tr>  
     <tr><td colspan='2' class='a-center'><?php echo html::submitButton() . html::resetButton();?></td></tr>
   </table>
