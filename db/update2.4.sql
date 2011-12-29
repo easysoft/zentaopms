5ae5f8359852a88ed5005d6eae545a2ba0e980bf
wangchunsheng <chunsheng@cnezsoft.com>
2011-12-29 05:57:33 +0000

    + task#664, 554

--- db/update2.4.sql
+++ db/update2.4.sql
@@ -1,6 +1,8 @@
+-- fix the error of gender. add commiter field.
 ALTER TABLE `zt_user` CHANGE `gendar` `gender` ENUM( 'f', 'm' ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'f' ;
 ALTER TABLE `zt_user` ADD `commiter` VARCHAR( 100 ) NOT NULL AFTER `nickname` ;
 
+-- fix the error of objectType.
 UPDATE `zt_action` SET `objectType` = 'product'     WHERE `objectType` = '`product`';
 UPDATE `zt_action` SET `objectType` = 'productplan' WHERE `objectType` = '`productPlan`';
 UPDATE `zt_action` SET `objectType` = 'project'     WHERE `objectType` = '`project`';
@@ -16,3 +18,9 @@ UPDATE `zt_action` SET `objectType` = 'doclib'      WHERE `objectType` = '`docli
 UPDATE `zt_action` SET `objectType` = 'testtask'    WHERE `objectType` = '`testtask`';
 UPDATE `zt_action` SET `objectType` = 'todo'        WHERE `objectType` = '`todo`';
 UPDATE `zt_bug` SET `os` = 'andriod' WHERE `os` = 'adriod';
+
+-- add stories and bugs to release and build table.
+ALTER TABLE `zt_release` ADD `stories` TEXT NOT NULL AFTER `date` ;
+ALTER TABLE `zt_release` ADD `bugs` TEXT NOT NULL AFTER `stories` ;
+ALTER TABLE `zt_build` ADD `stories` TEXT NOT NULL AFTER `date` ,
+ADD `bugs` TEXT NOT NULL AFTER `stories` ;
