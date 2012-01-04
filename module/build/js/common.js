c63ac90de82ca61b3400a8463f2425dd67a2f266
wangchunsheng <chunsheng@cnezsoft.com>
2012-01-04 06:18:16 +0000

    * change the scrolling param to true.

--- module/build/js/common.js
+++ module/build/js/common.js
@@ -1,4 +1,4 @@
 $(document).ready(function()
 {
-    $("a.preview").colorbox({width:1000, height:600, iframe:true, transition:'elastic', speed:350, scrolling:false});
+    $("a.preview").colorbox({width:1000, height:600, iframe:true, transition:'elastic', speed:350, scrolling:true});
 })
