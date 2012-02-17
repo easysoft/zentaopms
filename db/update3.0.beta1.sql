e7cd094f750bd0c02f43a2cb13b6ffdb16a560fd
shiyangyangwork@yahoo.cn <shiyangyang@cnezsoft.com>
2012-02-17 06:30:29 +0000

    * adjust the 'diff' in zt_history, change the text to mediumtext.

--- db/update3.0.beta1.sql
+++ db/update3.0.beta1.sql
@@ -1,2 +1,3 @@
 UPDATE `zt_story` SET `source` = 'customer' WHERE `source` = 'custom';
 ALTER TABLE `zt_action` CHANGE `product` `product` VARCHAR( 255 ) NOT NULL ;
+ALTER TABLE `zt_history` CHANGE `diff` `diff` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ;
