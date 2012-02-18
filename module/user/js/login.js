0fa4ea8d9853110632c829db525af682b05e3a97
shiyangyangwork@yahoo.cn <shiyangyang@cnezsoft.com>
2012-02-18 02:56:12 +0000

    * finish task #680.

--- module/user/js/login.js
+++ module/user/js/login.js
@@ -2,4 +2,14 @@ function setForm(){}
 $(document).ready(function()
 {
     $('#account').focus();
+    var iframe = document.getElementById("updater");
+    iframe.src = url;
+    if (iframe.attachEvent)
+    {
+        iframe.attachEvent("onload", function(){$('#updater').show();});
+    }
+    else
+    {
+        iframe.onload = function(){$('#updater').show();};
+    }
 })
