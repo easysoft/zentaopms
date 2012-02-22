35b7a350f0819ad6890cbd39ee24404d706db0ba
zhujinyong <zhujinyong@cnezsoft.com>
2012-02-22 12:23:55 +0000

    - remove register and bind method. * change 'login' to 'bind'

--- module/admin/control.php
+++ module/admin/control.php
@@ -21,12 +21,12 @@ class admin extends control
 		$user = $this->loadModel('setting')->getItem('system', 'global', 'community');
 		if($user != '' and $user != 'na')
 		{
-			$this->view->login   = true;
+			$this->view->bind    = true;
 			$this->view->account = $user;
 		}
 		else
 		{
-			$this->view->login   = false;
+			$this->view->bind    = false;
 			$this->view->account = '';
 		}
 		if($this->loadModel('setting')->getItem('system', 'global', 'community') != '')
@@ -37,17 +37,17 @@ class admin extends control
 		{
 			$this->view->ignore = false;
 		}
-		$this->view->latestRelease = $this->loadModel('install')->getLatestRelease();
+		$this->app->loadLang('misc');
 		$this->display();
     }
 
 	/**
-	 * Ignore notice of register and login.
+	 * Ignore notice of register and bind.
 	 * 
 	 * @access public
 	 * @return void
 	 */
-	public function ignoreNotice()
+	public function ignore()
 	{
 		$this->loadModel('setting')->setItem('system', 'global', 'community', 'na');
 		die(js::locate(inlink('index'), 'parent'));
@@ -72,8 +72,8 @@ class admin extends control
 			}
 			die($response);
 		}
-		$this->view->reg = $this->admin->getRegisterInfo();
-		$this->view->sn  = $this->loadModel('setting')->getItem('system', 'global', 'sn');
+		$this->view->register = $this->admin->getRegisterInfo();
+		$this->view->sn       = $this->loadModel('setting')->getItem('system', 'global', 'sn');
 		$this->display();
 	}
 
@@ -91,7 +91,7 @@ class admin extends control
 			if($response == 'success') 
 			{
 				$this->loadModel('setting')->setItem('system', 'global', 'community', $this->post->account);
-				echo js::alert($this->lang->admin->login->success);
+				echo js::alert($this->lang->admin->bind->success);
 				die(js::locate(inlink('index'), 'parent'));
 			}
 			die($response);
