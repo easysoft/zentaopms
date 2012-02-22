83a208bb0ce8fba7fe26da13c11475e58a990d3b
zhujinyong <zhujinyong@cnezsoft.com>
2012-02-22 12:10:10 +0000

    * move the content of links to links.html.php.

--- module/misc/view/about.html.php
+++ module/misc/view/about.html.php
@@ -6,26 +6,7 @@
       <h3><?php printf($lang->misc->zentao->version, $config->version);?></h3>
     </td>
     <td> 
-      <table class='table-1'>
-        <tr class='colhead'>
-          <?php foreach($lang->misc->zentao->labels as $label) echo "<th class='w-p25'>$label</th>";?>
-        </tr>
-        <?php
-        unset($lang->misc->zentao->version);
-        unset($lang->misc->zentao->labels);
-        ?>
-        <tr class='a-left' valign='top'>
-          <?php foreach($lang->misc->zentao as $groupItems):?>
-          <td>
-            <ul>
-              <?php foreach($groupItems as $item => $label):?>
-              <li><?php echo html::a("http://www.zentao.net/goto.php?item=$item", $label, '_blank');;?></li>
-              <?php endforeach;?>
-            </ul>
-          </td>
-          <?php endforeach;?>
-        </tr>
-      </table>
+	  <?php include './links.html.php';?>
       <div class='a-right'><?php echo $lang->misc->copyright;?></div>
     </td>
   </tr>
