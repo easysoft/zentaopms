c2d27b682ba5de2ea93a085fc2b752c520d90f2f
zhujinyong <zhujinyong@cnezsoft.com>
2012-02-22 12:18:00 +0000

    - remove lang of abandon content. * adjust lang of register and bind zentao community.

--- module/admin/lang/en.php
+++ module/admin/lang/en.php
@@ -19,44 +19,22 @@ $lang->admin->welcome = 'Welcome to ZenTaoPMS.';
 $lang->admin->browseCompany = 'Browse Company';
 
 $lang->admin->info->caption        = 'ZentaoPMS information';
-$lang->admin->info->urls           = 'Related links';
 $lang->admin->info->currentVersion = 'Current version';
-$lang->admin->info->community      = 'Zentao home page';
-$lang->admin->info->ask            = 'Zentao ask';
-$lang->admin->info->document       = 'Help document';
-$lang->admin->info->feedback       = 'Feedback and suggestion';
-$lang->admin->info->faq            = 'FAQ';
-$lang->admin->info->extension      = 'Extension';
-$lang->admin->info->donation       = 'Donation';
-$lang->admin->info->service        = 'Service fee';
-$lang->admin->info->account        = 'Community account';
+$lang->admin->info->version 	   = 'The current version of the system is %s,';
+$lang->admin->info->links          = 'You can visit the following link:';
+$lang->admin->info->account        = 'Your account in zentao community is %s. ';
 
-$lang->admin->notice->join         = "Tips: You have not registered in Zentao community (www.zentao.net), %s to register and get the latest information about ZentaoPMS.";
-$lang->admin->notice->ignore       = "ignore";
+$lang->admin->notice->register = "Tips: You have not registered in Zentao community (www.zentao.net), %s to register and get the latest information about ZentaoPMS.";
+$lang->admin->notice->ignore   = "ignore";
 
-$lang->admin->reg->caption    = 'Register zentao community';
-$lang->admin->reg->join       = 'click';
-$lang->admin->reg->lblAccount = 'Numbers and letters, at least three';
-$lang->admin->reg->lblPasswd  = 'Numbers and letters, at least six';
-$lang->admin->reg->submit     = 'Register';
-$lang->admin->reg->login      = "If you have community account, %s to associate it";
-$lang->admin->reg->success    = "Register success";
+$lang->admin->register->caption    = 'Register zentao community';
+$lang->admin->register->click      = 'click';
+$lang->admin->register->lblAccount = 'Numbers and letters, at least three';
+$lang->admin->register->lblPasswd  = 'Numbers and letters, at least six';
+$lang->admin->register->submit     = 'Register';
+$lang->admin->register->bind       = "If you have community account, %s to bind it";
+$lang->admin->register->success    = "Register success";
 
-$lang->admin->login->caption = 'Associated with community account';
-$lang->admin->login->join    = 'associate';
-$lang->admin->login->success = "Associate success";
-
-$lang->admin->info->version = 'The current version of the system is %s,';
-$lang->admin->info->latest  = 'latest version is %s, please download and upgrade.';
-$lang->admin->info->new     = "it's the latest verion.";
-$lang->admin->info->links   = 'You can visit the following link:';
-$lang->admin->info->account = 'Your account in zentao community is';
-
-$lang->admin->desc->community = ':Log on the official website of the Zentao, get the latest news';
-$lang->admin->desc->ask       = ':Resolve the problems encountered in the process of using zentao, and get technical support';
-$lang->admin->desc->document  = ':The document about using zentao and developing extension';
-$lang->admin->desc->feedback  = ':Give your suggestion, let us do better';
-$lang->admin->desc->faq       = ':The FAQ in the process of using zentao';
-$lang->admin->desc->extension = ':The list of zentao extension';
-$lang->admin->desc->donation  = ':Click the link, donate to us';
-$lang->admin->desc->service   = ':itechnical support, agility training and customized development';
+$lang->admin->bind->caption = 'Bind with community account';
+$lang->admin->bind->action  = 'bind';
+$lang->admin->bind->success = "Bind success";
--- module/admin/lang/zh-cn.php
+++ module/admin/lang/zh-cn.php
@@ -19,44 +19,21 @@ $lang->admin->welcome = '欢迎使用禅道管理软件后台管理系统';
 $lang->admin->browseCompany = '浏览公司';
 
 $lang->admin->info->caption        = '禅道系统信息';
-$lang->admin->info->urls           = '相关链接';
-$lang->admin->info->currentVersion = '当前版本';
-$lang->admin->info->community      = '禅道主页';
-$lang->admin->info->ask            = '禅道问答';
-$lang->admin->info->document       = '帮助文档';
-$lang->admin->info->feedback       = '反馈建议';
-$lang->admin->info->faq            = '常见问题';
-$lang->admin->info->extension      = '禅道插件';
-$lang->admin->info->donation       = '捐助禅道';
-$lang->admin->info->service        = '收费服务';
-$lang->admin->info->account        = '社区账号';
+$lang->admin->info->version        = '当前系统的版本是%s，';
+$lang->admin->info->links          = '您可以访问以下链接：';
+$lang->admin->info->account        = "您的禅道社区账户为%s。";
 
-$lang->admin->notice->join         = "友情提示：您还未在禅道社区(www.zentao.net)登记，%s进行登记，以及时获得禅道最新信息。";
-$lang->admin->notice->ignore       = "不再提示";
+$lang->admin->notice->register = "友情提示：您还未在禅道社区(www.zentao.net)登记，%s进行登记，以及时获得禅道最新信息。";
+$lang->admin->notice->ignore   = "不再提示";
 
-$lang->admin->reg->caption    = '禅道社区登记';
-$lang->admin->reg->join       = '点击此处';
-$lang->admin->reg->lblAccount = '请设置您的用户名，英文字母和数字的组合，三位以上。';
-$lang->admin->reg->lblPasswd  = '请设置您的密码。数字和字母的组合，六位以上。';
-$lang->admin->reg->submit     = '登记';
-$lang->admin->reg->login      = "如果您已经拥有社区账号，%s关联账户";
-$lang->admin->reg->success    = "登记账户成功";
+$lang->admin->register->caption    = '禅道社区登记';
+$lang->admin->register->click      = '点击此处';
+$lang->admin->register->lblAccount = '请设置您的用户名，英文字母和数字的组合，三位以上。';
+$lang->admin->register->lblPasswd  = '请设置您的密码。数字和字母的组合，六位以上。';
+$lang->admin->register->submit     = '登记';
+$lang->admin->register->bind       = "如果您已经拥有社区账号，%s关联账户";
+$lang->admin->register->success    = "登记账户成功";
 
-$lang->admin->login->caption = '关联社区账号';
-$lang->admin->login->join    = '关联';
-$lang->admin->login->success = "关联账户成功";
-
-$lang->admin->info->version = '当前系统的版本是%s，';
-$lang->admin->info->latest  = '最新版本为%s，请及时下载升级。';
-$lang->admin->info->new     = '为最新版本。';
-$lang->admin->info->links   = '您可以访问以下链接：';
-$lang->admin->info->account = '您的禅道社区账户为';
-
-$lang->admin->desc->community = ':登录禅道官网，了解禅道最新动态';
-$lang->admin->desc->ask       = ':解决使用过程中遇到的问题，获得技术支持';
-$lang->admin->desc->document  = ':禅道使用及扩展开发的帮助文档';
-$lang->admin->desc->feedback  = ':提出您的建议，让我们做的更好';
-$lang->admin->desc->faq       = ':包含使用过程中的常见问题';
-$lang->admin->desc->extension = ':禅道的扩展插件列表';
-$lang->admin->desc->donation  = ':点击这个链接，捐助我们';
-$lang->admin->desc->service   = ':远程协助、敏捷培训以及定制开发';
+$lang->admin->bind->caption  = '关联社区账号';
+$lang->admin->bind->action   = '关联';
+$lang->admin->bind->success  = "关联账户成功";
