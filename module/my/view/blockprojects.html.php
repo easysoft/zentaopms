1a316adf3b2ed8abef76565afe1f3dde83c4bffd
wangchunsheng <chunsheng@cnezsoft.com>
2012-03-03 01:19:53 +0000

    * adjust the css code.  - remove emptybg class.

--- module/my/view/blockprojects.html.php
+++ module/my/view/blockprojects.html.php
@@ -1,6 +1,6 @@
 <div class='block linkbox1' id='projectbox'>
 <?php if(count($projectStats) == 0):?>
-<table class='table-1 a-center emptybg' height='138px'>
+<table class='table-1 a-center bg-gray' height='138px'>
   <caption><span class='icon-title'></span><?php echo $lang->my->home->projects;?></caption>
   <tr>
     <td valign='middle'>
