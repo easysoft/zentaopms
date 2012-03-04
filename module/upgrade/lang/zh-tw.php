1d7a142b247a5ea2c7e768845ccaa90d260b5173
wangchunsheng <chunsheng@cnezsoft.com>
2012-03-04 02:21:48 +0000

    * fix the error when upgrade.

--- module/upgrade/lang/zh-tw.php
+++ module/upgrade/lang/zh-tw.php
@@ -59,3 +59,4 @@ $lang->upgrade->fromVersions['2_2']       = '2.2';
 $lang->upgrade->fromVersions['2_3']       = '2.3';
 $lang->upgrade->fromVersions['2_4']       = '2.4';
 $lang->upgrade->fromVersions['3_0_beta1'] = '3.0.beta1';
+$lang->upgrade->fromVersions['3_0_beta2'] = '3.0.beta2';
