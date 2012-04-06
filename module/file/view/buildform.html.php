eaeaaae3ec30e198464b20cdc53d92cc233b5d80
wangyidong <wangyidong@cnezsoft.com>
2012-04-06 07:18:57 +0000

    * fix a bug: no icon.

--- module/file/view/buildform.html.php
+++ module/file/view/buildform.html.php
@@ -1,9 +1,13 @@
 <div id='fileform'>
-<?php for($i = 0; $i < $fileCount; $i ++):?>
-<span class='icon'><span class='icon-file'></span><?php echo $lang->file->common . ($i + 1) . " "?></span><input type='file' name='files[]' id="file<?php echo $i;?>"  tabindex='-1' />
-<label id='label<?php echo $i;?>' tabindex='-1' class='strong'><?php echo $lang->file->label;?></label>
-<input type='text' name='labels[]' class='text-3' tabindex='-1' /><br />
-<?php endfor;?>
+  <?php for($i = 0; $i < $fileCount; $i ++):?>
+  <span class='icon'>
+    <span class='icon-file'></span>
+    <?php echo $lang->file->common . ($i + 1) . " "?>
+  </span>
+  <input type='file' name='files[]' id="file<?php echo $i;?>"  tabindex='-1' />
+  <label id='label<?php echo $i;?>' tabindex='-1' class='strong'><?php echo $lang->file->label;?></label>
+  <input type='text' name='labels[]' class='text-3' tabindex='-1' /><br />
+  <?php endfor;?>
 </div>
 <script language='javascript'>
 function setFileFormWidth(percent)
