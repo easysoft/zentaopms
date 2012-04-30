00fa11813d07b3a35150fc7fa32cd73096f2d413
wangchunsheng <chunsheng@cnezsoft.com>
2012-04-30 07:37:34 +0000

    * when create diff, add the veryfy as the multiline field.

--- module/common/control.php
+++ module/common/control.php
@@ -231,7 +231,7 @@ class common extends control
             if($value != $old->$key)
             { 
                 $diff = '';
-                if(substr_count($value, "\n") > 1 or substr_count($old->$key, "\n") > 1 or strpos('name,title,desc,spec,steps,content,digest', strtolower($key)) !== false) $diff = commonModel::diff($old->$key, $value);
+                if(substr_count($value, "\n") > 1 or substr_count($old->$key, "\n") > 1 or strpos('name,title,desc,spec,steps,content,digest,verify', strtolower($key)) !== false) $diff = commonModel::diff($old->$key, $value);
                 $changes[] = array('field' => $key, 'old' => $old->$key, 'new' => $value, 'diff' => $diff);
             }
         }
