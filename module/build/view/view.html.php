42c4d84a62d826bfc18638b387fda81e2a390573
wangchunsheng <chunsheng@cnezsoft.com>
2012-04-30 14:18:19 +0000

    * adjust the release and build page.

--- module/build/view/view.html.php
+++ module/build/view/view.html.php
@@ -40,63 +40,59 @@
   <tr>
     <th class='rowhead'><?php echo $lang->build->stories;?></th>
     <td>
-      <div style="height:225px; overflow-y:auto">
-        <table class='table-1 fixed'>
-          <tr>
-            <th class='w-id'><?php echo $lang->idAB;?></th>
-            <th class='w-pri'><?php echo $lang->priAB;?></th>
-            <th><?php echo $lang->story->title;?></th>
-            <th class='w-user'><?php echo $lang->openedByAB;?></th>
-            <th class='w-hour'><?php echo $lang->story->estimateAB;?></th>
-            <th class='w-hour'><?php echo $lang->statusAB;?></th>
-            <th class='w-100px'><?php echo $lang->story->stageAB;?></th>
-          </tr>
-          <?php foreach($stories as $storyID => $story):?>
-          <?php $storyLink = $this->createLink('story', 'view', "storyID=$story->id");?>
-          <tr class='a-center'>
-            <td><?php echo sprintf('%03d', $story->id);?></td>
-            <td><?php echo $lang->story->priList[$story->pri];?></td>
-            <td class='a-left nobr'><?php echo html::a($storyLink,$story->title, '', "class='preview'");?></td>
-            <td><?php echo $users[$story->openedBy];?></td>
-            <td><?php echo $story->estimate;?></td>
-            <td class='<?php echo $story->status;?>'><?php echo $lang->story->statusList[$story->status];?></td>
-            <td><?php echo $lang->story->stageList[$story->stage];?></td>
-          </tr>
-          <?php endforeach;?>
-         <tr><td colspan=7 class='a-left strong'><?php echo sprintf($lang->build->finishStories, count($stories));?></td></tr>
-        </table>
-      </div>
+      <table class='table-1 fixed'>
+        <tr>
+          <th class='w-id'><?php echo $lang->idAB;?></th>
+          <th class='w-pri'><?php echo $lang->priAB;?></th>
+          <th><?php echo $lang->story->title;?></th>
+          <th class='w-user'><?php echo $lang->openedByAB;?></th>
+          <th class='w-hour'><?php echo $lang->story->estimateAB;?></th>
+          <th class='w-hour'><?php echo $lang->statusAB;?></th>
+          <th class='w-100px'><?php echo $lang->story->stageAB;?></th>
+        </tr>
+        <?php foreach($stories as $storyID => $story):?>
+        <?php $storyLink = $this->createLink('story', 'view', "storyID=$story->id");?>
+        <tr class='a-center'>
+          <td><?php echo sprintf('%03d', $story->id);?></td>
+          <td><?php echo $lang->story->priList[$story->pri];?></td>
+          <td class='a-left nobr'><?php echo html::a($storyLink,$story->title, '', "class='preview'");?></td>
+          <td><?php echo $users[$story->openedBy];?></td>
+          <td><?php echo $story->estimate;?></td>
+          <td class='<?php echo $story->status;?>'><?php echo $lang->story->statusList[$story->status];?></td>
+          <td><?php echo $lang->story->stageList[$story->stage];?></td>
+        </tr>
+        <?php endforeach;?>
+       <tr><td colspan=7 class='a-left strong'><?php echo sprintf($lang->build->finishStories, count($stories));?></td></tr>
+      </table>
     </td>
   </tr>
   <tr>
     <th class='rowhead'><?php echo $lang->build->bugs;?></th>
     <td>
-      <div style="height:225px; overflow-y:auto">
-        <table class='table-1 fixed'>
-          <tr>
-            <th class='w-id'>       <?php echo $lang->idAB;?></th>
-            <th><?php echo $lang->bug->title;?></th>
-            <th class='w-100px'><?php echo $lang->bug->status;?></th>
-            <th class='w-user'><?php echo $lang->openedByAB;?></th>
-            <th class='w-date'><?php echo $lang->bug->openedDateAB;?></th>
-            <th class='w-user'><?php echo $lang->bug->resolvedByAB;?></th>
-            <th class='w-date'><?php echo $lang->bug->resolvedDateAB;?></th>
-          </tr>
-          <?php foreach($bugs as $bug):?>
-          <?php $bugLink = $this->createLink('bug', 'view', "bugID=$bug->id");?>
-          <tr class='a-center'>
-            <td><?php echo sprintf('%03d', $bug->id);?></td>
-            <td class='a-left nobr'><?php echo html::a($bugLink, $bug->title, '', "class='preview'");?></td>
-            <td><?php echo $lang->bug->statusList[$bug->status];?></td>
-            <td><?php echo $users[$bug->openedBy];?></td>
-            <td><?php echo substr($bug->openedDate, 5, 11)?></td>
-            <td><?php echo $users[$bug->resolvedBy];?></td>
-            <td><?php echo substr($bug->resolvedDate, 5, 11)?></td>
-          </tr>
-          <?php endforeach;?>
-         <tr><td colspan=7 class='a-left strong'><?php echo sprintf($lang->build->resolvedBugs, count($bugs));?></td></tr>
-        </table>
-      </div>
+      <table class='table-1 fixed'>
+        <tr>
+          <th class='w-id'><?php echo $lang->idAB;?></th>
+          <th><?php echo $lang->bug->title;?></th>
+          <th class='w-100px'><?php echo $lang->bug->status;?></th>
+          <th class='w-user'><?php echo $lang->openedByAB;?></th>
+          <th class='w-date'><?php echo $lang->bug->openedDateAB;?></th>
+          <th class='w-user'><?php echo $lang->bug->resolvedByAB;?></th>
+          <th class='w-date'><?php echo $lang->bug->resolvedDateAB;?></th>
+        </tr>
+        <?php foreach($bugs as $bug):?>
+        <?php $bugLink = $this->createLink('bug', 'view', "bugID=$bug->id");?>
+        <tr class='a-center'>
+          <td><?php echo sprintf('%03d', $bug->id);?></td>
+          <td class='a-left nobr'><?php echo html::a($bugLink, $bug->title, '', "class='preview'");?></td>
+          <td><?php echo $lang->bug->statusList[$bug->status];?></td>
+          <td><?php echo $users[$bug->openedBy];?></td>
+          <td><?php echo substr($bug->openedDate, 5, 11)?></td>
+          <td><?php echo $users[$bug->resolvedBy];?></td>
+          <td><?php echo substr($bug->resolvedDate, 5, 11)?></td>
+        </tr>
+        <?php endforeach;?>
+       <tr><td colspan=7 class='a-left strong'><?php echo sprintf($lang->build->resolvedBugs, count($bugs));?></td></tr>
+      </table>
     </td>
   </tr>
   <tr>
