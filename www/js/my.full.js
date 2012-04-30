14a5c919ab3913fabcda542fc3f9181e54a0ead8
chencongzhi520@gmail.com <azhi@cnezsoft.com>
2012-04-30 06:38:05 +0000

    - remove checkAll() and checkReverse.

--- www/js/my.full.js
+++ www/js/my.full.js
@@ -814,24 +814,3 @@ $(document).bind('keydown', 'Ctrl+g', function(evt)
     evt.preventDefault( );
     return false;
 });
-
-/* Select All. */
-function checkAll()
-{
-    var checkOBJ = $('input');
-    for(var i = 0; i < checkOBJ.length; i++)
-    {
-        checkOBJ.get(i).checked = true;
-    }
-}
-
-/* Check reverse. */
-function checkReverse()
-{
-    var checkOBJ = $('input');
-    for(var i = 0; i < checkOBJ.length; i++)
-    {
-        checkOBJ.get(i).checked = !checkOBJ.get(i).checked;
-    }
-    return;
-}
