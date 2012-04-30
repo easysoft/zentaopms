df7e661c2b915c003b18513eb16beabd4a253321
chencongzhi520@gmail.com <azhi@cnezsoft.com>
2012-04-30 01:36:17 +0000

    * adjust the checkdb.php.

--- bin/checkdb.php
+++ bin/checkdb.php
@@ -3,11 +3,11 @@ include dirname(dirname(__FILE__)) . "/config/my.php";
 
 if($config->requestType == 'PATH_INFO')
 {
-    system("php ztcli \"http://localhost/admin-checkdb\"", $requestVar);
+    system('php ztcli "http://localhost/admin-checkdb"', $requestVar);
 }
 elseif($config->requestType == 'GET')
 {
-    system("php ztcli \"http://localhost/?m=admin&f=checkdb\"", $requestVar);
+    system('php ztcli "http://localhost/?m=admin&f=checkdb"', $requestVar);
 }
 
 if(!$requestVar)
