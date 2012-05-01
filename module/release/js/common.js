0304bb1800c37858acc2643bcd07baf95eb4a5bd
wangchunsheng <chunsheng@cnezsoft.com>
2012-05-01 13:10:07 +0000

    * when load stories and bugs, call the setOutBox() auto.

--- module/release/js/common.js
+++ module/release/js/common.js
@@ -4,6 +4,7 @@ function loadStoriesAndBugs(buildID,productID)
     $('#linkStoriesAndBugs').load(link, function()
     {
         $("a.preview").colorbox({width:1000, height:600, iframe:true, transition:'elastic', speed:350, scrolling:true});
+        setOuterBox();
     })
 }
 
