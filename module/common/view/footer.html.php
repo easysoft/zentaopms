24c2615dd2c53556f40cfc52dfe8661e705fc702
wangchunsheng <chunsheng@cnezsoft.com>
2012-05-02 06:13:10 +0000

    * move the hiddenwin to the top of the divider.

--- module/common/view/footer.html.php
+++ module/common/view/footer.html.php
@@ -1,8 +1,8 @@
   </div>
   <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
+  <iframe frameborder='0' name='hiddenwin' id='hiddenwin' scrolling='no' class='<?php $config->debug ? print("debugwin") : print('hidden')?>'></iframe>
   <div id='divider'></div>
 </div>
-<iframe frameborder='0' name='hiddenwin' id='hiddenwin' scrolling='no' class='<?php $config->debug ? print("debugwin") : print('hidden')?>'></iframe>
 <div id='footer'>
   <table class='cont' >
     <tr>
