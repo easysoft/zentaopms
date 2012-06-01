bb20161a461ad734628d3b179873162c953d9851
chencongzhi520@gmail.com <azhi@cnezsoft.com>
2012-06-01 01:03:43 +0000

    * adjust the comment.

--- module/task/view/batchedit.html.php
+++ module/task/view/batchedit.html.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The batch edit view of story module of ZenTaoPMS.
+ * The batch edit view of task module of ZenTaoPMS.
  *
  * @copyright   Copyright 2009-2012 青岛易软天创网络科技有限公司 (QingDao Nature Easy Soft Network Technology Co,LTD www.cnezsoft.com)
  * @license     LGPL (http://www.gnu.org/licenses/lgpl.html)
