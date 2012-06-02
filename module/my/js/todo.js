a3327c0ab8df571545fbe783fd1e8a06bbd66c24
chencongzhi520@gmail.com <azhi@cnezsoft.com>
2012-06-02 08:17:10 +0000

    * code for task#804.

--- module/my/js/todo.js
+++ module/my/js/todo.js
@@ -3,3 +3,18 @@ function changeDate(date)
     link = createLink('my', 'todo', 'date=' + date);
     location.href=link;
 }
+
+/**
+ * Change form action.
+ * 
+ * @param  formName   $formName 
+ * @param  actionName $actionName 
+ * @param  actionLink $actionLink 
+ * @access public
+ * @return void
+ */
+function changeAction(formName, actionName, actionLink)
+{
+    $('#' + formName).attr('action', actionLink).submit();
+}
+
