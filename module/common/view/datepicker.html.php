f508ac76f3b23b082dd3d6798da36a26551589a0
chencongzhi520@gmail.com <azhi@cnezsoft.com>
2012-06-12 00:18:38 +0000

    * Adjust the css of datepicker.

--- module/common/view/datepicker.html.php
+++ module/common/view/datepicker.html.php
@@ -13,7 +13,7 @@ $.dpText = {
     TEXT_NEXT_YEAR      :   '明年',
     TEXT_NEXT_MONTH     :   '下个月',
     TEXT_CLOSE          :   '关闭',
-    TEXT_CHOOSE_DATE    :   '...'
+    TEXT_CHOOSE_DATE    :   '&nbsp;&nbsp;&nbsp;&nbsp;'
 }
 Date.dayNames       = ['日', '一', '二', '三', '四', '五', '六'];
 Date.abbrDayNames   = ['星期天', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
