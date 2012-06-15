23295c02cd537799a10afef2a89fee8116204ded
chencongzhi520@gmail.com <azhi@cnezsoft.com>
2012-06-15 07:08:59 +0000

    * Adjust the browse page of case.

--- module/testcase/view/browse.html.php
+++ module/testcase/view/browse.html.php
@@ -74,7 +74,7 @@ var moduleID   = '<?php echo $moduleID;?>';
               <?php $viewLink = inlink('view', "caseID=$case->id");?>
               <td><?php echo html::a($viewLink, sprintf('%03d', $case->id));?></td>
               <td><?php echo $case->pri?></td>
-              <td class='a-left'><?php echo html::a($viewLink, $case->title);?></td>
+              <td class='a-left nobr'><?php echo html::a($viewLink, $case->title);?></td>
               <?php if($browseType == 'needconfirm'):?>
               <td class='a-left'><?php echo html::a($this->createLink('story', 'view', "storyID=$case->story"), $case->storyTitle, '_blank');?></td>
               <td><?php echo html::a(inlink('confirmStoryChange', "caseID=$case->id"), $lang->confirm, 'hiddenwin');?></td>
