61a7852947eeec720e8007000dc9fdb9c5be0979
zhujinyong <zhujinyong@cnezsoft.com>
2012-06-29 10:37:34 +0000

    - remove date limit of workload report.

--- module/report/control.php
+++ module/report/control.php
@@ -59,24 +59,8 @@ class report extends control
         $this->display(); 
     }
 
-    public function workload($begin = 0, $end = 0)
+    public function workload()
     {
-        if($begin == 0) 
-        {
-            $begin = date('Y-m-d', strtotime('last month'));
-        }
-        else
-        {
-            $begin = date('Y-m-d', strtotime($begin));
-        }
-        if($end == 0)
-        {
-            $end = date('Y-m-d', strtotime('now'));
-        }
-        else
-        {
-            $end = date('Y-m-d', strtotime($end));
-        }
         $this->view->header->title = $this->lang->report->workload;
         $this->view->workload      = $this->report->getWorkload();
         $this->display();
