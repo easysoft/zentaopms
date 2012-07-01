987668855e495a7a62982af7f22605ff86693bba
wangchunsheng <chunsheng@cnezsoft.com>
2012-07-01 02:25:28 +0000

    * adjust the dynamic counts.

--- module/my/config.php
+++ module/my/config.php
@@ -1,7 +1,7 @@
 <?php
 $config->my->editprofile->requiredFields = 'account,realname';
 
-$config->my->dynamicCounts = 15; 
+$config->my->dynamicCounts = 14; 
 $config->my->todoCounts    = 10; 
 $config->my->taskCounts    = 10; 
 $config->my->bugCounts     = 10; 
