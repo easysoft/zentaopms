39e89dba2efa689d26815a62a4dbf91037470e0b
wangchunsheng <chunsheng@cnezsoft.com>
2012-07-01 02:14:45 +0000

    * adjust the dynamic records.

--- module/my/config.php
+++ module/my/config.php
@@ -1,7 +1,7 @@
 <?php
 $config->my->editprofile->requiredFields = 'account,realname';
 
-$config->my->dynamicCounts = 16; 
+$config->my->dynamicCounts = 15; 
 $config->my->todoCounts    = 10; 
 $config->my->taskCounts    = 10; 
 $config->my->bugCounts     = 10; 
