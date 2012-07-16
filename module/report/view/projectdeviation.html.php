5ba97f144ae574db941b4762dcc798ccee719b70
zhujinyong <zhujinyong@cnezsoft.com>
2012-07-16 01:31:58 +0000

    * fix a bug : data perhaps not set.

--- module/report/view/projectdeviation.html.php
+++ module/report/view/projectdeviation.html.php
@@ -30,9 +30,15 @@
           <tr class="a-center">
             <td><?php echo $id;?></td>
             <td align="left"><?php echo $project->name;?></td>
-            <td><?php echo $project->tasks;?></td>
-            <td><?php echo $project->stories;?></td>
-            <td><?php echo $project->bugs;?></td>
+            <td><?php echo isset($project->tasks) ? $project->tasks : 0;?></td>
+            <td><?php echo isset($project->stories) ? $project->stories : 0;?></td>
+            <td><?php echo isset($project->bugs) ? $project->bugs : 0;?></td>
+            <?php
+                $project->devConsumed  = isset($project->devConsumed) ? $project->devConsumed : 0;
+                $project->testConsumed = isset($project->testConsumed) ? $project->testConsumed : 0;
+                $project->estimate     = isset($project->estimate) ? $project->estimate : 0;
+                $project->consumed     = isset($project->consumed) ? $project->consumed : 0;
+            ?>
             <td><?php echo $project->devConsumed;?></td>
             <td><?php echo $project->testConsumed;?></td>
             <td><?php echo round($project->devConsumed / (($project->testConsumed < 1) ? 1 : $project->testConsumed), 1);?></td>
