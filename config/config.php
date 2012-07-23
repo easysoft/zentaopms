57d7a7a81af82941ce016e648b677271e844728d
wangyidong <wangyidong@cnezsoft.com>
2012-07-23 14:51:29 +0000

    * adjust db prefix for extension.

--- config/config.php
+++ config/config.php
@@ -69,12 +69,12 @@ $configRoot = dirname(__FILE__) . DIRECTORY_SEPARATOR;
 $myConfig   = $configRoot . 'my.php';
 if(file_exists($myConfig)) include $myConfig;
 
+/* Set default table prefix. */
+if(!isset($config->db->prefix)) $config->db->prefix = 'zt_';
+
 /* Include extension config files. */
 $extConfigFiles = glob($configRoot . 'ext/*.php');
 if($extConfigFiles) foreach($extConfigFiles as $extConfigFile) include $extConfigFile;
-
-/* Set default table prefix. */
-if(!isset($config->db->prefix)) $config->db->prefix = 'zt_';
 
 /* Define the tables. */
 define('TABLE_COMPANY',       '`' . $config->db->prefix . 'company`');
