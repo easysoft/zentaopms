96f15df6195ef06f0a5f9ce49a4702e1c97f8a5e
zhujinyong <zhujinyong@cnezsoft.com>
2012-08-07 06:27:27 +0000

    * finish task #817.

--- module/report/view/productinfo.html.php
+++ module/report/view/productinfo.html.php
@@ -11,36 +11,35 @@
         <thead>
         <tr class='colhead'>
           <th width='150'><?php echo $lang->product->name;?></th>
-          <th class="w-100px"><?php echo $lang->productplan->common;?></th>
-          <th><?php echo $lang->productplan->desc;?></th>
-          <th class="w-80px"><?php echo $lang->productplan->begin;?></th>
-          <th class="w-80px"><?php echo $lang->productplan->end;?></th>
-          <th width="300" colspan='4'><?php echo $lang->report->details;?></th>
+          <th width='150'><?php echo $lang->product->PO;?></th>
+          <th width="200"><?php echo $lang->productplan->common;?></th>
+          <th class="w-100px"><?php echo $lang->productplan->begin;?></th>
+          <th class="w-100px"><?php echo $lang->productplan->end;?></th>
+          <th width="w-50px"><?php echo $lang->story->statusList['draft'];?></th>
+          <th width="w-50px"><?php echo $lang->story->statusList['active'];?></th>
+          <th width="w-50px"><?php echo $lang->story->statusList['changed'];?></th>
         </tr>
         </thead>
         <tbody>
         <?php foreach($products as $product):?>
           <tr class="a-center">
             <?php $count = isset($product->plans) ? count($product->plans) : 1;?>
-            <td align='left' rowspan="<?php echo $count;?>"><?php echo '<p>' . $product->name . "</p><p>{$lang->product->PO}: " . $users[$product->PO] . '</p>';?></td>
+            <td align='left' rowspan="<?php echo $count;?>"><?php echo '<p>' . $product->name . "</p>";?></td>
+            <td align='left' rowspan="<?php echo $count;?>"><?php echo "<p>" . $users[$product->PO] . '</p>';?></td>
             <?php if(isset($product->plans)):?>
             <?php $id = 1;?>
             <?php foreach($product->plans as $plan):?>
             <?php if($id != 1) echo "<tr class='a-center'>"?>
               <td align='left'><?php echo $plan->title;?></td>
-              <td align='left'><?php echo $plan->desc;?></td>
               <td><?php echo $plan->begin;?></td>
               <td><?php echo $plan->end;?></td>
-              <td align='left'><?php echo $lang->story->statusList['draft']   . ' : ' . (isset($plan->status['draft']) ? $plan->status['draft'] : 0);?></td>
-              <td align='left'><?php echo $lang->story->statusList['active']  . ' : ' . (isset($plan->status['active']) ? $plan->status['active'] : 0);?></td>
-              <td align='left'><?php echo $lang->story->statusList['closed']  . ' : ' . (isset($plan->status['closed']) ? $plan->status['closed'] : 0);?></td>
-              <td align='left'><?php echo $lang->story->statusList['changed'] . ' : ' . (isset($plan->status['changed']) ? $plan->status['changed'] : 0);?></td>
+              <td><?php echo (isset($plan->status['draft']) ? $plan->status['draft'] : 0);?></td>
+              <td><?php echo (isset($plan->status['active']) ? $plan->status['active'] : 0);?></td>
+              <td><?php echo (isset($plan->status['changed']) ? $plan->status['changed'] : 0);?></td>
             <?php if($id != 1) echo "</tr>"?>
             <?php $id ++;?>
             <?php endforeach;?>
             <?php else:?>
-              <td></td>
-              <td></td>
               <td></td>
               <td></td>
               <td></td>
