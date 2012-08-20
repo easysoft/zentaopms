29c34ec21375c3db2b737234b90cc009ccd75c17
areyou123456 <areyou123456>
2012-08-20 09:03:58 +0000

    * finish the task #889

--- module/story/view/affected.html.php
+++ module/story/view/affected.html.php
@@ -1,4 +1,4 @@
-<div class='linkbox'>
+<div style='<?php if(count($story->projects)==0) echo "display:none";?>'>
   <table class='table-1 mt-10px'>
     <caption><?php echo $lang->story->affectedProjects;?></caption>
 
@@ -16,67 +16,79 @@
             <th><?php echo $lang->task->consumed;?></th>
             <th><?php echo $lang->task->left;?></th>
           </tr>
-          <?php if(isset($story->tasks[$projectID])):?>
-          <?php foreach($story->tasks[$projectID] as $task):?> 
-          <tr class='a-center'>
-            <td><?php echo $task->id;?></td>
-            <td class='a-left'><?php echo html::a($this->createLink('task', 'view', "taskID=$task->id"), $task->name, '_blank');?></td>
-            <td><?php echo $users[$task->assignedTo];?></td>
-            <td><?php echo $lang->task->statusList[$task->status];?></td>
-            <td><?php echo $task->consumed;?></td>
-            <td><?php echo $task->left;?></td>
-          </tr>
-          <?php endforeach;?>
-          <?php endif;?>
         </table>
+        <?php if(isset($story->tasks[$projectID])):?>
+        <div class='<?php if(count($story->tasks[$projectID]) > 5)  echo "linkbox";?>'>
+          <table class='table-1'>
+          <?php foreach($story->tasks[$projectID] as $task):?> 
+            <tr class='a-center'>
+              <td><?php echo $task->id;?></td>
+              <td class='a-left'><?php echo html::a($this->createLink('task', 'view', "taskID=$task->id"), $task->name, '_blank');?></td>
+              <td><?php echo $users[$task->assignedTo];?></td>
+              <td><?php echo $lang->task->statusList[$task->status];?></td>
+              <td><?php echo $task->consumed;?></td>
+              <td><?php echo $task->left;?></td>
+            </tr>
+            <?php endforeach;?>
+          </table>
+        </div>
+        <?php endif;?>
       </td>
     </tr>
     <?php endforeach;?>
   </table>
 </div>
-<div class='linkbox'>
+<div style='<?php if(count($story->bugs)==0) echo "display:none";?>'>
   <table class='table-1'>
     <caption><?php echo $lang->story->affectedBugs;?></caption>
-    <tr>
-      <th class='w-p10'><?php echo $lang->bug->id;?></th>
-      <th class='w-p40'><?php echo $lang->bug->title;?></th>
-      <th class='w-p10'><?php echo $lang->bug->status;?></th>
-      <th class='w-p10'><?php echo $lang->bug->openedBy;?></th>
-      <th><?php echo $lang->bug->resolvedBy;?></th>
-      <th><?php echo $lang->bug->resolution;?></th>
-      <th><?php echo $lang->bug->lastEditedBy;?></th>
-    </tr>
-    <?php foreach($story->bugs as $bug):?>
-    <tr class='a-center'>
-      <td><?php echo $bug->id;?></td>
-      <td class='a-left'><?php echo html::a($this->createLink('bug', 'view', "bugID=$bug->id"), $bug->title, '_blank');?></td>
-      <td><?php echo $lang->bug->statusList[$bug->status];?></td>
-      <td><?php echo $users[$bug->openedBy];?></td>
-      <td><?php echo $users[$bug->resolvedBy];?></td>
-      <td><?php echo $lang->bug->resolutionList[$bug->resolution];?></td>
-      <td><?php echo $users[$bug->lastEditedBy];?></td>
-    </tr>
-    <?php endforeach;?>
+      <tr>
+        <th class='w-p10'><?php echo $lang->bug->id;?></th>
+        <th class='w-p40'><?php echo $lang->bug->title;?></th>
+        <th class='w-p10'><?php echo $lang->bug->status;?></th>
+        <th class='w-p10'><?php echo $lang->bug->openedBy;?></th>
+        <th><?php echo $lang->bug->resolvedBy;?></th>
+        <th><?php echo $lang->bug->resolution;?></th>
+        <th><?php echo $lang->bug->lastEditedBy;?></th>
+      </tr>
   </table>
+  <div class= '<?php if(count($story->bugs) > 7) echo "linkbox";?>'>
+    <table class='table-1'>
+    <?php foreach($story->bugs as $bug):?>
+      <tr class='a-center'>
+        <td class='w-p10'><?php echo $bug->id;?></td>
+        <td class='w-p40'><?php echo html::a($this->createLink('bug', 'view', "bugID=$bug->id"), $bug->title, '_blank');?></td>
+        <td class='w-p10'><?php echo $lang->bug->statusList[$bug->status];?></td>
+        <td class='w-p10'><?php echo $users[$bug->openedBy];?></td>
+        <td><?php echo $users[$bug->resolvedBy];?></td>
+        <td><?php echo $lang->bug->resolutionList[$bug->resolution];?></td>
+        <td><?php echo $users[$bug->lastEditedBy];?></td>
+      </tr>
+      <?php endforeach;?>
+    </table>
+  </div>
 </div>
-<div class='linkbox'>
+<div style='<?php if(count($story->cases)==0) echo "display:none";?>'>
   <table class='table-1'>
     <caption><?php echo $lang->story->affectedCases;?></caption>
-    <tr>
-      <th class='w-p10'><?php echo $lang->testcase->id;?></th>
-      <th class='w-p40'><?php echo $lang->testcase->title;?></th>
-      <th class='w-p10'><?php echo $lang->testcase->status;?></th>
-      <th class='w-p10'><?php echo $lang->testcase->openedBy;?></th>
-      <th><?php echo $lang->testcase->lastEditedBy;?></th>
-    </tr>
-    <?php foreach($story->cases as $case):?>
-    <tr class='a-center'>
-      <td><?php echo $case->id;?></td>
-      <td class='a-left'><?php echo html::a($this->createLink('testcase', 'view', "caseID=$case->id"), $case->title, '_blank');?></td>
-      <td><?php echo $lang->testcase->statusList[$case->status];?></td>
-      <td><?php echo $users[$case->openedBy];?></td>
-      <td><?php echo $users[$case->lastEditedBy];?></td>
-    </tr>
-    <?php endforeach;?>
+      <tr>
+        <th class='w-p10'><?php echo $lang->testcase->id;?></th>
+        <th class='w-p40'><?php echo $lang->testcase->title;?></th>
+        <th class='w-p10'><?php echo $lang->testcase->status;?></th>
+        <th class='w-p10'><?php echo $lang->testcase->openedBy;?></th>
+        <th><?php echo $lang->testcase->lastEditedBy;?></th>
+      </tr>
   </table>
+  <div class='<?php if(count($story->cases) > 7)  echo "linkbox";?>'>
+    <table class='table-1'>
+    <?php foreach($story->cases as $case):?>
+      <tr class='a-center'>
+        <td class='w-p10'><?php echo $case->id;?></td>
+        <td class='w-p40'><?php echo html::a($this->createLink('testcase', 'view', "caseID=$case->id"), $case->title, '_blank');?></td>
+        <td class='w-p10'><?php echo $lang->testcase->statusList[$case->status];?></td>
+        <td class='w-p10'><?php echo $users[$case->openedBy];?></td>
+        <td><?php echo $users[$case->lastEditedBy];?></td>
+      </tr>
+      <?php endforeach;?>
+    </table>
+  </div>
 </div>
