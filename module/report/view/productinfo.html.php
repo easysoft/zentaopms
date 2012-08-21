769085669f19a643efc47a3e403cbf15d05171f6
zhujinyong <zhujinyong@cnezsoft.com>
2012-08-21 07:54:00 +0000

    * fix a bug : class error.

--- module/report/view/productinfo.html.php
+++ module/report/view/productinfo.html.php
@@ -12,13 +12,13 @@
         <tr class='colhead'>
           <th width='200'><?php echo $lang->product->name;?></th>
           <th width='120'><?php echo $lang->product->PO;?></th>
-          <th width="300"><?php echo $lang->productplan->common;?></th>
+          <th><?php echo $lang->productplan->common;?></th>
           <th width="150"><?php echo $lang->productplan->begin;?></th>
           <th width="150"><?php echo $lang->productplan->end;?></th>
-          <th width="w-50px"><?php echo $lang->story->statusList['draft'];?></th>
-          <th width="w-50px"><?php echo $lang->story->statusList['active'];?></th>
-          <th width="w-50px"><?php echo $lang->story->statusList['changed'];?></th>
-          <th width="w-50px"><?php echo $lang->story->statusList['closed'];?></th>
+          <th class="w-50px"><?php echo $lang->story->statusList['draft'];?></th>
+          <th class="w-50px"><?php echo $lang->story->statusList['active'];?></th>
+          <th class="w-50px"><?php echo $lang->story->statusList['changed'];?></th>
+          <th class="w-50px"><?php echo $lang->story->statusList['closed'];?></th>
         </tr>
         </thead>
         <tbody>
