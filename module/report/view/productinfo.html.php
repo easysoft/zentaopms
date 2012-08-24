c90800e6cbe19289013806cc4df9d9f0d8d8a1e0
zhujinyong <zhujinyong@cnezsoft.com>
2012-08-24 07:54:02 +0000

    * fix bug #474.

--- module/report/view/productinfo.html.php
+++ module/report/view/productinfo.html.php
@@ -1,4 +1,7 @@
 <?php include '../../common/view/header.html.php';?>
+<style>
+.rowcolor{background:#F9F9F9;}
+</style>
 <table class="cont-lt1">
   <tr valign='top'>
     <td class='side'>
@@ -22,6 +25,7 @@
         </tr>
         </thead>
         <tbody>
+        <?php $color = false;?>
         <?php foreach($products as $product):?>
           <tr class="a-center">
             <?php $count = isset($product->plans) ? count($product->plans) : 1;?>
@@ -30,25 +34,29 @@
             <?php if(isset($product->plans)):?>
             <?php $id = 1;?>
             <?php foreach($product->plans as $plan):?>
-            <?php if($id != 1) echo "<tr class='a-center'>"?>
-              <td align='left'><?php echo $plan->title;?></td>
-              <td><?php echo $plan->begin;?></td>
-              <td><?php echo $plan->end;?></td>
-              <td><?php echo (isset($plan->status['draft']) ? $plan->status['draft'] : 0);?></td>
-              <td><?php echo (isset($plan->status['active']) ? $plan->status['active'] : 0);?></td>
-              <td><?php echo (isset($plan->status['changed']) ? $plan->status['changed'] : 0);?></td>
-              <td><?php echo (isset($plan->status['closed']) ? $plan->status['closed'] : 0);?></td>
-            <?php if($id != 1) echo "</tr>"?>
-            <?php $id ++;?>
+              <?php $class = $color ? 'rowcolor' : '';?>
+              <?php if($id != 1) echo "<tr class='a-center'>"?>
+                <td align='left' class="<?php echo $class;?>"><?php echo $plan->title;?></td>
+                <td class="<?php echo $class;?>"><?php echo $plan->begin;?></td>
+                <td class="<?php echo $class;?>"><?php echo $plan->end;?></td>
+                <td class="<?php echo $class;?>"><?php echo (isset($plan->status['draft']) ? $plan->status['draft'] : 0);?></td>
+                <td class="<?php echo $class;?>"><?php echo (isset($plan->status['active']) ? $plan->status['active'] : 0);?></td>
+                <td class="<?php echo $class;?>"><?php echo (isset($plan->status['changed']) ? $plan->status['changed'] : 0);?></td>
+                <td class="<?php echo $class;?>"><?php echo (isset($plan->status['closed']) ? $plan->status['closed'] : 0);?></td>
+              <?php if($id != 1) echo "</tr>"?>
+              <?php $id ++;?>
+              <?php $color = !$color;?>
             <?php endforeach;?>
             <?php else:?>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
+              <?php $class = $color ? 'rowcolor' : '';?>
+              <td class="<?php echo $class;?>"></td>
+              <td class="<?php echo $class;?>"></td>
+              <td class="<?php echo $class;?>"></td>
+              <td class="<?php echo $class;?>"></td>
+              <td class="<?php echo $class;?>"></td>
+              <td class="<?php echo $class;?>"></td>
+              <td class="<?php echo $class;?>"></td>
+              <?php $color = !$color;?>
             <?php endif;?>
           </tr>
         <?php endforeach;?>
