54195c44e52db4b2dd349a51b7091997e537e4b6
wangchunsheng <chunsheng@cnezsoft.com>
2012-08-30 08:37:10 +0000

    * finish task#882.

--- module/file/view/buildform.html.php
+++ module/file/view/buildform.html.php
@@ -1,22 +1,78 @@
 <div id='fileform'>
-  <?php for($i = 0; $i < $fileCount; $i ++):?>
-  <span class='icon'>
-    <span class='icon-file'></span>
-    <?php echo $lang->file->common . ($i + 1) . " "?>
-  </span>
-  <input type='file' name='files[]' id="file<?php echo $i;?>"  tabindex='-1' />
-  <label id='label<?php echo $i;?>' tabindex='-1'><?php echo $lang->file->label;?></label>
-  <input type='text' name='labels[]' class='text-3' tabindex='-1' /><br />
-  <?php endfor;?>
+  <?php 
+  /* Define the html code of a file row. */
+  $fileRow = <<<EOT
+  <div class='fileBox' id='fileBox\$i'>
+    <span class='icon'><span class='icon-file'></span>{$lang->file->common}<span class='fileID'>\$i</span></span>
+    <input type='file' name='files[]' class='fileControl'  tabindex='-1' />
+    <label tabindex='-1' class='fileLabel'>{$lang->file->label}</label>
+    <input type='text' name='labels[]' class='text-3' tabindex='-1' /> 
+    <input type='button' onclick='addFile(this)' value='add'></input>
+    <input type='button' onclick='delFile(this)'  value='del'></input>
+  </div>
+EOT;
+  for($i = 1; $i <= $fileCount; $i ++) echo str_replace('$i', $i, $fileRow);
+?>
 </div>
 <script language='javascript'>
+/**
+ * Set the width of the file form.
+ * 
+ * @param  float  $percent 
+ * @access public
+ * @return void
+ */
 function setFileFormWidth(percent)
 {
     totalWidth = Math.round($('#fileform').parent().width() * percent);
-    titleWidth = totalWidth - $('#file0').width() - $('#label0').width() - $('.icon').width();
+    titleWidth = totalWidth - $('.fileControl').width() - $('.fileLabel').width() - $('.icon').width();
     if($.browser.mozilla) titleWidth  -= 8;
     if(!$.browser.mozilla) titleWidth -= 12;
     $('#fileform .text-3').css('width', titleWidth + 'px');
 };
+
+/**
+ * Add a file input control.
+ * 
+ * @param  object $clickedButton 
+ * @access public
+ * @return void
+ */
+function addFile(clickedButton)
+{
+    fileRow = <?php echo json_encode($fileRow);?>;
+    fileRow = fileRow.replace('$i', $('.fileID').size() + 1);
+    $(clickedButton).parent().after(fileRow);
+
+    setFileFormWidth(<?php echo $percent;?>);
+    updateID();
+}
+
+/**
+ * Delete a file input control.
+ * 
+ * @param  object $clickedButton 
+ * @access public
+ * @return void
+ */
+function delFile(clickedButton)
+{
+    if($('.fileBox').size() == 1) return;
+    $(clickedButton).parent().remove();
+    updateID();
+}
+
+/**
+ * Update the file id labels.
+ * 
+ * @access public
+ * @return void
+ */
+function updateID()
+{
+    i = 1;
+    $('.fileID').each(function(){$(this).html(i ++)});
+}
+
 $(function(){setFileFormWidth(<?php echo $percent;?>)});
 </script>
