d9b3b7b1e50e52f9a28160775fd9037b53bbbf73
wangchunsheng <chunsheng@cnezsoft.com>
2012-09-02 08:40:32 +0000

    * adjust title.

--- module/story/control.php
+++ module/story/control.php
@@ -368,7 +368,7 @@ class story extends control
         /* Set the menu. */
         $this->product->setMenu($this->product->getPairs(), $product->id);
 
-        $header['title'] = "STORY #$story->id $story->title/$product->name";
+        $header['title'] = "STORY #$story->id $story->title - $product->name";
         $position[]      = html::a($this->createLink('product', 'browse', "product=$product->id"), $product->name);
         $position[]      = $this->lang->story->view;
 
