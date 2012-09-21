da98e09af12ba0fbcbfbe0b6fbb890f97f218ef0
zhujinyong <zhujinyong@cnezsoft.com>
2012-09-21 08:19:22 +0000

    * change locked field type.

--- db/update3.3.sql
+++ db/update3.3.sql
@@ -1,2 +1,3 @@
 ALTER TABLE  `zt_user` ADD  `fails` TINYINT( 5 ) NOT NULL DEFAULT  '0' AFTER  `last` ,
 ADD  `locked` DATE NOT NULL DEFAULT  '0000-00-00' AFTER  `fails`;
+ALTER TABLE  `zt_user` CHANGE  `locked`  `locked` DATETIME NOT NULL DEFAULT  '0000-00-00 00:00:00';
