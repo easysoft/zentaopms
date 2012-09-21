b8e1144ab97533c2cf91c04200f8183ddbf37a0b
zhujinyong <zhujinyong@cnezsoft.com>
2012-09-21 00:59:06 +0000

    * fix bug in login locked sql.

--- db/update3.3.sql
+++ db/update3.3.sql
@@ -1,2 +1,2 @@
-ALTER TABLE  `zt_user` ADD  `fails` TINYINT( 5 ) NOT NULL AFTER  `maxLogin` ,
-ADD  `locked` VARCHAR( 10 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER  `fails`;
+ALTER TABLE  `zt_user` ADD  `fails` TINYINT( 5 ) NOT NULL DEFAULT  '0' AFTER  `last` ,
+ADD  `locked` DATE NOT NULL DEFAULT  '0000-00-00' AFTER  `fails`;
