f2f2e990b914c1b08d48d84572bfd09040b3c599
wangchunsheng <chunsheng@cnezsoft.com>
2012-09-27 09:15:54 +0000

    * adjust the include path.

--- module/misc/view/getsid.html.php
+++ module/misc/view/getsid.html.php
@@ -1,2 +1,2 @@
-<?php include '../../common/view/header.html.php';?>
+<?php include dirname(dirname(dirname(__FILE__))) . '/common/view/header.html.php';?>
 Output from the main view file: <?php echo $sid;?>
