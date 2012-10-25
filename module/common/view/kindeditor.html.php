3889ee0c7a751965e90d9710bed9e301893b6e49
zhujinyong <zhujinyong@cnezsoft.com>
2012-10-25 00:58:20 +0000

    * update kindeditor4.1.3.

--- module/common/view/kindeditor.html.php
+++ module/common/view/kindeditor.html.php
@@ -5,23 +5,27 @@ $method = $this->methodName;
 if(!isset($config->$module->editor->$method)) return;
 $editor = $config->$module->editor->$method;
 $editor['id'] = explode(',', $editor['id']);
+$editorLangs  = array('en' => 'en', 'zh-cn' => 'zh_CN', 'zh-tw' => 'zh_TW');
+$editorLang   = isset($editorLangs[$app->getClientLang()]) ? $editorLangs[$app->getClientLang()] : 'en';
 ?>
-<script src='<?php echo $jsRoot;?>jquery/kindeditor/kindeditor.<?php echo $app->getClientLang();?>.js' type='text/javascript'></script>
+<link rel="stylesheet" href="<?php echo $jsRoot;?>jquery/kindeditor/themes/default/default.css" />
+<script src='<?php echo $jsRoot;?>jquery/kindeditor/kindeditor-min.js' type='text/javascript'></script>
+<script src='<?php echo $jsRoot;?>jquery/kindeditor/lang/<?php echo $editorLang;?>.js' type='text/javascript'></script>
 <script language='javascript'>
 var editor = <?php echo json_encode($editor);?>;
 
 var bugTools =
-[ 'title', 'fontname', 'fontsize','textcolor', 'bgcolor', 'bold', 'italic','underline', 
+[ 'formatblock', 'fontname', 'fontsize','forecolor', 'bgcolor', 'bold', 'italic','underline', 
 'justifyleft', 'justifycenter', 'justifyright', 'insertorderedlist', 'insertunorderedlist', '|',
-'emoticons', 'image', 'link', '|', 'removeformat','undo', 'redo', 'fullscreen', 'source', 'savetemplate', 'about'];
+'emoticons', 'image', 'code', 'link', '|', 'removeformat','undo', 'redo', 'fullscreen', 'source', 'savetemplate', 'about'];
 
 var simpleTools = 
-[ 'title', 'fontname', 'fontsize', 'textcolor', 'bgcolor', 'bold', 'italic','underline', 
+[ 'formatblock', 'fontname', 'fontsize', 'forecolor', 'bgcolor', 'bold', 'italic','underline', 
 'justifyleft', 'justifycenter', 'justifyright', 'insertorderedlist', 'insertunorderedlist', '|',
-'emoticons', 'image', 'link', '|', 'removeformat','undo', 'redo', 'fullscreen', 'source', 'about'];
+'emoticons', 'image', 'code', 'link', '|', 'removeformat','undo', 'redo', 'fullscreen', 'source', 'about'];
 
 var fullTools = 
-[ 'title', 'fontname', 'fontsize','textcolor', '|',
+[ 'formatblock', 'fontname', 'fontsize','forecolor', '|',
 'bgcolor', 'bold', 'italic','underline', '|',
 'justifyleft', 'justifycenter', 'justifyright', 'justifyfull', '|',
 'insertorderedlist', 'insertunorderedlist', '|',
@@ -39,12 +43,27 @@ $(document).ready(function()
         editorTool = simpleTools;
         if(editor.tools == 'bugTools')  editorTool = bugTools;
         if(editor.tools == 'fullTools') editorTool = fullTools;
-        KE.show({id:editorID, items:editorTool, filterMode:true, urlType:'relative', imageUploadJson: createLink('file', 'ajaxUpload')});
 
-        $('form').submit(function() 
-        {
-            KE.util.setData(editorID);
-        })
+        KindEditor.ready(function(K){
+            K.create('#'+editor.id,{
+                items:editorTool,
+                filterMode:true, 
+                htmlTags :{
+                'pre' : ['class'],
+                'p,span,h1,h2,h3,h4,em,u,strong,br,ol,ul,li,img,a': ['/']
+                },
+                cssPath:['<?php echo $jsRoot?>jquery/kindeditor/plugins/code/prettify.css'],
+                urlType:'relative', 
+                uploadJson: createLink('file', 'ajaxUpload'),
+                allowFileManager:true,
+                langType:'<?php echo $editorLang?>'
+            });
+
+            $('form').submit(function() 
+            {
+                K.sync('#'+editor.id);
+            });
+        });
     })
-})  
+})
 </script>
