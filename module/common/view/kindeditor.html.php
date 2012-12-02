1316901b23a19250925121a5e31643b379f7ffb9
wangchunsheng <chunsheng@cnezsoft.com>
2012-12-02 00:29:52 +0000

    * adjust the tool items of kindeditor.  * adjust the kindeditor path.

--- module/common/view/kindeditor.html.php
+++ module/common/view/kindeditor.html.php
@@ -8,33 +8,31 @@ $editor['id'] = explode(',', $editor['id']);
 $editorLangs  = array('en' => 'en', 'zh-cn' => 'zh_CN', 'zh-tw' => 'zh_TW');
 $editorLang   = isset($editorLangs[$app->getClientLang()]) ? $editorLangs[$app->getClientLang()] : 'en';
 ?>
-<link rel="stylesheet" href="<?php echo $jsRoot;?>jquery/kindeditor/themes/default/default.css" />
-<script src='<?php echo $jsRoot;?>jquery/kindeditor/kindeditor-min.js' type='text/javascript'></script>
-<script src='<?php echo $jsRoot;?>jquery/kindeditor/lang/<?php echo $editorLang;?>.js' type='text/javascript'></script>
+<link rel="stylesheet" href="<?php echo $jsRoot;?>kindeditor/themes/default/default.css" />
+<script src='<?php echo $jsRoot;?>kindeditor/kindeditor-min.js' type='text/javascript'></script>
+<script src='<?php echo $jsRoot;?>kindeditor/lang/<?php echo $editorLang;?>.js' type='text/javascript'></script>
 <script language='javascript'>
 var editor = <?php echo json_encode($editor);?>;
 
 var bugTools =
-[ 'formatblock', 'fontname', 'fontsize','forecolor', 'bgcolor', 'bold', 'italic','underline', 
+[ 'formatblock', 'fontname', 'fontsize', '|', 'forecolor', 'hilitecolor', 'bold', 'italic','underline', '|', 
 'justifyleft', 'justifycenter', 'justifyright', 'insertorderedlist', 'insertunorderedlist', '|',
 'emoticons', 'image', 'code', 'link', '|', 'removeformat','undo', 'redo', 'fullscreen', 'source', 'savetemplate', 'about'];
 
 var simpleTools = 
-[ 'formatblock', 'fontname', 'fontsize', 'forecolor', 'bgcolor', 'bold', 'italic','underline', 
+[ 'formatblock', 'fontname', 'fontsize', '|', 'forecolor', 'hilitecolor', 'bold', 'italic','underline', '|', 
 'justifyleft', 'justifycenter', 'justifyright', 'insertorderedlist', 'insertunorderedlist', '|',
 'emoticons', 'image', 'code', 'link', '|', 'removeformat','undo', 'redo', 'fullscreen', 'source', 'about'];
 
 var fullTools = 
-[ 'formatblock', 'fontname', 'fontsize','forecolor', '|',
-'bgcolor', 'bold', 'italic','underline', '|',
+[ 'formatblock', 'fontname', 'fontsize', 'lineheight', '|', 'forecolor', 'hilitecolor', '|', 'bold', 'italic','underline', 'strikethrough', '|',
 'justifyleft', 'justifycenter', 'justifyright', 'justifyfull', '|',
 'insertorderedlist', 'insertunorderedlist', '|',
-'emoticons', 'image','link', 'unlink', '|',
-'removeformat','undo', 'redo',  'fullscreen', 'source', 'about', '-',
-'cut', 'copy', 'paste', 'plainpaste', 'wordpaste', '|',
+'emoticons', 'image', 'insertfile', 'hr', '|', 'link', 'unlink', '/',
+'undo', 'redo', '|', 'selectall', 'cut', 'copy', 'paste', '|', 'plainpaste', 'wordpaste', '|', 'removeformat', 'clearhtml','quickformat', '|',
 'indent', 'outdent', 'subscript', 'superscript', '|',
-'selectall', 'strikethrough', 'removeformat', '|',
-'flash', 'media', 'advtable', 'hr', 'print'];
+'table', 'code', '|', 'pagebreak', 'anchor', '|', 
+'fullscreen', 'source', 'preview', 'about'];
 
 $(document).ready(function() 
 {
@@ -48,10 +46,6 @@ $(document).ready(function()
             editor = K.create('#'+editor.id,{
                 items:editorTool,
                 filterMode:true, 
-                htmlTags :{
-                'pre' : ['class'],
-                'p,span,h1,h2,h3,h4,em,u,strong,br,ol,ul,li,img,a': ['/']
-                },
                 cssPath:['<?php echo $jsRoot?>jquery/kindeditor/plugins/code/prettify.css'],
                 urlType:'relative', 
                 uploadJson: createLink('file', 'ajaxUpload'),
