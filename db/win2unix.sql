edf9c7eac88afa47767312ec9dd26b8298a21836
wangyidong <wangyidong@cnezsoft.com>
2012-12-05 06:21:11 +0000

    + add win to unix sql.

---
+++ db/win2unix.sql
@@ -0,0 +1,15 @@
+RENAME TABLE `zt_casestep` TO `zt_caseStep`;
+RENAME TABLE `zt_doclib` TO `zt_docLib`;
+RENAME TABLE `zt_grouppriv` TO `zt_groupPriv`;
+RENAME TABLE `zt_productplan` TO `zt_productPlan`;
+RENAME TABLE `zt_projectproduct` TO `zt_projectProduct`;
+RENAME TABLE `zt_projectstory` TO `zt_projectStory`;
+RENAME TABLE `zt_relationoftasks` TO `zt_relationOfTasks`;
+RENAME TABLE `zt_storyspec` TO `zt_storySpec`;
+RENAME TABLE `zt_taskestimate` TO `zt_taskEstimate`;
+RENAME TABLE `zt_testresult` TO `zt_testResult`;
+RENAME TABLE `zt_testrun` TO `zt_testRun`;
+RENAME TABLE `zt_testtask` TO `zt_testTask`;
+RENAME TABLE `zt_usergroup` TO `zt_userGroup`;
+RENAME TABLE `zt_userquery` TO `zt_userQuery`;
+RENAME TABLE `zt_usertpl` TO `zt_userTPL`;
