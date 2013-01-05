fa7c8ad84600f666ea0b91d95af2ecc1e77ed400
zhujinyong <zhujinyong@cnezsoft.com>
2013-01-05 05:51:16 +0000

    + add role field.

--- db/update4.0.beta2.sql
+++ db/update4.0.beta2.sql
@@ -1,1 +1,2 @@
 ALTER TABLE  `zt_build` CHANGE  `desc`  `desc` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE  `zt_group` ADD  `role` CHAR( 10 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
