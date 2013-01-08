9d9de72cf646baaa69c7ef0efcab73ebc3bedfa5
zhujinyong <zhujinyong@cnezsoft.com>
2013-01-08 07:13:54 +0000

    * change table task_estimate.

--- db/update4.0.beta2.sql
+++ db/update4.0.beta2.sql
@@ -1,3 +1,5 @@
 ALTER TABLE  `zt_build` CHANGE  `desc`  `desc` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
 ALTER TABLE  `zt_group` ADD  `role` CHAR( 10 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE  `zt_taskestimate` CHANGE  `estimater`  `account` CHAR( 30 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  '';
+ALTER TABLE  `zt_taskestimate` ADD  `consumed` TINYINT( 3 ) UNSIGNED NOT NULL AFTER  `estimate`;
 UPDATE `zt_group` SET `role` = 'guest' WHERE `name` = 'guest';
