24fb9a6236ab955098efa2773c2357eca42c36ed
zhujinyong <zhujinyong@cnezsoft.com>
2013-01-08 08:37:17 +0000

    * change estimate to left.

--- db/update4.0.beta2.sql
+++ db/update4.0.beta2.sql
@@ -3,3 +3,4 @@ ALTER TABLE  `zt_group` ADD  `role` CHAR( 10 ) CHARACTER SET utf8 COLLATE utf8_g
 ALTER TABLE  `zt_taskestimate` CHANGE  `estimater`  `account` CHAR( 30 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  '';
 ALTER TABLE  `zt_taskestimate` ADD  `consumed` TINYINT( 3 ) UNSIGNED NOT NULL AFTER  `estimate`;
 UPDATE `zt_group` SET `role` = 'guest' WHERE `name` = 'guest';
+ALTER TABLE  `zt_taskestimate` CHANGE  `estimate`  `left` TINYINT( 3 ) UNSIGNED NOT NULL DEFAULT  '0';
