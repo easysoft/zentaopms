7e10192cbf1d6b307125eb0b408f08ee8c9e5bb0
wangchunsheng <chunsheng@cnezsoft.com>
2013-01-09 02:33:04 +0000

    * change the taskestimate instead taskEstimate.

--- db/update4.0.beta2.sql
+++ db/update4.0.beta2.sql
@@ -1,7 +1,7 @@
 ALTER TABLE  `zt_build` CHANGE  `desc`  `desc` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
 ALTER TABLE  `zt_group` ADD  `role` CHAR( 10 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
-ALTER TABLE  `zt_taskestimate` CHANGE  `estimater`  `account` CHAR( 30 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  '';
-ALTER TABLE  `zt_taskestimate` ADD  `consumed` TINYINT( 3 ) UNSIGNED NOT NULL AFTER  `estimate`;
+ALTER TABLE  `zt_taskEstimate` CHANGE  `estimater`  `account` CHAR( 30 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  '';
+ALTER TABLE  `zt_taskEstimate` ADD  `consumed` TINYINT( 3 ) UNSIGNED NOT NULL AFTER  `estimate`;
 UPDATE `zt_group` SET `role` = 'guest' WHERE `name` = 'guest';
-ALTER TABLE  `zt_taskestimate` CHANGE  `estimate`  `left` TINYINT( 3 ) UNSIGNED NOT NULL DEFAULT  '0';
-ALTER TABLE  `zt_taskestimate` CHANGE  `date`  `date` DATETIME NOT NULL;
+ALTER TABLE  `zt_taskEstimate` CHANGE  `estimate`  `left` TINYINT( 3 ) UNSIGNED NOT NULL DEFAULT  '0';
+ALTER TABLE  `zt_taskEstimate` CHANGE  `date`  `date` DATETIME NOT NULL;
