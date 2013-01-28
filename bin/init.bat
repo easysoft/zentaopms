cc84bba6b5d2f5b7e9472fe5295bc429a023310e
zhujinyong <zhujinyong@cnezsoft.com>
2013-01-28 05:14:34 +0000

    + add cron.bat.

--- bin/init.bat
+++ bin/init.bat
@@ -15,8 +15,8 @@ if not exist %phpcli% (
 )
 :: get requestType
 SET requestType= 'PATH_INFO' 
-for /f "tokens=3" %%f in ('find /c "'PATH_INFO'" "..\config\my.php"') do set findNum=%%f
-if %findNum% == 0 SET requestType= 'GET'
+for /f "tokens=3" %%f in ('find /c "'PATH_INFO'" "..\config\my.php"') do set count=%%f
+if %count% == 0 SET requestType= 'GET'
 
 ::ztcli
 SET ztcli= %phpcli% ztcli $*
@@ -45,5 +45,16 @@ if %requestType% == 'PATH_INFO' (
 )
 echo %checkdb% > checkdb.bat
 echo checkdb.bat ok
+
+::cron
+if not exist cron md cron
+echo # system cron. > cron\sys.cron
+echo # minute hour day month week  command. >> cron\sys.cron
+echo 1 1  * * *   %phpcli% backup.php      # backup database and file. >> cron\sys.cron
+echo 1 23 * * *   %phpcli% computeburn.php # compute burndown chart.  >> cron\sys.cron
+
+SET cron= %phpcli% php\crond.php
+echo %cron% > crond.bat
+echo cron.bat ok
 
 pause
