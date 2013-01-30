884dfdaafac18dbbc43439e360d25d2f48483c72
wangchunsheng <chunsheng@cnezsoft.com>
2013-01-30 05:21:13 +0000

    * adjust code.  * fix the error of sys.cron.  * use absolute path when create sys.cron.

--- bin/init.bat
+++ bin/init.bat
@@ -2,9 +2,10 @@
 SET PATH=%systemRoot%;%systemRoot%\system32;%PATH%
 SET lastDir=%cd%
 SET baseDir=%~dp0
-%baseDir:~0,2%
+SET cronDir=%baseDir%cron\
+set sysCron=%cronDir%sys.cron
 
-::get phpcli
+:: get phpcli
 SET phpcli=%1
 :input
 IF "%1"=="" SET /P phpcli="Please input your php path:(example: c:\windows\php.exe)"
@@ -15,19 +16,19 @@ if not exist %phpcli% (
 :: get requestType
 SET requestType= 'PATH_INFO' 
 for /f "tokens=3" %%f in ('find /c "'PATH_INFO'" "%baseDir%..\config\my.php"') do set count=%%f
-if %count% == 0 SET requestType= 'GET'
+if %count% == 0 SET requestType='GET'
 
-::ztcli
+:: create ztcli
 SET ztcli= %phpcli% %baseDir%ztcli %*
 echo %ztcli% > %baseDir%ztcli.bat
 echo ztcli.bat ok
 
-::backup database
+:: create backup.bat
 SET backup= %phpcli% %baseDir%php\backup.php 
 echo %backup% > %baseDir%backup.bat
 echo backup.bat ok
 
-::compute burn
+:: create computeburn.bat
 if %requestType% == 'PATH_INFO' (
   SET computeburn= %phpcli% %baseDir%ztcli "http://localhost/project-computeburn"
 )else (
@@ -36,7 +37,7 @@ if %requestType% == 'PATH_INFO' (
 echo %computeburn% > %baseDir%computeburn.bat
 echo computeburn.bat ok
 
-::check database
+:: create checkdb.bat
 if %requestType% == 'PATH_INFO' (
   SET checkdb= %phpcli% %baseDir%ztcli "http://localhost/admin-checkdb"
 )else (
@@ -45,15 +46,17 @@ if %requestType% == 'PATH_INFO' (
 echo %checkdb% > %baseDir%checkdb.bat
 echo checkdb.bat ok
 
-::cron
-if not exist cron md cron
-echo # system cron. > cron\sys.cron
-echo # minute hour day month week  command. >> cron\sys.cron
-echo 1 1  * * *   %phpcli% %baseDir%backup.php      # backup database and file. >> cron\sys.cron
-echo 1 23 * * *   %phpcli% %baseDir%computeburn.php # compute burndown chart.  >> cron\sys.cron
-
+:: create crond.bat
 SET cron= %phpcli% %baseDir%php\crond.php
 echo %cron% > %baseDir%crond.bat
-echo cron.bat ok
+echo crond.bat ok
 
+:: create system cron.
+if not exist %cronDir% md %cronDir%
+echo # system cron. > %sysCron%
+echo # minute hour day month week  command. >> %sysCron%
+echo 1 1  * * *   %baseDir%backup.bat      # backup database and file. >> %sysCron%
+echo 1 23 * * *   %baseDir%computeburn.bat # compute burndown chart.   >> %sysCron%
+
+:: return 0 when success.
 exit /b 0
