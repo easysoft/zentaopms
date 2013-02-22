1eaf070881c37e1aa3cb2b9ef280522332029780
wangchunsheng <chunsheng@cnezsoft.com>
2013-02-22 06:09:41 +0000

    * add the port checking and setting feature for windows xampp package.

--- build/windows/services/remove.bat
+++ build/windows/services/remove.bat
@@ -5,10 +5,14 @@ net stop apachezt
 echo removing apache 2.4
 ..\apache\bin\httpd -k uninstall -n apachezt
 
+del port.apache
+
 echo 
 echo stopping mysql
 net stop mysqlzt
 echo removing mysql
 ..\mysql\bin\mysqld.exe --remove mysqlzt
+
+del port.mysql
 
 pause
