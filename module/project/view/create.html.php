c5b0382c252389c9771e0df8ddfe7d312acba104
chencongzhi520@gmail.com <azhi@cnezsoft.com>
2013-02-22 01:18:19 +0000

    * adjust the colorbox of project-tips.

--- module/project/view/create.html.php
+++ module/project/view/create.html.php
@@ -20,7 +20,7 @@ var projectID = <?php echo $projectID;?>;
 defaultURL    = createLink('project', 'task', 'projectID=' + projectID);
 $(document).ready(function() 
 {
-    $.fn.colorbox({html:tips, open:true, width:480, height:280});
+    $.fn.colorbox({html:tips, open:true, transition:'none', width:450, height:250});
     setTimeout( function() {location.href=defaultURL}, 5000);
 });
 </script>
