a0148bdc736cdbf9c954003a51a4ac2fd736a922
zhujinyong <zhujinyong@cnezsoft.com>
2013-02-26 01:23:27 +0000

    * refresh when close colorbox.

--- module/project/js/task.js
+++ module/project/js/task.js
@@ -2,7 +2,7 @@ $(function()
 {
     setOuterBox();
     if(browseType == 'bysearch') ajaxGetSearchForm();
-    $('.iframe').colorbox({width:900, height:500, iframe:true});
+    $('.iframe').colorbox({width:900, height:500, iframe:true, onCleanup:function(){parent.location.href=parent.location.href;}});
 });
 
 function showProject()
