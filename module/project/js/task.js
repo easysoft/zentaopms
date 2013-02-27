6490992b00c31b5d9a07fd886f8316783d803c3a
zhujinyong <zhujinyong@cnezsoft.com>
2013-02-27 06:05:03 +0000

    - remove transition.

--- module/project/js/task.js
+++ module/project/js/task.js
@@ -2,7 +2,7 @@ $(function()
 {
     setOuterBox();
     if(browseType == 'bysearch') ajaxGetSearchForm();
-    $('.iframe').colorbox({width:900, height:500, iframe:true, onCleanup:function(){parent.location.href=parent.location.href;}});
+    $('.iframe').colorbox({width:900, height:500, iframe:true, transition:'none', onCleanup:function(){parent.location.href=parent.location.href;}});
 });
 
 function showProject()
