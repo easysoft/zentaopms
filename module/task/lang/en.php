7e928b89b7b2f9609afeced211dea607ea3432a3
wangchunsheng <chunsheng@cnezsoft.com>
2013-03-02 13:16:54 +0000

    - remove the totalConsumed item.

--- module/task/lang/en.php
+++ module/task/lang/en.php
@@ -45,7 +45,6 @@ $lang->task->estimateAB        = 'Est';
 $lang->task->left              = 'Left';
 $lang->task->leftAB            = 'Left';
 $lang->task->consumed          = 'Consumed';
-$lang->task->totalConsumed     = 'Total Consumed';
 $lang->task->consumedAB        = 'Use';
 $lang->task->hour              = 'Hour';
 $lang->task->consumedThisTime  = 'Effort';
