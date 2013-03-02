ea9aff842534f4e0106745a396199a40ad3c44f5
zhujinyong <zhujinyong@cnezsoft.com>
2013-03-02 14:44:53 +0000

    * change record to recordEstimate.

--- module/task/view/view.html.php
+++ module/task/view/view.html.php
@@ -24,13 +24,13 @@
   if(!$task->deleted)
   {
       ob_start();
-      common::printIcon('task', 'assignTo', "projectID=$task->project&taskID=$task->id", $task, 'button', '', '', 'iframe', true);
-      common::printIcon('task', 'start',    "taskID=$task->id", $task, 'button', '', '', 'iframe', true);
-      common::printIcon('task', 'record',   "taskID=$task->id", $task, 'button', '', '', 'iframe', true);
-      common::printIcon('task', 'finish',   "taskID=$task->id", $task, 'button', '', '', 'iframe', true);
-      common::printIcon('task', 'close',    "taskID=$task->id", $task, 'button', '', '', 'iframe', true);
-      common::printIcon('task', 'activate', "taskID=$task->id", $task, 'button', '', '', 'iframe', true);
-      common::printIcon('task', 'cancel',   "taskID=$task->id", $task, 'button', '', '', 'iframe', true);
+      common::printIcon('task', 'assignTo',       "projectID=$task->project&taskID=$task->id", $task, 'button', '', '', 'iframe', true);
+      common::printIcon('task', 'start',          "taskID=$task->id", $task, 'button', '', '', 'iframe', true);
+      common::printIcon('task', 'recordEstimate', "taskID=$task->id", $task, 'button', '', '', 'iframe', true);
+      common::printIcon('task', 'finish',         "taskID=$task->id", $task, 'button', '', '', 'iframe', true);
+      common::printIcon('task', 'close',          "taskID=$task->id", $task, 'button', '', '', 'iframe', true);
+      common::printIcon('task', 'activate',       "taskID=$task->id", $task, 'button', '', '', 'iframe', true);
+      common::printIcon('task', 'cancel',         "taskID=$task->id", $task, 'button', '', '', 'iframe', true);
 
       common::printDivider();
       common::printIcon('task', 'edit',  "taskID=$task->id");
