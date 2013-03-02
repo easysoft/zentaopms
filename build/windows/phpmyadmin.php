a50933ae1a09de34697559d1978cd73e81578275
chencongzhi520@gmail.com <azhi@cnezsoft.com>
2013-03-02 06:25:39 +0000

    * add port config.

--- build/windows/phpmyadmin.php
+++ build/windows/phpmyadmin.php
@@ -2,6 +2,7 @@
 $cfg['Servers'][1]['auth_type']       = 'config';
 $cfg['Servers'][1]['user']            = 'root';
 $cfg['Servers'][1]['password']        = ''; 
+$cfg['Servers'][1]['port']            = '3306';
 $cfg['Servers'][1]['host']            = 'localhost';
 $cfg['Servers'][1]['connect_type']    = 'tcp';
 $cfg['Servers'][1]['extension']       = 'mysqli';
