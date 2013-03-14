faadff0e5f5af5d7aebfe5bc481db951399a3b21
wangyidong <wangyidong@cnezsoft.com>
2013-03-14 03:01:34 +0000

    * finish task #1177.

--- module/build/view/create.html.php
+++ module/build/view/create.html.php
@@ -43,81 +43,60 @@
     <tr>
       <th class='rowhead'><?php echo $lang->build->linkStoriesAndBugs;?></th>
       <td>
-        <div class="w-p90">
 
-          <div class='half-left'>
-            <table class='mainTable'>
-              <tr style='border-bottom:none'>
-                <td style='border-bottom:none; padding:0px'>
-                  <table class='headTable'>
-                    <caption><?php echo $lang->build->linkStories;?></caption>
-                    <tr>
-                      <th class='w-id a-left'> <?php echo html::selectAll('story', 'checkbox') . $lang->idAB;?></th>
-                      <th><?php echo $lang->story->title;?></th>
-                      <th class='w-hour'><?php echo $lang->statusAB;?></th>
-                      <th class='w-100px'><?php echo $lang->story->stageAB;?></th>
-                    </tr>
-                  </table>
-                </td>
-              </tr>
-              <tr style='border-bottom:none'>
-                <td style='border-bottom:none; padding:0px'>
-                  <div class='contentDiv'>
-                    <table class='f-left table-1 fixed' id='story'>
-                      <?php foreach($stories as $key => $story):?>
-                      <?php $storyLink = $this->createLink('story', 'view', "storyID=$story->id", '', true); ?>
-                      <tr class='a-center'>
-                        <td class='w-id a-left'>
-                          <input type='checkbox' name='stories[]' value="<?php echo $story->id;?>" <?php if($story->stage == 'developed' or $story->status == 'closed') echo 'checked';?>> <?php echo sprintf('%03d', $story->id);?>
-                        </td>
-                        <td class='a-left nobr'><?php echo html::a($storyLink,$story->title, '', "class='preview'");?></td>
-                        <td class='<?php echo $story->status;?> w-50px'><?php echo $lang->story->statusList[$story->status];?></td>
-                        <td class='w-80px'><?php echo $lang->story->stageList[$story->stage];?></td>
-                      </tr>
-                    <?php endforeach;?>
-                    </table>
-                  </div>
-                </td>
-              </tr>
-            </table>
-          </div>
-
-          <div class='half-right'>
-            <table class='mainTable'>
-              <tr style='border-bottom:none'>
-                <td style='border-bottom:none; padding:0px'>
-                  <table class='headTable'>
-                    <caption><?php echo $lang->build->linkBugs;?></caption>
-                    <tr>
-                      <th class='w-id a-left'><?php echo html::selectAll('bug', 'checkbox') . $lang->idAB;?></th>
-                      <th><?php echo $lang->bug->title;?></th>
-                      <th class='w-100px'><?php echo $lang->bug->status;?></th>
-                    </tr>
-                  </table>
-                </td>
-              </tr>
-              <tr style='border-bottom:none'>
-                <td style='border-bottom:none; padding:0px'>
-                  <div class='contentDiv'>
-                    <table class='f-left table-1 fixed' id='bug'>
-                      <?php foreach($bugs as $bug):?>
-                      <?php $bugLink = $this->createLink('bug', 'view', "bugID=$bug->id", '', true);?>
-                      <tr class='a-center'>
-                        <td class='w-id a-left'>
-                          <input type='checkbox' name='bugs[]' value="<?php echo $bug->id;?>" <?php if($bug->status == 'resolved' or $bug->status == 'closed') echo "checked";?>> <?php echo sprintf('%03d', $bug->id);?>
-                        </td>
-                        <td class='a-left nobr'><?php echo html::a($bugLink, $bug->title, '', "class='preview'");?></td>
-                        <td class='w-80px'><?php echo $lang->bug->statusList[$bug->status];?></td>
-                      </tr>
+        <table class='w-p90 bd-none' style='margin-bottom:0px'>
+          <tr>
+            <td class='w-p50'>
+              <table class='mainTable'>
+                <caption><?php echo html::selectAll('story', 'checkbox') . ' ' . $lang->build->linkStories;?></caption>
+                <tr style='border-bottom:none'>
+                  <td style='border-bottom:none; padding:0px'>
+                    <div class='contentDiv'>
+                      <table class='table-1 fixed bd-none' id='story'>
+                        <?php foreach($stories as $key => $story):?>
+                        <?php $storyLink = $this->createLink('story', 'view', "storyID=$story->id", '', true); ?>
+                        <tr class='a-center'>
+                          <td class='w-id a-left'>
+                            <input type='checkbox' name='stories[]' value="<?php echo $story->id;?>" <?php if($story->stage == 'developed' or $story->status == 'closed') echo 'checked';?>> <?php echo sprintf('%03d', $story->id);?>
+                          </td>
+                          <td class='a-left nobr'><?php echo html::a($storyLink,$story->title, '', "class='preview'");?></td>
+                          <td class='<?php echo $story->status;?> w-50px'><?php echo $lang->story->statusList[$story->status];?></td>
+                          <td class='w-80px'><?php echo $lang->story->stageList[$story->stage];?></td>
+                        </tr>
                       <?php endforeach;?>
-                    </table>
-                  </div>
-                </td>
-              </tr>
-            </table>
-          </div>
+                      </table>
+                    </div>
+                  </td>
+                </tr>
+              </table>
+            </td>
 
-        </div>
+            <td class='w-p50'>
+              <table class='mainTable'>
+                <caption><?php echo html::selectAll('bug', 'checkbox') . ' ' . $lang->build->linkBugs;?></caption>
+                <tr style='border-bottom:none'>
+                  <td style='border-bottom:none; padding:0px'>
+                    <div class='contentDiv'>
+                      <table class='table-1 fixed bd-none' id='bug'>
+                        <?php foreach($bugs as $bug):?>
+                        <?php $bugLink = $this->createLink('bug', 'view', "bugID=$bug->id", '', true);?>
+                        <tr class='a-center'>
+                          <td class='w-id a-left'>
+                            <input type='checkbox' name='bugs[]' value="<?php echo $bug->id;?>" <?php if($bug->status == 'resolved' or $bug->status == 'closed') echo "checked";?>> <?php echo sprintf('%03d', $bug->id);?>
+                          </td>
+                          <td class='a-left nobr'><?php echo html::a($bugLink, $bug->title, '', "class='preview'");?></td>
+                          <td class='w-80px'><?php echo $lang->bug->statusList[$bug->status];?></td>
+                        </tr>
+                        <?php endforeach;?>
+                      </table>
+                    </div>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+
       </td>
     </tr>
     <tr>
--- module/build/view/edit.html.php
+++ module/build/view/edit.html.php
@@ -43,77 +43,79 @@
     <tr>
       <th class='rowhead'><?php echo $lang->build->linkStoriesAndBugs;?></th>
       <td>
-        <div class="w-p90">
 
-          <div class='half-left'>
-            <table class='mainTable'>
-              <tr style='border-bottom:none'>
-                <td style='border-bottom:none;padding:0px'>
-                  <table class='headTable'>
-                    <caption><?php echo $lang->build->linkStories;?></caption>
-                    <tr>
-                      <th class='w-id a-left'><?php echo html::selectAll('story', 'checkbox') . $lang->idAB;?></th>
-                      <th><?php echo $lang->story->title;?></th>
-                      <th class='w-hour'><?php echo $lang->statusAB;?></th>
-                      <th class='w-100px'><?php echo $lang->story->stageAB;?></th>
-                    </tr>
-                  </table>
-                </td>
-              </tr>
-              <tr style='border-bottom:none'>
-                <td style='border-bottom:none; padding:0px'>
-                  <div class="contentDiv">
-                    <table class='f-left table-1 fixed'>
-                      <?php foreach($stories as $key => $story):?>
-                      <?php $storyLink = $this->createLink('story', 'view', "storyID=$story->id", '', true);?>
-                      <tr class='a-center'>
-                        <td class='w-id a-left' id='story'><input type='checkbox' name='stories[]' value="<?php echo $story->id;?>" <?php if(strpos(',' . $build->stories . ',', ',' . $story->id . ',') !== false) echo 'checked';?>> <?php echo sprintf('%03d', $story->id);?></td>
-                        <td class='a-left nobr'><?php echo html::a($storyLink, $story->title, '', "class='preview'");?></td>
-                        <td class='<?php echo $story->status;?> w-50px'><?php echo $lang->story->statusList[$story->status];?></td>
-                        <td class='w-80px'><?php echo $lang->story->stageList[$story->stage];?></td>
+        <table class='w-p90 bd-none' style='margin-bottom:0px'>
+          <tr>
+            <td class='w-p50'>
+              <table class='mainTable bd-none'>
+                <tr style='border-bottom:none'>
+                  <td style='border-bottom:none;padding:0px'>
+                    <table class='headTable'>
+                      <caption><?php echo $lang->build->linkStories;?></caption>
+                      <tr style='border-left:1px solid #e4e4e4; border-right:1px solid #e4e4e4;'>
+                        <th class='w-id a-left'><?php echo html::selectAll('story', 'checkbox') . ' ' . $lang->idAB;?></th>
+                        <th><?php echo $lang->story->title;?></th>
+                        <th class='w-hour'><?php echo $lang->statusAB;?></th>
+                        <th class='w-100px'><?php echo $lang->story->stageAB;?></th>
                       </tr>
-                      <?php endforeach;?>
                     </table>
-                  </div>
-                </td>
-              </tr>
-            </table>
-          </div>
+                  </td>
+                </tr>
+                <tr style='border-bottom:none'>
+                  <td style='border-bottom:none; padding:0px'>
+                    <div class="contentDiv" style='border:1px solid #e4e4e4;'>
+                      <table class='f-left table-1 fixed bd-none'>
+                        <?php foreach($stories as $key => $story):?>
+                        <?php $storyLink = $this->createLink('story', 'view', "storyID=$story->id", '', true);?>
+                        <tr class='a-center'>
+                          <td class='w-id a-left' id='story'><input type='checkbox' name='stories[]' value="<?php echo $story->id;?>" <?php if(strpos(',' . $build->stories . ',', ',' . $story->id . ',') !== false) echo 'checked';?>> <?php echo sprintf('%03d', $story->id);?></td>
+                          <td class='a-left nobr'><?php echo html::a($storyLink, $story->title, '', "class='preview'");?></td>
+                          <td class='<?php echo $story->status;?> w-50px'><?php echo $lang->story->statusList[$story->status];?></td>
+                          <td class='w-80px'><?php echo $lang->story->stageList[$story->stage];?></td>
+                        </tr>
+                        <?php endforeach;?>
+                      </table>
+                    </div>
+                  </td>
+                </tr>
+              </table>
+            </td>
 
-          <div class='half-right'>
-            <table class='mainTable'>
-              <tr style='border-bottom:none'>
-                <td style='border-bottom:none; padding:0px'>
-                  <table class='headTable'>
-                    <caption><?php echo $lang->build->linkBugs;?></caption>
-                    <tr>
-                      <th class='w-id a-left'><?php echo html::selectAll('bug', 'checkbox') . $lang->idAB;?></th>
-                      <th><?php echo $lang->bug->title;?></th>
-                      <th class='w-100px'><?php echo $lang->bug->status;?></th>
-                    </tr>
-                  </table>
-                </td>
-              </tr>
-              <tr style='border-bottom:none'>
-                <td style='border-bottom:none; padding:0px'>
-                  <div class='contentDiv'>
-                    <table class='f-left table-1 fixed'>
-                      <?php foreach($bugs as $bug):?>
-                      <?php $bugLink = $this->createLink('bug', 'view', "bugID=$bug->id", '', true);?>
-                      <tr class='a-center'>
-                        <td class='w-id a-left' id='bug'><input type='checkbox' name='bugs[]' value="<?php echo $bug->id;?>" <?php if(strpos(',' . $build->bugs . ',', ',' . $bug->id . ',') !== false) echo 'checked';?>> <?php echo sprintf('%03d', $bug->id);?></td>
-                        <td class='a-left nobr'><?php echo html::a($bugLink, $bug->title, '', "class='preview'");?></td>
-                        <td class='w-80px'><?php echo $lang->bug->statusList[$bug->status];?></td>
+            <td class='w-p50'>
+              <table class='mainTable bd-none'>
+                <tr style='border-bottom:none'>
+                  <td style='border-bottom:none; padding:0px'>
+                    <table class='headTable'>
+                      <caption><?php echo $lang->build->linkBugs;?></caption>
+                      <tr style='border-left:1px solid #e4e4e4; border-right:1px solid #e4e4e4;'>
+                        <th class='w-id a-left'><?php echo html::selectAll('bug', 'checkbox') . ' ' . $lang->idAB;?></th>
+                        <th><?php echo $lang->bug->title;?></th>
+                        <th class='w-100px'><?php echo $lang->bug->status;?></th>
                       </tr>
-                      <?php endforeach;?>
                     </table>
-                  </div>
-                </td>
-              </tr>
-            </table>
-          </div>
+                  </td>
+                </tr>
+                <tr style='border-bottom:none'>
+                  <td style='border-bottom:none; padding:0px'>
+                    <div class='contentDiv' style='border:1px solid #e4e4e4;'>
+                      <table class='f-left table-1 fixed bd-none'>
+                        <?php foreach($bugs as $bug):?>
+                        <?php $bugLink = $this->createLink('bug', 'view', "bugID=$bug->id", '', true);?>
+                        <tr class='a-center'>
+                          <td class='w-id a-left' id='bug'><input type='checkbox' name='bugs[]' value="<?php echo $bug->id;?>" <?php if(strpos(',' . $build->bugs . ',', ',' . $bug->id . ',') !== false) echo 'checked';?>> <?php echo sprintf('%03d', $bug->id);?></td>
+                          <td class='a-left nobr'><?php echo html::a($bugLink, $bug->title, '', "class='preview'");?></td>
+                          <td class='w-80px'><?php echo $lang->bug->statusList[$bug->status];?></td>
+                        </tr>
+                        <?php endforeach;?>
+                      </table>
+                    </div>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
 
-        </div>
       </td>
     </tr>
     <tr>
