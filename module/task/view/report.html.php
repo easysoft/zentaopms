0f315457f48868e8cdc6672430df5fa66874ef3c
chencongzhi520@gmail.com <azhi@cnezsoft.com>
2013-04-17 02:01:26 +0000

    * finish task#1326.

--- module/task/view/report.html.php
+++ module/task/view/report.html.php
@@ -11,11 +11,7 @@
  */
 ?>
 <?php include '../../common/view/header.html.php';?>
-<div id='featurebar'>
-  <div class='f-left'><?php echo $lang->report->common;?></div>
-  <div class='f-right'><?php common::printRPN($this->createLink('project', 'task', "projectID=$projectID&browseType=$browseType")); ?></div>
-</div>
-
+<?php include '../../common/view/colorize.html.php';?>
 <table class='cont-lt1'>
   <tr valign='top'>
     <td class='side'>
@@ -33,25 +29,32 @@
     <td class='divider'></td>
     <td>
       <table class='table-1'>
-        <caption><?php echo $lang->task->report->common;?></caption>
+        <caption>
+          <div class='f-left'><?php echo $lang->task->report->common;?></div>
+          <div class='f-right'><?php echo html::a($this->createLink('project', 'task', "projectID=$projectID&browseType=$browseType"), $lang->goback);?></div>
+        </caption>
         <?php foreach($charts as $chartType => $chartContent):?>
         <tr valign='top'>
           <td><?php echo $chartContent;?></td>
           <td width='300'>
-            <table class='table-1'>
-              <tr>
-                <th><?php echo $lang->report->item;?></th>
-                <th><?php echo $lang->report->value;?></th>
-                <th><?php echo $lang->report->percent;?></th>
-              </tr>
-              <?php foreach($datas[$chartType] as $key => $data):?>
-              <tr class='a-center'>
-                <td><?php echo $data->name;?></td>
-                <td><?php echo $data->value;?></td>
-                <td><?php echo ($data->percent * 100) . '%';?></td>
-              </tr>
-              <?php endforeach;?>
-            </table>
+            <?php $height = zget($lang->task->report->$chartType, 'height', $lang->story->report->options->height) . 'px'; ?>
+            <div style="height:<?php echo $height;?>; overflow:auto">
+              <table class='table-1 colored'>
+                <caption><?php echo $lang->task->report->charts[$chartType];?></caption>
+                <tr>
+                  <th><?php echo $lang->report->item;?></th>
+                  <th><?php echo $lang->report->value;?></th>
+                  <th><?php echo $lang->report->percent;?></th>
+                </tr>
+                <?php foreach($datas[$chartType] as $key => $data):?>
+                <tr class='a-center'>
+                  <td><?php echo $data->name;?></td>
+                  <td><?php echo $data->value;?></td>
+                  <td><?php echo ($data->percent * 100) . '%';?></td>
+                </tr>
+                <?php endforeach;?>
+              </table>
+            </div>
           </td>
         </tr>
         <?php endforeach;?>
