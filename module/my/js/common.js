7d3355b9118dfc124efcd1f15dae63e84eb44269
chencongzhi520@gmail.com <azhi@cnezsoft.com>
2013-04-18 08:26:36 +0000

    * code for task#1279.

---
+++ module/my/js/common.js
@@ -0,0 +1,5 @@
+$(function() 
+{ 
+    //$("#submenuchangePassword").colorbox({width:600, height:400, iframe:true, transition:'none', scrolling:false});
+    $(function(){$('.iframe').colorbox({width:900, height:500, iframe:true, transition:'none', onCleanup:function(){parent.location.href=parent.location.href;}});})
+});
