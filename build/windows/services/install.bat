4671a93a50ff9fe7dc4b799612cdbf4e15d59a4c
chencongzhi520@gmail.com <azhi@cnezsoft.com>
2013-05-02 02:49:20 +0000

    * finish task#1342.

--- build/windows/services/install.bat
+++ build/windows/services/install.bat
@@ -1,16 +1,27 @@
 @echo off
+set currentDir=%cd%
 
-echo First I need set the path for php and mysql
-..\php\php.exe .\set.php
+echo %currentDir% | find ":\xampp\services" > null
+if %ERRORLEVEL% == 1 goto notRootDirectory
+if %ERRORLEVEL% == 0 goto normalStart 
 
-echo Installing apache 2.4 as service...
-..\apache\bin\httpd -k install -n apachezt
-echo starting apache
-net start apachezt
+:notRootDirectory
+  echo "Please extract the xampp to root directory, example:D:\\xampp"
+  pause 
+  exit
 
-echo Installing mysql as service...
-..\mysql\bin\mysqld.exe --install mysqlzt
-echo starting mysql
-net start mysqlzt
+:normalStart
+  echo First I need set the path for php and mysql
+  ..\php\php.exe .\set.php
 
-pause
+  echo Installing apache 2.4 as service...
+  ..\apache\bin\httpd -k install -n apachezt
+  echo starting apache
+  net start apachezt
+
+  echo Installing mysql as service...
+  ..\mysql\bin\mysqld.exe --install mysqlzt
+  echo starting mysql
+  net start mysqlzt
+
+  pause
