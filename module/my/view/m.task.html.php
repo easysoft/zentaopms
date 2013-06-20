97b2ebc892b61a41be9d5c2134c2a05b75efdc0b
wangyidong <wangyidong@cnezsoft.com>
2013-06-20 02:04:35 +0000

    * finish task #1393.

--- module/my/view/m.task.html.php
+++ module/my/view/m.task.html.php
@@ -1,14 +1,27 @@
 <?php
 /**
- * The todo view file of dashboard module of ZenTaoPMS.
+ * The task view file of dashboard module of ZenTaoPMS.
  *
  * @copyright   Copyright 2009-2013 青岛易软天创网络科技有限公司 (QingDao Nature Easy Soft Network Technology Co,LTD www.cnezsoft.com)
  * @license     LGPL (http://www.gnu.org/licenses/lgpl.html)
  * @author      Yidong Wang <yidong@cnezsoft.com>
  * @package     my
- * @version     $Id: todo.html.php 4735 2013-05-03 08:30:02Z chencongzhi520@gmail.com $
+ * @version     $Id: task.html.php 4735 2013-05-03 08:30:02Z chencongzhi520@gmail.com $
  * @link        http://www.zentao.net
  */
 ?>
 <?php include '../../common/view/m.header.html.php';?>
+<div data-role='navbar' style='margin:-15px 0 15px 0;'>
+  <ul>
+    <?php foreach($config->mobile->taskBar as $menu):?>
+    <?php $active = $type == $menu ? 'ui-btn-active' : ''?>
+    <li><?php echo html::a($this->createLink('my', 'task', "type=$menu"), $lang->my->taskMenu->{$menu . 'Me'}, '', "class='$active'")?></li>
+    <?php endforeach;?>
+  </ul>
+</div>
+<ul data-role='listview'>
+    <?php foreach($tasks as $task):?>
+    <li><?php echo html::a($this->createLink('task', 'view', "taskID=$task->id"), $task->name)?></li>
+    <?php endforeach;?>
+</ul>
 <?php include '../../common/view/m.footer.html.php';?>
