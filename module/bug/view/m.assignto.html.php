b835d6413af17528c91b5b9d8810ee42d415303f
chencongzhi520@gmail.com <azhi@cnezsoft.com>
2013-06-21 05:26:00 +0000

    * code for task#1416.

--- module/bug/view/m.assignto.html.php
+++ module/bug/view/m.assignto.html.php
@@ -6,6 +6,6 @@
   <div><?php echo $lang->comment . html::textarea('comment', '', "rows='6' class='area-1'");?></div>
   <?php echo html::submitButton('', 'data-inline="true" data-theme="b"'); ?>
   <?php echo html::backButton("data-inline='true'");?>
-  <?php include '../../common/view/action.html.php';?>
 </form>
+<?php include '../../common/view/action.html.php';?>
 <?php include '../../common/view/m.footer.html.php';?>
