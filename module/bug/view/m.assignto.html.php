c6d8069293ca94a6d086bdac875deaa7a5177752
chencongzhi520@gmail.com <azhi@cnezsoft.com>
2013-06-21 05:23:38 +0000

    * code for task#1416.

--- module/bug/view/m.assignto.html.php
+++ module/bug/view/m.assignto.html.php
@@ -4,16 +4,8 @@
   <div><?php echo $bug->title;?></div>
   <div><?php echo $lang->bug->assignedTo . html::select('assignedTo', $users, $bug->assignedTo, "class='text-3'");?></div>
   <div><?php echo $lang->comment . html::textarea('comment', '', "rows='6' class='area-1'");?></div>
+  <?php echo html::submitButton('', 'data-inline="true" data-theme="b"'); ?>
+  <?php echo html::backButton("data-inline='true'");?>
   <?php include '../../common/view/action.html.php';?>
-  <div data-role='footer' data-position='fixed'>
-    <div data-role='navbar'>
-      <ul>
-      <?php
-      echo '<li>' . html::submitButton() . '</li>';
-      echo '<li>' . html::linkButton($lang->goback, $this->server->http_referer) . '</li>';
-      ?>
-      </ul>
-    </div>
-  </div>
 </form>
 <?php include '../../common/view/m.footer.html.php';?>
