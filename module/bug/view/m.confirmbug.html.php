60a07641e0d16c282e23b4d6dfdf16a118c0626a
chencongzhi520@gmail.com <azhi@cnezsoft.com>
2013-06-21 05:21:31 +0000

    * finish task#1415.

---
+++ module/bug/view/m.confirmbug.html.php
@@ -0,0 +1,12 @@
+<?php include '../../common/view/m.header.html.php';?>
+</div>
+<form method='post' target='hiddenwin'>
+  <div><?php echo $bug->title;?></div>
+  <div><?php echo $lang->bug->assignedTo . html::select('assignedTo', $users, $bug->assignedTo, "class='text-3'");?></div>
+  <div><?php echo $lang->comment . html::textarea('comment', '', "rows='6' class='area-1'");?></div>
+  <?php echo html::submitButton($lang->bug->buttonConfirm, 'data-inline="true" data-theme="b"');?>
+  <?php echo html::backButton("data-inline='true'");?>
+  </table>
+  <?php include '../../common/view/action.html.php';?>
+</form>
+<?php include '../../common/view/m.footer.html.php';?>
