8f1aebca6892e0367a1c1608ab16806371a2b0b2
zhujinyong <zhujinyong@cnezsoft.com>
2013-06-21 09:00:47 +0000

    * code for task#1411.

---
+++ module/task/view/m.activate.html.php
@@ -0,0 +1,20 @@
+<?php include '../../common/view/m.header.html.php';?>
+</div>
+<form method='post' target='hiddenwin' onsubmit='return checkLeft();'>
+    <h3><?php echo $lang->task->activate . $this->lang->colon . $task->name;?></h3>
+    <div data-role="fieldcontain">
+      <?php echo '<label for="assignedTo">' . $lang->task->assignedTo. '</label>' . html::select('assignedTo', $users, $task->openedBy);?>
+    </div>
+    <div data-role="fieldcontain">
+      <?php echo '<label for="left">' . $lang->task->left . '(' . $lang->task->hour . ')</label>' . html::input('left', '');?>
+    </div>
+    <div data-role="fieldcontain">
+      <?php echo html::textarea('comment', '', "placeholder='$lang->comment'");?>
+    </div>
+    <div data-role="fieldcontain">
+      <?php echo html::submitButton(); ?>
+    </div>
+  </table>
+  <?php include '../../common/view/m.action.html.php';?>
+</form>
+<?php include '../../common/view/m.footer.html.php';?>
