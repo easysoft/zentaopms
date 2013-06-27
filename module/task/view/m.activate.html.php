182059772d448cf148a38f7f977d7a50d3b90ef1
zhujinyong <zhujinyong@cnezsoft.com>
2013-06-27 08:54:45 +0000

    * adjust active task.

--- module/task/view/m.activate.html.php
+++ module/task/view/m.activate.html.php
@@ -1,20 +1,25 @@
 <?php include '../../common/view/m.header.html.php';?>
 </div>
 <form method='post' target='hiddenwin' onsubmit='return checkLeft();'>
-    <h3><?php echo $lang->task->activate . $this->lang->colon . $task->name;?></h3>
-    <div data-role="fieldcontain">
-      <?php echo '<label for="assignedTo">' . $lang->task->assignedTo. '</label>' . html::select('assignedTo', $users, $task->openedBy);?>
+  <h3><?php echo $lang->task->activate . $this->lang->colon . $task->name;?></h3>
+
+  <table class='table-1'>
+    <tr>
+      <td class="w-60px"><?php echo $lang->task->assignedTo;?></td> 
+      <td><?php echo html::select('assignedTo', $users, $task->openedBy);?>
+    </tr>
+    <tr>
+      <td class="w-60px"><?php echo $lang->task->left;?></td>
+      <td><?php echo html::input('left', '');?>
     </div>
-    <div data-role="fieldcontain">
-      <?php echo '<label for="left">' . $lang->task->left . '(' . $lang->task->hour . ')</label>' . html::input('left', '');?>
-    </div>
-    <div data-role="fieldcontain">
-      <?php echo html::textarea('comment', '', "placeholder='$lang->comment'");?>
-    </div>
-    <div data-role="fieldcontain">
-      <?php echo html::submitButton(); ?>
-    </div>
+    <tr>
+      <td class='w-60px'><?php echo $lang->comment;?></td>
+      <td><?php echo html::textarea('comment', '', "data-mini='true'");?></td>
+    </tr>
   </table>
-  <?php include '../../common/view/m.action.html.php';?>
+  <div class="a-center">
+    <?php echo html::submitButton('', 'data-inline="true" data-theme="b"');?>
+  </div>
 </form>
+<?php include '../../common/view/m.action.html.php';?>
 <?php include '../../common/view/m.footer.html.php';?>
