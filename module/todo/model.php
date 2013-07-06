37b5c3d95f3968183c2a47d194ed47797a6d118c
wangyidong <wangyidong@cnezsoft.com>
2013-07-06 05:21:58 +0000

    * adjust code.

--- module/todo/model.php
+++ module/todo/model.php
@@ -86,10 +86,7 @@ class todoModel extends model
                     echo js::error(dao::getError());
                     die(js::reload('parent'));
                 }
-                else
-                {
-                    $this->loadModel('action')->create('todo', $this->dao->lastInsertID(), 'opened');
-                }
+                $this->loadModel('action')->create('todo', $this->dao->lastInsertID(), 'opened');
             }
             else
             {
