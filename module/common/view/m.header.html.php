60250e6b1dacd00087af3a473fcc2f4a38f2b465
chencongzhi520@gmail.com <azhi@cnezsoft.com>
2013-07-06 07:54:03 +0000

    * adjust the border-bottom of navBar.

--- module/common/view/m.header.html.php
+++ module/common/view/m.header.html.php
@@ -2,7 +2,7 @@
 if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}
 include 'm.header.lite.html.php';
 ?>
-<div data-role="header" data-position='fixed'>
+<div data-role="header" data-position='fixed' style='border-bottom:1px solid #ddd'>
   <h1 style='margin-left:0px;text-align:left'><?php echo $app->company->name?></h1>
   <?php echo html::a($this->createLink('user', 'logout'), $lang->logout, '', "id='logout' data-icon='delete' class='ui-btn-right'")?>
   <div data-role="navbar" id='mainMenu'>
