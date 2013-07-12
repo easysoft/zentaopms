d2ae7e7d6b762f3adda8059213a188419bfd2afa
wangchunsheng <chunsheng@cnezsoft.com>
2013-07-12 14:10:40 +0000

    + import the setting files for zentaomos.

---
+++ build/mos/phpmyadmin.php
@@ -0,0 +1,10 @@
+<?php
+$cfg['blowfish_secret']               = 'zentaomos';
+$cfg['Servers'][1]['auth_type']       = 'config';
+$cfg['Servers'][1]['user']            = 'root';
+$cfg['Servers'][1]['password']        = ''; 
+$cfg['Servers'][1]['port']            = '3306';
+$cfg['Servers'][1]['host']            = 'localhost';
+$cfg['Servers'][1]['connect_type']    = 'tcp';
+$cfg['Servers'][1]['extension']       = 'mysql';
+$cfg['Servers'][1]['AllowNoPassword'] = true;
