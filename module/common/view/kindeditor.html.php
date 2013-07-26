882a119c7c2f0479cf127ed8fd7df8813b9e7ceb
wyd621 <wyd621@163.com>
2013-07-26 12:41:10 +0800

    * code for task #1607.

--- module/common/view/kindeditor.html.php
+++ module/common/view/kindeditor.html.php
@@ -56,19 +56,50 @@ $(document).ready(function()
                 afterBlur: function(){this.sync();},
                 afterCreate : function()
                 {
-                    if(!K.GECKO) return false;
                     var doc = this.edit.doc; 
-                    K(doc.body).bind('paste', function()
+                    var cmd = this.edit.cmd; 
+                    /* Paste in chrome.*/
+                    /* Code reference from http://www.foliotek.com/devblog/copy-images-from-clipboard-in-javascript/. */
+                    if(K.WEBKIT)
                     {
-                        setTimeout(function()
+                        $(doc.body).bind('paste', function(ev)
                         {
-                            var html = K(doc.body).html();
-                            if(html.search(/<img src="data:.+;base64,/) > -1)
+                            var $this = $(this);
+                            var original =  ev.originalEvent;
+                            var file =  original.clipboardData.items[0].getAsFile();
+                            var reader = new FileReader();
+                            reader.onload = function (evt) 
                             {
-                                $.post(createLink('file', 'ajaxEditorImage'), {editor: html}, function(data){K(doc.body).html(data);});
-                            }
-                        }, 80);
-                    });
+                                var result = evt.target.result; 
+                                var result = evt.target.result;
+                                var arr = result.split(",");
+                                var data = arr[1]; // raw base64
+                                var contentType = arr[0].split(";")[0].split(":")[1];
+
+                                html = '<img src="' + result + '" alt="" />';
+                                $.post(createLink('file', 'ajaxEditorImage'), {editor: html}, function(data){cmd.inserthtml(data);});
+                            };
+
+                            reader.readAsDataURL(file);
+                        });
+                    }
+
+                    /* Paste in firfox.*/
+                    if(K.GECKO)
+                    {
+                        K(doc.body).bind('paste', function(ev)
+                        {
+                            setTimeout(function()
+                            {
+                                var html = K(doc.body).html();
+                                if(html.search(/<img src="data:.+;base64,/) > -1)
+                                {
+                                    $.post(createLink('file', 'ajaxEditorImage'), {editor: html}, function(data){K(doc.body).html(data);});
+                                }
+                            }, 80);
+                        });
+                    }
+                    /* End */
                 }
             });
         });
