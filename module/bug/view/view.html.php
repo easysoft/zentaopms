4a161ac051e5af1504ae4df7a935a419ba679bdd
xia0ta0 <xia0ta0@outlook.com>
2013-07-29 16:38:25 +0800

    * add plan field of bug view.

--- module/bug/view/view.html.php
+++ module/bug/view/view.html.php
@@ -84,6 +84,10 @@
             <th class='rowhead'><?php echo $lang->bug->product;?></th>
             <td><?php if(!common::printLink('bug', 'browse', "productID=$bug->product", $productName)) echo $productName;?>
           </tr>
+          <tr valign='middle'>
+            <th class='rowhead'><?php echo $lang->bug->productplan;?></th>
+            <td><?php if(!$bug->plan or !common::printLink('productplan', 'linkBug', "planID=$bug->plan", $bug->planName)) echo $bug->planName;?>
+          </tr>
           <tr>
             <th class='rowhead'><?php echo $lang->bug->module;?></th>
             <td> 
