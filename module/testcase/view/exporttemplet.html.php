fb95de9777ea86b4e73c6421790de6d545953605
wyd621 <wyd621@163.com>
2013-07-29 15:11:26 +0800

    + add view file for exportTemplet.

---
+++ module/testcase/view/exporttemplet.html.php
@@ -0,0 +1,38 @@
+<?php include '../../common/view/header.lite.html.php';?>
+<script>
+function setDownloading()
+{
+    if($.browser.opera) return true;   // Opera don't support, omit it.
+
+    $.cookie('downloading', 0);
+    time = setInterval("closeWindow()", 300);
+    return true;
+}
+
+function closeWindow()
+{
+    if($.cookie('downloading') == 1 || i >= 30)
+    {
+        parent.$.fn.colorbox.close();
+        $.cookie('downloading', null);
+        clearInterval(time);
+    }
+    i ++;
+}
+</script>
+<form method='post' target='hiddenwin' onsubmit='setDownloading();'>
+<table class='table-1 mt-10px'>
+  <caption><?php echo $lang->testcase->exportTemplet?></caption>
+  <tr>
+    <td align='center'>
+    <?php
+    echo $lang->testcase->num;
+    echo html::input('num', '10');
+    echo html::select('encode', $config->charsets[$this->cookie->lang], 'utf-8');
+    echo html::submitButton();
+    ?>
+    </td>
+  <tr>
+</table>
+</form>
+<?php include '../../common/view/footer.lite.html.php';?>
