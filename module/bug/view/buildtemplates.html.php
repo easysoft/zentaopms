acabc4f81e0a14c258f28d22ef9867f515d5aef4
Catouse <catouse@gmail.com>
2013-09-17 11:35:28 +0800

    * ajust style of search form.

--- module/bug/view/buildtemplates.html.php
+++ module/bug/view/buildtemplates.html.php
@@ -7,7 +7,7 @@ foreach($templates as $key => $template)
 {
     echo "<span id='tplBox$template->id'>";
     echo $lang->arrow. "<span id='tplTitleBox$template->id' onclick='setTemplate($template->id)' style='text-decoration:underline; color:blue' class='hand'>$template->title</span>";
-    echo html::commonButton('&nbsp;', "onclick=deleteTemplate($template->id) class='icon-delete'");
+    echo html::commonButton('&nbsp;', "onclick=deleteTemplate($template->id) class='icon-remove icon-large'");
     echo "<span id='template$template->id' class='hidden'>$template->content</span>";
     echo '<br /></span>';
 }
