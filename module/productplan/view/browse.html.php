ba3e7d6d6287b4934a8526b2e7401b8942710a2a
azhi <chencongzhi520@gmail.com>
2013-10-14 13:28:15 +0800

    * fix bug: change " to ' in desc'title attr when desc include link.

--- module/productplan/view/browse.html.php
+++ module/productplan/view/browse.html.php
@@ -33,7 +33,7 @@
   <tr class='a-center'>
     <td><?php echo $plan->id;?></td>
     <td class='a-left' title="<?php echo $plan->title?>"><?php echo html::a(inlink('view', "id=$plan->id"), $plan->title);?></td>
-    <td class='a-left content' title="<?php echo $plan->desc?>"><?php echo $plan->desc;?></td>
+    <td class='a-left content' title='<?php echo $plan->desc;?>'><?php echo $plan->desc;?></td>
     <td><?php echo $plan->begin;?></td>
     <td><?php echo $plan->end;?></td>
     <td class='a-center'>
