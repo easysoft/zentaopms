bf552c1fa96be8eb7a14e5cd0161746d76a78215
wyd621 <wyd621@163.com>
2014-03-21 09:09:02 +0000

    * adjust zdb.

--- lib/zdb/zdb.class.php
+++ lib/zdb/zdb.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The zdb library of zentaopms. This lib operate db.
+ * The zdb library of zentaopms, can be used to bakup and restore a database.
  *
  * @copyright   Copyright 2009-2013 QingDao Nature Easy Soft Network Technology Co,LTD (www.cnezsoft.com)
  * @license     LGPL (http://www.gnu.org/licenses/lgpl.html)
@@ -14,7 +14,7 @@ class zdb
     /**
      * dbh 
      * 
-     * @var int   
+     * @var object   
      * @access public
      */
     public $dbh;
@@ -32,58 +32,122 @@ class zdb
     }
 
     /**
-     * Backup db. 
+     * Dump db. 
      * 
      * @param  string $fileName 
      * @param  array  $tables 
      * @access public
-     * @return string|int
+     * @return object
      */
-    public function backupDB($fileName, $tables = array())
+    public function dump($fileName, $tables = array())
     {
-        if(empty($tables))
+        /* Init the return. */
+        $return = new stdclass();
+        $return->result = true;
+        $return->error  = '';
+
+        /* Get all tables in database. */
+        $allTables = array();
+        $stmt      = $this->dbh->query('show tables');
+        while($table = $stmt->fetch(PDO::FETCH_ASSOC))
         {
-            $stmt = $this->dbh->query('show tables');
-            while($table = $stmt->fetch(PDO::FETCH_ASSOC)) $tables[] = current($table);
+            $table = current($table);
+            $allTables[$table] = $table;
         }
 
-        if(empty($fileName)) return 'Has not file';
-        if(!is_writable(dirname($fileName))) return 'The directory is not writable';
-        $fp = fopen($fileName, 'w');
+        /* Dump all tables when tables is empty. */
+        if(empty($tables)) $tables = $allTables;
 
+        /* Check file. */
+        if(empty($fileName))
+        {
+            $return->result = false;
+            $return->error  = 'Has not file';
+            return $return;
+        }
+        if(!is_writable(dirname($fileName)))
+        {
+            $return->result = false;
+            $return->error  = 'The directory is not writable';
+            return $return;
+        }
+
+        /* Open this file. */
+        $fp = fopen($fileName, 'w');
         fwrite($fp, "SET NAMES utf8;\n");
         foreach($tables as $table)
         {
-            $backupSql = "DROP TABLE IF EXISTS `$table`;\n";
+            /* Check table exists. */
+            if(!isset($allTables[$table])) continue;
 
-            $createSql  = $this->dbh->query("show create table `$table`")->fetch(PDO::FETCH_ASSOC);
-            $backupSql .= $createSql['Create Table'] . ";\n";
+            /* Create sql code. */
+            $backupSql  = "DROP TABLE IF EXISTS `$table`;\n";
+            $backupSql .= $this->getSchemaSQL($table);
+            $backupSql .= $this->getDataSQL($table);
 
-            $datas = $this->dbh->query("select * from `$table`")->fetchAll(PDO::FETCH_ASSOC);
-            if(!empty($datas))
-            {
-                $keys = array_keys(current($datas));
-                $keys = array_map('addslashes', $keys);
-                $keys = join('`,`', $keys);
-                $keys = "`" . $keys . "`";
-
-                $values = array();
-                foreach($datas as $data)
-                {
-                    $value = array_values($data);
-                    $value = array_map('addslashes', $value);
-                    $value = join("','", $value);
-                    $value = "'" . $value . "'";
-
-                    $values[] = "($value)";
-                }
-                $backupSql .= "INSERT INTO `$table`($keys) VALUES" . join(',', $values) . ";\n";
-            }
-
+            /* Write sql code. */
             fwrite($fp, $backupSql);
         }
         fclose ($fp);
+        return $return;
+    }
 
-        return 0;
+    /**
+     * Import DB 
+     * 
+     * @access public
+     * @return void
+     */
+    public function import()
+    {
+    }
+
+    /**
+     * Get schema SQL.
+     * 
+     * @param  string    $table 
+     * @access public
+     * @return string
+     */
+    public function getSchemaSQL($table)
+    {
+        $createSql = $this->dbh->query("show create table `$table`")->fetch(PDO::FETCH_ASSOC);
+        return $createSql['Create Table'] . ";\n";
+    }
+
+    /**
+     * Get data SQL.
+     * 
+     * @param  string    $table 
+     * @access public
+     * @return string
+     */
+    public function getDataSQL($table)
+    {
+        $rows = $this->dbh->query("select * from `$table`")->fetchAll(PDO::FETCH_ASSOC);
+        $sql  = '';
+        if(!empty($rows))
+        {
+            /* Create key sql for insert. */
+            $keys = array_keys(current($rows));
+            $keys = array_map('addslashes', $keys);
+            $keys = join('`,`', $keys);
+            $keys = "`" . $keys . "`";
+
+            /* Create all value sql. */
+            $values = array();
+            foreach($rows as $row)
+            {
+                $value = array_values($row);
+                $value = array_map('addslashes', $value);
+                $value = join("','", $value);
+                $value = "'" . $value . "'";
+
+                $values[] = "($value)";
+            }
+
+            $sql .= "INSERT INTO `$table`($keys) VALUES" . join(',', $values) . ";\n";
+        }
+        return $sql;
     }
 }
