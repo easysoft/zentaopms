e6706bf6859d44d5fb20342d58e1a9dcc31d381c
Catouse <catouse@gmail.com>
2014-04-10 20:09:32 +0800

    * ajusted ui of debug in 'footer.lite.html.php'.

--- module/common/view/footer.lite.html.php
+++ module/common/view/footer.lite.html.php
@@ -1,5 +1,5 @@
 <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
-<iframe frameborder='0' name='hiddenwin' id='hiddenwin' scrolling='no' class='<?php $config->debug ? print("debugwin") : print('hidden')?>'></iframe>
+<?php if($config->debug) include 'debug.html.php';?>
 <script laguage='Javascript'>
 $().ready(function(){setDebugWin('white')})
 <?php if(isset($pageJS)) echo $pageJS;?>
