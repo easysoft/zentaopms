def3baca157c4ed4f42fc095b7265de30428d9b3
xia0ta0 <xia0ta0@outlook.com>
2014-06-20 14:24:32 +0800

    * code for task#1915.

--- module/bug/js/browse.js
+++ module/bug/js/browse.js
@@ -3,10 +3,4 @@ $(document).ready(function()
     $('#' + browseType + 'Tab').addClass('active'); 
     $('#module' + moduleID).addClass('active'); 
     if(browseType == 'bysearch') ajaxGetSearchForm();
-
-    /* If customed and the browse is ie6, remove the ie6.css. */
-    if(customed && $.browser.msie && Math.floor(parseInt($.browser.version)) == 6)
-    {
-        $("#browsecss").attr('href', '');
-    }
 });
