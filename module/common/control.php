8a789b9fa209f55ff8138dbbe16739779712dc62
wangyidong <yidong@cnezsoft.com>
2014-07-22 01:05:04 +0000

    * code for task #1978.

--- module/common/control.php
+++ module/common/control.php
@@ -56,25 +56,6 @@ class common extends control
     }
 
     /**
-     * Check upgrade's status file is ok or not.
-     * 
-     * @access public
-     * @return void
-     */
-    public function checkUpgradeStatus()
-    {
-        $statusFile = $this->app->getAppRoot() . 'www' . $this->pathFix . 'ok';
-        if(!file_exists($statusFile) or time() - filemtime($statusFile) > 3600)
-        {
-            $this->app->loadLang('upgrade');
-            echo "<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8' /></head><body>";
-            echo "<table align='center' style='margin-top:100px; border:1px solid gray; font-size:14px;'><tr><td>";
-            printf($this->lang->upgrade->setStatusFile, $statusFile, $statusFile, $statusFile);
-            die('</td></tr></table></body></html>');
-        }
-    }
-
-    /**
      * Check the user has permisson of one method of one module.
      * 
      * @param  string $module 
