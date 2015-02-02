24f81f356ebe161e5693c0427e3f09ddc1fb9454
wangyidong <yidong@cnezsoft.com>
2015-02-02 09:55:33 +0800

    * change version and upgrade.

--- module/upgrade/lang/en.php
+++ module/upgrade/lang/en.php
@@ -87,3 +87,4 @@ $lang->upgrade->fromVersions['6_0']       = '6.0';
 $lang->upgrade->fromVersions['6_1']       = '6.1';
 $lang->upgrade->fromVersions['6_2']       = '6.2';
 $lang->upgrade->fromVersions['6_3']       = '6.3';
+$lang->upgrade->fromVersions['6_4']       = '6.4';
