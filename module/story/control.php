5f4a2ffd9d378a28b2233c304581c83db0fe6559
wangyidong <yidong@cnezsoft.com>
2015-05-04 13:25:58 +0800

    * finish task #2240.

--- module/story/control.php
+++ module/story/control.php
@@ -168,7 +168,7 @@ class story extends control
         $this->view->contactLists     = $this->user->getContactLists($this->app->user->account, 'withnote');
         $this->view->moduleID         = $moduleID;
         $this->view->moduleOptionMenu = $moduleOptionMenu;
-        $this->view->plans            = $this->loadModel('productplan')->getPairs($productID);
+        $this->view->plans            = $this->loadModel('productplan')->getPairs($productID, 'unexpired');
         $this->view->planID           = $planID;
         $this->view->source           = $source;
         $this->view->pri              = $pri;
