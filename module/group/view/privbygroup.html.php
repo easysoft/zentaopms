1ba3da9ce2933bb811e7e0aedfd7862743a28ac8
wangyidong <yidong@cnezsoft.com>
2015-05-18 17:06:34 +0800

    * fix a bug.

--- module/group/view/privbygroup.html.php
+++ module/group/view/privbygroup.html.php
@@ -40,6 +40,7 @@
       </tr>
     </thead>
     <?php foreach($lang->resource as $moduleName => $moduleActions):?>
+    <?php if(!count((array)$moduleActions)) continue;?>
     <?php if(!$this->group->checkMenuModule($menu, $moduleName)) continue;?>
     <?php
     /* Check method in select version. */
