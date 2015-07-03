fc67542419514128e8da1da470e39f70345ee179
wangyidong <yidong@cnezsoft.com>
2015-07-03 11:39:11 +0800

    * fix a bug #712.

--- module/user/view/delete.html.php
+++ module/user/view/delete.html.php
@@ -24,7 +24,7 @@
       </th>
       <td>
         <div class="required required-wrapper"></div>
-        <?php echo html::input('verifyPassword', '', "class='form-control' placeholder='{$lang->user->placeholder->verify}'");?>
+        <?php echo html::password('verifyPassword', '', "class='form-control' placeholder='{$lang->user->placeholder->verify}'");?>
       </td>
       <td class='w-100px'><?php echo html::submitButton();?></td>
     </tr>
