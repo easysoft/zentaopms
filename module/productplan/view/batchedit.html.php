43ff689c19e9c9e785cabd61a74e2b242ce40afd
wangyidong <yidong@cnezsoft.com>
2015-09-16 16:25:33 +0800

    + add for task 2283.

---
+++ module/productplan/view/batchedit.html.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The batchEdit view file of productplan module of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2015 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     ZPL (http://zpl.pub/page/zplv11.html)
+ * @author      Yidong Wang <yidong@cnezsoft.com>
+ * @package     productplan
+ * @version     $Id$
+ * @link        http://www.zentao.net
+ */
+?>
+<?php include '../../common/view/header.html.php';?>
+<?php include '../../common/view/datepicker.html.php';?>
+<div id='titlebar'>
+  <div class='heading'><?php echo $lang->productplan->batchEdit;?></div>
+</div>
+<form method='post' target='hiddenwin'>
+<table class='table table-form'>
+  <thead>
+    <tr>
+      <th class='w-60px'><?php echo $lang->productplan->id?></th>
+      <th><?php echo $lang->productplan->title?></th>
+      <th class='w-150px'><?php echo $lang->productplan->begin?></th>
+      <th class='w-150px'><?php echo $lang->productplan->end?></th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php foreach($plans as $plan):?>
+    <tr>
+      <td><?php echo $plan->id . html::hidden("id[$plan->id]", $plan->id);?></td>
+      <td><?php echo html::input("title[$plan->id]", $plan->title, "class='form-control'")?></td>
+      <td><?php echo html::input("begin[$plan->id]", $plan->begin, "class='form-control form-date'")?></td>
+      <td><?php echo html::input("end[$plan->id]", $plan->end, "class='form-control form-date'")?></td>
+    </tr>
+  <?php endforeach;?>
+  </tbody>
+  <tfoot>
+    <tr><td colspan='4' class='text-center'><?php echo html::submitButton() . html::backButton();?></td></tr>
+  </tfoot>
+</table>
+</form>
+<?php include '../../common/view/footer.html.php';?>
