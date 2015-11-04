f4751b5fae226a3984e7d2c8fe123ef0fd07a9a9
wangyidong <yidong@cnezsoft.com>
2015-11-04 13:19:07 +0800

    * fix bug for create task from story-view page.

--- module/story/view/view.html.php
+++ module/story/view/view.html.php
@@ -46,7 +46,7 @@
         common::printIcon('story', 'close',      "storyID=$story->id", $story, 'button', '', '', 'iframe text-danger', true);
         common::printIcon('story', 'activate',   "storyID=$story->id", $story, 'button', '', '', 'iframe text-success', true);
         common::printIcon('story', 'createCase', "productID=$story->product&branch=$story->branch&moduleID=0&from=&param=0&storyID=$story->id", '', 'button', 'sitemap');
-        if($from == 'project') common::printIcon('task', 'create', "project=$param&storyID=$story->id", '', 'button', 'smile');
+        if($from == 'project') common::printIcon('task', 'create', "project=$param&storyID=$story->id&moduleID=$story->module", '', 'button', 'smile');
         echo '</div>';
 
         echo "<div class='btn-group'>";
