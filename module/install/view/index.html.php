e98ceb95c70d16ece6f46d18126c7d6a79f415e6
wangyidong <yidong@cnezsoft.com>
2015-11-09 13:14:12 +0800

    * finish task #2333.

--- module/install/view/index.html.php
+++ module/install/view/index.html.php
@@ -38,12 +38,11 @@
         </tr> 
         <tr>
           <td colspan='2'>
+            <h5><?php echo $lang->install->promotion?></h5>
             <div class='row'>
-              <h5><?php echo $lang->install->promotion?></h5>
               <div class='col-md-4'>
                 <a class="card ad" href="http://www.chanzhi.org" target="_blank">
                   <div class="img-wrapper" style="background-image:url(<?php echo $defaultTheme . 'images/main/chanzhi.png'?>)"><img src="<?php echo $defaultTheme . 'images/main/chanzhi.png'?>" alt=""></div>
-                  <strong class="card-heading"><?php echo $lang->install->chanzhi->name?></strong>
                   <div class="card-reveal">
                     <h5 class="card-heading"><?php echo $lang->install->chanzhi->name?></h5>
                     <div class="card-content"><?php echo $lang->install->chanzhi->desc?></div>
@@ -53,7 +52,6 @@
               <div class='col-md-4'>
                 <a class="card ad" href="http://www.ranzhico.com" target="_blank">
                   <div class="img-wrapper" style="background-image:url(<?php echo $defaultTheme . 'images/main/ranzhi.png'?>)"><img src="<?php echo $defaultTheme . 'images/main/ranzhi.png'?>" alt=""></div>
-                  <strong class="card-heading"><?php echo $lang->install->ranzhi->name?></strong>
                   <div class="card-reveal">
                     <h5 class="card-heading"><?php echo $lang->install->ranzhi->name?></h5>
                     <div class="card-content"><?php echo $lang->install->ranzhi->desc?></div>
@@ -63,7 +61,6 @@
               <div class='col-md-4'>
                 <a class="card ad" href="http://www.yidouio.com" target="_blank">
                   <div class="img-wrapper" style="background-image:url(<?php echo $defaultTheme . 'images/main/yidou.png'?>)"><img src="<?php echo $defaultTheme . 'images/main/yidou.png'?>" alt=""></div>
-                  <strong class="card-heading"><?php echo $lang->install->yidou->name?></strong>
                   <div class="card-reveal">
                     <h5 class="card-heading"><?php echo $lang->install->yidou->name?></h5>
                     <div class="card-content"><?php echo $lang->install->yidou->desc?></div>
