59c99c29f3ce5b84e619626604b0e5742a4c9558
wangyidong <yidong@cnezsoft.com>
2016-03-04 11:36:21 +0800

    * adjust code for bug #792.

--- module/user/model.php
+++ module/user/model.php
@@ -42,8 +42,11 @@ class userModel extends model
      */
     public function setUserList($users, $account)
     {
-        $user = $this->getById($account);
-        if($user->deleted) $users[$account] = zget($user, 'realname', $account);
+        if(!isset($users[$account]))
+        {
+            $user = $this->getById($account);
+            if($user and $user->deleted) $users[$account] = zget($user, 'realname', $account);
+        }
         return html::select('account', $users, $account, "onchange=\"switchAccount(this.value, '{$this->app->getMethodName()}')\" class='form-control chosen'");
     }
 
