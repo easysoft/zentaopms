d907cda1afe343e5b4f3ce80a1addb1246990895
Catouse <catouse@gmail.com>
2016-03-22 10:41:15 +0800

    * enable option 'initialState' in tree to preserve tree state.

--- www/js/my.full.js
+++ www/js/my.full.js
@@ -1636,7 +1636,7 @@ $(document).ready(function()
     fixStyle();
 
     // Init tree menu
-    $('.tree').tree();
+    $('.tree').tree({initialState: 'preserve'});
 
     $(window).resize(saveWindowSize);   // When window resized, call it again.
 
