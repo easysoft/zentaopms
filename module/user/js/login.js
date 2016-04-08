88843220e9c9134a9e61dd0401a60194b5b6858e
wangyidong <yidong@cnezsoft.com>
2016-04-08 10:32:39 +0800

    * change for md5 password when login.

--- module/user/js/login.js
+++ module/user/js/login.js
@@ -12,10 +12,12 @@ $(document).ready(function()
     mBtn.popover({html: true, container: 'body'}).click(function(event)
     {
         event.stopPropagation();
-        $(document).one('click', function()
-        {
-            $('#mobile').popover('hide');
-        });
+        $(document).one('click', function(){$('#mobile').popover('hide');});
     });
     mBtn.attr('title', mBtn.attr('data-original-title'));
+    $('#login-form form').submit(function()
+    {
+        var password = $('input:password').val();
+        if(password.length != 32 && typeof(md5) == 'function') $('input:password').val(md5(password));
+    });
 })
