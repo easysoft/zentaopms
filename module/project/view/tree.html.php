ceb8e4fe67780636ce8cb6368357b4dae356028f
Catouse <catouse@gmail.com>
2016-04-21 12:06:52 +0800

    * ajust code.

--- module/project/view/tree.html.php
+++ module/project/view/tree.html.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * The task view file of project module of ZenTaoPMS.
+ * The project tree view file of project module of ZenTaoPMS.
  *
  * @copyright   Copyright 2009-2015 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
  * @license     ZPL (http://zpl.pub/page/zplv12.html)
- * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
+ * @author      Hao Sun <sunhao@cnezsoft.com>
  * @package     project
- * @version     $Id: task.html.php 4894 2013-06-25 01:28:39Z wyd621@gmail.com $
+ * @version     $Id: tree.html.php 4894 2013-06-25 01:28:39Z wyd621@gmail.com $
  * @link        http://www.zentao.net
  */
 ?>
