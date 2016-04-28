f4525e9554eea08768f05881275ca2738291555b
Catouse <catouse@gmail.com>
2016-04-28 14:06:29 +0800

    * change feature menu UI of project/task.

--- module/project/view/task.html.php
+++ module/project/view/task.html.php
@@ -19,69 +19,6 @@ js::set('moduleID', $moduleID);
 js::set('productID', $productID);
 js::set('browseType', $browseType);
 ?>
-<div class='sub-featurebar'>
-  <ul class='nav'>
-    <li style='padding-top:5px;'>
-      <span>
-        <?php
-        if($productID)
-        {
-            $product    = $this->product->getById($productID);
-            $removeLink = $browseType == 'byproduct' ? inlink('task', "projectID=$projectID&browseType=$status&param=0&orderBy=$orderBy&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}") : 'javascript:removeCookieByKey("productBrowseParam")';
-            echo $product->name;
-            echo '&nbsp;' . html::a($removeLink, "<i class='icon icon-remove'></i>") . '&nbsp;';
-        }
-        elseif($moduleID)
-        {
-            $module     = $this->tree->getById($moduleID);
-            $removeLink = $browseType == 'bymodule' ? inlink('task', "projectID=$projectID&browseType=$status&param=0&orderBy=$orderBy&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}") : 'javascript:removeCookieByKey("moduleBrowseParam")';
-            echo $module->name;
-            echo '&nbsp;' . html::a($removeLink, "<i class='icon icon-remove'></i>") . '&nbsp;';
-        }
-        else
-        {
-            echo $this->lang->tree->all;
-        }
-        echo " <i class='icon-angle-right'></i>&nbsp; ";
-        ?>
-      </span>
-    </li>
-    <?php foreach(customModel::getFeatureMenu($this->moduleName, $this->methodName) as $menuItem):?>
-    <?php
-    if(isset($menuItem->hidden)) continue;
-    $type = $menuItem->name;
-    if(strpos($type, 'QUERY') === 0)
-    {
-        $queryID = (int)substr($type, 5);
-        echo "<li id='{$type}Tab'>" . html::a(inlink('task', "project=$projectID&type=bySearch&param=$queryID"), $menuItem->text) . '</li>' ;
-    }
-    elseif($type != 'status')
-    {
-        echo "<li id='{$type}Tab'>" . html::a(inlink('task', "project=$projectID&type=$type"), $menuItem->text) . '</li>' ;
-    }
-    elseif($type == 'status')
-    {
-        echo "<li id='statusTab' class='dropdown'>";
-        $taskBrowseType = isset($status) ? $this->session->taskBrowseType : '';
-        $current        = zget($lang->project->statusSelects, $taskBrowseType, '');
-        if(empty($current)) $current = $menuItem->text;
-        echo html::a('javascript:;', $current . " <span class='caret'></span>", '', "data-toggle='dropdown'");
-        echo "<ul class='dropdown-menu'>";
-        foreach ($lang->project->statusSelects as $key => $value)
-        {
-            if($key == '') continue;
-            echo '<li' . ($key == $taskBrowseType ? " class='active'" : '') . '>';
-            echo html::a($this->createLink('project', 'task', "project=$projectID&type=$key"), $value);
-        }
-        echo '</ul></li>';
-    }
-    ?>
-    <?php endforeach;?>
-    <?php echo "<li id='bysearchTab'><a href='#'><i class='icon-search icon'></i>&nbsp;{$lang->project->byQuery}</a></li> ";?>
-  </ul>
-  <div id='querybox' class='<?php if($browseType == 'bysearch') echo 'show';?>'></div>
-</div>
-
 <div class='side' id='taskTree'>
   <a class='side-handle' data-id='projectTree'><i class='icon-caret-left'></i></a>
   <div class='side-body'>
