5331afa0baa1e8cefc57689cad933dabe5275a0b
chenfeiCF <chenfei@qdphp.com>
2016-05-13 10:31:38 +0800

    * user module does not have featurebar so it is no use showing search tag on bar menu.

--- module/search/view/savequery.html.php
+++ module/search/view/savequery.html.php
@@ -14,12 +14,14 @@
 <form target='hiddenwin' method='post' style='padding: 20px 5% 30px'>
   <div class='input-group'>
     <input name='title' id='title' class="form-control" autocomplete="off" type="text">
+    <?php if($module != 'user'):?>
     <span class='input-group-addon'>
       <label class="checkbox-inline">
         <input type="checkbox" name="onMenuBar" value="1" id="onMenuBar" />
         <?php echo $lang->search->onMenuBar?>
       </label>
     </span>
+    <?php endif;?>
     <span class='input-group-btn'><?php echo html::submitButton() . html::hidden('module', $module)?></span>
   </div>
 </form>
