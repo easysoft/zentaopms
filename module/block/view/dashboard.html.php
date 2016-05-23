16f8c282020a7e3bcd98caf8adc6f4dab8e78155
wangyidong <yidong@cnezsoft.com>
2016-05-23 09:20:59 +0800

    * adjust power for block.

--- module/block/view/dashboard.html.php
+++ module/block/view/dashboard.html.php
@@ -33,11 +33,13 @@ if(isset($pageCSS)) css::internal($pageCSS);
               <a href='javascript:;' data-toggle='dropdown' class='panel-action'><i class='icon icon-ellipsis-v'></i></a>
               <ul class='dropdown-menu pull-right'>
                 <li><a href='javascript:;' class='refresh-panel'><i class='icon-repeat'></i> <?php echo $lang->block->refresh ?></a></li>
+                <?php if($this->app->user->account != 'guest'):?>
                 <li><a data-toggle='modal' href="<?php echo $this->createLink("block", "admin", "id=$block->id&module=$module"); ?>" class='edit-block' data-title='<?php echo $block->title; ?>' data-icon='icon-pencil'><i class='icon-pencil'></i> <?php echo $lang->edit; ?></a></li>
                 <?php if(!$block->source and $block->block == 'html'):?>
                 <li><a href="javascript:hiddenBlock(<?php echo $index;?>)" class="hidden-panel"><i class='icon-eye-close'></i><?php echo $lang->block->hidden; ?></a></li>
                 <?php endif;?>
                 <li><a href='javascript:;' class='remove-panel'><i class='icon-remove'></i> <?php echo $lang->delete; ?></a></li>
+                <?php endif;?>
               </ul>
             </div>
           </div>
@@ -56,12 +58,4 @@ config.confirmRemoveBlock = '<?php echo $lang->block->confirmRemoveBlock; ?>';
 var module = '<?php echo $module?>';
 </script>
 <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
-<?php 
-if(isset($pageJS)) js::execute($pageJS);
-
-/* Load hook files for current page. */
-$extPath      = dirname(dirname(dirname(realpath($viewFile)))) . '/common/ext/view/';
-$extHookRule  = $extPath . 'footer.*.hook.php';
-$extHookFiles = glob($extHookRule);
-if($extHookFiles) foreach($extHookFiles as $extHookFile) include $extHookFile;
-?>
+<?php if(isset($pageJS)) js::execute($pageJS);?>
