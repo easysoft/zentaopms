978257a4187099a23692e9c9e5179d02ded25607
wangyidong <yidong@cnezsoft.com>
2016-06-06 14:24:57 +0800

    * finish task #2617.

--- module/bug/view/create.html.php
+++ module/bug/view/create.html.php
@@ -71,7 +71,7 @@ js::set('refresh', $lang->refresh);
             <?php endif;?>
             <span id='buildBox'><?php echo html::select('openedBuild[]', $builds, $buildID, "size=4 multiple=multiple class='chosen form-control'");?></span>
             <span class='input-group-addon' id='buildBoxActions'></span>
-            <span class='input-group-btn'><?php echo html::commonButton('<i class="icon icon-refresh"></i>', "class='btn btn-default' data-toggle='tooltip' onclick='loadAllBuilds()' title='{$lang->bug->allBuilds}'")?></span>
+            <span class='input-group-btn'><?php echo html::commonButton('<i class="icon icon-filter"></i>', "class='btn btn-default' data-toggle='tooltip' onclick='loadAllBuilds()' title='{$lang->bug->allBuilds}'")?></span>
           </div>
         </td>
       </tr>
@@ -80,7 +80,7 @@ js::set('refresh', $lang->refresh);
         <td>
           <div class='input-group'>
             <span id='assignedToBox'><?php echo html::select('assignedTo', $projectMembers, $assignedTo, "class='form-control chosen'");?></span>
-            <span class='input-group-btn'><?php echo html::commonButton('<i class="icon icon-refresh"></i>', "class='btn btn-default' onclick='loadAllUsers()' data-toggle='tooltip' title='{$lang->bug->allUsers}'");?></span>
+            <span class='input-group-btn'><?php echo html::commonButton('<i class="icon icon-filter"></i>', "class='btn btn-default' onclick='loadAllUsers()' data-toggle='tooltip' title='{$lang->bug->allUsers}'");?></span>
           </div>
         </td>
       </tr>
