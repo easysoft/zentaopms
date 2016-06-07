a0a9c3864fb0d6ebee12954da061c9e3f7538273
wangyidong <yidong@cnezsoft.com>
2016-06-07 11:45:12 +0800

    * finish task #2613.

--- module/product/view/roadmap.html.php
+++ module/product/view/roadmap.html.php
@@ -13,44 +13,46 @@
 <?php include '../../common/view/header.html.php';?>
 <div>
   <div id='titlebar'><div class='heading'><i class='icon-code-fork'></i> <?php echo $lang->product->roadmap;?></div></div>
-  <table class='table-grid w-p100'>
-    <?php 
-    $years = array_keys($roadmaps);
-    echo '<tr class="text-center">';
-    foreach($years as $year)
-    {
-        $colspans = count($roadmaps[$year]);
-        $year = $year == '0000' ? $lang->future : $year . $lang->year;
-        echo "<th" . (empty($branches) ? '' : " colspan='$colspans'") . "><h4>$year</h4></th>";
-    }
-    echo '</tr>';
-    echo '<tr class="text-center text-top">';
-    foreach($years as $year)
-    {
-        foreach($roadmaps[$year] as $branch => $roadmapData)
-        {
-            echo '<td>';
-            if(!empty($branches)) echo "<div class='roadmap branch'>{$branches[$branch]}</div>";
-            foreach($roadmapData as $key => $roadmap)
-            {
-                if(isset($roadmap->build))
-                {
-                    echo "<div class='roadmap release'>";
-                    echo "<h5>" . html::a($this->createLink('release', 'view', "releaseID=$roadmap->id"), $roadmap->name, '_blank') . '</h5>' . $roadmap->date;
-                }
-                else
-                {
-                    echo "<div class='roadmap plan'>";
-                    echo "<h5>" . html::a($this->createLink('productplan', 'view', "planID=$roadmap->id"), $roadmap->title, '_blank') . '</h5>' . $roadmap->begin . ' ~ ' . $roadmap->end;
-                }
-                echo "</div>";
-                if(isset($roadmaps[$year][$branch][$key + 1])) echo "<h5>{$lang->downArrow}</h5>";
-            }
-            echo '</td>';
-        }
-    }
-    echo '</tr>';
-    ?>
-  </table>
+  <div id='roadmapBox' style='overflow-x:auto'>
+    <table class='table table-grid active-disabled'>
+      <?php 
+      $years = array_keys($roadmaps);
+      echo '<tr class="text-center">';
+      foreach($years as $year)
+      {
+          $colspans = count($roadmaps[$year]);
+          $year = $year == '0000' ? $lang->future : $year . $lang->year;
+          echo "<th" . (empty($branches) ? '' : " colspan='$colspans'") . " style='border-right:1px solid #ddd'><h4>$year</h4></th>";
+      }
+      echo '</tr>';
+      echo '<tr class="text-center text-top">';
+      foreach($years as $year)
+      {
+          foreach($roadmaps[$year] as $branch => $roadmapData)
+          {
+              echo "<td>";
+              if(!empty($branches)) echo "<div class='roadmap branch'>{$branches[$branch]}</div>";
+              foreach($roadmapData as $key => $roadmap)
+              {
+                  if(isset($roadmap->build))
+                  {
+                      echo "<div class='roadmap release'>";
+                      echo "<h5>" . html::a($this->createLink('release', 'view', "releaseID=$roadmap->id"), $roadmap->name, '_blank') . '</h5>' . $roadmap->date;
+                  }
+                  else
+                  {
+                      echo "<div class='roadmap plan'>";
+                      echo "<h5>" . html::a($this->createLink('productplan', 'view', "planID=$roadmap->id"), $roadmap->title, '_blank') . '</h5>' . $roadmap->begin . ' ~ ' . $roadmap->end;
+                  }
+                  echo "</div>";
+                  if(isset($roadmaps[$year][$branch][$key + 1])) echo "<h5>{$lang->downArrow}</h5>";
+              }
+              echo '</td>';
+          }
+      }
+      echo '</tr>';
+      ?>
+    </table>
+  </div>
 </div>
 <?php include '../../common/view/footer.html.php';?>
