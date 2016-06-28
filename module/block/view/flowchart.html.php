ca7df0332e95aad0a82a9fa24b1a7ea7ddcd1273
Catouse <catouse@gmail.com>
2016-06-28 10:14:22 +0800

    * change flowchart UI and improve browser compatibility.

--- module/block/view/flowchart.html.php
+++ module/block/view/flowchart.html.php
@@ -11,10 +11,12 @@
 <style>
 .flowchart {padding: 10px 15px 1px 10px; min-width: 500px;}
 .flow-item {float: left; width: 16.66667%; max-width: 180px; text-align: center; margin-bottom: 9px; padding-right: 5px;}
-.flow-item > div {position: relative; padding: 5px 0 5px 12px; line-height: 20px; background: #90A4AE; white-space:nowrap; overflow: visible; color: #fff}
+.flow-item > div {position: relative; padding: 5px 0 5px 12px; line-height: 20px; background: #90A4AE; white-space:nowrap; overflow: visible; color: #fff;}
 .flow-item > div:before, .flow-item > div:after {content: ' '; display: block; width: 0; height: 0; border-style: solid; border-width: 15px 0 15px 15px; border-color: transparent transparent transparent #90A4AE; position: absolute; left: 0; top: 0}
+.ie-8 .flow-item > div:before {display: none}
 .flow-item > div:before {border-left-color: #fff; z-index: 1}
-.flow-item > div:after {left: auto; right: -15px; z-index: 2}
+.flow-item > div:after {left: auto; right: -14px; z-index: 2}
+.ie-8 .flow-item > div {margin-right: 10px}
 .flow-item-0 > div:before {display: none}
 .flow-item-1 > div {background: #1976D2}
 .flow-item-1 > div:after {border-left-color: #1976D2}
