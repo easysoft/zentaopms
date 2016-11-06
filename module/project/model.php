250be506421d5b714b998de6d069ac062633c81e
wangyidong <yidong@cnezsoft.com>
2016-11-06 19:02:42 +0800

    * fix bug for project link doclib.

--- module/project/model.php
+++ module/project/model.php
@@ -273,7 +273,7 @@ class projectModel extends model
             $lib->project = $projectID;
             $lib->name    = $project->name;
             $lib->main    = '1';
-            $lib->acl     = $project->acl;
+            $lib->acl     = $project->acl == 'open' ? 'open' : 'custom';
             if($project->acl == 'custom') $lib->groups = $project->whitelist;
             if($project->acl == 'private')
             {
@@ -340,7 +340,7 @@ class projectModel extends model
         {
             if($project->acl != $oldProject->acl)
             {
-                $this->dao->update(TABLE_DOCLIB)->set('acl')->eq($project->acl)->where('project')->eq($projectID)->exec();
+                $this->dao->update(TABLE_DOCLIB)->set('acl')->eq($project->acl == 'open' ? 'open' : 'custom')->where('project')->eq($projectID)->exec();
                 if($project->acl == 'open')    $this->dao->update(TABLE_DOCLIB)->set('groups')->eq('')->set('users')->eq('')->where('project')->eq($projectID)->exec();
                 if($project->acl == 'custom')  $this->dao->update(TABLE_DOCLIB)->set('groups')->eq($project->whitelist)->where('project')->eq($projectID)->exec();
                 if($project->acl == 'private')
@@ -1423,7 +1423,7 @@ class projectModel extends model
             }
         }
 
-        $acl = $this->dao->select('acl')->from(TABLE_PROJECT)->where('project')->eq($projectID)->fetch('acl');
+        $acl = $this->dao->select('acl')->from(TABLE_PROJECT)->where('id')->eq($projectID)->fetch('acl');
         if($acl == 'private')
         {
             $docLibs = $this->dao->select('id,users')->from(TABLE_DOCLIB)->where('project')->eq($projectID)->fetchAll('id');
