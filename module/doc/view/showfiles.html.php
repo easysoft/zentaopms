df531594e8966648854f1f291204405a2ebdb323
wangyidong <yidong@cnezsoft.com>
2016-11-14 14:11:20 +0800

    * finish task #2804.

--- module/doc/view/showfiles.html.php
+++ module/doc/view/showfiles.html.php
@@ -45,7 +45,7 @@
         <i class='icon icon-download'></i>
       </a>
       <div class='card-heading'>
-        <a href='<?php echo $this->createLink($file->objectType, 'view', "objectID=$file->objectID"); ?>'><?php echo $lang->doc->fileObject . ' ' . strtoupper($file->objectType) . ' #' . $file->objectID ?></a>
+        <a href='<?php echo $this->createLink($file->objectType, 'view', "objectID=$file->objectID");?>'><?php echo $file->title . '.' . $file->extension . ' [' . strtoupper($file->objectType) . ' #' . $file->objectID . ']';?></a>
         <a href='<?php echo $this->createLink('file', 'delete', "fileID=$file->id"); ?>' target='hiddenwin' title='<?php echo $lang->delete?>' class='delete pull-right'><i class='icon icon-remove'></i></a>
       </div>
     </div>
