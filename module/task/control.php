9f5591d2ead797456ee82ab0e9c6ab19e9f0abda
wangyidong <yidong@cnezsoft.com>
2017-01-05 14:39:03 +0800

    * adjust for members when edit.

--- module/task/control.php
+++ module/task/control.php
@@ -282,7 +282,7 @@ class task extends control
         unset($noclosedProjects[$this->view->project->id]);
         $this->view->projects = array($this->view->project->id => $this->view->project->name) + $noclosedProjects;
 
-        if(!isset($members[$this->view->task->assignedTo])) $members[$this->view->task->assignedTo] = $this->view->task->assignedTo;
+        if(!isset($this->view->members[$this->view->task->assignedTo])) $this->view->members[$this->view->task->assignedTo] = $this->view->task->assignedTo;
         $this->view->title      = $this->lang->task->edit . 'TASK' . $this->lang->colon . $this->view->task->name;
         $this->view->position[] = $this->lang->task->common;
         $this->view->position[] = $this->lang->task->edit;
