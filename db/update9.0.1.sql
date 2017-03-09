e306f0d364f0db146db688ef0cc11a3c528bb435
wangyidong <yidong@cnezsoft.com>
2017-03-09 09:08:09 +0800

    * update db.

--- db/update9.0.1.sql
+++ db/update9.0.1.sql
@@ -22,3 +22,27 @@ ALTER TABLE `zt_case` ADD `lib` mediumint(8) unsigned NOT NULL DEFAULT '0' AFTER
 ALTER TABLE `zt_case` ADD `fromLib` mediumint(8) unsigned NOT NULL AFTER `fromBug`;
 ALTER TABLE `zt_case` ADD `reviewedBy` varchar(255) NOT NULL AFTER `openedDate`;
 ALTER TABLE `zt_case` ADD `reviewedDate` date NOT NULL AFTER `reviewedBy`;
+ALTER TABLE `zt_casestep` ADD `parent` mediumint(8) unsigned NOT NULL DEFAULT '0' AFTER `id`;
+ALTER TABLE `zt_casestep` ADD `type` varchar(10) NOT NULL DEFAULT 'item' AFTER `version`;
+CREATE TABLE `zt_testreport` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `product` mediumint(8) unsigned NOT NULL,
+  `project` mediumint(8) unsigned NOT NULL,
+  `tasks` varchar(255) NOT NULL,
+  `builds` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `begin` date NOT NULL,
+  `end` date NOT NULL,
+  `owner` char(30) NOT NULL,
+  `members` text NOT NULL,
+  `stories` text NOT NULL,
+  `bugs` text NOT NULL,
+  `cases` text NOT NULL,
+  `report` text NOT NULL,
+  `objectType` varchar(20) NOT NULL,
+  `objectID` mediumint(8) unsigned NOT NULL,
+  `createdBy` char(30) NOT NULL,
+  `createdDate` datetime NOT NULL,
+  `deleted` enum('0','1') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
