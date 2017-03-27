dce2c6a5e83b3fdb5ec814d29477e60889369d96
wangyidong <yidong@cnezsoft.com>
2017-03-27 15:25:54 +0800

    * fix bug #994.

--- module/user/view/testcase.html.php
+++ module/user/view/testcase.html.php
@@ -42,10 +42,11 @@
   </thead>
   <tbody>
     <?php foreach($cases as $case):?>
+    <?php $caseID = $type == 'case2Him' ? $case->case : $case->id?>
     <tr class='text-center'>
-      <td><?php echo html::a($this->createLink('testcase', 'view', "testcaseID=$case->id"), sprintf('%03d', $case->id));?></td>
+      <td><?php echo html::a($this->createLink('testcase', 'view', "testcaseID=$caseID&version=$case->version"), sprintf('%03d', $caseID));?></td>
       <td><span class='<?php echo 'pri' . zget($lang->testcase->priList, $case->pri, $case->pri)?>'><?php echo zget($lang->testcase->priList, $case->pri, $case->pri)?></span</td>
-      <td class='text-left'><?php echo html::a($this->createLink('testcase', 'view', "testcaseID=$case->id"), $case->title);?></td>
+      <td class='text-left'><?php echo html::a($this->createLink('testcase', 'view', "testcaseID=$caseID&version=$case->version"), $case->title);?></td>
       <td><?php echo $lang->testcase->typeList[$case->type];?></td>
       <td><?php echo $users[$case->openedBy];?></td>
       <td><?php echo $users[$case->lastRunner];?></td>
