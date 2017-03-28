3b6ab707ff57160785e53dc9bfdd484c23e4290a
chenfeiCF <chenfei@qdphp.com>
2017-03-28 09:43:12 +0800

    * delete useless mobile web file.

--- module/bug/view/m.view.html.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$this->session->set('bugID', $bug->id);
-if($this->session->bugType) die($this->locate($this->createLink('my', 'bug', "type={$this->session->bugType}")));
-die($this->locate($this->createLink('bug', 'browse', "productID=$productID")));
-?>
