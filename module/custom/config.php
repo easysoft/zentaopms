2edc510b7743b44e16c2003ff9896073d81ba1ba
wangyidong <yidong@cnezsoft.com>
2017-05-03 16:18:49 +0800

    * can not add custom item for reviewResult.

--- module/custom/config.php
+++ module/custom/config.php
@@ -1,6 +1,6 @@
 <?php
 $config->custom = new stdClass();
-$config->custom->canAdd['story']    = 'reasonList,reviewResultList,sourceList,priList';
+$config->custom->canAdd['story']    = 'reasonList,sourceList,priList';
 $config->custom->canAdd['task']     = 'priList,typeList,reasonList';
 $config->custom->canAdd['bug']      = 'priList,severityList,osList,browserList,typeList,resolutionList';
 $config->custom->canAdd['testcase'] = 'priList,typeList,stageList,resultList,statusList';
