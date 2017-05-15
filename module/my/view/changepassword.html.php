954539c4c83f2c2a83ba12f82b16afe310e67764
wangyidong <yidong@cnezsoft.com>
2017-05-15 16:22:43 +0800

    * adjust for safe.

--- module/my/view/changepassword.html.php
+++ module/my/view/changepassword.html.php
@@ -46,6 +46,7 @@
     <?php if(!empty($this->app->user->modifyPasswordReason)):?>
     <?php $this->app->loadLang('admin');?>
     <div class='alert alert-info'>
+      <?php echo $lang->admin->safe->common . ' : ';?>
       <?php echo $this->app->user->modifyPasswordReason == 'weak' ? $lang->admin->safe->changeWeak : $lang->admin->safe->modifyPasswordFirstLogin;?>
     </div>
     <?php endif;?>
