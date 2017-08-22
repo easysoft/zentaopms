7a318c92ab43b05da9bf48f819a2923e4ef9502e
Catouse <catouse@gmail.com>
2017-08-22 15:33:45 +0800

    * support open todo view modal on load page with hash prefix 'todo-'.

--- module/my/js/todo.js
+++ module/my/js/todo.js
@@ -4,3 +4,21 @@ function changeDate(date)
     link = createLink('my', 'todo', 'type=' + date);
     location.href=link;
 }
+
+
+$(function()
+{
+    // Support hash start with todo-1
+    var hash = window.location.hash.toLowerCase();
+    if(hash.indexOf('#todo-') === 0)
+    {
+        var todoId = hash.substr(6);
+        var url = createLink('todo', 'view', 'id=1&from=my&onlybody=yes');
+        var $a = $('<a/>');
+        $a.attr({href: url}).modalTrigger(
+        {
+            'data-toggle': 'modal',
+            type: 'iframe'
+        }).trigger('click');
+    }
+});
