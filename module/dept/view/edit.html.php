3400e5a1433354569703a6f9d2e46e76b32c903a
wangyidong <yidong@cnezsoft.com>
2017-09-06 14:36:48 +0800

    * adjust for edit in modal.

--- module/dept/view/edit.html.php
+++ module/dept/view/edit.html.php
@@ -16,13 +16,11 @@ $jsRoot  = $webRoot . "js/";
 ?>
 <?php include '../../common/view/chosen.html.php';?>
 <div class='modal-dialog w-500px'>
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+    <h4 class="modal-title"><strong><small class='text-muted'><?php echo html::icon($lang->icons['edit']);?></small> <?php echo $lang->dept->edit;?></strong></h4>
+  </div>
   <div class='modal-body'>
-    <div id='titlebar'>
-      <div class='heading'>
-        <span class='prefix'><?php echo html::icon($lang->icons['tree']);?></span>
-        <strong><small class='text-muted'><?php echo html::icon($lang->icons['edit']);?></small> <?php echo $lang->dept->edit;?></strong>
-      </div>
-    </div>
     <form action="<?php echo inlink('edit', 'deptID=' . $dept->id);?>" target='hiddenwin' class='form-condensed' method='post' class='mt-10px' id='dataform'>
       <table class='table table-form' style='width:100%'> 
         <tr>
