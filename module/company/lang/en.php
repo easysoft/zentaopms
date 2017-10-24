7eefad2430248f8d53f1f090b080a09a0535111c
sgm0422 <976204163@qq.com>
2017-10-24 09:19:44 +0800

    * Fix bug #1237.

--- module/company/lang/en.php
+++ module/company/lang/en.php
@@ -31,5 +31,5 @@ $lang->company->product = $lang->productCommon;
 $lang->company->project = $lang->projectCommon;
 $lang->company->user    = 'User';
 
-$lang->company->guestList[0] = 'On';
-$lang->company->guestList[1] = 'Off';
+$lang->company->guestList[0] = 'Off';
+$lang->company->guestList[1] = 'On';
