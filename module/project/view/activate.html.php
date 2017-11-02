e6b8226f535cd0e06d80c2683ed2366460eb5606
wangyidong <yidong@cnezsoft.com>
2017-11-02 16:55:50 +0800

    * finish task #3295.

--- module/project/view/activate.html.php
+++ module/project/view/activate.html.php
@@ -26,22 +26,22 @@
     <table class='table table-form'>
       <tr>
         <th class='w-80px text-right'><?php echo $lang->project->beginAndEnd;?></th>
-        <td class='w-200px'><?php echo $project->begin . ' ~ ' . $project->end;?></td>
+        <td class='w-250px'>
+          <div id='sourceTimeBox'>
+            <span style='display:inline-block; padding:5px 10px; width:85px'><?php echo $project->begin?></span>
+            <span style='padding:5px 10px'> ~ </span>
+            <span style='display:inline-block; padding:5px 10px; width:85px'><?php echo $project->end;?></span>
+          </div>
+          <div id='readjustTimeBox' class='hide'>
+            <span style='display:inline-block; width:85px'><?php echo html::input('begin', $newBegin, "class='form-control form-date'")?></span>
+            <span style='padding:5px 10px;'> ~ </span>
+            <span style='display:inline-block; width:85px'><?php echo html::input('end', $newEnd, "class='form-control form-date'");?></span>
+          </div>
+        </td>
+        </td>
         <td>
           <label class="checkbox-inline"><input name="readjustTime" value="1" id="readjustTime" type="checkbox"> <strong><?php echo $lang->project->readjustTime?></strong></label>
-        </td>
-      </tr>
-      <tr id='readjustTimeBox' class='hide'>
-        <th></th>
-        <td colspan='2'>
-          <div class='input-group'>
-            <?php echo html::input('begin', $newBegin, "class='form-control form-date'")?>
-            <span class='input-group-addon'> ~ </span>
-            <?php echo html::input('end', $newEnd, "class='form-control form-date'")?>
-            <span class='input-group-addon'>
-              <label class="checkbox-inline"><input name="readjustTask" value="1" id="readjustTask" type="checkbox"> <strong><?php echo $lang->project->readjustTask?></strong></label>
-            </span>
-          </div>
+          <span id='readjustTaskBox' class='hide'><label class="checkbox-inline"><input name="readjustTask" value="1" id="readjustTask" type="checkbox"> <strong><?php echo $lang->project->readjustTask?></strong></label></span>
         </td>
       </tr>
       <tr>
@@ -61,7 +61,9 @@ $(function()
 {
     $('#readjustTime').change(function()
     {
+        $('#sourceTimeBox').toggle(!$(this).prop('checked'))
         $('#readjustTimeBox').toggle($(this).prop('checked'))
+        $('#readjustTaskBox').toggle($(this).prop('checked'))
     })
 })
 </script>
