ec374fd2d7d135c71e277c9143078b30fd5dcfb3
wangyidong <yidong@cnezsoft.com>
2018-03-23 10:59:11 +0800

    * code for task #3685.

--- module/doc/view/index.html.php
+++ module/doc/view/index.html.php
@@ -43,9 +43,9 @@
           ?>
           <div class='libs-group-heading libs-product-heading text-ellipsis'>
             <?php
+            if($subLibCount > 3) echo html::a(inlink('objectLibs', "type=product&objectID=$product->id&from=doc"), "{$lang->more}<i class='icon icon-double-angle-right'></i>", '', "title='{$lang->more}' class='pull-right'");
             $label = $objectNum == 1 ? "<span class='label label-primary'>{$lang->doclib->product}</span> " : '';
             echo html::a(inlink('objectLibs', "type=product&objectID=$product->id&from=doc"), $label . $product->name, '', "title='{$product->name}'");
-            if($subLibCount > 3) echo html::a(inlink('objectLibs', "type=product&objectID=$product->id&from=doc"), "{$lang->more}<i class='icon icon-double-angle-right'></i>", '', "title='{$lang->more}' class='pull-right'");
             ?>
           </div>
           <div class='libs-group clearfix'>
