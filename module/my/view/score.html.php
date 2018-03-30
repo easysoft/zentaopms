b51a42c506c9573e423526f8a8d9128080e363fc
wangyidong <yidong@cnezsoft.com>
2018-03-30 11:25:07 +0800

    * change for my score page.

--- module/my/view/score.html.php
+++ module/my/view/score.html.php
@@ -10,45 +10,49 @@
  */
 ?>
 <?php include '../../common/view/header.html.php';?>
-<div id='titlebar'>
-  <div class='heading'><?php echo html::icon($lang->icons['score']); ?><?php echo $lang->score->record; ?></div>
-  <div class='actions'>
-    <a class='btn disabled'><strong><?php echo $lang->score->current; ?>:</strong><?php echo $user->score; ?></a>
-    <span class='btn disabled hidden'><label><?php echo $lang->score->level; ?>:</label><?php echo $user->scoreLevel; ?></span>
-    <?php common::printLink('score', 'rule', '', $lang->my->scoreRule, '', "class='btn btn-primary'");?>
+<main id="main">
+  <div class="container">
+    <div id="mainMenu" class="clearfix">
+      <div class="btn-toolbar pull-left">
+        <span class='btn btn-link btn-active-text'><span class='text'><?php echo $lang->score->record;?></span></span>
+      </div>
+      <div class="btn-toolbar pull-right">
+        <span class='btn text'><strong><?php echo $lang->score->current; ?>:</strong><?php echo $user->score; ?></span>
+        <span class='btn text hidden'><label><?php echo $lang->score->level; ?>:</label><?php echo $user->scoreLevel; ?></span>
+        <?php common::printLink('score', 'rule', '', $lang->my->scoreRule, '', "class='btn btn-primary'");?>
+      </div>
+    </div>
+    <div id="mainContent">
+      <table class="main-table table has-sort-head table-lg table-fixed">
+        <thead>
+          <tr>
+            <th class="w-200px"><?php echo $lang->score->time; ?></th>
+            <th class="w-150px"><?php echo $lang->score->module; ?></th>
+            <th class="w-150px"><?php echo $lang->score->method; ?></th>
+            <th class="w-100px"><?php echo $lang->score->before; ?></th>
+            <th class="w-100px"><?php echo $lang->score->score; ?></th>
+            <th class="w-100px"><?php echo $lang->score->after; ?></th>
+            <th><?php echo $lang->score->desc; ?></th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach($scores as $score):?>
+          <tr>
+            <td><?php echo $score->time; ?></td>
+            <td><?php echo $lang->score->modules[$score->module]; ?></td>
+            <td><?php echo $lang->score->methods[$score->module][$score->method]; ?></td>
+            <td><?php echo $score->before; ?></td>
+            <td><?php echo $score->score; ?></td>
+            <td><?php echo $score->after; ?></td>
+            <td><?php echo $score->desc; ?></td>
+          </tr>
+          <?php endforeach;?>
+        </tbody>
+      </table>
+      <?php if($scores):?>
+      <div class="table-footer"><?php $pager->show('right', 'pagerjs');?></div>
+      <?php endif;?>
+    </div>
   </div>
-</div>
-<table class='table tablesorter'>
-  <thead>
-    <tr class='colhead'>
-      <th class="w-200px"><?php echo $lang->score->time; ?></th>
-      <th class="w-150px"><?php echo $lang->score->module; ?></th>
-      <th class="w-150px"><?php echo $lang->score->method; ?></th>
-      <th class="w-100px"><?php echo $lang->score->before; ?></th>
-      <th class="w-100px"><?php echo $lang->score->score; ?></th>
-      <th class="w-100px"><?php echo $lang->score->after; ?></th>
-      <th><?php echo $lang->score->desc; ?></th>
-    </tr>
-  </thead>
-  <?php if(!empty($scores)):?>
-  <tbody>
-    <?php foreach($scores as $score):?>
-    <tr class='text-center'>
-      <td><?php echo $score->time; ?></td>
-      <td><?php echo $lang->score->modules[$score->module]; ?></td>
-      <td><?php echo $lang->score->methods[$score->module][$score->method]; ?></td>
-      <td><?php echo $score->before; ?></td>
-      <td><?php echo $score->score; ?></td>
-      <td><?php echo $score->after; ?></td>
-      <td><?php echo $score->desc; ?></td>
-    </tr>
-    <?php endforeach;?>
-  </tbody>
-  <?php endif;?>
-  <tfoot>
-    <tr>
-      <td colspan='7'><?php $pager->show();?></td>
-    </tr>
-  </tfoot>
-</table>
+</main>
 <?php include '../../common/view/footer.html.php';?>
