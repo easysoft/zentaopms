6c9760c77135d59ccc8561be1383418e24c415f4
JackWuLieHao <wuliehao@cnezsoft.com>
2018-04-04 17:02:49 +0800

    * adjust style

--- module/user/view/edit.html.php
+++ module/user/view/edit.html.php
@@ -28,7 +28,7 @@
         <th class='w-90px'><?php echo $lang->user->realname;?></th>
         <td class='w-p40'><?php echo html::input('realname', $user->realname, "class='form-control' autocomplete='off'");?></td>
         <th class='w-90px'><?php echo $lang->user->join;?></th>
-        <td><?php echo html::input('join', formatTime($user->join, "class='form-control form-date'");?></td>  
+        <td><?php echo html::input('join', formatTime($user->join), "class='form-control form-date'");?></td>  
       </tr>
       <tr>
         <th class='w-90px'><?php echo $lang->user->dept;?></th>
