f1db15d15b160e4c1caf48326c9534d6131013f8
wangyidong <yidong@cnezsoft.com>
2018-04-18 13:14:07 +0800

    * adjust for branch manage page.

--- module/branch/view/manage.html.php
+++ module/branch/view/manage.html.php
@@ -11,45 +11,47 @@
  */
 ?>
 <?php include '../../common/view/header.html.php';?>
-<div class="container mw-700px">
-  <div id="titlebar">
-    <div class="heading"><strong><?php printf($lang->branch->manageTitle, $this->lang->product->branchName[$this->session->currentProductType])?></strong></div>
+<div id='mainContent' class='main-content'>
+  <div class="center-block mw-700px">
+    <div class="main-header">
+      <h2><?php printf($lang->branch->manageTitle, $this->lang->product->branchName[$this->session->currentProductType])?></h2>
+    </div>
+    <form method='post' target='hiddenwin'>
+      <table class="table table-form">
+        <tr>
+          <td class='w-50px'></td>
+          <td class="w-300px">
+            <div id='branches'>
+              <?php foreach($branches as $branchID => $branch):?>
+              <div class='input-group' data-id='<?php echo $branchID?>'>
+                <?php echo html::input("branch[$branchID]", $branch, "class='form-control'")?>
+                <?php if(common::hasPriv('branch', 'sort')):?>
+                <span class='input-group-addon sort-handler'><a><i class='icon icon-move'></i></a></span>
+                <?php endif;?>
+                <?php if(common::hasPriv('branch', 'delete')):?>
+                <span class='input-group-addon'><?php echo html::a(inlink('delete', "branchID=$branchID"), "<i class='icon icon-trash'></i>", 'hiddenwin')?></span>
+                <?php endif;?>
+              </div>
+              <?php endforeach;?>
+             </div>
+             <div id='newbranches'>
+              <?php for($i = 0; $i < 2; $i++):?>
+              <div class='input-group'>
+                <?php echo html::input("newbranch[]", '', "class='form-control'")?>
+                 <span class='input-group-addon'><a href='javascript:;' onclick='addItem(this)'><i class='icon icon-plus'></i></a></span>
+                 <span class='input-group-addon'><a href='javascript:;' onclick='deleteItem(this)'><i class='icon icon-trash'></i></a></span>
+              </div>
+              <?php endfor;?>
+             </div>
+           </td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><?php echo html::submitButton();?> </td><td></td>
+        </tr>
+      </table>
+    </form>
   </div>
-  <form method='post' target='hiddenwin'>
-  <table class="table table-form">
-    <tr>
-      <td class='w-50px'></td>
-      <td class="w-300px">
-        <div id='branches'>
-          <?php foreach($branches as $branchID => $branch):?>
-          <div class='input-group' data-id='<?php echo $branchID?>'>
-            <?php echo html::input("branch[$branchID]", $branch, "class='form-control'")?>
-            <?php if(common::hasPriv('branch', 'sort')):?>
-            <span class='input-group-addon sort-handler'><a><i class='icon icon-move'></i></a></span>
-            <?php endif;?>
-            <?php if(common::hasPriv('branch', 'delete')):?>
-            <span class='input-group-addon'><?php echo html::a(inlink('delete', "branchID=$branchID"), "<i class='icon icon-trash'></i>", 'hiddenwin')?></span>
-            <?php endif;?>
-          </div>
-          <?php endforeach;?>
-         </div>
-         <div id='newbranches'>
-          <?php for($i = 0; $i < 2; $i++):?>
-          <div class='input-group'>
-            <?php echo html::input("newbranch[]", '', "class='form-control'")?>
-             <span class='input-group-addon'><a href='javascript:;' onclick='addItem(this)'><i class='icon icon-plus'></i></a></span>
-             <span class='input-group-addon'><a href='javascript:;' onclick='deleteItem(this)'><i class='icon icon-trash'></i></a></span>
-          </div>
-          <?php endfor;?>
-         </div>
-       </td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td><?php echo html::submitButton();?> </td><td></td>
-    </tr>
-  </table>
-  </form>
 </div>
 <?php include '../../common/view/footer.html.php';?>
