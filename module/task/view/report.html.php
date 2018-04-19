83a01c416f18a7dadb0f1c289963c2bf291dfc15
liugang <liugang@cnezsoft.com>
2018-04-19 11:58:41 +0800

    * Refact report view of product stories and project tasks.

--- module/task/view/report.html.php
+++ module/task/view/report.html.php
@@ -11,79 +11,75 @@
  */
 ?>
 <?php include '../../common/view/header.html.php';?>
-<?php include '../../common/view/chart.html.php';?>
-<div id='titlebar'>
-  <div class='heading'>
-    <span class='prefix'><?php echo html::icon($lang->icons['task']);?></span>
-    <strong><small class='text-muted'><?php echo html::icon($lang->icons['report']);?></small> <?php echo $lang->task->report->common;?></strong>
-  </div>
-  <div class='actions'>
+<div id="mainMenu" class="clearfix">
+  <div class="btn-toolbar pull-left">
     <?php echo html::a($this->createLink('project', 'task', "projectID=$projectID&browseType=$browseType"), $lang->goback, '', "class='btn'");?>
+    <div class='divider'></div>
+    <div class='page-title'>
+      <span class='text'><?php echo $lang->task->report->common;?></span>
+    </div>
   </div>
 </div>
-<div class='row'>
-  <div class='col-md-3 col-lg-2'>
-    <div class='panel panel-sm'>
+<div id="mainContent" class='main-row'>
+  <div class='side-col col-lg'>
+    <div class='panel'>
       <div class='panel-heading'>
-        <strong><?php echo $lang->task->report->select;?></strong>
+        <div class='panel-title'><?php echo $lang->task->report->select;?></div>
       </div>
-      <div class='panel-body' style='padding-top:0'>
-        <form method='post'>
-          <?php echo html::checkBox('charts', $lang->task->report->charts, $checkedCharts, '', 'block')?>
-          <?php echo html::selectAll('', "button", false, 'btn-sm')?>
-          <?php echo html::submitButton($lang->task->report->create, "", 'btn btn-sm btn-primary');?>
+      <div class='panel-body'>
+        <form method='post' id='chartTypesForm'>
+          <div class='checkboxes'>
+            <?php echo html::checkBox('charts', $lang->task->report->charts, $checkedCharts, '', 'block')?>
+          </div>
+          <div class='btn-toolbar'>
+            <?php echo html::selectAll();?>
+            <?php echo html::submitButton($lang->task->report->create, "", 'btn btn-primary');?>
+          </div>
         </form>
       </div>
     </div>
   </div>
-  <div class='col-md-9 col-lg-10'>
-    <div class='panel panel-sm'>
-      <div class='panel-heading'>
-        <strong>
-          <?php echo $lang->task->report->common;?>
-          <div class="btn-group">
-          <?php foreach($lang->report->typeList as $type => $typeName):?>
-          <?php echo html::a("javascript:changeChartType(\"$type\")", $typeName, '', "class='btn btn-sm " . ($type == $chartType ? 'active' : '') . "'")?>
-          <?php endforeach;?>
-          </div>
-          <span><?php echo $lang->report->notice->help?></span>
-        </strong>
-      </div>
-      <table class='table active-disabled'>
-        <?php foreach($charts as $chartType => $chartOption):?>
-        <tr class='text-top'>
-          <td>
-            <div class='chart-wrapper text-center'>
-              <h5><?php echo $lang->task->report->charts[$chartType];?></h5>
-              <div class='chart-canvas'><canvas id='chart-<?php echo $chartType ?>' width='<?php echo $chartOption->width;?>' height='<?php echo $chartOption->height;?>' data-responsive='true'></canvas></div>
-            </div>
-          </td>
-          <td style='width: 320px'>
-            <div style="overflow:auto;" class='table-wrapper'>
-              <table class='table table-condensed table-hover table-striped table-bordered table-chart' data-chart='<?php echo $chartOption->type; ?>' data-target='#chart-<?php echo $chartType ?>' data-animation='false'>
-                <thead>
-                  <tr>
-                    <th class='chart-label' colspan='2'><?php echo $lang->task->report->$chartType->item;?></th>
-                    <th><?php echo $lang->task->report->value;?></th>
-                    <th><?php echo $lang->report->percent;?></th>
-                  </tr>
-                </thead>
-                <tbody>
-                <?php foreach($datas[$chartType] as $key => $data):?>
-                <tr class='text-center'>
-                  <td class='chart-color w-20px'><i class='chart-color-dot icon-circle'></i></td>
-                  <td class='chart-label'><?php echo $data->name;?></td>
-                  <td class='chart-value'><?php echo $data->value;?></td>
-                  <td><?php echo ($data->percent * 100) . '%';?></td>
-                </tr>
-                <?php endforeach;?>
-                </tbody>
-              </table>
-            </div>
-          </td>
-        </tr>
+  <div class='main-col'>
+    <div class='cell'>
+      <div class='btn-toolbar'>
+        <?php foreach($lang->report->typeList as $type => $typeName):?>
+        <?php echo html::a("javascript:changeChartType(\"$type\")", "<i class='icon icon-chart-{$type}'> </i>" . $typeName, '', "class='btn btn-link " . ($type == $chartType ? 'btn-active-line' : '') . "'")?>
         <?php endforeach;?>
-      </table>
+        <div class='pull-right with-padding text-muted'><?php echo $lang->report->notice->help;?></div>
+      </div>
+      <?php foreach($charts as $chartType => $chartOption):?>
+      <div class='table-row chart-row'>
+        <div class='main-col'>
+          <div class='chart-wrapper text-center'>
+            <h4><?php echo $lang->task->report->charts[$chartType];?></h4>
+            <div class='chart-canvas'><canvas id='chart-<?php echo $chartType ?>' width='<?php echo $chartOption->width;?>' height='<?php echo $chartOption->height;?>' data-responsive='true'></canvas></div>
+          </div>
+        </div>
+        <div class='side-col col-xl'>
+          <div style="overflow:auto;" class='table-wrapper'>
+            <table class='table table-condensed table-hover table-striped table-bordered table-chart' data-chart='<?php echo $chartOption->type; ?>' data-target='#chart-<?php echo $chartType ?>' data-animation='false'>
+              <thead>
+                <tr>
+                  <th class='chart-label' colspan='2'><?php echo $lang->task->report->$chartType->item;?></th>
+                  <th><?php echo $lang->task->report->value;?></th>
+                  <th><?php echo $lang->report->percent;?></th>
+                </tr>
+              </thead>
+              <tbody>
+              <?php foreach($datas[$chartType] as $key => $data):?>
+              <tr class='text-center'>
+                <td class='chart-color w-20px'><i class='chart-color-dot'></i></td>
+                <td class='chart-label'><?php echo $data->name;?></td>
+                <td class='chart-value'><?php echo $data->value;?></td>
+                <td><?php echo ($data->percent * 100) . '%';?></td>
+              </tr>
+              <?php endforeach;?>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <?php endforeach;?>
     </div>
   </div>
 </div>
