7719ba82d477eaaf193b62c8413ee8c4552c8df2
wangyidong <yidong@cnezsoft.com>
2018-04-23 17:35:36 +0800

    * adjust for mail pager.

--- module/mail/view/browse.html.php
+++ module/mail/view/browse.html.php
@@ -11,68 +11,70 @@
  */
 ?>
 <?php include $this->app->getModuleRoot() . 'message/view/header.html.php';?>
-<div id='titlebar'>
-  <div class='heading'><?php echo $lang->mail->browse?></div>
+<div id='mainContent' class='main-content'>
+<div class='main-header'>
+  <div class='heading'>
+    <strong><?php echo $lang->mail->browse?></strong>
+  </div>
 </div>
-<form method='post' action='<?php echo inlink('batchDelete')?>' target='hiddenwin' id='mailForm'>
-<div class='panel'>
-  <table class='table table-condensed table-bordered active-disabled table-fixed tablesorter table-selectable'>
-    <?php $vars = "orderBy=%s&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}"; ?>
-    <thead>
-      <tr>
-        <th class='w-id'>    <?php common::printOrderLink('id',          $orderBy, $vars, $lang->idAB);?></th>
-        <th class='w-80px'>  <?php common::printOrderLink('toList',      $orderBy, $vars, $lang->mail->toList);?></th>
-        <th class='w-150px'> <?php common::printOrderLink('subject',     $orderBy, $vars, $lang->mail->subject);?></th>
-        <th class='w-80px'>  <?php common::printOrderLink('createdBy',   $orderBy, $vars, $lang->mail->createdBy);?></th>
-        <th class='w-150px'> <?php common::printOrderLink('createdDate', $orderBy, $vars, $lang->mail->createdDate);?></th>
-        <th class='w-150px'> <?php common::printOrderLink('sendTime',    $orderBy, $vars, $lang->mail->sendTime);?></th>
-        <th class='w-60px'>  <?php common::printOrderLink('status',      $orderBy, $vars, $lang->mail->status);?></th>
-        <th>                 <?php echo $lang->mail->failReason;?></th>
-        <th class='w-80px'>  <?php echo $lang->actions;?></th>
-      </tr>
-    </thead>
-    <tbody class='text-center'>
-      <?php foreach($queueList as $queue):?>
-      <tr>
-        <td class='cell-id'>
-          <input type='checkbox' name='mailIDList[]'  value='<?php echo $queue->id;?>'/> 
-          <?php echo $queue->id?>
-        </td>
-        <td><?php echo zget($users, $queue->toList, $queue->toList)?></td>
-        <td class='text-left'><?php echo $queue->subject?></td>
-        <td><?php echo zget($users, $queue->createdBy)?></td>
-        <td><?php echo $queue->createdDate?></td>
-        <td><?php echo $queue->sendTime?></td>
-        <td><?php echo zget($lang->mail->statusList, $queue->status, '')?></td>
-        <td class='text-left'><?php echo $queue->failReason?></td>
-        <td class='text-left'>
-          <?php
-          if(common::hasPriv('mail', 'delete')) echo html::a(inlink('delete', "id=$queue->id"), $lang->delete, 'hiddenwin');
-          if(common::hasPriv('mail', 'resend') and $queue->status == 'fail') echo html::a(inlink('resend', "id=$queue->id"), $lang->mail->resend, 'hiddenwin');
-          ?>
-        </td>
-      </tr>
-      <?php endforeach;?>
-    </tbody>
-    <tfoot>
-      <tr>
-        <td colspan='9'>
-        <?php
-        if(count($queueList))
-        {
-            echo "<div class='table-actions'>";
-            echo html::selectButton();
-            if(common::hasPriv('mail', 'batchDelete')) echo html::submitButton($lang->delete);
-            echo '</div>';
-        }
-        $pager->show();
-        ?>
-        </td>
-      </tr>
-    </tfoot>
-  </table>
+  <form class='main-table' method='post' action='<?php echo inlink('batchDelete')?>' target='hiddenwin' id='mailForm' data-ride='table'>
+    <table class='table has-sort-head table-fixed'>
+      <?php $vars = "orderBy=%s&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}"; ?>
+      <thead>
+        <tr>
+          <th class='c-id'>
+            <div class="checkbox-primary check-all" title="<?php echo $lang->selectAll?>">
+              <label></label>
+            </div>
+            <?php common::printOrderLink('id', $orderBy, $vars, $lang->idAB);?>
+          </th>
+          <th class='w-80px'>  <?php common::printOrderLink('toList',      $orderBy, $vars, $lang->mail->toList);?></th>
+          <th class='w-150px'> <?php common::printOrderLink('subject',     $orderBy, $vars, $lang->mail->subject);?></th>
+          <th class='w-80px'>  <?php common::printOrderLink('createdBy',   $orderBy, $vars, $lang->mail->createdBy);?></th>
+          <th class='w-150px'> <?php common::printOrderLink('createdDate', $orderBy, $vars, $lang->mail->createdDate);?></th>
+          <th class='w-150px'> <?php common::printOrderLink('sendTime',    $orderBy, $vars, $lang->mail->sendTime);?></th>
+          <th class='w-80px'>  <?php common::printOrderLink('status',      $orderBy, $vars, $lang->mail->status);?></th>
+          <th>                 <?php echo $lang->mail->failReason;?></th>
+          <th class='w-80px'>  <?php echo $lang->actions;?></th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach($queueList as $queue):?>
+        <tr>
+          <td class='c-id'>
+            <div class="checkbox-primary">
+              <input type='checkbox' name='mailIDList[]'  value='<?php echo $queue->id;?>'/> 
+              <label></label>
+              <?php echo $queue->id?>
+            </div>
+          </td>
+          <td><?php echo zget($users, $queue->toList, $queue->toList)?></td>
+          <td class='text-left'><?php echo $queue->subject?></td>
+          <td><?php echo zget($users, $queue->createdBy)?></td>
+          <td><?php echo $queue->createdDate?></td>
+          <td><?php echo $queue->sendTime?></td>
+          <td><?php echo zget($lang->mail->statusList, $queue->status)?></td>
+          <td class='text-left'><?php echo $queue->failReason?></td>
+          <td class='c-actions'>
+            <?php
+            if(common::hasPriv('mail', 'delete')) echo html::a(inlink('delete', "id=$queue->id"), $lang->delete, 'hiddenwin', "class='btn btn-link'");
+            if(common::hasPriv('mail', 'resend') and $queue->status == 'fail') echo html::a(inlink('resend', "id=$queue->id"), $lang->mail->resend, 'hiddenwin', "class='btn btn-link'");
+            ?>
+          </td>
+        </tr>
+        <?php endforeach;?>
+      </tbody>
+    </table>
+    <?php if($queueList):?>
+    <div class='table-footer'>
+      <div class="checkbox-primary check-all"><label><?php echo $lang->selectAll?></label></div>
+      <div class="table-actions btn-toolbar">
+        <?php if(common::hasPriv('mail', 'batchDelete')) echo html::submitButton($lang->delete);?>
+      </div>
+      <?php $pager->show('right', 'pagerjs');?>
+    </div>
+    <?php endif;?>
+  </form>
 </div>
-</form>
-<script>$(function(){fixedTfootAction('#mailForm')})</script>
 <?php include '../../common/view/footer.html.php';?>
 
