41a1985f4dc825ffad9f1bff42b16f98420fca89
wangyidong <yidong@cnezsoft.com>
2018-04-23 15:04:13 +0800

    * adjust for extension page.

--- module/extension/view/erase.html.php
+++ module/extension/view/erase.html.php
@@ -11,22 +11,26 @@
  */
 ?>
 <?php include '../../common/view/header.lite.html.php';?>
-<div id='titlebar'>
-  <div class='heading'>
-    <span class='prefix' title='EXTENSION'><?php echo html::icon($lang->icons['extension']);?></span>
-    <strong><?php echo $title;?></strong>
-    <small class='text-danger'><?php echo $lang->extension->erase;?> <?php echo html::icon('trash');?></small>
-  </div>
-</div>
-<div class='alert alert-success with-icon'>
-  <i class='icon-ok-sign'></i>
-  <div class='content'>
-    <h3><?php echo $title;?></h3>
-    <?php if($removeCommands):?>
-    <p><strong><?php echo $lang->extension->unremovedFiles;?></strong></p>
-    <p><?php echo join($removeCommands, '<br />');?></p>
-    <?php endif;?>
-    <p class='text-center'><?php echo html::commonButton($lang->extension->viewAvailable, 'onclick=parent.location.href="' . inlink('browse', 'type=available') . '"');?></p>
+<div id='mainContent' class='main-content'>
+  <div class='center-block'>
+    <div class='main-header'>
+      <h2>
+        <span class='prefix' title='EXTENSION'><?php echo html::icon($lang->icons['extension']);?></span>
+        <strong><?php echo $title;?></strong>
+        <small class='text-danger'><?php echo $lang->extension->erase;?> <?php echo html::icon('trash');?></small>
+      </h2>
+    </div>
+    <div class='alert alert-success with-icon'>
+      <i class='icon-ok-sign'></i>
+      <div class='content'>
+        <h3><?php echo $title;?></h3>
+        <?php if($removeCommands):?>
+        <p><strong><?php echo $lang->extension->unremovedFiles;?></strong></p>
+        <p><?php echo join($removeCommands, '<br />');?></p>
+        <?php endif;?>
+        <p class='text-center'><?php echo html::commonButton($lang->extension->viewAvailable, 'onclick=parent.location.href="' . inlink('browse', 'type=available') . '"');?></p>
+      </div>
+    </div>
   </div>
 </div>
 </body>
