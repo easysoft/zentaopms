e993f33007951f1dda7fcc64c3a5d0f3c72544b8
wangyidong <yidong@cnezsoft.com>
2018-04-23 16:37:10 +0800

    * adjust for admin page.

--- module/admin/view/index.html.php
+++ module/admin/view/index.html.php
@@ -11,21 +11,25 @@
  */
 ?>
 <?php include '../../common/view/header.html.php';?>
-<?php if(!$bind and !$ignore and common::hasPriv('admin', 'register')):?>
-<div id="notice" class='alert alert-success'>
-  <?php echo html::a(inlink('ignore'), '<i class="icon-remove"></i> ' . $lang->admin->notice->ignore, 'hiddenwin', 'class="close" data-dismiss="alert" style="font-size: 12px"');?>
-  <div class="content"><i class='icon-info-sign'></i> <?php echo sprintf($lang->admin->notice->register, html::a(inlink('register'), $lang->admin->register->click, '', 'class="alert-link"'));?></div>
-</div>
-<?php endif;?>
-
-<div id='titlebar'>
-  <div class='heading'>
-    <?php 
-    echo sprintf($lang->admin->info->version, $config->version);
-    if($bind) echo sprintf($lang->admin->info->account, '<span class="red">' . $account . '</span>');
-    echo $lang->admin->info->links;
-    ?>
+<div id='mainContent' class='main-content'>
+  <?php if(!$bind and !$ignore and common::hasPriv('admin', 'register')):?>
+  <div id="notice" class='alert alert-success'>
+    <?php echo html::a(inlink('ignore'), '<i class="icon-remove"></i> ' . $lang->admin->notice->ignore, 'hiddenwin', 'class="close" data-dismiss="alert" style="font-size: 12px"');?>
+    <div class="content"><i class='icon-info-sign'></i> <?php echo sprintf($lang->admin->notice->register, html::a(inlink('register'), $lang->admin->register->click, '', 'class="alert-link"'));?></div>
   </div>
+  <?php endif;?>
+  
+  <div class='main-header'>
+    <div class='heading'>
+      <strong>
+        <?php 
+        printf($lang->admin->info->version, $config->version);
+        if($bind) echo sprintf($lang->admin->info->account, '<span class="red">' . $account . '</span>');
+        echo $lang->admin->info->links;
+        ?>
+      </strong>
+    </div>
+  </div>
+  <?php include '../../misc/view/links.html.php';?>
 </div>
-<?php include '../../misc/view/links.html.php';?>
 <?php include '../../common/view/footer.html.php';?>
--- module/admin/view/safe.html.php
+++ module/admin/view/safe.html.php
@@ -11,46 +11,48 @@
  */
 ?>
 <?php include '../../common/view/header.html.php';?>
-<div id='featurebar'>
-  <ul class='nav'>
-    <li class='active'><?php common::printLink('admin', 'safe', '', $lang->admin->safe->set);?></li>
-    <li><?php common::printLink('admin', 'checkWeak', '', $lang->admin->safe->checkWeak);?></li>
-  </ul>
+<div id='mainMenu' class='clearfix'>
+  <div class='btn-toolbar pull-left'>
+    <?php common::printLink('admin', 'safe', '', "<span class='text'>{$lang->admin->safe->set}</span>", '', "class='btn btn-link btn-active-text'");?>
+    <?php common::printLink('admin', 'checkWeak', '', "<span class='text'>{$lang->admin->safe->checkWeak}</span>", '', "class='btn btn-link'");?></li>
+  </div>
 </div>
-<div class='container mw-800px'>
-  <form method='post' target='hiddenwin'>
-    <table class='table table-form'>
-      <tr>
-        <th class='w-130px'><?php echo $lang->admin->safe->password?></th>
-        <td><?php echo html::radio('mode', $lang->admin->safe->modeList, isset($config->safe->mode) ? $config->safe->mode : 0, "onclick=showModeRule(this.value)")?></td>
-        <td><?php echo $lang->admin->safe->noticeMode?></td>
-      </tr>
-      <tr id='mode1Rule' class='hidden'>
-        <th></th>
-        <td colspan='2'><span style='color:#03b8cf;font-weight:bold;'><?php echo $lang->admin->safe->modeRuleList[1] . $lang->admin->safe->noticeStrong;?></span></td>
-      </tr>
-      <tr id='mode2Rule' class='hidden'>
-        <th></th>
-        <td colspan='2'><span style='color:#03b8cf;font-weight:bold;'><?php echo $lang->admin->safe->modeRuleList[2] . $lang->admin->safe->noticeStrong;?></span></td>
-      </tr>
-      <tr>
-        <th><?php echo $lang->admin->safe->weak?></th>
-        <td colspan='2'><?php echo html::textarea('weak', $config->safe->weak, "class='form-control' rows='4'")?></td>
-      </tr>
-      <tr>
-        <th><?php echo $lang->admin->safe->changeWeak?></th>
-        <td colspan='2'><?php echo html::radio('changeWeak', $lang->admin->safe->modifyPasswordList, isset($config->safe->changeWeak) ? $config->safe->changeWeak : 0)?></td>
-      </tr>
-      <tr>
-        <th><?php echo $lang->admin->safe->modifyPasswordFirstLogin?></th>
-        <td colspan='2'><?php echo html::radio('modifyPasswordFirstLogin', $lang->admin->safe->modifyPasswordList, isset($config->safe->modifyPasswordFirstLogin) ? $config->safe->modifyPasswordFirstLogin : 0)?></td>
-      </tr>
-      <tr>
-        <th></th>
-        <td colspan='2'><?php echo html::submitButton();?></td>
-      </tr>
-    </table>
-  </form>
+<div id='mainContent' class='main-content'>
+  <div class='center-block mw-800px'>
+    <form method='post' target='hiddenwin'>
+      <table class='table table-form'>
+        <tr>
+          <th style='width:130px'><?php echo $lang->admin->safe->password?></th>
+          <td class='w-200px'><?php echo html::radio('mode', $lang->admin->safe->modeList, isset($config->safe->mode) ? $config->safe->mode : 0, "onclick=showModeRule(this.value)")?></td>
+          <td><?php echo $lang->admin->safe->noticeMode?></td>
+        </tr>
+        <tr id='mode1Rule' class='hidden'>
+          <th></th>
+          <td colspan='2'><span style='color:#03b8cf;font-weight:bold;'><?php echo $lang->admin->safe->modeRuleList[1] . $lang->admin->safe->noticeStrong;?></span></td>
+        </tr>
+        <tr id='mode2Rule' class='hidden'>
+          <th></th>
+          <td colspan='2'><span style='color:#03b8cf;font-weight:bold;'><?php echo $lang->admin->safe->modeRuleList[2] . $lang->admin->safe->noticeStrong;?></span></td>
+        </tr>
+        <tr>
+          <th><?php echo $lang->admin->safe->weak?></th>
+          <td colspan='2'><?php echo html::textarea('weak', $config->safe->weak, "class='form-control' rows='4'")?></td>
+        </tr>
+        <tr>
+          <th><?php echo $lang->admin->safe->changeWeak?></th>
+          <td colspan='2'><?php echo html::radio('changeWeak', $lang->admin->safe->modifyPasswordList, isset($config->safe->changeWeak) ? $config->safe->changeWeak : 0)?></td>
+        </tr>
+        <tr>
+          <th><?php echo $lang->admin->safe->modifyPasswordFirstLogin?></th>
+          <td colspan='2'><?php echo html::radio('modifyPasswordFirstLogin', $lang->admin->safe->modifyPasswordList, isset($config->safe->modifyPasswordFirstLogin) ? $config->safe->modifyPasswordFirstLogin : 0)?></td>
+        </tr>
+        <tr>
+          <th></th>
+          <td colspan='2'><?php echo html::submitButton();?></td>
+        </tr>
+      </table>
+    </form>
+  </div>
 </div>
 <script>
 $(function()
