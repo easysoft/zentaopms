e6c4e06d2fe6d7badaf52d63a910cac1e4c4107a
wangyidong <yidong@cnezsoft.com>
2018-05-02 15:59:55 +0800

    * adjust for group module.

--- module/git/view/diff.html.php
+++ module/git/view/diff.html.php
@@ -12,6 +12,8 @@
 ?>
 <?php include '../../common/view/header.lite.html.php';?>
 <?php $catLink = $this->git->buildURL('cat', $path, $revision);?>
-<div class='box-title'><?php echo html::a($catLink, "$path@$revision");?></div>
-<div class='box-content'><xmp><?php echo $diff;?></xmp></div>
+<div class='detail'>
+  <div class='detail-title'><?php echo html::a($catLink, "$path@$revision");?></div>
+  <div class='detail-content'><xmp><?php echo $diff;?></xmp></div>
+</div>
 <?php include '../../common/view/footer.lite.html.php';?>
