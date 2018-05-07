433b28e7c11832799d5d318d383fc0f02a85e1f3
chenfeiCF <chenfei@qdphp.com>
2018-05-07 09:23:42 +0800

    * Adjust user todo and story view file.

--- module/user/view/featurebar.html.php
+++ module/user/view/featurebar.html.php
@@ -1,44 +1,46 @@
 <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
-<div id='featurebar'>
-  <ul class='nav'>
+<div id="mainMenu" class="clearfix">
+  <div class="btn-toolbar pull-left">
   <?php
     if(!isset($type))   $type   = 'today';
     if(!isset($period)) $period = 'today';
     $date = isset($date) ? $date : helper::today();
 
     echo "<li class='w-150px'>" . $userList . '</li>';
+
     if($config->global->flow == 'full')
     {
-        echo "<li id='todoTab'>"; common::printLink('user', 'todo',     "account=$account", $lang->user->todo); echo '</li>';
+        $label  = "<span class='text'>{$lang->user->todo}</span>";
+        echo html::a($this->createLink('user', 'todo', "account=$account"), $label);
     }
 
     if($config->global->flow != 'onlyTask' and $config->global->flow != 'onlyTest')
     {
-        echo "<li id='storyTab'>";   common::printLink('user', 'story',    "account=$account", $lang->user->story); echo '</li>';
+        echo html::a($this->createLink('user', 'story', "account=$account"), $lang->user->story);
     }
 
     if($config->global->flow == 'full' or $config->global->flow == 'onlyTask') 
     {
-        echo "<li id='taskTab'>";    common::printLink('user', 'task',     "account=$account", $lang->user->task);  echo '</li>';
+        echo html::a($this->createLink('user', 'task', "account=$account"), $lang->user->task);
     }
 
     if($config->global->flow == 'full' or $config->global->flow == 'onlyTest') 
     {
-        echo "<li id='bugTab'>" ;    common::printLink('user', 'bug',      "account=$account", $lang->user->bug);   echo '</li>';
-        echo "<li id='testTab'>";    common::printLink('user', 'testtask', "account=$account", $lang->user->test);  echo '</li>';
+        echo html::a($this->createLink('user', 'bug', "account=$account"), $lang->user->bug);
+        echo html::a($this->createLink('user', 'testtask', "account=$account"), $lang->user->test);
     }
-    echo "<li id='dynamicTab'>"; common::printLink('user', 'dynamic',  "type=today&account=$account", $lang->user->dynamic); echo '</li>' ;
+    echo html::a($this->createLink('user', 'dynamic',  "type=today&account=$account"), $lang->user->dynamic);
 
     if($config->global->flow == 'full' or $config->global->flow == 'onlyTask')
     {
-        echo "<li id='projectTab'>"; common::printLink('user', 'project',  "account=$account", $lang->user->project); echo '</li>';
+        echo html::a($this->createLink('user', 'project',  "account=$account"), $lang->user->project);
     }
-    echo "<li id='profileTab'>"; common::printLink('user', 'profile',  "account=$account", $lang->user->profile); echo '</li>';
+    echo html::a($this->createLink('user', 'profile',  "account=$account"), $lang->user->profile);
 
     $activedSpan = $this->app->getMethodName() . 'Tab';
     echo "<script>$('#$activedSpan').addClass('active')</script>";
     ?>
-  </ul>
+  </div>
 </div>
 <script>
 var type   = '<?php echo $type;?>';
