36b6e5863d79d3d3962a90cf7076ca98653460bd
滔哥 <admin@lvtao.net>
2018-05-08 23:27:12 +0800

    * Task#3942

--- module/project/model.php
+++ module/project/model.php
@@ -2761,8 +2761,9 @@ class projectModel extends model
                     $html .= '<a class="tree-toggle"><span class="label label-type">' . (empty($tree->parent) ? $this->lang->tree->module : $this->lang->tree->child) . '</span><span class="title">' . $tree->name . '</span></a>';
                     break;
                 case 'task':
+                    $link = helper::createLink('task', 'view', "taskID={$tree->id}");
                     $html .= '<li class="item-task">';
-                    $html .= '<a class="tree-toggle"><span class="label label-id">' . $tree->id . '</span><span class="label label-type">' . (empty($tree->parent) ? $this->lang->task->common : $this->lang->task->children) . '</span><span class="title">' . $tree->title . '</span> <span class="user"><i class="icon icon-person"></i> ' . (empty($tree->assignedTo) ? $tree->openedBy : $tree->assignedTo) . '</span></a>';
+                    $html .= '<a class="tree-link" href="' . $link . '"><span class="label label-id">' . $tree->id . '</span><span class="label label-type">' . (empty($tree->parent) ? $this->lang->task->common : $this->lang->task->children) . '</span><span class="title">' . $tree->title . '</span> <span class="user"><i class="icon icon-person"></i> ' . (empty($tree->assignedTo) ? $tree->openedBy : $tree->assignedTo) . '</span></a>';
                     break;
                 case 'product':
                     $this->app->loadLang('product');
@@ -2771,8 +2772,9 @@ class projectModel extends model
                     break;
                 case 'story':
                     $this->app->loadLang('story');
+                    $link = helper::createLink('story', 'view', "storyID={$tree->storyId}");
                     $html .= '<li class="item-story">';
-                    $html .= '<a class="tree-toggle"><span class="label label-id">' . $tree->storyId . '</span><span class="label label-type">' . $this->lang->story->common . '</span><span class="title">' . $tree->title . '</span> <span class="user"><i class="icon icon-person"></i> ' . (empty($tree->assignedTo) ? $tree->openedBy : $tree->assignedTo) . '</span></a>';
+                    $html .= '<a class="tree-link" href="' . $link . '"><span class="label label-id">' . $tree->storyId . '</span><span class="label label-type">' . $this->lang->story->common . '</span><span class="title">' . $tree->title . '</span> <span class="user"><i class="icon icon-person"></i> ' . (empty($tree->assignedTo) ? $tree->openedBy : $tree->assignedTo) . '</span></a>';
                     break;
             }
             if(isset($tree->children))
