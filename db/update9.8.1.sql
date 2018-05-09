bcb5137abea90e5f0e99005eed510ec6c8ffdd12
daitingting <daitingting@xirangit.com>
2018-05-09 14:40:50 +0800

    * Move update sql to 10.0.beta.

--- db/update9.8.1.sql
+++ db/update9.8.1.sql
@@ -1,9 +1,4 @@
 <<<<<<< HEAD
-ALTER TABLE `zt_release` ADD `marker` enum('0','1') NOT NULL default '0' AFTER `name`;
-ALTER TABLE `zt_doc` ADD `collector` text NOT NULL AFTER `views`;
-ALTER TABLE `zt_doc` ADD `visitedDate` datetime NOT NULL AFTER `editedDate`;
-ALTER TABLE `zt_doclib` ADD `collector` text NOT NULL AFTER `main`;
-ALTER TABLE `zt_module` ADD `collector` text NOT NULL AFTER `owner`;
 =======
 ALTER TABLE `zt_user` CHANGE `realname` `realname` varchar(100) COLLATE 'utf8_general_ci' NOT NULL DEFAULT '' AFTER `password`;
 ALTER TABLE `zt_project` CHANGE `openedDate` `openedDate` datetime NOT NULL,
