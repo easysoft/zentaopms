86cd21ccd2957e1d40a41d921060d2a57c1a1ba5
wangyidong <yidong@cnezsoft.com>
2018-05-09 09:17:31 +0800

    * adjust for zentaoux.

--- module/my/view/score.html.php
+++ module/my/view/score.html.php
@@ -20,35 +20,37 @@
     <?php common::printLink('score', 'rule', '', $lang->my->scoreRule, '', "class='btn btn-primary'");?>
   </div>
 </div>
-<div id="mainContent" class='main-content'>
-  <table class="main-table table table-fixed">
-    <thead>
-      <tr>
-        <th class="w-200px"><?php echo $lang->score->time; ?></th>
-        <th class="w-150px"><?php echo $lang->score->module; ?></th>
-        <th class="w-150px"><?php echo $lang->score->method; ?></th>
-        <th class="w-100px"><?php echo $lang->score->before; ?></th>
-        <th class="w-100px"><?php echo $lang->score->score; ?></th>
-        <th class="w-100px"><?php echo $lang->score->after; ?></th>
-        <th><?php echo $lang->score->desc; ?></th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach($scores as $score):?>
-      <tr>
-        <td><?php echo $score->time; ?></td>
-        <td><?php echo $lang->score->modules[$score->module]; ?></td>
-        <td><?php echo $lang->score->methods[$score->module][$score->method]; ?></td>
-        <td><?php echo $score->before; ?></td>
-        <td><?php echo $score->score; ?></td>
-        <td><?php echo $score->after; ?></td>
-        <td><?php echo $score->desc; ?></td>
-      </tr>
-      <?php endforeach;?>
-    </tbody>
-  </table>
-  <?php if($scores):?>
-  <div class="table-footer"><?php $pager->show('right', 'pagerjs');?></div>
-  <?php endif;?>
+<div id="mainContent">
+  <form class='main-table'>
+    <table class="table table-fixed">
+      <thead>
+        <tr>
+          <th class="w-200px"><?php echo $lang->score->time; ?></th>
+          <th class="w-150px"><?php echo $lang->score->module; ?></th>
+          <th class="w-150px"><?php echo $lang->score->method; ?></th>
+          <th class="w-100px"><?php echo $lang->score->before; ?></th>
+          <th class="w-100px"><?php echo $lang->score->score; ?></th>
+          <th class="w-100px"><?php echo $lang->score->after; ?></th>
+          <th><?php echo $lang->score->desc; ?></th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach($scores as $score):?>
+        <tr>
+          <td><?php echo $score->time; ?></td>
+          <td><?php echo $lang->score->modules[$score->module]; ?></td>
+          <td><?php echo $lang->score->methods[$score->module][$score->method]; ?></td>
+          <td><?php echo $score->before; ?></td>
+          <td><?php echo $score->score; ?></td>
+          <td><?php echo $score->after; ?></td>
+          <td><?php echo $score->desc; ?></td>
+        </tr>
+        <?php endforeach;?>
+      </tbody>
+    </table>
+    <?php if($scores):?>
+    <div class="table-footer"><?php $pager->show('right', 'pagerjs');?></div>
+    <?php endif;?>
+  </form>
 </div>
 <?php include '../../common/view/footer.html.php';?>
