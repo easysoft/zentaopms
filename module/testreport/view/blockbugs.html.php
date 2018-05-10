3a62ffb6226726b07a1b9ca2de5f25b2fd3c9ae0
wangyidong <yidong@cnezsoft.com>
2018-05-10 14:40:44 +0800

    * finish task #4044.

--- module/testreport/view/blockbugs.html.php
+++ module/testreport/view/blockbugs.html.php
@@ -7,7 +7,7 @@
       <th>                <?php echo $lang->testreport->bugTitle;?></th>
       <th class='w-user'> <?php echo $lang->openedByAB;?></th>
       <th class='w-user'><?php echo $lang->bug->resolvedBy;?></th>
-      <th class='w-130px'><?php echo $lang->bug->resolvedDate;?></th>
+      <th class='w-140px'><?php echo $lang->bug->resolvedDate;?></th>
       <th class='w-80px'> <?php echo $lang->statusAB;?></th>
     </tr>
   </thead>
