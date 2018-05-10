a3bd5c29706bf4340035e6249a614b688e82cdad
wangyidong <yidong@cnezsoft.com>
2018-05-10 14:44:18 +0800

    * adjust for table style.

--- module/my/view/project.html.php
+++ module/my/view/project.html.php
@@ -19,43 +19,41 @@
     <?php if(common::hasPriv('project', 'create')) echo html::a(helper::createLink('project', 'create'), "<i class='icon-plus'></i> " . $lang->my->home->createProject, '', "class='btn'") ?>
   </div>
 </div>
-<div id="mainContent">
-  <form class='main-table'>
-    <table class="table has-sort-head table-fixed" id='projectList'>
-      <thead>
-        <tr>
-          <th class='w-id'><?php echo $lang->idAB;?></th>
-          <th class='w-160px'><?php echo $lang->project->code;?></th>
-          <th class="text-left"><?php echo $lang->project->name;?></th>
-          <th class='w-date'><?php echo $lang->project->begin;?></th>
-          <th class='w-date'><?php echo $lang->project->end;?></th>
-          <th class='w-status'><?php echo $lang->statusAB;?></th>
-          <th class='w-user'><?php echo $lang->team->role;?></th>
-          <th class='w-date'><?php echo $lang->team->join;?></th>
-          <th class='w-110px'><?php echo $lang->team->hours;?></th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php foreach($projects as $project):?>
-        <?php $projectLink = $this->createLink('project', 'browse', "projectID=$project->id");?>
-        <tr>
-          <td><?php echo html::a($projectLink, $project->id);?></td>
-          <td class='text-left'><?php echo $project->code;?></td>
-          <td class='text-left'><?php echo html::a($projectLink, $project->name);?></td>
-          <td><?php echo $project->begin;?></td>
-          <td><?php echo $project->end;?></td>
-          <?php if(isset($project->delay)):?>
-          <td class='project-delay'><?php echo $lang->project->delayed;?></td>
-          <?php else:?>
-          <td class='project-<?php echo $project->status?>'><?php echo $lang->project->statusList[$project->status];?></td>
-          <?php endif;?>
-          <td><?php echo $project->role;?></td>
-          <td><?php echo $project->join;?></td>
-          <td><?php echo $project->hours;?></td>
-        </tr>
-        <?php endforeach;?>
-      </tbody>
-    </table>
-  </form>
+<div id="mainContent" class='main-table'>
+  <table class="table has-sort-head table-fixed" id='projectList'>
+    <thead>
+      <tr>
+        <th class='w-id'><?php echo $lang->idAB;?></th>
+        <th class='w-160px'><?php echo $lang->project->code;?></th>
+        <th class="text-left"><?php echo $lang->project->name;?></th>
+        <th class='w-date'><?php echo $lang->project->begin;?></th>
+        <th class='w-date'><?php echo $lang->project->end;?></th>
+        <th class='w-status'><?php echo $lang->statusAB;?></th>
+        <th class='w-user'><?php echo $lang->team->role;?></th>
+        <th class='w-date'><?php echo $lang->team->join;?></th>
+        <th class='w-110px'><?php echo $lang->team->hours;?></th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach($projects as $project):?>
+      <?php $projectLink = $this->createLink('project', 'browse', "projectID=$project->id");?>
+      <tr>
+        <td><?php echo html::a($projectLink, $project->id);?></td>
+        <td class='text-left'><?php echo $project->code;?></td>
+        <td class='text-left'><?php echo html::a($projectLink, $project->name);?></td>
+        <td><?php echo $project->begin;?></td>
+        <td><?php echo $project->end;?></td>
+        <?php if(isset($project->delay)):?>
+        <td class='project-delay'><?php echo $lang->project->delayed;?></td>
+        <?php else:?>
+        <td class='project-<?php echo $project->status?>'><?php echo $lang->project->statusList[$project->status];?></td>
+        <?php endif;?>
+        <td><?php echo $project->role;?></td>
+        <td><?php echo $project->join;?></td>
+        <td><?php echo $project->hours;?></td>
+      </tr>
+      <?php endforeach;?>
+    </tbody>
+  </table>
 </div>
 <?php include '../../common/view/footer.html.php';?>
--- module/my/view/testtask.html.php
+++ module/my/view/testtask.html.php
@@ -22,52 +22,50 @@
     ?>
   </div>
 </div>
-<div id="mainContent">
-  <form class='main-table'>
-    <table class="table has-sort-head table-fixed" id='taskList'>
-      <?php $vars = "type=$type&orderBy=%s&recTotal=$recTotal&recPerPage=$recPerPage&pageID=$pageID"; ?>
-      <thead>
-        <tr>
-          <th class='w-id'>   <?php common::printOrderLink('id',      $orderBy, $vars, $lang->idAB);?></th>
-          <th>                <?php common::printOrderLink('name',    $orderBy, $vars, $lang->testtask->name);?></th>
-          <th>                <?php common::printOrderLink('project', $orderBy, $vars, $lang->testtask->project);?></th>
-          <th>                <?php common::printOrderLink('build',   $orderBy, $vars, $lang->testtask->build);?></th>
-          <th class='w-90px'> <?php common::printOrderLink('begin',   $orderBy, $vars, $lang->testtask->begin);?></th>
-          <th class='w-90px'> <?php common::printOrderLink('end',     $orderBy, $vars, $lang->testtask->end);?></th>
-          <th class='w-80px'> <?php common::printOrderLink('status',  $orderBy, $vars, $lang->statusAB);?></th>
-          <th class='w-150px'><?php echo $lang->actions;?></th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php foreach($tasks as $task):?>
-        <tr>
-          <td class="c-id"><?php printf('%03d', $task->id);?></td>
-          <td class='text-left nobr'><?php echo html::a($this->createLink('testtask', 'view', "taskID=$task->id"), $task->name);?></td>
-          <td class='nobr'><?php echo $task->projectName?></td>
-          <td class='nobr'><?php $task->build == 'trunk' ? print($lang->trunk) : print(html::a($this->createLink('build', 'view', "buildID=$task->build"), $task->buildName));?></td>
-          <td><?php echo $task->begin?></td>
-          <td><?php echo $task->end?></td>
-          <td class='status-<?php echo $task->status?>'><?php echo $lang->testtask->statusList[$task->status];?></td>
-          <td class='c-actions'>
-            <?php
-            common::printIcon('testtask', 'cases',    "taskID=$task->id", 'play', 'list', 'sitemap');
-            common::printIcon('testtask', 'linkCase', "taskID=$task->id", '', 'list', 'link');
-            common::printIcon('testtask', 'edit',     "taskID=$task->id", '', 'list');
+<div id="mainContent" class='main-table'>
+  <table class="table has-sort-head table-fixed" id='taskList'>
+    <?php $vars = "type=$type&orderBy=%s&recTotal=$recTotal&recPerPage=$recPerPage&pageID=$pageID"; ?>
+    <thead>
+      <tr>
+        <th class='w-id'>   <?php common::printOrderLink('id',      $orderBy, $vars, $lang->idAB);?></th>
+        <th>                <?php common::printOrderLink('name',    $orderBy, $vars, $lang->testtask->name);?></th>
+        <th>                <?php common::printOrderLink('project', $orderBy, $vars, $lang->testtask->project);?></th>
+        <th>                <?php common::printOrderLink('build',   $orderBy, $vars, $lang->testtask->build);?></th>
+        <th class='w-90px'> <?php common::printOrderLink('begin',   $orderBy, $vars, $lang->testtask->begin);?></th>
+        <th class='w-90px'> <?php common::printOrderLink('end',     $orderBy, $vars, $lang->testtask->end);?></th>
+        <th class='w-80px'> <?php common::printOrderLink('status',  $orderBy, $vars, $lang->statusAB);?></th>
+        <th class='w-150px'><?php echo $lang->actions;?></th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach($tasks as $task):?>
+      <tr>
+        <td class="c-id"><?php printf('%03d', $task->id);?></td>
+        <td class='text-left nobr'><?php echo html::a($this->createLink('testtask', 'view', "taskID=$task->id"), $task->name);?></td>
+        <td class='nobr'><?php echo $task->projectName?></td>
+        <td class='nobr'><?php $task->build == 'trunk' ? print($lang->trunk) : print(html::a($this->createLink('build', 'view', "buildID=$task->build"), $task->buildName));?></td>
+        <td><?php echo $task->begin?></td>
+        <td><?php echo $task->end?></td>
+        <td class='status-<?php echo $task->status?>'><?php echo $lang->testtask->statusList[$task->status];?></td>
+        <td class='c-actions'>
+          <?php
+          common::printIcon('testtask', 'cases',    "taskID=$task->id", 'play', 'list', 'sitemap');
+          common::printIcon('testtask', 'linkCase', "taskID=$task->id", '', 'list', 'link');
+          common::printIcon('testtask', 'edit',     "taskID=$task->id", '', 'list');
 
-            if(common::hasPriv('testtask', 'delete'))
-            {
-                $deleteURL = $this->createLink('testtask', 'delete', "taskID=$task->id&confirm=yes");
-                echo html::a("javascript:ajaxDelete(\"$deleteURL\",\"taskList\",confirmDelete)", '<i class="icon-trash"></i>', '', "title='{$lang->testtask->delete}' class='btn'");
-            }
-            ?>
-          </td>
-        </tr>
-        <?php endforeach;?>
-      </tbody>
-    </table>
-    <?php if($tasks):?>
-    <div class="table-footer"><?php $pager->show('right', 'pagerjs');?></div>
-    <?php endif;?>
-  </form>
+          if(common::hasPriv('testtask', 'delete'))
+          {
+              $deleteURL = $this->createLink('testtask', 'delete', "taskID=$task->id&confirm=yes");
+              echo html::a("javascript:ajaxDelete(\"$deleteURL\",\"taskList\",confirmDelete)", '<i class="icon-trash"></i>', '', "title='{$lang->testtask->delete}' class='btn'");
+          }
+          ?>
+        </td>
+      </tr>
+      <?php endforeach;?>
+    </tbody>
+  </table>
+  <?php if($tasks):?>
+  <div class="table-footer"><?php $pager->show('right', 'pagerjs');?></div>
+  <?php endif;?>
 </div>
 <?php include '../../common/view/footer.html.php';?>
