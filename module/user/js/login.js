6986b5721fec26de5c1be6bae13b939446a7d0b4
Catouse <catouse@gmail.com>
2018-05-17 10:53:40 +0800

    * add fade effection to user/login view.

--- module/user/js/login.js
+++ module/user/js/login.js
@@ -25,4 +25,4 @@ $(document).ready(function()
         var rand = $('input#verifyRand').val();
         if(password.length != 32 && typeof(md5) == 'function') $('input:password').val(md5(md5(password) + rand));
     });
-})
+});
