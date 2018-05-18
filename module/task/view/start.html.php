9eda304837e48653ea7103f140fd9f22a61ed3a1
daitingting <daitingting@xirangit.com>
2018-05-18 13:46:36 +0800

    * Adjust linkButton.

--- module/task/view/start.html.php
+++ module/task/view/start.html.php
@@ -68,7 +68,7 @@
         <tr>
           <td colspan='3' class='text-center'>
             <?php echo html::submitButton($lang->task->start, '', 'btn btn-wide btn-primary');?>
-            <?php echo html::linkButton($lang->goback, $this->session->taskList, '', '', 'btn btn-wide');?>
+            <?php echo html::linkButton($lang->goback, $this->session->taskList, 'self', '', 'btn btn-wide');?>
           </td>
         </tr>
       </table>
