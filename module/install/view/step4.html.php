2b0c7c4ef33f6230c6837b2e841a864e1ba5c551
wangyidong <yidong@cnezsoft.com>
2018-05-18 15:04:53 +0800

    * adjust for style.

--- module/install/view/step4.html.php
+++ module/install/view/step4.html.php
@@ -43,7 +43,7 @@
       <strong><?php echo $lang->install->getPriv;?></strong>
     </div>
     <div class='modal-body'>
-      <form class='form-condensed' method='post' target='hiddenwin'>
+      <form method='post' target='hiddenwin'>
         <table class='table table-form mw-400px' style='margin: 0 auto'>
           <tr>
             <th class='w-100px'><?php echo $lang->install->company;?></th>
