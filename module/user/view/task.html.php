dbabcd9c92214b9896645a61700ea83b87653099
wangyidong <yidong@cnezsoft.com>
2018-05-23 14:05:56 +0800

    * finish task #4230.

--- module/user/view/task.html.php
+++ module/user/view/task.html.php
@@ -36,7 +36,7 @@
   </nav>
 
   <div class='main-table'>
-    <table class='table has-sort-head table-lg' id='tasktable'>
+    <table class='table has-sort-head' id='tasktable'>
       <thead>
         <tr class='colhead'>
           <th class='w-id'><?php echo $lang->idAB;?></th>
