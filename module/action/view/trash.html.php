ae4c8f518b45751624fbeb811ee674ddbe3b9d0b
wangyidong <yidong@cnezsoft.com>
2018-05-23 16:40:00 +0800

    * finish task #4215.

--- module/action/view/trash.html.php
+++ module/action/view/trash.html.php
@@ -21,8 +21,8 @@
   </div>
 </div>
 
-<div id='mainContent' class='main-content'>
-  <div class='main-table'>
+<div id='mainContent'>
+  <div class='main-table' data-ride='table'>
     <table class='table has-sort-head'>
       <?php $vars = "type=$type&orderBy=%s&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}"; ?>
       <thead>
@@ -38,7 +38,7 @@
       <tbody>
         <?php foreach($trashes as $action):?>
         <?php $module = $action->objectType == 'case' ? 'testcase' : $action->objectType;?>
-        <tr class='text-center'>
+        <tr>
           <td><?php echo zget($lang->action->objectTypes, $action->objectType, '');?></td>
           <td><?php echo $action->objectID;?></td>
           <td class='text-left'>
