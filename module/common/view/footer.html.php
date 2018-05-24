4c38efb845f85dc8f96877e585fc98b384c6776d
Catouse <catouse@gmail.com>
2018-05-24 17:05:57 +0800

    * improve UI of links on footer.

--- module/common/view/footer.html.php
+++ module/common/view/footer.html.php
@@ -1,9 +1,9 @@
-  </div><?php /* end '.outer' in 'header.html.php'. */ ?>
-  <script>setTreeBox()</script>
-  <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
+</div><?php /* end '.outer' in 'header.html.php'. */ ?>
+<script>setTreeBox()</script>
+<?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
 
-  <div id='divider'></div>
-  <iframe frameborder='0' name='hiddenwin' id='hiddenwin' scrolling='no' class='debugwin hidden'></iframe>
+<div id='divider'></div>
+<iframe frameborder='0' name='hiddenwin' id='hiddenwin' scrolling='no' class='debugwin hidden'></iframe>
 
 <?php if($onlybody != 'yes'):?>
 </main><?php /* end '#wrap' in 'header.html.php'. */ ?>
@@ -11,7 +11,7 @@
   <div class="container">
     <?php commonModel::printBreadMenu($this->moduleName, isset($position) ? $position : ''); ?>
     <div id='poweredBy'>
-      <a href='<?php echo $lang->website;?>' target='_blank' class='text-primary'><i class='icon-zentao'></i> <?php echo $lang->zentaoPMS . $config->version;?></a> &nbsp;
+      <a href='<?php echo $lang->website;?>' target='_blank'><i class='icon-zentao'></i> <?php echo $lang->zentaoPMS . $config->version;?></a> &nbsp;
       <?php echo $lang->proVersion;?>
       <?php commonModel::printNotifyLink();?>
     </div>
@@ -38,7 +38,7 @@ $(function()
     {
         $.get(createLink('message', 'ajaxGetMessage', "windowBlur=" + (windowBlur ? '1' : '0')), function(data)
         {
-           if(!windowBlur)
+            if(!windowBlur)
             {
                 $('#noticeBox').append(data);
                 adjustNoticePosition();
