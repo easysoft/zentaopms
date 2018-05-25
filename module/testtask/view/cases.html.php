3e31e43c049d7dc367f20e961c858265b9828375
滔哥 <admin@lvtao.net>
2018-05-25 16:08:56 +0800

    * Finish task#4262.

--- module/testtask/view/cases.html.php
+++ module/testtask/view/cases.html.php
@@ -129,6 +129,10 @@
         <?php endif;?>
         <?php $pager->show('right', 'pagerjs');?>
       </div>
+      <?php else:?>
+      <div class="table-empty-tip">
+        <p><span class="text-muted"><?php echo $lang->testcase->noCase;?></span> <?php common::printLink('testtask', 'linkCase', "taskID={$taskID}", "<i class='icon icon-plus'></i> " . $lang->testtask->linkCase, '', "class='btn btn-info'");?></p>
+      </div>
       <?php endif;?>
     </form>
   </div>
