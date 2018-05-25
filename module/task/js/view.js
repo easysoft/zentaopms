52bcaad54715faac787d0d52e2deccf26f00a8ba
滔哥 <admin@lvtao.net>
2018-05-25 14:07:59 +0800

    * Test task#4278

--- module/task/js/view.js
+++ module/task/js/view.js
@@ -4,3 +4,12 @@ function assign(taskID, assignedTo)
   $('.assign').height(40);
   $('.assign').load(createLink('user', 'ajaxGetUser', 'taskID=' + taskID + '&assignedTo=' + assignedTo));
 }
+
+$(document).ready(function()
+{
+  var num = $("#mainActions .btn-toolbar a").size();
+  if(num == 0)
+  {
+    $("#mainActions .btn-toolbar").hide();
+  }
+});
