65c761bd2e5964c9d48df51e3219611b3ab7c10b
Catouse <catouse@gmail.com>
2018-05-29 09:44:16 +0800

    * refactor codes.

--- module/bug/js/view.js
+++
@@ -1,4 +0,0 @@
-function setModal4List(colorboxClass, replaceID)
-{
-    if(config.onlybody != 'yes') $('.iframe').modalTrigger({width:900, type:'iframe', afterHide:function(){location.href=location.href;}})
-}
