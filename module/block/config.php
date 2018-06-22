01683e7ec4d762c7d270b33f7f34d8a38b19a781
liugang <liugang@cnezsoft.com>
2018-06-22 11:15:08 +0800

    * Finish task #4477

--- module/block/config.php
+++ module/block/config.php
@@ -13,5 +13,16 @@ $config->block = new stdclass();
 $config->block->editor = new stdclass();
 $config->block->editor->set = array('id' => 'html', 'tools' => 'simple');
 
+$config->block->longBlock = array();
+$config->block->longBlock['']['flowchart']        = 'flowchart';
+$config->block->longBlock['']['welcome']          = 'welcome';
+$config->block->longBlock['product']['statistic'] = 'statistic';
+$config->block->longBlock['project']['statistic'] = 'statistic';
+$config->block->longBlock['qa']['statistic']      = 'statistic';
+
+$config->block->shortBlock = array();
+$config->block->shortBlock['product']['overview'] = 'overview';
+$config->block->shortBlock['project']['overview'] = 'overview';
+
 $config->statistic = new stdclass();
 $config->statistic->storyStages = array('wait', 'planned', 'developing', 'testing', 'released');
