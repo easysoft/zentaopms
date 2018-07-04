babcaa6d7f26e2797d31df81970702825d636f30
Your Name <you@example.com>
2018-07-04 14:13:14 +0800

    * finish task #4516.

--- module/project/control.php
+++ module/project/control.php
@@ -2301,7 +2301,7 @@ class project extends control
     public function importPlanStories($projectID, $planID)
     {
         $planStories  = $planProducts = array();
-        $planStory    = $this->loadModel('story')->getPlanStories($planID);
+        $planStory    = $this->loadModel('story')->getPlanStories($planID, 'changed,active,reviewing');
         if(!empty($planStory))
         {
             $planStories = array_keys($planStory);
