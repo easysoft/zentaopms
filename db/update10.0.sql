51ce3062dd20ccbc428a2f855305be93e5b54ea7
daitingting <daitingting@xirangit.com>
2018-07-12 10:57:06 +0800

    * Delete done status of project.  * Update data.

--- db/update10.0.sql
+++ db/update10.0.sql
@@ -1,5 +1,7 @@
 ALTER TABLE `zt_storyspec` CHANGE `title` `title` varchar(255) NOT NULL;
 update `zt_todo` set assignedTo = 'closed', assignedDate = closedDate where status = 'closed';
+update `zt_project` set status = 'closed' where status = 'done';
+
 DROP TABLE IF EXISTS `zt_webhookdatas`;
 
 ALTER TABLE `zt_webhook` ADD `domain` varchar(255) NOT NULL AFTER `url`;
