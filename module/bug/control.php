841de3126608382aeb7ce2bbd88f975acbc56d0a
滔哥 <admin@lvtao.net>
2018-09-11 17:08:03 +0800

    * Fix bug for php7.2

--- module/bug/control.php
+++ module/bug/control.php
@@ -328,9 +328,10 @@ class bug extends control
         $pri        = '';
         $color      = '';
 
-        /* Parse the extras. */
+        /* Parse the extras. extract fix php7.2*/
         $extras = str_replace(array(',', ' '), array('&', ''), $extras);
-        parse_str($extras);
+        parse_str($extras, $output);
+        extract($output);
 
         if($runID and $resultID) extract($this->bug->getBugInfoFromResult($resultID, 0, 0, isset($stepIdList) ? $stepIdList : ''));// If set runID and resultID, get the result info by resultID as template.
         if(!$runID and $caseID)  extract($this->bug->getBugInfoFromResult($resultID, $caseID, $version, isset($stepIdList) ? $stepIdList : ''));// If not set runID but set caseID, get the result info by resultID and case info.
