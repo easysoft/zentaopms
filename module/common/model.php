ef8317accc17b769d690033838ad7d14a57fd5d1
Catouse <catouse@gmail.com>
2018-09-13 16:29:06 +0800

    * finish task #4741.

--- module/common/model.php
+++ module/common/model.php
@@ -777,7 +777,7 @@ class commonModel extends model
         <h4 class="modal-title">{$lang->action->create}</h4>
       </div>
       <div class="modal-body">
-    <form action="$commentFormLink" target='hiddenwin' method='post'>
+    <form class="load-indicator" action="$commentFormLink" target='hiddenwin' method='post'>
           <div class="form-group">
             <textarea id='comment' name='comment' class="form-control" rows="8" autofocus="autofocus"></textarea>
           </div>
