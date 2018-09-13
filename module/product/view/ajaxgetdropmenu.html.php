27a71dbb14a09d80646d7a0eed439068f22270ff
Catouse <catouse@gmail.com>
2018-09-13 19:03:29 +0800

    * finish task #4758.

--- module/product/view/ajaxgetdropmenu.html.php
+++ module/product/view/ajaxgetdropmenu.html.php
@@ -1,66 +1,80 @@
-<div class="list-group">
 <?php js::set('productID', $productID);?>
 <?php js::set('module', $module);?>
 <?php js::set('method', $method);?>
 <?php js::set('extra', $extra);?>
-  <?php
-  $iCharges = 0;
-  $others   = 0;
-  $closeds  = 0;
-  $productNames = array();
-  foreach($products as $product)
-  {
-      if($product->status == 'normal' and $product->PO == $this->app->user->account) $iCharges++;
-      if($product->status == 'normal' and !($product->PO == $this->app->user->account)) $others++;
-      if($product->status == 'closed') $closeds++;
-      $productNames[] = $product->name;
-  }
-  $productsPinYin = common::convert2Pinyin($productNames);
- 
-  foreach($products as $product)
-  {
-      if($product->status == 'normal' and $product->PO == $this->app->user->account) 
-      {
-          if($product->type != 'platform' && $module == 'branch' && $method == 'manage')
-          {
-              echo html::a(sprintf($link, $productID), "<i class='icon-cube'></i> " . $product->name, '', "title='{$product->name}' data-key='" . zget($productsPinYin, $product->name, '') . "'");
-          }
-          else
-          {
-              echo html::a(sprintf($link, $product->id), "<i class='icon-cube'></i> " . $product->name, '', "title='{$product->name}' data-key='" . zget($productsPinYin, $product->name, '') . "'");
-          }
-      }
-  }
- 
-  foreach($products as $product)
-  {
-      if($product->status == 'normal' and !($product->PO == $this->app->user->account)) 
-      {
-          if($product->type != 'platform' && $module == 'branch' && $method == 'manage')
-          {
-              echo html::a(sprintf($link, $productID), "<i class='icon-cube'></i> " . $product->name, '', "title='{$product->name}' data-key='" . zget($productsPinYin, $product->name, '') . "'");
-          }
-          else
-          {
-              echo html::a(sprintf($link, $product->id), "<i class='icon-cube'></i> " . $product->name, '', "title='{$product->name}' data-key='" . zget($productsPinYin, $product->name, '') . "'");
-          }
-      }
-  }
+<?php
+$iCharges = 0;
+$others   = 0;
+$closeds  = 0;
+$productNames = array();
+foreach($products as $product)
+{
+    if($product->status == 'normal' and $product->PO == $this->app->user->account) $iCharges++;
+    if($product->status == 'normal' and !($product->PO == $this->app->user->account)) $others++;
+    if($product->status == 'closed') $closeds++;
+    $productNames[] = $product->name;
+}
+$productsPinYin = common::convert2Pinyin($productNames);
+$myProductsHtml     = '';
+$normalProductsHtml = '';
+$closedProductsHtml = '';
 
-  foreach($products as $product)
-  {
-      if($product->status == 'closed')
-      {
+foreach($products as $product)
+{
+    if($product->status == 'normal' and $product->PO == $this->app->user->account)
+    {
+        if($product->type != 'platform' && $module == 'branch' && $method == 'manage')
+        {
+            $myProductsHtml .= html::a(sprintf($link, $productID), "<i class='icon-cube'></i> " . $product->name, '', "class='text-important' title='{$product->name}' data-key='" . zget($productsPinYin, $product->name, '') . "'");
+        }
+        else
+        {
+            $myProductsHtml .= html::a(sprintf($link, $product->id), "<i class='icon-cube'></i> " . $product->name, '', "title='{$product->name}' data-key='" . zget($productsPinYin, $product->name, '') . "'");
+        }
+    }
+    else if($product->status == 'normal' and !($product->PO == $this->app->user->account))
+    {
+        if($product->type != 'platform' && $module == 'branch' && $method == 'manage')
+        {
+            $normalProductsHtml .= html::a(sprintf($link, $productID), "<i class='icon-cube'></i> " . $product->name, '', "title='{$product->name}' data-key='" . zget($productsPinYin, $product->name, '') . "'");
+        }
+        else
+        {
+            $normalProductsHtml .= html::a(sprintf($link, $product->id), "<i class='icon-cube'></i> " . $product->name, '', "title='{$product->name}' data-key='" . zget($productsPinYin, $product->name, '') . "'");
+        }
+    }
+    else if($product->status == 'closed')
+    {
 
-          if($product->type != 'platform' && $module == 'branch' && $method == 'manage')
-          {
-              echo html::a(sprintf($link, $productID), "<i class='icon-cube'></i> " . $product->name, '', "title='{$product->name}' class='closed' data-key='" . zget($productsPinYin, $product->name, '') . "'");
-          }
-          else
-          {
-              echo html::a(sprintf($link, $product->id), "<i class='icon-cube'></i> " . $product->name, '', "title='{$product->name}' class='closed' data-key='" . zget($productsPinYin, $product->name, '') . "'");
-          }
-      }
-  }
-  ?>
+        if($product->type != 'platform' && $module == 'branch' && $method == 'manage')
+        {
+            $closedProductsHtml .= html::a(sprintf($link, $productID), "<i class='icon-cube'></i> " . $product->name, '', "title='{$product->name}' class='closed' data-key='" . zget($productsPinYin, $product->name, '') . "'");
+        }
+        else
+        {
+            $closedProductsHtml .= html::a(sprintf($link, $product->id), "<i class='icon-cube'></i> " . $product->name, '', "title='{$product->name}' class='closed' data-key='" . zget($productsPinYin, $product->name, '') . "'");
+        }
+    }
+}
+?>
+<div class="table-row">
+  <div class="table-col col-left">
+    <div class='list-group'>
+    <?php
+    echo $myProductsHtml;
+    echo $normalProductsHtml;
+    ?>
+    </div>
+    <div class="col-footer">
+      <?php echo html::a(helper::createLink('product', 'all'), '<i class="icon icon-cards-view muted"></i> ' . $lang->product->all); ?>
+      <a class='pull-right toggle-right-col'><?php echo $lang->product->closed?><i class='icon icon-angle-right'></i></a>
+    </div>
+  </div>
+  <div class="table-col col-right">
+   <div class='list-group'>
+    <?php
+    echo $closedProductsHtml;
+    ?>
+    </div>
+  </div>
 </div>
--- module/project/view/ajaxgetdropmenu.html.php
+++ module/project/view/ajaxgetdropmenu.html.php
@@ -2,40 +2,54 @@
 <?php js::set('module', $module);?>
 <?php js::set('method', $method);?>
 <?php js::set('extra', $extra);?>
-<div class="list-group">
-  <?php
-  $iCharges = 0;
-  $others   = 0;
-  $dones    = 0;
-  $projectNames = array();
-  foreach($projects as $project)
-  {
-      if($project->status != 'done' and $project->status != 'closed' and $project->PM == $this->app->user->account) $iCharges++;
-      if($project->status != 'done' and $project->status != 'closed' and !($project->PM == $this->app->user->account)) $others++;
-      if($project->status == 'done' or $project->status == 'closed') $dones++;
-      $projectNames[] = $project->name;
-  }
-  $projectsPinYin = common::convert2Pinyin($projectNames);
- 
-  foreach($projects as $project)
-  {
-      if($project->status != 'done' and $project->status != 'closed' and $project->PM == $this->app->user->account)
-      {
-          echo html::a(sprintf($link, $project->id), "<i class='icon-folder-close-alt'></i> " . $project->name, '', "title='{$project->name}' data-key='" . zget($projectsPinYin, $project->name, '') . "'");
-      }
-  }
- 
-  foreach($projects as $project)
-  {
-      if($project->status != 'done' and $project->status != 'closed' and !($project->PM == $this->app->user->account))
-      {
-          echo html::a(sprintf($link, $project->id), "<i class='icon-folder-close-alt'></i> " . $project->name, '', "title='{$project->name}' data-key='" . zget($projectsPinYin, $project->name, '') . "'");
-      }
-  }
+<?php
+$iCharges = 0;
+$others   = 0;
+$dones    = 0;
+$projectNames = array();
+$myProjectsHtml     = '';
+$normalProjectsHtml = '';
+$closedProjectsHtml = '';
+foreach($projects as $project)
+{
+    if($project->status != 'done' and $project->status != 'closed' and $project->PM == $this->app->user->account) $iCharges++;
+    if($project->status != 'done' and $project->status != 'closed' and !($project->PM == $this->app->user->account)) $others++;
+    if($project->status == 'done' or $project->status == 'closed') $dones++;
+    $projectNames[] = $project->name;
+}
+$projectsPinYin = common::convert2Pinyin($projectNames);
 
-  foreach($projects as $project)
-  {
-      if($project->status == 'done' or $project->status == 'closed') echo html::a(sprintf($link, $project->id), "<i class='icon-folder-close-alt'></i> " . $project->name, '', "title='{$project->name}' data-key='" . zget($projectsPinYin, $project->name, '') . "'");
-  }
-  ?>
+foreach($projects as $project)
+{
+    if($project->status != 'done' and $project->status != 'closed' and $project->PM == $this->app->user->account)
+    {
+        $myProjectsHtml .= html::a(sprintf($link, $project->id), "<i class='icon-folder-close-alt'></i> " . $project->name, '', "class='text-important' title='{$project->name}' data-key='" . zget($projectsPinYin, $project->name, '') . "'");
+    }
+    else if($project->status != 'done' and $project->status != 'closed' and !($project->PM == $this->app->user->account))
+    {
+        $normalProjectsHtml .= html::a(sprintf($link, $project->id), "<i class='icon-folder-close-alt'></i> " . $project->name, '', "title='{$project->name}' data-key='" . zget($projectsPinYin, $project->name, '') . "'");
+    }
+    else if($project->status == 'done' or $project->status == 'closed') $closedProjectsHtml .= html::a(sprintf($link, $project->id), "<i class='icon-folder-close-alt'></i> " . $project->name, '', "title='{$project->name}' data-key='" . zget($projectsPinYin, $project->name, '') . "'");
+}
+?>
+<div class="table-row">
+  <div class="table-col col-left">
+    <div class='list-group'>
+    <?php
+    echo $myProjectsHtml;
+    echo $normalProjectsHtml;
+    ?>
+    </div>
+    <div class="col-footer">
+      <?php echo html::a(helper::createLink('project', 'all'), '<i class="icon icon-cards-view muted"></i> ' . $lang->project->all); ?>
+      <a class='pull-right toggle-right-col'><?php echo $lang->project->doneProjects?><i class='icon icon-angle-right'></i></a>
+    </div>
+  </div>
+  <div class="table-col col-right">
+   <div class='list-group'>
+    <?php
+    echo $closedProjectsHtml;
+    ?>
+    </div>
+  </div>
 </div>
