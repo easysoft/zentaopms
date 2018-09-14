64b34ba5c6a7c109856da1ea2ee61ee1e89ce3c2
滔哥 <admin@lvtao.net>
2018-09-14 14:26:54 +0800

    * Finish task#4806.

--- module/block/control.php
+++ module/block/control.php
@@ -1016,7 +1016,11 @@ class block extends control
         $today     = date(DT_DATE1);
         $yesterday = date(DT_DATE1, strtotime('yesterday'));
 
-        $bugs = $this->loadModel('bug')->getByList($bugIDList);
+        $this->app->loadLang('bug');
+        $bugs = $this->dao->select('id, assignedTo, resolvedDate, closedDate, status')->from(TABLE_BUG)
+                     ->where('deleted')->eq(0)
+                     ->andWhere('id')->in($bugIDList)
+                     ->fetchAll('id');
         $confirmedBugs = $this->dao->select('objectID')->from(TABLE_ACTION)
             ->where('objectType')->eq('bug')
             ->andWhere('action')->eq('bugconfirmed')
