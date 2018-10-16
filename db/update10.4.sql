6d90d291cfe95fd7ce27c16ccb76bdda484b0224
chenfeiCF <chenfei@qdphp.com>
2018-10-16 13:25:21 +0800

    * Add type field for doc lib.

--- db/update10.4.sql
+++ db/update10.4.sql
@@ -4,3 +4,8 @@ CHANGE `projects` `projects` mediumtext COLLATE 'utf8_general_ci' NOT NULL AFTER
 ALTER TABLE `zt_task` ADD `finishedList` text NOT NULL AFTER `finishedDate`;
 update zt_config set `value` = 'files,customFiles' where `module` = 'doc' and `section` = 'custom' and `key` = 'objectLibs';
 ALTER TABLE `zt_file` CHANGE `title` `title` varchar(255) COLLATE 'utf8_general_ci' NOT NULL AFTER `pathname`;
+
+ALTER TABLE `zt_doclib` ADD `type` varchar(30) COLLATE 'utf8_general_ci' NOT NULL AFTER `id`, COMMENT='';
+UPDATE `zt_doclib` SET `type` = 'product' WHERE `product` > 0;
+UPDATE `zt_doclib` SET `type` = 'project' WHERE `project` > 0;
+UPDATE `zt_doclib` SET `type` = 'custom'  WHERE `product` = 0 AND `project` = 0;
