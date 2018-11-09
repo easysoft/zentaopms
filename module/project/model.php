5cc4c04930a6246763b1a6d89b05eb55e442ccb9
Yagami <sgm0422@qq.com>
2018-11-09 16:17:05 +0800

    * Fix task search option.

--- module/project/model.php
+++ module/project/model.php
@@ -2367,7 +2367,7 @@ class projectModel extends model
         $this->config->project->search['actionURL'] = $actionURL;
         $this->config->project->search['queryID']   = $queryID;
         $this->config->project->search['params']['project']['values'] = array(''=>'', $projectID => $projects[$projectID], 'all' => $this->lang->project->allProject);
-        $this->config->project->search['params']['module']['values']  = $this->loadModel('tree')->getTaskOptionMenu($projectID, $startModuleID = 0);
+        $this->config->project->search['params']['module']['values']  = $this->loadModel('tree')->getOptionMenu($projectID, 'task');
 
         $this->loadModel('search')->setSearchParams($this->config->project->search);
     }
