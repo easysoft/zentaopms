79b442d71a3071e506e05139efdc0b8e2414d41a
wangyidong <yidong@cnezsoft.com>
2018-12-12 17:55:24 +0800

    * adjust for task #5152.

--- module/todo/view/footer.html.php
+++ module/todo/view/footer.html.php
@@ -1,6 +1,2 @@
 <?php js::set('account', $app->user->account)?>
-<?php if($this->app->viewType == 'xhtml'):?>
-<?php include '../../common/view/footer.lite.html.php';?>
-<?php else:?>
 <?php include '../../common/view/footer.html.php';?>
-<?php endif;?>
