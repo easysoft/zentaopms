da8ab3422b33f0b439ef0747e10bbe7fe8ec2916
Yagami <sgm0422@qq.com>
2018-12-18 14:17:09 +0800

    * Add common lang.

--- xuanxuan/module/chat/ext/lang/zh-cn/xuanxuan.php
+++ xuanxuan/module/chat/ext/lang/zh-cn/xuanxuan.php
@@ -31,8 +31,8 @@ $lang->chat->xxdServerTip    = 'XXD服务器地址为完整的协议+地址+端�
 $lang->chat->xxdServerEmpty  = 'XXD服务器地址为空。';
 $lang->chat->xxdSchemeError  = '服务器地址应该以<strong>http://</strong>或<strong>https://</strong>开头。';
 $lang->chat->xxdPortError    = '服务器地址应该包含有效的端口号，默认为<strong>11443</strong>。';
-$lang->chat->xxClientConfirm = '禅道新增客户端，点击下载体验！';
-$lang->chat->xxServerConfirm = '禅道新增客户端，点击确认前往后台配置客户端服务器。';
+$lang->chat->xxClientConfirm = '禅道新增客户端，与禅道深度集成，支持成员沟通，小组讨论，文件传输，任务指派，更加方便的项目管理，更加流畅的团队协作！点击确认下载客户端。';
+$lang->chat->xxServerConfirm = '禅道新增客户端，与禅道深度集成，支持成员沟通，小组讨论，文件传输，任务指派，更加方便的项目管理，更加流畅的团队协作！点击确认前往后台配置客户端服务器。';
 
 $lang->chat->xxd = new stdclass();
 $lang->chat->xxd->os             = '操作系统';
