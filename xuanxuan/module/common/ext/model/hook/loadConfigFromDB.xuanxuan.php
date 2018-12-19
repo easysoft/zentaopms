20f7e52241e46ada43b3424a36e7412a3d5c1470
Yagami <sgm0422@qq.com>
2018-12-19 11:10:45 +0800

    * Fix download xxd bugs.

--- xuanxuan/module/common/ext/model/hook/loadConfigFromDB.xuanxuan.php
+++ xuanxuan/module/common/ext/model/hook/loadConfigFromDB.xuanxuan.php
@@ -1,7 +1,15 @@
 <?php
-$sn = $this->loadModel('setting')->getItem('owner=system&module=common&section=xuanxuan&key=key');
+$sn         = $this->loadModel('setting')->getItem('owner=system&module=common&section=xuanxuan&key=key');
+$chatPort   = $this->loadModel('setting')->getItem('owner=system&module=common&section=xuanxuan&key=chatPort');
+$commonPort = $this->loadModel('setting')->getItem('owner=system&module=common&section=xuanxuan&key=commonPort');
+$ip         = $this->loadModel('setting')->getItem('owner=system&module=common&section=xuanxuan&key=ip');
+$isHttps    = $this->loadModel('setting')->getItem('owner=system&module=common&section=xuanxuan&key=isHttps');
 if(empty($sn))
 {
     $this->setting->setItem('system.common.xuanxuan.turnon', 1);
     $this->setting->setItem('system.common.xuanxuan.key', $this->setting->computeSN());
 }
+if(!isset($chatPort))   $this->setting->setItem('system.common.xuanxuan.chatPort', 11444);
+if(!isset($commonPort)) $this->setting->setItem('system.common.xuanxuan.commonPort', 11443);
+if(!isset($ip))         $this->setting->setItem('system.common.xuanxuan.ip', '0.0.0.0');
+if(!isset($isHttps))    $this->setting->setItem('system.common.xuanxuan.isHttps', 0);
