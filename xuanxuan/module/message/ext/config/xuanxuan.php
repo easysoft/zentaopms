19daa2b0b7e368d82bc6ab79e2ed872143fd90d0
wangyidong <yidong@cnezsoft.com>
2018-12-20 10:43:44 +0800

    * adjust for message setting.

--- xuanxuan/module/message/ext/config/xuanxuan.php
+++ xuanxuan/module/message/ext/config/xuanxuan.php
@@ -1,3 +1,6 @@
 <?php
-$config->message->available['xuanxuan'] = $config->message->objectTypes;
+$config->message->available['xuanxuan']['story'] = array('opened', 'edited', 'commented', 'frombug', 'changed', 'reviewed', 'closed', 'activated');
+$config->message->available['xuanxuan']['task']  = array('opened', 'edited', 'commented', 'assigned', 'confirmed', 'started', 'finished', 'paused', 'canceled', 'restarted', 'closed', 'activated');
+$config->message->available['xuanxuan']['bug']   = array('opened', 'edited', 'commented', 'assigned', 'confirmed', 'bugconfirmed', 'resolved', 'closed', 'activated');
+$config->message->available['xuanxuan']['todo']  = array('opened', 'edited');
 $config->message->setting['xuanxuan']['setting'] = $config->message->available['xuanxuan'];
