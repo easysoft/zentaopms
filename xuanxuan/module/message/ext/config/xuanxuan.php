e35503d49345b1ac5eabc34dc849c2dd2f06edfe
wangyidong <yidong@cnezsoft.com>
2018-12-20 11:01:27 +0800

    * adjust message setting for xuanxuan

--- xuanxuan/module/message/ext/config/xuanxuan.php
+++ xuanxuan/module/message/ext/config/xuanxuan.php
@@ -1,6 +1,6 @@
 <?php
-$config->message->available['xuanxuan']['story'] = array('opened', 'edited', 'commented', 'frombug', 'changed', 'reviewed', 'closed', 'activated');
-$config->message->available['xuanxuan']['task']  = array('opened', 'edited', 'commented', 'assigned', 'confirmed', 'started', 'finished', 'paused', 'canceled', 'restarted', 'closed', 'activated');
-$config->message->available['xuanxuan']['bug']   = array('opened', 'edited', 'commented', 'assigned', 'confirmed', 'bugconfirmed', 'resolved', 'closed', 'activated');
-$config->message->available['xuanxuan']['todo']  = array('opened', 'edited');
+$config->message->available['xuanxuan']['story'] = $config->message->objectTypes['story'];
+$config->message->available['xuanxuan']['task']  = $config->message->objectTypes['task'];
+$config->message->available['xuanxuan']['bug']   = $config->message->objectTypes['bug'];
+$config->message->available['xuanxuan']['todo']  = $config->message->objectTypes['todo'];
 $config->message->setting['xuanxuan']['setting'] = $config->message->available['xuanxuan'];
