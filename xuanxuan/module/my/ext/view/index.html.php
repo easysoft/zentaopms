ac162d1c1555e5dd5a88c645b617c7ed3940ece0
Yagami <sgm0422@qq.com>
2018-12-20 16:49:13 +0800

    * Move xuanxuan notice file to my-index.

--- xuanxuan/module/my/ext/view/index.html.php
+++ xuanxuan/module/my/ext/view/index.html.php
@@ -1,7 +1,15 @@
 <?php
-$moduleName = $this->app->moduleName;
-$methodName = $this->app->methodName;
+/**
+ * The html template file of index method of index module of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2015 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     ZPL (http://zpl.pub/page/zplv12.html)
+ * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
+ * @package     ZenTaoPMS
+ * @version     $Id: index.html.php 1947 2011-06-29 11:58:03Z wwccss $
+ */
 ?>
+<?php include '../../../common/view/header.html.php';?>
 <script>
 <?php 
 $this->app->loadLang('chat');
@@ -19,3 +27,5 @@ var result = myModalTrigger.show();
 $.get(createLink('admin', 'ajaxSaveXXStatus', 'type=noticed'));
 <?php endif;?>
 </script>
+<?php echo $this->fetch('block', 'dashboard', 'module=my');?>
+<?php include '../../../common/view/footer.html.php';?>
