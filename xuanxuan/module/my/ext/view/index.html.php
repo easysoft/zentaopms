7e44c4b9c3fe4166cc02bf567c3399fb5e29adbe
Yagami <sgm0422@qq.com>
2018-12-21 11:17:59 +0800

    * Fix my index notice.

--- xuanxuan/module/my/ext/view/index.html.php
+++ xuanxuan/module/my/ext/view/index.html.php
@@ -13,18 +13,19 @@
 <script>
 <?php 
 $this->app->loadLang('chat');
+$account = $this->app->user->account;
 $xxInstalled = $this->app->user->account . 'installed';
 ?>
 <?php if(isset($config->xxserver->installed) and $config->xuanxuan->turnon and !isset($config->xxclient->$xxInstalled) and $config->global->flow == 'full' and $moduleName == 'my' and $methodName == 'index'):?>
 var myModalTrigger = new $.zui.ModalTrigger({custom: '<?php echo $lang->chat->xxClientConfirm;?>', title: '<?php echo $lang->chat->zentaoClient;?>'});
 var result = myModalTrigger.show();
-$.get(createLink('admin', 'ajaxSaveXXStatus', 'type=client'))
+<?php $this->loadModel('setting')->setItem("system.common.xxclient.{$account}installed", 1);?>
 <?php endif;?>
 
 <?php if(!isset($config->xxserver->noticed) and $this->app->user->admin and $config->global->flow == 'full' and $moduleName == 'my' and $methodName == 'index'):?>
 var myModalTrigger = new $.zui.ModalTrigger({custom: '<?php echo $lang->chat->xxServerConfirm;?>', title: '<?php echo $lang->chat->zentaoClient;?>'});
 var result = myModalTrigger.show();
-$.get(createLink('admin', 'ajaxSaveXXStatus', 'type=noticed'));
+<?php $this->loadModel('setting')->setItem("system.common.xxserver.noticed", 1);?>
 <?php endif;?>
 </script>
 <?php echo $this->fetch('block', 'dashboard', 'module=my');?>
