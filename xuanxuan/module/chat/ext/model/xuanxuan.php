2ced50d58a154c38f8fbc3594812bb85978bfa22
Yagami <sgm0422@qq.com>
2019-01-23 15:37:22 +0800

    * Move xuanxuan method.

--- xuanxuan/module/chat/ext/model/xuanxuan.php
+++ xuanxuan/module/chat/ext/model/xuanxuan.php
@@ -6,33 +6,6 @@ public function getExtensionList($userID)
 
 public function getUserListOutput($idList = array(), $userID)
 {
-    $output = new stdclass();
-    $output->module = 'chat';
-    $output->method = 'userGetList';
-
-    $users = $this->getUserList($status = '', $idList, $idAsKey = false);
-    if(dao::isError())
-    {
-        $output->result  = 'fail';
-        $output->message = 'Get userlist failed.';
-    }
-    else
-    {
-        $output->result = 'success';
-        $output->users  = !empty($userID) ? array($userID) : array();
-        $output->data   = $users;
-
-        $this->app->loadLang('user', 'sys');
-        $roles = $this->lang->user->roleList;
-
-        $allDepts = $this->loadModel('dept')->getListByType('dept');
-        $depts = array();
-        foreach($allDepts as $id => $dept)
-        {
-            $depts[$id] = array('name' => $dept->name, 'order' => (int)$dept->order, 'parent' => (int)$dept->parent);
-        }
-        $output->roles = $roles;
-        $output->depts = $depts;
-    }
-    return $output;
+    return $this->loadExtension('xuanxuan')->getUserListOutput($idList, $userID);
 }
+
