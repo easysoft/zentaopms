8f3604ec7ccc4b3b7d31bfe3cf3b48030f495495
Yagami <sgm0422@qq.com>
2019-02-20 13:46:33 +0800

    * Finish task #5270.

--- module/project/view/bug.html.php
+++ module/project/view/bug.html.php
@@ -100,7 +100,7 @@
           <?php
           $params = "bugID=$bug->id";
           common::printIcon('bug', 'confirmBug', $params, $bug, 'list', 'confirm', '', 'iframe', true);
-          common::printIcon('bug', 'resolve', $params, $bug, 'list', 'check', '', 'iframe', true);
+          common::printIcon('bug', 'resolve', $params, $bug, 'list', 'checked', '', 'iframe', true);
           common::printIcon('bug', 'close',   $params, $bug, 'list', '', '', 'iframe', true);
           common::printIcon('bug', 'create', "product=$bug->product&branch=$bug->branch&extra=$params", $bug, 'list', 'copy');
           common::printIcon('bug', 'edit',   $params, $bug, 'list');
