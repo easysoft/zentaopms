11393c442e8f2bd6e3b2490d0638a9b154f3fb5b
Yagami <sgm0422@qq.com>
2019-02-22 13:30:14 +0800

    * Finish task #5295.

--- module/doc/view/browse.html.php
+++ module/doc/view/browse.html.php
@@ -154,8 +154,8 @@ var browseType = '<?php echo $browseType;?>';
               <td class="c-name"><?php echo html::a(inlink('view', "docID=$doc->id"), "<i class='icon icon-file-text text-muted'></i> &nbsp;" . $doc->title);?></td>
               <td class="c-num"><?php echo $doc->fileSize ? $doc->fileSize : '-';?></td>
               <td class="c-user"><?php echo zget($users, $doc->addedBy);?></td>
-              <td class="c-datetime"><?php echo formatTime($doc->addedDate, 'm-d H:i');?></td>
-              <td class="c-datetime"><?php echo formatTime($doc->editedDate, 'm-d H:i');?></td>
+              <td class="c-datetime"><?php echo formatTime($doc->addedDate, 'y-m-d');?></td>
+              <td class="c-datetime"><?php echo formatTime($doc->editedDate, 'y-m-d');?></td>
               <td class="c-actions">
                 <?php if(common::hasPriv('doc', 'collect')):?>
                 <a data-url="<?php echo $this->createLink('doc', 'collect', "objectID=$doc->id&objectType=doc");?>" title="<?php echo $collectTitle;?>" class='btn btn-link ajaxCollect'><i class='icon <?php echo $star;?>'></i></a>
