5b23f6aba9e4234553fad0257628aa428d299faf
wangyidong <yidong@cnezsoft.com>
2019-02-25 10:51:49 +0800

    * finish task #5275.

--- module/doc/view/sort.html.php
+++ module/doc/view/sort.html.php
@@ -1,5 +1,6 @@
 <div class='libs-group sort'>
   <style>
+  .side-col .tabs .tab-content{overflow-x:hidden;}
   .libs-group.sort {padding-left:15px;}
   .libs-group.sort .lib {padding:4px 0 4px 6px;cursor: pointer; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;}
   </style>
