d2d5b68e4d692c443948673ca84d6fc64d0bd74b
reneeteng <crystaltoffee@hotmail.com>
2019-03-11 14:48:37 +0800

    *lang change.

--- module/webhook/lang/en.php
+++ module/webhook/lang/en.php
@@ -2,13 +2,13 @@
 $lang->webhook->common   = 'Webhook';
 $lang->webhook->list     = 'Hook List';
 $lang->webhook->api      = 'API';
-$lang->webhook->entry    = 'Entry';
+$lang->webhook->entry    = 'Application';
 $lang->webhook->log      = 'Log';
-$lang->webhook->assigned = 'Assigned To';
+$lang->webhook->assigned = 'Assign To';
 $lang->webhook->setting  = 'Setting';
 
 $lang->webhook->browse = 'Browse';
-$lang->webhook->create = 'Create';
+$lang->webhook->create = 'Add Application';
 $lang->webhook->edit   = 'Edit';
 $lang->webhook->delete = 'Delete';
 
@@ -16,7 +16,7 @@ $lang->webhook->id          = 'ID';
 $lang->webhook->type        = 'Type';
 $lang->webhook->name        = 'Name';
 $lang->webhook->url         = 'Webhook Address';
-$lang->webhook->domain      = 'Zentao Domain';
+$lang->webhook->domain      = 'ZenTao Domain';
 $lang->webhook->contentType = 'Content Type';
 $lang->webhook->sendType    = 'Send Type';
 $lang->webhook->product     = "{$lang->productCommon}";
@@ -25,9 +25,9 @@ $lang->webhook->params      = 'Params';
 $lang->webhook->action      = 'Action';
 $lang->webhook->desc        = 'Desc';
 $lang->webhook->createdBy   = 'Created By';
-$lang->webhook->createdDate = 'Created Date';
+$lang->webhook->createdDate = 'Created';
 $lang->webhook->editedby    = 'Edited By';
-$lang->webhook->editedDate  = 'Edited Date';
+$lang->webhook->editedDate  = 'Edited';
 $lang->webhook->date        = 'Date';
 $lang->webhook->data        = 'Data';
 $lang->webhook->result      = 'Result';
@@ -37,7 +37,7 @@ $lang->webhook->typeList['bearychat'] = 'Bearychat';
 $lang->webhook->typeList['dingding']  = 'Dingding';
 $lang->webhook->typeList['default']   = 'Others';
 
-$lang->webhook->sendTypeList['sync']  = 'Synchronization';
+$lang->webhook->sendTypeList['sync']  = 'Synchronous';
 $lang->webhook->sendTypeList['async'] = 'Asynchronous';
 
 $lang->webhook->paramsList['objectType'] = 'Object Type';
@@ -48,14 +48,14 @@ $lang->webhook->paramsList['action']     = 'Action';
 $lang->webhook->paramsList['actor']      = 'Actor';
 $lang->webhook->paramsList['date']       = 'Date';
 $lang->webhook->paramsList['comment']    = 'Comment';
-$lang->webhook->paramsList['text']       = 'Action Desc';
+$lang->webhook->paramsList['text']       = 'Action Description';
 
-$lang->webhook->confirmDelete = 'Are your sure delete this hook?';
+$lang->webhook->confirmDelete = 'Do you want to delete this hook?';
 
 $lang->webhook->trimWords = '';
 
 $lang->webhook->note = new stdClass();
-$lang->webhook->note->async   = 'If the send type is asynchronous, need open cron.';
+$lang->webhook->note->async   = 'If the send type is asynchronous, you need open cron.';
 $lang->webhook->note->product = "All actions will trigger the hook if the {$lang->productCommon} is empty, else only actions of the {$lang->productCommon} will trigger it.";
 $lang->webhook->note->project = "All actions will trigger the hook if the {$lang->projectCommon} is empty, else only actions of the {$lang->projectCommon} will trigger it.";
 
