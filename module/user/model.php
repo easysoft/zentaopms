a60f376f1f222ed900af79baabb38239231051d0
yfl <554665488@qq.com>
2019-04-19 11:32:04 +0800

    * except feedback user mv zentaobiz

--- module/user/model.php
+++ module/user/model.php
@@ -91,7 +91,6 @@ class userModel extends model
         $users = $this->dao->select($fields)->from(TABLE_USER)
             ->where('1')
             ->beginIF(strpos($params, 'nodeleted') !== false or empty($this->config->user->showDeleted))->andWhere('deleted')->eq('0')->fi()
-            ->beginIF(strpos($params, 'nofeedback') !== false )->andWhere('feedback')->ne('1')->fi()
             ->orderBy($orderBy)
             ->fetchAll('account');
         if($usersToAppended) $users += $this->dao->select($fields)->from(TABLE_USER)->where('account')->in($usersToAppended)->fetchAll('account');
