240c2412822d238e3761e424231e87a8b32fb629
wangyidong <yidong@cnezsoft.com>
2019-04-26 17:58:15 +0800

    * move sendDownHeader method to model.

--- module/file/model.php
+++ module/file/model.php
@@ -821,4 +821,40 @@ class fileModel extends model
             unset($_SESSION['album'][$uid]);
         }
     }
+
+    /**
+     * Send the download header to the client.
+     * 
+     * @param  string    $fileName 
+     * @param  string    $fileType 
+     * @param  string    $content 
+     * @access public
+     * @return void
+     */
+    public function sendDownHeader($fileName, $fileType, $content)
+    {
+        /* Clean the ob content to make sure no space or utf-8 bom output. */
+        $obLevel = ob_get_level();
+        for($i = 0; $i < $obLevel; $i++) ob_end_clean();
+
+        /* Set the downloading cookie, thus the export form page can use it to judge whether to close the window or not. */
+        setcookie('downloading', 1);
+
+        /* Append the extension name auto. */
+        $extension = '.' . $fileType;
+        if(strpos($fileName, $extension) === false) $fileName .= $extension;
+
+        /* urlencode the filename for ie. */
+        if(strpos($this->server->http_user_agent, 'MSIE') !== false or strpos($this->server->http_user_agent, 'Trident') !== false or strpos($this->server->http_user_agent, 'Edge') !== false) $fileName = urlencode($fileName);
+
+        /* Judge the content type. */
+        $mimes = $this->config->file->mimes;
+        $contentType = isset($mimes[$fileType]) ? $mimes[$fileType] : $mimes['default'];
+
+        header("Content-type: $contentType");
+        header("Content-Disposition: attachment; filename=\"$fileName\"");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+        die($content);
+    }
 }
