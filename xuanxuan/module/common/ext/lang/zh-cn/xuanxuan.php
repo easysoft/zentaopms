7cf950a2eb96303f5bd1808e2a2dc5fcf5c9bde7
wangyidong <yidong@cnezsoft.com>
2019-05-07 10:49:03 +0800

    * hide update xuanxuan.

--- xuanxuan/module/common/ext/lang/zh-cn/xuanxuan.php
+++ xuanxuan/module/common/ext/lang/zh-cn/xuanxuan.php
@@ -4,9 +4,9 @@ $lang->admin->menu->xuanxuan = array('link' => '客户端|admin|xuanxuan', 'subM
 
 $lang->admin->subMenu->xuanxuan = new stdclass();
 $lang->admin->subMenu->xuanxuan->setting = array('link' => '参数|admin|xuanxuan');
-$lang->admin->subMenu->xuanxuan->update  = array('link' => '更新|client|browse', 'subModule' => 'client');
+//$lang->admin->subMenu->xuanxuan->update  = array('link' => '更新|client|browse', 'subModule' => 'client');
 $lang->admin->subMenuOrder->xuanxuan[0] = 'setting';
-$lang->admin->subMenuOrder->xuanxuan[5] = 'update';
+//$lang->admin->subMenuOrder->xuanxuan[5] = 'update';
 
 $lang->menugroup->client = 'admin';
 
