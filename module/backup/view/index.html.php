7157e51ea6bf4708de3cd63eec5b740a40175e9b
Yagami <976204163@qq.com>
2019-06-20 09:18:18 +0800

    * Finish task #5815.

--- module/backup/view/index.html.php
+++ module/backup/view/index.html.php
@@ -51,7 +51,7 @@
         <td <?php if($rowspan > 1) echo "rowspan='$rowspan'"?>><?php echo date(DT_DATETIME1, $backupFile->time);?></td>
         <?php endif;?>
         <td class='text-left' style='padding-left:5px;'><?php echo $file;?></td>
-        <td><?php echo $size / 1024 >= 1024 ? round($size / 1024 / 1024, 2) . 'MB' : round($size / 1024, 2) . 'KB';?></td>
+        <td><?php echo $size / 1024 / 1024 >= 1024 ? round($size / 1024 / 1024 / 1024, 2) . 'GB' : ($size / 1024 >= 1024 ? round($size / 1024 / 1024, 2) . 'MB' : round($size / 1024, 2) . 'KB');?></td>
         <?php if($i == 0):?>
         <td <?php if($rowspan > 1) echo "rowspan='$rowspan'"?>>
           <?php
