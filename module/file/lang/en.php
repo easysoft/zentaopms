dc33a59896dc4388127d503a5a5cb55c05eb721f
Yagami <976204163@qq.com>
2019-07-01 10:15:01 +0800

    * Fix export style.

--- module/file/lang/en.php
+++ module/file/lang/en.php
@@ -20,7 +20,7 @@ $lang->file->delete        = 'Delete File';
 $lang->file->label         = 'Label:';
 $lang->file->maxUploadSize = "<span class='text-red'>%s</span>";
 $lang->file->applyTemplate = "Apply Template";
-$lang->file->tplTitle      = "Name";
+$lang->file->tplTitle      = "Template Name";
 $lang->file->setPublic     = "Set Public Template";
 $lang->file->exportFields  = "Fields";
 $lang->file->exportRange   = "Data";
