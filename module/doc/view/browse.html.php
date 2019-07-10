4e85182b8f0e2b9f71bf94a75a2e3f2a51d809ea
wangyidong <yidong@cnezsoft.com>
2019-07-10 14:38:38 +0800

    * finish task #5959.

--- module/doc/view/browse.html.php
+++ module/doc/view/browse.html.php
@@ -52,7 +52,7 @@ var browseType = '<?php echo $browseType;?>';
             </ul>
           </div>
           <?php if(common::hasPriv('doc', 'create')):?>
-          <div class="dropdown">
+          <div class="dropdown" id='createDropdown'>
             <button class='btn btn-primary' type='button' data-toggle='dropdown'><i class='icon icon-plus'></i> <?php echo $lang->doc->create;?> <span class='caret'></span></button>
             <ul class='dropdown-menu'>
               <?php foreach($lang->doc->typeList as $typeKey => $typeName):?>
