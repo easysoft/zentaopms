f047de96cb95fffe71a69bddc96636b308100250
jinianlan <jinianlan@qq.com>
2019-07-15 13:59:50 +0800

    * finish task #6105

--- module/block/view/todoblock.html.php
+++ module/block/view/todoblock.html.php
@@ -21,7 +21,7 @@ if(!$selfCall) die(include('./todolist.html.php'));
 .block-todoes .todoes {padding: 0 10px 10px 10px; margin: 0 -20px; max-height: 350px; overflow: auto; overflow-x:hidden}
 .block-todoes .todoes > li {position: relative; padding: 5px 10px 5px 35px; list-style: none; white-space:nowrap; overflow: auto; overflow-x:hidden;}
 .block-todoes .todoes > li:hover {background-color: #e9f2fb;}
-.block-todoes .todo-title {padding: 5px;}
+.block-todoes .todo-title {padding: 5px 15px 5px 5px;}
 .block-todoes .todo-pri {margin: 0 5px;}
 .block-todoes .todo-time {display: inline-block; padding: 0 5px; font-size: 12px; color: #8e939a; width: 75px;}
 .block-todoes .todo-check {position: absolute; top: 5px; left: 10px; display: block; width: 20px; height: 20px; font-size: 20px; color: transparent; cursor: pointer; background: #fff; border: 2px solid #eee; border-radius: 50%;}
