c0451b593b8ecdf4f1e2d5362153171be6f2653f
wangyidong <yidong@cnezsoft.com>
2019-08-16 16:10:03 +0800

    * code for task #6180.

--- module/testreport/js/view.js
+++ module/testreport/js/view.js
@@ -2,4 +2,12 @@ $(function()
 {
     $('.tab-pane').removeClass('active');
     $('.tab-pane:first').addClass('active');
+    $('.nav.nav-tabs a[data-toggle="tab"]').on('shown.zui.tab', function(e)
+    {
+        var href = $(e.target).data('target');
+        if(href == '#tabComment')
+        {
+            $('.tab-content #tabComment .article-content img').each(function(){setImageSize($(this), 0 , 0);});
+        }
+    });
 });
