8bdcfef9801eb7f192f4eb0ccd17151b13562d29
wangyidong <yidong@cnezsoft.com>
2019-08-21 10:11:44 +0800

    * finish task #6243.

--- framework/base/helper.class.php
+++ framework/base/helper.class.php
@@ -699,7 +699,7 @@ function isLocalIP()
     global $config;
     if(isset($config->islocalIP)) return $config->isLocalIP;
     $serverIP = $_SERVER['SERVER_ADDR'];
-    if($serverIP == '127.0.0.1') return true;
+    if($serverIP == '127.0.0.1' or $serverIP == '::1') return true;
     if(strpos($serverIP, '10.70') !== false) return false;
     return !filter_var($serverIP, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE);
 }
