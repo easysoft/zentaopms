09c04815ea910b962cfca00b4c486cfbfa643cde
wangyidong <yidong@cnezsoft.com>
2019-09-24 10:41:04 +0800

    * add changelog.

--- module/misc/lang/zh-tw.php
+++ module/misc/lang/zh-tw.php
@@ -82,6 +82,7 @@ $lang->misc->feature = new stdclass();
 $lang->misc->feature->lastest  = '最新版本';
 $lang->misc->feature->detailed = '詳情';
 
+$lang->misc->releaseDate['11.6.3']      = '2019-09-24';
 $lang->misc->releaseDate['11.6.2']      = '2019-09-06';
 $lang->misc->releaseDate['11.6.1']      = '2019-08-23';
 $lang->misc->releaseDate['11.6.stable'] = '2019-07-12';
@@ -114,16 +115,15 @@ $lang->misc->releaseDate['7.2.stable']  = '2015-05-22';
 $lang->misc->releaseDate['7.1.stable']  = '2015-03-07';
 $lang->misc->releaseDate['6.3.stable']  = '2014-11-07';
 
-$lang->misc->feature->all['11.6.2'][]      = array('title'=>'完善細節，修復Bug', 'desc' => '');
-
-$lang->misc->feature->all['11.6.1'][]      = array('title'=>'完善細節，修復Bug', 'desc' => '');
+$lang->misc->feature->all['11.6.3'][] = array('title'=>'修復Bug', 'desc' => '');
+$lang->misc->feature->all['11.6.2'][] = array('title'=>'完善細節，修復Bug', 'desc' => '');
+$lang->misc->feature->all['11.6.1'][] = array('title'=>'完善細節，修復Bug', 'desc' => '');
 
 $lang->misc->feature->all['11.6.stable'][] = array('title'=>'改善國際版界面', 'desc' => '');
 $lang->misc->feature->all['11.6.stable'][] = array('title'=>'添加翻譯功能', 'desc' => '');
 
-$lang->misc->feature->all['11.5.2'][]      = array('title'=>'增加禪道安全性，增加登錄禪道弱口令檢查', 'desc' => '');
-
-$lang->misc->feature->all['11.5.1'][]      = array('title'=>'新增第三方應用免密登錄禪道，修復Bug', 'desc' => '');
+$lang->misc->feature->all['11.5.2'][] = array('title'=>'增加禪道安全性，增加登錄禪道弱口令檢查', 'desc' => '');
+$lang->misc->feature->all['11.5.1'][] = array('title'=>'新增第三方應用免密登錄禪道，修復Bug', 'desc' => '');
 
 $lang->misc->feature->all['11.5.stable'][] = array('title'=>'完善細節，修復Bug', 'desc' => '');
 $lang->misc->feature->all['11.5.stable'][] = array('title'=>'新增動態過濾機制', 'desc' => '');
