8ef5ab336c918ea92d7a88cf195ca805268e8a35
wangyidong <yidong@cnezsoft.com>
2019-10-16 13:12:58 +0800

    * code for task #6421.

--- xuanxuan/module/action/ext/model/hook/create.xuanxuan.php
+++ xuanxuan/module/action/ext/model/hook/create.xuanxuan.php
@@ -1,5 +1,5 @@
 <?php
-if(strtolower($actionType) == 'reconnectxuanxuan' or strtolower($actionType) == 'reconnectxuanxuan')
+if(strtolower($actionType) == 'reconnectxuanxuan' or strtolower($actionType) == 'loginxuanxuan')
 {
     $this->dao->update(TABLE_USER)->set('visits = visits + 1')->set('ip')->eq($ip)->set('last')->eq($last)->where('account')->eq($actor)->exec();
 }
