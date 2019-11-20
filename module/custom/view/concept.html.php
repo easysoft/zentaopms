4c9b1d2d4daf1d0b349628568bffa06826cdb72b
guanxiying <guanxiying@cnezsoft.com>
2019-11-20 16:59:23 +0800

    * Fix style of concept page.

--- module/custom/view/concept.html.php
+++ module/custom/view/concept.html.php
@@ -8,46 +8,49 @@
  * @package     ZenTaoPMS
  * @version     $Id: deny.html.php 4129 2013-01-18 01:58:14Z wwccss $
  */
-include '../../common/view/header.lite.html.php';
 ?>
 <?php if($this->config->conceptSetted):?>
-<?php include '../../common/view/header.html.php';?>
+<?php include 'header.html.php';?>
 <?php else:?>
 <?php include '../../common/view/header.lite.html.php';?>
 <?php endif;?>
+<?php if($this->config->conceptSetted):?>
+<div id='mainContent' class='main-content'>
+  <div class='main-header'>
+    <div class='heading'>
+      <strong><?php echo $lang->custom->concept?></strong>
+    </div>
+  </div>
+<?php else:?>
 <div class='container'>
-<?php if($_POST):?>
   <div class='modal-dialog'>
     <div class='alert alert-success'>
     <?php printf($lang->custom->notice->conceptResult, $lang->productCommon, $lang->projectCommon, $lang->storyCommon, $lang->hourCommon);?>
     </div>
   </div>
-<?php else:?>
+<?php endif;?>
   <form id='ajaxForm' method='post'>
-    <div class='modal-dialog'>
-      <div class='modal-header'><strong><?php echo $lang->custom->concept;?></strong></div>
-          <div class='modal-body'>
-            <div class="form-group">
-              <label><?php echo $lang->custom->conceptQuestions['overview']?></label>
-              <div class="checkbox"> <?php echo html::radio('productProject', $lang->custom->productProject->relation, zget($this->config->custom, 'productProject', '0_0'))?> </div>
-            </div>
-            <?php if(!common::checkNotCN()):?>
-            <div class="form-group">
-              <label><?php echo $lang->custom->conceptQuestions['story']?></label>
-              <div class="checkbox"> <?php echo html::radio('storyRequirement', $lang->custom->conceptOptions->story, zget($this->config->custom, 'storyRequirement', '0'));?></div>
-            </div>
-            <?php endif;?>
-            <div class="form-group">
-              <label><?php echo $lang->custom->conceptQuestions['storypoint'];?></label>
-              <div class="checkbox"> <?php echo html::radio('hourPoint', $lang->custom->conceptOptions->hourPoint, '0')?> </div>
-            </div>
-          </div>
-      <div class='modal-footer'>
-        <div class='text-center'><?php echo html::submitButton();?></div>
+    <div class='modal-body'>
+      <div class="form-group">
+        <label><?php echo $lang->custom->conceptQuestions['overview']?></label>
+        <div class="checkbox"> <?php echo html::radio('productProject', $lang->custom->productProject->relation, zget($this->config->custom, 'productProject', '0_0'))?> </div>
+      </div>
+      <?php if(!common::checkNotCN()):?>
+      <div class="form-group">
+        <label><?php echo $lang->custom->conceptQuestions['story']?></label>
+        <div class="checkbox"> <?php echo html::radio('storyRequirement', $lang->custom->conceptOptions->story, zget($this->config->custom, 'storyRequirement', '0'));?></div>
+      </div>
+      <?php endif;?>
+      <div class="form-group">
+        <label><?php echo $lang->custom->conceptQuestions['storypoint'];?></label>
+        <div class="checkbox"> <?php echo html::radio('hourPoint', $lang->custom->conceptOptions->hourPoint, '0')?> </div>
+      </div>
+      <div class="form-group">
+        <label></label>
+        <div><?php echo html::submitButton();?></div>
       </div>
     </div>
   </form>
-<?php endif;?>
 <?php if($this->config->conceptSetted):?>
 <?php include '../../common/view/footer.html.php';?>
 <?php else:?>
