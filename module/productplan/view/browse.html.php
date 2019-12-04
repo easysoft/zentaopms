abbc22452709590e7e0e0c5a762a6942e3b0b91b
wangyidong <yidong@cnezsoft.com>
2019-12-04 11:18:45 +0800

    * code for task #6621.

--- module/productplan/view/browse.html.php
+++ module/productplan/view/browse.html.php
@@ -103,7 +103,7 @@
           ?>
         </td>
         <?php if($this->session->currentProductType != 'normal'):?>
-        <td class='c-branch'><?php echo $branches[$plan->branch];?></td>
+        <td class='c-branch' title='<?php echo $branches[$plan->branch];?>'><?php echo $branches[$plan->branch];?></td>
         <?php endif;?>
         <td><?php echo $plan->begin == '2030-01-01' ? $lang->productplan->future : $plan->begin;?></td>
         <td><?php echo $plan->end == '2030-01-01' ? $lang->productplan->future : $plan->end;?></td>
