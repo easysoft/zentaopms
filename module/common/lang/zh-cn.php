8a7c25b6015cc031d64a6aedf8ef8e164594eb03
wangyidong <yidong@cnezsoft.com>
2019-12-11 16:07:49 +0800

    * finish task #6673.

--- module/common/lang/zh-cn.php
+++ module/common/lang/zh-cn.php
@@ -330,7 +330,7 @@ $lang->caselib->menu->testcase  = array('link' => '用例|testcase|browse|');
 $lang->caselib->menu->testtask  = array('link' => '测试单|testtask|browse|');
 $lang->caselib->menu->testsuite = array('link' => '套件|testsuite|browse|');
 $lang->caselib->menu->report    = array('link' => '报告|testreport|browse|');
-$lang->caselib->menu->caselib   = array('link' => '用例库|caselib|browse', 'alias' => 'create,createcase,view,edit,batchcreatecase,showimport', 'subModule' => 'tree,testcase');
+$lang->caselib->menu->caselib   = array('link' => '用例库|caselib|browse|libID=%s', 'alias' => 'create,createcase,view,edit,batchcreatecase,showimport', 'subModule' => 'tree,testcase');
 
 $lang->repo = new stdclass();
 $lang->repo->menu = new stdclass();
