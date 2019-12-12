ad947eb802bab5c79b11c685af887f4a5ab445ab
wangyidong <yidong@cnezsoft.com>
2019-12-12 14:36:18 +0800

    * finish task #6668.

--- module/project/js/task.js
+++ module/project/js/task.js
@@ -1,6 +1,11 @@
 $(function()
 {
     if($('#taskList thead th.c-name').width() < 150) $('#taskList thead th.c-name').width(150);
+    $('#taskList td.has-child .task-toggle').each(function()
+    {
+        $td = $(this).closest('td');
+        $td.find('a').eq(0).css('max-width', $td.width() - $td.find('.label').width() - 40);
+    });
 });
 
 $('#module' + moduleID).closest('li').addClass('active');
