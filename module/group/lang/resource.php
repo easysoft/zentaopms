3b46b721918d6789d3b32ed11b978d2d8f6d34c2
wangyidong <yidong@cnezsoft.com>
2020-01-03 11:00:22 +0800

    * remove annualData priv.

--- module/group/lang/resource.php
+++ module/group/lang/resource.php
@@ -908,14 +908,12 @@ $lang->resource->report->productSummary   = 'productSummary';
 $lang->resource->report->bugCreate        = 'bugCreate';
 $lang->resource->report->bugAssign        = 'bugAssign';
 $lang->resource->report->workload         = 'workload';
-$lang->resource->report->annualData       = 'annual';
 
 $lang->report->methodOrder[0]  = 'index';
 $lang->report->methodOrder[5]  = 'projectDeviation';
 $lang->report->methodOrder[10] = 'productSummary';
 $lang->report->methodOrder[15] = 'bugCreate';
 $lang->report->methodOrder[20] = 'workload';
-$lang->report->methodOrder[25] = 'annualData';
 
 /* Search. */
 $lang->resource->search = new stdclass();
