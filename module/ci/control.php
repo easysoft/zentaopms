59a4a2c894482e08e30edddd7cd65d47ef6e0561
aaronchen2k <462826@qq.com>
2020-01-18 10:34:39 +0800

    fix isssue related to delete cron job

--- module/ci/control.php
+++ module/ci/control.php
@@ -328,7 +328,7 @@ class ci extends control
         $this->ci->delete(TABLE_CI_TASK, $id);
         if(dao::isError()) $this->send(array('result' => 'fail', 'message' => dao::getError()));
 
-        $command = 'moduleName=ci&methodName=exeCitask&id=' . $id;
+        $command = 'moduleName=ci&methodName=exeCitask&parm=' . $id;
         $this->dao->delete()->from(TABLE_CRON)->where('command')->eq($command)->exec();
 
         $this->send(array('result' => 'success'));
