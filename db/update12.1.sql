da17966c41b7db1c55cfd555dd55377ec73fb6f9
aaronchen2k <462826@qq.com>
2020-01-31 17:43:35 +0800

    update db schema

--- db/update12.1.sql
+++ db/update12.1.sql
@@ -1,45 +1,26 @@
 
 ALTER TABLE `zt_repo`
-ADD COLUMN `credentials` MEDIUMINT(8) NULL AFTER `password`;
-ALTER TABLE `zt_repo`
 ADD COLUMN `desc` TEXT NULL AFTER `lastSync`;
-
-CREATE TABLE `zt_credentials` (
-  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `type` varchar(10) NOT NULL DEFAULT 'account',
-  `name` varchar(50) NOT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `publicKey` text,
-  `privateKey` text,
-  `passphrase` varchar(20) DEFAULT NULL,
-  `desc` text NOT NULL,
-  `createdBy` varchar(30) NOT NULL,
-  `createdDate` datetime NOT NULL,
-  `editedBy` varchar(30) NOT NULL,
-  `editedDate` datetime NOT NULL,
-  `deleted` enum('0','1') NOT NULL DEFAULT '0',
-  `token` varchar(500) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `zt_jenkins` (
   `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `username` varchar(255) DEFAULT NULL,
   `serviceUrl` varchar(255) DEFAULT NULL,
+  `account` varchar(30) NOT NULL,
   `token` varchar(255) DEFAULT NULL,
+  `password` varchar(30) NOT NULL,
+  `encrypt` varchar(30) NOT NULL DEFAULT 'plain',
   `desc` text NOT NULL,
   `createdBy` varchar(30) NOT NULL,
   `createdDate` datetime NOT NULL,
   `editedBy` varchar(30) NOT NULL,
   `editedDate` datetime NOT NULL,
   `deleted` enum('0','1') NOT NULL DEFAULT '0',
-  `credentials` mediumint(8) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `zt_citask` (
+CREATE TABLE `zt_cijob` (
   `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `repo` mediumint(8) unsigned NOT NULL,
@@ -55,20 +36,21 @@ CREATE TABLE `zt_citask` (
   `tagKeywords` varchar(255) DEFAULT NULL,
   `commentKeywords` varchar(255) DEFAULT NULL,
   `desc` text,
-  `lastExec` DATETIME DEFAULT NULL,
-  `lastStatus` VARCHAR(255) DEFAULT NULL,
   `createdBy` varchar(30) NOT NULL,
   `createdDate` datetime NOT NULL,
   `editedBy` varchar(30) NOT NULL,
   `editedDate` datetime NOT NULL,
   `deleted` enum('0','1') NOT NULL DEFAULT '0',
+  `lastExec` datetime DEFAULT NULL,
+  `lastStatus` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `zt_cibuild` (
   `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
-  `citask` mediumint(8) unsigned NOT NULL,
+  `cijob` mediumint(8) unsigned NOT NULL,
+  `queueItem` mediumint(8) NOT NULL,
   `status` varchar(255) NOT NULL,
   `logs` text,
   `createdBy` varchar(30) NOT NULL,
@@ -76,7 +58,7 @@ CREATE TABLE `zt_cibuild` (
   `updateDate` datetime NOT NULL,
   `deleted` enum('0','1') NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=75 DEFAULT CHARSET=utf8;
 
 INSERT INTO `zt_cron` (`m`, `h`, `dom`, `mon`, `dow`, `command`, `remark`, `type`, `buildin`, `status`, `lastTime`) VALUES
 ('*/5',  '*',    '*',    '*',    '*',    'moduleName=citask&methodName=checkBuildStatus', '同步Jenkins任务状态', 'zentao', -1, 'normal',   '0000-00-00 00:00:00');
