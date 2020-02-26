ae877bbd3048bd29682382f8165b72d761cbc139
wangyidong <yidong@cnezsoft.com>
2020-02-26 18:55:32 +0800

    * change update sql for ci.

--- db/update12.0.1.sql
+++ db/update12.0.1.sql
@@ -1,1 +1,61 @@
+ALTER TABLE `zt_story` CHANGE `type` `type` varchar(30) COLLATE 'utf8_general_ci' NOT NULL DEFAULT 'story' AFTER `keywords`;
 UPDATE zt_story SET `type` = 'story' WHERE `type` = '';
+
+-- DROP TABLE IF EXISTS `zt_jenkins`;
+CREATE TABLE IF NOT EXISTS `zt_jenkins` (
+  `id` smallint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `account` varchar(30) DEFAULT NULL,
+  `password` varchar(30) NOT NULL,
+  `encrypt` varchar(30) NOT NULL DEFAULT 'plain',
+  `token` varchar(255) DEFAULT NULL,
+  `createdBy` varchar(30) NOT NULL,
+  `createdDate` datetime NOT NULL,
+  `editedBy` varchar(30) NOT NULL,
+  `editedDate` datetime NOT NULL,
+  `deleted` enum('0','1') NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- DROP TABLE IF EXISTS `zt_integration`;
+CREATE TABLE IF NOT EXISTS `zt_integration` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `repo` mediumint(8) unsigned NOT NULL,
+  `jenkins` mediumint(8) unsigned NOT NULL,
+  `jenkinsJob` varchar(500) NOT NULL,
+  `triggerType` varchar(255) NOT NULL,
+  `svnFolder` varchar(255) NOT NULL,
+  `scheduleDay` varchar(255) DEFAULT NULL,
+  `tagKeywords` varchar(255) DEFAULT NULL,
+  `commentKeywords` varchar(255) DEFAULT NULL,
+  `createdBy` varchar(30) NOT NULL,
+  `createdDate` datetime NOT NULL,
+  `editedBy` varchar(30) NOT NULL,
+  `editedDate` datetime NOT NULL,
+  `deleted` enum('0','1') NOT NULL DEFAULT '0',
+  `lastExec` datetime DEFAULT NULL,
+  `lastStatus` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- DROP TABLE IF EXISTS `zt_compile`;
+CREATE TABLE IF NOT EXISTS `zt_compile` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `cijob` mediumint(8) unsigned NOT NULL,
+  `queueItem` mediumint(8) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `logs` text,
+  `createdBy` varchar(30) NOT NULL,
+  `createdDate` datetime NOT NULL,
+  `updateDate` datetime NOT NULL,
+  `deleted` enum('0','1') NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+INSERT INTO `zt_cron` (`m`, `h`, `dom`, `mon`, `dow`, `command`, `remark`, `type`, `buildin`, `status`, `lastTime`) VALUES
+('1',    '1',    '*',    '*',    '*',    'moduleName=ci&methodName=buildTodayJob', '创建周期性任务', 'zentao', 1, 'normal',   '0000-00-00 00:00:00'),
+('*/5',  '*',    '*',    '*',    '*',    'moduleName=ci&methodName=checkBuildStatus', '同步Jenkins任务状态', 'zentao', 1, 'normal',   '0000-00-00 00:00:00'),
+('*/5',  '*',    '*',    '*',    '*',    'moduleName=ci&methodName=exec', '执行Jenkins任务', 'zentao', 1, 'normal',   '0000-00-00 00:00:00');
--- db/zentao.sql
+++ db/zentao.sql
@@ -213,6 +213,20 @@ CREATE TABLE IF NOT EXISTS `zt_company` (
   `deleted` enum('0','1') NOT NULL default '0',
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+-- DROP TABLE IF EXISTS `zt_compile`;
+CREATE TABLE IF NOT EXISTS `zt_compile` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `cijob` mediumint(8) unsigned NOT NULL,
+  `queueItem` mediumint(8) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `logs` text,
+  `createdBy` varchar(30) NOT NULL,
+  `createdDate` datetime NOT NULL,
+  `updateDate` datetime NOT NULL,
+  `deleted` enum('0','1') NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 -- DROP TABLE IF EXISTS `zt_config`;
 CREATE TABLE IF NOT EXISTS `zt_config` (
   `id` mediumint(8) unsigned NOT NULL auto_increment,
@@ -407,6 +421,43 @@ CREATE TABLE IF NOT EXISTS `zt_history` (
   `diff` mediumtext NOT NULL,
   PRIMARY KEY (`id`),
   KEY `action` (`action`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+-- DROP TABLE IF EXISTS `zt_integration`;
+CREATE TABLE IF NOT EXISTS `zt_integration` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `repo` mediumint(8) unsigned NOT NULL,
+  `jenkins` mediumint(8) unsigned NOT NULL,
+  `jenkinsJob` varchar(500) NOT NULL,
+  `triggerType` varchar(255) NOT NULL,
+  `svnFolder` varchar(255) NOT NULL,
+  `scheduleDay` varchar(255) DEFAULT NULL,
+  `tagKeywords` varchar(255) DEFAULT NULL,
+  `commentKeywords` varchar(255) DEFAULT NULL,
+  `createdBy` varchar(30) NOT NULL,
+  `createdDate` datetime NOT NULL,
+  `editedBy` varchar(30) NOT NULL,
+  `editedDate` datetime NOT NULL,
+  `deleted` enum('0','1') NOT NULL DEFAULT '0',
+  `lastExec` datetime DEFAULT NULL,
+  `lastStatus` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+-- DROP TABLE IF EXISTS `zt_jenkins`;
+CREATE TABLE IF NOT EXISTS `zt_jenkins` (
+  `id` smallint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `account` varchar(30) DEFAULT NULL,
+  `password` varchar(30) NOT NULL,
+  `encrypt` varchar(30) NOT NULL DEFAULT 'plain',
+  `token` varchar(255) DEFAULT NULL,
+  `createdBy` varchar(30) NOT NULL,
+  `createdDate` datetime NOT NULL,
+  `editedBy` varchar(30) NOT NULL,
+  `editedDate` datetime NOT NULL,
+  `deleted` enum('0','1') NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 -- DROP TABLE IF EXISTS `zt_module`;
 CREATE TABLE IF NOT EXISTS `zt_module` (
@@ -640,7 +691,7 @@ CREATE TABLE IF NOT EXISTS `zt_story` (
   `fromBug` mediumint(8) unsigned NOT NULL default '0',
   `title` varchar(255) NOT NULL,
   `keywords` varchar(255) NOT NULL,
-  `type` varchar(30) NOT NULL default '',
+  `type` varchar(30) NOT NULL default 'story',
   `pri` tinyint(3) unsigned NOT NULL default '3',
   `estimate` float unsigned NOT NULL,
   `status` enum('','changed','active','draft','closed') NOT NULL default '',
@@ -1100,61 +1151,6 @@ CREATE TABLE IF NOT EXISTS `zt_repofiles` (
   PRIMARY KEY (`id`),
   KEY `path` (`path`),
   KEY `parent` (`parent`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE `zt_jenkins` (
-  `id` smallint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `url` varchar(255) DEFAULT NULL,
-  `account` varchar(30) DEFAULT NULL,
-  `password` varchar(30) NOT NULL,
-  `encrypt` varchar(30) NOT NULL DEFAULT 'plain',
-  `token` varchar(255) DEFAULT NULL,
-  `createdBy` varchar(30) NOT NULL,
-  `createdDate` datetime NOT NULL,
-  `editedBy` varchar(30) NOT NULL,
-  `editedDate` datetime NOT NULL,
-  `deleted` enum('0','1') NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE `zt_integration` (
-  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `repo` mediumint(8) unsigned NOT NULL,
-  `jenkins` mediumint(8) unsigned NOT NULL,
-  `jenkinsJob` varchar(500) NOT NULL,
-  `triggerType` varchar(255) NOT NULL,
-  `svnFolder` varchar(255) NOT NULL,
-  `scheduleType` varchar(255) NOT NULL,
-  `cronExpression` varchar(255) DEFAULT NULL,
-  `scheduleDay` varchar(255) DEFAULT NULL,
-  `scheduleTime` varchar(255) DEFAULT NULL,
-  `scheduleInterval` mediumint(8) DEFAULT NULL,
-  `tagKeywords` varchar(255) DEFAULT NULL,
-  `commentKeywords` varchar(255) DEFAULT NULL,
-  `createdBy` varchar(30) NOT NULL,
-  `createdDate` datetime NOT NULL,
-  `editedBy` varchar(30) NOT NULL,
-  `editedDate` datetime NOT NULL,
-  `deleted` enum('0','1') NOT NULL DEFAULT '0',
-  `lastExec` datetime DEFAULT NULL,
-  `lastStatus` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE `zt_compile` (
-  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `cijob` mediumint(8) unsigned NOT NULL,
-  `queueItem` mediumint(8) NOT NULL,
-  `status` varchar(255) NOT NULL,
-  `logs` text,
-  `createdBy` varchar(30) NOT NULL,
-  `createdDate` datetime NOT NULL,
-  `updateDate` datetime NOT NULL,
-  `deleted` enum('0','1') NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 INSERT INTO `zt_cron` (`m`, `h`, `dom`, `mon`, `dow`, `command`, `remark`, `type`, `buildin`, `status`, `lastTime`) VALUES
