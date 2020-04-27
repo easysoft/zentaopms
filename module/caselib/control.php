08c8461844cef7e69b3d907071122238ada0dace
wangyidong <yidong@cnezsoft.com>
2020-04-27 13:23:39 +0800

    * finish task #7069.

--- module/caselib/control.php
+++ module/caselib/control.php
@@ -204,7 +204,7 @@ class caselib extends control
 
         $this->loadModel('datatable');
         $this->loadModel('tree');
-        $showModule = !empty($this->config->datatable->caselibLibrary->showModule) ? $this->config->datatable->caselibLibrary->showModule : '';
+        $showModule = !empty($this->config->datatable->caselibBrowse->showModule) ? $this->config->datatable->caselibBrowse->showModule : '';
         $this->view->modulePairs = $showModule ? $this->tree->getModulePairs($libID, 'caselib', $showModule) : array();
 
         $this->view->title      = $this->lang->caselib->common . $this->lang->colon . $libraries[$libID];
