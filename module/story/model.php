3e26e60560a922591ab653d5383fff7cbe794a88
wangyidong <yidong@cnezsoft.com>
2020-04-30 13:10:08 +0800

    * finish task #7091.

--- module/story/model.php
+++ module/story/model.php
@@ -2075,6 +2075,7 @@ class storyModel extends model
         $stories = $this->dao->select('id, title')->from(TABLE_STORY)
             ->where('deleted')->eq(0)
             ->andWhere('parent')->le(0)
+            ->andWhere('type')->eq('story')
             ->andWhere('status')->notin('closed,draft')
             ->andWhere('product')->eq($productID)
             ->beginIF($append)->orWhere('id')->in($append)->fi()
