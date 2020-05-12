5e2a7e17583f88ad10f154907410921d3219edf9
Catouse <catouse@gmail.com>
2020-05-12 13:39:31 +0800

    * add english language  data file.

---
+++ module/program/lang/en.php
@@ -0,0 +1,62 @@
+<?php
+$lang->program->index        = 'Project';
+$lang->program->common       = 'Project';
+$lang->program->stage        = 'Stage';
+$lang->program->name         = 'Name';
+$lang->program->type         = 'Type';
+$lang->program->category     = 'Category';
+$lang->program->desc         = 'Description';
+$lang->program->code         = 'Code';
+$lang->program->begin        = 'Begin';
+$lang->program->end          = 'End';
+$lang->program->status       = 'Status';
+$lang->program->PM           = 'Project Manager';
+$lang->program->create       = 'Create';
+$lang->program->edit         = 'Edit';
+$lang->program->all          = 'All';
+$lang->program->start        = 'Start';
+$lang->program->finish       = 'Finish';
+$lang->program->suspend      = 'Suspend';
+$lang->program->delete       = 'Delete';
+$lang->program->close        = 'Close';
+$lang->program->activate     = 'Activate';
+$lang->program->budget       = 'Budget';
+$lang->program->dateRange    = 'Date Range';
+$lang->program->to           = ' to ';
+$lang->program->realFinished = 'Real finished';
+$lang->program->realStarted  = 'Real started';
+$lang->program->bygrid       = 'Grid';
+$lang->program->bylist       = 'List';
+$lang->program->mine         = 'Participated';
+
+$lang->program->durationEstimation  = 'Workload estimate';
+$lang->program->noProgram           = 'No projects';
+
+$lang->program->unitList[''] = '';
+$lang->program->unitList['yuan']   = '元';
+$lang->program->unitList['dollar'] = 'Dollars';
+
+$lang->program->typeList['scrum'] = "Scrum";
+$lang->program->typeList['cmmi']  = "CMMI";
+
+$lang->program->categoryList['single']   = "Single product";
+$lang->program->categoryList['multiple'] = "Multiple products";
+
+$lang->program->featureBar['all']       = 'All';
+$lang->program->featureBar['doing']     = 'Going';
+$lang->program->featureBar['wait']      = 'Waiting';
+$lang->program->featureBar['suspended'] = 'Suspended';
+$lang->program->featureBar['closed']    = 'Closed';
+
+$lang->program->chooseProgramType = 'Choose management type';
+$lang->program->nextStep          = 'Next step';
+$lang->program->hoursUnit         = '%s hours';
+$lang->program->membersUnit       = '%s members';
+$lang->program->lastIteration     = 'Latest iterations';
+$lang->program->ongoingStage      = 'Ongoing stage';
+$lang->program->scrum             = 'Scrum';
+$lang->program->scrumTitle        = 'Scrum development full process project management';
+$lang->program->scrumDesc         = '<strong>Introduction: </strong>Iterate in small steps, release quickly<br><strong>Contains function points: </strong>Project overview, iterations, plans, stories, etc.';
+$lang->program->cmmi              = 'CMMI';
+$lang->program->cmmiTitle         = 'CMMI management';
+$lang->program->cmmiDesc          = '<strong>Introduction: </strong>Standardized management by stages<br><strong>Contains function points: </strong>Estimate, plan, stage, report, etc.';
