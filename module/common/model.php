7a9cc4070aef3b1e177aaf166ed02cf9b4272cfe
wangyidong <yidong@cnezsoft.com>
2020-05-28 09:52:41 +0800

    * fix for security weaknesses.

--- module/common/model.php
+++ module/common/model.php
@@ -167,10 +167,19 @@ class commonModel extends model
         {
             if(stripos($method, 'ajax') !== false) return true;
             if($module == 'misc' and $method == 'downloadclient') return true;
+            if($module == 'misc' and $method == 'changelog')  return true;
+            if($module == 'tutorial' and $method == 'start')  return true;
+            if($module == 'tutorial' and $method == 'index')  return true;
+            if($module == 'tutorial' and $method == 'quit')   return true;
+            if($module == 'tutorial' and $method == 'wizard') return true;
+            if($module == 'block' and $method == 'admin') return true;
+            if($module == 'block' and $method == 'set') return true;
+            if($module == 'block' and $method == 'sort') return true;
+            if($module == 'block' and $method == 'resize') return true;
+            if($module == 'block' and $method == 'dashboard') return true;
+            if($module == 'block' and $method == 'printblock') return true;
             if($module == 'block' and $method == 'main') return true;
-            if($module == 'misc' and $method == 'changelog') return true;
-            if($module == 'tutorial') return true;
-            if($module == 'block') return true;
+            if($module == 'block' and $method == 'delete') return true;
             if($module == 'product' and $method == 'showerrornone') return true;
             if($module == 'report' and $method == 'annualdata') return true;
         }
