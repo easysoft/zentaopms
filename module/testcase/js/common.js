d05924db1722648dbabbb5778e36458fd937de09
z <wyd621@163.com>
2020-06-19 15:01:22 +0800

    * finish task #7279.

--- module/testcase/js/common.js
+++ module/testcase/js/common.js
@@ -124,7 +124,7 @@ function setStories()
     productID = $('#product').val();
     branch    = $('#branch').val();
     if(typeof(branch) == 'undefined') branch = 0;
-    link = createLink('story', 'ajaxGetProductStories', 'productID=' + productID + '&branch=' + branch + '&moduleID=' + moduleID + '&storyID=0&onlyOption=false&status=&limit=50');
+    link = createLink('story', 'ajaxGetProductStories', 'productID=' + productID + '&branch=' + branch + '&moduleID=' + moduleID + '&storyID=0&onlyOption=false&status=noclosed&limit=50');
     $.get(link, function(stories)
     {
         var value = $('#story').val();
