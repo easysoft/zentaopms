02397f801de06e2226f50d47867a359f232f897f
z <wyd621@163.com>
2020-06-19 14:30:18 +0800

    * finish task #7282.

--- db/update12.3.2.sql
+++ db/update12.3.2.sql
@@ -1,1 +1,6 @@
 UPDATE `zt_story` SET `plan` = '' WHERE `parent` = '-1' AND `plan` != 0 AND `plan` != '';
+
+DELETE FROM `zt_story` WHERE `module` = 'api' AND `method` = 'getModel';
+DELETE FROM `zt_story` WHERE `module` = 'api' AND `method` = 'sql';
+
+ALTER TABLE `zt_jenkins` CHANGE `password` `password` varchar(255) COLLATE 'utf8_general_ci' NOT NULL AFTER `account`;
