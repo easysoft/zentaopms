9a1555f2eece2f3f90204c29cba2bcfca7b1411d
sgm0422 <976204163@qq.com>
2020-07-13 11:09:37 +0800

    * Add story stage title.

--- module/story/model.php
+++ module/story/model.php
@@ -2828,6 +2828,13 @@ class storyModel extends model
             else if($id == 'stage')
             {
                 $style .= 'overflow: visible;';
+                if(isset($storyStages[$story->id]))
+                {
+                    foreach($storyStages[$story->id] as $storyBranch => $storyStage)
+                    {    
+                        if(isset($branches[$storyBranch])) $title .= $branches[$storyBranch] . ": " . $this->lang->story->stageList[$storyStage->stage] . "\n";
+                    }
+                }
             }
 
             echo "<td class='" . $class . "' title='$title' style='$style'>";
