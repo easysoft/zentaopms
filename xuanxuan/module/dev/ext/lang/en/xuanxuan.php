deed071f8e80f0415e98d927a5466abcf0f9f034
z <wyd621@163.com>
2020-07-25 11:03:33 +0800

    * finish task #7539.

--- xuanxuan/module/dev/ext/lang/en/xuanxuan.php
+++ xuanxuan/module/dev/ext/lang/en/xuanxuan.php
@@ -1,5 +1,11 @@
 <?php
-$lang->dev->tableList['im_chat']          = 'Chat List'; 
-$lang->dev->tableList['im_message']       = 'Chat Message'; 
-$lang->dev->tableList['im_chatuser']      = 'Chat User'; 
-$lang->dev->tableList['im_messagestatus'] = 'Chat Status'; 
+$lang->dev->groupList['xuanxuan']= 'Client';
+
+$lang->dev->tableList['im_chatuser']         = 'Client Users';
+$lang->dev->tableList['im_message']          = 'Client Message';
+$lang->dev->tableList['im_messagestatus']    = 'Client Message Status';
+$lang->dev->tableList['im_chat']             = 'Client Chat';
+$lang->dev->tableList['im_client']           = 'Client Upgrade';
+$lang->dev->tableList['im_conference']       = 'Client Conference';
+$lang->dev->tableList['im_conferenceaction'] = 'Client Conference Action';
+$lang->dev->tableList['im_queue']            = 'Client Queue';
