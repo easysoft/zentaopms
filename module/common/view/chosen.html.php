b1cd0ed599bc550bcd6a6c5e5084c04a66245c56
z <wyd621@163.com>
2020-08-06 13:36:41 +0800

    * code for task #7600.

--- module/common/view/chosen.html.php
+++ module/common/view/chosen.html.php
@@ -1,5 +1,8 @@
 <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
+<?php css::import($jsRoot . 'zui/picker/zui.picker.min.css'); ?>
+<?php js::import($jsRoot . 'zui/picker/zui.picker.min.js'); ?>
 <style>
+.picker-single .picker-selection-remove{z-index: 1000;}
 .picker-selection-single:after, .picker-multi.picker-focus .picker-selections:before{font-family: ZentaoIcon !important; content: '\f0d7' !important;}
 </style>
 <script>
