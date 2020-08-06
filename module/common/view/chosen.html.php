c2b35db7d106675d787361e986492a123dd06ae9
Catouse <catouse@gmail.com>
2020-08-06 10:48:30 +0800

    * fix picker undefined error in some pages without import picker js.

--- module/common/view/chosen.html.php
+++ module/common/view/chosen.html.php
@@ -1,18 +1,19 @@
 <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
-<?php css::import($jsRoot . 'zui/picker/zui.picker.min.css'); ?>
-<?php js::import($jsRoot . 'zui/picker/zui.picker.min.js'); ?>
 <style>
 .picker-selection-single:after, .picker-multi.picker-focus .picker-selections:before{font-family: ZentaoIcon !important; content: '\f0d7' !important;}
 </style>
 <script>
 var chooseUsersToMail = '<?php echo $lang->chooseUsersToMail;?>';
-$(document).ready(function()
+if($.fn.picker)
 {
-    $(".picker-select[data-pickertype!='remote']").picker({chosenMode: true});
-    $("[data-pickertype='remote']").each(function()
+    $(document).ready(function()
     {
-        var pickerremote = $(this).attr('data-pickerremote');
-        $(this).picker({chosenMode: true, remote: pickerremote});
-    })
-});
+        $(".picker-select[data-pickertype!='remote']").picker({chosenMode: true});
+        $("[data-pickertype='remote']").each(function()
+        {
+            var pickerremote = $(this).attr('data-pickerremote');
+            $(this).picker({chosenMode: true, remote: pickerremote});
+        })
+    });
+}
 </script>
