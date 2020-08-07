0bfb09736e5272ca3fe0417417c37386e77ee5bb
tianshujie98 <tianshujie0123@163.com>
2020-08-07 17:31:04 +0800

    * Change back to the previous version.

--- module/testtask/config.php
+++ module/testtask/config.php
@@ -5,8 +5,8 @@ $config->testtask->edit   = new stdclass();
 $config->testtask->create->requiredFields = 'project,build,begin,end,name';
 $config->testtask->edit->requiredFields   = 'project,build,begin,end,name';
 
-$config->testtask->importUnitResult = new stdclass();
-$config->testtask->importUnitResult->requiredFields = 'project,build,begin,end,name,resultFile';
+$config->testtask->importunitresult = new stdclass();
+$config->testtask->importunitresult->requiredFields = 'project,build,begin,end,name,resultFile';
 
 $config->testtask->editor = new stdclass();
 $config->testtask->editor->create           = array('id' => 'desc', 'tools' => 'simpleTools');
@@ -16,7 +16,7 @@ $config->testtask->editor->start            = array('id' => 'comment', 'tools' =
 $config->testtask->editor->close            = array('id' => 'report,comment', 'tools' => 'simpleTools');
 $config->testtask->editor->block            = array('id' => 'comment', 'tools' => 'simpleTools');
 $config->testtask->editor->activate         = array('id' => 'comment', 'tools' => 'simpleTools');
-$config->testtask->editor->importUnitResult = array('id' => 'desc', 'tools' => 'simpleTools');
+$config->testtask->editor->importunitresult = array('id' => 'desc', 'tools' => 'simpleTools');
 
 $config->testtask->datatable = new stdclass();
 $config->testtask->datatable->defaultField = array('id', 'pri', 'title', 'type', 'assignedTo', 'lastRunner', 'lastRunDate', 'lastRunResult', 'status', 'bugs', 'results', 'stepNumber','actions');
