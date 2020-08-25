7853944ceaeaaff87e916d101b5ec16acf8f7851
leiyong <1549684884@qq.com>
2020-08-25 18:51:53 +0800

    * Add scrum overview block.

--- module/block/view/scrumoverallblock.html.php
+++ module/block/view/scrumoverallblock.html.php
@@ -13,48 +13,74 @@
 <?php if(empty($totalData)): ?>
 <div class='empty-tip'><?php common::printLink('project', 'create', '', "<i class='icon-plus'></i> " . $lang->project->create, '', "class='btn btn-primary'")?></div>
 <?php else:?>
-<div class="panel-body has-table scrollbar-hover">
-  <table class='table table-borderless table-hover'>
-    <thead>
-      <tr class=''>
-        <th colspan="3" class='c-name text-left'><?php echo '总投入';?></th>
-        <th class="c-name"><?php echo $lang->block->totalStory;?></th>
-        <th class="c-name"><?php echo $lang->block->totalBug;?></th>
-      </tr>
-    </thead>
-    <tbody class="text-center">
-      <tr>
-        <td class="c-name"><i class="icon icon-user"></i><?php echo '总人数';?></td>
-        <td class="c-name"><i class="icon icon-time"></i><?php echo '已消耗工时';?></td>
-        <td class="c-name"><i class="icon icon-yen"></i><?php echo '已花费';?></td>
-        <td class="c-name"><?php echo $totalData[$programID]->allStories;?></td>
-        <td class="c-name"><?php echo $totalData[$programID]->allBugs;?></td>
-      </tr>
-      <tr>
-        <td class="c-data"><?php echo $totalData[$programID]->teamCount;?></td>
-        <td class="c-data"><?php echo $totalData[$programID]->consumed;?></td>
-        <td class="c-data"><?php echo '无数据'?></td>
-        <td class="c-data">
-          <div class="progress">
-            <div class="progress-bar" role="progressbar" aria-valuenow="<?php echo $totalData[$programID]->doneStories;?>" aria-valuemin="0" aria-valuemax="<?php echo $totalData[$programID]->allStories;?>" style="width: <?php echo (($totalData[$programID]->doneStories/$totalData[$programID]->allStories)*100).'%'; ?>">
-            </div>
+<style>
+#toal-investment{padding-bottom: 5px;}
+.over-title{font-size: 14px; color: #838a9d;}
+.extra-title{font-size: 12px; color: #a1a7b7;}
+.progress{margin-bottom: 8px;}
+.total-div{padding: 0px 25px;}
+.extra-div{position: relative; padding: 0px 5px 5px; display: table-cell;text-align: center;}
+.extra-div > small {display: block; color: #A6AAB8;}
+.extra-div > span {display: block; color: #3c4353; font-weight: bold;}
+</style>
+<div class="panel-body conatiner-fluid">
+  <div class="table-row">
+    <div class="col-6 tile">
+      <div class="text-left" id="toal-investment"><strong>总投入</strong></div>
+      <div class="table-row">
+        <div class="col-4 tile">
+          <div class="over-title"><i class="icon-program-group icon-group"></i> 我的任务</div>
+          <div class="tile-amount"><a href="/my-task-assignedTo.html">96</a>
           </div>
-        </td>
-        <td class="c-data">
-          <div class="progress">
-            <div class="progress-bar" role="progressbar" aria-valuenow="<?php echo $totalData[$programID]->doneBugs;?>" aria-valuemin="0" aria-valuemax="<?php echo $totalData[$programID]->allBugs;?>" style="width: <?php echo (($totalData[$programID]->doneBugs/$totalData[$progaramID]->allBugs)*100).'%'; ?>">
-            </div>
+        </div>
+        <div class="col-4 tile">
+          <div class="over-title"><i class="icon icon-time"></i> 已消耗工时</div>
+          <div class="tile-amount"><a href="/my-task-assignedTo.html">96</a>
           </div>
-        </td>
-      </tr>
-      <tr>
-        <td></td>
-        <td class="c-data"><?php echo '总预计'.' '.$totalData[$programID]->estimate;?></td>
-        <td class="c-data"><?php echo '无数据';?></td>
-        <td class="c-data"><?php echo "已完成".'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'."剩余".'<br/>'.$totalData[$programID]->doneStories.'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$totalData[$programID]->leftStories;?></td>
-        <td class="c-data"><?php echo "已完成".'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'."剩余".'<br/>'.$totalData[$programID]->doneBugs.'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$totalData[$programID]->leftBugs;?></td>
-      </tr>
-    </tbody>
-  </table>
+          <div class="tile-info">
+          <span class="extra-title">总预计 10</span>
+          </div>
+        </div>
+        <div class="col-4 tile">
+          <div class="over-title"><i class="icon icon-circle"></i> 已花费</div>
+          <div class="tile-amount"><a href="/my-task-assignedTo.html">￥200,00</a>
+          </div>
+          <div class="tile-info">
+          <span class="extra-title">预算 ￥1000,00</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-3 total-div">
+      <div><strong>总需求</strong></div>
+      <div class="tile-amount"><a href="/my-task-assignedTo.html">80</a></div>
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%"></div>
+      </div>
+      <div class="extra-div">
+        <small>已完成</small>
+        <span>0</span>
+      </div>
+      <div class="extra-div">
+        <small>剩余</small>
+        <span>0</span>
+      </div>
+    </div>
+    <div class="col-3 total-div">
+      <div><strong>总Bug</strong></div>
+      <div class="tile-amount"><a href="/my-task-assignedTo.html">80</a></div>
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%"></div>
+      </div>
+      <div class="extra-div">
+        <small>已完成</small>
+        <span>0</span>
+      </div>
+      <div class="extra-div">
+        <small>剩余</small>
+        <span>0</span>
+      </div>
+    </div>
+  </div>
 </div>
 <?php endif;?>
