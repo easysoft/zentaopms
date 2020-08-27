d117275c5baf8d160e52cd3eec54b87eca5f6e15
Yagami <976204163@qq.com>
2020-08-27 13:01:57 +0800

    * Fix group programs.

--- module/group/control.php
+++ module/group/control.php
@@ -285,7 +285,7 @@ class group extends control
         $this->view->position     = $position;
         $this->view->allUsers     = $allUsers;
         $this->view->group        = $group;
-        $this->view->programs     = $this->dao->select('id, name')->from(TABLE_PROJECT)->where('program')->eq(0)->andWhere('deleted')->eq(0)->fetchPairs();
+        $this->view->programs     = $this->dao->select('id, name')->from(TABLE_PROJECT)->where('program')->eq(0)->andWhere('deleted')->eq(0)->andWhere('isCat')->eq(0)->fetchPairs();
         $this->view->deptTree     = $this->loadModel('dept')->getTreeMenu($rooteDeptID = 0, array('deptModel', 'createManagePgmAdminLink'), $groupID);
         $this->view->groupUsers   = $groupUsers;
         $this->view->userPrograms = $userPrograms;
