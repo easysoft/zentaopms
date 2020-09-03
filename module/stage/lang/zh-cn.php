24b8fe642b6d5e39a954ef5e69d7012a55cea361
tianshujie98 <tianshujie0123@163.com>
2020-09-03 17:43:46 +0800

    * Remove spaces.

--- module/stage/lang/zh-cn.php
+++ module/stage/lang/zh-cn.php
@@ -10,20 +10,20 @@
  * @link        http://www.zentao.net
  */
 /* Actions. */
-$lang->stage->browse       = '浏览列表';
-$lang->stage->create       = '新建';
-$lang->stage->batchCreate  = '批量新建';
-$lang->stage->edit         = '编辑';
-$lang->stage->delete       = '删除';
-$lang->stage->view         = '阶段详情';
+$lang->stage->browse      = '浏览列表';
+$lang->stage->create      = '新建';
+$lang->stage->batchCreate = '批量新建';
+$lang->stage->edit        = '编辑';
+$lang->stage->delete      = '删除';
+$lang->stage->view        = '阶段详情';
 
 /* Fields. */
-$lang->stage->common       = '阶段';
-$lang->stage->id           = '编号';
-$lang->stage->name         = '阶段名称';
-$lang->stage->type         = '阶段分类';
-$lang->stage->percent      = '工作量比例';
-$lang->stage->setType      = '设置类型';
+$lang->stage->common  = '阶段';
+$lang->stage->id      = '编号';
+$lang->stage->name    = '阶段名称';
+$lang->stage->type    = '阶段分类';
+$lang->stage->percent = '工作量比例';
+$lang->stage->setType = '设置类型';
 
 $lang->stage->typeList['request'] = '需求';
 $lang->stage->typeList['design']  = '设计';
