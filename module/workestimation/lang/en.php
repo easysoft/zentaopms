d9db4ced519bab6a012c18ba89b40464a160d9df
tianshujie98 <tianshujie0123@163.com>
2020-09-03 17:33:02 +0800

    * Modify English language items.

--- module/workestimation/lang/en.php
+++ module/workestimation/lang/en.php
@@ -1,17 +1,17 @@
 <?php
-$lang->workestimation->common         = 'Workload estimation';
+$lang->workestimation->common         = 'Workload Estimation';
 $lang->workestimation->budget         = 'Estimate';
 $lang->workestimation->index          = 'Overview';
 $lang->workestimation->scale          = 'Scale';
 $lang->workestimation->productivity   = 'Productivity';
-$lang->workestimation->duration       = 'Estimated total man-hour';
+$lang->workestimation->duration       = 'Total Estimation';
 $lang->workestimation->unitLaborCost  = 'Unit Labour cost';
-$lang->workestimation->totalLaborCost = 'Estimate labor costs';
-$lang->workestimation->dayHour        = 'Daily working hours';
+$lang->workestimation->totalLaborCost = 'Estimated Labor Cost';
+$lang->workestimation->dayHour        = 'Daily Man-hours';
 $lang->workestimation->hour           = 'Man-hour';
-$lang->workestimation->consumed       = 'Man-hours consumed';
+$lang->workestimation->consumed       = 'Cost Man-hours';
 
-$lang->workestimation->programScaleTip = "Actual scale of current project：<strong>%s{$lang->hourCommon}</strong> <a href='###' id='useScale' class='btn btn-xs'>use</span>";
+$lang->workestimation->programScaleTip = "Actual scale of current project: <strong>%s{$lang->hourCommon}</strong> <a href='###' id='useScale' class='btn btn-xs'> uses </span>";
 
 $lang->workestimation->placeholder = new stdclass(); 
 $lang->workestimation->placeholder->scale          = '';
