e41293a86f7dd9b755340f2b3415c7a3d03a3373
hufangzhou <746775970@qq.com>
2020-09-17 10:37:57 +0800

    * Finish task#7938.

--- module/block/view/recentprogramblock.html.php
+++ module/block/view/recentprogramblock.html.php
@@ -29,87 +29,90 @@
 #cards .program-stage-item + .program-stage-item:after {content: ' '; display: block; left: -50%; right: 50%; height: 2px; background-color: #D1D1D1; top: 3px; position: absolute; z-index: 0;}
 #cards .program-stage-item.is-going {color: #333;}
 #cards .program-stage-item.is-going::before {background-color: #0C64EB;}
+.block-recentprogram .panel-body{padding: 0;}
 </style>
-<div class='row' id='cards'>
-  <?php foreach ($programs as $programID => $program):?>
-  <div class='col' data-id='<?php echo $programID?>'>
-    <div class='panel' data-url='<?php echo $this->createLink('program', 'index', "programID=$program->id");?>'>
-      <div class='panel-heading'>
-        <?php $parentName = $program->parentName ? $program->parentName . '/' : '';?>
-        <strong class='program-name' title='<?php echo $parentName . $program->name;?>'> <?php echo html::a($this->createLink('program', 'index', "programID=$program->id", '', '', $program->id), $parentName . $program->name);?> </strong>
-        <?php if($program->model === 'waterfall'): ?>
-        <span class='program-type-label label label-warning label-outline'><?php echo $lang->program->waterfall; ?></span>
-        <?php else: ?>
-        <span class='program-type-label label label-info label-outline'><?php echo $lang->program->scrum; ?></span>
-        <?php endif; ?>
-        <nav class='panel-actions nav nav-default'>
-          <li class='dropdown'>
-            <a href='javascript:;' data-toggle='dropdown' class='panel-action'><i class='icon icon-ellipsis-v'></i></a>
-            <ul class='dropdown-menu pull-right'>
-              <li><?php common::printIcon('program', 'group', "programID=$program->id", $program, 'button', 'group');?></li>
-              <li><?php common::printIcon('program', 'manageMembers', "programID=$program->id", $program, 'button', 'persons');?></li>
-              <li><?php common::printicon('program', 'activate', "programid=$program->id", $program, 'button', '', '', 'iframe', true);?></li>
-              <li><?php if(common::hasPriv('program', 'edit')) echo html::a($this->createLink("program", "edit", "programID=$program->id"), "<i class='icon-edit'></i> " . $lang->edit, '', "");?></li>
-              <li><?php common::printIcon('program', 'start',   "programID=$program->id", $program, 'button', '', '', 'iframe', true);?></li>
-              <li><?php common::printIcon('program', 'suspend', "programID=$program->id", $program, 'button', '', '', 'iframe', true);?></li>
-              <li><?php common::printIcon('program', 'close',   "programID=$program->id", $program, 'button', '', '', 'iframe', true);?></li>
-              <li><?php if(common::hasPriv('program', 'delete'))  echo html::a($this->createLink("program", "delete", "programID=$program->id"), "<i class='icon-trash'></i> " . $lang->delete, 'hiddenwin', "");?></li>
-            </ul>
-          </li>
-        </nav>
-      </div>
-      <div class='panel-body'>
-        <div class='program-infos'>
-          <span><i class='icon icon-group'></i> <?php printf($lang->program->membersUnit, $program->teamCount); ?></span>
-          <span><i class='icon icon-clock'></i> <?php printf($lang->program->hoursUnit, $program->estimate); ?></span>
-          <span><i class='icon icon-cost'></i> <?php echo $program->budget . '' . zget($lang->program->unitList, $program->budgetUnit);?></span>
-        </div>
-        <?php if($program->model === 'waterfall'): ?>
-        <div class='program-detail program-stages'>
-          <p class='text-muted'><?php echo $lang->program->ongoingStage; ?></p>
-          <?php
-          $programProjects = array();
-          foreach($program->projects as $project)
-          {   
-              if(!$project->parent) $programProjects[] = $project;
-          }   
-          ?>  
-          <?php if(empty($programProjects)): ?>
-          <div class='label label-outline'><?php echo zget($lang->project->statusList, $program->status);?></div>
+<div class="panel-body">
+  <div class='row' id='cards'>
+    <?php foreach ($programs as $programID => $program):?>
+    <div class='col' data-id='<?php echo $programID?>'>
+      <div class='panel' data-url='<?php echo $this->createLink('program', 'index', "programID=$program->id");?>'>
+        <div class='panel-heading'>
+          <?php $parentName = $program->parentName ? $program->parentName . '/' : '';?>
+          <strong class='program-name' title='<?php echo $parentName . $program->name;?>'> <?php echo html::a($this->createLink('program', 'index', "programID=$program->id", '', '', $program->id), $parentName . $program->name);?> </strong>
+          <?php if($program->model === 'waterfall'): ?>
+          <span class='program-type-label label label-warning label-outline'><?php echo $lang->program->waterfall; ?></span>
           <?php else: ?>
-          <div class='program-stages-container scrollbar-hover'>
-            <div class='program-stages-row'>
-              <?php foreach ($programProjects as $project): ?>
-              <div class='program-stage-item is-<?php echo $project->status;?><?php if($project->status !== 'wait') echo ' is-going'; ?>'>
-                <div><?php echo $project->name; ?></div>
+          <span class='program-type-label label label-info label-outline'><?php echo $lang->program->scrum; ?></span>
+          <?php endif; ?>
+          <nav class='panel-actions nav nav-default'>
+            <li class='dropdown'>
+              <a href='javascript:;' data-toggle='dropdown' class='panel-action'><i class='icon icon-ellipsis-v'></i></a>
+              <ul class='dropdown-menu pull-right'>
+                <li><?php common::printIcon('program', 'group', "programID=$program->id", $program, 'button', 'group');?></li>
+                <li><?php common::printIcon('program', 'manageMembers', "programID=$program->id", $program, 'button', 'persons');?></li>
+                <li><?php common::printicon('program', 'activate', "programid=$program->id", $program, 'button', '', '', 'iframe', true);?></li>
+                <li><?php if(common::hasPriv('program', 'edit')) echo html::a($this->createLink("program", "edit", "programID=$program->id"), "<i class='icon-edit'></i> " . $lang->edit, '', "");?></li>
+                <li><?php common::printIcon('program', 'start',   "programID=$program->id", $program, 'button', '', '', 'iframe', true);?></li>
+                <li><?php common::printIcon('program', 'suspend', "programID=$program->id", $program, 'button', '', '', 'iframe', true);?></li>
+                <li><?php common::printIcon('program', 'close',   "programID=$program->id", $program, 'button', '', '', 'iframe', true);?></li>
+                <li><?php if(common::hasPriv('program', 'delete'))  echo html::a($this->createLink("program", "delete", "programID=$program->id"), "<i class='icon-trash'></i> " . $lang->delete, 'hiddenwin', "");?></li>
+              </ul>
+            </li>
+          </nav>
+        </div>
+        <div class='panel-body'>
+          <div class='program-infos'>
+            <span><i class='icon icon-group'></i> <?php printf($lang->program->membersUnit, $program->teamCount); ?></span>
+            <span><i class='icon icon-clock'></i> <?php printf($lang->program->hoursUnit, $program->estimate); ?></span>
+            <span><i class='icon icon-cost'></i> <?php echo $program->budget . '' . zget($lang->program->unitList, $program->budgetUnit);?></span>
+          </div>
+          <?php if($program->model === 'waterfall'): ?>
+          <div class='program-detail program-stages'>
+            <p class='text-muted'><?php echo $lang->program->ongoingStage; ?></p>
+            <?php
+            $programProjects = array();
+            foreach($program->projects as $project)
+            {   
+                if(!$project->parent) $programProjects[] = $project;
+            }   
+            ?>  
+            <?php if(empty($programProjects)): ?>
+            <div class='label label-outline'><?php echo zget($lang->project->statusList, $program->status);?></div>
+            <?php else: ?>
+            <div class='program-stages-container scrollbar-hover'>
+              <div class='program-stages-row'>
+                <?php foreach ($programProjects as $project): ?>
+                <div class='program-stage-item is-<?php echo $project->status;?><?php if($project->status !== 'wait') echo ' is-going'; ?>'>
+                  <div><?php echo $project->name; ?></div>
+                </div>
+                <?php endforeach; ?>
               </div>
-              <?php endforeach; ?>
             </div>
+            <?php endif; ?>
+          </div>
+          <?php else: ?>
+          <?php $project = $program->projects ? current($program->projects) : '';?>
+          <div class='program-detail program-iteration'>
+            <p class='text-muted'><?php echo $lang->program->lastIteration; ?></p>
+            <?php if($project):?>
+            <div class='row'>
+              <div class='col-xs-5'><?php echo $project->name;?></div>
+              <div class='col-xs-7'>
+              <div class="progress progress-text-left">
+                <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="<?php echo $project->hours->progress;?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $project->hours->progress;?>%">
+                <span class="progress-text"><?php echo $project->hours->progress;?>%</span>
+                </div>
+              </div>
+              </div>
+            </div>
+            <?php endif; ?>
           </div>
           <?php endif; ?>
         </div>
-        <?php else: ?>
-        <?php $project = $program->projects ? current($program->projects) : '';?>
-        <div class='program-detail program-iteration'>
-          <p class='text-muted'><?php echo $lang->program->lastIteration; ?></p>
-          <?php if($project):?>
-          <div class='row'>
-            <div class='col-xs-5'><?php echo $project->name;?></div>
-            <div class='col-xs-7'>
-            <div class="progress progress-text-left">
-              <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="<?php echo $project->hours->progress;?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $project->hours->progress;?>%">
-              <span class="progress-text"><?php echo $project->hours->progress;?>%</span>
-              </div>
-            </div>
-            </div>
-          </div>
-          <?php endif; ?>
-        </div>
-        <?php endif; ?>
       </div>
     </div>
+    <?php endforeach;?>
   </div>
-  <?php endforeach;?>
 </div>
 <script>
 /* Auto resize stages */
