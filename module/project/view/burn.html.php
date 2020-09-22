c321a1181c92a20bbc8aa388198a46cd8561eaa1
hufangzhou <746775970@qq.com>
2020-09-22 10:09:49 +0800

    * Adjust the display of the burn chart.

--- module/project/view/burn.html.php
+++ module/project/view/burn.html.php
@@ -48,6 +48,7 @@
       <canvas id="burnCanvas"></canvas>
     </div>
     <div id="burnYUnit"><?php echo $burnBy == 'storyPoint' ?  "({$lang->project->storyPoint})" : "({$lang->project->workHour})";?></div>
+    <div id="burnXUnit"><?php echo $lang->project->burnXUnit;?></div>
     <div id="burnLegend">
       <div class="line-ref"><div class='barline'></div><?php echo $lang->project->charts->burn->graph->reference;?></div>
       <div class="line-real"><div class='barline bg-primary'></div><?php echo $lang->project->charts->burn->graph->actuality;?></div>
