719db21e0cfb90e31128408b9d8c30a057304984
leiyong <1549684884@qq.com>
2020-09-28 17:17:57 +0800

    * Adjust code .

--- module/story/model.php
+++ module/story/model.php
@@ -2392,7 +2392,7 @@ class storyModel extends model
             ->leftJoin(TABLE_PRODUCT)->alias('t2')->on('t1.product = t2.id')
             ->where('t1.deleted')->eq(0)
             ->andWhere('t1.type')->eq($storyType)
-            ->beginIF($programID)->andWhere('t1.program')->eq($programID)->fi()
+            ->beginIF($programID)->andWhere('t1.PRJ')->eq($programID)->fi()
             ->beginIF($type != 'closedBy' and $this->app->moduleName == 'block')->andWhere('t1.status')->ne('closed')->fi()
             ->beginIF($type != 'all')
             ->beginIF($type == 'assignedTo')->andWhere('assignedTo')->eq($account)->fi()
