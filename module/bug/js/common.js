0fe2cd2e96e5003d13ba53a48e583474c9d28cdf
tianshujie98 <tianshujie0123@163.com>
2020-09-28 16:32:59 +0800

    * Unified format.

--- module/bug/js/common.js
+++ module/bug/js/common.js
@@ -206,7 +206,7 @@ function loadAllProductBuilds(productID, buildBox)
         }
         if(buildBox == 'resolvedBuildBox')
         {
-            link = createLink('build', 'ajaxGetProductBuilds', 'productID=' + productID + '&varName=resolvedBuild&build=' + oldResolvedBuild + '&branch' + branch);
+            link = createLink('build', 'ajaxGetProductBuilds', 'productID=' + productID + '&varName=resolvedBuild&build=' + oldResolvedBuild + '&branch' + branch + '&index=0&type=all');
             $('#resolvedBuildBox').load(link, function(){$(this).find('select').chosen()});
         }
     }
