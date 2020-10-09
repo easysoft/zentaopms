1af3353ae1ab81ac084949858c83c0623198b67d
z <wyd621@163.com>
2020-10-09 11:02:01 +0800

    * adjust notice.

--- module/common/model.php
+++ module/common/model.php
@@ -1877,8 +1877,8 @@ EOD;
      *
      * @param  string       $url
      * @param  string|array $data
-     * @param  bool         $optHeader
-     * @param  string       $userPWD
+     * @param  array        $options   This is option and value pair, like CURLOPT_HEADER => true. Use curl_setopt function to set options.
+     * @param  array        $headers   Set request headers.
      * @static
      * @access public
      * @return string
@@ -1889,6 +1889,9 @@ EOD;
         if(!extension_loaded('curl')) return json_encode(array('result' => 'fail', 'message' => $lang->error->noCurlExt));
 
         commonModel::$requestErrors = array();
+
+        if(!is_array($headers)) $headers = (array)$headers;
+        $headers[] = "API-RemoteIP: " . zget($_SERVER, 'REMOTE_ADDR', '');
 
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
@@ -1902,17 +1905,19 @@ EOD;
         curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
         curl_setopt($curl, CURLOPT_HEADER, FALSE);
         curl_setopt($curl, CURLINFO_HEADER_OUT, TRUE);
-
-        $headers[] = "API-RemoteIP: " . $_SERVER['REMOTE_ADDR'];
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($curl, CURLOPT_URL, $url);
+
         if(!empty($data))
         {
             curl_setopt($curl, CURLOPT_POST, true);
             curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
         }
 
-        foreach($options as $option => $value) curl_setopt($curl, $option, $value);
+        if($options)
+        {
+            foreach($options as $option => $value) curl_setopt($curl, $option, $value);
+        }
 
         $response = curl_exec($curl);
         $errors   = curl_error($curl);
