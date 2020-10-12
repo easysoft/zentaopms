0001012632ec3602b8634f4ea51169389eb692f8
hufangzhou <746775970@qq.com>
2020-10-12 16:10:28 +0800

    * Finish task#8156: Optimize the structure of the code.

--- module/project/js/common.js
+++ module/project/js/common.js
@@ -239,29 +239,6 @@ $(function()
         e.preventDefault();
     });
 
-    /* Assign value to the manage products by the different request type.*/
-    if(window.config.currentMethod === 'create')
-    {
-        var url = window.location.href;
-        if(window.config.requestType === 'GET')
-        {
-            var paramStr  = url.split('?')[1];
-            var paramArr  = paramStr.split('&');
-            var productID = paramArr[2] ? paramArr[2].split('=')[1] : 0;
-        }
-        else
-        {
-            var urlArr    = url.split('/');
-            var paramStr  = urlArr[urlArr.length-1];
-            var paramArr  = paramStr.split('-');
-            var productID = paramArr[2] ? paramArr[2].split('.')[0] : 0;
-        }
-        var product = $('#products0');
-        $(product).val(productID);
-        $(product).trigger("chosen:updated");
-        loadBranches($(product));
-    }
-
     adjustProductBoxMargin();
     adjustPlanBoxMargin();
 });
