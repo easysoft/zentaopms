7a1add17e8089e5dde49fc3e1f9795b055ee16bc
z <wyd621@163.com>
2020-10-12 17:14:48 +0800

    * remove repeated repo sql.

--- db/zentao.sql
+++ db/zentao.sql
@@ -1129,63 +1129,6 @@ CREATE TABLE IF NOT EXISTS `zt_relation` (
   UNIQUE KEY `relation` (`relation`,`AType`,`BType`, `AID`, `BID`)
 ) ENGINE='MyISAM' DEFAULT CHARSET=utf8;
 
--- DROP TABLE IF EXISTS `zt_repo`;
-CREATE TABLE IF NOT EXISTS `zt_repo` (
-  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `path` varchar(255) NOT NULL,
-  `prefix` varchar(100) NOT NULL,
-  `encoding` varchar(20) NOT NULL,
-  `SCM` varchar(10) NOT NULL,
-  `client` varchar(100) NOT NULL,
-  `commits` mediumint(8) unsigned NOT NULL,
-  `account` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `encrypt` varchar(30) NOT NULL DEFAULT 'plain',
-  `acl` text NOT NULL,
-  `synced` tinyint(1) NOT NULL DEFAULT '0',
-  `lastSync` datetime NOT NULL,
-  `desc` TEXT NULL,
-  `deleted` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- DROP TABLE IF EXISTS `zt_repobranch`;
-CREATE TABLE IF NOT EXISTS `zt_repobranch` (
-  `repo` mediumint(8) unsigned NOT NULL,
-  `revision` mediumint(8) unsigned NOT NULL,
-  `branch` varchar(255) NOT NULL,
-  UNIQUE KEY `repo_revision_branch` (`repo`,`revision`,`branch`),
-  KEY `branch` (`branch`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- DROP TABLE IF EXISTS `zt_repohistory`;
-CREATE TABLE IF NOT EXISTS `zt_repohistory` (
-  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
-  `repo` mediumint(9) NOT NULL,
-  `revision` varchar(40) NOT NULL,
-  `commit` mediumint(8) unsigned NOT NULL,
-  `comment` text NOT NULL,
-  `committer` varchar(100) NOT NULL,
-  `time` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `repo` (`repo`,`revision`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- DROP TABLE IF EXISTS `zt_repofiles`;
-CREATE TABLE IF NOT EXISTS `zt_repofiles` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `repo` mediumint(8) unsigned NOT NULL,
-  `revision` mediumint(8) unsigned NOT NULL,
-  `path` varchar(255) NOT NULL,
-  `parent` varchar(255) NOT NULL,
-  `type` varchar(20) NOT NULL,
-  `action` char(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `path` (`path`),
-  KEY `parent` (`parent`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
 INSERT INTO `zt_cron` (`m`, `h`, `dom`, `mon`, `dow`, `command`, `remark`, `type`, `buildin`, `status`, `lastTime`) VALUES
 ('*',    '*',    '*',    '*',    '*',    '', '监控定时任务', 'zentao', 1, 'normal',   '0000-00-00 00:00:00'),
 ('30',   '23',   '*',    '*',    '*',    'moduleName=project&methodName=computeburn', '更新燃尽图',      'zentao', 1, 'normal', '0000-00-00 00:00:00'),
