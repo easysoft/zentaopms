35ecdc8290398a19e8def911585b5018b44f335c
holan20180123 <56391770@qq.com>
2020-10-21 09:31:28 +0800

    * Remove duplicate query code.

--- module/common/model.php
+++ module/common/model.php
@@ -1464,7 +1464,6 @@ EOD;
 
         if(isset($this->app->user))
         {
-            if(!defined('IN_UPGRADE')) $this->session->user->view = $this->loadModel('user')->grantUserView();
             $this->app->user = $this->session->user;
 
             if(!commonModel::hasPriv($module, $method)) $this->deny($module, $method);
