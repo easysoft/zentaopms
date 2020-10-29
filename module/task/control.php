bd59b4a92042659836a7df15163d3f1d6f951322
z <wyd621@163.com>
2020-10-29 13:24:16 +0800

    * finish task #12974.

--- module/task/control.php
+++ module/task/control.php
@@ -45,6 +45,9 @@ class task extends control
      */
     public function create($projectID = 0, $storyID = 0, $moduleID = 0, $taskID = 0, $todoID = 0)
     {
+        $projects  = $this->project->getPairs();
+        $projectID = $this->project->saveState($projectID, $projects);
+
         $this->project->getLimitedProject();
         $limitedProjects = !empty($_SESSION['limitedProjects']) ? $_SESSION['limitedProjects'] : '';
         if(strpos(",{$limitedProjects},", ",$projectID,") !== false)
@@ -85,7 +88,7 @@ class task extends control
         $storyLink = $this->session->storyList ? $this->session->storyList : $this->createLink('project', 'story', "projectID=$projectID");
 
         /* Set menu. */
-        $this->project->setMenu($this->project->getPairs(), $project->id);
+        $this->project->setMenu($projects, $project->id);
 
         if(!empty($_POST))
         {
