770309794d98419b44dfbf2397f337c8dc8155d0
tianshujie98 <tianshujie0123@163.com>
2020-11-04 10:12:49 +0800

    * Add comment.

--- module/doc/control.php
+++ module/doc/control.php
@@ -432,6 +432,7 @@ class doc extends control
      * View a doc.
      *
      * @param  int    $docID
+     * @param  int    $version
      * @access public
      * @return void
      */
@@ -445,6 +446,7 @@ class doc extends control
         {
             $hyperdown    = $this->app->loadClass('hyperdown');
             $doc->content = $hyperdown->makeHtml($doc->content);
+            /* Add a newline attribute to the table. */
             $doc->content = str_replace("<th>", "<th style='word-break: break-word;'>", $doc->content);
             $doc->content = str_replace("<td>", "<td style='word-break: break-word;'>", $doc->content);
             $doc->digest  = $hyperdown->makeHtml($doc->digest);
