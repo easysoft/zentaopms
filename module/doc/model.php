1db241cb431ddafdd98419c91e89d3765c44527f
tianshujie98 <tianshujie0123@163.com>
2020-11-05 15:30:33 +0800

    * Fix bug.

--- module/doc/model.php
+++ module/doc/model.php
@@ -619,7 +619,7 @@ class docModel extends model
             ->join('groups', ',')
             ->join('users', ',')
             ->join('mailto', ',')
-            ->remove('files,labels,uid')
+            ->remove('files,labels,uid,contactListMenu')
             ->get();
 
         /* Fix bug #2929. strip_tags($this->post->contentMarkdown, $this->config->allowedTags)*/
