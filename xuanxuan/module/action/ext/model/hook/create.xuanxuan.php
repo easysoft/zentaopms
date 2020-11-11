aa56f5567ff9676bed1f0a370ea5ff01d9242da7
Catouse <catouse@gmail.com>
2020-11-11 14:53:37 +0800

    * task #8244, bug #3543, fix recording wrong user ip on user login.

--- xuanxuan/module/action/ext/model/hook/create.xuanxuan.php
+++ xuanxuan/module/action/ext/model/hook/create.xuanxuan.php
@@ -1,7 +1,7 @@
 <?php
 if(strtolower($actionType) == 'reconnectxuanxuan' or strtolower($actionType) == 'loginxuanxuan')
 {
-    $ip   = $this->server->remote_addr;
+    $ip   = helper::getRemoteIp();
     $last = $this->server->request_time;
     $this->dao->update(TABLE_USER)->set('visits = visits + 1')->set('ip')->eq($ip)->set('last')->eq($last)->where('account')->eq($actor)->exec();
 }
