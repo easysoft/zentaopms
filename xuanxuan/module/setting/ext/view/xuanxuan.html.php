025e1e039dabfe7489eda7fc5e152d70a5ae2034
z <wyd621@163.com>
2020-11-18 16:37:13 +0800

    * adjust for setting xuanxuan.

--- xuanxuan/module/setting/ext/view/xuanxuan.html.php
+++ xuanxuan/module/setting/ext/view/xuanxuan.html.php
@@ -11,7 +11,7 @@
  */
 ?>
 <?php include '../../../common/view/header.html.php';?>
-<?php $debug = zget($config->xuanxuan, 'debug', 0);?>
+<?php $this->app->loadLang('client')?>
 <div id="mainMenu" class="clearfix">
   <div class="btn-toolbar pull-left"><?php common::printAdminSubMenu('xuanxuan');?></div>
 </div>
@@ -25,35 +25,44 @@
     <form method='post' id='ajaxForm' class='form-ajax'>
       <table class='table table-form'>
         <tr>
+          <th class='w-120px'><?php echo $lang->im->version;?></th>
+          <td class='w-p30'><?php echo $config->xuanxuan->version;?></td>
+        </tr>
+        <tr>
           <th class='w-120px'><?php echo $lang->im->turnon;?></th>
           <td class='w-p30'><?php echo $type == 'edit' ? html::radio('turnon', $lang->im->turnonList, $turnon) : zget($lang->im->turnonList, $config->xuanxuan->turnon);?></td>
           <td></td>
         </tr>
         <tr>
-          <th><?php echo $lang->im->version;?></th>
-          <td><?php echo $config->xuanxuan->version;?></td>
-        </tr>
-        <tr>
           <th><?php echo $lang->im->key;?></th>
-          <td>
-            <?php $key = zget($config->xuanxuan, 'key', '');?>
-            <?php echo $type == 'edit' ? html::input('key', $key, "class='form-control' readonly='readonly'") : $key;?>
+          <td class='code'>
+            <?php $key   = zget($config->xuanxuan, 'key', '');?>
+            <?php $style = (!$key or $key == str_repeat(8, 32)) ? "style='margin-bottom: 0px; border-color: rgb(149, 59, 57);'" : '';?>
+            <?php echo $type == 'edit' ? html::input('key', zget($config->xuanxuan, 'key', ''), "class='form-control' readonly='readonly' $style") : zget($config->xuanxuan, 'key', '');?>
+            <?php if($type == 'edit' && (!$key or $key == str_repeat(8, 32))):?>
+            <span id="keyLabel" for="key" class="text-error red"><?php echo !$key ? $lang->im->errorKey : $lang->im->defaultKey;?></span>
+            <?php endif;?>
           </td>
           <td><?php echo $type == 'edit' ? html::a('javascript:void(0)', $lang->im->createKey, '', 'onclick="createKey()"') : '';?></td>
         </tr>
         <tr>
-          <th><?php echo $lang->im->debug;?></th>
-          <td><?php echo $type == 'edit' ? html::radio('debug', $lang->im->debugStatus, $debug) : zget($lang->im->debugStatus, $debug);?></td>
+          <th><?php echo $lang->im->backendLang;?></th>
+          <td><?php echo $type == 'edit' ? html::select('backendLang', $config->langs, $config->xuanxuan->backendLang, "class='form-control'") : zget($config->langs, $config->xuanxuan->backendLang, '');?></td>
           <td></td>
         </tr>
         <tr>
           <th><?php echo $lang->im->xxdServer;?></th>
           <td><?php echo $type == 'edit' ? html::input('server', $domain, "class='form-control'") : $domain;?></td>
-          <td><?php if($type == 'edit') echo $lang->im->xxdServerTip;?></td>
+          <td class='text-muted'><?php if($type == 'edit') echo $lang->im->xxdServerTip;?></td>
+        </tr>
+        <tr>
+          <th><?php echo $lang->im->pollingInterval;?></th>
+          <td><?php echo $type == 'edit' ? html::input('pollingInterval', zget($config->xuanxuan, 'pollingInterval', 60), " class='form-control'") : zget($config->xuanxuan, 'pollingInterval', 60) . $lang->im->secs;?></td>
+          <td class='text-muted'><?php if($type == 'edit') echo $lang->im->xxdPollIntTip;?></td>
         </tr>
         <tr>
           <th><?php echo $lang->im->xxd->ip;?></th>
-          <td><?php echo $type == 'edit' ? html::input('ip', zget($config->xuanxuan, 'ip', '0.0.0.0'), "class='form-control' placeholder='{$lang->im->placeholder->xxd->ip}'") : zget($config->xuanxuan, 'ip', '0.0.0.0');?></td>
+          <td class='code'><?php echo $type == 'edit' ? html::input('ip', zget($config->xuanxuan, 'ip', '0.0.0.0'), "class='form-control' placeholder='{$lang->im->placeholder->xxd->ip}'") : zget($config->xuanxuan, 'ip', '0.0.0.0');?></td>
           <td></td>
         </tr>
         <tr>
@@ -81,22 +90,22 @@
           </td>
           <td></td>
         </tr>
-        <?php if($type == 'edit'):?>
         <tr>
-          <th><?php echo $lang->im->xxdIsHttps;?></th>
+          <th><?php echo $lang->im->xxd->aes;?></th>
           <td>
-            <?php echo html::hidden('https', $https);?>
-            <?php echo $type ? html::radio('https', $lang->im->httpsOptions, $https, "class='checkbox'") : $lang->im->httpsOptions[$https];?>
+            <?php $aes = zget($config->xuanxuan, 'aes', 'on');?>
+            <?php echo $type == 'edit' ? html::radio('aes', $lang->im->aesOptions, $aes, "class='checkbox'") : zget($lang->im->aesOptions, $aes);?>
+          </td>
+          <td class='text-muted'><?php if($type == 'edit') echo $lang->im->xxdAESTip;?></td>
+        </tr>
+        <tr>
+          <th><?php echo $lang->im->xxd->https;?></th>
+          <td>
+            <?php $https = zget($config->xuanxuan, 'https', 'off');?>
+            <?php echo $type == 'edit' ? html::radio('https', $lang->im->httpsOptions, $https, "class='checkbox'") : zget($lang->im->httpsOptions, $https);?>
           </td>
           <td></td>
         </tr>
-        <?php else:?>
-        <tr>
-          <th><?php echo $lang->im->xxdIsHttps;?></th>
-          <td><?php echo zget($lang->im->httpsOptions, $https, '');?> </td>
-          <td></td>
-        </tr>
-        <?php endif;?>
         <tr class='sslTR <?php if($https == 'off' || empty($type)) echo 'hide';?>'>
           <th><?php echo $lang->im->xxd->sslcrt;?></th>
           <td><?php echo html::textarea('sslcrt',  zget($config->xuanxuan, 'sslcrt', ''), "placeholder='{$lang->im->placeholder->xxd->sslcrt}' class='form-control'");?></td>
@@ -107,7 +116,15 @@
           <td><?php echo html::textarea('sslkey',  zget($config->xuanxuan, 'sslkey', ''), "placeholder='{$lang->im->placeholder->xxd->sslkey}' class='form-control'");?></td>
           <td></td>
         </tr>
-        <?php if(!$type):?>
+        <tr>
+          <th><?php echo $lang->im->debug;?></th>
+          <td>
+            <?php $debug = zget($config->xuanxuan, 'debug', 0);?>
+            <?php echo $type == 'edit' ? html::radio('debug', $lang->im->debugStatus, $debug) : zget($lang->im->debugStatus, $debug);?>
+          </td>
+          <td></td>
+        </tr>
+        <?php if($type != 'edit'):?>
         <tr>
           <th><?php echo $lang->im->xxd->os;?></th>
           <td><?php echo html::select('os', $lang->im->osList, zget($config->xuanxuan, $os), "class='form-control chosen'");?></td>
