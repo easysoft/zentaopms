d5f7193f3f969947ca764acf69e15e903108a024
hufangzhou <746775970@qq.com>
2020-11-27 16:16:32 +0800

    * Modify the language file.

--- module/my/lang/zh-tw.php
+++ module/my/lang/zh-tw.php
@@ -27,12 +27,10 @@ $lang->my->score          = '我的積分';
 $lang->my->scoreRule      = '積分規則';
 $lang->my->noTodo         = '暫時沒有待辦。';
 
-$lang->my->myExecutions  = "我參與的階段/{$lang->projectCommon}/看板";
-$lang->my->name          = '名稱';
-$lang->my->code          = '代號';
-$lang->my->projects      = '項目';
-$lang->my->myRequirement = '用戶需求或史詩';
-$lang->my->myStory       = '軟件需求或故事';
+$lang->my->myExecutions = "我參與的階段/{$lang->projectCommon}/看板";
+$lang->my->name         = '名稱';
+$lang->my->code         = '代號';
+$lang->my->projects     = '項目';
 
 $lang->my->taskMenu = new stdclass();
 $lang->my->taskMenu->assignedToMe = '指派給我';
