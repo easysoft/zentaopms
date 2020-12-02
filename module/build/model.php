c4a2463e14a627d91a885c88145b34a2c3dc02b5
tianshujie98 <tianshujie0123@163.com>
2020-12-02 10:30:26 +0800

    * Fix bug.

--- module/build/model.php
+++ module/build/model.php
@@ -138,20 +138,12 @@ class buildModel extends model
         /* If there are saved query conditions, reset the session. */
         if((int)$queryID)
         {
-            $buildQuery = $this->loadModel('search')->getQuery($queryID);
-            if($buildQuery)
+            $query = $this->loadModel('search')->getQuery($queryID);
+            if($query)
             {
                 $this->session->set('projectBuildQuery', $buildQuery->sql);
                 $this->session->set('projectBuildForm', $buildQuery->form);
             }
-            else
-            {
-                $this->session->set('projectBuildQuery', ' 1 = 1');
-            }
-        }
-        else
-        {
-             if($this->session->projectBuildQuery == false) $this->session->set('projectBuildQuery', ' 1 = 1');
         }
 
         $buildQuery = $this->session->projectBuildQuery;
