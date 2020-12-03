e1e314c297a649663dbe536570db182dfe858aa8
holan20180123 <56391770@qq.com>
2020-12-03 13:24:46 +0800

    * Fix bug #3668.

--- module/testcase/control.php
+++ module/testcase/control.php
@@ -1243,9 +1243,10 @@ class testcase extends control
                         $childId ++;
                     }
                 }
-                $case->stepDesc   = trim($case->stepDesc);
-                $case->stepExpect = trim($case->stepExpect);
-                $case->real       = trim($case->real);
+                $case->stepDesc     = trim($case->stepDesc);
+                $case->stepExpect   = trim($case->stepExpect);
+                $case->real         = trim($case->real);
+                $case->precondition = html_entity_decode($case->precondition, ENT_QUOTES);
 
                 if($this->post->fileType == 'csv')
                 {
