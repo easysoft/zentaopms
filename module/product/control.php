81807b4491853e64dcb5dd7d55a2dcfaa626b465
qiyu-xie <qiyu_xie@163.com>
2020-12-04 15:53:01 +0800

    * Modify select product of second navigation under product.

--- module/product/control.php
+++ module/product/control.php
@@ -689,7 +689,8 @@ class product extends control
      */
     public function ajaxGetDropMenu($productID, $module, $method, $extra)
     {
-        $products = ($module == 'product') ? $this->product->getList() : $this->product->getProductsByProject($this->session->PRJ);
+        $inProduct = zget($this->lang->navGroup, $module) == 'product';
+        $products = $inProduct ? $this->product->getList() : $this->product->getProductsByProject($this->session->PRJ);
 
         $this->view->link      = $this->product->getProductLink($module, $method, $extra);
         $this->view->productID = $productID;
