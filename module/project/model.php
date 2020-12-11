e63b3cdce0c1caa26909536f36f8a57b4cf00373
tianshujie98 <tianshujie0123@163.com>
2020-12-11 10:34:50 +0800

    * Adjust code style.

--- module/project/model.php
+++ module/project/model.php
@@ -2967,8 +2967,8 @@ class projectModel extends model
         $this->loadModel('build');
 
         /* Set search param. */
-        if($type == 'execution') $this->config->build->search['module']    = 'executionBuild';
-        if($type == 'project')   $this->config->build->search['module']    = 'projectBuild';
+        if($type == 'execution') $this->config->build->search['module'] = 'executionBuild';
+        if($type == 'project')   $this->config->build->search['module'] = 'projectBuild';
         $this->config->build->search['actionURL'] = $actionURL;
         $this->config->build->search['queryID']   = $queryID;
         $this->config->build->search['params']['product']['values'] = $products;
