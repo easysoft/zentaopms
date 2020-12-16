dbcc5c949b55f1a6be69c7412e02977c5735e197
hufangzhou <746775970@qq.com>
2020-12-16 08:25:44 +0800

    * Adjust the code.

--- module/program/view/pgmproduct.html.php
+++ module/program/view/pgmproduct.html.php
@@ -30,8 +30,8 @@
       <table id="productList" class="table has-sort-head table-fixed">
         <?php $vars = "programID=$program->id&browseType=$browseType&orderBy=%s&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}&pageID={$pager->pageID}";?>
         <thead>
-          <tr>
-            <th class='c-id'>
+          <tr class="text-center">
+            <th class='c-id text-left'>
               <?php if($canBatchEdit):?>
                 <div class="checkbox-primary check-all" title="<?php echo $lang->selectAll;?>">
                   <label></label>
@@ -39,25 +39,25 @@
               <?php endif;?>
               <?php common::printOrderLink('id', $orderBy, $vars, $lang->idAB);?>
             </th>
-            <th><?php common::printOrderLink('name', $orderBy, $vars, $lang->product->name);?></th>
-            <th class='w-200px' title='<?php echo $lang->product->program;?>'><?php echo $lang->product->program;?></th>
-            <th class='w-120px text-center' title='<?php echo $lang->product->activeStoriesTitle;?>'><?php echo $lang->product->activeStories;?></th>
-            <th class='w-120px text-center' title='<?php echo $lang->product->changedStoriesTitle;?>'><?php echo $lang->product->changedStories;?></th>
-            <th class='w-100px text-center' title='<?php echo $lang->product->draftStoriesTitle;?>'><?php echo $lang->product->draftStories;?></th>
-            <th class='w-120px text-center' title='<?php echo $lang->product->closedStoriesTitle;?>'><?php echo $lang->product->closedStories;?></th>
-            <th class='w-70px text-center' title='<?php echo $lang->product->plans;?>'><?php echo $lang->product->plans;?></th>
-            <th class='w-70px text-center' title='<?php echo $lang->product->releases;?>'><?php echo $lang->product->releases;?></th>
-            <th class='w-80px text-center' title='<?php echo $lang->product->unResolvedBugsTitle;?>'><?php echo $lang->product->unResolvedBugs;?></th>
-            <th class='w-110px text-center' title='<?php echo $lang->product->assignToNullBugsTitle;?>'><?php echo $lang->product->assignToNullBugs;?></th>
+            <th class="text-left"><?php common::printOrderLink('name', $orderBy, $vars, $lang->product->name);?></th>
+            <th class='w-200px text-left' title='<?php echo $lang->product->program;?>'><?php echo $lang->product->program;?></th>
+            <th class='w-120px' title='<?php echo $lang->product->activeStoriesTitle;?>'><?php echo $lang->product->activeStories;?></th>
+            <th class='w-120px' title='<?php echo $lang->product->changedStoriesTitle;?>'><?php echo $lang->product->changedStories;?></th>
+            <th class='w-100px' title='<?php echo $lang->product->draftStoriesTitle;?>'><?php echo $lang->product->draftStories;?></th>
+            <th class='w-120px' title='<?php echo $lang->product->closedStoriesTitle;?>'><?php echo $lang->product->closedStories;?></th>
+            <th class='w-70px' title='<?php echo $lang->product->plans;?>'><?php echo $lang->product->plans;?></th>
+            <th class='w-70px' title='<?php echo $lang->product->releases;?>'><?php echo $lang->product->releases;?></th>
+            <th class='w-80px' title='<?php echo $lang->product->unResolvedBugsTitle;?>'><?php echo $lang->product->unResolvedBugs;?></th>
+            <th class='w-110px' title='<?php echo $lang->product->assignToNullBugsTitle;?>'><?php echo $lang->product->assignToNullBugs;?></th>
             <?php if($canOrder):?>
-            <th class='w-70px sort-default text-center'><?php common::printOrderLink('order', $orderBy, $vars, $lang->product->updateOrder);?></th>
+            <th class='w-70px sort-default'><?php common::printOrderLink('order', $orderBy, $vars, $lang->product->updateOrder);?></th>
             <?php endif;?>
           </tr>
         </thead>
         <tbody class="sortable" id="productTableList">
         <?php foreach($products as $product):?>
-        <tr data-id='<?php echo $product->id ?>' data-order='<?php echo $product->code;?>'>
-          <td class='c-id'>
+        <tr class="text-center" data-id='<?php echo $product->id ?>' data-order='<?php echo $product->code;?>'>
+          <td class='c-id text-left'>
             <?php if($canBatchEdit):?>
             <?php echo html::checkbox('productIDList', array($product->id => sprintf('%03d', $product->id)));?>
             <?php else:?>
@@ -65,17 +65,17 @@
             <?php endif;?>
           </td>
           <td class="c-name" title='<?php echo $product->name?>'><?php echo html::a($this->createLink('product', 'browse', 'product=' . $product->id), $product->name);?></td>
-          <td><?php echo $program->name;?></td>
-          <td class='text-center'><?php echo $product->stories['active'];?></td>
-          <td class='text-center'><?php echo $product->stories['changed'];?></td>
-          <td class='text-center'><?php echo $product->stories['draft'];?></td>
-          <td class='text-center'><?php echo $product->stories['closed'];?></td>
-          <td class='text-center'><?php echo $product->plans;?></td>
-          <td class='text-center'><?php echo $product->releases;?></td>
-          <td class='text-center'><?php echo $product->unResolved;?></td>
-          <td class='text-center'><?php echo $product->assignToNull;?></td>
+          <td class="c-name"><?php echo $program->name;?></td>
+          <td><?php echo $product->stories['active'];?></td>
+          <td><?php echo $product->stories['changed'];?></td>
+          <td><?php echo $product->stories['draft'];?></td>
+          <td><?php echo $product->stories['closed'];?></td>
+          <td><?php echo $product->plans;?></td>
+          <td><?php echo $product->releases;?></td>
+          <td><?php echo $product->unResolved;?></td>
+          <td><?php echo $product->assignToNull;?></td>
           <?php if($canOrder):?>
-          <td class='c-actions sort-handler text-center'><i class="icon icon-move"></i></td>
+          <td class='c-actions sort-handler'><i class="icon icon-move"></i></td>
           <?php endif;?>
         </tr>
         <?php endforeach;?>
