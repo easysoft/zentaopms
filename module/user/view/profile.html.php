cf23b03eb57d7ef8fbeb0c56aec12c48817a4462
holan20180123 <56391770@qq.com>
2020-12-28 14:29:09 +0800

    * Finish task #8744.

--- module/user/view/profile.html.php
+++ module/user/view/profile.html.php
@@ -24,13 +24,12 @@
       </div>
       <div class='row'>
         <div class='col-md-6'>
+          <div class='info-title'><?php echo $lang->user->basicInfo;?></div>
           <dl class='dl-horizontal info'>
-            <dt><?php echo $lang->user->account;?></dt>
-            <dd><?php echo $user->account;?></dd>
             <dt><?php echo $lang->user->realname;?></dt>
             <dd><?php echo $user->realname;?></dd>
-            <dt><?php echo $lang->user->gender;?></dt>
-            <dd><?php echo zget($lang->user->genderList, $user->gender);?></dd>
+            <dt><?php echo $lang->user->account;?></dt>
+            <dd><?php echo $user->account;?></dd>
             <dt><?php echo $lang->user->dept;?></dt>
             <dd>
             <?php
@@ -48,76 +47,60 @@
             }
             ?>
             </dd>
+            <dt><?php echo $lang->user->join;?></dt>
+            <dd><?php echo formatTime($user->join);?></dd>
+          </dl>
+          <div class='info-title'><?php echo $lang->user->contactInfo;?></div>
+          <dl class='dl-horizontal contact'>
+            <dt><?php echo $lang->user->mobile;?></dt>
+            <dd><?php echo $user->mobile;?></dd>
+            <dt><?php echo $lang->user->phone;?></dt>
+            <dd><?php echo $user->phone;?></dd>
+            <dt><?php echo $lang->user->weixin;?></dt>
+            <dd><?php echo $user->weixin;?></dd>
+            <dt><?php echo $lang->user->qq;?></dt>
+            <dd><?php echo $user->qq;?></dd>
+            <dt><?php echo $lang->user->zipcode;?></dt>
+            <dd><?php echo $user->zipcode;?></dd>
+          </dl>
+          <div class='info-title'><?php echo $lang->user->else;?></div>
+          <dl class='dl-horizontal else'>
+            <dt><?php echo $lang->user->commiter;?></dt>
+            <dd><?php echo $user->commiter;?></dd>
+            <dt><?php echo $lang->user->ip;?></dt>
+            <dd><?php echo $user->ip;?></dd>
+          </dl>
+        </div>
+        <div class='col-md-6'>
+          <dl class='dl-horizontal right-info'>
+            <dt><?php echo $lang->user->gender;?></dt>
+            <dd><?php echo zget($lang->user->genderList, $user->gender);?></dd>
+            <dt><?php echo $lang->user->email;?></dt>
+            <dd title='<?php echo $user->email;?>'><?php echo $user->email;?></dd>
             <dt><?php echo $lang->user->role;?></dt>
             <dd><?php echo zget($lang->user->roleList, $user->role);?></dd>
             <dt><?php echo $lang->group->priv;?></dt>
             <dd><?php foreach($groups as $group) echo $group->name . ' '; ?></dd>
-            <dt><?php echo $lang->user->commiter;?></dt>
-            <dd><?php echo $user->commiter;?></dd>
-            <dt><?php echo $lang->user->join;?></dt>
-            <dd><?php echo formatTime($user->join);?></dd>
-            <dt><?php echo $lang->user->visits;?></dt>
-            <dd><?php echo $user->visits;?></dd>
-            <dt><?php echo $lang->user->ip;?></dt>
-            <dd><?php echo $user->ip;?></dd>
-            <dt><?php echo $lang->user->last;?></dt>
-            <dd><?php echo $user->last;?></dd>
-            <?php if($user->ranzhi):?>
-            <dt><?php echo $lang->user->ranzhi;?></dt>
-            <dd>
-              <?php echo $user->ranzhi . ' ';?>
-              <?php if(common::hasPriv('my', 'unbind')) echo html::a($this->createLink('my', 'unbind'), "<i class='icon-unlink'></i>", 'hiddenwin', "class='bin-icon' title='{$lang->user->unbind}'");?>
-            </dd>
-            <?php endif;?>
           </dl>
-        </div>
-        <div class='divider'></div>
-        <div class='col-md-6'>
-          <dl class='dl-horizontal contact'>
-            <dt><?php echo $lang->user->email;?></dt>
-            <dd title='<?php echo $user->email;?>'><?php echo $user->email;?></dd>
-            <?php if(!empty($config->user->contactField)):?>
-            <?php foreach(explode(',', $config->user->contactField) as $field):?>
-            <dt><?php echo $lang->user->$field;?></dt>
-            <dd>
-              <?php
-              if($field == 'skype' and $user->$field)
-              {
-                  echo html::a("callto://$user->skype", $user->skype);
-              }
-              elseif($field == 'qq' and $user->$field)
-              {
-                  echo html::a("tencent://message/?uin=$user->qq", $user->qq);
-              }
-              else
-              {
-                  echo $user->$field;
-              }
-              ?>
-            </dd>
-            <?php endforeach;?>
-            <?php endif;?>
+          <dl class='dl-horizontal right-info'>
+            <dt><?php echo $lang->user->skype;?></dt>
+            <dd><?php if($user->skype) echo html::a("callto://$user->skype", $user->skype);?></dd>
+            <dt><?php echo $lang->user->whatsapp;?></dt>
+            <dd><?php echo $user->whatsapp;?></dd>
+            <dt><?php echo $lang->user->slack;?></dt>
+            <dd><?php echo $user->slack;?></dd>
+            <dt><?php echo $lang->user->dingding;?></dt>
+            <dd><?php echo $user->dingding;?></dd>
             <dt><?php echo $lang->user->address;?></dt>
             <dd title='<?php echo $user->address;?>'><?php echo $user->address;?></dd>
-            <dt><?php echo $lang->user->zipcode;?></dt>
-            <dd><?php echo $user->zipcode;?></dd>
+          </dl>
+          <dl class='dl-horizontal right-info'>
+            <dt><?php echo $lang->user->visits;?></dt>
+            <dd><?php echo $user->visits;?></dd>
+            <dt><?php echo $lang->user->last;?></dt>
+            <dd><?php echo $user->last;?></dd>
           </dl>
         </div>
-      </div>
-    </div>
-  </div>
-  <div class='side-col col-6'>
-    <div class='cell'>
-      <div class='main-header'>
-        <h2><?php echo $lang->user->legendContribution;?></h2>
-      </div>
-      <div class='row tiles'>
-        <?php foreach($lang->user->personalData as $key => $title):?>
-        <div class='col-4 col tile'>
-          <div class='tile-title'><?php echo $title;?></div>
-          <div class='tile-amount'><?php echo zget($personalData, $key, 0);?></div>
-        </div>
-        <?php endforeach;?>
       </div>
     </div>
   </div>
