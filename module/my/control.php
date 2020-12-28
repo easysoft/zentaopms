0bde3027f17981b984f28221b422ca0e1d4859bd
holan20180123 <56391770@qq.com>
2020-12-28 13:28:10 +0800

    * Add comments.

--- module/my/control.php
+++ module/my/control.php
@@ -134,6 +134,7 @@ class my extends control
      * @param  string $type
      * @param  int    $userID
      * @param  string $status
+     * @param  string $orderBy
      * @param  int    $recTotal
      * @param  int    $recPerPage
      * @param  int    $pageID
