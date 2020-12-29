1ae6ec73ed2769ad685d73cab68c212f697c10fd
qiyu-xie <qiyu_xie@163.com>
2020-12-29 10:06:51 +0800

    * Finish task#8738. Add budget notes when change parent program.

--- module/program/js/pgmcreate.js
+++ module/program/js/pgmcreate.js
@@ -1,5 +1,15 @@
-$(function(){$('#parent').change();})
+$(function()
+{
+    setAclList($("#parent").val());
+});
 
+/**
+ * Set acl list.
+ *
+ * @param  programID $programID
+ * @access public
+ * @return void
+ */
 function setAclList(programID)
 {
     if(programID != 0)
@@ -10,4 +20,15 @@ function setAclList(programID)
     {
         $('.aclBox').html($('#PGMAcl').html());
     }
+}
+
+/**
+ * Set parent program.
+ *
+ * @access public
+ * @return void
+ */
+function setParentProgram(programID)
+{
+    location.href = createLink('program', 'PGMCreate', 'programID=' + programID);
 }
