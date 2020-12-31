75af6f563dea665e6a5d076ee33d9e3302d18c94
Catouse <catouse@gmail.com>
2020-12-31 21:55:48 +0800

    * add style for btn-span in .header-angle-btn.

--- module/product/model.php
+++ module/product/model.php
@@ -495,7 +495,7 @@ class productModel extends model
         $active = isset($currentProduct->type) && $currentProduct->type != 'normal' ? '' : 'active';
 
         $dropMenuLink = helper::createLink('product', 'ajaxGetDropMenu', "objectID=$productID&module=$currentModule&method=$currentMethod&extra=$extra");
-        $output .= "<div class='btn-group header-angle-btn $active' id='swapper'><button data-toggle='dropdown' type='button' class='btn' id='currentItem' title='{$currentProductName}'><span class='text'>{$currentProductName}</span> <span class='caret'></span></button><div id='dropMenu' class='dropdown-menu search-list' data-ride='searchList' data-url='$dropMenuLink'>";
+        $output .= "<div class='btn-group header-angle-btn $active' id='swapper'><div class='btn btn-span'>{$this->lang->productCommon}</div><button data-toggle='dropdown' type='button' class='btn' id='currentItem' title='{$currentProductName}'><span class='text'>{$currentProductName}</span> <span class='caret'></span></button><div id='dropMenu' class='dropdown-menu search-list' data-ride='searchList' data-url='$dropMenuLink'>";
         $output .= '<div class="input-control search-box has-icon-left has-icon-right search-example"><input type="search" class="form-control search-input" /><label class="input-control-icon-left search-icon"><i class="icon icon-search"></i></label><a class="input-control-icon-right search-clear-btn"><i class="icon icon-close icon-sm"></i></a></div>';
         $output .= "</div></div>";
 
@@ -1592,18 +1592,18 @@ class productModel extends model
         $SRPairs = $this->custom->getSRPairs();
 
         /* Replace menu lang. */
-        foreach($this->lang->product->menu as $key => $menu)    
+        foreach($this->lang->product->menu as $key => $menu)
         {
-            if($key == 'requirement') 
+            if($key == 'requirement')
             {
                 $link    = explode('|', $menu['link']);
                 $link[0] = zget($URPairs, $product->storyConcept, $this->lang->URCommon);
                 $this->lang->product->menu->$key = implode('|', $link);
             }
-            if($key == 'story') 
+            if($key == 'story')
             {
                 $link    = explode('|', $menu['link']);
-                $link[0] = zget($SRPairs, $product->storyConcept, $this->lang->SRCommon); 
+                $link[0] = zget($SRPairs, $product->storyConcept, $this->lang->SRCommon);
                 $this->lang->product->menu->$key = implode('|', $link);
             }
         }
--- module/program/model.php
+++ module/program/model.php
@@ -357,7 +357,7 @@ class programModel extends model
         }
 
         $dropMenuLink = helper::createLink('program', 'ajaxGetPGMDropMenu', "objectID=$programID&module=$currentModule&method=$currentMethod");
-        $output  = "<div class='btn-group header-angle-btn $active' id='swapper'><button data-toggle='dropdown' type='button' class='btn' id='currentItem' title='{$currentProgramName}'><span class='text'>{$currentProgramName}</span> <span class='caret'></span></button><div id='dropMenu' class='dropdown-menu search-list' data-ride='searchList' data-url='$dropMenuLink'>";
+        $output  = "<div class='btn-group header-angle-btn2 $active' id='swapper'><div class='btn btn-span'>{$this->lang->program->common}</div><button data-toggle='dropdown' type='button' class='btn' id='currentItem' title='{$currentProgramName}'><span class='text'>{$currentProgramName}</span> <span class='caret'></span></button><div id='dropMenu' class='dropdown-menu search-list' data-ride='searchList' data-url='$dropMenuLink'>";
         $output .= '<div class="input-control search-box has-icon-left has-icon-right search-example"><input type="search" class="form-control search-input" /><label class="input-control-icon-left search-icon"><i class="icon icon-search"></i></label><a class="input-control-icon-right search-clear-btn"><i class="icon icon-close icon-sm"></i></a></div>'; $output .= "</div></div>";
 
         return $output;
@@ -417,7 +417,7 @@ class programModel extends model
 
         krsort($programMenu);
         $programMenu = array_pop($programMenu);
-        $lastMenu    = "<ul class='tree' data-ride='tree' id='programTree' data-name='tree-program'>{$programMenu}</ul>\n";
+        $lastMenu    = "<ul class='tree tree-simple' data-ride='tree' id='programTree' data-name='tree-program'>{$programMenu}</ul>\n";
 
         return $lastMenu;
     }
@@ -786,7 +786,7 @@ class programModel extends model
         }
 
         $dropMenuLink = helper::createLink('program', 'ajaxGetPRJDropMenu', "objectID=$projectID&module=$currentModule&method=$currentMethod");
-        $output  = "<div class='btn-group header-angle-btn active' id='swapper'><button data-toggle='dropdown' type='button' class='btn' id='currentItem' title='{$currentProjectName}'><span class='text'>{$currentProjectName}</span> <span class='caret'></span></button><div id='dropMenu' class='dropdown-menu search-list' data-ride='searchList' data-url='$dropMenuLink'>";
+        $output  = "<div class='btn-group header-angle-btn active' id='swapper'><div class='btn btn-span'>{$this->lang->program->project}</div><button data-toggle='dropdown' type='button' class='btn' id='currentItem' title='{$currentProjectName}'><span class='text'>{$currentProjectName}</span> <span class='caret'></span></button><div id='dropMenu' class='dropdown-menu search-list' data-ride='searchList' data-url='$dropMenuLink'>";
         $output .= '<div class="input-control search-box has-icon-left has-icon-right search-example"><input type="search" class="form-control search-input" /><label class="input-control-icon-left search-icon"><i class="icon icon-search"></i></label><a class="input-control-icon-right search-clear-btn"><i class="icon icon-close icon-sm"></i></a></div>';
         $output .= "</div></div>";
 
@@ -1427,7 +1427,7 @@ class programModel extends model
                 $lib->product = $productID;
                 $lib->name    = $this->lang->doclib->main['product'];
                 $lib->type    = 'product';
-                $lib->main    = '1'; 
+                $lib->main    = '1';
                 $lib->acl     = 'default';
                 $this->dao->insert(TABLE_DOCLIB)->data($lib)->exec();
             }
