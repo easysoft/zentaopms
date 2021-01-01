ce7cf652d55318a9ba40beb61c354ce64707d6b8
tianshujie98 <tianshujie0123@163.com>
2021-01-01 11:34:53 +0800

    * Fix bug.

--- module/build/control.php
+++ module/build/control.php
@@ -41,7 +41,7 @@ class build extends control
         $this->loadModel('user');
 
         /* Set session and get execution by id. */
-        $this->session->set('buildCreate', $this->app->getURI(true));
+        $this->session->set('buildCreate', $this->createLink('build', 'create', "executionID=$executionID", '', '', $this->session->PRJ));
         $execution = $this->loadModel('project')->getExecutionById($executionID);
 
         /* Set menu. */
