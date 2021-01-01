f353c56b70833fb220a8390f04e51880f10069ef
tianshujie98 <tianshujie0123@163.com>
2021-01-01 14:50:53 +0800

    * Add PRJ.

--- module/testcase/view/createbug.html.php
+++ module/testcase/view/createbug.html.php
@@ -11,6 +11,7 @@
  */
 ?>
 <?php include '../../common/view/header.lite.html.php';?>
+<?php js::set('PRJID', $this->session->PRJ);?>
 <div id='mainContent' class='main-content'>
   <div class='main-header'>
     <h2><?php echo $lang->testcase->createBug;?></h2>
@@ -30,7 +31,7 @@ function createBug(obj)
 
     var onlybody    = config.onlybody;
     config.onlybody = 'no';
-    window.open(createLink('bug', 'create', params + ',stepIdList=' + stepIdList), '_blank');
+    window.open(createLink('bug', 'create', params + ',stepIdList=' + stepIdList, '', '', PRJID), '_blank');
     config.onlybody = onlybody;
 }
 
