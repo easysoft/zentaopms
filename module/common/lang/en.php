d1801f772bfe78358e06d82023302aca401c3b16
Yagami <976204163@qq.com>
2021-01-04 15:42:23 +0800

    * Add program menu.

--- module/common/lang/en.php
+++ module/common/lang/en.php
@@ -29,6 +29,8 @@ $lang->help            = 'Help';
 $lang->aboutZenTao     = 'About';
 $lang->profile         = 'Profile';
 $lang->changePassword  = 'Password';
+$lang->unfoldMenu      = 'Unfold Menu';
+$lang->collapseMenu    = 'Collapse Menu';
 $lang->runInfo         = "<div class='row'><div class='u-1 a-center' id='debugbar'>Time %s MS, Memory %s KB, Query %s.  </div></div>";
 $lang->agreement       = "I have read and agreed to the terms and conditions of <a href='http://zpl.pub/page/zplv12.html' target='_blank'> Z PUBLIC LICENSE 1.2 </a>. <span class='text-danger'>Without authorization, I should not remove, hide or cover any logos/links of ZenTao.</span>";
 $lang->designedByAIUX  = "<a href='https://api.zentao.pm/goto.php?item=aiux' class='link-aiux' target='_blank'>Designed by <strong>AIUX</strong></a>";
