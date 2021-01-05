9846e2e6969f7d3e232d0f01b05e065ad4e94ea8
tianshujie98 <tianshujie0123@163.com>
2021-01-05 13:23:29 +0800

    * Adjust table.

--- module/custom/view/browsestoryconcept.html.php
+++ module/custom/view/browsestoryconcept.html.php
@@ -22,28 +22,28 @@
   <div class='main-col main-content main-table'>
       <table class='table table-form'>
         <thead>
-        <tr>
-          <th class='text-left'><?php echo $lang->custom->URConcept;?> </th>
-          <th class='text-left'><?php echo $lang->custom->SRConcept;?> </th>
-          <th class='w-100px text-center'><?php echo $lang->custom->buildin;?> </th>
-          <th class='w-100px text-center'><?php echo $lang->custom->isDefault;?> </th>
-          <th class='w-100px text-left'><?php echo $lang->actions;?> </th>
-        </tr>
+          <tr>
+            <th class='text-left'><?php echo $lang->custom->URConcept;?> </th>
+            <th class='text-left'><?php echo $lang->custom->SRConcept;?> </th>
+            <th class='w-100px text-center'><?php echo $lang->custom->buildin;?> </th>
+            <th class='w-100px text-center'><?php echo $lang->custom->isDefault;?> </th>
+            <th class='w-100px text-left'><?php echo $lang->actions;?> </th>
+          </tr>
         </thead>
         <tbody>
         <?php foreach($URSRList as $key => $URSR):?>
-        <tr>
-          <td class='text-left'><?php echo $URSR['URName'];?></td>
-          <td class='text-left'><?php echo $URSR['SRName'];?></td>
-          <td class='text-center'><?php echo zget($lang->custom->tipRangeList, $URSR['system'], '');?></td>
-          <td class='text-center'><?php if($key == $config->custom->URSR) echo "<i class='icon icon-check'></i>";?></td>
-          <td class='c-actions'>
-            <?php $disabled = $URSR['system'] ? "disabled=disabled" : '';?>
-            <?php if(common::hasPriv('custom', 'setDefaultConcept'))  echo html::a($this->createLink('custom', 'setDefaultConcept', "id=$key"), "<i class='icon icon-hand-right'></i>", 'hiddenwin', "class='btn' title={$lang->custom->setDefaultConcept}");?>
-            <?php if(common::hasPriv('custom', 'editStoryConcept'))   echo html::a($this->createLink('custom', 'editStoryConcept', "id=$key", '', true), "<i class='icon icon-edit'></i>", '', "class='btn iframe' title={$lang->edit}");?>
-            <?php if(common::hasPriv('custom', 'deleteStoryConcept')) echo html::a($this->createLink('custom', 'deleteStoryConcept', "id=$key"), "<i class='icon icon-trash'></i>", 'hiddenwin', "class='btn' $disabled title={$lang->delete}");?>
-          </td>
-        </tr>
+          <tr>
+            <td class='text-left'><?php echo $URSR['URName'];?></td>
+            <td class='text-left'><?php echo $URSR['SRName'];?></td>
+            <td class='text-center'><?php echo zget($lang->custom->tipRangeList, $URSR['system'], '');?></td>
+            <td class='text-center'><?php if($key == $config->custom->URSR) echo "<i class='icon icon-check'></i>";?></td>
+            <td class='c-actions'>
+              <?php $disabled = $URSR['system'] ? "disabled=disabled" : '';?>
+              <?php if(common::hasPriv('custom', 'setDefaultConcept'))  echo html::a($this->createLink('custom', 'setDefaultConcept', "id=$key"), "<i class='icon icon-hand-right'></i>", 'hiddenwin', "class='btn' title={$lang->custom->setDefaultConcept}");?>
+              <?php if(common::hasPriv('custom', 'editStoryConcept'))   echo html::a($this->createLink('custom', 'editStoryConcept', "id=$key", '', true), "<i class='icon icon-edit'></i>", '', "class='btn iframe' title={$lang->edit}");?>
+              <?php if(common::hasPriv('custom', 'deleteStoryConcept')) echo html::a($this->createLink('custom', 'deleteStoryConcept', "id=$key"), "<i class='icon icon-trash'></i>", 'hiddenwin', "class='btn' $disabled title={$lang->delete}");?>
+            </td>
+          </tr>
         <?php endforeach;?>
         </tbody>
       </table>
