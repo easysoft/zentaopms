46d00971576e859b213a2ddd6a1ddc8efe9b7ab9
hufangzhou <746775970@qq.com>
2021-01-07 09:15:27 +0800

    * Finish task#8939.

--- module/product/js/project.js
+++ module/product/js/project.js
@@ -1,6 +1,6 @@
 $(function()
 {
-    $('#PRJMine1').click(function()
+    $('#mainMenu input[name^="PRJMine"]').click(function()
     {
         var PRJMine = $(this).is(':checked') ? 1 : 0;
         $.cookie('PRJMine', PRJMine, {expires:config.cookieLife, path:config.webRoot});
