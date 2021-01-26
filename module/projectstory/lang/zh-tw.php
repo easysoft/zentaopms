4af8364493094b6e582cb3abb49807c3d849d159
hufangzhou <746775970@qq.com>
2021-01-26 11:24:48 +0800

    * Adjust the lang file.

--- module/projectstory/lang/zh-tw.php
+++ module/projectstory/lang/zh-tw.php
@@ -1,9 +1,9 @@
 <?php
 $lang->projectstory->common      = '項目需求';
 $lang->projectstory->index       = '需求主頁';
-$lang->projectstory->view        = $lang->SRCommon . '詳情';
+$lang->projectstory->view        = '需求詳情';
 $lang->projectstory->story       = $lang->generalSR;
-$lang->projectstory->track       = '跟蹤矩陣';
+$lang->projectstory->track       = '矩陣';
 $lang->projectstory->linkStory   = '關聯' . $lang->SRCommon;
 $lang->projectstory->unlinkStory = '移除' . $lang->SRCommon;
 
