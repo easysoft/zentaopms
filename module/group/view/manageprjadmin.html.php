c72ba492cfb2fe5395ef56c06feb42e635577c8c
leiyong <1549684884@qq.com>
2021-03-02 11:38:29 +0800

    * Finish task#36315.

--- module/group/view/manageprjadmin.html.php
+++ module/group/view/manageprjadmin.html.php
@@ -39,7 +39,7 @@
           </td>
           <td class='pv-10px' colspan='2'>
             <div class='input-group'>
-              <span class='input-group-addon'> <?php echo $lang->group->PRJ;?></span>
+              <span class='input-group-addon'> <?php echo $lang->group->manageProject;?></span>
               <?php echo html::select("program[$account][]", $programs, $program, "class='form-control chosen' multiple");?>
             </div>
           </td>
@@ -58,7 +58,7 @@
           </td>
           <td class='pv-10px' colspan='2'>
             <div class='input-group'>
-              <span class='input-group-addon'> <?php echo $lang->group->PRJ;?></span>
+              <span class='input-group-addon'> <?php echo $lang->group->manageProject;?></span>
               <?php echo html::select('program[0][]', $programs, '', "class='form-control chosen' multiple");?>
             </div>
           </td>
