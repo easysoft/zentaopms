106bb9c004270376f8f2731865ca0703a1b8f4e7
hufangzhou <746775970@qq.com>
2021-03-02 09:51:42 +0800

    * Finish task #36366.

--- module/datatable/config.php
+++ module/datatable/config.php
@@ -1,5 +1,4 @@
 <?php
 $config->datatable->moduleAlias['product-browse']     = 'story';
-$config->datatable->moduleAlias['projectstory-story'] = 'story';
 $config->datatable->moduleAlias['project-task']       = 'task';
 $config->datatable->moduleAlias['testtask-cases']     = 'testcase';
