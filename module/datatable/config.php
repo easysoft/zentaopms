1566dce5854dca4421cbc000dc6753c80a0d7a8c
Yagami <976204163@qq.com>
2021-03-04 13:56:58 +0800

    * Fix sql and rename project.

--- module/datatable/config.php
+++ module/datatable/config.php
@@ -1,4 +1,4 @@
 <?php
 $config->datatable->moduleAlias['product-browse']     = 'story';
-$config->datatable->moduleAlias['project-task']       = 'task';
+$config->datatable->moduleAlias['execution-task']     = 'task';
 $config->datatable->moduleAlias['testtask-cases']     = 'testcase';
