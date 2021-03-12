3bd0242fdeb7baa0647defcd3191c164db37acaa
leiyong <1549684884@qq.com>
2021-03-12 10:19:11 +0800

    * Finish task#36338.

--- module/execution/control.php
+++ module/execution/control.php
@@ -2526,7 +2526,7 @@ class execution extends control
                 $executionsPinYin = common::convert2Pinyin($executionsName);
                 foreach($executions as $execution)
                 {
-                    $link = helper::createLink('execution', 'task', 'executionID=' . $execution->id, '', false, $execution->execution);
+                    $link = helper::createLink('execution', 'task', 'executionID=' . $execution->id, '', false, $execution->project);
                     $execution->code = empty($execution->code) ? $execution->name : $execution->code;
                     $dataKey = 'date-key="' . zget($executionsPinYin, $execution->name, $execution->name) . '"';
                     $class   = "class='search-list-item $color' title='$execution->name' $dataKey";
