ff5c09c76f3aa095d680e3d502ef2a06248693e5
hufangzhou <746775970@qq.com>
2021-03-12 08:21:40 +0800

    * Adjust the code.

--- module/custom/view/header.html.php
+++ module/custom/view/header.html.php
@@ -4,13 +4,15 @@
   <?php
   foreach($lang->custom->object as $object => $name)
   {
-      if(strpos('story|todo|block', $object) !== false and count($lang->custom->object) > 1) echo "<span class='divider'></span>";
+      if(strpos('story|todo|block', $object) !== false) echo "<span class='divider'></span>";
       if(strpos('execution|product', $object) !== false) common::printLink('custom', $object, "", "<span class='text'>{$lang->custom->$object}</span>", '', "class='btn btn-link' id='{$object}Tab'");
       if(strpos('execution|product', $object) === false) common::printLink('custom', 'set', "module=$object&field=" . key($lang->custom->{$object}->fields), "<span class='text'>{$name}</span>", '', "class='btn btn-link' id='{$object}Tab'");
+      if($object == 'user') common::printLink('custom', 'required', "", "<span class='text'>{$lang->custom->required}</span>", '', "class='btn btn-link' id='requiredTab'");
   }
 
   foreach($lang->custom->system as $sysObject)
   {
+      if($sysObject == 'required') continue;
       common::printLink('custom', $sysObject, "", "<span class='text'>{$lang->custom->$sysObject}</span>", '', "class='btn btn-link' id='{$sysObject}Tab'");
   }
   if((isset($config->systemMode) and $config->systemMode == 'classic') || (isset($config->global->upgradeStep) and $config->global->upgradeStep == 'mergeProgram'))
