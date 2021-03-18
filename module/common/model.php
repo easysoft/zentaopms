d6e6641c1cb07315f1eb6f58fe8dff1e31789014
tianshujie98 <tianshujie0123@163.com>
2021-03-18 17:00:06 +0800

    * Fix bug.

--- module/common/model.php
+++ module/common/model.php
@@ -696,7 +696,6 @@ class commonModel extends model
                 /* Avoid highlighting the same navigation method at the same time. */
                 if($currentModule == 'feedback' and $currentMethod == 'admin') $active = '';
                 if($currentModule == 'product' and $currentMethod == 'browse') $active = '';
-                if($currentModule == 'product' and $currentMethod == 'create') $active = '';
                 if($currentModule == 'story') $active = '';
 
                 $misc = $openApp != $lang->navGroup->$module ? "data-app='$openApp'" : '';
