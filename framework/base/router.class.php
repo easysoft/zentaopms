7f52574a2f2c8ff64096cfde4dcccb219dae7fae
Yagami <976204163@qq.com>
2021-03-19 09:31:44 +0800

    * Code for app session.

--- framework/base/router.class.php
+++ framework/base/router.class.php
@@ -2512,8 +2512,7 @@ class super
         }
         elseif($this->scope == 'session')
         {
-            //$openApp = $openApp ? $openApp : $this->openApp;
-            //$openApp ? $_SESSION["app-$openApp"][$key] = $value : $_SESSION[$key] = $value;
+            if($openApp) $_SESSION["app-$openApp"][$key] = $value;
             $_SESSION[$key] = $value;
         }
         elseif($this->scope == 'env')
@@ -2561,8 +2560,8 @@ class super
         }
         elseif($this->scope == 'session')
         {
-            //$openApp = $this->openApp;
-            //if($openApp and isset($_SESSION["app-$openApp"][$key])) return $_SESSION["app-$openApp"][$key];
+            $openApp = $this->openApp;
+            if($openApp and isset($_SESSION["app-$openApp"][$key])) return $_SESSION["app-$openApp"][$key];
             if(isset($_SESSION[$key])) return $_SESSION[$key];
             return false;
         }
