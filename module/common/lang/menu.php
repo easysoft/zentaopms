e6c2eb9bbd2ae3dfda00d85e7ad22da0a13243d8
qiyu-xie <qiyu_xie@163.com>
2021-03-22 16:17:20 +0800

    * Fix bug.

--- module/common/lang/menu.php
+++ module/common/lang/menu.php
@@ -295,7 +295,7 @@ $lang->execution->menu->settings['subMenu']->products  = array('link' => "$lang-
 $lang->execution->menu->settings['subMenu']->team      = array('link' => "{$lang->team->common}|execution|team|executionID=%s", 'alias' => 'managemembers');
 $lang->execution->menu->settings['subMenu']->whitelist = array('link' => "$lang->whitelist|execution|whitelist|executionID=%s", 'subModule' => 'personnel', 'alias' => 'addwhitelist');
 
-$lang->execution->dividerMenu = ',story,build,setting,';
+$lang->execution->dividerMenu = ',story,build,settings,';
 
 /* QA menu.*/
 $lang->qa->menu = new stdclass();
