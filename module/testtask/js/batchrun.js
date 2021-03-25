a0323a2b376bb3af0440b61795c5d1fc07b64a49
hufangzhou <746775970@qq.com>
2021-03-25 16:05:32 +0800

    * Fix a bug.

--- module/testtask/js/batchrun.js
+++ module/testtask/js/batchrun.js
@@ -10,8 +10,3 @@ $(document).on('keyup', 'form input:text', function()
         $(preSelect).val('fail');
     }
 })
-
-$(function()
-{
-    $('#subNavbar li[data-id="testcase"]').addClass('active');
-})
