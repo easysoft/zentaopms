b697bccff196f119a00ea2560fd83cb94b638a4b
hufangzhou <746775970@qq.com>
2021-04-02 09:33:00 +0800

    * Adjust the required style.

--- module/testtask/view/importunitresult.html.php
+++ module/testtask/view/importunitresult.html.php
@@ -13,6 +13,9 @@
 <?php include '../../common/view/header.html.php';?>
 <?php include '../../common/view/datepicker.html.php';?>
 <?php include '../../common/view/kindeditor.html.php';?>
+<style>
+.input-group>.chosen-container.required:after {top: 6px;}
+</style>
 <?php js::import($jsRoot . 'misc/date.js');?>
 <div id='mainContent' class='main-content'>
   <div class='center-block'>
