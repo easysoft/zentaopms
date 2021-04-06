b72b709435e7c86e2d580c25ed12894cd0c957e1
leiyong <1549684884@qq.com>
2021-04-06 05:00:16 +0300

    * Fix bug#11595.

--- module/report/view/blockreportlist.html.php
+++ module/report/view/blockreportlist.html.php
@@ -21,9 +21,11 @@
       </div>
     </div>
   </div>
+  <?php if(!isset($config->proVersion) and !isset($config->bizVersion) and !isset($config->maxVersion)):?>
   <div class='panel panel-body' style='padding: 10px 6px'>
     <div class='text proversion'>
       <strong class='text-danger small text-latin'>PRO</strong> &nbsp;<span class='text-important'><?php echo (!empty($config->isINT)) ? $lang->report->proVersionEn : $lang->report->proVersion;?></span>
     </div>
   </div>
+  <?php endif;?>
 </div>
