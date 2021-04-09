54d29dcafe80216e8644e2d18c94acc6e01c9e87
Yagami <976204163@qq.com>
2021-04-09 13:40:54 +0800

    * Add upgrade logic.

--- module/misc/lang/zh-cn.php
+++ module/misc/lang/zh-cn.php
@@ -84,6 +84,8 @@ $lang->misc->feature = new stdclass();
 $lang->misc->feature->lastest  = '最新版本';
 $lang->misc->feature->detailed = '详情';
 
+$lang->misc->releaseDate['15.0.rc2']    = '2021-04-09';
+$lang->misc->releaseDate['15.0.rc1']    = '2021-04-05';
 $lang->misc->releaseDate['12.5.3']      = '2021-01-06';
 $lang->misc->releaseDate['12.5.2']      = '2020-12-18';
 $lang->misc->releaseDate['12.5.1']      = '2020-11-30';
@@ -138,6 +140,8 @@ $lang->misc->releaseDate['7.2.stable']  = '2015-05-22';
 $lang->misc->releaseDate['7.1.stable']  = '2015-03-07';
 $lang->misc->releaseDate['6.3.stable']  = '2014-11-07';
 
+$lang->misc->feature->all['15.0.rc2'][]    = array('title' => '修复Bug，优化界面交互', 'desc' => '');
+$lang->misc->feature->all['15.0.rc1'][]    = array('title' => '升级到15版本，重构导航、文档库，增加项目集管理', 'desc' => '');
 $lang->misc->feature->all['12.5.3'][]      = array('title' => '优化年度总结', 'desc' => '');
 $lang->misc->feature->all['12.5.2'][]      = array('title' => '修复Bug', 'desc' => '');
 $lang->misc->feature->all['12.5.1'][]      = array('title' => '修复漏洞。', 'desc' => '');
