7528daadd1b47194f09d9d60dac2a4cc9a6c6cb7
hufangzhou <746775970@qq.com>
2021-04-12 09:31:13 +0800

    * Fix bug #11952.

--- module/branch/view/ajaxgetdropmenu.html.php
+++ module/branch/view/ajaxgetdropmenu.html.php
@@ -2,12 +2,13 @@
 <?php js::set('module', $module);?>
 <?php js::set('method', $method);?>
 <?php js::set('extra', $extra);?>
+<style> #mainHeader #dropMenu>.list-group {padding: 5px 0 0 10px;} </style>
 <div class="list-group">
   <?php
   foreach($branches as $branchID => $branch)
   {
       $linkHtml = $this->branch->setParamsForLink($module, $link, $projectID, $productID, $branchID);
-      echo html::a($linkHtml, "<i class='icon-cube'></i> " . $branch, '', "data-key='{$branchesPinyin[$branch]}'");
+      echo html::a($linkHtml, $branch, '', "data-key='{$branchesPinyin[$branch]}'");
   }
   ?>
 </div>
