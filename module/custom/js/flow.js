9ce2232cbf422c269a4f9275ebfdeca6c7965789
Yagami <976204163@qq.com>
2021-04-15 16:09:51 +0800

    * Add change flow.

--- module/custom/js/flow.js
+++ module/custom/js/flow.js
@@ -1,15 +1,4 @@
 $(document).ready(function()
 { 
     $('#mainMenu #flowTab').addClass('btn-active-text');
-
-    toggleStoryOrRequirement($("[name='storyRequirement']:checked").val());
-    $("[name='storyRequirement']").change(function(){ toggleStoryOrRequirement($(this).val())});
 });
-
-function toggleStoryOrRequirement(value)
-{
-    $('#requirementpoint').toggle(value == 0);
-    $('#storypoint').toggle(value != 0);
-    $('#hourPoint1').closest('.radio-inline').toggle(value != 0);
-    $('#hourPoint2').closest('.radio-inline').toggle(value == 0);
-}
