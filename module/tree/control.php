89ec9cec9cb167200e120e46e7c9ced1b1fa4ffc
qiyu-xie <qiyu_xie@163.com>
2021-04-22 11:35:45 +0800

    * Fix bug#12065.

--- module/tree/control.php
+++ module/tree/control.php
@@ -398,6 +398,7 @@ class tree extends control
             $confirmLang = $this->lang->tree->confirmDelete;
             if($module->type == 'line') $confirmLang = $this->lang->tree->confirmDeleteLine;
             if($module->type == 'host') $confirmLang = $this->lang->tree->confirmDeleteHost;
+            if($module->type == 'feedback') $confirmLang = $this->lang->tree->confirmDelCategory;
             die(js::confirm($confirmLang, $this->createLink('tree', 'delete', "rootID=$rootID&moduleID=$moduleID&confirm=yes")));
         }
         else
