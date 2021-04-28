77ac56be5be787c90a8e324dd5da5c85a0882f8f
tianshujie98 <tianshujie0123@163.com>
2021-04-28 15:52:21 +0800

    * Fix bug #12437.

--- module/group/control.php
+++ module/group/control.php
@@ -252,22 +252,23 @@ class group extends control
             if(isonlybody()) die(js::closeModal('parent.parent', 'this'));
             die(js::locate($this->createLink('group', 'browse'), 'parent'));
         }
-        $group      = $this->group->getById($groupID);
-        $groupUsers = $this->group->getUserPairs($groupID);
-        $allUsers   = $this->loadModel('dept')->getDeptUserPairs($deptID);
-        $otherUsers = array_diff_assoc($allUsers, $groupUsers);
+        $group        = $this->group->getById($groupID);
+        $groupUsers   = $this->group->getUserPairs($groupID);
+        $allUsers     = $this->loadModel('dept')->getDeptUserPairs($deptID);
+        $otherUsers   = array_diff_assoc($allUsers, $groupUsers);
+        $outsideUsers = $this->user->getPairs('outside|noclosed|noletter|noempty');
 
         $title      = $this->lang->company->common . $this->lang->colon . $group->name . $this->lang->colon . $this->lang->group->manageMember;
         $position[] = $group->name;
         $position[] = $this->lang->group->manageMember;
 
-        $this->view->title      = $title;
-        $this->view->position   = $position;
-        $this->view->group      = $group;
-        $this->view->deptTree   = $this->loadModel('dept')->getTreeMenu($rooteDeptID = 0, array('deptModel', 'createGroupManageMemberLink'), $groupID);
-        $this->view->groupUsers = $groupUsers;
-        $this->view->otherUsers = $otherUsers;
-
+        $this->view->title        = $title;
+        $this->view->position     = $position;
+        $this->view->group        = $group;
+        $this->view->deptTree     = $this->loadModel('dept')->getTreeMenu($rooteDeptID = 0, array('deptModel', 'createGroupManageMemberLink'), $groupID);
+        $this->view->groupUsers   = $groupUsers;
+        $this->view->otherUsers   = $otherUsers;
+        $this->view->outsideUsers = array_diff_assoc($outsideUsers, $groupUsers);
         $this->display();
     }
 
