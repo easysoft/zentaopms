06694e49110675d6b9c27c9599eaab7aad49efcd
qiyu-xie <qiyu_xie@163.com>
2021-05-13 13:53:23 +0800

    * Finish task#38174.

--- module/custom/lang/fr.php
+++ module/custom/lang/fr.php
@@ -96,6 +96,7 @@ $lang->custom->project->defaultCurrency    = 'Default Currency';
 $lang->custom->project->fields['unitList'] = 'Unit List';
 
 $lang->custom->story = new stdClass();
+$lang->custom->story->fields['categoryList']     = 'Category';
 $lang->custom->story->fields['priList']          = 'Priorité';
 $lang->custom->story->fields['sourceList']       = 'Source';
 $lang->custom->story->fields['reasonList']       = 'Raison Fermeture';
