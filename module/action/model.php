a5a6aecb056047369b15c35b90b519dc9ce35eb7
holan20180123 <56391770@qq.com>
2021-05-14 14:43:32 +0800

    * Fix bug #12657,#12661.

--- module/action/model.php
+++ module/action/model.php
@@ -181,7 +181,7 @@ class actionModel extends model
         }
 
         /* Only process these object types. */
-        if(strpos(',story,productplan,release,task,build,bug,case,testtask,doc,issue,risk,repo,team,', ",{$objectType},") !== false)
+        if(strpos(',story,productplan,release,task,build,bug,case,testtask,doc,issue,risk,team,', ",{$objectType},") !== false)
         {
             if(!isset($this->config->objectTables[$objectType])) return $emptyRecord;
 
