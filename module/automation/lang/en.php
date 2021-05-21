8dcfa8f18939f2ca6ddba6b536cc83f9adea2b5a
tianshujie98 <tianshujie0123@163.com>
2021-05-21 15:21:31 +0800

    * Add English item.

---
+++ module/automation/lang/en.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * The automation module zh-cn file of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2015 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     ZPL (http://zpl.pub/page/zplv12.html)
+ * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
+ * @package     automation
+ * @version     $Id: zh-cn.php 4767 2013-05-05 06:10:13Z wwccss $
+ * @link        http://www.zentao.net
+ */
+$lang->automation->common = 'Automated testing';
+$lang->automation->browse = 'Index';
+
+$lang->automation->title = 'Automated test solutions';
+
+$lang->automation->ztfSite     = 'ZTF Website';
+$lang->automation->ztfDownload = 'ZTF Download';
+$lang->automation->ztfManual   = 'ZTF Manual';
+
+$lang->automation->ztfFeature1 = 'ZTF solves various problems for you.';
+$lang->automation->ztfFeature2 = 'ZTF solves the following problems for you.';
+$lang->automation->ztfFeature3 = 'Management of use case information.';
+$lang->automation->ztfFeature4 = 'Comparison of test results';
+$lang->automation->ztfFeature5 = 'Test the execution of the script';
+$lang->automation->ztfFeature6 = 'Defect Bug submission.';
+
+$lang->automation->ztfFeature7  = 'ZTF has no dependence on the environment.';
+$lang->automation->ztfFeature8  = 'Developed in the Go language.';
+$lang->automation->ztfFeature9  = 'Deploy without any dependencies.';
+$lang->automation->ztfFeature10 = 'It works on all platforms.';
+$lang->automation->ztfFeature11 = 'There is only one executable.';
+
+$lang->automation->ztfFeature12 = 'ZTF has no requirement for automated testing.';
+$lang->automation->ztfFeature13 = 'Support for a variety of common development languages.';
+$lang->automation->ztfFeature14 = 'Can be integrated with known testing frameworks.';
+$lang->automation->ztfFeature15 = 'There are no requirements for how the script is implemented.';
+
+$lang->automation->ztfFeature16 = 'You can batch export Zen test cases to scripts';
+$lang->automation->ztfFeature17 = 'The results of the execution can be submitted to the Zentao';
+$lang->automation->ztfFeature18 = 'Execution results can be created as bug';
+$lang->automation->ztfFeature19 = 'Deep integration of ZTF and Zentao';
+
+$lang->automation->zendataSite     = 'ZenData Website';
+$lang->automation->zendataDownload = 'ZenData Download';
+$lang->automation->zendataManual   = 'ZenData Manual';
+
+$lang->automation->zendata         = 'Generic data generation tool';
+$lang->automation->zendataFeature1 = 'Do not understand the code can also be used, easy to get started';
+$lang->automation->zendataFeature2 = 'The data format is completely customizable without any restrictions';
+$lang->automation->zendataFeature3 = 'One click generates a million pieces of data, no pressure';
+
+$lang->automation->zendataFeature4 = 'Cross-platform and cross-language';
+$lang->automation->zendataFeature5 = 'Only one executable is required to run';
+$lang->automation->zendataFeature6 = 'You can use Windows, Linux, MacOS';
+$lang->automation->zendataFeature7 = 'A variety of languages can be used';
+
+$lang->automation->zendataFeature8  = 'All kinds of black tricks';
+$lang->automation->zendataFeature9  = 'Interval, Step Size, Random, Repeat, References';
+$lang->automation->zendataFeature10 = 'Specify a list of files to be queried in Excel';
+$lang->automation->zendataFeature11 = 'Parallel generation, recursive generation, reverse parsing';
+
+$lang->automation->groupTips        = 'Welcome to join the automated test QQ communication group';
+$lang->automation->groupTitle       = 'Software test communication group:';
+$lang->automation->qqGroup          = '522506766';
+$lang->automation->groupDescription = 'Software testing technology exchange community for software testing practitioners, sharing software testing tools and methods, help everyone to improve the level of software testing skills.';
