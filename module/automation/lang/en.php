cc475e474021d87212d8448267e574a0d72e40b6
tianshujie98 <tianshujie0123@163.com>
2021-05-24 12:08:41 +0800

    * Modify English language file.

--- module/automation/lang/en.php
+++ module/automation/lang/en.php
@@ -9,58 +9,58 @@
  * @version     $Id: zh-cn.php 4767 2013-05-05 06:10:13Z wwccss $
  * @link        http://www.zentao.net
  */
-$lang->automation->common = 'Automated testing';
-$lang->automation->browse = 'Index';
+$lang->automation->common = 'Automated Testing';
+$lang->automation->browse = 'Home';
 
-$lang->automation->title = 'Automated test solutions';
+$lang->automation->title = 'Test Automation Solutions';
 
 $lang->automation->ztfSite     = 'ZTF Website';
 $lang->automation->ztfDownload = 'ZTF Download';
 $lang->automation->ztfManual   = 'ZTF Manual';
 
-$lang->automation->ztfFeature1 = 'ZTF solves various problems for you.';
-$lang->automation->ztfFeature2 = 'ZTF solves the following problems for you.';
-$lang->automation->ztfFeature3 = 'Management of use case information.';
+$lang->automation->ztfFeature1 = 'ZTF solves problems for you.';
+$lang->automation->ztfFeature2 = 'ZTF solves the following problems for you:';
+$lang->automation->ztfFeature3 = 'Management of use cases';
 $lang->automation->ztfFeature4 = 'Comparison of test results';
-$lang->automation->ztfFeature5 = 'Test the execution of the script';
-$lang->automation->ztfFeature6 = 'Defect Bug submission.';
+$lang->automation->ztfFeature5 = 'Execution of test scripts';
+$lang->automation->ztfFeature6 = 'Bug reporting';
 
-$lang->automation->ztfFeature7  = 'ZTF has no dependence on the environment.';
-$lang->automation->ztfFeature8  = 'Developed in the Go language.';
-$lang->automation->ztfFeature9  = 'Deploy without any dependencies.';
-$lang->automation->ztfFeature10 = 'It works on all platforms.';
-$lang->automation->ztfFeature11 = 'There is only one executable.';
+$lang->automation->ztfFeature7  = 'ZTF is not depended on the environment';
+$lang->automation->ztfFeature8  = 'Written in Go';
+$lang->automation->ztfFeature9  = 'Deploy with no dependencies';
+$lang->automation->ztfFeature10 = 'Works on all platforms';
+$lang->automation->ztfFeature11 = 'Has only one executable';
 
-$lang->automation->ztfFeature12 = 'ZTF has no requirement for automated testing.';
-$lang->automation->ztfFeature13 = 'Support for a variety of common development languages.';
-$lang->automation->ztfFeature14 = 'Can be integrated with known testing frameworks.';
-$lang->automation->ztfFeature15 = 'There are no requirements for how the script is implemented.';
+$lang->automation->ztfFeature12 = 'ZTF is for all automated testing types';
+$lang->automation->ztfFeature13 = 'Support for common development languages';
+$lang->automation->ztfFeature14 = 'Can be integrated with known testing frameworks';
+$lang->automation->ztfFeature15 = 'No specific script implementation requried';
 
-$lang->automation->ztfFeature16 = 'You can batch export Zen test cases to scripts';
-$lang->automation->ztfFeature17 = 'The results of the execution can be submitted to the Zentao';
-$lang->automation->ztfFeature18 = 'Execution results can be created as bug';
-$lang->automation->ztfFeature19 = 'Deep integration of ZTF and Zentao';
+$lang->automation->ztfFeature16 = 'Export test cases to scripts in bulk';
+$lang->automation->ztfFeature17 = 'Submit execution results to ZenTao';
+$lang->automation->ztfFeature18 = 'Create bugs from execution results';
+$lang->automation->ztfFeature19 = 'Integrate ZenTao ALM';
 
 $lang->automation->zendataSite     = 'ZenData Website';
 $lang->automation->zendataDownload = 'ZenData Download';
 $lang->automation->zendataManual   = 'ZenData Manual';
 
-$lang->automation->zendata         = 'Generic data generation tool';
-$lang->automation->zendataFeature1 = 'Do not understand the code can also be used, easy to get started';
-$lang->automation->zendataFeature2 = 'The data format is completely customizable without any restrictions';
-$lang->automation->zendataFeature3 = 'One click generates a million pieces of data, no pressure';
+$lang->automation->zendata         = 'A generic data generation tool';
+$lang->automation->zendataFeature1 = 'Easy to get started with no coding knowledge required';
+$lang->automation->zendataFeature2 = 'Customizable data format with no restrictions';
+$lang->automation->zendataFeature3 = 'Generate a million pieces of data with one click';
 
 $lang->automation->zendataFeature4 = 'Cross-platform and cross-language';
-$lang->automation->zendataFeature5 = 'Only one executable is required to run';
-$lang->automation->zendataFeature6 = 'You can use Windows, Linux, MacOS';
-$lang->automation->zendataFeature7 = 'A variety of languages can be used';
+$lang->automation->zendataFeature5 = 'Only one executable is required to run it';
+$lang->automation->zendataFeature6 = 'For Windows, Linux, and MacOS';
+$lang->automation->zendataFeature7 = 'For various languages';
 
 $lang->automation->zendataFeature8  = 'All kinds of black tricks';
-$lang->automation->zendataFeature9  = 'Interval, Step Size, Random, Repeat, References';
+$lang->automation->zendataFeature9  = 'Interval, Step Size, Random, Repeat, and Reference';
 $lang->automation->zendataFeature10 = 'Specify a list of files to be queried in Excel';
-$lang->automation->zendataFeature11 = 'Parallel generation, recursive generation, reverse parsing';
+$lang->automation->zendataFeature11 = 'Parallel generation, recursive generation, and reverse parsing';
 
-$lang->automation->groupTips        = 'Welcome to join the automated test QQ communication group';
-$lang->automation->groupTitle       = 'Software test communication group:';
+$lang->automation->groupTips        = 'Welcome to the automated testing QQ group';
+$lang->automation->groupTitle       = 'Software testing group:';
 $lang->automation->qqGroup          = '522506766';
-$lang->automation->groupDescription = 'Software testing technology exchange community for software testing practitioners, sharing software testing tools and methods, help everyone to improve the level of software testing skills.';
+$lang->automation->groupDescription = 'Software testing community for software testers to share software testing tools and methods and improve software testing skills';
