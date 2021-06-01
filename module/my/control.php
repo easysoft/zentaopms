fbc5f1404ec483b221a69d07ef6284da26349f74
holan20180123 <56391770@qq.com>
2021-06-01 15:33:31 +0800

    * Fix bug 12781.

--- module/my/control.php
+++ module/my/control.php
@@ -181,11 +181,7 @@ class my extends control
     public function story($type = 'assignedTo', $orderBy = 'id_desc', $recTotal = 0, $recPerPage = 20, $pageID = 1)
     {
         /* Save session. */
-        if($this->app->viewType != 'json')
-        {
-            $this->session->set('storyList', $this->app->getURI(true), 'product');
-            $this->session->set('storyList', $this->app->getURI(true), 'my');
-        }
+        if($this->app->viewType != 'json') $this->session->set('storyList', $this->app->getURI(true), 'product');
 
         /* Load pager. */
         $this->app->loadClass('pager', $static = true);
