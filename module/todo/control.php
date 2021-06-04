b157d011e81968ad3c7f951925bc8eeaa66073a8
tianshujie98 <tianshujie0123@163.com>
2021-06-04 08:56:21 +0800

    * Fix bug #13022.

--- module/todo/control.php
+++ module/todo/control.php
@@ -394,7 +394,7 @@ class todo extends control
         $this->view->from            = $from;
         $this->view->projects        = $projects;
         $this->view->executions      = $this->loadModel('execution')->getPairs($this->session->project);
-        $this->view->products        = $this->loadModel('product')->getPairs();
+        $this->view->products        = $todo->type == 'opportunity' ? $this->loadModel('product')->getPairsByProjectModel('waterfall') : $this->loadModel('product')->getPairs();
         $this->view->projectProducts = $this->loadModel('product')->getProductPairsByProject($this->session->project);
 
         $this->display();
