08567f098a89edb1c74cc1130320cbc2fdfed79a
Guanxiying <guanxiying@easycorp.ltd>
2021-06-07 18:53:52 +0800

    * Rename jenkins table. + Add pipline module.

---
+++ module/pipline/config.php
@@ -0,0 +1,5 @@
+<?php
+$config->pipline->create = new stdclass();
+$config->pipline->edit   = new stdclass();
+$config->pipline->create->requiredFields = 'name,url,type';
+$config->pipline->edit->requiredFields   = 'name,url,type';
