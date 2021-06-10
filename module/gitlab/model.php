7e8c167b87685fbd110443b6a90b6287990eae15
Lichengjun <lichengjun@easycorp.ltd>
2021-06-10 14:55:50 +0800

    * Add test commit

--- module/gitlab/model.php
+++ module/gitlab/model.php
@@ -99,6 +99,7 @@ class gitlabModel extends model
      * @access public
      * @return array
      */
+
     public function getUserBindList($host, $token)
     {
         $host  = rtrim($host, '/') .'/api/v4/users?private_token='.$token ;
