b3a5fff35ece8ed37a421da01d97e0c19c081ea9
dingguodong <dingguodong@easycorp.ltd>
2021-06-29 13:32:39 +0800

    * Push bug when creating bug using productID.

--- module/bug/control.php
+++ module/bug/control.php
@@ -559,7 +559,9 @@ class bug extends control
 
         /* Set gitlabProjects. */
         $allGitlabs     = $this->loadModel('gitlab')->getPairs();
-        $gitlabProjects = $this->loadModel('gitlab')->getProjectsByExecution($executionID);
+        if(!empty($executionID)) $gitlabProjects = $this->loadModel('gitlab')->getProjectsByExecution($executionID);
+        elseif(!empty($productID)) $gitlabProjects = $this->loadModel('gitlab')->getProjectsByProduct($productID);
+
         foreach($allGitlabs as $id => $name) if($id and !isset($gitlabProjects[$id])) unset($allGitlabs[$id]);
         $this->view->gitlabList     = $allGitlabs;
         $this->view->gitlabProjects = $gitlabProjects;
