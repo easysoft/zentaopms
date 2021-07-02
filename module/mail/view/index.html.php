790dcefa53315ec4e72dedbc4e7e27ff4ac6a62e
wangyidong <wyd621@163.com>
2021-07-02 16:16:22 +0800

    * remove ztCloud.

--- module/mail/view/index.html.php
+++ module/mail/view/index.html.php
@@ -21,9 +21,6 @@
     <table class='table table-form' id='selectmta'>
       <tr>
         <td class='text-center'>
-          <?php if(!common::checkNotCN() and common::hasPriv('mail', 'ztCloud')):?>
-          <?php echo html::a(inlink('ztCloud'), $lang->mail->ztCloud, '', "class='btn w-120px'")?>
-          <?php endif;?>
           <?php if(common::hasPriv('mail', 'detect')):?>
           <?php echo html::a(inlink('detect'), $lang->mail->smtp, '', "class='btn w-120px'")?>
           <?php endif;?>
