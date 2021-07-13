132d5484f95e3f92c11816cc8b59403a4bcc10e6
tianshujie98 <tianshujie0123@163.com>
2021-07-13 15:17:04 +0800

    * Adjust code.

--- module/project/model.php
+++ module/project/model.php
@@ -801,6 +801,7 @@ class projectModel extends model
 
                 $this->dao->insert(TABLE_PRODUCT)->data($product)->exec();
                 $productID = $this->dao->lastInsertId();
+                $this->dao->update(TABLE_PRODUCT)->set('`order`')->eq($productID * 5)->where('id')->eq($productID)->exec();
                 if($product->acl != 'open') $this->loadModel('user')->updateUserView($productID, 'product');
 
                 $projectProduct = new stdclass();
