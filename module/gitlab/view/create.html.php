04467052dcfeb5d3a623ebfb2ec9dc6a77c6f795
dingguodong <dingguodong@easycorp.ltd>
2021-07-14 15:00:35 +0800

    * Optimize back button for bug #13628.

--- module/gitlab/view/create.html.php
+++ module/gitlab/view/create.html.php
@@ -36,8 +36,8 @@
           <tr>
             <th></th>
             <td class='text-center form-actions'>
-              <?php echo html::submitButton();?>
-              <?php echo html::backButton();?>
+              <?php echo html::submitButton(); ?>
+              <?php if(!isonlybody()) echo html::a(inlink('browse', ""), $lang->goback, '', 'class="btn btn-wide"'); ?>
             </td>
           </tr>
         </table>
