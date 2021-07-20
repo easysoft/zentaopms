8ce3d4de2a89dc464edbc3fb5ab098e2ba7ccd02
wangyidong <wyd621@163.com>
2021-07-20 09:04:54 +0800

    * fix error.

--- db/update15.0.3.sql
+++ db/update15.0.3.sql
@@ -6,6 +6,7 @@ ALTER TABLE `zt_storyestimate` MODIFY COLUMN `average` float NOT NULL;
 
 ALTER TABLE `zt_jenkins` ADD COLUMN `type` char(30) NOT NULL AFTER `id`;
 ALTER TABLE `zt_jenkins` ADD COLUMN `private` char(32) NOT NULL AFTER `token`;
+UPDATE `zt_jenkins` SET `type`='jenkins';
 RENAME TABLE `zt_jenkins` TO `zt_pipeline`;
 
 ALTER TABLE `zt_relation` DROP INDEX `relation`;
