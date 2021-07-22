efbfe56d27e861931212bc8cdc2ce6dbeca52d18
dingguodong <dingguodong@easycorp.ltd>
2021-07-22 09:28:36 +0800

    * Comment gitlab menu instead of deleting it.

--- module/common/lang/menu.php
+++ module/common/lang/menu.php
@@ -317,12 +317,14 @@ $lang->qa->dividerMenu = ',bug,testtask,caselib,';
 $lang->devops->menu = new stdclass();
 $lang->devops->menu->code     = array('link' => "{$lang->repo->common}|repo|browse|repoID=%s", 'alias' => 'diff,view,revision,log,blame,showsynccommit');
 $lang->devops->menu->compile  = array('link' => "{$lang->devops->compile}|job|browse", 'subModule' => 'compile,job');
+// $lang->devops->menu->gitlab   = array('link' => "Gitlab|gitlab|browse", 'alias' => 'create,edit');
 $lang->devops->menu->jenkins  = array('link' => "Jenkins|jenkins|browse", 'alias' => 'create,edit');
 $lang->devops->menu->maintain = array('link' => "{$lang->devops->repo}|repo|maintain", 'alias' => 'create,edit');
 $lang->devops->menu->rules    = array('link' => "{$lang->devops->rules}|repo|setrules");
 
 $lang->devops->menuOrder[5]  = 'code';
 $lang->devops->menuOrder[10] = 'compile';
+// $lang->devops->menuOrder[15] = 'gitlab';
 $lang->devops->menuOrder[20] = 'jenkins';
 $lang->devops->menuOrder[25] = 'maintain';
 $lang->devops->menuOrder[30] = 'rules';
