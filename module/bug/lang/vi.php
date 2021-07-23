b9156e2ce1ca59e00de3335e8b9af0a037e9a597
tianshujie98 <tianshujie@easycorp.ltd>
2021-07-23 10:16:57 +0800

    * Add lang.

--- module/bug/lang/vi.php
+++ module/bug/lang/vi.php
@@ -79,6 +79,7 @@ $lang->bug->lastEditedDate   = 'Ngày sửa';
 $lang->bug->fromCase         = 'Từ tình huống';
 $lang->bug->toCase           = 'Tới tình huống';
 $lang->bug->colorTag         = 'Màu';
+$lang->bug->repairRate       = 'Repair Rate';
 
 /* Method list. */
 $lang->bug->index              = 'Trang Bug';
