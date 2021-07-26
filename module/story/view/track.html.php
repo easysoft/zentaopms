9ec3e847d97b5fec0454a858a34131470834e69b
wangyidong <wyd621@163.com>
2021-07-26 13:07:31 +0800

    * code for task #40466.

--- module/story/view/track.html.php
+++ module/story/view/track.html.php
@@ -58,7 +58,7 @@
           <?php $rowspan = count($track);?>
           <tr>
             <?php if($config->URAndSR):?>
-            <td <?php if($rowspan != 0) echo "rowspan=" . $rowspan;?> class='requirement'>
+            <td <?php if($rowspan != 0) echo "rowspan=" . $rowspan;?> class='requirement' title='<?php echo $key != 'noRequirement' ? $requirement->title : $lang->story->noRequirement;?>'>
               <?php if($key != 'noRequirement'):?>
               <span class="label label-primary label-outline"><?php echo zget($lang->story->statusList, $requirement->status);?></span>
               <?php endif;?>
