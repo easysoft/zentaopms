4e99693ed9ed1b67b1f4689dc0261a751356e122
dingguodong <dingguodong@easycorp.ltd>
2021-07-26 16:24:32 +0800

    - Remove gitlabpipeline.

--- module/gitlabpipeline/lang/zh-cn.php
+++
@@ -1,4 +0,0 @@
-<?php
-$lang->gitlabpipeline->runPipeline = '运行流水线'; // run pipeline manually
-$lang->gitlabpipeline->sendExec    = '流水线已运行！当前状态为：%s';
-$lang->gitlabpipeline->execError   = '不要重复运行流水线，请等待运行结果！';
