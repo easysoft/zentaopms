d909bbcf0f3f444eaca3e05b0dfb9a2d12e684d2
zenggang <zenggang@easycorp.ltd>
2021-08-06 10:56:20 +0800

    * Finish task #40894

--- module/execution/js/edit.js
+++ module/execution/js/edit.js
@@ -19,4 +19,10 @@ $(function()
             $(this).siblings('div').find('span').attr('title', tip);
         }
     });
+    
+    oldProject = $("#project").val();
+    $('#project').change(function()
+    {
+        if(!confirm('修改所属项目后，执行关联的原项目需求如果要更改所属项目，需手动修改到新项目，是否继续修改？')) $("#project").val(oldProject).trigger("chosen:updated");
+    });
 })
