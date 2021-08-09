26ce07e38d6a0b9465d22fcf0baf62d5581e383a
hufangzhou <hufangzhou@easycorp.ltd>
2021-08-09 13:53:49 +0800

    * Fix bug #13762.

--- module/task/model.php
+++ module/task/model.php
@@ -3016,7 +3016,7 @@ class taskModel extends model
                 echo $this->lang->task->typeList[$task->type];
                 break;
             case 'status':
-                $storyChanged ? print("<span class='status-story status-changed'>{$this->lang->story->changed}</span>") : print("<span class='status-task status-{$task->status}'> " . $this->processStatus('task', $task) . "</span>");
+                $storyChanged ? print("<span class='status-story status-changed' title='{$this->lang->story->changed}'>{$this->lang->story->changed}</span>") : print("<span class='status-task status-{$task->status}' title='{$this->processStatus('task', $task)}'> " . $this->processStatus('task', $task) . "</span>");
                 break;
             case 'estimate':
                 echo round($task->estimate, 1) . $this->lang->execution->workHourUnit;
