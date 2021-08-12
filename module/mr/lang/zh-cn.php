b8113c047b6734464f1f4a9bd557c0c93421bbb4
lichengjun <lichengjun@easycorp.ltd>
2021-08-12 06:24:48 +0000

    * Add Language configuration items.

--- module/mr/lang/zh-cn.php
+++ module/mr/lang/zh-cn.php
@@ -2,3 +2,12 @@
 $lang->mr->common = "合并请求";
 $lang->mr->create = "创建{$lang->mr->common}";
 $lang->mr->add    = "添加GitLab项目";
+
+
+$lang->mr->id          = 'ID';
+$lang->mr->name        = '名称';
+$lang->mr->source      = '源项目分支';
+$lang->mr->targer      = '目标分支';
+$lang->mr->pipeline    = '流水线';
+$lang->mr->audit       = '审核状态';
+$lang->mr->execute     = '执行状态';
