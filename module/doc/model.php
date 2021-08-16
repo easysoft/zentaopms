0d208f38f596b389903383ab5a49ee4291f22e28
holan20180123 <liyuchun@easycorp.ltd>
2021-08-16 13:16:20 +0800

    * Finish task #41245.

--- module/doc/model.php
+++ module/doc/model.php
@@ -515,6 +515,7 @@ class docModel extends model
             if(empty($docContent->content)) return dao::$errors['content'] = sprintf($this->lang->error->notempty, $this->lang->doc->content);
         }
 
+        $doc->tempContent = $doc->content;
         $this->dao->insert(TABLE_DOC)->data($doc, 'content')->autoCheck()
             ->batchCheck($requiredFields, 'notempty')
             ->exec();
