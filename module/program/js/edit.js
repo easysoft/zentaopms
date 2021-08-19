45c1153e17fbdd5e16763137f3ba8f8d3ff80683
tianshujie98 <tianshujie@easycorp.ltd>
2021-08-19 09:36:08 +0800

    * Finish task #41573,41575.

--- module/program/js/edit.js
+++ module/program/js/edit.js
@@ -63,6 +63,11 @@ $(function()
             alert(exRateNotEmpty);
             return false;
         }
+        else if(isNaN(exchangeRate))
+        {
+            alert(exRateNum);
+            return false;
+        }
         else if(exchangeRate < 0)
         {
             alert(exRateNotNegative);
