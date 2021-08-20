c385974191f15833fc7fe227ff8a0315a5a389d1
tianshujie98 <tianshujie@easycorp.ltd>
2021-08-20 08:10:44 +0800

    * Finish task #41584.

--- module/projectstory/control.php
+++ module/projectstory/control.php
@@ -36,9 +36,6 @@ class projectStory extends control
      */
     public function story($projectID = 0, $productID = 0, $branch = 0, $browseType = '', $param = 0, $storyType = 'story', $orderBy = '', $recTotal = 0, $recPerPage = 20, $pageID = 1)
     {
-        $this->session->set('storyList', $this->app->getURI(true), 'product');
-        $this->session->set('executionList', $this->app->getURI(true), 'execution');
-
         $this->products = $this->loadModel('product')->getProductPairsByProject($projectID);
         if(empty($this->products)) die($this->locate($this->createLink('product', 'showErrorNone', 'moduleName=project&activeMenu=story&projectID=' . $projectID)));
         echo $this->fetch('product', 'browse', "productID=$productID&branch=$branch&browseType=$browseType&param=$param&storyType=$storyType&orderBy=$orderBy&recTotal=$recTotal&recPerPage=$recPerPage&pageID=$pageID&projectID=$projectID");
