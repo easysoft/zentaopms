38f7285ff41ad3214ef549fd1b92926a3c66eb6f
dingguodong <dingguodong@easycorp.ltd>
2021-08-23 17:01:40 +0800

    * Update zt_mr table.

--- db/zentao.sql
+++ db/zentao.sql
@@ -546,11 +546,13 @@ CREATE TABLE IF NOT EXISTS `zt_module` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 -- DROP TABLE IF EXISTS `zt_mr`;
 CREATE TABLE `zt_mr` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
-  `repoID` mediumint(8) unsigned NOT NULL,
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `gitlabID` mediumint(8) unsigned NOT NULL,
-  `projectID` mediumint(8) unsigned NOT NULL,
-  `mrID` mediumint(8) unsigned NOT NULL,
+  `sourceProject` int unsigned NOT NULL,
+  `sourceBranch` varchar(100) NOT NULL,
+  `targetProject` int unsigned NOT NULL,
+  `targetBranch` varchar(100) NOT NULL,
+  `mriid` int unsigned NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `assignee` varchar(255) NOT NULL,
@@ -560,6 +562,8 @@ CREATE TABLE `zt_mr` (
   `editedBy` varchar(30) NOT NULL,
   `editedDate` datetime NOT NULL,
   `deleted` tinyint(1) NOT NULL,
+  `status` char(30) NOT NULL,
+  `mergeStatus` char(30) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 -- DROP TABLE IF EXISTS `zt_notify`;
