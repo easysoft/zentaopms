a9b309fefc19c82c80c88bdcd48ff70ab178ae0b
王怡栋 <wangyidong@cnezsoft.com>
2021-08-24 13:39:45 +0800

    * adjust for lower php version.

--- module/my/view/bug.html.php
+++ module/my/view/bug.html.php
@@ -108,7 +108,12 @@
           <td><span class='label-pri <?php echo 'label-pri-' . $bug->pri?>' title='<?php echo zget($lang->bug->priList, $bug->pri);?>'><?php echo zget($lang->bug->priList, $bug->pri)?></span></td>
           <td class='text-left nobr'><?php echo html::a($this->createLink('bug', 'view', "bugID=$bug->id"), $bug->title, null, "style='color: $bug->color' title={$bug->title}");?></td>
           <?php $param = $config->productLink == 'product-all' ? '' : "productID=$bug->product";?>
-          <td class='text-left nobr'><?php echo html::a($this->createLink('product', explode('-', $config->productLink)[1], $param), $bug->productName, null, "title={$bug->productName}");?></td>
+          <td class='text-left nobr'>
+            <?php
+            $productLink = explode('-', $config->productLink);
+            echo html::a($this->createLink('product', $productLink[1], $param), $bug->productName, null, "title={$bug->productName}");
+            ?>
+          </td>
           <td title="<?php echo zget($lang->bug->typeList, $bug->type, '');?>"><?php echo zget($lang->bug->typeList, $bug->type, '');?></td>
           <?php if($type != 'openedBy'): ?>
           <td><?php echo zget($users, $bug->openedBy);?></td>
