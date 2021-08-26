8c32895ad801b05cb7190236d4d4e3cea27589c0
Guan Xiying <guanxiying@easycorp.ltd>
2021-08-26 17:44:44 +0800

    * Fix comments.

--- module/mr/view/create.html.php
+++ module/mr/view/create.html.php
@@ -3,7 +3,7 @@
  * The create view file of mr module of ZenTaoPMS.
  *
  * @copyright   Copyright 2009-2012 青岛易软天创网络科技有限公司 (QingDao Nature Easy Soft Network Technology Co,LTD www.cnezsoft.com)
- * @author      Wang Yidong, Zhu Jinyong
+ * @author      Guodong
  * @package     mr
  * @version     $Id: create.html.php $
  */
