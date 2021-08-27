b97e80568efa89603e770f79095de23e953ee6f2
tianshujie <tianshujie@easycorp.ltd>
2021-08-27 15:42:03 +0800

    * Modify the testsuite return link problem.

--- module/testsuite/control.php
+++ module/testsuite/control.php
@@ -294,10 +294,11 @@ class testsuite extends control
      * @param  int    $recTotal
      * @param  int    $recPerPage
      * @param  int    $pageID
+     * @param  string $from
      * @access public
      * @return void
      */
-    public function linkCase($suiteID, $param = 0, $recTotal = 0, $recPerPage = 20, $pageID = 1)
+    public function linkCase($suiteID, $param = 0, $recTotal = 0, $recPerPage = 20, $pageID = 1, $from = 'browse')
     {
         /* Save session. */
         $this->session->set('caseList', $this->app->getURI(true), 'qa');
@@ -341,6 +342,7 @@ class testsuite extends control
         $this->view->suiteID = $suiteID;
         $this->view->pager   = $pager;
         $this->view->suite   = $suite;
+        $this->view->from    = $from;
 
         $this->display();
     }
