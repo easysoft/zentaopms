99fdb293270306b3f0b1d188d4d0bf94b1ab44fa
lichengjun <lichengjun@easycorp.ltd>
2021-08-30 07:04:48 +0000

    * Adjust GitLab view html.

--- module/gitlab/view/view.html.php
+++ module/gitlab/view/view.html.php
@@ -12,13 +12,8 @@
 ?>
 <?php include '../../common/view/header.html.php';?>
 <?php js::set('sysurl', common::getSysUrl());?>
-<?php $browseLink = $app->session->gitlabList ? $app->session->gitlabList : inlink('browse');?>
 <div id="mainMenu" class="clearfix">
   <div class="btn-toolbar pull-left">
-    <?php if(!isonlybody()):?>
-    <?php echo html::a($browseLink, '<i class="icon icon-back icon-sm"></i> ' . $lang->goback, '', "class='btn btn-secondary'");?>
-    <div class="divider"></div>
-    <?php endif;?>
     <div class="page-title">
       <span class="label label-id"><?php echo $gitlab->id?></span>
       <span class="text" title="<?php echo $gitlab->name;?>" style='color: #3c4354'><?php echo $gitlab->name;?></span>
@@ -27,28 +22,9 @@
 </div>
 <div id="mainContent" class="main-row">
   <div class="main-col col-8">
-    <?php $this->printExtendFields($gitlab, 'div', "position=left&inForm=0&inCell=1");?>
     <div class='cell'><?php include '../../common/view/action.html.php';?></div>
-    <?php
-    $params        = "gitlabID=$gitlab->id";
-    ?>
-    <div class='main-actions'>
-      <div class="btn-toolbar">
-        <?php common::printBack($browseLink);?>
-        <?php if(!$gitlab->deleted):?>
-        <div class='divider'></div>
-        <?php
-        common::printIcon('gitlab', 'delete', $params, $gitlab, 'button', 'trash', 'hiddenwin');
-        ?>
-        <?php endif;?>
-      </div>
-    </div>
   </div>
 </div>
 
-<div id="mainActions" class='main-actions'>
-  <?php common::printPreAndNext($preAndNext);?>
-</div>
-<?php include '../../common/view/syntaxhighlighter.html.php';?>
 <?php include '../../common/view/footer.html.php';?>
 
