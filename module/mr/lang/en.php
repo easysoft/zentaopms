94f4d9110700bb0cabd0dbdca09ad98e462de0d9
dingguodong <dingguodong@easycorp.ltd>
2021-09-06 11:29:52 +0800

    * Modify en lang for MR.

--- module/mr/lang/en.php
+++ module/mr/lang/en.php
@@ -9,16 +9,16 @@ $lang->mr->view         = "view";
 $lang->mr->source       = 'source';
 $lang->mr->target       = 'target';
 $lang->mr->viewDiff     = 'view diff';
-$lang->mr->viewInGitlab = 'view GitLab';
+$lang->mr->viewInGitlab = 'view in GitLab';
 
 $lang->mr->id          = 'ID';
-$lang->mr->mriid       = "MR原始ID";
-$lang->mr->name        = 'name';
-$lang->mr->status      = 'status';
-$lang->mr->author      = 'author';
-$lang->mr->assignee    = 'assignee';
-$lang->mr->reviewer    = 'reviewer';
-$lang->mr->mergeStatus = 'merge status';
+$lang->mr->mriid       = "raw MR ID";
+$lang->mr->name        = 'Name';
+$lang->mr->status      = 'Status';
+$lang->mr->author      = 'Author';
+$lang->mr->assignee    = 'Assignee';
+$lang->mr->reviewer    = 'Reviewer';
+$lang->mr->mergeStatus = 'Merge status';
 $lang->mr->commits     = 'commits';
 $lang->mr->changes     = 'changes';
 
@@ -32,19 +32,19 @@ $lang->mr->mergeStatusList['checking']         = 'checking';
 $lang->mr->mergeStatusList['can_be_merged']    = 'can be merged';
 $lang->mr->mergeStatusList['cannot_be_merged'] = 'cannot be merged';
 
-$lang->mr->description   = 'description';
+$lang->mr->description   = 'Description';
 $lang->mr->confirmDelete = 'Are you sure to delete this merge request?';
-$lang->mr->sourceProject = 'source project';
-$lang->mr->sourceBranch  = 'source branch';
-$lang->mr->targetProject = 'target project';
-$lang->mr->targetBranch  = 'target branch';
+$lang->mr->sourceProject = 'Source project';
+$lang->mr->sourceBranch  = 'Source branch';
+$lang->mr->targetProject = 'Target project';
+$lang->mr->targetBranch  = 'Target branch';
 
-$lang->mr->usersTips = 'Tip: If you cannot choose the designator and reviewer, please go to the GitLab page to bind the user first.';
-$lang->mr->notFound  = "Merge Reqest does not exist!";
+$lang->mr->usersTips = 'Tip: If you cannot choose the assignee and reviewer, please go to the GitLab page to bind the user first.';
+$lang->mr->notFound  = "Merge Request does not exist!";
 
 $lang->mr->apiError = new stdclass;
-$lang->mr->apiError->createMR = "Failed to create a merge request through API. Reason for failure:%s";
-$lang->mr->apiError->sudo     = "Unable to operate with the GitLab account bound to the current user, the reason for the failure:%s";
+$lang->mr->apiError->createMR = "Failed to create a merge request through API. Reason: %s";
+$lang->mr->apiError->sudo     = "Unable to operate with the GitLab account bound to the current user. Reason: %s";
 
 $lang->mr->createFailedFromAPI = "Failed to create Merge Request.";
 $lang->mr->accessGitlabFailed  = "Unable to connect to the GitLab server.";
@@ -53,10 +53,10 @@ $lang->mr->from = "from";
 $lang->mr->to   = "to";
 $lang->mr->at   = "at";
 
-$lang->mr->pipeline        = "Pipline";
+$lang->mr->pipeline        = "Pipeline";
 $lang->mr->pipelineSuccess = "Success";
 $lang->mr->pipelineFailed  = "Failed";
-$lang->mr->pipelineCancled = "Cancled";
+$lang->mr->pipelineCancled = "Canceled";
 $lang->mr->pipelineUnknown = "Unknown";
 
 $lang->mr->pipelineStatus = array();
@@ -65,8 +65,8 @@ $lang->mr->pipelineStatus['failed']   = "failed";
 $lang->mr->pipelineStatus['canceled'] = "canceled";
 
 $lang->mr->MRHasConflicts = "Merge Request has a conflict";
-$lang->mr->hasConflicts   = "Merge Request conflict";
-$lang->mr->hasNoConflict  = "Can merge requests";
+$lang->mr->hasConflicts   = "There are merge conflicts";
+$lang->mr->hasNoConflict  = "Can merge request";
 $lang->mr->mergeByManual  = "This merge request can be merged manually, please refer to";
 $lang->mr->commandLine    = "Merge Request command";
 $lang->mr->acceptMR       = "Accept Merge request ";
@@ -76,7 +76,7 @@ $lang->mr->mergeSuccess   = "Merge Request Successfully";
 /**
  * Merge Command Document.
  *
- * %s source_roject::http_url_to_repo
+ * %s source_project::http_url_to_repo
  * %s mr::source_branch
  * %s source_project::path_with_namespace . '-' . mr::source_branch
  * %s mr::target_branch
@@ -84,26 +84,26 @@ $lang->mr->mergeSuccess   = "Merge Request Successfully";
  * %s mr::target_branch
  */
 $lang->mr->commandDocument = <<< EOD
-<div class='detail-title'>Check, Review and Merge manually locally</div>
+<div class='detail-title'>Check out, review and merge locally</div>
 <div class='detail-content'>
   <p>
-    step 1. Get and view the branches of this merge request.
+    step 1. Fetch and check out the branch for this merge request
     <pre>
     git fetch "%s" %s
     git checkout -b "%s" FETCH_HEAD</pre>
   </p>
   <p>
-    step 2. View changes locally
+    step 2. Review the changes locally
   </p>
   <p>
-    step 3. Merge branches and resolve any conflicts that arise
+    step 3. Merge the branch and fix any conflicts that come up
     <pre>
     git fetch origin
     git checkout "%s"
     git merge --no-ff "%s"</pre>
   </p>
   <p>
-    step 4. Push the merge  to GitLab
+    step 4. Push the result of the merge to GitLab
     <pre> git push origin "%s" </pre>
   </p>
 </div>
