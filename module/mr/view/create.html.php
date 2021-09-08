6e3be6bdf2c3361aa8812e04464534f494344cc8
Guan Xiying <guanxiying@easycorp.ltd>
2021-09-08 10:27:05 +0800

    * Fix bug #15006.

--- module/mr/view/create.html.php
+++ module/mr/view/create.html.php
@@ -62,11 +62,11 @@
             <td><?php echo $lang->mr->usersTips;?></td>
           </tr>
           <tr>
-            <th></th>
-            <td colspan='2' class='text-left form-actions'>
+            <td colspan='2' class='text-center form-actions'>
               <?php echo html::submitButton(); ?>
               <?php if(!isonlybody()) echo html::a(inlink('browse', ""), $lang->goback, '', 'class="btn btn-wide"');?>
             </td>
+            <td></td>
           </tr>
         </table>
       </form>
