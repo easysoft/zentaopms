bf5242da87caef0abeda8e52107e4d86d1a7d885
tianshujie <tianshujie@easycorp.ltd>
2021-09-08 14:12:10 +0800

    * Finish task #42413.

--- module/task/js/editestimate.js
+++ module/task/js/editestimate.js
@@ -1,7 +1,14 @@
+/**
+ * Confirm left.
+ *
+ * @access public
+ * @return void
+ */
 function confirmLeft()
 {
     if($('#left').val() === '0') return confirm(confirmRecord);
 }
+
 $(function()
 {
     $('.form-date').datetimepicker('setEndDate', today);
