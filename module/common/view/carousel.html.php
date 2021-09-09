6b3b5661dba877bdcb79de8f4f072bad8ad088e5
Hao Sun <sunhao@easycorp.ltd>
2021-09-09 12:33:32 +0800

    + add corousel component.

---
+++ module/common/view/carousel.html.php
@@ -0,0 +1,3 @@
+<?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
+<?php css::import($jsRoot . 'zui/carousel/min.css'); ?>
+<?php js::import($jsRoot . 'zui/carousel/min.js'); ?>
