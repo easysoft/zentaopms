a3205654597eb9f1d3e65c0e0399ae8a05f8fffa
tianshujie <tianshujie@easycorp.ltd>
2021-09-14 09:28:02 +0800

    * Fix bug #14858.

--- module/task/view/create.html.php
+++ module/task/view/create.html.php
@@ -15,6 +15,12 @@
 <?php include '../../common/view/sortable.html.php';?>
 <?php js::set('toTaskList', !empty($task->id));?>
 <?php js::set('blockID', $blockID);?>
+<?php if(!empty($storyID)):?>
+<style>
+.title-group.required > .required:after {right: 83px;}
+.input-control .colorpicker .btn {right: 10px;}
+</style>
+<?php endif;?>
 <div id='mainContent' class='main-content'>
   <div class='center-block'>
     <div class='main-header'>
