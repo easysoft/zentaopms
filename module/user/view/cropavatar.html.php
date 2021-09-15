bc312bfe2df306a84324416849feee16dd682729
Hao Sun <sunhao@easycorp.ltd>
2021-09-15 19:23:02 +0800

    * improve UI for cropavatar dialog and fix conflict with autosize feature of ModalTrigger.

--- module/user/view/cropavatar.html.php
+++ module/user/view/cropavatar.html.php
@@ -14,27 +14,21 @@
 <script> /*! TangBin: image.ready.js http://www.planeart.cn/?p=1121 */ !function(n){"use strict";n.zui.imgReady=function(){var n=[],l=null,e=function(){for(var l=0;l<n.length;l++)n[l].end?n.splice(l--,1):n[l]();!n.length&&o()},o=function(){clearInterval(l),l=null};return function(o,r,t,u){var c,i,a,d,f,h=new Image;return h.src=o,h.complete?(r.call(h),void(t&&t.call(h))):(i=h.width,a=h.height,h.onerror=function(){u&&u.call(h),c.end=!0,h=h.onload=h.onerror=null},c=function(){d=h.width,f=h.height,(d!==i||f!==a||d*f>1024)&&(r.call(h),c.end=!0)},c(),h.onload=function(){!c.end&&c(),t&&t.call(h),h=h.onload=h.onerror=null},void(c.end||(n.push(c),null===l&&(l=setInterval(e,40)))))}}()}(jQuery); </script>
 <?php js::import($this->app->getWebRoot() . 'js/zui/imgcutter/min.js');?>
 <?php css::import($this->app->getWebRoot() . 'js/zui/imgcutter/min.css');?>
-<div id='mainContent' class='main-content'>
+<div id='mainContent'>
   <div class='center-block'>
     <div class='main-header'>
       <h2><?php echo $lang->user->cropAvatar;?></h2>
     </div>
-    <table class='table table-form'>
-      <tr>
-        <td>
-          <div class="img-cutter fixed-ratio" id="imgCutter" style="max-width: 100%">
-            <div class="canvas">
-              <?php echo html::image($image->webPath);?>
-            </div>
-            <div class="form-actions">
-              <h5 id='avatarCropTip'><?php echo $lang->user->cropAvatarTip;?></h5>
-              <div class="img-cutter-preview"></div>
-              <button type="button" class="btn btn-primary img-cutter-submit"><?php echo $lang->save;?></button>
-            </div>
-          </div>
-        </td>
-      </tr>
-    </table>
+    <div class="img-cutter fixed-ratio" id="imgCutter" style="max-width: 540px">
+      <div class="canvas">
+        <?php echo html::image($image->webPath);?>
+      </div>
+      <div class="form-actions">
+        <h5 id='avatarCropTip'><?php echo $lang->user->cropAvatarTip;?></h5>
+        <div class="img-cutter-preview"></div>
+        <button type="button" class="btn btn-primary img-cutter-submit"><?php echo $lang->save;?></button>
+      </div>
+    </div>
   </div>
 </div>
 <script>
