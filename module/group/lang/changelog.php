86c8930cddcf986a404bffde20cfd722b275591d
王怡栋 <wangyidong@cnezsoft.com>
2021-09-22 16:48:09 +0800

    * adjust for apache error.

---
+++ module/group/lang/changelog.php
@@ -0,0 +1,426 @@
+<?php
+/* Every version of new privilege. */
+$lang->changelog['1.0.1'][] = 'project-computeBurn';
+
+$lang->changelog['1.1'][]   = 'search-saveQuery';
+$lang->changelog['1.1'][]   = 'search-deleteQuery';
+
+$lang->changelog['1.2'][]   = 'product-doc';
+$lang->changelog['1.2'][]   = 'project-doc';
+$lang->changelog['1.2'][]   = 'bug-saveTemplate';
+$lang->changelog['1.2'][]   = 'bug-deleteTemplate';
+$lang->changelog['1.2'][]   = 'doc-index';
+$lang->changelog['1.2'][]   = 'doc-browse';
+$lang->changelog['1.2'][]   = 'doc-createLib';
+$lang->changelog['1.2'][]   = 'doc-editLib';
+$lang->changelog['1.2'][]   = 'doc-deleteLib';
+$lang->changelog['1.2'][]   = 'doc-create';
+$lang->changelog['1.2'][]   = 'doc-view';
+$lang->changelog['1.2'][]   = 'doc-edit';
+$lang->changelog['1.2'][]   = 'doc-delete';
+$lang->changelog['1.2'][]   = 'doc-deleteFile';
+
+$lang->changelog['1.3'][]   = 'task-start';
+$lang->changelog['1.3'][]   = 'task-complete';
+$lang->changelog['1.3'][]   = 'task-cancel';
+$lang->changelog['1.3'][]   = 'file-delete';
+
+$lang->changelog['1.4'][]   = 'my-testTask';
+$lang->changelog['1.4'][]   = 'my-testCase';
+$lang->changelog['1.4'][]   = 'task-finish';
+$lang->changelog['1.4'][]   = 'task-close';
+$lang->changelog['1.4'][]   = 'task-activate';
+$lang->changelog['1.4'][]   = 'search-select';
+
+$lang->changelog['1.5'][]   = 'task-batchClose';
+
+$lang->changelog['2.0'][]   = 'my-dynamic';
+$lang->changelog['2.0'][]   = 'bug-export';
+$lang->changelog['2.0'][]   = 'story-export';
+$lang->changelog['2.0'][]   = 'story-reportChart';
+$lang->changelog['2.0'][]   = 'task-export';
+$lang->changelog['2.0'][]   = 'task-reportChart';
+$lang->changelog['2.0'][]   = 'taskcase-export';
+$lang->changelog['2.0'][]   = 'company-dynamic';
+$lang->changelog['2.0'][]   = 'user-dynamic';
+$lang->changelog['2.0'][]   = 'extension-browse';
+$lang->changelog['2.0'][]   = 'extension-obtain';
+$lang->changelog['2.0'][]   = 'extension-install';
+$lang->changelog['2.0'][]   = 'extension-uninstall';
+$lang->changelog['2.0'][]   = 'extension-activate';
+$lang->changelog['2.0'][]   = 'extension-deactivate';
+$lang->changelog['2.0'][]   = 'extension-upload';
+$lang->changelog['2.0'][]   = 'extension-erase';
+
+$lang->changelog['2.1'][]   = 'extension-upgrade';
+
+$lang->changelog['2.2'][]   = 'file-edit';
+
+$lang->changelog['2.3'][]   = 'product-dynamic';
+$lang->changelog['2.3'][]   = 'project-dynamic';
+$lang->changelog['2.3'][]   = 'project-importBug';
+$lang->changelog['2.3'][]   = 'story-batchCreate';
+$lang->changelog['2.3'][]   = 'task-batchCreate';
+$lang->changelog['2.3'][]   = 'testcase-batchCreate';
+$lang->changelog['2.3'][]   = 'bug-confirmBug';
+$lang->changelog['2.3'][]   = 'svn-diff';
+$lang->changelog['2.3'][]   = 'svn-cat';
+$lang->changelog['2.3'][]   = 'svn-apiSync';
+
+$lang->changelog['2.4'][]   = 'task-assign';
+$lang->changelog['2.4'][]   = 'project-testtask';
+$lang->changelog['2.4'][]   = 'todo-export';
+$lang->changelog['2.4'][]   = 'product-project';
+
+$lang->changelog['3.0.beta2'][] = 'extension-structure';
+$lang->changelog['3.0.beta2'][] = 'product-order';
+$lang->changelog['3.0.beta2'][] = 'project-order';
+
+$lang->changelog['3.1'][] = 'todo-batchCreate';
+
+$lang->changelog['3.2'][] = 'my-changePassword';
+$lang->changelog['3.2'][] = 'story-batchClose';
+$lang->changelog['3.2'][] = 'task-batchEdit';
+$lang->changelog['3.2'][] = 'release-export';
+$lang->changelog['3.2'][] = 'report-index';
+$lang->changelog['3.2'][] = 'report-projectDeviation';
+$lang->changelog['3.2'][] = 'report-productSummary';
+$lang->changelog['3.2'][] = 'report-bugCreate';
+$lang->changelog['3.2'][] = 'report-workload';
+$lang->changelog['3.2'][] = 'tree-fix';
+
+$lang->changelog['3.3'][] = 'report-bugAssign';
+
+$lang->changelog['4.0.beta1'][] = 'user-batchCreate';
+$lang->changelog['4.0.beta1'][] = 'user-unlock';
+$lang->changelog['4.0.beta1'][] = 'admin-checkDB';
+
+$lang->changelog['4.0.beta2'][] = 'todo-batchEdit';
+$lang->changelog['4.0.beta2'][] = 'story-batchEdit';
+$lang->changelog['4.0.beta2'][] = 'bug-batchEdit';
+$lang->changelog['4.0.beta2'][] = 'testcase-batchEdit';
+$lang->changelog['4.0.beta2'][] = 'testtask-batchRun';
+$lang->changelog['4.0.beta2'][] = 'user-batchEdit';
+$lang->changelog['4.0.beta2'][] = 'user-manageContacts';
+$lang->changelog['4.0.beta2'][] = 'user-deleteContacts';
+
+$lang->changelog['4.0'][] = 'todo-finish';
+$lang->changelog['4.0'][] = 'product-close';
+$lang->changelog['4.0'][] = 'project-start';
+$lang->changelog['4.0'][] = 'project-activate';
+$lang->changelog['4.0'][] = 'project-putoff';
+$lang->changelog['4.0'][] = 'project-suspend';
+$lang->changelog['4.0'][] = 'project-close';
+$lang->changelog['4.0'][] = 'task-record';
+$lang->changelog['4.0'][] = 'testtask-start';
+$lang->changelog['4.0'][] = 'testtask-close';
+$lang->changelog['4.0'][] = 'action-hideOne';
+$lang->changelog['4.0'][] = 'action-hideAll';
+$lang->changelog['4.0'][] = 'task-editEstimate';
+$lang->changelog['4.0'][] = 'task-deleteEstimate';
+
+$lang->changelog['4.1'][] = 'todo-batchFinish';
+$lang->changelog['4.1'][] = 'productplan-batchUnlinkStory';
+$lang->changelog['4.1'][] = 'company-view';
+$lang->changelog['4.1'][] = 'user-story';
+$lang->changelog['4.1'][] = 'user-testTask';
+$lang->changelog['4.1'][] = 'user-testCase';
+
+$lang->changelog['4.2.beta'][] = 'tree-browseTask';
+
+$lang->changelog['4.3.beta'][] = 'product-batchEdit';
+$lang->changelog['4.3.beta'][] = 'project-batchEdit';
+$lang->changelog['4.3.beta'][] = 'story-batchReview';
+$lang->changelog['4.3.beta'][] = 'story-batchChangePlan';
+$lang->changelog['4.3.beta'][] = 'story-batchChangeStage';
+$lang->changelog['4.3.beta'][] = 'productplan-linkBug';
+$lang->changelog['4.3.beta'][] = 'productplan-unlinkBug';
+$lang->changelog['4.3.beta'][] = 'productplan-batchUnlinkBug';
+$lang->changelog['4.3.beta'][] = 'bug-batchCreate';
+$lang->changelog['4.3.beta'][] = 'testcase-exportTemplet';
+$lang->changelog['4.3.beta'][] = 'testcase-import';
+$lang->changelog['4.3.beta'][] = 'testcase-showImport';
+$lang->changelog['4.3.beta'][] = 'testcase-confirmChange';
+$lang->changelog['4.3.beta'][] = 'mail-reset';
+$lang->changelog['4.3.beta'][] = 'api-debug';
+$lang->changelog['4.3.beta'][] = 'action-editComment';
+
+$lang->changelog['5.0.beta1'][] = 'bug-batchConfirm';
+$lang->changelog['5.0.beta1'][] = 'bug-batchResolve';
+$lang->changelog['5.0.beta1'][] = 'custom-index';
+$lang->changelog['5.0.beta1'][] = 'custom-set';
+$lang->changelog['5.0.beta1'][] = 'custom-restore';
+
+$lang->changelog['5.0.beta2'][] = 'git-diff';
+$lang->changelog['5.0.beta2'][] = 'git-cat';
+$lang->changelog['5.0.beta2'][] = 'git-apiSync';
+
+$lang->changelog['5.3'][] = 'bug-batchClose';
+
+$lang->changelog['6.1'][] = 'story-zeroCase';
+$lang->changelog['6.1'][] = 'testcase-groupCase';
+$lang->changelog['6.1'][] = 'testtask-groupCase';
+
+$lang->changelog['6.2'][] = 'task-pause';
+$lang->changelog['6.2'][] = 'task-restart';
+$lang->changelog['6.2'][] = 'testcase-createBug';
+
+$lang->changelog['6.3'][] = 'bug-batchAssignTo';
+$lang->changelog['6.3'][] = 'task-batchAssignTo';
+$lang->changelog['6.3'][] = 'file-uploadImages';
+$lang->changelog['6.3'][] = 'project-batchUnlinkStory';
+
+$lang->changelog['6.4'][] = 'api-sql';
+$lang->changelog['6.4'][] = 'backup-index';
+$lang->changelog['6.4'][] = 'backup-backup';
+$lang->changelog['6.4'][] = 'backup-restore';
+$lang->changelog['6.4'][] = 'backup-delete';
+$lang->changelog['6.4'][] = 'build-linkStory';
+$lang->changelog['6.4'][] = 'build-unlinkStory';
+$lang->changelog['6.4'][] = 'build-batchUnlinkStory';
+$lang->changelog['6.4'][] = 'build-linkBug';
+$lang->changelog['6.4'][] = 'build-unlinkBug';
+$lang->changelog['6.4'][] = 'build-batchUnlinkBug';
+$lang->changelog['6.4'][] = 'dept-edit';
+$lang->changelog['6.4'][] = 'release-linkStory';
+$lang->changelog['6.4'][] = 'release-unlinkStory';
+$lang->changelog['6.4'][] = 'release-batchUnlinkStory';
+$lang->changelog['6.4'][] = 'release-linkBug';
+$lang->changelog['6.4'][] = 'release-unlinkBug';
+$lang->changelog['6.4'][] = 'release-batchUnlinkBug';
+$lang->changelog['6.4'][] = 'story-batchAssignTo';
+
+$lang->changelog['7.1'][] = 'cron-index';
+$lang->changelog['7.1'][] = 'cron-turnon';
+$lang->changelog['7.1'][] = 'cron-create';
+$lang->changelog['7.1'][] = 'cron-edit';
+$lang->changelog['7.1'][] = 'cron-toggle';
+$lang->changelog['7.1'][] = 'cron-delete';
+$lang->changelog['7.1'][] = 'mail-browse';
+$lang->changelog['7.1'][] = 'mail-delete';
+$lang->changelog['7.1'][] = 'mail-batchDelete';
+$lang->changelog['7.1'][] = 'dev-api';
+$lang->changelog['7.1'][] = 'dev-db';
+
+$lang->changelog['7.2'][] = 'admin-safeIndex';
+$lang->changelog['7.2'][] = 'admin-checkWeak';
+$lang->changelog['7.2'][] = 'backup-change';
+$lang->changelog['7.2'][] = 'custom-flow';
+$lang->changelog['7.2'][] = 'group-manageView';
+$lang->changelog['7.2'][] = 'product-updateOrder';
+$lang->changelog['7.2'][] = 'project-updateOrder';
+
+$lang->changelog['7.3'][] = 'project-fixFirst';
+$lang->changelog['7.3'][] = 'productplan-batchEdit';
+$lang->changelog['7.3'][] = 'admin-sso';
+$lang->changelog['7.3'][] = 'cron-openProcess';
+$lang->changelog['7.3'][] = 'mail-sendCloud';
+$lang->changelog['7.3'][] = 'mail-sendcloudUser';
+
+$lang->changelog['7.4.beta'][] = 'release-changeStatus';
+$lang->changelog['7.4.beta'][] = 'user-unbind';
+$lang->changelog['7.4.beta'][] = 'branch-manage';
+$lang->changelog['7.4.beta'][] = 'branch-delete';
+$lang->changelog['7.4.beta'][] = 'my-unbind';
+
+$lang->changelog['8.0'][] = 'story-batchChangeBranch';
+
+$lang->changelog['8.0.1'][] = 'bug-linkBugs';
+$lang->changelog['8.0.1'][] = 'story-linkStory';
+$lang->changelog['8.0.1'][] = 'testcase-linkCases';
+
+$lang->changelog['8.1.3'][] = 'story-batchChangeModule';
+$lang->changelog['8.1.3'][] = 'task-batchChangeModule';
+$lang->changelog['8.1.3'][] = 'bug-batchChangeModule';
+$lang->changelog['8.1.3'][] = 'testcase-batchChangeModule';
+$lang->changelog['8.1.3'][] = 'my-manageContacts';
+$lang->changelog['8.1.3'][] = 'my-deleteContacts';
+
+$lang->changelog['8.2.beta'][] = 'product-all';
+$lang->changelog['8.2.beta'][] = 'project-tree';
+$lang->changelog['8.2.beta'][] = 'project-all';
+$lang->changelog['8.2.beta'][] = 'project-kanban';
+$lang->changelog['8.2.beta'][] = 'project-tree';
+
+$lang->changelog['8.3'][] = 'doc-allLibs';
+$lang->changelog['8.3'][] = 'doc-objectLibs';
+$lang->changelog['8.3'][] = 'doc-showFiles';
+
+$lang->changelog['8.4'][] = 'branch-sort';
+$lang->changelog['8.4'][] = 'story-bugs';
+$lang->changelog['8.4'][] = 'story-cases';
+
+$lang->changelog['9.0'][] = 'testcase-bugs';
+$lang->changelog['9.0'][] = 'mail-resend';
+
+$lang->changelog['9.1'][] = 'testcase-review';
+$lang->changelog['9.1'][] = 'testcase-batchReview';
+$lang->changelog['9.1'][] = 'testcase-importFromLib';
+$lang->changelog['9.1'][] = 'testcase-batchCaseTypeChange';
+$lang->changelog['9.1'][] = 'testcase-batchConfirmStoryChange';
+$lang->changelog['9.1'][] = 'testreport-browse';
+$lang->changelog['9.1'][] = 'testreport-create';
+$lang->changelog['9.1'][] = 'testreport-view';
+$lang->changelog['9.1'][] = 'testreport-delete';
+$lang->changelog['9.1'][] = 'testreport-edit';
+$lang->changelog['9.1'][] = 'testsuite-index';
+$lang->changelog['9.1'][] = 'testsuite-browse';
+$lang->changelog['9.1'][] = 'testsuite-create';
+$lang->changelog['9.1'][] = 'testsuite-view';
+$lang->changelog['9.1'][] = 'testsuite-edit';
+$lang->changelog['9.1'][] = 'testsuite-delete';
+$lang->changelog['9.1'][] = 'testsuite-linkCase';
+$lang->changelog['9.1'][] = 'testsuite-unlinkCase';
+$lang->changelog['9.1'][] = 'testsuite-batchUnlinkCases';
+$lang->changelog['9.1'][] = 'testsuite-library';
+$lang->changelog['9.1'][] = 'testsuite-createLib';
+$lang->changelog['9.1'][] = 'testsuite-createCase';
+$lang->changelog['9.1'][] = 'testsuite-libView';
+$lang->changelog['9.1'][] = 'caselib-library';
+$lang->changelog['9.1'][] = 'caselib-createLib';
+$lang->changelog['9.1'][] = 'caselib-edit';
+$lang->changelog['9.1'][] = 'caselib-createCase';
+$lang->changelog['9.1'][] = 'caselib-libView';
+$lang->changelog['9.1'][] = 'testtask-activate';
+$lang->changelog['9.1'][] = 'testtask-block';
+$lang->changelog['9.1'][] = 'testtask-report';
+
+$lang->changelog['9.2'][] = 'custom-working';
+$lang->changelog['9.2'][] = 'doc-sort';
+$lang->changelog['9.2'][] = 'product-build';
+$lang->changelog['9.2'][] = 'testsuite-batchCreateCase';
+$lang->changelog['9.2'][] = 'testsuite-exportTemplet';
+$lang->changelog['9.2'][] = 'testsuite-import';
+$lang->changelog['9.2'][] = 'testsuite-showImport';
+$lang->changelog['9.5'][] = 'bug-batchActivate';
+
+$lang->changelog['9.6'][] = 'custom-setPublic';
+$lang->changelog['9.6'][] = 'datatable-setGlobal';
+$lang->changelog['9.6'][] = 'product-export';
+$lang->changelog['9.6'][] = 'project-export';
+$lang->changelog['9.6'][] = 'project-storyKanban';
+$lang->changelog['9.6'][] = 'project-storySort';
+
+$lang->changelog['9.8'][] = 'message-index';
+$lang->changelog['9.8'][] = 'message-setting';
+$lang->changelog['9.8'][] = 'todo-createCycle';
+$lang->changelog['9.8'][] = 'project-importPlanStories';
+$lang->changelog['9.8'][] = 'todo-assignTo';
+$lang->changelog['9.8'][] = 'todo-activate';
+$lang->changelog['9.8'][] = 'todo-close';
+
+$lang->changelog['10.0.alpha'][] = 'my-calendar';
+$lang->changelog['10.0.alpha'][] = 'doc-collect';
+
+$lang->changelog['10.1'][] = 'todo-batchClose';
+$lang->changelog['10.1'][] = 'project-treeTask';
+$lang->changelog['10.1'][] = 'project-treeStory';
+
+$lang->changelog['10.6'][] = 'backup-setting';
+$lang->changelog['10.6'][] = 'backup-rmPHPHeader';
+
+$lang->changelog['11.6.2'][] = 'message-browser';
+
+$lang->changelog['12.3'][] = 'testtask-browseUnits';
+$lang->changelog['12.3'][] = 'testtask-unitCases';
+$lang->changelog['12.3'][] = 'testtask-importUnitResult';
+$lang->changelog['12.3'][] = 'job-view';
+$lang->changelog['12.3'][] = 'ci-commitResult';
+
+$lang->changelog['12.5'][] = 'story-batchToTask';
+$lang->changelog['12.5'][] = 'custom-product';
+$lang->changelog['12.5'][] = 'custom-project';
+
+$lang->changelog['15.0.rc1'][] = 'program-browse';
+$lang->changelog['15.0.rc1'][] = 'program-view';
+$lang->changelog['15.0.rc1'][] = 'program-product';
+$lang->changelog['15.0.rc1'][] = 'program-create';
+$lang->changelog['15.0.rc1'][] = 'program-edit';
+$lang->changelog['15.0.rc1'][] = 'program-start';
+$lang->changelog['15.0.rc1'][] = 'program-suspend';
+$lang->changelog['15.0.rc1'][] = 'program-activate';
+$lang->changelog['15.0.rc1'][] = 'program-close';
+$lang->changelog['15.0.rc1'][] = 'program-delete';
+$lang->changelog['15.0.rc1'][] = 'program-project';
+$lang->changelog['15.0.rc1'][] = 'program-stakeholder';
+$lang->changelog['15.0.rc1'][] = 'program-createStakeholder';
+$lang->changelog['15.0.rc1'][] = 'program-unlinkStakeholder';
+$lang->changelog['15.0.rc1'][] = 'program-batchUnlinkStakeholders';
+$lang->changelog['15.0.rc1'][] = 'program-unbindWhitelist';
+$lang->changelog['15.0.rc1'][] = 'program-export';
+$lang->changelog['15.0.rc1'][] = 'project-execution';
+$lang->changelog['15.0.rc1'][] = 'project-group';
+$lang->changelog['15.0.rc1'][] = 'project-createGroup';
+$lang->changelog['15.0.rc1'][] = 'project-manageView';
+$lang->changelog['15.0.rc1'][] = 'project-managePriv';
+$lang->changelog['15.0.rc1'][] = 'project-manageGroupMember';
+$lang->changelog['15.0.rc1'][] = 'project-copyGroup';
+$lang->changelog['15.0.rc1'][] = 'project-editGroup';
+$lang->changelog['15.0.rc1'][] = 'project-whitelist';
+$lang->changelog['15.0.rc1'][] = 'project-addWhitelist';
+$lang->changelog['15.0.rc1'][] = 'project-unbindWhitelist';
+$lang->changelog['15.0.rc1'][] = 'project-qa';
+$lang->changelog['15.0.rc1'][] = 'project-createGuide';
+$lang->changelog['15.0.rc1'][] = 'project-moduleOpen';
+$lang->changelog['15.0.rc1'][] = 'personnel-accessible';
+$lang->changelog['15.0.rc1'][] = 'personnel-invest';
+$lang->changelog['15.0.rc1'][] = 'personnel-whitelist';
+$lang->changelog['15.0.rc1'][] = 'personnel-addWhitelist';
+$lang->changelog['15.0.rc1'][] = 'personnel-unbindWhitelist';
+$lang->changelog['15.0.rc1'][] = 'my-workAction';
+$lang->changelog['15.0.rc1'][] = 'my-contributeAction';
+$lang->changelog['15.0.rc1'][] = 'my-testtask';
+$lang->changelog['15.0.rc1'][] = 'my-execution';
+$lang->changelog['15.0.rc1'][] = 'my-doc';
+$lang->changelog['15.0.rc1'][] = 'my-calendarAction';
+$lang->changelog['15.0.rc1'][] = 'story-trackAB';
+$lang->changelog['15.0.rc1'][] = 'story-processStoryChange';
+$lang->changelog['15.0.rc1'][] = 'projectstory-story';
+$lang->changelog['15.0.rc1'][] = 'projectstory-trackAction';
+$lang->changelog['15.0.rc1'][] = 'projectstory-view';
+$lang->changelog['15.0.rc1'][] = 'projectstory-linkStory';
+$lang->changelog['15.0.rc1'][] = 'projectstory-unlinkStory';
+$lang->changelog['15.0.rc1'][] = 'projectstory-importplanstories';
+$lang->changelog['15.0.rc1'][] = 'execution-whitelist';
+$lang->changelog['15.0.rc1'][] = 'execution-addWhitelist';
+$lang->changelog['15.0.rc1'][] = 'execution-unbindWhitelist';
+$lang->changelog['15.0.rc1'][] = 'automation-browse';
+$lang->changelog['15.0.rc1'][] = 'group-manageProjectAdmin';
+$lang->changelog['15.0.rc1'][] = 'user-execution';
+$lang->changelog['15.0.rc1'][] = 'custom-setStoryConcept';
+$lang->changelog['15.0.rc1'][] = 'custom-editStoryConcept';
+$lang->changelog['15.0.rc1'][] = 'custom-browseStoryConcept';
+$lang->changelog['15.0.rc1'][] = 'custom-setDefaultConcept';
+$lang->changelog['15.0.rc1'][] = 'custom-deleteStoryConcept';
+$lang->changelog['15.0.rc1'][] = 'search-index';
+$lang->changelog['15.0.rc1'][] = 'search-buildIndex';
+$lang->changelog['15.0.rc1'][] = 'projectbuild-browse';
+$lang->changelog['15.0.rc1'][] = 'projectrelease-browseAction';
+$lang->changelog['15.0.rc1'][] = 'projectrelease-create';
+$lang->changelog['15.0.rc1'][] = 'projectrelease-edit';
+$lang->changelog['15.0.rc1'][] = 'projectrelease-delete';
+$lang->changelog['15.0.rc1'][] = 'projectrelease-view';
+$lang->changelog['15.0.rc1'][] = 'projectrelease-export';
+$lang->changelog['15.0.rc1'][] = 'projectrelease-linkStory';
+$lang->changelog['15.0.rc1'][] = 'projectrelease-unlinkStory';
+$lang->changelog['15.0.rc1'][] = 'projectrelease-batchUnlinkStory';
+$lang->changelog['15.0.rc1'][] = 'projectrelease-linkBug';
+$lang->changelog['15.0.rc1'][] = 'projectrelease-unlinkBug';
+$lang->changelog['15.0.rc1'][] = 'projectrelease-batchUnlinkBug';
+$lang->changelog['15.0.rc1'][] = 'projectrelease-changeStatus';
+$lang->changelog['15.0.rc1'][] = 'stakeholder-browse';
+$lang->changelog['15.0.rc1'][] = 'stakeholder-create';
+$lang->changelog['15.0.rc1'][] = 'stakeholder-batchCreate';
+$lang->changelog['15.0.rc1'][] = 'stakeholder-edit';
+$lang->changelog['15.0.rc1'][] = 'stakeholder-delete';
+$lang->changelog['15.0.rc1'][] = 'stakeholder-viewAction';
+$lang->changelog['15.0.rc1'][] = 'stakeholder-issue';
+$lang->changelog['15.0.rc1'][] = 'stakeholder-viewIssueAction';
+$lang->changelog['15.0.rc1'][] = 'stakeholder-communicate';
+$lang->changelog['15.0.rc1'][] = 'stakeholder-expect';
+$lang->changelog['15.0.rc1'][] = 'stakeholder-expectation';
+$lang->changelog['15.0.rc1'][] = 'stakeholder-deleteExpect';
+$lang->changelog['15.0.rc1'][] = 'stakeholder-createExpect';
+$lang->changelog['15.0.rc1'][] = 'stakeholder-editExpect';
+$lang->changelog['15.0.rc1'][] = 'stakeholder-viewExpect';
+$lang->changelog['15.0.rc1'][] = 'stakeholder-userIssue';
