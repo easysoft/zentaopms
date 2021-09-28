353bef2e83f8e290b2e297a1ea3fdf2236435694
tianshujie <tianshujie@easycorp.ltd>
2021-09-28 15:38:34 +0800

    * Finish task #42867.

--- module/company/js/dynamic.js
+++ module/company/js/dynamic.js
@@ -4,7 +4,8 @@ $('#account, #product, #project, #execution').change(function()
     var productID   = $('#product').val();
     var projectID   = systemMode == 'new' ? $('#project').val() : 0;
     var executionID = $('#execution').val();
-    browseType    = browseType == 'bysearch' ? 'all' : browseType;
+
+    browseType = browseType == 'bysearch' ? 'all' : browseType;
     link = createLink('company', 'dynamic', 'browseType=' + browseType + '&param=&recTotal=0&date=&direction=next&userID=' + userID + '&productID=' + productID + '&projectID=' + projectID + '&executionID=' + executionID);
     location.href = link;
 })
