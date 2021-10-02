b9bb3f446ba0ceb2460e10fa65034dcfb65a2ae1
xieqiyu <xieqiyu@easycorp.ltd>
2021-10-02 18:29:29 +0800

    * Modify product and branch yaml.

--- test/data/zentao/config.php
+++ test/data/zentao/config.php
@@ -11,5 +11,5 @@ $builder->deptGrade3 = array('rows' => 10, 'data' => array('dept', 'deptgrade3')
 $builder->company   = array('rows' => 1,   'data' => array('company'));
 $builder->user      = array('rows' => 350, 'data' => array('user'));
 $builder->usergroup = array('rows' => 349, 'data' => array('usergroup'));
-$builder->product   = array('rows' => 21,  'data' => array('product'));
-$builder->branch    = array('rows' => 28,  'data' => array('branch'));
+$builder->product   = array('rows' => 120, 'data' => array('product'));
+$builder->branch    = array('rows' => 160, 'data' => array('branch'));
