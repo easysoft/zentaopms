0bbe006055304f7ae4d77220e28aa858afdb84ca
xieqiyu <xieqiyu@easycorp.ltd>
2021-10-08 14:56:50 +0800

    * Add products with long names.

--- test/data/zentao/config.php
+++ test/data/zentao/config.php
@@ -16,7 +16,7 @@ $builder->deptGrade3 = array('rows' => 10, 'data' => array('dept', 'deptgrade3')
 $builder->company   = array('rows' => 1,   'data' => array('company'));
 $builder->user      = array('rows' => 350, 'data' => array('user'));
 $builder->usergroup = array('rows' => 349, 'data' => array('usergroup'));
-$builder->product   = array('rows' => 180, 'data' => array('product'));
+$builder->product   = array('rows' => 190, 'data' => array('product'));
 $builder->branch    = array('rows' => 160, 'data' => array('branch'));
 
 $builder->blockDefault   = array('rows' => 150, 'data' => array('blockdefault'));
