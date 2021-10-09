40a362c7f2e91a4ebbff815ac64c2b9b66fefaec
xieqiyu <xieqiyu@easycorp.ltd>
2021-10-09 13:12:47 +0800

    * Add data of deleted products.

--- test/data/zentao/config.php
+++ test/data/zentao/config.php
@@ -16,7 +16,7 @@ $builder->deptGrade3 = array('rows' => 10, 'data' => array('dept', 'deptgrade3')
 $builder->company     = array('rows' => 1,   'data' => array('company'));
 $builder->user        = array('rows' => 346, 'data' => array('user'));
 $builder->usergroup   = array('rows' => 349, 'data' => array('usergroup'));
-$builder->product     = array('rows' => 190, 'data' => array('product'));
+$builder->product     = array('rows' => 200, 'data' => array('product'));
 $builder->productplan = array('rows' => 120, 'data' => array('productplan'));
 $builder->branch      = array('rows' => 160, 'data' => array('branch'));
 
