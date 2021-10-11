5f35a398ad246b34dea0b97aea4e1170669c5355
zenggang <zenggang@easycorp.ltd>
2021-10-11 07:26:18 +0000

    * Finish task#43179

--- module/common/model.php
+++ module/common/model.php
@@ -400,17 +400,26 @@ class commonModel extends model
                     $params       = "objectType=&objectID=0&libID=0";
                     $createMethod = 'selectLibType';
                 }
+                if($objectType == 'project')
+                {
+                    if(isset($config->maxVersion) and !defined('TUTORIAL'))
+                    {
+                        $params       = "programID=0&copyProjectID=0&extra=from=global";
+                        $createMethod = 'createGuide';
+                        $attr         = 'data-toggle="modal" data-target="#guideDialog"';
+                    }
+                    else $params = "model=scrum&programID=0&copyProjectID=0&extra=from=global";
+                }
 
                 if($objectType == 'bug')       $params = "productID=$productID&branch=&extras=from=global";
                 if($objectType == 'story')     $params = "productID=$productID&branch=0&moduleID=0&storyID=0&objectID=0&bugID=0&planID=0&todoID=0&extra=from=global";
                 if($objectType == 'task')      $params = "executionID=0&storyID=0&moduleID=0&taskID=0&todoID=0&extra=from=global";
                 if($objectType == 'testcase')  $params = "productID=$productID&branch=&moduleID=0&from=&param=0&storyID=0&extras=from=global";
                 if($objectType == 'execution') $params = "projectID=&executionID=0&copyExecutionID=0&planID=0&confirm=no&productID=0&extra=from=global";
-                if($objectType == 'project')   $params = "model=scrum&programID=0&copyProjectID=0&extra=from=global";
                 if($objectType == 'product')   $params = "programID=&extra=from=global";
                 if($objectType == 'program')   $params = "parentProgramID=0&extra=from=global";
 
-                $html .= '<li>' . html::a(helper::createLink($objectType, $createMethod, $params, '', $isOnlyBody), "<i class='icon icon-$objectIcon'></i> " . $lang->createObjects[$objectType], '', $isOnlyBody ? $attr : '') . '</li>';
+                $html .= '<li>' . html::a(helper::createLink($objectType, $createMethod, $params, '', $isOnlyBody), "<i class='icon icon-$objectIcon'></i> " . $lang->createObjects[$objectType], '', $attr) . '</li>';
             }
         }
 
