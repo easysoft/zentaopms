abc5c4a302bc1a5feb815ccc337a637beade1bf9
zhujinyong <zhujinyong@cnezsoft.com>
2021-10-18 09:20:56 +0800

    * Fix code error.

--- test/api/tokens/post.php
+++ test/api/tokens/post.php
@@ -12,8 +12,8 @@ pid=1
 使用正确用户名和密码获取token >> 登录失败，请检查您的用户名或密码是否填写正确。
 
 */
-$pass = $rest->post('/tokens', array('account' => 'admin', 'password' => '123456'))
-$fail = $rest->post('/tokens', array('account' => 'admin', 'password' => '123'))
+$pass = $rest->post('/tokens', array('account' => 'admin', 'password' => '123qwe!@#'));
+$fail = $rest->post('/tokens', array('account' => 'admin', 'password' => '123'));
 
 r($pass) && c(201) && p('token') && e('`[A-Za-z0-9]+`'); // 使用正确用户名和密码获取token
 r($fail) && c(400) && p('error') && e('登录失败，请检查您的用户名或密码是否填写正确。'); // 使用正确用户名和密码获取token
