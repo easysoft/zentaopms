0ea60016a7f8cf447393f94cd4383d0917139551
xiawenlong <c8EEx7s8ZQdm2EzADfL6>
2021-10-19 15:09:13 +0800

    add permission.

--- module/api/lang/en.php
+++ module/api/lang/en.php
@@ -18,6 +18,7 @@ $lang->api->index            = 'Api Doc Home';
 $lang->api->editLib          = 'Edit Api Doc';
 $lang->api->deleteLib        = 'Delete Api Doc';
 $lang->api->publish          = 'Publish';
+$lang->api->editPublish      = 'Edit Publish';
 $lang->api->createLib        = 'Create Api Library';
 $lang->api->createApi        = 'Create Api';
 $lang->api->edit             = 'Edit';
