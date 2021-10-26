43d52ddb9bee790529887e55830dd4f0c3a5037e
Hao Sun <sunhao@easycorp.ltd>
2021-10-26 14:54:16 +0800

    * improve style for affixed header in kanban.

--- module/common/view/kanban.html.php
+++ module/common/view/kanban.html.php
@@ -429,7 +429,10 @@ $.extend($.fn.kanban.Constructor.DEFAULTS,
         }
         $kanban.find('.kanban-header-col[data-type="doingProject"] > .title > .count').text(doingProjectCount || 0);
         $kanban.find('.kanban-header-col[data-type="doingExecution"] > .title > .count').text(doingExecutionCount || 0);
-
+    },
+    onCreate(kanban)
+    {
+        kanban.$.on('scroll', updateKanbanAffixState);
         updateKanbanAffixState();
     }
 });
