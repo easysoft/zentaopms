aadb69106de3f0a98d8f1c655b92f7438fc0e5a5
hufangzhou <hufangzhou@easycorp.ltd>
2021-11-01 16:32:28 +0800

    * Code for task #43423.

--- module/kanban/model.php
+++ module/kanban/model.php
@@ -629,14 +629,17 @@ class kanbanModel extends model
         $orderList = $this->dao->select('id,type,`order`')->from(TABLE_KANBANLANE)
             ->where('execution')->eq($executionID)
             ->andWhere('type')->in(array($currentType, $targetType))
+            ->andWhere('groupby')->eq('')
             ->fetchAll('type');
 
         $this->dao->update(TABLE_KANBANLANE)->set('`order`')->eq($orderList[$targetType]->order)
             ->where('id')->eq($orderList[$currentType]->id)
+            ->andWhere('groupby')->eq('')
             ->exec();
 
         $this->dao->update(TABLE_KANBANLANE)->set('`order`')->eq($orderList[$currentType]->order)
             ->where('id')->eq($orderList[$targetType]->id)
+            ->andWhere('groupby')->eq('')
             ->exec();
     }
 
