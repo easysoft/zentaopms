5db6060e900600c8f6a622da384bd18e38715b55
孙广明 <sunguangming@cnezsoft.com>
2021-11-04 09:53:03 +0800

    * Code for task #43985.

--- module/doc/view/side.html.php
+++ module/doc/view/side.html.php
@@ -64,100 +64,15 @@ $sideWidth = common::checkNotCN() ? '270' : '238';
       <?php endif;?>
     </div>
 <script>
-$(function()
+$('#modules').sortable(
 {
-    if($.cookie('docSideType'))
+    trigger: '.icon-move',
+    selector: 'li',
+    nested: true,
+    canMoveHere: function($ele, $target)
     {
-        var type = $.cookie('docSideType');
-        var $tabs = $('#mainRow .side-col .tabs');
-        if($tabs.find('.tab-content .tab-pane#' + type).length >0)
-        {
-            $tabs.find('.nav-tabs li').removeClass('active');
-            $tabs.find('.nav-tabs li a[href="#' + type + '"]').parent().addClass('active');
-            $tabs.find('.tab-content .tab-pane').removeClass('active');
-            $tabs.find('.tab-content .tab-pane#' + type).addClass('active');
-        }
-        $.cookie('docSideType', '');
-        $('#mainRow .side-col .side-footer #orderLib').toggleClass('hidden', (type == 'product' || type == 'execution'));
+        if($ele && $target && $ele.parent().closest('li').attr('data-idx') !== $target.parent().closest('li').attr('data-idx')) return false;
     }
-
-    $('#mainRow .side-col .tabs .nav-tabs li a').click(function()
-    {
-        var href     = $(this).attr('href');
-        var canOrder = !(href.indexOf('product') > 0 || href.indexOf('execution') > 0);
-        if(!canOrder)
-        {
-            $(this).closest('.side-col').find('.side-footer #orderLib').addClass('hidden');
-            $(this).closest('.side-col').find('.side-footer #saveOrder').addClass('hidden');
-        }
-
-        var $orderLib  = $(this).closest('.side-col').find('.side-footer #orderLib');
-        var $saveOrder = $(this).closest('.side-col').find('.side-footer #saveOrder');
-
-        var execute = false;
-        $(this).on('shown.zui.tab', function()
-        {
-            if(!execute)
-            {
-                var $tabPane   = $('#mainRow .side-col .tabs .tab-content .tab-pane.active');
-                if($tabPane.find('.libs-group.sort').length > 0 && canOrder)
-                {
-                    $orderLib.addClass('hidden');
-                    $saveOrder.removeClass('hidden');
-                    execute = true;
-                }
-                if($tabPane.find('.libs-group.sort').length == 0 && canOrder)
-                {
-                    $orderLib.removeClass('hidden');
-                    $saveOrder.addClass('hidden');
-                    execute = true;
-                }
-            }
-        });
-    });
-
-    $('#orderLib').click(function()
-    {
-        var $tabPane = $('#mainRow .side-col .tabs .tab-content .tab-pane.active');
-        var type     = $tabPane.attr('id');
-        $.get(createLink('doc', 'sort', "type=" + type), function(data)
-        {
-            $tabPane.html(data);
-            $tabPane.closest('.side-col').find('.side-footer #orderBox #orderLib').addClass('hidden');
-            $tabPane.closest('.side-col').find('.side-footer #orderBox #saveOrder').removeClass('hidden');
-            $tabPane.find('.libs-group.sort').sortable(
-            {
-                trigger:  '.lib',
-                selector: '.lib',
-            });
-        });
-    });
 });
-
-function saveOrder()
-{
-    var $tabPane  = $('#mainRow .side-col .tabs .tab-content .tab-pane.active');
-    var type      = $tabPane.attr('id');
-    var orders    = {};
-    var orderNext = 1;
-
-    $tabPane.find('.libs-group.sort .lib').not('.files').not('.addbtn').each(function()
-    {
-        orders[$(this).data('id')] = orderNext ++;
-    });
-
-    $.post(createLink('doc', 'sort'), orders, function(data)
-    {
-        if(data.result == 'success')
-        {
-            $.cookie('docSideType', type);
-            return location.reload();
-        }
-        else
-        {
-            bootbox.alert(data.message);
-        }
-    }, 'json');
-}
 </script>
 </div>
