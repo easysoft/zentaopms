acd66decf464ea80dbc10649edfaff096b5614c2
xieqiyu <xieqiyu@easycorp.ltd>
2021-11-08 13:40:03 +0800

    * Add lang.

--- module/branch/lang/en.php
+++ module/branch/lang/en.php
@@ -24,6 +24,7 @@ $lang->branch->closedDate  = 'Closed Date';
 $lang->branch->desc        = 'Desc';
 $lang->branch->order       = 'Order';
 $lang->branch->deleted     = 'Delete';
+$lang->branch->closed      = 'Closed';
 
 $lang->branch->confirmDelete = 'Do you want to delete this @branch@?';
 $lang->branch->canNotDelete  = 'There is data in @branch@. It cannot be deleted.';
