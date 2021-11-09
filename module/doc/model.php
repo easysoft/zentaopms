dc2e2cba795787584e2eb690eb512e13bae55466
孙广明 <sunguangming@cnezsoft.com>
2021-11-09 14:57:42 +0800

    * Adjust doc css.

--- module/doc/model.php
+++ module/doc/model.php
@@ -2373,7 +2373,7 @@ EOT;
                     if($currentMethod == 'objectlibs')
                     {
                         $treeMenu[$module->id] .= "<div class='tree-group'><span class='module-name'>" . html::a(inlink('objectLibs', "type=$type&objectID=$objectID&libID=$libID&docID={$doc->id}"), "<i class='icon icon-file-text text-muted'></i> &nbsp;" . $doc->title, '', "data-app='{$this->app->tab}' class='doc-title' title='{$doc->title}'") . '</span>';
-                        $treeMenu[$module->id] .= "<div class='tree-actions'>";
+                        $treeMenu[$module->id] .= "<div class='treeActions'>";
                         $treeMenu[$module->id] .= html::a(helper::createLink('doc', 'edit', "docID=$docID&comment=false&objectType=$type&objectID=$objectID&libID=$libID"), "<i class='icon icon-edit'></i>", '', "title={$this->lang->doc->edit}");
                         $treeMenu[$module->id] .= '<div class="dropdown dropdown-hover">';
                         $treeMenu[$module->id] .= "<a class='tree-action' href='javascript:;' data-toggle='dropdown'><i class='icon icon-plus'></i></a>";
@@ -2388,11 +2388,6 @@ EOT;
                             if($typeKey == 'url') $treeMenu[$module->id] .= '<li class="divider"></li>';
                         }
 
-                        if(common::hasPriv('doc', 'createLib'))
-                        {
-                            $treeMenu[$module->id] .= '<li class="divider"></li>';
-                            $treeMenu[$module->id] .= '<li>' . html::a(helper::createLink('doc', 'createLib', "type=$type&objectID=$objectID"), "<i class='icon-doc-lib icon'></i> " . $this->lang->doc->createLib, '', "class='iframe' data-width='70%'") . '</li>';
-                        }
                         $treeMenu[$module->id] .= "</ul>";
                         $treeMenu[$module->id] .= "</div>";
                         $treeMenu[$module->id] .= "<a class='sort-handler tree-action' href='javascript:;' data-type='sort' title={$this->lang->tree->dragAndSort}><i class='icon icon-move'></i></a>";
@@ -2421,7 +2416,7 @@ EOT;
             else
             {
                 $li  = "<div class='tree-group'><span class='module-name'><a title='{$module->name}'>" . $module->name . '</a></span>';
-                $li .= "<div class='tree-actions'>";
+                $li .= "<div class='treeActions'>";
                 $li .= html::a(helper::createLink('tree', 'edit', "module=$module->id&type=doc"), "<i class='icon icon-edit'></i>", '', "data-toggle='modal' title={$this->lang->doc->editType}");
                 $li .= html::a(helper::createLink('tree', 'browse', "rootID=$libID&type=doc&module=$module->id", '', 1), "<i class='icon icon-split'></i>", '', "class='iframe' title={$this->lang->doc->editType}");
                 $li .='<div class="dropdown dropdown-hover">';
@@ -2437,11 +2432,6 @@ EOT;
                     if($typeKey == 'url') $li .= '<li class="divider"></li>';
                 }
 
-                if(common::hasPriv('doc', 'createLib'))
-                {
-                    $li .= '<li class="divider"></li>';
-                    $li .= '<li>' . html::a(helper::createLink('doc', 'createLib', "type=$type&objectID=$objectID"), "<i class='icon-doc-lib icon'></i> " . $this->lang->doc->createLib, '', "class='iframe' data-width='70%'") . '</li>';
-                }
                 $li .= "</ul>";
                 $li .= '</div>';
                 $li .= "<a class='sort-handler tree-action' href='javascript:;' data-type='sort' title={$this->lang->tree->dragAndSort}><i class='icon icon-move'></i></a>";
