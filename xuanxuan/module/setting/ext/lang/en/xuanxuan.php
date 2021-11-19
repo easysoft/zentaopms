2fcb053765fe7ce9a3af6aa498a0402a25d9a24d
Lwrless <happy.lwr@gmail.com>
2021-11-19 16:33:10 +0800

    * remove owt related stuff, moved to zentaobiz.

--- xuanxuan/module/setting/ext/lang/en/xuanxuan.php
+++ xuanxuan/module/setting/ext/lang/en/xuanxuan.php
@@ -1,5 +1,4 @@
-<?php
+<?php 
 $lang->setting->common      = 'Settings';
 $lang->setting->xuanxuan    = 'Client';
-$lang->setting->owt         = 'Conference';
 $lang->setting->downloadXXD = 'Download XXD';
