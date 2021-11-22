da47c729d9a5c87ee04b34fed388de150e52cd4d
liyuchun <liyuchun@easycorp.ltd>
2021-11-22 13:36:52 +0800

    * Adjust execution button name.

--- module/execution/view/start.html.php
+++ module/execution/view/start.html.php
@@ -31,7 +31,7 @@
           <td><?php echo html::textarea('comment', '', "rows='6' class='form-control kindeditor' hidefocus='true'");?></td>
         </tr>
         <tr>
-          <td colspan='2' class='text-center form-actions'><?php echo html::submitButton($lang->execution->start) . ' ' .  html::linkButton($lang->goback, $this->session->taskList, 'self', '', 'btn btn-wide'); ?></td>
+          <td colspan='2' class='text-center form-actions'><?php echo html::submitButton($lang->execution->start . $lang->executionCommon) . ' ' .  html::linkButton($lang->goback, $this->session->taskList, 'self', '', 'btn btn-wide'); ?></td>
         </tr>
       </tbody>
     </table>
