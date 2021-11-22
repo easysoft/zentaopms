5d6a255a733cc09763bb3806d7b00de1f808b28a
Hao Sun <sunhao@easycorp.ltd>
2021-11-22 13:43:23 +0800

    * support for using zentao apps in client left navbar.

---
+++ xuanxuan/module/index/ext/js/index/xuanxuan.js
@@ -0,0 +1,21 @@
+/** Update zentao client app menu */
+function updateAppMenu()
+{
+    const menuItems = appsMenuItems.map(function(item)
+    {
+        if (item === 'divider') return '-';
+        var $title = $('<div>' + item.title + '</div>');
+        return [item.code, ($title.find('.icon').attr('class') || '').replace('icon ', ''),$title.text().trim()];
+    });
+    window.open('xxc://extension.zentao-integrated.updateAppMenu/' + encodeURIComponent(JSON.stringify(menuItems)));
+}
+
+updateAppMenu();
+
+$(function()
+{
+    $(document).on('showapp', function(e, app)
+    {
+        window.open('xxc://extension.zentao-integrated.activeAppMenuItem/' + app.code);
+    });
+});
