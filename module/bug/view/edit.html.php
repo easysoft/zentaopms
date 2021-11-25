4c090eefe26b73624e819dbd4ebd138955bd0581
liumengyi <liumengyi@easycorp.ltd>
2021-11-25 15:58:10 +0800

    * Finish task #45155.

--- module/bug/view/edit.html.php
+++ module/bug/view/edit.html.php
@@ -134,7 +134,7 @@ js::set('confirmUnlinkBuild'     , sprintf($lang->bug->confirmUnlinkBuild, zget(
                 </tr>
                 <tr>
                   <th><?php echo $lang->bug->status;?></th>
-                  <td><?php echo $lang->bug->statusList[$bug->status];?></td>
+                  <td><?php echo zget($lang->bug->statusList, $bug->status);?></td>
                 </tr>
                 <tr>
                   <th><?php echo $lang->bug->confirmed;?></th>
