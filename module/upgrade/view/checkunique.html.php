eb6bd799632b9947612534b4a6e991def8f753a7
孙广明 <sunguangming@cnezsoft.com>
2021-11-29 13:09:43 +0800

    * Remove unused file.

--- module/upgrade/view/checkunique.html.php
+++
@@ -1,9 +0,0 @@
-<?php include '../../common/view/header.lite.html.php';?>
-<div id='mainContent' class='main-content'>
-  <div class='center-block'>
-    <div class='main-header'>
-      <h2>同一个项目集下不能有重名项目</h2>
-    </div>
-  </div>
-</div>
-<?php include '../../common/view/footer.lite.html.php';?>
