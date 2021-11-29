78134ccb9b2672bcf482ef4f2dcef245ef33f669
xieqiyu <xieqiyu@easycorp.ltd>
2021-11-29 14:14:10 +0800

    * Code for task#45082. Restore code.

--- module/productplan/model.php
+++ module/productplan/model.php
@@ -221,7 +221,7 @@ class productplanModel extends model
         $plans = $this->dao->select('id,title,parent,begin,end')->from(TABLE_PRODUCTPLAN)
             ->where('product')->in($product)
             ->andWhere('deleted')->eq(0)
-            ->beginIF($branch != '')->andWhere("branch")->in("0,$branch")->fi()
+            ->beginIF($branch)->andWhere("branch")->in("0,$branch")->fi()
             ->beginIF($skipParent)->andWhere('parent')->ne(-1)->fi()
             ->orderBy('begin desc')
             ->fetchAll('id');
