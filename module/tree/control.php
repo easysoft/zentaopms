36fa35d3d1f91cbf61e07a3995fc590a7fb25282
hufangzhou <hufangzhou@easycorp.ltd>
2021-12-01 17:09:52 +0800

    * Fix bug #15832.

--- module/tree/control.php
+++ module/tree/control.php
@@ -31,7 +31,7 @@ class tree extends control
 
         if($this->app->tab == 'product')
         {
-            $this->product->setMenu($rootID, $branch, 0, '', $viewType);
+            $this->product->setMenu($this->session->product, $branch, 0, '', $viewType);
         }
         else if($this->app->tab == 'qa' and $viewType != 'caselib')
         {
