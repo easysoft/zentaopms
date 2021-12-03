4b53f349dff38ad97c6ddbe59c221b34d477102a
zenggang <zenggang@easycorp.ltd>
2021-12-03 01:59:04 +0000

    * Fix bug#16736,17037,16844

--- module/gitlab/view/createuser.html.php
+++ module/gitlab/view/createuser.html.php
@@ -72,7 +72,7 @@
             <th><?php echo $lang->gitlab->user->avatar;?></th>
             <td>
               <div id="avatarUpload" class="text-center">
-                <?php echo html::avatar(array('avatar'=>' ', 'account'=>''), 50); ?>
+                <?php echo html::avatar(array('avatar'=>$defaultTheme . 'images/repo/avatar.jpeg', 'account'=>''), 50); ?>
                 <input type="file" name="avatar" id="files" class="form-control hidden">
                 <?php echo html::a('javascript:void(0);', '<i class="icon icon-pencil icon-2x"></i>', '', "class='btn-avatar' id='avatarUploadBtn' data-toggle='tooltip' data-container='body' data-placement='bottom' title='{$lang->gitlab->user->avatar}'");?>
               </div>
