88eac3a1da0449030f89bebf6df8d888d4874660
hufangzhou <hufangzhou@easycorp.ltd>
2021-12-06 20:09:06 +0800

    * Fallback version of the xuanxuan folder.

--- xuanxuan/module/message/ext/config/xuanxuan.php
+++ xuanxuan/module/message/ext/config/xuanxuan.php
@@ -1,6 +1,7 @@
 <?php
-$config->message->available['xuanxuan']['story'] = $config->message->objectTypes['story'];
-$config->message->available['xuanxuan']['task']  = $config->message->objectTypes['task'];
-$config->message->available['xuanxuan']['bug']   = $config->message->objectTypes['bug'];
-$config->message->available['xuanxuan']['todo']  = $config->message->objectTypes['todo'];
-$config->message->setting['xuanxuan']['setting'] = $config->message->available['xuanxuan'];
+$config->message->available['xuanxuan']['story']   = $config->message->objectTypes['story'];
+$config->message->available['xuanxuan']['task']    = $config->message->objectTypes['task'];
+$config->message->available['xuanxuan']['bug']     = $config->message->objectTypes['bug'];
+$config->message->available['xuanxuan']['todo']    = $config->message->objectTypes['todo'];
+$config->message->available['xuanxuan']['release'] = $config->message->objectTypes['release'];
+$config->message->setting['xuanxuan']['setting']   = $config->message->available['xuanxuan'];
