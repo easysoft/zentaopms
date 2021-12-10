37d9aaeb3a65fb64b5cc991804b1f72a16ca7513
liumengyi <liumengyi@easycorp.ltd>
2021-12-10 09:28:40 +0800

    * Finish task #45825.

--- module/kanban/lang/en.php
+++ module/kanban/lang/en.php
@@ -100,6 +100,9 @@ $lang->kanban->laneGroup          = 'Lane Group';
 $lang->kanban->cardsSort          = 'Cards Sortting';
 $lang->kanban->moreAction         = 'More Action';
 $lang->kanban->noGroup            = 'None';
+$lang->kanban->limitExceeded      = 'Limit Exceeded';
+$lang->kanban->fullScreen         = 'Full Screen';
+$lang->kanban->setting            = 'Setting';
 
 $lang->kanban->error = new stdclass();
 $lang->kanban->error->mustBeInt       = 'The WIPs must be positive integer.';
@@ -178,8 +181,11 @@ $lang->kanbancolumn->limit = $lang->kanban->WIPCount;
 
 $lang->kanbanlane = new stdclass();
 $lang->kanbanlane->name    = $lang->kanban->laneName;
+$lang->kanbanlane->common  = 'Lane';
 $lang->kanbanlane->default = 'Default Lane';
 
 $lang->kanbanregion = new stdclass();
 $lang->kanbanregion->name    = 'Kanban Region';
 $lang->kanbanregion->default = 'Default Region';
+
+$lang->kanbancard = new stdclass();
