4244e0cc23ffd5ab85d7f6a9f14e800bc86cb0c0
liumengyi <liumengyi@easycorp.ltd>
2021-12-14 13:54:53 +0800

    * Remove useless code.

--- module/kanban/config.php
+++ module/kanban/config.php
@@ -37,7 +37,7 @@ $config->kanban->editor->editspace   = array('id' => 'desc', 'tools' => 'simpleT
 $config->kanban->editor->createcard  = array('id' => 'desc', 'tools' => 'simpleTools');
 $config->kanban->editor->close       = array('id' => 'comment', 'tools' => 'simpleTools');
 $config->kanban->editor->editcard    = array('id' => 'desc', 'tools' => 'simpleTools');
-$config->kanban->editor->viewcard    = array('id' => 'comment,lastcomment', 'tools' => 'simpleTools');
+$config->kanban->editor->viewcard    = array('id' => 'comment', 'tools' => 'simpleTools');
 
 $config->kanban->default = new stdclass();
 $config->kanban->default->story  = new stdclass();
