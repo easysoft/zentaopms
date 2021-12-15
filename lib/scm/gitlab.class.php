d1f43a50252e81452fa7226cbc174e5254e85ff3
zhouxin <zhouxin@easycorp.ltd>
2021-12-15 07:31:04 +0000

    Fix bug #17508.

--- lib/scm/gitlab.class.php
+++ lib/scm/gitlab.class.php
@@ -613,7 +613,6 @@ class gitlab
 
         $param = new stdclass();
         $param->path     = urldecode($path);
-        $param->ref_name = $this->branch;
 
         $fromDate = $this->getCommittedDate($fromRevision);
         $toDate   = $this->getCommittedDate($toRevision);
