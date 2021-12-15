4adf6e0a52c93cf8d2d0bbf4b19db91a3d470f8e
mayue <mayue@easycorp.ltd>
2021-12-15 17:17:57 +0800

    * Fix bug #17015.

--- module/execution/control.php
+++ module/execution/control.php
@@ -42,7 +42,7 @@ class execution extends control
 
         if(defined('IN_UPGRADE') and IN_UPGRADE) return false;
         $this->executions = $this->execution->getPairs(0, 'all', 'nocode');
-        $skipCreateStep   = array('computeburn', 'ajaxgetdropmenu', 'executionkanban', 'ajaxgetteammembers');
+        $skipCreateStep   = array('computeburn', 'ajaxgetdropmenu', 'executionkanban', 'ajaxgetteammembers', 'all');
         if(!in_array($this->methodName, $skipCreateStep) and $this->app->tab == 'execution')
         {
             if(!$this->executions and $this->methodName != 'index' and $this->methodName != 'create' and $this->app->getViewType() != 'mhtml') $this->locate($this->createLink('execution', 'create'));
