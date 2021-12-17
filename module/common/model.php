6ee1345b07e562e51710e0d04d6dc21de423599b
孙广明 <sunguangming@cnezsoft.com>
2021-12-17 10:13:49 +0800

    * Reset project priv.

--- module/common/model.php
+++ module/common/model.php
@@ -2110,6 +2110,8 @@ EOD;
 
         if(isset($this->app->user))
         {
+            if(in_array($module, $this->config->programPriv->waterfall)) return true;
+
             $this->app->user = $this->session->user;
             if(!commonModel::hasPriv($module, $method)) $this->deny($module, $method);
         }
