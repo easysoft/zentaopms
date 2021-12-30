b45ec10f40ca105741fff8435517e2ba03a31c7a
liyuchun <liyuchun@easycorp.ltd>
2021-12-30 15:36:33 +0800

    * Fix bug #18078.

--- module/mr/lang/en.php
+++ module/mr/lang/en.php
@@ -1,6 +1,7 @@
 <?php
 $lang->mr = new stdclass;
 $lang->mr->common       = "Merge Request";
+$lang->mr->view         = "Survey";
 $lang->mr->overview     = "Survey";
 $lang->mr->create       = "Create";
 $lang->mr->apiCreate    = "Interface: Create";
