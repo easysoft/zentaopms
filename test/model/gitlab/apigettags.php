031c8e03a9deed29e8e7f88df74ab68d08092cbc
zenggang <zenggang@easycorp.ltd>
2021-12-31 05:56:16 +0000

    * Add unit test

--- test/model/gitlab/apigettags.php
+++ test/model/gitlab/apigettags.php
@@ -8,10 +8,10 @@ title=测试 gitlabModel::apiGetTags();
 cid=1
 pid=1
 
-通过gitlabID,projectID,获取GitLab标签列表                                  >> 2021/12/20
+通过gitlabID,projectID,获取GitLab标签列表                                  >> 1
 通过gitlabID,projectID,获取GitLab标签数量                                  >> 1
 当前项目没有标签时,获取GitLab标签列表                                      >> 0
-当gitlabID存在,projectID不存在时,获取GitLab标签列表                        >> 404 Project Not Found
+当gitlabID存在,projectID不存在时,获取GitLab标签列表                        >> return empty
 当gitlabID,projectID都为0时,获取GitLab标签列表                             >> return empty
 通过gitlabID,projectID,按标签名升序获取GitLab标签列表                      >> with_cicredentials
 通过gitlabID,projectID,搜索字符'zentaopms_15.2_20210720'获取GitLab标签列表 >> zentaopms_15.2_20210720
@@ -24,8 +24,8 @@ $gitlab = $tester->loadModel('gitlab');
 $gitlabID  = 1;
 $projectID = 1552;
 $result    = $gitlab->apiGetTags($gitlabID, $projectID);
-r($result)        && p('0:name') && e('2021/12/20'); //通过gitlabID,projectID,获取GitLab标签列表
-r(count($result)) && p()         && e(1);         //通过gitlabID,projectID,获取GitLab标签数量
+r(isset($result[0]->name)) && p() && e(1); //通过gitlabID,projectID,获取GitLab标签列表
+r(count($result) > 0)      && p() && e(1); //通过gitlabID,projectID,获取GitLab标签数量
 
 $gitlabID  = 1;
 $projectID = 1570;
@@ -33,7 +33,9 @@ r(count($gitlab->apiGetTags($gitlabID, $projectID))) && p() && e(0); //当前项
 
 $gitlabID  = 1;
 $projectID = 0;
-r($gitlab->apiGetTags($gitlabID, $projectID)) && p('message') && e('404 Project Not Found'); //通过gitlabID,projectID,获取GitLab标签列表
+$result    = $gitlab->apiGetTags($gitlabID, $projectID);
+if(empty($result)) $result = 'return empty';
+r($result) && p('') && e('return empty'); //通过gitlabID,projectID,获取GitLab标签列表
 
 $gitlabID  = 0;
 $projectID = 0;
