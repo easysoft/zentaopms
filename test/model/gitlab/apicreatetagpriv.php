c8786ef9eee2060d28759e49f12c2803fdcd5442
liyuchun <liyuchun@easycorp.ltd>
2022-01-04 15:16:53 +0800

    * Adjust unit case.

--- test/model/gitlab/apicreatetagpriv.php
+++ test/model/gitlab/apicreatetagpriv.php
@@ -12,7 +12,7 @@ pid=1
 使用正确的gitlabID,空的projectID、保护标签对象创建GitLab保护标签  >> return false
 使用正确的gitlabID，空的保护标签信息，错误的projectID创建保护标签 >> return false
 使用正确的gitlabID、保护标签信息，错误的projectID创建保护标签     >> 404 Project Not Found
-tlabID、保护标签信息，错误的projectID创建保护标签                 >> test_tag1
+使用正确的gitlabID、projectID、保护标签信息创建保护标签           >> test_tag1
 使用重复的保护标签信息创建保护标签                                >> 名称已经被使用
 
 */
@@ -43,5 +43,5 @@ r($gitlab->apiCreateTagPriv($gitlabID, $projectID, $tag)) && p('message') && e('
 
 $projectID = 1555;
 $gitlab->apiDeleteTagPriv($gitlabID, $projectID, $tag->name);
-r($gitlab->apiCreateTagPriv($gitlabID, $projectID, $tag)) && p('name')    && e('test_tag1'); //使用正确的gitlabID、保护标签信息，错误的projectID创建保护标签
+r($gitlab->apiCreateTagPriv($gitlabID, $projectID, $tag)) && p('name')      && e('test_tag1'); //使用正确的gitlabID、projectID、保护标签信息创建保护标签
 r($gitlab->apiCreateTagPriv($gitlabID, $projectID, $tag)) && p('message:0') && e("名称已经被使用"); //使用重复的保护标签信息创建保护标签
