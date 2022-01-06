05192bdb3e070fc48eae2a50e65a1d78df6a0e28
zhouxin <zhouxin@easycorp.ltd>
2022-01-06 03:21:03 +0000

    Fix bug.

--- module/project/config.php
+++ module/project/config.php
@@ -131,5 +131,5 @@ $config->project->removePriv['doc']       = array('catalog', 'index');
 $config->project->removePriv['auditplan'] = array('delete');
 
 $config->project->checkList = new stdclass();
-$config->project->checkList->scrum     = array('bug', 'execution', 'meeting', 'release', 'build', 'doc', 'testtask', 'risk', 'issue', 'case');
+$config->project->checkList->scrum     = array('bug', 'execution', 'build', 'doc', 'case');
 $config->project->checkList->waterfall = array('execution', 'design', 'doc', 'bug', 'case', 'build', 'release', 'testtask', 'review', 'build', 'researchplan', 'issue', 'risk', 'opportunity', 'auditplan', 'gapanalysis', 'meeting');
