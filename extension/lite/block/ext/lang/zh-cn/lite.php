061e01b22a73089fd14717dabac7e70d0275e491
Wenrui LI <liwenrui@easycorp.ltd>
2022-01-11 16:56:52 +0800

    * adjust flowchart block for lite vision, task #47137.

---
+++ extension/lite/block/ext/lang/zh-cn/lite.php
@@ -0,0 +1,5 @@
+<?php
+$lang->block->flowchart            = array();
+$lang->block->flowchart['admin']   = array('管理员', '维护公司', '添加用户', '维护权限');
+$lang->block->flowchart['project'] = array('项目负责人', '创建项目', '维护团队', "维护目标", '创建看板');
+$lang->block->flowchart['dev']     = array('执行人员', '创建任务', '认领任务', '执行任务');
