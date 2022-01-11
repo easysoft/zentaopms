7b498302d08eec8d2a0d6ba57f59b00e47a67aba
Wenrui LI <liwenrui@easycorp.ltd>
2022-01-11 10:32:08 +0800

    * tweak project list for lite vision, task #47144.

--- extension/lite/project/ext/view/browse.lite.html.hook.php
+++ extension/lite/project/ext/view/browse.lite.html.hook.php
@@ -1,4 +1,8 @@
 <script>
+$('#sidebarHeader').remove();
+$('#sidebar').remove();
+$('.panel-actions').remove();
+$('.icon-project-manageProducts.icon-link').parent().remove();
 $(function()
 {
     $('.create-project-btn').attr('href', createLink('project', 'create'));
