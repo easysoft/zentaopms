b3d4163c110f707187b8d53d8a26673440282860
王怡栋 <wangyidong@cnezsoft.com>
2022-01-13 10:30:49 +0800

    * code for task #47151.

--- extension/lite/story/ext/lang/zh-cn/lite.php
+++ extension/lite/story/ext/lang/zh-cn/lite.php
@@ -3,3 +3,5 @@ $lang->story->module     = '所属目录';
 $lang->story->parent     = '父目标';
 $lang->story->legendSpec = '目标描述';
 $lang->story->children   = '子目标';
+
+$lang->story->legendProjectAndTask = '看板任务';
