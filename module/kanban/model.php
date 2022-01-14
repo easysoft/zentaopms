da89583f198c150a8dac32ff391737fa5701d380
hufangzhou <hufangzhou@easycorp.ltd>
2022-01-14 09:06:44 +0800

    * Adjust the format code.

--- module/kanban/model.php
+++ module/kanban/model.php
@@ -830,7 +830,7 @@ class kanbanModel extends model
         $cards = $this->dao->select('t1.*,t2.kanban,t2.lane,t2.column')->from(TABLE_KANBANCARD)->alias('t1')
             ->leftJoin(TABLE_KANBANCELL)->alias('t2')->on('t1.kanban=t2.kanban')
             ->where('deleted')->eq(0)
-            ->andWhere("INSTR(t2.cards,CONCAT(',',t1.id,','))")->gt(0)
+            ->andWhere("INSTR(t2.cards, CONCAT(',',t1.id,','))")->gt(0)
             ->andWhere('archived')->eq(0)
             ->andWhere('t2.kanban')->eq($kanbanID)
             //->orderBy('`order` asc')
