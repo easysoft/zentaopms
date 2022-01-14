f8ef544d94d276d8c4d441eac1913fbd6376642e
zenggang <zenggang@easycorp.ltd>
2022-01-14 01:54:48 +0000

    * Adjust code and fix bugs

--- module/group/lang/resource.php
+++ module/group/lang/resource.php
@@ -1389,7 +1389,6 @@ $lang->resource->sonarqube->create        = 'create';
 $lang->resource->sonarqube->edit          = 'edit';
 $lang->resource->sonarqube->delete        = 'delete';
 $lang->resource->sonarqube->browseProject = 'browseProject';
-$lang->resource->sonarqube->createProject = 'createProject';
 $lang->resource->sonarqube->deleteProject = 'deleteProject';
 $lang->resource->sonarqube->execJob       = 'execJob';
 
@@ -1398,7 +1397,6 @@ $lang->sonarqube->methodOrder[10] = 'create';
 $lang->sonarqube->methodOrder[15] = 'edit';
 $lang->sonarqube->methodOrder[20] = 'delete';
 $lang->sonarqube->methodOrder[25] = 'browseProject';
-$lang->sonarqube->methodOrder[30] = 'createProject';
 $lang->sonarqube->methodOrder[35] = 'deleteProject';
 $lang->sonarqube->methodOrder[40] = 'execJob';
 
