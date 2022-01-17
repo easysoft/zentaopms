4b0d2e842a8ccbea8f8081d5f45c628c11ce50aa
zenggang <zenggang@easycorp.ltd>
2022-01-17 06:49:36 +0000

    * Fix bug#18456,18393,18392

--- module/sonarqube/lang/en.php
+++ module/sonarqube/lang/en.php
@@ -33,7 +33,7 @@ $lang->sonarqube->placeholder->url  = "Please fill in the access address of the 
 
 $lang->sonarqube->nameRepeatError      = "Server name  already exists!";
 $lang->sonarqube->urlRepeatError       = 'Server address already exists!';
-$lang->sonarqube->validError           = 'Sonarqube user authority authentication failed!';
+$lang->sonarqube->validError           = 'SonarQube user authority authentication failed!';
 $lang->sonarqube->hostError            = "Invalid SonarQube service address.";
 $lang->sonarqube->confirmDelete        = 'Do you want to delete this SonarQube server?';
 $lang->sonarqube->confirmDeleteProject = 'Do you want to delete this SonarQube project?';
