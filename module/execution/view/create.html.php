361d245ac638f74215034da16416f0f728f18105
liumengyi <liumengyi@easycorp.ltd>
2022-01-19 11:13:43 +0800

    * Remove useless variables.

--- module/execution/view/create.html.php
+++ module/execution/view/create.html.php
@@ -43,7 +43,6 @@
 <?php js::set('multiBranchProducts', $multiBranchProducts);?>
 <?php js::set('systemMode', $config->systemMode);?>
 <?php js::set('projectID', $projectID);?>
-<?php js::set('projectModel', isset($project->model) ? $project->model : '');?>
 <div id='mainContent' class='main-content'>
   <div class='center-block'>
     <div class='main-header'>
