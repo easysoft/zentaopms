446f6e59d3349547d4b34eec616bf4c93283fc94
zhengrunyu <zhenrunyu@easycorp.ltd>
2022-01-21 14:30:13 +0800

    *Finish task 47997 48003.*Finish task 47997 48003.*Finish task 47997 48003.*Finish task 47997 48003.*Finish task 47997 48003.*Finish task 47997 48003.*Finish task 47997 48003.*Finish task 47997 48003.

--- db/update16.2.sql
+++ db/update16.2.sql
@@ -4,5 +4,8 @@ UPDATE `zt_kanbanspace` SET `type` = 'cooperation';
 ALTER TABLE `zt_kanban` ADD `importObject` varchar(255) NOT NULL AFTER `displayCards`;
 ALTER TABLE `zt_kanban` ADD `performable` enum ('0', '1') NOT NULL DEFAULT '0' AFTER `archived`;
 
+ALTER TABLE `zt_kanbancard` ADD `fromID` int NOT NULL  AFTER `group`;
+ALTER TABLE `zt_kanbancard` ADD `fromType` varchar(50) NOT NULL AFTER `fromID`;
+
 ALTER TABLE `zt_job` ADD `sonarqubeServer` mediumint(8) unsigned NOT NULL AFTER `triggerType`;
 ALTER TABLE `zt_job` ADD `projectKey` varchar(255) NOT NULL AFTER `sonarqubeServer`;
