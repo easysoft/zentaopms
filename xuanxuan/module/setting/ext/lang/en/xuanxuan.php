9da1c180f21f32e1ff29a95f6f7a973d89c4435d
songchenxuan <songchenxuan@easycorp.ltd>
2022-01-21 09:21:16 +0800

    *Xuanxuan lang.

--- xuanxuan/module/setting/ext/lang/en/xuanxuan.php
+++ xuanxuan/module/setting/ext/lang/en/xuanxuan.php
@@ -1,4 +1,8 @@
-<?php 
-$lang->setting->common      = 'Settings';
-$lang->setting->xuanxuan    = 'Client';
-$lang->setting->downloadXXD = 'Download XXD';
+<?php
+$lang->setting->common         = 'Settings';
+$lang->setting->xuanxuan       = 'Client';
+$lang->setting->downloadXXD    = 'Download XXD';
+$lang->setting->langs['zh-cn'] = 'Simplified Chinese';
+$lang->setting->langs['zh-tw'] = 'Traditional Chinese';
+$lang->setting->langs['en']    = 'English';
+
