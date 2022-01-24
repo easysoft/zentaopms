7234d6bc9b9d2d448319566d0f618b6cdcf07533
tianshujie <tianshujie@easycorp.ltd>
2022-01-24 16:37:46 +0800

    * Fix bug #18861.

--- module/my/view/execution.html.php
+++ module/my/view/execution.html.php
@@ -54,11 +54,9 @@
         <td><?php echo html::a($link, sprintf('%03d', $execution->id));?></td>
         <td class='c-name text-left'>
           <?php
-          if(isset($this->config->maxVersion))
-          {
-              if($execution->type === 'stage') echo "<span class='project-type-label label label-outline label-warning'>{$lang->project->stage}</span> ";
-              if($execution->type === 'sprint') echo "<span class='project-type-label label label-outline label-info'>{$lang->executionCommon}</span> ";
-          }
+          if($execution->type === 'stage') echo "<span class='project-type-label label label-outline label-warning'>{$lang->project->stage}</span> ";
+          if($execution->type === 'sprint') echo "<span class='project-type-label label label-outline label-info'>{$lang->executionCommon}</span> ";
+          if($execution->type === 'kanban') echo "<span class='project-type-label label label-outline label-info'>{$lang->execution->kanban}</span> ";
           echo html::a($link, $execution->name, '', "title='$execution->name'");
           ?>
         </td>
