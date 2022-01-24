23c9ebab657e28fe020cd1a4e208c898d13957b5
zhengrunyu <zhenrunyu@easycorp.ltd>
2022-01-24 08:47:23 +0800

    *Finish task 47997 48003.

--- module/kanban/view/enablearchived.html.php
+++ module/kanban/view/enablearchived.html.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The setarchivedfunction file of kanban module of ZenTaoPMS.
+ * The enableArchived file of kanban module of ZenTaoPMS.
  *
  * @copyright   Copyright 2009-2022 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
  * @license     ZPL (http://zpl.pub/page/zplv12.html)
