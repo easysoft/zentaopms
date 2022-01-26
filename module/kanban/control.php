aba10f04350a2f320ec4483fd42763ef22ff100a
liumengyi <liumengyi@easycorp.ltd>
2022-01-26 13:36:12 +0800

    * Fix bug #19020.

--- module/kanban/control.php
+++ module/kanban/control.php
@@ -208,7 +208,7 @@ class kanban extends control
         $users      = $this->loadModel('user')->getPairs('noclosed|nodeleted', '', 0, $spaceUsers);
 
         $this->view->users      = $users;
-        $this->view->spacePairs = array(0 => '') + $this->kanban->getSpacePairs($space->type);
+        $this->view->spacePairs = array(0 => '') + array($kanban->space => $space->name) + $this->kanban->getSpacePairs($space->type);
         $this->view->kanban     = $kanban;
         $this->view->type       = $space->type;
 
