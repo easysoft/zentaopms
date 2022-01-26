e398afaf884f0c57eb2ff7a67af4a2a7a50651cd
xieqiyu <xieqiyu@easycorp.ltd>
2022-01-26 17:18:54 +0800

    * Modify pop-up style.

--- module/kanban/view/importbuild.html.php
+++ module/kanban/view/importbuild.html.php
@@ -22,14 +22,13 @@
       <h2><?php echo $lang->kanban->importAB . $lang->kanban->importBuild;?></h2>
     </div>
   </div>
-  <div class='table-row p-10px'>
-    <div class='table-col w-150px text-center'><h4><?php echo $lang->kanban->selectedProject;?></h4></div>
-    <div class='table-col'><?php echo html::select('project', $projects, $selectedProjectID, "onchange='reloadObjectList(this.value)' class='form-control chosen' data-drop_direction='down'");?></div>
+  <div class='input-group space'>
+    <span class='input-group-addon'><?php echo $lang->kanban->selectedProject;?></span>
+    <?php echo html::select('project', $projects, $selectedProjectID, "onchange='reloadObjectList(this.value)' class='form-control chosen' data-drop_direction='down'");?>
+    <span class='input-group-addon'><?php echo $lang->kanban->selectedLane;?></span>
+    <?php echo html::select('lane', $lanePairs, '', "onchange='setTargetLane(this.value)' class='form-control chosen' data-drop_direction='down'");?>
   </div>
-  <div class='table-row p-10px'>
-    <div class='table-col w-150px text-center'><h4><?php echo $lang->kanban->selectedLane;?></h4></div>
-    <div class='table-col'><?php echo html::select('lane', $lanePairs, '', "onchange='setTargetLane(this.value)' class='form-control chosen' data-drop_direction='down'");?></div>
-  </div>
+  <?php if($builds2Imported):?>
   <form class='main-table' method='post' data-ride='table' target='hiddenwin' id='importBuildForm'>
     <table class='table table-fixed' id='buildList'>
       <thead>
@@ -73,13 +72,15 @@
         <tr><?php echo html::hidden('targetLane', key($lanePairs));?></tr>
       </tbody>
     </table>
-    <?php if($builds2Imported):?>
     <div class='table-footer'>
       <div class="checkbox-primary check-all"><label><?php echo $lang->selectAll?></label></div>
       <div class="table-actions btn-toolbar show-always"><?php echo html::submitButton($lang->kanban->importAB, '', 'btn btn-default');?></div>
       <?php $pager->show('right', 'pagerjs');?>
     </div>
-    <?php endif;?>
   </form>
+  <?php else:?>
+  <div class='table-empty-tip'><?php echo $lang->noData;?></div>
+  <?php endif;?>
 </div>
+<style>#project_chosen {width: 45% !important}</style>
 <?php include '../../common/view/footer.lite.html.php';?>
--- module/kanban/view/importplan.html.php
+++ module/kanban/view/importplan.html.php
@@ -22,14 +22,13 @@
       <h2><?php echo $lang->kanban->importAB . $lang->kanban->importPlan;?></h2>
     </div>
   </div>
-  <div class='table-row p-10px'>
-    <div class='table-col w-150px text-center'><h4><?php echo $lang->kanban->selectedProduct;?></h4></div>
-    <div class='table-col'><?php echo html::select('product', $products, $selectedProductID, "onchange='reloadObjectList(this.value)' class='form-control chosen' data-drop_direction='down'");?></div>
+  <div class='input-group space'>
+    <span class='input-group-addon'><?php echo $lang->kanban->selectedProduct;?></span>
+    <?php echo html::select('product', $products, $selectedProductID, "onchange='reloadObjectList(this.value)' class='form-control chosen' data-drop_direction='down'");?>
+    <span class='input-group-addon'><?php echo $lang->kanban->selectedLane;?></span>
+    <?php echo html::select('lane', $lanePairs, '', "onchange='setTargetLane(this.value)' class='form-control chosen' data-drop_direction='down'");?>
   </div>
-  <div class='table-row p-10px'>
-    <div class='table-col w-150px text-center'><h4><?php echo $lang->kanban->selectedLane;?></h4></div>
-    <div class='table-col'><?php echo html::select('lane', $lanePairs, '', "onchange='setTargetLane(this.value)' class='form-control chosen' data-drop_direction='down'");?></div>
-  </div>
+  <?php if($plans2Imported):?>
   <form class='main-table' method='post' data-ride='table' target='hiddenwin' id='importPlanForm'>
     <table class='table table-fixed' id='planList'>
       <thead>
@@ -77,13 +76,15 @@
         <tr><?php echo html::hidden('targetLane', key($lanePairs));?></tr>
       </tbody>
     </table>
-    <?php if($plans2Imported):?>
     <div class='table-footer'>
       <div class="checkbox-primary check-all"><label><?php echo $lang->selectAll?></label></div>
       <div class="table-actions btn-toolbar show-always"><?php echo html::submitButton($lang->kanban->importAB, '', 'btn btn-default');?></div>
       <?php $pager->show('right', 'pagerjs');?>
     </div>
-    <?php endif;?>
   </form>
+  <?php else:?>
+  <div class='table-empty-tip'><?php echo $lang->noData;?></div>
+  <?php endif;?>
 </div>
+<style>#product_chosen {width: 45% !important}</style>
 <?php include '../../common/view/footer.lite.html.php';?>
