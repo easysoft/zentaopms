5166aa63bb91dc5307de026c8012de850bf3d97b
Wenrui LI <liwenrui@easycorp.ltd>
2022-01-26 17:10:25 +0800

    * adjust message setting page.

--- extension/lite/action/ext/lang/zh-cn/lite.php
+++ extension/lite/action/ext/lang/zh-cn/lite.php
@@ -1,3 +1,5 @@
 <?php
 $lang->action->label->execution = "看板|execution|task|executionID=%s";
 $lang->action->label->task      = '卡片|task|view|taskID=%s';
+
+$lang->action->objectTypes['task'] = '卡片';
