aa36dc65fccc56691453f523ce6a2497d4028822
caoyanyi <caoyanyi@easycorp.ltd>
2022-01-27 05:28:52 +0000

    * Modify param and check value.

--- test/model/sonarqube/apivalidate.php
+++ test/model/sonarqube/apivalidate.php
@@ -18,8 +18,8 @@ $sonarqube = $tester->loadModel('sonarqube');
 $sonarqubeID     = 2;
 $sonarqubeServer = $tester->loadModel('pipeline')->getByID($sonarqubeID);
 $result = $sonarqube->apiValidate($sonarqubeServer->url, $sonarqubeServer->token);
-r($result) && p('valid') && e(1); //通过host,token检验api权限
+r($result) && p('valid') && e('1'); //通过host,token检验api权限
 
 $result = $sonarqube->apiValidate($sonarqubeServer->url, $sonarqubeServer->token . 'a');
 if($result->valid == false) $result = 'return false';
-r($result) && p('') && e(''); //通过正确的host，错误的token获取api权限
+r($result) && p('') && e('return false'); //通过正确的host，错误的token获取api权限
