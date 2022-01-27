fe653f643868c153ea8898941ebf252f1031ef09
hufangzhou <hufangzhou@easycorp.ltd>
2022-01-27 15:42:53 +0800

    * Fix bug #19105.

--- module/common/lang/common.php
+++ module/common/lang/common.php
@@ -79,6 +79,7 @@ $lang->file        = new stdclass();
 $lang->misc        = new stdclass();
 $lang->acl         = new stdclass();
 $lang->curd        = new stdclass();
+$lang->sonarqube   = new stdclass();
 
 $lang->projectbuild   = new stdclass();
 $lang->projectrelease = new stdclass();
