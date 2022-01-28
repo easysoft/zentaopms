c1692827e502480435af00c887165b7fc62636d9
tanghucheng <tanghucheng@easycorp.ltd>
2022-01-28 14:04:52 +0800

    * fix bug #18992.

--- extension/lite/story/ext/lang/zh-cn/lite.php
+++ extension/lite/story/ext/lang/zh-cn/lite.php
@@ -4,4 +4,5 @@ $lang->story->parent     = '父目标';
 $lang->story->legendSpec = '目标描述';
 $lang->story->children   = '子目标';
 
+$lang->story->legendLifeTime       = '目标的一生';
 $lang->story->legendProjectAndTask = '看板任务';
