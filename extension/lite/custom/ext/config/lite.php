eeccbc262845c4f7c2f779fc1c52058a8c1bb1cf
tanghucheng <tanghucheng@easycorp.ltd>
2022-02-08 13:02:21 +0800

    * conceal custom config.

---
+++ extension/lite/custom/ext/config/lite.php
@@ -0,0 +1,7 @@
+<?php
+$config->custom->requiredModules = array();
+$config->custom->requiredModules[10] = 'project';
+$config->custom->requiredModules[20] = 'story';
+$config->custom->requiredModules[35] = 'execution';
+$config->custom->requiredModules[40] = 'task';
+$config->custom->requiredModules[85] = 'user';
