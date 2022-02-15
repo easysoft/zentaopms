6e7b0af56204bb7cf09e2e8fb62e127e01fd3bbe
王怡栋 <wangyidong@cnezsoft.com>
2022-02-15 09:57:45 +0800

    * code for task #48841.

--- db/update16.3.sql
+++ db/update16.3.sql
@@ -16,3 +16,6 @@ ALTER TABLE `zt_block` ADD `vision` varchar(10) NOT NULL DEFAULT 'rnd' AFTER `ac
 ALTER TABLE `zt_block` DROP INDEX `account_module_type_order`, ADD UNIQUE `account_vision_module_type_order` (`account`, `vision`, `module`, `type`, `order`);
 ALTER TABLE `zt_effort` ADD `vision` varchar(10) NOT NULL DEFAULT 'rnd' AFTER `id`;
 ALTER TABLE `zt_story` ADD `vision` varchar(10) NOT NULL DEFAULT 'rnd' AFTER `id`;
+
+ALTER TABLE `zt_user` ADD `visions` varchar(20) NOT NULL AFTER `visits`;
+UPDATE `zt_user` SET `visions`='rnd,lite';
