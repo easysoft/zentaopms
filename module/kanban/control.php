66cf431d7d05e484e1912d3053ed2ee1fdcb18a6
zhangzilong <zhangzilong@easycorp.ltd>
2022-02-15 16:35:25 +0800

    * Fix bug #19312.

--- module/kanban/control.php
+++ module/kanban/control.php
@@ -1317,7 +1317,6 @@ class kanban extends control
         if($confirm == 'no')
         {
             $card   = $this->kanban->getCardByID($cardID);
-            a($card);
             $column = $this->dao->select('*')->from(TABLE_KANBANCOLUMN)->where('id')->eq($card->column)->fetch();
             if($column->archived or $column->deleted) die(js::alert(sprintf($this->lang->kanbancard->confirmRestoreTip, $column->name)));
 
