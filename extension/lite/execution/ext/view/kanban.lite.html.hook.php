99fa22440426d59191ffe2a5392c2e07d343d7e1
liumengyi <liumengyi@easycorp.ltd>
2022-02-17 15:07:51 +0800

    * Finish task #49371.

--- extension/lite/execution/ext/view/kanban.lite.html.hook.php
+++ extension/lite/execution/ext/view/kanban.lite.html.hook.php
@@ -1,6 +1,9 @@
 <style>
-.kanban-card .title i.icon-checked {display:none;}
-.kanban-card .infos .info-id {display:none;}
+.kanban-card .title i.icon-checked {display: none;}
+.kanban-card .infos .info-id {display: none;}
+.course-groupBy {width: 110px;}
+.course-groupBy li > a {float: left;}
+.course-groupBy .icon-check:before {margin-top: 5px; padding-top: 5px;}
 </style>
 <script>
 $('#mainMenu').remove();
