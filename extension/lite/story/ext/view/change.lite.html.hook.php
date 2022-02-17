41e878cf477b48c5e2dec39956428a63e8cc3e60
Zongjun Lan <jasonlan@lucasgc.com>
2022-02-17 17:22:48 +0800

    隐藏验收标准和影响范围

--- extension/lite/story/ext/view/change.lite.html.hook.php
+++ extension/lite/story/ext/view/change.lite.html.hook.php
@@ -1,4 +1,6 @@
 <script>
 $("a[href^='#affectedBugs']").parent().remove();
 $("a[href^='#affectedCases']").parent().remove();
+$("#mainContent").find("#verify").parent().parent().addClass("hide");
+$("#mainContent").find("#affectedProjects").parent().parent().parent().addClass("hide");
 </script>
