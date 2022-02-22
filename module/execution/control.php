c9685d4441f6ca9b6490a3275b865e389c3cbffa
zhengrunyu <zhenrunyu@easycorp.ltd>
2022-02-22 15:52:32 +0800

    *Finish task 49694 49695.

--- module/execution/control.php
+++ module/execution/control.php
@@ -2069,6 +2069,7 @@ class execution extends control
         $this->view->groupBy          = $groupBy;
         $this->view->productID        = $productID;
         $this->view->branchID         = $branchID;
+        $this->view->projectID        = $this->loadModel('task')->getProjectID($execution);
         $this->view->allPlans         = $allPlans;
         $this->view->kanbanData       = $kanbanData;
         $this->view->executionActions = $executionActions;
