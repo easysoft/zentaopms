f650bc44c77cfd4fb9b374adb2929f0c44c886e0
tanghucheng <tanghucheng@easycorp.ltd>
2022-02-22 09:19:27 +0800

    * Finish task #49415,49418,49425.

--- extension/lite/project/ext/view/edit.lite.html.hook.php
+++ extension/lite/project/ext/view/edit.lite.html.hook.php
@@ -1,6 +1,7 @@
 <script>
 $('#dataform table tbody tr:first-child').hide();
+$('#dataform table tbody tr:nth-child(5)').hide();
 $('#dataform table tbody tr:nth-child(7)').hide();
 $('#dataform table tbody tr:nth-child(8)').hide();
-$('#dataform table tbody tr:nth-child(12)').hide();
+$('#dataform table tbody tr:nth-child(13)').hide();
 </script>
