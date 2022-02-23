b8d6136e670d2326492ad1cb188138433fc32114
周旭东 <zhouxudong@easycorp.ltd>
2022-02-23 05:01:37 +0000

    Update control.php

--- module/bug/control.php
+++ module/bug/control.php
@@ -1531,7 +1531,7 @@ class bug extends control
     public function batchResolve($resolution, $resolvedBuild = '')
     {
         if(!$this->post->bugIDList) return print(js::locate($this->session->bugList, 'parent'));
-        
+
         $bugIDList = array_unique($this->post->bugIDList);
         $changes   = $this->bug->batchResolve($bugIDList, $resolution, $resolvedBuild);
         if(dao::isError()) return print(js::error(dao::getError()));
