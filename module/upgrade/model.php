f0c6a6933c4636575cd6037d0770ac176e1e840b
liumengyi <liumengyi@easycorp.ltd>
2022-02-23 09:37:52 +0800

    * Finish task #49661.

--- module/upgrade/model.php
+++ module/upgrade/model.php
@@ -5775,7 +5775,7 @@ class upgradeModel extends model
 
     /**
      * Move extent files.
-     * 
+     *
      * @access public
      * @return array
      */
@@ -5803,6 +5803,7 @@ class upgradeModel extends model
                 }
             }
             copy($fromPath, $toPath);
+            $this->replaceEXTFile($toPath);
         }
 
         return $response;
@@ -5840,5 +5841,32 @@ class upgradeModel extends model
          }
 
         return $response;
+    }
+
+    /**
+     * Replace extent file.
+     *
+     * @param  string $file
+     * @access public
+     * @return void
+     */
+    public function replaceEXTFile($file)
+    {
+        $filePathInfo = pathinfo($file);
+        $namePathInfo = pathinfo($filePathInfo['filename']);
+
+        $content = file_get_contents($file);
+        if(isset($namePathInfo['extension']) and $namePathInfo['extension'] == 'html')
+        {
+            $content = str_replace("include '../../", "include '../../../", $content);
+            $ch = 97;
+            for ($i = 0; $i < 26; $i ++)
+            {
+                $letter = chr($ch + $i);
+                $content = str_replace("../../../" . $letter, "../../../module/" . $letter, $content);
+            }
+            $content = str_replace("../../../module/module", "../../../module", $content);
+            file_put_contents($file, $content);
+        }
     }
 }
