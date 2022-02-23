51e54dcf74a6adbe60c97bd71006fa75d9335e8e
孙华伟 <sunhuawei@easycorp.ltd>
2022-02-23 17:15:56 +0800

    *Story #30529.

--- module/execution/control.php
+++ module/execution/control.php
@@ -839,7 +839,6 @@ class execution extends control
         $this->view->branchGroups      = $branchGroups;
         $this->view->canBeChanged      = common::canModify('execution', $execution); // Determines whether an object is editable.
         $this->view->showBranch        = $showBranch;
-
         $this->display();
     }
 
