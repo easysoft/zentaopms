0ba3f443cfb7566f78c4847fdb078fc548d70a9b
zhujinyong <zhujinyong@cnezsoft.com>
2022-02-24 14:58:02 +0800

    * Fix loadExtension.

--- framework/base/model.class.php
+++ framework/base/model.class.php
@@ -260,7 +260,9 @@ class baseModel
         $moduleExtPath = $this->app->getModuleExtPath($this->appName, $moduleName, 'model');
         if(!empty($moduleExtPath['site'])) $extensionFile = $moduleExtPath['site'] . 'class/' . $extensionName . '.class.php';
         if(!isset($extensionFile) or !file_exists($extensionFile)) $extensionFile = $moduleExtPath['custom'] . 'class/' . $extensionName . '.class.php';
-        if(!file_exists($extensionFile)) $extensionFile = $moduleExtPath['common'] . 'class/' . $extensionName . '.class.php';
+        if(!isset($extensionFile) or !file_exists($extensionFile)) $extensionFile = $moduleExtPath['vision'] . 'class/' . $extensionName . '.class.php';
+        if(!isset($extensionFile) or !file_exists($extensionFile)) $extensionFile = $moduleExtPath['xuan'] . 'class/' . $extensionName . '.class.php';
+        if(!isset($extensionFile) or !file_exists($extensionFile)) $extensionFile = $moduleExtPath['common'] . 'class/' . $extensionName . '.class.php';
 
         /* 设置扩展类的名字。Set the extension class name. */
         $extensionClass = $extensionName . ucfirst($moduleName);
