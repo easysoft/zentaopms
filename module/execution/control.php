0e15ad41b6ce238a822b7f97035fb512903a837a
zhouxudong <zhouxudong@easycorp.ltd>
2022-02-25 16:17:40 +0800

    * Finish task #49696.

--- module/execution/control.php
+++ module/execution/control.php
@@ -515,6 +515,20 @@ class execution extends control
             $mails = $this->execution->importBug($executionID);
             if(dao::isError()) return print(js::error(dao::getError()));
 
+            /* If link from no head then reload. */
+            if(isonlybody())
+            {
+                $kanbanData = $this->loadModel('kanban')->getRDKanban($executionID, $this->session->execLaneType ? $this->session->execLaneType : 'all');
+                $kanbanData = json_encode($kanbanData);
+                    
+                return print(js::reload('parent', '', "parent.parent.updateKanban($kanbanData)"));
+                // return print(js::closeModal('parent.parent', '', "parent.parent.updateKanban($kanbanData)"));
+            }
+            else
+            {
+                return print(js::reload('parent.parent'));
+            }
+            if(isonlybody()) return print(js::reload('parent.parent'));
             return print(js::locate($this->createLink('execution', 'importBug', "executionID=$executionID"), 'parent'));
         }
 
