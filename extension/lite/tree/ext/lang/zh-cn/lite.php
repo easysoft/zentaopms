449b45b9186cee4831508e0fce06b88fa749d01d
Zongjun Lan <jasonlan@lucasgc.com>
2022-02-28 13:25:34 +0800

    * fix task 49340

--- extension/lite/tree/ext/lang/zh-cn/lite.php
+++ extension/lite/tree/ext/lang/zh-cn/lite.php
@@ -3,13 +3,20 @@
 $lang->tree->all             = '所有目录';
 $lang->tree->allMenu         = $lang->tree->all;
 $lang->tree->manageMenu      = '维护目录';
-$lang->tree->manage          = '维护目录';
-$lang->tree->common          = '目录维护';
-$lang->tree->manageExecution = "维护{$lang->executionCommon}视图目录";
-$lang->tree->manageTaskChild = "维护{$lang->executionCommon}子目录";
-$lang->tree->name            = '目录名称';
 
 global $app;
+if ($app->rawMethod == 'tree' and $app->rawMethod == 'browsetask') 
+{
+    $lang->tree->manage          = '维护目录';
+    $lang->tree->common          = '目录维护';
+    $lang->tree->manageExecution = "维护{$lang->executionCommon}视图目录";
+    $lang->tree->manageTaskChild = "维护{$lang->executionCommon}子目录";
+    $lang->tree->name            = '目录名称';
+    $lang->tree->child           = '子目录';
+    $lang->tree->edit            = '编辑目录';
+    $lang->tree->delete          = '删除目录';
+}
+
 if($app->rawModule == 'tree' and $app->rawMethod == 'browse')
 {
     $lang->tree->edit             = '编辑目录';
