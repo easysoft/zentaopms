0512526c2391c685bf3e5157d9939215f4528c23
Zongjun Lan <jasonlan@lucasgc.com>
2022-03-03 10:07:55 +0800

    * fix bug 19713

--- extension/lite/tree/ext/control/browse.php
+++ extension/lite/tree/ext/control/browse.php
@@ -2,6 +2,18 @@
 helper::importControl('tree');
 class myTree extends tree
 {
+    /**
+     * Module browse.
+     *
+     * @param  int    $rootID
+     * @param  string $view story|bug|case|doc
+     * @param  int    $currentModuleID
+     * @param  int    $branch
+     * @param  string $from
+     * @param  int $projectID
+     * @access public
+     * @return void
+     */
     public function browse($rootID, $view, $currentModuleID = 0, $branch = 0, $from = '', $projectID = 0)
     {
         $this->view->projectID   = $projectID;
