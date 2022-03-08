b279fbbc3e6058e57d1c1c4c581997e5c2d31fb0
tanghucheng <tanghucheng@easycorp.ltd>
2022-03-08 10:19:07 +0800

    * Fix bug #50369.

--- extension/lite/story/ext/view/review.lite.html.hook.php
+++ extension/lite/story/ext/view/review.lite.html.hook.php
@@ -1,4 +1,4 @@
 <script>
-$("a[href^='#affectedBugs']").parent().remove();
-$("a[href^='#affectedCases']").parent().remove();
+$("#mainContent").find("#verify").closest("tr").addClass("hide");
+$("#mainContent").find("#affectedProjects").closest("tr").addClass("hide");
 </script>
