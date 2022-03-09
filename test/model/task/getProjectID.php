9257884f9a8948032ef4f7ffb677c7052b6f97a5
liumengyi <liumengyi@easycorp.ltd>
2022-03-09 10:22:01 +0800

    * Add auto test.

---
+++ test/model/task/getProjectID.php
@@ -0,0 +1,21 @@
+#!/usr/bin/env php
+<?php
+include dirname(dirname(dirname(__FILE__))) . '/lib/init.php';
+include dirname(dirname(dirname(__FILE__))) . '/class/task.class.php';
+su('admin');
+
+/**
+
+title=taskModel->getByList();
+cid=1
+pid=1
+
+根据executionID查找projectID >> 11
+
+*/
+
+$executionID = '101';
+
+$task = new taskTest();
+r($task->getProjectIDTest($executionID)) && p('project') && e('11'); //根据executionID查找projectID
+system("./ztest init");
