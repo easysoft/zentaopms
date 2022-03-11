bdfecb629d1929580d7154a44ae8f76290c65487
songchenxuan <songchenxuan@easycorp.ltd>
2022-03-11 11:07:29 +0800

    *Fix pri bug.

--- api/v1/entries/task.php
+++ api/v1/entries/task.php
@@ -102,7 +102,7 @@ class taskEntry extends Entry
         $oldTask = $this->loadModel('task')->getByID($taskID);
 
         /* Set $_POST variables. */
-        $fields = 'name,type,assignedTo,estimate,left,consumed,story,parent,execution,module,closedReason,status,estStarted,deadline';
+        $fields = 'name,type,assignedTo,pri,estimate,left,consumed,story,parent,execution,module,closedReason,status,estStarted,deadline';
         $this->batchSetPost($fields, $oldTask);
 
         $control = $this->loadController('task', 'edit');
