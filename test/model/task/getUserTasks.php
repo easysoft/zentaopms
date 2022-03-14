3c75a1555e6825565241e8c086d312a6c31111af
liumengyi <liumengyi@easycorp.ltd>
2022-03-14 14:39:59 +0800

    * Add auto test of task.

--- test/model/task/getUserTasks.php
+++ test/model/task/getUserTasks.php
@@ -19,3 +19,4 @@ $assignedTo = 'user92';
 
 $task = new taskTest();
 r($task->getUserTasksTest($taskID,$assignedTo)) && p('2:name') && e('开发任务12'); // 根据指派人员查看任务
+system("./ztest init");
--- test/model/task/processtasks.php
+++ test/model/task/processtasks.php
@@ -11,8 +11,8 @@ cid=1
 pid=1
 
 根据tasks计算executionID为1的执行下所有任务的进度 任务1   >> ,100
-根据tasks计算executionID为1的执行下所有任务的进度 任务601 >> ,100
-根据tasks计算executionID为1的执行下所有任务的进度 任务602 >> ,80
+根据tasks计算executionID为1的执行下所有任务的进度 任务601 >> 7,100
+根据tasks计算executionID为1的执行下所有任务的进度 任务602 >> 8,80
 根据tasks计算executionID为1的执行下所有任务的进度 任务602 >> ,71
 根据tasks计算executionID为2的执行下所有任务的进度 任务2   >> ,80
 根据tasks计算executionID为2的执行下所有任务的进度 任务602 >> 10,67
@@ -21,62 +21,14 @@ pid=1
 
 */
 
-$now = helper::now();
-
-$tasks1 = $dao->select('*')->from(TABLE_TASK)->where('execution')->eq('101')->andWhere('deleted')->eq(0)->fetchAll('id');
-$parents = array();
-foreach($tasks1 as $task)
-{
-    if($task->parent > 0) $parents[$task->parent] = $task->parent;
-}
-$parents = $dao->select('*')->from(TABLE_TASK)->where('id')->in($parents)->fetchAll('id');
-foreach($tasks1 as $task)
-{
-    if($task->parent > 0)
-    {
-        if(isset($tasks[$task->parent]))
-        {
-            $tasks[$task->parent]->children[$task->id] = $task;
-            unset($tasks[$task->id]);
-        }
-        else
-        {
-            $parent = $parents[$task->parent];
-            $task->parentName = $parent->name;
-        }
-    }
-}
-
-$tasks2 = $dao->select('*')->from(TABLE_TASK)->where('execution')->eq('102')->andWhere('deleted')->eq(0)->fetchAll('id');
-$parents = array();
-foreach($tasks2 as $task)
-{
-    if($task->parent > 0) $parents[$task->parent] = $task->parent;
-}
-$parents = $dao->select('*')->from(TABLE_TASK)->where('id')->in($parents)->fetchAll('id');
-foreach($tasks2 as $task)
-{
-    if($task->parent > 0)
-    {
-        if(isset($tasks[$task->parent]))
-        {
-            $tasks[$task->parent]->children[$task->id] = $task;
-            unset($tasks[$task->id]);
-        }
-        else
-        {
-            $parent = $parents[$task->parent];
-            $task->parentName = $parent->name;
-        }
-    }
-}
+$executionID = array('101', '102');
 
 $task = new taskTest();
-r($task->processTasksTest($tasks1)) && p('1:delay,progress')   && e(',100');  //根据tasks计算executionID为1的执行下所有任务的进度 任务1
-r($task->processTasksTest($tasks1)) && p('601:delay,progress') && e(',100');  //根据tasks计算executionID为1的执行下所有任务的进度 任务601
-r($task->processTasksTest($tasks1)) && p('602:delay,progress') && e(',80');   //根据tasks计算executionID为1的执行下所有任务的进度 任务602
-r($task->processTasksTest($tasks1)) && p('603:delay,progress') && e(',71');   //根据tasks计算executionID为1的执行下所有任务的进度 任务603
-r($task->processTasksTest($tasks2)) && p('2:delay,progress')   && e(',80');   //根据tasks计算executionID为2的执行下所有任务的进度 任务2
-r($task->processTasksTest($tasks2)) && p('604:delay,progress') && e('10,67'); //根据tasks计算executionID为2的执行下所有任务的进度 任务604
-r($task->processTasksTest($tasks2)) && p('605:delay,progress') && e(',64');   //根据tasks计算executionID为2的执行下所有任务的进度 任务605
-r($task->processTasksTest($tasks2)) && p('606:delay,progress') && e(',62');   //根据tasks计算executionID为2的执行下所有任务的进度 任务606
+r($task->processTasksTest($executionID['0'])) && p('1:delay,progress')   && e(',100');  //根据tasks计算executionID为1的执行下所有任务的进度 任务1
+r($task->processTasksTest($executionID['0'])) && p('601:delay,progress') && e('7,100');  //根据tasks计算executionID为1的执行下所有任务的进度 任务601
+r($task->processTasksTest($executionID['0'])) && p('602:delay,progress') && e('8,80');   //根据tasks计算executionID为1的执行下所有任务的进度 任务602
+r($task->processTasksTest($executionID['0'])) && p('603:delay,progress') && e(',71');   //根据tasks计算executionID为1的执行下所有任务的进度 任务603
+r($task->processTasksTest($executionID['1'])) && p('2:delay,progress')   && e(',80');   //根据tasks计算executionID为2的执行下所有任务的进度 任务2
+r($task->processTasksTest($executionID['1'])) && p('604:delay,progress') && e('10,67'); //根据tasks计算executionID为2的执行下所有任务的进度 任务604
+r($task->processTasksTest($executionID['1'])) && p('605:delay,progress') && e(',64');   //根据tasks计算executionID为2的执行下所有任务的进度 任务605
+r($task->processTasksTest($executionID['1'])) && p('606:delay,progress') && e(',62');   //根据tasks计算executionID为2的执行下所有任务的进度 任务606
