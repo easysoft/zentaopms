937c1a64ab2423438b15b9493a8739f7bad5f5d7
tianshujie <tianshujie@easycorp.ltd>
2022-03-14 16:49:46 +0800

    * Fix the problem that professional R&D Kanban does not have permission to view.

--- module/group/lang/resource.php
+++ module/group/lang/resource.php
@@ -785,6 +785,7 @@ $lang->resource->execution->addWhitelist      = 'addWhitelist';
 $lang->resource->execution->unbindWhitelist   = 'unbindWhitelist';
 $lang->resource->execution->storyEstimate     = 'storyEstimate';
 $lang->resource->execution->executionkanban   = 'kanbanAction';
+$lang->resource->execution->kanban            = 'kanban';
 //if($config->systemMode == 'classic') $lang->resource->project->list = 'list';
 
 //$lang->execution->methodOrder[0]   = 'index';
@@ -840,6 +841,7 @@ $lang->execution->methodOrder[250] = 'addWhitelist';
 $lang->execution->methodOrder[255] = 'unbindWhitelist';
 $lang->execution->methodOrder[260] = 'storyEstimate';
 $lang->execution->methodOrder[265] = 'executionkanban';
+$lang->execution->methodOrder[270] = 'kanban';
 
 /* Task. */
 $lang->resource->task = new stdclass();
