0fecbc4834a2c7c3209942646336d7220f88e116
liyang <liyang@easycorp.ltd>
2022-03-15 17:13:16 +0800

    add execution test

---
+++ test/model/execution/fixOrder.php
@@ -0,0 +1,23 @@
+#!/usr/bin/env php
+<?php
+include dirname(dirname(dirname(__FILE__))) . '/lib/init.php';
+include dirname(dirname(dirname(__FILE__))) . '/class/execution.class.php';
+su('admin');
+
+/**
+
+title=测试executionModel->fixOrderTest();
+cid=1
+pid=1
+
+敏捷执行关联用例 >> 101,1,1
+瀑布执行关联用例 >> 131,43,169
+看板执行关联用例 >> 161,68,269
+敏捷执行关联用例统计 >> 4
+瀑布执行关联用例统计 >> 4
+看板执行关联用例统计 >> 4
+
+*/
+
+$execution = new executionTest();
+r($execution->fixOrderTest()) && p('101:order') && e('15');  // 重置order
