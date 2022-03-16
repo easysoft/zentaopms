3b66cf35f2278a51dc955f993ead72460d171293
Zongjun Lan <jasonlan@lucasgc.com>
2022-03-16 14:37:42 +0800

    * fix bug #20787

--- extension/lite/group/ext/lang/en/lite.php
+++ extension/lite/group/ext/lang/en/lite.php
@@ -8,4 +8,8 @@ if($this->config->edition != 'open')
     $lang->resource->company->allTodo   = 'allTodo';
     $lang->resource->company->effort    = 'companyEffort';
     $lang->resource->company->alleffort = 'allEffort';
+    $lang->resource->sms                = new stdclass();
+    $lang->resource->sms->index         = 'index';
+    $lang->resource->sms->test          = 'test';
+    $lang->resource->sms->reset         = 'reset';
 }
