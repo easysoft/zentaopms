96f9a0819669495745a9d48eb58aa8a3cf7566d1
zhengrunyu <zhenrunyu@easycorp.ltd>
2022-03-18 13:15:31 +0800

    *Add api for child story.

--- config/routes.php
+++ config/routes.php
@@ -49,6 +49,7 @@ $routes['/stories/:id/close']          = 'storyClose';
 $routes['/stories/:id/active']         = 'storyActive';
 $routes['/stories/:id/assign']         = 'storyAssignto';
 $routes['/stories/:id/recordEstimate'] = 'storyRecordEstimate';
+$routes['/stories/:id/child']          = 'storyChild';
 
 $routes['/module/:id/stories'] = 'moduleStories';
 
