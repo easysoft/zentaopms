181cd4d58f0c587ce675f2e524a0ea7178671ddd
zhengrunyu <zhenrunyu@easycorp.ltd>
2022-03-18 14:32:42 +0800

    *Fix api.

--- api/v1/entries/productplans.php
+++ api/v1/entries/productplans.php
@@ -36,7 +36,6 @@ class productplansEntry extends entry
 
             foreach($plans as $plan)
             {
-                $plan->name = $plan->title;
                 if($plan->parent > 0 and isset($result[$plan->parent]))
                 {
                     $parentPlan = $result[$plan->parent];
