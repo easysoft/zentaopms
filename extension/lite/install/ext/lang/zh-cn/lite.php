ad7b6144f932886b9a1ea918b022d8b2ec2f5638
tanghucheng <tanghucheng@easycorp.ltd>
2022-03-18 14:35:02 +0800

    * Fix bug.

--- extension/lite/install/ext/lang/zh-cn/lite.php
+++ extension/lite/install/ext/lang/zh-cn/lite.php
@@ -21,3 +21,4 @@ $lang->install->successNoticeLabel = "<p>您已经成功安装禅道迅捷版%s�
 $lang->install->joinZentao         = <<<EOT
 <p>友情提示：为了您及时获得禅道迅捷版的最新动态，请在禅道社区(<a href='https://www.zentao.net' class='alert-link' target='_blank'>www.zentao.net</a>)进行登记。</p>
 EOT;
+
