c9904a1eae874480bbb14b25c5cdce4e9432cc9e
denghongtao <[denghongtao@easycorp.ltd]>
2022-03-18 11:31:33 +0800

    change getpairs.php.die;

--- test/model/productplan/getpairs.php
+++ test/model/productplan/getpairs.php
@@ -5,5 +5,4 @@ include dirname(dirname(dirname(__FILE__))) . '/class/productplan.class.php';
 
 $productplan = new Productplan('admin');
 
-var_dump($productplan->getPairs($product = array(28, 29, 30), $branch = '1', $expired = '', $skipParent = false));die;
 ?>
