c470946d4e2ef8fe889245bcff3153fe5833388d
sunjun <sunjun@easycorp.ltd>
2022-03-21 14:08:52 +0800

    fixbug #17317

--- module/projectrelease/control.php
+++ module/projectrelease/control.php
@@ -51,7 +51,7 @@ class projectrelease extends control
 
         $this->view->products = $this->products;
         $this->view->product  = $product;
-        $this->view->branches = (isset($product->type) and $product->type == 'normal') ? array() : $this->loadModel('branch')->getPairs($productID);
+        $this->view->branches = (isset($product->type) and $product->type == 'normal') ? array() : $this->loadModel('branch')->getPairs($productID, 'active');
         $this->view->branch   = $branch;
         $this->view->project  = $this->project->getByID($projectID);
     }
