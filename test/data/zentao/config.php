2d93fb840bb0067b8ce224fb142621286e6a3479
liyang <liyang@easycorp.ltd>
2022-03-22 14:02:51 +0800

    add data of relation

--- test/data/zentao/config.php
+++ test/data/zentao/config.php
@@ -19,6 +19,7 @@ $builder->story        = array('rows' => 400, 'extends' => array('story'));
 $builder->storymodule  = array('rows' => 800, 'extends' => array('module','storymodule'));
 $builder->storyplan    = array('rows' => 400, 'extends' => array('planstory'));
 $builder->storyspec    = array('rows' => 60, 'extends' => array('storyspec'));
+$builder->relation     = array('rows' => 12, 'extends' => array('relation'));
 $builder->task         = array('rows' => 600, 'extends' => array('task','task'));
 $builder->taskmore     = array('rows' => 300, 'extends' => array('task','moretask'));
 $builder->taskspec     = array('rows' => 600, 'extends' => array('taskspec'));
