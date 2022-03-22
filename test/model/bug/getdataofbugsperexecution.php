d9d2eea3b96c93ee4b9f2683b9b08346ad4e2374
liumengyi <liumengyi@easycorp.ltd>
2022-03-22 15:01:20 +0800

    * Add auto test of bug.

--- test/model/bug/getdataofbugsperexecution.php
+++ test/model/bug/getdataofbugsperexecution.php
@@ -9,9 +9,9 @@ title=bugModel->getDataOfBugsPerExecution();
 cid=1
 pid=1
 
-获取执行101数据 >> 迭代1,3,迭代1
-获取执行102数据 >> 迭代2,3,迭代2
-获取执行103数据 >> 迭代3,3,迭代3
+获取执行101数据 >> 迭代1,4,迭代1
+获取执行102数据 >> 迭代2,4,迭代2
+获取执行103数据 >> 迭代3,4,迭代3
 获取执行131数据 >> 阶段31,3,阶段31
 获取执行132数据 >> 阶段32,3,阶段32
 获取执行133数据 >> 阶段33,3,阶段33
@@ -23,9 +23,9 @@ pid=1
 
 
 $bug=new bugTest();
-r($bug->getDataOfBugsPerExecutionTest()) && p('101:name,value,title') && e('迭代1,3,迭代1');   // 获取执行101数据
-r($bug->getDataOfBugsPerExecutionTest()) && p('102:name,value,title') && e('迭代2,3,迭代2');   // 获取执行102数据
-r($bug->getDataOfBugsPerExecutionTest()) && p('103:name,value,title') && e('迭代3,3,迭代3');   // 获取执行103数据
+r($bug->getDataOfBugsPerExecutionTest()) && p('101:name,value,title') && e('迭代1,4,迭代1');   // 获取执行101数据
+r($bug->getDataOfBugsPerExecutionTest()) && p('102:name,value,title') && e('迭代2,4,迭代2');   // 获取执行102数据
+r($bug->getDataOfBugsPerExecutionTest()) && p('103:name,value,title') && e('迭代3,4,迭代3');   // 获取执行103数据
 r($bug->getDataOfBugsPerExecutionTest()) && p('131:name,value,title') && e('阶段31,3,阶段31'); // 获取执行131数据
 r($bug->getDataOfBugsPerExecutionTest()) && p('132:name,value,title') && e('阶段32,3,阶段32'); // 获取执行132数据
 r($bug->getDataOfBugsPerExecutionTest()) && p('133:name,value,title') && e('阶段33,3,阶段33'); // 获取执行133数据
