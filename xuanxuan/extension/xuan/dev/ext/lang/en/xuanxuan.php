9424577291c62a84a65fb004ae75adbd23e8c500
Wenrui LI <liwenrui@easycorp.ltd>
2022-03-23 11:22:27 +0800

    * change client to chat, and remove xuan promotional message, task #51510.

--- xuanxuan/extension/xuan/dev/ext/lang/en/xuanxuan.php
+++ xuanxuan/extension/xuan/dev/ext/lang/en/xuanxuan.php
@@ -1,11 +1,11 @@
 <?php
-$lang->dev->groupList['xuanxuan']= 'Client';
+$lang->dev->groupList['xuanxuan']= 'Chat';
 
-$lang->dev->tableList['im_chatuser']         = 'Client Users';
-$lang->dev->tableList['im_message']          = 'Client Message';
-$lang->dev->tableList['im_messagestatus']    = 'Client Message Status';
-$lang->dev->tableList['im_chat']             = 'Client Chat';
-$lang->dev->tableList['im_client']           = 'Client Upgrade';
-$lang->dev->tableList['im_conference']       = 'Client Conference';
-$lang->dev->tableList['im_conferenceaction'] = 'Client Conference Action';
-$lang->dev->tableList['im_queue']            = 'Client Queue';
+$lang->dev->tableList['im_chatuser']         = 'Chat Users';
+$lang->dev->tableList['im_message']          = 'Chat Message';
+$lang->dev->tableList['im_messagestatus']    = 'Chat Message Status';
+$lang->dev->tableList['im_chat']             = 'Chats';
+$lang->dev->tableList['im_client']           = 'Chat Client Upgrade';
+$lang->dev->tableList['im_conference']       = 'Chat Conference';
+$lang->dev->tableList['im_conferenceaction'] = 'Chat Conference Action';
+$lang->dev->tableList['im_queue']            = 'Chat Push Queue';
