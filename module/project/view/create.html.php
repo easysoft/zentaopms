3291a491c925ee94450930d092bcf0978311efce
zhouxin <zhouxin@easycorp.ltd>
2022-03-24 08:55:56 +0000

    Fix bug.

--- module/project/view/create.html.php
+++ module/project/view/create.html.php
@@ -89,8 +89,8 @@
           </td>
           <td colspan='2'><?php echo html::radio('delta', $lang->project->endList , '', "onclick='computeEndDate(this.value)'");?></td>
         </tr>
-        <?php $hiddenDays = $model == 'scrum' ? '' : 'hidden';?>
-        <tr id='daysBox' class="<?php echo $hiddenDays;?>">
+        <?php if($model == 'scrum'):?>
+        <tr id='daysBox'>
           <th><?php echo $lang->execution->days;?></th>
           <td>
             <div class='input-group'>
@@ -99,6 +99,7 @@
             </div>
           </td><td></td><td></td>
         </tr>
+        <?php endif;?>
         <tr>
           <th><?php echo $lang->project->manageProducts;?></th>
           <td class='text-left' id='productsBox' colspan="3">
