e4189b329c1498e19072c0e367ed3e46f4b938f9
孙广明 <sunguangming@cnezsoft.com>
2022-03-25 10:21:50 +0800

    * Fix whitelist bug.

--- test/model/story/getproductstories.php
+++ test/model/story/getproductstories.php
@@ -12,10 +12,12 @@ pid=1
 */
 
 global $tester;
-$stories1 = $tester->loadModel('story')->getProductStories(1);
+$tester->loadModel('story');
+$stories1 = $tester->story->getProductStories(1);
+$stories2 = $tester->story->getProductStories(1, 0, 0, 'all', 'requirement', 'id_asc', true);
+$stories3 = $tester->story->getProductStories(3, 0, 1830, 'all', 'story', 'id_asc', true);
+a($stories3);die;
 
 r(count($stories1)) && p()                 && e('2');             // 获取需求1可关联的需求数量
 r(count($stories2)) && p()                 && e('2');             // 获取需求2可关联的需求数量
 r($stories1)        && p('2:type,product') && e('story,1');       // 获取需求1可关联的需求id、product
-
-r() && p() && e();
