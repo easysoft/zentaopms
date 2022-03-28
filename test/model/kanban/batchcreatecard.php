737be004b150af172afd95aa05203b7c19b462f0
liumengyi <liumengyi@easycorp.ltd>
2022-03-28 17:16:42 +0800

    * Add auto test of kanban.

---
+++ test/model/kanban/batchcreatecard.php
@@ -0,0 +1,25 @@
+#!/usr/bin/env php
+<?php
+include dirname(dirname(dirname(__FILE__))) . '/lib/init.php';
+include dirname(dirname(dirname(__FILE__))) . '/class/kanban.class.php';
+su('admin');
+
+/**
+
+title=测试 kanbanModel->batchCreateCard();
+cid=1
+pid=1
+
+*/
+
+$no_name    = array('0' => '', '1' => '批量创建的卡片2', '2' => '批量创建的卡片3', '3' => '批量创建的卡片4');
+$estimate   = array('0' => '1', '1' => '-1', '2' => '-2', '3' => '-3');
+$endDitto   = array('1' => 'off', '2' => 'off', '3' => 'off', '4' => 'off');
+
+$kanban = new kanbanTest();
+
+r($kanban->batchCreateCardTest(array('name' => $no_name)))      && p('kanban,column,lane,type,cards') && e('1,1,1,common,,803,802,801,1,2,');                      // 批量创建卡片 其中一个没有名称
+r($kanban->batchCreateCardTest(array('estimate' => $estimate))) && p()                                && e('预计不能为负数!');                                     // 批量创建卡片 预计为负数
+r($kanban->batchCreateCardTest(array(), true))                  && p()                                && e('"截止日期"不能小于"预计开始"!');                       // 批量创建看板 截止日期小于开始日期
+r($kanban->batchCreateCardTest())                               && p('kanban,column,lane,type,cards') && e('1,1,1,common,,810,809,808,807,804,803,802,801,1,2,');  // 批量创建看板
+system("./ztest init");
