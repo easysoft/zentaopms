df1dc3dd9ba9a1f59197e4e890850513e91d96c2
Zongjun Lan <jasonlan@lucasgc.com>
2022-03-30 16:51:14 +0800

    * fix bug #20165

--- module/product/view/project.html.php
+++ module/product/view/project.html.php
@@ -16,7 +16,8 @@
     <?php echo html::a(inlink("project", "status=$key&productID=$productID"), "<span class='text'>{$label}</span>" . ($status == $key ? " <span class='label label-light label-badge'>" . count($projectStats) . "</span>" : ''), '', "class='btn btn-link" . ($status == $key ? ' btn-active-text' : '') . "' id='{$key}Tab'");?>
     <?php endforeach;?>
     <?php echo html::checkbox('involved', array('1' => $lang->project->mine), '', $this->cookie->involved ? 'checked=checked' : '');?>
-    <div class="tip"><icon class='icon icon-help' data-toggle='popover' data-trigger='focus hover' data-placement='right' data-tip-class='text-muted popover-sm' data-content=<?php echo $lang->product->projectInfo;?>></icon></div>
+    <div class="tip"><a data-toggle='tooltip' title='<?php echo $lang->product->projectInfo;?>'><i class='icon-help'></i></a></div>
+
   </div>
   <?php if($config->systemMode == 'new' and $branchStatus != 'closed'):?>
   <div class="btn-toolbar pull-right">
