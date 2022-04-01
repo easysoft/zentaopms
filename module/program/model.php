06f066f45d1b614fdd7af07d6e5a48c8b5738b33
Zongjun Lan <jasonlan@lucasgc.com>
2022-04-01 14:15:11 +0800

    * fix bug #18722

--- module/program/model.php
+++ module/program/model.php
@@ -240,6 +240,12 @@ class programModel extends model
             foreach($products as $product)
             {
                 $product->plans    = zget($plans, $product->id, array());
+                
+                /* Convert predefined HTML entities to characters. */
+                !empty($product->plans) && array_map(function($planVal){
+                    return $planVal->title = htmlspecialchars_decode($planVal->title,ENT_QUOTES);
+                },$product->plans);
+                
                 $product->releases = zget($releases, $product->id, array());
                 $projects          = zget($projectGroup, $product->id, array());
                 foreach($projects as $project)
