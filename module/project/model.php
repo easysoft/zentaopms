b980998c425d025f3ec25e1004b8a26e041d22d0
zhouxin <zhouxin@easycorp.ltd>
2022-04-01 06:35:26 +0000

    * Fix bug 20288.

--- module/project/model.php
+++ module/project/model.php
@@ -243,8 +243,7 @@ class projectModel extends model
     {
         if($project->parent == 0) return '';
 
-        $parentName = $this->dao->select('id,name')->from(TABLE_PROGRAM)->where('id')->in(trim($project->path, ','))->fetchPairs();
-        unset($parentName[$project->id]);
+        $parentName = $this->dao->select('id,name')->from(TABLE_PROGRAM)->where('id')->in(trim($project->path, ','))->andWhere('grade')->lt($project->grade)->fetchPairs();
 
         $parentProgram = '';
         foreach($parentName as $name) $parentProgram .= $name . '/';
