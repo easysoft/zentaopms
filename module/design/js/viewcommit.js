a1eb251907e9f3f7e8d592af07acef042cb1cbbc
tianshujie <tianshujie@easycorp.ltd>
2022-04-02 10:13:13 +0800

    * Fix bug #15460,17587.

---
+++ module/design/js/viewcommit.js
@@ -0,0 +1,20 @@
+$(function()
+{
+    $('#linkCommit').click(function()
+    {
+        if(repos.length == 0)
+        {
+            var onlybody    = config.onlybody;
+            config.onlybody = 'no';
+
+            var link = createLink('repo', 'create', 'objectID=' + projectID);
+
+            config.onlybody = onlybody;
+            window.parent.$.apps.open(link, 'devops');
+
+            parent.$('#triggerModal').modal('hide');
+
+            return false;
+        }
+    });
+});
