3f65617afc8a4eaf43dae1f514267f50057eebb0
liumengyi <liumengyi@easycorp.ltd>
2022-04-06 17:16:35 +0800

    * Add auto test of action.

---
+++ test/model/action/gettrashes.php
@@ -0,0 +1,24 @@
+#!/usr/bin/env php
+<?php
+include dirname(dirname(dirname(__FILE__))) . '/lib/init.php';
+include dirname(dirname(dirname(__FILE__))) . '/class/action.class.php';
+su('admin');
+
+/**
+
+title=测试 actionModel->getTrashes();
+cid=1
+pid=1
+
+*/
+
+$typeList = array('all', 'hidden');
+$orderBy  = array('id_desc', 'id_asc');
+$pager    = null;
+
+$action = new actionTest();
+
+r($action->getTrashesTest($typeList[0], $orderBy[0], $pager)) && p('0:objectType,objectId,objectName;1:objectType,objectId,objectName') && e('testsuite,,这是测试套件名称87;story,,软件需求48'); // 查询type all id desc排序的回收站信息
+r($action->getTrashesTest($typeList[0], $orderBy[1], $pager)) && p('0:objectType,objectId,objectName;1:objectType,objectId,objectName') && e('story,,软件需求48;testsuite,,这是测试套件名称87'); // 查询type all id desc排序的回收站信息
+r($action->getTrashesTest($typeList[1], $orderBy[0], $pager)) && p('0:objectType,objectId,objectName')                                  && e('testcase,,这个是测试用例9');                       // 查询type hidden id asc排序的回收站信息
+r($action->getTrashesTest($typeList[1], $orderBy[1], $pager)) && p('0:objectType,objectId,objectName')                                  && e('testcase,,这个是测试用例9');                       // 查询type hidden id asc排序的回收站信息
