19d4f476f79b57250e452d6adcf1641e95d64d92
Zongjun Lan <jasonlan@lucasgc.com>
2022-04-08 14:45:48 +0800

    * fix bug #21424

--- extension/lite/kanban/ext/view/view.lite.html.hook.php
+++ extension/lite/kanban/ext/view/view.lite.html.hook.php
@@ -1,3 +1,7 @@
 <script>
+<?php if(trim($config->visions, ',') == 'lite'): ?>
+$('#headerActions').css("right", '130px');
+<?php else: ?>
 $('#headerActions').css("right", '210px');
+<?php endif; ?>
 </script>
