a9c203b8665604df9164eba366a8448ca853b426
mayue <mayue@easycorp.ltd>
2022-04-11 08:38:13 +0800

    * Fix bug #21616.

--- module/testcase/js/common.js
+++ module/testcase/js/common.js
@@ -366,16 +366,27 @@ function loadStories(productID, moduleID, num)
     var storyLink    = createLink('story', 'ajaxGetProductStories', 'productID=' + productID + '&branch=' + branchID + '&moduleID=' + moduleID + '&storyID=0&onlyOption=false&status=noclosed&limit=50&type=full&hasParent=1&executionID=0&number=' + num);
     $.get(storyLink, function(stories)
     {
-        if(!stories) modules = '<select id="story' + num + '" name="story[' + num + ']" class="form-control"></select>';
+      if(!stories) stories = '<select id="story' + num + '" name="story[' + num + ']" class="form-control"></select>';
+      if(config.currentMethod == 'batchcreate')
+      {
         for(var i = num; i < 10 ; i ++)
         {
-            if(i != num && $('#module' + i).val() != 'ditto') break;
-            var nowStories = stories.replaceAll('story' + num, 'story' + i);
-            $('#story' + i).replaceWith(nowStories);
-            $('#story' + i + "_chosen").remove();
-            $('#story' + i).next('.picker').remove();
-            $('#story' + i).attr('name', 'story[' + i + ']');
-            $('#story' + i).chosen();
+          if(i != num && $('#module' + i).val() != 'ditto') break;
+          var nowStories = stories.replaceAll('story' + num, 'story' + i);
+          $('#story' + i).replaceWith(nowStories);
+          $('#story' + i + "_chosen").remove();
+          $('#story' + i).next('.picker').remove();
+          $('#story' + i).attr('name', 'story[' + i + ']');
+          $('#story' + i).chosen();
         }
+      }
+      else
+      {
+        $('#story' + num).replaceWith(stories);
+        $('#story' + num + "_chosen").remove();
+        $('#story' + num).next('.picker').remove();
+        $('#story' + num).attr('name', 'story[' + i + ']');
+        $('#story' + num).chosen();
+      }
     });
 }
