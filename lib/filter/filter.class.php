5a6ea32f8efc58be776939a4bd5701667da1af74
liyuchun <563917701@qq.com>
2022-04-28 09:09:08 +0000

    * Fix error.

--- lib/filter/filter.class.php
+++ lib/filter/filter.class.php
@@ -101,6 +101,11 @@ class fixer extends baseFixer
      * @return object
      */
     public function filterEmoji($value){
+        if(is_object($value) or is_array($value))
+        {
+            foreach($value as $subValue) $this->filterEmoji($subValue);
+        }
+
         $value = preg_replace_callback('/./u', function (array $match)
         {
             return strlen($match[0]) >= 4 ? '' : $match[0];
