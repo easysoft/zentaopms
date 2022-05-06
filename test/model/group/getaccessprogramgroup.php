d1580bd9e9d96c5cf55c1ed1b524fbafe9926d97
孙华伟 <sunhuawei@easycorp.ltd>
2022-05-06 15:50:38 +0800

    *Test getAccessProgramGroup.

--- test/model/group/getaccessprogramgroup.php
+++ test/model/group/getaccessprogramgroup.php
@@ -10,8 +10,11 @@ title=测试 groupModel->getAccessProgramGroup();
 cid=1
 pid=1
 
+测试获取group中用户个数 >> 14
+
 */
 
 $group = new groupTest();
+$AccessProgramGroup = $group->getAccessProgramGroupTest();
 
-r($group->getAccessProgramGroupTest()) && p() && e();
+r(count($AccessProgramGroup)) && p('') && e('14'); //测试获取group中用户个数
