557247387f33c07d49f8dc8ea41eaccaa486d988
孙华伟 <sunhuawei@easycorp.ltd>
2022-05-06 16:57:28 +0800

    *Test getList.

--- test/model/group/getlist.php
+++ test/model/group/getlist.php
@@ -10,8 +10,14 @@ title=测试 groupModel->getList();
 cid=1
 pid=1
 
+测试获取 groupList 的信息 >> 其他,others,其他
+测试获取 groupList 的信息 >> guest,guest,For guest
+测试获取 groupList 的信息 >> 项目管理员,projectAdmin,项目管理员可以维护项目的权限
+
 */
 
 $group = new groupTest();
 
-r($group->getListTest()) && p() && e();
+r($group->getListTest()) && p('9:name,role,desc')  && e('其他,others,其他');                                     // 测试获取 groupList 的信息
+r($group->getListTest()) && p('10:name,role,desc') && e('guest,guest,For guest');                                // 测试获取 groupList 的信息
+r($group->getListTest()) && p('12:name,role,desc') && e('项目管理员,projectAdmin,项目管理员可以维护项目的权限'); // 测试获取 groupList 的信息
