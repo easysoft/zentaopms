19835d835a6648e36d093ed043c517b69c25eaf3
孙华伟 <sunhuawei@easycorp.ltd>
2022-05-06 13:48:14 +0800

    *Test group getByAccount.

--- test/model/group/getbyaccount.php
+++ test/model/group/getbyaccount.php
@@ -10,8 +10,17 @@ title=测试 groupModel->getByAccount();
 cid=1
 pid=1
 
+测试获取account 1 的信息 >> 3,测试,qa
+测试获取account 2 的信息 >> 2,研发,dev
+测试获取account 3 的信息 >> 2,研发,dev
+测试获取account 4 的信息 >> 2,研发,dev
+测试获取account 5 的信息 >> 3,测试,qa
+
 */
 
 $group = new groupTest();
-
-r($group->getByAccountTest()) && p() && e();
+r($group->getByAccountTest('test2')) && p('3:id,name,role') && e('3,测试,qa');  //测试获取account 1 的信息
+r($group->getByAccountTest('dev2'))  && p('2:id,name,role') && e('2,研发,dev'); //测试获取account 2 的信息
+r($group->getByAccountTest('po48'))  && p('2:id,name,role') && e('2,研发,dev'); //测试获取account 3 的信息
+r($group->getByAccountTest('td2'))   && p('2:id,name,role') && e('2,研发,dev'); //测试获取account 4 的信息
+r($group->getByAccountTest('pd2'))   && p('3:id,name,role') && e('3,测试,qa');  //测试获取account 5 的信息
