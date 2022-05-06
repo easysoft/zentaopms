3f01f1a0a195f85f91052cde059ecc3c0f378ea1
zhouxudong <zhouxudong@easycorp.ltd>
2022-05-06 09:10:18 +0000

    * Add message auto test.

--- test/model/message/gettolist.php
+++ test/model/message/gettolist.php
@@ -14,9 +14,9 @@ pid=1
 
 $message = new messageTest();
 
-r($message->getToListTest('todo'))     && p() && e('admin'); //通过一条todo数据展示$toList
-r($message->getToListTest('testtask')) && p() && e('user3'); //通过一条testtask数据展示$toList
+r($message->getToListTest('todo'))     && p() && e('admin');        //通过一条todo数据展示$toList
+r($message->getToListTest('testtask')) && p() && e('user3');        //通过一条testtask数据展示$toList
 r($message->getToListTest('meeting'))  && p() && e('admin,admin,'); //通过一条meeting数据展示$toList
-r($message->getToListTest('mr'))       && p() && e('admin,admin'); //通过一条mr数据展示$toList
-r($message->getToListTest('release'))  && p() && e('po1'); //通过一条release数据展示$toList
-r($message->getToListTest('task'))     && p() && e('po82'); //通过一条task数据展示$toList
+r($message->getToListTest('mr'))       && p() && e('admin,admin');  //通过一条mr数据展示$toList
+r($message->getToListTest('release'))  && p() && e('po1');          //通过一条release数据展示$toList
+r($message->getToListTest('task'))     && p() && e('po82');         //通过一条task数据展示$toList
