c82550c55c6357dcffe54d6ad87f71f961867aed
wangyidong <wangyidong@cnezsoft.com>
2022-05-07 14:57:44 +0800

    * Finish jenkins test.

--- test/model/git/getrepologs.php
+++ test/model/git/getrepologs.php
@@ -18,9 +18,6 @@ pid=1
 
 $git = new gitTest();
 
-/* Fix error Gitlab url. */
-$tester->dao->update(TABLE_PIPELINE)->set('url')->eq('http://10.0.1.161:51080')->where('id')->eq(1)->exec();
-
 $repo = $tester->dao->select('*')->from(TABLE_REPO)->limit(1)->fetch();
 if(strtolower($repo->SCM) == 'gitlab') $repo = $tester->loadModel('repo')->processGitlab($repo);
 r($git->getRepoLogs($repo)) && p() && e(1);     // 获取版本库的tags
---
+++ test/model/jenkins/getbyid.php
@@ -0,0 +1,30 @@
+#!/usr/bin/env php
+<?php
+include dirname(dirname(dirname(__FILE__))) . '/lib/init.php';
+include dirname(dirname(dirname(__FILE__))) . '/class/jenkins.class.php';
+su('admin');
+
+/**
+
+title=测试jenkinsModel->gitById();
+cid=1
+pid=1
+
+使用存在的ID   >> 3
+使用空的ID     >> 0
+使用不存在的ID >> 0
+
+*/
+
+$jenkins = new jenkinsTest();
+
+$jenkinsID = 3;
+r($jenkins->getById($jenkinsID)) && p('id') && e('3');    // 使用存在的ID
+
+$jenkinsID = 0;
+r($jenkins->getById($jenkinsID)) && p() && e(0);     // 使用空的ID
+
+$jenkinsID = 111;
+r($jenkins->getById($jenkinsID)) && p() && e(0);     // 使用不存在的ID
+
+system("./ztest init");
