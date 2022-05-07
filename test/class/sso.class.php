d27114d98d78e86276cc7b50f14be859c248f287
caoyanyi <caoyanyi@easycorp.ltd>
2022-05-07 10:32:17 +0800

    * Remove bind func.

--- test/class/sso.class.php
+++ test/class/sso.class.php
@@ -26,34 +26,4 @@ class ssoTest
         $object = $this->objectModel->getBindUser($param['account']);
         return $object;
     }
-
-    /**
-     * Test bind a user.
-     *
-     * @param  array  $param
-     * @param  string $bindUser
-     * @access public
-     * @return void
-     */
-    public function bindTest($param = array(), $bindUser = '')
-    {
-        global $tester;
-        if($bindUser)
-        {
-            $tester->session->ssoData = new stdClass();
-            $tester->session->ssoData->account = $bindUser;
-        }
-
-        foreach($param as $key => $value) $_POST[$key] = $value;
-        if(isset($param['password1'])) $_POST['passwordStrength'] = strlen($param['password1']);
-
-        $object = $this->objectModel->bind();
-
-        unset($_POST);
-
-        if(!$object) return 'fail';
-        if(is_int($object)) return 'user get error';
-
-        return $object;
-    }
 }
