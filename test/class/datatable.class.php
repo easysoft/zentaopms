3447fa62374901595ca70399f5a63a32f3d7e5d1
zenggang <zenggang@easycorp.ltd>
2022-05-07 09:43:25 +0000

    * Dev unit

--- test/class/datatable.class.php
+++ test/class/datatable.class.php
@@ -8,6 +8,14 @@ class datatableTest
         $this->objectModel = $tester->loadModel('datatable');
     }
 
+    /**
+     * Test get field list.
+     *
+     * @param string $module
+     * @param string $method
+     * @access public
+     * @return void
+     */
     public function getFieldListTest($module, $method)
     {
         $module = zget($this->objectModel->config->datatable->moduleAlias, "$module-$method", $module);
@@ -15,6 +23,14 @@ class datatableTest
         return $result;
     }
 
+    /**
+     * Test get save setting field.
+     *
+     * @param string $module
+     * @param string $method
+     * @access public
+     * @return void
+     */
     public function getSettingTest($module, $method)
     {
         $this->objectModel->app->methodName = $method;
@@ -22,6 +38,13 @@ class datatableTest
         return $result;
     }
 
+    /**
+     * Test print table head.
+     *
+     * @param array $data
+     * @access public
+     * @return void
+     */
     public function printHeadTest($data)
     {
         ob_start();
@@ -36,6 +59,13 @@ class datatableTest
         return $result;
     }
 
+    /**
+     * Test set fixed field width.
+     *
+     * @param object $setting
+     * @access public
+     * @return void
+     */
     public function setFixedFieldWidthTest($setting)
     {
         $result = $this->objectModel->setFixedFieldWidth($setting);
