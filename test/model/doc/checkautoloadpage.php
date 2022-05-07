3aaa5e2a4f27b921cccbfdb6ed8dd528e0f996b4
wangxiaomeng <wangxiaomeng@easycorp.ltd>
2022-05-07 15:30:01 +0800

    edit doc

--- test/model/doc/checkautoloadpage.php
+++ test/model/doc/checkautoloadpage.php
@@ -10,10 +10,14 @@ title=测试 docModel->checkAutoloadPage();
 cid=1
 pid=1
 
-
-
+查询文档type为text时数据 >>1
+查询文档type为markdown时数据 >>1
+查询文档不存在时数据 >>1
 */
+$docID = array('1', '3', '');
 
 $doc = new docTest();
 
-r($doc->checkAutoloadPageTest()) && p() && e();
+r($doc->checkAutoloadPageTest($docID[0])) && p() && e('1'); //查询文档type为text时数据
+r($doc->checkAutoloadPageTest($docID[1])) && p() && e('1'); //查询文档type为markdown时数据
+r($doc->checkAutoloadPageTest($docID[2])) && p() && e('1'); //查询文档不存在时数据
