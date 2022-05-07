a6d2780d2e84da88781c6be2460cf51f7dc818c8
renying <renying@easycorp.ltd>
2022-05-07 08:37:36 +0800

    Add weekly module auto test.

--- test/class/weekly.class.php
+++ test/class/weekly.class.php
@@ -79,87 +79,167 @@ class weeklyTest
         return $objects;
     }
 
+    /**
+     * Test getStaff
+     *
+     * @param  int    $project
+     * @param  string $date
+     * @access public
+     * @return array
+     */
     public function getStaffTest($project, $date = '')
     {
-        $objects = $this->objectModel->getStaff($project, $date = '');
+        $objects = $this->objectModel->getStaff($project, $date);
 
         if(dao::isError()) return dao::getError();
 
         return $objects;
     }
 
-    public function getFinishedTest($project, $date = '', $pager = null)
+    /**
+     * Test getFinished
+     *
+     * @param  int    $project
+     * @param  string $date
+     * @access public
+     * @return void
+     */
+    public function getFinishedTest($project, $date = '')
     {
-        $objects = $this->objectModel->getFinished($project, $date = '', $pager = null);
+        $objects = $this->objectModel->getFinished($project, $date);
 
         if(dao::isError()) return dao::getError();
 
         return $objects;
     }
 
+    /**
+     * Test getPostponed
+     *
+     * @param  int    $project
+     * @param  string $date
+     * @access public
+     * @return void
+     */
     public function getPostponedTest($project, $date = '')
     {
-        $objects = $this->objectModel->getPostponed($project, $date = '');
+        $objects = $this->objectModel->getPostponed($project, $date);
 
         if(dao::isError()) return dao::getError();
 
         return $objects;
     }
 
+    /**
+     * Test getTasksOfNextWeek
+     *
+     * @param  int    $project
+     * @param  string $date
+     * @access public
+     * @return void
+     */
     public function getTasksOfNextWeekTest($project, $date = '')
     {
-        $objects = $this->objectModel->getTasksOfNextWeek($project, $date = '');
+        $objects = $this->objectModel->getTasksOfNextWeek($project, $date);
 
         if(dao::isError()) return dao::getError();
 
         return $objects;
     }
 
+    /**
+     * Test getWorkloadByType
+     *
+     * @param  int    $project
+     * @param  string $date
+     * @access public
+     * @return object
+     */
     public function getWorkloadByTypeTest($project, $date = '')
     {
-        $objects = $this->objectModel->getWorkloadByType($project, $date = '');
+        $objects = $this->objectModel->getWorkloadByType($project, $date);
 
         if(dao::isError()) return dao::getError();
 
         return $objects;
     }
 
+    /**
+     * Test getPlanedTaskByWeek
+     *
+     * @param  int    $project
+     * @param  string $date
+     * @access public
+     * @return array
+     */
     public function getPlanedTaskByWeekTest($project, $date = '')
     {
-        $objects = $this->objectModel->getPlanedTaskByWeek($project, $date = '');
+        $objects = $this->objectModel->getPlanedTaskByWeek($project, $date);
 
         if(dao::isError()) return dao::getError();
 
         return $objects;
     }
 
+    /**
+     * Test getPV
+     *
+     * @param  int    $project
+     * @param  string $date
+     * @access public
+     * @return int
+     */
     public function getPVTest($projectID, $date = '')
     {
-        $objects = $this->objectModel->getPV($projectID, $date = '');
+        $objects = $this->objectModel->getPV($projectID, $date);
 
         if(dao::isError()) return dao::getError();
 
         return $objects;
     }
 
+    /**
+     * Test get EV data.
+     *
+     * @param  int    $projectID
+     * @param  string $date
+     * @access public
+     * @return int
+     */
     public function getEVTest($projectID, $date = '')
     {
-        $objects = $this->objectModel->getEV($projectID, $date = '');
+        $objects = $this->objectModel->getEV($projectID, $date);
 
         if(dao::isError()) return dao::getError();
 
         return $objects;
     }
 
+    /**
+     * Test get AC data.
+     *
+     * @param  int    $project
+     * @param  string $date
+     * @access public
+     * @return int
+     */
     public function getACTest($project, $date = '')
     {
-        $objects = $this->objectModel->getAC($project, $date = '');
+        $objects = $this->objectModel->getAC($project, $date);
 
         if(dao::isError()) return dao::getError();
 
         return $objects;
     }
 
+    /**
+     * Test get SV data.
+     *
+     * @param  int    $ev
+     * @param  int    $pv
+     * @access public
+     * @return int
+     */
     public function getSVTest($ev, $pv)
     {
         $objects = $this->objectModel->getSV($ev, $pv);
@@ -169,6 +249,14 @@ class weeklyTest
         return $objects;
     }
 
+    /**
+     * Test getCV
+     *
+     * @param  int    $ev
+     * @param  int    $ac
+     * @access public
+     * @return int
+     */
     public function getCVTest($ev, $ac)
     {
         $objects = $this->objectModel->getCV($ev, $ac);
@@ -178,9 +266,17 @@ class weeklyTest
         return $objects;
     }
 
+    /**
+     * Test getTips
+     *
+     * @param  string $type
+     * @param  int    $data
+     * @access public
+     * @return string
+     */
     public function getTipsTest($type = 'progress', $data = 0)
     {
-        $objects = $this->objectModel->getTips($type = 'progress', $data = 0);
+        $objects = $this->objectModel->getTips($type, $data);
 
         if(dao::isError()) return dao::getError();
 
--- test/model/weekly/getev.php
+++ test/model/weekly/getev.php
@@ -10,8 +10,22 @@ title=测试 weeklyModel->getEV();
 cid=1
 pid=1
 
+测试projectID值为0，date值为2022-05-01 >> 771.55
+测试projectID值为0，date值为空 >> 2342.78
+测试projectID值为11，date值为2022-05-01 >> 15.3
+测试projectID值为11，date值为空 >> 54.18
+测试projectID值为41，date值为2022-05-01 >> 6.91
+测试projectID值为41，date值为空 >> 19.87
+
 */
+$projectIDList = array(0, 11, 41);
+$dateList      = array('2022-05-01', '');
 
 $weekly = new weeklyTest();
 
-r($weekly->getEVTest()) && p() && e();
+r($weekly->getEVTest($projectIDList[0], $dateList[0])) && p() && e('771.55');  //测试projectID值为0，date值为2022-05-01
+r($weekly->getEVTest($projectIDList[0], $dateList[1])) && p() && e('2342.78'); //测试projectID值为0，date值为空
+r($weekly->getEVTest($projectIDList[1], $dateList[0])) && p() && e('15.3');    //测试projectID值为11，date值为2022-05-01
+r($weekly->getEVTest($projectIDList[1], $dateList[1])) && p() && e('54.18');   //测试projectID值为11，date值为空
+r($weekly->getEVTest($projectIDList[2], $dateList[0])) && p() && e('6.91');    //测试projectID值为41，date值为2022-05-01
+r($weekly->getEVTest($projectIDList[2], $dateList[1])) && p() && e('19.87');   //测试projectID值为41，date值为空
