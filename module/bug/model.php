91366891f49c5677041c3280d8d0f88a8dd72001
wangyuting2 <851424971@qq.com>
2022-05-09 13:46:59 +0800

    * Adjust bug action menu.

--- module/bug/model.php
+++ module/bug/model.php
@@ -3241,22 +3241,22 @@ class bugModel extends model
         $convertParams = "productID=$bug->product&branch=$bug->branch&moduleID=0&from=bug&bugID=$bug->id";
         $toStoryParams = "product=$bug->product&branch=$bug->branch&module=0&story=0&execution=0&bugID=$bug->id";
 
-        $menu  .= $this->buildMenu('bug', 'confirmBug', $params, $bug, $type, 'ok', '', "iframe", true);
+        $menu .= $this->buildMenu('bug', 'confirmBug', $params, $bug, $type, 'ok', '', "iframe", true);
         if($type == 'view') $menu .= $this->buildMenu('bug', 'assignTo', $params, $bug, $type, '', '', "iframe", true);
-        $menu  .= $this->buildMenu('bug', 'resolve', $params, $bug, $type, 'checked', '', "iframe", true);
-        $menu  .= $this->buildMenu('bug', 'close', $params, $bug, $type, '', '', "text-danger iframe showinonlybody", true);
-        if($type == 'view') $menu  .= $this->buildMenu('bug', 'activate', $params, $bug, $type, '', '', "text-success iframe showinonlybody", true);
-        if($this->app->tab != 'product')
+        $menu .= $this->buildMenu('bug', 'resolve', $params, $bug, $type, 'checked', '', "iframe", true);
+        $menu .= $this->buildMenu('bug', 'close', $params, $bug, $type, '', '', "text-danger iframe showinonlybody", true);
+        if($type == 'view') $menu .= $this->buildMenu('bug', 'activate', $params, $bug, $type, '', '', "text-success iframe showinonlybody", true);
+        if($type == 'view' && $this->app->tab != 'product')
         {
-            $menu  .= $this->buildMenu('bug', 'toStory', $toStoryParams, $bug, $type, $this->lang->icons['story'], '', '', '', "data-app='product'", $this->lang->bug->toStory);
-            $menu  .= $this->buildMenu('bug', 'createCase', $convertParams, $bug, $type, 'sitemap');
+            $menu .= $this->buildMenu('bug', 'toStory', $toStoryParams, $bug, $type, $this->lang->icons['story'], '', '', '', "data-app='product'", $this->lang->bug->toStory);
+            $menu .= $this->buildMenu('bug', 'createCase', $convertParams, $bug, $type, 'sitemap');
         }
-        if($type == 'view') $menu  .= "<div class='divider'></div>";
-        $menu  .= $this->buildFlowMenu('bug', $bug, $type, 'direct');
-        if($type == 'view') $menu  .= "<div class='divider'></div>";
-        $menu  .= $this->buildMenu('bug', 'edit', $params, $bug, $type);
+        if($type == 'view') $menu .= "<div class='divider'></div>";
+        $menu .= $this->buildFlowMenu('bug', $bug, $type, 'direct');
+        if($type == 'view') $menu .= "<div class='divider'></div>";
+        $menu .= $this->buildMenu('bug', 'edit', $params, $bug, $type);
         if($this->app->tab != 'product') $menu .= $this->buildMenu('bug', 'create', $copyParams, $bug, $type, 'copy');
-        $menu  .= $this->buildMenu('bug', 'delete', $params, $bug, $type, 'trash', 'hiddenwin', "showinonlybody");
+        if($type == 'view') $menu .= $this->buildMenu('bug', 'delete', $params, $bug, $type, 'trash', 'hiddenwin', "showinonlybody");
         return $menu;
     }
 }
