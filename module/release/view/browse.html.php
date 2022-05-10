d88c1310b6a1bdad3636e8cccd28a14ae3d14682
sunjun <sunjun@easycorp.ltd>
2022-05-10 07:37:12 +0000

    sprint_sunjun_53654

--- module/release/view/browse.html.php
+++ module/release/view/browse.html.php
@@ -105,5 +105,8 @@
     </tbody>
   </table>
   <?php endif;?>
+<div class='table-footer'>
+  <?php $pager->show('right', 'pagerjs');?>
+</div>
 </div>
 <?php include '../../common/view/footer.html.php';?>
