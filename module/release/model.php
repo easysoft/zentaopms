0a23be8a5b8b05932c6f8b13cbbf8e9ff8533bd2
sunjun <sunjun@easycorp.ltd>
2022-05-10 08:22:25 +0000

    task_sunjun_53672

--- module/release/model.php
+++ module/release/model.php
@@ -121,9 +121,6 @@ class releaseModel extends model
         /* Check build if build is required. */
         if(strpos($this->config->release->create->requiredFields, 'build') !== false and $this->post->build == false) return dao::$errors[] = sprintf($this->lang->error->notempty, $this->lang->release->build);
 
-        /* Check date must be not more than today. */
-        if($this->post->date > date('Y-m-d')) return dao::$errors[] = $this->lang->release->errorDate;
-
         $release = fixer::input('post')
             ->add('product', (int)$productID)
             ->add('branch',  (int)$branch)
