1b5b443174be469f682053478e26d5f3a3ee411d
lanzongjun <lanzongjun@easycorp.ltd>
2022-05-10 15:28:18 +0800

    * fix task #53591

--- db/update16.5.sql
+++ db/update16.5.sql
@@ -89,3 +89,17 @@ REPLACE INTO `zt_zoutput` (`id`, `activity`, `name`, `content`, `optional`, `sta
 DELETE FROM `zt_config` WHERE `section` = 'customMenu';
 
 UPDATE `zt_story` SET `plan` = '' WHERE `plan` = 0;
+
+UPDATE `zt_workflowfield` SET `control` = 'date' WHERE `module` = 'product' and `field` = 'createdDate';
+UPDATE `zt_workflowfield` SET `control` = 'date' WHERE `module` = 'productplan' and `field` = 'begin';
+UPDATE `zt_workflowfield` SET `control` = 'date' WHERE `module` = 'productplan' and `field` = 'end';
+UPDATE `zt_workflowfield` SET `control` = 'radio' WHERE `module` = 'productplan' and `field` = 'deleted';
+UPDATE `zt_workflowfield` SET `control` = 'select', `options` = 12 WHERE `module` = 'productplan' and `field` = 'parent';
+UPDATE `zt_workflowfield` SET `control` = 'date' WHERE `module` = 'release' and `field` = 'date';
+UPDATE `zt_workflowfield` SET `control` = 'richtext' WHERE `module` = 'release' and `field` = 'desc';
+UPDATE `zt_workflowfield` SET `control` = 'radio' WHERE `module` = 'release' and `field` = 'deleted';
+UPDATE `zt_workflowfield` SET `control` = 'date' WHERE `module` = 'story' and `field` = 'assignedDate';
+UPDATE `zt_workflowfield` SET `control` = 'date' WHERE `module` = 'story' and `field` = 'lastEditedDate';
+UPDATE `zt_workflowfield` SET `control` = 'date' WHERE `module` = 'story' and `field` = 'reviewedDate';
+UPDATE `zt_workflowfield` SET `control` = 'date' WHERE `module` = 'story' and `field` = 'closedDate';
+UPDATE `zt_workflowfield` SET `control` = 'radio' WHERE `module` = 'story' and `field` = 'deleted';
