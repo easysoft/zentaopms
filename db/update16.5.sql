1f72825b20f9114fabff44c8b7ce82c72189602e
孙华伟 <sunhuawei@easycorp.ltd>
2022-05-11 09:36:27 +0000

    Update update16.5.sql

--- db/update16.5.sql
+++ db/update16.5.sql
@@ -140,7 +140,7 @@ ALTER TABLE `zt_workflowdatasource` ADD UNIQUE `code` (`code`);
 REPLACE INTO `zt_workflowdatasource` (`type`, `name`, `code`, `buildin`, `datasource`, `view`, `keyField`, `valueField`) VALUES
 ('lang', '项目模型', 'projectModel', '1', 'projectModel', '', '', ''),
 ('lang', '反馈类型', 'feedbackType', '1', 'feedbackType', '', '', ''),
-('lang', '反馈处理方案', 'feedbackSolution',     '1', 'feedbackSolution',     '', '', '');
+('lang', '反馈处理方案', 'feedbackSolution',     '1', 'feedbackSolution',     '', '', '')，
 ('lang', '反馈关闭原因', 'feedbackclosedReason', '1', 'feedbackclosedReason', '', '', '');
 
 REPLACE INTO `zt_workflowfield` (`module`, `field`, `type`, `length`, `name`, `control`, `expression`, `options`, `default`, `rules`, `placeholder`, `order`, `searchOrder`, `exportOrder`, `canExport`, `canSearch`, `isValue`, `readonly`, `buildin`, `desc`, `createdBy`, `createdDate`, `editedBy`, `editedDate`) VALUES
