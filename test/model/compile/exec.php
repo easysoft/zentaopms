c45f539b546c3bd21d0635298730e00f5a99e9f8
zhouxudong <zhouxudong@easycorp.ltd>
2022-05-11 06:30:56 +0000

    * Finish auto test from compile.

--- test/model/compile/exec.php
+++ test/model/compile/exec.php
@@ -11,7 +11,18 @@ cid=1
 pid=1
 
 */
+$compile1 = new stdclass();
+$compile1->id   = 2;
+$compile1->name = '这是一个compile数据';
+$compile1->job  = 3;
+
+$compile2 = new stdclass();
+$compile2->id   = 3;
+$compile2->name = '这是一个compile数据';
+$compile2->job  = 1;
 
 $compile = new compileTest();
 
-r($compile->execTest()) && p() && e();
+r($compile->execTest($compile2));
+r($compile->execTest($compile1)) && p() && e('0'); //检测是否能执行编译
+r($compile->execTest($compile2)) && p() && e('1'); //检测是否能执行编译
