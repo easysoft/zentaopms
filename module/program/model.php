3de0b23ab8f70545088275fc295d855dad293dbf
孙华伟 <sunhuawei@easycorp.ltd>
2022-05-12 13:26:19 +0800

    *Optimize the bug.

--- module/program/model.php
+++ module/program/model.php
@@ -1339,6 +1339,7 @@ class programModel extends model
 
         /* Init vars. */
         $projects = $this->getProjectList($programID, $browseType, $queryID, $orderBy, $pager, $programTitle, $involved, $queryAll);
+
         if(empty($projects)) return array();
 
         $projectKeys = array_keys($projects);
