731ce65bcff051eb9f22991b0d0f3a1cd17c4236
liumengyi <liumengyi@easycorp.ltd>
2022-05-13 10:51:22 +0800

    * Add unit test of block.

--- test/model/block/islongblock.php
+++ test/model/block/islongblock.php
@@ -10,10 +10,27 @@ title=测试 blockModel->isLongBlock();
 cid=1
 pid=1
 
-
+测试 空 对象 检查是否为长区块 >> 2
+测试 对象grid为 5 的时候 检查是否为长区块 >> 2
+测试 对象grid为 6 的时候 检查是否为长区块 >> 1
+测试 对象grid为 7 的时候 检查是否为长区块 >> 1
 
 */
 
+$block1 = new stdclass();
+
+$block2 = new stdclass();
+$block2->grid = 5;
+
+$block3 = new stdclass();
+$block3->grid = 6;
+
+$block4 = new stdclass();
+$block4->grid = 7;
+
 $block = new blockTest();
 
-r($block->isLongBlockTest()) && p() && e();
+r($block->isLongBlockTest($block1)) && p() && e('2'); // 测试 空 对象 检查是否为长区块
+r($block->isLongBlockTest($block2)) && p() && e('2'); // 测试 对象grid为 5 的时候 检查是否为长区块
+r($block->isLongBlockTest($block3)) && p() && e('1'); // 测试 对象grid为 6 的时候 检查是否为长区块
+r($block->isLongBlockTest($block4)) && p() && e('1'); // 测试 对象grid为 7 的时候 检查是否为长区块
