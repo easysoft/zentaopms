bf4ac6a4659610734a46871d8cba30242592d34c
mayue <mayue@easycorp.ltd>
2022-05-18 02:48:51 +0000

    * Fix bug #22574.

--- module/productplan/view/edit.html.php
+++ module/productplan/view/edit.html.php
@@ -39,12 +39,10 @@
             <th><?php echo $lang->productplan->title;?></th>
             <td><?php echo html::input('title', $plan->title, "class='form-control' required");?></td><td></td><td></td>
           </tr>
-          <?php if($plan->parent >= 0):?>
           <tr <?php echo $plan->parent == '-1' ? "class='hidden'" : ''?>>
             <th><?php echo $lang->productplan->parent;?></th>
-            <td><?php echo html::select('parent', array('0' => '') + $parentPlanPairs, $plan->parent, "class='form-control chosen'");?>
+            <td><?php echo html::select('parent', array('0' => '') + $parentPlanPairs, $plan->parent != '-1' ? $plan->parent : '', "class='form-control chosen'");?>
           </tr>
-          <?php endif;?>
           <tr>
             <th><?php echo $lang->productplan->status;?></th>
             <?php $disabled = $plan->parent == -1 ? "disabled='disabled'" : '' ;?>
