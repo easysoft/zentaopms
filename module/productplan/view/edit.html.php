117865e63d23b533a7ed0ccb6c8632c3909faf6e
mayue <mayue@easycorp.ltd>
2022-05-18 03:11:12 +0000

    * Optimize code.

--- module/productplan/view/edit.html.php
+++ module/productplan/view/edit.html.php
@@ -39,10 +39,12 @@
             <th><?php echo $lang->productplan->title;?></th>
             <td><?php echo html::input('title', $plan->title, "class='form-control' required");?></td><td></td><td></td>
           </tr>
-          <tr <?php echo $plan->parent == '-1' ? "class='hidden'" : ''?>>
+          <?php if($plan->parent != '-1'):?>
+          <tr>
             <th><?php echo $lang->productplan->parent;?></th>
-            <td><?php echo html::select('parent', array('0' => '') + $parentPlanPairs, $plan->parent != '-1' ? $plan->parent : '', "class='form-control chosen'");?>
+            <td><?php echo html::select('parent', array('0' => '') + $parentPlanPairs, $plan->parent, "class='form-control chosen'");?>
           </tr>
+          <?php endif;?>
           <tr>
             <th><?php echo $lang->productplan->status;?></th>
             <?php $disabled = $plan->parent == -1 ? "disabled='disabled'" : '' ;?>
