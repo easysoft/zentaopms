2d3f07a300632f52389bb7118cd8816e85d2fd88
liumengyi <liumengyi@easycorp.ltd>
2022-05-19 10:35:12 +0800

    * Code format fot task #54318.

--- module/task/model.php
+++ module/task/model.php
@@ -2187,8 +2187,8 @@ class taskModel extends model
         $fields = 'DISTINCT t1.*, t2.id AS storyID, t2.title AS storyTitle, t2.product, t2.branch, t2.version AS latestStoryVersion, t2.status AS storyStatus, t3.realname AS assignedToRealName';
         $this->config->edition == 'max' && $fields .= ', t6.name as designName, t6.version as latestDesignVersion';
 
-        $actionID = array();
-        if($type == 'assignedbyme') $actionID = $this->dao->select('objectID')->from(TABLE_ACTION)->where('objectType')->eq('task')->andWhere('action')->eq('assigned')->andWhere('actor')->eq($this->app->user->account)->fetchPairs('objectID', 'objectID');
+        $actionIDList = array();
+        if($type == 'assignedbyme') $actionIDList = $this->dao->select('objectID')->from(TABLE_ACTION)->where('objectType')->eq('task')->andWhere('action')->eq('assigned')->andWhere('actor')->eq($this->app->user->account)->fetchPairs('objectID', 'objectID');
 
         $tasks  = $this->dao->select($fields)
             ->from(TABLE_TASK)->alias('t1')
@@ -2213,7 +2213,7 @@ class taskModel extends model
             ->beginIF($type == 'delayed')->andWhere('t1.deadline')->gt('1970-1-1')->andWhere('t1.deadline')->lt(date(DT_DATE1))->andWhere('t1.status')->in('wait,doing')->fi()
             ->beginIF(is_array($type) or strpos(',all,undone,needconfirm,assignedtome,delayed,finishedbyme,myinvolved,assignedbyme,', ",$type,") === false)->andWhere('t1.status')->in($type)->fi()
             ->beginIF($modules)->andWhere('t1.module')->in($modules)->fi()
-            ->beginIF($type == 'assignedbyme')->andWhere('t1.id')->in($actionID)->andWhere('t1.status')->ne('closed')->fi()
+            ->beginIF($type == 'assignedbyme')->andWhere('t1.id')->in($actionIDList)->andWhere('t1.status')->ne('closed')->fi()
             ->andWhere('t1.deleted')->eq(0)
             ->orderBy($orderBy)
             ->page($pager, 't1.id')
