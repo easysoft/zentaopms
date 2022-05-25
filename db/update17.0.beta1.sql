ddb25449d2fd18318ca49384c0ef2e09f0fed9f2
wangyidong <wangyidong@cnezsoft.com>
2022-05-25 16:38:38 +0800

    * code for task #54955.

--- db/update17.0.beta1.sql
+++ db/update17.0.beta1.sql
@@ -5,3 +5,13 @@ ALTER TABLE `zt_kanbanspace` ADD COLUMN `activatedDate` datetime NOT NULL AFTER 
 
 ALTER TABLE `zt_kanban` ADD COLUMN `activatedBy` char(30) NOT NULL AFTER `closedDate`;
 ALTER TABLE `zt_kanban` ADD COLUMN `activatedDate` datetime NOT NULL AFTER `activatedBy`;
+
+DELETE FROM `zt_workflowaction` WHERE `module`='program' AND `action`='view';
+DELETE FROM `zt_workflowaction` WHERE `module`='story'   AND `action`='browse';
+DELETE FROM `zt_workflowaction` WHERE `module`='task'    AND `action`='browse';
+DELETE FROM `zt_workflowaction` WHERE `module`='build'   AND `action`='browse';
+
+UPDATE `zt_workflow` SET `app`='execution' WHERE `module`='task' AND `vision`='rnd';
+UPDATE `zt_workflow` SET `app`='execution' WHERE `module`='build' AND `vision`='rnd';
+UPDATE `zt_workflow` SET `app`='execution' WHERE `module`='execution' AND `vision`='rnd';
+UPDATE `zt_workflow` SET `app`='program'   WHERE `module`='program' AND `vision`='rnd';
