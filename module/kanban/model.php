52fc5e45ec6e7d80a668cdcf914a1c18c6248581
liumengyi <liumengyi@easycorp.ltd>
2022-05-25 13:40:38 +0800

    * Fix bug #22665.

--- module/kanban/model.php
+++ module/kanban/model.php
@@ -3703,7 +3703,6 @@ class kanbanModel extends model
                 $count = $this->dao->select('COUNT(id) AS count')->from(TABLE_KANBANLANE)
                     ->where('deleted')->eq('0')
                     ->andWhere('region')->eq($object->region)
-                    ->beginIF($action == 'deletelane')->andWhere('type')->eq($object->type)->fi()
                     ->beginIF($action == 'sortlane')->andWhere('`group`')->eq($object->group)->fi()
                     ->fetch('count');
                 return $count > 1;
