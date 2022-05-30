037a010a6f33da7539c540d9996335187e88ae2a
xieqiyu <xieqiyu@easycorp.ltd>
2022-05-31 00:11:29 +0800

    * Code for task#55626.

--- module/execution/view/team.html.php
+++ module/execution/view/team.html.php
@@ -15,7 +15,10 @@
 <?php js::set('noAccess', $lang->user->error->noAccess)?>
 <div id='mainMenu' class='clearfix'>
   <div class='btn-toolbar pull-left'>
-    <span class='btn btn-link btn-active-text'><span class='text'><?php echo $lang->execution->team;?><span class="label label-light label-badge"><?php echo count($teamMembers);?></span></span></span>
+    <span class='btn btn-link btn-active-text'>
+      <span class='text'><?php echo $lang->execution->team;?></span>
+      <span class="label label-light label-badge"><?php echo count($teamMembers);?></span>
+    </span>
   </div>
   <div class='btn-toolbar pull-right'>
     <?php
