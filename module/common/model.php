982d1c72aabb0e6c201a1d7f5347f14949337dc3
zenggang <zenggang@easycorp.ltd>
2022-06-02 05:32:33 +0000

    * Adjust code

--- module/common/model.php
+++ module/common/model.php
@@ -2400,12 +2400,15 @@ EOD;
             if($inProject && $app->session->project && strpos(",{$app->user->rights['projects']},", ",{$app->session->project},") !== false) return true;
         }
 
-        /* White list of method. */
-        if(in_array($module, array('user', 'task', 'story', 'bug', 'testcase')) and $method == 'showimport') return true;
-
         /* If not super admin, check the rights. */
         $rights = $app->user->rights['rights'];
         $acls   = $app->user->rights['acls'];
+
+        /* White list of method. */
+        if(in_array($module, array('user', 'task', 'story', 'bug', 'testcase')) and $method == 'showimport')
+        {
+            if(isset($rights[$module]['import']) and commonModel::hasDBPriv($object, $module, 'import')) return true;
+        }
 
         if(isset($rights[$module][$method]))
         {
