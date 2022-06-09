f1c720c4a0d598f833d249bbf4f0eab6b1be9f21
liyuchun <563917701@qq.com>
2022-06-09 06:36:52 +0000

    * Finish task #56556.

--- module/common/model.php
+++ module/common/model.php
@@ -2349,7 +2349,7 @@ EOD;
            $module == 'tutorial' ||
            $module == 'install' ||
            $module == 'upgrade' ||
-          ($module == 'user' && strpos('|login|deny|logout|', "|{$method}|") !== false) ||
+          ($module == 'user' && strpos('|login|deny|logout|reset|', "|{$method}|") !== false) ||
           ($module == 'my' && strpos('|changepassword|preference|', "|{$method}|") !== false) ||
           ($module == 'file' && strpos('|read|download|uploadimages|ajaxwopifiles|', "|{$method}|") !== false) ||
           ($module == 'sso' && $method == 'login'))
