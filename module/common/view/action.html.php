32642d45ae778a10dc4344cb3077f29f28fa6062
leiyong <1549684884@qq.com>
2022-06-15 10:00:58 +0000

    * Fix bug#23793.

--- module/common/view/action.html.php
+++ module/common/view/action.html.php
@@ -7,7 +7,9 @@
 <style>
 .histories-list > li {word-break: break-word; word-wrap: break-word;}
 .history-changes del {padding-left: 3px;}
-#actionbox > .detail-content {width:100%; max-height: 800px; overflow: scroll;}
+#actionbox {margin: unset;}
+#actionbox .detail-title {padding-left: 10px;}
+#actionbox > .detail-content {padding-left: 10px; width:100%; max-height: 800px; overflow: scroll;}
 </style>
   <script>
   $(function()
