64310b7dfb8f0797f8afe39ab5832a82e1efcde2
zhouxudong <zhouxudong@easycorp.ltd>
2022-06-20 08:17:44 +0000

    * Finish task #57871.

--- module/productplan/model.php
+++ module/productplan/model.php
@@ -211,12 +211,11 @@ class productplanModel extends model
      */
     public function getTopPlanPairs($productID, $branch = '', $exclude = '')
     {
-        $statusList = array('done','closed');
         $planPairs = $this->dao->select("id,title")->from(TABLE_PRODUCTPLAN)
             ->where('product')->eq($productID)
             ->beginIF($branch !== '')->andWhere('branch')->eq($branch)->fi()
             ->andWhere('parent')->le(0)
-            ->andWhere('status')->notin($statusList)
+            ->andWhere('status')->notin('done,closed')
             ->andWhere('deleted')->eq(0)
             ->orderBy('id_desc')
             ->fetchPairs();
