c896e5ac6fe99d26ecba388601513ed86c84551b
tanghucheng <tanghucheng@easycorp.ltd>
2022-06-30 11:22:53 +0800

    * Code for lite html hook.

--- extension/lite/workflowlayout/ext/view/admin.flow.html.hook.php
+++
@@ -1,1 +0,0 @@
-<?php include $this->app->getExtensionRoot() . 'biz/workflowlayout/ext/view/' . basename(__FILE__);?>
--- module/upgrade/config.php
+++ module/upgrade/config.php
@@ -338,6 +338,30 @@ $config->delete['17_0_beta1'][] = 'extension/max/sso/ext/model/bizext.php';
 
 $config->delete['17_2'][] = 'extension/biz/my/ext/view/todo.calendar.html.hook.php';
 $config->delete['17_2'][] = 'extension/max/my/ext/view/todo.calendar.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/attend/ext/view/stat.oa.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/flow/ext/view/browse.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/flow/ext/view/create.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflow/ext/view/browsedb.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflow/ext/view/browseflow.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflow/ext/view/edit.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflow/ext/view/flowchart.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflow/ext/view/release.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflow/ext/view/setcss.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflow/ext/view/setjs.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflow/ext/view/ui.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflowaction/ext/view/browse.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflowaction/ext/view/edit.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflowaction/ext/view/setnotice.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflowdatasource/ext/view/browse.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflowdatasource/ext/view/edit.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflowfield/ext/view/browse.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflowhook/ext/view/create.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflowhook/ext/view/edit.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflowlabel/ext/view/browse.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflowlayout/ext/view/admin.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/workflowrelation/ext/view/admin.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/extension/lite/workflowrule/ext/view/browse.flow.html.hook.php';
+$config->delete['17_2'][] = 'extension/lite/extension/lite/workflowrule/ext/view/view.flow.html.hook.php';
 
 $config->upgrade->openModules = array('action', 'admin', 'api', 'automation', 'backup', 'block', 'branch', 'budget', 'bug', 'build', 'caselib', 'ci', 'client', 'common', 'company', 'compile', 'convert', 'cron', 'custom', 'datatable', 'dept', 'design', 'dev', 'doc', 'durationestimation', 'entry', 'execution', 'extension', 'file', 'git', 'gitlab', 'group', 'holiday', 'im', 'index', 'index.html', 'install', 'issue', 'jenkins', 'job', 'kanban', 'license', 'mail', 'message', 'misc', 'mr', 'my', 'personnel', 'pipeline', 'product', 'productplan', 'productset', 'program', 'programplan', 'project', 'projectbuild', 'projectrelease', 'projectstory', 'qa', 'release', 'repo', 'report', 'risk', 'score', 'search', 'setting', 'sonarqube', 'sso', 'stage', 'stakeholder', 'story', 'subject', 'svn', 'task', 'testcase', 'testreport', 'testsuite', 'testtask', 'todo', 'tree', 'tutorial', 'upgrade', 'user', 'webhook', 'weekly', 'workestimation');
 
