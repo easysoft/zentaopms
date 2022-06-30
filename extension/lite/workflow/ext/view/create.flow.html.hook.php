20ef9ee98e874b43d6d66973a215df1d3f1b6c55
wangyuting2 <851424971@qq.com>
2022-06-30 14:38:45 +0800

    * Add openedApproval config.

--- extension/lite/workflow/ext/view/create.flow.html.hook.php
+++ extension/lite/workflow/ext/view/create.flow.html.hook.php
@@ -1,4 +1,3 @@
 <script>
-$('input[name=approval]').parents('tr').hide();
 $('#submit').after('<input type="hidden" name="vision" id="vision" value="<?php echo $this->config->vision;?>">');
 </script>
