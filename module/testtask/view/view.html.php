16e0011b79f0243ea0c919ea77e3f45456976050
liumengyi <liumengyi@easycorp.ltd>
2022-07-01 09:43:22 +0800

    * Fix bug #24651.

--- module/testtask/view/view.html.php
+++ module/testtask/view/view.html.php
@@ -97,7 +97,7 @@
             </tr>
             <tr>
               <th><?php echo $lang->testtask->pri;?></th>
-              <td><?php echo $lang->testtask->priList[$task->pri];?></td>
+              <td><?php echo zget($lang->testtask->priList, $task->pri);?></td>
             </tr>
             <tr>
               <th><?php echo $lang->testtask->begin;?></th>
