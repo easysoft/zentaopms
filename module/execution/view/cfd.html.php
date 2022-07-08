b8c23acb6970db96b2f8dfe010c452d21713b655
zenggang <zenggang@easycorp.ltd>
2022-07-08 01:42:17 +0000

    * Adjust code

--- module/execution/view/cfd.html.php
+++ module/execution/view/cfd.html.php
@@ -38,9 +38,9 @@
     <div id="cfdChart" style="width: 1200px; height: 600px"></div>
     <div id="burnStatistics" class="hidden">
       <div class="stat-title"><span class="bg-primary">&nbsp;</span> <?php echo $lang->execution->charts->cfd->cycleTime;?> <i class="icon icon-help" data-toggle="tooltip" data-tip-class="tooltip-help" data-placement="bottom" title="<?php echo $lang->execution->charts->cfd->cycleTimeTip;?>"></i></div>
-      <h3><?php echo $cycleTimeAvg ? ($cycleTimeAvg . $lang->day) : $lang->noData;?></h3>
+      <h3><?php echo !empty($cycleTimeAvg) ? ($cycleTimeAvg . $lang->day) : $lang->noData;?></h3>
       <div class="stat-title"><span class="bg-primary">&nbsp;</span> <?php echo $lang->execution->charts->cfd->throughput;?> <i class="icon icon-help" data-toggle="tooltip" data-tip-class="tooltip-help" data-placement="bottom" title="<?php echo $lang->execution->charts->cfd->throughputTip;?>"></i></div>
-      <h3><?php echo $throughput ? $throughput : $lang->noData;?></h3>
+      <h3><?php echo !empty($throughput) ? $throughput : $lang->noData;?></h3>
     </div>
   </div>
   <?php else:?>
