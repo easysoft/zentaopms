d09ea56ed1d642a98643460df1b0ed2e40e86a5d
宋辰轩 <songchenxuan@easycorp.ltd>
2022-07-08 10:56:31 +0800

    * Delete.

--- module/story:q/js/x.create.js
+++
@@ -1,8 +0,0 @@
-$(function()
-{
-    $('#sourceNote').prev('span').remove();
-    $('#sourceNote').remove();
-    $('table tbody tr th').addClass('w-100px');
-
-    $('#tplBoxWrapper .btn-toolbar .btn-group').remove();
-})
