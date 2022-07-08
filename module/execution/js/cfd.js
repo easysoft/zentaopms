1df55e32a781efea91c0c082544c58f4bb5cf1da
mayue <mayue@easycorp.ltd>
2022-07-08 09:18:53 +0800

    * Fix bug #24848#24817.

--- module/execution/js/cfd.js
+++ module/execution/js/cfd.js
@@ -7,107 +7,110 @@ $(function()
     var colors     = ['#33B4DB', '#7ECF69', '#FFC73A', '#FF5A61', '#50C8D0', '#AF5AFF', '#4EA3FF', '#FF8C5A', '#6C73FF'];
     //var background = ['#E1F4FA', '#ECF8E9', '#FFF7E2', '#FFE7E8', '#E5F7F8', '#F3E7FF', '#E5F1FF', '#FFEEE7', '#E9EAFF'];
 
-    $.each(chartData['line'], function(label, set)
+    if(Object.keys(chartData).length)
     {
-        series.push({
-          name: label,
-          type: 'line',
-          stack: 'Total',
-          color: colors[i],
-          areaStyle: {
+        $.each(chartData['line'], function(label, set)
+        {
+          series.push({
+            name: label,
+            type: 'line',
+            stack: 'Total',
             color: colors[i],
-            opacity: 0.2
-          },
-          itemStyle: {
-            normal: {
-              lineStyle:{
-                width:1//设置线条粗细
+            areaStyle: {
+              color: colors[i],
+              opacity: 0.2
+            },
+            itemStyle: {
+              normal: {
+                lineStyle:{
+                  width:1//设置线条粗细
+                }
               }
+            },
+            emphasis: {
+              focus: 'series'
+            },
+            data: eval(set)
+          })
+          i ++;
+        })
+
+        var chartDom = document.getElementById('cfdChart');
+        var CFD      = echarts.init(chartDom);
+        var option;
+
+        option = {
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross',
+              label: {
+                backgroundColor: '#6a7985'
+              }
+            },
+            textStyle: {
+              fontWeight: 100
             }
           },
-          emphasis: {
-            focus: 'series'
+          legend: {
+            data: Object.keys(chartData['line']).reverse()
           },
-          data: eval(set)
-        })
-        i ++;
-    })
-
-    var chartDom = document.getElementById('cfdChart');
-    var CFD      = echarts.init(chartDom);
-    var option;
-
-    option = {
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'cross',
-          label: {
-            backgroundColor: '#6a7985'
-          }
-        },
-        textStyle: {
-          fontWeight: 100
-        }
-      },
-      legend: {
-        data: Object.keys(chartData['line']).reverse()
-      },
-      grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-      },
-      xAxis: [
-      {
-          type: 'category',
-          boundaryGap: false,
-          data: chartData['labels'],
-          name: XUnit,
-          axisLine:
-          {
-              show: true,
-              lineStyle:
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          xAxis: [
+            {
+              type: 'category',
+              boundaryGap: false,
+              data: chartData['labels'],
+              name: XUnit,
+              axisLine:
               {
+                show: true,
+                lineStyle:
+                {
                   color: '#999',
                   width:1
+                }
               }
-          }
-      }],
-      yAxis: [
-      {
-          type: 'value',
-          minInterval: 1,
-          name: YUnit,
-          nameTextStyle:
-          {
-              fontWeight: 'normal'
-          },
-          axisPointer:
-          {
-              label:
+            }],
+          yAxis: [
+            {
+              type: 'value',
+              minInterval: 1,
+              name: YUnit,
+              nameTextStyle:
               {
+                fontWeight: 'normal'
+              },
+              axisPointer:
+              {
+                label:
+                {
                   show: true,
                   precision: 0
+                },
               },
-          },
-          axisLine:
-          {
-              show: true,
-              lineStyle:
+              axisLine:
               {
+                show: true,
+                lineStyle:
+                {
                   color: ['#999'],
                   width: 1
+                }
               }
-          }
-        }
-      ],
-      series: series,
-    };
+            }
+          ],
+          series: series,
+        };
 
-    option && CFD.setOption(option);
-    window.addEventListener('resize', CFD.resize);
+        option && CFD.setOption(option);
+        window.addEventListener('resize', CFD.resize);
+    }
 
     $('#type').change(function()
     {
