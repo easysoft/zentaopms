dafb0452d37757e754d8c0e4b8f13083091ccc58
guofeilong <guofeilong@easycorp.ltd>
2022-07-14 05:50:28 +0000

    * Fix bug #23351

--- module/task/view/batchcreate.html.php
+++ module/task/view/batchcreate.html.php
@@ -340,7 +340,6 @@ $colspan = count($visibleFields) + 3;
 $(function()
 {
     parent.$('#triggerModal .modal-content .modal-header .close').hide();
-    // $('#closeModal').on('click', function(){window.parent.$.closeModal();});
 });
 </script>
 <?php endif;?>
