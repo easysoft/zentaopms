773400bb27e3d187820f57bd0446a14271a927bc
zenggang <zenggang@easycorp.ltd>
2022-07-19 01:47:18 +0000

    * Adjuct code

--- module/group/js/managemember.js
+++ module/group/js/managemember.js
@@ -8,27 +8,27 @@ $(function()
 
     $('#other,#group,#outside').selectable(
     {
-      selector: 'input',
-      listenClick: false,
-      select: function(e)
-      {
-        $('[data-id=' + e.id + ']').prop('checked', true);
+        selector: 'input',
+        listenClick: false,
+        select: function(e)
+        {
+            $('[data-id=' + e.id + ']').prop('checked', true);
 
-        var checked          = true;
-        var memberNum        = $('[data-id=' + e.id + ']').closest('td').find('input').length;
-        var checkedMemberNum = $('[data-id=' + e.id + ']').closest('td').find('input:checked').length;
-        if(memberNum > checkedMemberNum) checked = false;
-        $('[data-id=' + e.id + ']').closest('tr').find('.check-all input').prop('checked', checked);
-      },
-      unselect: function(e)
-      {
-        $('[data-id=' + e.id + ']').prop('checked', false);
+            var checked          = true;
+            var memberNum        = $('[data-id=' + e.id + ']').closest('td').find('input').length;
+            var checkedMemberNum = $('[data-id=' + e.id + ']').closest('td').find('input:checked').length;
+            if(memberNum > checkedMemberNum) checked = false;
+            $('[data-id=' + e.id + ']').closest('tr').find('.check-all input').prop('checked', checked);
+        },
+        unselect: function(e)
+        {
+            $('[data-id=' + e.id + ']').prop('checked', false);
 
-        var checked          = true;
-        var memberNum        = $('[data-id=' + e.id + ']').closest('td').find('input').length;
-        var checkedMemberNum = $('[data-id=' + e.id + ']').closest('td').find('input:checked').length;
-        if(memberNum > checkedMemberNum) checked = false;
-        $('[data-id=' + e.id + ']').closest('tr').find('.check-all input').prop('checked', checked);
-      }
+            var checked          = true;
+            var memberNum        = $('[data-id=' + e.id + ']').closest('td').find('input').length;
+            var checkedMemberNum = $('[data-id=' + e.id + ']').closest('td').find('input:checked').length;
+            if(memberNum > checkedMemberNum) checked = false;
+            $('[data-id=' + e.id + ']').closest('tr').find('.check-all input').prop('checked', checked);
+        }
     });
 });
