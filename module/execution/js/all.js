bdab39e7f7562f6f0de346eb6f960f13dd88d77f
tianshujie <tianshujie0123@163.com>
2022-07-20 16:54:43 +0800

    * Fixed an issue where advanced tables cannot be selected entirely.

--- module/execution/js/all.js
+++ module/execution/js/all.js
@@ -59,6 +59,26 @@ $(function()
             $('.table td.has-child > .plan-toggle').removeClass('collapsed');
         }
     });
+
+    $('#executionsForm').table(
+    {
+        statisticCreator: function(table)
+        {
+            var $table = table.getTable();
+            var $checkedRows = $table.find(table.isDataTable ? '.datatable-row-left.checked' : 'tbody>tr.checked');
+            var $originTable = table.isDataTable ? table.$.find('.datatable-origin') : null;
+            var $rows = $table.find(table.isDataTable ? '.datatable-rows .datatable-row-left' : 'tbody>tr');
+
+            var taskIdList      = [];
+            $rows.each(function()
+            {
+                var $row = $(this);
+                if($originTable) $row = $originTable.find('tbody>tr[data-id="' + $row.data('id') + '"]');
+                var data = $row.data();
+                taskIdList.push(data.id);
+            });
+        }
+    })
 });
 
 function byProduct(productID, projectID, status)
