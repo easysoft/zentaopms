dba57b45cd8fcfe0bf3fa561c7b6701333f0d9d5
zenggang <zenggang@easycorp.ltd>
2022-07-22 01:04:24 +0000

    * Adjust code

--- module/program/control.php
+++ module/program/control.php
@@ -26,7 +26,7 @@ class program extends control
      * @access public
      * @return void
      */
-    public function browse($status = 'doing', $orderBy = 'order_asc', $recTotal = 0, $recPerPage = 10, $pageID = 1, $param = 0)
+    public function browse($status = 'all', $orderBy = 'order_asc', $recTotal = 0, $recPerPage = 10, $pageID = 1, $param = 0)
     {
         if(common::hasPriv('program', 'create')) $this->lang->pageActions = html::a($this->createLink('program', 'create'), "<i class='icon icon-plus'></i> " . $this->lang->program->create, '', "class='btn btn-primary create-program-btn'");
 
