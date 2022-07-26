4bf59548a602f7b7bcadb939d7fa9150e5635ad5
zhouxudong <zhouxudong@easycorp.ltd>
2022-07-26 02:28:43 +0000

    * Finish task #61908.

--- module/group/control.php
+++ module/group/control.php
@@ -191,7 +191,7 @@ class group extends control
             if($type == 'byModule') $result = $this->group->updatePrivByModule();
             if(dao::isError()) return $this->send(array('result' => 'fail', 'message' => dao::getError()));
 
-            if($type == 'byGroup') return $this->send(array('result' => 'success', 'message' => $this->lang->saveSuccess, 'locate' => inlink('managePriv', "type=byGroup&param=$param&menu=$menu")));
+            if($type == 'byGroup') return $this->send(array('result' => 'success', 'message' => $this->lang->saveSuccess, 'locate' => inlink('managePriv', "type=byGroup&param=$param&menu=$menu&version=$version")));
             if($type == 'byModule') return print(js::alert($this->lang->saveSuccess) . js::execute('window.parent.location.reload()'));
         }
 
