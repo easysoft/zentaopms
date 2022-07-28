1375165c73e72fcc135daa1f0ee1a8cc6087a042
liumengyi <liumengyi@easycorp.ltd>
2022-07-28 10:02:43 +0800

    * Optimize code for task #62494.

--- module/execution/view/taskkanban.html.php
+++ module/execution/view/taskkanban.html.php
@@ -71,7 +71,7 @@
     </div>
 
     <?php
-    $width = strpos($this->app->getClientLang(), 'zh') === false ? '600px' : '500px';
+    $width = common::checkNotCN ? '600px' : '470px';
     echo "<div class='btn-group menu-actions'>";
     echo html::a('javascript:;', "<i class='icon icon-ellipsis-v'></i>", '', "data-toggle='dropdown' class='btn btn-link'");
     echo "<ul class='dropdown-menu pull-right'>";
