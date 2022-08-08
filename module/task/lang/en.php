327336b7bf4e27d774c79d2666e6860a0d1ed537
liuyongkai <liuyongkai@easycorp.ltd>
2022-08-08 05:59:13 +0000

    * Fix bug 25690.

--- module/task/lang/en.php
+++ module/task/lang/en.php
@@ -161,7 +161,7 @@ $lang->task->recordEstimateAction = 'Record Estimate';
 
 $lang->task->ditto             = 'Ditto';
 $lang->task->dittoNotice       = "This Task is not linked to %s like the last one!";
-$lang->task->selectTestStory   = 'Select Story Testd';
+$lang->task->selectTestStory   = 'Select Story Tested';
 $lang->task->selectAllUser     = 'All Users';
 $lang->task->noStory           = 'No Story Linked';
 $lang->task->noAssigned        = 'Unassigned';
