45ba659ef9271e03e42c657d38f025c667a1cc34
liumengyi <liumengyi@easycorp.ltd>
2022-08-09 15:16:47 +0800

    * Add language item for task #63694.

--- module/user/lang/en.php
+++ module/user/lang/en.php
@@ -224,8 +224,13 @@ $lang->user->placeholder->loginAccount  = 'Enter your account';
 $lang->user->placeholder->loginUrl      = 'Enter your ZenTao address';
 $lang->user->placeholder->email         = 'Enter your email';
 
+$lang->user->placeholder->passwordStrength[0] = '≥6 letters.';
 $lang->user->placeholder->passwordStrength[1] = '>= 6 letters and numbers';
 $lang->user->placeholder->passwordStrength[2] = '>= 10 letters, numbers and special characters';
+
+$lang->user->placeholder->passwordStrengthCheck[0] = 'The password should be ≥ 6 letters.';
+$lang->user->placeholder->passwordStrengthCheck[1] = 'The password should be ≥ 6 letters, combination of uppercase, lowercase letters and numbers.';
+$lang->user->placeholder->passwordStrengthCheck[2] = 'The password should be ≥ 10 letters, combination of uppercase, lowercase letters, numbers, and special symbols.';
 
 $lang->user->error = new stdclass();
 $lang->user->error->account        = "ID %s，account must be >= 3 letters, underline or numbers";
