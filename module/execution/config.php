aa031fd1301b309850375721dbf97dcfc783676f
tianshujie <tianshujie0123@163.com>
2022-08-10 09:11:49 +0800

    * In classic mode, search execution does not show the owning project.

--- module/execution/config.php
+++ module/execution/config.php
@@ -125,7 +125,7 @@ $config->execution->all->search['module'] = 'execution';
 $config->execution->all->search['fields']['name']           = $lang->execution->execName;
 $config->execution->all->search['fields']['id']             = $lang->execution->execId;
 $config->execution->all->search['fields']['status']         = $lang->execution->execStatus;
-$config->execution->all->search['fields']['project']        = $lang->execution->project;
+if($config->systemMode == 'new') $config->execution->all->search['fields']['project'] = $lang->execution->project;
 $config->execution->all->search['fields']['PM']             = $lang->execution->owner;
 $config->execution->all->search['fields']['openedBy']       = $lang->execution->openedBy;
 $config->execution->all->search['fields']['openedDate']     = $lang->execution->openedDate;
@@ -140,7 +140,7 @@ $config->execution->all->search['fields']['closedDate']     = $lang->execution->
 $config->execution->all->search['params']['name']           = array('operator' => 'include', 'control' => 'input',  'values' => '');
 $config->execution->all->search['params']['id']             = array('operator' => '=',       'control' => 'input',  'values' => '');
 $config->execution->all->search['params']['status']         = array('operator' => '=',       'control' => 'select', 'values' => array('') + $lang->execution->statusList);
-$config->execution->all->search['params']['project']        = array('operator' => '=',       'control' => 'select', 'values' => '');
+if($config->systemMode == 'new') $config->execution->all->search['params']['project'] = array('operator' => '=',       'control' => 'select', 'values' => '');
 $config->execution->all->search['params']['PM']             = array('operator' => '=',       'control' => 'select', 'values' => 'users');
 $config->execution->all->search['params']['openedBy']       = array('operator' => '=',       'control' => 'select', 'values' => 'users');
 $config->execution->all->search['params']['openedDate']     = array('operator' => '=',       'control' => 'input',  'values' => '', 'class' => 'date');
