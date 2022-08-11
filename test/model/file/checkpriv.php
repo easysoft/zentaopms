a978f21f69ede6ba0ac69909589658c98d4f34c5
sunguangming <sunguangming@easycorp.ltd>
2022-08-11 13:31:51 +0800

    * Fix file unit case.

--- test/model/file/checkpriv.php
+++ test/model/file/checkpriv.php
@@ -2,7 +2,6 @@
 <?php
 include dirname(dirname(dirname(__FILE__))) . '/lib/init.php';
 include dirname(dirname(dirname(__FILE__))) . '/class/file.class.php';
-su('user10');
 
 /**
 
@@ -20,6 +19,12 @@ $file1  = $tester->file->getByID(1);
 $file10 = $tester->file->getByID(10);
 $file45 = $tester->file->getByID(45);
 
-r($tester->file->checkPriv($file1))  && p() && e(1); // 查看user10是否有附件1的权限
-r($tester->file->checkPriv($file10)) && p() && e(1); // 查看user10是否有附件10的权限
-r($tester->file->checkPriv($file45)) && p() && e(0); // 查看user10是否有附件45的权限
+su('user10');
+r($tester->file->checkPriv($file1))  && p() && e(1); // 查看用户user10是否有下载附件1的权限
+r($tester->file->checkPriv($file10)) && p() && e(1); // 查看用户user10是否有下载附件10的权限
+r($tester->file->checkPriv($file45)) && p() && e(0); // 查看用户user10是否有下载附件45的权限
+
+su('dev38');
+r($tester->file->checkPriv($file1))  && p() && e(1); // 查看用户dev38是否有下载附件1的权限
+r($tester->file->checkPriv($file10)) && p() && e(1); // 查看用户dev38是否有下载附件10的权限
+r($tester->file->checkPriv($file45)) && p() && e(0); // 查看用户dev38是否有下载附件45的权限
