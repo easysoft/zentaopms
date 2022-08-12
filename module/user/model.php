698510fadfa88681f9d3d56e0b60458721263ef7
liumengyi <liumengyi@easycorp.ltd>
2022-08-12 13:13:38 +0800

    * Fix bug #26325.

--- module/user/model.php
+++ module/user/model.php
@@ -796,9 +796,9 @@ class userModel extends model
         }
 
         $this->dao->update(TABLE_USER)->data($user)->autoCheck()->where('id')->eq((int)$userID)->exec();
+        $_SESSION['user']->password      = $user->password;
         $this->app->user->password       = $user->password;
         $this->app->user->modifyPassword = false;
-        $_SESSION['user']->password = $user->password;
         if(!dao::isError())
         {
             if(!empty($this->app->user->modifyPasswordReason)) $this->app->user->modifyPasswordReason = '';
