9bf8e1925a1e3c653f7f6f7c3533576291c7ce88
caoyanyi <caoyanyi@easycorp.ltd>
2022-08-12 14:23:19 +0800

    * Modify gogos check.

--- module/mr/model.php
+++ module/mr/model.php
@@ -1059,9 +1059,9 @@ class mrModel extends model
             $url = sprintf($apiRoot, "/projects/$projectID/merge_requests/$MRID/merge");
             return json_decode(commonModel::http($url, null, array(CURLOPT_CUSTOMREQUEST => 'PUT')));
         }
-        elseif($host->type == 'gitea')
+        else
         {
-            $apiRoot = $this->loadModel('gitea')->getApiRoot($hostID);
+            $apiRoot = $this->loadModel($host->type)->getApiRoot($hostID);
             $url     = sprintf($apiRoot, "/repos/$projectID/pulls/$MRID/merge");
 
             $merge = ($MR and $MR->squash == '1') ? 'squash' : 'merge';
