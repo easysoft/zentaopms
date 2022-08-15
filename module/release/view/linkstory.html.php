a63c2252930393b7aa19e26302d5728c788ca3c6
liuhong <liuhong@easycorp.ltd>
2022-08-15 02:47:34 +0000

    *Fix bug #26210.

--- module/release/view/linkstory.html.php
+++ module/release/view/linkstory.html.php
@@ -57,7 +57,7 @@
           <td><?php echo zget($users, $story->openedBy);?></td>
           <td><?php echo zget($users, $story->assignedTo);?></td>
           <td class='text-right' title="<?php echo $story->estimate . ' ' . $lang->hourCommon;?>"><?php echo $story->estimate . $config->hourUnit;?></td>
-          <td class='text-center'><span class='status-story status-<?php echo $story->status?>'><?php echo $this->processStatus('story', $story);?></span></td>
+          <td class='text-center' style='padding-right:10px;'><span class='status-story status-<?php echo $story->status?>'><?php echo $this->processStatus('story', $story);?></span></td>
           <td><?php echo zget($lang->story->stageList, $story->stage);?></td>
         </tr>
         <?php $unlinkedCount++;?>
