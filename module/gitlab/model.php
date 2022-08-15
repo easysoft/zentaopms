6afa13390f1884e2a0fded3961e7472e44352d24
caoyanyi <caoyanyi@easycorp.ltd>
2022-08-15 09:14:50 +0800

    * Add tips when  gitlab has not pipelines.

--- module/gitlab/model.php
+++ module/gitlab/model.php
@@ -2879,4 +2879,21 @@ class gitlabModel extends model
         $html .= '</div>';
         return $html;
     }
+
+    /**
+     * Get pipeline with api.
+     *
+     * @param  int    $gitlabID
+     * @param  int    $projectID
+     * @param  string $branch
+     * @access public
+     * @return void
+     */
+    public function apiGetPipeline($gitlabID, $projectID, $branch)
+    {
+        $apiRoot = $this->getApiRoot($gitlabID);
+        $url     = sprintf($apiRoot, "/projects/$projectID/pipelines") . "&ref=$branch";
+        a($url);
+        return json_decode(commonModel::http($url));
+    }
 }
