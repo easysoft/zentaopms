68f35f5262db0f522b896ea50571d8bdb6ae83d3
tanghucheng <tanghucheng@easycorp.ltd>
2022-08-16 13:58:19 +0800

    * Fix bug #26520,26520.

--- module/port/view/header.html.php
+++ module/port/view/header.html.php
@@ -24,6 +24,7 @@ thead > tr > th{width:150px;}
 .c-id{width:50px}
 .c-team {width:100px; padding:0px 0px 8px 0px !important;}
 .c-estimate-1 {width:50px;padding:0px 0px 8px 8px !important;}
+.load-indicator{width: 100px; height: 500px;}
 </style>
 <?php if(!empty($suhosinInfo)):?>
 <div class='alert alert-info'><?php echo $suhosinInfo?></div>
