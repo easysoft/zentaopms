33fa12239e9d0734e8cab7bf462572522ab49cc3
liumengyi <liumengyi@easycorp.ltd>
2022-08-17 10:56:09 +0800

    * Fix bug #25319.

--- module/admin/js/safe.js
+++ module/admin/js/safe.js
@@ -2,3 +2,16 @@ $(function()
 {
     $("#mainHeader #navbar li[data-id='system']").addClass('active');
 })
+
+/**
+ * When changeWeask change.
+ *
+ * @param  bool   changeWeak
+ * @access public
+ * @return void
+ */
+function changeWeakChange(changeWeak)
+{
+    if(changeWeak == 1) $('#mainContent .table td.notice').html(adminLang.safe.noticeWeakMode);
+    if(changeWeak == 0) $('#mainContent .table td.notice').html(adminLang.safe.noticeMode);
+}
