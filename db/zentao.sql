88ae33eaef171906d89faa025ee0f9a2444000d3
sunguangming <sunguangming@easycorp.ltd>
2022-08-18 15:21:40 +0800

    * Code for import task bug and add sql.

--- db/zentao.sql
+++ db/zentao.sql
@@ -1605,6 +1605,20 @@ CREATE TABLE IF NOT EXISTS `zt_taskspec` (
   `deadline` date NOT NULL,
   UNIQUE KEY `task` (`task`,`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- DROP TABLE IF EXISTS `zt_taskteam`;
+CREATE TABLE `zt_taskteam` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `task` mediumint(8) unsigned NOT NULL,
+  `account` char(30) NOT NULL,
+  `estimate` decimal(12,2) NOT NULL,
+  `consumed` decimal(12,2) NOT NULL,
+  `left` decimal(12,2) NOT NULL,
+  `transfer` char(30) NOT NULL,
+  `status` enum('wait','doing','done') NOT NULL DEFAULT 'wait',
+  `order` tinyint(3) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `task` (`task`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 -- DROP TABLE IF EXISTS `zt_team`;
 CREATE TABLE IF NOT EXISTS `zt_team` (
   `id` mediumint(8) unsigned NOT NULL auto_increment,
