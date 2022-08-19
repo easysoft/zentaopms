634342230fef0871bdc0c7c882b12554a39e709a
caoyanyi <caoyanyi@easycorp.ltd>
2022-08-19 15:22:12 +0800

    * Modify control params.

--- api/v1/entries/projectbugs.php
+++ api/v1/entries/projectbugs.php
@@ -24,7 +24,7 @@ class projectBugsEntry extends entry
         if(empty($projectID)) return $this->sendError(400, 'Need project id.');
 
         $control = $this->loadController('project', 'bug');
-        $control->bug($projectID, $this->param('product', 0), $this->param('order', 'status,id_desc'), $this->param('build', 0), $this->param('status', 'all'), 0, 0, $this->param('limit', 20), $this->param('page', 1));
+        $control->bug($projectID, $this->param('product', 0), $this->param('branch', 0), $this->param('order', 'status,id_desc'), $this->param('build', 0), $this->param('status', 'all'), 0, 0, $this->param('limit', 20), $this->param('page', 1));
 
         $data = $this->getData();
 
