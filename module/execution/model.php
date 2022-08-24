11c273036102bac3e9e44e63b8c8432f98cfc607
liuhong <liuhong@easycorp.ltd>
2022-08-24 03:28:26 +0000

    *Fix bug #26725.

--- module/execution/model.php
+++ module/execution/model.php
@@ -1561,11 +1561,11 @@ class executionModel extends model
      * @param  string  $status
      * @param  int     $limit
      * @param  string  $pairs
-     * @param  string  $projectsToAppended
+     * @param  int     $appenedID
      * @access public
      * @return array
      */
-    public function getByProject($projectID, $status = 'all', $limit = 0, $pairs = false, $devel = false, $projectsToAppended = '')
+    public function getByProject($projectID, $status = 'all', $limit = 0, $pairs = false, $devel = false, $appendedID = 0)
     {
         if(defined('TUTORIAL')) return $this->loadModel('tutorial')->getExecutionPairs();
 
@@ -1582,15 +1582,10 @@ class executionModel extends model
             ->beginIF($status != 'all' and $status != 'undone' and $status != 'noclosed')->andWhere('status')->in($status)->fi()
             ->beginIF($status == 'noclosed')->andWhere('status')->ne('closed')->fi()
             ->beginIF($devel === true)->andWhere('attribute')->in('dev,qa,release')->fi()
+            ->beginIF($appendedID)->orWhere('id')->eq($appendedID)->fi()
             ->orderBy($orderBy)
             ->beginIF($limit)->limit($limit)->fi()
             ->fetchAll('id');
-
-        if($projectsToAppended)
-        {
-            $path = sprintf("%s,%s", $projectID, $projectsToAppended);
-            $executions += $this->dao->select('*')->from(TABLE_EXECUTION)->where('path')->like("%,$path,%")->fetchAll('id');
-        }
 
         if(isset($project->model) and $project->model == 'waterfall')
         {
