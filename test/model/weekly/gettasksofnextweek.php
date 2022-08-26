0e77885d3cbb1c35b8d182beb596a8a981facea7
lanzongjun <lanzongjun@easycorp.ltd>
2022-08-26 15:28:19 +0800

    success my and weekly

--- test/model/weekly/gettasksofnextweek.php
+++ test/model/weekly/gettasksofnextweek.php
@@ -10,23 +10,22 @@ title=测试 weeklyModel->getTasksOfNextWeek();
 cid=1
 pid=1
 
-测试project值为0，date值为2022-05-01 >> 子任务10
-测试project值为0，date值为空 >> 开发任务606
-测试project值为1，date值为2022-05-01 >> 0
+测试project值为0，date值为2022-07-30 >> 子任务6
+测试project值为0，date值为空 >> 0
+测试project值为1，date值为2022-07-30 >> 0
 测试project值为1，date值为空 >> 0
-测试project值为11，date值为2022-05-01 >> 开发任务551
-测试project值为11，date值为空 >> 开发任务11
+测试project值为11，date值为2022-07-30 >> 开发任务371
+测试project值为11，date值为空 >> 0
 
 */
 
 $projectList = array(0, 1, 11);
-$dateList    = array('2022-05-01', '');
+$dateList    = array('2022-07-30', '');
 
 $weekly = new weeklyTest();
-
-r($weekly->getTasksOfNextWeekTest($projectList[0], $dateList[0])) && p('910:name') && e('子任务10');    //测试project值为0，date值为2022-05-01
-r($weekly->getTasksOfNextWeekTest($projectList[0], $dateList[1])) && p('596:name') && e('开发任务606'); //测试project值为0，date值为空
-r($weekly->getTasksOfNextWeekTest($projectList[1], $dateList[0])) && p()           && e('0');           //测试project值为1，date值为2022-05-01
-r($weekly->getTasksOfNextWeekTest($projectList[1], $dateList[1])) && p()           && e('0');           //测试project值为1，date值为空
-r($weekly->getTasksOfNextWeekTest($projectList[2], $dateList[0])) && p('541:name') && e('开发任务551'); //测试project值为11，date值为2022-05-01
-r($weekly->getTasksOfNextWeekTest($projectList[2], $dateList[1])) && p('1:name')   && e('开发任务11');  //测试project值为11，date值为空
+r($weekly->getTasksOfNextWeekTest($projectList[0], $dateList[0])) && p('906:name') && e('子任务6');     //测试project值为0，date值为2022-07-30
+r($weekly->getTasksOfNextWeekTest($projectList[0], $dateList[1])) && p()           && e('0');          //测试project值为0，date值为空
+r($weekly->getTasksOfNextWeekTest($projectList[1], $dateList[0])) && p()           && e('0');          //测试project值为1，date值为2022-07-30
+r($weekly->getTasksOfNextWeekTest($projectList[1], $dateList[1])) && p()           && e('0');          //测试project值为1，date值为空
+r($weekly->getTasksOfNextWeekTest($projectList[2], $dateList[0])) && p('361:name') && e('开发任务371'); //测试project值为11，date值为2022-07-30
+r($weekly->getTasksOfNextWeekTest($projectList[2], $dateList[1])) && p()           && e('0');          //测试project值为11，date值为空
