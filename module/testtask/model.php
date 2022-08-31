c5f3a49071ff5ca58897af2ab7985a12c40d601e
zhaoke <zhaoke@easycorp.ltd>
2022-08-31 09:52:31 +0800

    * Add insert case result for unit ciresult.

--- module/testtask/model.php
+++ module/testtask/model.php
@@ -2112,6 +2112,7 @@ class testtaskModel extends model
             if(!isset($suites[$suiteIndex])) $suites[$suiteIndex] = $suite;
 
             $case = new stdclass();
+            if(isset($caseResult->id)) $case->id = $caseResult->id;
             $case->product    = $productID;
             $case->title      = $caseResult->title;
             $case->pri        = 3;
@@ -2121,7 +2122,7 @@ class testtaskModel extends model
             $case->openedBy   = $this->app->user->account;
             $case->openedDate = $now;
             $case->version    = 1;
-            $case->auto       = 'unit';
+            $case->auto       = 'func';
             $case->frame      = $frame;
 
             $result = new stdclass();
