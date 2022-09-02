54c54108e64d2b2a85536b9319a7a75efd58c69b
sunguangming <sunguangming@easycorp.ltd>
2022-09-02 14:17:39 +0800

    * Code for task effort.

--- module/project/model.php
+++ module/project/model.php
@@ -505,9 +505,8 @@ class projectModel extends model
         $projects = array();
 
         $totalConsumeds = $this->dao->select('t2.project,ROUND(SUM(t1.consumed), 1) AS totalConsumed')->from(TABLE_EFFORT)->alias('t1')
-            ->leftJoin(TABLE_TASK)->alias('t2')->on('t1.objectID=t2.id')
+            ->leftJoin(TABLE_TASK)->alias('t2')->on("t1.objectID=t2.id and t1.objectType = 'task'")
             ->where('t2.project')->in($projectIdList)
-            ->andWhere('t1.objectType')->eq('task')
             ->beginIF($time == 'THIS_YEAR')->andWhere('LEFT(t1.`date`, 4)')->eq(date('Y'))->fi()
             ->andWhere('t2.deleted')->eq(0)
             ->andWhere('t2.parent')->lt(1)
