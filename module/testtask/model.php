9d5f645870c47890cecf438e850bec95a5ba1c2e
zhaoke <zhaoke@easycorp.ltd>
2022-09-06 14:34:20 +0800

    * Add do not change case title when submit unit result.

--- module/testtask/model.php
+++ module/testtask/model.php
@@ -1726,6 +1726,7 @@ class testtaskModel extends model
      */
     public function processAutoResult($testtaskID, $productID, $suites, $cases, $results, $suiteNames = array(), $caseTitles = array(), $auto = 'unit')
     {
+        var_dump($cases);die;
         if(empty($cases)) return print(js::alert($this->lang->testtask->noImportData));
 
         /* Import cases and link task and insert result. */
@@ -2114,7 +2115,7 @@ class testtaskModel extends model
             $case = new stdclass();
             if(!empty($caseResult->id)) $case->id = $caseResult->id;
             $case->product    = $productID;
-            $case->title      = $caseResult->title;
+            if(empty($caseResult->id)) $case->title = $caseResult->title;
             $case->pri        = 3;
             $case->type       = 'unit';
             $case->stage      = 'unittest';
