19a69c8cae1fbafd286a67b50f2c709f8e9cc5a3
wangyidong <wangyidong@cnezsoft.com>
2022-09-06 09:30:22 +0800

    * code for task #67405.

--- module/task/view/lineareffort.html.php
+++ module/task/view/lineareffort.html.php
@@ -21,92 +21,94 @@ foreach($efforts as $effort)
     if($app->user->account == $account) $myOrders[$order] = $order;
 }
 ?>
-<?php if($myOrders):?>
-<div class='tabs' id='linearefforts'>
-  <ul class='nav nav-tabs'>
-    <li class='active'><a href='#legendMyEffort' data-toggle='tab'><?php echo $lang->task->myEffort;?></a></li>
-    <li><a href='#legendAllEffort' data-toggle='tab'><?php echo $lang->task->allEffort;?></a></li>
-  </ul>
-  <div class='tab-content'>
-    <div class='tab-pane active' id='legendMyEffort'>
-      <?php foreach($myOrders as $order):?>
-      <div class='caption'>
-        <span class='label label-badge'><?php echo $order + 1;?></span>
-        <span class='account'><?php echo zget($users, $app->user->account);?></span>
+<div id='linearefforts'>
+  <?php if($myOrders):?>
+  <div class='tabs'>
+    <ul class='nav nav-tabs'>
+      <li class='active'><a href='#legendMyEffort' data-toggle='tab'><?php echo $lang->task->myEffort;?></a></li>
+      <li><a href='#legendAllEffort' data-toggle='tab'><?php echo $lang->task->allEffort;?></a></li>
+    </ul>
+    <div class='tab-content'>
+      <div class='tab-pane active' id='legendMyEffort'>
+        <?php foreach($myOrders as $order):?>
+        <div class='caption'>
+          <span class='label label-badge'><?php echo $order + 1;?></span>
+          <span class='account'><?php echo zget($users, $app->user->account);?></span>
+        </div>
+        <table class='table table-bordered table-fixed table-recorded'>
+          <thead>
+            <tr class='text-center'>
+              <th class="w-120px"><?php echo $lang->task->date;?></th>
+              <th class="w-120px"><?php echo $lang->task->recordedBy;?></th>
+              <th><?php echo $lang->comment;?></th>
+              <th class="thWidth"><?php echo $lang->task->consumed;?></th>
+              <th class="thWidth"><?php echo $lang->task->left;?></th>
+              <th class='c-actions-2'><?php echo $lang->actions;?></th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach($allEfforts[$order] as $effort):?>
+            <tr class="text-center">
+              <td><?php echo $effort->date;?></td>
+              <td><?php echo zget($users, $effort->account);?></td>
+              <td class="text-left" title="<?php echo $effort->work;?>"><?php echo $effort->work;?></td>
+              <td title="<?php echo $effort->consumed . ' ' . $lang->execution->workHour;?>"><?php echo $effort->consumed . ' ' . $lang->execution->workHourUnit;?></td>
+              <td title="<?php echo $effort->left     . ' ' . $lang->execution->workHour;?>"><?php echo $effort->left     . ' ' . $lang->execution->workHourUnit;?></td>
+              <td align='center' class='c-actions'>
+                <?php
+                $canOperateEffort = $this->task->canOperateEffort($task, $effort);
+                common::printIcon($this->config->edition == 'open' ? 'task' : 'effort', $this->config->edition == 'open' ? 'editEstimate' : 'edit', "effortID=$effort->id", '', 'list', 'edit', '', 'showinonlybody', true, $canOperateEffort ? '' : 'disabled');
+                common::printIcon($this->config->edition == 'open' ? 'task' : 'effort', $this->config->edition == 'open' ? 'deleteEstimate' : 'delete', "effortID=$effort->id", '', 'list', 'trash', 'hiddenwin', 'showinonlybody', false, ($canOperateEffort and $effort->left > 0) ? '' : 'disabled');
+                ?>
+              </td>
+            </tr>
+            <?php endforeach;?>
+          </tbody>
+        </table>
+        <?php endforeach;?>
       </div>
-      <table class='table table-bordered table-fixed table-recorded'>
-        <thead>
-          <tr class='text-center'>
-            <th class="w-120px"><?php echo $lang->task->date;?></th>
-            <th class="w-120px"><?php echo $lang->task->recordedBy;?></th>
-            <th><?php echo $lang->comment;?></th>
-            <th class="thWidth"><?php echo $lang->task->consumed;?></th>
-            <th class="thWidth"><?php echo $lang->task->left;?></th>
-            <th class='c-actions-2'><?php echo $lang->actions;?></th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php foreach($allEfforts[$order] as $effort):?>
-          <tr class="text-center">
-            <td><?php echo $effort->date;?></td>
-            <td><?php echo zget($users, $effort->account);?></td>
-            <td class="text-left" title="<?php echo $effort->work;?>"><?php echo $effort->work;?></td>
-            <td title="<?php echo $effort->consumed . ' ' . $lang->execution->workHour;?>"><?php echo $effort->consumed . ' ' . $lang->execution->workHourUnit;?></td>
-            <td title="<?php echo $effort->left     . ' ' . $lang->execution->workHour;?>"><?php echo $effort->left     . ' ' . $lang->execution->workHourUnit;?></td>
-            <td align='center' class='c-actions'>
-              <?php
-              $canOperateEffort = $this->task->canOperateEffort($task, $effort);
-              common::printIcon($this->config->edition == 'open' ? 'task' : 'effort', $this->config->edition == 'open' ? 'editEstimate' : 'edit', "effortID=$effort->id", '', 'list', 'edit', '', 'showinonlybody', true, $canOperateEffort ? '' : 'disabled');
-              common::printIcon($this->config->edition == 'open' ? 'task' : 'effort', $this->config->edition == 'open' ? 'deleteEstimate' : 'delete', "effortID=$effort->id", '', 'list', 'trash', 'hiddenwin', 'showinonlybody', false, ($canOperateEffort and $effort->left > 0) ? '' : 'disabled');
-              ?>
-            </td>
-          </tr>
-          <?php endforeach;?>
-        </tbody>
-      </table>
-      <?php endforeach;?>
-    </div>
-    <div class='tab-pane' id='legendAllEffort'>
-<?php endif;?>
-      <?php foreach($recorders as $order => $accounts):?>
-      <div class='caption'>
-        <span class='label label-badge'><?php echo $order + 1;?></span>
-        <span class='account'><?php foreach($accounts as $account) echo zget($users, $account) . ' ';?></span>
+      <div class='tab-pane' id='legendAllEffort'>
+  <?php endif;?>
+        <?php foreach($recorders as $order => $accounts):?>
+        <div class='caption'>
+          <span class='label label-badge'><?php echo $order + 1;?></span>
+          <span class='account'><?php foreach($accounts as $account) echo zget($users, $account) . ' ';?></span>
+        </div>
+        <table class='table table-bordered table-fixed table-recorded'>
+          <thead>
+            <tr class='text-center'>
+              <th class="w-120px"><?php echo $lang->task->date;?></th>
+              <th class="w-120px"><?php echo $lang->task->recordedBy;?></th>
+              <th><?php echo $lang->comment;?></th>
+              <th class="thWidth"><?php echo $lang->task->consumed;?></th>
+              <th class="thWidth"><?php echo $lang->task->left;?></th>
+              <th class='c-actions-2'><?php echo $lang->actions;?></th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach($allEfforts[$order] as $effort):?>
+            <tr class="text-center">
+              <td><?php echo $effort->date;?></td>
+              <td><?php echo zget($users, $effort->account);?></td>
+              <td class="text-left" title="<?php echo $effort->work;?>"><?php echo $effort->work;?></td>
+              <td title="<?php echo $effort->consumed . ' ' . $lang->execution->workHour;?>"><?php echo $effort->consumed . ' ' . $lang->execution->workHourUnit;?></td>
+              <td title="<?php echo $effort->left     . ' ' . $lang->execution->workHour;?>"><?php echo $effort->left     . ' ' . $lang->execution->workHourUnit;?></td>
+              <td align='center' class='c-actions'>
+                <?php
+                $canOperateEffort = $this->task->canOperateEffort($task, $effort);
+                common::printIcon($this->config->edition == 'open' ? 'task' : 'effort', $this->config->edition == 'open' ? 'editEstimate' : 'edit', "effortID=$effort->id", '', 'list', 'edit', '', 'showinonlybody', true, $canOperateEffort ? '' : 'disabled');
+                common::printIcon($this->config->edition == 'open' ? 'task' : 'effort', $this->config->edition == 'open' ? 'deleteEstimate' : 'delete', "effortID=$effort->id", '', 'list', 'trash', 'hiddenwin', 'showinonlybody', false, ($canOperateEffort and $effort->left > 0) ? '' : 'disabled');
+                ?>
+              </td>
+            </tr>
+            <?php endforeach;?>
+          </tbody>
+        </table>
+        <?php endforeach;?>
+  <?php if($myOrders):?>
       </div>
-      <table class='table table-bordered table-fixed table-recorded'>
-        <thead>
-          <tr class='text-center'>
-            <th class="w-120px"><?php echo $lang->task->date;?></th>
-            <th class="w-120px"><?php echo $lang->task->recordedBy;?></th>
-            <th><?php echo $lang->comment;?></th>
-            <th class="thWidth"><?php echo $lang->task->consumed;?></th>
-            <th class="thWidth"><?php echo $lang->task->left;?></th>
-            <th class='c-actions-2'><?php echo $lang->actions;?></th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php foreach($allEfforts[$order] as $effort):?>
-          <tr class="text-center">
-            <td><?php echo $effort->date;?></td>
-            <td><?php echo zget($users, $effort->account);?></td>
-            <td class="text-left" title="<?php echo $effort->work;?>"><?php echo $effort->work;?></td>
-            <td title="<?php echo $effort->consumed . ' ' . $lang->execution->workHour;?>"><?php echo $effort->consumed . ' ' . $lang->execution->workHourUnit;?></td>
-            <td title="<?php echo $effort->left     . ' ' . $lang->execution->workHour;?>"><?php echo $effort->left     . ' ' . $lang->execution->workHourUnit;?></td>
-            <td align='center' class='c-actions'>
-              <?php
-              $canOperateEffort = $this->task->canOperateEffort($task, $effort);
-              common::printIcon($this->config->edition == 'open' ? 'task' : 'effort', $this->config->edition == 'open' ? 'editEstimate' : 'edit', "effortID=$effort->id", '', 'list', 'edit', '', 'showinonlybody', true, $canOperateEffort ? '' : 'disabled');
-              common::printIcon($this->config->edition == 'open' ? 'task' : 'effort', $this->config->edition == 'open' ? 'deleteEstimate' : 'delete', "effortID=$effort->id", '', 'list', 'trash', 'hiddenwin', 'showinonlybody', false, ($canOperateEffort and $effort->left > 0) ? '' : 'disabled');
-              ?>
-            </td>
-          </tr>
-          <?php endforeach;?>
-        </tbody>
-      </table>
-      <?php endforeach;?>
-<?php if($myOrders):?>
     </div>
   </div>
+  <?php endif;?>
 </div>
-<?php endif;?>
 <?php endif;?>
