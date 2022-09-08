7c2244c088ae2dcc61c9d5c70ee3d78694cf0e4f
zenggang <zenggang@easycorp.ltd>
2022-09-08 00:46:20 +0000

    * Code for task#67792

--- db/update17.6.sql
+++ db/update17.6.sql
@@ -13,3 +13,5 @@ CREATE TABLE `zt_taskteam` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ALTER TABLE `zt_taskestimate` ADD `order` tinyint unsigned NULL DEFAULT '0';
 ALTER TABLE `zt_effort` ADD `order` tinyint unsigned NOT NULL DEFAULT '0' AFTER `end`;
+
+REPLACE INTO `zt_config` (`owner`, `module`, `section`, `key`, `value`) VALUES ('system', 'common', 'global', 'syncProductFeedback', '0');
