3b97ed9213fec8f845492e73e62ec2010f83deb8
liugang <liugang@easycorp.ltd>
2022-09-15 09:30:46 +0800

    * Fix bug of upgrade the exist projects.

--- db/update17.6.1.sql
+++ db/update17.6.1.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `zt_product` ADD `shadow` tinyint(1) unsigned NOT NULL AFTER `code`;
-ALTER TABLE `zt_project` ADD `hasProduct` tinyint(1) unsigned NOT NULL AFTER `code`;
+ALTER TABLE `zt_project` ADD `hasProduct` tinyint(1) unsigned NOT NULL DEFAULT 1 AFTER `code`;
