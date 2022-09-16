e8481ba4a235de5a2238571c7e9f8c998ac31b0e
daitingting <daitingting@xirangit.com>
2022-09-16 02:02:50 +0000

    * Finish task #69245.

--- module/custom/view/header.html.php
+++ module/custom/view/header.html.php
@@ -20,7 +20,7 @@
       if($object == 'user') common::printLink('custom', 'required', "", "<span class='text'>{$lang->custom->required}</span>", '', "class='btn btn-link' id='requiredTab'");
   }
 
-  if($config->systemMode == 'classic') common::printLink('custom', 'mode', "", "<span class='text'>{$lang->custom->mode}</span>", '', "class='btn btn-link' id='modeTab'");
+  common::printLink('custom', 'mode', "", "<span class='text'>{$lang->custom->mode}</span>", '', "class='btn btn-link' id='modeTab'");
   ?>
   </div>
 </div>
