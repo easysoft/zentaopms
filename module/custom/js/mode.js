8f92f57390848b1175721d6fe25146fc949154ea
lanzongjun <lanzongjun@easycorp.ltd>
2022-09-19 14:52:21 +0800

    * finish task #69255

--- module/custom/js/mode.js
+++ module/custom/js/mode.js
@@ -3,6 +3,13 @@ $(function()
     $('#modeTab').addClass('btn-active-text');
     $('[name=mode]').change(function()
     {
-        $(this).val() == 'new' ? $('#changeModeTips').removeClass('hidden') : $('#changeModeTips').addClass('hidden');
+        if($(this).val() == 'lean')
+        {
+            $("#selectDefaultProgram").removeClass('hidden');
+        }
+        else
+        {
+            $("#selectDefaultProgram").addClass('hidden');
+        }
     });
 })
--- module/custom/view/mode.html.php
+++ module/custom/view/mode.html.php
@@ -18,23 +18,26 @@
         <strong><?php echo $lang->custom->mode?></strong>
       </div>
     </div>
-    <table class='table table-form'>
+    <table class='table table-form mw-300px'>
       <tr>
         <th class='text-top'><?php echo $lang->custom->mode;?></th>
         <td>
           <p>
-            <?php $isDisabled = $changedMode == 'yes' ? ' disabled' : '';?>
-            <label class="radio-inline"><input type="radio" name="mode" value="lean" <?php echo $mode == 'lean'? "checked='checked'" : ''; echo $isDisabled;?> id="modelean"><?php echo $lang->upgrade->to18Mode['lean'];?></label>
-            <label class="radio-inline"><input type="radio" name="mode" value="new" <?php echo $mode == 'new'? "checked='checked'" : ''; echo $isDisabled;?> id="modenew"><?php echo $lang->upgrade->to18Mode['new'];?></label>
+            <label class="radio-inline"><input type="radio" name="mode" value="lean" <?php echo $mode == 'lean'? "checked='checked'" : '';?> id="modelean"><?php echo $lang->upgrade->to18Mode['lean'];?></label>
+            <label class="radio-inline"><input type="radio" name="mode" value="new" <?php echo $mode == 'new'? "checked='checked'" : '';?> id="modenew"><?php echo $lang->upgrade->to18Mode['new'];?></label>
           </p>
-          <p class='text-info' id='modeTips'><?php echo $lang->upgrade->selectedModeTips['new'];?></p>
         </td>
+      </tr>
+      <?php
+      $class = ''; if($mode == 'new') $class="class='hidden'";?>
+      <tr id="selectDefaultProgram" <?php echo $class;?>>
+        <td></td>
+        <td><?php echo html::select('program', $program, '', "class='form-control chosen'");?></td>
       </tr>
       <tr>
         <td></td>
         <td>
-          <?php if($changedMode != 'yes') echo html::submitButton($lang->custom->switch);?>
-          <div id='changeModeTips' class='text-danger hidden'><?php echo $lang->custom->changeModeTips;?></div>
+          <?php echo html::submitButton($lang->custom->switch);?>
         </td>
       </tr>
     </table>
