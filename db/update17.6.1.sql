3a9b2c495255d7d96479aa24ff771b4df67bb001
tianshujie <tianshujie0123@163.com>
2022-09-26 08:12:53 +0800

    * Modify the SQL.

--- db/update17.6.1.sql
+++ db/update17.6.1.sql
@@ -1,1 +1,1 @@
-UPDATE `zt_workflowlabel` SET `label` = '全部' where AND `label` = '所有' AND `action` = 'browse';
+UPDATE `zt_workflowlabel` SET `label` = '全部' where `label` = '所有' AND `action` = 'browse';
