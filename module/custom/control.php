dc8b4f4ccd0c41ef442bc8245d88db153995bc32
daitingting <daitingting@xirangit.com>
2022-09-27 05:19:40 +0000

    * Optimize codes.

--- module/custom/control.php
+++ module/custom/control.php
@@ -638,8 +638,8 @@ class custom extends control
             $this->loadModel('setting')->setItem('system.common.global.mode', $mode);
             $this->setting->setItem('system.common.global.defaultProgram', $program);
 
-            /* 只有没有关联项目集的产品和项目关联到默认项目集下. */
-            $this->loadModel('upgrade')->relationDefaultProgram($program);
+            /* Set default program for product and project with no program. */
+            $this->loadModel('upgrade')->relateDefaultProgram($program);
 
             $this->custom->disableFeaturesByMode($mode);
 
--- module/upgrade/model.php
+++ module/upgrade/model.php
@@ -7722,28 +7722,19 @@ class upgradeModel extends model
     }
 
     /**
-     * Relation default program.
+     * Relate default program.
      *
      * @param  int $programID
      * @access public
      * @return bool
      */
-    public function relationDefaultProgram($programID)
+    public function relateDefaultProgram($programID)
     {
-        /* product */
-        $this->dao->update(TABLE_PRODUCT)
-            ->set('program')->eq($programID)
-            ->where('program')->eq(0)
-            ->exec();
+        $this->dao->update(TABLE_PRODUCT)->set('program')->eq($programID)->where('program')->eq(0)->exec();
 
-        /* project */
-        $this->dao->update(TABLE_PROJECT)
-            ->set("path = CONCAT(',{$programID}', path)")
-            ->set('grade = grade+1')
-            ->where('type')->eq('project')->andWhere('parent')->eq(0)->andWhere('grade')->eq(1)
-            ->exec();
+        $this->dao->update(TABLE_PROJECT)->set("path = CONCAT(',{$programID}', path)")->set('grade = grade + 1')->where('type')->eq('project')->andWhere('parent')->eq(0)->andWhere('grade')->eq(1)->exec();
 
-        return true;
+        return !dao::isError();
     }
 
     /**
