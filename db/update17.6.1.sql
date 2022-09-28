6c2a324e400f25c6a675ea7bc5bf68525fb17cbf
liugang <liugang@easycorp.ltd>
2022-09-28 16:30:28 +0800

    * Create view to filter the shadow products.

--- db/update17.6.1.sql
+++ db/update17.6.1.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `zt_product` ADD `shadow` tinyint(1) unsigned NOT NULL AFTER `code`;
 ALTER TABLE `zt_project` ADD `hasProduct` tinyint(1) unsigned NOT NULL DEFAULT 1 AFTER `code`;
 
-CREATE VIEW `ztv_normalproduct` AS SELECT * FROM `zt_product` WHERE `shadow` = 0;
+CREATE OR REPLACE VIEW `ztv_normalproduct` AS SELECT * FROM `zt_product` WHERE `shadow` = 0;
