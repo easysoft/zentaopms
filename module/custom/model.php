d33cc87da9dbda7ca3435f57a522e136dfde5bcb
wangyuting2 <851424971@qq.com>
2022-09-29 05:50:16 +0000

    * Fix bug in review code.

--- module/custom/model.php
+++ module/custom/model.php
@@ -992,7 +992,7 @@ class customModel extends model
     {
         if($this->config->edition == 'max')
         {
-            return $this->dao->select('id')->from(TABLE_EXECUTION)->alias('t1')
+            return $this->dao->select('id')->from(TABLE_PROJECT)->alias('t1')
                 ->leftJoin(TABLE_MEETING)->alias('t2')->on('t1.id = t2.project')
                 ->where('t1.model')->eq('scrum')
                 ->andWhere('t1.deleted')->eq('0')
@@ -1012,7 +1012,7 @@ class customModel extends model
     {
         if($this->config->edition == 'max')
         {
-            return $this->dao->select('id')->from(TABLE_EXECUTION)->alias('t1')
+            return $this->dao->select('id')->from(TABLE_PROJECT)->alias('t1')
                 ->leftJoin(TABLE_AUDITPLAN)->alias('t2')->on('t1.id = t2.project')
                 ->where('t1.model')->eq('scrum')
                 ->andWhere('t1.deleted')->eq('0')
