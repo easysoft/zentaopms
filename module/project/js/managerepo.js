804cf64783813ec921fbb710f65790e92c86fe08
wangjianhua <wangjianhua@easycorp.ltd>
2022-09-30 15:01:31 +0800

    * Improve css and js of manage repo page.

--- module/project/js/managerepo.js
+++ module/project/js/managerepo.js
@@ -1,14 +1,7 @@
 $(function()
 {
-    $('#reposBox input:checkbox').each(function()
-    {
-        var $cb = $(this);
-        if($cb.prop('checked')) $cb.closest('.repo').addClass('checked');
-    });
-
     $('#reposBox input:checkbox').change(function()
     {
-        var $cb = $(this);
-        $cb.closest('.repo').toggleClass('checked', $cb.prop('checked'));
+        $(this).closest('.repo').toggleClass('checked', $(this).prop('checked'));
     });
 });
