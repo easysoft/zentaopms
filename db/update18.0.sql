0c11f5b232f9188235c5321b03567921084adfc0
liugang <liugang@easycorp.ltd>
2022-09-30 09:36:41 +0800

    * Optimize.

--- db/update18.0.sql
+++ db/update18.0.sql
@@ -1,9 +1,6 @@
 ALTER TABLE `zt_product` ADD `shadow` tinyint(1) unsigned NOT NULL AFTER `code`;
 ALTER TABLE `zt_project` ADD `hasProduct` tinyint(1) unsigned NOT NULL DEFAULT 1 AFTER `code`;
-
-ALTER TABLE `zt_product` MODIFY `name` varchar(110) NOT NULL;
-CREATE OR REPLACE VIEW `ztv_normalproduct` AS SELECT * FROM `zt_product` WHERE `shadow` = 0;
-
+ALTER table `zt_project` ADD `multiple` enum('0', '1') NOT NULL DEFAULT '1';
 ALTER TABLE `zt_repo` ADD `projects` varchar(255) NOT NULL AFTER `product`;
 
-ALTER table `zt_project` ADD `multiple` enum('0', '1') NOT NULL DEFAULT '1';
+CREATE OR REPLACE VIEW `ztv_normalproduct` AS SELECT * FROM `zt_product` WHERE `shadow` = 0;
