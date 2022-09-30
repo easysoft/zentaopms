f8c9dad863bc0c51b47ad010722074233f10b6b4
xieqiyu <xieqiyu@easycorp.ltd>
2022-09-30 07:28:48 +0000

    * Fix bug#26061.

--- module/testsuite/control.php
+++ module/testsuite/control.php
@@ -323,7 +323,7 @@ class testsuite extends control
 
         /* Build the search form. */
         $this->loadModel('testcase');
-        $this->config->testcase->search['params']['module']['values'] = $this->loadModel('tree')->getOptionMenu($productID, $viewType = 'case');
+        $this->config->testcase->search['params']['module']['values'] = $this->loadModel('tree')->getOptionMenu($productID, $viewType = 'case', 0, 'all');
         $this->config->testcase->search['module']    = 'testsuite';
         $this->config->testcase->search['actionURL'] = inlink('linkCase', "suiteID=$suiteID&param=myQueryID");
         unset($this->config->testcase->search['fields']['product']);
