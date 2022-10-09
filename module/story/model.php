4e07ba29dd1bde1480e3e15eb473b59e51ea8490
liugang <liugang@easycorp.ltd>
2022-10-09 14:25:09 +0800

    * Fix bug #28168.

--- module/story/model.php
+++ module/story/model.php
@@ -4932,7 +4932,7 @@ class storyModel extends model
                 echo $story->version;
                 break;
             case 'actions':
-                if($tab == 'execution' || (isset($_SESSION['multiple']) && empty($_SESSION['multiple'])))
+                if($tab == 'execution' || ($tab == 'project' && isset($_SESSION['multiple']) && empty($_SESSION['multiple'])))
                 {
                     $menuType = 'execution';
                 }
