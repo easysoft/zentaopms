19feba79335bdff6542be45d8f19792710842cb4
wangyidong <wangyidong@cnezsoft.com>
2022-10-11 16:09:53 +0800

    * fix for task #71610.

--- module/testtask/model.php
+++ module/testtask/model.php
@@ -296,7 +296,7 @@ class testtaskModel extends model
      */
     public function getByUser($account, $pager = null, $orderBy = 'id_desc', $type = '')
     {
-        return $this->dao->select("t1.*, t2.name AS executionName, t2.multiple as executionMultiple, t4.name as projectName, t3.name AS buildName")
+        return $this->dao->select("t1.*, t2.name AS executionName, t2.multiple as executionMultiple, t5.name as projectName, t3.name AS buildName")
             ->from(TABLE_TESTTASK)->alias('t1')
             ->leftJoin(TABLE_EXECUTION)->alias('t2')->on('t1.execution = t2.id')
             ->leftJoin(TABLE_BUILD)->alias('t3')->on('t1.build = t3.id')
