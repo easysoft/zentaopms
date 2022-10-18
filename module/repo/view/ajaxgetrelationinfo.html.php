f6d7c410fa71e6e6d0e6ccc607b31a6de23f80ad
liyuchun <563917701@qq.com>
2022-10-18 16:54:38 +0800

    * Adjust repo code style.

--- module/repo/view/ajaxgetrelationinfo.html.php
+++ module/repo/view/ajaxgetrelationinfo.html.php
@@ -17,17 +17,17 @@ include '../../common/view/header.lite.html.php';
       <div class="left-content col-md-8">
         <table class="menu-title">
           <?php if($objectType == 'story'):?>
-          <tr><th class='text-right'><?php echo $lang->release->storyTitle;?>: </th><td class="text"><?php echo $object->title; ?></td></tr>
-          <tr><th class='text-right'><?php echo $lang->story->legendSpec;?>: </th><td class="text"><div class="detail-content article-content"><?php echo $object->spec; ?></div></td></tr>
-          <tr><th class='text-right'><?php echo $lang->story->verify;?>: </th><td class="text"><div class="detail-content article-content"><?php echo $object->verify; ?></div></td></tr>
+          <tr><th class='text-left'><?php echo $lang->release->storyTitle;?>: </th><td class="text"><?php echo $object->title; ?></td></tr>
+          <tr><th class='text-left'><?php echo $lang->story->legendSpec;?>: </th><td class="text"><div class="detail-content article-content"><?php echo $object->spec; ?></div></td></tr>
+          <tr><th class='text-left'><?php echo $lang->story->verify;?>: </th><td class="text"><div class="detail-content article-content"><?php echo $object->verify; ?></div></td></tr>
           <?php elseif($objectType == 'task'):?>
-          <tr><th class='text-right'><?php echo $lang->task->name;?>: </th><td class="text"><?php echo $object->name; ?></td></tr>
-          <tr><th class='text-right'><?php echo $lang->task->desc;?>: </th><td class="text"><div class="detail-content article-content"><?php echo $object->desc; ?></div></td></tr>
-          <tr><th class='text-right'><?php echo $lang->task->story;?>: </th><td class="text"><div class="detail-content article-content"><?php echo $object->storyTitle; ?></div></td></tr>
+          <tr><th class='text-left'><?php echo $lang->task->name;?>: </th><td class="text"><?php echo $object->name; ?></td></tr>
+          <tr><th class='text-left'><?php echo $lang->task->desc;?>: </th><td class="text"><div class="detail-content article-content"><?php echo $object->desc; ?></div></td></tr>
+          <tr><th class='text-left'><?php echo $lang->task->story;?>: </th><td class="text"><div class="detail-content article-content"><?php echo $object->storyTitle; ?></div></td></tr>
           <?php elseif($objectType == 'bug'):?>
-          <tr><th class='text-right'><?php echo $lang->bug->title;?>: </th><td class="text"><?php echo $object->title; ?></td></tr>
-          <tr><th class='text-right'><?php echo $lang->bug->type;?>: </th><td class="text"><?php echo zget($lang->bug->typeList, $object->type); ?></td></tr>
-          <tr><th class='text-right'><?php echo $lang->bug->steps;?>: </th><td class="text"><div class="detail-content article-content"><?php echo $object->steps; ?></div></td></tr>
+          <tr><th class='text-left'><?php echo $lang->bug->title;?>: </th><td class="text"><?php echo $object->title; ?></td></tr>
+          <tr><th class='text-left'><?php echo $lang->bug->type;?>: </th><td class="text"><?php echo zget($lang->bug->typeList, $object->type); ?></td></tr>
+          <tr><th class='text-left'><?php echo $lang->bug->steps;?>: </th><td class="text"><div class="detail-content article-content"><?php echo $object->steps; ?></div></td></tr>
           <?php endif;?>
         </table>
       </div>
