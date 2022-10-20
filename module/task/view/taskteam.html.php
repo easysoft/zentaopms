ae0c045c0250d9d111341c97fc3bf49666041d3d
tianshujie <tianshujie0123@163.com>
2022-10-20 16:06:26 +0800

    * Optimize the style of maintaining multi-tasking teams.

--- module/task/view/taskteam.html.php
+++ module/task/view/taskteam.html.php
@@ -3,6 +3,7 @@
 #taskTeamEditor .estimateBox span {background-color: #fff;}
 #taskTeamEditor .estimateBox input {background-color: #fff; border-right-width: 0px;}
 #taskTeamEditor input, #taskTeamEditor span, #taskTeamEditor .chosen-container > a {border-color: #eee;}
+#taskTeamEditor td.sort-handler {padding-bottom: 12px;}
 </style>
 <?php if($app->rawMethod != 'create'):?>
 <style>
@@ -12,7 +13,7 @@
 <?php endif;?>
 <?php if($app->rawMethod == 'assignto'):?>
 <style>
-#taskTeamEditor td.sort-handler {width: 70px !important; padding-bottom: 12px;}
+#taskTeamEditor td.sort-handler {width: 70px !important;}
 </style>
 <?php endif;?>
 <?php $hiddenArrow = (empty($task->mode) or $task->mode == 'linear') ? '' : 'hidden';?>
