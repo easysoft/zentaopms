541ea48b72400cd40fafe46f26ed29aaebae0315
wangyuting2 <851424971@qq.com>
2022-10-24 06:14:53 +0000

    * Finish task #72986.

--- module/product/js/edit.js
+++ module/product/js/edit.js
@@ -8,33 +8,10 @@ $('#program').change(function()
     var programID = $(this).val();
     programID > 0 ? $("#line").closest('tr').removeClass('hidden') : $("#line").closest('tr').addClass('hidden');
 
-    if(singleLinkProjects.length !== 0 || multipleLinkProjects.length !== 0)
-    {
-        $('#changeProgram').modal({show: true});
-    }
-
     $.get(createLink('product', 'ajaxGetLine', 'programID=' + programID), function(data)
     {
         $('#line_chosen').remove();
         $('#line').replaceWith(data);
         $('#line').chosen();
     })
-})
-
-/**
- * Set projects of change program.
- *
- * @access public
- * @return void
- */
-function setChangeProjects()
-{
-    var projects = ',';
-    $("input[name^='projects']:checked").each(function()
-    {
-        projects += $(this).val() + ',';
-        $('#changeProjects').val(projects);
-    });
-
-    $('#changeProgram').modal('hide');
-}
+});
