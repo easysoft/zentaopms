36127e77172d61cf3939d021dac9dcc3401604eb
tianshujie <tianshujie0123@163.com>
2022-10-25 08:47:02 +0800

    * Fix bug #28878.

--- module/build/control.php
+++ module/build/control.php
@@ -287,7 +287,7 @@ class build extends control
         $this->view->canBeChanged = common::canBeChanged('build', $build); // Determines whether an object is editable.
         $this->view->users        = $this->loadModel('user')->getPairs('noletter');
         $this->view->build        = $build;
-        $this->view->buildPairs   = $this->build->getBuildPairs(0, 0, 'noempty,notrunk', $build->execution, 'execution');
+        $this->view->buildPairs   = $this->build->getBuildPairs(0, 'all', 'noempty,notrunk', $build->execution, 'execution');
         $this->view->actions      = $this->loadModel('action')->getList('build', $buildID);
         $this->view->link         = $link;
         $this->view->param        = $param;
