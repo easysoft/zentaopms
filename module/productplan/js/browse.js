e28284d001c5c3779c0a141e8faf175c10953577
caoyanyi <caoyanyi@easycorp.ltd>
2022-10-26 15:04:13 +0800

    * Code for task #73764.

--- module/productplan/js/browse.js
+++ module/productplan/js/browse.js
@@ -18,7 +18,7 @@ $(function()
         var content  = $content.find('div').html();
         if(content.indexOf('<br') >= 0 || content.indexOf('<img') >= 0)
         {
-            $content.append("<a href='###' class='more'><i class='icon icon-chevron-double-down'></i></a>");
+            $content.append("<a href='###' class='more'><i class='icon icon-angle-right'></i></a>");
         }
     });
 
@@ -192,14 +192,14 @@ $(document).on('click', 'td.content .more', function(e)
         $toggle.removeClass('open');
         $toggle.closest('.content').find('div').css('height', '25px');
         $toggle.css('padding-top', 0);
-        $toggle.find('i').removeClass('icon-chevron-double-up').addClass('icon-chevron-double-down');
+        $toggle.find('i').removeClass('rotate-down');
     }
     else
     {
         $toggle.addClass('open');
         $toggle.closest('.content').find('div').css('height', 'auto');
         $toggle.css('padding-top', ($toggle.closest('.content').find('div').height() - $toggle.height()) / 2);
-        $toggle.find('i').removeClass('icon-chevron-double-down').addClass('icon-chevron-double-up');
+        $toggle.find('i').addClass('rotate-down');
     }
 });
 
