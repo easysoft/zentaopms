53421d6093cc826869a4e3fc558420766fbc7ffb
liyuchun <563917701@qq.com>
2022-10-27 13:48:51 +0800

    * Add unit test.

--- test/model/repo/gethistory.php
+++ test/model/repo/gethistory.php
@@ -12,6 +12,22 @@ pid=1
 
 */
 
-$repo = new repoTest();
+global $tester, $dao;
+$repo      = $tester->loadModel('repo')->getRepoByID(1);
+$logs      = $tester->repo->getUnsyncedCommits($repo);
+$revision  = 1;
+$revisions = array();
+foreach($logs as $log)
+{
+    $tester->repo->saveOneCommit($repo->id, $log, $revision);
+    $revisions[] = $log->revision;
+    $revision ++;
+}
 
-r($repo->getHistoryTest()) && p() && e();
+$repoTest = new repoTest();
+
+r($repoTest->getHistoryTest(1, $revisions)) && p('1') && e('d5789b703dc09c44be1e89a991c3506de1f678e3');
+r($repoTest->getHistoryTest(1, array()))    && p('')  && e('empty');
+
+$dao->exec('truncate table zt_repohistory');
+$dao->exec('truncate table zt_repobranch');
