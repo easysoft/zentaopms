af0be6c87a7aa083c490a28d92c32da42463c734
tianshujie <tianshujie0123@163.com>
2022-11-02 16:28:26 +0800

    * Merge.

--- db/update17.7.sql
+++ db/update17.7.sql
@@ -1,12 +1,12 @@
 update zt_kanban
-set 
+set
   colWidth    = if(colWidth < 200, 200, colWidth),
   minColWidth = if(minColWidth < 200, 200, minColWidth),
   maxColWidth = if(maxColWidth <= 200 and minColWidth <= 200, 201, maxColWidth)
 where colWidth < 200 or minColWidth < 200 or maxColWidth < 200;
 
 update zt_project
-set 
+set
   colWidth    = if(colWidth < 200, 200, colWidth),
   minColWidth = if(minColWidth < 200, 200, minColWidth),
   maxColWidth = if(maxColWidth <= 200 and minColWidth <= 200, 201, maxColWidth)
