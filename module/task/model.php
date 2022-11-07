0159cabcff5ce97adc21d6c6df36b15a2c33e48b
liumengyi <liumengyi@easycorp.ltd>
2022-11-07 09:05:56 +0800

    * Fix bug #29272.

--- module/task/model.php
+++ module/task/model.php
@@ -1466,11 +1466,6 @@ class taskModel extends model
                 return false;
             }
 
-            if($this->config->systemMode == 'new')
-            {
-                $project = $this->loadModel('project')->getByID($oldTask->project);
-                if($project->model == 'waterfall') $this->config->task->edit->requiredFields .= ',estStarted,deadline';
-            }
             foreach(explode(',', $this->config->task->edit->requiredFields) as $field)
             {
                 $field = trim($field);
