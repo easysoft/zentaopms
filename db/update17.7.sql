cb56e049e66ec5f00a7db9d33d9abd86437a554c
宋辰轩 <songchenxuan@easycorp.ltd>
2022-11-08 10:50:17 +0800

    * Modify sql for 18-7.

--- db/update17.7.sql
+++ db/update17.7.sql
@@ -8,7 +8,8 @@ INSERT INTO `zt_module` (`root`, `branch`, `name`, `parent`, `path`, `grade`, `o
 (0, 0, '项目', 0, ',0,', 1, 20, 'chart', 'system', '', '', '0'),
 (0, 0, '测试', 0, ',0,', 1, 30, 'chart', 'system', '', '', '0'),
 (0, 0, '组织', 0, ',0,', 1, 40, 'chart', 'system', '', '', '0');
-UPDATE `zt_module` SET `root` = 1 where `owner` = 'system';
+UPDATE `zt_module` SET `root` = 1 where `type` = 'chart';
+UPDATE `zt_module` SET `root` = 1 where `type` = 'dashboard';
 
 UPDATE `zt_chart` SET `group` = (SELECT `id` FROM `zt_module` WHERE `type` = 'chart' AND `name` = '产品' limit 1);
 UPDATE `zt_module` SET `path` = CONCAT(',', `id`, ',') WHERE `type` = 'chart' AND `name` in ('产品', '项目', '测试', '组织');
@@ -29,10 +30,10 @@ CREATE TABLE IF NOT EXISTS `zt_dimension` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 INSERT INTO `zt_dimension` (`name`, `code`, `desc`, `createdBy`, `createdDate`, `editedBy`, `editedDate`) VALUES('default', 'default', '', 'system', CURDATE(), 'system', CURDATE());
 
-ALTER TABLE `zt_report`    ADD `dimension` int(8) NOT NULL default 1 AFTER `name`;
-ALTER TABLE `zt_chart`     ADD `dimension` int(8) NOT NULL default 1 AFTER `name`;
-ALTER TABLE `zt_dataset`   ADD `dimension` int(8) NOT NULL default 1 AFTER `name`;
-ALTER TABLE `zt_dashboard` ADD `dimension` int(8) NOT NULL default 1 AFTER `name`;
+ALTER TABLE `zt_report`    ADD `dimension` int(8) NOT NULL default 0 AFTER `name`;
+ALTER TABLE `zt_chart`     ADD `dimension` int(8) NOT NULL default 0 AFTER `name`;
+--ALTER TABLE `zt_dataset`   ADD `dimension` int(8) NOT NULL default 0 AFTER `name`;
+ALTER TABLE `zt_dashboard` ADD `dimension` int(8) NOT NULL default 0 AFTER `name`;
 
 UPDATE `zt_chart`     SET `dimension` = 1 WHERE `dimension` = 0;
 UPDATE `zt_report`    SET `dimension` = 1 WHERE `dimension` = 0;
