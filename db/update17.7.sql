d84d4a26c4c0b056d9f6439e084200bd634624e8
宋辰轩 <songchenxuan@easycorp.ltd>
2022-11-08 13:49:10 +0800

    * Delete unused sql.

--- db/update17.7.sql
+++ db/update17.7.sql
@@ -8,8 +8,6 @@ INSERT INTO `zt_module` (`root`, `branch`, `name`, `parent`, `path`, `grade`, `o
 (0, 0, '项目', 0, ',0,', 1, 20, 'chart', 'system', '', '', '0'),
 (0, 0, '测试', 0, ',0,', 1, 30, 'chart', 'system', '', '', '0'),
 (0, 0, '组织', 0, ',0,', 1, 40, 'chart', 'system', '', '', '0');
-UPDATE `zt_module` SET `root` = 1 where `type` = 'chart';
-UPDATE `zt_module` SET `root` = 1 where `type` = 'dashboard';
 
 UPDATE `zt_chart` SET `group` = (SELECT `id` FROM `zt_module` WHERE `type` = 'chart' AND `name` = '产品' limit 1);
 UPDATE `zt_module` SET `path` = CONCAT(',', `id`, ',') WHERE `type` = 'chart' AND `name` in ('产品', '项目', '测试', '组织');
@@ -38,4 +36,4 @@ ALTER TABLE `zt_dashboard` ADD `dimension` int(8) NOT NULL default 0 AFTER `name
 UPDATE `zt_chart`     SET `dimension` = 1 WHERE `dimension` = 0;
 UPDATE `zt_report`    SET `dimension` = 1 WHERE `dimension` = 0;
 UPDATE `zt_dashboard` SET `dimension` = 1 WHERE `dimension` = 0;
-UPDATE `zt_module`    SET `root` = 1      WHERE `root` = 0 AND `type` in ('chart');
+UPDATE `zt_module`    SET `root` = 1      WHERE `root` = 0 AND `type` in ('chart', 'dashboard');
