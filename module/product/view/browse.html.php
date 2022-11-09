f67c4801cb5f1fd665a37b245e5ec4b539e6a882
xieqiyu <xieqiyu@easycorp.ltd>
2022-11-09 02:03:57 +0000

    * Finish task#75379,#75324.

--- module/product/view/browse.html.php
+++ module/product/view/browse.html.php
@@ -494,9 +494,9 @@ $projectIDParam = $isProjectStory ? "projectID=$projectID&" : '';
             </ul>
           </div>
 
+          <?php if(($product->type != 'normal' and $branchID != 'all') or $product->type == 'normal'):?>
           <?php $isShowModuleBTN = ($this->app->tab == 'project' and $browseType != 'bybranch') ? false : true;?>
           <?php if($canBatchChangeModule and $productID and $isShowModuleBTN):?>
-          <?php if(($product->type != 'normal' and $branchID != 'all') or $product->type == 'normal')?>
           <div class="btn-group dropup">
             <button data-toggle="dropdown" type="button" class="btn"><?php echo $lang->story->moduleAB;?> <span class="caret"></span></button>
             <?php $withSearch = count($modules) > 8;?>
@@ -553,6 +553,7 @@ $projectIDParam = $isProjectStory ? "projectID=$projectID&" : '';
               </div>
             </div>
           </div>
+          <?php endif;?>
           <?php endif;?>
 
           <?php if($canBatchAssignTo):?>
