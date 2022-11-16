cdb40c819028a38950bf18affce48fb7ab8c24ab
宋辰轩 <songchenxuan@easycorp.ltd>
2022-11-16 16:27:09 +0800

    * Modify user api bug.

--- api/v1/entries/user.php
+++ api/v1/entries/user.php
@@ -408,8 +408,11 @@ class userEntry extends entry
         $this->setPost('gender', $gender);
 
         $password = $this->request('password', zget($_POST, 'password', ''));
-        $this->setPost('password1', md5($password));
-        $this->setPost('password2', md5($password));
+        if($password)
+        {
+            $this->setPost('password1', md5($password));
+            $this->setPost('password2', md5($password));
+        }
         $this->setPost('verifyPassword', md5($this->app->user->password . $this->app->session->rand));
 
         $control = $this->loadController('user', 'edit');
