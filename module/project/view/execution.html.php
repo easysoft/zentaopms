d626bfbaef13730b48610f45c813bf1fa7f35304
tanghucheng <tanghucheng@easycorp.ltd>
2022-11-16 08:49:31 +0800

    * Fix bug #29820.

--- module/project/view/execution.html.php
+++ module/project/view/execution.html.php
@@ -42,7 +42,7 @@
     <?php if(common::hasPriv('execution', 'task')) echo html::checkbox('showTask', array('1' => $lang->programplan->stageCustom->task), '', $this->cookie->showTask ? 'checked=checked' : '');?>
   </div>
   <div class='btn-toolbar pull-right'>
-    <?php if($project->model == 'waterfall'):?>
+    <?php if($project->model == 'waterfall' and $this->config->edition == 'max'):?>
     <div class="btn-group">
       <?php echo html::a('', "<i class='icon-list'></i> &nbsp;", '', "class='btn btn-icon text-primary switchBtn' title='{$lang->project->bylist}'");?>
       <?php echo html::a($this->createLink('programplan', 'browse', "projectID=$projectID&productID=$productID&type=gantt"), "<i class='icon-gantt-alt'></i> &nbsp;", '', "class='btn btn-icon switchBtn' title='{$lang->programplan->gantt}'");?>
