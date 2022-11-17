49f6118bb2f864c8e99f8b10e9f4e1c6e7297290
Lufei <lufei@easycorp.ltd>
2022-11-17 14:01:46 +0800

    - Remove useless config items.

--- module/admin/config.php
+++ module/admin/config.php
@@ -1,14 +1,4 @@
 <?php
-$config->url = new stdclass();
-$config->url->community = 'https://www.zentao.net';
-$config->url->ask       = 'https://www.zentao.net/ask-browse.html';
-$config->url->document  = 'https://www.zentao.net/help-book-zentaopmshelp.html';
-$config->url->feedback  = 'https://www.zentao.net/forum-board-1074.html';
-$config->url->faq       = 'https://www.zentao.net/ask-faq.html';
-$config->url->extension = 'https://www.zentao.net/extension-browse.html';
-$config->url->donation  = 'https://www.zentao.net/help-donation.html';
-$config->url->service   = 'https://www.cnezsoft.com/article-browse-1078.html';
-
 $config->admin->apiRoot = 'https://www.zentao.net';
 
 $config->admin->log = new stdclass();
