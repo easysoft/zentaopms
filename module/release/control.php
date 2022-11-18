fb9aa7d0ddddadd657a8ee913c8840da527d324d
王怡栋 <wangyidong@cnezsoft.com>
2022-11-18 10:57:15 +0800

    * code for task #76438.

--- module/release/control.php
+++ module/release/control.php
@@ -525,7 +525,7 @@ class release extends control
         }
         else
         {
-            $allStories = $this->story->getExecutionStories($executionIdList, $build->product, 0, 't1.`order`_desc', 'byBranch', $release->branch, 'story', $release->stories, $pager);
+            $allStories = $this->story->getExecutionStories($executionIdList, $release->product, 0, 't1.`order`_desc', 'byBranch', $release->branch, 'story', $release->stories, $pager);
         }
 
         $this->view->allStories     = $allStories;
