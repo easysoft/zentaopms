827f04e98a8dda978d97ff397ef2822515e5ed73
王怡栋 <wangyidong@cnezsoft.com>
2022-11-22 15:34:40 +0800

    * fix bug #30094.

--- module/projectrelease/view/browse.html.php
+++ module/projectrelease/view/browse.html.php
@@ -73,7 +73,7 @@
       ?>
       <tr data-type='<?php echo $release->status;?>'>
         <?php if($i == 0):?>
-        <td <?php echo $rowspan?>><?php echo html::a(inlink('view', "releaseID=$release->id"), sprintf('%03d', $release->id));?></td>
+        <td class='c-id' <?php echo $rowspan?>><?php echo html::a(inlink('view', "releaseID=$release->id"), sprintf('%03d', $release->id));?></td>
         <td <?php echo $rowspan?>>
           <?php
           $flagIcon = $release->marker ? "<icon class='icon icon-flag red' title='{$lang->release->marker}'></icon> " : '';
