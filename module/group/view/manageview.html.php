c366e947492e116384397423a2548286acd30d2a
daitingting <daitingting@xirangit.com>
2022-11-25 05:52:42 +0000

    * Remove debugging codes.

--- module/group/view/manageview.html.php
+++ module/group/view/manageview.html.php
@@ -105,7 +105,7 @@
         <th class='text-right text-top'><?php echo $lang->group->dynamic?></th>
         <td class='pl-0px pt-0px'>
           <table class='table table-form'>
-            <?php a($lang->mainNav);foreach($lang->mainNav as $module => $title):?>
+            <?php foreach($lang->mainNav as $module => $title):?>
             <?php if(!is_string($title)) continue;?>
             <?php
             /* Ignore null actions menus. */
@@ -134,7 +134,7 @@
                 </div>
               </th>
               <td>
-                <?php if(isset($lang->action->dynamicAction->$module)): a($lang->action->dynamicAction);?>
+                <?php if(isset($lang->action->dynamicAction->$module)):?>
                 <div class='clearfix'>
                   <?php foreach($lang->action->dynamicAction->$module as $action => $actionTitle):?>
                   <div class='action-item'>
