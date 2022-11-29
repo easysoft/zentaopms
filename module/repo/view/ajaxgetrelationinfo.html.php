39cbc4f2b5e31dd8ad79c52dbcad8dbb791bc420
caoyanyi <caoyanyi@easycorp.ltd>
2022-11-29 13:23:50 +0800

    * Modify style for repo relation.

--- module/repo/view/ajaxgetrelationinfo.html.php
+++ module/repo/view/ajaxgetrelationinfo.html.php
@@ -10,6 +10,8 @@
 ?>
 <?php
 include '../../common/view/header.lite.html.php';
+js::set('objectType', $objectType);
+js::set('objectID', $object->id);
 ?>
 <div class="main-col linkContent main">
   <div class="content pane">
@@ -18,31 +20,60 @@ include '../../common/view/header.lite.html.php';
         <table class="menu-title">
           <?php if($objectType == 'story'):?>
           <tr>
-            <td class="text text-primary"><strong><?php echo $object->title; ?></strong></td>
+            <td class="text">
+              <span class="detail-title"><?php echo $lang->story->title;?></span>
+              <strong class='text-primary'><?php echo $object->title;?></strong>
+            </td>
             <td></td>
           </tr>
           <tr>
-            <td class="text"><div class="spec-content detail-content article-content"><?php echo $object->spec; ?></div></td>
-            <td class="text right-content"><div class="detail-content article-content"><?php echo $object->verify; ?></div></td>
+            <td class="text">
+              <div class="spec-content detail-content article-content">
+                <span class="strong detail-title"><?php echo $lang->story->spec;?></span><?php echo $object->spec; ?>
+              </div>
+            </td>
+            <td class="text right-content">
+              <div class="detail-content article-content">
+                <span class="strong detail-title"><?php echo $lang->story->verify;?></span><?php echo $object->verify; ?>
+              </div>
+            </td>
           </tr>
           <?php elseif($objectType == 'task'):?>
           <tr>
-            <td class="text text-primary"><strong><?php echo $object->name; ?></strong></td>
+            <td class="text">
+              <span class="detail-title"><?php echo $lang->task->name;?></span>
+              <strong class='text-primary'><?php echo $object->name;?></strong>
+            </td>
             <td></td>
           </tr>
           <tr>
-            <td class="text"><div class="spec-content detail-content article-content"><?php echo $object->desc; ?></div></td>
-            <td class="text right-content"><div class="detail-content article-content"><?php echo $object->storyTitle; ?></div></td>
+            <td class="text">
+              <div class="spec-content detail-content article-content">
+                <div class="strong detail-title"><?php echo $lang->task->desc;?></div><?php echo $object->desc; ?>
+              </div>
+            </td>
+            <td class="text right-content">
+              <div class="detail-content article-content">
+                <div class="strong detail-title"><?php echo $lang->task->story;?></div><?php echo $object->storyTitle; ?>
+              </div>
+            </td>
           </tr>
           <?php elseif($objectType == 'bug'):?>
           <tr>
-            <td class="text text-primary"><strong><?php echo $object->title; ?></strong></td>
-            <td></td>
+            <td class="text">
+              <span class="detail-title"><?php echo $lang->bug->title;?></span>
+              <strong class='text-primary' data-id='<?php echo $object->id;?>'><?php echo $object->title;?></strong>
+            </td>
           </tr>
+          <?php if($object->steps):?>
           <tr>
-            <td class="text"><div class="spec-content detail-content article-content"><?php echo $object->steps; ?></div></td>
-            <td class="text right-content"><div class="detail-content article-content"><?php echo zget($lang->bug->typeList, $object->type);?></div></td>
+            <td class="text">
+              <div class="spec-content detail-content article-content">
+                <span class="strong detail-title"><?php echo $lang->bug->steps;?></span><?php echo $object->steps; ?>
+              </div>
+            </td>
           </tr>
+          <?php endif;?>
           <?php endif;?>
         </table>
       </div>
@@ -59,5 +90,12 @@ $(function()
         $(this).replaceWith(content);
     })
     $('blockquote a').empty();
+
+    $('.linkContent strong.text-primary').on('click',function()
+    {
+        var link = createLink(objectType, 'view', objectType + 'ID=' + objectID);
+        var app = objectType == 'bug' ? 'qa' : (objectType == 'task' ? 'execution' : 'product');
+        parent.parent.$.apps.open(link, app);
+    });
 });
 </script>
