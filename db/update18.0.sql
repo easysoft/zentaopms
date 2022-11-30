86577288b25086269ccb72dfb691098ae6bffe8d
xieqiyu <xieqiyu@easycorp.ltd>
2022-11-30 09:50:37 +0800

    * Code for task#77794. Modify sql.

--- db/update18.0.sql
+++ db/update18.0.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `zt_story` ADD COLUMN `siblings` varchar(255) NOT NULL AFTER `linkRequirements`;
 
-ALTER TABLE `zt_productplan` MODIFY COLUMN `branch` varchar(255) NOT NULL;
+ALTER TABLE `zt_productplan` MODIFY COLUMN `branch` varchar(255) NOT NULL DEFAULT '0';
