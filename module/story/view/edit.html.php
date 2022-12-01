aff72c1c262897757313ecf5c90478723d7503b4
lanzongjun <lanzongjun@easycorp.ltd>
2022-12-01 09:44:04 +0800

    * fix bug #30402

--- module/story/view/edit.html.php
+++ module/story/view/edit.html.php
@@ -192,7 +192,8 @@
                 <th><?php echo $lang->story->plan;?></th>
                 <td>
                   <div class='input-group' id='planIdBox'>
-                  <?php $multiple = ($this->session->currentProductType != 'normal' and empty($story->branch) and count($story->planTitle) > 1) ? 'multiple' : '';?>
+                  <?php $planCount = !empty($story->planTitle) ? count($story->planTitle) : 0?>
+                  <?php $multiple  = ($this->session->currentProductType != 'normal' and empty($story->branch) and $planCount > 1) ? 'multiple' : '';?>
                   <?php echo html::select(!empty($multiple) ? 'plan[]' : 'plan', $plans, $story->plan, "class='form-control chosen' " . $multiple);
                   if(count($plans) == 1)
                   {
