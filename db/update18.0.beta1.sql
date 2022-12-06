f1b869d003effd2d00eb19377937d8e2142d802b
xieqiyu <xieqiyu@easycorp.ltd>
2022-12-06 16:59:47 +0800

    * Merge SQL.

--- db/update18.0.beta1.sql
+++ db/update18.0.beta1.sql
@@ -1,1 +1,3 @@
+ALTER TABLE `zt_story` ADD COLUMN `siblings` varchar(255) NOT NULL AFTER `linkRequirements`;
+ALTER TABLE `zt_productplan` MODIFY COLUMN `branch` varchar(255) NOT NULL DEFAULT '0';
 ALTER TABLE `zt_effort` ADD `extra` text COLLATE 'utf8_general_ci' NOT NULL AFTER `end`;
