c73d589b54f1454826396138cb5ef6583a2a9368
lanzongjun <lanzongjun@easycorp.ltd>
2022-12-08 16:28:38 +0800

    * Finish Bug #30664

--- module/build/model.php
+++ module/build/model.php
@@ -457,7 +457,7 @@ class buildModel extends model
             ->get();
 
         $product = $this->loadModel('product')->getByID($build->product);
-        if(!empty($productgetBuildPairs) and $product->type != 'normal' and $this->post->isIntegrated == 'no' and !isset($_POST['branch']))
+        if(!empty($product) and $product->type != 'normal' and !isset($_POST['branch']))
         {
             $this->lang->product->branch = sprintf($this->lang->product->branch, $this->lang->product->branchName[$product->type]);
             dao::$errors['branch'] = sprintf($this->lang->error->notempty, $this->lang->product->branch);
