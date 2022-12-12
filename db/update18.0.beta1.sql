d3d10ea6a7811074ffa83a05c30d85a07f9e403d
liuhong <liuhong@easycorp.ltd>
2022-12-12 12:03:10 +0000

    * Adjust code task #79661.

--- db/update18.0.beta1.sql
+++ db/update18.0.beta1.sql
@@ -2,3 +2,5 @@ ALTER TABLE `zt_story` ADD COLUMN `twins` varchar(255) NOT NULL AFTER `linkRequi
 ALTER TABLE `zt_productplan` MODIFY COLUMN `branch` varchar(255) NOT NULL DEFAULT '0';
 ALTER TABLE `zt_effort` ADD `extra` text COLLATE 'utf8_general_ci' NOT NULL AFTER `end`;
 ALTER TABLE `zt_build` MODIFY COLUMN `branch` varchar(255) NOT NULL DEFAULT '0';
+ALTER TABLE `zt_feedback` ADD COLUMN `activatedBy` varchar(30) NOT NULL AFTER `assignedDate`;
+ALTER TABLE `zt_feedback` ADD COLUMN `activatedDate` date NOT NULL AFTER `activatedBy`;
