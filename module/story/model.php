4cf85d5ed7f495b24366a4aed9c90b0a9d4d8365
liuhong <liuhong@easycorp.ltd>
2022-12-12 07:17:47 +0000

    * Fix task #79669.

--- module/story/model.php
+++ module/story/model.php
@@ -4508,6 +4508,7 @@ class storyModel extends model
         if($action == 'activate')   return $story->status == 'closed';
         if($action == 'assignto')   return $story->status != 'closed';
         if($action == 'batchcreate' and $story->parent > 0) return false;
+        if($action == 'batchcreate' and !empty($story->twins)) return false;
         if($action == 'batchcreate' and $story->type == 'requirement' and $story->status != 'closed') return strpos('draft,reviewing,changing', $story->status) === false;
         if($action == 'batchcreate' and $config->vision == 'lite' and ($story->status == 'active' and ($story->stage == 'wait' or $story->stage == 'projected'))) return true;
         /* Adjust code, hide split entry. */
