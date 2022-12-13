9013eda03bf18b1a5903ca78b2c5151c4f2590f0
liuhong <liuhong@easycorp.ltd>
2022-12-13 02:35:04 +0000

    * Add Sql column for zt_feedback #79661.

--- db/update18.0.beta1.sql
+++ db/update18.0.beta1.sql
@@ -3,4 +3,4 @@ ALTER TABLE `zt_productplan` MODIFY COLUMN `branch` varchar(255) NOT NULL DEFAUL
 ALTER TABLE `zt_effort` ADD `extra` text COLLATE 'utf8_general_ci' NOT NULL AFTER `end`;
 ALTER TABLE `zt_build` MODIFY COLUMN `branch` varchar(255) NOT NULL DEFAULT '0';
 ALTER TABLE `zt_feedback` ADD COLUMN `activatedBy` varchar(30) NOT NULL AFTER `assignedDate`;
-ALTER TABLE `zt_feedback` ADD COLUMN `activatedDate` date NOT NULL AFTER `activatedBy`;
+ALTER TABLE `zt_feedback` ADD COLUMN `activatedDate` datetime NOT NULL AFTER `activatedBy`;
