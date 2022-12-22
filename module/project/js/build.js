0369dba360b8ec0aac4d676b2c61dc81bd0fdfe8
wangyidong <wangyidong@cnezsoft.com>
2022-12-22 19:24:49 +0800

    * adjust for execution field width for project build page.

--- module/project/js/build.js
+++ module/project/js/build.js
@@ -10,3 +10,19 @@ function changeProduct(productID)
     link = createLink('project', 'build', 'projectID=' + projectID + '&type=product&param=' + productID);
     location.href = link;
 }
+
+$(function()
+{
+    $('td .execution').each(function()
+    {
+        $deleted = $(this).find('.label-danger');
+        if($deleted.length > 0)
+        {
+            $td = $(this).closest('td');
+            if($td.width() < $(this).width())
+            {
+                $(this).find('.executionName').css('display', 'inline-block').css('width', $td.width() - $deleted.width()).css('overflow', 'hidden').css('float', 'left');
+            }
+        }
+    })
+})
