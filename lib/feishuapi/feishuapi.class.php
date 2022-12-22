435852fd144e9c1c57c1e4aaa222a0a6fda05f2e
chaideqing <chaideqing@easycorp.ltd>
2022-12-22 11:53:20 +0800

    * Fix feedback 1799 .

--- lib/feishuapi/feishuapi.class.php
+++ lib/feishuapi/feishuapi.class.php
@@ -265,6 +265,8 @@ class feishuapi
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 
             $urlHandlers[] = $ch;
             curl_multi_add_handle($curl, $ch);
