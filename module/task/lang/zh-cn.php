db2851ceae0b0f017c8ec9e8f981759174c032f9
田淑杰 <tianshujie0123@163.com>
2022-12-26 09:54:51 +0800

    * Fix bug #28021.

--- module/task/lang/zh-cn.php
+++ module/task/lang/zh-cn.php
@@ -46,6 +46,7 @@ $lang->task->export              = "导出数据";
 $lang->task->exportAction        = "导出任务";
 $lang->task->reportChart         = "报表统计";
 $lang->task->fromBug             = '来源Bug';
+$lang->task->fromBugID           = '来源Bug编号';
 $lang->task->case                = '相关用例';
 $lang->task->process             = '处理任务';
 $lang->task->confirmStoryChange  = "确认{$lang->SRCommon}变动";
