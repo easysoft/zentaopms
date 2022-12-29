9b3d6387a555c58db19f26b2d46838816f8a8a1d
孙广明 <sunguangming@cnezsoft.com>
2022-12-29 10:54:32 +0800

    * Finish task #81192.

--- module/story/control.php
+++ module/story/control.php
@@ -1909,7 +1909,6 @@ class story extends control
 
         /* Get edited stories. */
         $stories = $this->story->getByList($storyIdList);
-        $productStoryList = array();
         $productList      = array();
         $ignoreTwins      = array();
         $twinsCount       = array();
@@ -1934,7 +1933,6 @@ class story extends control
 
             $storyProduct = isset($productList[$story->product]) ? $productList[$story->product] : $this->product->getByID($story->product);
             $branch       = $storyProduct->type == 'branch' ? ($story->branch > 0 ? $story->branch : '0') : 'all';
-            if(!isset($productStoryList[$story->product][$story->branch])) $productStoryList[$story->product][$story->branch] = $this->story->getProductStoryPairs($story->product, $branch, 0, 'all', 'id_desc', 0, '', $story->type);
 
             if(!empty($story->twins))
             {
@@ -2030,7 +2028,6 @@ class story extends control
         $this->view->storyIdList      = $storyIdList;
         $this->view->storyType        = $storyType;
         $this->view->reasonList       = $this->lang->story->reasonList;
-        $this->view->productStoryList = $productStoryList;
         $this->view->twinsCount       = $twinsCount;
 
         $this->display();
@@ -3064,5 +3061,21 @@ class story extends control
 
         if(!dao::isError()) $this->loadModel('action')->create('story', $twinID, 'relieved');
         return $this->send(array('result' => 'success', 'silbingsCount' => count($twins)-1));
+    }
+
+    /**
+     * Ajax get story pairs.
+     * 
+     * @param  int    $storyID 
+     * @access public
+     * @return void
+     */
+    public function ajaxGetStoryPairs($storyID)
+    {
+        $this->app->loadLang('bug');
+        $story   = $this->story->getByID($storyID);
+        $stories = $this->story->getProductStoryPairs($story->product, $story->branch, 0, 'all', 'id_desc', 0, '', $story->type);
+
+        return print html::select("duplicateStoryIDList[$storyID]", $stories, '', "class='form-control' placeholder='{$this->lang->bug->duplicateTip}'");
     }
 }
