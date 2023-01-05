e3654d8d2b58c71929f87eab8b3e37150ac3aaf1
tianshujie <tianshujie0123@163.com>
2023-01-05 09:35:24 +0800

    * Add test of program model.

--- test/model/program/getbyid.php
+++ test/model/program/getbyid.php
@@ -12,7 +12,6 @@ $program->name->setFields(array(
     array('field' => 'name2', 'range' => '1-10')
 ));
 $program->gen(5);
-var_dump($program->fields);
 
 /**
 
@@ -22,13 +21,16 @@ pid=1
 
 通过id字段获取id=1的项目集并验证它的name。 >> 项目集1
 通过id字段获取id=1000的项目集，返回空 >> 0
+通过id字段获取id=0的项目集，返回空 >> 0
 
 */
 
 $programTester = new programTest();
 
-$program1 = $programTester->getByIdTest(1);
-$program2 = $programTester->getByIdTest(1000);
+$program1 = $programTester->getByIDTest(1);
+$program2 = $programTester->getByIDTest(1000);
+$program3 = $programTester->getByIDTest(0);
 
 r($program1) && p('name') && e('项目集1'); // 通过id字段获取id=1的项目集并验证它的name。
 r($program2) && p()       && e('0');       // 通过id字段获取id=1000的项目集，返回空
+r($program3) && p()       && e('0');       // 通过id字段获取id=0的项目集，返回空
