44cbe6c451d66d31d67ebd13c83b98900ca60ee6
hufangzhou <hufangzhou@easycorp.ltd>
2023-01-06 06:47:27 +0000

    * Modify the getgroupsbyvisions unit testcase.

--- test/model/user/getgroupsbyvisions.php
+++ test/model/user/getgroupsbyvisions.php
@@ -10,15 +10,15 @@ title=测试 userModel::getGroupsByVisions();
 cid=1
 pid=1
 
-获取系统中所有研发界面分组的数量 >> 14
+获取系统中所有研发界面分组的数量 >> 13
 获取系统中所有迅捷界面分组的数量 >> 3
 传空Visions，返回空数组 >> 0
-获取系统中所有用户分组的数量 >> 17
+获取系统中所有用户分组的数量 >> 16
 
 */
 $user = new userTest();
 
-r(count($user->getGroupsByVisionsTest(array('rnd'))))         && p() && e('14'); // 获取系统中所有研发界面分组的数量
+r(count($user->getGroupsByVisionsTest(array('rnd'))))         && p() && e('13'); // 获取系统中所有研发界面分组的数量
 r(count($user->getGroupsByVisionsTest(array('lite'))))        && p() && e('3');  // 获取系统中所有迅捷界面分组的数量
 r($user->getGroupsByVisionsTest(array()))                     && p() && e('0');  // 传空Visions，返回空数组
-r(count($user->getGroupsByVisionsTest(array('rnd','lite'))))  && p() && e('17'); // 获取系统中所有用户分组的数量
+r(count($user->getGroupsByVisionsTest(array('rnd','lite'))))  && p() && e('16'); // 获取系统中所有用户分组的数量
