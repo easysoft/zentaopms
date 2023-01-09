9e8757d42ec5f672a4a887f8c33f15a98b063f4b
田淑杰 <tianshujie0123@163.com>
2023-01-09 14:14:47 +0800

    * Add case of execution model.

--- test/model/execution/checkpriv.php
+++ test/model/execution/checkpriv.php
@@ -2,7 +2,18 @@
 <?php
 include dirname(dirname(dirname(__FILE__))) . '/lib/init.php';
 include dirname(dirname(dirname(__FILE__))) . '/class/execution.class.php';
+zdTable('user')->gen(5);
 su('admin');
+
+$execution = zdTable('project');
+$execution->id->range('1-5');
+$execution->name->range('1-5')->prefix('执行');
+$execution->type->range('sprint,stage,kanban');
+$execution->status->range('wait{3},suspended,closed,doing');
+$execution->openedBy->range('admin,user1');
+$execution->begin->range('20220112 000000:0')->type('timestamp')->format('YY/MM/DD');
+$execution->end->range('20220212 000000:0')->type('timestamp')->format('YY/MM/DD');
+$execution->gen(5);
 
 /**
 
@@ -12,20 +23,18 @@ pid=1
 
 测试传入空值 >> 0
 测试传入0 >> 0
-测试传入正确的项目ID的权限判断 >> 1
 测试传入迭代ID的权限判断 >> 1
 测试传入阶段ID的权限判断 >> 1
 测试传入看板ID的权限判断 >> 1
 
 */
 
-$executionIdList = array('', 0, 1, 101, 131, 161);
+$executionIdList = array('', 0, 1, 2, 3);
 
-$execution = new executionTest();
+$executionTester = new executionTest();
 
-r($execution->checkPrivTest($executionIdList[0])) && p() && e('0'); // 测试传入空值
-r($execution->checkPrivTest($executionIdList[1])) && p() && e('0'); // 测试传入0
-r($execution->checkPrivTest($executionIdList[2])) && p() && e('1'); // 测试传入正确的项目ID的权限判断
-r($execution->checkPrivTest($executionIdList[3])) && p() && e('1'); // 测试传入迭代ID的权限判断
-r($execution->checkPrivTest($executionIdList[4])) && p() && e('1'); // 测试传入阶段ID的权限判断
-r($execution->checkPrivTest($executionIdList[5])) && p() && e('1'); // 测试传入看板ID的权限判断
+r($executionTester->checkPrivTest($executionIdList[0])) && p() && e('0'); // 测试传入空值
+r($executionTester->checkPrivTest($executionIdList[1])) && p() && e('0'); // 测试传入0
+r($executionTester->checkPrivTest($executionIdList[2])) && p() && e('1'); // 测试传入迭代ID的权限判断
+r($executionTester->checkPrivTest($executionIdList[3])) && p() && e('1'); // 测试传入阶段ID的权限判断
+r($executionTester->checkPrivTest($executionIdList[4])) && p() && e('1'); // 测试传入看板ID的权限判断
