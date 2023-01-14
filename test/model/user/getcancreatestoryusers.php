634823e27fa15b6c4e9718a071b2023a23d7a097
liyang <liyang@easycorp.ltd>
2023-01-14 16:00:03 +0800

    * Update test for user model.

--- test/model/user/getcancreatestoryusers.php
+++ test/model/user/getcancreatestoryusers.php
@@ -2,6 +2,7 @@
 <?php
 include dirname(dirname(dirname(__FILE__))) . '/lib/init.php';
 include dirname(dirname(dirname(__FILE__))) . '/class/user.class.php';
+zdTable('user')->gen(1000);
 su('admin');
 
 /**
@@ -10,9 +11,9 @@ title=测试 userModel->getCanCreateStoryUsers();
 cid=1
 pid=1
 
-查找系统中有权限创建、批量创建需求的用户 >> A:admin;T:产品主管48
+查找系统中有权限创建、批量创建需求的用户 >> A:admin;T:研发主管48
 
 */
 
 $user = new userTest();
-r($user->getCanCreateStoryUsersTest()) && p('admin;td48') && e('A:admin;T:产品主管48'); //查找系统中有权限创建、批量创建需求的用户
+r($user->getCanCreateStoryUsersTest()) && p('admin;td48') && e('A:admin;T:研发主管48'); //查找系统中有权限创建、批量创建需求的用户
--- test/model/user/getpersonaldata.php
+++ test/model/user/getpersonaldata.php
@@ -2,6 +2,11 @@
 <?php
 include dirname(dirname(dirname(__FILE__))) . '/lib/init.php';
 include dirname(dirname(dirname(__FILE__))) . '/class/user.class.php';
+zdTable('user')->gen(500);
+zdTable('story')->gen(400);
+zdTable('case')->gen(400);
+zdTable('bug')->gen(300);
+
 su('admin');
 
 /**
@@ -10,9 +15,9 @@ title=测试 userModel::getPersonalData();
 cid=1
 pid=1
 
-获取admin用户的createdBugs数量 >> 270
+获取admin用户的createdBugs数量 >> 30
 获取test2用户createdCases数量 >> 4
-当获取一个空用户的数据时，系统默认返回当前登录用户的数据 >> 2
+当获取一个空用户的数据时，系统默认返回当前登录用户的数据 >> 3
 
 */
 $user = new userTest();
@@ -20,6 +25,6 @@ $adminData = $user->getPersonalDataTest('admin');
 $test2Data = $user->getPersonalDataTest('test2');
 $emptyData = $user->getPersonalDataTest('');
 
-r($adminData) && p('createdBugs')  && e('270'); //获取admin用户的createdBugs数量
+r($adminData) && p('createdBugs')  && e('30'); //获取admin用户的createdBugs数量
 r($test2Data) && p('createdCases') && e('4');   //获取test2用户createdCases数量
-r($emptyData) && p('createdTodos') && e('2');   //当获取一个空用户的数据时，系统默认返回当前登录用户的数据
+r($emptyData) && p('createdTodos') && e('3');   //当获取一个空用户的数据时，系统默认返回当前登录用户的数据
