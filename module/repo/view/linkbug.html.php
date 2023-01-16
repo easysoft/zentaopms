7bd4534ae3ffc0a55f9c02cb0ffd6582f5b61d92
zenggang <zenggang@easycorp.ltd>
2023-01-16 02:34:06 +0000

    * Fix bug#31514

--- module/repo/view/linkbug.html.php
+++ module/repo/view/linkbug.html.php
@@ -15,6 +15,7 @@
 <?php include '../../common/view/tablesorter.html.php';?>
 <style>
 #queryBox {padding-top: 25px;}
+#queryBox.show {min-height: 66px;}
 .search-form #bug-search .form-actions {width: 115px !important;}
 </style>
 <div class='main-content' id='mainContent'>
