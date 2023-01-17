f5e68c045e2b18d271a75e80bd7c79f6607847b3
xieqiyu <xieqiyu@easycorp.ltd>
2023-01-17 10:00:03 +0800

    * Fix bug#31708.

--- module/story/model.php
+++ module/story/model.php
@@ -352,7 +352,7 @@ class storyModel extends model
                 if($executionID != 0)
                 {
                     $this->linkStory($executionID, $this->post->product, $storyID);
-                    if($this->config->systemMode == 'new' and $executionID != $this->session->project) $this->linkStory($this->session->project, $this->post->product, $storyID);
+                    if($this->config->systemMode == 'ALM' and $executionID != $this->session->project) $this->linkStory($this->session->project, $this->post->product, $storyID);
 
                     $this->loadModel('kanban');
 
