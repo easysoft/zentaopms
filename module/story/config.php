dec6df72b245210497e43d9a6cafa96247e1ab75
tianshujie <tianshujie0123@163.com>
2023-01-29 16:15:06 +0800

    * Fix bug #25848.

--- module/story/config.php
+++ module/story/config.php
@@ -254,5 +254,5 @@ $config->story->datatable->fieldList['caseCount']['name']     = $lang->story->ca
 
 $config->story->datatable->fieldList['actions']['title']    = 'actions';
 $config->story->datatable->fieldList['actions']['fixed']    = 'right';
-$config->story->datatable->fieldList['actions']['width']    = $app->tab == 'project' ? '220' : '215';
+$config->story->datatable->fieldList['actions']['width']    = $app->tab == 'project' ? '230' : '220';
 $config->story->datatable->fieldList['actions']['required'] = 'yes';
