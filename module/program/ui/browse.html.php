4c64d874c3f3e2cc4beebebcd473f96e1e97f512
Hao Sun <sunhao@easycorp.ltd>
2023-01-30 18:00:41 +0800

    * zin: change example page

--- module/program/ui/browse.html.php
+++ module/program/ui/browse.html.php
@@ -3,16 +3,27 @@ namespace zin;
 
 page
 (
-    set('title', $title),
-    h('h1', 'hello'),
-    button('BUTTON'),
-    btn('Primary')->primary()->rounded(),
+    set    ('title', $title),
+    h      ('h1', 'hello2')->setClass('text-danger'),
+    btn
+    (
+        'Primary',
+        set('.', 'primary'),
+        set('active', true),
+        set('icon', 'flag'),
+    ),
+    icon('project'),
     div
     (
-        setClass('primary-pale'),
-        h2('Headings2'),
-        h3('Headings3'),
-        html('<div>test</div>'),
-        p('lorem', strong('bold'))
+        icon('star'),
+        setClass ('primary-pale'),
+        h2       ('Headings2'),
+        h3       ('Headings3'),
+        html     ('<div>test</div>'),
+        p
+        (
+            'lorem',
+            strong('bold'),
+        )
     )
-)->x();
+);
