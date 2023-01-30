7ecab32294f7c44517490e33d1338b3470ff24ce
Hao Sun <sunhao@easycorp.ltd>
2023-01-30 18:02:51 +0800

    * zin: refactor page

--- zin/wg/page/v1.php
+++ zin/wg/page/v1.php
@@ -6,11 +6,11 @@ require_once dirname(__DIR__) . DS . 'pagebase' . DS . 'v1.php';
 
 class page extends pagebase
 {
-    static $customProps = 'metas,title,bodyProps,zui';
+    static $defaultProps = array('zui' => true);
 
     public function init()
     {
-        $this->setDefaultProps(array('zui' => true));
+        // $this->setDefaultProps(array('zui' => true));
 
         parent::init();
     }
