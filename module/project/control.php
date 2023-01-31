6c97562b576674d85b8b9b05b0b75d3ce2a882c9
Yagami <sunguangming@easycorp.ltd>
2023-01-31 09:49:58 +0800

    * Code for agile plus project.

--- module/project/control.php
+++ module/project/control.php
@@ -873,7 +873,7 @@ class project extends control
             $this->view->programList = $this->loadModel('program')->getPairsByList($programList);
         }
 
-        if(empty($project) || strpos('scrum,waterfall,kanban', $project->model) === false)
+        if(empty($project) || strpos('scrum,waterfall,kanban,agileplus', $project->model) === false)
         {
             if(defined('RUN_MODE') && RUN_MODE == 'api') return $this->send(array('status' => 'fail', 'code' => 404, 'message' => '404 Not found'));
             return print(js::error($this->lang->notFound) . js::locate($this->createLink('project', 'browse')));
