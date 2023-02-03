1f1e8b0836fdaa8a688fe88651be1da517ab2692
wangyidong <wangyidong@cnezsoft.com>
2023-02-03 08:48:18 +0800

    * fix bug #31519.

--- module/project/js/build.js
+++ module/project/js/build.js
@@ -16,10 +16,20 @@ $(function()
     $('td .execution .label-danger').each(function()
     {
         $execution = $(this).closest('.execution');
-        $td = $(this).closest('td');
+        $td        = $(this).closest('td');
         if($td.width() < $execution.width())
         {
             $execution.find('.executionName').css('display', 'inline-block').css('width', $td.width() - $(this).width()).css('overflow', 'hidden').css('float', 'left');
+        }
+    })
+
+    $('td.c-name .build .icon-code-fork').each(function()
+    {
+        $build = $(this).closest('.build');
+        $td    = $(this).closest('td');
+        if($td.width() < $build.width())
+        {
+            $build.find('.buildName').css('display', 'inline-block').css('width', $td.width() - $(this).width()).css('overflow', 'hidden').css('float', 'left');
         }
     })
 })
