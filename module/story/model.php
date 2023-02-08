534b5e64714edc5a9abbe8a2da6f712ad6048082
liuhong <liuhong@easycorp.ltd>
2023-02-08 08:25:27 +0000

    * Adjust code for ticket #326

--- module/story/model.php
+++ module/story/model.php
@@ -1247,7 +1247,7 @@ class storyModel extends model
         if(empty($childrenStatus)) return $this->dao->update(TABLE_STORY)->set('parent')->eq('0')->where('id')->eq($parentID)->exec();
 
         $status = $oldParentStory->status;
-        if(count($childrenStatus) == 1 and $oldParentStory->status != 'reviewing')
+        if(count($childrenStatus) == 1)
         {
             $status = current($childrenStatus);
             if($status == 'draft' or $status == 'changing') $status = 'active';
