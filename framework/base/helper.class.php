55c9922a49341972feb228373e7cb0049bacf1d2
zhaoke <zhaoke@easycorp.ltd>
2023-02-09 09:14:17 +0000

    * Fix error for php5.3.

--- framework/base/helper.class.php
+++ framework/base/helper.class.php
@@ -998,3 +998,18 @@ if(!function_exists('array_column'))
         return $output;
     }
 }
+
+if (!function_exists('getallheaders')) {
+    function getallheaders()
+    {
+        $headers = array();
+        foreach ($_SERVER as $name => $value) 
+        {
+            if (substr($name, 0, 5) == 'HTTP_')
+            {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
