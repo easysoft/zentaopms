0d8410cdca900e3ddbb119200bd3f8181d56d760
hufangzhou <hufangzhou@easycorp.ltd>
2023-02-09 06:05:51 +0000

    * Change update sql.

--- db/zentaopms284.sql
+++ db/zentaopms284.sql
@@ -3,4 +3,4 @@ update `zt_task` set `assignedTo` = '' where `mode` = 'multi' and `status` != 'd
 REPLACE INTO `zt_lang` (`lang`, `module`, `section`, `key`, `value`, `system`, `vision`) SELECT `lang`, `module`, 'scrumClassify', `key`, `value`, `system`, `vision` FROM `zt_lang` WHERE `module` = 'process' and `section` = 'classify' ORDER BY id ASC;
 REPLACE INTO `zt_lang` (`lang`, `module`, `section`, `key`, `value`, `system`, `vision`) SELECT `lang`, `module`, 'agileplusClassify', `key`, `value`, `system`, `vision` FROM `zt_lang` WHERE `module` = 'process' and `section` = 'classify' ORDER BY id ASC;
 
-UPDATE `zt_project` AS child INNER JOIN (select `id`,`attribute` from `zt_project` where `parent` != 0 and `type` = 'stage') AS parent ON child.`parent` = parent.`id` SET child.`attribute`=parent.`attribute`;
+UPDATE `zt_project` AS parent INNER JOIN (select `id`,`parent`,`attribute` from `zt_project` where `parent` != 0 and `type` = 'stage') AS child ON parent.`id` = child.`parent` SET parent.`attribute`='mix' where parent.`grade`=1 and parent.`type`='stage' and parent.`attribute` != child.`attribute`;
