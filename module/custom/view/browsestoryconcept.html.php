5263854da6ce61fbe9824b71636a043cebe943ac
guofeilong <guofeilong@easycorp.ltd>
2023-02-09 14:21:55 +0800

    * Code for delete box-shadow .

--- module/custom/view/browsestoryconcept.html.php
+++ module/custom/view/browsestoryconcept.html.php
@@ -21,7 +21,7 @@
         <?php if(common::hasPriv('custom', 'setstoryconcept')) echo html::a($this->createLink('custom', 'setstoryconcept', '', '', true), $lang->custom->setStoryConcept, '', "class='btn btn-primary iframe' data-width=" . ($this->config->URAndSR ? "590px" : "405px"));?>
       </div>
     </div>
-    <table class='table table-form'>
+    <table class='table table-form box-shadow-none'>
       <thead>
         <tr>
           <?php if(common::hasPriv('custom', 'setDefaultConcept')):?>
