b5f18edb351038df23c2ce061b2055ade309f40a
guofeilong <guofeilong@easycorp.ltd>
2023-02-10 10:46:00 +0800

    * Code for task #748

--- module/mail/view/detect.html.php
+++ module/mail/view/detect.html.php
@@ -12,13 +12,7 @@
 ?>
 <?php include $this->app->getModuleRoot() . 'message/view/header.html.php';?>
 <div id='mainContent' class='main-content'>
-  <div class='center-block mw-700px'>
-    <div class='main-header'>
-      <h2>
-        <?php echo $lang->mail->common;?>
-        <small class='text-muted'> <?php echo $lang->arrow . $lang->mail->detect;?></small>
-      </h2>
-    </div>
+  <div class='mw-700px'>
     <form class='pdt-20' method='post' target='hiddenwin'>
       <table class='table table-form'>
         <tr><th style='width:140px'><?php echo $lang->mail->inputFromEmail; ?></th><td class='w-p50'><?php echo html::input('fromAddress', $fromAddress, "class='form-control'");?></td><td><?php echo html::submitButton($lang->mail->nextStep, '', 'btn btn-primary');?></td></tr>
