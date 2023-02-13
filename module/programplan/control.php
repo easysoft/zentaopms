a097e0ca5279bd43fb9ecd9535a46003c20dbe8d
tianshujie <tianshujie0123@163.com>
2023-02-13 14:33:35 +0800

    * Merge master.

--- module/programplan/control.php
+++ module/programplan/control.php
@@ -253,6 +253,7 @@ class programplan extends control
         $this->view->parentStageList    = $this->programplan->getParentStageList($this->session->project, $planID, $plan->product);
         $this->view->enableOptionalAttr = (empty($parentStage) or (!empty($parentStage) and $parentStage->attribute == 'mix'));
         $this->view->isTopStage         = $this->programplan->checkTopStage($planID);
+        $this->view->PMUsers            = $this->loadModel('user')->getPairs('noclosed|nodeleted|pmfirst',  $plan->PM);
 
         $this->display();
     }
