ecabeac970ec29c6031c81dbfae126ad745c8322
Hao Sun <sunhao@easycorp.ltd>
2023-02-17 15:36:45 +0800

    * zin: refactor dtable with zui

--- zin/wg/dtable/v1.php
+++ zin/wg/dtable/v1.php
@@ -4,19 +4,8 @@ namespace zin;
 
 class dtable extends wg
 {
-    protected static $defineProps = 'data,height,plugins,responsive';
-
     protected function build()
     {
-        $id = $this->prop('id');
-        $options = json_encode($this->props->skip('id'));
-        return h::div(
-            setId($id),
-            h::js(<<<END
-            var options = $options;
-            console.log(options);
-            const dtable = new zui.DTable('#$id', options);
-            END)
-        );
+        return zui::dtable(inherit($this));
     }
 }
