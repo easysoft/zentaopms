72b51603f47e4cee166bd01d35031d1d40d11eb3
wangyidong <wangyidong@cnezsoft.com>
2023-02-17 09:00:46 +0800

    * fix bug #32214.

--- xuanxuan/extension/xuan/conference/ext/view/admin.html.php
+++ xuanxuan/extension/xuan/conference/ext/view/admin.html.php
@@ -24,12 +24,12 @@
   </div>
   <div class="tab-content">
     <div class="tab-pane fade <?php echo $type == 'server' || $type == 'edit' ? 'active in' : '';?>" id="serverContent">
-      <form method='post' id='conference-admin-form' class='form-ajax<?php if($enabled) echo ' conference-enabled';?><?php if(!empty($backendType)) echo " $backendType-selected";?>'>
-        <div class='main-header'>
-          <div class='heading'>
-            <strong><?php echo $lang->conference->server?></strong>
-          </div>
+      <div class='main-header'>
+        <div class='heading'>
+          <strong><?php echo $lang->conference->server?></strong>
         </div>
+      </div>
+      <form method='post' id='conference-admin-form' class='form-ajax<?php if($enabled) echo ' conference-enabled';?><?php if(!empty($backendType)) echo " $backendType-selected";?>'>
         <table class='table table-form'>
           <tr>
             <th class="w-150px"><?php echo $lang->conference->enabled;?></th>
@@ -56,7 +56,7 @@
                 <?php echo html::radio('backendType', $lang->conference->backend->types, $backendType);?>
               <?php else: echo html::radio('backendType', $lang->conference->backend->types, $backendType, 'disabled'); endif; ?>
             </td>
-            <td><?php echo $lang->conference->backendTypeTip;?></td>
+            <td></td>
           </tr>
           <tr class='edit-row common-row'>
             <th class="w-120px"><?php echo $lang->conference->serverAddr;?></th>
@@ -68,17 +68,17 @@
             </td>
             <td><?php echo $lang->conference->serverAddrTip;?></td>
           </tr>
-          <tr class='edit-row common-row'>
+          <tr class='edit-row srs-row'>
             <th class="w-120px"><?php echo $lang->conference->https;?></th>
             <td class="w-400px">
               <?php if($type != 'edit'): ?>
               <div class="checkbox-primary disabled <?php if($https) echo 'checked';?>">
-                <label><?php echo $lang->conference->httpsTip;?></label>
+                <label id="httpsTip"><?php echo $lang->conference->httpsTip;?></label>
               </div>
               <?php else: ?>
               <div class="checkbox-primary">
                 <input type="checkbox" name="https" id='https' value="true" <?php if($https) echo 'checked';?> <?php if($type != 'edit') echo 'disabled';?>>
-                <label for='https'><?php echo $lang->conference->httpsTip;?></label>
+                <label id="httpsTip" for='https'><?php echo $lang->conference->httpsTip;?></label>
               </div>
               <?php endif; ?>
             </td>
@@ -89,10 +89,10 @@
             <td class="w-400px code">
               <?php if($type == 'edit'):?>
                 <div class='required required-wrapper'></div>
-                <input type="number" name="apiPort" id="apiPort" <?php echo empty($apiPort) ? '' : "value='$apiPort'";?> min="1" max="65535" class="form-control">
+                <input type="number" name="apiPort" id="apiPort" <?php echo empty($apiPort) ? "value='3004'" : "value='$apiPort'";?> min="1" max="65535" class="form-control">
               <?php else: echo empty($apiPort) ? $lang->conference->notset : $apiPort; endif; ?>
             </td>
-            <td><?php echo $type == 'edit' ? $lang->conference->apiPortTip : '';?></td>
+            <td id="apiPortTip"><?php echo $type == 'edit' ? ($backendType == 'srs' ? $lang->conference->apiPortSrsTip : $lang->conference->apiPortOwtTip) : '';?></td>
           </tr>
           <tr class='edit-row owt-row'>
             <th class="w-120px"><?php echo $lang->conference->mgmtPort;?></th>
@@ -143,15 +143,30 @@
             </td>
           </tr>
         </table>
+        <div id="setupTip">
+          <h3><?php echo $lang->conference->setupTitle;?></h3>
+          <p><?php echo $lang->conference->setupDescription;?></p>
+          <a href="https://www.xuanim.com/page/download.html" target="_blank"><?php echo $lang->conference->download;?></a>
+          <hr>
+          <h4><?php echo $lang->conference->srsSetupTitle;?></h4>
+          <a href="https://www.xuanim.com/book/xuanxuanserver/273.html" target="_blank"><?php echo $lang->conference->setupDoc;?></a>
+          <br>
+          <a href="https://www.xuanim.com/book/xxbservice/274.html" target="_blank"><?php echo $lang->conference->configDoc;?></a>
+          <hr>
+          <h4><?php echo $lang->conference->owtSetupTitle;?></h4>
+          <a href="https://www.xuanim.com/book/xuanxuanserver/237.html" target="_blank"><?php echo $lang->conference->setupDoc;?></a>
+          <br>
+          <a href="https://www.xuanim.com/book/xxbservice/239.html" target="_blank"><?php echo $lang->conference->configDoc;?></a>
+        </div>
       </form>
     </div>
     <div class="tab-pane fade <?php echo $type == 'video' ? 'active in' : '';?>" id="videoContent">
-      <form method='post' id='ajaxForm' class='form-ajax' action=<?php echo $this->createLink("conference", 'admin', 'type=video');?>>
-        <div class='main-header'>
-          <div class='heading'>
-            <strong><?php echo $lang->conference->video?></strong>
-          </div>
+      <div class='main-header'>
+        <div class='heading'>
+          <strong><?php echo $lang->conference->video?></strong>
         </div>
+      </div>
+      <form method='post' id='ajaxForm' class='form-ajax' action=<?php echo $this->createLink("conference", 'admin', 'type=video');?>>
         <table class='table table-form'>
           <tr>
             <th class="w-120px"><?php echo $lang->conference->resolutionWidth;?></th>
@@ -189,6 +204,8 @@
 <script>
 $(function()
 {
+    let owtPort = '3004';
+    let srsPort = '1985';
     $('.side-col .nav a[data-toggle=tab]').click(function()
     {
         $(this).parent().find('a').removeClass('active');
@@ -205,11 +222,19 @@ $(function()
         {
             $('#conference-admin-form').removeClass('srs-selected');
             $('#conference-admin-form').addClass('owt-selected');
+            document.getElementById('https').checked = true;
+            document.getElementById('apiPortTip').innerHTML = '<?php echo $lang->conference->apiPortOwtTip;?>';
+            srsPort = document.getElementById('apiPort').value;
+            document.getElementById('apiPort').value = owtPort;
         }
         if(e.target.value == 'srs')
         {
             $('#conference-admin-form').removeClass('owt-selected');
             $('#conference-admin-form').addClass('srs-selected');
+            document.getElementById('https').checked = false;
+            document.getElementById('apiPortTip').innerHTML = '<?php echo $lang->conference->apiPortSrsTip;?>';
+            owtPort = document.getElementById('apiPort').value;
+            document.getElementById('apiPort').value = srsPort;
         }
     });
 });
