42ed5661d6008b18bf4508e5a67aa79aafa8e8c6
dingyongliang <dingyongliang@easycorp.ltd>
2023-02-20 13:16:20 +0800

    * zin: refactor checkbox.

--- zin/wg/checkbox/v1.php
+++ zin/wg/checkbox/v1.php
@@ -5,16 +5,24 @@ class checkbox extends wg
 {
     protected static $defineProps = 'text?:string,checked?:bool';
 
+    public function onAddChild($child)
+    {
+        if(is_string($child) && !$this->props->has('text'))
+        {
+            $this->props->set('text', $child);
+            return false;
+        }
+    }
+
     protected function build()
     {
+        $input = h::checkbox(set($this->props->skip(array_keys(static::getDefinedProps()))));
+        if($this->prop('checked')) $input->setProp('checked', true);
+
         return h::label
         (
             setClass('checkbox'),
-            h::checkbox
-            (
-                set('checked', $this->prop('checked')),
-                set($this->props->skip(array_keys(static::getDefinedProps()))),
-            ),
+            $input,
             $this->prop('text')
         );
     }
