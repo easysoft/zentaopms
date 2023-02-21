f3800e9e3838841e05096076ac90dbec31fabab0
hufangzhou <hufangzhou@easycorp.ltd>
2023-02-21 01:38:01 +0000

    * Code for task #85049.

--- module/project/control.php
+++ module/project/control.php
@@ -167,10 +167,16 @@ class project extends control
     public function ajaxGetCopyProjects()
     {
         $data = fixer::input('post')->get();
+
+        $model = $data->model;
+        if($data->model == 'agileplus')     $model = array('scrum', 'agileplus');
+        if($data->model == 'waterfallplus') $model = array('waterfall', 'waterfallplus');
+
         $projectPairs = $this->dao->select('id, name')->from(TABLE_PROJECT)
             ->where('type')->eq('project')
             ->andWhere('deleted')->eq(0)
             ->andWhere('vision')->eq($this->config->vision)
+            ->andWhere('model')->in($model)
             ->beginIF(!$this->app->user->admin)->andWhere('id')->in($this->app->user->view->projects)->fi()
             ->beginIF(trim($data->name))->andWhere('name')->like("%$data->name%")->fi()
             ->fetchPairs();
