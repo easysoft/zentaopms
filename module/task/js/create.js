87b8f8f9b7a8f9cadc8645ef64acdfab7b65266c
tianshujie <tianshujie0123@163.com>
2023-02-28 16:53:39 +0800

    * Code for fix bug #32506.

--- module/task/js/create.js
+++ module/task/js/create.js
@@ -1,5 +1,40 @@
 $(function()
 {
+    if(attribute == 'request' || attribute == 'review')
+    {
+        $('#story').closest('tr').hide();
+        $("input[name='after'][value='toStoryList']").parent().hide();
+        $("input[name='after'][value='continueAdding']").parent().hide();
+        $("input[name='after'][value='toTaskList']").prop('checked', true);
+        $("input[name='after'][value='toTaskList']").parent().css('margin-left', '0px');
+    }
+
+    execAttribute = attribute;
+    $('#execution').change(function()
+    {
+        link = createLink('execution', 'ajaxGetAttribute', "executionID=" + $('#execution').val());
+        $.get(link, function(attribute)
+        {
+            execAttribute = attribute;
+            if(attribute == 'request' || attribute == 'review')
+            {
+                $('#story').closest('tr').hide();
+                $("input[name='after'][value='toStoryList']").parent().hide();
+                $("input[name='after'][value='continueAdding']").parent().hide();
+                $("input[name='after'][value='toTaskList']").prop('checked', true);
+                $("input[name='after'][value='toTaskList']").parent().css('margin-left', '0px');
+            }
+            else
+            {
+                $('#story').closest('tr').show();
+                $("input[name='after'][value='toStoryList']").parent().show();
+                $("input[name='after'][value='continueAdding']").parent().show();
+                $("input[name='after'][value='continueAdding']").prop('checked', true);
+                $("input[name='after'][value='toTaskList']").parent().css('margin-left', '10px');
+            }
+        });
+    })
+
     $('#customField').click(function()
     {
         hiddenRequireFields();
