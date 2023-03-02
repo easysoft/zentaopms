846129297dc3f24470ea441457049653c20d8c72
liuyongkai <liuyongkai@easycorp.ltd>
2023-03-02 17:32:47 +0800

    * Fix bug #32374.

--- module/project/model.php
+++ module/project/model.php
@@ -1447,7 +1447,7 @@ class projectModel extends model
 
                 $this->dao->insert(TABLE_PRODUCT)->data($product)->exec();
                 $productID = $this->dao->lastInsertId();
-                $this->loadModel('personnel')->updateWhitelist($whitelist, 'product', $productID);
+                if(!$project->hasProduct) $this->loadModel('personnel')->updateWhitelist($whitelist, 'product', $productID);
                 $this->loadModel('action')->create('product', $productID, 'opened');
                 $this->dao->update(TABLE_PRODUCT)->set('`order`')->eq($productID * 5)->where('id')->eq($productID)->exec();
                 if($product->acl != 'open') $this->loadModel('user')->updateUserView($productID, 'product');
