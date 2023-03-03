0420fdee09272e5e9d42b4f16dadae9f1ccd634c
wangyuting2 <851424971@qq.com>
2023-03-03 14:10:24 +0800

    * Upgrade to new goview.

---
+++ www/static/js/javascript-c7e5956c.js
@@ -0,0 +1,6 @@
+import{conf as t,language as e}from"./typescript-9a6e50ce.js";import"./index-7e31d2b2.js";import"./index.js";import"./editorWorker-a0599278.js";import"./storage-6178a491.js";import"./icon-f0dc0744.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var l=t,c={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{l as conf,c as language};
