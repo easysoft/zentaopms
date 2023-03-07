fc5b256feb542f8d55dbe8b4a3ab3bfb3dfa2c3d
guofeilong <guofeilong@easycorp.ltd>
2023-03-07 17:04:02 +0800

    * Code for tree in permissionedit .

--- module/group/js/permissionedit.js
+++ module/group/js/permissionedit.js
@@ -1,6 +1,34 @@
 $(function()
 {
-    $(".panel-list").on('mouseover', ".priv-item", function()
+    function initRecomendTree(data)
+    {
+        $(".menuTree.depend").tree(
+        {
+            data: data,
+            initialState: 'active',
+            itemCreator: function($li, item)
+            {
+                $li.append('<a class="priv-item" data-has-children="' + (item.children ? !!item.children.length : false) + '" href=# title="' + item.title + '">' + item.title + (item.children ? '' : '<i class="icon icon-close hidden"></i>') +  '</a>');
+                if (item.active) $li.addClass('active open in');
+            }
+        });
+    };
+
+    function initDependTree(data)
+    {
+        $(".menuTree.recommend").tree(
+        {
+            data: data,
+            initialState: 'active',
+            itemCreator:  function($li, item)
+            {
+                $li.append('<a class="priv-item" data-has-children="' + (item.children ? !!item.children.length : false) + '" href=# title="' + item.title + '">' + item.title + (item.children ? '' : '<i class="icon icon-close hidden"></i>') + '</a>');
+                if (item.active) $li.addClass('active open in');
+            }
+        });
+    };
+
+    $(".menuTree").on('mouseover', ".priv-item", function()
     {
         $(this).addClass('text-primary');
         $(this).find('.icon').removeClass('hidden');
@@ -8,5 +36,31 @@ $(function()
     {
         $(this).removeClass('text-primary');
         $(this).find('.icon').addClass('hidden');
-    })
+    });
+
+    var data = [
+      {
+          title: '地盘',
+          children: [
+              {title: '地盘仪表盘'},
+              {title: '我的项目'},
+              {title: '个性化设置'},
+              {title: '维护联系人'},
+              {title: '我的日程'},
+              {title: '地盘仪表盘'},
+              {title: '我的项目'},
+              {title: '个性化设置'},
+              {title: '维护联系人'},
+              {title: '我的日程'},
+              {title: '地盘仪表盘'},
+              {title: '我的项目'},
+              {title: '个性化设置'},
+              {title: '维护联系人'},
+              {title: '我的日程'},
+          ]
+      }
+    ];
+    initRecomendTree(data);
+    initDependTree(data);
+    $('li.has-list > ul').addClass("menu-active-primary menu-hover-primary");
 });
--- module/group/view/permissionedit.html.php
+++ module/group/view/permissionedit.html.php
@@ -18,8 +18,8 @@
   </div>
   <div class="btn-toolbar">
     <div class="btn-group">
-    <?php echo html::a($this->createLink('', '', '', '', false), '<i class="icon icon-list"></i>', '', 'class="btn"');?>
-    <?php echo html::a($this->createLink('', '', '', '', false), '<i class="icon icon-cards-view"></i>', '', 'class="btn"');?>
+    <?php echo html::a($this->createLink('', '', '', '', false), '<i class="icon icon-cards-view"></i>', '', 'class="btn btn-icon text-primary"');?>
+    <?php echo html::a($this->createLink('', '', '', '', false), '<i class="icon icon-list"></i>', '', 'class="btn btn-icon text-primary"');?>
     </div>
     <a href="#" class="btn btn-primary"><?php echo $lang->group->addPriv?></a>
     <?php if(common::hasPriv('group', 'managePrivPackage')) echo html::a($this->createLink('group', 'managePrivPackage', ''), $lang->group->managePrivPackage, '', 'class="btn btn-primary"');?>
@@ -44,26 +44,18 @@
     <div class="priv-panel">
       <div class="panel-title">依赖的权限</div>
       <div class="panel-content">
-        <div class="priv-title"><i class="icon icon-caret-right"></i>地盘</div>
-        <div class="panel-list">
-          <div class="priv-item"> 地盘仪表盘 <i class="icon icon-close hidden"></i></div>
-          <div class="priv-item"> 地盘仪表盘 <i class="icon icon-close hidden"></i></div>
-          <div class="priv-item"> 我的项目 <i class="icon icon-close hidden"></i></div>
-          <div class="priv-item"> 我的项目 <i class="icon icon-close hidden"></i></div>
-        </div>
+        <div class="menuTree depend menu-active-primary menu-hover-primary"></div>
+      </div>
+      <div class="panel-bottom"> 
         <?php echo html::commonButton ('<i class="icon icon-plus"></i>' . $lang->group->add, '', 'btn btn-primary');?>
       </div>
     </div>
     <div class="priv-panel mt-m">
-      <div class="panel-title">依赖的权限</div>
+      <div class="panel-title">推荐的权限</div>
       <div class="panel-content">
-        <div class="priv-title"><i class="icon icon-caret-right"></i>地盘</div>
-        <div class="panel-list">
-          <div class="priv-item"> 地盘仪表盘 <i class="icon icon-close hidden"></i></div>
-          <div class="priv-item"> 地盘仪表盘 <i class="icon icon-close hidden"></i></div>
-          <div class="priv-item"> 我的项目 <i class="icon icon-close hidden"></i></div>
-          <div class="priv-item"> 我的项目 <i class="icon icon-close hidden"></i></div>
-        </div>
+        <div class="menuTree recommend menu-active-primary menu-hover-primary"></div>
+      </div>
+      <div class="panel-bottom"> 
         <?php echo html::commonButton ('<i class="icon icon-plus"></i>' . $lang->group->add, '', 'btn btn-primary');?>
       </div>
     </div>
