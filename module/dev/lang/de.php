7dcb0300df76a3d7ecf8ed18116837836554e466
tianshujie <tianshujie0123@163.com>
2023-03-13 09:34:44 +0800

    * Fix bug 32826.

--- module/dev/lang/de.php
+++ module/dev/lang/de.php
@@ -38,6 +38,9 @@ $lang->dev->fields['type']   = 'Typ';
 $lang->dev->fields['length'] = 'Länge';
 $lang->dev->fields['null']   = 'Null';
 
+$lang->dev->switchList['1'] = 'On';
+$lang->dev->switchList['0'] = 'Off';
+
 $lang->dev->tableList = array();
 $lang->dev->tableList['action']                = 'Aktion';
 $lang->dev->tableList['bug']                   = 'Bug';
