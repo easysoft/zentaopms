0c4f0775ac8cb8cc397319c764134da50b99f911
chaideqing <chaideqing@easycorp.ltd>
2023-03-13 14:42:38 +0800

    * Make unit test.

--- test/class/execution.class.php
+++ test/class/execution.class.php
@@ -2846,4 +2846,17 @@ class executionTest
         }
         return $executionIDList;
     }
+
+    /**
+     * Get user custom workflow field.
+     *
+     * @param  string  $moduleName
+     * @access public
+     * @return array
+     */
+    public function getExtendFieldsTest($moduleName = 'project')
+    {
+        $extendFields = $this->executionModel->getExtendFields($moduleName);
+        return $extendFields;
+    }
 }
