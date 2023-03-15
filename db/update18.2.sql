1e882e4a9cca2bcd55926ac6e5be6208c7a25ea5
宋辰轩 <songchenxuan@easycorp.ltd>
2023-03-15 17:02:16 +0800

    * Remove sql to 18-3.

--- db/update18.2.sql
+++ db/update18.2.sql
@@ -1,4 +1,3 @@
 ALTER TABLE `zt_lang` MODIFY COLUMN `section` varchar(50) NOT NULL;
 DELETE FROM `zt_grouppriv` WHERE `module` = 'dev' AND `method` = 'editor';
 REPLACE INTO `zt_config` (`owner`, `module`, `section`, `key`, `value`) VALUES ('system','common','','setPercent','1');
-DROP TABLE IF EXISTS `zt_dimension`;
