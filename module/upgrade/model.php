4933578964fa0ae12615c467320c45e895d83931
caoyanyi <caoyanyi@easycorp.ltd>
2023-03-17 14:06:58 +0800

    * Replace self to static.

--- module/upgrade/model.php
+++ module/upgrade/model.php
@@ -2028,7 +2028,7 @@ class upgradeModel extends model
             $sqlToLower = strtolower($sql);
             if(strpos($sqlToLower, 'fulltext') !== false and strpos($sqlToLower, 'innodb') !== false and $mysqlVersion < 5.6)
             {
-                self::$errors[] = $this->lang->install->errorEngineInnodb;
+                static::$errors[] = $this->lang->install->errorEngineInnodb;
                 return false;
             }
 
@@ -2045,7 +2045,7 @@ class upgradeModel extends model
                 $this->saveLogs($e->getMessage());
                 $errorInfo = $e->errorInfo;
                 $errorCode = $errorInfo[1];
-                if(strpos($ignoreCode, "|$errorCode|") === false) self::$errors[] = $e->getMessage() . "<p>The sql is: $sql</p>";
+                if(strpos($ignoreCode, "|$errorCode|") === false) static::$errors[] = $e->getMessage() . "<p>The sql is: $sql</p>";
             }
         }
     }
@@ -3260,7 +3260,7 @@ class upgradeModel extends model
      */
     public function isError()
     {
-        return !empty(self::$errors);
+        return !empty(static::$errors);
     }
 
     /**
@@ -3271,8 +3271,8 @@ class upgradeModel extends model
      */
     public function getError()
     {
-        $errors = self::$errors;
-        self::$errors = array();
+        $errors = static::$errors;
+        static::$errors = array();
         return $errors;
     }
 
