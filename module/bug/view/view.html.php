214602e4d745524d9ee3b2d6b9059ccc1f5eb2b2
xieqiyu <xieqiyu@easycorp.ltd>
2023-03-20 07:48:13 +0000

    * Fix bug#26428.

--- module/bug/view/view.html.php
+++ module/bug/view/view.html.php
@@ -192,7 +192,10 @@
                 </tr>
                 <tr>
                   <th><?php echo $lang->bug->pri;?></th>
-                  <td><span class='label-pri <?php echo 'label-pri-' . $bug->pri;?>' title='<?php echo zget($lang->bug->priList, $bug->pri);?>'><?php echo zget($lang->bug->priList, $bug->pri)?></span></td>
+                  <td>
+                    <?php if($bug->pri):?>
+                    <span class='label-pri <?php echo 'label-pri-' . $bug->pri;?>' title='<?php echo zget($lang->bug->priList, $bug->pri);?>'><?php echo zget($lang->bug->priList, $bug->pri)?></span></td>
+                    <?php endif;?>
                 </tr>
                 <tr>
                   <th><?php echo $lang->bug->status;?></th>
