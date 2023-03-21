8c7414226ce0af5e7dc67489c8b63f9f55fafeb8
zhaoke <yuaiwuhen@hotmail.com>
2023-03-21 10:14:12 +0800

    * Fix bug #33114.

--- module/zanode/model.php
+++ module/zanode/model.php
@@ -631,7 +631,7 @@ class zanodemodel extends model
 
                 if($host->status != 'online' || time() - strtotime($host->heartbeat) > 60)
                 {
-                    $l->status = self::STATUS_SHUTOFF;
+                    $l->status = $l->hostType == '' ? 'wait' : 'offline';
                     continue;
                 }
 
@@ -641,6 +641,7 @@ class zanodemodel extends model
                 }
             }
         }
+        var_dump($list);die;
         return $list;
     }
 
