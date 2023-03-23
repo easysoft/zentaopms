23caed765a52b8af5c17c6a576b53edbe50f48a0
zhaoke <yuaiwuhen@hotmail.com>
2023-03-23 08:37:35 +0800

    * Fix case version error in ciresult api.

--- module/testtask/model.php
+++ module/testtask/model.php
@@ -1888,11 +1888,13 @@ class testtaskModel extends model
 
                     $caseID  = $case->id;
                     $oldCase = $this->dao->select('*')->from(TABLE_CASE)->where('id')->eq($caseID)->fetch();
-                    $this->dao->update(TABLE_CASE)->data($case)->where('id')->eq($caseID)->exec();
-
+                    $case->version    = $oldCase->version;
+                    $case->openedDate = $oldCase->openedDate;
+                    
                     $changes = common::createChanges($oldCase, $case);
                     if($changes)
                     {
+                        $this->dao->update(TABLE_CASE)->data($case)->where('id')->eq($caseID)->exec();
                         $actionID = $this->action->create('case', $caseID, 'Edited');
                         $this->action->logHistory($actionID, $changes);
                     }
@@ -2329,7 +2331,7 @@ class testtaskModel extends model
 
                     $caseStep = new stdclass();
                     $caseStep->type   = 'step';
-                    $caseStep->desc   = '';
+                    $caseStep->desc   = $step->name;
                     $caseStep->expect = $step->checkPoints[0]->expect;
 
                     $case->steps[] = $caseStep;
