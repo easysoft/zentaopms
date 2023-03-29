d3861768fc661e2dc5e78e7d67184e8c9452527d
宋辰轩 <songchenxuan@easycorp.ltd>
2023-03-29 10:56:42 +0800

    * Upgrade 18beta2 will execute sql,but  the installation does not, so add it to upgrade and install again.

--- db/bi.sql
+++ db/bi.sql
@@ -1,3 +1,39 @@
+/* Upgrade 18.0.beta2 will execute sql, but the installation does not. */
+CREATE TABLE IF NOT EXISTS `zt_dimension` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(90) NOT NULL,
+  `code` varchar(45) NOT NULL,
+  `desc` text NOT NULL,
+  `createdBy` varchar(30) NOT NULL,
+  `createdDate` datetime NOT NULL,
+  `editedBy` varchar(30) NOT NULL,
+  `editedDate` datetime NOT NULL,
+  `deleted` enum('0','1') NOT NULL default '0',
+  PRIMARY KEY (`id`),
+  KEY `code` (`code`)
+)
+
+ALTER TABLE `zt_chart` ADD `dimension` mediumint(8) unsigned NOT NULL DEFAULT 0 AFTER `name`;
+ALTER TABLE `zt_chart` ADD `group` mediumint(8) unsigned NOT NULL DEFAULT 0 AFTER `type`;
+ALTER TABLE `zt_chart` ADD `fields` mediumtext NOT NULL AFTER `filters`;
+ALTER TABLE `zt_chart` ADD `sql` text NOT NULL AFTER `fields`;
+ALTER TABLE `zt_chart` ADD `builtin` tinyint(1) unsigned NOT NULL AFTER `sql`;
+ALTER TABLE `zt_chart` ADD `objects` mediumtext NOT NULL AFTER `builtin`;
+ALTER TABLE `zt_chart` ADD `editedBy` varchar(30) NOT NULL AFTER `createdDate`;
+ALTER TABLE `zt_chart` ADD `editedDate` datetime NOT NULL AFTER `editedBy`;
+ALTER TABLE `zt_chart` MODIFY COLUMN `desc` text NOT NULL;
+
+ALTER TABLE `zt_dashboard` ADD `dimension` int(8) NOT NULL default 0 AFTER `name`;
+ALTER TABLE `zt_report`    ADD `dimension` int(8) NOT NULL default 0 AFTER `name`;
+
+UPDATE `zt_chart`     SET `dimension` = 1 WHERE `dimension` = 0;
+UPDATE `zt_report`    SET `dimension` = 1 WHERE `dimension` = 0;
+UPDATE `zt_dashboard` SET `dimension` = 1 WHERE `dimension` = 0;
+
+UPDATE `zt_grouppriv` SET `module` = 'dataview' WHERE `module` = 'dataset' AND `method` in ('create', 'browse', 'edit', 'delete');
+DELETE FROM `zt_grouppriv` WHERE `module` = 'dataset' AND `method` = 'view';
+/* */
+
 CREATE TABLE `zt_chart_back` LIKE `zt_chart`; INSERT INTO `zt_chart_back` SELECT * FROM `zt_chart`;
 
 ALTER TABLE `zt_chart` MODIFY `group` varchar(255) NOT NULL;
