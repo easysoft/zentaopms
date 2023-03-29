b7dc108a2ed56d1302d54319ab19e4ecede5af59
Hao Sun <sunhao@easycorp.ltd>
2023-03-29 10:35:15 +0800

    + zin: support to render content as fragment

--- zin/core/render.func.php
+++ zin/core/render.func.php
@@ -13,7 +13,7 @@ namespace zin;
 
 require_once 'zin.class.php';
 
-function render($wgName = '\\zin\\page')
+function render($wgName = 'page')
 {
     $args = array();
     foreach(zin::$globalRenderList as $item)
@@ -22,14 +22,9 @@ function render($wgName = '\\zin\\page')
         $args[] = $item;
     }
 
-    if(is_string($wgName))
-    {
-        if(strpos($wgName, '\\zin\\') !== false)  $wgName = str_replace('\\zin\\', '', $wgName);
-        if(isset(zin::$globalRenderMap[$wgName])) $wgName = zin::$globalRenderMap[$wgName];
-    }
+    if(is_string($wgName) && isset(zin::$globalRenderMap[$wgName])) $wgName = zin::$globalRenderMap[$wgName];
 
-    $className = "\\zin\\$wgName";
-    $wg        = class_exists($className) ? (new $className($args)) : $className($args);
+    $wg = createWg($wgName, $args);
 
     if(!$wg->displayed) $wg->display();
 }
