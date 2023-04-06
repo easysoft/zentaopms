ee09cb527500057aaab910b8ab1d96a3f556c882
Hao Sun <sunhao@easycorp.ltd>
2023-04-06 18:00:07 +0800

    * zin: add default className to dtable

--- zin/wg/dtable/v1.php
+++ zin/wg/dtable/v1.php
@@ -3,6 +3,8 @@ namespace zin;
 
 class dtable extends wg
 {
+    static $defineProps = 'className?:string="shadow rounded"';
+
     protected function build()
     {
         return zui::dtable(inherit($this));
