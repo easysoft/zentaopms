6578f95eb2cc98c79cbe18d04b993761f1fb32ff
Catouse <catouse@gmail.com>
2023-04-06 11:26:40 +0800

    * zin: refactor

--- zin/wg/dtable/v1.php
+++ zin/wg/dtable/v1.php
@@ -3,11 +3,6 @@ namespace zin;
 
 class dtable extends wg
 {
-    protected static $defineProps = array
-    (
-        'js-render?:bool=true'
-    );
-
     protected function build()
     {
         return zui::dtable(inherit($this));
