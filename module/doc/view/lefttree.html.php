fbae4bca3a1dd6cc3adef840ed0941d09857affd
guofeilong <guofeilong@easycorp.ltd>
2023-04-06 11:04:47 +0800

    * Adjust code .

--- module/doc/view/lefttree.html.php
+++ module/doc/view/lefttree.html.php
@@ -23,12 +23,12 @@
 .input-tree {width: 120px;}
 .tree-icon {position: absolute; right: 0;}
 .tree li.has-input {overflow: hidden;}
-.img-lib {flex: 0 0 14px; height: 14px;}
+.img-lib {flex: 0 0 14px; height: 14px; margin-right: 5px;}
 .tree-icon {position: absolute; right: 0;}
 .tree li > a {max-width: 100%; padding: 2px;}
 .file-tree  a.show-icon > div {padding-right: 15px;}
 .tree li.has-input {overflow: hidden;}
-.tree-text {margin-left: 5px; overflow: hidden;}
+.tree-text {overflow: hidden;}
 i.btn-info, i.btn-info:hover {border: none; background: #fff; box-shadow: unset;}
 .tree-version-trigger {padding: 0 10px; width: 54px; border-radius: 5px; background: #F9F9F9; display: flex; align-items: center;}
 .tree-version-trigger > .text {overflow: hidden; flex: 0 0 30px;}
