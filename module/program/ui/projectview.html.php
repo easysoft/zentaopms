74e9763a1605268c9786f3e39869e5da746a7423
Hao Sun <sunhao@easycorp.ltd>
2023-04-06 19:07:05 +0800

    * zin: refactor

--- module/program/ui/projectview.html.php
+++ module/program/ui/projectview.html.php
@@ -47,46 +47,9 @@ foreach($programs as $program)
     $data[] = $program;
 }
 
-/* Render Cell. */
-js
-(
-<<<RENDERCELL
-var langWorkerDay = '{$lang->program->workerDay}';
-var langPostponed = '{$lang->project->statusList['delay']}';
-
-window.footerGenerator = function()
-{
-    const count = this.layout.allRows.filter((x) => x.data.type === "product").length;
-    const statistic = '{$summary}'.replace('%s', ' ' + count + ' ');
-    return [{children: statistic, className: "text-dark"}, "flex", "pager"];
-}
-
-window.renderCell = function(result, {col, row})
-{
-    if(col.name === 'name')
-    {
-        if(row.data.postponed) result[result.length] = {html:'<span class="label size-sm circle danger-pale">' + langPostponed + '</span>', className:'flex items-end w-full', style:{flexDirection:"column"}};
-        return result;
-    }
-
-    if(col.name === 'budget')
-    {
-        result[0] = {html: '<div>' + row.data.budget + ' <span class="icon icon-exclamation-sign mr-2 text-danger"></span></div>', className:'flex items-end w-full items-end', style:{flexDirection:"column"}};
-        return result;
-    }
-
-    if(col.name === 'invested')
-    {
-        result[0] = {html: '<div>' + row.data.invested + ' <small class="text-gray">' + langWorkerDay + '</small></div>', className:'flex items-end w-full items-end', style:{flexDirection:"column"}};
-        return result;
-    }
-
-    return result;
-}
-RENDERCELL
-);
-
-set::title($lang->program->projectView);
+jsVar('langWorkerDay', $lang->program->workerDay);
+jsVar('langPostponed', $lang->project->statusList['delay']);
+jsVar('summeryTpl',    $summary);
 
 featureBar
 (
@@ -103,28 +66,29 @@ featureBar
 
 toolbar
 (
-    item(set(array(
+    item(set
+    ([
         'text' => $lang->program->createProject,
         'icon' => 'plus',
         'class'=> 'btn secondary',
         'url'  => createLink('program', 'exportTable')
-    ))),
-    item(set(array(
+    ])),
+    item(set
+    ([
         'text' => $lang->program->create,
         'icon' => 'plus',
         'class'=> 'btn primary',
         'url'  => createLink('program', 'exportTable')
-    ))),
+    ])),
 );
 
 dtable
 (
-    set::className('shadow rounded'),
     set::cols($cols),
     set::data($data),
-    set::footPager(usePager()),
     set::nested(true),
     set::onRenderCell(jsRaw('window.renderCell')),
+    set::footPager(usePager()),
     set::footer(jsRaw('function(){return window.footerGenerator.call(this);}'))
 );
 
