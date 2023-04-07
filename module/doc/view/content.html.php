3cf6f46f62b885f3d28a9b3660903e4f7e6f22d1
guofeilong <guofeilong@easycorp.ltd>
2023-04-07 10:21:57 +0800

    * Code for task #89913 .

--- module/doc/view/content.html.php
+++ module/doc/view/content.html.php
@@ -30,6 +30,7 @@
             <div class="time"></div>
           </div>
           <div class="actions">
+            <span class='text'><?php echo$lang->doc->diff?></span>
             <?php
             echo html::a("javascript:fullScreen()", '<i class="icon-fullscreen"></i>', '', "title='{$lang->fullscreen}' class='btn btn-link fullscreen-btn'");
             if(common::hasPriv('doc', 'edit'))
@@ -43,16 +44,19 @@
                 }
                 echo html::a(inlink('edit', "docID=$doc->id&comment=false&objectType=$objectType&objectID=$object->id&libID=$libID", '', $onlybody), '<i class="icon-edit"></i>', '', "title='{$lang->doc->edit}' class='btn btn-link $iframe' data-app='{$this->app->tab}'");
             }
-            if(common::hasPriv('doc', 'delete'))
-            {
-                $deleteURL = $this->createLink('doc', 'delete', "docID=$doc->id&confirm=yes&from=lib");
-                echo html::a("javascript:ajaxDeleteDoc(\"$deleteURL\", \"docList\", confirmDelete)", '<i class="icon-trash"></i>', '', "title='{$lang->doc->delete}' class='btn btn-link'");
-            }
             ?>
             <?php if(common::hasPriv('doc', 'collect')):?>
             <?php $star = strpos($doc->collector, ',' . $this->app->user->account . ',') !== false ? 'star' : 'star-empty';?>
             <a data-url="<?php echo $this->createLink('doc', 'collect', "objectID=$doc->id&objectType=doc");?>" title="<?php echo $lang->doc->collect;?>" class='ajaxCollect btn btn-link'><?php echo html::image("static/svg/{$star}.svg", "class='$star'");?></a>
             <?php endif;?>
+
+            <?php
+            if(common::hasPriv('doc', 'delete'))
+            {
+                $deleteURL = $this->createLink('doc', 'delete', "docID=$doc->id&confirm=yes&from=lib");
+                echo html::a("javascript:ajaxDeleteDoc(\"$deleteURL\", \"docList\", confirmDelete)", '<i class="icon-trash"></i>', '', "title='{$lang->doc->delete}' class='btn btn-link'");
+            }?>
+            <a href="###" class="btn btn-link" title=<?php echo $lang->history?>><i class="icon icon-clock"></i></a>
 
             <?php if($this->config->edition == 'max' and $this->app->tab == 'project'):?>
             <?php
