d06c47f6cfb3810fbca1aa1ebc6285a072922002
Hao Sun <sunhao@easycorp.ltd>
2023-04-11 09:46:05 +0800

    * zin: auto set id for dtable

--- zin/wg/dtable/v1.php
+++ zin/wg/dtable/v1.php
@@ -3,7 +3,15 @@ namespace zin;
 
 class dtable extends wg
 {
-    static $defineProps = 'className?:string="shadow-sm rounded"';
+    static $defineProps = 'className?:string="shadow-sm rounded", id?:string';
+
+    static $dtableID = 0;
+
+    protected function created()
+    {
+        $this->setDefaultProps(['id' => static::$dtableID ? static::$dtableID : 'dtable']);
+        static::$dtableID++;
+    }
 
     public static function getPageCSS()
     {
