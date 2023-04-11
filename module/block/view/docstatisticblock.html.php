675f25a19194fd567638898d44f4802a535f0da5
caoyanyi <caoyanyi@easycorp.ltd>
2023-04-11 16:47:45 +0800

    * Modify style for right block.

--- module/block/view/docstatisticblock.html.php
+++ module/block/view/docstatisticblock.html.php
@@ -16,22 +16,37 @@
 .block-statistic .statistic {flex: 0 1 32%;}
 .block-statistic .created {flex: 0 1 48%;}
 .block-statistic .edited {flex: 0 1 16%; padding-left: 20px;}
-.block-statistic .divider {border-right: 1px solid #eee; padding-right: 35px;}
+.block-statistic .divider {border-right: 1px solid #eee; padding-right: 12%;}
 .block-statistic .title-line {height: 32px;}
 .block-statistic .panel-title {display: none;}
 .block-statistic .panel-body {margin-top: -32px; padding: 30px;}
 .block-statistic .tile-title {white-space: nowrap;}
+
+.block-statistic.block-sm .flex {justify-content: start;}
+.block-statistic.block-sm .panel-body.flex {flex-direction: column;}
+.block-statistic.block-sm .divider {border-right: none;}
+.block-statistic.block-sm .panel-title {display: block;}
+.block-statistic.block-sm .statistic .title-line {display: none;}
+.block-statistic.block-sm .statistic .flex {margin-top: -28px;}
+.block-statistic.block-sm .title-line {height: 0; padding-top: 10px;}
+.block-statistic.block-sm .tile {padding: 10px 32px 20px;}
+.block-statistic.block-sm .panel-body {margin-top: 0px; padding: 10px;}
+.block-statistic.block-sm .flex-column .flex .tile:nth-child(1) {padding-left: 0;}
+.block-statistic.block-sm .edited {padding-left: 10px;}
+.block-statistic.block-sm .flex-column {border-bottom: 1px solid #eee;}
+.block-statistic.block-sm .flex-column:last-child {border-bottom: none;}
 </style>
+<div class='panel-move-handler'></div>
 <div class="panel-body flex">
   <div class='flex flex-column statistic'>
     <h4 class="title-line"><?php echo $lang->doc->docStatistic;?></h4>
     <div class='flex'>
       <div class="tile">
-        <div class="tile-amount text-primary"><?php echo (int)$statistic->totalDocs;?></div>
+        <div class="tile-amount"><?php echo (int)$statistic->totalDocs;?></div>
         <div class="tile-title"><?php echo $lang->doc->allDoc;?></div>
       </div>
       <div class="tile divider">
-        <div class="tile-amount text-primary"><?php echo (int)$statistic->todayEditedDocs;?></div>
+        <div class="tile-amount"><?php echo (int)$statistic->todayEditedDocs;?></div>
         <div class="tile-title"><?php echo $lang->doc->todayEdited;?></div>
       </div>
     </div>
@@ -39,16 +54,24 @@
   <div class='flex flex-column created'>
     <h4 class="title-line"><?php echo $lang->doc->openedByMe;?></h4>
     <div class='flex'>
+      <?php if(common::hasPriv('doc', 'mySpace')):?>
+      <a class="tile" href="<?php echo $this->createLink('doc', 'mySpace', 'type=createdBy');?>">
+      <?php else:?>
       <div class="tile">
-        <div class="tile-amount text-primary"><?php echo (int)$statistic->myDocs;?></div>
+      <?php endif;?>
+        <div class="tile-amount"><?php echo (int)$statistic->myDocs;?></div>
         <div class="tile-title"><?php echo $lang->doc->docCreated;?></div>
+      <?php if(common::hasPriv('doc', 'mySpace')):?>
+      </a>
+      <?php else:?>
       </div>
+      <?php endif;?>
       <div class="tile">
-        <div class="tile-amount text-primary"><?php echo (int)$statistic->myDoc->docViews;?></div>
+        <div class="tile-amount"><?php echo (int)$statistic->myDoc->docViews;?></div>
         <div class="tile-title"><?php echo $lang->doc->docViews;?></div>
       </div>
       <div class="tile divider">
-        <div class="tile-amount text-primary"><?php echo (int)$statistic->myDoc->docCollects;?></div>
+        <div class="tile-amount"><?php echo (int)$statistic->myDoc->docCollects;?></div>
         <div class="tile-title"><?php echo $lang->doc->docCollects;?></div>
       </div>
     </div>
@@ -57,7 +80,7 @@
     <h4 class="title-line"><?php echo $lang->doc->editedByMe;?></h4>
     <div class='flex'>
       <div class="tile">
-        <div class="tile-amount text-primary"><?php echo (int)$statistic->myEditedDocs;?></div>
+        <div class="tile-amount"><?php echo (int)$statistic->myEditedDocs;?></div>
         <div class="tile-title"><?php echo $lang->doc->docEdited;?></div>
       </div>
     </div>
