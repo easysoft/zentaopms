5424de798ca8a261491f581da78fbbed2fbe996f
宋辰轩 <songchenxuan@easycorp.ltd>
2023-04-14 16:55:50 +0800

    * Finish task #91131.

--- module/screen/view/browse.html.php
+++ module/screen/view/browse.html.php
@@ -1,4 +1,5 @@
 <?php include $app->getModuleRoot() . 'common/view/header.html.php';?>
+<?php js::set('firstGuide', $firstGuide);?>
 <div id="mainContent">
   <?php if(empty($screens)):?>
   <div class="table-empty-tip">
@@ -40,5 +41,18 @@
     <?php endforeach;?>
   </div>
   <?php endif;?>
+</div>
+
+<div class="modal fade" id="firstGuide">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-body">
+        <div class="background-img"></div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-wide btn-primary" data-dismiss="modal"><?php echo $lang->close;?></button>
+      </div>
+    </div>
+  </div>
 </div>
 <?php include $app->getModuleRoot() . 'common/view/footer.html.php';?>
