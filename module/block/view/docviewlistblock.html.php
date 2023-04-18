59afadec4a46c81de479eb53f1c8900d5c7193f5
tianshujie <tianshujie0123@163.com>
2023-04-18 09:03:43 +0800

    * Code for task #91389,91390.

--- module/block/view/docviewlistblock.html.php
+++ module/block/view/docviewlistblock.html.php
@@ -17,8 +17,10 @@
 .block-docviewlist .doc-list > .doc-title > .label-rank-1 {color: #FC5959; background-color: #FFE2D9;}
 .block-docviewlist .doc-list > .doc-title > .label-rank-2 {color: #FF8058; background-color: #FFE2D9;}
 .block-docviewlist .doc-list > .doc-title > .label-rank-3 {color: #FF9F46; background-color: #FFECDB;}
-.block-docviewlist .doc-list > .doc-title > a {overflow: hidden; max-width: calc(100% - 30px);}
+.block-docviewlist .doc-list > .doc-title > .doc-name {overflow: hidden; max-width: calc(100% - 110px);}
 .block-docviewlist .doc-list > .doc-title > .label-view-count {margin-left: 10px;}
+.block-docviewlist .doc-list > .doc-title > .label-view-count > .icon-flame {margin-bottom: 3px;}
+.block-docviewlist .doc-list > .doc-title > .label-view-count > .icon-flame.gray {filter: grayscale(100%);}
 .block-docviewlist .doc-list > .doc-title > .label-view-count > .view-text {color: #838A9D;}
 </style>
 <div class="panel-body">
@@ -33,14 +35,16 @@
       <?php
       if(common::hasPriv('doc', 'view'))
       {
-          echo html::a($this->createLink('doc', 'view', "docID=$doc->id"), $doc->title, '', "title='{$doc->title}' class='doc-title' data-app='{$this->app->tab}'");
+          echo html::a($this->createLink('doc', 'view', "docID=$doc->id"), $doc->title, '', "title='{$doc->title}' class='doc-name' data-app='{$this->app->tab}'");
       }
       else
       {
-          echo "<span class='doc-title'>{$doc->title}</span>";
+          echo "<span class='doc-name' title='{$doc->title}'>{$doc->title}</span>";
       }
       ?>
       <div class='label-view-count'>
+        <?php $flameClass = $rank < 4 ? '' : 'gray';?>
+        <?php echo html::image("static/svg/flame.svg", "class='icon-flame $flameClass'");?>
         <span class='view-text'><?php echo sprintf($lang->doc->viewCount, $doc->views);?></span>
       </div>
     </div>
