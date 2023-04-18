ab04e42c0bb67964ad2107e60d08d1a38cd04020
tianshujie <tianshujie0123@163.com>
2023-04-18 14:44:27 +0800

    * Fix bug #34316.

--- module/doc/view/tablecontents.html.php
+++ module/doc/view/tablecontents.html.php
@@ -23,7 +23,7 @@
     <?php foreach($lang->doc->featureBar['tableContents'] as $barType => $barName):?>
     <?php $active     = $barType == $browseType ? 'btn-active-text' : '';?>
     <?php $linkParams = "objectID=$objectID&libID=$libID&moduleID=$moduleID&browseType=$barType";?>
-    <?php echo html::a($this->createLink('doc', $app->rawMethod, $linkParams), "<span class='text'>{$barName}</span>" . ($active ? " <span class='label label-light label-badge'>{$pager->recTotal}</span>" : ''), '', "class='btn btn-link $active' id='{$barType}Tab'");?>
+    <?php echo html::a($this->createLink($app->rawModule, $app->rawMethod, $linkParams), "<span class='text'>{$barName}</span>" . ($active ? " <span class='label label-light label-badge'>{$pager->recTotal}</span>" : ''), '', "class='btn btn-link $active' id='{$barType}Tab'");?>
     <?php endforeach;?>
     <?php endif;?>
     <a class="btn btn-link querybox-toggle" id='bysearchTab'><i class="icon icon-search muted"></i> <?php echo $lang->doc->searchDoc;?></a>
