9bf494d4cea16ae44b2fc8d8ee8dd282320c0a1f
wangyidong <wangyidong@cnezsoft.com>
2023-04-24 13:18:57 +0800

    * fix bug.

--- lib/mobile/mobile.class.php
+++ lib/mobile/mobile.class.php
@@ -1215,7 +1215,7 @@ class mobile
     {
         if(empty($userAgent) and empty($this->userAgent)) return false;
 
-        $match = (bool) preg_match(sprintf('#%s#is', $regex), $userAgent ? $userAgent : $this->userAgent), $matches);
+        $match = (bool) preg_match(sprintf('#%s#is', $regex), ($userAgent ? $userAgent : $this->userAgent), $matches);
         // If positive match is found, store the results for debug.
         if ($match) {
             $this->matchingRegex = $regex;
