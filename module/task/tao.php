6cca4af5dcd9d060ac1230df18363d0165d8d3d5
liumengyi <liumengyi@easycorp.ltd>
2023-04-26 19:09:29 +0800

    + Add file comment.

--- module/task/tao.php
+++ module/task/tao.php
@@ -1,4 +1,15 @@
 <?php
+declare(strict_types=1);
+/**
+ * The tao file of task module of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.com)
+ * @license     ZPL(http://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Shujie Tian <tianshujie@easysoft.ltd>
+ * @package     task
+ * @link        https://www.zentao.net
+ */
+
 class taskTao extends taskModel
 {
     /**
