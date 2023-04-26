8b939f7fbe0cc67649eae1ea0370152310f7b1c0
Wangyuting <851424971@qq.com>
2023-04-26 11:27:02 +0800

    * Fix welcome page bug.

--- module/block/ui/welcome.html.php
+++ module/block/ui/welcome.html.php
@@ -1,6 +1,5 @@
 <?php
 namespace zin;
-
 div
 (
     set('class', 'panel-body conatiner-fluid'),
@@ -9,14 +8,154 @@ div
         set('class', 'table-row'),
         div
         (
-            set('class', 'col col-left hide-in-sm'),
-            h4(date(DT_DATE3)),
+            set('class', 'col col-left'),
             h4
             (
                 set('class', 'user-welcome'),
-                printf($lang->block->welcomeList[$welcomeType], $app->user->realname)
+                sprintf($lang->block->welcomeList[$welcomeType], $app->user->realname)
+            ),
+            avatar
+            (
+                set::text($app->user->realname),
+                set::src($app->user->avatar)
             )
-        )
+        ),
+        div
+        (
+            set('class', 'col col-center'),
+            h4('待我评审：'),
+            div
+            (
+                set('class', 'row tiles'),
+                div
+                (
+                    set('class', 'col tile'),
+                    div
+                    (
+                        set('class', 'tile-amount text-primary'),
+                        '123',
+                    ),
+                    div
+                    (
+                        set('class', 'tile-title'),
+                        '反馈数:'
+                    ),
+                    !empty($delay['task'])
+                        ? div
+                        (
+                            set('class', 'tile-info'),
+                            span
+                            (
+                                set('class', 'label label-danger label-outline welcome-label'),
+                                $lang->block->delayed . ' ' . $delay['task']
+                            )
+                        )
+                        : null
+                ),
+                div
+                (
+                    set('class', 'col tile'),
+                    div
+                    (
+                        set('class', 'tile-amount text-primary'),
+                        333,
+                    ),
+                    div
+                    (
+                        set('class', 'tile-title'),
+                        '用例数'
+                    ),
+                    div
+                    (
+                        set('class', 'tile-info'),
+                        span
+                        (
+                            set('class', 'label label-danger label-outline welcome-label'),
+                            $lang->block->delayed . ' ' . $delay['task']
+                        )
+                    )
+                ),
+                div
+                (
+                    set('class', 'col tile'),
+                    div
+                    (
+                        set('class', 'tile-amount text-primary'),
+                        444,
+                    ),
+                    div
+                    (
+                        set('class', 'tile-title'),
+                        '基线数'
+                    ),
+
+                    div
+                    (
+                        set('class', 'tile-info'),
+                        span
+                        (
+                            set('class', 'label label-danger label-outline welcome-label'),
+                            $lang->block->delayed . ' ' . $delay['task']
+                        )
+                    )
+                )
+            )
+        ), 
+        div
+        (
+            set('class', 'col col-right'),
+            h4('指派给我：'),
+            div
+            (
+                set('class', 'row tiles'),
+                div
+                (
+                    set('class', 'col tile'),
+                    div
+                    (
+                        set('class', 'tile-amount text-primary'),
+                        1313,
+                    ),
+                    div
+                    (
+                        set('class', 'tile-title'),
+                        $lang->block->myTask
+                    ),
+                    div
+                    (
+                        set('class', 'tile-info'),
+                        span
+                        (
+                            set('class', 'label label-danger label-outline welcome-label'),
+                            $lang->block->delayed . ' ' . $delay['task']
+                        )
+                    )
+                ),
+                div
+                (
+                    set('class', 'col tile'),
+                    div
+                    (
+                        set('class', 'tile-amount text-primary'),
+                        3333,
+                    ),
+                    div
+                    (
+                        set('class', 'tile-title'),
+                        $lang->block->myBug
+                    ),
+                    div
+                    (
+                        set('class', 'tile-info'),
+                        span
+                        (
+                            set('class', 'label label-danger label-outline welcome-label'),
+                            $lang->block->delayed . ' ' . $delay['task']
+                        )
+                    )
+                )
+            )
+        ) 
     )
 );
 render();
