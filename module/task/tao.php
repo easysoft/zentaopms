5b418eb8db8f6b58ad7db9c688cf9b1b9d39273f
tianshujie <tianshujie@easycorp.ltd>
2023-04-27 09:16:12 +0800

    * Modify comment of tao.php.

--- module/task/tao.php
+++ module/task/tao.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 /**
  * The tao file of task module of ZenTaoPMS.
  *
- * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.com)
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
  * @license     ZPL(http://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
  * @author      Shujie Tian <tianshujie@easysoft.ltd>
  * @package     task
