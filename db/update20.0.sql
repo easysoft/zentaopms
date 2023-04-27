d94b7ebb23f60718b4aba7df6507490fc37e205a
liuruogu <liuruogu@easycorp.ltd>
2023-04-27 19:18:31 +0800

    * Modify sql of update.

--- db/update20.0.sql
+++ db/update20.0.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `zt_block` ADD `dashboard` varchar(20) NOT NULL DEFAULT '' AFTER `account`;
 UPDATE `zt_block` SET `dashboard` = `module`;
+
+ALTER TABLE `zt_todo`  CHANGE `idvalue` `objectID` mediumint(8) unsigned default '0' NOT NULL AFTER `type`;
