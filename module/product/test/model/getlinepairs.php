cd00738d77f6e1f712d09586b68060ded042e481
wangyidong <wangyidong@cnezsoft.com>
2023-04-27 15:59:09 +0800

    * adjust for product create.

--- module/product/test/model/getlinepairs.php
+++ module/product/test/model/getlinepairs.php
@@ -3,28 +3,32 @@
 include dirname(__FILE__, 5) . "/test/lib/init.php";
 include dirname(__FILE__, 2) . '/product.class.php';
 
+function initData()
+{
+    $module = zdTable('module');
+    $module->id->range('1-1000');
+    $module->root->range('1-5');  //产品线所属项目集
+    $module->name->prefix("产品线")->range('1-1000');
+    $module->type->range("line");
+    $module->parent->range("0");
+
+    $module->gen(10);
+}
+initData();
+
 /**
 
 title=productModel->getLinePairs();
 cid=1
 pid=1
 
-测试获取程序集1的信息 >> 产品线1,产品线11
-测试获取程序集2的信息 >> 产品线2,产品线12
-测试获取程序集3的信息 >> 产品线3,产品线13
-测试获取程序集4的信息 >> 产品线4,产品线14
-测试获取程序集5的信息 >> 产品线5,产品线15
-测试获取不存在程序集的信息 >> 0
-
 */
-
-$programIDList = array('1', '2', '3', '4', '5', '1000001');
-
 $product = new productTest('admin');
 
-r($product->getLinePairsTest($programIDList[0])) && p('1,11') && e('产品线1,产品线11');   // 测试获取程序集1的信息
-r($product->getLinePairsTest($programIDList[1])) && p('2,12') && e('产品线2,产品线12');   // 测试获取程序集2的信息
-r($product->getLinePairsTest($programIDList[2])) && p('3,13') && e('产品线3,产品线13');   // 测试获取程序集3的信息
-r($product->getLinePairsTest($programIDList[3])) && p('4,14') && e('产品线4,产品线14');   // 测试获取程序集4的信息
-r($product->getLinePairsTest($programIDList[4])) && p('5,15') && e('产品线5,产品线15');   // 测试获取程序集5的信息
-r($product->getLinePairsTest($programIDList[5])) && p('1,11') && e('0');                      // 测试获取不存在程序集的信息
+r($product->getLinePairsTest(-1))    && p()      && e('0');                 // 测试获取程序集-1的信息
+r($product->getLinePairsTest(0))     && p()      && e('0');                 // 测试获取程序集0的信息
+r($product->getLinePairsTest(1))     && p('1,6') && e('产品线1,产品线6');   // 测试获取程序集1的信息
+r($product->getLinePairsTest(2))     && p('2,7') && e('产品线2,产品线7');   // 测试获取程序集2的信息
+r($product->getLinePairsTest(3))     && p('3,8') && e('产品线3,产品线8');   // 测试获取程序集3的信息
+r($product->getLinePairsTest(4))     && p('4,9') && e('产品线4,产品线9');   // 测试获取程序集4的信息
+r($product->getLinePairsTest(10001)) && p()      && e('0');                 // 测试获取不存在程序集的信息
