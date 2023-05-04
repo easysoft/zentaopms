928ff7ee667cc3e56ac0fcf47f4d91af03cdee7e
liuruogu <liuruogu@easycorp.ltd>
2023-05-04 15:11:18 +0800

    + Add Welcome Block.

--- module/block/js/create.ui.js
+++ module/block/js/create.ui.js
@@ -6,3 +6,27 @@ function getForm(event)
     const url    = $.createLink('block', 'create', 'dashboard='+ dashboard +'&module=' + module + '&code=' + (code ? code : ''));
     loadPage(url, '#codesRow, #paramsRow');
 }
+
+function onParamsTypeChange(event)
+{
+    const lang  = config.clientLang;
+    const $code = document.querySelector('#code');
+
+    if($('#module').val() == 'scrumtest' && $('#paramstype').val() != 'all')
+    {
+        $('#title').val($code.options[$code.selectedIndex].text);
+    }
+    else
+    {
+        if(lang.indexOf('zh') >= 0)
+        {
+            const $paramstype = document.querySelector('#paramstype');
+            const blockTitle  = $paramstype.options[$paramstype.selectedIndex].text + of + $code.options[$code.selectedIndex].text;
+            $('#title').val(blockTitle);
+        }
+        else
+        {
+            /* TODO */
+        }
+    }
+}
