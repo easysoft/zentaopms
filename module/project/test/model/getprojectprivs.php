32ca0f99f2ed7a22e48527d9d2906415420852cc
caoyanyi <caoyanyi@easycorp.ltd>
2023-05-05 15:44:08 +0800

    * Add unit test.

---
+++ module/project/test/model/getprojectprivs.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php
+<?php
+include dirname(__FILE__, 5) . "/test/lib/init.php";
+su('admin');
+
+/**
+
+title=测试 projectModel->getProjectPrivs();
+timeout=0
+cid=1
+
+*/
+
+global $tester;
+$projectTester = $tester->loadModel('project');
+
+r($projectTester->getProjectPrivs(''))         && p()             && e('0');            // 传递空类型的情况
+r($projectTester->getProjectPrivs('test'))     && p()             && e('0');            // 传递错误的类型的情况
+r($projectTester->getProjectPrivs('scrum'))    && p('bug:delete') && e('deleteAction'); // 传递正确类型的情况
