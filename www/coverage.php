23994447a2bad6aede3ccac3537a76e0fda76319
liuyongkai <liuyongkai@easycorp.ltd>
2023-05-06 07:55:42 +0000

    * Show ztf report.

--- www/coverage.php
+++ www/coverage.php
@@ -3,9 +3,20 @@ include_once(dirname(__FILE__, 2) . "/test/lib/coverage.php");
 global $zentaoRoot;
 $zentaoRoot = dirname(__FILE__, 2);
 
-$type     = isset($_GET['module']) ? 'module' : 'summary';
-$coverage = new coverage();
-$report   = '';
+$type      = isset($_GET['module']) ? 'module' : 'summary';
+$coverage  = new coverage();
+$report    = '';
+$ztfReport = $coverage->getZtfReport();
+if($ztfReport)
+{
+    $ztfHtml = "<p>%s 执行<strong>%s个</strong>用例，耗时<strong>%s秒</strong>。<strong>%s(%s%%) </strong>通过，<strong>%s(%s%%)</strong> 失败，<strong>%s(%s%%)</strong> 忽略。</p>";
+    $ztfHtml = sprintf($ztfHtml, $ztfReport->time, $ztfReport->total, $ztfReport->duration, $ztfReport->pass, $ztfReport->passPercent, $ztfReport->fail, $ztfReport->failPercent, $ztfReport->skip, $ztfReport->skipPercent);
+}
+else
+{
+    $ztfHtml = "<p>没有找到ZTF测试报告。</p>";
+}
+
 
 switch($type)
 {
@@ -21,6 +32,73 @@ switch($type)
         $report = $coverage->genSummaryReport();
         break;
 }
+?>
+<html>
+<head>
+  <meta charset="UTF-8">
+    <title>单元测试行覆盖率报告</title>
+  </head>
+<style>
+  body {
+    font-family: Arial, sans-serif;
+    font-size: 16px;
+    line-height: 1.5;
+    margin: 0;
+    padding: 0;
+  }
+
+  table {
+    border-collapse: collapse;
+    max-width: 100%;
+    width: 100%;
+    margin: 20px 0;
+  }
+
+  th {
+    border: 1px solid #ccc;
+    padding: 8px;
+    text-align: center;
+    background-color: #eee;
+    white-space: nowrap;
+  }
+
+  caption {
+    font-weight: bold;
+    margin: 10px 0;
+    font-size: 18px;
+  }
+
+  h2 {
+    margin-top: 20px;
+    font-size: 24px;
+  }
+
+  .red {
+    color: red;
+  }
+
+  .green {
+    color: green;
+  }
+
+  /* table hover effect */
+  tbody tr:hover {
+    background-color: #f5f5f5;
+  }
+
+  /* table striped rows */
+  tbody tr:nth-child(even) {
+    background-color: #f9f9f9;
+  }
+
+  h1 {
+    text-align: center;
+  }
+</style>
+<body>
+<h1>单元测试行覆盖率报告</h1>
+<?php
+echo $ztfHtml;
 echo $report;
 echo "<script>var type = '$type';</script>";
 ?>
