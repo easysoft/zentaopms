03edd3440f97162fed6fcae49aba74b30b0eb679
liuhong <liuhong@easycorp.ltd>
2023-05-08 16:44:21 +0800

    * submit unit test.

--- module/project/test/model/getprogrammaxend.php
+++ module/project/test/model/getprogrammaxend.php
@@ -15,14 +15,10 @@ pid=1
 global $tester;
 
 $tester->loadModel('project');
-$project1 = $tester->project->getProgramMaxEnd(1);
-$project2 = $tester->project->getProgramMaxEnd(3);
-$project3 = $tester->project->getProgramMaxEnd(6);
+$project4 = $tester->project->getProgramMaxEnd(1);
+$project5 = $tester->project->getProgramMaxEnd(3);
+$project6 = $tester->project->getProgramMaxEnd(6);
 
-a($project1);die;
-r($project1) && p() && e(); //获取一级项目集最小开始时间
-r($project1) && p() && e(); //获取一级项目集最大结束时间
-r($project2) && p() && e(); //获取二级项目集最小开始时间
-r($project2) && p() && e(); //获取二级项目集最大结束时间
-r($project3) && p() && e(); //获取三级项目集最小开始时间
-r($project3) && p() && e(); //获取三级项目集最大结束时间
+r($project6) && p('maxEnd')   && e('2023-06-12'); //获取一级项目集最大结束时间:尾级项目结束时间最大
+r($project5) && p('maxEnd')   && e('2023-07-03'); //获取二级项目集最大结束时间:顶级项目结束时间最大
+r($project4) && p('maxEnd')   && e('2023-05-08'); //获取三级项目集最大结束时间:开始时间等于结束时间
