5d506c28788545b8cc0e28c9ee99eb86f3fcebba
wangzemei <wangzemei@easycorp.ltd>
2023-05-08 17:50:36 +0800

    * Split cycle config.

--- module/todo/js/edit.ui.js
+++ module/todo/js/edit.ui.js
@@ -13,3 +13,15 @@ function changeType(typeSelect)
 {
     loadList($(typeSelect).find('select').val(), '', idvalue);
 }
+
+/**
+ * 更改待办日期。
+ * Change todo date.
+ *
+ * @param  object tab
+ * @return void
+ */
+function changeCreateDate(dateInput)
+{
+    $('#switchDate').prop('checked', !$(dateInput).val());
+}
