544b484065650f94e042d9cd6b66c21ad448f284
daitingting <daitingting@xirangit.com>
2023-05-08 10:28:51 +0000

    * Fix test for updatelinkbug.

--- module/bug/test/tao/updatelinkbug.php
+++ module/bug/test/tao/updatelinkbug.php
@@ -34,9 +34,7 @@ cid=1
  - 属性2 @~~
  - 属性3 @1
 
-- 测试关联bug2的关联bug同步更新为1， bug3的关联bug为1
- - 属性2 @1
- - 属性3 @1
+- 测试关联bug2的关联bug同步更新为1属性2 @1
 
 - 测试关联bug3的关联bug同步更新为1,2属性3 @1,2
 
@@ -51,5 +49,5 @@ $bugIDList = array(1, 2, 3);
 $bug = new bugTest();
 r($bug->updateLinkBugTest($bugIDList[0], '2', ''))    && p('2', ';')   && e('1');    //测试关联bug2的关联bug同步更新为1
 r($bug->updateLinkBugTest($bugIDList[0], '3', '2'))   && p('2;3', ';') && e('~~;1'); //测试关联bug2的关联bug同步更新为空，bug3的关联bug为1
-r($bug->updateLinkBugTest($bugIDList[0], '2,3', '3')) && p('2;3', ';') && e('1;1');  //测试关联bug2的关联bug同步更新为1， bug3的关联bug为1
+r($bug->updateLinkBugTest($bugIDList[0], '2,3', '3')) && p('2')        && e('1');    //测试关联bug2的关联bug同步更新为1
 r($bug->updateLinkBugTest($bugIDList[1], '1,3', '1')) && p('3', ';')   && e('1,2');  //测试关联bug3的关联bug同步更新为1,2
