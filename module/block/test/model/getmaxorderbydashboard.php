57668d2fe5d1f577419e926f48de9ccfc2f759af
liuruogu <liuruogu@easycorp.ltd>
2023-05-08 18:02:09 +0800

    + Add test script.

--- module/block/test/model/getmaxorderbydashboard.php
+++ module/block/test/model/getmaxorderbydashboard.php
@@ -2,20 +2,19 @@
 <?php
 include dirname(__FILE__, 5) . "/test/lib/init.php";
 
-$userModel = new userModel();
-$user = $userModel->identify('admin', 'Asd123456');
-$userModel->login($user);
+su('admin');
 
 function initData()
 {
     $block = zdTable('block');
     $block->id->range('2-5');
-    $block->account->range('admin,test1,test2');
+    $block->account->range('admin');
     $block->vision->range('rnd,lite');
-    $block->module->range('my,qa,project');
-    $block->title->prefix('区块')->range('2-5');
-    $block->dashboard->range('my,qa');
-    $block->order->range('5-2');
+    $block->dashboard->range('my');
+    $block->module->range('project');
+    $block->code->range('list,statistic');
+    $block->title->prefix('区块名称')->range('2-5');
+    $block->order->range('1-4');
 
     $block->gen(4);
 }
@@ -26,16 +25,6 @@ title=测试 blockModel->getMaxOrderByDashboard();
 timeout=0
 cid=1
 
-- 执行block模块的getByID方法，参数是2
- - 属性account @admin
- - 属性vision @rnd
- - 属性module @my
- - 属性title @区块2
- - 属性source @my
- - 属性block @bug
- - 属性order @5
-
-
 */
 
 global $tester;
@@ -43,6 +32,6 @@ $tester->loadModel('block');
 
 initData();
 
-r($tester->block->getMaxOrderByDashboard('my')) && p('') && e('5'); //  测试 my 模块下最大order 为 5
-r($tester->block->getMaxOrderByDashboard('my')) && p('') && e('5'); //  测试 my 模块下最大order 为 5
+r($tester->block->getMaxOrderByDashboard('my')) && p('') && e('4'); //  测试 my 模块下最大order 为 4
+r($tester->block->getMaxOrderByDashboard('my')) && p('') && e('4'); //  测试 my 模块下最大order 为 4
 r($tester->block->getMaxOrderByDashboard('bug')) && p('') && e('0'); // 测试数据库中不存在 模块的最大排序  为 0
