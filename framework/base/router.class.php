02bf7ad2be19f480388d724fc2590c363a56a339
Guanxiying <guanxiying@easycorp.ltd>
2023-05-08 09:50:51 +0800

    * Replace die and exit with helper::end().

--- framework/base/router.class.php
+++ framework/base/router.class.php
@@ -2164,7 +2164,7 @@ class baseRouter
         if($extension == '302')
         {
             header("location: $url");
-            exit;
+            helper::end();
         }
 
         if($extension == 'api')
@@ -2186,7 +2186,7 @@ class baseRouter
             $output .= $response;
             if(isset($viewFiles['hookFiles'])) foreach($viewFiles['hookFiles'] as $hookFile) $output .= $this->control->printViewFile($hookFile);
             $output .= $this->control->printViewFile($footFile);
-            die($output);
+            helper::end($output);
         }
     }
 
@@ -2437,14 +2437,14 @@ class baseRouter
         $nameRule = $filter->{$this->moduleName}->{$this->methodName}->paramName ?? $filter->default->paramName;
         foreach($passedParams as $param => $value)
         {
-            if(!validater::checkByRule($param, $nameRule)) die('Bad Request!');
+            if(!validater::checkByRule($param, $nameRule)) helper::end('Bad Request!');
             $valueRule = $filter->default->paramValue;
             if(isset($filter->{$this->moduleName}->{$this->methodName}->paramValue[$param]))
             {
                 $valueRule = $filter->{$this->moduleName}->{$this->methodName}->paramValue[$param];
             }
 
-            if($value and !validater::checkByRule($value, $valueRule)) die('Bad Request!');
+            if($value and !validater::checkByRule($value, $valueRule)) helper::end('Bad Request!');
         }
 
         $passedParams = array_values($passedParams);
@@ -2873,7 +2873,7 @@ class baseRouter
         if(preg_match('/[^\x00-\x80]/', $message)) $message = helper::convertEncoding($message, 'gbk');
 
         /* Only show error when debug is open. */
-        if(!$this->config->debug) die();
+        if(!$this->config->debug) helper::end();
 
         $log = "ERROR: $message in $file on line $line";
         if(isset($_SERVER['SCRIPT_URI'])) $log .= ", request: $_SERVER[SCRIPT_URI]";;
@@ -2983,12 +2983,12 @@ class baseRouter
          * */
         if($level == E_ERROR or $level == E_PARSE or $level == E_CORE_ERROR or $level == E_COMPILE_ERROR or $level == E_USER_ERROR)
         {
-            if(empty($this->config->debug)) die();
-            if(PHP_SAPI == 'cli') die($errorLog);
+            if(empty($this->config->debug)) helper::end();
+            if(PHP_SAPI == 'cli') helper::end($errorLog);
 
             $htmlError  = "<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8' /></head>";
             $htmlError .= "<body>" . nl2br($errorLog) . "</body></html>";
-            die($htmlError);
+            helper::end($htmlError);
         }
     }
 
