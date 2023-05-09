832dd14b5638a86f1a6783b74751f825a903fb35
liuyue <liuyue@easycorp.ltd>
2023-05-09 11:53:34 +0000

    * Add comment for edit.js.

---
+++ module/programplan/js/edit.ui.js
@@ -0,0 +1,59 @@
+/**
+ * 父阶段更改值操作。
+ * change parent stage.
+ *
+ * @param  stageID $stageID
+ * @access public
+ * @return viod
+ */
+function changeParentStage(stageID)
+{
+    if(stageID == 0) $('#acl').attr('disabled', false);
+    $('#acl').attr('disabled', true);
+
+    $.get(createLink('programplan', 'ajaxGetAttribute', 'stageID=' + stageID + '&attribute=' + plan.attribute), function(attribute)
+    {
+        $('#attributeType td:first').html(attribute);
+        $('#attribute' + '_chosen').remove();
+        $('#attribute').next('.picker').remove();
+        $('#attribute').chosen();
+    });
+}
+
+/**
+ * 编辑阶段提交操作。
+ * edit stage.
+ *
+ * @access public
+ * @return viod
+ */
+function editStage()
+{
+    if(plan.parent != $('#parent').val() && $('#parent').val() != 0)
+    {
+        var result = true;
+
+        $.ajaxSettings.async = false;
+        $.get($.createLink('programplan', 'ajaxGetStageAttr', 'stageID=' + $('#parent').val()), function(attribute)
+        {
+            if(attribute != 'mix' && plan.attribute != attribute)
+            {
+                result = confirm(changeAttrLang.replace('%s', stageTypeList[attribute]));
+            }
+        });
+        $.ajaxSettings.async = true;
+
+        if(!result) return false;
+    }
+
+    var currentAttribute    = $('#attribute').val();
+    var currentParent       = $('#parent').val();
+    var hasChangedAttribute = (currentAttribute && currentAttribute != 'mix' && plan.attribute != currentAttribute);
+    var hasChangedParent    = ((isTopStage && $('#parent').val() != 0) || (!isTopStage && plan.parent != $('#parent').val()));
+    if(hasChangedAttribute && !hasChangedParent && !isLeafStage)
+    {
+        var result = confirm(changeAttrLang.replace('%s', stageTypeList[currentAttribute]));
+
+        if(!result) return false;
+    }
+}
