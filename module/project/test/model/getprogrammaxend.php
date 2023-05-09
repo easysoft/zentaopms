e5ba3ae87e50cbed38cf682547dfaf6070e40b6c
liuhong <liuhong@easycorp.ltd>
2023-05-09 11:27:03 +0800

    * Adjust code for getprogrammaxend and getprogramminbegin unit test.

--- module/project/test/model/getprogrammaxend.php
+++ module/project/test/model/getprogrammaxend.php
@@ -1,24 +1,32 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . "/test/lib/init.php";
+include dirname(__FILE__, 5) . '/test/lib/init.php';
 su('admin');
 
-$project = zdTable('project')->config('project')->gen(6);
+$project = zdTable('project')->config('project')->gen(11);
 
 /**
 
 title=测试 projectModel->getProgramMaxEnd();
+timeout=0
 cid=1
 pid=1
+
+- 获取二级项目集下唯一项目最大结束时间属性maxEnd @2023-06-12
+
+- 获取一级项目集下项目最大结束时间属性maxEnd @2023-07-21
+
+- 获取二级项目集下多个项目最大结束时间属性maxEnd @2023-05-20
+
 */
 
 global $tester;
 
 $tester->loadModel('project');
-$project4 = $tester->project->getProgramMaxEnd(1);
-$project5 = $tester->project->getProgramMaxEnd(3);
-$project6 = $tester->project->getProgramMaxEnd(6);
+$project1 = $tester->project->getProgramMaxEnd(1);
+$project2 = $tester->project->getProgramMaxEnd(4);
+$project3 = $tester->project->getProgramMaxEnd(8);
 
-r($project6) && p('maxEnd')   && e('2023-06-12'); //获取一级项目集最大结束时间:尾级项目结束时间最大
-r($project5) && p('maxEnd')   && e('2023-07-03'); //获取二级项目集最大结束时间:顶级项目结束时间最大
-r($project4) && p('maxEnd')   && e('2023-05-08'); //获取三级项目集最大结束时间:开始时间等于结束时间
+r($project1) && p('maxEnd')   && e('2023-06-12'); //获取二级项目集下唯一项目最大结束时间
+r($project2) && p('maxEnd')   && e('2023-07-21'); //获取一级项目集下项目最大结束时间
+r($project3) && p('maxEnd')   && e('2023-05-20'); //获取二级项目集下多个项目最大结束时间
