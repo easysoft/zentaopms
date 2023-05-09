d7706f30959a2e86e960734b1715078d014531b1
dingguodong <dingguodong@easycorp.ltd>
2023-05-09 20:49:53 +0800

    * Correct parameter e() for ztf.

--- module/todo/test/model/close.php
+++ module/todo/test/model/close.php
@@ -16,8 +16,8 @@ $tester->loadModel('todo');
 zdTable('todo')->config('close')->gen(1);
 
 r($tester->todo->getByID(1)) && p('status') && e('wait');   // 判断生成的待办的状态是等待状态
-r($tester->todo->close(1))   && p()         && e(1);        // 关闭这个待办
+r($tester->todo->close(1))   && p()         && e('1');      // 关闭这个待办
 r($tester->todo->getByID(1)) && p('status') && e('closed'); // 判断关闭了的待办的状态是关闭状态
 
-r($tester->todo->close(1))   && p()         && e(1);        // 测试关闭已经关闭了的待办
+r($tester->todo->close(1))   && p()         && e('1');      // 测试关闭已经关闭了的待办
 r($tester->todo->getByID(1)) && p('status') && e('closed'); // 判断关闭了待办的状态是关闭状态
