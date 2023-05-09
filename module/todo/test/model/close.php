5c895909ee9b2c3b919f3f5718bf4427a56023bb
dingguodong <dingguodong@easycorp.ltd>
2023-05-09 09:07:32 +0800

    * Add comment for UTC.

--- module/todo/test/model/close.php
+++ module/todo/test/model/close.php
@@ -6,11 +6,8 @@ include dirname(__FILE__, 5) . "/test/lib/init.php";
 su('admin');
 
 /**
-
-title=测试 todoModel->close();
+title=测试关闭待办 todoModel::close();
 cid=1
-pid=1
-
 */
 
 global $tester;
@@ -18,9 +15,9 @@ $tester->loadModel('todo');
 
 zdTable('todo')->config('close')->gen(1);
 
-r($tester->todo->getByID(1)) && p('status') && e('wait');
-r($tester->todo->close(1))   && p()         && e(1);
-r($tester->todo->getByID(1)) && p('status') && e('closed');
+r($tester->todo->getByID(1)) && p('status') && e('wait');   // 判断生成的待办的状态是等待状态
+r($tester->todo->close(1))   && p()         && e(1);        // 关闭这个待办
+r($tester->todo->getByID(1)) && p('status') && e('closed'); // 判断关闭了的待办的状态是关闭状态
 
-r($tester->todo->close(1))   && p()         && e(1);
-r($tester->todo->getByID(1)) && p('status') && e('closed');
+r($tester->todo->close(1))   && p()         && e(1);        // 测试关闭已经关闭了的待办
+r($tester->todo->getByID(1)) && p('status') && e('closed'); // 判断关闭了待办的状态是关闭状态
