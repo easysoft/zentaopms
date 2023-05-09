d6c8fb4194dd3102981f6a5f9cffe4e47a57ac3c
caoyanyi <caoyanyi@easycorp.ltd>
2023-05-09 14:54:00 +0800

    * Remoe '@' symbol.

--- framework/base/router.class.php
+++ framework/base/router.class.php
@@ -3003,8 +3003,9 @@ class baseRouter
         $sqlLog = $this->getLogRoot() . 'sql.' . date('Ymd') . '.log.php';
         if(!is_file($sqlLog)) file_put_contents($sqlLog, "<?php\n die();\n?" . ">\n");
 
-        $fh = @fopen($sqlLog, 'a');
-        if(!$fh) return false;
+        if(!is_writable($sqlLog)) return false;
+
+        $fh = fopen($sqlLog, 'a');
         fwrite($fh, date('Ymd H:i:s') . ": " . $this->getURI() . "\n");
         foreach(dao::$querys as $query) fwrite($fh, "  $query\n");
         fwrite($fh, "\n");
@@ -3492,7 +3493,7 @@ class ztSessionHandler
         $time = time();
         foreach(glob("$this->sessSavePath/sess_*") as $fileName)
         {
-            if(filemtime($fileName) + $maxlifeTime < $time) @unlink($fileName);
+            if(is_writable($fileName) and filemtime($fileName) + $maxlifeTime < $time) unlink($fileName);
         }
         return true;
     }
