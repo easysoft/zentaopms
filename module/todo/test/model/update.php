d2860b956a4f45b7756800a772c0e525752c11e2
lanzongjun <lanzongjun@easycorp.ltd>
2023-05-09 20:40:46 +0800

    * Add todo update test case comments.

--- module/todo/test/model/update.php
+++ module/todo/test/model/update.php
@@ -27,6 +27,6 @@ $t_uptype = array('type' => 'bug', 'objectID' => '1');
 $t_unname = array('name' => '我的待办2');
 
 $todo = new todoTest();
-r($todo->updateTest(1, $t_upname)) && p('0:field,old,new') && e('name,我的待办1,我的待办11');
-r($todo->updateTest(1, $t_uptype)) && p('0:field,old,new') && e('type,custom,bug');
-r($todo->updateTest(2, $t_unname)) && p()                  && e('没有数据更新');
+r($todo->updateTest(1, $t_upname)) && p('0:field,old,new') && e('name,我的待办1,我的待办11'); // 更新待办数据名称
+r($todo->updateTest(1, $t_uptype)) && p('0:field,old,new') && e('type,custom,bug');           // 更新待办数据类型
+r($todo->updateTest(2, $t_unname)) && p()                  && e('没有数据更新');              // 不更新待办数据的情况
