ed740c65acb7eff330914f171952ec105f50b295
chaideqing <chaideqing@easycorp.ltd>
2023-05-09 20:38:59 +0800

    * Refactor project model function getBudgetUnitList.

--- module/project/test/model/getbudgetunitlist.php
+++ module/project/test/model/getbudgetunitlist.php
@@ -5,14 +5,18 @@ include dirname(__FILE__, 5) . "/test/lib/init.php";
 /**
 
 title=测试 projectModel::getBudgetUnitList();
+timeout=0
 cid=1
 pid=1
 
-获取系统中的货币单位 >> 人民币,美元
+- 执行project模块的getBudgetUnitList方法
+ - 属性CNY @人民币
+ - 属性USD @美元
 
 */
 
 global $tester;
 $tester->loadModel('project');
+$tester->config->project->unitList = 'CNY,USD';
 
-r($tester->project->getBudgetUnitList()) && p('CNY,USD') && e('人民币,美元'); //获取系统中的货币单位
+r($tester->project->getBudgetUnitList()) && p('CNY,USD') && e('人民币,美元');
