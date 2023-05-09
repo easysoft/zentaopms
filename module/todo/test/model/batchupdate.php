bd5f43e8dd535760504d7ec3f726600c07a4d2ad
lanzongjun <lanzongjun@easycorp.ltd>
2023-05-09 13:34:03 +0800

    * Add gettodosbyidlist test case and optimized code.

--- module/todo/test/model/batchupdate.php
+++ module/todo/test/model/batchupdate.php
@@ -1,6 +1,6 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . 'test/lib/init.php';
+include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/todo.class.php';
 su('admin');
 
@@ -42,6 +42,6 @@ $changeStatus = $todos;
 $changeStatus[3]->status = 'doing';
 
 $todo = new todoTest();
-r($todo->batchUpdateTest($changeType, 1))   && p('0:field,old,new') && e('type,custom,bug');    // 批量修改todo类型
-r($todo->batchUpdateTest($changePri, 2))    && p('0:field,old,new') && e('pri,3,1');            // 批量修改todo优先级
-r($todo->batchUpdateTest($changeStatus, 3)) && p('0:field,old,new') && e('status,wait,doing');  // 批量修改todo状态
+r($todo->batchUpdateTest($changeType, 1))   && p('0:field,old,new') && e('type,custom,bug');   // 批量修改todo类型
+r($todo->batchUpdateTest($changePri, 2))    && p('0:field,old,new') && e('pri,3,1');           // 批量修改todo优先级
+r($todo->batchUpdateTest($changeStatus, 3)) && p('0:field,old,new') && e('status,wait,doing'); // 批量修改todo状态
---
+++ module/todo/test/model/gettodosbyidlist.php
@@ -0,0 +1,28 @@
+#!/usr/bin/env php
+<?php
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+su('admin');
+
+function initData()
+{
+    zdTable('todo')->config('gettodosbyidlist')->gen(5);
+}
+
+/**
+
+title=测试 todoModel->getTodosByIdList();
+timeout=0
+cid=1
+
+*/
+
+initData();
+
+global $tester;
+$tester->loadModel('todo');
+
+$todoIdList      = array(1, 2, 3);
+$todoIdListError = array(110);
+
+r($tester->todo->gettodosbyidlist($todoIdList))      && p('1:name') && e('自定义的待办1'); // 获取数据库中存在的待办信息
+r($tester->todo->gettodosbyidlist($todoIdListError)) && p()         && e('0');           // 获取数据库中不存在的待办信息
