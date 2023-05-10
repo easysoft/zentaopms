393cac71e06f3c8f32f164ebf90a3e1424776342
lanzongjun <lanzongjun@easycorp.ltd>
2023-05-10 10:26:41 +0800

    * Modify todo update test case.

--- module/todo/test/model/batchupdate.php
+++ module/todo/test/model/batchupdate.php
@@ -3,7 +3,6 @@
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/todo.class.php';
 su('admin');
-
 function initData()
 {
     zdTable('todo')->config('batchupdate')->gen(5);
@@ -19,29 +18,24 @@ cid=1
 
 initData();
 
-$todos = array();
-$todos[1] = new stdclass();
-$todos[2] = new stdclass();
-$todos[3] = new stdclass();
-$changeType = $todos;
+$changeType = array();
+$changeType[1]           = new stdclass();
 $changeType[1]->type     = 'bug';
+$changeType[1]->status   = 'wait';
 $changeType[1]->objectID = 1;
 
-$todos = array();
-$todos[1] = new stdclass();
-$todos[2] = new stdclass();
-$todos[3] = new stdclass();
-$changePri = $todos;
-$changePri[2]->pri = 1;
+$changePri = array();
+$changePri[2]         = new stdclass();
+$changePri[2]->pri    = '1';
+$changePri[2]->status = 'wait';
+$changePri[2]->type   = 'custom';
 
-$todos = array();
-$todos[1] = new stdclass();
-$todos[2] = new stdclass();
-$todos[3] = new stdclass();
-$changeStatus = $todos;
+$changeStatus = array();
+$changeStatus[3]         = new stdclass();
 $changeStatus[3]->status = 'doing';
+$changeStatus[3]->type   = 'custom';
 
 $todo = new todoTest();
-r($todo->batchUpdateTest($changeType, 1))   && p('0:field,old,new') && e('type,custom,bug');   // 批量修改todo类型
-r($todo->batchUpdateTest($changePri, 2))    && p('0:field,old,new') && e('pri,3,1');           // 批量修改todo优先级
-r($todo->batchUpdateTest($changeStatus, 3)) && p('0:field,old,new') && e('status,wait,doing'); // 批量修改todo状态
+r($todo->batchUpdateTest($changeType, 1))   && p('0:field,old,new')  && e('type,custom,bug');   // 批量修改todo类型
+r($todo->batchUpdateTest($changePri, 2))    && p('0:field,old,new')  && e('pri,3,1');           // 批量修改todo优先级
+r($todo->batchUpdateTest($changeStatus, 3)) && p('0:field,old,new')  && e('status,wait,doing'); // 批量修改todo状态
