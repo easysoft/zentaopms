c6f7ef2644866f07617e51a62b1520cc9384b98e
liuhong <liuhong@easycorp.ltd>
2023-05-10 14:18:15 +0800

    * Submit projectModel::getExecutionMembers unit test

---
+++ module/project/test/model/getexecutionmembers.php
@@ -0,0 +1,29 @@
+#!/usr/bin/env php
+<?php
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+su('admin');
+
+zdTable('project')->config('project')->gen(7);
+zdTable('team')->config('team')->gen(4);
+zdTable('user')->config('user')->gen(3);
+
+/**
+
+title=测试 projectModel->getExecutionMembers();
+timeout=0
+cid=1
+
+- 获取admin的执行键值对总数 @2
+
+- 获取admin的执行键值对 @迭代1-2
+
+*/
+
+global $tester;
+$tester->loadModel('project');
+
+$executionIdList  = array(2, 4, 5, 7);
+$executionMembers = $tester->project->getExecutionMembers('admin', $executionIdList);
+
+r(count($executionMembers)) && p() && e('2');       // 获取admin的执行键值对总数
+r($executionMembers[2])     && p() && e('迭代1-2'); // 获取admin的执行键值对
