4dc1db7fa8373964c65127b26cedac5601d5bc6e
tianshujie <tianshujie@easycorp.ltd>
2023-05-11 13:23:00 +0800

    \#Remove this unused "$appName" local variable.

--- module/release/model.php
+++ module/release/model.php
@@ -630,17 +630,12 @@ class releaseModel extends model
         $this->app->loadConfig('mail');
 
         /* Load module and get vars. */
-        $users   = $this->loadModel('user')->getPairs('noletter');
         $release = $this->getByID($releaseID);
         $suffix  = empty($release->product) ? '' : ' - ' . $this->loadModel('product')->getById($release->product)->name;
         $subject = 'Release #' . $release->id . ' ' . $release->name . $suffix;
 
-        $stories  = $this->dao->select('*')->from(TABLE_STORY)->where('id')->in($release->stories)->andWhere('deleted')->eq(0)->fetchAll('id');
-        $bugs     = $this->dao->select('*')->from(TABLE_BUG)->where('id')->in($release->bugs)->andWhere('deleted')->eq(0)->fetchAll();
-        $leftBugs = $this->dao->select('*')->from(TABLE_BUG)->where('id')->in($release->leftBugs)->andWhere('deleted')->eq(0)->fetchAll();
-
         /* Get mail content. */
-        $modulePath = $this->app->getModulePath($appName = '', 'release');
+        $modulePath = $this->app->getModulePath('', 'release');
         $oldcwd     = getcwd();
         $viewFile   = $modulePath . 'view/sendmail.html.php';
         chdir($modulePath . 'view');
