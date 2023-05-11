664727d6cebc538ea26f7fbc2f05ea25037363b3
zenggang <zenggang@easycorp.ltd>
2023-05-11 11:09:27 +0000

    * Task batchedit method and adjust code style

--- module/task/control.php
+++ module/task/control.php
@@ -30,17 +30,17 @@ class task extends control
      * 创建一个任务。
      * Create a task.
      *
-     * @param  string $executionID
-     * @param  string $storyID
-     * @param  string $moduleID
-     * @param  string $taskID
-     * @param  string $todoID
+     * @param  int    $executionID
+     * @param  int    $storyID
+     * @param  int    $moduleID
+     * @param  int    $taskID
+     * @param  int    $todoID
      * @param  string $extra
-     * @param  string $bugID
+     * @param  int    $bugID
      * @access public
      * @return void
      */
-    public function create(string $executionID = '0', string $storyID = '0', string $moduleID = '0', string $taskID = '0', string $todoID = '0', string $extra = '', string $bugID = '0')
+    public function create(int $executionID = 0, int $storyID = 0, int $moduleID = 0, int $taskID = 0, int $todoID = 0, string $extra = '', int $bugID = 0)
     {
         /* Analytic parameter. */
         $extra = str_replace(array(',', ' '), array('&', ''), $extra);
@@ -95,23 +95,19 @@ class task extends control
     /**
      * Batch create task.
      *
-     * @param  string $executionID
-     * @param  string $storyID
-     * @param  string $moduleID
-     * @param  string $taskID
+     * @param  int    $executionID
+     * @param  int    $storyID
+     * @param  int    $moduleID
+     * @param  int    $taskID
      * @param  string $iframe
      * @param  string $extra
      * @access public
      * @return void
      */
-    public function batchCreate(string $executionID, string $storyID = '', string $moduleID = '', string $taskID = '', string $extra = '')
+    public function batchCreate(int $executionID, int $storyID = 0, int $moduleID = 0, int $taskID = 0, string $extra = '')
     {
         /* Init vars. */
-        $executionID = (int)$executionID;
-        $storyID     = (int)$storyID;
-        $moduleID    = (int)$moduleID;
-        $taskID      = (int)$taskID;
-        $extra       = str_replace(array(',', ' '), array('&', ''), $extra);
+        $extra = str_replace(array(',', ' '), array('&', ''), $extra);
         parse_str($extra, $output);
 
         /* 判断不能访问的执行。 Judge execution without access. */
@@ -158,14 +154,13 @@ class task extends control
      * 编辑一个任务。
      * Edit a task.
      *
-     * @param  string $taskID
+     * @param  int    $taskID
      * @param  string $from   ''|taskkanban
      * @access public
      * @return void
      */
-    public function edit(string $taskID, string $from = '')
+    public function edit(int $taskID, string $from = '')
     {
-        $taskID = (int)$taskID;
         $this->taskZen->commonAction($taskID);
 
         if(!empty($_POST))
@@ -202,156 +197,47 @@ class task extends control
     }
 
     /**
+     * 批量编辑任务。
      * Batch edit task.
      *
      * @param  int    $executionID
      * @access public
      * @return void
      */
-    public function batchEdit($executionID = 0)
+    public function batchEdit(int $executionID = 0)
     {
         if($this->post->names)
         {
-            $allChanges = $this->task->batchUpdate();
-            if(dao::isError()) return print(js::error(dao::getError()));
+            /* Batch edit tasks. */
+            $postData   = form::data($this->config->task->form->batchEdit)->get();
+            $allChanges = $this->task->batchUpdate($postData);
+            if(dao::isError()) return $this->send(array('result' => 'fail', 'message' => dao::getError()));
 
-            if(!empty($allChanges))
-            {
-                /* updateStatus is a description of whether to update the responsibility performance*/
-                $waitTaskID = false;
-                foreach($allChanges as $taskID => $changes)
-                {
-                    if(empty($changes)) continue;
-
-                    /* Determine whether the status of a task has been changed, if the status of a task has been changed, set $updateStatus to taskID*/
-                    if($waitTaskID)
-                    {
-                        foreach($changes as $changeField)
-                        {
-                            if($changeField['field'] == 'status' && $changeField['new'] == 'doing')
-                            {
-                                $waitTaskID = $taskID;
-                                break;
-                            }
-                        }
-                    }
-
-                    $actionID = $this->loadModel('action')->create('task', $taskID, 'Edited');
-                    $this->action->logHistory($actionID, $changes);
-
-                    $task = $this->task->getById($taskID);
-                    if($task->fromBug != 0)
-                    {
-                        foreach($changes as $change)
-                        {
-                            if($change['field'] != 'status') continue;
-                            $confirmURL = $this->createLink('bug', 'view', "id=$task->fromBug");
-                            $cancelURL  = $this->server->HTTP_REFERER;
-                            return print(js::confirm(sprintf($this->lang->task->remindBug, $task->fromBug), $confirmURL, $cancelURL, 'parent', 'parent'));
-                        }
-                    }
-                    if($waitTaskID !== false) $this->loadModel('common')->syncPPEStatus($waitTaskID);
-                }
-            }
-            $this->loadModel('score')->create('ajax', 'batchOther');
-            return print(js::locate($this->session->taskList, 'parent'));
+            $response = $this->taskZen->responseAfterBatchEdit($allChanges);
+            return $this->send($response);
         }
 
-        if(!$this->post->taskIDList) return print(js::locate($this->session->taskList, 'parent'));
+        if(!$this->post->taskIDList) return $this->locate($this->session->taskList);
+        $taskIdList = array_unique($this->post->taskIDList);
 
-        $taskIDList = array_unique($this->post->taskIDList);
+        /* Set parameters based on whether the page is execution or my. */
+        $this->taskZen->batchEdit4Pages($executionID);
 
-        /* The tasks of execution. */
-        if($executionID)
-        {
-            $execution = $this->execution->getById($executionID);
-            $this->execution->setMenu($execution->id);
-
-            /* Set modules and members. */
-            $showAllModule = isset($this->config->task->allModule) ? $this->config->task->allModule : '';
-            $modules       = $this->tree->getTaskOptionMenu($executionID, 0, 0, $showAllModule ? 'allModule' : '');
-            $modules       = array('ditto' => $this->lang->task->ditto) + $modules;
-
-            $this->view->title      = $execution->name . $this->lang->colon . $this->lang->task->batchEdit;
-            $this->view->position[] = html::a($this->createLink('execution', 'browse', "executionID=$execution->id"), $execution->name);
-            $this->view->execution  = $execution;
-            $this->view->modules    = $modules;
-        }
-        /* The tasks of my. */
-        else
-        {
-            /* Set my menu. */
-            $this->loadModel('my');
-            $this->lang->my->menu->work['subModule'] = 'task';
-
-            $this->view->position[] = html::a($this->createLink('my', 'task'), $this->lang->my->task);
-            $this->view->title      = $this->lang->task->batchEdit;
-            $this->view->users      = $this->loadModel('user')->getPairs('noletter');
-        }
-
-        /* Get edited tasks. */
-        $tasks = $this->dao->select('*')->from(TABLE_TASK)->where('id')->in($taskIDList)->fetchAll('id');
-        $teams = $this->dao->select('*')->from(TABLE_TASKTEAM)->where('task')->in($taskIDList)->fetchGroup('task', 'id');
-
-        /* Get execution teams. */
-        $executionIDList = array();
-        foreach($tasks as $task) if(!in_array($task->execution, $executionIDList)) $executionIDList[] = $task->execution;
-        $executionTeams = $this->dao->select('*')->from(TABLE_TEAM)->where('root')->in($executionIDList)->andWhere('type')->eq('execution')->fetchGroup('root', 'account');
-
-        /* Judge whether the editedTasks is too large and set session. */
-        $countInputVars  = count($tasks) * (count(explode(',', $this->config->task->custom->batchEditFields)) + 3);
-        $showSuhosinInfo = common::judgeSuhosinSetting($countInputVars);
-        if($showSuhosinInfo) $this->view->suhosinInfo = extension_loaded('suhosin') ? sprintf($this->lang->suhosinInfo, $countInputVars) : sprintf($this->lang->maxVarsInfo, $countInputVars);
-
-        /* Set Custom*/
-        if(isset($execution))
-        {
-            foreach(explode(',', $this->config->task->customBatchEditFields) as $field)
-            {
-                if($execution->type == 'stage' and strpos('estStarted,deadline', $field) !== false) continue;
-                $customFields[$field] = $this->lang->task->$field;
-            }
-        }
-        else
-        {
-            foreach(explode(',', $this->config->task->customBatchEditFields) as $field) $customFields[$field] = $this->lang->task->$field;
-        }
-
-        $this->view->customFields = $customFields;
-        $this->view->showFields   = $this->config->task->custom->batchEditFields;
-
-        /* Assign. */
-        $this->view->position[]     = $this->lang->task->common;
-        $this->view->position[]     = $this->lang->task->batchEdit;
-        $this->view->executionID    = $executionID;
-        $this->view->priList        = array('0' => '', 'ditto' => $this->lang->task->ditto) + $this->lang->task->priList;
-        $this->view->statusList     = array('' => '',  'ditto' => $this->lang->task->ditto) + $this->lang->task->statusList;
-        $this->view->typeList       = array('' => '',  'ditto' => $this->lang->task->ditto) + $this->lang->task->typeList;
-        $this->view->taskIDList     = $taskIDList;
-        $this->view->tasks          = $tasks;
-        $this->view->teams          = $teams;
-        $this->view->executionTeams = $executionTeams;
-        $this->view->executionName  = isset($execution) ? $execution->name : '';
-        $this->view->executionType  = isset($execution) ? $execution->type : '';
-        $this->view->users          = $this->loadModel('user')->getPairs('nodeleted');
-
-        $this->display();
+        $this->taskZen->buildBatchEditForm($taskIdList, $executionID);
     }
 
     /**
      * 指派任务。
      * Update assign of task.
      *
-     * @param  stirng $executionID
-     * @param  string $taskID
+     * @param  int    $executionID
+     * @param  int    $taskID
      * @param  string $from        ''|taskkanban
      * @access public
      * @return void
      */
-    public function assignTo(string $executionID, string $taskID, string $from = '')
+    public function assignTo(int $executionID, int $taskID, string $from = '')
     {
-        $executionID = (int)$executionID;
-        $taskID      = (int)$taskID;
         $this->taskZen->commonAction($taskID);
 
         if(!empty($_POST))
@@ -407,15 +293,14 @@ class task extends control
      * 批量指派任务。
      * Batch update assign of task.
      *
-     * @param  string    $execution
+     * @param  int    $executionID
      * @access public
      * @return void
      */
-    public function batchAssignTo(string $executionID)
+    public function batchAssignTo(int $executionID)
     {
         if(!empty($_POST))
         {
-            $executionID = (int)$executionID;
             if(!is_array($this->post->taskIDList)) return print(js::locate($this->createLink('execution', 'task', "executionID={$executionID}"), 'parent'));
 
             $this->loadModel('action');
@@ -1877,16 +1762,14 @@ class task extends control
      * 管理多人任务的团队。
      * Update assign of multi task.
      *
-     * @param  string $executionID
-     * @param  string $taskID
+     * @param  int    $executionID
+     * @param  int    $taskID
      * @param  string $from        ''|taskkanban
      * @access public
      * @return void
      */
-    public function manageTeam(string $executionID, string $taskID, string $from = '')
+    public function manageTeam(int $executionID, int $taskID, string $from = '')
     {
-        $executionID = (int)$executionID;
-        $taskID      = (int)$taskID;
         $this->taskZen->commonAction($taskID);
 
         if(!empty($_POST))
@@ -1910,6 +1793,6 @@ class task extends control
 
         $this->view->members = $this->loadModel('user')->getTeamMemberPairs($executionID, 'execution', 'nodeleted');
         $this->view->users   = $this->loadModel('user')->getPairs();
-        $this->display('', 'editteam');
+        $this->display();
     }
 }
