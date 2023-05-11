5b03aabb962d53ce79bfec98d456d9d2cc7aa736
guofeilong <guofeilong@easycorp.ltd>
2023-05-11 19:36:46 +0800

    * Fix notes problem in project model update function .

--- module/project/model.php
+++ module/project/model.php
@@ -1528,10 +1528,10 @@ class projectModel extends model
 
         /* 更新项目的关联信息。*/
         /* Update relation info of this project. */
-        $this->updateUserView($projectID, $project->acl);                     // 更新用户视图。
-        $this->updateShadowProduct($project, $oldProject);                    // 更新影子产品关联信息。
-        $this->updateWhitelist($project, $oldProject);                        // 更新关联的白名单列表。
-        $this->updateProductStage($projectID, $oldProject->stageBy);          // 更新关联的所有产品的阶段。
+        $this->updateUserView($projectID, $project->acl);            // 更新用户视图。
+        $this->updateShadowProduct($project, $oldProject);           // 更新影子产品关联信息。
+        $this->updateWhitelist($project, $oldProject);               // 更新关联的白名单列表。
+        $this->updateProductStage($projectID, $oldProject->stageBy); // 更新关联的所有产品的阶段。
 
         $this->file->updateObjectID($this->post->uid, $projectID, 'project'); // 通过uid更新文件id。
 
