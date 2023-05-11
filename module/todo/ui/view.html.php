225d613b9da04d7aaae8facd7d92bc6301e2b0c4
liuyue <liuyue@easycorp.ltd>
2023-05-11 02:55:23 +0000

    * Update comment of view page.

--- module/todo/ui/view.html.php
+++ module/todo/ui/view.html.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 /**
- * The view file of view method of todo module of ZenTaoPMS.
+ * The view file of todo module of ZenTaoPMS.
  *
  * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
  * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
@@ -121,7 +121,7 @@ function buildBtnGroup(object $todo, object $config, object $session, object $ap
                         set::items($toolbarDropdownItems)
                     )
                 )
-            ): null
+            ) : null
         )
     );
 }
