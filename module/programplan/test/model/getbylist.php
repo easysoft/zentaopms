d6fddf8eb898ca226f810f483adf185699fa560c
jukui <jukui@easycorp.ltd>
2023-05-11 13:12:44 +0800

    * Modify test case of getByList function.

--- module/programplan/test/model/getbylist.php
+++ module/programplan/test/model/getbylist.php
@@ -4,26 +4,27 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/programplan.class.php';
 su('admin');
 
+zdTable('project')->config('project')->gen(8);
 /**
 
 title=测试 programplanModel->getByList();
 cid=1
 pid=1
 
-测试获取项目11 12的信息 >> 项目1;项目2
-测试获取项目13 14的信息 >> 项目3;项目4
-测试获取项目15 16 17的信息 >> 项目5;项目6;项目7
-测试获取项目18 19 20的信息 >> 项目8;项目9;项目10
-测试获取项目21 22 23 24的信息 >> 项目11;项目12;项目13;项目14
-
 */
 
-$planIDList = array(array(11, 12), array(13, 14), array(15, 16, 17), array(18, 19, 20), array(21, 22, 23, 24));
+$planIDList = array(array(2), array(3, 4), array(5, 6, 7), array(10000));
 
 $programplan = new programplanTest();
 
-r($programplan->getByListTest($planIDList[0])) && p('11:name;12:name')                 && e('项目1;项目2');                 // 测试获取项目11 12的信息
-r($programplan->getByListTest($planIDList[1])) && p('13:name;14:name')                 && e('项目3;项目4');                 // 测试获取项目13 14的信息
-r($programplan->getByListTest($planIDList[2])) && p('15:name;16:name;17:name')         && e('项目5;项目6;项目7');           // 测试获取项目15 16 17的信息
-r($programplan->getByListTest($planIDList[3])) && p('18:name;19:name;20:name')         && e('项目8;项目9;项目10');          // 测试获取项目18 19 20的信息
-r($programplan->getByListTest($planIDList[4])) && p('21:name;22:name;23:name;24:name') && e('项目11;项目12;项目13;项目14'); // 测试获取项目21 22 23 24的信息
+r($programplan->getByListTest($planIDList[0])) && p('2:name')   && e('瀑布项目2'); // 测试获取项目2的名称
+r($programplan->getByListTest($planIDList[0])) && p('2:status') && e('doing');     // 测试获取项目2的状态
+
+$result = $programplan->getByListTest($planIDList[1]);
+r(count($result)) && p() && e('2'); // 测试获取项目3 4的取出的数量
+
+$result = $programplan->getByListTest($planIDList[2]);
+r($result[5]) && p('name,desc') && e('瀑布项目5,项目描述5'); // 测试获取项目5的名称和描述信息
+r(count($result)) && p() && e('3');                          // 测试获取5 6 7 项目数量
+
+r($programplan->getByListTest($planIDList[3])) && p() && e('0'); // 测试获取不存在的项目信息
