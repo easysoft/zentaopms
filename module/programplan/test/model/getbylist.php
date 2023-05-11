6a3799f68b7fe973a3a0477670072e1870dafb68
jukui <jukui@easycorp.ltd>
2023-05-11 13:31:49 +0800

    * Modify format for getbylist test case.

--- module/programplan/test/model/getbylist.php
+++ module/programplan/test/model/getbylist.php
@@ -17,14 +17,13 @@ $planIDList = array(array(2), array(3, 4), array(5, 6, 7), array(10000));
 
 $programplan = new programplanTest();
 
-r($programplan->getByListTest($planIDList[0])) && p('2:name')   && e('瀑布项目2'); // 测试获取项目2的名称
-r($programplan->getByListTest($planIDList[0])) && p('2:status') && e('doing');     // 测试获取项目2的状态
-
-$result = $programplan->getByListTest($planIDList[1]);
-r(count($result)) && p() && e('2'); // 测试获取项目3 4的取出的数量
-
-$result = $programplan->getByListTest($planIDList[2]);
-r($result[5]) && p('name,desc') && e('瀑布项目5,项目描述5'); // 测试获取项目5的名称和描述信息
-r(count($result)) && p() && e('3');                          // 测试获取5 6 7 项目数量
-
-r($programplan->getByListTest($planIDList[3])) && p() && e('0'); // 测试获取不存在的项目信息
+$result  = $programplan->getByListTest($planIDList[0]);
+$result2 = $programplan->getByListTest($planIDList[1]);
+$result3 = $programplan->getByListTest($planIDList[2]);
+$result4 = $programplan->getByListTest($planIDList[3]);
+r($result[2])      && p('name')      && e('瀑布项目2');           // 测试获取项目2的名称
+r($result[2])      && p('status')  && e('doing');                 // 测试获取项目2的状态
+r(count($result2)) && p()            && e('2');                   // 测试获取项目3 4的取出的数量
+r($result3[5])     && p('name,desc') && e('瀑布项目5,项目描述5'); // 测试获取项目5的名称和描述信息
+r(count($result3)) && p()            && e('3');                   // 测试获取5 6 7 项目数量
+r($result4) && p() &&                e('0');                      // 测试获取不存在的项目信息
