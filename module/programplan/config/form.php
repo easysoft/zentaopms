c93c5df56a3a57dd5c289ab470d7754782a1ffa7
dingguodong <dingguodong@easycorp.ltd>
2023-05-11 15:13:27 +0800

    * Fix overrided variable.

--- module/programplan/config/form.php
+++ module/programplan/config/form.php
@@ -6,7 +6,7 @@ $config->programplan->create->form       = array();
 $config->programplan->create->formFields = array('planIDList', 'names', 'PM', 'percents', 'attributes', 'acl', 'milestone', 'begin', 'end', 'realBegan', 'realEnd', 'desc', 'orders', 'type');
 foreach($config->programplan->create->formFields as $field) $config->programplan->create->form[$field] = array('required' => false, 'type' => 'array');
 
-$config->programplan->edit = new stdClass();
+if(!isset($config->programplan->edit)) $config->programplan->edit = new stdClass();
 $config->programplan->edit->form = array();
 $config->programplan->edit->form['begin']     = array('required' => false, 'type' => 'string',     'default' => '0000-00-00');
 $config->programplan->edit->form['end']       = array('required' => false, 'type' => 'string',     'default' => '0000-00-00');
