deb7222dc9b6b14d43bf83ef7dc4f4e89dac1de0
dingguodong <dingguodong@easycorp.ltd>
2023-05-11 13:47:28 +0800

    * Change the comment in file header.

--- module/programplan/model.php
+++ module/programplan/model.php
@@ -4,12 +4,11 @@ declare(strict_types=1);
 /**
  * The model file of programplan module of ZenTaoPMS.
  *
- * @copyright   Copyright 2009-2015 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.cnezsoft.com)
- * @license     ZPL(http://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
  * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
  * @package     programplan
- * @version     $Id: model.php 5079 2013-07-10 00:44:34Z chencongzhi520@gmail.com $
- * @link        http://www.zentao.net
+ * @link        https://www.zentao.net
  */
 class programplanModel extends model
 {
