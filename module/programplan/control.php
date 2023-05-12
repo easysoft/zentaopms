cfd75b5082e71935434c8120106fd82e03686d4f
xushenjie <xushenjie@easycorp.ltd>
2023-05-12 01:54:23 +0000

    * Update comment of programplanControl.

--- module/programplan/control.php
+++ module/programplan/control.php
@@ -2,14 +2,13 @@
 declare(strict_types=1);
 
 /**
- * The control file of programplan currentModule of ZenTaoPMS.
+ * The control file of programplan module of ZenTaoPMS.
  *
- * @copyright   Copyright 2009-2015 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.cnezsoft.com)
- * @license     ZPL(http://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
  * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
  * @package     programplan
- * @version     $Id: control.php 5107 2013-07-12 01:46:12Z chencongzhi520@gmail.com $
- * @link        http://www.zentao.net
+ * @link        https://www.zentao.net
  */
 class programplan extends control
 {
