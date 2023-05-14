fd24f63d113c11d9568a643dc3258d03123a4952
Yagami <sunguangming@easycorp.ltd>
2023-05-14 11:19:10 +0800

    * Fix bug #35120.

--- module/project/model.php
+++ module/project/model.php
@@ -1252,6 +1252,7 @@ class projectModel extends model
             ->setIF($this->post->acl   == 'open', 'whitelist', '')
             ->setIF(!isset($_POST['whitelist']), 'whitelist', '')
             ->setIF(!isset($_POST['multiple']), 'multiple', '1')
+            ->setIF($this->post->model == 'ipd', 'division', '0')
             ->setDefault('openedBy', $this->app->user->account)
             ->setDefault('openedDate', helper::now())
             ->setDefault('team', $this->post->name)
