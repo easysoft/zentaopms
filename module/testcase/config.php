5760d75024feadfeb88e73801acb888982d3d8f0
chaideqing <chaideqing@easycorp.ltd>
2023-05-17 13:25:33 +0800

    * Fix bug#35105, add the configuration item of whether to use datatabel in config.php of testcase.

--- module/testcase/config.php
+++ module/testcase/config.php
@@ -3,6 +3,7 @@ $config->testcase = new stdclass();
 $config->testcase->defaultSteps = 3;
 $config->testcase->batchCreate  = 10;
 $config->testcase->needReview   = 0;
+$config->testcase->useDatatable = false;
 
 $config->testcase->create = new stdclass();
 $config->testcase->edit   = new stdclass();
