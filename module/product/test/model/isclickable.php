33f6a60a6dec16ff0c8415e6a219c56747ac3b9f
zhouxin <zhouxin@easycorp.ltd>
2023-05-19 01:40:32 +0000

    * Add unit test to product-isClickable.

--- module/product/test/model/isclickable.php
+++ module/product/test/model/isclickable.php
@@ -2,27 +2,37 @@
 <?php
 include dirname(__FILE__, 5) . "/test/lib/init.php";
 include dirname(__FILE__, 2) . '/product.class.php';
-
 su('admin');
+
+zdTable('product')->config('product')->gen(6);
 
 /**
 
 title=测试productModel->isClickable();
+timeout=0
 cid=1
-pid=1
-
-status为normal,action为close >> true
-status为close,action为close >> false
-status为normal,action为start >> true
-status为close,action为start >> true
 
 */
-
 $product = new productTest('admin');
 
-$t_status = array('2', '75', 'close', 'start');
+$normalProducts = array(1, 2, 3);
+$closedProducts = array(4, 5, 6);
 
-r($product->testIsClickable($t_status[0], $t_status[2])) && p() && e('true');  // status为normal,action为close
-r($product->testIsClickable($t_status[1], $t_status[2])) && p() && e('false'); // status为close,action为close
-r($product->testIsClickable($t_status[0], $t_status[3])) && p() && e('true');  // status为normal,action为start
-r($product->testIsClickable($t_status[1], $t_status[3])) && p() && e('true');  // status为close,action为start
+r($product->testIsClickable($normalProducts[0], 'start')) && p() && e('1');  // 产品1状态为为normal,action为start
+r($product->testIsClickable($normalProducts[1], 'start')) && p() && e('1');  // 产品2状态为为normal,action为start
+r($product->testIsClickable($normalProducts[2], 'start')) && p() && e('1');  // 产品3状态为为normal,action为start
+r($product->testIsClickable($normalProducts[0], 'close')) && p() && e('1');  // 产品1状态为为normal,action为close
+r($product->testIsClickable($normalProducts[1], 'close')) && p() && e('1');  // 产品2状态为为normal,action为close
+r($product->testIsClickable($normalProducts[2], 'close')) && p() && e('1');  // 产品3状态为为normal,action为close
+
+r($product->testIsClickable($closedProducts[0], 'start')) && p() && e('1');  // 产品4状态为为closed,action为start
+r($product->testIsClickable($closedProducts[1], 'start')) && p() && e('1');  // 产品5状态为为closed,action为start
+r($product->testIsClickable($closedProducts[2], 'start')) && p() && e('1');  // 产品6状态为为closed,action为start
+r($product->testIsClickable($closedProducts[0], 'close')) && p() && e('0');  // 产品4状态为为closed,action为close
+r($product->testIsClickable($closedProducts[1], 'close')) && p() && e('0');  // 产品5状态为为closed,action为close
+r($product->testIsClickable($closedProducts[2], 'close')) && p() && e('0');  // 产品6状态为为closed,action为close
+
+r($product->testIsClickable($normalProducts[0], 'START')) && p() && e('1');  // 产品1状态为为normal,action为START
+r($product->testIsClickable($normalProducts[1], 'CLOSE')) && p() && e('1');  // 产品2状态为为normal,action为CLOSE
+r($product->testIsClickable($closedProducts[0], 'START')) && p() && e('1');  // 产品4状态为为closed,action为START
+r($product->testIsClickable($closedProducts[1], 'CLOSE')) && p() && e('0');  // 产品5状态为为closed,action为CLOSE
