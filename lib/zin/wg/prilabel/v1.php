36693f58f22a5d3d49356ff736a81cd84963cd18
dingyongliang <dingyongliang@easycorp.ltd>
2023-05-22 15:32:12 +0800

    * zin: refactor prilabel.

--- lib/zin/wg/prilabel/v1.php
+++ lib/zin/wg/prilabel/v1.php
@@ -17,15 +17,6 @@ class priLabel extends wg
         }
     }
 
-    private function setThemeStyle(int $pri): \zin\directive
-    {
-        if($pri == 1) return setClass('danger-outline');
-        if($pri == 2) return setClass('warning-outline');
-        if($pri == 3) return setClass('secondary-outline');
-        if($pri == 4) return setStyle(array('color' => '#95D3E0', 'box-shadow' => 'rgb(255, 255, 255) 0px 0px 0px 0px, rgb(149, 211, 224) 0px 0px 0px 1px, rgba(0, 0, 0, 0) 0px 0px 0px 0px'));
-        return setClass('gray-outline');
-    }
-
     protected function build(): wg
     {
         $pri = (int)$this->prop('pri');
@@ -33,8 +24,7 @@ class priLabel extends wg
         return span
         (
             set($this->props->skip(array_keys(static::getDefinedProps()))),
-            setClass('center', 'rounded-full', 'aspect-square', 'bg', 'h-4', 'w-4'),
-            $this->setThemeStyle($pri),
+            setClass("pri-$pri"),
             $pri
         );
     }
