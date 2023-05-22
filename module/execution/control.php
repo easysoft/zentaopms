afd0986bb7154e6483fef1fcf000ffc95a437e68
Guanxiying <guanxiying@easycorp.ltd>
2023-05-22 11:14:41 +0800

    * Replace tab with spaces.

--- module/execution/control.php
+++ module/execution/control.php
@@ -1030,8 +1030,8 @@ class execution extends control
         $execution   = $this->commonAction($executionID);
         $project     = $this->loadModel('project')->getByID($execution->project);
         $executionID = $execution->id;
-	$products    = $this->product->getProducts($execution->id);
-	if(count($products) === 1) $productID = current($products)->id;
+        $products    = $this->product->getProducts($execution->id);
+        if(count($products) === 1) $productID = current($products)->id;
 
         if($execution->hasProduct)
         {
