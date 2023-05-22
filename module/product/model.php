cd3277f61435d11a01007436988bccd57582f813
zhouxin <zhouxin@easycorp.ltd>
2023-05-22 07:53:50 +0000

    * Remove unused getLatestProject method.

--- module/product/model.php
+++ module/product/model.php
@@ -1619,25 +1619,6 @@ class productModel extends model
     }
 
     /**
-     * get the latest project of the product.
-     *
-     * @param  int    $productID
-     * @access public
-     * @return object
-     */
-    public function getLatestProject(int $productID): object
-    {
-        return $this->dao->select('t2.id, t2.name')->from(TABLE_PROJECTPRODUCT)->alias('t1')
-            ->leftJoin(TABLE_PROJECT)->alias('t2')->on('t1.project = t2.id')
-            ->where('t1.product')->eq((int)$productID)
-            ->andWhere('t2.status')->ne('closed')
-            ->andWhere('t2.deleted')->eq('0')
-            ->orderBy('t2.begin desc')
-            ->limit(1)
-            ->fetch();
-    }
-
-    /**
      * Change the projects set of the program.
      *
      * @param  int   $productID
