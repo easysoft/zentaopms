bc0a02c0e68a1e3fc0c3f8cf25ff86fe44276871
dingguodong <dingguodong@easycorp.ltd>
2023-05-23 13:28:55 +0800

    * Complete test case for productModel::getProductPairsByProject.

--- module/product/test/model/getproductpairsbyproject.php
+++ module/product/test/model/getproductpairsbyproject.php
@@ -1,30 +1,31 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . "/test/lib/init.php";
+include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/product.class.php';
 
 /**
-
-title=productModel->getProductPairsByProject();
+title=测试通过项目查询产品信息 productModel->getProductPairsByProject();
 cid=1
 pid=1
-
 */
+
+zdTable('project')->gen(50);
+zdTable('projectproduct')->gen(50);
 
 $projectIDList = array('11', '12', '13', '14', '15', '1000001');
 $statusList    = array('all', 'noclosed');
 
 $product = new productTest('admin');
 
-r($product->getProductPairsByProjectTest($projectIDList[0], $statusList[0])) && p('1;81;91') && e('正常产品1;多平台产品81;多平台产品91'); // 测试获取项目11 状态为all的产品信息
-r($product->getProductPairsByProjectTest($projectIDList[1], $statusList[0])) && p('2;82;92') && e('正常产品2;多平台产品82;多平台产品92'); // 测试获取项目12 状态为all的产品信息
-r($product->getProductPairsByProjectTest($projectIDList[2], $statusList[0])) && p('3;83;93') && e('正常产品3;多平台产品83;多平台产品93'); // 测试获取项目13 状态为all的产品信息
-r($product->getProductPairsByProjectTest($projectIDList[3], $statusList[0])) && p('4;84;94') && e('正常产品4;多平台产品84;多平台产品94'); // 测试获取项目14 状态为all的产品信息
-r($product->getProductPairsByProjectTest($projectIDList[4], $statusList[0])) && p('5;85;95') && e('正常产品5;多平台产品85;多平台产品95'); // 测试获取项目15 状态为all的产品信息
-r($product->getProductPairsByProjectTest($projectIDList[5], $statusList[0])) && p()          && e('0');                                   // 测试获取不存在的项目状态为all的产品信息
-r($product->getProductPairsByProjectTest($projectIDList[0], $statusList[1])) && p('1;81;91') && e('正常产品1;多平台产品81;多平台产品91'); // 测试获取项目11 状态为unclosed的产品信息
-r($product->getProductPairsByProjectTest($projectIDList[1], $statusList[1])) && p('2;82;92') && e('正常产品2;多平台产品82;多平台产品92'); // 测试获取项目12 状态为unclosed的产品信息
-r($product->getProductPairsByProjectTest($projectIDList[2], $statusList[1])) && p('3;83;93') && e('正常产品3;多平台产品83;多平台产品93'); // 测试获取项目13 状态为unclosed的产品信息
-r($product->getProductPairsByProjectTest($projectIDList[3], $statusList[1])) && p('4;84;94') && e('正常产品4;多平台产品84;多平台产品94'); // 测试获取项目14 状态为unclosed的产品信息
-r($product->getProductPairsByProjectTest($projectIDList[4], $statusList[1])) && p('5;85;95') && e('正常产品5;多平台产品85;多平台产品95'); // 测试获取项目15 状态为unclosed的产品信息
-r($product->getProductPairsByProjectTest($projectIDList[5], $statusList[1])) && p()          && e('0');                                   // 测试获取不存在的项目状态为unclosed的产品信息
+r($product->getProductPairsByProjectTest($projectIDList[0], $statusList[0])) && p('1') && e('正常产品1'); // 测试获取项目11 状态为all的产品信息
+r($product->getProductPairsByProjectTest($projectIDList[1], $statusList[0])) && p('2') && e('正常产品2'); // 测试获取项目12 状态为all的产品信息
+r($product->getProductPairsByProjectTest($projectIDList[2], $statusList[0])) && p('3') && e('正常产品3'); // 测试获取项目13 状态为all的产品信息
+r($product->getProductPairsByProjectTest($projectIDList[3], $statusList[0])) && p('4') && e('正常产品4'); // 测试获取项目14 状态为all的产品信息
+r($product->getProductPairsByProjectTest($projectIDList[4], $statusList[0])) && p('5') && e('正常产品5'); // 测试获取项目15 状态为all的产品信息
+r($product->getProductPairsByProjectTest($projectIDList[5], $statusList[0])) && p()    && e('0');         // 测试获取不存在的项目状态为all的产品信息
+r($product->getProductPairsByProjectTest($projectIDList[0], $statusList[1])) && p('1') && e('正常产品1'); // 测试获取项目11 状态为unclosed的产品信息
+r($product->getProductPairsByProjectTest($projectIDList[1], $statusList[1])) && p('2') && e('正常产品2'); // 测试获取项目12 状态为unclosed的产品信息
+r($product->getProductPairsByProjectTest($projectIDList[2], $statusList[1])) && p('3') && e('正常产品3'); // 测试获取项目13 状态为unclosed的产品信息
+r($product->getProductPairsByProjectTest($projectIDList[3], $statusList[1])) && p('4') && e('正常产品4'); // 测试获取项目14 状态为unclosed的产品信息
+r($product->getProductPairsByProjectTest($projectIDList[4], $statusList[1])) && p('5') && e('正常产品5'); // 测试获取项目15 状态为unclosed的产品信息
+r($product->getProductPairsByProjectTest($projectIDList[5], $statusList[1])) && p()    && e('0');         // 测试获取不存在的项目状态为unclosed的产品信息
