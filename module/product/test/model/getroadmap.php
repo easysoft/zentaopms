763a82ff1b68085a02ba8f937c99085597229986
dingguodong <dingguodong@easycorp.ltd>
2023-05-23 14:07:49 +0800

    * Complete test case for productModel::getRoadmap().

--- module/product/test/model/getroadmap.php
+++ module/product/test/model/getroadmap.php
@@ -1,30 +1,25 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . "/test/lib/init.php";
+include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/product.class.php';
 
 /**
-
-title=productModel->getRoadmap();
+title=测试获取产品的路线图数量 productModel->getRoadmap();
 cid=1
 pid=1
-
-测试获取产品1的roadmap数量 >> 1
-测试获取产品2的roadmap数量 >> 1
-测试获取产品3的roadmap数量 >> 1
-测试获取产品4的roadmap数量 >> 1
-测试获取产品5的roadmap数量 >> 1
-测试获取不存在产品的roadmap数量 >> 0
-
 */
+
+zdTable('productplan')->gen(500);
+zdTable('product')->gen(50);
+zdTable('release')->gen(500);
 
 $productIDList = array('1', '2', '3', '4', '5', '1000001');
 
 $product = new productTest('admin');
 
-r($product->getRoadmapTest($productIDList[0])) && p('total') && e('1');   // 测试获取产品1的roadmap数量
-r($product->getRoadmapTest($productIDList[1])) && p('total') && e('1');   // 测试获取产品2的roadmap数量
-r($product->getRoadmapTest($productIDList[2])) && p('total') && e('1');   // 测试获取产品3的roadmap数量
-r($product->getRoadmapTest($productIDList[3])) && p('total') && e('1');   // 测试获取产品4的roadmap数量
-r($product->getRoadmapTest($productIDList[4])) && p('total') && e('1');   // 测试获取产品5的roadmap数量
+r($product->getRoadmapTest($productIDList[0])) && p('total') && e('261'); // 测试获取产品1的roadmap数量
+r($product->getRoadmapTest($productIDList[1])) && p('total') && e('107'); // 测试获取产品2的roadmap数量
+r($product->getRoadmapTest($productIDList[2])) && p('total') && e('6');   // 测试获取产品3的roadmap数量
+r($product->getRoadmapTest($productIDList[3])) && p('total') && e('9');   // 测试获取产品4的roadmap数量
+r($product->getRoadmapTest($productIDList[4])) && p('total') && e('12');  // 测试获取产品5的roadmap数量
 r($product->getRoadmapTest($productIDList[5])) && p('total') && e('0');   // 测试获取不存在产品的roadmap数量
