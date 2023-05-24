db662918909e1d977864587a3274be5d5fda1fa0
hufangzhou <hufangzhou@easycorp.ltd>
2023-05-24 11:20:06 +0800

    * Finish task #93587.

--- db/update18.4.alpha1.sql
+++ db/update18.4.alpha1.sql
@@ -4,3 +4,5 @@ ADD `estimate` float NOT NULL DEFAULT '0' AFTER `progress`,
 ADD `left` float NOT NULL DEFAULT '0' AFTER `estimate`,
 ADD `consumed` float NOT NULL DEFAULT '0' AFTER `left`,
 ADD `teamCount` int NOT NULL DEFAULT '0' AFTER `consumed`;
+
+REPLACE INTO zt_privrelation (`priv`, `type`, `relationPriv`) VALUES (59, 'depend', 41),(62, 'depend', 41),(60, 'depend', 41),(61, 'depend', 41),(56, 'depend', 41),(54, 'depend', 41),(53, 'depend', 41),(57, 'depend', 41),(55, 'depend', 41);
