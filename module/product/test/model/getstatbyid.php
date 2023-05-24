8f3f877902dc3ac30d9bcbc0e9d6e10cb5096a81
dingguodong <dingguodong@easycorp.ltd>
2023-05-24 09:42:46 +0800

    * Complete test case for productModel::getStatByID.

--- module/product/test/model/getstatbyid.php
+++ module/product/test/model/getstatbyid.php
@@ -1,23 +1,29 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . "/test/lib/init.php";
+include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/product.class.php';
 
 /**
-
 title=productModel->getStatByID();
 cid=1
 pid=1
-
 */
 
-$productIDList = array('1', '2', '3', '4', '5', '1000001');
+zdTable('story')->gen(50);
+zdTable('productplan')->gen(50);
+zdTable('release')->gen(50);
+zdTable('build')->gen(50);
+zdTable('case')->gen(50);
+zdTable('project')->gen(50);
+zdTable('projectproduct')->gen(50);
+zdTable('bug')->gen(50);
+zdTable('doc')->gen(50);
+
+$productIDList = array('1', '2', '3', '1000001');
 
 $product = new productTest('admin');
 
-r($product->getStatByIDTest($productIDList[0])) && p('name,plans,releases,builds,cases,projects,executions,bugs,docs,progress') && e('正常产品1,0,5,2,7,2,14,9,1,0');   // 测试获取产品1的stat信息
-r($product->getStatByIDTest($productIDList[1])) && p('name,plans,releases,builds,cases,projects,executions,bugs,docs,progress') && e('正常产品2,0,2,2,7,2,14,4,1,0');   // 测试获取产品2的stat信息
-r($product->getStatByIDTest($productIDList[2])) && p('name,plans,releases,builds,cases,projects,executions,bugs,docs,progress') && e('正常产品3,0,0,2,7,2,14,4,1,0');   // 测试获取产品3的stat信息
-r($product->getStatByIDTest($productIDList[3])) && p('name,plans,releases,builds,cases,projects,executions,bugs,docs,progress') && e('正常产品4,0,0,2,7,2,14,4,1,0');   // 测试获取产品4的stat信息
-r($product->getStatByIDTest($productIDList[4])) && p('name,plans,releases,builds,cases,projects,executions,bugs,docs,progress') && e('正常产品5,0,0,2,7,2,14,4,1,0');   // 测试获取产品5的stat信息
-r($product->getStatByIDTest($productIDList[5])) && p('name,plans,releases,builds,cases,projects,executions,bugs,docs,progress') && e('0');                              // 测试获取不存在产品的stat信息
+r($product->getStatByIDTest($productIDList[0])) && p('name,plans,releases,builds,cases,projects,executions,bugs,docs,progress') && e('正常产品1,0,5,4,3,1,25,1,0,25'); // 测试获取产品1的stat信息
+r($product->getStatByIDTest($productIDList[1])) && p('name,plans,releases,builds,cases,projects,executions,bugs,docs,progress') && e('正常产品2,0,5,4,3,1,10,1,0,25'); // 测试获取产品2的stat信息
+r($product->getStatByIDTest($productIDList[2])) && p('name,plans,releases,builds,cases,projects,executions,bugs,docs,progress') && e('正常产品3,0,5,4,3,1,0,1,0,25');  // 测试获取产品3的stat信息
+r($product->getStatByIDTest($productIDList[3])) && p('name,plans,releases,builds,cases,projects,executions,bugs,docs,progress') && e('0');                             // 测试获取不存在产品的stat信息
