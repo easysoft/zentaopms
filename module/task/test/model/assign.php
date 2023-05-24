6a7c9cd469996b854c3d3f770e6e1d84ed09b582
zhaoke <zhaoke@easycorp.ltd>
2023-05-24 20:50:59 +0800

    * Modify test case for assign method.

--- module/task/test/model/assign.php
+++ module/task/test/model/assign.php
@@ -9,71 +9,35 @@ title=taskModel->assign();
 timeout=0
 cid=1
 
-- wait状态任务指派
- - 第0条的field属性 @assignedTo
- - 第0条的old属性 @old1
- - 第0条的new属性 @user92
-
-- wait状态任务指派修改预计剩余
- - 第1条的field属性 @left
- - 第1条的old属性 @0
- - 第1条的new属性 @1
-
-- doing状态任务指派
- - 第0条的field属性 @assignedTo
- - 第0条的old属性 @old2
- - 第0条的new属性 @user93
-
-- doing状态任务指派,预计剩余为0 @『预计剩余』不能为空。
-
-- done状态任务指派
- - 第0条的field属性 @assignedTo
- - 第0条的old属性 @old3
- - 第0条的new属性 @user94
-
-- pause状态任务指派
- - 第0条的field属性 @assignedTo
- - 第0条的old属性 @old4
- - 第0条的new属性 @user95
-
-- cancel状态任务指派
- - 第0条的field属性 @assignedTo
- - 第0条的old属性 @old5
- - 第0条的new属性 @user96
-
 */
 
-$task = zdTable('task');
-$task->id->range('1-6');
-$task->execution->range('2,3,3,4');
-$task->name->prefix("任务")->range('1-6');
-$task->left->range('0');
-$task->assignedTo->prefix("old")->range('1-6');
-$task->status->range("wait,doing,done,pause,cancel,closed");
-$task->gen(6);
+zdTable('task')->config('task')->gen(6);
+zdTable('user')->config('user')->gen(50);
 
-$user = zdTable('user');
-$user->id->range('1-100');
-$user->account->range('1-100')->prefix('user');
-$user->password->range('f8e41d6c31824c01e5d67c61a8ae49e9,e10adc3949ba59abbe56e057f20f883e');
-$user->realname->range('1-100')->prefix("开发");
-$user->gen(50);
+su('user12');
+
+$project = zdTable('project');
+$project->id->range('1-100');
+$project->gen(10);
 
 $taskIDlist = array('1','2','3','4','5','6');
 
-$waitTask      = array('assignedTo' => 'user92','status' => 'wait');
-$waitTaskLeft  = array('assignedTo' => 'user91','status' => 'wait', 'left' => 1);
-$doingTask     = array('assignedTo' => 'user93','status' => 'doing');
-$doingTaskLeft = array('assignedTo' => 'user90','status' => 'doing', 'left' => 0);
-$doneTask      = array('assignedTo' => 'user94','status' => 'done');
-$pauseTask     = array('assignedTo' => 'user95','status' => 'pause');
-$cancelTask    = array('assignedTo' => 'user96','status' => 'cancel');
+$waitTask       = array('assignedTo' => 'user12','status' => 'wait');
+$waitTaskLeft   = array('assignedTo' => 'user11','status' => 'wait', 'left' => 1);
+$doingTask      = array('assignedTo' => 'user13','status' => 'doing');
+$doingTaskLeft  = array('assignedTo' => 'user10','status' => 'doing', 'left' => 0);
+$doneTask       = array('assignedTo' => 'user14','status' => 'done');
+$pauseTask      = array('assignedTo' => 'user15','status' => 'pause');
+$cancelTask     = array('assignedTo' => 'user16','status' => 'cancel');
+$closedTaskLeft = array('assignedTo' => 'user17','status' => 'closed');
+
+$_SERVER['HTTP_HOST'] = 'pms.zentao.com';
 
 $task = new taskTest();
-r($task->assignTest($taskIDlist[0], $waitTask))      && p('0:field,old,new') && e('assignedTo,old1,user92'); // wait状态任务指派
-r($task->assignTest($taskIDlist[0], $waitTaskLeft))  && p('1:field,old,new') && e('left,0,1');               // wait状态任务指派修改预计剩余
-r($task->assignTest($taskIDlist[1], $doingTask))     && p('0:field,old,new') && e('assignedTo,old2,user93'); // doing状态任务指派
-r($task->assignTest($taskIDlist[1], $doingTaskLeft)) && p()                  && e('『预计剩余』不能为空。'); // doing状态任务指派,预计剩余为0
-r($task->assignTest($taskIDlist[2], $doneTask))      && p('0:field,old,new') && e('assignedTo,old3,user94'); // done状态任务指派
-r($task->assignTest($taskIDlist[3], $pauseTask))     && p('0:field,old,new') && e('assignedTo,old4,user95'); // pause状态任务指派
-r($task->assignTest($taskIDlist[4], $cancelTask))    && p('0:field,old,new') && e('assignedTo,old5,user96'); // cancel状态任务指派
+r($task->assignTest($taskIDlist[0], $waitTask))       && p('0:field,old,new') && e('assignedTo,old1,user12'); // wait状态任务指派
+r($task->assignTest($taskIDlist[0], $waitTaskLeft))   && p('1:field,old,new') && e('left,0,1');               // wait状态任务指派修改预计剩余
+r($task->assignTest($taskIDlist[1], $doingTask))      && p('0:field,old,new') && e('assignedTo,old2,user13'); // doing状态任务指派
+r($task->assignTest($taskIDlist[1], $doingTaskLeft))  && p()                  && e('『预计剩余』不能为空。');   // doing状态任务指派,预计剩余为0
+r($task->assignTest($taskIDlist[2], $doneTask))       && p('0:field,old,new') && e('assignedTo,old3,user14'); // done状态任务指派
+r($task->assignTest($taskIDlist[3], $pauseTask))      && p('0:field,old,new') && e('assignedTo,old4,user15'); // pause状态任务指派
+r($task->assignTest($taskIDlist[4], $cancelTask))     && p('0:field,old,new') && e('assignedTo,old5,user16'); // cancel状态任务指派
