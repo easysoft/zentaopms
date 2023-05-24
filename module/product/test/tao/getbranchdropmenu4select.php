dfc7baf119551c62ea762ced80db26329bc8aa4d
zhouxin <zhouxin@easycorp.ltd>
2023-05-24 15:46:57 +0800

    * Batch change double quote to single quote.

--- module/product/test/tao/getbranchdropmenu4select.php
+++ module/product/test/tao/getbranchdropmenu4select.php
@@ -1,6 +1,6 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . "/test/lib/init.php";
+include dirname(__FILE__, 5) . '/test/lib/init.php';
 
 zdTable('product')->gen(50);
 $branch = zdTable('branch');
