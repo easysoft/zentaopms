4b984301257d103a77b0aa3982945a0479e3fd6f
tianshujie <tianshujie@easycorp.ltd>
2023-05-24 15:35:56 +0800

    * Modify test case for afterbatchcreate method.

--- module/task/test/model/afterbatchcreate.php
+++ module/task/test/model/afterbatchcreate.php
@@ -5,7 +5,7 @@ include dirname(__FILE__, 2) . '/task.class.php';
 su('admin');
 
 zdTable('project')->config('project')->gen('5');
-zdTable('task')->config('task')->gen(8);
+zdTable('task')->config('task')->gen(10);
 zdTable('taskspec')->gen(0);
 
 /**
@@ -23,3 +23,7 @@ $taskTester = new taskTest();
 r($taskTester->afterBatchCreateObject($taskIdList[0])) && p() && e('1'); // 测试批量创建迭代下的任务后的数据处理
 r($taskTester->afterBatchCreateObject($taskIdList[1])) && p() && e('1'); // 测试批量创建阶段下的任务后的数据处理
 r($taskTester->afterBatchCreateObject($taskIdList[2])) && p() && e('1'); // 测试批量创建看板下的任务后的数据处理
+
+r($taskTester->afterBatchCreateObject($taskIdList[0], 1)) && p() && e('1'); // 测试批量创建迭代下的子任务后的数据处理
+r($taskTester->afterBatchCreateObject($taskIdList[1], 1)) && p() && e('1'); // 测试批量创建阶段下的子任务后的数据处理
+r($taskTester->afterBatchCreateObject($taskIdList[2], 1)) && p() && e('1'); // 测试批量创建看板下的子任务后的数据处理
