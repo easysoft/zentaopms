272672145b4411af7d47c27b539187009277dd9a
zhouxin <zhouxin@easycorp.ltd>
2023-05-24 14:12:24 +0800

    * Add unit test for productModel-getLines.

---
+++ module/product/test/model/getlines.php
@@ -0,0 +1,29 @@
+#!/usr/bin/env php
+<?php
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+
+zdTable('module')->config('lines', $useCommon = true)->gen(20);
+
+/**
+
+title=productModel->getLines();
+cid=1
+pid=1
+
+*/
+$product = $tester->loadModel('product');
+
+$programIdList1 = array();
+$programIdList2 = array(1);
+$programIdList3 = array(1, 2);
+$programIdList4 = array(1, 2, 3);
+$programIdList5 = array(1, 2, 3, 4);
+$programIdList6 = array(1, 2, 3, 4, 5);
+
+r(count($product->getLines()))                && p() && e('20'); // 测试不传参数的情况
+r(count($product->getLines($programIdList1))) && p() && e('20'); // 测试传入空数组的情况
+r(count($product->getLines($programIdList2))) && p() && e('5');  // 测试获取项目集1的产品线
+r(count($product->getLines($programIdList3))) && p() && e('10'); // 测试获取项目集1,2的产品线
+r(count($product->getLines($programIdList4))) && p() && e('15'); // 测试获取项目集1,2,3的产品线
+r(count($product->getLines($programIdList5))) && p() && e('20'); // 测试获取项目集1,2,3,4的产品线
+r(count($product->getLines($programIdList6))) && p() && e('20'); // 测试获取项目集1,2,3,4,5的产品线
