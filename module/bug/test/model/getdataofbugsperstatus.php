7ec23418c9b5dfa918b79a83d58f88871804071c
daitingting <daitingting@xirangit.com>
2023-05-25 06:29:49 +0000

    * Refactor getDataOfBugsPerStatus test.

--- module/bug/test/model/getdataofbugsperstatus.php
+++ module/bug/test/model/getdataofbugsperstatus.php
@@ -1,21 +1,32 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . '/test/lib/init.php'; su('admin');
+include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/bug.class.php';
+su('admin');
+
+zdTable('bug')->config('status')->gen(10);
 
 /**
 
 title=bugModel->getDataOfBugsPerStatus();
+timeout=0
 cid=1
-pid=1
 
-获取状态为active的数据 >> 激活,165
-获取状态为resolved的数据 >> 已解决,90
-获取状态为closed的数据 >> 已关闭,60
+- 获取状态为激活的数据
+ - 第active条的name属性 @激活
+ - 第active条的value属性 @2
+
+- 获取状态为已解决的数据
+ - 第resolved条的name属性 @已解决
+ - 第resolved条的value属性 @2
+
+- 取状态为关闭的数据
+ - 第closed条的name属性 @已关闭
+ - 第closed条的value属性 @6
 
 */
 
-$bug=new bugTest();
-r($bug->getDataOfBugsPerStatusTest()) && p('active:name,value')   && e('激活,165');  // 获取状态为active的数据
-r($bug->getDataOfBugsPerStatusTest()) && p('resolved:name,value') && e('已解决,90'); // 获取状态为resolved的数据
-r($bug->getDataOfBugsPerStatusTest()) && p('closed:name,value')   && e('已关闭,60'); // 获取状态为closed的数据
+$bug = new bugTest();
+r($bug->getDataOfBugsPerStatusTest()) && p('active:name,value')   && e('激活,2');   //获取状态为激活的数据
+r($bug->getDataOfBugsPerStatusTest()) && p('resolved:name,value') && e('已解决,2'); //获取状态为已解决的数据
+r($bug->getDataOfBugsPerStatusTest()) && p('closed:name,value')   && e('已关闭,6'); //取状态为关闭的数据
