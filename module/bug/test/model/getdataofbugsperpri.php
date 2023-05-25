1fc8e1d2d46cf5a1f86565bc164b8ca0303f7d69
daitingting <daitingting@xirangit.com>
2023-05-25 06:33:21 +0000

    * Refactor getDataOfBugsPerPri test.

--- module/bug/test/model/getdataofbugsperpri.php
+++ module/bug/test/model/getdataofbugsperpri.php
@@ -1,23 +1,37 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . '/test/lib/init.php'; su('admin');
+include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/bug.class.php';
+su('admin');
+
+zdTable('bug')->gen(10);
 
 /**
 
 title=bugModel->getDataOfBugsPerPri();
+timeout=0
 cid=1
-pid=1
 
-获取pri为1的数据 >> 优先级:1,79
-获取pri为2的数据 >> 优先级:2,79
-获取pri为3的数据 >> 优先级:3,79
-获取pri为4的数据 >> 优先级:4,78
+- 获取优先级为1的数据
+ - 第1条的name属性 @优先级:1
+ - 第1条的value属性 @3
+
+- 获取优先级为2的数据
+ - 第2条的name属性 @优先级:2
+ - 第2条的value属性 @3
+
+- 获取优先级为3的数据
+ - 第3条的name属性 @优先级:3
+ - 第3条的value属性 @2
+
+- 获取优先级为4的数据
+ - 第4条的name属性 @优先级:4
+ - 第4条的value属性 @2
 
 */
 
-$bug=new bugTest();
-r($bug->getDataOfBugsPerPriTest()) && p('1:name,value') && e('优先级:1,79'); // 获取pri为1的数据
-r($bug->getDataOfBugsPerPriTest()) && p('2:name,value') && e('优先级:2,79'); // 获取pri为2的数据
-r($bug->getDataOfBugsPerPriTest()) && p('3:name,value') && e('优先级:3,79'); // 获取pri为3的数据
-r($bug->getDataOfBugsPerPriTest()) && p('4:name,value') && e('优先级:4,78'); // 获取pri为4的数据
+$bug = new bugTest();
+r($bug->getDataOfBugsPerPriTest()) && p('1:name,value') && e('优先级:1,3'); //获取优先级为1的数据
+r($bug->getDataOfBugsPerPriTest()) && p('2:name,value') && e('优先级:2,3'); //获取优先级为2的数据
+r($bug->getDataOfBugsPerPriTest()) && p('3:name,value') && e('优先级:3,2'); //获取优先级为3的数据
+r($bug->getDataOfBugsPerPriTest()) && p('4:name,value') && e('优先级:4,2'); //获取优先级为4的数据
