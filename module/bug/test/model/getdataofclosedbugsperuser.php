0a3a7fd35228c111df02d2e394a57c71d53f7c9b
daitingting <daitingting@xirangit.com>
2023-05-25 05:37:27 +0000

    * Refactor getDataOfClosedBugsPerUser test.

--- module/bug/test/model/getdataofclosedbugsperuser.php
+++ module/bug/test/model/getdataofclosedbugsperuser.php
@@ -1,17 +1,22 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . "/test/lib/init.php"; su('admin');
+include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/bug.class.php';
+su('admin');
+
+zdTable('bug')->config('closedby')->gen(10);
 
 /**
 
 title=bugModel->getDataOfClosedBugsPerUser();
+timeout=0
 cid=1
-pid=1
 
-获取admin关闭的数据 >> admin,100
+- 获取 admin 关闭的 bug 数据
+ - 第admin条的name属性 @admin
+ - 第admin条的value属性 @10
 
 */
 
-$bug=new bugTest();
-r($bug->getDataOfClosedBugsPerUserTest()) && p('admin:name,value') && e('admin,100');   // 获取admin关闭的数据
+$bug = new bugTest();
+r($bug->getDataOfClosedBugsPerUserTest()) && p('admin:name,value') && e('admin,10'); // 获取 admin 关闭的 bug 数据
