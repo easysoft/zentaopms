b9c65dc5b7d10ab3de764ff427c79aa986936390
daitingting <daitingting@xirangit.com>
2023-05-25 05:32:11 +0000

    * Refactor getDataOfResolvedBugsPerUser test.

--- module/bug/test/model/getdataofresolvedbugsperuser.php
+++ module/bug/test/model/getdataofresolvedbugsperuser.php
@@ -1,19 +1,22 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . "/test/lib/init.php"; su('admin');
+include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/bug.class.php';
+su('admin');
+
+zdTable('bug')->config('resolvedby')->gen(10);
 
 /**
 
 title=bugModel->getDataOfResolvedBugsPerUser();
+timeout=0
 cid=1
-pid=1
 
-获取admin解决的bug数 >> admin,60
-获取 >> ,90
+- 获取 admin 解决的 bug 数
+ - 第admin条的name属性 @admin
+ - 第admin条的value属性 @10
 
 */
 
-$bug=new bugTest();
-r($bug->getDataOfResolvedBugsPerUserTest()) && p('admin:name,value')       && e('admin,60');       // 获取admin解决的bug数
-r($bug->getDataOfResolvedBugsPerUserTest()) && p('$assignedTo:name,value') && e('$assignedTo,90'); // 获取$assignTo解决的bug数
+$bug = new bugTest();
+r($bug->getDataOfResolvedBugsPerUserTest()) && p('admin:name,value') && e('admin,10'); // 获取 admin 解决的 bug 数
