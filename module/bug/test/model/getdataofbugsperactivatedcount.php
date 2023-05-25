85f072112144cb912e609d8546a79580eb66fb87
daitingting <daitingting@xirangit.com>
2023-05-25 06:46:43 +0000

    * Refactor getDataOfBugsPerActivatedCount test.

--- module/bug/test/model/getdataofbugsperactivatedcount.php
+++ module/bug/test/model/getdataofbugsperactivatedcount.php
@@ -1,17 +1,32 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . '/test/lib/init.php'; su('admin');
+include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/bug.class.php';
+su('admin');
+
+zdTable('bug')->config('activatedcount')->gen(10);
 
 /**
 
 title=bugModel->getDataOfBugsPerActivatedCount();
+timeout=0
 cid=1
-pid=1
 
-获取bug激活次数数据 >> 激活次数:0,315
+- 获取 bug 激活次数为 0 的数据
+ - 第0条的name属性 @激活次数:0
+ - 第0条的value属性 @5
+
+- 获取 bug 激活次数为 1 的数据
+ - 第1条的name属性 @激活次数:1
+ - 第1条的value属性 @3
+
+- 获取 bug 激活次数为 2 的数据
+ - 第2条的name属性 @激活次数:2
+ - 第2条的value属性 @2
 
 */
 
-$bug=new bugTest();
-r($bug->getDataOfBugsPerActivatedCountTest()) && p('0:name,value') && e('激活次数:0,315');   // 获取bug激活次数数据
+$bug = new bugTest();
+r($bug->getDataOfBugsPerActivatedCountTest()) && p('0:name,value') && e('激活次数:0,5'); //获取 bug 激活次数为 0 的数据
+r($bug->getDataOfBugsPerActivatedCountTest()) && p('1:name,value') && e('激活次数:1,3'); //获取 bug 激活次数为 1 的数据
+r($bug->getDataOfBugsPerActivatedCountTest()) && p('2:name,value') && e('激活次数:2,2'); //获取 bug 激活次数为 2 的数据
