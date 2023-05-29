4d589f3ba4a7d0af28bd02bb431d3ae4c4f007d1
liumengyi <liumengyi@easycorp.ltd>
2023-05-29 11:22:01 +0800

    * Modify unit test of bugModel-assign.

--- module/bug/test/model/assign.php
+++ module/bug/test/model/assign.php
@@ -26,6 +26,7 @@ $bug1->assignedDate   = $now;
 $bug1->lastEditedBy   = 'admin';
 $bug1->lastEditedDate = $now;
 $bug1->mailto         = 'user1,user3';
+$bug1->comment        = '';
 
 $bug2 = new stdclass();
 $bug2->id             = 2;
@@ -34,6 +35,7 @@ $bug2->assignedDate   = $now;
 $bug2->lastEditedBy   = 'admin';
 $bug2->lastEditedDate = $now;
 $bug2->mailto         = 'user2';
+$bug2->comment        = '';
 
 $bug3 = new stdclass();
 $bug3->id             = 3;
@@ -42,6 +44,7 @@ $bug3->assignedDate   = $now;
 $bug3->lastEditedBy   = 'admin';
 $bug3->lastEditedDate = $now;
 $bug3->mailto         = 'user3';
+$bug3->comment        = '';
 
 $bug4 = new stdclass();
 $bug4->id             = 4;
@@ -50,6 +53,7 @@ $bug4->assignedDate   = $now;
 $bug4->lastEditedBy   = 'admin';
 $bug4->lastEditedDate = $now;
 $bug4->mailto         = 'user1';
+$bug4->comment        = '';
 
 $bug5 = new stdclass();
 $bug5->id             = 5;
@@ -58,6 +62,7 @@ $bug5->assignedDate   = $now;
 $bug5->lastEditedBy   = 'admin';
 $bug5->lastEditedDate = $now;
 $bug5->mailto         = 'user2';
+$bug5->comment        = '';
 
 $bug6 = new stdclass();
 $bug6->id             = 6;
@@ -66,11 +71,12 @@ $bug6->assignedDate   = $now;
 $bug6->lastEditedBy   = 'admin';
 $bug6->lastEditedDate = $now;
 $bug6->mailto         = 'user3';
+$bug6->comment        = '';
 
 $bug = new bugTest();
 r($bug->assignTest($bug1))  && p('assignedTo;mailto', ';') && e('user2;user1,user3;'); // 指派bug状态为激活的bug 更改指派人
 r($bug->assignTest($bug2))  && p('assignedTo;mailto', ';') && e('user2;user2;');       // 指派bug状态为解决的bug 更改指派人
-r($bug->assignTest($bug3))  && p('assignedTo;mailto', ';') && e('user1;admin');        // 指派bug状态为关闭的bug 更改指派人
+r($bug->assignTest($bug3))  && p('assignedTo;mailto', ';') && e('user2;user3');        // 指派bug状态为关闭的bug 更改指派人
 r($bug->assignTest($bug4))  && p('assignedTo;mailto', ';') && e('user1;user1;');       // 指派bug状态为激活的bug 不更改指派人
 r($bug->assignTest($bug5))  && p('assignedTo;mailto', ';') && e('user1;user2;');       // 指派bug状态为解决的bug 不更改指派人
-r($bug->assignTest($bug6))  && p('assignedTo;mailto', ';') && e('user1;admin');        // 指派bug状态为关闭的bug 不更改指派人
+r($bug->assignTest($bug6))  && p('assignedTo;mailto', ';') && e('user1;user3');        // 指派bug状态为关闭的bug 不更改指派人
