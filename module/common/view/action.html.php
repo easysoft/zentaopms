f390fe7bd428d0ab77d166e492fcf1d126c18778
朱金勇 <zhujinyong@easycorp.ltd>
2023-05-29 05:18:23 +0000

    * Fix ambiguity of if.

--- module/common/view/action.html.php
+++ module/common/view/action.html.php
@@ -55,9 +55,9 @@
         <?php
         $action->actor = zget($users, $action->actor);
         if($action->action == 'assigned' or $action->action == 'toaudit') $action->extra = zget($users, $action->extra);
-        if(strpos($action->actor, ':') !== false) $action->actor = substr($action->actor, strpos($action->actor, ':') + 1);
+        if(strpos($action->actor, ':') !== false) { $action->actor = substr($action->actor, strpos($action->actor, ':') + 1); }
+        $this->loadModel('action')->printAction($action);
         ?>
-        <?php $this->loadModel('action')->printAction($action);?>
         <?php if(!empty($action->history)):?>
           <button type='button' class='btn btn-mini switch-btn btn-icon btn-expand' title='<?php echo $lang->switchDisplay;?>'><i class='change-show icon icon-plus icon-sm'></i></button>
           <div class='history-changes' id='changeBox<?php echo $i;?>'>
