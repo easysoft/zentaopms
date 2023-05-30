5b64240d13948e4baf8f504efd39ff87f952e48b
dingyongliang <dingyongliang@easycorp.ltd>
2023-05-30 13:16:07 +0800

    + zin: refactor echarts wg.

--- lib/zin/wg/echarts/v1.php
+++ lib/zin/wg/echarts/v1.php
@@ -4,11 +4,24 @@ namespace zin;
 
 class echarts extends wg
 {
-    public function size(string|int $width, string|int $height)
+    public static function getPageJS(): string|false
+    {
+        $echarts = dirname(__FILE__, 5) . DS . implode(DS, array('www', 'js', 'echarts', 'echarts.common.min.js'));
+        return file_get_contents($echarts);
+    }
+
+    public function size(string|int $width, string|int $height): echarts
     {
         if(is_numeric($width))  $width  = "{$width}px";
         if(is_numeric($height)) $height = "{$height}px";
         $this->setProp('_size', array($width, $height));
+        return $this;
+    }
+
+    public function theme(string|array $value): echarts
+    {
+        $this->setProp('theme', $value);
+        return $this;
     }
 
     protected function build()
