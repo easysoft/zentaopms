1038dcc538f7c2c51e61834486254fc75d1f1b4d
wangyidong <wangyidong@cnezsoft.com>
2023-05-30 09:38:33 +0800

    + Add unit test for docreatereviewer method.

---
+++ module/story/test/tao/docreatereviewer.php
@@ -0,0 +1,29 @@
+#!/usr/bin/env php
+<?php
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+include dirname(__FILE__, 2) . '/story.class.php';
+su('admin');
+
+zdTable('storyreview')->gen(1);
+
+/**
+
+title=测试 storyModel->doCreateReviewer();
+cid=1
+pid=1
+
+*/
+
+global $tester;
+$storyModel = $tester->loadModel('story');
+
+r($storyModel->doCreateReviewer(0, array()))  && p() && e('0'); // 不传入任何数据。
+r($storyModel->doCreateReviewer(0, array(1))) && p() && e('0'); // 只传入评审人列表。
+r($storyModel->doCreateReviewer(1, array()))  && p() && e('0'); // 只传入软件需求 ID。
+
+$story = new storyTest();
+$requirementResult = $story->doCreateReviewerTest(1, array('admin', 'test1'));
+
+r(count($requirementResult)) && p()                 && e('2');       // 传入软件需求 ID 和 评审人列表，查看storyreview表记录的数量。
+r($requirementResult[0])     && p('story,reviewer') && e('1,admin'); // 传入软件需求 ID 和 评审人列表，查看storyreview表记录的关系。
+r($requirementResult[1])     && p('story,reviewer') && e('1,test1'); // 传入软件需求 ID 和 评审人列表，查看storyreview表记录的关系。
