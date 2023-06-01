0487b17b29b04ddc4b99cb06689104c3af9e0304
chentao <chentao@easycorp.ltd>
2023-06-01 14:47:01 +0800

    * Remove explode operation on array data from batchEdit function of product control.

--- module/product/control.php
+++ module/product/control.php
@@ -327,7 +327,7 @@ class product extends control
         if($this->app->tab == 'program') $this->loadModel('program')->setMenu(0);
 
         /* 构造批量编辑页面表单配置数据。*/
-        $this->productZen->buildBatchEditForm($programID, explode(',', $productIdList));
+        $this->productZen->buildBatchEditForm($programID, $productIdList);
     }
 
     /**
