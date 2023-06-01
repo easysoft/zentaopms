110f0ee5c116d5fbb3900897e0a8b7400aa4eb0d
tianshujie <tianshujie@easycorp.ltd>
2023-06-01 13:28:54 +0800

    * Refactor test case for addTaskEffort method.

--- module/task/test/model/addtaskeffort.php
+++ module/task/test/model/addtaskeffort.php
@@ -4,17 +4,14 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/task.class.php';
 su('admin');
 
+zdTable('project')->config('project', true)->gen(5);
+zdTable('task')->config('task', true)->gen(5);
+
 /**
 
 title=taskModel->addTaskEffort();
 timeout=0
 cid=1
-
-插入task为1 left为0 consumed为3的任务 >> 601,1,0,3
-插入task为601 left为0 consumed为3的任务 >> 602,601,0,3
-插入task为601 left为1 consumed为4的任务 >> 603,602,1,4
-插入task为904 left为3 consumed为6的任务 >> 604,904,3,6
-插入task为907 left为6 consumed为9的任务 >> 605,907,6,9
 
 */
 
@@ -26,31 +23,31 @@ $record1->consumed = 3;
 
 $record2 = new stdclass();
 $record2->account  = 'po82';
-$record2->task     = 601;
+$record2->task     = 2;
 $record2->left     = 0;
 $record2->consumed = 3;
 
 $record3 = new stdclass();
 $record3->account  = 'po82';
-$record3->task     = 602;
+$record3->task     = 3;
 $record3->left     = 1;
 $record3->consumed = 4;
 
 $record4 = new stdclass();
 $record4->account  = 'po82';
-$record4->task     = 904;
+$record4->task     = 4;
 $record4->left     = 3;
 $record4->consumed = 6;
 
 $record5 = new stdclass();
 $record5->account  = 'po82';
-$record5->task     = 907;
+$record5->task     = 5;
 $record5->left     = 6;
 $record5->consumed = 9;
 
 $task = new taskTest();
-r($task->addTaskEffortTest($record1)) && p('id,task,left,consumed') && e("601,1,0,3");   // 插入task为1 left为0 consumed为3的任务
-r($task->addTaskEffortTest($record2)) && p('id,task,left,consumed') && e("602,601,0,3"); // 插入task为601 left为0 consumed为3的任务
-r($task->addTaskEffortTest($record3)) && p('id,task,left,consumed') && e("603,602,1,4"); // 插入task为601 left为1 consumed为4的任务
-r($task->addTaskEffortTest($record4)) && p('id,task,left,consumed') && e("604,904,3,6"); // 插入task为904 left为3 consumed为6的任务
-r($task->addTaskEffortTest($record5)) && p('id,task,left,consumed') && e("605,907,6,9"); // 插入task为907 left为6 consumed为9的任务
+r($task->addTaskEffortTest($record1)) && p('objectID,left,consumed') && e("1,0,3"); // 插入task为1 left为0 consumed为3的任务
+r($task->addTaskEffortTest($record2)) && p('objectID,left,consumed') && e("2,0,3"); // 插入task为601 left为0 consumed为3的任务
+r($task->addTaskEffortTest($record3)) && p('objectID,left,consumed') && e("3,1,4"); // 插入task为601 left为1 consumed为4的任务
+r($task->addTaskEffortTest($record4)) && p('objectID,left,consumed') && e("4,3,6"); // 插入task为904 left为3 consumed为6的任务
+r($task->addTaskEffortTest($record5)) && p('objectID,left,consumed') && e("5,6,9"); // 插入task为907 left为6 consumed为9的任务
