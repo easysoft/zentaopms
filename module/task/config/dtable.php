5c833d30afe6fef58c3a9b49af33a86d4191f785
caoyanyi <caoyanyi@easycorp.ltd>
2023-06-02 16:54:04 +0800

    * Adjust table field sort.

--- module/task/config/dtable.php
+++ module/task/config/dtable.php
@@ -12,7 +12,6 @@ $config->task->dtable->fieldList['name']['title']    = $lang->task->name;
 $config->task->dtable->fieldList['name']['name']     = 'name';
 $config->task->dtable->fieldList['name']['fixed']    = 'left';
 $config->task->dtable->fieldList['name']['flex']     = 1;
-$config->task->dtable->fieldList['name']['maxWidth'] = 300;
 $config->task->dtable->fieldList['name']['type']     = 'nestedTitle';
 $config->task->dtable->fieldList['name']['sortType'] = true;
 $config->task->dtable->fieldList['name']['link']     = helper::createLink('task', 'view', 'taskID={id}');
@@ -21,18 +20,6 @@ $config->task->dtable->fieldList['pri']['title']    = $lang->priAB;
 $config->task->dtable->fieldList['pri']['name']     = 'pri';
 $config->task->dtable->fieldList['pri']['type']     = 'pri';
 $config->task->dtable->fieldList['pri']['sortType'] = true;
-
-$config->task->dtable->fieldList['assignedTo']['title']       = $lang->task->assignedTo;
-$config->task->dtable->fieldList['assignedTo']['name']        = 'assignedTo';
-$config->task->dtable->fieldList['assignedTo']['type']        = 'assign';
-$config->task->dtable->fieldList['assignedTo']['currentUser'] = $app->user->account;
-$config->task->dtable->fieldList['assignedTo']['assignLink']  = helper::createLink('task', 'assignTo', "executionID={execution}&taskID={id}", '', true);
-$config->task->dtable->fieldList['assignedTo']['sortType']    = true;
-
-$config->task->dtable->fieldList['assignedDate']['title']    = $lang->task->assignedDate;
-$config->task->dtable->fieldList['assignedDate']['name']     = 'assignedDate';
-$config->task->dtable->fieldList['assignedDate']['type']     = 'datetime';
-$config->task->dtable->fieldList['assignedDate']['sortType'] = true;
 
 $config->task->dtable->fieldList['type']['title']    = $lang->task->typeAB;
 $config->task->dtable->fieldList['type']['name']     = 'type';
@@ -46,10 +33,47 @@ $config->task->dtable->fieldList['status']['type']      = 'status';
 $config->task->dtable->fieldList['status']['statusMap'] = $lang->task->statusList;
 $config->task->dtable->fieldList['status']['sortType']  = true;
 
+$config->task->dtable->fieldList['openedBy']['title']    = $lang->task->openedByAB;
+$config->task->dtable->fieldList['openedBy']['name']     = 'openedBy';
+$config->task->dtable->fieldList['openedBy']['type']     = 'user';
+$config->task->dtable->fieldList['openedBy']['sortType'] = true;
+
+$config->task->dtable->fieldList['openedDate']['title']    = $lang->task->openedDate;
+$config->task->dtable->fieldList['openedDate']['name']     = 'openedDate';
+$config->task->dtable->fieldList['openedDate']['type']     = 'date';
+$config->task->dtable->fieldList['openedDate']['sortType'] = true;
+
+$config->task->dtable->fieldList['assignedTo']['title']       = $lang->task->assignedTo;
+$config->task->dtable->fieldList['assignedTo']['name']        = 'assignedTo';
+$config->task->dtable->fieldList['assignedTo']['type']        = 'assign';
+$config->task->dtable->fieldList['assignedTo']['currentUser'] = $app->user->account;
+$config->task->dtable->fieldList['assignedTo']['assignLink']  = helper::createLink('task', 'assignTo', "executionID={execution}&taskID={id}", '', true);
+$config->task->dtable->fieldList['assignedTo']['sortType']    = true;
+
+$config->task->dtable->fieldList['assignedDate']['title']    = $lang->task->assignedDate;
+$config->task->dtable->fieldList['assignedDate']['name']     = 'assignedDate';
+$config->task->dtable->fieldList['assignedDate']['type']     = 'date';
+$config->task->dtable->fieldList['assignedDate']['sortType'] = true;
+
+$config->task->dtable->fieldList['estStarted']['title']    = $lang->task->estStarted;
+$config->task->dtable->fieldList['estStarted']['name']     = 'estStarted';
+$config->task->dtable->fieldList['estStarted']['type']     = 'date';
+$config->task->dtable->fieldList['estStarted']['sortType'] = true;
+
+$config->task->dtable->fieldList['realStarted']['title']    = $lang->task->realStarted;
+$config->task->dtable->fieldList['realStarted']['name']     = 'realStarted';
+$config->task->dtable->fieldList['realStarted']['type']     = 'date';
+$config->task->dtable->fieldList['realStarted']['sortType'] = true;
+
 $config->task->dtable->fieldList['finishedBy']['title']    = $lang->task->finishedByAB;
 $config->task->dtable->fieldList['finishedBy']['name']     = 'finishedBy';
 $config->task->dtable->fieldList['finishedBy']['type']     = 'user';
 $config->task->dtable->fieldList['finishedBy']['sortType'] = true;
+
+$config->task->dtable->fieldList['finishedDate']['title']    = $lang->task->finishedDateAB;
+$config->task->dtable->fieldList['finishedDate']['name']     = 'finishedDateAB';
+$config->task->dtable->fieldList['finishedDate']['type']     = 'date';
+$config->task->dtable->fieldList['finishedDate']['sortType'] = true;
 
 $config->task->dtable->fieldList['deadline']['title']    = $lang->task->deadlineAB;
 $config->task->dtable->fieldList['deadline']['name']     = 'deadline';
@@ -75,41 +99,6 @@ $config->task->dtable->fieldList['progress']['title'] = $lang->task->progressAB;
 $config->task->dtable->fieldList['progress']['name']  = 'progress';
 $config->task->dtable->fieldList['progress']['type']  = 'progress';
 
-$config->task->dtable->fieldList['openedBy']['title']    = $lang->task->openedByAB;
-$config->task->dtable->fieldList['openedBy']['name']     = 'openedBy';
-$config->task->dtable->fieldList['openedBy']['type']     = 'user';
-$config->task->dtable->fieldList['openedBy']['sortType'] = true;
-
-$config->task->dtable->fieldList['openedDate']['title']    = $lang->task->openedDate;
-$config->task->dtable->fieldList['openedDate']['name']     = 'openedDate';
-$config->task->dtable->fieldList['openedDate']['type']     = 'datetime';
-$config->task->dtable->fieldList['openedDate']['sortType'] = true;
-
-$config->task->dtable->fieldList['estStarted']['title']    = $lang->task->estStarted;
-$config->task->dtable->fieldList['estStarted']['name']     = 'estStarted';
-$config->task->dtable->fieldList['estStarted']['type']     = 'date';
-$config->task->dtable->fieldList['estStarted']['sortType'] = true;
-
-$config->task->dtable->fieldList['realStarted']['title']    = $lang->task->realStarted;
-$config->task->dtable->fieldList['realStarted']['name']     = 'realStarted';
-$config->task->dtable->fieldList['realStarted']['type']     = 'datetime';
-$config->task->dtable->fieldList['realStarted']['sortType'] = true;
-
-$config->task->dtable->fieldList['finishedDate']['title']    = $lang->task->finishedDateAB;
-$config->task->dtable->fieldList['finishedDate']['name']     = 'finishedDateAB';
-$config->task->dtable->fieldList['finishedDate']['type']     = 'datetime';
-$config->task->dtable->fieldList['finishedDate']['sortType'] = true;
-
-$config->task->dtable->fieldList['canceledBy']['title']    = $lang->task->canceledBy;
-$config->task->dtable->fieldList['canceledBy']['name']     = 'canceledBy';
-$config->task->dtable->fieldList['canceledBy']['type']     = 'user';
-$config->task->dtable->fieldList['canceledBy']['sortType'] = true;
-
-$config->task->dtable->fieldList['canceledDate']['title']    = $lang->task->canceledDate;
-$config->task->dtable->fieldList['canceledDate']['name']     = 'canceledDate';
-$config->task->dtable->fieldList['canceledDate']['type']     = 'datetime';
-$config->task->dtable->fieldList['canceledDate']['sortType'] = true;
-
 $config->task->dtable->fieldList['closedBy']['title']    = $lang->task->closedBy;
 $config->task->dtable->fieldList['closedBy']['name']     = 'closedBy';
 $config->task->dtable->fieldList['closedBy']['type']     = 'user';
@@ -127,6 +116,16 @@ $config->task->dtable->fieldList['closedReason']['type']     = 'category';
 $config->task->dtable->fieldList['closedReason']['map']      = $lang->task->reasonList;
 $config->task->dtable->fieldList['closedReason']['sortType'] = true;
 
+$config->task->dtable->fieldList['canceledBy']['title']    = $lang->task->canceledBy;
+$config->task->dtable->fieldList['canceledBy']['name']     = 'canceledBy';
+$config->task->dtable->fieldList['canceledBy']['type']     = 'user';
+$config->task->dtable->fieldList['canceledBy']['sortType'] = true;
+
+$config->task->dtable->fieldList['canceledDate']['title']    = $lang->task->canceledDate;
+$config->task->dtable->fieldList['canceledDate']['name']     = 'canceledDate';
+$config->task->dtable->fieldList['canceledDate']['type']     = 'date';
+$config->task->dtable->fieldList['canceledDate']['sortType'] = true;
+
 $config->task->dtable->fieldList['lastEditedBy']['title']    = $lang->task->lastEditedBy;
 $config->task->dtable->fieldList['lastEditedBy']['name']     = 'lastEditedBy';
 $config->task->dtable->fieldList['lastEditedBy']['type']     = 'user';
@@ -134,7 +133,7 @@ $config->task->dtable->fieldList['lastEditedBy']['sortType'] = true;
 
 $config->task->dtable->fieldList['lastEditedDate']['title']    = $lang->task->lastEditedDate;
 $config->task->dtable->fieldList['lastEditedDate']['name']     = 'lastEditedDate';
-$config->task->dtable->fieldList['lastEditedDate']['type']     = 'datetime';
+$config->task->dtable->fieldList['lastEditedDate']['type']     = 'date';
 $config->task->dtable->fieldList['lastEditedDate']['sortType'] = true;
 
 $config->task->dtable->fieldList['activatedDate']['title']    = $lang->task->activatedDate;
@@ -145,6 +144,7 @@ $config->task->dtable->fieldList['activatedDate']['sortType'] = true;
 $config->task->dtable->fieldList['story']['title']    = $lang->task->storyAB;
 $config->task->dtable->fieldList['story']['name']     = 'storyTitle';
 $config->task->dtable->fieldList['story']['width']    = '80';
+$config->task->dtable->fieldList['story']['type']     = 'desc';
 $config->task->dtable->fieldList['story']['sortType'] = true;
 
 $config->task->dtable->fieldList['mailto']['title']    = $lang->task->mailto;
