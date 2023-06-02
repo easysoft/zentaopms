caf5fbd77b6de1ace98178e8203bf4abf8934894
liumengyi <liumengyi@easycorp.ltd>
2023-06-02 14:05:45 +0800

    - Remove unit test of bugModel-getOverdueBugs.

--- module/bug/test/bug.class.php
+++ module/bug/test/bug.class.php
@@ -362,39 +362,6 @@ class bugTest
     }
 
     /**
-     * Test get bugs the overdueBugs is active or unclosed.
-     *
-     * @param  string $productIDList
-     * @param  string $modules
-     * @access public
-     * @return string
-     */
-    public function getOverdueBugsTest($productIDList, $modules)
-    {
-        global $tester;
-        $executions = $tester->loadModel('execution')->getPairs('0', 'all', 'empty|withdelete');
-
-        $bugs = $this->objectModel->getOverdueBugs($productIDList, 'all', $modules, $executions, 'id_desc', null, 0);
-
-        $title = '';
-        foreach($bugs as $bug)
-        {
-            $title .= ',' . $bug->title;
-        }
-        $title = trim($title, ',');
-        $title = str_replace("'", '', $title);
-
-        if(dao::isError())
-        {
-            return dao::getError();
-        }
-        else
-        {
-            return $title;
-        }
-    }
-
-    /**
      * Test get by Sonarqube id.
      *
      * @param  int    $sonarqubeID
