83c460f0935e07ca8f12cc8afa77e00138f187b5
liumengyi <liumengyi@easycorp.ltd>
2023-06-02 14:08:39 +0800

    - Remove unit test of bugModel-getByOpenedbyme.

--- module/bug/test/bug.class.php
+++ module/bug/test/bug.class.php
@@ -232,39 +232,6 @@ class bugTest
     }
 
     /**
-     * Test get bugs of opened by me.
-     *
-     * @param  string $productIDList
-     * @param  string $moduleIDList
-     * @access public
-     * @return string
-     */
-    public function getByOpenedbymeTest($productIDList, $moduleIDList)
-    {
-        global $tester;
-        $executions = $tester->loadModel('execution')->getPairs('0', 'all', 'empty|withdelete');
-
-        $bugs = $this->objectModel->getByOpenedbyme($productIDList, 'all', $moduleIDList, $executions, 'id_desc', null, 0);
-
-        $title = '';
-        foreach($bugs as $bug)
-        {
-            $title .= ',' . $bug->title;
-        }
-        $title = trim($title, ',');
-        $title = str_replace("'", '', $title);
-
-        if(dao::isError())
-        {
-            return dao::getError();
-        }
-        else
-        {
-            return $title;
-        }
-    }
-
-    /**
      * Test get by Sonarqube id.
      *
      * @param  int    $sonarqubeID
