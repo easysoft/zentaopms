96690d653c5594920b8dd196c4134fb988dc99ae
chentao <chentao@easycorp.ltd>
2023-06-06 14:07:18 +0800

    * Add function to build action buttons for product story list page.

--- module/product/ui/browse.html.php
+++ module/product/ui/browse.html.php
@@ -17,34 +17,139 @@ $projectHasProduct = $isProjectStory && !empty($project->hasProduct);
 $projectIDParam    = $isProjectStory ? "projectID=$projectID&" : '';
 $storyBrowseType   = $this->session->storyBrowseType;
 
-/* More menus. */
-$featureBarMore = array();
-if(!\commonModel::isTutorialMode())
+/* Generate sidebar to display module tree menu. */
+$fnGenerateSideBar = function() use ($moduleTree, $moduleID)
 {
-    foreach($lang->product->moreSelects as $key => $value)
+    // TODO: implements module header.
+    sidebar
+    (
+        moduleMenu(set(array
+        (
+            'modules'   => $moduleTree,
+            'activeKey' => $moduleID,
+            'closeLink' => createLink('execution', 'task')
+        )))
+    );
+};
+
+/* Build create story button. */
+$fnBuildCreateStoryButton = function() use ($lang, $product, $isProjectStory, $storyType, $productID, $branch, $moduleID, $projectID, $from)
+{
+    if(!common::canModify('product', $product)) return null;
+
+    $createLink      = createLink('story', 'create', "product=$productID&branch=$branch&moduleID=$moduleID&storyID=0&projectID=$projectID&bugID=0&planID=0&todoID=0&extra=&storyType=$storyType");
+    $batchCreateLink = createLink('story', 'batchCreate', "productID=$productID&branch=$branch&moduleID=$moduleID&storyID=0&project=$projectID&plan=0&storyType=$storyType");
+
+    $createBtnLink  = '';
+    $createBtnTitle = '';
+    if(hasPriv($storyType, 'create'))
     {
-        $active = $key == $storyBrowseType ? 'btn-active-text' : '';
-        $featureBarMore[] = array(
-            'text' => $value,
-            'url' => createLink($this->app->rawModule, $this->app->rawMethod, $projectIDParam . "productID=$productID&branch=$branch&browseType=$key&param=0&storyType=$storyType"),
-            'class' => $active
+        $createBtnLink  = $createLink;
+        $createBtnTitle = $lang->story->create;
+    }
+    elseif(hasPriv($storyType, 'batchCreate'))
+    {
+        $createBtnLink  = empty($productID) ? '' : $batchCreateLink;
+        $createBtnTitle = $lang->story->batchCreate;
+    }
+
+    /* Without privilege, don't render create button. */
+    if(empty($createBtnLink)) return null;
+
+    if(!empty($productID) && hasPriv($storyType, 'batchCreate') && hasPriv($storyType, 'create'))
+    {
+        $items = array();
+
+        if(commonModel::isTutorialMode())
+        {
+            /* Tutorial create link. */
+            $wizardParams = helper::safe64Encode("productID=$productID&branch=$branch&moduleID=$moduleID");
+            if($isProjectStory) $wizardParams = helper::safe64Encode("productID=$productID&branch=$branch&moduleID=$moduleID&storyID=&projectID=$projectID");
+            $link = $this->createLink('tutorial', 'wizard', "module=story&method=create&params=$wizardParams");
+            $items[] = array('text' => $lang->story->createCommon, 'url' => $link);
+        }
+        else
+        {
+            $items[] = array('text' => $lang->story->create, 'url' => $createLink);
+        }
+
+        $items[] = array('text' => $lang->story->batchCreate, 'url' => $batchCreateLink);
+
+        return dropdown
+        (
+            icon('plus'),
+            $createBtnTitle,
+            span(setClass('caret')),
+            setClass('btn primary'),
+            set::items($items),
         );
     }
-}
 
-/* Create Button of toolbar. */
-$createBtnLink  = '';
-$createBtnTitle = '';
-if(hasPriv($storyType, 'create'))
+    return item(set(array
+    (
+        'text'  => $createBtnTitle,
+        'icon'  => 'plus',
+        'type'  => 'dropdown',
+        'class' => $from == 'project' ? 'secondary' : 'primary',
+        'url'   => $createBtnLink
+    )));
+};
+
+/* Build link story button. */
+$fnBuildLinkStoryButton = function() use($lang, $product, $productID, $projectHasProduct, $project, $projectID)
 {
-    $createBtnLink  = createLink('story', 'create', "product=$productID&branch=$branch&moduleID=$moduleID&storyID=0&projectID=$projectID&bugID=0&planID=0&todoID=0&extra=&storyType=$storyType");
-    $createBtnTitle = $lang->story->create;
-}
-elseif(hasPriv($storyType, 'batchCreate'))
-{
-    $createBtnLink  = empty($productID) ? '' : createLink('story', 'batchCreate', "productID=$productID&branch=$branch&moduleID=$moduleID&storyID=0&project=$projectID&plan=0&storyType=$storyType");
-    $createBtnTitle = $lang->story->batchCreate;
-}
+    if(!common::canModify('product', $product)) return null;
+
+    if(!$projectHasProduct) return null;
+
+    /* Tutorial mode. */
+    if(commonModel::isTutorialMode())
+    {
+        $wizardParams = helper::safe64Encode("project=$project->id");
+
+        return item(set(array
+        (
+            'text' => $lang->project->linkStory,
+            'url'  => createLink('tutorial', 'wizard', "module=project&method=linkStory&params=$wizardParams")
+        )));
+    }
+
+    $buttonLink  = '';
+    $buttonTitle = '';
+    $dataToggle  = '';
+    if(common::hasPriv('projectstory', 'importPlanStories'))
+    {
+        $buttonLink  = empty($productID) ? '' : '#linkStoryByPlan';
+        $buttonTitle = $lang->execution->linkStoryByPlan;
+        $dataToggle  = 'data-toggle="modal"';
+    }
+    if(common::hasPriv('projectstory', 'linkStory'))
+    {
+        $buttonLink  = $this->createLink('projectstory', 'linkStory', "project=$projectID");
+        $buttonTitle = $lang->execution->linkStory;
+        $dataToggle  = '';
+    }
+
+    if(empty($buttonLink)) return null;
+
+    if(!empty($productID) && common::hasPriv('projectstory', 'linkStory') && common::hasPriv('projectstory', 'importPlanStories'))
+    {
+        $items = array();
+        $items[] = array('text' => $lang->execution->linkStory,       'url' => createLink('projectstory', 'linkStory', "project=$projectID"));
+        $items[] = array('text' => $lang->execution->linkStoryByPlan, 'url' => '#linkStoryByPlan', 'data-toggle' => $dataToggle);
+
+        return dropdown
+        (
+            icon('link'),
+            $buttonTitle,
+            span(setClass('caret')),
+            setClass('btn primary'),
+            set::items($items),
+        );
+    }
+
+    return null;
+};
 
 /* DataTable. */
 $setting = $this->datatable->getSetting('product');
@@ -83,27 +188,16 @@ foreach($stories as $story)
 
 data('storyBrowseType', $storyBrowseType);
 
-sidebar
-(
-    moduleMenu(set(array(
-        'modules'   => $moduleTree,
-        'activeKey' => $moduleID,
-        'closeLink' => createLink('execution', 'task')
-    )))
-);
-
+$fnGenerateSideBar();
 featureBar
 (
     set::current($browseType),
     set::linkParams("productID={$productID}&browseType={key}"),
-    set::moreMenuLinkCallback
-    (
-        function($key, $value) use($projectIDParam, $productID, $branch, $storyType)
-        {
-            global $app;
-            return createLink($app->rawModule, $app->rawMethod, $projectIDParam . "productID=$productID&branch=$branch&browseType=$key&param=0&storyType=$storyType");
-        }
-    ),
+    set::moreMenuLinkCallback(function($browseType) use($projectIDParam, $productID, $branch, $storyType)
+    {
+        global $app;
+        return createLink($app->rawModule, $app->rawMethod, $projectIDParam . "productID=$productID&branch=$branch&browseType={$browseType}&param=0&storyType=$storyType");
+    }),
     li(searchToggle(set::module('story')))
 );
 
@@ -113,29 +207,24 @@ toolbar
     (
         'text' => $lang->project->report,
         'icon' => 'bar-chart',
-        'class' => 'secondary'
+        'class' => 'ghost'
     ))),
     item(set(array
     (
         'text'  => $lang->export,
         'icon'  => 'export',
-        'class' => 'secondary',
+        'class' => 'ghost',
         'url'   => createLink('story', 'export', "productID=$productID&orderBy=$orderBy&executionID=$projectID&browseType=$browseType&storyType=$storyType"),
     ))),
     item(set(array
     (
         'text'  => $lang->import,
         'icon'  => 'import',
-        'class' => 'secondary',
+        'class' => 'ghost',
         'url'   => createLink('story', 'import', "productID=$productID"),
     ))),
-    item(set(array
-    (
-        'text'  => $createBtnTitle,
-        'icon'  => 'plus',
-        'class' => $from == 'project' ? 'secondary' : 'primary',
-        'url'   => $createBtnLink
-    )))
+    $fnBuildCreateStoryButton(),
+    $fnBuildLinkStoryButton()
 );
 
 dtable
