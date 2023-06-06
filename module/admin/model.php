3d8cf850cbc51006a8ed4fe23283820f5c645469
liwenrui <liwenrui@easycorp.ltd>
2023-06-06 10:36:16 +0800

    * tweak admin menu printing, use link properly even if there are subMenu items.

--- module/admin/model.php
+++ module/admin/model.php
@@ -390,7 +390,8 @@ class adminModel extends model
                     }
                 }
             }
-            elseif(!empty($menu['link']) and strpos($menu['link'], '|') !== false)
+
+            if(!empty($menu['link']) and strpos($menu['link'], '|') !== false)
             {
                 list($module, $method) = explode('|', $menu['link']);
                 $menu['link'] = helper::createLink($module, $method);
