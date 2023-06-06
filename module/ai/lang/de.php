b0b1e69cff21799d7840e9326ab5be95d1efeab1
liwenrui <liwenrui@easycorp.ltd>
2023-06-06 11:08:42 +0800

    + add models configuration view, part of task #94375.

---
+++ module/ai/lang/de.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The ai module zh-cn file of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(http://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Wenrui LI <liwenrui@easycorp.ltd>
+ * @package     ai
+ * @link        https://www.zentao.net
+ */
+$lang->ai->common = 'AI';
+
+$lang->ai->prompts = new stdclass();
+$lang->ai->prompts->common = 'Prompts';
+
+$lang->ai->conversations = new stdclass();
+$lang->ai->conversations->common = 'Conversations';
+
+$lang->ai->models = new stdclass();
+$lang->ai->models->title          = 'Language Model Configuration';
+$lang->ai->models->common         = 'Language Model';
+$lang->ai->models->type           = 'Model';
+$lang->ai->models->apiKey         = 'API Key';
+$lang->ai->models->proxyType      = 'Proxy Type';
+$lang->ai->models->proxyAddr      = 'Proxy Address';
+$lang->ai->models->description    = 'Description';
+$lang->ai->models->testConnection = 'Test Connection';
+
+$lang->ai->models->statusList = array();
+$lang->ai->models->statusList['on']  = 'Enable';
+$lang->ai->models->statusList['off'] = 'Disable';
+
+$lang->ai->models->typeList = array();
+$lang->ai->models->typeList['openai-gpt35'] = 'OpenAI / GPT-3.5';
+$lang->ai->models->typeList['azure-gpt35']  = 'Azure / GPT-3.5';
+
+$lang->ai->models->proxyTypes = array();
+$lang->ai->models->proxyTypes['']       = 'No Proxy';
+$lang->ai->models->proxyTypes['socks5'] = 'SOCKS5';
---
+++ module/ai/lang/zh-cn.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The ai module zh-cn file of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(http://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Wenrui LI <liwenrui@easycorp.ltd>
+ * @package     ai
+ * @link        https://www.zentao.net
+ */
+$lang->ai->common = 'AI';
+
+$lang->ai->prompts = new stdclass();
+$lang->ai->prompts->common = '提词';
+
+$lang->ai->conversations = new stdclass();
+$lang->ai->conversations->common = '会话';
+
+$lang->ai->models = new stdclass();
+$lang->ai->models->title          = '语言模型配置';
+$lang->ai->models->common         = '语言模型';
+$lang->ai->models->type           = '语言模型';
+$lang->ai->models->apiKey         = 'API Key';
+$lang->ai->models->proxyType      = '代理类型';
+$lang->ai->models->proxyAddr      = '代理地址';
+$lang->ai->models->description    = '描述';
+$lang->ai->models->testConnection = '测试连接';
+
+$lang->ai->models->statusList = array();
+$lang->ai->models->statusList['on']  = '启用';
+$lang->ai->models->statusList['off'] = '停用';
+
+$lang->ai->models->typeList = array();
+$lang->ai->models->typeList['openai-gpt35'] = 'OpenAI / GPT-3.5';
+$lang->ai->models->typeList['azure-gpt35']  = 'Azure / GPT-3.5';
+
+$lang->ai->models->proxyTypes = array();
+$lang->ai->models->proxyTypes['']       = '不使用代理';
+$lang->ai->models->proxyTypes['socks5'] = 'SOCKS5';
