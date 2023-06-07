9ae2dc46606daa4107344d29945d3de496440f55
sunhao <sunhao@easycorp.ltd>
2023-06-07 16:46:41 +0800

    * zin: add customCols prop to dtable.

--- lib/zin/wg/dtable/v1.php
+++ lib/zin/wg/dtable/v1.php
@@ -4,16 +4,24 @@ namespace zin;
 class dtable extends wg
 {
     static $defineProps = array(
-        'className?:string="shadow-sm rounded"',
-        'id?:string'
+        'className?:string="shadow-sm rounded"', // 表格样式。
+        'id?:string',                            // ID。
+        'customCols?: bool|array'                // 是否支持自定义列。
     );
 
     static $dtableID = 0;
 
     protected function created()
     {
-        $this->setDefaultProps(['id' => static::$dtableID ? static::$dtableID : 'dtable']);
+        $this->setDefaultProps(array('id' => static::$dtableID ? static::$dtableID : 'dtable'));
         static::$dtableID++;
+
+        if($this->prop('customCols') === true)
+        {
+            global $app;
+            $app->loadLang('datatable');
+            $this->setProp('customCols', array('url' => createLink('datatable', 'ajaxcustom', "module=$app->rawModule&method=$app->rawMethod"), 'hint' => $app->lang->datatable->custom));
+        }
     }
 
     public static function getPageCSS(): string|false
