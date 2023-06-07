37ddf760f80e69a89a5a215183f62acdbba180f2
sunguangming <sunguangming@easycorp.ltd>
2023-06-07 09:04:19 +0800

    * Add dtable group.

--- module/execution/config/dtable.php
+++ module/execution/config/dtable.php
@@ -22,6 +22,7 @@ if(isset($config->setCode) and $config->setCode == 1)
 {
     $config->execution->dtable->fieldList['code']['title']    = $lang->execution->code;
     $config->execution->dtable->fieldList['code']['name']     = 'code';
+    $config->execution->dtable->fieldList['code']['fixed']    = 'left';
     $config->execution->dtable->fieldList['code']['type']     = 'text';
     $config->execution->dtable->fieldList['code']['sortType'] = true;
     $config->execution->dtable->fieldList['code']['width']    = '136';
@@ -32,76 +33,89 @@ $config->execution->dtable->fieldList['project']['name']     = 'project';
 $config->execution->dtable->fieldList['project']['type']     = 'desc';
 $config->execution->dtable->fieldList['project']['sortType'] = true;
 $config->execution->dtable->fieldList['project']['width']    = '160';
+$config->execution->dtable->fieldList['project']['group']    = '1';
 
-$config->execution->dtable->fieldList['status']['title']    = $lang->execution->status;
-$config->execution->dtable->fieldList['status']['name']     = 'status';
-$config->execution->dtable->fieldList['status']['type']     = 'status';
+$config->execution->dtable->fieldList['status']['title']     = $lang->execution->status;
+$config->execution->dtable->fieldList['status']['name']      = 'status';
+$config->execution->dtable->fieldList['status']['type']      = 'status';
 $config->execution->dtable->fieldList['status']['statusMap'] = $lang->execution->statusList;
-$config->execution->dtable->fieldList['status']['sortType'] = true;
-$config->execution->dtable->fieldList['status']['width']    = '80';
+$config->execution->dtable->fieldList['status']['sortType']  = true;
+$config->execution->dtable->fieldList['status']['width']     = '80';
+$config->execution->dtable->fieldList['status']['group']     = '1';
 
 $config->execution->dtable->fieldList['PM']['title']    = $lang->execution->PM;
 $config->execution->dtable->fieldList['PM']['name']     = 'PM';
 $config->execution->dtable->fieldList['PM']['type']     = 'avatarBtn';
 $config->execution->dtable->fieldList['PM']['sortType'] = true;
 $config->execution->dtable->fieldList['PM']['width']    = '100';
+$config->execution->dtable->fieldList['PM']['group']    = '2';
 
 $config->execution->dtable->fieldList['openedDate']['title']    = $lang->execution->openedDate;
 $config->execution->dtable->fieldList['openedDate']['name']     = 'openedDate';
 $config->execution->dtable->fieldList['openedDate']['type']     = 'date';
 $config->execution->dtable->fieldList['openedDate']['sortType'] = true;
 $config->execution->dtable->fieldList['openedDate']['width']    = '96';
+$config->execution->dtable->fieldList['openedDate']['group']    = '3';
 
 $config->execution->dtable->fieldList['begin']['title']    = $lang->execution->begin;
 $config->execution->dtable->fieldList['begin']['name']     = 'begin';
 $config->execution->dtable->fieldList['begin']['type']     = 'date';
 $config->execution->dtable->fieldList['begin']['sortType'] = true;
 $config->execution->dtable->fieldList['begin']['width']    = '96';
+$config->execution->dtable->fieldList['begin']['group']    = '3';
 
 $config->execution->dtable->fieldList['end']['title']    = $lang->execution->end;
 $config->execution->dtable->fieldList['end']['name']     = 'end';
 $config->execution->dtable->fieldList['end']['type']     = 'date';
 $config->execution->dtable->fieldList['end']['sortType'] = true;
 $config->execution->dtable->fieldList['end']['width']    = '96';
+$config->execution->dtable->fieldList['end']['group']    = '3';
 
 $config->execution->dtable->fieldList['realBegan']['title']    = $lang->execution->realBeganAB;
 $config->execution->dtable->fieldList['realBegan']['name']     = 'realBegan';
 $config->execution->dtable->fieldList['realBegan']['type']     = 'date';
 $config->execution->dtable->fieldList['realBegan']['sortType'] = true;
 $config->execution->dtable->fieldList['realBegan']['width']    = '106';
+$config->execution->dtable->fieldList['realBegan']['group']    = '3';
 
 $config->execution->dtable->fieldList['realEnd']['title']    = $lang->execution->realEndAB;
 $config->execution->dtable->fieldList['realEnd']['name']     = 'realEnd';
 $config->execution->dtable->fieldList['realEnd']['type']     = 'date';
 $config->execution->dtable->fieldList['realEnd']['sortType'] = true;
 $config->execution->dtable->fieldList['realEnd']['width']    = '106';
+$config->execution->dtable->fieldList['realEnd']['group']    = '3';
 
 $config->execution->dtable->fieldList['totalEstimate']['title']    = $lang->execution->totalEstimate;
 $config->execution->dtable->fieldList['totalEstimate']['name']     = 'totalEstimate';
 $config->execution->dtable->fieldList['totalEstimate']['type']     = 'number';
 $config->execution->dtable->fieldList['totalEstimate']['sortType'] = false;
 $config->execution->dtable->fieldList['totalEstimate']['width']    = '64';
+$config->execution->dtable->fieldList['totalEstimate']['group']    = '4';
 
 $config->execution->dtable->fieldList['totalConsumed']['title']    = $lang->execution->totalConsumed;
 $config->execution->dtable->fieldList['totalConsumed']['name']     = 'totalConsumed';
 $config->execution->dtable->fieldList['totalConsumed']['type']     = 'number';
 $config->execution->dtable->fieldList['totalConsumed']['sortType'] = false;
 $config->execution->dtable->fieldList['totalConsumed']['width']    = '64';
+$config->execution->dtable->fieldList['totalConsumed']['group']    = '4';
 
 $config->execution->dtable->fieldList['totalLeft']['title']    = $lang->execution->totalLeft;
 $config->execution->dtable->fieldList['totalLeft']['name']     = 'totalLeft';
 $config->execution->dtable->fieldList['totalLeft']['type']     = 'number';
 $config->execution->dtable->fieldList['totalLeft']['sortType'] = false;
 $config->execution->dtable->fieldList['totalLeft']['width']    = '64';
+$config->execution->dtable->fieldList['totalLeft']['group']    = '4';
 
 $config->execution->dtable->fieldList['progress']['title']    = $lang->execution->progress;
 $config->execution->dtable->fieldList['progress']['name']     = 'progress';
 $config->execution->dtable->fieldList['progress']['type']     = 'progress';
 $config->execution->dtable->fieldList['progress']['sortType'] = false;
 $config->execution->dtable->fieldList['progress']['width']    = '64';
+$config->execution->dtable->fieldList['progress']['group']    = '4';
 
 $config->execution->dtable->fieldList['burn']['title']    = $lang->execution->burn;
 $config->execution->dtable->fieldList['burn']['name']     = 'burn';
 $config->execution->dtable->fieldList['burn']['type']     = 'burn';
 $config->execution->dtable->fieldList['burn']['sortType'] = false;
 $config->execution->dtable->fieldList['burn']['width']    = '88';
+$config->execution->dtable->fieldList['burn']['group']    = '4';
