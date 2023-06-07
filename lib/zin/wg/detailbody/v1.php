d59c429e0299f09f439dd9e23b8564dc7ec4831e
dingyongliang <dingyongliang@easycorp.ltd>
2023-06-07 17:21:06 +0800

    * zin: refactor detailbody.

--- lib/zin/wg/detailbody/v1.php
+++ lib/zin/wg/detailbody/v1.php
@@ -4,13 +4,32 @@ namespace zin;
 
 class detailBody extends wg
 {
+    protected static $defineBlocks = array(
+        'main' => array('map' => 'sectionList'),
+        'side' => array('map' => 'detailSide'),
+        'bottom' => array('map' => 'history'),
+        'floating' => array('map' => 'floatToolbar'),
+    );
+
     protected function build()
     {
+        $main     = $this->block('main');
+        $side     = $this->block('side');
+        $bottom   = $this->block('bottom');
+        $floating = $this->block('floating');
+
         return div
         (
-            setClass('detail-body canvas shadow rounded flex'),
+            setClass('detail-body rounded flex gap-1'),
             set($this->props->skip(array_keys(static::getDefinedProps()))),
-            $this->children()
+            div
+            (
+                setClass('col gap-1 grow'),
+                $main,
+                $bottom,
+                center(setClass('pt-6'), $floating),
+            ),
+            $side
         );
     }
 }
