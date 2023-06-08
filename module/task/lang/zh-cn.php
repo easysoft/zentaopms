6fc1cecca2712c27ff093332847660699d6d5a10
caoyanyi <caoyanyi@easycorp.ltd>
2023-06-08 17:05:33 +0800

    + Add language.

--- module/task/lang/zh-cn.php
+++ module/task/lang/zh-cn.php
@@ -69,6 +69,7 @@ $lang->task->common            = '任务';
 $lang->task->id                = '编号';
 $lang->task->project           = '所属' . $lang->projectCommon;
 $lang->task->execution         = '所属' . $lang->execution->common;
+$lang->task->stage             = '所属' . $lang->executionCommon;
 $lang->task->module            = '所属模块';
 $lang->task->moduleAB          = '模块';
 $lang->task->design            = '相关设计';
