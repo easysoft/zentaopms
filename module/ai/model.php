494f896b338f9303ead9efffe8aecad76da01efb
liwenrui <liwenrui@easycorp.ltd>
2023-06-08 16:48:49 +0800

    * fix head comments of ai module.

--- module/ai/model.php
+++ module/ai/model.php
@@ -3,10 +3,10 @@
  * The model file of ai module of ZenTaoPMS.
  * 
  * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
- * @license     ZPL(http://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
  * @author      Wenrui LI <liwenrui@easycorp.ltd>
  * @package     ai
- * @link        http://www.zentao.net
+ * @link        https://www.zentao.net
  */
 class aiModel extends model
 {
