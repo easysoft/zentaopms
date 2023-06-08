b426f5dff6fde30ab98443cbd8c702bb60f7aeb7
sunguangming <sunguangming@easycorp.ltd>
2023-06-08 14:54:35 +0800

    * Add dtable config for project::execution page.

--- module/project/config/dtable.php
+++ module/project/config/dtable.php
@@ -122,3 +122,137 @@ $config->project->dtable->fieldList['actions']['actionsMap']['whitelist']['url']
 $config->project->dtable->fieldList['actions']['actionsMap']['delete']['icon'] = 'trash';
 $config->project->dtable->fieldList['actions']['actionsMap']['delete']['hint'] = $lang->project->delete;
 $config->project->dtable->fieldList['actions']['actionsMap']['delete']['url']  = helper::createLink('project', 'delete', 'projectID={id}');
+
+global $app;
+$app->loadLang('execution');
+$app->loadConfig('execution');
+
+$config->projectExecution = new stdclass();
+$config->projectExecution->dtable = new stdclass();
+
+$config->projectExecution->dtable->fieldList['rawID']['title']    = $lang->idAB;
+$config->projectExecution->dtable->fieldList['rawID']['name']     = 'rawID';
+$config->projectExecution->dtable->fieldList['rawID']['type']     = 'checkID';
+$config->projectExecution->dtable->fieldList['rawID']['sortType'] = 'desc';
+$config->projectExecution->dtable->fieldList['rawID']['checkbox'] = true;
+$config->projectExecution->dtable->fieldList['rawID']['width']    = '80';
+
+$config->projectExecution->dtable->fieldList['name']['title']          = $lang->execution->name;
+$config->projectExecution->dtable->fieldList['name']['name']           = 'name';
+$config->projectExecution->dtable->fieldList['name']['fixed']          = 'left';
+$config->projectExecution->dtable->fieldList['name']['flex']           = 1;
+$config->projectExecution->dtable->fieldList['name']['type']           = 'html';
+$config->projectExecution->dtable->fieldList['name']['nestedToggle']   = true;
+$config->projectExecution->dtable->fieldList['name']['iconRender']     = true;
+$config->projectExecution->dtable->fieldList['name']['sortType']       = true;
+$config->projectExecution->dtable->fieldList['name']['minWidth']       = '356';
+
+$config->projectExecution->dtable->fieldList['productName']['title']    = $lang->execution->product;
+$config->projectExecution->dtable->fieldList['productName']['name']     = 'productName';
+$config->projectExecution->dtable->fieldList['productName']['type']     = 'desc';
+$config->projectExecution->dtable->fieldList['productName']['sortType'] = true;
+$config->projectExecution->dtable->fieldList['productName']['minWidth'] = '160';
+$config->projectExecution->dtable->fieldList['productName']['group']    = '1';
+
+$config->projectExecution->dtable->fieldList['status']['title']     = $lang->execution->status;
+$config->projectExecution->dtable->fieldList['status']['name']      = 'status';
+$config->projectExecution->dtable->fieldList['status']['type']      = 'status';
+$config->projectExecution->dtable->fieldList['status']['statusMap'] = $lang->execution->statusList + $lang->task->statusList;
+$config->projectExecution->dtable->fieldList['status']['sortType']  = true;
+$config->projectExecution->dtable->fieldList['status']['width']     = '80';
+$config->projectExecution->dtable->fieldList['status']['group']     = '1';
+
+$config->projectExecution->dtable->fieldList['PM']['title']    = $lang->execution->PM;
+$config->projectExecution->dtable->fieldList['PM']['name']     = 'PM';
+$config->projectExecution->dtable->fieldList['PM']['type']     = 'avatarBtn';
+$config->projectExecution->dtable->fieldList['PM']['sortType'] = true;
+$config->projectExecution->dtable->fieldList['PM']['width']    = '100';
+$config->projectExecution->dtable->fieldList['PM']['group']    = '2';
+
+$config->projectExecution->dtable->fieldList['begin']['title']    = $lang->execution->begin;
+$config->projectExecution->dtable->fieldList['begin']['name']     = 'begin';
+$config->projectExecution->dtable->fieldList['begin']['type']     = 'date';
+$config->projectExecution->dtable->fieldList['begin']['sortType'] = true;
+$config->projectExecution->dtable->fieldList['begin']['width']    = '96';
+$config->projectExecution->dtable->fieldList['begin']['group']    = '3';
+
+$config->projectExecution->dtable->fieldList['end']['title']    = $lang->execution->end;
+$config->projectExecution->dtable->fieldList['end']['name']     = 'end';
+$config->projectExecution->dtable->fieldList['end']['type']     = 'date';
+$config->projectExecution->dtable->fieldList['end']['sortType'] = true;
+$config->projectExecution->dtable->fieldList['end']['width']    = '96';
+$config->projectExecution->dtable->fieldList['end']['group']    = '3';
+
+$config->projectExecution->dtable->fieldList['totalEstimate']['title']    = $lang->execution->totalEstimate;
+$config->projectExecution->dtable->fieldList['totalEstimate']['name']     = 'totalEstimate';
+$config->projectExecution->dtable->fieldList['totalEstimate']['type']     = 'number';
+$config->projectExecution->dtable->fieldList['totalEstimate']['sortType'] = false;
+$config->projectExecution->dtable->fieldList['totalEstimate']['width']    = '64';
+$config->projectExecution->dtable->fieldList['totalEstimate']['group']    = '4';
+
+$config->projectExecution->dtable->fieldList['totalConsumed']['title']    = $lang->execution->totalConsumed;
+$config->projectExecution->dtable->fieldList['totalConsumed']['name']     = 'totalConsumed';
+$config->projectExecution->dtable->fieldList['totalConsumed']['type']     = 'number';
+$config->projectExecution->dtable->fieldList['totalConsumed']['sortType'] = false;
+$config->projectExecution->dtable->fieldList['totalConsumed']['width']    = '64';
+$config->projectExecution->dtable->fieldList['totalConsumed']['group']    = '4';
+
+$config->projectExecution->dtable->fieldList['totalLeft']['title']    = $lang->execution->totalLeft;
+$config->projectExecution->dtable->fieldList['totalLeft']['name']     = 'totalLeft';
+$config->projectExecution->dtable->fieldList['totalLeft']['type']     = 'number';
+$config->projectExecution->dtable->fieldList['totalLeft']['sortType'] = false;
+$config->projectExecution->dtable->fieldList['totalLeft']['width']    = '64';
+$config->projectExecution->dtable->fieldList['totalLeft']['group']    = '4';
+
+$config->projectExecution->dtable->fieldList['progress']['title']    = $lang->execution->progress;
+$config->projectExecution->dtable->fieldList['progress']['name']     = 'progress';
+$config->projectExecution->dtable->fieldList['progress']['type']     = 'progress';
+$config->projectExecution->dtable->fieldList['progress']['sortType'] = false;
+$config->projectExecution->dtable->fieldList['progress']['width']    = '64';
+$config->projectExecution->dtable->fieldList['progress']['group']    = '4';
+
+$config->projectExecution->dtable->fieldList['burn']['title']    = $lang->execution->burn;
+$config->projectExecution->dtable->fieldList['burn']['name']     = 'burn';
+$config->projectExecution->dtable->fieldList['burn']['type']     = 'burn';
+$config->projectExecution->dtable->fieldList['burn']['sortType'] = false;
+$config->projectExecution->dtable->fieldList['burn']['width']    = '88';
+$config->projectExecution->dtable->fieldList['burn']['group']    = '4';
+
+$config->projectExecution->dtable->fieldList['actions']['name']       = 'actions';
+$config->projectExecution->dtable->fieldList['actions']['title']      = $lang->actions;
+$config->projectExecution->dtable->fieldList['actions']['type']       = 'actions';
+$config->projectExecution->dtable->fieldList['actions']['width']      = '160';
+$config->projectExecution->dtable->fieldList['actions']['sortType']   = false;
+$config->projectExecution->dtable->fieldList['actions']['fixed']      = 'right';
+$config->projectExecution->dtable->fieldList['actions']['list']       = $config->execution->actionList;
+$config->projectExecution->dtable->fieldList['actions']['scrum']      = array('start', 'createTask', 'edit', 'close|activate', 'delete');
+$config->projectExecution->dtable->fieldList['actions']['waterfall']  = array('start', 'createTask', 'createChildStage', 'edit', 'close|activate', 'delete');
+$config->projectExecution->dtable->fieldList['actions']['task']       = array('startTask', 'finishTask', 'closeTask', 'recordWorkhour', 'editTask', 'batchCreate');
+
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['startTask']['icon']        = 'play';
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['startTask']['hint']        = $lang->task->start;
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['startTask']['url']         = helper::createLink('task', 'start', 'taskID={rawID}', '', true);
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['startTask']['data-toggle'] = 'modal';
+
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['finishTask']['icon']        = 'checked';
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['finishTask']['hint']        = $lang->task->finish;
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['finishTask']['url']         = helper::createLink('task', 'finish', 'taskID={rawID}', '', true);
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['finishTask']['data-toggle'] = 'modal';
+
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['closeTask']['icon']        = 'off';
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['closeTask']['hint']        = $lang->task->close;
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['closeTask']['url']         = helper::createLink('task', 'close', 'taskID={rawID}', '', true);
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['closeTask']['data-toggle'] = 'modal';
+
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['recordWorkhour']['icon'] = 'time';
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['recordWorkhour']['hint'] = $lang->task->record;
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['recordWorkhour']['url']  = helper::createLink('task', 'recordWorkhour', 'taskID={rawID}', '', true);
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['recordWorkhour']['data-toggle'] = 'modal';
+
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['editTask']['icon']  = 'edit';
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['editTask']['hint']  = $lang->task->edit;
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['editTask']['url']   = helper::createLink('task', 'edit', 'taskID={rawID}');
+
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['batchCreate']['icon'] = 'split';
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['batchCreate']['hint'] = $lang->task->batchCreate;
+$config->projectExecution->dtable->fieldList['actions']['actionsMap']['batchCreate']['url']  = helper::createLink('task', 'batchCreate', 'execution={execution}&storyID={story}&moduleID={module}&taskID={rawID}');
