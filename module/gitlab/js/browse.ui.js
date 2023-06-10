59d4f03a00a5df2fdbc5a398a0c8aa508be8d250
zhaoke <zhaoke@easycorp.ltd>
2023-06-10 17:28:04 +0800

    * Add page of gitlab-browse.

---
+++ module/gitlab/js/browse.ui.js
@@ -0,0 +1,27 @@
+window.createSortLink = function(col)
+{
+    var sort = col.name + '_asc';
+    if(sort == orderBy) sort = col.name + '_desc';
+
+    return sortLink.replace('{orderBy}', sort);
+}
+
+window.renderCell = function(result, {col, row})
+{
+    if(col.name === 'name')
+    {
+        var browseProjectLink = $.createLink('gitlab', 'browseProject', 'gitlabID=' + row.data.id);
+        if(canBrowseProject) result[0] = {html:'<a href="' + browseProjectLink + '">' + row.data.name + '</a>', style:{flexDirection:"column"}};
+
+        return result;
+    }
+
+    if(col.name === 'url')
+    {
+        result[0] = {html:'<a href="' + row.data.url + '" target="_blank">' + row.data.url + '</a>', style:{flexDirection:"column"}};
+
+        return result;
+    }
+
+    return result;
+};
