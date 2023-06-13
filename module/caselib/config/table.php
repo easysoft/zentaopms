378f88effc0bb182e9a2f40307b539f96259268c
liumengyi <liumengyi@easycorp.ltd>
2023-06-13 10:29:40 +0800

    * Modify dtable config of caselib.

--- module/caselib/config/table.php
+++ module/caselib/config/table.php
@@ -7,14 +7,14 @@ $config->caselib->testcase = new stdclass();
 $config->caselib->testcase->actionList['edit']['icon']  = 'edit';
 $config->caselib->testcase->actionList['edit']['hint']  = $lang->testcase->edit;
 $config->caselib->testcase->actionList['edit']['text']  = $lang->testcase->edit;
-$config->caselib->testcase->actionList['edit']['url']   = helper::createLink('testcase', 'edit', 'caseID={id}');
+$config->caselib->testcase->actionList['edit']['url']   = array('module' => 'testcase', 'method' => 'edit', 'params' => 'caseID={id}');
 $config->caselib->testcase->actionList['edit']['order'] = 5;
 $config->caselib->testcase->actionList['edit']['show']  = 'clickable';
 
 $config->caselib->testcase->actionList['delete']['icon']  = 'trash';
 $config->caselib->testcase->actionList['delete']['hint']  = $lang->testcase->delete;
 $config->caselib->testcase->actionList['delete']['text']  = $lang->testcase->delete;
-$config->caselib->testcase->actionList['delete']['url']   = helper::createLink('testcase', 'delete', 'caseID={id}');
+$config->caselib->testcase->actionList['delete']['url']   = array('module' => 'testcase', 'method' => 'delete', 'params' => 'caseID={id}');
 $config->caselib->testcase->actionList['delete']['order'] = 10;
 $config->caselib->testcase->actionList['delete']['show']  = 'clickable';
 
