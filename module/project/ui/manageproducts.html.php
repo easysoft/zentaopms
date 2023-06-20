09869bfb429e31b60b8b83e34c8f897605d9bdab
caoyanyi <caoyanyi@easycorp.ltd>
2023-06-20 18:48:25 +0800

    * Change comment.

--- module/project/ui/manageproducts.html.php
+++ module/project/ui/manageproducts.html.php
@@ -1,11 +1,11 @@
 <?php
 declare(strict_types=1);
 /**
- * The manage product view file of execution module of ZenTaoPMS.
+ * The manage product view file of project module of ZenTaoPMS.
  * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
  * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
  * @author      Yanyi Cao<caoyanyi@easycorp.ltd>
- * @package     execution
+ * @package     project
  * @link        https://www.zentao.net
  */
 namespace zin;
