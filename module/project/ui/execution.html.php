b6dafe6e7d3286f8059eb38aa35783e862f5bc08
tianshujie <tianshujie@easycorp.ltd>
2023-06-21 13:09:28 +0800

    + Add param for label link.

--- module/project/ui/execution.html.php
+++ module/project/ui/execution.html.php
@@ -67,6 +67,7 @@ featureBar
         'closeLink' => '#'
     ]))),
     set::current($status),
+    set::linkParams("status={key}&projectID={$projectID}&orderBy={$orderBy}&productID={$productID}&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}&pageID={$pager->pageID}"),
     li(searchToggle()),
     li
     (
