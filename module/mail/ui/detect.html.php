5a3b32c214734717045bdec2a8c61bf8408b5f84
dingguodong <dingguodong@easycorp.ltd>
2023-06-21 18:26:05 +0800

    + Add mail config detection view.

---
+++ module/mail/ui/detect.html.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The detect view file of mail module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      dingguodong <dingguodong@easycorp.ltd>
+ * @package     mail
+ * @link        https://www.zentao.net
+ */
+declare(strict_types=1);
+namespace zin;
+
+formPanel(
+    set::labelWidth('11em'),
+    formGroup(
+        set::width('1/2'),
+        set::label($lang->mail->inputFromEmail),
+        inputGroup(
+            input(
+                set::name('fromAddress'),
+                set::value($fromAddress),
+                set::required(true),
+                set::autofocus(true)
+            )
+        )
+    ),
+    set::actions(array('submit')),
+    set::submitBtnText($lang->mail->nextStep)
+);
+
+render();
+
