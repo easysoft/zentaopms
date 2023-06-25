7bff7494135a626ade1e0741351fc14190b1d36e
dingguodong <dingguodong@easycorp.ltd>
2023-06-25 16:32:33 +0800

    + Add the export view file of program module.

---
+++ module/program/ui/export.html.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The export view file of program module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      dingguodong <dingguodong@easycorp.ltd>
+ * @package     program
+ * @link        https://www.zentao.net
+ */
+declare(strict_types=1);
+
+include '../../file/ui/export.html.php';
