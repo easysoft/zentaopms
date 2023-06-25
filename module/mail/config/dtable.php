ced03e4f30fce91d02a01817958c796ae316aba7
chentao <chentao@easycorp.ltd>
2023-06-25 16:54:36 +0800

    * Add fields list of data table on email queue page within mail module.

---
+++ module/mail/config/dtable.php
@@ -0,0 +1,76 @@
+<?php
+global $lang;
+
+$config->mail->browse = new stdclass();
+$config->mail->browse->dtable = new stdclass();
+
+$config->mail->browse->dtable->fieldList['id']['name']     = 'id';
+$config->mail->browse->dtable->fieldList['id']['title']    = $lang->idAB;
+$config->mail->browse->dtable->fieldList['id']['type']     = 'id';
+$config->mail->browse->dtable->fieldList['id']['checkbox'] = true;
+$config->mail->browse->dtable->fieldList['id']['sortType'] = true;
+$config->mail->browse->dtable->fieldList['id']['required'] = true;
+$config->mail->browse->dtable->fieldList['id']['group']    = 1;
+
+$config->mail->browse->dtable->fieldList['toList']['name']     = 'toList';
+$config->mail->browse->dtable->fieldList['toList']['title']    = $lang->mail->toList;
+$config->mail->browse->dtable->fieldList['toList']['type']     = 'text';
+$config->mail->browse->dtable->fieldList['toList']['sortType'] = true;
+$config->mail->browse->dtable->fieldList['toList']['required'] = true;
+$config->mail->browse->dtable->fieldList['toList']['group']    = 2;
+
+$config->mail->browse->dtable->fieldList['subject']['name']     = 'subject';
+$config->mail->browse->dtable->fieldList['subject']['title']    = $lang->mail->subject;
+$config->mail->browse->dtable->fieldList['subject']['type']     = 'text';
+$config->mail->browse->dtable->fieldList['subject']['sortType'] = true;
+$config->mail->browse->dtable->fieldList['subject']['required'] = true;
+$config->mail->browse->dtable->fieldList['subject']['group']    = 3;
+
+$config->mail->browse->dtable->fieldList['createdBy']['name']     = 'createdBy';
+$config->mail->browse->dtable->fieldList['createdBy']['title']    = $lang->mail->createdBy;
+$config->mail->browse->dtable->fieldList['createdBy']['type']     = 'user';
+$config->mail->browse->dtable->fieldList['createdBy']['sortType'] = true;
+$config->mail->browse->dtable->fieldList['createdBy']['required'] = true;
+$config->mail->browse->dtable->fieldList['createdBy']['group']    = 4;
+
+$config->mail->browse->dtable->fieldList['createdDate']['name']     = 'createdDate';
+$config->mail->browse->dtable->fieldList['createdDate']['title']    = $lang->mail->createdDate;
+$config->mail->browse->dtable->fieldList['createdDate']['type']     = 'date';
+$config->mail->browse->dtable->fieldList['createdDate']['sortType'] = true;
+$config->mail->browse->dtable->fieldList['createdDate']['required'] = true;
+$config->mail->browse->dtable->fieldList['createdDate']['group']    = 5;
+
+$config->mail->browse->dtable->fieldList['sendTime']['name']     = 'sendTime';
+$config->mail->browse->dtable->fieldList['sendTime']['title']    = $lang->mail->sendTime;
+$config->mail->browse->dtable->fieldList['sendTime']['type']     = 'date';
+$config->mail->browse->dtable->fieldList['sendTime']['sortType'] = true;
+$config->mail->browse->dtable->fieldList['sendTime']['required'] = true;
+$config->mail->browse->dtable->fieldList['sendTime']['group']    = 6;
+
+$config->mail->browse->dtable->fieldList['status']['name']      = 'status';
+$config->mail->browse->dtable->fieldList['status']['title']     = $lang->mail->status;
+$config->mail->browse->dtable->fieldList['status']['type']      = 'status';
+$config->mail->browse->dtable->fieldList['status']['statusMap'] = $lang->mail->statusList;
+$config->mail->browse->dtable->fieldList['status']['sortType']  = true;
+$config->mail->browse->dtable->fieldList['status']['required']  = true;
+$config->mail->browse->dtable->fieldList['status']['group']     = 7;
+
+$config->mail->browse->dtable->fieldList['failReason']['name']     = 'failReason';
+$config->mail->browse->dtable->fieldList['failReason']['title']    = $lang->mail->failReason;
+$config->mail->browse->dtable->fieldList['failReason']['type']     = 'text';
+$config->mail->browse->dtable->fieldList['failReason']['required'] = true;
+$config->mail->browse->dtable->fieldList['failReason']['group']    = 8;
+
+$config->mail->browse->dtable->fieldList['actions']['name']     = 'actions';
+$config->mail->browse->dtable->fieldList['actions']['title']    = $lang->actions;
+$config->mail->browse->dtable->fieldList['actions']['type']     = 'actions';
+$config->mail->browse->dtable->fieldList['actions']['required'] = true;
+$config->mail->browse->dtable->fieldList['actions']['fixed']    = 'right';
+$config->mail->browse->dtable->fieldList['actions']['group']    = 9;
+
+$config->mail->browse->dtable->fieldList['actions']['menu'] = array('delete', 'resend');
+$config->mail->browse->dtable->fieldList['actions']['list'] = array
+(
+    'delete' => array('icon' => 'trash', 'class' => array('ajax-submit px-1.5'), 'url' => helper::createLink('mail', 'delete', 'id={id}'), 'hint' => $lang->delete),
+    'resend' => array('icon' => 'share', 'class' => array('ajax-submit px-1.5'), 'url' => helper::createLink('mail', 'resend', 'id={id}'), 'hint' => $lang->mail->resend),
+);
