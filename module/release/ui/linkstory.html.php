35fb4aa284d9b43bddd0a10c14f651479ea80841
caoyanyi <caoyanyi@easycorp.ltd>
2023-06-26 14:49:17 +0800

    * Change licenses data.

--- module/release/ui/linkstory.html.php
+++ module/release/ui/linkstory.html.php
@@ -1,11 +1,11 @@
 <?php
 declare(strict_types=1);
 /**
- * The linkStory view file of productplan module of ZenTaoPMS.
+ * The linkStory view file of release module of ZenTaoPMS.
  * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
  * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
- * @author      Wang Yidong <yidong@easycorp.ltd>
- * @package     productplan
+ * @author      Yanyi Cao <caoyanyi@easycorp.ltd>
+ * @package     release
  * @link        https://www.zentao.net
  */
 namespace zin;
