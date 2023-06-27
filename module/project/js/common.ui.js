f6b1fae36c87a1c66f247e17dff5691ad4f010ce
sunguangming <sunguangming@easycorp.ltd>
2023-06-27 09:36:25 +0800

    * Hide branch when add line.

--- module/project/js/common.ui.js
+++ module/project/js/common.ui.js
@@ -436,6 +436,7 @@ window.addNewLine = function(e)
     newLine.find("select[name^='products']").attr('name', 'products[' + index + ']').attr('id', 'products' + index).val('');
     newLine.find("select[name^='plans']").attr('name', 'plans[' + index + '][' + 0 + '][]').val('');
     newLine.find("select[name^='branch']").val('');
+    newLine.find('.form-group').eq(1).addClass('hidden');
     newLine.find("div[id^='plan']").attr('id', 'plan' + index);
 
     $(obj).closest('.form-row').after(newLine);
