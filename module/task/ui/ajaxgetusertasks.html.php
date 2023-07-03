6df4ebdc3bf4ac7c791ba3ffeaae27ff6b3fd8a0
caoyanyi <caoyanyi@easycorp.ltd>
2023-07-03 18:57:21 +0800

    + Add task select page.

---
+++ module/task/ui/ajaxgetusertasks.html.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+/**
+ * The ajaxGetUserTasks view file of task module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Yanyi Cao<caoyanyi@easycorp.ltd>
+ * @package     task
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+
+$filedName = $id ? "tasks[$id]" : 'task';
+control
+(
+    set::name($filedName),
+    set::items($tasks),
+    set::required(true)
+);
+
+render();
