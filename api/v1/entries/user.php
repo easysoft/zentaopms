382d6e1b377b100caa6cb29a4ac172fc6169ef2d
caoyanyi <caoyanyi@easycorp.ltd>
2023-07-03 14:42:54 +0800

    + Add rights return.

--- api/v1/entries/user.php
+++ api/v1/entries/user.php
@@ -370,7 +370,7 @@ class userEntry extends entry
                     $info->rights = array();
                     $info->rights['admin']  = (!empty($inAdminGroup) or $this->app->user->admin);
 
-                    $usedModules = ',bug,project,execution,task,file,effort,feedback,story,testcase,program,projectstory,testtask,doc,projectbuild,release,meeting,';
+                    $usedModules = ',bug,project,execution,task,file,effort,feedback,story,testcase,program,projectstory,testtask,doc,projectbuild,release,meeting,product,qa,';
                     $info->rights['rights'] = array();
                     foreach($this->app->user->rights['rights'] as $module => $priv) if(strpos($usedModules, ",{$module},") !== false) $info->rights['rights'][$module] = $priv;
             }
