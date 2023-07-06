8eef9fb9ffb9e0f18b8aca3e88b2082a6ef5dae3
liumengyi <liumengyi@easycorp.ltd>
2023-07-06 16:42:46 +0800

    + Add comments.

--- module/testtask/js/runcase.ui.js
+++ module/testtask/js/runcase.ui.js
@@ -2,6 +2,13 @@ $(function()
 {
     loadResult();
 });
+
+/**
+ * Load result.
+ *
+ * @access public
+ * @return void
+ */
 function loadResult()
 {
     loadCurrentPage({url: resultsLink, selector: '#casesResults', partial: true});
