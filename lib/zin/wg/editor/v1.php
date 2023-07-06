b7a1e97b99db66557fee4b97c735c6b0c2d26f96
liugang <liugang@easycorp.ltd>
2023-07-06 13:59:55 +0800

    * editorWidget: set the default mode of menu bar to compact and default width to 100%.

--- lib/zin/wg/editor/v1.php
+++ lib/zin/wg/editor/v1.php
@@ -6,17 +6,17 @@ require_once dirname(__DIR__) . DS . 'textarea' . DS . 'v1.php';
 class editor extends wg
 {
     protected static $defineProps = array(
-        'createInput?: bool=true',     // 是否创建一个隐藏的 input 存储编辑器内容
-        'uploadUrl?: string=""',       // 图片上传链接
-        'placeholder?: string=""',     // 占位文本
-        'fullscreenable?: bool=true',  // 是否可全屏
-        'resizable?: bool=true',       // 是否可自适应
-        'exposeEditor?: bool=true',    // 是否将编辑器实例挂载到 window
-        'size?: string="sm"',          // 尺寸
-        'hideMenubar?: bool=false',    // 是否隐藏 menubar
-        'bubbleMenu?: bool=false',     // 是否启用菜单冒泡
-        'menubarMode?: string',        // 菜单栏模式
-        'value?: string'               // 内容
+        'createInput?: bool=true',        // 是否创建一个隐藏的 input 存储编辑器内容
+        'uploadUrl?: string=""',          // 图片上传链接
+        'placeholder?: string=""',        // 占位文本
+        'fullscreenable?: bool=true',     // 是否可全屏
+        'resizable?: bool=true',          // 是否可自适应
+        'exposeEditor?: bool=true',       // 是否将编辑器实例挂载到 window
+        'size?: string="sm"',             // 尺寸
+        'hideMenubar?: bool=false',       // 是否隐藏 menubar
+        'bubbleMenu?: bool=false',        // 是否启用菜单冒泡
+        'menubarMode?: string="compact"', // 菜单栏模式
+        'value?: string'                  // 内容
         // 'collaborative?: bool=false',
         // 'hocuspocus?: string=""',
         // 'docName?: string=""',
@@ -26,17 +26,21 @@ class editor extends wg
 
     protected function build()
     {
-        $props = $this->props->pick(array('createInput', 'uploadUrl', 'placeholder', 'fullscreenable', 'resizable', 'exposeEditor', 'size', 'hideMenubar', 'bubbleMenu', 'menubarMode', 'collaborative', 'hocuspocus', 'docName', 'username', 'userColor'));
         $editor = new h(set::tagName('tiptap-editor'));
+        $props  = $this->props->pick(array('createInput', 'uploadUrl', 'placeholder', 'fullscreenable', 'resizable', 'exposeEditor', 'size', 'hideMenubar', 'bubbleMenu', 'menubarMode', 'collaborative', 'hocuspocus', 'docName', 'username', 'userColor'));
         foreach ($props as $key => $value)
         {
             if($value === true || (is_string($value) && !empty($value))) $editor->add(set(uncamelize($key), $value));
         }
+
         $customProps = $this->props->skip(array_keys(static::getDefinedProps()));
-        if(!isset($customProps['id'])) $customProps['id'] = $customProps['name'];
+        if(!isset($customProps['id']))    $customProps['id'] = $customProps['name'];
+        if(!isset($customProps['class'])) $customProps['class'] = 'w-full';
+
         $editor->add(set($customProps));
         $editor->add($this->prop('value'));
         $editor->add($this->children());
+
         return $editor;
     }
 }
