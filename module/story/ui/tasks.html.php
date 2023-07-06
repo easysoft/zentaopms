745bc9d19ddb7a1e730315bdd9d438e4c65f9dd1
dingguodong <dingguodong@easycorp.ltd>
2023-07-06 14:27:14 +0800

    * Add a icon before title text.

--- module/story/ui/tasks.html.php
+++ module/story/ui/tasks.html.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  */
 namespace zin;
 
-modalHeader();
+modalHeader(set::titleClass('icon icon-bar-chart'));
 
 $tableData = initTableData($tasks, $config->story->taskTable->fieldList, $this->story);
 
