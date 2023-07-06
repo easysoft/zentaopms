b62dcfa533ac448f63d0f89d1c3022da198a3a18
dingguodong <dingguodong@easycorp.ltd>
2023-07-06 15:50:14 +0800

    * Allow customized export for stories.

--- module/story/control.php
+++ module/story/control.php
@@ -2133,7 +2133,7 @@ class story extends control
 
         $this->view->fileName        = $fileName;
         $this->view->allExportFields = $this->config->story->exportFields;
-        $this->view->customExport    = false;
+        $this->view->customExport    = true;
         $this->display();
     }
 
