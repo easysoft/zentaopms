e25ba875b25f2d163eafd9bd17fe167b9dbfe099
sunhao <sunhao@easycorp.ltd>
2023-07-07 14:50:14 +0800

    * zin: fix datepicker not work.

--- lib/zin/wg/datepicker/v1.php
+++ lib/zin/wg/datepicker/v1.php
@@ -13,11 +13,14 @@ class datePicker extends wg
      */
     protected function build(): wg
     {
+        list($name, $id, $defaultValue) = $this->prop(array('name', 'id', 'defaultValue'));
         return zui::datePicker
         (
             set::_class('form-group-wrapper'),
+            set::_map(array('value' => 'defaultValue')),
             set::icon('calendar'),
-            set($this->props)
+            set($this->props),
+            h::formHidden($name, $defaultValue, setID($id))
         );
     }
 }
