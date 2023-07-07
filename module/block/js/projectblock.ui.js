92984faf9031cf75ae7770ae63ac06d0155a3cbd
wangyuting <wangyuting@easycorp.ltd>
2023-07-07 13:17:29 +0800

    * Adjust label width.

--- module/block/js/projectblock.ui.js
+++ module/block/js/projectblock.ui.js
@@ -2,7 +2,7 @@ window.onRenderProjectNameCell = function(result, info)
 {
     if(info.col.name === 'name' && info.row.data.delay > 0)
     {
-        const html = {html : '<span class="label size-sm circle danger-pale">' + delayInfo.replace('%s', info.row.data.delay) + '</span>', className : 'flex items-end w-24', style : {flexDirection : "column"}};
+        const html = {html : '<span class="label size-sm circle danger-pale">' + delayInfo.replace('%s', info.row.data.delay) + '</span>', className : 'flex items-end w-28', style : {flexDirection : "column"}};
         result.push(html);
     }
 
