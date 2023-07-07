0c184465332f8fdbb3052c835eeb72c3218f6693
dingguodong <dingguodong@easycorp.ltd>
2023-07-07 16:39:09 +0800

    - Remove blank line.

--- module/datatable/model.php
+++ module/datatable/model.php
@@ -93,7 +93,6 @@ class datatableModel extends model
 
         $module = zget($this->config->datatable->moduleAlias, "$module-$method", $module);
 
-
         if(!isset($this->config->$module)) $this->loadModel($module);
         if(isset($this->config->datatable->$datatableId->cols)) $setting = json_decode($this->config->datatable->$datatableId->cols, true);
 
