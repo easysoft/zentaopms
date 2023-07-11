33f860134a4bba98bd17a69ffbc188fc63d62777
caoyanyi <caoyanyi@easycorp.ltd>
2023-07-11 16:02:57 +0800

    - Remove unused code.

--- module/mail/js/browse.ui.js
+++ module/mail/js/browse.ui.js
@@ -13,37 +13,3 @@ window.onClickBatchDelete = function(event)
 
     $.ajaxSubmit({"url": $(event.target).attr('href'), "data": postData});
 };
-
-window.confirmDelete = function(actionUrl)
-{
-    zui.Modal
-    .confirm({message: confirmDeleteTip, icon:'icon-info-sign', iconClass: 'warning-pale rounded-full icon-2x'})
-    .then((confirmed) =>
-    {
-        if(confirmed) $.ajaxSubmit({url: actionUrl});
-    });
-};
-
-window.resendAlert = function(type, msg)
-{
-    zui.Messager.show({content: msg, type: type});
-
-    loadCurrentPage();
-};
-
-window.renderCellActions = function(result, {col, row})
-{
-    if(col.name !== 'actions') return result;
-
-    return [
-        {
-            html: row.data[col.name].map(action =>
-            {
-                const setting = col.setting.actionsMap[action.name];
-                const url     = setting.url.replace('{id}', row.data.id);
-
-                return `<a href='${url}' class='${setting.class[0]}'>${setting.hint}</a>`;
-            }).join(' '),
-        }
-    ];
-};
