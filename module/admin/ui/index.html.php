bdc51660c954c1e695bad81e77d6a06710388efd
liugang <liugang@easycorp.ltd>
2023-07-12 19:00:59 +0800

    * admin: refactor setting block in index page of admin module.

--- module/admin/ui/index.html.php
+++ module/admin/ui/index.html.php
@@ -16,6 +16,8 @@ jsVar('hasInternet', $zentaoData->hasData);
 $settingItems = array();
 foreach($lang->admin->menuList as $menuKey => $menu)
 {
+    if($config->vision == 'lite' and !in_array($menuKey, $config->admin->liteMenuList)) continue;
+
     $link = '';
     if(!empty($menu['link'])) $link = $menu['link'];
     if(!empty($menu['subMenu']))
@@ -36,49 +38,91 @@ foreach($lang->admin->menuList as $menuKey => $menu)
 
     $url = $module && $method ? createLink($module, $method) : '';
 
-    if($config->vision == 'lite' and !in_array($menuKey, $config->admin->liteMenuList)) continue;
-    $settingItems[] = button
+    $settingItems[] = div
     (
-        setClass('flex col justify-between basis-20% shrink-1 border m-2 px-2 py-2 w-1/5 h-32'),
-        set(array
+        setClass('setting-box p-2 w-1/5 h-32'),
+        set(array('data-id' => $menuKey)),
+        button
         (
-            'data-on' => $url ? 'click' : '',
-            'data-call' => $url ? "window.location.href='{$url}'" : '',
-            'title' => $menu['disabled'] ? $lang->admin->noPriv : '',
-        )),
-        h4
-        (
-            setClass('flex'),
-            img(set::src("static/svg/admin-{$menuKey}.svg")),
-            $menu['name'],
-            a
+            setClass('flex col border rounded-md px-2.5 py-1 w-full h-full'),
+            set(array
             (
-                setClass('text-gray'),
-                set::href($config->admin->helpURL[$menuKey]),
-                set::title($lang->help),
-                set::target('_blank'),
-                icon('help'),
+                'data-on' => $url ? 'click' : '',
+                'data-call' => $url ? "window.location.href='{$url}'" : '',
+                'title' => $menu['disabled'] ? $lang->admin->noPriv : '',
+            )),
+            h4
+            (
+                setClass('flex align-center my-2.5 w-full'),
+                div
+                (
+                    setClass('flex align-center gap-1 font-bold text-md'),
+                    img(set::src("static/svg/admin-{$menuKey}.svg")),
+                    $menu['name'],
+                    a
+                    (
+                        setClass('text-gray'),
+                        set::href($config->admin->helpURL[$menuKey]),
+                        set::title($lang->help),
+                        set::target('_blank'),
+                        icon('help'),
+                    )
+                ),
             ),
-        ),
-        p
-        (
-            setClass('text-left'),
-            set::title($menu['desc']),
-            $menu['desc']
+            p
+            (
+                setClass('text-left text-gray pb-4 h-12 leading-6'),
+                set::title($menu['desc']),
+                $menu['desc']
+            )
         )
     );
 }
 
-col
+div
 (
-    setClass('w-2/3'),
+    set::style(array('width' => '70%')),
     panel
     (
         setID('settings'),
+        setClass('mb-4 px-2'),
         set::title($lang->admin->setting),
-        set::bodyClass('flex flex-wrap pt-0 pb-4 px-1'),
+        set::bodyClass('flex flex-wrap'),
         $settingItems
+    ),
+    panel
+    (
+        setID('plugin'),
+        setClass('mb-4 px-2'),
+        set::title($lang->admin->pluginRecommendation),
+    ),
+    div
+    (
+        setClass('flex'),
+        panel
+        (
+            setClass('mr-4 '),
+            set::style(array('width' => '30%')),
+            set::title($lang->admin->officialAccount),
+        ),
+        panel
+        (
+            set::style(array('width' => '70%')),
+            set::title($lang->admin->publicClass),
+        )
     )
 );
+
+panel
+(
+    setClass('ml-4'),
+    set::style(array('width' => '30%')),
+    set::title($lang->admin->zentaoInfo),
+    div
+    (
+        setClass('h-56px'),
+    )
+);
+
 
 render();
