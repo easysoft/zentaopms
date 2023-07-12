0485fb217966cbdd6354e3f5baf67559dab15558
zhouxin <zhouxin@easycorp.ltd>
2023-07-12 17:29:05 +0800

    * Add metric and unit test: count_of_annual_created_release_in_product.

--- module/metric/calc/product/scale/count_of_annual_created_release_in_product.php
+++ module/metric/calc/product/scale/count_of_annual_created_release_in_product.php
@@ -8,15 +8,12 @@
  * 目的：scale
  * 度量名称：按产品统计的年度新增发布数
  * 单位：个
- * 描述：产品中发布时间为某年的发布个数求和
-过滤已删除的发布
-过滤已删除的产品
-过滤无效时间
+ * 描述：产品中发布时间为某年的发布个数求和 过滤已删除的发布 过滤已删除的产品 过滤无效时间
  * 度量库：
  * 收集方式：realtime
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
- * @author    qixinzhi <qixinzhi@easycorp.ltd>
+ * @author    zhouxin <zhouxin@easycorp.ltd>
  * @package
  * @uses      func
  * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
@@ -24,21 +21,39 @@
  */
 class count_of_annual_created_release_in_product extends baseCalc
 {
-    public $dataset = '';
+    public $dataset = 'getProductReleases';
 
-    public $fieldList = array();
+    public $fieldList = array('t1.id', 't1.product', 't1.createdDate');
 
     public $result = array();
 
-    //public function getStatement()
-    //{
-    //}
+    public function calculate($row)
+    {
+        $year = substr($row->createdDate, 0, 4);
 
-    //public function calculate($data)
-    //{
-    //}
+        if($year == '0000') return null;
+        if(empty($year))    return null;
 
-    //public function getResult()
-    //{
-    //}
+        if(!isset($this->result[$year])) $this->result[$year] = array();
+        if(!isset($this->result[$year][$row->product])) $this->result[$year][$row->product] = 0;
+        $this->result[$year][$row->product] ++;
+    }
+
+    public function getResult($options = array())
+    {
+        $records = array();
+        foreach($this->result as $year => $products)
+        {
+            foreach($products as $product => $count)
+            {
+                $records[] = (object)array(
+                    'year'    => $year,
+                    'product' => $product,
+                    'value'   => $count,
+                );
+            }
+        }
+
+        return $this->filterByOptions($records, $options);
+    }
 }
---
+++ module/metric/test/calc/product/scale/count_of_annual_created_release_in_product.php
@@ -0,0 +1,30 @@
+#!/usr/bin/env php
+<?php
+include dirname(__FILE__, 7) . '/test/lib/init.php';
+include dirname(__FILE__, 4) . '/calc.class.php';
+
+zdTable('release')->config('release_create')->gen(100);
+
+$metric = new metricTest();
+$calc   = $metric->calcMetric(__FILE__);
+
+/**
+
+title=count_of_annual_created_release_in_product
+timeout=0
+cid=1
+
+- 测试分组数。 @10
+
+- 测试产品1的发布数。第0条的value属性 @10
+
+- 测试产品10的发布数。第0条的value属性 @10
+
+- 测试不存在的产品的发布数。 @0
+
+*/
+
+r(count($calc->getResult()))                                     && p('')        && e('10'); // 测试分组数。
+r($calc->getResult(array('product' => '1', 'year' => '2018')))   && p('0:value') && e('10'); // 测试产品1的发布数。
+r($calc->getResult(array('product' => '10', 'year' => '2018')))  && p('0:value') && e('10'); // 测试产品10的发布数。
+r($calc->getResult(array('product' => '999', 'year' => '2018'))) && p('')        && e('0');  // 测试不存在的产品的发布数。
