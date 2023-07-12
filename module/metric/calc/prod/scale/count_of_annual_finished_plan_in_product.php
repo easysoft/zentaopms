7f62a7f9d3d518e5cc1a7fcea4af303ded18ddb7
zhouxin <zhouxin@easycorp.ltd>
2023-07-12 14:34:07 +0800

    * Add metric: count_of_annual_finished_plan_in_product.

--- module/metric/calc/prod/scale/count_of_annual_finished_plan_in_product.php
+++ module/metric/calc/prod/scale/count_of_annual_finished_plan_in_product.php
@@ -1,21 +1,18 @@
 <?php
 /**
  * 按产品统计的年度完成计划数。
- * .
  *
  * 范围：prod
  * 对象：productplan
  * 目的：scale
  * 度量名称：按产品统计的年度完成计划数
  * 单位：个
- * 描述：产品中完成时间为某年的计划个数求和
-过滤已删除的计划
-过滤已删除的产品
+ * 描述：产品中完成时间为某年的计划个数求和 过滤已删除的计划 过滤已删除的产品
  * 度量库：
  * 收集方式：realtime
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
- * @author    qixinzhi <qixinzhi@easycorp.ltd>
+ * @author    zhouxin <zhouxin@easycorp.ltd>
  * @package
  * @uses      func
  * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
@@ -23,21 +20,40 @@
  */
 class count_of_annual_finished_plan_in_product extends baseCalc
 {
-    public $dataset = '';
+    public function getStatement()
+    {
+        return $this->dao->select('t1.id,t1.product,year(t1.finishedDate) as year')->from(TABLE_PRODUCTPLAN)->alias('t1')
+            ->leftJoin(TABLE_PRODUCT)->alias('t2')->on('t1.product=t2.id')
+            ->where('t1.deleted')->eq(0)
+            ->andWhere('t2.deleted')->eq(0)
+            ->query();
+    }
 
-    public $fieldList = array();
+    public function calculate($row)
+    {
+        if(!empty($row->year))
+        {
+            if(!isset($this->result[$row->year])) $this->result[$row->year] = array();
+            if(!isset($this->result[$row->year][$row->product])) $this->result[$row->year][$row->product] = 0;
+            $this->result[$row->year][$row->product] ++;
+        }
+    }
 
-    public $result = array();
+    public function getResult($options = array())
+    {
+        $records = array();
+        foreach($this->result as $year => $products)
+        {
+            foreach($products as $product => $count)
+            {
+                $records[] = (object)array(
+                    'year'    => $year,
+                    'product' => $product,
+                    'count'   => $count,
+                );
+            }
+        }
 
-    //public function getStatement()
-    //{
-    //}
-
-    //public function calculate($data)
-    //{
-    //}
-
-    //public function getResult()
-    //{
-    //}
+        return $records;
+    }
 }
