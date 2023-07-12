883e2a68d7cf506a2794311bf6314e2cd9ac6372
chaideqing <chaideqing@easycorp.ltd>
2023-07-12 01:10:24 +0000

    * Add story type to check story duplicate.

--- module/common/model.php
+++ module/common/model.php
@@ -2327,6 +2327,7 @@ EOF;
             ->andWhere($titleField)->in($titles)
             ->andWhere($dateField)->ge($date)->fi()
             ->beginIF($condition)->andWhere($condition)->fi()
+            ->beginIF($type == 'story')->andWhere('type')->eq($data->type)
             ->fetchPairs();
 
         if($duplicate and is_string($titles)) return array('stop' => true, 'duplicate' => key($duplicate));
