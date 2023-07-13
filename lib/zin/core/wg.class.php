9fafdd88d10d4f320d277c44cfbcc6ebc9b84162
sunhao <sunhao@easycorp.ltd>
2023-07-13 11:39:30 +0800

    * zin: fix event binding on widget without id not work.

--- lib/zin/core/wg.class.php
+++ lib/zin/core/wg.class.php
@@ -263,6 +263,7 @@ class wg
         if($prop === 'id' && $value === '$GID') $value = $this->gid;
         if($prop[0] === '@')
         {
+            $this->setDefaultProps(array('id' => $this->gid));
             context::current()->addWgWithEvents($this);
         }
         $this->props->set($prop, $value);
