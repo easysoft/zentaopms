69aec0edb7275c0b1236cf247254ef886697ef64
zhouxin <zhouxin@easycorp.ltd>
2023-07-14 16:04:44 +0800

    * Add metrics.

--- module/metric/calc/product/qc/rate_of_approved_story_in_product.php
+++ module/metric/calc/product/qc/rate_of_approved_story_in_product.php
@@ -1,43 +1,66 @@
 <?php
 /**
- * 按产品统计的研发需求评审通过率。
+ * 按产品统计的已立项研发需求的用例覆盖率
  * .
  *
  * 范围：product
  * 对象：story
- * 目的：qc
- * 度量名称：按产品统计的研发需求评审通过率
- * 单位：%
- * 描述：按产品统计的所有研发需求评审通过率=（按产品统计的不需要评审的研发需求数+评审结果确认通过的研发需求数）/（按产品统计的不需要评审的研发需求数+有评审结果的研发需求数）
+ * 目的：scale
+ * 度量名称：按产品统计的已立项研发需求的用例覆盖率
+ * 单位：个
+ * 描述：公式：
+按产品统计的已立项研发需求的用例覆盖率=按产品统计的有用例的已立项研发需求数/按产品统计的已立项的研发需求数
 过滤已删除的研发需求
 过滤已删除的产品
  * 度量库：
  * 收集方式：realtime
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
- * @author    qixinzhi <qixinzhi@easycorp.ltd>
+ * @author    zhouxin <zhouxin@easycorp.ltd>
  * @package
  * @uses      func
  * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
  * @Link      https://www.zentao.net
  */
-class rate_of_approved_story_in_product extends baseMetric
+class testcase_coverage_of_story_in_product extends baseCalc
 {
-    public $dataset = '';
-
-    public $fieldList = array();
-
     public $result = array();
 
-    //public function getStatement()
-    //{
-    //}
+    public $idList = array();
 
-    //public function calculate($data)
-    //{
-    //}
+    public function getStatement()
+    {
+       return $this->dao->select('t1.id,t1.product,t3.id as caseid')->from(TABLE_STORY)->alias('t1')
+          ->leftJoin(TABLE_PRODUCT)->alias('t2')->on('t1.product=t2.id')
+          ->leftJoin(TABLE_CASE)->alias('t3')->on('t1.id=t3.story')
+          ->where('t1.stage')->eq('projected')
+          ->andWhere('t1.deleted')->eq(0)
+          ->andWhere('t2.deleted')->eq(0)
+          ->query();
+    }
 
-    //public function getResult()
-    //{
-    //}
+    public function calculate($row)
+    {
+       if($row->stage != 'projected') return;
+       if(in_array($row->id, $this->idList)) return;
+
+       if(!isset($this->result[$row->product])) $this->result[$row->product] = array('total' => 0, 'haveCase' => 0);
+
+       $this->result[$row->product]['total'] ++;
+       if($row->caseid !== null) $this->result[$row->product]['haveCase'] ++;
+
+       $this->idList[] = $row->id;
+    }
+
+    public function getResult($options = array())
+    {
+       $records = array();
+       foreach($this->result as $productID => $result)
+       {
+           $records[] = array(
+               'product' => $productID,
+               'rate'    => $result['total'] ? round($result['haveCase'] / $result['total'], 2) : 0,
+           );
+       }
+    }
 }
--- module/metric/calc/product/rate/rate_of_finish_story_in_product.php
+++ module/metric/calc/product/rate/rate_of_finish_story_in_product.php
@@ -18,29 +18,41 @@
  * 收集方式：realtime
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
- * @author    qixinzhi <qixinzhi@easycorp.ltd>
+ * @author    zhouxin <zhouxin@easycorp.ltd>
  * @package
  * @uses      func
  * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
  * @Link      https://www.zentao.net
  */
-class rate_of_finish_story_in_product extends baseMetric
+class rate_of_finish_story_in_product extends baseCale
 {
-    public $dataset = '';
+    public $dataset = 'getDevStories';
 
-    public $fieldList = array();
+    public $fieldList = array('t1.product', 't1.id', 't1.status', 't1.closedReason');
 
     public $result = array();
 
-    //public function getStatement()
-    //{
-    //}
+    public function calculate($row)
+    {
+        $result = array();
+        if(!isset($result[$row->product])) $result[$row->product] = array('finished' => 0, 'total' => 0, 'invalid' => 0);
 
-    //public function calculate($data)
-    //{
-    //}
+        $result[$row->product]['total'] ++;
+        if($row->status == 'done') $result[$row->product]['finished'] ++;
+        if($row->status == 'closed' and in_array($row->closedReason, array('duplicate', 'willnotdo', 'bydesign', 'cancel'))) $result[$row->product]['invalid'] ++;
 
-    //public function getResult()
-    //{
-    //}
+        $this->result = $result;
+    }
+
+    public function getResult()
+    {
+        $records = array();
+        foreach($this->result as $productID => $storyInfo)
+        {
+            $records[] = array(
+                'product' => $productID,
+                'value'   => $storyInfo['total'] ? round($storyInfo['finished'] / ($storyInfo['total'] - $storyInfo['invalid']) * 100, 2) : 0,
+            );
+        }
+    }
 }
