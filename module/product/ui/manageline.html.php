99910048100e126b9ce691040d42369108f9e37b
wangyidong <wangyidong@cnezsoft.com>
2023-07-15 11:17:14 +0800

    * Code for task #97077.

--- module/product/ui/manageline.html.php
+++ module/product/ui/manageline.html.php
@@ -12,6 +12,34 @@ namespace zin;
 
 $lineMenuList = null;
 $formRowList  = null;
+$formRowList[] = formRow
+(
+    cell
+    (
+        setClass('flex flex-1'),
+        formGroup
+        (
+            set::width($config->systemMode == 'ALM' ? '1/2' : 'full'),
+            set::label($lang->product->lineName),
+            set::labelClass('font-bold'),
+        ),
+        $config->systemMode == 'ALM' ? formGroup
+        (
+            set::width('1/2'),
+            set::class('ml-4'),
+            set::label($lang->product->program),
+            set::labelClass('font-bold required'),
+        ) : null,
+    ),
+    cell
+    (
+        set::width('100px'),
+        formGroup
+        (
+            span(setClass('form-label font-bold'), set::style(array('justify-content' =>'flex-start')), $lang->actions),
+        ),
+    )
+);
 foreach($lines as $line)
 {
     $lineMenuList[] = div
@@ -30,20 +58,33 @@ foreach($lines as $line)
 
     $formRowList[] = formRow
     (
-        formGroup
+        cell
         (
-            set::width('1/3'),
-            set::name("modules[id$line->id]"),
-            set::value($line->name),
+            setClass('flex flex-1'),
+            formGroup
+            (
+                set::width($config->systemMode == 'ALM' ? '1/2' : 'full'),
+                set::name("modules[id$line->id]"),
+                set::value($line->name),
+            ),
+            $config->systemMode == 'ALM' ? formGroup
+            (
+                setID("programs_id{$line->id}"),
+                set::width('1/2'),
+                set::class('ml-4'),
+                set::name("programs[id$line->id]"),
+                set::items($programs),
+                set::value($line->root),
+            ) : null,
         ),
-        $config->systemMode == 'ALM' ? formGroup
+        cell
         (
-            set::width('1/3'),
-            set::class('ml-4 required'),
-            set::name("programs[id$line->id]"),
-            set::items($programs),
-            set::value($line->root),
-        ) : null,
+            set::width('100px'),
+            formGroup
+            (
+                setClass('ml-2 pl-2 flex self-center'),
+            )
+        )
     );
 }
 
@@ -52,41 +93,49 @@ for($i = 0; $i <= 5; $i ++)
     $formRowList[] = formRow
     (
         set::class('line-row-add'),
-        formGroup(set::width('1/3'), set::name("modules[$i]")),
-        $config->systemMode == 'ALM' ? formGroup
+        cell
         (
-            set::width('1/3'),
-            set::class('ml-4 required'),
-            set::name("programs[$i]"),
-            set::items($programs),
-        ) : null,
-        formGroup
+            setClass('flex flex-1'),
+            formGroup
+            (
+                set::width($config->systemMode == 'ALM' ? '1/2' : 'full'),
+                set::name("modules[$i]"),
+            ),
+            $config->systemMode == 'ALM' ? formGroup
+            (
+                set::width('1/2'),
+                setID("programs_{$i}"),
+                set::class('ml-4'),
+                set::name("programs[$i]"),
+                set::items($programs),
+            ) : null,
+        ),
+        cell
         (
-            setClass('ml-2 pl-2 flex self-center'),
-            btn
+            set::width('100px'),
+            formGroup
             (
-                setClass('btn btn-link text-gray addLine'),
-                icon('plus'),
-                on::click('addNewLine')
-            ),
-            btn
-            (
-                setClass('btn btn-link text-gray removeLine'),
-                icon('trash'),
-                on::click('removeLine'),
-            ),
+                setClass('ml-2 pl-2 flex self-center'),
+                btn
+                (
+                    setClass('btn btn-link text-gray addLine'),
+                    icon('plus'),
+                    on::click('addNewLine')
+                ),
+                btn
+                (
+                    setClass('btn btn-link text-gray removeLine'),
+                    icon('trash'),
+                    on::click('removeLine'),
+                ),
+            )
         )
     );
 }
 
 jsVar('+index', $i);
 
-to::header
-(
-    span(set::class('article-h2 w-1/4'), $lang->product->line),
-    span(set::class('article-h2'), $lang->product->manageLine),
-);
-
+modalHeader(set::title($lang->product->manageLine), set::titleClass('text-lg font-bold'));
 div
 (
     set::class('flex'),
@@ -94,7 +143,13 @@ div
     (
         set::width('1/3'),
         set::class('lineTree mr-1'),
-        $lineMenuList
+        h2(setClass('article-h2'), $lang->product->line),
+        div
+        (
+            setClass('mt-4 mr-4 pl-5 pt-2 pt-2 pb-2'),
+            set::style(array('background' => 'var(--color-gray-100)')),
+            $lineMenuList,
+        )
     ),
     cell
     (
