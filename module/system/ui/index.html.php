9e6fdf4f02e8239d47dd7d94e86d127e21f337b8
zenggang <zenggang@easycorp.ltd>
2023-07-17 07:03:41 +0000

    * Devops menu

---
+++ module/system/ui/index.html.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+/**
+ * The index view file of system module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Zeng Gang<zenggang@easycorp.ltd>
+ * @package     system
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+
+
+render();
