3dd2da13ca14901c24ab50e875efda724c7dbcd1
chentao <chentao@easycorp.ltd>
2023-07-17 15:12:53 +0800

    * Remove redundant spaces from form configuration.

--- module/programplan/config/form.php
+++ module/programplan/config/form.php
@@ -8,11 +8,11 @@ foreach($config->programplan->create->formFields as $field) $config->programplan
 
 if(!isset($config->programplan->edit)) $config->programplan->edit = new stdClass();
 $config->programplan->edit->form = array();
-$config->programplan->edit->form['begin']     = array('required' => false, 'type' => 'string',     'default' => '0000-00-00');
-$config->programplan->edit->form['end']       = array('required' => false, 'type' => 'string',     'default' => '0000-00-00');
-$config->programplan->edit->form['realBegan'] = array('required' => false, 'type' => 'string',     'default' => '0000-00-00');
-$config->programplan->edit->form['realEnd']   = array('required' => false, 'type' => 'string',     'default' => '0000-00-00');
-$config->programplan->edit->form['output']    = array('required' => false, 'type' => 'array',      'default' => array());
+$config->programplan->edit->form['begin']     = array('required' => false, 'type' => 'string', 'default' => '0000-00-00');
+$config->programplan->edit->form['end']       = array('required' => false, 'type' => 'string', 'default' => '0000-00-00');
+$config->programplan->edit->form['realBegan'] = array('required' => false, 'type' => 'string', 'default' => '0000-00-00');
+$config->programplan->edit->form['realEnd']   = array('required' => false, 'type' => 'string', 'default' => '0000-00-00');
+$config->programplan->edit->form['output']    = array('required' => false, 'type' => 'array',  'default' => array());
 
 $config->programplan->ajaxCustom = new stdClass();
 $config->programplan->ajaxCustom->form = array();
