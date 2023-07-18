57d68cf6e0d07e539b71ace3fb45f190abfebd86
caoyanyi <caoyanyi@easycorp.ltd>
2023-07-18 14:37:57 +0800

    * Adjust dynamic list style.

--- lib/zin/wg/dynamic/v1.php
+++ lib/zin/wg/dynamic/v1.php
@@ -15,34 +15,65 @@ class dynamic extends wg
         return file_get_contents(__DIR__ . DS . 'css' . DS . 'v1.css');
     }
 
+    public function getStatusClass(object $dynamic): string
+    {
+        $action     = strtolower($dynamic->action);
+        $objectType = strtolower($dynamic->objectType);
+
+        // if($dynamic->major) return 'active';
+
+        if($objectType == 'release' && $action == 'opened') return 'trophy';
+        if($objectType == 'project' && $action == 'closed') return 'trophy';
+
+        if(strpos($action, 'open')   !== false || strpos($action, 'activate') !== false) return 'active';
+        if(strpos($action, 'finish') !== false || strpos($action, 'resolve')  !== false) return 'green';
+        if(strpos($action, 'assign') !== false || strpos($action, 'collect')  !== false) return 'yellow';
+
+        return '';
+    }
+
     protected function dynamicItem(object $dynamic, array $users): wg
     {
         $dynamicLabel = zget($dynamic, 'dynamicLabel', '');
         if(empty($dynamicLabel)) $dynamicLabel = zget($dynamic, 'actionLabel', '');
+
+        $dynamicClass = $this->getStatusClass($dynamic);
         return li
         (
-            setClass($dynamic->major ? 'active': ''),
+            setClass($dynamicClass),
             div
             (
-                span(
-                    setClass('dynamic-tag'),
+                span
+                (
+                    setClass('dynamic-tag p-1'),
                     $dynamic->date
                 ),
-                div(
-                    setClass('dynamic-text clip'),
-                    zget($users, $dynamic->actor),
-                    span
+                div
+                (
+                    setClass('dynamic-text flex flex-nowrap justify-between items-center'),
+                    div
                     (
-                        setClass('text-gray px-1'),
-                        $dynamicLabel
+                        setClass('clip p-1'),
+                        zget($users, $dynamic->actor),
+                        span
+                        (
+                            setClass('text-gray px-1'),
+                            $dynamicLabel
+                        ),
+                        span($dynamic->objectLabel, setClass('pr-1')),
+                        a
+                        (
+                            set::href($dynamic->objectLink),
+                            set::title($dynamic->objectName),
+                            $dynamic->objectName
+                        )
                     ),
-                    span($dynamic->objectLabel, setClass('pr-1')),
-                    a
+                    $dynamicClass == 'trophy' ? h::img
                     (
-                        set::href($dynamic->objectLink),
-                        set::title($dynamic->objectName),
-                        $dynamic->objectName
-                    )
+                        set::src('static/svg/trophy.svg'),
+                        setClass('ml-2'),
+                        set::width(30)
+                    ) : null
                 )
             )
         );
