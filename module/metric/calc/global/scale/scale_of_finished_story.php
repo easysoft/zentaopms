4a804d9697d8b9d9ec6732398709f8a744e435a1
zhouxin <zhouxin@easycorp.ltd>
2023-07-18 16:52:58 +0800

    * measurement: finish task #97980. add scale_of_finished_story.

--- module/metric/calc/global/scale/scale_of_finished_story.php
+++ module/metric/calc/global/scale/scale_of_finished_story.php
@@ -9,9 +9,7 @@
  * 度量名称：按全局统计的已完成研发需求规模数
  * 单位：个
  * 描述：按全局统计的已完成研发需求规模数表示已经完成的研发需求的规模总数。该度量项反映了组织已经完成的研发需求的规模总数，可以用于评估组织的研发需求规模管理和成果。
- * 定义：所有的研发需求规模数求和
-关闭原因为已完成
-过滤已删除的研发需求
+ * 定义：所有的研发需求规模数求和 关闭原因为已完成 过滤已删除的研发需求
  * 度量库：
  * 收集方式：realtime
  *
@@ -24,16 +22,17 @@
  */
 class scale_of_finished_story extends baseCalc
 {
-    public $dataset = null;
+    public $dataset = 'getAllDevStories';
 
-    public $fieldList = array();
+    public $fieldList = array('t1.estimate', 't1.closedReason');
 
-    //public funtion getStatement($dao)
-    //{
-    //}
+    public $result = 0;
 
-    public function calculate($data)
+    public function calculate($row)
     {
+        if(empty($row->estimate)) return null;
+
+        if($row->closedReason == 'done') $this->result += $row->estimate;
     }
 
     public function getResult($options = array())
---
+++ module/metric/test/calc/global/scale/scale_of_finished_story.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php
+<?php
+include dirname(__FILE__, 7) . '/test/lib/init.php';
+include dirname(__FILE__, 4) . '/calc.class.php';
+
+zdTable('story')->config('story', $useCommon = true, $levels = 4)->gen(100);
+
+$metric = new metricTest();
+$calc   = $metric->calcMetric(__FILE__);
+
+/**
+
+title=scale_of_finished_story
+timeout=0
+cid=1
+
+*/
+
+r($calc->getResult()) && p('') && e('10'); // 测试按全局统计的已完成研发需求规模数
