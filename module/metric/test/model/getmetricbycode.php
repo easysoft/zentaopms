c923f78588e174ae93bc44ac32d8194b7dc71197
qixinzhi <qixinzhi@easycorp.ltd>
2023-07-18 08:33:26 +0000

    * Fix getmetricbycode unit test.

--- module/metric/test/model/getmetricbycode.php
+++ module/metric/test/model/getmetricbycode.php
@@ -15,8 +15,8 @@ pid=1
 
 */
 
-$options = array('product' => '50');
-r($metric->getMetricByCode('count_of_plan_in_product', $options)) && p('0:value') && e('2');  // 测试按产品统计的计划数
+$options = array('product' => '7');
+r($metric->getMetricByCode('count_of_plan_in_product', $options)) && p('0:value') && e('50');  // 测试按产品统计的计划数
 
-$options = array('product' => '50', 'year' => '2020');
+$options = array('product' => '9', 'year' => '2014');
 r($metric->getMetricByCode('count_of_annual_closed_feedback_in_product', $options)) && p('0:value') && e('0');  // 测试按产品统计年度关闭反馈数
