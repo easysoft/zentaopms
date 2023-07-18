5bacdd134b07757ac0b4f0f46e21b6ed881029cf
liwenrui <liwenrui@easycorp.ltd>
2023-07-18 10:17:02 +0800

    * add confirm to unpublishing.

--- module/ai/view/promptview.html.php
+++ module/ai/view/promptview.html.php
@@ -35,7 +35,7 @@
         <?php if($prompt->status == 'draft') echo html::a(helper::createLink('ai', 'promptassignrole', "prompt=$prompt->id"), '<i class="icon icon-design icon-sm"></i> ' . $lang->ai->prompts->action->design, '', "class='btn'");?>
         <?php if($prompt->status == 'draft' && $this->ai->isExecutable($prompt)) echo html::a($this->ai->getTestingLocation($prompt), '<i class="icon icon-bug icon-sm"></i> ' . $lang->ai->prompts->action->test, '', "class='btn'");?>
         <?php if($prompt->status == 'draft' && $this->ai->isExecutable($prompt)) echo html::a(helper::createLink('ai', 'promptpublish', "id={$prompt->id}"), '<i class="icon icon-publish icon-sm"></i> ' . $lang->ai->prompts->action->publish, '', "class='btn'");?>
-        <?php if($prompt->status == 'active') echo html::a(helper::createLink('ai', 'promptunpublish', "id={$prompt->id}"), '<i class="icon icon-ban icon-sm"></i> ' . $lang->ai->prompts->action->unpublish, '', "class='btn'");?>
+        <?php if($prompt->status == 'active') echo html::a(helper::createLink('ai', 'promptunpublish', "id={$prompt->id}"), '<i class="icon icon-ban icon-sm"></i> ' . $lang->ai->prompts->action->unpublish, '', "class='btn' id='unpublish-btn'");?>
         <div class='divider'></div>
         <?php echo html::a(helper::createLink('ai', 'promptedit', "prompt=$prompt->id"), '<i class="icon icon-edit icon-sm"></i>', '', "class='btn' title='{$lang->ai->prompts->action->edit}'");?>
         <?php echo html::a(helper::createLink('ai', 'promptdelete'), '<i class="icon icon-trash icon-sm"></i>', '', "class='btn deleter' title='{$lang->ai->prompts->action->delete}'");?>
@@ -50,10 +50,22 @@
 <script>
   $(function()
   {
-    /* TODO: use new modal for this deleter. */
-    $('.deleter').on('click', function()
+    /* TODO: use new modal for these. */
+    $('.deleter').click(function()
     {
       if(confirm('<?php echo $lang->ai->prompts->action->deleteConfirm;?>'))
+      {
+        $.getJSON($(this).attr('href'), function(resp)
+        {
+          if(resp.result != 'success') $.zui.messager.danger(resp.message);
+          return location.reload();
+        });
+      }
+      return false;
+    });
+    $('#unpublish-btn').click(function()
+    {
+      if(confirm('<?php echo $lang->ai->prompts->action->draftConfirm;?>'))
       {
         $.getJSON($(this).attr('href'), function(resp)
         {
