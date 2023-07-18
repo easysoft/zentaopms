189f82eb11138dc191d2408a52e42b6774e3d6b3
zhouxin <zhouxin@easycorp.ltd>
2023-07-18 14:12:00 +0800

    * Add metric: rate_of_delivery_story_in_product.

--- module/metric/calc/product/rate/rate_of_delivery_story_in_product.php
+++ module/metric/calc/product/rate/rate_of_delivery_story_in_product.php
@@ -9,38 +9,47 @@
  * 度量名称：按产品统计的研发需求交付率
  * 单位：%
  * 描述：复用：
-按产品统计的已交付研发需求数
-按产品统计的无效研发需求数
-按产品统计的研发需求总数
-公式：
-按产品统计的研发需求完成率=按产品统计的已交付研发需求数/（按产品统计的研发需求总数-按产品统计的无效研发需求数）*100%
+ *       按产品统计的已交付研发需求数
+ *       按产品统计的无效研发需求数
+ *       按产品统计的研发需求总数
+ *       公式：
+ *       按产品统计的研发需求完成率=按产品统计的已交付研发需求数/（按产品统计的研发需求总数-按产品统计的无效研发需求数）*100%
  * 度量库：
  * 收集方式：realtime
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
- * @author    qixinzhi <qixinzhi@easycorp.ltd>
+ * @author    zhouxin <zhouxin@easycorp.ltd>
  * @package
  * @uses      func
  * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
  * @Link      https://www.zentao.net
  */
-class rate_of_delivery_story_in_product extends baseMetric
+class rate_of_delivery_story_in_product extends baseCalc
 {
-    public $dataset = '';
+    public $dataset = 'getDevStories';
 
-    public $fieldList = array();
+    public $fieldList = array('t1.product', 't1.closedReason', 't1.stage');
 
-    public $result = array();
+    public function calculate($row)
+    {
+        if(!isset($this->result[$row->product])) $this->result[$row->product] = array('delivered' => 0, 'valid' => 0);
 
-    //public function getStatement()
-    //{
-    //}
+        if($row->stage == 'released' || $row->closedReason == 'done')                            $this->result[$row->product]['delivered'] ++;
+        if(!in_array($row->closedReason, array('duplicate', 'willnotdo', 'bydesign', 'cancel'))) $this->result[$row->product]['valid'] ++;
+    }
 
-    //public function calculate($data)
-    //{
-    //}
+    public function getResult($options = array())
+    {
+        $records = array();
+        foreach($this->result as $productID => $storyInfo)
+        {
+            $records[] = array(
+                'product' => $productID,
+                'value'   => $storyInfo['valid'] ? round($storyInfo['delivered'] / $storyInfo['valid'], 4) : 0,
+            );
+        }
 
-    //public function getResult()
-    //{
-    //}
+        return $this->filterByOptions($records, $options);
+
+    }
 }
