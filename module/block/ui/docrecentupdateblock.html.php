2998d882e11e884c829b458f61b700b79a5c7d09
daitingting <daitingting@xirangit.com>
2023-07-19 05:17:32 +0000

    * Refactor the recent update doc block, finish task #98156.

--- module/block/ui/docrecentupdateblock.html.php
+++ module/block/ui/docrecentupdateblock.html.php
@@ -10,57 +10,60 @@ declare(strict_types=1);
 */
 
 namespace zin;
-?>
-<?php $canView = common::hasPriv('doc', 'view');?>
-<div>
-  <?php if(empty($docList)):?>
-    <div class='table-empty-tip'><p><span class='text-muted'><?php echo $lang->doc->noDoc;?></p></span></div>
-  <?php else:?>
-  <div class="doc-list">
-    <?php foreach($docList as $doc):?>
-    <div class="doc-box">
-      <a href='<?php echo $canView ? $this->createLink("doc", "view", "docID=$doc->id") : 'javascript:;';?>'>
-        <div class="p-2 border">
-            <span class='date-interval text-muted text-gray'>
-              <?php
-              $interval = $doc->editInterval;
-              $editTip  = $lang->doc->todayUpdated;
-              if($interval->year)
-              {
-                $editTip = sprintf($lang->doc->yearsUpdated, $interval->year);
-              }
-              elseif($interval->month)
-              {
-                $editTip = sprintf($lang->doc->monthsUpdated, $interval->month);
-              }
-              elseif($interval->day)
-              {
-                $editTip = sprintf($lang->doc->daysUpdated, $interval->day);
-              }
-              echo $editTip;
-              ?>
-            </span>
-            <h4 class="plug-title my-2 text-black font-bold" title="<?php echo $doc->title;?>">
-              <?php
-              $docType = $doc->type == 'text' ? 'wiki-file' : $doc->type;
-              echo html::image("static/svg/{$docType}.svg", "class='file-icon inline'");
-              ?>
-              <?php echo $doc->title;?>
-            </h4>
-            <p class='edit-date text-muted text-gray mb-2'><?php echo $lang->doc->editedDate . (common::checkNotCN() ? ': ' : '：') . $doc->editedDate;?></p>
-        </div>
-      </a>
-    </div>
-    <?php endforeach;?>
-  </div>
-  <?php endif;?>
-</div>
-<?php
-panel
+
+$canView = common::hasPriv('doc', 'view');
+
+$docItems = array();
+
+foreach($docList as $doc)
+{
+    $editTip  = $lang->doc->todayUpdated;
+    $interval = $doc->editInterval;
+    if($interval->year)
+    {
+      $editTip = sprintf($lang->doc->yearsUpdated, $interval->year);
+    }
+    elseif($interval->month)
+    {
+      $editTip = sprintf($lang->doc->monthsUpdated, $interval->month);
+    }
+    elseif($interval->day)
+    {
+      $editTip = sprintf($lang->doc->daysUpdated, $interval->day);
+    }
+
+    $docType = $doc->type == 'text' ? 'wiki-file' : $doc->type;
+
+    $docItems[] = cell
+    (
+        setClass('doc-box border rounded-lg shadow p-2 relative'),
+        set::width('32%'),
+        span
+        (
+            setClass('text-gray absolute right-2 my-2'),
+            $editTip,
+        ),
+        div
+        (
+            setClass('article-h3 pr-1 my-2 mr-2'),
+            img
+            (
+                setClass('inline pr-1'),
+                set('src', "static/svg/{$docType}.svg")
+            ),
+            $doc->title
+        ),
+        p
+        (
+            setClass('edit-date text-gray'),
+            $lang->doc->editedDate . (common::checkNotCN() ? ': ' : '：') . $doc->editedDate
+        )
+    );
+}
+
+blockPanel
 (
-    set('class', 'docrecentupdate-block ' . ($longBlock ? 'block-long' : 'block-sm')),
-    set::title($block->title),
-    rawContent()
+    empty($docList) ? $lang->doc->noDoc : div(setClass('doc-list flex flex-wrap content-between gap-3'), $docItems)
 );
 
 render();
