0c2585c79b5e86184c40ea4eec2b6a6635600126
qixinzhi <qixinzhi@easycorp.ltd>
2023-07-19 05:24:45 +0000

    * Finish task#97963.

--- module/metric/calc/global/scale/count_of_finished_story.php
+++ module/metric/calc/global/scale/count_of_finished_story.php
@@ -10,8 +10,8 @@
  * 单位：个
  * 描述：按全局统计的已完成研发需求数表示已经完成的研发需求的数量。该度量项反映了组织中已经完成的研发需求的数量，可以用于评估组织的研发需求执行能力和绩效。
  * 定义：所有的研发需求个数求和
-关闭原因为已完成
-过滤已删除的研发需求
+ *       关闭原因为已完成
+ *       过滤已删除的研发需求
  * 度量库：
  * 收集方式：realtime
  *
@@ -24,20 +24,20 @@
  */
 class count_of_finished_story extends baseCalc
 {
-    public $dataset = null;
+    public $dataset = 'getDevStories';
 
-    public $fieldList = array();
+    public $fieldList = array('t1.stage', 't1.closedReason');
 
-    //public funtion getStatement($dao)
-    //{
-    //}
+    public $result = 0;
 
-    public function calculate($data)
+    public function calculate($row)
     {
+        if($row->stage == 'closed' and $row->closedReason == 'done') $this->result ++;
     }
 
     public function getResult($options = array())
     {
-        return $this->filterByOptions($this->result, $options);
+        $records = array(array('value' => $this->result));
+        return $this->filterByOptions($records, $options);
     }
 }
