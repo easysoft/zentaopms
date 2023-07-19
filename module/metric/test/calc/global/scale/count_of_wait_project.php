577ba79a0ea0dd8bf80272345d6035c3d91b0c20
zhouxin <zhouxin@easycorp.ltd>
2023-07-19 13:28:00 +0800

    * measurement: and unit test for metric.

---
+++ module/metric/test/calc/global/scale/count_of_wait_project.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php
+<?php
+include dirname(__FILE__, 7) . '/test/lib/init.php';
+include dirname(__FILE__, 4) . '/calc.class.php';
+
+zdTable('project')->config('project_status', $useCommon = true, $levels = 4)->gen(200);
+
+$metric = new metricTest();
+$calc   = $metric->calcMetric(__FILE__);
+
+/**
+
+title=count_of_wait_project
+timeout=0
+cid=1
+
+*/
+
+r($calc->getResult()) && p('') && e('100');
