0f28c574906560762a14a94e9c8d1ce6a3d05eeb
qixinzhi <qixinzhi@easycorp.ltd>
2023-07-19 03:06:31 +0000

    * Fix comment of unit test.

--- module/metric/test/calc/global/scale/count_of_invalid_story.php
+++ module/metric/test/calc/global/scale/count_of_invalid_story.php
@@ -13,14 +13,15 @@ cid=1
 
 */
 
+zdTable('product')->config('product', $useCommon = true, $levels = 4)->gen(10);
 zdTable('story')->config('story_status_closedreason', $useCommon = true, $levels = 4)->gen(500);
 $calc = $metric->calcMetric(__FILE__);
-r($calc->getResult()) && p('0:value') && e('146'); // 测试500条数据全局有效研发需求数。
+r($calc->getResult()) && p('0:value') && e('80'); // 测试500条数据全局无效研发需求数。
 
 zdTable('story')->config('story_status_closedreason', $useCommon = true, $levels = 4)->gen(839);
 $calc = $metric->calcMetric(__FILE__);
-r($calc->getResult()) && p('0:value') && e('240'); // 测试839条数据全局有效研发需求数。
+r($calc->getResult()) && p('0:value') && e('120'); // 测试839条数据全局无效研发需求数。
 
 zdTable('story')->config('story_status_closedreason', $useCommon = true, $levels = 4)->gen(1252);
 $calc = $metric->calcMetric(__FILE__);
-r($calc->getResult()) && p('0:value') && e('360'); // 测试1252条数据全局有效研发需求数。
+r($calc->getResult()) && p('0:value') && e('180'); // 测试1252条数据全局无效研发需求数。
