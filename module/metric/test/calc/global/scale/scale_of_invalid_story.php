2f4ee97e6d9dcc4182fd2ce460f3f5083ade8924
zhouxin <zhouxin@easycorp.ltd>
2023-07-19 09:25:41 +0800

    * measurement: add unit test for metric.

---
+++ module/metric/test/calc/global/scale/scale_of_invalid_story.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php
+include dirname(__FILE__, 7) . '/test/lib/init.php';
+include dirname(__FILE__, 4) . '/calc.class.php';
+
+zdTable('story')->config('story', $useCommon = true, $levels = 4)->gen(100);
+
+$metric = new metricTest();
+$calc   = $metric->calcMetric(__FILE__);
+
+/**
+
+title=scale_of_invalid_story
+timeout=0
+cid=1
+
+*/
+
+r($calc->getResult()) && p('') && e('21'); // 测试按全局统计的无效研发需求规模数
+
