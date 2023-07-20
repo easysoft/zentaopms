afafac92ef4063df74397571f9287cb0b50fa9dd
liumengyi <liumengyi@easycorp.ltd>
2023-07-20 13:20:57 +0800

    * Modify fields order.

--- module/block/config/todoblock.php
+++ module/block/config/todoblock.php
@@ -13,12 +13,11 @@ $config->block->todo->dtable->fieldList['id']['type']  = 'id';
 
 $config->block->todo->dtable->fieldList['name']        = $config->my->todo->dtable->fieldList['name'];
 $config->block->todo->dtable->fieldList['pri']         = $config->my->todo->dtable->fieldList['pri'];
+$config->block->todo->dtable->fieldList['status']      = $config->my->todo->dtable->fieldList['status'];
+$config->block->todo->dtable->fieldList['type']        = $config->my->todo->dtable->fieldList['type'];
 $config->block->todo->dtable->fieldList['date']        = $config->my->todo->dtable->fieldList['date'];
 $config->block->todo->dtable->fieldList['begin']       = $config->my->todo->dtable->fieldList['begin'];
 $config->block->todo->dtable->fieldList['end']         = $config->my->todo->dtable->fieldList['end'];
-$config->block->todo->dtable->fieldList['status']      = $config->my->todo->dtable->fieldList['status'];
-$config->block->todo->dtable->fieldList['type']        = $config->my->todo->dtable->fieldList['type'];
-$config->block->todo->dtable->fieldList['assignedBy']  = $config->my->todo->dtable->fieldList['assignedBy'];
 
 $config->block->todo->dtable->short = new stdclass();
 $config->block->todo->dtable->short->fieldList['id']     = $config->block->todo->dtable->fieldList['id'];
