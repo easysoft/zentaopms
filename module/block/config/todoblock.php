e3ac5adbda9bcdb1338c56ddaa27a48f04cfa2b7
liumengyi <liumengyi@easycorp.ltd>
2023-07-20 14:28:17 +0800

    * Modify todo block config.

--- module/block/config/todoblock.php
+++ module/block/config/todoblock.php
@@ -10,14 +10,26 @@ $config->block->todo->dtable->fieldList = array();
 $config->block->todo->dtable->fieldList['id']['name']  = 'id';
 $config->block->todo->dtable->fieldList['id']['title'] = $lang->idAB;
 $config->block->todo->dtable->fieldList['id']['type']  = 'id';
+$config->block->todo->dtable->fieldList['id']['fixed'] = 'left';
 
-$config->block->todo->dtable->fieldList['name']        = $config->my->todo->dtable->fieldList['name'];
-$config->block->todo->dtable->fieldList['pri']         = $config->my->todo->dtable->fieldList['pri'];
-$config->block->todo->dtable->fieldList['status']      = $config->my->todo->dtable->fieldList['status'];
-$config->block->todo->dtable->fieldList['type']        = $config->my->todo->dtable->fieldList['type'];
-$config->block->todo->dtable->fieldList['date']        = $config->my->todo->dtable->fieldList['date'];
-$config->block->todo->dtable->fieldList['begin']       = $config->my->todo->dtable->fieldList['begin'];
-$config->block->todo->dtable->fieldList['end']         = $config->my->todo->dtable->fieldList['end'];
+$config->block->todo->dtable->fieldList['name']          = $config->my->todo->dtable->fieldList['name'];
+$config->block->todo->dtable->fieldList['name']['fixed'] = 'left';
+
+$config->block->todo->dtable->fieldList['pri']          = $config->my->todo->dtable->fieldList['pri'];
+$config->block->todo->dtable->fieldList['pri']['width'] = '60px';
+
+$config->block->todo->dtable->fieldList['status'] = $config->my->todo->dtable->fieldList['status'];
+$config->block->todo->dtable->fieldList['type']   = $config->my->todo->dtable->fieldList['type'];
+$config->block->todo->dtable->fieldList['date']   = $config->my->todo->dtable->fieldList['date'];
+$config->block->todo->dtable->fieldList['begin']  = $config->my->todo->dtable->fieldList['begin'];
+$config->block->todo->dtable->fieldList['end']    = $config->my->todo->dtable->fieldList['end'];
+
+unset($config->block->todo->dtable->fieldList['pri']['group']);
+unset($config->block->todo->dtable->fieldList['status']['group']);
+unset($config->block->todo->dtable->fieldList['type']['group']);
+unset($config->block->todo->dtable->fieldList['date']['group']);
+unset($config->block->todo->dtable->fieldList['begin']['group']);
+unset($config->block->todo->dtable->fieldList['end']['group']);
 
 $config->block->todo->dtable->short = new stdclass();
 $config->block->todo->dtable->short->fieldList['id']     = $config->block->todo->dtable->fieldList['id'];
