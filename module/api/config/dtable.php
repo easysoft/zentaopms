bd1820e37aec7b05d212130fbe26067224e65e83
sunguangming <sunguangming@easycorp.ltd>
2023-07-20 14:26:01 +0800

    * Finish task #97860.

---
+++ module/api/config/dtable.php
@@ -0,0 +1,50 @@
+<?php
+global $lang;
+$config->api->dtable = new stdclass();
+$config->api->dtable->struct = new stdclass();
+
+$config->api->dtable->struct->fieldList['id']['title']    = $lang->idAB;
+$config->api->dtable->struct->fieldList['id']['type']     = 'checkID';
+$config->api->dtable->struct->fieldList['id']['sortType'] = 'desc';
+$config->api->dtable->struct->fieldList['id']['name']     = 'id';
+$config->api->dtable->struct->fieldList['id']['checkbox'] = false;
+$config->api->dtable->struct->fieldList['id']['width']    = '60';
+
+$config->api->dtable->struct->fieldList['type']['title']    = $lang->api->structType;
+$config->api->dtable->struct->fieldList['type']['type']     = 'category';
+$config->api->dtable->struct->fieldList['type']['name']     = 'type';
+$config->api->dtable->struct->fieldList['type']['fixed']    = 'left';
+$config->api->dtable->struct->fieldList['type']['width']    = '60';
+$config->api->dtable->struct->fieldList['type']['sortType'] = true;
+
+$config->api->dtable->struct->fieldList['name']['title']    = $lang->api->structName;
+$config->api->dtable->struct->fieldList['name']['type']     = 'title';
+$config->api->dtable->struct->fieldList['name']['name']     = 'name';
+$config->api->dtable->struct->fieldList['name']['fixed']    = 'left';
+$config->api->dtable->struct->fieldList['name']['sortType'] = false;
+
+$config->api->dtable->struct->fieldList['addedName']['title'] = $lang->api->addedBy;
+$config->api->dtable->struct->fieldList['addedName']['type']  = 'user';
+$config->api->dtable->struct->fieldList['addedName']['name']  = 'addedName';
+
+$config->api->dtable->struct->fieldList['addedDate']['title'] = $lang->api->structAddedDate;
+$config->api->dtable->struct->fieldList['addedDate']['type']  = 'date';
+$config->api->dtable->struct->fieldList['addedDate']['name']  = 'addedDate';
+
+$config->api->dtable->struct->fieldList['actions']['title']    = $lang->actions;
+$config->api->dtable->struct->fieldList['actions']['type']     = 'actions';
+$config->api->dtable->struct->fieldList['actions']['width']    = '100';
+$config->api->dtable->struct->fieldList['actions']['sortType'] = false;
+$config->api->dtable->struct->fieldList['actions']['fixed']    = 'right';
+$config->api->dtable->struct->fieldList['actions']['name']     = 'actions';
+$config->api->dtable->struct->fieldList['actions']['menu']     = array('editStruct', 'deleteStruct');
+
+$config->api->dtable->struct->fieldList['actions']['list']['editStruct']['icon']  = 'edit';
+$config->api->dtable->struct->fieldList['actions']['list']['editStruct']['hint']  = $lang->api->editStruct;
+$config->api->dtable->struct->fieldList['actions']['list']['editStruct']['url']   = helper::createLink('api', 'editStruct', 'lib={lib}&id={id}');
+$config->api->dtable->struct->fieldList['actions']['list']['editStruct']['order'] = 5;
+
+$config->api->dtable->struct->fieldList['actions']['list']['deleteStruct']['icon']  = 'trash';
+$config->api->dtable->struct->fieldList['actions']['list']['deleteStruct']['hint']  = $lang->api->deleteStruct;
+$config->api->dtable->struct->fieldList['actions']['list']['deleteStruct']['url']   = helper::createLink('api', 'deleteStruct', 'lib={lib}&id={id}');
+$config->api->dtable->struct->fieldList['actions']['list']['deleteStruct']['order'] = 5;
---
+++ module/api/ui/struct.html.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+/**
+ * The struct view file of api module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Sun Guangming <sunguangming@easycorp.ltd>
+ * @package     my
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+
+featureBar
+(
+    li(backBtn(setClass('ghost'), set::icon('back'), $lang->goback)),
+    div(setClass('divider')),
+    li
+    (
+        set::class('nav-item'),
+        $lang->struct->list
+    )
+);
+
+toolbar
+(
+    hasPriv('api', 'createStruct') ? item(set(array
+    (
+        'icon'        => 'plus',
+        'text'        => $lang->api->createStruct,
+        'class'       => 'primary',
+        'url'         => createLink('api', 'createStruct', "libID=$libID"),
+    ))) : null,
+);
+
+$structs = initTableData($structs, $config->api->dtable->struct->fieldList, $this->api);
+$cols = array_values($config->api->dtable->struct->fieldList);
+$data = array_values($structs);
+dtable
+(
+    set::cols($cols),
+    set::data($data),
+    set::userMap($users),
+    set::footPager(usePager()),
+);
