2c68a4e556150b6ebd6b437ea911435dfb26db82
wangyuting <wangyuting@easycorp.ltd>
2023-07-21 17:13:19 +0800

    * Optimize batch actions on my story.

--- module/my/ui/story.html.php
+++ module/my/ui/story.html.php
@@ -24,14 +24,8 @@ $canBatchReview   = common::hasPriv('story', 'batchReview');
 $canBatchAssignTo = common::hasPriv('story', 'batchAssignTo');
 $canBatchClose    = common::hasPriv('story', 'batchClose');
 $canBatchAction   = $canBatchEdit || $canBatchReview || $canBatchAssignTo || $canBatchClose;
-$footToolbar = array('items' => array
-(
-   $canBatchEdit     ? array('text' => $lang->edit, 'className' => 'batch-btn', 'data-url' => helper::createLink('story', 'batchEdit', "productID=0&executionID=0&branch=0&storyType=story&from={$app->rawMethod}")) : null,
-   $canBatchReview   ? array('caret' => 'up', 'text' => $lang->story->review, 'url' => '#navReview', 'data-toggle' => 'dropdown', 'data-placement' => 'top-start') : null,
-   $canBatchAssignTo ? array('caret' => 'up', 'text' => $lang->story->assignedTo, 'url' => '#navAssignedTo', 'data-toggle' => 'dropdown', 'data-placement' => 'top-start') : null,
-   $canBatchClose    ? array('text' => $lang->story->close, 'className' => 'batch-btn', 'data-url' => helper::createLink('story', 'batchClose', "productID=0&executionID=0&storyType=story&from={$app->rawMethod}")) : null,
-), 'btnProps' => array('size' => 'sm', 'btnType' => 'secondary'));
 
+$reviewItems = array();
 if($canBatchReview)
 {
     $rejectItems = array();
@@ -41,7 +35,6 @@ if($canBatchReview)
         $rejectItems[] = array('text' => $reason, 'class' => 'batch-btn ajax-btn not-open-url', 'data-url' => helper::createLink('story', 'batchReview', "result=reject&reason={$key}&storyType=story"));
     }
 
-    $reviewItems = array();
     foreach($lang->story->reviewResultList as $key => $result)
     {
         if(!$key || $key == 'revert') continue;
@@ -54,31 +47,25 @@ if($canBatchReview)
             $reviewItems[] = array('text' => $result, 'class' => 'batch-btn ajax-btn not-open-url', 'data-url' => helper::createLink('story', 'batchReview', "result={$key}&reason=&storyType=story"));
         }
     }
-
-    menu
-    (
-        set::id('navReview'),
-        set::class('menu dropdown-menu'),
-        set::items($reviewItems)
-    );
 }
 
+$assignedToItems = array();
 if($canBatchAssignTo)
 {
-    $assignedToItems = array();
     foreach($users as $key => $value)
     {
         if(empty($key) || $key == 'closed') continue;
         $assignedToItems[] = array('text' => $value, 'class' => 'batch-btn ajax-btn not-open-url', 'data-url' => helper::createLink('story', 'batchAssignTo', "storyType=story&assignedTo={$key}"));
     }
-
-    menu
-    (
-        set::id('navAssignedTo'),
-        set::class('dropdown-menu'),
-        set::items($assignedToItems)
-    );
 }
+
+$footToolbar = array('items' => array
+(
+   $canBatchEdit     ? array('text' => $lang->edit, 'className' => 'batch-btn', 'data-url' => helper::createLink('story', 'batchEdit', "productID=0&executionID=0&branch=0&storyType=story&from={$app->rawMethod}")) : null,
+   $canBatchReview   ? array('caret' => 'up', 'text' => $lang->story->review, 'type' => 'dropdown', 'items' => $reviewItems, 'data-placement' => 'top-start') : null,
+   $canBatchAssignTo ? array('caret' => 'up', 'text' => $lang->story->assignedTo, 'type' => 'dropdown', 'items' => $assignedToItems, 'data-placement' => 'top-start') : null,
+   $canBatchClose    ? array('text' => $lang->story->close, 'className' => 'batch-btn', 'data-url' => helper::createLink('story', 'batchClose', "productID=0&executionID=0&storyType=story&from={$app->rawMethod}")) : null,
+), 'btnProps' => array('size' => 'sm', 'btnType' => 'secondary'));
 
 if($canBatchAction) $config->my->story->dtable->fieldList['id']['type'] = 'checkID';
 
