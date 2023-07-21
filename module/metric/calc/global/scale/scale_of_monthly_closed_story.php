f93effa3007650193775779cdcf213c7a156d30b
zhouxin <zhouxin@easycorp.ltd>
2023-07-21 15:04:29 +0800

    * Finish task #97988.

--- module/metric/calc/global/scale/scale_of_monthly_closed_story.php
+++ module/metric/calc/global/scale/scale_of_monthly_closed_story.php
@@ -10,13 +10,13 @@
  * 单位：个
  * 描述：按全局统计的月度关闭研发需求规模数表示某月度关闭的研发需求的规模总数。该度量项反映了组织每月关闭的研发需求的规模总数，可以用于评估组织的研发需求规模管理和调整情况。
  * 定义：所有的研发需求规模数求和
-关闭时间为某年某月
-过滤已删除的研发需求
+ *       关闭时间为某年某月
+ *       过滤已删除的研发需求
  * 度量库：
  * 收集方式：realtime
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
- * @author    qixinzhi <qixinzhi@easycorp.ltd>
+ * @author    zhouxin <zhouxin@easycorp.ltd>
  * @package
  * @uses      func
  * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
@@ -24,20 +24,35 @@
  */
 class scale_of_monthly_closed_story extends baseCalc
 {
-    public $dataset = null;
+    public $dataset = 'getDevStories';
 
-    public $fieldList = array();
+    public $fieldList = array('t1.status', 't1.closedDate', 't1.estimate');
 
-    //public funtion getStatement($dao)
-    //{
-    //}
-
-    public function calculate($data)
+    public function calculate($row)
     {
+        if(empty($row->closedDate) || $row->status != 'closed') return;
+
+        $year  = substr($row->closedDate, 0, 4);
+        $month = substr($row->closedDate, 5, 2);
+
+        if($year == '0000') return;
+
+        if(!isset($this->result[$year])) $this->result[$year] = array();
+        if(!isset($this->result[$year][$month])) $this->result[$year][$month] = 0;
+
+        $this->result[$year][$month] += $row->estimate;
     }
 
     public function getResult($options = array())
     {
-        return $this->filterByOptions($this->result, $options);
+        $records = array();
+        foreach($this->result as $year => $months)
+        {
+            foreach($months as $month => $value)
+            {
+                $records[] = array('year' => $year, 'month' => $month, 'value' => $value);
+            }
+        }
+        return $this->filterByOptions($records, $options);
     }
 }
