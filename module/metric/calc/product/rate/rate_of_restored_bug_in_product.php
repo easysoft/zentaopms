6ba09c7038234bee300f059f97178630123544ae
zhouxin <zhouxin@easycorp.ltd>
2023-07-21 13:13:08 +0800

    * measurement: finish task #98225.

--- module/metric/calc/product/rate/rate_of_restored_bug_in_product.php
+++ module/metric/calc/product/rate/rate_of_restored_bug_in_product.php
@@ -13,7 +13,7 @@
  * 收集方式：realtime
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
- * @author    qixinzhi <qixinzhi@easycorp.ltd>
+ * @author    zhouxin <zhouxin@easycorp.ltd>
  * @package
  * @uses      func
  * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
@@ -21,20 +21,28 @@
  */
 class rate_of_restored_bug_in_product extends baseCalc
 {
-    public $dataset = null;
+    public $dataset = 'getBugs';
 
-    public $fieldList = array();
+    public $fieldList = array('t1.resolution', 't1.status');
 
-    //public funtion getStatement($dao)
-    //{
-    //}
-
-    public function calculate($data)
+    public function calculate($row)
     {
+        if(!isset($this->result[$row->product])) $this->result[$row->product] = array('fixed' => 0, 'valid' => 0);
+
+        if($row->resolution == 'fixed' and $row->status == 'closed') $this->result[$row->product]['fixed'] ++;
+        if($row->resolution == 'fixed' or $row->resolution == 'postponed' or $row->status == 'active') $this->result[$row->product]['valid'] ++;
     }
 
     public function getResult($options = array())
     {
-        return $this->filterByOptions($this->result, $options);
+        $records = array();
+        foreach($this->result as $productID => $bugInfo)
+        {
+            $records[] = array(
+                'product' => $productID,
+                'value'   => $bugInfo['valid'] ? round($bugInfo['fixed'] / $bugInfo['valid'], 4) : 0,
+            );
+        }
+        return $this->filterByOptions($records, $options);
     }
 }
