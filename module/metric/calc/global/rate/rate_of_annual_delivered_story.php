d528c4ce690d905a53fdc05de5cb9ef630f2761a
zhouxin <zhouxin@easycorp.ltd>
2023-07-21 15:26:11 +0800

    * Finish task #97992.

--- module/metric/calc/global/rate/rate_of_annual_delivered_story.php
+++ module/metric/calc/global/rate/rate_of_annual_delivered_story.php
@@ -10,15 +10,15 @@
  * 单位：%
  * 描述：按全局统计的研发需求交付率表示按全局统计的年度已交付的研发需求规模数相对于按全局统计的有效研发需求数。这个度量项衡量了研发团队按时交付年度需求的能力。交付率越高，代表研发团队能够按时将年度需求交付给其他团队，实现年度目标的实现。
  * 定义：复用：
-按产品统计的年度交付研发需求数
-按产品统计的年度有效研发需求数
-公式：
-按产品统计的年度研发需求完成率=按产品统计的年度交付研发需求数/按产品统计的年度有效研发需求数*100%
+ *       按产品统计的年度交付研发需求数
+ *       按产品统计的年度有效研发需求数
+ *       公式：
+ *       按产品统计的年度研发需求完成率=按产品统计的年度交付研发需求数/按产品统计的年度有效研发需求数*100%
  * 度量库：
  * 收集方式：realtime
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
- * @author    qixinzhi <qixinzhi@easycorp.ltd>
+ * @author    zhouxin <zhouxin@easycorp.ltd>
  * @package
  * @uses      func
  * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
@@ -26,20 +26,39 @@
  */
 class rate_of_annual_delivered_story extends baseCalc
 {
-    public $dataset = null;
+    public $dataset = 'getDevStories';
 
-    public $fieldList = array();
-
-    //public funtion getStatement($dao)
-    //{
-    //}
+    public $fieldList = array('t1.stage', 't1.releasedDate', 't1.closedReason', 't1.closedDate');
 
     public function calculate($data)
     {
+        $stage        = $data->stage;
+        $closedReason = $data->closedReason;
+        $releasedDate = $data->releasedDate;
+        $closedDate   = $data->closedDate;
+
+        $date = null;
+        if($closedReason == 'done') $date = $closedDate;
+        if($stage == 'released' && !empty($closedDate)) $date = $releasedDate;
+
+        if($date === null) return;
+
+        $year = substr($date, 0, 4);
+        if($year == '0000') return;
+
+        if(!isset($this->result[$year])) $this->result[$year] = array('finished' => 0, 'valid' => 0);
+        if($stage == 'released' || $closedReason == 'done') $this->result[$year]['finished'] += 1;
+        if(!in_array($closedReason, array('duplicate', 'willnotdo', 'bydesign', 'cancel'))) $this->result[$year]['valid'] += 1;
     }
 
     public function getResult($options = array())
     {
-        return $this->filterByOptions($this->result, $options);
+        $records = array();
+        foreach($this->result as $year => $storyInfo)
+        {
+            $deliveredRate = $storyInfo['valid'] ? round($storyInfo['finished'] / $storyInfo['valid'], 4) : 0;
+            $records[] = array('year' => $year, 'value' => $deliveredRate);
+        }
+        return $this->filterByOptions($records, $options);
     }
 }
