23ad56885f6efdf52a1ad32b116298caf79e73f4
liumengyi <liumengyi@easycorp.ltd>
2023-07-24 18:34:47 +0800

    * Add prefix and suffix to formRowGroup.

--- lib/zin/wg/formrowgroup/v1.php
+++ lib/zin/wg/formrowgroup/v1.php
@@ -3,12 +3,18 @@ declare(strict_types=1);
 namespace zin;
 
 require_once dirname(__DIR__) . DS . 'formrow' . DS . 'v1.php';
+require_once dirname(__DIR__) . DS . 'icon' . DS . 'v1.php';
 
 class formRowGroup extends formRow
 {
     protected static array $defineProps = array(
         'title: array',
         'items: array',
+    );
+
+    protected static array $defineBlocks = array(
+        'prefix' => array(),
+        'suffix' => array(),
     );
 
     public static function getPageCSS(): string|false
@@ -23,8 +29,10 @@ class formRowGroup extends formRow
             setClass('form-row-group border-b border-b-1'),
             div
             (
+                $this->block('prefix'),
                 setClass('row-group-title font-black px-3 py-1'),
-                $this->prop('title')
+                $this->prop('title'),
+                $this->block('suffix'),
             ),
             $this->prop('items'),
         );
