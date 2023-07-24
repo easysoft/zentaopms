c578e2fcf106289b9b336ba4193276f8bc68ec42
zhouxin <zhouxin@easycorp.ltd>
2023-07-24 14:46:49 +0800

    * Add metrics about productplan.

--- module/metric/calc/global/scale/count_of_finished_productplan.php
+++ module/metric/calc/global/scale/count_of_finished_productplan.php
@@ -10,13 +10,13 @@
  * 单位：个
  * 描述：按全局统计的已完成计划数表示已经完成的计划数量。该度量项反映了组织中已经完成的计划数量，可以用于评估组织的计划执行能力和绩效。
  * 定义：所有计划的个数求和
-状态为已完成
-过滤已删除的计划
+ *       状态为已完成
+ *       过滤已删除的计划
  * 度量库：
  * 收集方式：realtime
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
- * @author    qixinzhi <qixinzhi@easycorp.ltd>
+ * @author    zhouxin <zhouxin@easycorp.ltd>
  * @package
  * @uses      func
  * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
@@ -24,20 +24,20 @@
  */
 class count_of_finished_productplan extends baseCalc
 {
-    public $dataset = null;
+    public $dataset = 'getPlans';
 
-    public $fieldList = array();
+    public $fieldList = array('t1.status');
 
-    //public funtion getStatement($dao)
-    //{
-    //}
+    public $result = 0;
 
-    public function calculate($data)
+    public function calculate($row)
     {
+        if($row->status == 'done') $this->result ++;
     }
 
     public function getResult($options = array())
     {
-        return $this->filterByOptions($this->result, $options);
+        $records = array(array('value' => $this->result));
+        return $this->filterByOptions($records, $options);
     }
 }
--- module/metric/calc/global/scale/count_of_unfinished_productplan.php
+++ module/metric/calc/global/scale/count_of_unfinished_productplan.php
@@ -10,15 +10,15 @@
  * 单位：个
  * 描述：按全局统计的未完成计划数表示未完成的计划数量。该度量项反映了组织中未完成的计划数量，可以用于评估组织的计划执行进展和挑战。
  * 定义：复用：
-按全局统计的已完成计划数
-按全局统计的计划总数
-公式：
-按全局统计的未完成计划数=按全局统计的计划总数-按全局统计的已完成计划数
+ *       按全局统计的已完成计划数
+ *       按全局统计的计划总数
+ *       公式：
+ *       按全局统计的未完成计划数=按全局统计的计划总数-按全局统计的已完成计划数
  * 度量库：
  * 收集方式：realtime
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
- * @author    qixinzhi <qixinzhi@easycorp.ltd>
+ * @author    zhouxin <zhouxin@easycorp.ltd>
  * @package
  * @uses      func
  * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
@@ -26,20 +26,20 @@
  */
 class count_of_unfinished_productplan extends baseCalc
 {
-    public $dataset = null;
+    public $dataset = 'getPlans';
 
-    public $fieldList = array();
+    public $fieldList = array('t1.status');
 
-    //public funtion getStatement($dao)
-    //{
-    //}
+    public $result = 0;
 
-    public function calculate($data)
+    public function calculate($row)
     {
+        if($row->status != 'done') $this->result ++;
     }
 
     public function getResult($options = array())
     {
-        return $this->filterByOptions($this->result, $options);
+        $records = array(array('value' => $this->result));
+        return $this->filterByOptions($records, $options);
     }
 }
