634178de0dc2bc8df63c63266e92e5895c4082db
daitingting <daitingting@xirangit.com>
2023-07-24 10:03:55 +0000

    * Refactor the about view, finish task #98695.

---
+++ module/misc/ui/about.html.php
@@ -0,0 +1,105 @@
+<?php
+declare(strict_types=1);
+/**
+ * The about view file of misc module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Tingting Dai <daitingting@easycorp.ltd>
+ * @package     misc 
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+
+set::title(' ');
+set::size('lg');
+
+$zentaoItems = array();
+foreach($lang->misc->zentao as $label => $groupItems)
+{
+    if(strpos(',labels,icons,version,others,', ",$label,") !== false) continue;
+
+    $api = $this->app->getClientLang() == 'en' ? $config->misc->enApi : $config->misc->api;
+
+    $liItems = array();
+    foreach($groupItems as $groupItem => $groupLabel)
+    {
+        $bizLink   = $this->app->getClientLang() == 'en' ? 'https://www.zentao.pm' : 'https://www.zentao.net/page/enterprise.html';
+        $link      = $groupItem == 'bizversion' ? $bizLink : "{$api}/goto.php?item={$groupItem}&from=about" ;
+        $liItems[] = h::li
+        (
+            setStyle('line-height', '24px'),
+            a
+            (
+                setID($groupItem),
+                set('href', $link),
+                set('target', '_blank'),
+                $groupLabel,
+            )
+        );
+    }
+
+    $zentaoItems[] = panel
+    (
+        set::class('w-1/5 mx-3'),
+        set::title($lang->misc->zentao->labels[$label]),
+        set::titleClass('strong'),
+        set::headingClass('gray-200'),
+        set::bodyClass('pd-10px'),
+        h::ul
+        (
+            set::class('pl-5 mb-2'),
+            setStyle('list-style-type', 'disc'),
+            $liItems
+        )
+    );
+}
+
+h::table
+(
+    set::class('table borderless'),
+    h::tr
+    (
+        h::td
+        (
+            set::class('text-center'),
+            zui::width('177px'),
+            img
+            (
+                set('src', $config->webRoot . 'theme/default/images/main/' . $lang->logoImg),
+            ),
+            h4
+            (
+                set::class('text-center my-2 font-bold'),
+                trim($config->visions, ',') == 'lite' ? $lang->liteName . $config->liteVersion : sprintf($lang->misc->zentao->version, $config->version)
+            )
+        ),
+        h::td
+        (
+            div
+            (
+                set::class('flex flex-nowrap justify-start'),
+                $zentaoItems,
+            )
+        ),
+    ),
+    h::tr
+    (
+        h::td
+        (
+            set::class('copyright text-right align-middle'),
+            set('colspan', 2),
+            div
+            (
+                set::class('pull-left'),
+                html($lang->designedByAIUX),
+            ),
+            div
+            (
+                set::class('pull-right'),
+                html($lang->misc->copyright)
+            )
+        )
+    )
+);
+
+render('modalDialog');
