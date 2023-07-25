a910079ff29247d32ea84c366b9964ea02ccc82a
qixinzhi <qixinzhi@easycorp.ltd>
2023-07-25 08:08:26 +0000

    * Finish task#98452.

--- module/metric/calc/global/scale/count_of_unclosed_bug.php
+++ module/metric/calc/global/scale/count_of_unclosed_bug.php
@@ -8,12 +8,12 @@
  * 目的：scale
  * 度量名称：按全局统计的未关闭Bug数
  * 单位：个
- * 描述：按全局统计的未关闭Bug数是指当前仍然存在但未关闭的Bug数量。这个度量项反映了系统或项目中仍然存在的待处理问题数量。较高的未关闭Bug数可能需要关注Bug解决的效率和质量。 
+ * 描述：按全局统计的未关闭Bug数是指当前仍然存在但未关闭的Bug数量。这个度量项反映了系统或项目中仍然存在的待处理问题数量。较高的未关闭Bug数可能需要关注Bug解决的效率和质量。
  * 定义：复用：
-按全局统计的Bug总数
-按全局统计的已关闭Bug数
-公式：
-按全局统计的未关闭Bug数=按全局统计的Bug总数-按全局统计的已关闭Bug数
+ *       按全局统计的Bug总数
+ *       按全局统计的已关闭Bug数
+ *       公式：
+ *       按全局统计的未关闭Bug数=按全局统计的Bug总数-按全局统计的已关闭Bug数
  * 度量库：
  * 收集方式：realtime
  *
@@ -26,16 +26,15 @@
  */
 class count_of_unclosed_bug extends baseCalc
 {
-    public $dataset = null;
+    public $dataset = 'getBugs';
 
-    public $fieldList = array();
+    public $fieldList = array('t1.status');
 
-    //public funtion getStatement($dao)
-    //{
-    //}
+    public $result = 0;
 
     public function calculate($row)
     {
+        if($row->status != 'closed') $this->result += 1;
     }
 
     public function getResult($options = array())
