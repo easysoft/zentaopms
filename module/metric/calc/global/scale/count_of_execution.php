740c81228c9207e40e6c165be11ceac7161b4a14
qixinzhi <qixinzhi@easycorp.ltd>
2023-07-25 02:59:32 +0000

    * Finish task#98371.

--- module/metric/calc/global/scale/count_of_execution.php
+++ module/metric/calc/global/scale/count_of_execution.php
@@ -10,7 +10,7 @@
  * 单位：个
  * 描述：按全局统计的执行总数表示在整个系统中所有执行项的数量，可以用来评估项目的规模和任务的总量。
  * 定义：所有的执行个数求和
-过滤已删除的执行
+ *       过滤已删除的执行
  * 度量库：
  * 收集方式：realtime
  *
@@ -23,16 +23,15 @@
  */
 class count_of_execution extends baseCalc
 {
-    public $dataset = null;
+    public $dataset = 'getAllExecutions';
 
-    public $fieldList = array();
+    public $fieldList = array('id');
 
-    //public funtion getStatement($dao)
-    //{
-    //}
+    public $result = 0;
 
     public function calculate($row)
     {
+        $this->result += 1;
     }
 
     public function getResult($options = array())
