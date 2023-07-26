a2b1f095763639641e697ab85b3e6b02cbbfbffc
qixinzhi <qixinzhi@easycorp.ltd>
2023-07-26 02:43:10 +0000

    * Finish task#98454.

--- module/metric/calc/global/scale/count_of_valid_bug.php
+++ module/metric/calc/global/scale/count_of_valid_bug.php
@@ -10,10 +10,10 @@
  * 单位：个
  * 描述：按全局统计的有效Bug数是指系统或项目中真正具有影响和价值的Bug数量。有效Bug通常是指导致系统不正常运行或影响用户体验的Bug。统计有效Bug数可以帮助评估系统或项目的稳定性和质量也可以评估测试人员之前的协作或对产品的了解程度。
  * 定义：所有Bug个数求和
-解决方案为已解决和延期处理
-或状态为激活的Bug数
-过滤已删除的Bug
-过滤已删除的产品
+ *       解决方案为已解决和延期处理
+ *       或状态为激活的Bug数
+ *       过滤已删除的Bug
+ *       过滤已删除的产品
  * 度量库：
  * 收集方式：realtime
  *
@@ -26,16 +26,15 @@
  */
 class count_of_valid_bug extends baseCalc
 {
-    public $dataset = null;
+    public $dataset = 'getBugs';
 
-    public $fieldList = array();
+    public $fieldList = array('t1.status', 't1.resolution');
 
-    //public funtion getStatement($dao)
-    //{
-    //}
+    public $result = 0;
 
     public function calculate($row)
     {
+        if($row->status != 'active' or $row->resolution == 'fixed' or $row->resolution == 'postponed') $this->result += 1;
     }
 
     public function getResult($options = array())
