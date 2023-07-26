317cc8bf9ddea21a3b246baf72ef2189fa6268b6
qixinzhi <qixinzhi@easycorp.ltd>
2023-07-26 05:02:49 +0000

    * Finish task#98455.

--- module/metric/calc/global/scale/count_of_annual_created_bug.php
+++ module/metric/calc/global/scale/count_of_annual_created_bug.php
@@ -10,9 +10,9 @@
  * 单位：个
  * 描述：按全局统计的年度新增Bug数是指在一年内新发现的Bug数量。这个度量项反映了系统或项目在一年内出现的新缺陷数量。年度新增Bug数的增加可能意味着质量控制存在问题，需要及时进行处理和改进。
  * 定义：所有Bug个数求和
-创建时间为某年
-过滤已删除的Bug
-过滤已删除的产品
+ *       创建时间为某年
+ *       过滤已删除的Bug
+ *       过滤已删除的产品
  * 度量库：
  * 收集方式：realtime
  *
@@ -25,21 +25,27 @@
  */
 class count_of_annual_created_bug extends baseCalc
 {
-    public $dataset = null;
+    public $dataset = 'getBugs';
 
-    public $fieldList = array();
+    public $fieldList = array('t1.openedDate');
 
-    //public funtion getStatement($dao)
-    //{
-    //}
+    public $result = array();
 
     public function calculate($row)
     {
+        $openedDate = $row->openedDate;
+        if(empty($openedDate)) return;
+
+        $year = substr($openedDate, 0, 4);
+        if($year == '0000') return;
+
+        if(!isset($this->result[$year])) $this->result[$year] = 0;
+        $this->result[$year] += 1;
     }
 
     public function getResult($options = array())
     {
-        $records = $this->getRecords(array('value'));
+        $records = $this->getRecords(array('year', 'value'));
         return $this->filterByOptions($records, $options);
     }
 }
