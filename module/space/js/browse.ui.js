fd29c7032b2ad5fdab79c5a5839e3ee647d096cb
zenggang <zenggang@easycorp.ltd>
2023-07-26 02:43:19 +0000

    * Fix app list.

--- module/space/js/browse.ui.js
+++ module/space/js/browse.ui.js
@@ -16,6 +16,18 @@ window.renderInstanceList = function (result, {col, row, value})
         result[0] = {html: '<span class="' + statusClass + '">' + result[0] + '</span>'};
         return result;
     }
+    else if(col.name === 'name')
+    {
+        if(row.data.externalID)
+        {
+            if(row.data.appName == 'Gitea') result[0] = {html: '<a href="' + $.createLink(row.data.appName, 'view', 'id=' + row.data.externalID) + '" data-toggle="modal">' + result[0] + '</a>'};
+        }
+        else
+        {
+            result[0] = {html: '<a href="' + $.createLink('instance', 'view', 'id=' + row.id) + '">' + result[0] + '</a>'};
+        }
+        return result;
+    }
 
     return result;
 }
