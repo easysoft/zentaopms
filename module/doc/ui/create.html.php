4406e82765abedf3ae5dbf0d8f803d3ca90e5339
sunguangming <sunguangming@easycorp.ltd>
2023-07-27 10:11:06 +0800

    * Code for doc::create method.

--- module/doc/ui/create.html.php
+++ module/doc/ui/create.html.php
@@ -10,211 +10,247 @@ declare(strict_types=1);
  */
 namespace zin;
 
-jsVar('titleNotEmpty', sprintf($lang->error->notempty, $lang->doc->title));
-jsVar('requiredFields', ',' . $config->doc->create->requiredFields . ',');
-jsVar('libNotEmpty', sprintf($lang->error->notempty, $lang->doc->lib));
-jsVar('keywordsNotEmpty', sprintf($lang->error->notempty, $lang->doc->keywords));
-jsVar('contentNotEmpty', sprintf($lang->error->notempty, $lang->doc->content));
-
-$projectRow = null;
-if($linkType == 'project')
+if($docType != '' and strpos($config->doc->officeTypes, $docType) !== false)
 {
-    $projectRow = formRow
-    (
-        formGroup
+    set::title($lang->doc->create);
+    if($this->config->edition != 'open')
+    {
+        div
         (
-            setClass('w-1/2'),
-            set::label($lang->doc->project),
-            picker
+            setClass('alert warning-pale bd bd-warning'),
+            html
             (
-                set::name('project'),
-                set::id('project'),
-                set::items($objects),
-                isset($execution) ? set::value($execution->project) : set::value($objectID),
-                on::change('loadExecutions')
+                sprintf($lang->doc->notSetOffice, zget($lang->doc->typeList, $docType),
+                common::hasPriv('custom', 'libreoffice') ? $this->createLink('custom', 'libreoffice', '', '', true) : '###')
             )
-        ),
-        ($this->app->tab == 'doc' and $config->vision == 'rnd') ? formGroup
+        );
+    }
+    else
+    {
+        div
         (
-            setClass('w-1/2'),
-            set::label($lang->doc->execution),
-            picker
-            (
-                set::name('execution'),
-                set::id('execution'),
-                set::items($executions),
-                set::value(isset($execution) ? $objectID : ''),
-            )
-        ) : null,
-    );
+            setClass('alert warning-pale bd bd-warning'),
+            html(sprintf($lang->doc->cannotCreateOffice, zget($lang->doc->typeList, $docType)))
+        );
+    }
 }
+else
+{
+    jsVar('titleNotEmpty', sprintf($lang->error->notempty, $lang->doc->title));
+    jsVar('requiredFields', ',' . $config->doc->create->requiredFields . ',');
+    jsVar('libNotEmpty', sprintf($lang->error->notempty, $lang->doc->lib));
+    jsVar('keywordsNotEmpty', sprintf($lang->error->notempty, $lang->doc->keywords));
+    jsVar('contentNotEmpty', sprintf($lang->error->notempty, $lang->doc->content));
 
-form
-(
-    set::actions(''),
+    $projectRow = null;
+    if($linkType == 'project')
+    {
+        $projectRow = formRow
+            (
+                formGroup
+                (
+                    setClass('w-1/2'),
+                    set::label($lang->doc->project),
+                    picker
+                    (
+                        set::name('project'),
+                        set::id('project'),
+                        set::items($objects),
+                        isset($execution) ? set::value($execution->project) : set::value($objectID),
+                        on::change('loadExecutions')
+                    )
+                ),
+                ($this->app->tab == 'doc' and $config->vision == 'rnd') ? formGroup
+                (
+                    setClass('w-1/2'),
+                    set::label($lang->doc->execution),
+                    picker
+                    (
+                        set::name('execution'),
+                        set::id('execution'),
+                        set::items($executions),
+                        set::value(isset($execution) ? $objectID : ''),
+                    )
+                ) : null,
+            );
+    }
+
     div
     (
-        setClass('flex titleBox'),
-        backBtn(setClass('btn secondary'), set::icon('back'), $lang->goback),
-        formGroup
+        set::id('main'),
+        form
         (
-            set::id('titleBox'),
-            input
+            set::actions(''),
+            div
             (
-                set::name('title'),
-                set::placeholder($lang->doc->titlePlaceholder),
-            ),
-        ),
-        btn
-        (
-            set
-            (
-                array
+                setClass('flex titleBox'),
+                backBtn(setClass('btn secondary'), set::icon('back'), $lang->goback),
+                formGroup
                 (
-                    'class' => 'btn secondary',
-                    'text'  => $lang->doc->saveDraft,
-                    'id'    => 'saveDraft',
-                    'btnType' => 'submit',
-                )
-            )
-        ),
-        btn
-        (
-            set
-            (
-                array
-                (
-                    'class'       => 'btn primary',
-                    'text'        => $lang->doc->release,
-                    'data-toggle' => 'modal',
-                    'id'          => 'basicInfoLink',
-                    'url'         => '#modalBasicInfo'
-                )
-            )
-        )
-    ),
-    editor
-    (
-        set::name('content'),
-    ),
-    formHidden('status', 'normal'),
-    formHidden('contentType', 'html'),
-    formHidden('type', 'text'),
-    modalTrigger
-    (
-        modal
-        (
-            set::id('modalBasicInfo'),
-            on::change('#product',   "loadObjectModules"),
-            on::change('#project',   "loadObjectModules"),
-            on::change('#execution', "loadObjectModules"),
-            $projectRow,
-            ($linkType == 'execution') ? formGroup
-            (
-                set::width('1/2'),
-                set::label($lang->doc->execution),
-                picker
-                (
-                    set::name('execution'),
-                    set::id('execution'),
-                    set::items($objects),
-                    set::value($objectID) 
-                )
-            ) : null,
-            ($linkType == 'product') ? formGroup
-            (
-                set::width('1/2'),
-                set::label($lang->doc->product),
-                picker
-                (
-                    set::name('product'),
-                    set::id('product'),
-                    set::items($objects),
-                    set::value($objectID) 
-                )
-            ) : null,
-            formGroup
-            (
-                set::width('1/2'),
-                set::label($lang->doc->lib),
-                picker
-                (
-                    set::name('module'),
-                    set::items($moduleOptionMenu),
-                    set::value($moduleID) 
-                )
-            ),
-            formGroup
-            (
-                set::label($lang->doc->keywords),
-                input
-                (
-                    set::name('keywords'),
-                )
-            ),
-            formGroup
-            (
-                set::label($lang->doc->files),
-                upload()
-            ),
-            formGroup
-            (
-                set::label($lang->doc->mailto),
-                picker
-                (
-                    set::multiple(true),
-                    set::name('mailto[]'),
-                    set::items($users),
-                )
-            ),
-            formGroup
-            (
-                set::label($lang->doclib->control),
-                radioList
-                (
-                    set::name('acl'),
-                    set::items($lang->doc->aclList),
-                    set::value($objectType == 'mine' ? 'private' : 'open'),
-                    on::change('toggleWhiteList')
-                )
-            ),
-            formGroup
-            (
-                setClass('hidden'),
-                set::label($lang->doc->whiteList),
-                set::id('whitelistBox'),
-                picker
-                (
-                    set::name('groups[]'),
-                    set::items($groups),
-                    set::multiple(true),
-                ),
-                picker
-                (
-                    set::name('users[]'),
-                    set::items($users),
-                    set::multiple(true),
-                )
-            ),
-            formRow
-            (
-                div
-                (
-                    setClass('form-actions form-group no-label'),
-                    btn
+                    set::id('titleBox'),
+                    input
                     (
-                        set
+                        set::name('title'),
+                        set::placeholder($lang->doc->titlePlaceholder),
+                    ),
+                ),
+                btn
+                (
+                    set
+                    (
+                        array
                         (
-                            array
-                            (
-                                'class'   => 'btn primary',
-                                'btnType' => 'submit',
-                            )
+                            'class' => 'btn secondary',
+                            'text'  => $lang->doc->saveDraft,
+                            'id'    => 'saveDraft',
+                            'btnType' => 'submit',
+                        )
+                    )
+                ),
+                btn
+                (
+                    set
+                    (
+                        array
+                        (
+                            'class'       => 'btn primary',
+                            'text'        => $lang->doc->release,
+                            'data-toggle' => 'modal',
+                            'id'          => 'basicInfoLink',
+                            'url'         => '#modalBasicInfo'
+                        )
+                    )
+                )
+            ),
+            editor
+            (
+                set::name('content'),
+            ),
+            formHidden('status', 'normal'),
+            formHidden('contentType', 'html'),
+            formHidden('type', 'text'),
+            modalTrigger
+            (
+                modal
+                (
+                    set::id('modalBasicInfo'),
+                    on::change('#product',   "loadObjectModules"),
+                    on::change('#project',   "loadObjectModules"),
+                    on::change('#execution', "loadObjectModules"),
+                    $projectRow,
+                    ($linkType == 'execution') ? formGroup
+                    (
+                        set::width('1/2'),
+                        set::label($lang->doc->execution),
+                        picker
+                        (
+                            set::name('execution'),
+                            set::id('execution'),
+                            set::items($objects),
+                            set::value($objectID) 
+                        )
+                    ) : null,
+                    ($linkType == 'product') ? formGroup
+                    (
+                        set::width('1/2'),
+                        set::label($lang->doc->product),
+                        picker
+                        (
+                            set::name('product'),
+                            set::id('product'),
+                            set::items($objects),
+                            set::value($objectID) 
+                        )
+                    ) : null,
+                    formGroup
+                    (
+                        set::width('1/2'),
+                        set::label($lang->doc->lib),
+                        picker
+                        (
+                            set::name('module'),
+                            set::items($moduleOptionMenu),
+                            set::value($moduleID) 
+                        )
+                    ),
+                    formGroup
+                    (
+                        set::label($lang->doc->keywords),
+                        input
+                        (
+                            set::name('keywords'),
+                        )
+                    ),
+                    formGroup
+                    (
+                        set::label($lang->doc->files),
+                        upload()
+                    ),
+                    formGroup
+                    (
+                        set::label($lang->doc->mailto),
+                        picker
+                        (
+                            set::multiple(true),
+                            set::name('mailto[]'),
+                            set::items($users),
+                        )
+                    ),
+                    formGroup
+                    (
+                        set::label($lang->doclib->control),
+                        radioList
+                        (
+                            set::name('acl'),
+                            set::items($lang->doc->aclList),
+                            set::value($objectType == 'mine' ? 'private' : 'open'),
+                            on::change('toggleWhiteList')
+                        )
+                    ),
+                    formGroup
+                    (
+                        setClass('hidden'),
+                        set::label($lang->doc->whiteList),
+                        set::id('whitelistBox'),
+                        picker
+                        (
+                            set::name('groups[]'),
+                            set::items($groups),
+                            set::multiple(true),
                         ),
-                        $lang->doc->release
+                        picker
+                        (
+                            set::name('users[]'),
+                            set::items($users),
+                            set::multiple(true),
+                        )
+                    ),
+                    formRow
+                    (
+                        div
+                        (
+                            setClass('form-actions form-group no-label'),
+                            btn
+                            (
+                                set
+                                (
+                                    array
+                                    (
+                                        'class'   => 'btn primary',
+                                        'btnType' => 'submit',
+                                    )
+                                ),
+                                $lang->doc->release
+                            )
+                        )
                     )
                 )
             )
         )
-    )
-);
+    );
+}
+
+set::zui(true);
+to::main(false);
+to::header(false);
+render(isInModal() ? 'modalDialog' : 'pagebase');
