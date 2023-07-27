4879ab16560f5b5c3a370ba72d07cd8880d70cb9
sunguangming <sunguangming@easycorp.ltd>
2023-07-27 08:27:49 +0800

    * Remove oldpage.

--- module/index/config.php
+++ module/index/config.php
@@ -41,7 +41,6 @@ $config->index->oldPages[] = 'kanban-setting';
 $config->index->oldPages[] = 'admin-safe';
 $config->index->oldPages[] = 'admin-checkweak';
 $config->index->oldPages[] = 'admin-resetpwdsetting';
-$config->index->oldPages[] = 'admin-tableengine';
 $config->index->oldPages[] = 'webhook-browse';
 $config->index->oldPages[] = 'webhook-create';
 $config->index->oldPages[] = 'dev-api';
