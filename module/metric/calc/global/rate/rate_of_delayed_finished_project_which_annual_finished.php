c607e4986036e9be9cb36e974ab40c408c66e85f
zhouxin <zhouxin@easycorp.ltd>
2023-07-27 13:51:02 +0800

    * Finish task #98394,98395.

--- module/metric/calc/global/rate/rate_of_delayed_finished_project_which_annual_finished.php
+++ module/metric/calc/global/rate/rate_of_delayed_finished_project_which_annual_finished.php
@@ -10,15 +10,15 @@
  * 单位：个
  * 描述：按全局统计的年度完成项目中项目的延期完成率是指按全局统计的年度完成项目中延期完成项目数与关闭项目数之比。这个度量项可以帮助团队评估某年度项目按期关闭的能力和效果，并作为项目管理的绩效指标之一。较高的延期完成率可能需要团队关注项目计划和资源安排的问题。
  * 定义：复用：
-按全局统计的年度关闭项目数
-按全局统计的年度延期关闭项目数
-公式：
-按全局统计的年度项目延期关闭率=按全局统计的年度延期关闭项目数/按全局统计的年度关闭项目数
+ *       按全局统计的年度关闭项目数
+ *       按全局统计的年度完成项目中延期完成项目数
+ *       公式：
+ *       按全局统计的年度项目延期关闭率=按全局统计的年度延期关闭项目数/按全局统计的年度关闭项目数
  * 度量库：
  * 收集方式：realtime
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
- * @author    qixinzhi <qixinzhi@easycorp.ltd>
+ * @author    zhouxin <zhouxin@easycorp.ltd>
  * @package
  * @uses      func
  * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
@@ -26,20 +26,25 @@
  */
 class rate_of_delayed_finished_project_which_annual_finished extends baseCalc
 {
-    public $dataset = null;
+    public $dataset = 'getAllProjects';
 
-    public $fieldList = array();
+    public $fieldList = array('status', 'closedDate', 'realEnd', 'firstEnd');
 
-    //public funtion getStatement($dao)
-    //{
-    //}
-
-    public function calculate($data)
+    public function calculate($row)
     {
+        if(empty($row->closedDate)) return false;
+
+        $closedYear = substr($row->closedDate, 0, 4);
+
+        if(!isset($this->result[$closedYear])) $this->result[$closedYear] = array('closed' => 0, 'delayed' => 0);
+        if($row->status == 'closed') $this->result[$closedYear]['closed'] ++;
+        if($row->status == 'closed' && $row->realEnd > $row->firstEnd) $this->result[$closedYear]['delayed'] ++;
     }
 
     public function getResult($options = array())
     {
-        return $this->filterByOptions($this->result, $options);
+        $records = array();
+        foreach($this->result as $year => $result) $this->result[$year] = $result['closed'] ? round($result['delayed'] / $result['closed'], 4) : 0;
+        return $this->filterByOptions($records, $options);
     }
 }
