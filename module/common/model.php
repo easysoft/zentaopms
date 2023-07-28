e946d835ad2dcdda23bfe9ef4582c1d57149beb7
dingguodong <dingguodong@easycorp.ltd>
2023-07-28 13:55:51 +0800

    * Refactor common::http method.

--- module/common/model.php
+++ module/common/model.php
@@ -3237,11 +3237,11 @@ EOF;
      * @param  string       $dataType
      * @param  string       $method    POST|PATCH|PUT
      * @param  int          $timeout
-     * @param  bool         $httpCode
-     * @param  bool         $log
+     * @param  bool         $httpCode  Return a array contains response, http code, body, header. such as [response, http_code, 'body' => body, 'header' => header].
+     * @param  bool         $log       Save to log or not
      * @static
      * @access public
-     * @return string
+     * @return string|array
      */
     public static function http($url, $data = null, $options = array(), $headers = array(), $dataType = 'data', $method = 'POST', $timeout = 30, $httpCode = false, $log = true)
     {
@@ -3254,8 +3254,12 @@ EOF;
 
         commonModel::$requestErrors = array();
 
+        $requestType = 'GET';
         if(!is_array($headers)) $headers = (array)$headers;
-        $headers[] = "API-RemoteIP: " . helper::getRemoteIp();
+
+        $headers[] = 'API-RemoteIP: ' . helper::getRemoteIp(); /* Real IP of real user. */
+        $headers[] = 'API-LocalIP: ' . zget($_SERVER, 'SERVER_ADDR', ''); /* Server IP of self. */
+
         if($dataType == 'json')
         {
             $headers[] = 'Content-Type: application/json;charset=utf-8';
@@ -3268,7 +3272,7 @@ EOF;
         curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $timeout);
         curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_ENCODING, "");
+        curl_setopt($curl, CURLOPT_ENCODING,'');
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
@@ -3283,14 +3287,36 @@ EOF;
             if(is_object($data)) $data = (array) $data;
             if($method == 'POST') curl_setopt($curl, CURLOPT_POST, true);
             if(in_array($method, array('PATCH', 'PUT'))) curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+            $requestType = $method;
             curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
         }
 
         if($options) curl_setopt_array($curl, $options);
         $response = curl_exec($curl);
-        $errors   = curl_error($curl);
 
-        if($httpCode) $httpCode = curl_getinfo($curl,CURLINFO_HTTP_CODE);
+        $errno  = curl_errno($curl);
+        $errors = empty($errno)  ? 0 : curl_error($curl);
+        $info   = curl_getinfo($curl);
+
+        if($httpCode)
+        {
+            $httpCode     = $info['http_code'] ?? curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            $headerSize   = $info['header_size'] ?? curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+            $headerString = substr($response, 0, $headerSize);
+            $body         = substr($response, $headerSize);
+
+            /* Parse header. */
+            $header    = explode('\n', $headerString);
+            $newHeader = array();
+            foreach($header as $item)
+            {
+                $field = explode(':', $item);
+                if(count($field) < 2) continue;
+                $headerKey = array_shift($field);
+                $newHeader[$headerKey] = implode('', $field);
+            }
+        }
+
         curl_close($curl);
 
         if($log or $app->config->debug)
@@ -3301,18 +3327,25 @@ EOF;
             $fh = fopen($logFile, 'a');
             if($fh)
             {
-                fwrite($fh, date('Ymd H:i:s') . ": " . $app->getURI() . "\n");
-                fwrite($fh, "url:    " . $url . "\n");
-                if(!empty($data)) fwrite($fh, "data:   " . print_r($data, true) . "\n");
-                fwrite($fh, "results:" . print_r($response, true) . "\n");
-                if(!empty($errors)) fwrite($fh, "errors: " . $errors . "\n");
+                fwrite($fh, date('Ymd H:i:s') . ': ' . $app->getURI() . '\n');
+                fwrite($fh, "{$requestType} url:    " . $url . '\n');
+                if(!empty($data)) fwrite($fh, 'data:   ' . print_r($data, true) . '\n');
+                fwrite($fh, 'results:' . print_r($response, true) . '\n');
+
+                if(!empty($errors))
+                {
+                    fwrite($fh, 'errno: ' . $errno . '\n');
+                    fwrite($fh, 'errors: ' . $errors . '\n');
+                    fwrite($fh, 'info: ' . print_r($info, true) . '\n');
+                }
+
                 fclose($fh);
             }
         }
 
         if($errors) commonModel::$requestErrors[] = $errors;
 
-        return $httpCode ? array($response, $httpCode) : $response;
+        return $httpCode ? array($response, $httpCode, 'body' => $body, 'header' => $newHeader, 'errno' => $errno, 'info' => $info, 'response' => $response) : $response;
     }
 
     /**
