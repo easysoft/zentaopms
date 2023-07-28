4890e938ddb4368c2cf4afe4a8bd951e02fb6caa
liumengyi <liumengyi@easycorp.ltd>
2023-07-28 13:48:17 +0800

    * Remove header in user::reset.

--- module/user/ui/reset.html.php
+++ module/user/ui/reset.html.php
@@ -10,95 +10,99 @@ declare(strict_types=1);
  */
 namespace zin;
 
-if($needCreateFile)
-{
-    div
+$content = $needCreateFile ? div
+(
+    setClass('create-file cell flex justify-center mt-24'),
+    panel
     (
-        setClass('create-file cell flex justify-center mt-24'),
-        panel
+        setClass('create-file-panel w-full'),
+        set::title($lang->user->resetPassword),
+        cell
         (
-            setClass('create-file-panel w-full'),
-            set::title($lang->user->resetPassword),
-            cell
+            setClass('alert-info p-4 mb-6'),
+            html(sprintf($lang->user->noticeResetFile, $resetFileName)),
+        ),
+        cell
+        (
+            setClass('flex justify-center mb-4'),
+            btn
             (
-                setClass('alert-info p-4 mb-6'),
-                html(sprintf($lang->user->noticeResetFile, $resetFileName)),
+                setClass('px-8 mx-4'),
+                set::type('primary'),
+                set::url(inlink('reset')),
+                $lang->refresh,
             ),
-            cell
+            backBtn
             (
-                setClass('flex justify-center mb-4'),
-                btn
-                (
-                    setClass('px-8 mx-4'),
-                    set::type('primary'),
-                    set::url(inlink('reset')),
-                    $lang->refresh,
-                ),
-                backBtn
-                (
-                    setClass('px-8 mx-4'),
-                    set::back('GLOBAL'),
-                    $lang->goback
-                ),
+                setClass('px-8 mx-4'),
+                set::back('GLOBAL'),
+                $lang->goback
             ),
         ),
-    );
-}
-else
-{
-    div
+    ),
+) : div
+(
+    setClass('reset-password cell flex justify-center mt-24'),
+    formPanel
     (
-        setClass('reset-password cell flex justify-center mt-24'),
-        formPanel
+        setClass('reset-form w-full'),
+        set::title($lang->user->resetPassword),
+        formRow
         (
-            setClass('reset-form w-full'),
-            set::title($lang->user->resetPassword),
-            formRow
+            formGroup
             (
-                formGroup
-                (
-                    set::label($lang->user->account),
-                    set::name('account'),
-                    set::value(''),
-                    set::required(true),
-                ),
-            ),
-            formRow
-            (
-                formGroup
-                (
-                    set::label($lang->user->password),
-                    password(set::checkStrength(true)),
-                    set::required(true),
-                ),
-            ),
-            formRow
-            (
-                formGroup
-                (
-                    set::label($lang->user->password2),
-                    set::control('password'),
-                    set::name('password2'),
-                    set::value(''),
-                    set::required(true),
-                )
-            ),
-            formRow
-            (
-                setClass('hidden'),
-                formGroup
-                (
-                    set::name('passwordLength'),
-                    set::value(0),
-                ),
-                formGroup
-                (
-                    set::name('verifyRand'),
-                    set::value($rand),
-                ),
+                set::label($lang->user->account),
+                set::name('account'),
+                set::value(''),
+                set::required(true),
             ),
         ),
-    );
-}
+        formRow
+        (
+            formGroup
+            (
+                set::label($lang->user->password),
+                password(set::checkStrength(true)),
+                set::required(true),
+            ),
+        ),
+        formRow
+        (
+            formGroup
+            (
+                set::label($lang->user->password2),
+                set::control('password'),
+                set::name('password2'),
+                set::value(''),
+                set::required(true),
+            )
+        ),
+        formRow
+        (
+            setClass('hidden'),
+            formGroup
+            (
+                set::name('passwordLength'),
+                set::value(0),
+            ),
+            formGroup
+            (
+                set::name('verifyRand'),
+                set::value($rand),
+            ),
+        ),
+    ),
+);
 
-render();
+set::zui(true);
+div
+(
+    set::id('main'),
+    div
+    (
+        set::id('mainContent'),
+        $content,
+    ),
+);
+
+render('pagebase');
