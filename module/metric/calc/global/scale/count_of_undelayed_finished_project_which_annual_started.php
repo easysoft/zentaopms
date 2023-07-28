0eed4aece86762088434237a6c1e94cd56cbe280
qixinzhi <qixinzhi@easycorp.ltd>
2023-07-28 05:05:47 +0000

    * Finish task.

--- module/metric/calc/global/scale/count_of_undelayed_finished_project_which_annual_started.php
+++ module/metric/calc/global/scale/count_of_undelayed_finished_project_which_annual_started.php
@@ -10,9 +10,9 @@
  * 单位：个
  * 描述：按全局统计的年度启动项目中按期完成项目数是指在某年度启动的项目中按预定计划时间关闭的项目数量。这个度量项可以帮助团队评估某年度项目的时间管理和执行能力，并衡量项目的进展和交付效果。较高的按时关闭项目数表明团队能够按时交付项目，有助于保持项目的正常进行和客户满意度。
  * 定义：所有的项目个数求和
-启动时间为某年
-完成日期<=项目启动时的计划截止日期（根据历史记录推算）
-过滤已删除的项目
+ *       启动时间为某年
+ *       完成日期<=项目启动时的计划截止日期（根据历史记录推算）
+ *       过滤已删除的项目
  * 度量库：
  * 收集方式：realtime
  *
@@ -25,20 +25,27 @@
  */
 class count_of_undelayed_finished_project_which_annual_started extends baseCalc
 {
-    public $dataset = null;
+    public $dataset = 'getAllProjects';
 
-    public $fieldList = array();
+    public $fieldList = array('realBegan', 'realEnd', 'firstEnd');
 
-    //public funtion getStatement($dao)
-    //{
-    //}
+    public $result = array();
 
-    public function calculate($data)
+    public function calculate($row)
     {
+        if(empty($row->realBegan) or empty($row->realEnd) or empty($row->firstEnd)) return false;
+
+        $beginYear = substr($row->realBegan, 0, 4);
+        if($beginYear == '0000') return false;
+
+        if(!isset($this->result[$beginYear])) $this->result[$beginYear] = 0;
+
+        if(strtotime($row->realEnd) <= strtotime($row->firstEnd)) $this->result[$beginYear] += 1;
     }
 
     public function getResult($options = array())
     {
-        return $this->filterByOptions($this->result, $options);
+        $records = $this->getRecords(array('year', 'value'));
+        return $this->filterByOptions($records, $options);
     }
 }
