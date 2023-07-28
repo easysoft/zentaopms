5a2aad409a0502ed63b8ad0bf8e8dc2908db1990
daitingting <daitingting@xirangit.com>
2023-07-28 02:47:16 +0000

    * Refactor the structure view.

---
+++ module/extension/ui/structure.html.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+/**
+ * The structure view file of extension module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Tingting Dai <daitingting@easycorp.ltd>
+ * @package     extension
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+
+$appRoot = $this->app->getAppRoot();
+$files   = json_decode($extension->files);
+$fileItems = array();
+foreach($files as $file => $md5) $fileItems[] = $appRoot . $file . "\n";
+
+set::title($extension->name . '[' . $extension->code . ']' . $lang->extension->structure . ':');
+
+div
+(
+    set::class('border bg-surface p-2'),
+    $fileItems
+);
+
+render();
