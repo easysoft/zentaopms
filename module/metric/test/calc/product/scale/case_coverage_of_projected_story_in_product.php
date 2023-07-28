1cece9987be0d1e78f47b99f26a552c5b9e56f9d
qixinzhi <qixinzhi@easycorp.ltd>
2023-07-28 02:28:04 +0000

    * Fix unit test.

--- module/metric/test/calc/product/scale/case_coverage_of_projected_story_in_product.php
+++ module/metric/test/calc/product/scale/case_coverage_of_projected_story_in_product.php
@@ -3,9 +3,9 @@
 include dirname(__FILE__, 7) . '/test/lib/init.php';
 include dirname(__FILE__, 4) . '/calc.class.php';
 
-zdTable('product')->gen(50);
-zdTable('story')->config('story')->gen(50);
-zdTable('case')->config('case')->gen(100);
+zdTable('product')->config('product', $useCommon = true, $levels = 4)->gen(10);
+zdTable('story')->config('story_projected', $useCommon = true, $levels = 4)->gen(1000);
+zdTable('case')->config('case', $useCommon = true, $levels = 4)->gen(1000);
 
 $metric = new metricTest();
 $calc   = $metric->calcMetric(__FILE__);
@@ -18,8 +18,8 @@ cid=1
 
 */
 
-r($calc->getResult(array('product' => '1')))  && p('0:value') && e('0.6'); // 测试产品1的已立项研发需求用例覆盖率数。
-r($calc->getResult(array('product' => '2')))  && p('0:value') && e('0.4'); // 测试产品2的已立项研发需求用例覆盖率数。
-r($calc->getResult(array('product' => '3')))  && p('0:value') && e('1');   // 测试产品3的已立项研发需求用例覆盖率数。
-r($calc->getResult(array('product' => '4')))  && p('0:value') && e('0');   // 测试产品4的已立项研发需求用例覆盖率数。
-r($calc->getResult(array('product' => '5')))  && p('0:value') && e('0');   // 测试产品5的已立项研发需求用例覆盖率数。
+r($calc->getResult(array('product' => '1')))  && p('0:value') && e('0.34'); // 测试产品1的已立项研发需求用例覆盖率数。
+r($calc->getResult(array('product' => '2')))  && p('0:value') && e('0');    // 测试产品2的已立项研发需求用例覆盖率数。
+r($calc->getResult(array('product' => '3')))  && p('0:value') && e('0.34'); // 测试产品3的已立项研发需求用例覆盖率数。
+r($calc->getResult(array('product' => '4')))  && p('0:value') && e('0');    // 测试产品4的已立项研发需求用例覆盖率数。
+r($calc->getResult(array('product' => '5')))  && p('0:value') && e('0.32'); // 测试产品5的已立项研发需求用例覆盖率数。
