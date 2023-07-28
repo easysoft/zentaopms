3320ae21c27b6bfb6ff09e4cbbbd698a256f5093
caoyanyi <caoyanyi@easycorp.ltd>
2023-07-28 13:33:31 +0800

    * Change task report page data.

--- module/task/js/report.ui.js
+++ module/task/js/report.ui.js
@@ -1,11 +1,17 @@
 window.createChart = function()
 {
     const formData = new FormData($('#chartForm')[0]);
-    postAndLoadPage($('#chartForm').attr('action'), formData, '#chartContainer');
+    postAndLoadPage($('#chartForm').attr('action'), formData, '#chartContainer,pageJS/.zin-page-js,#configJS');
 }
 
 window.triggerChecked = function()
 {
     $(".btn-select-all").toggleClass('checked');
     $("#chartForm").find("input[name^=charts]").prop("checked", $('.btn-select-all').hasClass('checked'));
+}
+
+window.changeTab = function(e)
+{
+    $('#chartForm').attr('action', $(e.target).closest('.font-medium').data('param'))
+    createChart();
 }
--- module/task/ui/report.html.php
+++ module/task/ui/report.html.php
@@ -13,21 +13,16 @@ namespace zin;
 jsVar('executionID', $executionID);
 jsVar('browseType', $browseType);
 
-toolbar
+detailHeader
 (
-    item(set(
-    [
-        'text'  => $lang->goback,
-        'icon'  => 'back',
-        'class' => 'ghost',
-        'url'   => createLink('execution', 'task', "executionID=$executionID")
-    ])),
-    item(set::type('divider')),
-    div
+    to::title
     (
-        $lang->task->report->common,
-        set::class('font-semibold'),
-    )
+        entityLabel
+        (
+            set::level(1),
+            set::text($lang->task->report->common)
+        )
+    ),
 );
 
 $selectCharts = array();
@@ -40,160 +35,87 @@ foreach($lang->task->report->charts as $code => $name)
     $selectCharts[] = $chart;
 }
 
-sidebar
-(
-    formPanel
-    (
-        setID('chartForm'),
-        set('title', $lang->task->report->select),
-        set('actions', array
-        (
-            array
-            (
-                'text'  => $lang->selectAll,
-                'class' => 'btn-select-all space',
-                'url'   => 'javascript:triggerChecked();'
-            ),
-            array
-            (
-                'type'  => 'primary',
-                'text'  => $lang->task->report->create,
-                'class' => 'btn-select-all space',
-                'url'   => 'javascript:createChart();'
-            ),
-        )),
-        checkList
-        (
-            set::primary(true),
-            set::name('charts[]'),
-            set::value('code'),
-            set::inline(false),
-            set::items($selectCharts),
-        ),
-    ),
-);
-
-foreach($lang->report->typeList as $type => $name)
+$echarts = array();
+foreach($charts as $type => $option)
 {
-    $chartTypeList[] = item
+    $chartData = $datas[$type];
+    $echarts[] = tableChart
     (
-        set::text($name),
-        set::url(createLink('task', 'report', "executionID=$executionID&browseType=$browseType&type=$type")),
-        set::icon($type == 'default' ? 'list-box' : "chart-{$type}"),
-        setClass('btn ghost' . ($type == $chartType ? ' btn-active-line' : ''))
+        set::type($option->type),
+        set::title($lang->task->report->charts[$type]),
+        set::datas((array)$chartData),
+        set::tableWidth('40%'),
+        set::tableHeaders(array(
+            'item'    => $lang->task->report->$type->item,
+            'value'   => $lang->task->report->value,
+            'percent' => $lang->report->percent,
+        )),
     );
 }
-
-$notice = str_replace('%tab%', $lang->task->unclosed . $lang->task->common, $lang->report->notice->help);
-$ths    = array('item', 'value', 'percent');
 
 $chartContents = array();
-foreach($charts as $chartType => $chartOption)
+foreach($lang->report->typeList as $type => $typeName)
 {
-    foreach($datas[$chartType] as $key => $data)
-    {
-        $trs[] = h::tr
-        (
-            h::td
-            (
-                icon('chart-color-dot'),
-                setClass('chart-color'),
-            ),
-            h::td
-            (
-                $data->name,
-                setClass('chart-label'),
-                set('align', 'left'),
-                set('title', zget($data, 'title', $data->name)),
-            ),
-            h::td
-            (
-                $data->value,
-                setClass('chart-value'),
-                set('align', 'right'),
-            ),
-            h::td
-            (
-                ($data->percent * 100) . $lang->percent,
-                set('align', 'right'),
-            )
-        );
-    }
-
-    $chartContents[] = div
+    $link = createLink('task', 'report', "executionID=$executionID&browseType=$browseType&type=%s");
+    $chartContents[] = tabPane
     (
-        setClass('flex items-center'),
-        cell
+        set::title($typeName),
+        set::active($type == $chartType),
+        set::param(sprintf($link, $type)),
+        to::prefix(icon($type == 'default' ? 'list-alt' : "chart-{$type}")),
+        div
         (
-            set::width('70%'),
-            div
-            (
-                setClass('chart-canvas'),
-                canvas
-                (
-                    setID("chart-{$chartType}"),
-                    set('width', $chartOption->width),
-                    set('height', $chartOption->height),
-                    set('data-responsive', true),
-                )
-            ),
+            set::class('pb-4'),
+            span(set::class('text-gray'),
+            html(str_replace('%tab%', $lang->task->wait . $lang->testcase->common, $lang->report->notice->help)))
         ),
-        cell
-        (
-            h::table
-            (
-                setClass('table table-bordered table-condensed table-hover table-chart'),
-                set('data-chart', $chartOption->type),
-                set('data-target', '#chart-' . $chartType),
-                set('data-animation', false),
-
-                h::thead
-                (
-                    h::tr
-                    (
-                        h::th
-                        (
-                            $lang->report->item,
-                            setClass('chart-label'),
-                            set('colspan', 2),
-                        ),
-                        h::th
-                        (
-                            $lang->report->value,
-                            set('align', 'right'),
-                            set('width', 50),
-                        ),
-                        h::th
-                        (
-                            $lang->report->percent,
-                            set('align', 'right'),
-                            set('width', 60),
-                        ),
-                    )
-                ),
-                h::tbody($trs),
-            )
-        ),
+        div($echarts)
     );
 }
 
-panel
+div
 (
-    setID('chartContainer'),
-    toolbar
+    setClass('flex flex-nowrap'),
+    sidebar
     (
-        set::current($chartType),
-        $chartTypeList,
-        to::after
+        formPanel
         (
-            div
+            setID('chartForm'),
+            set('title', $lang->task->report->select),
+            set('actions', array
             (
-                html($notice),
-                setClass('text-gray')
-            )
-        )
+                array
+                (
+                    'text'  => $lang->selectAll,
+                    'class' => 'btn-select-all space',
+                    'url'   => 'javascript:triggerChecked();'
+                ),
+                array
+                (
+                    'type'  => 'primary',
+                    'text'  => $lang->task->report->create,
+                    'class' => 'btn-select-all space',
+                    'url'   => 'javascript:createChart();'
+                ),
+            )),
+            checkList
+            (
+                set::primary(true),
+                set::name('charts[]'),
+                set::value('code'),
+                set::inline(false),
+                set::items($selectCharts),
+            ),
+        ),
     ),
-    $chartContents
+    cell
+    (
+        setID('chartContainer'),
+        setClass('flex-auto ml-4 bg-white px-4 py-2'),
+        tabs
+        (
+            on::click('.font-medium', 'changeTab'),
+            $chartContents
+        )
+    )
 );
-
-render();
