9d65f47491766ce1676ef14b12a4e06c8f662ecf
qixinzhi <qixinzhi@easycorp.ltd>
2023-08-01 07:52:44 +0000

    * Finish task#99221.

--- module/metric/calc/execution/scale/count_of_invalid_story_in_execution.php
+++ module/metric/calc/execution/scale/count_of_invalid_story_in_execution.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * 按执行统计的无效研发需求数。
- * count_of_invalid_story_in_execution.
+ * Count of invalid story in execution.
  *
  * 范围：execution
  * 对象：story
@@ -9,7 +9,7 @@
  * 度量名称：按执行统计的无效研发需求数
  * 单位：个
  * 描述：按执行统计的无效研发需求数是指被判定为无效的研发需求数量。无效需求可能包括重复需求、不可实现的需求、或者与项目策略和目标不符的需求。通过对无效需求的统计，可以帮助执行团队优化需求管理和筛选机制，以提高需求有效性和资源利用率。较高的无效需求数量可能需要对需求收集和评估流程进行改进。
- * 定义：执行中研发需求的个数求和;关闭原因为重复、不做、设计如此和已取消;过滤已删除的研发需求;过滤已删除的执行;过滤已删除的项目
+ * 定义：执行中研发需求的个数求和;关闭原因为重复、不做、设计如此和已取消;过滤已删除的研发需求;过滤已删除的执行;过滤已删除的项目;
  * 度量库：
  * 收集方式：realtime
  *
@@ -22,21 +22,22 @@
  */
 class count_of_invalid_story_in_execution extends baseCalc
 {
-    public $dataset = null;
+    public $dataset = 'getDevStoriesWithExecution';
 
-    public $fieldList = array();
+    public $fieldList = array('t3.project', 't1.closedReason');
 
-    //public funtion getStatement($dao)
-    //{
-    //}
+    public $result = array();
 
     public function calculate($row)
     {
+        $project = $row->project;
+        if(!isset($this->result[$project])) $this->result[$project] = 0;
+        if(in_array($row->closedReason, array('duplicate','willnotdo','bydesign'))) $this->result[$project] += 1;
     }
 
     public function getResult($options = array())
     {
-        $records = $this->getRecords(array('value'));
+        $records = $this->getRecords(array('project', 'value'));
         return $this->filterByOptions($records, $options);
     }
 }
