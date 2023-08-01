5da655e3d981cd522e008ed776da1389978dbb27
qixinzhi <qixinzhi@easycorp.ltd>
2023-08-01 06:21:09 +0000

    * Finish task#99186.

--- module/metric/calc/project/scale/count_of_valid_story_in_project.php
+++ module/metric/calc/project/scale/count_of_valid_story_in_project.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * 按项目统计的有效研发需求数。
- * count_of_valid_story_in_project.
+ * Count of valid story in project.
  *
  * 范围：project
  * 对象：story
@@ -9,7 +9,7 @@
  * 度量名称：按项目统计的有效研发需求数
  * 单位：个
  * 描述：按项目统计的有效研发需求数是指被确认为有效的研发需求数量。有效需求指的是符合项目策略和目标，可以实施并且对用户有价值的需求。通过对有效需求的统计，可以帮助项目团队评估项目需求的质量和重要性，并进行优先级排序和资源分配。较高的有效需求数量通常表示项目的功能和特性满足了用户和市场的期望，有利于实现项目的成功交付和用户满意度。
- * 定义：复用：;按项目统计的无效研发需求数;按项目统计的研发需求总数;公式：;按执行统计的有效研发需求数=按执行统计的研发需求总数-按执行统计的无效研发需求数
+ * 定义：复用：;按项目统计的无效研发需求数;按项目统计的研发需求总数;公式：;按执行统计的有效研发需求数=按执行统计的研发需求总数-按执行统计的无效研发需求数;
  * 度量库：
  * 收集方式：realtime
  *
@@ -22,21 +22,22 @@
  */
 class count_of_valid_story_in_project extends baseCalc
 {
-    public $dataset = null;
+    public $dataset = 'getDevStoriesWithProject';
 
-    public $fieldList = array();
+    public $fieldList = array('t3.project', 't1.closedReason');
 
-    //public funtion getStatement($dao)
-    //{
-    //}
+    public $result = array();
 
     public function calculate($row)
     {
+        $project = $row->project;
+        if(!isset($this->result[$project])) $this->result[$project] = 0;
+        if(!in_array($row->closedReason, array('duplicate','willnotdo','budesign'))) $this->result[$project] += 1;
     }
 
     public function getResult($options = array())
     {
-        $records = $this->getRecords(array('value'));
+        $records = $this->getRecords(array('project', 'value'));
         return $this->filterByOptions($records, $options);
     }
 }
