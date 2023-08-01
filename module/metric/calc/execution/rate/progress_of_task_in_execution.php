7a9d86bba2628c33661817791a1f222e0b25b2b8
zhouxin <zhouxin@easycorp.ltd>
2023-08-01 14:11:49 +0800

    * Finish task #99214.

--- module/metric/calc/execution/rate/progress_of_task_in_execution.php
+++ module/metric/calc/execution/rate/progress_of_task_in_execution.php
@@ -14,7 +14,7 @@
  * 收集方式：realtime
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
- * @author    qixinzhi <qixinzhi@easycorp.ltd>
+ * @author    zhouxin <zhouxin@easycorp.ltd>
  * @package
  * @uses      func
  * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
@@ -22,21 +22,29 @@
  */
 class progress_of_task_in_execution extends baseCalc
 {
-    public $dataset = null;
+    public $dataset = 'getTasks';
 
-    public $fieldList = array();
-
-    //public funtion getStatement($dao)
-    //{
-    //}
+    public $fieldList = array('t1.consumed', 't1.left', 't1.execution');
 
     public function calculate($row)
     {
+        $consumed = !empty($row->consumed) ? $row->consumed : 0;
+        $left     = !empty($row->left)     ? $row->left : 0;
+
+        if(!isset($this->result[$row->execution])) $this->result[$row->execution] = array('consumed' => 0, 'left' => 0);
+        $this->result[$row->execution]['consumed'] += $consumed;
+        $this->result[$row->execution]['left']     += $left;
     }
 
     public function getResult($options = array())
     {
-        $records = $this->getRecords(array('value'));
+        $records = array();
+        foreach($this->result as $execution => $taskInfo)
+        {
+            $total = $taskInfo['consumed'] + $taskInfo['left'];
+            $progress = $total ? round($taskInfo['consumed'] / $total, 4) : 0;
+            $records[] = array('execution' => $execution, 'value' => $progress);
+        }
         return $this->filterByOptions($records, $options);
     }
 }
