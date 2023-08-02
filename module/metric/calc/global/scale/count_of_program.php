c02023f4b6c5f788f2661437fe3f62266f1dc362
zhouxin <zhouxin@easycorp.ltd>
2023-08-02 14:10:25 +0800

    * Add unit test for metric.

--- module/metric/calc/global/scale/count_of_program.php
+++ module/metric/calc/global/scale/count_of_program.php
@@ -35,7 +35,7 @@ class count_of_program extends baseCalc
 
     public function getResult($options = array())
     {
-        $records = array(array('value' => $this->result));
+        $records = $this->getRecords($options);
         return $this->filterByOptions($records, $options);
     }
 }
