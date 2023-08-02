7e04efcf9b049d47b49052f745811029aaa8df6d
qixinzhi <qixinzhi@easycorp.ltd>
2023-08-02 08:12:52 +0000

    * Finish task#99202.

--- module/metric/calc/project/hour/ac_of_all_in_waterfall.php
+++ module/metric/calc/project/hour/ac_of_all_in_waterfall.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * 按瀑布项目统计的实际花费工时(AC)。
- * ac_of_all_in_waterfall.
+ * Ac of all in waterfall.
  *
  * 范围：project
  * 对象：effort
@@ -9,7 +9,7 @@
  * 度量名称：按瀑布项目统计的实际花费工时(AC)
  * 单位：h
  * 描述：按瀑布项目统计的实际花费工时指的是在瀑布项目管理方法中，实际花费的工时总数。这个度量项用于评估实际工作量和预计工作量之间的差异，有助于估计项目的真实进展情况。AC的值越接近EV，代表项目团队在任务执行方面表现得越好。
- * 定义：瀑布项目中所有日志记录的工时之和;过滤已删除的项目
+ * 定义：瀑布项目中所有日志记录的工时之和;过滤已删除的项目;
  * 度量库：
  * 收集方式：realtime
  *
@@ -22,21 +22,32 @@
  */
 class ac_of_all_in_waterfall extends baseCalc
 {
-    public $dataset = null;
+    public $result = array();
 
-    public $fieldList = array();
-
-    //public funtion getStatement($dao)
-    //{
-    //}
+    public function getStatement()
+    {
+        return $this->dao->select('t3.id as project, SUM(t1.consumed) as ac')
+            ->from(TABLE_EFFORT)->alias('t1')
+            ->leftJoin(TABLE_PROJECT)->alias('t2')->on('t1.execution=t2.id')
+            ->leftJoin(TABLE_PROJECT)->alias('t3')->on('t2.project=t3.id')
+            ->where('t3.deleted')->eq('0')
+            ->andWhere('t3.type')->eq('project')
+            ->andWhere('t3.model')->eq('waterfall')
+            ->groupBy('t3.id')
+            ->query();
+    }
 
     public function calculate($row)
     {
+        $project = $row->project;
+        $ac      = $row->ac;
+
+        if(!isset($this->result[$project])) $this->result[$project] = $ac;
     }
 
     public function getResult($options = array())
     {
-        $records = $this->getRecords(array('value'));
+        $records = $this->getRecords(array('project', 'value'));
         return $this->filterByOptions($records, $options);
     }
 }
