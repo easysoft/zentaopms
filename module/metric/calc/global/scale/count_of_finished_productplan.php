ecec146b641a54ff341358877168c6eed0521991
zhouxin <zhouxin@easycorp.ltd>
2023-08-02 15:23:54 +0800

    * Change definition of metric: count_of_finished_productplan.

--- module/metric/calc/global/scale/count_of_finished_productplan.php
+++ module/metric/calc/global/scale/count_of_finished_productplan.php
@@ -9,7 +9,7 @@
  * 度量名称：按全局统计的已完成计划数
  * 单位：个
  * 描述：按全局统计的已完成计划数表示已经完成的计划数量。该度量项反映了组织中已经完成的计划数量，可以用于评估组织的计划执行能力和绩效。
- * 定义：所有计划的个数求和;状态为已完成;过滤已删除的计划;
+ * 定义：所有计划的个数求和;状态为已完成或状态为已关闭且关闭原因为已完成;过滤已删除的计划;
  * 度量库：
  * 收集方式：realtime
  *
@@ -24,13 +24,13 @@ class count_of_finished_productplan extends baseCalc
 {
     public $dataset = 'getPlans';
 
-    public $fieldList = array('t1.status');
+    public $fieldList = array('t1.status', 't1.closedReason');
 
     public $result = 0;
 
     public function calculate($row)
     {
-        if($row->status == 'done') $this->result ++;
+        if($row->status == 'done' || ($row->status == 'closed' && $row->closedReason == 'done')) $this->result ++;
     }
 
     public function getResult($options = array())
