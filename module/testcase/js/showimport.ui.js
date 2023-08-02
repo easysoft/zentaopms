ab1d845c67499202431347a3dcc2b9bd9d11c264
wangyuting <wangyuting@easycorp.ltd>
2023-08-02 16:55:39 +0800

    * Finish testcase showimport zin page.

---
+++ module/testcase/js/showimport.ui.js
@@ -0,0 +1,26 @@
+window.handleRenderRow = function($row, index, row)
+{
+    let stepDesc   = "<input class='hidden' type='text' name='product[" + row.id + "]' value='" + productID + "'/>";
+    let stepExpect = '';
+
+    const descs  = stepData[index + 1]['desc'];
+    const expect = stepData[index + 1]['expect'];
+    if(descs.length)
+    {
+        $.each(descs, function(id, desc)
+        {
+            if(!desc.content) return;
+            stepDesc   += "<div class='flex col'><div class='cell flex border p-2'><div class='cell center'><input class='hidden' type='text' name='stepType[" + row.id + "][" + desc.number + "]' value='" + desc.type + "' /><span>" + desc.number + "、</span></div><div class='cell center flex-1'><textarea class='form-control form-batch-input' rows='10' name='desc[" + row.id + "][" + desc.number + "]' style='min-height: 32px; height:2rem;'>" + desc.content + "</textarea></div></div></div>";
+            stepExpect += "<div class='flex col'><div class='cell flex border p-2'><textarea class='form-control form-batch-input " + (desc.type != 'group' ? '' : 'disabled') + "'" + (desc.type != 'group' ? '' : 'readonly=readonly') + "' rows='10' name='expect[" + row.id + "][" + desc.number + "]' style='min-height: 32px; height:2rem;'>" + (expect[id]['content'] ? expect[id]['content'] : '') + "</textarea></div></div></div>";
+        })
+    }
+    else
+    {
+        stepDesc   += "<div class='flex col'><div class='cell flex border p-2'><div class='cell center'><input class='hidden' type='text' name='stepType[" + row.id + "][1]' value='step' /><span>1、</span></div><div class='cell center flex-1'><textarea class='form-control form-batch-input' rows='10' name='desc[" + row.id + "][1]' style='min-height: 32px; height:2rem;'></textarea></div></div></div>";
+        stepExpect += "<div class='flex col'><div class='cell flex border p-2'><textarea class='form-control form-batch-input' rows='10' name='expect[" + row.id + "][1]'  style='min-height: 32px; height:2rem;'></textarea></div></div>";
+    }
+
+
+    $row.find('td[data-name=stepDesc]').html(stepDesc);
+    $row.find('td[data-name=stepExpect]').html(stepExpect);
+}
---
+++ module/testcase/ui/showimport.html.php
@@ -0,0 +1,128 @@
+<?php
+declare(strict_types=1);
+/**
+ * The showimport view file of testcase module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Yuting Wang <wangyuting@easycorp.ltd>
+ * @package     testcase
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+jsVar('stepData', $stepData);
+jsVar('productID', $productID);
+
+$requiredFields = $config->testcase->create->requiredFields;
+$items[] = array
+(
+    'name'  => 'id',
+    'label' => $lang->idAB,
+    'control' => 'index',
+    'width' => '32px'
+);
+
+$items[] = array
+(
+    'name'  => 'title',
+    'label' => $lang->testcase->title,
+    'width' => '240px',
+    'required' => strpos(",$requiredFields,", ',title,') !== false
+);
+
+$caseModules = ($branchID and isset($modules[$branchID])) ? $modules[BRANCH_MAIN] + $modules[$branchID] : $modules[BRANCH_MAIN];
+$items[] = array
+(
+    'name'    => 'module',
+    'label'   => $lang->testcase->module,
+    'control' => 'picker',
+    'items'   => $caseModules,
+    'width'   => '200px',
+    'required' => strpos(",$requiredFields,", ',module,') !== false
+);
+
+$storyID = isset($case->story) ? $case->story : ((!empty($case->id) and isset($cases[$case->id])) ? $cases[$case->id]->story : '');
+$items[] = array
+(
+    'name'    => 'story',
+    'label'   => $lang->testcase->story,
+    'control' => 'picker',
+    'items'   => array($storyID => zget($stories, $storyID, '')),
+    'width'   => '240px',
+    'required' => strpos(",$requiredFields,", ',story,') !== false
+);
+
+$items[] = array
+(
+    'name'    => 'type',
+    'label'   => $lang->testcase->type,
+    'control' => 'picker',
+    'items'   => $lang->testcase->typeList,
+    'width'   => '160px',
+    'required' => strpos(",$requiredFields,", ',type,') !== false
+);
+
+$items[] = array
+(
+    'name'    => 'pri',
+    'label'   => $lang->testcase->pri,
+    'control' => 'pripicker',
+    'items'   => $lang->testcase->priList,
+    'width'   => '80px',
+    'required' => strpos(",$requiredFields,", ',pri,') !== false
+);
+
+$items[] = array
+(
+    'name'    => 'precondition',
+    'label'   => $lang->testcase->precondition,
+    'control' => 'textarea',
+    'width'   => '240px',
+    'required' => strpos(",$requiredFields,", ',precondition,') !== false
+);
+
+$items[] = array
+(
+    'name'  => 'keywords',
+    'label' => $lang->testcase->keywords,
+    'width' => '240px',
+    'required' => strpos(",$requiredFields,", ',keywords,') !== false
+);
+
+$items[] = array
+(
+    'name'    => 'stage',
+    'label'   => $lang->testcase->stage,
+    'control' => 'picker',
+    'multiple' => true,
+    'items'   => $lang->testcase->stageList,
+    'width'   => '240px',
+    'required' => strpos(",$requiredFields,", ',stage,') !== false
+);
+
+$items[] = array
+(
+    'name'  => 'stepDesc',
+    'label' => $lang->testcase->stepDesc,
+    'width' => '320px'
+);
+
+$items[] = array
+(
+    'name'  => 'stepExpect',
+    'label' => $lang->testcase->stepExpect,
+    'width' => '320px'
+);
+
+formBatchPanel
+(
+    set::title($lang->testcase->import),
+    set::items($items),
+    set::data(array_values($caseData)),
+    set::mode('edit'),
+    set::actionsText(false),
+    set::onRenderRow(jsRaw('handleRenderRow')),
+    input(set::class('hidden'), set::name('isEndPage'), set::value($isEndPage ? '1' : '0')),
+    input(set::class('hidden'), set::name('pagerID'), set::value($pagerID)),
+);
+
+render();
