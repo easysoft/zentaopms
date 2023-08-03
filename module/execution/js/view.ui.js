18ae4f93e235db9e3ba77546d290f8bd512002d0
caoyanyi <caoyanyi@easycorp.ltd>
2023-08-03 08:40:43 +0800

    - Remove unused code.

--- module/execution/js/view.ui.js
+++ module/execution/js/view.ui.js
@@ -1,23 +1,3 @@
-$(function()
-{
-    setTimeout(function()
-    {
-        blocks.forEach(block =>
-        {
-            if(block.domID)
-            {
-                block.content = {html: $(`#${block.domID}`).html()};
-                $(`#${block.domID}`).remove();
-                delete(block.domID);
-            }
-        });
-
-        const $dashboard = $('#executionDashBoard').data('zui.Dashboard')
-        $('#executionDashBoard .dashboard-block').attr('draggable', false);
-        $dashboard.render({blocks: blocks});
-    }, 10);
-});
-
 /**
  * 提示并删除执行。
  * Delete execution with tips.
