99854edd55430ee97bac1099000564f615dc7610
liumengyi <liumengyi@easycorp.ltd>
2023-08-03 10:02:18 +0800

    * Refactor ui of upgrade::license.

---
+++ module/upgrade/ui/license.html.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+/**
+ * The license view file of upgrade module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Mengyi Liu <liumengyi@easycorp.ltd>
+ * @package     upgrade
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+
+set::zui(true);
+
+jsVar('confirmLink', inlink('license', 'agree=true'));
+
+div
+(
+    set::id('main'),
+    div
+    (
+        set::id('mainContent'),
+        setClass('px-1 mt-2 mx-auto'),
+        width('1200px'),
+        panel
+        (
+            setClass('py-8 px-2'),
+            set::title($lang->upgrade->license),
+            set::titleClass('text-xl'),
+            set::actions(array()),
+            textarea
+            (
+                $license
+            ),
+            cell
+            (
+                setClass('mt-2'),
+                checkbox
+                (
+                    on::change('agreeChange'),
+                    set::checked(true),
+                    html($lang->agreement),
+                ),
+            ),
+            cell
+            (
+                setClass('text-center mt-6'),
+                btn
+                (
+                    setClass('px-8 btn-install'),
+                    set::url(inlink('license', 'agree=true')),
+                    set::type('primary'),
+                    $lang->confirm,
+                ),
+            ),
+        ),
+    ),
+);
+
+render('pagebase');
