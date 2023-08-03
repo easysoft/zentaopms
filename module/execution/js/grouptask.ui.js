43e1e16a59e549a6d811dbaadfa7c33a8d541a79
sunguangming <sunguangming@easycorp.ltd>
2023-08-03 11:01:44 +0800

    * Add toolbar and feature bat to grouptask.

--- module/execution/js/grouptask.ui.js
+++ module/execution/js/grouptask.ui.js
@@ -13,4 +13,22 @@ $(function()
         $('#tasksTable').find('tr[data-id="' + groupIndex + '"]').removeClass('hidden');
         $('#tasksTable').find('tr.group-summary[data-id="' + groupIndex + '"]').addClass('hidden');
     })
+
+    $('#featureBar').on('click', '.group-collapse-all', function()
+    {
+        $(this).closest('.nav-item').addClass('hidden');
+        $('.group-expand-all').closest('.nav-item').removeClass('hidden');
+
+        $('#tasksTable').find('tbody tr').addClass('hidden');
+        $('#tasksTable').find('tbody tr.group-summary').removeClass('hidden');
+    })
+
+    $('#featureBar').on('click', '.group-expand-all', function()
+    {
+        $(this).closest('.nav-item').addClass('hidden');
+        $('.group-collapse-all').closest('.nav-item').removeClass('hidden');
+
+        $('#tasksTable').find('tbody tr').removeClass('hidden');
+        $('#tasksTable').find('tbody tr.group-summary').addClass('hidden');
+    })
 })
