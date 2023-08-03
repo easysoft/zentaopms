b3b9e4c1a4876fa908cc56dbee400708ce53d547
qixinzhi <qixinzhi@easycorp.ltd>
2023-08-03 06:21:26 +0000

    * Finish task#99597.

--- module/metric/test/calc/project/scale/scale_of_annual_finished_story_in_project.php
+++ module/metric/test/calc/project/scale/scale_of_annual_finished_story_in_project.php
@@ -13,7 +13,7 @@ $calc   = $metric->calcMetric(__FILE__);
 
 /**
 
-title=count_of_annual_finished_story_in_project
+title=scale_of_annual_finished_story_in_project
 cid=1
 pid=1
 
@@ -21,4 +21,4 @@ pid=1
 
 r(count($calc->getResult())) && p('') && e('18'); // 测试分组数。
 
-r($calc->getResult(array('project' => '9', 'year' => '2018'))) && p('0:value') && e('3');  // 测试项目2。
+r($calc->getResult(array('project' => '9', 'year' => '2018'))) && p('0:value') && e('15');  // 测试项目2。
