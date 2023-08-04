c9c26ca116641377a3161fc642c158f677fdde29
zhouxin <zhouxin@easycorp.ltd>
2023-08-04 13:40:09 +0800

    * Finish task #99608.

--- module/metric/calc/user/scale/count_of_assigned_bug_in_user.php
+++ module/metric/calc/user/scale/count_of_assigned_bug_in_user.php
@@ -14,7 +14,7 @@
  * 收集方式：realtime
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
- * @author    qixinzhi <qixinzhi@easycorp.ltd>
+ * @author    zhouxin <zhouxin@easycorp.ltd>
  * @package
  * @uses      func
  * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
@@ -22,21 +22,24 @@
  */
 class count_of_assigned_bug_in_user extends baseCalc
 {
-    public $dataset = null;
+    public $dataset = 'getProjectBugs';
 
-    public $fieldList = array();
-
-    //public funtion getStatement($dao)
-    //{
-    //}
+    public $fieldList = array('t1.assignedTo');
 
     public function calculate($row)
     {
+        $assignedTo = $row->assignedTo;
+
+        if(empty($assignedTo) || $assignedTo == 'closed') return false;
+
+        if(!isset($this->result[$assignedTo])) $this->result[$assignedTo] = 0;
+        $this->result[$assignedTo] += 1;
+
     }
 
     public function getResult($options = array())
     {
-        $records = $this->getRecords(array('value'));
+        $records = $this->getRecords(array('user', 'value'));
         return $this->filterByOptions($records, $options);
     }
 }
