240103312cca0a5892265625a3776b89214889dd
dingguodong <dingguodong@easycorp.ltd>
2023-08-07 13:39:34 +0800

    * Replace zero date to null.

--- module/cron/model.php
+++ module/cron/model.php
@@ -142,7 +142,7 @@ class cronModel extends model
     public function getLastTime()
     {
         $cron = $this->dao->select('*')->from(TABLE_CRON)->orderBy('lastTime desc')->limit(1)->fetch();
-        return isset($cron->lastTime) ? $cron->lastTime : '0000-00-00 00:00:00';
+        return isset($cron->lastTime) ? $cron->lastTime : null;
     }
 
     /**
