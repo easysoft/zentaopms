ec8f7a79a77dffb3c9e2d0dc8482f1bee6851ee1
qixinzhi <qixinzhi@easycorp.ltd>
2023-08-08 06:51:43 +0000

    * Finish task#97123.

--- module/metric/calc/product/scale/scale_of_annual_delivered_story_in_product.php
+++ module/metric/calc/product/scale/scale_of_annual_delivered_story_in_product.php
@@ -22,15 +22,44 @@
  */
 class scale_of_annual_delivered_story_in_product extends baseCalc
 {
-    public $dataset = '';
+    public $dataset = 'getDevStories';
 
-    public $fieldList = array();
+    public $fieldList = array('t1.product', 't1.stage', 't1.releasedDate', 't1.closedReason', 't1.closedDate', 't1.estimate');
 
     public function calculate($row)
     {
+        $product      = $row->product;
+        $stage        = $row->stage;
+        $releasedDate = $row->releasedDate;
+        $closedReason = $row->closedReason;
+        $closedDate   = $row->closedDate;
+        $estimate     = $row->estimate;
+
+        $year = null;
+        if($stage == 'released')
+        {
+            if(empty($releasedDate)) return false;
+            $year = substr($releasedDate, 0, 4);
+            if($year == '0000') return false;
+        }
+
+        if($closedReason == 'done')
+        {
+            if(empty($closedDate)) return false;
+            $year = substr($closedDate, 0, 4);
+            if($year == '0000') return false;
+        }
+
+        if(empty($year)) return false;
+
+        if(!isset($this->result[$product])) $this->result[$product] = array();
+        if(!isset($this->result[$product][$year])) $this->result[$product][$year] = 0;
+        $this->result[$product][$year] += $estimate;
     }
 
     public function getResult($options = array())
     {
+        $records = $this->getRecords(array('product', 'year', 'value'));
+        return $this->filterByOptions($records, $options);
     }
 }
