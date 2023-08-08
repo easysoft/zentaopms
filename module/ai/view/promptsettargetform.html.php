c1ccdc11ae5c6f4635a9e9a493cc18e7eceafcf8
Chenjianyu <chenjianyu@@easycorp.ltd>
2023-08-08 09:09:55 +0800

    * fix the target form always show the scroll bar.

--- module/ai/view/promptsettargetform.html.php
+++ module/ai/view/promptsettargetform.html.php
@@ -30,7 +30,7 @@
   #prompt-preview .prompt-text-part + .prompt-text-part {margin-top: 4px;}
   #prompt-previewer {font-weight: bold;}
   #form-selector .header > * {display: inline-block;}
-  #form-selector .content {margin: 6px 0; height: calc(100% - 48px); overflow-y: auto; border: 1px solid #ccc; border-left: none;}
+  #form-selector .content {margin: 6px 0; max-height: calc(100% - 47px); overflow-y: auto; border: 1px solid #ccc; border-left: none;}
   .target-form-group {display: grid; grid-template-columns: 120px 1fr; grid-gap: 8px; border: 1px solid #ccc;}
   .target-form-group:first-of-type {border-top: none;}
   .target-form-group:last-of-type {border-bottom: none;}
