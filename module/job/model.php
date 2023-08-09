fc6a04e3a1853d280669a3fd2fa7e62717f045ef
zenggang <zenggang@easycorp.ltd>
2023-08-09 02:07:13 +0000

    * Fix bug#37124

--- module/job/model.php
+++ module/job/model.php
@@ -54,7 +54,7 @@ class jobModel extends model
      */
     public function getList($repoID = 0, $orderBy = 'id_desc', $pager = null, $engine = '', $pipeline = '')
     {
-        $jobs = $this->dao->select('t1.*, t2.name as repoName, t3.name as jenkinsName')->from(TABLE_JOB)->alias('t1')
+        return $this->dao->select('t1.*, t2.name as repoName, t3.name as jenkinsName')->from(TABLE_JOB)->alias('t1')
             ->leftJoin(TABLE_REPO)->alias('t2')->on('t1.repo=t2.id')
             ->leftJoin(TABLE_PIPELINE)->alias('t3')->on('t1.server=t3.id')
             ->where('t1.deleted')->eq('0')
@@ -64,13 +64,6 @@ class jobModel extends model
             ->orderBy($orderBy)
             ->page($pager)
             ->fetchAll('id');
-
-        /* Format datetime. */
-        foreach($jobs as $key => $job)
-        {
-            if(!empty($job->lastExec)) $jobs[$key]->lastExec = substr($job->lastExec, 5, 11);
-        }
-        return $jobs;
     }
 
      /**
