667f21e944869240d204d48d55afd675b35eabac
zenggang <zenggang@easycorp.ltd>
2023-08-10 08:25:25 +0000

    * Fix bug#37129

--- module/job/ui/create.html.php
+++ module/job/ui/create.html.php
@@ -153,7 +153,7 @@ formPanel
         formGroup
         (
             set::label(''),
-            set::name('atDay'),
+            set::name('atDay[]'),
             set::control('checkListInline'),
             set::items($lang->datepicker->dayNames),
         ),
