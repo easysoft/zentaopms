ee9f090a0d485b7f57698c5041e123483911924e
dingguodong <dingguodong@easycorp.ltd>
2023-08-10 09:57:22 +0800

    * Increase the frequency of notification for mail and webhook.

--- db/update18.6.sql
+++ db/update18.6.sql
@@ -39,3 +39,5 @@ ADD INDEX `status`(`status` ASC);
 
 CREATE INDEX deleted ON zt_bug (deleted);
 CREATE INDEX product_status_deleted ON zt_bug (product,status,deleted);
+
+UPDATE `zt_cron` SET `m` = '*/1' WHERE `command` in ('moduleName=mail&methodName=asyncSend', 'moduleName=webhook&methodName=asyncSend') and `type` = 'zentao';
