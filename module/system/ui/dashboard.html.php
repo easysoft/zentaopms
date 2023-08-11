612239308310e4b5149a32194f563394353e0f6e
zhaoke <zhaoke@easycorp.ltd>
2023-08-11 11:05:37 +0800

    * Add dashboard page of system.

---
+++ module/system/ui/dashboard.html.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+/**
+ * The dashboard view file of system module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Ke Zhao<zhaoke@easycorp.ltd>
+ * @package     system
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+
+div(1111);
