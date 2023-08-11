c06002a43984861a5ec6a910f5fcaccbe346611f
sunhao <sunhao@easycorp.ltd>
2023-08-11 10:15:39 +0800

    * zin: fix search form not work in modal.

--- module/search/ui/buildzinform.html.php
+++ module/search/ui/buildzinform.html.php
@@ -1,7 +1,8 @@
 <?php
 namespace zin;
 
-$opts = $this->search->buildSearchFormOptions($module, $fieldParams, $fields, $queries, $actionURL);
+$formName = empty($formName) ? '#searchFormPanel[data-module="' . $module . '"]' : $formName;
+$opts     = $this->search->buildSearchFormOptions($module, $fieldParams, $fields, $queries, $actionURL);
 
 $opts->groupName       = array($lang->search->group1, $lang->search->group2);
 $opts->savedQueryTitle = $lang->search->savedQuery;
@@ -9,11 +10,12 @@ $opts->formSession     = $formSession;
 $opts->module          = $module;
 $opts->actionURL       = $actionURL;
 $opts->groupItems      = $groupItems;
-$opts->onSubmit        = jsRaw('window.onSubmit');
+$opts->submitText      = $lang->search->common;
+$opts->resetText       = $lang->search->reset;
+$opts->onSubmit        = jsRaw("window.onSearchFormResult.bind(null, '$formName')");
 
 if(empty($opts->savedQuery)) unset($opts->savedQuery);
 
-$formName = empty($formName) ? '#searchFormPanel[data-module="' . $module . '"]' : $formName;
 zui::searchform(set((array)$opts), set::_to($formName), set::className('shadow'));
 
 jsVar('options',          isset($options) ? $options : null);
@@ -23,4 +25,4 @@ jsVar('onMenuBar',        $onMenuBar);
 
 js($pageJS);
 
-render();
+render('fragment');
