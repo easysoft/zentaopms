00f1905293225d208c99f26dde156c834eeb8778
wangyidong <wangyidong@cnezsoft.com>
2023-08-14 10:14:08 +0800

    * Rewrite for transfer export and exporttemplate page.

---
+++ module/transfer/ui/export.html.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+/**
+ * The export view file of transfer module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Wang Yidong <yidong@easycorp.ltd>
+ * @package     transfer
+ * @link        https://www.zentao.net
+ */
+
+include '../../file/ui/export.html.php';
---
+++ module/transfer/ui/exporttemplate.html.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+/**
+ * The export view file of transfer module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Wang Yidong <yidong@easycorp.ltd>
+ * @package     transfer
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+
+$this->app->loadLang('file');
+set::title($lang->transfer->exportTemplate);
+
+form
+(
+    set::target('_self'),
+    on::submit('setDownloading'),
+    formGroup
+    (
+        set::label($lang->transfer->num),
+        set::name('num'),
+        set::type('number'),
+        set::value(10),
+    ),
+    formGroup
+    (
+        set::label($lang->file->extension),
+        set::name('fileType'),
+        set::value('xlsx'),
+        set::control('picker'),
+        set::items(array('xlsx' => 'xlsx', 'xls' => 'xls')),
+    ),
+    set::actions(array('submit')),
+);
+
+js
+(
+    <<<JAVASCRIPT
+    function setDownloading()
+    {
+        if(navigator.userAgent.toLowerCase().indexOf("opera") > -1) return true; // Opera don't support, omit it.
+
+        $.cookie.set('downloading', 0);
+
+        time = setInterval(function()
+        {
+            if($.cookie.get('downloading') == 1)
+            {
+                $('.modal .modal-actions .close')[0].click();
+
+                $.cookie.set('downloading', null);
+
+                clearInterval(time);
+            }
+        }, 300);
+        return true;
+    }
+    JAVASCRIPT
+);
+
+render('modalDialog');
