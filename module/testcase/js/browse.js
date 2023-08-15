487fbaeda4b32444e40a39e51bb36f8413020001
liugang <liugang@easycorp.ltd>
2023-08-15 16:49:46 +0800

    * testcase: add function to filter the scene id list when export.

--- module/testcase/js/browse.js
+++ module/testcase/js/browse.js
@@ -1,3 +1,12 @@
+window.setCheckedCookie = function() {
+    var checkeds = [];
+    var $checkboxes = $('#mainContent .main-table tbody>tr input[type="checkbox"][name^=caseIDList]:checked');
+    $checkboxes.each(function() {
+        checkeds.push($(this).val());
+    });
+    $.cookie('checkedItem', checkeds.join(','), {expires: config.cookieLife, path: config.webRoot});
+};
+
 /**
  * Confirm batch delete cases.
  *
