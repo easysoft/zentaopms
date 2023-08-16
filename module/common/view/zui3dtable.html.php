f0e07abf50a75b6cb87fd121795d10dc7132c68b
sunhao <sunhao@easycorp.ltd>
2023-08-16 09:06:52 +0800

    * fix DTable undefined.

--- module/common/view/zui3dtable.html.php
+++ module/common/view/zui3dtable.html.php
@@ -5,4 +5,4 @@ css::import($jsRoot . 'dtable/min.css');
 <style>
 .dtable {box-shadow: 0 1px 1px rgba(0,0,0,.05), 0 2px 6px 0 rgba(0,0,0,.045)}
 </style>
-<script>DTable.defineFn();</script>
+<script>zui.DTable.defineFn();</script>
