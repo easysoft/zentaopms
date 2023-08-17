4ef1ebef7e4475db91eae5b574a1208d0aa5f62e
wangyidong <wangyidong@cnezsoft.com>
2023-08-17 16:42:16 +0800

    * Code for task #99437.

---
+++ module/story/ui/sendmail.html.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The mail file of story module of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2015 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.cnezsoft.com)
+ * @license     ZPL(http://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
+ * @package     bug
+ * @version     $Id: sendmail.html.php 4129 2013-01-18 01:58:14Z wwccss $
+ * @link        http://www.zentao.net
+ */
+?>
+<?php $mailTitle = 'STORY #' . $object->id . ' ' . $object->title;?>
+<?php include $this->app->getModuleRoot() . 'common/view/mail.header.html.php';?>
+<tr>
+  <td>
+    <table cellpadding='0' cellspacing='0' width='600' style='border: none; border-collapse: collapse;'>
+      <tr>
+        <td style='padding: 10px; background-color: #F8FAFE; border: none; font-size: 14px; font-weight: 500; border-bottom: 1px solid #e5e5e5;'>
+          <?php $color = empty($object->color) ? '#333' : $object->color;?>
+          <?php echo html::a($domain . helper::createLink('story', 'view', "storyID=$object->id", 'html'), $mailTitle, '', "style='color: {$color}; text-decoration: underline;'");?>
+        </td>
+      </tr>
+    </table>
+  </td>
+</tr>
+<tr>
+  <td style='padding: 10px; border: none;'>
+    <fieldset style='border: 1px solid #e5e5e5'>
+      <legend style='color: #114f8e'><?php echo $this->lang->story->legendSpec;?></legend>
+      <div style='padding:5px;'><?php echo $object->spec;?></div>
+    </fieldset>
+  </td>
+</tr>
+<?php include $this->app->getModuleRoot() . 'common/view/mail.footer.html.php';?>
