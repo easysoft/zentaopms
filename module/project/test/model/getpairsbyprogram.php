5d5dca31bdfade353542eb83767a8471299f1b14
caoyanyi <caoyanyi@easycorp.ltd>
2023-08-18 09:56:21 +0800

    * Change unit test check value.

--- module/project/test/model/getpairsbyprogram.php
+++ module/project/test/model/getpairsbyprogram.php
@@ -10,20 +10,14 @@ title=测试 projectModel->getPairsByProgram();
 cid=1
 pid=1
 
-查找管理员可查看的所有项目数量 >> 4
-查找独立项目数量 >> 0
-查找管理员可查看的所属项目集ID为1的项目数量 >> 4
-查找管理员可查看的所属项目集ID为1且状态为wait的项目数量 >> 1
-查找管理员可查看的所属项目集ID为1且状态不为closed的项目数量 >> 4
-
 */
 
 global $tester;
 $tester->loadModel('user')->updateProjectView(range(1, 50), array('admin'));
 $tester->loadModel('project');
 
-r(count($tester->project->getPairsByProgram(10, 'all', true))) && p() && e('4'); // 查找管理员可查看的所有项目数量
-r(count($tester->project->getPairsByProgram()))                && p() && e('0'); // 查找独立项目数量
-r(count($tester->project->getPairsByProgram(1)))               && p() && e('4'); // 查找管理员可查看的所属项目集ID为1的项目数量
-r(count($tester->project->getPairsByProgram(1, 'wait')))       && p() && e('1'); // 查找管理员可查看的所属项目集ID为1且状态为wait的项目数量
-r(count($tester->project->getPairsByProgram(1, 'noclosed')))   && p() && e('4'); // 查找管理员可查看的所属项目集ID为1且状态不为closed的项目数量
+r(count($tester->project->getPairsByProgram(10, 'all', true))) && p() && e('4');  // 查找管理员可查看的所有项目数量
+r(count($tester->project->getPairsByProgram()))                && p() && e('40'); // 查找独立项目数量
+r(count($tester->project->getPairsByProgram(1)))               && p() && e('4');  // 查找管理员可查看的所属项目集ID为1的项目数量
+r(count($tester->project->getPairsByProgram(1, 'wait')))       && p() && e('1');  // 查找管理员可查看的所属项目集ID为1且状态为wait的项目数量
+r(count($tester->project->getPairsByProgram(1, 'noclosed')))   && p() && e('4');  // 查找管理员可查看的所属项目集ID为1且状态不为closed的项目数量
