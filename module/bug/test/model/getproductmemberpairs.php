5b6861426b2309974f636b9878543fd6d00a542c
liumengyi <liumengyi@easycorp.ltd>
2023-08-21 16:17:06 +0800

    * Modify unit test result.

--- module/bug/test/model/getproductmemberpairs.php
+++ module/bug/test/model/getproductmemberpairs.php
@@ -21,17 +21,23 @@ title=bugModel->getProductMemberPairs();
 timeout=0
 cid=1
 
-- 测试获取productID为1的bug的团队成员 @A:admin,U:用户32
+- 测试获取productID为1的bug的团队成员 @A:admin,U:用户12,U:用户22,U:用户32
 
-- 测试获取productID为2的bug的团队成员 @U:用户23
 
-- 测试获取productID为3的bug的团队成员 @U:用户14,U:用户44
+- 测试获取productID为2的bug的团队成员 @U:用户3,U:用户13,U:用户23,U:用户33
 
-- 测试获取productID为4的bug的团队成员 @U:用户5,U:用户35
 
-- 测试获取productID为5的bug的团队成员 @U:用户26
+- 测试获取productID为3的bug的团队成员 @U:用户4,U:用户14,U:用户24,U:用户34
 
-- 测试获取productID为6的bug的团队成员 @U:用户17,U:用户47
+
+- 测试获取productID为4的bug的团队成员 @U:用户5,U:用户15,U:用户25,U:用户35
+
+
+- 测试获取productID为5的bug的团队成员 @U:用户6,U:用户16,U:用户26,U:用户36
+
+
+- 测试获取productID为6的bug的团队成员 @U:用户7,U:用户17,U:用户27,U:用户37
+
 
 - 测试获取不存在的product的bug的团队成员 @0
 
