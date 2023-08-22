b45b7015e2078aa4effbddea8047d84782518340
qixinzhi <qixinzhi@easycorp.ltd>
2023-08-22 08:09:08 +0000

    * Fix actionList definition.

--- module/metric/config.php
+++ module/metric/config.php
@@ -2,3 +2,16 @@
 $config->metric = new stdclass();
 $config->metric->scopeList = array('global', 'program', 'project', 'execution', 'product', 'user', 'dept');
 $config->metric->purposeList = array('scale', 'qc', 'hour', 'cost', 'rate', 'time');
+
+global $lang;
+$config->metric->actionList = array();
+$config->metric->actionList['edit']['icon'] = 'edit';
+$config->metric->actionList['edit']['text'] = $lang->edit;
+$config->metric->actionList['edit']['hint'] = $lang->edit;
+$config->metric->actionList['edit']['url']  = array('module' => 'metric', 'method' => 'edit', 'params' => "metricID={id}");
+
+$config->metric->actionList['delete']['icon']         = 'trash';
+$config->metric->actionList['delete']['hint']         = $lang->delete;
+$config->metric->actionList['delete']['url']          = helper::createLink('metric', 'delete', 'metricID={id}');
+$config->metric->actionList['delete']['class']        = 'ajax-submit';
+$config->metric->actionList['delete']['data-confirm'] = $lang->metric->confirmDelete;
