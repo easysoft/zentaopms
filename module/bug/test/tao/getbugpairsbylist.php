cd354fbdfeac7304d36d8b930a2234bfc812d6fc
liumengyi <liumengyi@easycorp.ltd>
2023-08-22 13:30:57 +0800

    * Optimize code.

--- module/bug/test/tao/getbugpairsbylist.php
+++ module/bug/test/tao/getbugpairsbylist.php
@@ -19,8 +19,8 @@ $bugIdList  = array('1,2,3', '1,4', '2,7', '2,6,9', '1000001');
 global $tester;
 $tester->loadModel('bug');
 
-r(implode(',', $tester->bug->getBugPairsByList($bugIdList[0]))) && p()  && e('测试单转Bug1,SonarQube_Bug2,测试单转Bug3'); // 获取ID等于1的bug
-r(implode(',', $tester->bug->getBugPairsByList($bugIdList[1]))) && p()  && e('测试单转Bug1,SonarQube_Bug4'); // 获取ID等于2的bug
-r(implode(',', $tester->bug->getBugPairsByList($bugIdList[2]))) && p()  && e('SonarQube_Bug2,测试单转Bug7'); // 获取ID等于3的bug
-r(implode(',', $tester->bug->getBugPairsByList($bugIdList[3]))) && p()  && e('SonarQube_Bug2,SonarQube_Bug6,测试单转Bug9'); // 获取ID等于4的bug
-r(implode(',', $tester->bug->getBugPairsByList($bugIdList[4]))) && p()  && e('0'); // 获取ID不存在的bug
+r(implode(',', $tester->bug->getBugPairsByList($bugIdList[0]))) && p()  && e('测试单转Bug1,SonarQube_Bug2,测试单转Bug3');   // 获取 ID 等于 1 2 3 的bug
+r(implode(',', $tester->bug->getBugPairsByList($bugIdList[1]))) && p()  && e('测试单转Bug1,SonarQube_Bug4');                // 获取 ID 等于 1 4 的bug
+r(implode(',', $tester->bug->getBugPairsByList($bugIdList[2]))) && p()  && e('SonarQube_Bug2,测试单转Bug7');                // 获取 ID 等于 2 7 的bug
+r(implode(',', $tester->bug->getBugPairsByList($bugIdList[3]))) && p()  && e('SonarQube_Bug2,SonarQube_Bug6,测试单转Bug9'); // 获取 ID 等于 2 6 9的bug
+r(implode(',', $tester->bug->getBugPairsByList($bugIdList[4]))) && p()  && e('0');                                          // 获取 ID 不存在的bug
