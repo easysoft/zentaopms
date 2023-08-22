19e59226f3a9aef0ca5f58314bef15ca7963789c
caoyanyi <caoyanyi@easycorp.ltd>
2023-08-22 09:24:24 +0800

    + Add unit test for project getProgramTree.

---
+++ module/project/test/model/getprogramtree.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+zdTable('project')->gen(20);
+su('admin');
+
+/**
+
+title=测试 projectModel::getInfoList;
+timeout=0
+cid=1
+
+*/
+
+global $tester;
+$tester->loadModel('project');
+
+$programs = $tester->project->getProgramTree();
+r($programs)        && p('0:name') && e('项目集1'); // 查询第一个项目集名称
+r(count($programs)) && p()         && e('4');       // 查询项目集数量
