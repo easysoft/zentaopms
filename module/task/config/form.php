ed9a7e1ff1f64db6f8b374601400e13c5862e13e
dingguodong <dingguodong@easycorp.ltd>
2023-08-23 09:56:29 +0800

    * Set the left field to not required.

--- module/task/config/form.php
+++ module/task/config/form.php
@@ -187,7 +187,7 @@ $config->task->form->recordWorkhour = array();
 $config->task->form->recordWorkhour['date']     = array('type' => 'date',   'required' => true, 'default' => array());
 $config->task->form->recordWorkhour['work']     = array('type' => 'string', 'required' => true, 'default' => array(), 'base' => true);
 $config->task->form->recordWorkhour['consumed'] = array('type' => 'float',  'required' => true, 'default' => array());
-$config->task->form->recordWorkhour['left']     = array('type' => 'float',  'required' => true, 'default' => array());
+$config->task->form->recordWorkhour['left']     = array('type' => 'float',  'required' => false, 'default' => array()); /* Set required to false as the required field can NOT be 0. */
 
 $config->task->form->editEffort = array();
 $config->task->form->editEffort['date']     = array('type' => 'date',   'required' => true,  'default' => '');
