956583dc070e2406c18bb4d3184ccbffa2fc96a1
sunguangming <sunguangming@easycorp.ltd>
2023-08-23 08:59:45 +0800

    * Finish task #101103.

---
+++ module/project/test/model/getnoproductlist.php
@@ -0,0 +1,31 @@
+#!/usr/bin/env php
+<?php
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+su('admin');
+
+zdTable('project')->config('project')->gen(50);
+
+$projectproduct = zdTable('projectproduct');
+$projectproduct->product->range('1-50');
+$projectproduct->project->range('1-50');
+$projectproduct->gen(50);
+
+/**
+
+title=测试 projectModel->getNoProductList();
+timeout=0
+cid=1
+
+- 查询所有没有产品的项目数量 @25
+
+- 查询没有产品的ID为17的项目详情
+ - 第17条的name属性 @敏捷项目17
+ - 第17条的code属性 @code17
+
+*/
+
+global $tester;
+$tester->loadModel('project');
+
+r(count($tester->project->getNoProductList())) && p() && e('25'); // 查询所有没有产品的项目数量
+r($tester->project->getNoProductList())        && p('17:name,code') && e('敏捷项目17,code17'); //查询没有产品的ID为17的项目详情
