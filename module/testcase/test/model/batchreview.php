00d5976098545ba1636c7a19a5f39f0be6a611a9
liugang <liugang@easycorp.ltd>
2023-08-23 15:29:45 +0800

    * testcase: generate data before run unit test script.

--- module/testcase/test/model/batchreview.php
+++ module/testcase/test/model/batchreview.php
@@ -4,6 +4,8 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/testcase.class.php';
 su('admin');
 
+zdTable('case')->gen(10);
+
 /**
 
 title=测试 testcaseModel->batchReview();
@@ -13,13 +15,13 @@ cid=1
 */
 
 $testcase   = new testcaseTest();
-$caseIDList = array(401, 402, 403, 404);
+$caseIDList = array(1, 2, 3, 4);
 
-r($testcase->batchReviewTest($caseIDList, 'clarify')) && p('401:status') && e('wait');        // 评审结果为 继续完善，状态是 wait        的用例评审后状态为 wait。
-r($testcase->batchReviewTest($caseIDList, 'clarify')) && p('402:status') && e('normal');      // 评审结果为 继续完善，状态是 normal      的用例评审后状态为 normal。
-r($testcase->batchReviewTest($caseIDList, 'clarify')) && p('403:status') && e('blocked');     // 评审结果为 继续完善，状态是 blocked     的用例评审后状态为 blocked。
-r($testcase->batchReviewTest($caseIDList, 'clarify')) && p('404:status') && e('investigate'); // 评审结果为 继续完善，状态是 investigate 的用例评审后状态为 investigate。
-r($testcase->batchReviewTest($caseIDList, 'pass'))    && p('401:status') && e('normal');      // 评审结果为 确认通过，状态是 wait        的用例评审后状态为 normal。
-r($testcase->batchReviewTest($caseIDList, 'pass'))    && p('402:status') && e('normal');      // 评审结果为 确认通过，状态是 normal      的用例评审后状态为 normal。
-r($testcase->batchReviewTest($caseIDList, 'pass'))    && p('403:status') && e('blocked');     // 评审结果为 确认通过，状态是 blocked     的用例评审后状态为 blocked。
-r($testcase->batchReviewTest($caseIDList, 'pass'))    && p('404:status') && e('investigate'); // 评审结果为 确认通过，状态是 investigate 的用例评审后状态为 investigate。
+r($testcase->batchReviewTest($caseIDList, 'clarify')) && p('1:status') && e('wait');        // 评审结果为 继续完善，状态是 wait        的用例评审后状态为 wait。
+r($testcase->batchReviewTest($caseIDList, 'clarify')) && p('2:status') && e('normal');      // 评审结果为 继续完善，状态是 normal      的用例评审后状态为 normal。
+r($testcase->batchReviewTest($caseIDList, 'clarify')) && p('3:status') && e('blocked');     // 评审结果为 继续完善，状态是 blocked     的用例评审后状态为 blocked。
+r($testcase->batchReviewTest($caseIDList, 'clarify')) && p('4:status') && e('investigate'); // 评审结果为 继续完善，状态是 investigate 的用例评审后状态为 investigate。
+r($testcase->batchReviewTest($caseIDList, 'pass'))    && p('1:status') && e('normal');      // 评审结果为 确认通过，状态是 wait        的用例评审后状态为 normal。
+r($testcase->batchReviewTest($caseIDList, 'pass'))    && p('2:status') && e('normal');      // 评审结果为 确认通过，状态是 normal      的用例评审后状态为 normal。
+r($testcase->batchReviewTest($caseIDList, 'pass'))    && p('3:status') && e('blocked');     // 评审结果为 确认通过，状态是 blocked     的用例评审后状态为 blocked。
+r($testcase->batchReviewTest($caseIDList, 'pass'))    && p('4:status') && e('investigate'); // 评审结果为 确认通过，状态是 investigate 的用例评审后状态为 investigate。
