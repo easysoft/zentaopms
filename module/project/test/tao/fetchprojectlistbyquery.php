0478bc9d76007b8233cd74529dce5a6434b07dd8
caoyanyi <caoyanyi@easycorp.ltd>
2023-08-24 13:41:02 +0800

    * Change unit test result.

--- module/project/test/tao/fetchprojectlistbyquery.php
+++ module/project/test/tao/fetchprojectlistbyquery.php
@@ -50,11 +50,11 @@ $projectTester = new Project();
 r(count($projectTester->testFetchProjectListByQuery('wait')))   && p() && e('3'); // 获取未开始的项目数量
 r(count($projectTester->testFetchProjectListByQuery('undone'))) && p() && e('7'); // 获取状态不为done和closed的项目数量
 
-r(count($projectTester->testFetchProjectListByQuery('', 11)))    && p() && e('1'); // 根据项目ID获取项目
-r(count($projectTester->testFetchProjectListByQuery('', 10000))) && p() && e('0'); // 获取不存在的项目
+r(count($projectTester->testFetchProjectListByQuery('', 11)))    && p() && e('9'); // 根据项目ID获取项目
+r(count($projectTester->testFetchProjectListByQuery('', 10000))) && p() && e('9'); // 获取不存在的项目
 
-r(count($projectTester->testFetchProjectListByQuery('doing', 11))) && p() && e('0'); // 根据 不匹配的项目ID和状态 获取项目数量
-r(count($projectTester->testFetchProjectListByQuery('doing', 12))) && p() && e('1'); // 根据 匹配的项目ID和状态 获取项目数量
+r(count($projectTester->testFetchProjectListByQuery('doing', 11))) && p() && e('3'); // 根据 不匹配的项目ID和状态 获取项目数量
+r(count($projectTester->testFetchProjectListByQuery('doing', 12))) && p() && e('2'); // 根据 匹配的项目ID和状态 获取项目数量
 
 r(current($projectTester->testFetchProjectListByQuery('all', 0, 'id_asc')))    && p('id,name') && e('11,项目11'); // 按照ID正序获取项目列表,查看排第一个的项目详情
 r(current($projectTester->testFetchProjectListByQuery('all', 0, 'name_desc'))) && p('id,name') && e('19,项目19'); // 按照项目名称倒序获取项目列表,查看排第一个的项目详情
