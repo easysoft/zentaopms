ad4674724a1d968c09647caab8f133ccd075c7a4
sunguangming <sunguangming@easycorp.ltd>
2023-08-24 13:51:32 +0800

    * Finish task #101163.

--- module/project/test/model/getstats4kanban.php
+++ module/project/test/model/getstats4kanban.php
@@ -4,7 +4,8 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 
 su('admin');
 
-$project = zdTable('project')->config('project')->gen(8);
+zdTable('project')->config('execution')->gen(100)->fixPath();
+zdTable('user')->gen(100);
 
 /**
 
@@ -12,20 +13,47 @@ title=测试 projectTao::getstats4kanban
 timeout=0
 cid=1
 
+- 获取到的看板分组数量 @2
+
+- 获取到的非我负责的看板数量 @1
+
+- 获取到的我负责的看板数量 @1
+
+- 获取到的进行中的执行数量 @4
+
+- 获取非我负责的第一个未开始项目的详情
+ - 属性name @敏捷项目65
+ - 属性status @wait
+
+- 获取非我负责的第一个进行中项目的详情
+ - 属性name @瀑布项目2
+ - 属性status @doing
+
+- 获取我负责的第一个未开始项目的详情
+ - 属性name @瀑布项目66
+ - 属性status @wait
+
+- 获取我负责的第一个进行中项目的详情
+ - 属性name @敏捷项目1
+ - 属性status @doing
+
+- 获取进行中的ID为100的执行详情
+ - 属性name @看板92
+ - 属性status @doing
+
 */
 
 global $tester;
 $tester->loadModel('project');
 list($kanbanGroup, $ongoingExecutions) = $tester->project->getStats4Kanban();
 
-r(count($kanbanGroup))          && p() && e('2');
-r(count($kanbanGroup['other'])) && p() && e('3');
-r(count($kanbanGroup['my']))    && p() && e('3');
-r(count($ongoingExecutions))    && p() && e('1');
+r(count($kanbanGroup))          && p() && e('2'); // 获取到的看板分组数量
+r(count($kanbanGroup['other'])) && p() && e('1'); // 获取到的非我负责的看板数量
+r(count($kanbanGroup['my']))    && p() && e('1'); // 获取到的我负责的看板数量
+r(count($ongoingExecutions))    && p() && e('4'); // 获取到的进行中的执行数量
 
-r($kanbanGroup['other'][2]['wait'][0])  && p('status') && e('wait');
-r($kanbanGroup['other'][4]['doing'][0]) && p('status') && e('doing');
-r($kanbanGroup['my'][1]['wait'][0])     && p('status') && e('wait');
-r($kanbanGroup['my'][3]['doing'][0])    && p('status') && e('doing');
-r($kanbanGroup['my'][5]['doing'][0])    && p('status') && e('doing');
-r($ongoingExecutions[6])                && p('status') && e('doing');
+r($kanbanGroup['other'][0]['wait'][0])  && p('name,status') && e('敏捷项目65,wait'); // 获取非我负责的第一个未开始项目的详情
+r($kanbanGroup['other'][0]['doing'][0]) && p('name,status') && e('瀑布项目2,doing'); // 获取非我负责的第一个进行中项目的详情
+r($kanbanGroup['my'][0]['wait'][0])     && p('name,status') && e('瀑布项目66,wait'); // 获取我负责的第一个未开始项目的详情
+r($kanbanGroup['my'][0]['doing'][0])    && p('name,status') && e('敏捷项目1,doing'); // 获取我负责的第一个进行中项目的详情
+r($ongoingExecutions[100])              && p('name,status') && e('看板92,doing');    // 获取进行中的ID为100的执行详情
