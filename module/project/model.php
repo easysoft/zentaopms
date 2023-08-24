eb20c6ec4ab6d8a843a8c7034686b3720f43ab48
caoyanyi <caoyanyi@easycorp.ltd>
2023-08-24 14:19:42 +0800

    * Change param type.

--- module/project/model.php
+++ module/project/model.php
@@ -931,7 +931,7 @@ class projectModel extends model
         if($model == 'waterfallplus') $model = array('waterfall', 'waterfallplus');
 
         /* Set first program to the project attribute. */
-        $model    = $model == 'all' ? array() : array($model);
+        $model    = $model == 'all' ? array() : (array)$model;
         $multiple = strpos($param, 'multiple') !== false;
         foreach($projects as $projectID => $project)
         {
