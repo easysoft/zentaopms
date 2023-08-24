9061b1fc6dffe1e7fc2399259bbd6e47297a757c
tianshujie <tianshujie@easycorp.ltd>
2023-08-24 10:40:18 +0800

    * Change the case of the checkworkhour file.

--- module/task/test/tao/checkworkhour.php
+++ module/task/test/tao/checkworkhour.php
@@ -6,25 +6,9 @@ su('admin');
 
 /**
 
-title=- 无消耗工时给出提示 @『ID \
+title=taskModel->checkWorkhour();
 timeout=0
 cid=1
-
-- 正常完成任务，是否通过检查 @1
-
-- 不在团队中的人维护工时，返回false @0
-
-- 在团队中的人维护工时，返回true @1
-
-- 无消耗工时给出提示 @1
-
-- 剩余不为数字，给出提示 @1
-
-- 消耗不为数字，给出提示 @1
-
-- 以记录工时的形式完成任务 @1
-
-- 正常记录工时 @1
 
 */
 $task = zdTable('task');
@@ -115,8 +99,8 @@ r($task->checkWorkhourTest(2, $multiTaskEffort))  && p() && e('0'); // 不在团
 su('user1');
 r($task->checkWorkhourTest(2, $multiTaskEffort))  && p() && e('1'); // 在团队中的人维护工时，返回true
 
-r(strpos($task->checkWorkhourTest(3, $noconsumedTaskEffort)[0], "工时』应当大于『0』") !== false)      && p() && e('1'); // 无消耗工时给出提示
-r(strpos($task->checkWorkhourTest(4, $leftNotNumTaskEffort)[0], '"预计剩余"必须为数字') !== false)     && p() && e('1'); // 剩余不为数字，给出提示
-r(strpos($task->checkWorkhourTest(6, $consumedNotNumTaskEffort)[0], '"总计消耗"必须为数字') !== false) && p() && e('1'); // 消耗不为数字，给出提示
-r($task->checkWorkhourTest(5, $finishTaskEffort)) && p() && e('1'); // 以记录工时的形式完成任务
-r($task->checkWorkhourTest(7, $normalTaskEffort)) && p() && e('1'); // 正常记录工时
+r(strpos(current($task->checkWorkhourTest(3, $noconsumedTaskEffort)), '请填写"消耗"') !== false)                   && p() && e('1'); // 无消耗工时给出提示
+r(strpos(current($task->checkWorkhourTest(4, $leftNotNumTaskEffort)), 'ID #1 "预计剩余"必须为数字') !== false)     && p() && e('1'); // 剩余不为数字，给出提示
+r(strpos(current($task->checkWorkhourTest(6, $consumedNotNumTaskEffort)), 'ID #1 "总计消耗"必须为数字') !== false) && p() && e('1'); // 消耗不为数字，给出提示
+r($task->checkWorkhourTest(5, $finishTaskEffort))                                                                  && p() && e('1'); // 以记录工时的形式完成任务
+r($task->checkWorkhourTest(7, $normalTaskEffort))                                                                  && p() && e('1'); // 正常记录工时
