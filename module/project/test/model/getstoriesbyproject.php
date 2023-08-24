ed11dd937248792cc8fb852e01963dbf9feebe0e
caoyanyi <caoyanyi@easycorp.ltd>
2023-08-24 10:09:45 +0800

    * Adjust unit test data.

--- module/project/test/model/getstoriesbyproject.php
+++ module/project/test/model/getstoriesbyproject.php
@@ -1,17 +1,17 @@
 #!/usr/bin/env php
 <?php
 include dirname(__FILE__, 5) . '/test/lib/init.php';
+zdTable('project')->gen(20);
+zdTable('product')->gen(20);
+zdTable('story')->gen(20);
+zdTable('projectstory')->gen(20);
 su('admin');
 
 /**
 
 title=测试 projectModel->getStoriesByProject();
+timeout=0
 cid=1
-pid=1
-
-获取项目11关联的所有产品数量 >> 2
-获取项目11关联的产品ID为91的详情 >> 91
-获取项目11关联的产品ID为91的需求ID >> 362,364
 
 */
 
@@ -19,7 +19,12 @@ global $tester;
 $tester->loadModel('project');
 
 $result = $tester->project->getStoriesByProject(11);
+r(count($result)) && p()                && e('1');   // 获取项目1关联的所有产品数量
+r($result[1])     && p('0:product')     && e('1');   // 获取项目1关联的产品ID为1的详情
+r($result[1])     && p('0:storyIDList', '|') && e('2,4'); // 获取项目1关联的产品ID为1的需求ID
 
-r(count($result)) && p()                && e('2');       // 获取项目11关联的所有产品数量
-r($result[91])    && p('0:product')     && e('91');      // 获取项目11关联的产品ID为91的详情
-r($result[91])    && p('0:storyIDList') && e('362,364'); // 获取项目11关联的产品ID为91的需求ID
+$result = $tester->project->getStoriesByProject(0);
+r(count($result)) && p() && e('6'); // 获取项目0关联的所有产品数量
+
+$result = $tester->project->getStoriesByProject(100);
+r(count($result)) && p() && e('0'); // 获取项目100关联的所有产品数量
