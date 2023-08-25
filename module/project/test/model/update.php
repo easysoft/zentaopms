54d79d8e61b4a7468f5dc1b4dadd0104886795df
caoyanyi <caoyanyi@easycorp.ltd>
2023-08-25 16:59:15 +0800

    * Change unit test data.

--- module/project/test/model/update.php
+++ module/project/test/model/update.php
@@ -31,32 +31,6 @@ title=测试 projectModel::update();
 timeout=0
 cid=1
 
-- 正常更新项目的情况属性name @测试更新项目十
-
-- 更新项目名称为空时属性name @~~
-
-- 当计划完成为空时更新项目信息属性begin @2022-07-06
-
-- 当项目的完成日期小于执行的完成日期时第end条的0属性 @『计划完成』应当大于『2022-07-06』。
-
-- 当项目的开始日期大于执行的开始日期时属性begin @2022-08-07
-
-- 无产品项目属性name @测试更新影子产品
-
-- 正常更新项目的情况属性name @测试更新项目十
-
-- 更新项目名称为空时属性name @~~
-
-- 当计划完成为空时更新项目信息属性begin @2022-07-06
-
-- 当项目的完成日期小于执行的完成日期时第end条的0属性 @『计划完成』应当大于『2022-07-06』。
-
-- 当项目的开始日期大于执行的开始日期时属性begin @2022-08-07
-
-- 无产品项目
- - 属性name @测试更新影子产品
- - 属性status @closed
-
 */
 
 global $tester;
@@ -68,6 +42,7 @@ $data->name       = '测试更新项目十';
 $data->begin      = '2022-07-06';
 $data->end        = '2022-10-26';
 $data->acl        = 'private';
+$data->model      = 'scrum';
 $data->budget     = '100';
 $data->budgetUnit = 'CNY';
 $data->hasProduct = 1;
@@ -80,7 +55,7 @@ $emptyTitleProject = clone $data;
 $emptyTitleProject->name = '';
 
 $emptyBeginProject = clone $data;
-$emptyBeginProject->begin = '';
+$emptyBeginProject->begin = null;
 
 $emptyEndProject = clone $data;
 $emptyEndProject->end = '';
@@ -96,7 +71,7 @@ $noProductProject->status     = 'closed';
 $oldProject = $tester->loadModel('project')->getById(1);
 r($project->update($normalProject,         $oldProject)) && p('name')  && e('测试更新项目十');                       // 正常更新项目的情况
 r($project->update($emptyTitleProject,     $oldProject)) && p('name')  && e('~~');                                   // 更新项目名称为空时
-r($project->update($emptyBeginProject,     $oldProject)) && p('begin') && e('2022-07-06');                           // 当计划完成为空时更新项目信息
+r($project->update($emptyBeginProject,     $oldProject)) && p('begin') && e('~~');                                   // 当计划完成为空时更新项目信息
 r($project->update($emptyEndProject,       $oldProject)) && p('end:0') && e('『计划完成』应当大于『2022-07-06』。'); // 当项目的完成日期小于执行的完成日期时
 r($project->update($beginGtExecutionBegin, $oldProject)) && p('begin') && e('2022-08-07');                           // 当项目的开始日期大于执行的开始日期时
 r($project->update($noProductProject,      $oldProject)) && p('name')  && e('测试更新影子产品');                     // 无产品项目
@@ -104,7 +79,7 @@ r($project->update($noProductProject,      $oldProject)) && p('name')  && e('测
 $oldProject = $tester->loadModel('project')->getById(2);
 r($project->update($normalProject,         $oldProject)) && p('name')        && e('测试更新项目十');                       // 正常更新项目的情况
 r($project->update($emptyTitleProject,     $oldProject)) && p('name')        && e('~~');                                   // 更新项目名称为空时
-r($project->update($emptyBeginProject,     $oldProject)) && p('begin')       && e('2022-07-06');                           // 当计划完成为空时更新项目信息
+r($project->update($emptyBeginProject,     $oldProject)) && p('begin')       && e('~~');                                   // 当计划完成为空时更新项目信息
 r($project->update($emptyEndProject,       $oldProject)) && p('end:0')       && e('『计划完成』应当大于『2022-07-06』。'); // 当项目的完成日期小于执行的完成日期时
 r($project->update($beginGtExecutionBegin, $oldProject)) && p('begin')       && e('2022-08-07');                           // 当项目的开始日期大于执行的开始日期时
-r($project->update($noProductProject,      $oldProject)) && p('name,status') && e('测试更新影子产品,closed');              // 无产品项目
+r($project->update($noProductProject,      $oldProject)) && p('name:0') && e('『项目名称』已经有『测试更新影子产品』这条记录了。如果您确定该记录已删除，请到后台-系统-数据-回收站还原。');              // 无产品项目
