57609d3e722436f789e3b72cdda3dbf6985ab211
wangxuepeng <wangxuepeng@easycorp.ltd>
2023-08-25 07:54:09 +0000

    * Refactor testsuite model getunlinkedcases unit test.

--- module/testsuite/test/model/getunlinkedcases.php
+++ module/testsuite/test/model/getunlinkedcases.php
@@ -4,24 +4,27 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/testsuite.class.php';
 su('admin');
 
+zdTable('case')->gen(2);
+zdTable('testsuite')->gen(2);
+zdTable('userquery')->config('userquery')->gen(1);
 /**
 
 title=测试 testsuiteModel->getUnlinkedCases();
 cid=1
 pid=1
 
-测试suiteID值为1,param值为0 >> 4,这个是测试用例4;3,这个是测试用例3
-测试suiteID值为1,param值为myQueryID >> 4,这个是测试用例4;3,这个是测试用例3
+测试suiteID值为1,param值为0 >> 2,这个是测试用例2;1,这个是测试用例1
+测试suiteID值为1,param值为1 >> 1,这个是测试用例1
 测试suiteID值为2,param值为0 >> 2,这个是测试用例2;1,这个是测试用例1
-测试suiteID值为2,param值为myQueryID >> 2,这个是测试用例2;1,这个是测试用例1
+测试suiteID值为2,param值为1 >> 1,这个是测试用例1
 
 */
 $suiteID = array(1, 2);
-$param = array(0, 'myQueryID');
+$param = array(0, 1);
 
 $testsuite = new testsuiteTest();
 
-r($testsuite->getUnlinkedCasesTest($suiteID[0], $param[0])) && p('0:id,title;1:id,title') && e('4,这个是测试用例4;3,这个是测试用例3');  //测试suiteID值为1,param值为0
-r($testsuite->getUnlinkedCasesTest($suiteID[0], $param[1])) && p('0:id,title;1:id,title') && e('4,这个是测试用例4;3,这个是测试用例3');  //测试suiteID值为1,param值为myQueryID
+r($testsuite->getUnlinkedCasesTest($suiteID[0], $param[0])) && p('0:id,title;1:id,title') && e('2,这个是测试用例2;1,这个是测试用例1');  //测试suiteID值为1,param值为0
 r($testsuite->getUnlinkedCasesTest($suiteID[1], $param[0])) && p('0:id,title;1:id,title') && e('2,这个是测试用例2;1,这个是测试用例1');  //测试suiteID值为2,param值为0
-r($testsuite->getUnlinkedCasesTest($suiteID[1], $param[1])) && p('0:id,title;1:id,title') && e('2,这个是测试用例2;1,这个是测试用例1');  //测试suiteID值为2,param值为myQueryID
+r($testsuite->getUnlinkedCasesTest($suiteID[1], $param[1])) && p('0:id,title') && e('1,这个是测试用例1');  //测试suiteID值为2,param值为1
+r($testsuite->getUnlinkedCasesTest($suiteID[0], $param[1])) && p('0:id,title') && e('1,这个是测试用例1');  //测试suiteID值为1,param值为1
