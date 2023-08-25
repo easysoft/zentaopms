1a0b57121a6e468d922e29012b69926a76aeb8d6
tianshujie <tianshujie@easycorp.ltd>
2023-08-25 15:45:23 +0800

    * Change the case of the getlistbycurrentuser file.

--- module/project/test/model/getlistbycurrentuser.php
+++ module/project/test/model/getlistbycurrentuser.php
@@ -3,22 +3,13 @@
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 su('admin');
 
-$project = zdTable('project')->config('project');
-$project->gen(4);
+zdTable('project')->config('project')->gen(4);
 
 /**
 
 title=测试 projectModel->getListByCurrentUser();
 timeout=0
 cid=1
-
-- 执行projectModel模块的getListByCurrentUser方法 @0
-
-- 执行projectModel模块的getListByCurrentUser方法 @1
-
-- 执行projectModel模块的getListByCurrentUser方法
- - 第11条的id属性 @11
- - 第11条的name属性 @项目1
 
 
 
@@ -33,5 +24,5 @@ $tester->app->config->vision = 'lite';
 r(count($projectModel->getListByCurrentUser())) && p() && e('0');
 
 $tester->app->config->vision = 'rnd';
-r(count($projectModel->getListByCurrentUser())) && p()             && e('1');
-r($projectModel->getListByCurrentUser())        && p('11:id,name') && e('11,项目1');
+r(count($projectModel->getListByCurrentUser())) && p()            && e('1');
+r($projectModel->getListByCurrentUser())        && p('1:id,name') && e('1,项目1');
