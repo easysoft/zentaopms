45a0548b0ad934fe81c28b94b68f29563dcee98d
zenggang <zenggang@easycorp.ltd>
2023-08-28 02:55:15 +0000

    * Adjust code

--- module/repo/ui/review.html.php
+++ module/repo/ui/review.html.php
@@ -16,10 +16,11 @@ foreach($bugs as $bug)
 
     $lines = explode(',', trim($bug->lines, ','));
     $bug->entry = $repo->name . '/' . $this->repo->decodePath($bug->entry);
+    $encodeEntry = urlencode($bug->entry);
     if(empty($bug->v1))
     {
         $revision = $repo->SCM != 'Subversion' ? $this->repo->getGitRevisionName($bug->v2, zget($historys, $bug->v2)) : $bug->v2;
-        $bug->link = $this->repo->createLink('view', "repoID=$repoID&objectID=0&entry={$bug->entry}&revision={$bug->v2}") . "#L$lines[0]";
+        $bug->link = $this->repo->createLink('view', "repoID=$repoID&objectID=0&entry={$encodeEntry}&revision={$bug->v2}") . "#L$lines[0]";
     }
     else
     {
@@ -27,7 +28,7 @@ foreach($bugs as $bug)
         $revision .= ' : ';
         $revision .= $repo->SCM != 'Subversion' ? substr($bug->v2, 0, 10) : $bug->v2;
         if($repo->SCM != 'Subversion') $revision .= ' (' . zget($historys, $bug->v1) . ' : ' . zget($historys, $bug->v2) . ')';
-        $bug->link = $this->repo->createLink('diff', "repoID=$repoID&objectID=0&entry={$bug->entry}&oldRevision={$bug->v1}&newRevision={$bug->v2}") . "#L$lines[0]";
+        $bug->link = $this->repo->createLink('diff', "repoID=$repoID&objectID=0&entry={$encodeEntry}&oldRevision={$bug->v1}&newRevision={$bug->v2}") . "#L$lines[0]";
     }
 }
 $bugs = initTableData($bugs, $config->repo->reviewDtable->fieldList);
