05d03266039f37622ec286d8c0c10461ea1a5d47
caoyanyi <caoyanyi@easycorp.ltd>
2023-08-28 10:32:45 +0800

    * Change unit test for project computeProgress.

--- module/project/test/model/computeprogress.php
+++ module/project/test/model/computeprogress.php
@@ -1,17 +1,17 @@
 #!/usr/bin/env php
 <?php
 include dirname(__FILE__, 5) . '/test/lib/init.php';
+
+zdTable('project')->config('execution')->gen(10);
+zdTable('task')->config('task')->gen(20);
+zdTable('taskteam')->gen(20);
 su('admin');
 
 /**
 
 title=测试 projectModel->computeProgress();
+timeout=0
 cid=1
-pid=1
-
-查看项目11下的所有执行的数量 >> 7
-查看计算后的执行工时汇总信息 >> 10,3,10,23.08,13
-查看计算后的执行工时汇总信息 >> 1,3,1,75,4
 
 */
 
@@ -22,6 +22,5 @@ $tester->loadModel('execution');
 $executions = $tester->execution->getList(11);
 $executions = $tester->project->computeProgress($executions);
 
-r(count($executions)) && p('')                                                             && e('7');                // 查看项目11下的所有执行的数量
-r($executions)        && p('551:totalEstimate,totalConsumed,totalLeft,progress,totalReal') && e('10,3,10,23.08,13'); // 查看计算后的执行工时汇总信息
-r($executions)        && p('641:totalEstimate,totalConsumed,totalLeft,progress,totalReal') && e('1,3,1,75,4');       // 查看计算后的执行工时汇总信息
+r(count($executions)) && p('')                                                             && e('1');                // 查看项目11下的所有执行的数量
+r($executions)        && p('101:totalEstimate,totalConsumed,totalLeft,progress,totalReal') && e('44,9,44,16.98,53'); // 查看计算后的执行工时汇总信息
