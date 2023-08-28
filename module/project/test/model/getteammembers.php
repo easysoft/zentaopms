046b314665dcbb78e0fca4d602bc5790ab05ea95
tianshujie <tianshujie@easycorp.ltd>
2023-08-28 08:45:23 +0800

    * Format the case.

--- module/project/test/model/getteammembers.php
+++ module/project/test/model/getteammembers.php
@@ -13,23 +13,13 @@ title=测试 projectModel->getTeamMembers();
 timeout=0
 cid=1
 
-- 获取id为1的项目团队成员个数 @0
-
-- 获取id为100的项目团队成员个数 @0
-
-- 获取id为11的项目团队成员个数 @5
-
-- 获取id为11的项目团队成员的详细信息第user1条的realname属性 @用户1
-
-- 获取id为11的项目团队成员的详细信息第user2条的realname属性 @用户2
-
 */
 
 global $tester;
 $tester->loadModel('project');
 
-r(count($tester->project->getTeamMembers(1)))   && p() && e('0'); // 获取id为1的项目团队成员个数
-r(count($tester->project->getTeamMembers(100))) && p() && e('0'); // 获取id为100的项目团队成员个数
-r(count($tester->project->getTeamMembers(11)))  && p() && e('5'); // 获取id为11的项目团队成员个数
+r(count($tester->project->getTeamMembers(1)))   && p() && e('0');                     // 获取id为1的项目团队成员个数
+r(count($tester->project->getTeamMembers(100))) && p() && e('0');                     // 获取id为100的项目团队成员个数
+r(count($tester->project->getTeamMembers(11)))  && p() && e('5');                     // 获取id为11的项目团队成员个数
 r($tester->project->getTeamMembers(11))         && p('user1:realname') && e('用户1'); // 获取id为11的项目团队成员的详细信息
 r($tester->project->getTeamMembers(11))         && p('user2:realname') && e('用户2'); // 获取id为11的项目团队成员的详细信息
--- module/project/test/tao/fetchprojectinfo.php
+++ module/project/test/tao/fetchprojectinfo.php
@@ -4,27 +4,24 @@ include dirname(__FILE__, 5) . "/test/lib/init.php";
 include dirname(__FILE__, 2) . '/project.class.php';
 su('admin');
 
-function initData()
-{
-    $project = zdTable('project');
-    $project->id->range('2-5');
-    $project->project->range('2-5');
-    $project->name->prefix("项目")->range('2-5');
-    $project->code->prefix("project")->range('2-5');
-    $project->model->range("scrum");
-    $project->auth->range("[]");
-    $project->path->range("[]");
-    $project->type->range("project");
-    $project->grade->range("1");
-    $project->days->range("1");
-    $project->status->range("wait");
-    $project->desc->range("[]");
-    $project->budget->range("100000,200000");
-    $project->budgetUnit->range("CNY");
-    $project->percent->range("0-0");
+$project = zdTable('project');
+$project->id->range('2-5');
+$project->project->range('2-5');
+$project->name->prefix("项目")->range('2-5');
+$project->code->prefix("project")->range('2-5');
+$project->model->range("scrum");
+$project->auth->range("[]");
+$project->path->range("[]");
+$project->type->range("project");
+$project->grade->range("1");
+$project->days->range("1");
+$project->status->range("wait");
+$project->desc->range("[]");
+$project->budget->range("100000,200000");
+$project->budgetUnit->range("CNY");
+$project->percent->range("0-0");
 
-    $project->gen(4);
-}
+$project->gen(4);
 
 /**
 
@@ -34,8 +31,6 @@ cid=1
 
 
 */
-
-initData();
 
 $projectTester = new Project();
 r($projectTester->testFetchProjectInfo(2))      && p('code,type') && e('project2,project');                 //获取ID等于2的项目
