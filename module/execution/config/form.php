12dd136f0a06480f68d751e06938f362cf1e4b67
caoyanyi <caoyanyi@easycorp.ltd>
2023-08-29 15:15:51 +0800

    + Add form config for execution importTask.

---
+++ module/execution/config/form.php
@@ -0,0 +1,6 @@
+<?php
+$config->execution->form = new stdclass();
+
+$config->execution->form->importTask = array();
+
+$config->execution->form->importTask['taskIdList'] = array('type' => 'int', 'required' => false, 'base' => true);
