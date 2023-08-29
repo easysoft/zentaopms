f01098c06299b0c50f3988f0dd775b5b35c8bbd4
zhaoke <zhaoke@easycorp.ltd>
2023-08-29 17:40:40 +0800

    * Fix load old page error.

--- www/js/zui3/zin.js
+++ www/js/zui3/zin.js
@@ -7,7 +7,7 @@
     const currentMethod = config.currentMethod;
     const isIndexPage   = currentModule === 'index' && currentMethod === 'index';
     const isInAppTab    = parent.window !== window;
-    const is18version   = config.version.split('.')[0] === '18';
+    const is18version   = true;
 
     const selfOpenList = new Set('index|tutorial|install|upgrade|sso|cron|misc|user-login|user-deny|user-logout|user-reset|user-forgetpassword|user-resetpassword|my-changepassword|my-preference|file-read|file-download|file-uploadimages|report-annualdata|misc-captcha|execution-printkanban|traincourse-playvideo'.split('|'));
     const isAllowSelfOpen = isIndexPage
@@ -46,7 +46,7 @@
     if(isIndexPage) return;
 
     const DEBUG       = config.debug;
-    const is18version = config.version.split('.')[0] === '18'; // 18+zin.
+    const is18version = true;
     const currentCode = window.name.substring(4);
     const isInAppTab  = parent.window !== window;
     const fetchTasks  = new Map();
