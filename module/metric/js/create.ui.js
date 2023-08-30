34254b2f14bb76beb491a5e3e813d340165e5d01
songchenxuan <1097180981@qq.com>
2023-08-30 15:49:16 +0800

    * Fix bug #37811.

--- module/metric/js/create.ui.js
+++ module/metric/js/create.ui.js
@@ -1,3 +1,8 @@
+$(document).ready()
+{
+    $('#addUnitBox').find("[name^='unit']").prop('disabled', true);
+}
+
 window.addUnit = function(e)
 {
     if($(e.target).prop('checked'))
@@ -5,11 +10,15 @@ window.addUnit = function(e)
         $('#unitBox').addClass('hidden');
         $('#addUnitBox').removeClass('hidden');
         $("[name^='customUnit']").prop('checked', true);
+
+        $('#addUnitBox').find("[name^='unit']").prop('disabled', false);
     }
     else
     {
         $('#unitBox').removeClass('hidden');
         $('#addUnitBox').addClass('hidden');
         $("[name^='customUnit']").prop('checked', false);
+
+        $('#addUnitBox').find("[name^='unit']").prop('disabled', true);
     }
 }
