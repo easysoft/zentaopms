75f80be864994bdfd8e3812c481b1ef4c291ad21
zhujinyong <zhujinyong@easycorp.ltd>
2023-08-30 16:09:28 +0800

    * Use mb_strpos instead of str_contains.

--- framework/base/router.class.php
+++ framework/base/router.class.php
@@ -2965,7 +2965,7 @@ class baseRouter
          * 忽略该错误：Redefining already defined constructor。
          * Skip the error: Redefining already defined constructor.
          **/
-        if(str_contains($message, 'Redefining')) return true;
+        if(mb_strpos($message, 'Redefining') !== false) return true;
 
         /*
          * 设置错误信息。
