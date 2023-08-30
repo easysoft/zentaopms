b5f6d33d3aff46a9badcbf92cfdfe024a7f4f915
Chenjianyu <chenjianyu@@easycorp.ltd>
2023-08-30 13:30:49 +0800

    * change the code style to avoid a code transition problem.

--- module/ai/control.php
+++ module/ai/control.php
@@ -527,7 +527,7 @@ class ai extends control
         if(!empty($stop))    return header("location: $location", true, 302);
 
         $response = $this->ai->executePrompt($prompt, $object);
-        if(is_int($response)) return $this->send(array('result' => 'fail', 'message' => sprintf($this->lang->ai->execute->failFormat, $this->lang->ai->execute->executeErrors[$response]) . (empty($this->ai->errors) ? '' : implode(', ', $this->ai->errors))));
+        if(is_int($response)) return $this->send(array('result' => 'fail', 'message' => sprintf($this->lang->ai->execute->failFormat, $this->lang->ai->execute->executeErrors["$response"]) . (empty($this->ai->errors) ? '' : implode(', ', $this->ai->errors))));
         if(empty($response))  return $this->send(array('result' => 'fail', 'message' => sprintf($this->lang->ai->execute->failFormat, $this->lang->ai->execute->failReasons['noResponse'])));
 
         $this->ai->setInjectData($prompt->targetForm, $response);
