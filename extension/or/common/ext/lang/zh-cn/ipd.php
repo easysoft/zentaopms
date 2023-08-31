40de2f3f127a4a9c02d1ce55914b952e022c8b03
tanghucheng <tanghucheng@easycorp.ltd>
2023-08-31 13:16:43 +0800

    * Add market icon.

--- extension/or/common/ext/lang/zh-cn/ipd.php
+++ extension/or/common/ext/lang/zh-cn/ipd.php
@@ -19,7 +19,7 @@ $lang->marketresearch = new stdclass();
 $lang->marketresearch->common = '调研';
 
 $lang->navIcons['demandpool'] = "<i class='icon icon-bars'></i>";
-$lang->navIcons['market']     = "<i class='icon icon-bars'></i>";
+$lang->navIcons['market']     = "<i class='icon icon-market'></i>";
 
 /* Main Navigation. */
 $lang->mainNav              = new stdclass();
