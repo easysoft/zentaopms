01b17d41f304b89e246f5aa6758de793334fe414
sunhao <sunhao@easycorp.ltd>
2023-09-01 11:13:25 +0800

    * zin: optimize priLabel and severityLabel widget.

--- lib/zin/wg/severitylabel/v1.php
+++ lib/zin/wg/severitylabel/v1.php
@@ -5,25 +5,40 @@ namespace zin;
 class severityLabel extends wg
 {
     protected static array $defineProps = array(
-        'text?: string',      // 标签文本
-        'level?: string|int', // 严重程度等级
-        'isIcon?: bool=false' // 是否显示为图标
+        'level: string|int',     // 严重程度等级。
+        'text?: string|array',   // 标签文本或标签文本映射对象。
+        'isIcon?: bool=false'    // 是否显示为图标。
     );
 
     protected function onAddChild(mixed $child): mixed
     {
-        if(is_string($child) && !$this->props->has('text'))
+        if(is_string($child) || is_int($child))
         {
-            $this->props->set('text', $child);
-            return false;
+            if(!$this->props->has('level'))
+            {
+                $this->props->set('level', $child);
+                return false;
+            }
+            if(!$this->props->has('text') && is_string($child))
+            {
+                $this->props->set('text', $child);
+                return false;
+            }
         }
+        return $child;
     }
 
     protected function build(): wg
     {
+        $level  = $this->prop('level', 0);
         $text   = $this->prop('text');
-        $level  = $this->prop('level');
         $isIcon = $this->prop('isIcon');
+
+        if(is_array($text)) $text = isset($text[$level]) ? $text[$level] : null;
+
+        $level = trim("$level");
+        if($text === null)   $text   = ($level === '0' || $level === '') ? '' : $level;
+        if($isIcon === null) $isIcon = is_numeric($text);
 
         return span
         (
