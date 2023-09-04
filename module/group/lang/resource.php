06e4da6652ed2db191d2c4e31c49ac5df829f0af
zhaoke <zhaoke@easycorp.ltd>
2023-09-04 15:01:00 +0800

    * Fix bug #38164.

--- module/group/lang/resource.php
+++ module/group/lang/resource.php
@@ -1382,22 +1382,9 @@ $lang->serverroom->methodOrder[15] = 'delete';
 $lang->serverroom->methodOrder[20] = 'view';
 
 $lang->resource->instance = new stdclass();
-$lang->resource->instance->install       = 'install';
-$lang->resource->instance->view          = 'view';
-$lang->resource->instance->visit         = 'visit';
-$lang->resource->instance->ajaxStatus    = 'ajaxStatus';
-$lang->resource->instance->ajaxStart     = 'ajaxStart';
-$lang->resource->instance->ajaxStop      = 'ajaxStop';
-$lang->resource->instance->ajaxUninstall = 'ajaxUninstall';
-$lang->resource->instance->upgrade       = 'upgrade';
-
-$lang->resource->space = new stdclass();
-$lang->resource->space->browse          = 'browse';
-$lang->resource->space->getStoreAppInfo = 'getStoreAppInfo';
-
-$lang->resource->store = new stdclass();
-$lang->resource->store->browse  = 'browse';
-$lang->resource->store->appView = 'appView';
+$lang->resource->store    = new stdclass();
+$lang->resource->space    = new stdclass();
+$lang->resource->space->browse = 'browse';
 
 /* Doc. */
 $lang->resource->doc = new stdclass();
