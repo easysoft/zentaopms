90092a8bc5fa43b8f79741306fe8aba611a6a462
qixinzhi <qixinzhi@easycorp.ltd>
2023-09-06 02:33:29 +0000

    * Add ajax change feature bar.

--- module/metric/js/preview.ui.js
+++ module/metric/js/preview.ui.js
@@ -5,15 +5,34 @@ window.renderHeight = function()
 
 $(document).ready()
 {
-    renderDTable();
-    $(document).on('change', '.checkbox-primary input[type="checkbox"]', function()
+  renderDTable();
+  $(document).on('change', '.checkbox-primary input[type="checkbox"]', function()
+  {
+    $('.checkbox-primary input[type="checkbox"]').each(function()
     {
-        $('.checkbox-primary input[type="checkbox"]').each(function()
-        {
-            if($(this).is(":checked")) $(this).closest('.checkbox-primary').addClass('metric-current');
-            if($(this).is(":not(:checked)")) $(this).closest('.checkbox-primary').removeClass('metric-current');
-        });
+      if($(this).is(":checked")) $(this).closest('.checkbox-primary').addClass('metric-current');
+      if($(this).is(":not(:checked)")) $(this).closest('.checkbox-primary').removeClass('metric-current');
     });
+  });
+
+  var itemSelector = 'menu.nav-ajax .nav-item a';
+  $(document).off('click', itemSelector)
+  $(document).on('click', itemSelector, function()
+  {
+    console.log(this);
+    var that  = this;
+    var scope = $(this).attr('id');
+    $.get($.createLink('metric', 'ajaxGetMetrics', 'scope=' + scope), function(resp)
+    {
+      var metrics = JSON.parse(resp);
+      var total = metrics.length;
+
+      $(itemSelector).removeClass('active');
+      $(itemSelector).find('span.label').remove();
+      $(that).addClass('active');
+      $(that).append(`<span class="label size-sm rounded-full white">${total}</span>`);
+    });
+  });
 }
 
 function renderDTable()
