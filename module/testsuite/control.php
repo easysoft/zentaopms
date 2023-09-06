6523ee46a89f76e265f83303b7afaaf4374d0219
liugang <liugang@easycorp.ltd>
2023-09-06 14:51:29 +0800

    * testsuite: remove unused property in file comment.

--- module/testsuite/control.php
+++ module/testsuite/control.php
@@ -2,12 +2,11 @@
 /**
  * The control file of testsuite module of ZenTaoPMS.
  *
- * @copyright   Copyright 2009-2015 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.cnezsoft.com)
- * @license     ZPL(http://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
- * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
- * @package     testsuite
- * @version     $Id: control.php 5114 2013-07-12 06:02:59Z chencongzhi520@gmail.com $
- * @link        http://www.zentao.net
+ * @copyright Copyright 2009-2015 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.cnezsoft.com)
+ * @license   ZPL(http://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author    Chunsheng Wang <chunsheng@cnezsoft.com>
+ * @package   testsuite
+ * @link      http://www.zentao.net
  */
 class testsuite extends control
 {
