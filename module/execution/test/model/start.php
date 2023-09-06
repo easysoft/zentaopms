68996615cb5bd5572e432d129bae853992d8f56f
dingguodong <dingguodong@easycorp.ltd>
2023-09-06 13:52:09 +0800

    * Remove invalid test case.

--- module/execution/test/model/start.php
+++ module/execution/test/model/start.php
@@ -13,26 +13,15 @@ $execution->gen(6);
 su('admin');
 
 /**
-
 title=测试executionModel->startTest();
 cid=1
 pid=1
-
-敏捷执行开始             >> status,wait,doing
-瀑布阶段开始             >> status,wait,doing
-看板执行开始             >> status,wait,doing
-重复执行开始             >> 此任务已被启动，不能重复启动！
-子瀑布开启获取父瀑布状态 >> doing
-
 */
 
-$executionIDList = array('2', '3', '4', '5');
-
-$noRealBegan   = array('realBegan' => '');
+$executionIDList = array(2, 3, 4, 5);
 
 $execution = new executionTest();
-r($execution->startTest($executionIDList[0]))                && p('1:field,old,new') && e('status,wait,doing');              // 敏捷执行开始
-r($execution->startTest($executionIDList[1]))                && p('1:field,old,new') && e('status,wait,doing');              // 瀑布阶段开始
-r($execution->startTest($executionIDList[2]))                && p('1:field,old,new') && e('status,wait,doing');              // 看板执行开始
-r($execution->startTest($executionIDList[0],$noRealBegan))   && p()                  && e('此任务已被启动，不能重复启动！'); // 重复执行开始
+r($execution->startTest($executionIDList[0]))                && p('0:field,old,new') && e('status,wait,doing');              // 敏捷执行开始
+r($execution->startTest($executionIDList[1]))                && p('0:field,old,new') && e('status,wait,doing');              // 瀑布阶段开始
+r($execution->startTest($executionIDList[2]))                && p('0:field,old,new') && e('status,wait,doing');              // 看板执行开始
 r($execution->startTest($executionIDList[3], array(), true)) && p('status')          && e('doing');                          // 子瀑布开启获取父瀑布状态
