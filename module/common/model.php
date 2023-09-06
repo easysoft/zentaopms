81b4e06a2d22fe366b3d5a03210ddb5daec8f6fc
wangyidong <wangyidong@cnezsoft.com>
2023-09-06 13:26:59 +0800

    * Fix bug #38291.

--- module/common/model.php
+++ module/common/model.php
@@ -2110,7 +2110,7 @@ EOF;
             if(strtolower($key) == 'closeddate'       and $value == '') continue;
             if(strtolower($key) == 'actualcloseddate' and $value == '') continue;
 
-            if(isset($old->$key))
+            if(isset($old->$key) && is_string($old->$key))
             {
                 if($config->edition != 'open' && isset($dateFields[$key])) $old->$key = formatTime($old->$key);
 
