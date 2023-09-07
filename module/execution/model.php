1086cafad96b83c6e731ee30231c374b0d66038a
tianshujie <tianshujie@easycorp.ltd>
2023-09-07 11:25:57 +0800

    + Add return type for the delete method.

--- module/execution/model.php
+++ module/execution/model.php
@@ -5338,7 +5338,7 @@ class executionModel extends model
      * @access public
      * @return void
      */
-    public function delete(string $table, int $executionID)
+    public function delete(string $table, int $executionID): void
     {
         $this->dao->update($table)->set('deleted')->eq(1)->where('id')->eq($executionID)->exec();
         $this->loadModel('action')->create('execution', $executionID, 'deleted');
