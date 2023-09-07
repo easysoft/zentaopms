0f605cfce6f936f839af8800d0099beb9f8898bf
caoyanyi <caoyanyi@easycorp.ltd>
2023-09-07 13:16:21 +0800

    * Change default back link.

--- lib/zin/wg/backbtn/v1.php
+++ lib/zin/wg/backbtn/v1.php
@@ -62,6 +62,7 @@ class backBtn extends btn
             'compile'        => 'compile-browse',
             'store'          => 'store-browse',
             'space'          => 'space-browse',
+            'instance'       => 'space-browse',
             'artifactrepo'   => 'artifactrepo-browse',
         );
 
