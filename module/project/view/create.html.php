29961590dc95f7cba4d5ad8329eb21c4d8b678c6
tanghucheng <tanghucheng@easycorp.ltd>
2023-09-11 04:43:46 -0400

    * Fix copy project for IPD.

--- module/project/view/create.html.php
+++ module/project/view/create.html.php
@@ -48,7 +48,7 @@
       <h2><?php echo $createTitle;?></h2>
       <?php if(!commonModel::isTutorialMode()): ?>
       <div class="pull-right btn-toolbar">
-      <?php if($config->edition != 'max' or $model == 'kanban'):?>
+      <?php if(!in_array($config->edition, array('max', 'ipd')) or $model == 'kanban' or $model == 'ipd'):?>
         <button type='button' class='btn btn-link' data-toggle='modal' data-target='#copyProjectModal'><?php echo html::icon($lang->icons['copy'], 'muted') . ' ' . $lang->project->copy;?></button>
       <?php else: ?>
         <button type='button' class='btn btn-link open-btn' data-toggle='modal' data-target='#maxCopyProjectModal'><?php echo html::icon($lang->icons['copy'], 'muted') . ' ' . $lang->project->copy;?></button>
