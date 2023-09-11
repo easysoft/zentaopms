db48aaa10510bc577bafa7dfaee5c54e0035e24a
wangxuepeng <wangxuepeng@easycorp.ltd>
2023-09-11 07:35:09 +0000

    * Unit test of testsuite getsuitepairs method.

--- module/testsuite/test/model/getsuitepairs.php
+++ module/testsuite/test/model/getsuitepairs.php
@@ -12,17 +12,23 @@ title=测试 testsuiteModel->getSuitePairs();
 cid=1
 pid=1
 
-测试productID值为1,orderBy为id_desc >> 这是测试套件名称1
-测试productID值为1,orderBy为id_asc >> 这是测试套件名称3
+测试productID值为1,orderBy为id_desc           >> 这是测试套件名称1
+测试productID值为1,orderBy为id_asc            >> 这是测试套件名称3
 测试productID值为1,orderBy为name_desc,id_desc >> 这是测试套件名称1
-测试productID值为1,orderBy为name_asc,id_desc >> 这是测试套件名称3
-测试productID值为1,orderBy为id_desc >> 0
-测试productID值为1,orderBy为id_asc >> 0
+测试productID值为1,orderBy为name_asc,id_desc  >> 这是测试套件名称3
+测试productID值为1,orderBy为id_desc           >> 0
+测试productID值为1,orderBy为id_asc            >> 0
 测试productID值为1,orderBy为name_desc,id_desc >> 0
-测试productID值为1,orderBy为name_asc,id_desc >> 0
+测试productID值为1,orderBy为name_asc,id_desc  >> 0
 
-*/
-$productID = array(1, 0);
+切换用户dev1
+
+测试productID值为1,orderBy为id_desc >> ~~
+测试productID值为1,orderBy为id_desc >> 这是测试套件名称1
+
+ */
+
+$productID = array(1, 0, 2);
 $orderBy   = array('id_desc', 'id_asc', 'name_desc,id_desc', 'name_asc,id_desc');
 
 $testsuite = new testsuiteTest();
@@ -31,7 +37,12 @@ r($testsuite->getSuitePairsTest($productID[0], $orderBy[0])) && p('1') && e('这
 r($testsuite->getSuitePairsTest($productID[0], $orderBy[1])) && p('3') && e('这是测试套件名称3');  //测试productID值为1,orderBy为id_asc
 r($testsuite->getSuitePairsTest($productID[0], $orderBy[2])) && p('1') && e('这是测试套件名称1');  //测试productID值为1,orderBy为name_desc,id_desc
 r($testsuite->getSuitePairsTest($productID[0], $orderBy[3])) && p('3') && e('这是测试套件名称3');  //测试productID值为1,orderBy为name_asc,id_desc
-r($testsuite->getSuitePairsTest($productID[1], $orderBy[0])) && p()    && e('0');  //测试productID值为1,orderBy为id_desc
-r($testsuite->getSuitePairsTest($productID[1], $orderBy[1])) && p()    && e('0');  //测试productID值为1,orderBy为id_asc
-r($testsuite->getSuitePairsTest($productID[1], $orderBy[2])) && p()    && e('0');  //测试productID值为1,orderBy为name_desc,id_desc
-r($testsuite->getSuitePairsTest($productID[1], $orderBy[3])) && p()    && e('0');  //测试productID值为1,orderBy为name_asc,id_desc
+r($testsuite->getSuitePairsTest($productID[1], $orderBy[0])) && p()    && e('0');                  //测试productID值为1,orderBy为id_desc
+r($testsuite->getSuitePairsTest($productID[1], $orderBy[1])) && p()    && e('0');                  //测试productID值为1,orderBy为id_asc
+r($testsuite->getSuitePairsTest($productID[1], $orderBy[2])) && p()    && e('0');                  //测试productID值为1,orderBy为name_desc,id_desc
+r($testsuite->getSuitePairsTest($productID[1], $orderBy[3])) && p()    && e('0');                  //测试productID值为1,orderBy为name_asc,id_desc
+
+su('dev1');
+
+r($testsuite->getSuitePairsTest($productID[0], $orderBy[2])) && p('2') && e('~~');                 //测试productID值为1,orderBy为id_desc
+r($testsuite->getSuitePairsTest($productID[0], $orderBy[2])) && p('1') && e('这是测试套件名称1');  //测试productID值为1,orderBy为id_desc
