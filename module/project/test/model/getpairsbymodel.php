c96544a7527c6e1c4c20ec09917ae73cad6f6bb6
dingguodong <dingguodong@easycorp.ltd>
2023-09-13 16:59:36 +0800

    * Fix wrong designed test case.

--- module/project/test/model/getpairsbymodel.php
+++ module/project/test/model/getpairsbymodel.php
@@ -16,14 +16,14 @@ cid=1
 global $tester;
 $tester->loadModel('project');
 
-r($tester->project->getPairsByModel('all'))           && p('21') && e('项目集6 / 项目26'); // 获取所有项目
-r($tester->project->getPairsByModel('scrum'))         && p('1')  && e('项目集1 / 项目21'); // 获取敏捷项目
-r($tester->project->getPairsByModel('waterfallplus')) && p('10') && e('项目集7 / 项目37'); // 获取融合瀑布类型项目
+r($tester->project->getPairsByModel('all'))           && p('26') && e('项目集6 / 项目26');  // 获取所有项目
+r($tester->project->getPairsByModel('scrum'))         && p('21')  && e('项目集1 / 项目21'); // 获取敏捷项目
+r($tester->project->getPairsByModel('waterfallplus')) && p('37') && e('项目集7 / 项目37');  // 获取融合瀑布类型项目
 
-r($tester->project->getPairsByModel('all', 'noclosed'))          && p('10') && e('项目集3 / 项目43'); // 获取未关闭的项目
-r($tester->project->getPairsByModel('all', 'multiple'))          && p('10') && e('项目集3 / 项目33'); // 获取启用迭代的项目
-r($tester->project->getPairsByModel('all', 'noclosed,multiple')) && p('10') && e('项目集3 / 项目43'); // 获取未关闭的启用迭代的项目
+r($tester->project->getPairsByModel('all', 'noclosed'))          && p('43') && e('项目集3 / 项目43'); // 获取未关闭的项目
+r($tester->project->getPairsByModel('all', 'multiple'))          && p('33') && e('项目集3 / 项目33'); // 获取启用迭代的项目
+r($tester->project->getPairsByModel('all', 'noclosed,multiple')) && p('43') && e('项目集3 / 项目43'); // 获取未关闭的启用迭代的项目
 
-r($tester->project->getPairsByModel('all', 'noclosed', 50))                && p('1') && e('项目集1 / 项目21'); // 获取所有项目
-r($tester->project->getPairsByModel('waterfall', 'noclosed', 11))          && p('1') && e('项目集2 / 项目22'); // 获取所有瀑布项目
-r($tester->project->getPairsByModel('agileplus', 'noclosed,multiple', 11)) && p('5') && e('项目集4 / 项目34'); // 获取所有瀑布项目
+r($tester->project->getPairsByModel('all', 'noclosed', 50))                && p('21') && e('项目集1 / 项目21'); // 获取所有项目
+r($tester->project->getPairsByModel('waterfall', 'noclosed', 11))          && p('22') && e('项目集2 / 项目22'); // 获取所有瀑布项目
+r($tester->project->getPairsByModel('agileplus', 'noclosed,multiple', 11)) && p('34') && e('项目集4 / 项目34'); // 获取所有瀑布项目
