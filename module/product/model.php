c6c5aace1887171e98d33b44a9aceee3a1df2f43
dingguodong <dingguodong@easycorp.ltd>
2023-09-14 15:39:07 +0800

    + Add function to get accessible product and save it to session.

--- module/product/model.php
+++ module/product/model.php
@@ -96,6 +96,19 @@ class productModel extends model
     }
 
     /**
+     * 获取可访问的产品，并保存到session中。
+     * Get accessible product and save it to session.
+     *
+     * @param  int   $productID
+     * @param  array $products
+     * @return int
+     */
+    public function checkAccess(int $productID, array $products): int
+    {
+        return $this->saveState($productID, $products);
+    }
+
+    /**
      * 检查是否有权限访问该产品。
      * Check privilege.
      *
