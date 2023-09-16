66c5609655987abd888b793700e44068d59332c5
wangxuepeng <wangxuepeng@easycorp.ltd>
2023-09-16 08:30:52 +0000

    * Unit test of the getbyid method of action module.

--- module/action/test/model/getbyid.php
+++ module/action/test/model/getbyid.php
@@ -4,14 +4,7 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/action.class.php';
 su('admin');
 
-$action = zdTable('action');
-$action->id->range('101-105');
-$action->objectType->range("product,story,productplan,release,project");
-$action->action->range("common,extra,opened,created,changed");
-$action->comment->range("1-5")->prefix("这是一个系统日志测试备注");
-$action->gen(5);
-
-//zdTable('action')->gen(5);
+zdTable('action')->config('action')->gen(5);
 
 /**
 
@@ -31,8 +24,8 @@ $actionIDList = array('101', '102', '103', '104', '105');
 
 $action = new actionTest();
 
-r($action->getByIdTest($actionIDList[0])) && p('objectType,objectId,action,comment') && e('product,,common,这是一个系统日志测试备注1');     // 测试获取动态1的信息
-r($action->getByIdTest($actionIDList[1])) && p('objectType,objectId,action,comment') && e('story,,extra,这是一个系统日志测试备注2');        // 测试获取动态2的信息
-r($action->getByIdTest($actionIDList[2])) && p('objectType,objectId,action,comment') && e('productplan,,opened,这是一个系统日志测试备注3'); // 测试获取动态3的信息
-r($action->getByIdTest($actionIDList[3])) && p('objectType,objectId,action,comment') && e('release,,created,这是一个系统日志测试备注4');    // 测试获取动态4的信息
-r($action->getByIdTest($actionIDList[4])) && p('objectType,objectId,action,comment') && e('project,,changed,这是一个系统日志测试备注5');    // 测试获取动态5的信息
+r($action->getByIdTest($actionIDList[0])) && p('objectType,objectId,action,comment') && e('product,~~,common,这是一个系统日志测试备注1');     // 测试获取动态1的信息
+r($action->getByIdTest($actionIDList[1])) && p('objectType,objectId,action,comment') && e('story,~~,extra,这是一个系统日志测试备注2');        // 测试获取动态2的信息
+r($action->getByIdTest($actionIDList[2])) && p('objectType,objectId,action,comment') && e('productplan,~~,opened,这是一个系统日志测试备注3'); // 测试获取动态3的信息
+r($action->getByIdTest($actionIDList[3])) && p('objectType,objectId,action,comment') && e('release,~~,created,这是一个系统日志测试备注4');    // 测试获取动态4的信息
+r($action->getByIdTest($actionIDList[4])) && p('objectType,objectId,action,comment') && e('project,~~,changed,这是一个系统日志测试备注5');    // 测试获取动态5的信息
