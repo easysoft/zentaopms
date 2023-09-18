1feb0c65ba3e9d387a19cbc6880b6a6b3bec5244
liumengyi <liumengyi@easycorp.ltd>
2023-09-18 13:57:10 +0800

    * Modify unit test of testcaseModel::getBySearch.

--- module/testcase/test/model/getbysearch.php
+++ module/testcase/test/model/getbysearch.php
@@ -4,6 +4,7 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/testcase.class.php';
 
 zdTable('case')->gen('20');
+zdTable('projectcase')->gen('20');
 zdTable('user')->gen('1');
 
 su('admin');
@@ -17,8 +18,8 @@ cid=1
 */
 
 $tabList       = array('qa', 'project');
-$projectIdList = array('0', '1', '2');
-$productIdList = array('0', '1', '2');
+$projectIdList = array('0', '101');
+$productIdList = array('0', '1');
 $branchIdList  = array(0, 'all');
 $autoList      = array('no', 'auto');
 $orderByList   = array('id_desc', 'id_asc');
@@ -33,13 +34,13 @@ r($testcase->getBySearchTest($tabList[0], $projectIdList[0], $productIdList[0], 
 r($testcase->getBySearchTest($tabList[0], $projectIdList[0], $productIdList[0], $branchIdList[1], 0, $autoList[0], $orderByList[1])) && p() && e('1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20'); // 测试搜索 qa 下 项目 0 产品 0 分支 all orderBy id_asc 下的用例
 r($testcase->getBySearchTest($tabList[0], $projectIdList[0], $productIdList[0], $branchIdList[0], 0, $autoList[1], $orderByList[1])) && p() && e('0');                                                  // 测试搜索 qa 下 项目 0 产品 0 auto auto orderBy id_asc 下的用例
 r($testcase->getBySearchTest($tabList[0], $projectIdList[0], $productIdList[0], $branchIdList[1], 0, $autoList[1], $orderByList[1])) && p() && e('0');                                                  // 测试搜索 qa 下 项目 0 产品 0 分支 all auto auto orderBy id_asc 下的用例
-r($testcase->getBySearchTest($tabList[0], $projectIdList[1], $productIdList[0], $branchIdList[0], 0, $autoList[0], $orderByList[0])) && p() && e('20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1'); // 测试搜索 qa 下 项目 1 产品 0 下的用例
+r($testcase->getBySearchTest($tabList[0], $projectIdList[1], $productIdList[0], $branchIdList[0], 0, $autoList[0], $orderByList[0])) && p() && e('20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1'); // 测试搜索 qa 下 项目 101 产品 0 下的用例
 r($testcase->getBySearchTest($tabList[0], $projectIdList[0], $productIdList[0], $branchIdList[0], 0, $autoList[0], $orderByList[0])) && p() && e('20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1'); // 测试搜索 qa 下 项目 0 产品 0 下的用例
 
 r($testcase->getBySearchTest($tabList[0], $projectIdList[0], $productIdList[1])) && p() && e('4,3,2,1'); // 测试搜索 qa 下 项目 0 产品 1 下的用例
-r($testcase->getBySearchTest($tabList[0], $projectIdList[1], $productIdList[1])) && p() && e('4,3,2,1'); // 测试搜索 qa 下 项目 1 产品 1 下的用例
+r($testcase->getBySearchTest($tabList[0], $projectIdList[1], $productIdList[1])) && p() && e('4,3,2,1'); // 测试搜索 qa 下 项目 101 产品 1 下的用例
 
-r($testcase->getBySearchTest($tabList[1], $projectIdList[0], $productIdList[0])) && p() && e('0'); // 测试搜索 project 下 项目 0 产品 0 下的用例
-r($testcase->getBySearchTest($tabList[1], $projectIdList[1], $productIdList[0])) && p() && e('0'); // 测试搜索 project 下 项目 1 产品 0 下的用例
-r($testcase->getBySearchTest($tabList[1], $projectIdList[0], $productIdList[1])) && p() && e('0'); // 测试搜索 project 下 项目 0 产品 1 下的用例
-r($testcase->getBySearchTest($tabList[1], $projectIdList[1], $productIdList[1])) && p() && e('0'); // 测试搜索 project 下 项目 1 产品 1 下的用例
+r($testcase->getBySearchTest($tabList[1], $projectIdList[0], $productIdList[0])) && p() && e('0');       // 测试搜索 project 下 项目 0 产品 0 下的用例
+r($testcase->getBySearchTest($tabList[1], $projectIdList[1], $productIdList[0])) && p() && e('4,3,2,1'); // 测试搜索 project 下 项目 101 产品 0 下的用例
+r($testcase->getBySearchTest($tabList[1], $projectIdList[0], $productIdList[1])) && p() && e('0');       // 测试搜索 project 下 项目 0 产品 1 下的用例
+r($testcase->getBySearchTest($tabList[1], $projectIdList[1], $productIdList[1])) && p() && e('4,3,2,1'); // 测试搜索 project 下 项目 101 产品 1 下的用例
