42e955d3482898cac97dad587d5286b740b08974
tianshujie <tianshujie@easycorp.ltd>
2023-09-18 16:02:41 +0800

    * Init user data for case of the getProducts method.

--- module/product/test/model/getproducts.php
+++ module/product/test/model/getproducts.php
@@ -3,6 +3,7 @@
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/product.class.php';
 
+zdTable('user')->gen(5);
 zdTable('product')->gen(10);
 zdTable('project')->gen(20);
 zdTable('projectproduct')->gen(50);
@@ -19,7 +20,6 @@ $projectIdList = array(11, 12, 13, 14, 15, 1000001);
 $statusList    = array('all', 'noclosed');
 
 $product = new productTest('admin');
-
 r($product->getProductsTest($projectIdList[0], $statusList[0])) && p('1:name') && e('正常产品1'); // 测试获取项目11 状态为all的产品信息
 r($product->getProductsTest($projectIdList[1], $statusList[0])) && p('2:name') && e('正常产品2'); // 测试获取项目12 状态为all的产品信息
 r($product->getProductsTest($projectIdList[2], $statusList[0])) && p('3:name') && e('正常产品3'); // 测试获取项目13 状态为all的产品信息
