a6cf425cbf692bb2c76f6682b82c407bdb529e0a
daitingting <daitingting@xirangit.com>
2023-09-18 05:46:05 +0000

    * Update expect.

--- module/testcase/test/tao/doupdate.php
+++ module/testcase/test/tao/doupdate.php
@@ -4,12 +4,26 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/testcase.class.php';
 su('admin');
 
+zdTable('case')->gen(10);
+
 /**
 
 title=测试 testcaseModel->doUpdate();
 timeout=0
 cid=1
 
+- 测试更新用例名称
+ - 属性title @修改后的用例
+ - 属性type @feature
+ - 属性pri @1
+- 测试更新用例类型
+ - 属性title @这个是测试用例2
+ - 属性type @install
+ - 属性pri @2
+- 测试更新用例优先级
+ - 属性title @这个是测试用例3
+ - 属性type @config
+ - 属性pri @1
 
 */
 
@@ -20,6 +34,6 @@ $type  = array('type'  => 'install');
 $pri   = array('pri'   => '1');
 
 $testcase = new testCaseTest();
-r($testcase->doUpdateTest($testcaseIdList[0], $title)) && p('title,type,pri') && e('修改后的用例,feature,3');      // 测试更新用例名称
-r($testcase->doUpdateTest($testcaseIdList[1], $type))  && p('title,type,pri') && e('测试创建测试用例2,install,1'); // 测试更新用例类型
-r($testcase->doUpdateTest($testcaseIdList[2], $pri))   && p('title,type,pri') && e('测试创建测试用例3,feature,1'); // 测试更新用例优先级
+r($testcase->doUpdateTest($testcaseIdList[0], $title)) && p('title,type,pri') && e('修改后的用例,feature,1');    // 测试更新用例名称
+r($testcase->doUpdateTest($testcaseIdList[1], $type))  && p('title,type,pri') && e('这个是测试用例2,install,2'); // 测试更新用例类型
+r($testcase->doUpdateTest($testcaseIdList[2], $pri))   && p('title,type,pri') && e('这个是测试用例3,config,1');  // 测试更新用例优先级
