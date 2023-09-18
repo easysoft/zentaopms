8f5ffae4a6810908858df63f355b99798875198d
tianshujie <tianshujie@easycorp.ltd>
2023-09-18 16:00:42 +0800

    * Change the case for the getProductPairsByProject method.

--- module/product/test/model/getproductpairsbyproject.php
+++ module/product/test/model/getproductpairsbyproject.php
@@ -4,19 +4,21 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/product.class.php';
 
 /**
+
 title=测试通过项目查询产品信息 productModel->getProductPairsByProject();
+timeout=0
 cid=1
-pid=1
+
 */
 
 zdTable('project')->gen(50);
+zdTable('user')->gen(5);
 zdTable('projectproduct')->gen(50);
 
 $projectIDList = array('11', '12', '13', '14', '15', '1000001');
 $statusList    = array('all', 'noclosed');
 
 $product = new productTest('admin');
-
 r($product->getProductPairsByProjectTest($projectIDList[0], $statusList[0])) && p('1') && e('正常产品1'); // 测试获取项目11 状态为all的产品信息
 r($product->getProductPairsByProjectTest($projectIDList[1], $statusList[0])) && p('2') && e('正常产品2'); // 测试获取项目12 状态为all的产品信息
 r($product->getProductPairsByProjectTest($projectIDList[2], $statusList[0])) && p('3') && e('正常产品3'); // 测试获取项目13 状态为all的产品信息
