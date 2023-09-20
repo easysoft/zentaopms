0d0d557024d77e2a72977fffa674b5272c8c53e0
liumengyi <liumengyi@easycorp.ltd>
2023-09-20 09:31:41 +0800

    * Modify unit test of caselibModel::delete.

--- module/caselib/test/model/delete.php
+++ module/caselib/test/model/delete.php
@@ -2,6 +2,10 @@
 <?php
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/caselib.class.php';
+
+zdTable('testsuite')->gen(5);
+zdTable('user')->gen(1);
+
 su('admin');
 
 /**
@@ -10,10 +14,14 @@ title=测试 caselibModel->delete();
 timeout=0
 cid=1
 
-- 测试删除之后deleted值是否为1属性deleted @1
-
 */
+
+$caselibIdList = array(1, 2, 3, 4, 5);
 
 $caselib = new caselibTest();
 
-r($caselib->deleteTest(1)) && p('deleted') && e('1');  //测试删除之后deleted值是否为1
+r($caselib->deleteTest($caselibIdList[0])) && p('deleted') && e('1');  // 测试删除之后用例库 1 deleted值是否为1
+r($caselib->deleteTest($caselibIdList[1])) && p('deleted') && e('1');  // 测试删除之后用例库 2 deleted值是否为1
+r($caselib->deleteTest($caselibIdList[2])) && p('deleted') && e('1');  // 测试删除之后用例库 3 deleted值是否为1
+r($caselib->deleteTest($caselibIdList[3])) && p('deleted') && e('1');  // 测试删除之后用例库 4 deleted值是否为1
+r($caselib->deleteTest($caselibIdList[4])) && p('deleted') && e('1');  // 测试删除之后用例库 5 deleted值是否为1
