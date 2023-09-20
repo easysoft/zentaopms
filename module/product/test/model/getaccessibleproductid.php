934c3072aadd2c9c711faf9f21c9ea80ed726515
caoyanyi <caoyanyi@easycorp.ltd>
2023-09-20 16:07:23 +0800

    + Add unit test for product getAccessibleProductID.

---
+++ module/product/test/model/getaccessibleproductid.php
@@ -0,0 +1,24 @@
+#!/usr/bin/env php
+<?php
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+
+zdTable('product')->gen(10);
+su('admin');
+
+/**
+
+title=测试 productModel::getAccessibleProductID;
+timeout=0
+cid=1
+
+*/
+
+global $tester;
+$tester->loadModel('product');
+
+$products = array(1 => 1, 2 => 2, 6 => 6);
+$idList   = array(0, 6, 10);
+r($tester->product->getAccessibleProductID($idList[0], $products)) && p() && e('1');  //不传入ID
+r($tester->product->getAccessibleProductID($idList[1], $products)) && p() && e('6');  //传入存在ID的值
+r($tester->product->getAccessibleProductID($idList[0], $products)) && p() && e('1');  //不传入ID，读取session信息
+r($tester->product->getAccessibleProductID($idList[2], $products)) && p() && e('10'); //传入正确的ID
