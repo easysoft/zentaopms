afcd95268dbda86832e24c77539dc68219517608
wangxuepeng <wangxuepeng@easycorp.ltd>
2023-09-20 11:34:28 +0000

    * Fixed method getHisotry for action module, support array and int.

--- module/action/model.php
+++ module/action/model.php
@@ -812,11 +812,11 @@ class actionModel extends model
      * 获取一个操作的历史记录。
      * Get histories of an action.
      *
-     * @param  array  $actionID
+     * @param  array|int  $actionID
      * @access public
      * @return array
      */
-    public function getHistory(array $actionID): array
+    public function getHistory(array|int $actionID): array
     {
         return $this->dao->select()->from(TABLE_HISTORY)->where('action')->in($actionID)->fetchGroup('action');
     }
