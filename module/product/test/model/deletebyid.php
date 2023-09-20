cbe1aaee692390a61c0157834b8541e1184d5352
caoyanyi <caoyanyi@easycorp.ltd>
2023-09-20 09:19:17 +0800

    * Move unit test to model.

--- module/product/test/model/deletebyid.php
+++ module/product/test/model/deletebyid.php
@@ -2,20 +2,34 @@
 <?php
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/product.class.php';
-su('admin');
 
-zdTable('product')->gen(5);
+function initData()
+{
+    zdTable('product')->config('product')->gen(10);
+    zdTable('doclib')->config('doclib')->gen(10);
+}
+initData();
 
 /**
+title=productTao->deleteByID();
+cid=2
 
-title=测试productModel->deleteByID();
-timeout=0
-cid=1
-
-*/
+ */
 
 $product = new productTest('admin');
 
-r($product->deleteByIdTest(0))  && p()               && e('0');           // ID为空
-r($product->deleteByIdTest(1))  && p('name,deleted') && e('正常产品1,1'); // 正常删除
-r($product->deleteByIdTest(10)) && p()               && e('0');           // 不存在的ID
+$products = $product->objectModel->getByIdList(array(1,2,3,4,5));
+r($products[3]->deleted) && p('') && e('0');
+
+$doclib = $product->objectModel->dao->select('deleted')->from(TABLE_DOCLIB)->where('product')->eq(3)->andWhere('deleted')->eq(0)->fetchAll();
+r(count($doclib)) && p('') && e(5);
+
+/* Delete product 3. */
+$product->objectModel->deleteByID(3);
+
+$products = $product->objectModel->getByIdList(array(1,2,3,4,5));
+r($products[3]->deleted) && p('') && e('1');
+
+$product->objectModel->dao->reset();
+$doclib = $product->objectModel->dao->select('deleted')->from(TABLE_DOCLIB)->where('product')->eq(3)->andWhere('deleted')->eq(1)->fetchAll();
+r(count($doclib)) && p('') && e(5);
