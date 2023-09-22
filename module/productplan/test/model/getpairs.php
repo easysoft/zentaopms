a084a11b86fa6e9d4eb79cbd2c2261a994d97f76
tianshujie <tianshujie@easycorp.ltd>
2023-09-22 15:53:09 +0800

    * Change the case result for getPairs method.

--- module/productplan/test/model/getpairs.php
+++ module/productplan/test/model/getpairs.php
@@ -33,7 +33,7 @@ r($planTester->getPairs($productIdList[1], $branchIdList[0], $paramList[1], $ski
 r($planTester->getPairs($productIdList[2], $branchIdList[0], $paramList[1], $skipParent[0])) && p()     && e('0');                                        // 获取产品6下的所有未过期计划
 r($planTester->getPairs($productIdList[0], $branchIdList[0], $paramList[2], $skipParent[0])) && p('3')  && e('计划3 [2022-01-01 ~ 2022-01-30]');          // 获取产品1下的所有未过期计划
 r($planTester->getPairs($productIdList[1], $branchIdList[0], $paramList[2], $skipParent[0])) && p('5')  && e('计划5 待定');                               // 获取产品2下的所有未过期计划
-r($planTester->getPairs($productIdList[2], $branchIdList[0], $paramList[2], $skipParent[0])) && p('18') && e('计划18 [2022-01-01 ~ 2022-01-30] / 分支3'); // 获取产品6下的所有未过期计划
+r($planTester->getPairs($productIdList[2], $branchIdList[0], $paramList[2], $skipParent[0])) && p('18') && e('计划18 [2022-01-01 ~ 2022-01-30] / 主干');  // 获取产品6下的所有未过期计划
 r($planTester->getPairs($productIdList[0], $branchIdList[0], $paramList[0], $skipParent[1])) && p('3')  && e('计划3 [2022-01-01 ~ 2022-01-30]');          // 获取产品1下的所有非父计划
 r($planTester->getPairs($productIdList[1], $branchIdList[0], $paramList[0], $skipParent[1])) && p('5')  && e('计划5 待定');                               // 获取产品2下的所有非父计划
 r($planTester->getPairs($productIdList[2], $branchIdList[0], $paramList[0], $skipParent[1])) && p('16') && e('计划16 [2021-01-01 ~ 2021-01-30] / 主干');  // 获取产品6下的所有非父计划
