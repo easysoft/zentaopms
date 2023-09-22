a167676e3baab3d91ecc0751a3df5ffedddde47a
wangxuepeng <wangxuepeng@easycorp.ltd>
2023-09-22 08:35:59 +0000

    * Add unit test for action getTrashObjectTypes.

--- module/action/test/model/gettrashobjecttypes.php
+++ module/action/test/model/gettrashobjecttypes.php
@@ -4,14 +4,16 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/action.class.php';
 su('admin');
 
+zdTable('action')->config('action')->gen(20);
+
 /**
 
 title=测试 actionModel->getTrashObjectTypes();
 cid=1
 pid=1
 
-查询type all 的对象类型列表 >> story
-查询type all 的对象类型列表 >> testcase
+查询type all    的对象类型列表 >> story
+查询type hidden 的对象类型列表 >> testcase
 
 */
 
@@ -19,5 +21,5 @@ $typeList = array('all', 'hidden');
 
 $action = new actionTest();
 
-r($action->getTrashObjectTypesTest($typeList[0])) && p('story:objectType') && e('story');       // 查询type all 的对象类型列表
-r($action->getTrashObjectTypesTest($typeList[1])) && p('testcase:objectType') && e('testcase'); // 查询type all 的对象类型列表
+r($action->getTrashObjectTypesTest($typeList[0])) && p('story:objectType')    && e('story');    // 查询type all    的对象类型列表
+r($action->getTrashObjectTypesTest($typeList[1])) && p('testcase:objectType') && e('testcase'); // 查询type hidden 的对象类型列表
