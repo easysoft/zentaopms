c18f17e3fee8387a268527bfe212b4f769cbadb6
daitingting <daitingting@xirangit.com>
2023-09-22 07:08:39 +0000

    * Refactor the createFromImport function.

--- module/caselib/model.php
+++ module/caselib/model.php
@@ -263,178 +263,32 @@ class caselibModel extends model
     }
 
     /**
+     * 创建导入的用例。
      * Create from import.
      *
      * @param  int    $libID
      * @access public
      * @return void
      */
-    public function createFromImport($libID)
+    public function createFromImport(int $libID)
     {
-        $this->loadModel('action');
-        $this->loadModel('testcase');
-        $this->loadModel('file');
-        $now  = helper::now();
         $data = fixer::input('post')->get();
 
-        if(!empty($_POST['id']))
-        {
-            $oldSteps = $this->dao->select('t2.*')->from(TABLE_CASE)->alias('t1')
-                ->leftJoin(TABLE_CASESTEP)->alias('t2')->on('t1.id = t2.case')
-                ->where('t1.id')->in(($_POST['id']))
-                ->andWhere('t1.version=t2.version')
-                ->orderBy('t2.id')
-                ->fetchGroup('case');
-            $oldCases = $this->dao->select('*')->from(TABLE_CASE)->where('id')->in($_POST['id'])->fetchAll('id');
-        }
+        /* 获取表单的用例数据，并且检查必填。*/
+        /* Get the form data and check required. */
+        $cases = $this->caselibTao->initImportedCase($data);
+        if(dao::isError()) return false;
 
-        $cases = array();
-        $line  = 1;
-        foreach($data->lib as $key => $lib)
-        {
-            $caseData = new stdclass();
-
-            $caseData->lib          = $lib;
-            $caseData->module       = $data->module[$key];
-            $caseData->title        = $data->title[$key];
-            $caseData->pri          = (int)$data->pri[$key];
-            $caseData->type         = $data->type[$key];
-            $caseData->stage        = join(',', $data->stage[$key]);
-            $caseData->keywords     = $data->keywords[$key];
-            $caseData->frequency    = 1;
-            $caseData->precondition = $data->precondition[$key];
-
-            if(isset($this->config->testcase->create->requiredFields))
-            {
-                $requiredFields = explode(',', $this->config->testcase->create->requiredFields);
-                foreach($requiredFields as $requiredField)
-                {
-                    $requiredField = trim($requiredField);
-                    if(!isset($caseData->$requiredField)) continue;
-                    if(empty($caseData->$requiredField)) dao::$errors[] = sprintf($this->lang->testcase->noRequire, $line, $this->lang->testcase->$requiredField);
-                }
-            }
-
-            $cases[$key] = $caseData;
-            $line++;
-        }
-        if(dao::isError()) helper::end(js::error(dao::getError()));
-
-        $forceNotReview = $this->testcase->forceNotReview();
+        $forceNotReview = $this->loadModel('testcase')->forceNotReview();
         foreach($cases as $key => $caseData)
         {
-            if(!empty($_POST['id'][$key]) and empty($_POST['insert']))
+            if($this->post->insert)
             {
-                $caseID      = $data->id[$key];
-                $stepChanged = false;
-                $oldStep     = isset($oldSteps[$caseID]) ? $oldSteps[$caseID] : array();
-                $oldCase     = $oldCases[$caseID];
-
-                /* Ignore updating cases for different libs. */
-                if($oldCase->lib != $caseData->lib) continue;
-
-                /* Remove the empty setps in post. */
-                $steps = array();
-                if(isset($_POST['desc'][$key]))
-                {
-                    foreach($data->desc[$key] as $id => $desc)
-                    {
-                        $desc = trim($desc);
-                        if(empty($desc)) continue;
-                        $step = new stdclass();
-                        $step->type   = $data->stepType[$key][$id];
-                        $step->desc   = htmlSpecialString($desc);
-                        $step->expect = htmlSpecialString(trim($data->expect[$key][$id]));
-
-                        $steps[] = $step;
-                    }
-                }
-
-                /* If step count changed, case changed. */
-                if((!$oldStep != !$steps) or (count($oldStep) != count($steps)))
-                {
-                    $stepChanged = true;
-                }
-                else
-                {
-                    /* Compare every step. */
-                    foreach($oldStep as $id => $oldStep)
-                    {
-                        if(trim($oldStep->desc) != trim($steps[$id]->desc) or trim($oldStep->expect) != $steps[$id]->expect)
-                        {
-                            $stepChanged = true;
-                            break;
-                        }
-                    }
-                }
-
-                $version           = $stepChanged ? $oldCase->version + 1 : $oldCase->version;
-                $caseData->version = $version;
-                $changes           = common::createChanges($oldCase, $caseData);
-                if(!$changes and !$stepChanged) continue;
-
-                if($changes or $stepChanged)
-                {
-                    $caseData->lastEditedBy   = $this->app->user->account;
-                    $caseData->lastEditedDate = $now;
-                    if($stepChanged and !$forceNotReview) $caseData->status = 'wait';
-                    $this->dao->update(TABLE_CASE)->data($caseData)->where('id')->eq($caseID)->autoCheck()->exec();
-                    if($stepChanged)
-                    {
-                        $parentStepID = 0;
-                        foreach($steps as $id => $step)
-                        {
-                            $step = (array)$step;
-                            if(empty($step['desc'])) continue;
-                            $stepData = new stdclass();
-                            $stepData->type    = ($step['type'] == 'item' and $parentStepID == 0) ? 'step' : $step['type'];
-                            $stepData->parent  = ($stepData->type == 'item') ? $parentStepID : 0;
-                            $stepData->case    = $caseID;
-                            $stepData->version = $version;
-                            $stepData->desc    = $step['desc'];
-                            $stepData->expect  = $step['expect'];
-                            $this->dao->insert(TABLE_CASESTEP)->data($stepData)->autoCheck()->exec();
-                            if($stepData->type == 'group') $parentStepID = $this->dao->lastInsertID();
-                            if($stepData->type == 'step')  $parentStepID = 0;
-                        }
-                    }
-                    $oldCase->steps  = $this->testcase->joinStep($oldStep);
-                    $caseData->steps = $this->testcase->joinStep($steps);
-                    $changes  = common::createChanges($oldCase, $caseData);
-                    $actionID = $this->action->create('case', $caseID, 'Edited');
-                    $this->action->logHistory($actionID, $changes);
-                }
+                $this->caselibTao->insertImportedCase($key, $caseData, $data, $forceNotReview);
             }
             else
             {
-                $caseData->project    = (int)$this->session->project;
-                $caseData->version    = 1;
-                $caseData->openedBy   = $this->app->user->account;
-                $caseData->openedDate = $now;
-                $caseData->status     = $forceNotReview ? 'normal' : 'wait';
-                $this->dao->insert(TABLE_CASE)->data($caseData)->autoCheck()->exec();
-
-                if(!dao::isError())
-                {
-                    $caseID       = $this->dao->lastInsertID();
-                    $parentStepID = 0;
-                    foreach($data->desc[$key] as $id => $desc)
-                    {
-                        $desc = trim($desc);
-                        if(empty($desc)) continue;
-                        $stepData = new stdclass();
-                        $stepData->type    = ($data->stepType[$key][$id] == 'item' and $parentStepID == 0) ? 'step' : $data->stepType[$key][$id];
-                        $stepData->parent  = ($stepData->type == 'item') ? $parentStepID : 0;
-                        $stepData->case    = $caseID;
-                        $stepData->version = 1;
-                        $stepData->desc    = htmlSpecialString($desc);
-                        $stepData->expect  = htmlSpecialString(trim($this->post->expect[$key][$id]));
-                        $this->dao->insert(TABLE_CASESTEP)->data($stepData)->autoCheck()->exec();
-                        if($stepData->type == 'group') $parentStepID = $this->dao->lastInsertID();
-                        if($stepData->type == 'step')  $parentStepID = 0;
-                    }
-                    $this->action->create('case', $caseID, 'Opened');
-                }
+                $this->caselibTao->updateImportedCase($key, $caseData, $data, $forceNotReview);
             }
         }
 
