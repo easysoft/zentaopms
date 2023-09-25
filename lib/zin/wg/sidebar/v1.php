9356935b48acb37e1203ee0b83ef0d37ffbd49e6
sunhao <sunhao@easycorp.ltd>
2023-09-25 17:27:44 +0800

    * zin: set default maxWidth to sidebar.

--- lib/zin/wg/sidebar/v1.php
+++ lib/zin/wg/sidebar/v1.php
@@ -7,6 +7,8 @@ class sidebar extends wg
     protected static array $defineProps = array(
         'side?:string="left"',
         'width?:string|number=40',
+        'maxWidth?:string|number=400',
+        'minWidth?:string|number',
         'showToggle?:bool=true',
         'parent?:string',
         'preserve?:string'
@@ -14,7 +16,7 @@ class sidebar extends wg
 
     protected function build(): wg
     {
-        list($side, $showToggle, $width, $preserve, $parent) = $this->prop(array('side', 'showToggle', 'width', 'preserve', 'parent'));
+        list($side, $showToggle, $width, $preserve, $parent, $maxWidth, $minWidth) = $this->prop(array('side', 'showToggle', 'width', 'preserve', 'parent', 'maxWidth', 'minWidth'));
         if($preserve === null)
         {
             global $app;
@@ -24,7 +26,7 @@ class sidebar extends wg
         (
             setClass('sidebar'),
             width($width),
-            setData(array('zui' => 'sidebar', 'side' => $side, 'toggleBtn' => $showToggle, 'preserve' => $preserve, 'parent' => $parent)),
+            setData(array('zui' => 'sidebar', 'side' => $side, 'toggleBtn' => $showToggle, 'preserve' => $preserve, 'parent' => $parent, 'maxWidth' => $maxWidth, 'minWidth' => $minWidth)),
             set($this->getRestProps()),
             $this->children(),
         );
