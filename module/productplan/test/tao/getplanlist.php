ef48136671d154b1ec504478ba6ed56343d3cadf
caoyanyi <caoyanyi@easycorp.ltd>
2023-09-25 11:30:19 +0800

    + Add unit test for productplan tao.

---
+++ module/productplan/test/tao/getplanlist.php
@@ -0,0 +1,22 @@
+#!/usr/bin/env php
+<?php
+/**
+
+title=测试 productplanTao->getPlanList()
+timeout=0
+cid=1
+
+*/
+
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+
+zdTable('productplan')->config('productplan')->gen(20);
+
+global $tester;
+$productplan = $tester->loadModel('productplan');
+
+r($productplan->getPlanList(array()))                               && p()          && e('0');       // 获取空数据
+r($productplan->getPlanList(array(1), '0', 'undone'))               && p('1:title') && e('计划1');   // 获取product=1的未完成的计划
+r($productplan->getPlanList(array(1), '0', 'undone', 'skipparent')) && p('2:title') && e('计划2');   // 获取product=1的所有的非父计划
+r($productplan->getPlanList(array(1), '1', 'all'))                  && p()          && e('0');       // 获取product=1的分支是1的计划
+r($productplan->getPlanList(array(1), '0', 'wait'))                 && p('1:title') && e('计划1'); // 获取product=1的所有未开始的计划
---
+++ module/productplan/test/tao/getplanprojects.php
@@ -0,0 +1,25 @@
+#!/usr/bin/env php
+<?php
+/**
+
+title=测试 productplanTao->getPlanProjects()
+timeout=0
+cid=1
+
+*/
+
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+
+zdTable('project')->config('execution')->gen(20);
+$projectproduct = zdTable('projectproduct')->config('projectproduct');
+$projectproduct->project->range('101-120');
+$projectproduct->gen(20);
+
+global $tester;
+$productplan = $tester->loadModel('productplan');
+
+r($productplan->getPlanProjects(array()))                 && p()                   && e('0');         // 获取空数据
+r($productplan->getPlanProjects(array(1))[1])             && p('101:project,name') && e('101,迭代5'); // 获取计划是1的项目
+r($productplan->getPlanProjects(array(1, 4), 0)[1])       && p()                   && e('0');         // 获取计划是1,4，产品是0的项目
+r($productplan->getPlanProjects(array(1, 4), 2)[4])       && p('102:project,name') && e('102,迭代6'); // 获取计划是1,4，产品是2的项目
+r($productplan->getPlanProjects(array(1, 4, 7), null)[7]) && p('103:project,name') && e('103,迭代7'); // 获取计划是1,4,7的项目
