2cb0ad1df4d021f283d3de273405e50fc0253c35
tianshujie <tianshujie@easycorp.ltd>
2023-09-27 15:01:01 +0800

    * Refactor case for the getBuildPairs method.

--- module/build/test/model/getbuildpairs.php
+++ module/build/test/model/getbuildpairs.php
@@ -1,38 +1,47 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . '/test/lib/init.php';
-include dirname(__FILE__, 2) . '/build.class.php';
-su('admin');
-
 /**
 
 title=测试 buildModel->getBuildPairs();
+timeout=0
 cid=1
-pid=1
-
-数组产品执行版本查询 >> 执行版本版本17
-单长产品执行版本查询 >> 执行版本版本11
-项目执行版本查询 >> 主干
-数组产品执行版本查询统计 >> 6
-单长产品执行版本查询统计 >> 4
-项目执行版本查询统计 >> 1
 
 */
 
-$products     = array('0', '1', '7');
-$branch       = 'all';
-$params       = array('noterminate','nodone', 'noempty', 'notrunk');
-$objectID     = array('0', '7', '17');
-$objectType   = array('execution', 'project', '');
-$buildIdList  = array('1', '7', '17');
-$replace      = array(true, false);
-$count        = array('0', '1');
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+include dirname(__FILE__, 2) . '/build.class.php';
 
-$build = new buildTest();
+zdTable('build')->config('build')->gen(10);
+zdTable('project')->config('execution')->gen(30);
+zdTable('branch')->config('branch')->gen(5);
+zdTable('release')->config('release')->gen(10);
+zdTable('product')->config('product')->gen(5);
+zdTable('user')->gen(5);
+su('admin');
 
-r($build->getBuildPairsTest($count[0], $products, $branch, $params[0], $objectID[0], $objectType[0], '',$replace[0]))              && p('17')    && e('执行版本版本17');//数组产品执行版本查询
-r($build->getBuildPairsTest($count[0], $products[1], $branch, $params[1], $objectID[0], $objectType[0], $buildIdList,$replace[0])) && p('11')    && e('执行版本版本11');//单长产品执行版本查询
-r($build->getBuildPairsTest($count[0], $products, $branch, $params[2], $objectID[1], $objectType[1], $buildIdList,$replace[0]))    && p('trunk') && e('主干');          //项目执行版本查询
-r($build->getBuildPairsTest($count[1], $products, $branch, $params[0], $objectID[0], $objectType[0], '',$replace[0]))              && p()        && e('6');             //数组产品执行版本查询统计
-r($build->getBuildPairsTest($count[1], $products[1], $branch, $params[1], $objectID[0], $objectType[0], $buildIdList,$replace[0])) && p()        && e('4');             //单长产品执行版本查询统计
-r($build->getBuildPairsTest($count[1], $products, $branch, $params[2], $objectID[1], $objectType[1], $buildIdList,$replace[0]))    && p()        && e('1');             //项目执行版本查询统计
+$productIdList[0] = array();
+$productIdList[1] = range(1, 5);
+$branchIdList     = array('all', '1');
+$paramsList       = array('noempty', 'notrunk', 'noterminate', 'withbranch', 'hasproject', 'noDeleted', 'singled', 'noreleased', 'releasedtag');
+$objectIdList     = array(0, 11, 101);
+$objectTypeList   = array('', 'project', 'execution');
+$buildIdList      = array('', '1,2,3,4,5');
+$replaceList      = array(false, true);
+
+global $tester;
+$tester->loadModel('build');
+r($tester->build->getBuildPairs($productIdList[0], $branchIdList[0], $paramsList[0], $objectIdList[0], $objectTypeList[0], $buildIdList[0], $replaceList[0])) && p('trunk') && e('主干');  // 测试产品为空时，获取版本数据
+r($tester->build->getBuildPairs($productIdList[1], $branchIdList[0], $paramsList[0], $objectIdList[0], $objectTypeList[0], $buildIdList[0], $replaceList[0])) && p('trunk') && e('主干');  // 测试产品1-5下的版本数据
+r($tester->build->getBuildPairs($productIdList[0], $branchIdList[1], $paramsList[0], $objectIdList[0], $objectTypeList[0], $buildIdList[0], $replaceList[0])) && p('trunk') && e('主干');  // 测试分支1下的版本数据
+r($tester->build->getBuildPairs($productIdList[0], $branchIdList[0], $paramsList[1], $objectIdList[0], $objectTypeList[0], $buildIdList[0], $replaceList[0])) && p('1')     && e('版本1'); // 测试不包含trunk的版本数据
+r($tester->build->getBuildPairs($productIdList[0], $branchIdList[0], $paramsList[2], $objectIdList[0], $objectTypeList[0], $buildIdList[0], $replaceList[0])) && p('1')     && e('版本1'); // 测试不包含停止维护的版本数据
+r($tester->build->getBuildPairs($productIdList[0], $branchIdList[0], $paramsList[3], $objectIdList[0], $objectTypeList[0], $buildIdList[0], $replaceList[0])) && p('1')     && e('版本1'); // 测试包含分支的版本数据
+r($tester->build->getBuildPairs($productIdList[0], $branchIdList[0], $paramsList[4], $objectIdList[0], $objectTypeList[0], $buildIdList[0], $replaceList[0])) && p('1')     && e('版本1'); // 测试包含项目的版本数据
+r($tester->build->getBuildPairs($productIdList[0], $branchIdList[0], $paramsList[5], $objectIdList[0], $objectTypeList[0], $buildIdList[0], $replaceList[0])) && p('1')     && e('版本1'); // 测试不包含删除的版本数据
+r($tester->build->getBuildPairs($productIdList[0], $branchIdList[0], $paramsList[6], $objectIdList[0], $objectTypeList[0], $buildIdList[0], $replaceList[0])) && p('1')     && e('版本1'); // 测试关联执行的版本数据
+r($tester->build->getBuildPairs($productIdList[0], $branchIdList[0], $paramsList[7], $objectIdList[0], $objectTypeList[0], $buildIdList[0], $replaceList[0])) && p('1')     && e('版本1'); // 测试不包含关联发布的版本数据
+r($tester->build->getBuildPairs($productIdList[0], $branchIdList[0], $paramsList[8], $objectIdList[0], $objectTypeList[0], $buildIdList[0], $replaceList[0])) && p('1')     && e('版本1'); // 测试包含发布标签的版本数据
+r($tester->build->getBuildPairs($productIdList[0], $branchIdList[0], $paramsList[0], $objectIdList[1], $objectTypeList[1], $buildIdList[0], $replaceList[0])) && p('1')     && e('版本1'); // 测试项目下的版本数据
+r($tester->build->getBuildPairs($productIdList[0], $branchIdList[0], $paramsList[0], $objectIdList[2], $objectTypeList[2], $buildIdList[0], $replaceList[0])) && p('1')     && e('版本1'); // 测试执行下的版本数据
+r($tester->build->getBuildPairs($productIdList[0], $branchIdList[0], $paramsList[0], $objectIdList[0], $objectTypeList[0], $buildIdList[1], $replaceList[0])) && p('2')     && e('版本2'); // 测试包含版本1-5的版本数据
+r($tester->build->getBuildPairs($productIdList[0], $branchIdList[0], $paramsList[0], $objectIdList[0], $objectTypeList[0], $buildIdList[0], $replaceList[1])) && p('1')     && e('版本1'); // 测试将发布名称替换为版本名称的数据
