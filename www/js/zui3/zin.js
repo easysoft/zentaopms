dd3117220eb795ba60aa26046f3926b1388698f0
sunhao <sunhao@easycorp.ltd>
2023-09-27 09:24:08 +0800

    * zin: load moduleMenu on loadTable.

--- www/js/zui3/zin.js
+++ www/js/zui3/zin.js
@@ -601,7 +601,7 @@
             target = urlInfo.moduleName ? `table-${urlInfo.moduleName}-${urlInfo.methodName}` : ($('.dtable').attr('id') || 'dtable');
         }
         if(target[0] !== '#' && target[0] !== '.') target = `#${target}`;
-        return loadComponent(target, $.extend({component: 'dtable', url: url, selector: `dtable/${target}:component,#featureBar>*`}, options));
+        return loadComponent(target, $.extend({component: 'dtable', url: url, selector: `dtable/${target}:component,#featureBar>*${$('#moduleMenu').length ? ',#moduleMenu' : ''}`}, options));
     }
 
     /**
