9f735072e448385a0421cb110a28fbb68586858b
qixinzhi <qixinzhi@easycorp.ltd>
2023-09-27 08:15:46 +0000

    * Update metric.

--- module/metric/calc/system/scale/count_of_undelayed_finished_execution_which_finished.php
+++ module/metric/calc/system/scale/count_of_undelayed_finished_execution_which_finished.php
@@ -1,17 +1,15 @@
 <?php
 /**
- * 按全局统计的已完成执行中按期完成执行数。
+ * 按系统统计的已完成执行中按期完成执行数。
  * Count of undelayed finished execution which finished.
  *
- * 范围：global
+ * 范围：system
  * 对象：execution
  * 目的：scale
- * 度量名称：按全局统计的已完成执行中按期完成执行数
+ * 度量名称：按系统统计的已完成执行中按期完成执行数
  * 单位：个
- * 描述：按全局统计的已完成执行中按时完成执行数表示在整个系统中按期完成的执行项的数量，可以用来评估团队的执行能力和效率。
+ * 描述：按系统统计的已完成执行中按时完成执行数表示在整个系统中按期完成执行的数量，可以用来评估团队的执行能力和效率。
  * 定义：所有的执行个数求和;状态为已关闭;关闭日期<=执行开始时计划截止日期;过滤已删除的执行;
- * 度量库：
- * 收集方式：realtime
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
  * @author    zhouxin <zhouxin@easycorp.ltd>
