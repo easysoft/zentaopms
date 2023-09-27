21778398f2f54e07fa74ea01ff0deb0513d80bcc
dingyongliang <dingyongliang@easycorp.ltd>
2023-09-27 16:41:06 +0800

    * Update zeneditor.

--- www/js/zeneditor/p-7646c878.js
+++
@@ -1,2 +0,0 @@
-ShadowRoot.prototype["createRange"]=document.createRange.bind(document);document.head.insertAdjacentHTML("beforeend",`<style> body > div.monaco-aria-container {display: none;} </style>`);const e=performance.getEntriesByType("resource").slice(-1)[0].name;document.head.insertAdjacentHTML("beforeend",`<link href="${e.replace(/(.*\/).*?$/,"$1")}codicon.css?t=${Date.now()}" rel="stylesheet" />`);
-//# sourceMappingURL=p-7646c878.js.map
