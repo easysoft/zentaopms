8ab0ee39f50999138b723681ccb97b7ed8ee0a42
daitingting <daitingting@xirangit.com>
2023-09-28 03:03:43 +0000

    * Split export function.

--- module/projectrelease/zen.php
+++ module/projectrelease/zen.php
@@ -12,7 +12,7 @@ class projectreleaseZen extends projectrelease
      * @access public
      * @return void
      */
-    public function commonAction(int $projectID = 0, int $productID = 0, int $branch = 0)
+    protected function commonAction(int $projectID = 0, int $productID = 0, int $branch = 0)
     {
         /* 获取当前项目的所有产品。*/
         /* Get product list by project. */
@@ -29,5 +29,104 @@ class projectreleaseZen extends projectrelease
         $this->view->branches = (isset($product->type) and $product->type == 'normal') ? array() : $this->loadModel('branch')->getPairs($productID, 'active', $projectID);
         $this->view->branch   = $branch;
         $this->view->project  = $this->project->getByID($projectID);
+    }
+
+    /**
+     * 生成关联的需求的 html。
+     * Generate story html.
+     *
+     * @access protected
+     * @return void
+     */
+    protected function generateStoryHtml(): string
+    {
+        $this->loadModel('story');
+
+        $html = "<h3>{$this->lang->release->stories}</h3>";
+
+        $fields = array('id' => $this->lang->story->id, 'title' => $this->lang->story->title);
+
+        /* 生成表头。*/
+        /* Generate thead.*/
+        $html .= '<table><tr>';
+        foreach($fields as $fieldLabel) $html .= "<th><nobr>$fieldLabel</nobr></th>\n";
+        $html .= '</tr>';
+
+        $stories = $this->dbh->query($this->session->storyQueryCondition . " ORDER BY " . strtr($this->session->storyOrderBy, '_', ' '))->fetchAll();
+        foreach($stories as $story)
+        {
+            $story->title = "<a href='" . common::getSysURL() . $this->createLink('story', 'view', "storyID=$story->id") . "' target='_blank'>$story->title</a>";
+
+            $html .= "<tr valign='top'>\n";
+            foreach($fields as $fieldName => $fieldLabel) $html .= '<td><nobr>' . zget($story, $fieldName, '') . "</nobr></td>\n";
+            $html .= "</tr>\n";
+        }
+        $html .= '</table>';
+
+        return $html;
+    }
+
+    /**
+     * 生成关联的 bug 的 html。
+     * Generate linked bug html.
+     *
+     * @access protected
+     * @return string
+     */
+    protected function generateBugHtml(): string
+    {
+        $this->loadModel('bug');
+        $html = "<h3>{$this->lang->release->bugs}</h3>";
+
+        $fields = array('id' => $this->lang->bug->id, 'title' => $this->lang->bug->title);
+
+        $html .= '<table><tr>';
+        foreach($fields as $fieldLabel) $html .= "<th><nobr>$fieldLabel</nobr></th>\n";
+        $html .= '</tr>';
+
+        $bugs = $this->dao->select('id, title')->from(TABLE_BUG)->where($this->session->linkedBugQueryCondition)->beginIF($this->session->bugOrderBy != false)->orderBy($this->session->bugOrderBy)->fi()->fetchAll('id');
+
+        foreach($bugs as $bug)
+        {
+            $bug->title = "<a href='" . common::getSysURL() . $this->createLink('bug', 'view', "bugID=$bug->id") . "' target='_blank'>$bug->title</a>";
+
+            $html .= "<tr valign='top'>\n";
+            foreach($fields as $fieldName => $fieldLabel) $html .= "<td><nobr>" . zget($bug, $fieldName, '') . "</nobr></td>\n";
+            $html .= "</tr>\n";
+        }
+        $html .= '</table>';
+
+        return $html;
+    }
+
+    /**
+     * 生成遗留的 bug 的 html。
+     * Generat left bug html.
+     *
+     * @access protected
+     * @return string
+     */
+    protected function generateLeftBugHtml(): string
+    {
+        $html = "<h3>{$this->lang->release->generatedBugs}</h3>";
+
+        $fields = array('id' => $this->lang->bug->id, 'title' => $this->lang->bug->title);
+
+        $html .= '<table><tr>';
+        foreach($fields as $fieldLabel) $html .= "<th><nobr>$fieldLabel</nobr></th>\n";
+        $html .= '</tr>';
+
+        $bugs = $this->dao->select('id, title')->from(TABLE_BUG)->where($this->session->leftBugsQueryCondition)->beginIF($this->session->bugOrderBy != false)->orderBy($this->session->bugOrderBy)->fi()->fetchAll('id');
+        foreach($bugs as $bug)
+        {
+            $bug->title = "<a href='" . common::getSysURL() . $this->createLink('bug', 'view', "bugID=$bug->id") . "' target='_blank'>$bug->title</a>";
+
+            $html .= "<tr valign='top'>\n";
+            foreach($fields as $fieldName => $fieldLabel) $html .= "<td><nobr>" . zget($bug, $fieldName, '') . "</nobr></td>\n";
+            $html .= "</tr>\n";
+        }
+        $html .= '</table>';
+
+        return $html;
     }
 }
