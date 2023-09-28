d801feb8ae5c1ce36883a94df9d617ca1a36a851
liumengyi <liumengyi@easycorp.ltd>
2023-09-28 08:50:20 +0800

    * Modify unit test.

--- module/testreport/test/model/getcaseidlist.php
+++ module/testreport/test/model/getcaseidlist.php
@@ -3,7 +3,7 @@
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/testreport.class.php';
 
-zdTable('testreport')->gen(5);
+zdTable('testreport')->gen(10);
 zdTable('user')->gen(1);
 
 su('admin');
