02f564ac2dde8f7c2e31d5e1934d16c5fae5c6c0
caoyanyi <caoyanyi@easycorp.ltd>
2023-10-08 16:48:43 +0800

    + Add statistic for za node.

---
+++ module/metric/test/calc/system/scale/count_of_node.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php
+<?php
+/**
+
+title=count_of_node
+timeout=0
+cid=1
+
+*/
+
+include dirname(__FILE__, 7) . '/test/lib/init.php';
+include dirname(__FILE__, 4) . '/calc.class.php';
+
+zdTable('host')->config('host', true, 4)->gen(10);
+
+$metric = new metricTest();
+$calc   = $metric->calcMetric(__FILE__);
+
+r($calc->getResult()) && p('0:value') && e('4'); // 测试执行节点总数。
