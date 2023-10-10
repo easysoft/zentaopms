5deca113aff1944ee162b2c0f328064f742b5940
songchenxuan <songchenxuan@easycorp.ltd>
2023-10-10 14:15:15 +0800

    * Fix bug #39389.

--- module/common/view/zui3dtable.html.php
+++ module/common/view/zui3dtable.html.php
@@ -37,16 +37,17 @@ zui.DTable.definePlugin(
     name: 'zentao18',
     options: function(options)
     {
+        var height = options.height ? options.height : function(actualHeight)
+        {
+            const height = Math.min(actualHeight, Math.max(0, window.innerHeight - ($('#mainContent').offset().top || 0))) - 1;
+            $('#sidebar>.cell').css('maxHeight', height).children('.tree').addClass('scrollbar-hover');
+            return height - ($('#mainContent .table-footer').outerHeight() || 0);
+        }
+
         return $.extend({fixedLeftWidth: '40%'}, options, {
             cols: convertCols(options.cols),
             responsive: 'window',
-            height: function(actualHeight)
-            {
-
-                const height = Math.min(actualHeight, Math.max(0, window.innerHeight - ($('#mainContent').offset().top || 0))) - 1;
-                $('#sidebar>.cell').css('maxHeight', height).children('.tree').addClass('scrollbar-hover');
-                return height - ($('#mainContent .table-footer').outerHeight() || 0);
-            }
+            height: height,
         });
     },
     onMounted: function()
