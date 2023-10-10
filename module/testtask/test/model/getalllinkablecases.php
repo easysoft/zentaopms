5e3b770b8c49005564564be1c3d1a7b1b03b25d4
liugang <liugang@easycorp.ltd>
2023-10-10 13:25:57 +0800

    * testtask: declare a variable to receive the instantiated object of the testtaskModel class.

--- module/testtask/test/model/getalllinkablecases.php
+++ module/testtask/test/model/getalllinkablecases.php
@@ -20,7 +20,7 @@ pid=1
 
 global $tester, $app;
 
-$tester->loadModel('testtask');
+$testtask = $tester->loadModel('testtask');
 
 $app->loadClass('pager', true);
 $app->setModuleName('testtask');
@@ -31,17 +31,17 @@ $task1 = (object)array('branch' => 0); // 测试单 1 关联到主干
 $task2 = (object)array('branch' => 1); // 测试单 2 关联到分支1
 $task3 = (object)array('branch' => 2); // 测试单 3 关联到分支2
 
-$cases1  = $tester->testtask->getAllLinkableCases($task1);
-$cases2  = $tester->testtask->getAllLinkableCases($task2);
-$cases3  = $tester->testtask->getAllLinkableCases($task3);
-$cases4  = $tester->testtask->getAllLinkableCases($task3, 'id < 22');
-$cases5  = $tester->testtask->getAllLinkableCases($task3, '', array(21));
-$cases6  = $tester->testtask->getAllLinkableCases($task3, '', array(), $pager);
-$cases7  = $tester->testtask->getAllLinkableCases($task3, 'id < 22', array(5));
-$cases8  = $tester->testtask->getAllLinkableCases($task3, 'id < 22', array(),  $pager);
-$cases9  = $tester->testtask->getAllLinkableCases($task3, '', array(5), $pager);
-$cases10 = $tester->testtask->getAllLinkableCases($task3, 'id < 22', array(5), $pager);
-$cases11 = $tester->testtask->getAllLinkableCases($task3, 'id < 10', array(5), $pager);
+$cases1  = $testtask->getAllLinkableCases($task1);
+$cases2  = $testtask->getAllLinkableCases($task2);
+$cases3  = $testtask->getAllLinkableCases($task3);
+$cases4  = $testtask->getAllLinkableCases($task3, 'id < 22');
+$cases5  = $testtask->getAllLinkableCases($task3, '', array(21));
+$cases6  = $testtask->getAllLinkableCases($task3, '', array(), $pager);
+$cases7  = $testtask->getAllLinkableCases($task3, 'id < 22', array(5));
+$cases8  = $testtask->getAllLinkableCases($task3, 'id < 22', array(),  $pager);
+$cases9  = $testtask->getAllLinkableCases($task3, '', array(5), $pager);
+$cases10 = $testtask->getAllLinkableCases($task3, 'id < 22', array(5), $pager);
+$cases11 = $testtask->getAllLinkableCases($task3, 'id < 10', array(5), $pager);
 
 r(count($cases1))  && p() && e('5');  // 获取可关联到测试单 1 的用例，检查数量。
 r(count($cases2))  && p() && e('10'); // 获取可关联到测试单 2 的用例，检查数量。
