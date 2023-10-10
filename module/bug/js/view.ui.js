98990424afcaeca20b690afe80c7cc7ccb2a718d
daitingting <daitingting@xirangit.com>
2023-10-10 05:52:42 +0000

    * Close toTask modal when locate to task view.

--- module/bug/js/view.ui.js
+++ module/bug/js/view.ui.js
@@ -17,6 +17,8 @@ $(document).on('click', '#toTaskButton', function()
 
     if(projectID && executionID != 0)
     {
+        zui.Modal.hide('#toTask');
+
         const url = $.createLink('task', 'create', 'executionID=' + executionID + '&storyID=0&moduleID=0&taskID=0&todoID=0&extra=projectID=' + projectID + '&bugID=' + bugID);
         openPage(url, 'execution');
     }
