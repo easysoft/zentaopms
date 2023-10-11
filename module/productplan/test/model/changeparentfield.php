0493c9bf0b3049f47ec2655f3583f73a75c0e783
tianshujie <tianshujie@easycorp.ltd>
2023-10-11 10:47:56 +0800

    * Change the case result of the changeParentField method.

--- module/productplan/test/model/changeparentfield.php
+++ module/productplan/test/model/changeparentfield.php
@@ -16,6 +16,6 @@ zdTable('productplan')->config('productplan')->gen(5);
 $planIdList = array(1, 2, 3);
 
 $planTester = new productplan('admin');
-r($planTester->changeParentFieldTest($planIdList[0])) && p('parent') && e('0'); // 测试更新父计划的parent字段
-r($planTester->changeParentFieldTest($planIdList[1])) && p('parent') && e('1'); // 测试更新子计划的parent字段
-r($planTester->changeParentFieldTest($planIdList[2])) && p('parent') && e('0'); // 测试更新普通计划的parent字段
+r($planTester->changeParentFieldTest($planIdList[0])) && p('parent') && e('-1'); // 测试更新父计划的parent字段
+r($planTester->changeParentFieldTest($planIdList[1])) && p('parent') && e('0');  // 测试更新子计划的parent字段
+r($planTester->changeParentFieldTest($planIdList[2])) && p('parent') && e('0');  // 测试更新普通计划的parent字段
