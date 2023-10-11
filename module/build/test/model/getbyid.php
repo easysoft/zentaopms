c235d9c7f56b1f3dd8875304ed03700452058917
tianshujie <tianshujie@easycorp.ltd>
2023-10-11 10:22:22 +0800

    * Change case data of the getByID method.

--- module/build/test/model/getbyid.php
+++ module/build/test/model/getbyid.php
@@ -11,14 +11,13 @@ cid=1
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/build.class.php';
 
-zdTable('build')->gen(20);
+zdTable('build')->config('build')->gen(20);
 su('admin');
 
 $buildID = array(7, 17);
 
-$build = new buildTest();
-
-r($build->getByIDTest($buildID[0], true))   && p('name')    && e('项目版本版本7');  //项目版本查询
-r($build->getByIDTest($buildID[1], false))  && p('name')    && e('执行版本版本17'); //执行版本查询
-r($build->getByIDTest(0, true))            && p()          && e('0');              //无id查询
-r($build->getByIDTest($buildID[0], 'test')) && p('project') && e('17');             //图片字段传字符串测试
+$buildTester = new buildTest();
+r($buildTester->getByIDTest($buildID[0], true))   && p('name')    && e('版本7');  // 项目版本查询
+r($buildTester->getByIDTest($buildID[1], false))  && p('name')    && e('版本17'); // 执行版本查询
+r($buildTester->getByIDTest(0, true))             && p()          && e('0');      // 无id查询
+r($buildTester->getByIDTest($buildID[0], 'test')) && p('project') && e('61');     // 图片字段传字符串测试
