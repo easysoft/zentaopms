aa7f6a99534f03480d2ac8a2c966ae8a7b5a7972
liugang <liugang@easycorp.ltd>
2023-10-11 14:39:14 +0800

    * testtask: refactor the script to test the getDataOfTestTaskPerRunner method.

--- module/testtask/test/model/getdataoftesttaskperrunner.php
+++ module/testtask/test/model/getdataoftesttaskperrunner.php
@@ -1,8 +1,11 @@
 #!/usr/bin/env php
 <?php
 include dirname(__FILE__, 5) . '/test/lib/init.php';
-include dirname(__FILE__, 2) . '/testtask.class.php';
 su('admin');
+
+zdTable('case')->config('case')->gen(20);
+zdTable('testrun')->config('testrun')->gen(20);
+zdTable('user')->gen(5);
 
 /**
 
@@ -10,19 +13,30 @@ title=测试 testtaskModel->getDataOfTestTaskPerRunner();
 cid=1
 pid=1
 
-获取测试单0的执行人分组 >> 0
-获取测试单1执行人分组，查看各个执行人的执行数量 >> 未执行,4
-获取测试单2执行人分组，查看各个执行人的执行数量 >> 未执行,4
-
 */
 
 global $tester;
-$tester->loadModel('testtask');
+$testtask = $tester->loadModel('testtask');
 
-$result0 = $tester->testtask->getDataOfTestTaskPerRunner(0);
-$result1 = $tester->testtask->getDataOfTestTaskPerRunner(1);
-$result2 = $tester->testtask->getDataOfTestTaskPerRunner(2);
+$result1 = $testtask->getDataOfTestTaskPerRunner(0);
+$result2 = $testtask->getDataOfTestTaskPerRunner(1);
+$result3 = $testtask->getDataOfTestTaskPerRunner(2);
 
-r($result0)          && p()             && e('0');        // 获取测试单0的执行人分组
-r(current($result1)) && p('name,value') && e('未执行,4'); // 获取测试单1执行人分组，查看各个执行人的执行数量
-r(current($result2)) && p('name,value') && e('未执行,4'); // 获取测试单2执行人分组，查看各个执行人的执行数量
+foreach($result2 as $key => $value)
+{
+    if($key === '') $result2[] = $value;
+}
+
+foreach($result3 as $key => $value)
+{
+    if($key === '') $result3[] = $value;
+}
+
+r($result1) && p() && e(0);                            // 测试单 0 中的用例数为 0。
+r($result2) && p('0:name,value')     && e('未执行,1'); // 获取测试单 1 中的未执行的用例数。
+r($result2) && p('user1:name,value') && e('用户1,2');  // 获取测试单 1 中的用户 1 执行的用例数。
+r($result2) && p('user2:name,value') && e('用户2,2');  // 获取测试单 1 中的用户 2 执行的用例数。
+r($result2) && p('user3:name,value') && e('用户3,3');  // 获取测试单 1 中的用户 3 执行的用例数。
+r($result2) && p('user4:name,value') && e('用户4,4');  // 获取测试单 1 中的用户 4 执行的用例数。
+r($result3) && p('0:name,value')     && e('未执行,3'); // 获取测试单 2 中的未执行的用例数。
+r($result3) && p('user1:name,value') && e('用户1,1');  // 获取测试单 2 中的用户 1 执行的用例数。
