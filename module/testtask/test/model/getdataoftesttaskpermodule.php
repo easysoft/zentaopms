a0c35b3093b256ee4148eeefe4df21deeb3fb5d7
liugang <liugang@easycorp.ltd>
2023-10-11 14:38:58 +0800

    - testtask: remove unused code.

--- module/testtask/test/model/getdataoftesttaskpermodule.php
+++ module/testtask/test/model/getdataoftesttaskpermodule.php
@@ -1,7 +1,6 @@
 #!/usr/bin/env php
 <?php
 include dirname(__FILE__, 5) . '/test/lib/init.php';
-include dirname(__FILE__, 2) . '/testtask.class.php';
 su('admin');
 
 zdTable('case')->config('case')->gen(20);
