40a41be954c117a0c343042bc0d4bb40572fe9fc
liugang <liugang@easycorp.ltd>
2023-10-12 10:22:20 +0800

    * testtask: refactor the script to test the getLinkableCasesByTestTask method.

--- module/testtask/test/model/getlinkablecasesbytesttask.php
+++ module/testtask/test/model/getlinkablecasesbytesttask.php
@@ -1,8 +1,10 @@
 #!/usr/bin/env php
 <?php
 include dirname(__FILE__, 5) . '/test/lib/init.php';
-include dirname(__FILE__, 2) . '/testtask.class.php';
 su('admin');
+
+zdTable('case')->config('case')->gen(10);
+zdTable('testrun')->config('testrun')->gen(10);
 
 /**
 
@@ -10,30 +12,76 @@ title=测试 testtaskModel->getLinkableCasesByTestTask();
 cid=1
 pid=1
 
-直接获取测试单1可关联的用例数量,排除掉ID为2的用例，查看数量 >> 2
-直接获取测试单2可关联的用例数量,查看数量 >> 3
-直接获取测试单3可关联的用例数量,查看数量 >> 3
-直接获取测试单1可关联的用例数量,排除掉ID为2的用例，查看获取到的第一个用例的详情 >> 3,这个是测试用例3,config,intergrate
-直接获取测试单2可关联的用例数量,查看获取到的第一个用例的详情 >> 6,这个是测试用例6,interface,bvt
-直接获取测试单3可关联的用例数量,查看获取到的第一个用例的详情 >> 10,这个是测试用例10,config,system
-
 */
 
-global $tester;
-$tester->loadModel('testtask');
-$tester->app->loadClass('pager', $static = true);
+global $tester, $app;
 
-$task1 = $tester->testtask->getById(1);
-$task2 = $tester->testtask->getById(2);
-$pager = new pager(0, 10, 1);
+$app->setModuleName('testtask');
+$app->setMethodName('linCase');
+$app->loadClass('pager', true);
+$pager = new pager(0, 5, 1);
 
-$cases1 = $tester->testtask->getLinkableCasesByTestTask(1, array(2), '1 = 1', $pager);
-$cases2 = $tester->testtask->getLinkableCasesByTestTask(2, '', '1 = 1', $pager);
-$cases3 = $tester->testtask->getLinkableCasesByTestTask(3, '', '1 = 1', $pager);
+$testtask = $tester->loadModel('testtask');
 
-r(count($cases1)) && p()                        && e('2'); // 直接获取测试单1可关联的用例数量,排除掉ID为2的用例，查看数量
-r(count($cases2)) && p()                        && e('3'); // 直接获取测试单2可关联的用例数量,查看数量
-r(count($cases3)) && p()                        && e('3'); // 直接获取测试单3可关联的用例数量,查看数量
-r($cases1)        && p('0:id,title,type,stage') && e('3,这个是测试用例3,config,intergrate'); // 直接获取测试单1可关联的用例数量,排除掉ID为2的用例，查看获取到的第一个用例的详情
-r($cases2)        && p('0:id,title,type,stage') && e('6,这个是测试用例6,interface,bvt');     // 直接获取测试单2可关联的用例数量,查看获取到的第一个用例的详情
-r($cases3)        && p('0:id,title,type,stage') && e('10,这个是测试用例10,config,system');   // 直接获取测试单3可关联的用例数量,查看获取到的第一个用例的详情
+r($testtask->getLinkableCasesByTestTask(0)) && p() && e(0); // 测试单 0 可关联的用例数为 0。
+r($testtask->getLinkableCasesByTestTask(2)) && p() && e(0); // 测试单 2 可关联的用例数为 0。
+
+$tasks = $testtask->getLinkableCasesByTestTask(1);
+r(count($tasks)) && p() && e(7); // 测试单 1 可关联的用例数为 7。
+r($tasks) && p('0:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('2,这个是测试用例2,2,performance,no,normal,user1,fail');
+r($tasks) && p('1:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('3,这个是测试用例3,3,config,no,blocked,user1,pass');
+r($tasks) && p('2:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('4,这个是测试用例4,4,install,no,investigate,user1,fail');
+
+$tasks = $testtask->getLinkableCasesByTestTask(1, array(1,2,3,4,5,6,7,8,9,10));
+r(count($tasks)) && p() && e(0); // 测试单 1 排除用例 1-10 后可关联的用例数为 0。
+
+$tasks = $testtask->getLinkableCasesByTestTask(1, array(2));
+r(count($tasks)) && p() && e(6); // 测试单 1 排除用例 2 后可关联的用例数为 6。
+r($tasks) && p('0:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('3,这个是测试用例3,3,config,no,blocked,user1,pass');
+r($tasks) && p('1:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('4,这个是测试用例4,4,install,no,investigate,user1,fail');
+r($tasks) && p('2:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('6,这个是测试用例6,2,interface,no,normal,user1,fail');
+
+$tasks = $testtask->getLinkableCasesByTestTask(1, array(2), 't1.id < 10');
+r(count($tasks)) && p() && e(5); // 测试单 1 排除用例 2 并查询 id < 10 的用例后可关联的用例数为 5。
+r($tasks) && p('0:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('3,这个是测试用例3,3,config,no,blocked,user1,pass');
+r($tasks) && p('1:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('4,这个是测试用例4,4,install,no,investigate,user1,fail');
+r($tasks) && p('2:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('6,这个是测试用例6,2,interface,no,normal,user1,fail');
+
+$tasks = $testtask->getLinkableCasesByTestTask(1, array(2), '', $pager);
+r(count($tasks)) && p() && e(5); // 测试单 1 排除用例 2 并限制每页查询 5 条后可关联的用例数为 5。
+r($tasks) && p('0:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('3,这个是测试用例3,3,config,no,blocked,user1,pass');
+r($tasks) && p('1:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('4,这个是测试用例4,4,install,no,investigate,user1,fail');
+r($tasks) && p('2:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('6,这个是测试用例6,2,interface,no,normal,user1,fail');
+
+$tasks = $testtask->getLinkableCasesByTestTask(1, array(), 't1.id > 10');
+r(count($tasks)) && p() && e(0); // 测试单 1 查询 id > 10 的用例后可关联的用例数为 0。
+
+$tasks = $testtask->getLinkableCasesByTestTask(1, array(), 't1.id < 10');
+r(count($tasks)) && p() && e(6); // 测试单 1 查询 id < 10 的用例后可关联的用例数为 6。
+r($tasks) && p('0:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('2,这个是测试用例2,2,performance,no,normal,user1,fail');
+r($tasks) && p('1:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('3,这个是测试用例3,3,config,no,blocked,user1,pass');
+r($tasks) && p('2:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('4,这个是测试用例4,4,install,no,investigate,user1,fail');
+
+$tasks = $testtask->getLinkableCasesByTestTask(1, array(), 't1.id < 10', $pager);
+r(count($tasks)) && p() && e(5); // 测试单 1 查询 id < 10 并限制每页查询 5 条的用例后可关联的用例数为 5。
+r($tasks) && p('0:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('2,这个是测试用例2,2,performance,no,normal,user1,fail');
+r($tasks) && p('1:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('3,这个是测试用例3,3,config,no,blocked,user1,pass');
+r($tasks) && p('2:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('4,这个是测试用例4,4,install,no,investigate,user1,fail');
+
+$tasks = $testtask->getLinkableCasesByTestTask(1, array(), '', $pager);
+r(count($tasks)) && p() && e(5); // 测试单 1 限制每页查询 5 条后可关联的用例数为 5。
+r($tasks) && p('0:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('2,这个是测试用例2,2,performance,no,normal,user1,fail');
+r($tasks) && p('1:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('3,这个是测试用例3,3,config,no,blocked,user1,pass');
+r($tasks) && p('2:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('4,这个是测试用例4,4,install,no,investigate,user1,fail');
+
+$tasks = $testtask->getLinkableCasesByTestTask(1, array(2), 't1.id < 10', $pager);
+r(count($tasks)) && p() && e(5); // 测试单 1 排除用例 2、查询 id < 10 的用例并限制每页查询 5 条后可关联的用例数为 5。
+r($tasks) && p('0:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('3,这个是测试用例3,3,config,no,blocked,user1,pass');
+r($tasks) && p('1:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('4,这个是测试用例4,4,install,no,investigate,user1,fail');
+r($tasks) && p('2:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('6,这个是测试用例6,2,interface,no,normal,user1,fail');
+
+$tasks = $testtask->getLinkableCasesByTestTask(1, array(2,3,4), 't1.id < 10', $pager);
+r(count($tasks)) && p() && e(3); // 测试单 1 排除用例 2-4、查询 id < 10 的用例并限制每页查询 5 条后可关联的用例数为 3。
+r($tasks) && p('0:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('6,这个是测试用例6,2,interface,no,normal,user1,fail');
+r($tasks) && p('1:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('7,这个是测试用例7,3,other,no,blocked,user1,pass');
+r($tasks) && p('2:id,title,pri,type,auto,status,lastRunner,lastRunResult') && e('8,这个是测试用例8,4,feature,no,investigate,user1,fail');
