998197ffdc99dfac5f6466b2f4a0fd4acb935a42
liumengyi <liumengyi@easycorp.ltd>
2023-10-12 17:10:36 +0800

    * Remove query week in actionTest::buildDateGroup.

--- module/action/test/model/builddategroup.php
+++ module/action/test/model/builddategroup.php
@@ -8,8 +8,8 @@ include dirname(__FILE__, 2) . '/action.class.php';
 zdTable('user')->gen(60);
 su('admin');
 
-zdTable('action')->config('action')->gen(60);
-zdTable('actionrecent')->config('action')->gen(60);
+zdTable('action')->config('action')->gen(60, true, false);
+zdTable('actionrecent')->config('action')->gen(60, true, false);
 
 /**
 
@@ -19,28 +19,20 @@ pid=1
 
 测试创建下一个动作日期组 今天 日期倒序 前一模块 my >> 1,1
 测试创建下一个动作日期组 昨天 日期倒序 前一模块 my >> 1,1
-测试创建下一个动作日期组 上周 日期倒序 前一模块 my >> 7,13
 测试创建下一个动作日期组 今天 日期倒序 前一模块 company >> 1,1
 测试创建下一个动作日期组 昨天 日期倒序 前一模块 company >> 1,1
-测试创建下一个动作日期组 上周 日期倒序 前一模块 company >> 7,13
 测试创建下一个动作日期组 今天 日期正序 前一模块 my >> 1,1
 测试创建下一个动作日期组 昨天 日期正序 前一模块 my >> 1,1
-测试创建下一个动作日期组 上周 日期正序 前一模块 my >> 7,13
 测试创建下一个动作日期组 今天 日期正序 前一模块 company >> 1,1
 测试创建下一个动作日期组 昨天 日期正序 前一模块 company >> 1,1
-测试创建下一个动作日期组 上周 日期正序 前一模块 company >> 7,13
 测试创建上一个动作日期组 今天 日期倒序 前一模块 my >> 1,1
 测试创建上一个动作日期组 昨天 日期倒序 前一模块 my >> 1,1
-测试创建上一个动作日期组 上周 日期倒序 前一模块 my >> 7,13
 测试创建上一个动作日期组 今天 日期倒序 前一模块 company >> 1,1
 测试创建上一个动作日期组 昨天 日期倒序 前一模块 company >> 1,1
-测试创建上一个动作日期组 上周 日期倒序 前一模块 company >> 7,13
 测试创建上一个动作日期组 今天 日期正序 前一模块 my >> 1,1
 测试创建上一个动作日期组 昨天 日期正序 前一模块 my >> 1,1
-测试创建上一个动作日期组 上周 日期正序 前一模块 my >> 7,13
 测试创建上一个动作日期组 今天 日期正序 前一模块 company >> 1,1
 测试创建上一个动作日期组 昨天 日期正序 前一模块 company >> 1,1
-测试创建上一个动作日期组 上周 日期正序 前一模块 company >> 7,13
 
 */
 
@@ -53,25 +45,17 @@ $action = new actionTest();
 
 r($action->buildDateGroupTest($directionList[0], $typeList[0], $orderByList[0], $rawMethodList[0])) && p('dateCount,dateActions') && e('1,1');  // 测试创建下一个动作日期组 今天 日期倒序 前一模块 my
 r($action->buildDateGroupTest($directionList[0], $typeList[1], $orderByList[0], $rawMethodList[0])) && p('dateCount,dateActions') && e('1,1');  // 测试创建下一个动作日期组 昨天 日期倒序 前一模块 my
-r($action->buildDateGroupTest($directionList[0], $typeList[2], $orderByList[0], $rawMethodList[0])) && p('dateCount,dateActions') && e('7,13'); // 测试创建下一个动作日期组 上周 日期倒序 前一模块 my
 r($action->buildDateGroupTest($directionList[0], $typeList[0], $orderByList[0], $rawMethodList[1])) && p('dateCount,dateActions') && e('1,1');  // 测试创建下一个动作日期组 今天 日期倒序 前一模块 company
 r($action->buildDateGroupTest($directionList[0], $typeList[1], $orderByList[0], $rawMethodList[1])) && p('dateCount,dateActions') && e('1,1');  // 测试创建下一个动作日期组 昨天 日期倒序 前一模块 company
-r($action->buildDateGroupTest($directionList[0], $typeList[2], $orderByList[0], $rawMethodList[1])) && p('dateCount,dateActions') && e('7,13'); // 测试创建下一个动作日期组 上周 日期倒序 前一模块 company
 r($action->buildDateGroupTest($directionList[0], $typeList[0], $orderByList[1], $rawMethodList[0])) && p('dateCount,dateActions') && e('1,1');  // 测试创建下一个动作日期组 今天 日期正序 前一模块 my
 r($action->buildDateGroupTest($directionList[0], $typeList[1], $orderByList[1], $rawMethodList[0])) && p('dateCount,dateActions') && e('1,1');  // 测试创建下一个动作日期组 昨天 日期正序 前一模块 my
-r($action->buildDateGroupTest($directionList[0], $typeList[2], $orderByList[1], $rawMethodList[0])) && p('dateCount,dateActions') && e('7,13'); // 测试创建下一个动作日期组 上周 日期正序 前一模块 my
 r($action->buildDateGroupTest($directionList[0], $typeList[0], $orderByList[1], $rawMethodList[1])) && p('dateCount,dateActions') && e('1,1');  // 测试创建下一个动作日期组 今天 日期正序 前一模块 company
 r($action->buildDateGroupTest($directionList[0], $typeList[1], $orderByList[1], $rawMethodList[1])) && p('dateCount,dateActions') && e('1,1');  // 测试创建下一个动作日期组 昨天 日期正序 前一模块 company
-r($action->buildDateGroupTest($directionList[0], $typeList[2], $orderByList[1], $rawMethodList[1])) && p('dateCount,dateActions') && e('7,13'); // 测试创建下一个动作日期组 上周 日期正序 前一模块 company
 r($action->buildDateGroupTest($directionList[1], $typeList[0], $orderByList[0], $rawMethodList[0])) && p('dateCount,dateActions') && e('1,1');  // 测试创建上一个动作日期组 今天 日期倒序 前一模块 my
 r($action->buildDateGroupTest($directionList[1], $typeList[1], $orderByList[0], $rawMethodList[0])) && p('dateCount,dateActions') && e('1,1');  // 测试创建上一个动作日期组 昨天 日期倒序 前一模块 my
-r($action->buildDateGroupTest($directionList[1], $typeList[2], $orderByList[0], $rawMethodList[0])) && p('dateCount,dateActions') && e('7,13'); // 测试创建上一个动作日期组 上周 日期倒序 前一模块 my
 r($action->buildDateGroupTest($directionList[1], $typeList[0], $orderByList[0], $rawMethodList[1])) && p('dateCount,dateActions') && e('1,1');  // 测试创建上一个动作日期组 今天 日期倒序 前一模块 company
 r($action->buildDateGroupTest($directionList[1], $typeList[1], $orderByList[0], $rawMethodList[1])) && p('dateCount,dateActions') && e('1,1');  // 测试创建上一个动作日期组 昨天 日期倒序 前一模块 company
-r($action->buildDateGroupTest($directionList[1], $typeList[2], $orderByList[0], $rawMethodList[1])) && p('dateCount,dateActions') && e('7,13'); // 测试创建上一个动作日期组 上周 日期倒序 前一模块 company
 r($action->buildDateGroupTest($directionList[1], $typeList[0], $orderByList[1], $rawMethodList[0])) && p('dateCount,dateActions') && e('1,1');  // 测试创建上一个动作日期组 今天 日期正序 前一模块 my
 r($action->buildDateGroupTest($directionList[1], $typeList[1], $orderByList[1], $rawMethodList[0])) && p('dateCount,dateActions') && e('1,1');  // 测试创建上一个动作日期组 昨天 日期正序 前一模块 my
-r($action->buildDateGroupTest($directionList[1], $typeList[2], $orderByList[1], $rawMethodList[0])) && p('dateCount,dateActions') && e('7,13'); // 测试创建上一个动作日期组 上周 日期正序 前一模块 my
 r($action->buildDateGroupTest($directionList[1], $typeList[0], $orderByList[1], $rawMethodList[1])) && p('dateCount,dateActions') && e('1,1');  // 测试创建上一个动作日期组 今天 日期正序 前一模块 company
 r($action->buildDateGroupTest($directionList[1], $typeList[1], $orderByList[1], $rawMethodList[1])) && p('dateCount,dateActions') && e('1,1');  // 测试创建上一个动作日期组 昨天 日期正序 前一模块 company
-r($action->buildDateGroupTest($directionList[1], $typeList[2], $orderByList[1], $rawMethodList[1])) && p('dateCount,dateActions') && e('7,13'); // 测试创建上一个动作日期组 上周 日期正序 前一模块 company
