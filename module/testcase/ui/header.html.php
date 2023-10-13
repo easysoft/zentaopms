8a838b8e8e98a9fe6d5eb19b5ba9bb942c2d5d28
daitingting <daitingting@xirangit.com>
2023-10-13 07:31:46 +0000

    * Set current feature bar.

--- module/testcase/ui/header.html.php
+++ module/testcase/ui/header.html.php
@@ -113,6 +113,7 @@ featureBar
     ]))) : null,
     set::linkParams($rawMethod == 'zerocase' || $rawMethod == 'browseunits' ? null : $linkParams),
     set::link($rawMethod == 'zerocase' || $rawMethod == 'browseunits' ? $browseLink : null),
+    set::current($this->session->caseBrowseType),
     set::load($load),
     $canSwitchCaseType ? to::leading
     (
