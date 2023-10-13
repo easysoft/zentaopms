206326726741ba0c849575ee727a0c4dc6292e4c
hufangzhou <hufangzhou@easycorp.ltd>
2023-10-13 16:53:53 +0800

    * Fix bug #39538.

--- module/execution/config.php
+++ module/execution/config.php
@@ -194,7 +194,7 @@ $config->execution->datatable->fieldList['id']['checkbox'] = true;
 
 $config->execution->datatable->fieldList['name']['name']         = 'name';
 $config->execution->datatable->fieldList['name']['title']        = $lang->execution->execName;
-$config->execution->datatable->fieldList['name']['width']        = '356';
+$config->execution->datatable->fieldList['name']['width']        = 'auto';
 $config->execution->datatable->fieldList['name']['type']         = 'html';
 $config->execution->datatable->fieldList['name']['fixed']        = 'left';
 $config->execution->datatable->fieldList['name']['sortType']     = true;
