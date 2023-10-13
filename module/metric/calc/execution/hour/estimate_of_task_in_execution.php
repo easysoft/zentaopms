4b944ec5c4fc72b10b997821095252ce456fae24
qixinzhi <qixinzhi@easycorp.ltd>
2023-10-13 07:35:35 +0000

    * Fix metric with definition.

--- module/metric/calc/execution/hour/estimate_of_task_in_execution.php
+++ module/metric/calc/execution/hour/estimate_of_task_in_execution.php
@@ -9,7 +9,7 @@
  * 度量名称：按执行统计的任务预计工时数
  * 单位：小时
  * 描述：按执行统计的任务预计工时数是指在执行管理中，对所有任务的预计工时进行统计和汇总的度量。该度量项反映了任务的预计复杂性和所需的资源投入，可以帮助团队管理者评估任务的难度并安排资源。
- * 定义：执行中任务的预计工时数求和;过滤已删除的任务;过滤父任务;过滤已删除的执行;过滤已删除的项目;
+ * 定义：执行中任务的预计工时数求和;过滤已删除的任务;过滤已取消的任务;过滤父任务;过滤已删除的执行;过滤已删除的项目;
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
  * @author    zhouxin <zhouxin@easycorp.ltd>
@@ -22,14 +22,16 @@ class estimate_of_task_in_execution extends baseCalc
 {
     public $dataset = 'getTasks';
 
-    public $fieldList = array('t1.execution', 't1.estimate', 't1.parent');
+    public $fieldList = array('t1.execution', 't1.estimate', 't1.parent', 't1.status');
 
     public $result = array();
 
     public function calculate($row)
     {
         $parent = $row->parent;
-        if($parent == '-1') return;
+        $status = $row->status;
+
+        if($parent == '-1' || $status == 'cancel') return false;
 
         if(!isset($this->result[$row->execution])) $this->result[$row->execution] = 0;
         $this->result[$row->execution] += $row->estimate;
--- module/metric/calc/system/hour/estimate_of_annual_closed_project.php
+++ module/metric/calc/system/hour/estimate_of_annual_closed_project.php
@@ -9,7 +9,7 @@
  * 度量名称：按系统统计的年度关闭项目的任务预计工时数
  * 单位：个
  * 描述：按系统统计的年度关闭项目的任务预计工时数是指在某年度关闭项目预计需要花费的总工时数。该度量项可以用来评估团队或组织在任务完成方面的工时规划和估算准确性。较准确的年度完成任务预计工时数可以帮助团队更好地安排资源和时间，提高任务的完成效率和进度控制。
- * 定义：所有项目任务的预计工时数求和;项目状态为已关闭;关闭时间为某年;过滤父任务;过滤已删除的任务;过滤已删除的项目;
+ * 定义：所有项目任务的预计工时数求和;项目状态为已关闭;关闭时间为某年;过滤父任务;过滤已删除的任务;过滤已取消的任务;过滤已删除的项目;
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
  * @author    zhouxin <zhouxin@easycorp.ltd>
@@ -22,14 +22,16 @@ class estimate_of_annual_closed_project extends baseCalc
 {
     public $dataset = 'getTasks';
 
-    public $fieldList = array('t1.estimate', 't1.parent', 't3.status', 't3.closedDate', 't1.parent');
+    public $fieldList = array('t1.estimate', 't1.parent', 't3.status', 't3.closedDate', 't1.parent', 't2.status');
 
     public $result = array();
 
     public function calculate($row)
     {
         $parent = $row->parent;
-        if($parent == '-1') return false;
+        $status = $row->status;
+
+        if($parent == '-1' || $status == 'cancel') return false;
 
         if($row->status != 'closed' || empty($row->closedDate) || $row->parent == -1) return false;
 
