6100bf6579d15bacf0e9b2f889505ab850dae5e5
qixinzhi <qixinzhi@easycorp.ltd>
2023-10-13 07:24:27 +0000

    * Update metric.

--- module/metric/calc/system/scale/count_of_application.php
+++ module/metric/calc/system/scale/count_of_application.php
@@ -7,9 +7,9 @@
  * 对象：application
  * 目的：scale
  * 度量名称：按系统统计应用总数
- * 单位：按系统统计的应用总数是指在禅道DevOps平台中使用的全部应用总数。
- * 描述：所有安装的应用个数求和
- * 定义：realtime;
+ * 单位：无
+ * 描述：按系统统计的应用总数是指在禅道DevOps平台中使用的全部应用总数。
+ * 定义：所有安装的应用个数求和;
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
  * @author    Yanyi Cao <caoyanyi@easycorp.ltd>
