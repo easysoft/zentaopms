666a34ba6b48875d2599f575bed91775bd768116
liugang <liugang@easycorp.ltd>
2023-10-16 14:44:59 +0800

    * testtask: refactor the script to test the getRunById method.

--- module/testtask/test/model/getrunbyid.php
+++ module/testtask/test/model/getrunbyid.php
@@ -1,13 +1,9 @@
 #!/usr/bin/env php
 <?php
 include dirname(__FILE__, 5) . '/test/lib/init.php';
-include dirname(__FILE__, 2) . '/testtask.class.php';
 
-zdTable('case')->gen(7);
-zdTable('testrun')->gen(7);
-zdTable('user')->gen(1);
-
-su('admin');
+zdTable('case')->config('case')->gen(5);
+zdTable('testrun')->gen(5);
 
 /**
 
@@ -15,16 +11,31 @@ title=测试 testtaskModel->getRunById();
 cid=1
 pid=1
 
-
-
 */
 
-$runIdList = array(1, 2, 3, 4, 5);
+global $tester;
 
-$testtask = new testtaskTest();
+$testtask = $tester->loadModel('testtask');
 
-r($testtask->getRunByIdTest($runIdList[0])) && p('id,task') && e('1,1'); // 查询run 1 的信息
-r($testtask->getRunByIdTest($runIdList[1])) && p('id,task') && e('2,1'); // 查询run 2 的信息
-r($testtask->getRunByIdTest($runIdList[2])) && p('id,task') && e('3,1'); // 查询run 3 的信息
-r($testtask->getRunByIdTest($runIdList[3])) && p('id,task') && e('4,1'); // 查询run 4 的信息
-r($testtask->getRunByIdTest($runIdList[4])) && p('id,task') && e('5,2'); // 查询run 5 的信息
+r($testtask->getRunById(0)) && p() && e(0); // 查询测试执行 1 的信息。
+r($testtask->getRunById(6)) && p() && e(0); // 查询测试执行 6 的信息。
+
+$run = $testtask->getRunById(1);
+r($run) && p('id,task,version') && e('1,1,1');       // 查询测试执行 1 的信息。
+r($run->case) && p('id,title')  && e('1,测试用例1'); // 查询测试执行 1 对应的用例信息。
+
+$run = $testtask->getRunById(2);
+r($run) && p('id,task,version') && e('2,1,1');       // 查询测试执行 2 的信息。
+r($run->case) && p('id,title')  && e('2,测试用例2'); // 查询测试执行 2 对应的用例信息。
+
+$run = $testtask->getRunById(3);
+r($run) && p('id,task,version') && e('3,1,1');       // 查询测试执行 3 的信息。
+r($run->case) && p('id,title')  && e('3,测试用例3'); // 查询测试执行 3 对应的用例信息。
+
+$run = $testtask->getRunById(4);
+r($run) && p('id,task,version') && e('4,1,1');       // 查询测试执行 4 的信息。
+r($run->case) && p('id,title')  && e('4,测试用例4'); // 查询测试执行 4 对应的用例信息。
+
+$run = $testtask->getRunById(5);
+r($run) && p('id,task,version') && e('5,2,1');       // 查询测试执行 5 的信息。
+r($run->case) && p('id,title')  && e('5,测试用例5'); // 查询测试执行 5 对应的用例信息。
