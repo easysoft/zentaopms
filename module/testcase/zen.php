f434287f42d6916a55454e17cfec8bf854a00e5f
zhouxin <zhouxin@easycorp.ltd>
2023-10-16 11:22:02 +0800

    * Fix errror of testcase-create in projectcase list.

--- module/testcase/zen.php
+++ module/testcase/zen.php
@@ -222,7 +222,7 @@ class testcaseZen extends testcase
             $objectID        = $this->app->tab == 'project' ? $this->session->project : $executionID;
             $productBranches = isset($product->type) && $product->type != 'normal' ? $this->loadModel('execution')->getBranchByProduct(array($productID), $objectID, 'noclosed|withMain') : array();
             $branches        = isset($productBranches[$productID]) ? $productBranches[$productID] : array();
-            $branch          = key($branches);
+            $branch          = !empty($branches) ? key($branches) : '';
         }
         else
         {
