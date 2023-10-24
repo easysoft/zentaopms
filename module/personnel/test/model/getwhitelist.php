9a2bd09c60d0ce1450403b1a5e203c32bc86e09c
wangxuepeng <wangxuepeng@easycorp.ltd>
2023-10-24 18:45:37 +0800

    * Fixed the error for personnelModel::getWhitelist.

--- module/personnel/test/model/getwhitelist.php
+++ module/personnel/test/model/getwhitelist.php
@@ -4,6 +4,9 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/personnel.class.php';
 su('admin');
 
+zdTable('acl')->gen(100);
+zdTable('user')->gen(100);
+
 /**
 
 title=测试 personnelModel->getWhitelist();
@@ -19,6 +22,6 @@ pid=1
 global $tester;
 $personnel = $tester->loadModel('personnel');
 
-r(count($personnel->getWhitelist(1,  'project'))) && p() && e('3'); //获取项目1白名单人员数量
+r(count($personnel->getWhitelist(1,  'project'))) && p() && e('5'); //获取项目1白名单人员数量
 r(count($personnel->getWhitelist('', 'project'))) && p() && e('0'); //当传入空时，查看匹配项目的人员数量
-r(count($personnel->getWhitelist(9,  'program'))) && p() && e('1'); //获取项目集9白名单人员数量
+r(count($personnel->getWhitelist(9,  'program'))) && p() && e('5'); //获取项目集9白名单人员数量
