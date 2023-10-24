6ec44216d4fae70709991b5ece0c1104784dd1f8
wangxuepeng <wangxuepeng@easycorp.ltd>
2023-10-24 19:53:41 +0800

    * Fixed the error for personndelModel::getInvolvedExecutions.

--- module/personnel/test/model/getinvolvedexecutions.php
+++ module/personnel/test/model/getinvolvedexecutions.php
@@ -3,13 +3,16 @@
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/personnel.class.php';
 
+zdTable('project')->config('execution')->gen(20);
+zdTable('team')->config('team')->gen(20);
+
 /**
 
 title=测试 personnelModel->getInvolvedExecutions();
 cid=1
 pid=1
 
-涉及到执行的人员计数 >> 8
+涉及到执行的人员计数 >> 2
 取出其中一个 >> 1
 传入不存在的id >> 0
 
@@ -17,14 +20,12 @@ pid=1
 
 $personnel = new personnelTest('admin');
 
-$project = array();
-$project[0] = array(11, 12);
-$project[1] = 12222;
+$project = array(array(11, 12), 10000);
 
 $result1 = count($personnel->getInvolvedExecutionsTest($project[0]));
 $result2 = $personnel->getInvolvedExecutionsTest($project[0]);
 $result3 = $personnel->getInvolvedExecutionsTest($project[1]);
 
-r($result1) && p()       && e('8'); //涉及到执行的人员计数
-r($result2) && p('pm72') && e('1'); //取出其中一个
-r($result3) && p()       && e('0'); //传入不存在的id
+r($result1) && p()        && e('2'); //涉及到执行的人员计数
+r($result2) && p('admin') && e('1'); //取出其中一个
+r($result3) && p()        && e('0'); //传入不存在的id
