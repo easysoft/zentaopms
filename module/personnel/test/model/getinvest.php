cd143b3056ece1151b0fb1ff293634bf7a1794f9
wangxuepeng <wangxuepeng@easycorp.ltd>
2023-10-24 19:54:45 +0800

    * Fixed the error for personndelModel::getInvest.

--- module/personnel/test/model/getinvest.php
+++ module/personnel/test/model/getinvest.php
@@ -4,6 +4,8 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/personnel.class.php';
 su('admin');
 
+zdTable('project')->gen(50);
+
 /**
 
 title=测试 personnelModel->getInvest();
@@ -20,7 +22,7 @@ pid=1
 $personnel = new personnelTest('admin');
 $programID = array(1,2);
 
-r($personnel->getInvestTest($programID[0])) && p('qa[admin]:role,createdBug')   && e('测试,28'); //项目集1下投入人员admin创建的bug数量
-r($personnel->getInvestTest($programID[0])) && p('qa[admin]:role,pendingBug')   && e('测试,6');  //项目集1下投入人员admin待处理的bug数量
-r($personnel->getInvestTest($programID[1])) && p('dev[po53]:role,createdTask')  && e('研发,0');  //项目集1下投入人员po53创建的任务数量
-r($personnel->getInvestTest($programID[1])) && p('dev[po53]:role,finishedTask') && e('研发,0');  //项目集1下投入人员po53完成的任务数量
+r($personnel->getInvestTest($programID[0])) && p('qa[admin]:role,createdBug')    && e('测试,0'); //项目集1下投入人员admin创建的bug数量
+r($personnel->getInvestTest($programID[0])) && p('qa[admin]:role,pendingBug')    && e('测试,0');  //项目集1下投入人员admin待处理的bug数量
+r($personnel->getInvestTest($programID[1])) && p('qa[user3]:role,createdTask')   && e('测试,0');  //项目集1下投入人员po53创建的任务数量
+r($personnel->getInvestTest($programID[1])) && p('qa[user3]:role,finishedTask')  && e('测试,0');  //项目集1下投入人员po53完成的任务数量
