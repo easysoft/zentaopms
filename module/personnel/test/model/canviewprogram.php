3415b19c493612cf14c487f38b33f283dbde4e61
wangxuepeng <wangxuepeng@easycorp.ltd>
2023-10-24 14:53:39 +0800

    * Fix the error of personnel::canviewprogram.

--- module/personnel/test/model/canviewprogram.php
+++ module/personnel/test/model/canviewprogram.php
@@ -2,7 +2,11 @@
 <?php
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/personnel.class.php';
-su('test2');
+
+zdTable('project')->config('program')->gen(3);
+zdTable('user')->config('user')->gen(3);
+zdTable('group')->config('group')->gen(3);
+zdTable('usergroup')->config('usergroup')->gen(3);
 
 /**
 
@@ -17,11 +21,20 @@ pid=1
 
 $personnel = new personnelTest('admin');
 
-$propramID = array();
-$programID[0] = 1;
+$programID = array(1, 2, 10000);
+$account   = array('admin', 'dev1', 'dev2');
 
-$account = array();
-$account[0] = 'test2';
+su($account[0]);
+r($personnel->canViewProgramTest($programID[0], $account[0])) && p() && e('1');     //看admin是否具有访问项目集1权限
+r($personnel->canViewProgramTest($programID[1], $account[0])) && p() && e('1');     //看admin是否具有访问项目集2权限
+r($personnel->canViewProgramTest($programID[2], $account[0])) && p() && e('1');     //看admin是否具有访问项目集10000权限
 
-r($personnel->canViewProgramTest(1, 'test2')) && p() && e('1');     //看是否具有访问权限有的情况
-r($personnel->canViewProgramTest(10000, 'test2')) && p() && e('1'); //看是否具有访问权限no的情况
+su($account[1]);
+r($personnel->canViewProgramTest($programID[0], $account[1])) && p() && e('1');     //看dev1是否具有访问项目集1权限
+r($personnel->canViewProgramTest($programID[1], $account[1])) && p() && e('1');     //看dev1是否具有访问项目集2权限
+r($personnel->canViewProgramTest($programID[2], $account[1])) && p() && e('1');     //看dev1是否具有访问项目集10000权限
+
+su($account[2]);
+r($personnel->canViewProgramTest($programID[0], $account[2])) && p() && e('1');     //看dev2是否具有访问项目集1权限
+r($personnel->canViewProgramTest($programID[1], $account[2])) && p() && e('1');     //看dev2是否具有访问项目集2权限
+r($personnel->canViewProgramTest($programID[2], $account[2])) && p() && e('1');     //看dev2是否具有访问项目集10000权限
