4256ffe2f89977829315c66a0d8856bf7cbacaf6
liumengyi <liumengyi@easycorp.ltd>
2023-10-24 14:06:32 +0800

    - Remove duplicate sql that has been executed in 17.6.2.

--- db/update17.7.sql
+++ db/update17.7.sql
@@ -11,5 +11,3 @@ set
   minColWidth = if(minColWidth < 200, 200, minColWidth),
   maxColWidth = if(maxColWidth <= 200 and minColWidth <= 200, 201, maxColWidth)
 where colWidth < 200 or minColWidth < 200 or maxColWidth < 200;
-ALTER TABLE `zt_kanban` ADD `minColWidth` smallint(4) NOT NULL DEFAULT '200' AFTER `colWidth`;
-ALTER TABLE `zt_project` ADD `minColWidth` smallint(4) NOT NULL DEFAULT '200' AFTER `colWidth`;
