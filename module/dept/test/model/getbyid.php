16253724ac32e16c41b91daf10a8ab159f716968
wangxuepeng <wangxuepeng@easycorp.ltd>
2023-10-25 19:17:12 +0800

    * Modify unit test for deptModel::getByID.

--- module/dept/test/model/getbyid.php
+++ module/dept/test/model/getbyid.php
@@ -4,13 +4,15 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/dept.class.php';
 su('admin');
 
+zdTable('dept')->gen(1);
+
 /**
 
 title=测试 deptModel->getByID();
 cid=1
 pid=1
 
-查找id为1的部门 >> 产品部
+查找id为1的部门 >> 产品部1
 查找id不存在的部门 >> 0
 
 */
@@ -18,5 +20,5 @@ pid=1
 $deptIDList = array('1','0');
 
 $dept = new deptTest();
-r($dept->getByIDTest($deptIDList[0])) && p('name') && e('产品部'); //查找id为1的部门
+r($dept->getByIDTest($deptIDList[0])) && p('name') && e('产品部1'); //查找id为1的部门
 r($dept->getByIDTest($deptIDList[1])) && p()       && e('0');      //查找id不存在的部门
--- module/dept/test/model/getdatastructure.php
+++ module/dept/test/model/getdatastructure.php
@@ -4,19 +4,21 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/dept.class.php';
 su('admin');
 
+zdTable('dept')->gen(30);
+
 /**
 
 title=测试 deptModel->getDataStructure();
 cid=1
 pid=1
 
-全部查询 >> 100,其他部门63
-全部查询统计 >> 78
+全部查询     >> 30,三级部门10
+全部查询统计 >> 30
 
 */
 
 $count = array('0', '1');
 
 $dept = new deptTest();
-r($dept->getDataStructureTest($count[0])) && p('77:id,name') && e('100,其他部门63'); //全部查询
-r($dept->getDataStructureTest($count[1])) && p()             && e('78');             //全部查询统计
+r($dept->getDataStructureTest($count[0])) && p('29:id,name')  && e('30,三级部门10'); //全部查询
+r($dept->getDataStructureTest($count[1])) && p()             && e('30');             //全部查询统计
