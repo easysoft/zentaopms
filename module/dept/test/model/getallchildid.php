82dc0e00c45bc9cd53da668a191999ec2366f011
wangxuepeng <wangxuepeng@easycorp.ltd>
2023-10-25 19:03:39 +0800

    * Modify unit test for deptModel::getAllChildID.

--- module/dept/test/model/getallchildid.php
+++ module/dept/test/model/getallchildid.php
@@ -4,6 +4,7 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/dept.class.php';
 su('admin');
 
+zdTable('dept')->config('dept')->gen(15);
 /**
 
 title=测试 deptModel->getAllChildId();
@@ -21,7 +22,7 @@ $deptIDList = array('2', '5');
 $count      = array('0', '1');
 
 $dept = new deptTest();
-r($dept->getAllChildIdTest($deptIDList[0], $count[0]))    && p('1') && e('5'); //有子部门查询
-r($dept->getAllChildIdTest($deptIDList[1], $count[0])[0]) && p()    && e('5'); //无子部门查询
-r($dept->getAllChildIdTest($deptIDList[0], $count[1]))    && p()    && e('3'); //子部门数量统计
+r($dept->getAllChildIdTest($deptIDList[0], $count[0]))    && p('0') && e('2'); //有子部门查询
+r($dept->getAllChildIdTest($deptIDList[1], $count[0]))    && p('0') && e('5'); //无子部门查询
+r($dept->getAllChildIdTest($deptIDList[0], $count[1]))    && p()    && e('1'); //子部门数量统计
 r($dept->getAllChildIdTest($deptIDList[1], $count[1]))    && p()    && e('1'); //无子部门数量统计
