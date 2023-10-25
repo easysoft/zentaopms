ba0190c2a00db1b8ff5061ff8b28421b1f195b0d
zhouxin <zhouxin@easycorp.ltd>
2023-10-25 13:57:16 +0800

    * Add product menu for execution-testcase.

--- module/execution/control.php
+++ module/execution/control.php
@@ -580,9 +580,11 @@ class execution extends control
 
         $this->executionZen->assignTestcaseVars($executionID, $productID, $branchID, $moduleID, $orderBy, $type, $pager);
 
-        $this->view->execution     = $execution;
-        $this->view->productOption = $productOption;
-        $this->view->branchOption  = $branchOption;
+        $this->view->execution      = $execution;
+        $this->view->productOption  = $productOption;
+        $this->view->branchOption   = $branchOption;
+        $this->view->switcherParams = "executionID={$executionID}&productID={$productID}&currentMethod=testcase";
+        $this->view->switcherText   = isset($products[$productID]) ? $products[$productID]->name : $this->lang->product->all;
 
         $this->display();
     }
