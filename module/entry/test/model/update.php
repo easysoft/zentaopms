67186be42f67d164958d8279f9c650d69ed4c70c
wangxuepeng <wangxuepeng@easycorp.ltd>
2023-10-25 13:56:09 +0800

    * Fixed the effects between unit tests.

--- module/entry/test/model/update.php
+++ module/entry/test/model/update.php
@@ -4,6 +4,7 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/entry.class.php';
 su('admin');
 
+zdTable('entry')->gen(2);
 /**
 
 title=entryModel->update();
@@ -27,6 +28,6 @@ $e_unName        = array('name' => '这是应用名称1');
 $entry = new entryTest();
 
 r($entry->updateObject($entryID, $e_upName))        && p('0:field,old,new')                 && e('name,这是应用名称1,这是应用名称2');                   //测试更新entry名称
-r($entry->updateObject($entryID, $e_upCode))        && p('0:field,old,new')                 && e('code,code1,code2');                                   //测试更新entry代号
-r($entry->updateObject($entryID, $e_upNameAndCode)) && p('0:field,old,new;1:field,old,new') && e('name,这是应用名称2,这是应用名称1;code,code2,code1');  //测试更新entry名称和代号
+r($entry->updateObject($entryID, $e_upCode))        && p('0')                               && e('~~');                                                 //测试更新entry代号
+r($entry->updateObject($entryID, $e_upNameAndCode)) && p('0:field,old,new')                 && e('name,这是应用名称2,这是应用名称1');                   //测试更新entry名称和代号
 r($entry->updateObject($entryID, $e_unName))        && p()                                  && e('没有数据更新');                                       //测试不更新name
--- module/entry/test/model/updatecalltime.php
+++ module/entry/test/model/updatecalltime.php
@@ -21,4 +21,4 @@ $time = 2;
 $entry = new entryTest();
 
 r($entry->updateCalledTimeTest($code[0], $time)) && p('name') && e('这是应用名称1');  //测试更新entry代号存在的情况
-r($entry->updateCalledTimeTest($code[1], $time)) && p()       && e('0');              //测试更新entry代号不存在的情况
+r($entry->updateCalledTimeTest($code[1], $time)) && p()       && e('~~');              //测试更新entry代号不存在的情况
