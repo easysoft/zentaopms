17d493a4ae9739a8d9c99b0e1a2bf3d106242267
wangxuepeng <wangxuepeng@easycorp.ltd>
2023-10-25 14:10:57 +0800

    * Fixed the error of companyModel::getList.

--- module/company/test/model/getlist.php
+++ module/company/test/model/getlist.php
@@ -4,6 +4,7 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/company.class.php';
 su('admin');
 
+zdTable('company')->gen(1);
 /**
 
 title=测试companyModel->getListTest();
@@ -18,5 +19,5 @@ pid=1
 $count = array('0','1');
 
 $company = new companyTest();
-r($company->getListTest($count[0])) && p('0:name,admins')  && e('易软天创网络科技有限公司,,admin,'); // 公司信息列表查询
-r($company->getListTest($count[1])) && p()                 && e('2');                                // 公司信息统计
+r($company->getListTest($count[0])) && p('0:name|admins', '|')  && e('易软天创网络科技有限公司|,admin,'); // 公司信息列表查询
+r($company->getListTest($count[1])) && p()                      && e('1');                                 // 公司信息统计
