610af645509f27b1fe7aa06fbb31629450c7c1a5
wangxuepeng <wangxuepeng@easycorp.ltd>
2023-10-25 20:12:28 +0800

    * Modify unit test for deptModel::manageChild.

--- module/dept/test/model/managechild.php
+++ module/dept/test/model/managechild.php
@@ -4,6 +4,8 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/dept.class.php';
 su('admin');
 
+zdTable('dept')->config('dept')->gen(30);
+
 /**
 
 title=测试 deptModel->manageChild();
@@ -22,9 +24,9 @@ $depts        = array('四级部门一', '四级部门二', '四级部门三');
 $count        = array('0', '1');
 
 $dept = new deptTest();
-r($dept->manageChildTest($parentDeptID, $depts, $count[0]))           && p('1') && e('102'); //三级部门下添加四级部门
+r($dept->manageChildTest($parentDeptID, $depts, $count[0]))           && p('1') && e('32'); //三级部门下添加四级部门
 r($dept->manageChildTest($parentDeptID, $depts, $count[1]))           && p()    && e('3');   //三级部门下添加四级部门统计
-r($dept->manageChildTest('', $depts, $count[0]))                      && p('1') && e('108'); //无父级部门下添加子级部门
+r($dept->manageChildTest('', $depts, $count[0]))                      && p('1') && e('38'); //无父级部门下添加子级部门
 r($dept->manageChildTest('', $depts, $count[1]))                      && p()    && e('3');   //无父级部门下添加子级部门统计
 r($dept->manageChildTest($parentDeptID, $depts = array(), $count[0])) && p()    && e('0');   //无部门名称
 
