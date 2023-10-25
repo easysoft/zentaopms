e92a42c17a5a73ad7a2b4360b52157347bd0dad0
wangxuepeng <wangxuepeng@easycorp.ltd>
2023-10-25 18:01:41 +0800

    * Modify unit test for deptModel::createManageLink.

--- module/dept/test/model/createmanagelink.php
+++ module/dept/test/model/createmanagelink.php
@@ -4,23 +4,32 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/dept.class.php';
 su('admin');
 
+zdTable('dept')->config('dept')->gen(10);
+
 /**
 
 title=测试 deptModel->createManageLink();
 cid=1
 pid=1
 
-编辑链接 >> 编辑
-下级部门 >> 下级部门
-删除链接 >> 删除
-数量 >> orders[5]
+判断是否生成了编辑链接     >> 1
+判断是否生成了下级部门链接 >> 1
+判断是否生成了删除链接     >> 1
+判断是否生成了排序列       >> 1
 
 */
 
 $deptID = '5';
 
 $dept = new deptTest();
-r($dept->createManageLinkTest($deptID)) && p() && e('编辑');      //编辑链接
-r($dept->createManageLinkTest($deptID)) && p() && e('下级部门');  //下级部门
-r($dept->createManageLinkTest($deptID)) && p() && e('删除');      //删除链接
-r($dept->createManageLinkTest($deptID)) && p() && e('orders[5]'); //数量
+$result = $dept->createManageLinkTest($deptID);
+
+$editLink = 'createmanagelink.php?m=dept&f=edit&deptid=5';
+$nextLink = 'createmanagelink.php?m=dept&f=browse&deptid=5';
+$deleteLink = 'createmanagelink.php?m=dept&f=delete&deptid=5';
+$orderColumn = "id='orders5'";
+
+r(strpos($result, $editLink) !== false)    && p('') && e('1');    //判断是否生成了编辑链接
+r(strpos($result, $nextLink) !== false)    && p('') && e('1');    //判断是否生成了下级部门链接
+r(strpos($result, $deleteLink) !== false)  && p('') && e('1');    //判断是否生成了删除链接
+r(strpos($result, $orderColumn) !== false) && p('') && e('1');    //判断是否生成了排序列
