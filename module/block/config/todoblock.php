4e217b81b0b3caeffd1a79ece507797ed990806a
liumengyi <liumengyi@easycorp.ltd>
2023-10-26 14:42:13 +0800

    * Add sort to todo block.

--- module/block/config/todoblock.php
+++ module/block/config/todoblock.php
@@ -11,12 +11,15 @@ $config->block->todo->dtable->fieldList['id']['name']  = 'id';
 $config->block->todo->dtable->fieldList['id']['title'] = $lang->idAB;
 $config->block->todo->dtable->fieldList['id']['type']  = 'id';
 $config->block->todo->dtable->fieldList['id']['fixed'] = 'left';
+$config->block->todo->dtable->fieldList['id']['sort']  = true;
 
 $config->block->todo->dtable->fieldList['name']          = $config->my->todo->dtable->fieldList['name'];
 $config->block->todo->dtable->fieldList['name']['fixed'] = 'left';
+$config->block->todo->dtable->fieldList['name']['sort']  = true;
 
 $config->block->todo->dtable->fieldList['pri']          = $config->my->todo->dtable->fieldList['pri'];
 $config->block->todo->dtable->fieldList['pri']['width'] = '60px';
+$config->block->todo->dtable->fieldList['pri']['sort']  = true;
 
 $config->block->todo->dtable->fieldList['status'] = $config->my->todo->dtable->fieldList['status'];
 $config->block->todo->dtable->fieldList['type']   = $config->my->todo->dtable->fieldList['type'];
@@ -24,12 +27,23 @@ $config->block->todo->dtable->fieldList['date']   = $config->my->todo->dtable->f
 $config->block->todo->dtable->fieldList['begin']  = $config->my->todo->dtable->fieldList['begin'];
 $config->block->todo->dtable->fieldList['end']    = $config->my->todo->dtable->fieldList['end'];
 
+$config->block->todo->dtable->fieldList['status']['sort'] = true;
+$config->block->todo->dtable->fieldList['type']['sort']   = true;
+$config->block->todo->dtable->fieldList['date']['sort']   = 'date';
+$config->block->todo->dtable->fieldList['begin']['sort']  = 'date';
+$config->block->todo->dtable->fieldList['end']['sort']    = 'date';
+
 unset($config->block->todo->dtable->fieldList['pri']['group']);
 unset($config->block->todo->dtable->fieldList['status']['group']);
 unset($config->block->todo->dtable->fieldList['type']['group']);
 unset($config->block->todo->dtable->fieldList['date']['group']);
 unset($config->block->todo->dtable->fieldList['begin']['group']);
 unset($config->block->todo->dtable->fieldList['end']['group']);
+unset($config->block->todo->dtable->fieldList['status']['sortType']);
+unset($config->block->todo->dtable->fieldList['type']['sortType']);
+unset($config->block->todo->dtable->fieldList['date']['sortType']);
+unset($config->block->todo->dtable->fieldList['begin']['sortType']);
+unset($config->block->todo->dtable->fieldList['end']['sortType']);
 
 $config->block->todo->dtable->short = new stdclass();
 $config->block->todo->dtable->short->fieldList['id']     = $config->block->todo->dtable->fieldList['id'];
