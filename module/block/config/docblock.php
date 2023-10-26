68b71e509dfca26005a6940ad65ff1af91340df5
liumengyi <liumengyi@easycorp.ltd>
2023-10-26 14:56:20 +0800

    + Add sort to doc block.

--- module/block/config/docblock.php
+++ module/block/config/docblock.php
@@ -9,15 +9,19 @@ $config->block->doc->dtable->fieldList['title']['name']  = 'title';
 $config->block->doc->dtable->fieldList['title']['title'] = $lang->doc->title;
 $config->block->doc->dtable->fieldList['title']['link']  = array('module' => 'doc', 'method' => 'view', 'params' => 'docID={id}');
 $config->block->doc->dtable->fieldList['title']['type']  = 'title';
+$config->block->doc->dtable->fieldList['title']['sort']  = true;
 
 $config->block->doc->dtable->fieldList['addedBy']['name']  = 'addedBy';
 $config->block->doc->dtable->fieldList['addedBy']['title'] = $lang->doc->addedBy;
 $config->block->doc->dtable->fieldList['addedBy']['type']  = 'user';
+$config->block->doc->dtable->fieldList['addedBy']['sort']  = true;
 
 $config->block->doc->dtable->fieldList['addedDate']['name']  = 'addedDate';
 $config->block->doc->dtable->fieldList['addedDate']['title'] = $lang->doc->addedDate;
 $config->block->doc->dtable->fieldList['addedDate']['type']  = 'date';
+$config->block->doc->dtable->fieldList['addedDate']['sort']  = 'date';
 
 $config->block->doc->dtable->fieldList['editedDate']['name']  = 'editedDate';
 $config->block->doc->dtable->fieldList['editedDate']['title'] = $lang->doc->editedDate;
 $config->block->doc->dtable->fieldList['editedDate']['type']  = 'date';
+$config->block->doc->dtable->fieldList['editedDate']['sort']  = 'date';
