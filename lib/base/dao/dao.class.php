c90f1d8949709fc2ae2b3247a56e7d9ef8da579a
daitingting <daitingting@xirangit.com>
2023-10-26 05:36:35 +0000

    * The field is seted when it is null.

--- lib/base/dao/dao.class.php
+++ lib/base/dao/dao.class.php
@@ -1159,7 +1159,7 @@ class baseDAO
          * 如果没数据中没有该字段，直接返回。
          * If no this field in the data, return.
          **/
-        if(!isset($this->sqlobj->data->$fieldName)) return $this;
+        if(!is_null($this->sqlobj->data->$fieldName) && !isset($this->sqlobj->data->$fieldName)) return $this;
 
         /* 设置字段值。 */
         /* Set the field label and value. */
