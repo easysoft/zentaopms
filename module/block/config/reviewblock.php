92509ca53e8e4ebff70973979f05d7d4da374e70
liumengyi <liumengyi@easycorp.ltd>
2023-10-26 14:59:55 +0800

    + Add sort to review block.

--- module/block/config/reviewblock.php
+++ module/block/config/reviewblock.php
@@ -7,8 +7,22 @@ $config->block->review = new stdclass();
 $config->block->review->dtable = new stdclass();
 $config->block->review->dtable->fieldList = array();
 $config->block->review->dtable->fieldList = $config->my->audit->dtable->fieldList;
+
+$config->block->review->dtable->fieldList['id']['sort']     = true;
+$config->block->review->dtable->fieldList['title']['sort']  = true;
+$config->block->review->dtable->fieldList['type']['sort']   = true;
+$config->block->review->dtable->fieldList['time']['sort']   = 'date';
+$config->block->review->dtable->fieldList['result']['sort'] = true;
+$config->block->review->dtable->fieldList['status']['sort'] = true;
+
 unset($config->block->review->dtable->fieldList['result']);
 unset($config->block->review->dtable->fieldList['actions']);
+unset($config->block->review->dtable->fieldList['id']['sortType']);
+unset($config->block->review->dtable->fieldList['title']['sortType']);
+unset($config->block->review->dtable->fieldList['type']['sortType']);
+unset($config->block->review->dtable->fieldList['time']['sortType']);
+unset($config->block->review->dtable->fieldList['result']['sortType']);
+unset($config->block->review->dtable->fieldList['status']['sortType']);
 
 $config->block->review->dtable->short = new stdclass();
 $config->block->review->dtable->short->fieldList['id']     = $config->block->review->dtable->fieldList['id'];
