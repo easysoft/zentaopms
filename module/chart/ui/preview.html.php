c5159b6fd646c9a0d655c58d2b2f01007973f9c1
songchenxuan <songchenxuan@easycorp.ltd>
2023-10-26 20:06:35 +0800

    Modify page desc.

--- module/chart/ui/preview.html.php
+++ module/chart/ui/preview.html.php
@@ -1,11 +1,11 @@
 <?php
 declare(strict_types = 1);
 /**
- * The preview view file of pivot module of ZenTaoPMS.
+ * The preview view file of chart module of ZenTaoPMS.
  * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
  * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
- * @author      Gang Liu <liugang@easycorp.ltd>
- * @package     pivot
+ * @author      Chenxuan Song <songchenxuan@easycorp.ltd>
+ * @package     chart
  * @link        https://www.zentao.net
  */
 namespace zin;
