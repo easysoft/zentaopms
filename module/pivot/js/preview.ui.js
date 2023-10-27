b3a96e90f9038c06279eda6936387cd2ed520a5b
liugang <liugang@easycorp.ltd>
2023-10-27 09:36:47 +0800

    + pivotJS: add function to reload workload table when query conditions changed.

--- module/pivot/js/preview.ui.js
+++ module/pivot/js/preview.ui.js
@@ -36,6 +36,7 @@ function loadProductSummary()
     const link   = $.createLink('pivot', 'preview', 'dimension=' + dimension + '&group=' + groupID + '&module=pivot&method=productSummary&params=' + params);
     loadPage(link, '#table-pivot-preview');
 }
+
 /**
  * 查询条件改变时重新加载执行偏差表。
  * Reload execution deviation table when query conditions changed.
@@ -50,6 +51,69 @@ function loadProjectDeviation()
     const params = window.btoa('begin=' + begin + '&end=' + end);
     const link   = $.createLink('pivot', 'preview', 'dimension=' + dimension + '&group=' + groupID + '&module=pivot&method=projectdeviation&params=' + params);
     loadPage(link, '#table-pivot-preview');
+}
+
+/**
+ * 查询条件改变时重新加载工作负载表。
+ * Reload workload table when query conditions changed.
+ *
+ * @access public
+ * @return void
+ */
+function loadWorkload()
+{
+    const begin    = $('#conditions').find('#begin').val();
+    const end      = $('#conditions').find('#end').val();
+    const workhour = $('#conditions').find('#workhour').val();
+    const dept     = $('#conditions').find('[name=dept]').val();
+    const assign   = $('#conditions').find('[name=assign]').val();
+    const days     = diffDate(begin, end);
+
+    $('#days').val(days);
+
+    const params = window.btoa('begin=' + begin.replaceAll('-', '') + '&end=' + end.replaceAll('-', '') + '&days=' + days + '&workhour=' + workhour + '&dept=' + dept + '&assign=' + assign);
+    const link   = $.createLink('pivot', 'preview', 'dimension=' + dimension + '&group=' + groupID + '&module=pivot&method=workload&params=' + params);
+    loadPage(link, '#table-pivot-preview');
+}
+
+/**
+ * 把日期字符串转换成日期对象。
+ * Convert date string to date object.
+ *
+ * @param  string $dateString
+ * @access public
+ * @return date
+ */
+function convertStringToDate(dateString)
+{
+    dateString = dateString.split('-');
+    return new Date(dateString[0], dateString[1] - 1, dateString[2]);
+}
+
+/**
+ * 计算两个日期之间的天数。
+ * Compute the days between two date.
+ *
+ * @param  string $date1
+ * @param  string $date1
+ * @access public
+ * @return int
+ */
+function diffDate(date1, date2)
+{
+    date1 = convertStringToDate(date1);
+    date2 = convertStringToDate(date2);
+    delta = (date2 - date1) / (1000 * 60 * 60 * 24) + 1;
+
+    weekEnds = 0;
+    for(i = 0; i < delta; i++)
+    {
+        if((weekend == 2 && date1.getDay() == 6) || date1.getDay() == 0) weekEnds ++;
+        date1 = date1.valueOf();
+        date1 += 1000 * 60 * 60 * 24;
+        date1 = new Date(date1);
+    }
+    return delta - weekEnds;
 }
 
 /**
