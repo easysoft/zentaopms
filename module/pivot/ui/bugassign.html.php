a59292173176f4148e42b3892c74b106adf1074c
liugang <liugang@easycorp.ltd>
2023-10-27 08:46:19 +0800

    + pivot: rewrite the bug assign table by zin.

---
+++ module/pivot/ui/bugassign.html.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types = 1);
+/**
+ * The bug assign view file of pivot module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Gang Liu <liugang@easycorp.ltd>
+ * @package     pivot
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+
+$cols = $config->pivot->dtable->bugAssign->fieldList;
+$cols['assignedTo']['map'] = $users;
+
+$generateData = function() use ($module, $method, $lang, $title, $cols, $bugs)
+{
+    if(empty($module) || empty($method)) return div(setClass('bg-white center text-gray w-full h-40'), $lang->error->noData);
+
+    return panel
+    (
+        setID('pivotPanel'),
+        set::title($title),
+        set::shadow(false),
+        dtable
+        (
+            set::striped(true),
+            set::bordered(true),
+            set::cols($cols),
+            set::data($bugs),
+            set::emptyTip($lang->error->noData),
+            set::height(jsRaw('getHeight')),
+            set::plugins(array('cellspan')),
+            set::getCellSpan(jsRaw('getCellSpan')),
+            set::cellSpanOptions(array(array('cols' => array('assignedTo', 'total'), 'rowspan' => 'rowspan')))
+        )
+    );
+};
