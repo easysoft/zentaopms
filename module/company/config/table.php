37128c5d4fe2c2741a248e806abd36a0a1174ba0
宋辰轩 <songchenxuan@easycorp.ltd>
2023-10-27 10:42:24 +0800

    revert de790916d8538555dfde7afd4e454c0f706da435
    revert * Use dtable sort.

--- module/company/config/table.php
+++ module/company/config/table.php
@@ -31,7 +31,7 @@ $config->company->user->dtable->fieldList['id']['name']     = 'id';
 $config->company->user->dtable->fieldList['id']['title']    = $lang->idAB;
 $config->company->user->dtable->fieldList['id']['type']     = 'checkID';
 $config->company->user->dtable->fieldList['id']['fixed']    = 'left';
-$config->company->user->dtable->fieldList['id']['sort']     = true;
+$config->company->user->dtable->fieldList['id']['sortType'] = true;
 $config->company->user->dtable->fieldList['id']['checkbox'] = true;
 $config->company->user->dtable->fieldList['id']['show']     = true;
 $config->company->user->dtable->fieldList['id']['group']    = 1;
@@ -41,33 +41,33 @@ $config->company->user->dtable->fieldList['realname']['title']    = $lang->user-
 $config->company->user->dtable->fieldList['realname']['type']     = 'text';
 $config->company->user->dtable->fieldList['realname']['group']    = '1';
 $config->company->user->dtable->fieldList['realname']['fixed']    = 'left';
-$config->company->user->dtable->fieldList['realname']['sort']     = true;
+$config->company->user->dtable->fieldList['realname']['sortType'] = true;
 $config->company->user->dtable->fieldList['realname']['group']    = '1';
 
 $config->company->user->dtable->fieldList['account']['name']     = 'account';
 $config->company->user->dtable->fieldList['account']['title']    = $lang->user->account;
 $config->company->user->dtable->fieldList['account']['type']     = 'text';
 $config->company->user->dtable->fieldList['account']['group']    = '1';
-$config->company->user->dtable->fieldList['account']['sort']     = true;
+$config->company->user->dtable->fieldList['account']['sortType'] = true;
 
 $config->company->user->dtable->fieldList['gender']['name']     = 'gender';
 $config->company->user->dtable->fieldList['gender']['title']    = $lang->user->gender;
 $config->company->user->dtable->fieldList['gender']['type']     = 'category';
 $config->company->user->dtable->fieldList['gender']['map']      = $lang->user->genderList;
-$config->company->user->dtable->fieldList['gender']['sort']     = true;
+$config->company->user->dtable->fieldList['gender']['sortType'] = true;
 $config->company->user->dtable->fieldList['gender']['group']    = '2';
 
 $config->company->user->dtable->fieldList['role']['name']     = 'role';
 $config->company->user->dtable->fieldList['role']['title']    = $lang->user->role;
 $config->company->user->dtable->fieldList['role']['type']     = 'category';
 $config->company->user->dtable->fieldList['role']['map']      = $lang->user->roleList;
-$config->company->user->dtable->fieldList['role']['sort']     = true;
+$config->company->user->dtable->fieldList['role']['sortType'] = true;
 $config->company->user->dtable->fieldList['role']['group']    = '3';
 
 $config->company->user->dtable->fieldList['phone']['name']     = 'phone';
 $config->company->user->dtable->fieldList['phone']['title']    = $lang->user->phone;
 $config->company->user->dtable->fieldList['phone']['type']     = 'text';
-$config->company->user->dtable->fieldList['phone']['sort']     = true;
+$config->company->user->dtable->fieldList['phone']['sortType'] = true;
 $config->company->user->dtable->fieldList['phone']['group']    = '4';
 
 if(!empty($this->config->isINT))
@@ -75,7 +75,7 @@ if(!empty($this->config->isINT))
     $config->company->user->dtable->fieldList['skype']['name']     = 'skype';
     $config->company->user->dtable->fieldList['skype']['title']    = $lang->user->skype;
     $config->company->user->dtable->fieldList['skype']['type']     = 'text';
-    $config->company->user->dtable->fieldList['skype']['sort']     = true;
+    $config->company->user->dtable->fieldList['skype']['sortType'] = true;
     $config->company->user->dtable->fieldList['skype']['group']    = '4';
 }
 else
@@ -83,7 +83,7 @@ else
     $config->company->user->dtable->fieldList['qq']['name']     = 'qq';
     $config->company->user->dtable->fieldList['qq']['title']    = $lang->user->qq;
     $config->company->user->dtable->fieldList['qq']['type']     = 'text';
-    $config->company->user->dtable->fieldList['qq']['sort']     = true;
+    $config->company->user->dtable->fieldList['qq']['sortType'] = true;
     $config->company->user->dtable->fieldList['qq']['group']    = '4';
 }
 
@@ -91,19 +91,19 @@ $config->company->user->dtable->fieldList['email']['name']     = 'email';
 $config->company->user->dtable->fieldList['email']['title']    = $lang->user->email;
 $config->company->user->dtable->fieldList['email']['type']     = 'text';
 $config->company->user->dtable->fieldList['email']['width']    = 200;
-$config->company->user->dtable->fieldList['email']['sort']     = true;
+$config->company->user->dtable->fieldList['email']['sortType'] = true;
 $config->company->user->dtable->fieldList['email']['group']    = '4';
 
 $config->company->user->dtable->fieldList['last']['name']     = 'last';
 $config->company->user->dtable->fieldList['last']['title']    = $lang->user->last;
 $config->company->user->dtable->fieldList['last']['type']     = 'date';
-$config->company->user->dtable->fieldList['last']['sort']     = 'date';
+$config->company->user->dtable->fieldList['last']['sortType'] = true;
 $config->company->user->dtable->fieldList['last']['group']    = '5';
 
 $config->company->user->dtable->fieldList['visits']['name']     = 'visits';
 $config->company->user->dtable->fieldList['visits']['title']    = $lang->user->visits;
 $config->company->user->dtable->fieldList['visits']['type']     = 'text';
-$config->company->user->dtable->fieldList['visits']['sort']     = 'number';
+$config->company->user->dtable->fieldList['visits']['sortType'] = true;
 $config->company->user->dtable->fieldList['visits']['group']    = '5';
 
 $config->company->user->dtable->fieldList['actions']['title'] = $lang->actions;
