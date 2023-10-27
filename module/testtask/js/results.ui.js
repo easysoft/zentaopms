677b0798967686b168340be2e3da94a6368424a9
liumengyi <liumengyi@easycorp.ltd>
2023-10-27 13:09:11 +0800

    - Remove code of loading first result.

--- module/testtask/js/results.ui.js
+++ module/testtask/js/results.ui.js
@@ -6,28 +6,4 @@ $(function()
     {
         $('tr').first().trigger('click');
     }
-    else
-    {
-        var times = 0;
-        var id    = $('tr').first().data('id')
-        var link  = $.createLink('testtask', 'ajaxGetResult', 'resultID=' + id);
-
-        var resultInterval = setInterval(() => {
-            times++;
-            if(times > 600)
-            {
-                clearInterval(resultInterval);
-            }
-
-            $.getJSON(link, function(result)
-            {
-                task = result.data;
-                if(task.ZTFResult != '')
-                {
-                    clearInterval(resultInterval);
-                    loadPage();
-                }
-            });
-        }, 1000);
-    }
 });
