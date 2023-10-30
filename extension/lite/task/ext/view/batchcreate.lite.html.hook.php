c2ff7d04fc7e443f9dc7d432e1914f886a0e36b5
tianshujie <tianshujie@easycorp.ltd>
2023-10-30 17:14:55 +0800

    - Remove unused file.

--- extension/lite/task/ext/view/batchcreate.lite.html.hook.php
+++
@@ -1,6 +0,0 @@
-<script>
-$(function()
-{
-    for(var index = 1; index <= 10; index++) $('#batchCreateForm').append(`<input type="hidden" name="vision[${index}]" id="vision[${index}]" value="lite">`);
-});
-</script>
