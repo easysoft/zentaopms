fffec5ea0a8f76c7c7ef967b2edc49b5817c3fc6
wangxuepeng <wangxuepeng@easycorp.ltd>
2023-10-30 15:17:05 +0800

    * Close modal after setting admin log.

--- module/admin/control.php
+++ module/admin/control.php
@@ -386,7 +386,7 @@ class admin extends control
 
             $this->loadModel('setting')->setItem('system.admin.log.saveDays', $this->post->days);
             if(dao::isError()) return $this->send(array('result' => 'fail', 'message' => dao::getError()));
-            return $this->send(array('result' => 'success', 'message' => $this->lang->saveSuccess, 'load' => true));
+            return $this->send(array('result' => 'success', 'message' => $this->lang->saveSuccess, 'load' => true, 'closeModal' => true));
         }
 
         $this->app->loadLang('webhook');
