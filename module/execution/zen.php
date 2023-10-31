6e33d495b98418a8e95a38f27ffe01239366b9a5
tianshujie <tianshujie@easycorp.ltd>
2023-10-31 09:11:49 +0800

    * Change the link of the execution 1.5 level dropmenu.

--- module/execution/zen.php
+++ module/execution/zen.php
@@ -1535,7 +1535,7 @@ class executionZen extends execution
     protected function getLink(string $module, string $method, string $type = ''): string
     {
         $executionModules = array('task', 'testcase', 'build', 'bug', 'case', 'testtask', 'testreport');
-        if(in_array($module, array('task', 'testcase')) && in_array($method, array('view', 'edit', 'batchedit', 'create', 'batchcreate'))) $method = $module;
+        if(in_array($module, array('task', 'testcase')) && in_array($method, array('view', 'edit', 'batchedit', 'create', 'batchcreate', 'report'))) $method = $module;
         if(in_array($module, $executionModules) && in_array($method, array('view', 'edit'))) $method = $module;
         if(in_array($module, $executionModules + array('story', 'product'))) $module = 'execution';
 
