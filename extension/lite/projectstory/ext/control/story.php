a485c1eb1c0dd1307b674e6f671a35cf9842c1eb
tianshujie <tianshujie@easycorp.ltd>
2023-10-31 11:13:03 +0800

    * Refactor story method of the projectstroy module.

--- extension/lite/projectstory/ext/control/story.php
+++ extension/lite/projectstory/ext/control/story.php
@@ -1,7 +1,24 @@
 <?php
 class projectStory extends control
 {
-    public function story($projectID = 0, $productID = 0, $branch = 0, $browseType = '', $param = 0, $storyType = 'story', $orderBy = '', $recTotal = 0, $recPerPage = 20, $pageID = 1)
+    /**
+     * 项目需求列表。
+     * Project story list.
+     *
+     * @param  int    $projectID
+     * @param  int    $productID
+     * @param  string $branch
+     * @param  string $browseType
+     * @param  int    $param
+     * @param  string $storyType
+     * @param  string $orderBy
+     * @param  int    $recTotal
+     * @param  int    $recPerPage
+     * @param  int    $pageID
+     * @access public
+     * @return void
+     */
+    public function story(int $projectID = 0, int $productID = 0, string $branch = '', string $browseType = '', int $param = 0, string $storyType = 'story', string $orderBy = '', int $recTotal = 0, int $recPerPage = 20, int $pageID = 1)
     {
         if(!$productID) $productID = $this->loadModel('product')->getProductIDByProject($projectID);
         echo $this->fetch('product', 'browse', "productID=$productID&branch=$branch&browseType=$browseType&param=$param&storyType=$storyType&orderBy=$orderBy&recTotal=$recTotal&recPerPage=$recPerPage&pageID=$pageID&projectID=$projectID");
