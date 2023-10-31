86dc6f8701e7372036ddc69c419a7a2924c1e7f4
liumengyi <liumengyi@easycorp.ltd>
2023-10-31 15:50:07 +0800

    * Set back config in execution app.

--- lib/zin/wg/backbtn/v1.php
+++ lib/zin/wg/backbtn/v1.php
@@ -45,8 +45,8 @@ class backBtn extends btn
         $backs = array(
             'task'           => 'execution-task,my-work,my-contribute,execution-tree',
             'story'          => 'product-browse,projectstory-story,execution-story,my-work,my-contribute,productplan-view',
-            'bug'            => 'bug-browse,project-bug,my-work,my-contribute,',
-            'testcase'       => 'testcase-browse,project-testcase,my-work,my-contribute,',
+            'bug'            => 'bug-browse,project-bug,my-work,my-contribute,execution-bug,',
+            'testcase'       => 'testcase-browse,project-testcase,my-work,my-contribute,execution-testcase,',
             'testsuite'      => 'testsuite-browse,testsuite-view,',
             'testtask'       => 'testtask-browse,testtask-cases,',
             'testreport'     => 'testreport-browse,project-testreport',
