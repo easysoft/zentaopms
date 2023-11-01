e0f162c7de4f2e592546026c8d521dd8ad724dc0
wangyuting <wangyuting@easycorp.ltd>
2023-11-01 14:21:26 +0800

    * Adjust kanban space page css.

--- module/kanban/ui/space.html.php
+++ module/kanban/ui/space.html.php
@@ -26,10 +26,42 @@ featureBar
         )
     )
 );
+
 toolbar
 (
-    !empty($unclosedSpace) and $browseType != 'involved' ? item(set::icon('plus'), set::text($lang->kanban->create), set::className('secondary'), set::url(createLink('kanban', 'create', "spaceID=0&type={$browseType}")), set('data-toggle', 'modal')) : null,
+    !empty($unclosedSpace) && $browseType != 'involved' ? item(set::icon('plus'), set::text($lang->kanban->create), set::className('secondary'), set::url(createLink('kanban', 'create', "spaceID=0&type={$browseType}")), set('data-toggle', 'modal')) : null,
     $browseType != 'involved' ? item(set::icon('plus'), set::text($lang->kanban->createSpace), set::className('primary'), set::url(createLink('kanban', 'createSpace', "type={$browseType}")), set('data-toggle', 'modal')) : null
 );
+
+$blocks = array();
+foreach($spaceList as $space)
+{
+    $spaceDescTitle = empty($space->desc) ? $lang->kanban->emptyDesc : str_replace("\n", '', strip_tags($space->desc));
+    $pattern        = '/<br[^>]*>|<img[^>]*>/';
+    $spaceDesc      = empty($space->desc) ? $lang->kanban->emptyDesc : preg_replace($pattern, '', $space->desc);
+
+    $childActions = array();
+    $childActions[] = array('icon' => 'cog-outline', 'url' => createLink('kanban', 'editSpace', "spaceID={$space->id}"), 'text' => $lang->kanban->settingSpace, 'data-toggle' => 'modal');
+    if($space->status != 'closed') $childActions[] = array('icon' => 'off',   'url' => createLink('kanban', 'closeSpace',    "spaceID={$space->id}"), 'text' => $lang->kanban->closeSpace,    'data-toggle' => 'modal');
+    if($space->status == 'closed') $childActions[] = array('icon' => 'magic', 'url' => createLink('kanban', 'activateSpace', "spaceID={$space->id}"), 'text' => $lang->kanban->activateSpace, 'data-toggle' => 'modal');
+    $childActions[] = array('icon' => 'trash', 'url' => createLink('kanban', 'editSpace', "spaceID={$space->id}"), 'text' => $lang->kanban->deleteSpace, 'data-toggle' => 'modal');
+
+    $headingActions = array();
+    if($space->status != 'closed' and $browseType != 'involved') $headingActions[] = array('type' => 'ghost', 'icon' => 'plus', 'url' => createLink('kanban', 'create', "spaceID={$space->id}&type={$space->type}"), 'text' => $lang->kanban->create, 'data-toggle' => 'modal', 'data-size' => 'lg');
+    $headingActions[] = array('type' => 'dropdown', 'btnType' => 'dropdown', 'icon' => 'cog-outline', 'text' => $lang->kanban->setting, 'caret' => true, 'items' => $childActions);
+
+    $blocks[] = blockPanel
+    (
+        set::className('mb-4'),
+        set::title($space->name),
+        set::titleIcon('cube'),
+        set::headingActions($headingActions),
+        div(html($spaceDesc)),
+        !empty($space->kanbans) ? div('123') : div(set::className('dtable'), div(set::className('dtable-empty-tip'), span(set::className('text-gray'),  $lang->kanban->empty)))
+    );
+}
+
+!empty($spaceList) ? $blocks : div(set::className('dtable'), div(set::className('dtable-empty-tip'), span(set::className('text-gray'),  $lang->kanbanspace->empty)));
+div(set::className('table-footer'), pager(set::className('pull-right')));
 
 render();
