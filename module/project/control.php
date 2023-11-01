b7d1fd9e5b074268dc2cec81427e6b3cd2924629
liumengyi <liumengyi@easycorp.ltd>
2023-11-01 17:34:50 +0800

    * Fix bug of recTotal in project::dynamic.

--- module/project/control.php
+++ module/project/control.php
@@ -695,8 +695,7 @@ class project extends control
         $date       = empty($date) ? '' : date('Y-m-d', $date);
         $actions    = $this->loadModel('action')->getDynamic($account, $period, $orderBy, 50, 'all', $projectID, 'all', $date, $direction);
         $dateGroups = $this->action->buildDateGroup($actions, $direction);
-
-        if(empty($recTotal)) $recTotal = count($dateGroups) < 2 ? count($actions) : $this->action->getDynamicCount();
+        if(empty($recTotal)) $recTotal = count($dateGroups) < 2 ? count(reset($dateGroups)) : $this->action->getDynamicCount();
 
         /* The header and position. */
         $project = $this->project->getByID($projectID);
