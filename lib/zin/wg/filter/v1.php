b55a484b885289a961ae2587d4b0d61f312d7ca8
liugang <liugang@easycorp.ltd>
2023-11-02 08:42:41 +0800

    * zin: refactor the filter widget.

--- lib/zin/wg/filter/v1.php
+++ lib/zin/wg/filter/v1.php
@@ -14,41 +14,84 @@ namespace zin;
 class filter extends wg
 {
     protected static array $defineProps = array(
-        'class?: string',
-        'title?: string',
-        'type?: string',
-        'name?: string',
-        'value?: string',
-        'items?: array'
+        'class?: string',                   // 样式。
+        'title?: string',                   // 控件标题。
+        'type?: string',                    // 控件类型。
+        'name?: string',                    // 控件名称。
+        'value?: string',                   // 控件默认值。
+        'items?: array',                    // picker 列表项或表项获取方法。
+        'menu?: array',                     // picker 附加的菜单选项。
+        'multiple?: boolean|number=false',  // picker 是否允许选择多个值，如果指定为数字，则限制多选的数目，默认 `false`。
     );
 
-    protected function buildControl(): wg|array
+    protected function buildPicker(): picker
     {
-        list($type, $name, $value, $items) = $this->prop(array('type', 'name', 'value', 'items'));
+        list($name, $value, $items, $menu, $multiple) = $this->prop(array('name', 'value', 'items', 'menu', 'multiple'));
 
-        if($type == 'select') return picker
+        return picker
         (
             setClass('w-full'),
             set::name($name),
             set::value($value),
-            set::items($items)
+            set::items($items),
+            set::menu($menu),
+            set::multiple($multiple)
         );
+    }
 
-        return input
+    protected function buildDatePicker(): datePicker|array
+    {
+        list($name, $value) = $this->prop(array('name', 'value'));
+
+        return datePicker
         (
+            setClass('w-full'),
             set::name($name),
-            set::type($type == 'datetime' ? 'datetime-local' : $type),
             set::value($value)
         );
     }
 
+    protected function buildDatetimePicker(): datetimePicker|array
+    {
+        list($name, $value) = $this->prop(array('name', 'value'));
+
+        return datetimePicker
+        (
+            setClass('w-full'),
+            set::name($name),
+            set::value($value)
+        );
+    }
+
+    protected function buildInput(): input
+    {
+        list($name, $value) = $this->prop(array('name', 'value'));
+
+        return input
+        (
+            set::name($name),
+            set::value($value)
+        );
+    }
+
+    protected function buildControl(string $type): wg|array
+    {
+        if($type == 'select')   return $this->buildPicker();
+        if($type == 'date')     return $this->buildDatePicker();
+        if($type == 'datetime') return $this->buildDatetimePicker();
+        return $this->buildInput();
+    }
+
     protected function build(): wg|array
     {
+        $type  = $this->prop('type');
+        $class = $this->prop('class', 'w-1/4');
+
         return inputGroup
         (
-            setClass('pr-4 mb-2 ' . $this->prop('class', 'w-1/4')),
+            setClass("filter filter-{$type} pr-4 mb-2 {$class}"),
             $this->prop('title'),
-            $this->buildControl()
+            $this->buildControl($type)
         );
     }
 }
