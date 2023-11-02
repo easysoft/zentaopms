be7732051f6e9e52a12a360aeed5981eb9d06f2d
qixinzhi <qixinzhi@easycorp.ltd>
2023-11-02 03:07:50 +0000

    * Update createdBy to system in chart and pivot,task#105354,bug#34620.

--- db/update18.8.sql
+++ db/update18.8.sql
@@ -1,3 +1,7 @@
 ALTER TABLE `zt_mr` ADD executionID mediumint(8) unsigned NOT NULL DEFAULT 0 AFTER `jobID`;
 ALTER TABLE `zt_testtask` ADD COLUMN `members` text NULL;
 UPDATE `zt_solutions` SET `deleted` = '0' WHERE `deleted` = '';
+
+/* Update createdBy to system in chart and pivot. */
+UPDATE `zt_chart` SET `createdBy` = 'system' WHERE `builtin` = '1';
+UPDATE `zt_pivot` SET `createdBy` = 'system' WHERE `id` >= 1000 and `id` <= 1027;
