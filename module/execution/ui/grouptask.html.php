97b95581008ee783f88a398488ce07b9be48a836
tianshujie <tianshujie@easycorp.ltd>
2023-11-02 14:40:11 +0800

    * Change the table head style of the group task page.

--- module/execution/ui/grouptask.html.php
+++ module/execution/ui/grouptask.html.php
@@ -132,7 +132,7 @@ $thead = function() use($lang, $groupList, $groupBy, $allCount)
 {
     return h::tr
     (
-        $allCount ? setClass('divider') : null,
+        $allCount ? setClass('border-divider') : null,
         h::th
         (
             setClass('c-side text-left has-btn group-menu'),
