cbaa9b924901da8d172f467e96d3718f26b6f287
songchenxuan <songchenxuan@easycorp.ltd>
2023-11-02 12:53:54 +0800

    * Fix bug #40120.

--- module/chart/model.php
+++ module/chart/model.php
@@ -620,7 +620,7 @@ class chartModel extends model
         $molecule    = $this->dao->query($moleculeSQL)->fetch();
         $denominator = $this->dao->query($denominatorSQL)->fetch();
 
-        $percent = round($molecule->count / $denominator->count, 4);
+        $percent = round($molecule->count / $denominator->count, 2);
 
         $series  = array(array('type' => 'liquidFill', 'data' => array($percent), 'outline' => array('show' => false), 'label' => array('fontSize' => 30)));
         $tooltip = array('show' => true);
