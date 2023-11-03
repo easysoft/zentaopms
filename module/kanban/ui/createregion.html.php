6823f69b7fde8b9d595278123e65062b297d47b8
wangyuting <wangyuting@easycorp.ltd>
2023-11-03 09:27:37 +0800

    * Finish kanban create and edit region ui page.

---
+++ module/kanban/ui/createregion.html.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+/**
+ * The createregion view file of kanban module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Yuting Wang <wangyuting@easycorp.ltd>
+ * @package     kanban
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+modalHeader(set::title($lang->kanban->createRegion), set::titleClass('article-h1'));
+
+formPanel
+(
+    formRow
+    (
+        formGroup
+        (
+            set::label($lang->kanbanregion->name),
+            set::name('name')
+        )
+    ),
+    formRow
+    (
+        formGroup
+        (
+            set::label($lang->kanbanregion->style),
+            picker
+            (
+                set::name('region'),
+                set::items($regions),
+                set::value('custom')
+            )
+        )
+    )
+);
+
+render();
---
+++ module/kanban/ui/editregion.html.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+/**
+ * The editregion view file of kanban module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Yuting Wang <wangyuting@easycorp.ltd>
+ * @package     kanban
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+modalHeader(set::title($lang->kanban->editRegion), set::entityText($region->name), set::entityID($region->id));
+
+formPanel
+(
+    formRow
+    (
+        formGroup
+        (
+            set::label($lang->kanbanregion->name),
+            set::name('name'),
+            set::value($region->name)
+        )
+    )
+);
+
+render();
