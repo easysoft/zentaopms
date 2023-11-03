4acbf314e4567b9c5589ed08c2b5cf9b02e237f4
songchenxuan <songchenxuan@easycorp.ltd>
2023-11-03 13:26:49 +0800

    * Fix bug #40087.

--- module/chart/lang/fr.php
+++ module/chart/lang/fr.php
@@ -40,4 +40,4 @@ $lang->chart->typeList['radar']       = 'Radar';
 $lang->chart->typeList['cluBarY']     = 'Clustered Bar Y';
 $lang->chart->typeList['stackedBarY'] = 'Stacked Bar Y';
 $lang->chart->typeList['cluBarX']     = 'Clustered Bar X';
-$lang->chart->typeList['stackedBar']  = 'Stacked Bar';
+$lang->chart->typeList['stackedBar']  = 'Stacked Bar X';
