276fc10eab6889bfb80ddf5b4ded843840226ef1
liugang <liugang@easycorp.ltd>
2023-11-03 14:22:41 +0800

    - chartZen: remove unused method.

--- module/chart/zen.php
+++ module/chart/zen.php
@@ -1,25 +1,4 @@
 <?php
 class chartZen extends chart
 {
-    /**
-     * Set pivot Menu of a dimension.
-     *
-     * @param  int    $dimension
-     * @access protected
-     * @return void
-     */
-    protected function setFeatureBar($dimension)
-    {
-        if(!$dimension) return false;
-
-        $groups = $this->loadModel('tree')->getGroupPairs($dimension, 0, 1);
-        if(!$groups) return false;
-
-        $this->lang->chart->featureBar['preview'] = array();
-        foreach($groups as $groupID => $groupName)
-        {
-            if(empty($groupID) || empty($groupName)) continue;
-            $this->lang->chart->featureBar['preview'][$groupID] = $groupName;
-        }
-    }
 }
