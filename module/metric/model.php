fa06dd0d7cd4553d12028ced8c9202ce88cb3809
qixinzhi <qixinzhi@easycorp.ltd>
2023-11-03 05:36:57 +0000

    * Fix insert empty data into metriclib.

--- module/metric/model.php
+++ module/metric/model.php
@@ -338,6 +338,7 @@ class metricModel extends model
         $this->dao->begin();
         foreach($records as $record)
         {
+            if(empty($record)) continue;
             $this->dao->insert(TABLE_METRICLIB)
                 ->data($record)
                 ->exec();
