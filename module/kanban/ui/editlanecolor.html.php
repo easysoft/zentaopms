f424f6a31439931aced654a41b6ab2dbd03dffe0
wangyuting <wangyuting@easycorp.ltd>
2023-11-03 09:20:01 +0800

    * Finish edit kanban lane ui page.

---
+++ module/kanban/ui/editlanecolor.html.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+/**
+ * The editlanecolor view file of kanban module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Yuting Wang <wangyuting@easycorp.ltd>
+ * @package     kanban
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+modalHeader(set::title($lang->kanban->editLaneColor), set::titleClass('article-h1'));
+
+formPanel
+(
+    formRow
+    (
+        formGroup
+        (
+            set::label($lang->kanbanlane->color),
+            colorPicker
+            (
+                set::name('color'),
+                set::items($config->kanban->laneColorList),
+                set::value($lane->color)
+            )
+        )
+    )
+);
+
+render();
---
+++ module/kanban/ui/editlanename.html.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+/**
+ * The editlanename view file of kanban module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Yuting Wang <wangyuting@easycorp.ltd>
+ * @package     kanban
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+modalHeader(set::title($lang->kanban->editLaneName), set::titleClass('article-h1'));
+
+formPanel
+(
+    formRow
+    (
+        formGroup
+        (
+            set::label($lang->kanbanlane->name),
+            set::name('name'),
+            set::value($lane->name)
+        )
+    ),
+    $from != 'kanban' ? formRow
+    (
+        formGroup
+        (
+            set::label($lang->kanban->WIPType),
+            set::disabled(true),
+            set::name('type'),
+            set::value(zget($lang->kanban->laneTypeList, $lane->type))
+        )
+    ) : null
+);
+
+render();
