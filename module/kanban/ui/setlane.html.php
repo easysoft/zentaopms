d02ee1a58fd0eacb40310bb83f820d218c0746cf
wangyuting <wangyuting@easycorp.ltd>
2023-11-06 09:31:17 +0800

    * Finish setlane ui page.

--- module/kanban/ui/setlane.html.php
+++ module/kanban/ui/setlane.html.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 /**
- * The editlanename view file of kanban module of ZenTaoPMS.
+ * The setlane view file of kanban module of ZenTaoPMS.
  * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
  * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
  * @author      Yuting Wang <wangyuting@easycorp.ltd>
@@ -9,7 +9,7 @@ declare(strict_types=1);
  * @link        https://www.zentao.net
  */
 namespace zin;
-modalHeader(set::title($lang->kanban->editLaneName), set::entityText($lane->name), set::entityID($lane->id));
+modalHeader(set::title($lang->kanban->setLane), set::entityText($lane->name), set::entityID($lane->id));
 
 formPanel
 (
@@ -27,11 +27,23 @@ formPanel
         formGroup
         (
             set::label($lang->kanban->WIPType),
-            set::disabled(true),
             set::name('type'),
             set::value(\zget($lang->kanban->laneTypeList, $lane->type))
         )
-    ) : null
+    ) : null,
+    formRow
+    (
+        formGroup
+        (
+            set::label($lang->kanbanlane->color),
+            colorPicker
+            (
+                set::name('color'),
+                set::items($config->kanban->laneColorList),
+                set::value($lane->color)
+            )
+        )
+    )
 );
 
 render();
