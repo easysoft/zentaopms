62080f23c9d7e9817c1c332bddd923f27705aed7
liugang <liugang@easycorp.ltd>
2023-11-06 10:16:45 +0800

    + chart: add method to preview checked charts.

--- module/chart/zen.php
+++ module/chart/zen.php
@@ -1,4 +1,28 @@
 <?php
 class chartZen extends chart
 {
+    /**
+     * 根据 chartList 获取要查看的图表。
+     * Get the charts to view by chartList.
+     *
+     * @param  array  $chartList
+     * @access public
+     * @return array
+     */
+    public function getChartsToView(array $chartList): array
+    {
+        $charts = array();
+        foreach($chartList as $chart)
+        {
+            $group = $chart['groupID'];
+            $chart = $this->chart->getByID($chart['chartID']);
+            if($chart)
+            {
+                $chart->currentGroup = $group;
+                $charts[] = $chart;
+            }
+        }
+
+        return $charts;
+    }
 }
