2117ec69738a59cbf7bbddefe519051e0f85af93
liugang <liugang@easycorp.ltd>
2023-11-07 10:55:38 +0800

    - dimensionZen: remove unused methods.

--- module/dimension/zen.php
+++ module/dimension/zen.php
@@ -11,47 +11,4 @@ declare(strict_types=1);
  */
 class dimensionZen extends dimension
 {
-    /**
-     * 构造1.5级导航的数据。
-     * Build the data of 1.5 level navigation.
-     *
-     * @param  array     $dimensions
-     * @param  int       $parentID
-     * @access protected
-     * @return void
-     */
-    protected function buildTree(array $dimensions): array
-    {
-        $result = array();
-        foreach($dimensions as $dimension)
-        {
-            $itemArray = array
-            (
-                'id'    => $dimension->id,
-                'text'  => $dimension->name,
-                'keys'  => zget(common::convert2Pinyin(array($dimension->name)), $dimension->name, ''),
-            );
-
-            $result[] = $itemArray;
-        }
-        return $result;
-    }
-
-    /**
-     * 获取下拉树菜单的链接。
-     * Get link for drop tree menu.
-     *
-     * @param  string    $moduleName
-     * @param  string    $methodName
-     * @param  int       $programID
-     * @param  string    $vars
-     * @param  string    $from
-     * @access protected
-     * @return string
-     */
-    protected function getLink(string $moduleName, string $methodName, string $dimensionID, string $vars = '', string $from = 'dimension'): string
-    {
-        return helper::createLink($moduleName, $methodName, "dimensionID={$dimensionID}");
-    }
 }
-
