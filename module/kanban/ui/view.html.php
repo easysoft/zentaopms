d77535d6f7c960b5086f65197335d34cf5aed703
sunguangming <sunguangming@easycorp.ltd>
2023-11-08 16:23:55 +0800

    * Add comment.

--- module/kanban/ui/view.html.php
+++ module/kanban/ui/view.html.php
@@ -1,5 +1,15 @@
 <?php
+declare(strict_types=1);
+/**
+ * The view file of kanban module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Sun Guangming <sunguangming@easycorp.ltd>
+ * @package     kanban
+ * @link        https://www.zentao.net
+ */
 namespace zin;
+
 $laneCount = 0;
 $groupCols = array(); // 卡片可以移动到的同一group下的列。
 foreach($kanbanList as $current => $region)
@@ -34,7 +44,6 @@ foreach($kanbanList as $current => $region)
 $regionMenu   = array();
 $regionMenu[] = li(set::className($regionID == 'all' ? 'active' : ''), a(set::href('javascript:;'), span(set::title($lang->kanbanregion->all), $lang->kanbanregion->all)), set('data-on', 'click'), set('data-call', 'clickRegionMenu'), set('data-params', 'event'), set('data-region', 'all'));
 foreach($regions as $currentRegionID => $regionName) $regionMenu[] = li(set::className($regionID == $currentRegionID ? 'active' : ''), a(set::href('javascript:;'), span(set::title($regionName), $regionName)), set('data-on', 'click'), set('data-call', 'clickRegionMenu'), set('data-params', 'event'), set('data-region', $currentRegionID));
-
 
 jsVar('laneCount',  $laneCount);
 jsVar('kanbanLang', $lang->kanban);
