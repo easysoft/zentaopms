b929153f653fd8fc310c2211da47a291ef41ff89
caoyanyi <caoyanyi@easycorp.ltd>
2023-11-08 15:57:33 +0800

    + Add git page.

---
+++ module/git/ui/diff.html.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+/**
+ * The diff view file of git module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Yanyi Cao <caoyanyi@easycorp.ltd>
+ * @package     git
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+
+modalHeader(set::titile("$path@$revision"));
+
+div
+(
+    setClass('canvas flex'),
+    h::xmp($diff)
+);
