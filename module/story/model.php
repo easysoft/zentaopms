09c379de06abb726fbb7d7f5ad1ac9e0cceee6d4
songchenxuan <songchenxuan@easycorp.ltd>
2023-11-10 09:23:25 +0800

    * Merge commit 08d5831c7a.

--- module/story/model.php
+++ module/story/model.php
@@ -2171,7 +2171,7 @@ class storyModel extends model
         $stories = $this->dao->select("*,IF(`pri` = 0, {$this->config->maxPriValue}, `pri`) as priOrder")->from(TABLE_STORY)
             ->where('product')->in($productID)
             ->andWhere('type')->eq($type)
-            ->beginIF($branch)->andWhere("branch")->eq($branch)->fi()
+            ->beginIF($branch and $branch != 'all')->andWhere("branch")->eq($branch)->fi()
             ->beginIF($modules)->andWhere("module")->in($modules)->fi()
             ->andWhere('deleted')->eq(0)
             ->andWhere("FIND_IN_SET('{$this->config->vision}', vision)")
