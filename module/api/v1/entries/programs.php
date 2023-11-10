2e22543f829536fbafe0a9388b06406283419ec9
songchenxuan <songchenxuan@easycorp.ltd>
2023-11-10 11:45:03 +0800

    * Merge commit ec26685c7c.

--- module/api/v1/entries/programs.php
+++ module/api/v1/entries/programs.php
@@ -35,12 +35,9 @@ class programsEntry extends entry
         if(isset($data->status) and $data->status == 'fail') return $this->sendError(zget($data, 'code', 400), $data->message);
 
         $programs     = $data->data->programs;
-        $progressList = $data->data->progressList;
-        $users        = $data->data->users;
         $result       = array();
         foreach($programs as $program)
         {
-            if(isset($progressList->{$program->id})) $program->progress = $progressList->{$program->id};
             $program = $this->format($program, 'begin:date,end:date,PO:user,PM:user,QD:user,RD:user,realBegan:date,realEnd:date,openedBy:user,openedDate:time,lastEditedDate:time,closedBy:user,closedDate:time,canceledBy:user,canceledDate:time,deleted:bool,whitelist:userList');
 
             if($mergeChildren)
