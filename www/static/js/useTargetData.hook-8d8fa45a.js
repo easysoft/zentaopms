d7b3a3764567182b30e9612a82cd993df17d6743
zhouxin <zhouxin@easycorp.ltd>
2023-11-10 16:39:37 +0800

    * Fix bug #40162.

---
+++ www/static/js/useTargetData.hook-8d8fa45a.js
@@ -0,0 +1,1 @@
+import{u as a}from"./chartEditStore-6adf2d5d.js";import{a5 as o}from"./index.js";const u=()=>{const t=a();return{targetData:o(()=>{const e=t.getComponentList,r=t.fetchTargetIndex();return e[r]}),chartEditStore:t}};export{u};
