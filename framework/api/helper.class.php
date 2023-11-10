688c27a58c4a2f12120a5a762dec28514ed6734b
songchenxuan <songchenxuan@easycorp.ltd>
2023-11-10 10:43:57 +0800

    * Merge commit 30bd866bd4.

--- framework/api/helper.class.php
+++ framework/api/helper.class.php
@@ -258,6 +258,19 @@ class helper extends baseHelper
                 return (string)$value;
         }
     }
+
+    /**
+     * 检查是否启用缓存。
+     * Check is enable cache.
+     *
+     * @return bool
+     */
+    public static function isCacheEnabled()
+    {
+        if(isset($_GET['_nocache']) || isset($_SERVER['HTTP_X_ZT_REFRESH'])) return false;
+        global $config;
+        return $config->cache->enable;
+    }
 }
 
 /**
