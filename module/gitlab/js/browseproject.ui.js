673013ea51e59cfd2d2fc59c5fb5438964042ad6
caoyanyi <caoyanyi@easycorp.ltd>
2023-11-13 09:56:42 +0800

    * Adjust gitlab project list style.

--- module/gitlab/js/browseproject.ui.js
+++ module/gitlab/js/browseproject.ui.js
@@ -2,7 +2,7 @@ window.renderCell = function(result, {col, row})
 {
     if(col.name === 'star')
     {
-        result[0] = {html:'<span title="' + projectStar + '" ><i class="icon icon-star"></i>' + row.data.star_count + '</span><span title="' + projectFork + '" class="ml-1"><i class="icon icon-code-fork"></i>' + row.data.forks_count + '</span>', style:{flexDirection:"column"}};
+        result[0] = {html:'<span title="' + projectStar + '" ><i class="icon icon-star mr-1"></i>' + row.data.star_count + '</span><span title="' + projectFork + '" class="ml-1"><i class="icon icon-code-fork"></i>' + row.data.forks_count + '</span>', style:{flexDirection:"column"}};
 
         return result;
     }
