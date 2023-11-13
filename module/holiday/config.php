176dced062c9b691655b3c46cfebe2eef26abdc4
Lufei <lufei@easycorp.ltd>
2023-11-13 09:03:06 +0800

    * Change holiday api root.
    close GH-127.

--- module/holiday/config.php
+++ module/holiday/config.php
@@ -4,4 +4,4 @@ $config->holiday->require = new stdclass();
 $config->holiday->require->create = 'name,begin,end';
 $config->holiday->require->edit   = 'name,begin,end';
 
-$config->holiday->apiRoot = 'https://ghproxy.com/https://raw.githubusercontent.com/NateScarlet/holiday-cn/master/%d.json';
+$config->holiday->apiRoot = 'https://raw.githubusercontent.com/NateScarlet/holiday-cn/master/%d.json';
