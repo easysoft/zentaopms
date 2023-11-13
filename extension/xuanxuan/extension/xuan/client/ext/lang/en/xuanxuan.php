34281533249cca3a123d480c1ddbb1493eeedb5b
liugang <liugang@easycorp.ltd>
2023-11-13 16:47:56 +0800

    + clientLang: add lang items.

--- extension/xuanxuan/extension/xuan/client/ext/lang/en/xuanxuan.php
+++ extension/xuanxuan/extension/xuan/client/ext/lang/en/xuanxuan.php
@@ -2,3 +2,5 @@
 $lang->client->menu = $lang->admin->menu;
 
 $lang->client->browseVersion = 'Browse Version';
+
+$lang->client->noClient = 'No client version yet.';
--- extension/xuanxuan/extension/xuan/client/ext/lang/zh-cn/xuanxuan.php
+++ extension/xuanxuan/extension/xuan/client/ext/lang/zh-cn/xuanxuan.php
@@ -1,4 +1,7 @@
 <?php
 $lang->client->common = '客户端版本更新';
+$lang->client->create = '添加版本';
 
 $lang->client->browseVersion = '版本列表';
+
+$lang->client->noClient = '暂时没有客户端版本。';
