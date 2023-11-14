25900488065d5a439e3aa9f0eda703395d09a2ad
liugang <liugang@easycorp.ltd>
2023-11-14 16:16:17 +0800

    * story: fix error the story title didn't display its color.

--- module/story/ui/view.html.php
+++ module/story/ui/view.html.php
@@ -154,11 +154,10 @@ detailHeader
         entityLabel
         (
             set::entityID($story->id),
-            set::style(array('color' => $story->color)),
             set::level(1),
             $story->parent > 0 ? label(setClass('circle child'), $lang->story->childrenAB) : null,
             $story->parent > 0 && isset($story->parentName) ? span(a(set::href(inlink('view', "storyID={$story->parent}&version=0&param=0&storyType=$story->type")), $story->parentName), ' / ') : null,
-            $story->title,
+            span(setStyle(array('color' => $story->color)), $story->title),
         ),
         count($versions) > 1 ? dropdown
         (
