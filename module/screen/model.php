797e2f0d1fd1bb7877f2823022a3ce09bd5da0d6
宋辰轩 <songchenxuan@easycorp.ltd>
2023-11-15 08:50:15 +0800

    revert 99ef169499efdca4dd82d2a35795d2d94dc48403
    revert * Fix bug for screen.

--- module/screen/model.php
+++ module/screen/model.php
@@ -358,7 +358,7 @@ class screenModel extends model
      */
     public function getAxisRotateOption($chart, $component)
     {
-        $settings = json_decode($chart->settings, true);
+        $settings = json_decode($chart->settings);
         $setting  = $settings[0];
 
         $component->chartConfig->xAxis = new stdclass();
