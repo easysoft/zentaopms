f9595c75d06a5f41df8f25176d88fa475a6a220e
wangyuting <wangyuting@cnezsoft.ltd>
2023-11-15 02:36:41 +0000

    * Fix project config error on lite vision.

--- extension/lite/project/ext/config/lite.php
+++ extension/lite/project/ext/config/lite.php
@@ -2,9 +2,9 @@
 $config->project->list->exportFields = 'id,code,name,status,PM,desc';
 
 if(!isset($config->project->datatable)) $config->project->datatable = new stdclass();
-$config->project->datatable->defaultField = array('id', 'name', 'status', 'PM', 'begin', 'end', 'progress', 'actions');
+$config->project->dtable->defaultField = array('id', 'name', 'status', 'PM', 'begin', 'end', 'progress', 'actions');
 
-unset($config->project->datatable->fieldList['budget']);
+unset($config->project->dtable->fieldList['budget']);
 
 unset($config->project->search['fields']['hasProduct']);
 unset($config->project->search['fields']['parent']);
