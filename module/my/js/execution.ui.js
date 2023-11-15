3d96994c4f5027133dad047331df6507d106089d
liumengyi <liumengyi@easycorp.ltd>
2023-11-15 16:51:11 +0800

    * Fix label type in my::execution.

--- module/my/js/execution.ui.js
+++ module/my/js/execution.ui.js
@@ -2,7 +2,7 @@ window.onRenderExecutionCell = function(result, info)
 {
     if(info.col.name === 'name' && systemMode == 'ALM')
     {
-        result.push({html: '<span class="label size-sm circle secondary-pale">' + typeList[info.row.data.type] + '</span>'}, {className:'row-reverse'});
+        result.push({html: '<span class="label size-sm circle ' + (info.row.data.type == 'stage' ? 'warning' : 'secondary') + '-pale">' + typeList[info.row.data.type] + '</span>'}, {className:'row-reverse'});
     }
 
     return result;
