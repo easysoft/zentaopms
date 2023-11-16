bc9a2cb0ef932ffae5462ad55a64ed155dde6209
qixinzhi <qixinzhi@easycorp.ltd>
2023-11-16 09:35:56 +0800

    * Remove unique key of code in zt_metric.

--- db/zentao.sql
+++ db/zentao.sql
@@ -15929,7 +15929,6 @@ CREATE TABLE IF NOT EXISTS `zt_metric` (
   `order` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `deleted` enum('0','1') NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `code` (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- DROP TABLE IF EXISTS `zt_metriclib`;
