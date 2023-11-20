293f8c3b5879f0d37447466a66664ff718abb789
hufangzhou <hufangzhou@easycorp.ltd>
2023-11-20 10:32:09 +0800

    * Merge 18.4 story module.

--- module/story/model.php
+++ module/story/model.php
@@ -3292,7 +3292,8 @@ class storyModel extends model
             if(!$this->session->storyOnlyCondition)
             {
                 preg_match_all('/[`"]' . trim(TABLE_STORY, '`') .'[`"] AS ([\w]+) /', $this->session->storyQueryCondition, $matches);
-                if(isset($matches[1][0])) return 'id in (' . preg_replace('/SELECT .* FROM/', "SELECT {$matches[1][0]}.id FROM", $this->session->storyQueryCondition) . ')';
+                $tableAlias = isset($matches[1][0]) ? $matches[1][0] . '.' : '';
+                return 'id in (' . preg_replace('/SELECT .* FROM/', "SELECT $tableAlias" . "id FROM", $this->session->storyQueryCondition) . ')';
             }
             return $this->session->storyQueryCondition;
         }
