0557257e9bba623bd8768e9d42c1188a044cabb9
liugang <liugang@easycorp.ltd>
2023-11-20 14:09:45 +0800

    + project: add functions to batch check date.

--- module/project/js/batchedit.ui.js
+++ module/project/js/batchedit.ui.js
@@ -1,3 +1,5 @@
+let batchIgnoreTips = [];
+
 /**
  * 更新可用工作日天数。
  * Update work days.
@@ -13,6 +15,66 @@ function batchComputeWorkDays()
     const beginDate = $tr.find('[name^=begin]').val();
     const endDate   = $tr.find('[name^=end]').val();
     $tr.find('[name^=days]').val(computeDaysDelta(beginDate, endDate));
+}
+
+/**
+ * 检查项目起止日期是否超出父项目集起止日期。
+ * Check whether the start and end dates of the project exceed the start and end dates of the parent program.
+ *
+ * @access public
+ * @return void
+ */
+function batchCheckDate()
+{
+    const $tr   = $(this).closest('tr');
+    const index = $tr.data('index');
+    if(batchIgnoreTips[index]) return;
+
+    const end   = $tr.find('[name^=end]').val();
+    const begin = $tr.find('[name^=begin]').val();
+    if(!begin || !end) return;
+
+    const selectedProgramID = $tr.find('[name^=parent]').val();
+    if(selectedProgramID == 0 || selectedProgramID == undefined)
+    {
+        $tr.next('tr.dateTip').remove();
+        return;
+    }
+
+    const projectID = $tr.find('[name^=id]').val();
+    $.get($.createLink('project', 'ajaxGetProjectFormInfo', 'objectType=project&objectID=' + projectID + '&selectedProgramID=' + selectedProgramID), function(response)
+    {
+        const data         = JSON.parse(response);
+        const parentEnd    = new Date(data.selectedProgramEnd);
+        const parentBegin  = new Date(data.selectedProgramBegin);
+        const projectEnd   = new Date(end);
+        const projectBegin = new Date(begin);
+
+        if(projectBegin >= parentBegin && projectEnd <= parentEnd)
+        {
+            $tr.next('tr.dateTip').remove();
+            return;
+        }
+
+        if($tr.next('tr.dateTip').length == 0) $tr.after($('#dateTipTemplate tr').clone());
+        $tr.next('tr.dateTip').find('.beginLess').toggleClass('hidden', projectBegin >= parentBegin).find('.parentBegin').text(data.selectedProgramBegin);
+        $tr.next('tr.dateTip').find('.endGreater').toggleClass('hidden', projectEnd <= parentEnd).find('.parentEnd').text(data.selectedProgramEnd);
+        $tr.next('tr.dateTip').find('a').attr('onclick', 'batchIgnoreTip(' + index + ')');
+    });
+}
+
+/**
+ * 忽略日期提示。
+ * Ignore date tips.
+ *
+ * @access public
+ * @param  int    index
+ * @return void
+ */
+batchIgnoreTip = function(index)
+{
+    $('tr[data-index="' + index + '"]').next('tr.dateTip').remove();
+    batchIgnoreTips[index] = true;
 }
 
 window.renderRowData = function($row, index, row)
