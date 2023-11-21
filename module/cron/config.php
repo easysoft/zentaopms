5cc96ab5663cdd831fd21c22009345327ed8b3e7
zhujinyong <zhujinyong@easycorp.ltd>
2023-11-21 17:29:43 +0800

    * Set max consumer to 2.

--- module/cron/config.php
+++ module/cron/config.php
@@ -5,5 +5,6 @@ $config->cron->edit   = new stdclass();
 $config->cron->create->requiredFields = 'm,h,dom,mon,dow,command';
 $config->cron->edit->requiredFields   = 'm,h,dom,mon,dow,command';
 
-$config->cron->maxRunDays = 8;
-$config->cron->maxRunTime = 65;
+$config->cron->maxRunDays  = 8;
+$config->cron->maxRunTime  = 65;
+$config->cron->maxConsumer = 2;
