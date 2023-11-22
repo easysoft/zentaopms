7f7c095cedb68f7e9209e9e556622d7b21ad5b3e
tianshujie <tianshujie@easycorp.ltd>
2023-11-22 14:16:06 +0800

    * Change the table data of zt_lang.

--- module/custom/test/model/getursrlist.php
+++ module/custom/test/model/getursrlist.php
@@ -3,15 +3,7 @@
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/custom.class.php';
 
-$langTable = zdTable('lang');
-$langTable->lang->range('zh-cn');
-$langTable->module->range('custom');
-$langTable->section->range('URSRList');
-$langTable->key->range('1-5');
-$langTable->value->range('`{"SRName":"\\\u8f6f\\\u4ef6\\\u9700\\\u6c42","URName":"\\\u7528\\\u6237\\\u9700\\\u6c42"}`,`{"SRName":"\\\u7814\\\u53d1\\\u9700\\\u6c42","URName":"\\\u7528\\\u6237\\\u9700\\\u6c42"}`,`{"SRName":"\\\u8f6f\\\u9700","URName":"\\\u7528\\\u9700"}`,`{"SRName":"\\\u6545\\\u4e8b","URName":"\\\u53f2\\\u8bd7"}`,`{"SRName":"\\\u9700\\\u6c42","URName":"\\\u7528\\\u6237\\\u9700\\\u6c42"}`');
-$langTable->system->range('1{4},0');
-$langTable->gen(5);
-
+zdTable('lang')->config('lang')->gen(5);
 zdTable('user')->gen(5);
 su('admin');
 
