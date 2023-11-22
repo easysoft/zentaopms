439c3decb52e1f0eb7c24ce2d366118d095434c8
caoyanyi <caoyanyi@easycorp.ltd>
2023-11-22 09:38:05 +0800

    + Add open method for log.

--- config/zentaopms.php
+++ config/zentaopms.php
@@ -232,6 +232,7 @@ $config->openMethods[] = 'feedback.mergeproductmodule';
 $config->openMethods[] = 'zanode.nodelist';
 $config->openMethods[] = 'action.restorestages';
 $config->openMethods[] = 'action.commentzin';
+$config->openMethods[] = 'action.editcommentzin';
 $config->openMethods[] = 'execution.browse';
 $config->openMethods[] = 'testcase.showImport';
 $config->openMethods[] = 'testsuite.index';
