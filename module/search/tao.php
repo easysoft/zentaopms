76379134fb8825fba4668a262efbd87e9adfdd9d
daitingting <daitingting@xirangit.com>
2023-11-23 09:16:08 +0000

    * Split buildQuery function.

--- module/search/tao.php
+++ module/search/tao.php
@@ -60,5 +60,158 @@ class searchTao extends searchModel
 
         return $searchConfig;
     }
-}
 
+    /**
+     * 处理查询表单的相关数据。
+     * Process query form datas.
+     *
+     * @param  object    $fieldParams
+     * @param  string    $field
+     * @param  string    $andOrName
+     * @param  string    $operatorName
+     * @param  string    $valueName
+     * @access protected
+     * @return array
+     */
+    protected function processQueryFormDatas(object $fieldParams, string $field, string $andOrName, string $operatorName, string $valueName): array
+    {
+        /* 设置分组之间的逻辑关系。*/
+        /* Set and or. */
+        $andOr = strtoupper($this->post->$andOrName);
+        if($andOr != 'AND' && $andOr != 'OR') $andOr = 'AND';
+
+        /* 设置操作符。*/
+        /* Set operator. */
+        $operator = $this->post->$operatorName;
+        if(!isset($this->lang->search->operators[$operator])) $operator = '=';
+
+        /* 设置字段的值。*/
+        /* Skip empty values. */
+        if($this->post->$valueName == 'ZERO') $this->post->$valueName = 0;   // ZERO is special, stands to 0.
+        if(isset($fieldParams->$field) && $fieldParams->$field->control == 'select' && $this->post->$valueName === 'null') $this->post->$valueName = '';   // Null is special, stands to empty if control is select. Fix bug #3279.
+        $value = addcslashes(trim($this->post->$valueName), '%');
+
+        return array($andOr, $operator, $value);
+    }
+
+    /**
+     * 根据字段，操作符和值设置查询条件。
+     * Set condition by field, operator and value.
+     *
+     * @param  string    $field
+     * @param  string    $operator
+     * @param  string    $value
+     * @access protected
+     * @return string
+     */
+    protected function setCondition(string $field, string $operator, string $value): string
+    {
+        $condition = '';
+        if($operator == 'include')
+        {
+            if($field == 'module')
+            {
+                $allModules = $this->loadModel('tree')->getAllChildId($value);
+                if($allModules) $condition = helper::dbIN($allModules);
+            }
+            else
+            {
+                $condition = ' LIKE ' . $this->dbh->quote("%$value%");
+            }
+        }
+        elseif($operator == "notinclude")
+        {
+            if($field == 'module')
+            {
+                $allModules = $this->loadModel('tree')->getAllChildId($value);
+                if($allModules)
+                {
+                    if(count($allModules) > 1) $condition = " NOT " . helper::dbIN($allModules);
+                    else $condition = " !" . helper::dbIN($allModules);
+                }
+            }
+            else
+            {
+                $condition = ' NOT LIKE ' . $this->dbh->quote("%$value%");
+            }
+        }
+        elseif($operator == 'belong')
+        {
+            if($field == 'module')
+            {
+                $allModules = $this->loadModel('tree')->getAllChildId($value);
+                if($allModules) $condition = helper::dbIN($allModules);
+            }
+            elseif($field == 'dept')
+            {
+                $allDepts = $this->loadModel('dept')->getAllChildId($value);
+                $condition = helper::dbIN($allDepts);
+            }
+            elseif($field == 'scene')
+            {
+                $allScenes = $value === '0' ? array() : ($value === '' ? array(0) : $this->loadModel('testcase')->getAllChildId($value));
+                if(count($allScenes)) $condition = helper::dbIN($allScenes);
+            }
+            else
+            {
+                $condition = ' = ' . $this->dbh->quote($value) . ' ';
+            }
+        }
+        else
+        {
+            if($operator == 'between' and !isset($this->config->search->dynamic[$value])) $operator = '=';
+            $condition = $operator . ' ' . $this->dbh->quote($value) . ' ';
+
+            if($operator == '=' and $field == 'id' and preg_match('/^[0-9]+(,[0-9]*)+$/', $value) and !preg_match('/[\x7f-\xff]+/', $value))
+            {
+                $values = array_filter(explode(',', trim($this->dbh->quote($value), "'")));
+                foreach($values as $value) $value = "'" . $value . "'";
+
+                $value     = implode(',', $values);
+                $operator  = 'IN';
+                $condition = $operator . ' (' . $value . ') ';
+            }
+        }
+        return $condition;
+    }
+
+    /**
+     * 设置 where 查询条件。
+     * Set where condition.
+     *
+     * @param  string    $where
+     * @param  string    $field
+     * @param  string    $operator
+     * @param  string    $value
+     * @param  string    $andOr
+     * @access protected
+     * @return string
+     */
+    protected function setWhere(string $where, string $field, string $operator, string $value, string $andOr): string
+    {
+        $condition = $this->setCondition($field, $operator, $value);
+        if($operator == '=' && preg_match('/^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$/', $value))
+        {
+            $condition  = '`' . $field . "` >= '$value' AND `" . $field . "` <= '$value 23:59:59'";
+            $where     .= " $andOr ($condition)";
+        }
+        elseif($operator == '!=' && preg_match('/^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$/', $value))
+        {
+            $condition  = '`' . $field . "` < '$value' OR `" . $field . "` > '$value 23:59:59'";
+            $where     .= " $andOr ($condition)";
+        }
+        elseif($operator == '<=' and preg_match('/^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$/', $value))
+        {
+            $where .= " $andOr " . '`' . $field . "` <= '$value 23:59:59'";
+        }
+        elseif($operator == '>' and preg_match('/^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$/', $value))
+        {
+            $where .= " $andOr " . '`' . $field . "` > '$value 23:59:59'";
+        }
+        elseif($condition)
+        {
+            $where .= " $andOr " . '`' . $field . '` ' . $condition;
+        }
+        return $where;
+    }
+}
