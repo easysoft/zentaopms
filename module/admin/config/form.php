6394d69824ccd8aec1fc829d2510bdaafe19b9cd
xieqiyu <xieqiyu@easycorp.ltd>
2023-11-23 09:01:36 +0800

    * Code for task#106488.

--- module/admin/config/form.php
+++ module/admin/config/form.php
@@ -16,6 +16,5 @@ $config->admin->form->sso['addr']     = array('type' => 'string', 'required' => 
 $config->admin->form->sso['code']     = array('type' => 'string', 'required' => false, 'filter' => 'trim');
 $config->admin->form->sso['key']      = array('type' => 'string', 'required' => false, 'filter' => 'trim');
 
-$defaultFeatures = array('myScore' => 0, 'productRoadmap' => 1, 'productTrack' => 1, 'productUR' => 1, 'otherDevops' => 1, 'otherKanban' => 1);
 $config->admin->form->setmodule = array();
-$config->admin->form->setmodule['module'] = array('type'=> 'array', 'required' => true, 'default' => $defaultFeatures);
+$config->admin->form->setmodule['module'] = array('type'=> 'array', 'required' => true, 'default' => array('myScore' => 0, 'productRoadmap' => 1, 'productTrack' => 1, 'productUR' => 1, 'otherDevops' => 1, 'otherKanban' => 1));
