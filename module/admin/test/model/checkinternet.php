65fd19e08c67f393bc4f17125d6d6f5d61cff50c
xieqiyu <xieqiyu@easycorp.ltd>
2023-11-24 16:21:39 +0800

    * Delete unused unit test.

--- module/admin/test/model/checkinternet.php
+++ module/admin/test/model/checkinternet.php
@@ -1,19 +1,14 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . '/test/lib/init.php';
-
 /**
 
 title=测试 adminModel::checkInternet();
 cid=1
 pid=1
 
-验证网络连接 >> 1
-
 */
 
-global $tester;
-$tester->loadModel('admin');
-$result = (int)$tester->admin->checkInternet();
+include dirname(__FILE__, 5) . '/test/lib/init.php';
 
-r($result) && p() && e('1'); // 验证网络连接
+global $tester;
+r($tester->loadModel('admin')->checkInternet()) && p() && e('1'); // 验证网络连接
