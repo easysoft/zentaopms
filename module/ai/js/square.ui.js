5041b5d340ca986e2f23ec594c60f48def10968d
dingyongliang <dingyongliang@easycorp.ltd>
2023-11-24 15:31:16 +0800

    * fix data-url not update.

--- module/ai/js/square.ui.js
+++ module/ai/js/square.ui.js
@@ -3,18 +3,18 @@ if(!window.aiSquare) window.aiSquare = {};
 window.aiSquare.handleStarBtnClick = function()
 {
     const $btn = $(this);
-    const url = $btn.data('url');
+    const url = $btn.attr('data-url');
     $.get(url, function(response)
     {
         if(response.status == '1')
         {
             $btn.children('img').attr('src', 'static/svg/star.svg');
-            $btn.data('url', $btn.data('url').replace('false', 'true'));
+            $btn.attr('data-url', $btn.attr('data-url').replace('false', 'true'));
         }
         else
         {
             $btn.children('img').attr('src', 'static/svg/star-empty.svg');
-            $btn.data('url', $btn.data('url').replace('true', 'false'));
+            $btn.attr('data-url', $btn.attr('data-url').replace('true', 'false'));
         }
     }, 'json');
 };
