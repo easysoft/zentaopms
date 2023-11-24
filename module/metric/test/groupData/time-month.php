46c1e7f08d0a26647339d19a555ac686e46ca57e
songchenxuan <songchenxuan@easycorp.ltd>
2023-11-24 09:19:17 +0800

    * Add groupTableby week.

--- module/metric/test/groupData/time-month.php
+++ module/metric/test/groupData/time-month.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 适用于按年统计的对象个数，例如 按系统统计的年度新增一级项目集数 */
+/* 适用于按月统计的对象个数，例如 按系统统计的每月新增发布数 */
 
 $resultHeader = array();
 $resultData   = array();
@@ -11,6 +11,8 @@ $resultHeader[] = array('name' => 'calcTime', 'title' => '采集时间', 'width'
 
 $resultData[] = (object) array('date' => '2021年11月', 'dateString' => '2021-11', 'dateType' => 'month', 'value' => 1, 'calcTime' => '2021-11-23 0:23');
 $resultData[] = (object) array('date' => '2022年11月', 'dateString' => '2022-11', 'dateType' => 'month', 'value' => 2, 'calcTime' => '2022-11-23 0:23');
+$resultData[] = (object) array('date' => '2023年5月',  'dateString' => '2023-5',  'dateType' => 'month', 'value' => 3, 'calcTime' => '2023-11-23 0:23');
+$resultData[] = (object) array('date' => '2023年6月',  'dateString' => '2023-6',  'dateType' => 'month', 'value' => 2, 'calcTime' => '2023-11-23 0:23');
 $resultData[] = (object) array('date' => '2023年7月',  'dateString' => '2023-7',  'dateType' => 'month', 'value' => 1, 'calcTime' => '2023-11-23 0:23');
 $resultData[] = (object) array('date' => '2023年8月',  'dateString' => '2023-8',  'dateType' => 'month', 'value' => 3, 'calcTime' => '2023-11-23 0:23');
 $resultData[] = (object) array('date' => '2023年9月',  'dateString' => '2023-9',  'dateType' => 'month', 'value' => 4, 'calcTime' => '2023-11-23 0:23');
