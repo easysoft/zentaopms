f6148b9346f74c514269f5404dd8d34465e34bb5
liugang <liugang@easycorp.ltd>
2023-11-25 14:41:07 +0800

    * userJS: refactor javascript functions in batch create user page.

--- module/user/js/batchcreate.ui.js
+++ module/user/js/batchcreate.ui.js
@@ -1,65 +1,30 @@
-function changeType(event)
+function batchChangeType(event)
 {
-    const type        = $(event.target).val();
-    const $dept       = $('[data-name="dept"]');
-    const $company    = $('[data-name="companyItem"]');
-    const $join       = $('[data-name="join"]');
-    if(type == 'inside')
-    {
-        $dept.removeClass('hidden');
-        $company.addClass('hidden');
-        $join.removeClass('hidden');
-    }
-    else
-    {
-        $dept.addClass('hidden');
-        $company.removeClass('hidden');
-        $join.addClass('hidden');
-    }
+    const isInside = type == 'inside';
+    $('[data-name="companyItem"]').toggleClass('hidden', isInside);
+    $('[data-name="dept"], [data-name="join"]').toggleClass('hidden', !isInside);
     $('#userType').val(type);
 }
 
-function toggleNew(event)
+function batchToggleNew(event)
 {
-    const $target     = $(event.target);
-    const $currentRow = $target.closest('tr');
-    const $company    = $currentRow.find('div[data-name="company"]').closest('.picker-box');
-    const $newCompany = $currentRow.find('input[data-name="newCompany"]');
-    $target.toggleClass('checked');
-    if($target.hasClass('checked'))
-    {
-        $company.addClass('hidden');
-        $newCompany.removeClass('hidden');
-    }
-    else
-    {
-        $company.removeClass('hidden');
-        $newCompany.addClass('hidden');
-    }
+    const $currentRow = $(event.target).closest('tr');
+    const $company    = $currentRow.find('div[data-name="company"]').closest('.picker-box').toggleClass('hidden');
+    const $newCompany = $currentRow.find('input[data-name="newCompany"]').toggleClass('hidden');
 }
 
 /**
  * Toggle checkbox and check password strength.
  *
- * @param  event  $event
+ * @param  event  event
  * @access public
  * @return void
  */
-function togglePasswordStrength(event)
+function batchTogglePasswordStrength(event)
 {
-    const $target     = $(event.target);
-    const $currentRow = $target.closest('tr');
-    const password    = $target.val();
-
-    let $passwordStrength = $currentRow.find('.passwordStrength');
-    if(password == '')
-    {
-        $passwordStrength.addClass('hidden');
-        $passwordStrength.html('');
-    }
-    else
-    {
-        $passwordStrength.html(passwordStrengthList[computePasswordStrength(password)]);
-        $passwordStrength.removeClass('hidden');
-    }
+    const $target   = $(event.target);
+    const password  = $target.val();
+    const $strength = $target.closest('tr').find('.passwordStrength');
+    $strength.toggleClass('hidden', password == '');
+    $strength.html(password == '' ? '' : passwordStrengthList[computePasswordStrength(password)]);
 }
