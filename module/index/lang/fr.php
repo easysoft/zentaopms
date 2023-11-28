6514eadd0ff431ddc1436d4ba2ee444bbb45bc43
hufangzhou <hufangzhou@easycorp.ltd>
2023-11-28 09:12:26 +0800

    * Merge 18.9 index module.

--- module/index/lang/fr.php
+++ module/index/lang/fr.php
@@ -16,3 +16,12 @@ $lang->index->upgrade        = 'Upgrade';
 $lang->index->log            = 'Log';
 $lang->index->detailed       = 'Details';
 $lang->index->website        = 'Please visit the official website';
+
+$lang->index->chat = new stdclass();
+$lang->index->chat->chat = 'Chat';
+$lang->index->chat->ai   = 'AI';
+$lang->index->chat->unconfiguredFormat  = '%s is currently not configured. %s.';
+$lang->index->chat->goConfigureFormat   = 'To configure the function, click to navigate to <a class="text-primary configure-chat-button" href="%s">%s configuration</a>';
+$lang->index->chat->contactAdminForHelp = 'Please contact an administrator for assistance';
+$lang->index->chat->unauthorized        = 'You are not authorized to access AI chatting. Please contact an administrator for permission.';
+$lang->index->chat->reloadTip           = 'You may <a class="text-primary" id="reload-ai-chat">reload this page</a> if you believe that the configuration has been completed.';
