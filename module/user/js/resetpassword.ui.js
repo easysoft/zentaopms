d4fd4787493750c2f56e5e46a845a50ff62a33c6
liugang <liugang@easycorp.ltd>
2023-11-28 17:02:59 +0800

    - userJS: remove unused code in resetpassword.ui.js.

--- module/user/js/resetpassword.ui.js
+++ module/user/js/resetpassword.ui.js
@@ -9,28 +9,4 @@ $(function()
             $('#time').text(time - 1 <= 0 ? 0 : time - 1);
         }, 1000);
     }
-
-    var password1Encrypted = false
-    var password2Encrypted = false
-    $('#password1').on('change', function(){password1Encrypted = false});
-    $('#password2').on('change', function(){password2Encrypted = false});
-
-    $('form .btn[type="submit"]').on('click', function()
-    {
-        if(!password1Encrypted || !password2Encrypted)
-        {
-            var password1 = $('#password1').val().trim();
-            var password2 = $('#password2').val().trim();
-            if(!password1Encrypted)
-            {
-                $("#passwordLength").val(password1.length);
-            }
-
-            var rand      = $('input#verifyRand').val();
-            if(password1 && !password1Encrypted) $('#password1').val(md5(password1) + rand);
-            if(password2 && !password2Encrypted) $('#password2').val(md5(password2) + rand);
-            password1Encrypted = true;
-            password2Encrypted = true;
-        }
-    })
 })
