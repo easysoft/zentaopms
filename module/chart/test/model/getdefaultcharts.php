0eb71c34b3166bee726e73056b2307f3240238ed
xieqiyu <xieqiyu@easycorp.ltd>
2023-11-29 13:29:52 +0800

    * Add test file for the method of getDefaultCharts.

---
+++ module/chart/test/model/getdefaultcharts.php
@@ -0,0 +1,22 @@
+#!/usr/bin/env php
+<?php
+/**
+
+title=测试 chartModel::getDefaultCharts();
+cid=1
+pid=1
+
+*/
+
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+
+zdTable('module')->config('module')->gen(27);
+zdTable('user')->gen(5);
+su('admin');
+
+global $tester;
+$chart = $tester->loadModel('chart');
+
+r($chart->getDefaultCharts(36))  && p() && e('0');                //测试传入的分组是子分组时，返回空数据
+r($chart->getDefaultCharts(123)) && p() && e('0');                //测试查询不存在的分组
+r($chart->getDefaultCharts(35))  && p('0:currentGroup') && e(45); //获取组织分组下第一个图表的当前分组ID
--- module/chart/test/model/getfirstgroup.php
+++ module/chart/test/model/getfirstgroup.php
@@ -10,10 +10,11 @@ pid=1
 
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 
-zdTable('module')->config('module')->gen(12);
+zdTable('module')->config('module')->gen(27);
 zdTable('user')->gen(5);
 su('admin');
 
 global $tester;
 
-r($tester->loadModel('chart')->getFirstGroup(1)) && p() && e(1); //测试获取宏观管理维度下的第一个分组ID
+r($tester->loadModel('chart')->getFirstGroup(1))   && p() && e(32); //测试获取宏观管理维度下的第一个分组ID
+r($tester->loadModel('chart')->getFirstGroup(111)) && p() && e(0);  //测试获取一个不存在的维度的分组ID
