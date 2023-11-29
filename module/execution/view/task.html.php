33ca7b3a39fd8bede0bd806f1eb720ed667b34c8
hufangzhou <hufangzhou@easycorp.ltd>
2023-11-29 15:38:08 +0800

    * Fix bug #40672.

--- module/execution/view/task.html.php
+++ module/execution/view/task.html.php
@@ -15,7 +15,7 @@ include '../../common/view/header.html.php';
 include '../../common/view/datatable.fix.html.php';
 include '../../common/view/zui3dtable.html.php';
 $cols      = $this->task->generateCol($orderBy);
-$tasks     = $this->task->generateRow($tasks, $users, $executionID, $showBranch, $branchGroups, $modulePairs);
+$tasks     = $this->task->generateRow($tasks, $users, $execution, $showBranch, $branchGroups, $modulePairs);
 $assignApp = $setModule ? '' : '#app=project';
 $sortLink  = helper::createLink('execution', 'task', "executionID={$execution->id}&status={$status}&param={$param}&orderBy={orderBy}&recTotal={$recTotal}&recPerPage={$recPerPage}") . $assignApp;
 js::set('moduleID', $moduleID);
