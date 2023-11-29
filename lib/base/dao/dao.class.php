3eeb0a585b1bead5ad5a318acef0c1c75e839be2
zhujinyong <zhujinyong@easycorp.ltd>
2023-11-29 14:20:39 +0800

    * Compatible with 'NULL' in sql.

--- lib/base/dao/dao.class.php
+++ lib/base/dao/dao.class.php
@@ -2356,6 +2356,7 @@ class baseSQL
      */
     public function quote($value)
     {
+        if($value === null) return 'NULL';
         if($this->magicQuote) $value = stripslashes($value);
         return $this->dbh->quote((string)$value);
     }
