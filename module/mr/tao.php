771547263323711ce240a4fd4408adec6bb1485f
caoyanyi <caoyanyi@easycorp.ltd>
2023-11-30 11:13:47 +0800

    * Adjust mr model methods.

--- module/mr/tao.php
+++ module/mr/tao.php
@@ -28,4 +28,25 @@ class mrTao extends mr
 
         return !dao::isError();
     }
+
+    /**
+     * 根据合并请求获取关联对象信息。
+     * Get story,task,bug pairs which linked MR.
+     *
+     * @param  int    $MRID
+     * @param  string $objectType story|task|bug
+     * @access public
+     * @return array
+     */
+    protected function getLinkedObjectPairs(int $MRID, string $objectType = 'story'): array
+    {
+        $table = $this->config->objectTables[$objectType];
+        return $this->dao->select('relation.BID')->from(TABLE_RELATION)->alias('relation')
+            ->leftJoin($table)->alias('object')->on('relation.BID = object.id')
+            ->where('relation.AType')->eq('mr')
+            ->andWhere('relation.BType')->eq($objectType)
+            ->andWhere('relation.AID')->eq($MRID)
+            ->andWhere('object.deleted')->eq(0)
+            ->fetchPairs();
+    }
 }
