34783d2510be50b1f3505116c8f518fb5859c245
sunguangming <sunguangming@easycorp.ltd>
2023-12-01 15:37:03 +0800

    * Modify unit test.

--- module/setting/test/model/updateversion.php
+++ module/setting/test/model/updateversion.php
@@ -7,14 +7,14 @@ su('admin');
 /**
 
 title=测试 settingModel->updateVersion();
+timeout=0
 cid=1
-pid=1
 
-测试version值为16.5，添加数据 >> 16.5
-测试version值为max3.0，更新数据 >> max3.0
-测试version值为max2.0，更新数据 >> max2.0
-测试version值为biz6.5，更新数据 >> biz6.5
-测试version值为0，更新数据 >> 0
+- 测试version值为16.5，添加数据第1条的value属性 @16.5
+- 测试version值为max3.0，更新数据第1条的value属性 @max3.0
+- 测试version值为max2.0，更新数据第1条的value属性 @max2.0
+- 测试version值为biz6.5，更新数据第1条的value属性 @biz6.5
+- 测试version值为0，更新数据第1条的value属性 @0
 
 */
 
@@ -27,4 +27,3 @@ r($setting->updateVersionTest($versionList[1])) && p('1:value') && e('max3.0'); 
 r($setting->updateVersionTest($versionList[2])) && p('1:value') && e('max2.0'); //测试version值为max2.0，更新数据
 r($setting->updateVersionTest($versionList[3])) && p('1:value') && e('biz6.5'); //测试version值为biz6.5，更新数据
 r($setting->updateVersionTest($versionList[4])) && p('1:value') && e('0');      //测试version值为0，更新数据
-
