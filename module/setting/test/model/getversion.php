1eadc4498667e5609c2b16622e0183c80a691274
sunguangming <sunguangming@easycorp.ltd>
2023-12-01 15:36:25 +0800

    * Modify unit test.

--- module/setting/test/model/getversion.php
+++ module/setting/test/model/getversion.php
@@ -4,16 +4,18 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/setting.class.php';
 su('admin');
 
+zdTable('config')->gen(7);
+
 /**
 
 title=测试 settingModel->getVersion();
+timeout=0
 cid=1
-pid=1
 
-测试正常查询version >> 16.5
+- 测试正常查询version @10.0
 
 */
 
 $setting = new settingTest();
 
-r($setting->getVersionTest()) && p() && e('16.5'); //测试正常查询version
+r($setting->getVersionTest()) && p() && e('10.0'); //测试正常查询version
