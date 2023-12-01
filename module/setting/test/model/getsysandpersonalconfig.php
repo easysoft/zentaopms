4df759a5ca7735893b3036c2b97091e0581edd69
sunguangming <sunguangming@easycorp.ltd>
2023-12-01 15:36:12 +0800

    * Modify unit test.

--- module/setting/test/model/getsysandpersonalconfig.php
+++ module/setting/test/model/getsysandpersonalconfig.php
@@ -4,16 +4,18 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/setting.class.php';
 su('admin');
 
+zdTable('config')->gen(7);
+
 /**
 
 title=测试 settingModel->getSysAndPersonalConfig();
+timeout=0
 cid=1
-pid=1
 
-测试account值为system，可正常查询system的数据 >> 1
-测试account值为admin，可正常查询system和admin的数据 >> 1
-测试account值为dev，可正常查询system和dev的数据 >> 1
-测试account值为空，可正常查询system的数据 >> 1
+- 测试account值为system，可正常查询system的数据 @1
+- 测试account值为admin，可正常查询system和admin的数据 @1
+- 测试account值为dev，可正常查询system和dev的数据 @1
+- 测试account值为空，可正常查询system的数据 @1
 
 */
 
