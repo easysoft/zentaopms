e360c2fb3bcacd2699781fe9ba6f2391632c2f6b
sunguangming <sunguangming@easycorp.ltd>
2023-12-01 15:36:37 +0800

    * Modify unit test.

--- module/setting/test/model/parseitempath.php
+++ module/setting/test/model/parseitempath.php
@@ -4,16 +4,36 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/setting.class.php';
 su('admin');
 
+zdTable('config')->gen(7);
+
 /**
 
 title=测试 settingModel->parseItemPath();
+timeout=0
 cid=1
-pid=1
 
-解析参数包含vision、owner、module、section、key的数据 >> rnd,system,common,global,version
-解析参数包含vision、owner、module、key的数据 >> rnd,system,common,,hourPoint
-解析参数包含owner、module、section、key的数据 >> system,common,global,mode
-解析参数包含owner、module、key的数据 >> system,common,,CRProduct
+- 解析参数包含vision、owner、module、section、key的数据
+ - 属性vision @rnd
+ - 属性owner @system
+ - 属性module @common
+ - 属性section @global
+ - 属性key @version
+- 解析参数包含vision、owner、module、key的数据
+ - 属性vision @rnd
+ - 属性owner @system
+ - 属性module @common
+ - 属性section @~~
+ - 属性key @hourPoint
+- 解析参数包含owner、module、section、key的数据
+ - 属性owner @system
+ - 属性module @common
+ - 属性section @global
+ - 属性key @mode
+- 解析参数包含owner、module、key的数据
+ - 属性owner @system
+ - 属性module @common
+ - 属性section @~~
+ - 属性key @CRProduct
 
 */
 
@@ -26,6 +46,6 @@ $params[2]  = "system.common.global.mode";
 $params[3]  = "system.common.CRProduct";
 
 r($setting->parseItemPathTest($params[0]))  && p('vision,owner,module,section,key') && e('rnd,system,common,global,version'); // 解析参数包含vision、owner、module、section、key的数据
-r($setting->parseItemPathTest($params[1]))  && p('vision,owner,module,section,key') && e('rnd,system,common,,hourPoint');     // 解析参数包含vision、owner、module、key的数据
+r($setting->parseItemPathTest($params[1]))  && p('vision,owner,module,section,key') && e('rnd,system,common,~~,hourPoint');   // 解析参数包含vision、owner、module、key的数据
 r($setting->parseItemPathTest($params[2]))  && p('owner,module,section,key')        && e('system,common,global,mode');        // 解析参数包含owner、module、section、key的数据
-r($setting->parseItemPathTest($params[3]))  && p('owner,module,section,key')        && e('system,common,,CRProduct');         // 解析参数包含owner、module、key的数据
+r($setting->parseItemPathTest($params[3]))  && p('owner,module,section,key')        && e('system,common,~~,CRProduct');       // 解析参数包含owner、module、key的数据
