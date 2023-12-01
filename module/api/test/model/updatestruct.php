d49010dc910e636d5d14fbb4298c709b7d7b39e0
wangyuting <wangyuting@cnezsoft.ltd>
2023-12-01 13:48:45 +0800

    * Optimize api updateStruct function and finish unit test script.

--- module/api/test/model/updatestruct.php
+++ module/api/test/model/updatestruct.php
@@ -1,43 +1,37 @@
 #!/usr/bin/env php
 <?php
 include dirname(__FILE__, 5) . '/test/lib/init.php';
-include dirname(__FILE__, 2) . '/api.class.php';
 su('admin');
 
+zdTable('doclib')->config('doclib')->gen(10);
+zdTable('apistruct')->gen(10);
+zdTable('apistruct_spec')->gen(10);
 /**
 
 title=测试 apiModel->updateStruct();
+timeout=0
 cid=1
-pid=1
 
-正常的修改 >> editStruct
-没有名称的修改 >> 『结构名』不能为空。
+- 测试正常更新数据结构。 @1
+- 测试结构名称为空时更新数据结构。 @0
+- 测试结构名称为空时更新数据结构返回的错误信息。第name条的0属性 @『结构名』不能为空。
 
 */
 
 global $tester;
-$api = new apiTest();
+$tester->loadModel('api');
 
-$normalStruct = new stdclass();
-$normalStruct->name      = 'struct';
-$normalStruct->type      = 'formData';
-$normalStruct->attribute = '[{"field":"field1","paramsType":"string","required":true,"desc":"desc","structType":"formData","sub":1,"key":"l2u5qy3jc1se6ghigll","children":[]}]';
-$normalStruct->desc      = '';
-$normalStruct->addedBy   = $tester->app->user->account;
-$normalStruct->addedDate = helper::now();
+$formData = new stdclass();
+$formData->id   = 1;
+$formData->lib  = 1;
+$formData->name = '数据结构A';
 
-$data = array(
-    'name'      => 'editStruct',
-    'type'      => 'formData',
-    'attribute' => '[{"field":"field1","paramsType":"string","required":true,"desc":"desc","structType":"formData","sub":1,"key":"l2u5qy3jc1se6ghigll","children":[]}]',
-    'desc'      => '',
-);
+r($tester->api->updateStruct($formData)) && p() && e(1); // 测试正常更新数据结构。
 
-$normalEditStruct = $data;
+$formData = new stdclass();
+$formData->id   = 2;
+$formData->lib  = 1;
+$formData->name = '';
 
-$emptyNameEditStruct = $data;
-$emptyNameEditStruct['name'] = '';
-
-$struct = $api->createStructTest($normalStruct, false);
-r($api->updateStructTest($struct->id, $normalEditStruct, false)) && p('0:new') && e('editStruct');                //正常的修改
-r($api->updateStructTest($struct->id, $emptyNameEditStruct)) && p('name:0') && e('『结构名』不能为空。'); //没有名称的修改
+r($tester->api->updateStruct($formData)) && p() && e(0);        // 测试结构名称为空时更新数据结构。
+r(dao::getError()) && p('name:0') && e('『结构名』不能为空。'); // 测试结构名称为空时更新数据结构返回的错误信息。
