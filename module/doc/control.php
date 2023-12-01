9a8d617f7cc822b3818c277177f388040c592bbd
tianshujie <tianshujie@easycorp.ltd>
2023-12-01 17:04:29 +0800

    - Remove unused doc::deleteFile method.

--- module/doc/control.php
+++ module/doc/control.php
@@ -686,41 +686,6 @@ class doc extends control
     }
 
     /**
-     * Delete file for doc.
-     *
-     * @param int $docID
-     * @param int $fileID
-     * @param string $confirm
-     * @access public
-     * @return void
-     */
-    public function deleteFile($docID, $fileID, $confirm = 'no')
-    {
-        $this->loadModel('file');
-        $this->loadModel('action');
-        if($confirm == 'no')
-        {
-            return print(js::confirm($this->lang->file->confirmDelete, inlink('deleteFile', "docID=$docID&fileID=$fileID&confirm=yes")));
-        }
-        else
-        {
-            $docContent = $this->dao->select('t1.*')->from(TABLE_DOCCONTENT)->alias('t1')
-                ->leftJoin(TABLE_DOC)->alias('t2')->on('t1.doc=t2.id and t1.version=t2.version')
-                ->where('t2.id')->eq($docID)
-                ->fetch();
-            unset($docContent->id);
-            $docContent->files = trim(str_replace(",{$fileID},", ',', ",{$docContent->files},"), ',');
-            $docContent->version += 1;
-            $this->dao->insert(TABLE_DOCCONTENT)->data($docContent)->exec();
-            $this->dao->update(TABLE_DOC)->set('version')->eq($docContent->version)->where('id')->eq($docID)->exec();
-
-            $file = $this->file->getById($fileID);
-            if(in_array($file->extension, $this->config->file->imageExtensions)) $this->action->create($file->objectType, $file->objectID, 'deletedFile', '', $extra = $file->title);
-            return print(js::locate($this->createLink('doc', 'view', "docID=$docID"), 'parent'));
-        }
-    }
-
-    /**
      * Collect doc, doclib or module of doclib.
      *
      * @param int $objectID
