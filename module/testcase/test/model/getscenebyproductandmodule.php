dde1de303ad7ec591bf1c56ed659d241000e20e2
liumengyi <liumengyi@easycorp.ltd>
2023-12-02 15:00:18 +0800

    * Modify unit test of testcaseModel::getSceneByProductAndModule after changing unit test data.

--- module/testcase/test/model/getscenebyproductandmodule.php
+++ module/testcase/test/model/getscenebyproductandmodule.php
@@ -21,16 +21,16 @@ $moduleIdList  = array(0, 1821, 1825, 1829);
 
 $testcase = new testcaseTest();
 
-r($testcase->getSceneByProductAndModuleTest($productIdList[0], $moduleIdList[0])) && p() && e('0');                                  // 获取产品 0 模块 0 的场景信息
-r($testcase->getSceneByProductAndModuleTest($productIdList[1], $moduleIdList[0])) && p() && e('sceneMaps:1 2 3 4;topScenes:1 2 3;'); // 获取产品 1 模块 0 的场景信息
-r($testcase->getSceneByProductAndModuleTest($productIdList[1], $moduleIdList[1])) && p() && e('sceneMaps:1;topScenes:1;');           // 获取产品 1 模块 1821 的场景信息
-r($testcase->getSceneByProductAndModuleTest($productIdList[1], $moduleIdList[2])) && p() && e('0');                                  // 获取产品 1 模块 1825 的场景信息
-r($testcase->getSceneByProductAndModuleTest($productIdList[1], $moduleIdList[3])) && p() && e('0');                                  // 获取产品 1 模块 1829 的场景信息
-r($testcase->getSceneByProductAndModuleTest($productIdList[2], $moduleIdList[0])) && p() && e('sceneMaps:5 6 7 8;');                 // 获取产品 2 模块 0 的场景信息
-r($testcase->getSceneByProductAndModuleTest($productIdList[2], $moduleIdList[1])) && p() && e('0');                                  // 获取产品 2 模块 1821 的场景信息
-r($testcase->getSceneByProductAndModuleTest($productIdList[2], $moduleIdList[2])) && p() && e('sceneMaps:5;');                       // 获取产品 2 模块 1825 的场景信息
-r($testcase->getSceneByProductAndModuleTest($productIdList[2], $moduleIdList[3])) && p() && e('0');                                  // 获取产品 2 模块 1829 的场景信息
-r($testcase->getSceneByProductAndModuleTest($productIdList[3], $moduleIdList[0])) && p() && e('sceneMaps:9 10 11 12;');              // 获取产品 3 模块 0 的场景信息
-r($testcase->getSceneByProductAndModuleTest($productIdList[3], $moduleIdList[1])) && p() && e('0');                                  // 获取产品 3 模块 1821 的场景信息
-r($testcase->getSceneByProductAndModuleTest($productIdList[3], $moduleIdList[2])) && p() && e('0');                                  // 获取产品 3 模块 1825 的场景信息
-r($testcase->getSceneByProductAndModuleTest($productIdList[3], $moduleIdList[3])) && p() && e('sceneMaps:10;');                      // 获取产品 3 模块 1829 的场景信息
+r($testcase->getSceneByProductAndModuleTest($productIdList[0], $moduleIdList[0])) && p() && e('0');                                          // 获取产品 0 模块 0 的场景信息
+r($testcase->getSceneByProductAndModuleTest($productIdList[1], $moduleIdList[0])) && p() && e('sceneMaps:1 2 3 4;topScenes:1;');             // 获取产品 1 模块 0 的场景信息
+r($testcase->getSceneByProductAndModuleTest($productIdList[1], $moduleIdList[1])) && p() && e('sceneMaps:1;topScenes:1;');                   // 获取产品 1 模块 1821 的场景信息
+r($testcase->getSceneByProductAndModuleTest($productIdList[1], $moduleIdList[2])) && p() && e('0');                                          // 获取产品 1 模块 1825 的场景信息
+r($testcase->getSceneByProductAndModuleTest($productIdList[1], $moduleIdList[3])) && p() && e('0');                                          // 获取产品 1 模块 1829 的场景信息
+r($testcase->getSceneByProductAndModuleTest($productIdList[2], $moduleIdList[0])) && p() && e('sceneMaps:5 6 7 8;topScenes:5 6;');           // 获取产品 2 模块 0 的场景信息
+r($testcase->getSceneByProductAndModuleTest($productIdList[2], $moduleIdList[1])) && p() && e('0');                                          // 获取产品 2 模块 1821 的场景信息
+r($testcase->getSceneByProductAndModuleTest($productIdList[2], $moduleIdList[2])) && p() && e('sceneMaps:5;topScenes:5;');                   // 获取产品 2 模块 1825 的场景信息
+r($testcase->getSceneByProductAndModuleTest($productIdList[2], $moduleIdList[3])) && p() && e('0');                                          // 获取产品 2 模块 1829 的场景信息
+r($testcase->getSceneByProductAndModuleTest($productIdList[3], $moduleIdList[0])) && p() && e('sceneMaps:9 10 11 12;topScenes:9 10 11 12;'); // 获取产品 3 模块 0 的场景信息
+r($testcase->getSceneByProductAndModuleTest($productIdList[3], $moduleIdList[1])) && p() && e('0');                                          // 获取产品 3 模块 1821 的场景信息
+r($testcase->getSceneByProductAndModuleTest($productIdList[3], $moduleIdList[2])) && p() && e('0');                                          // 获取产品 3 模块 1825 的场景信息
+r($testcase->getSceneByProductAndModuleTest($productIdList[3], $moduleIdList[3])) && p() && e('sceneMaps:10;topScenes:10;');                 // 获取产品 3 模块 1829 的场景信息
