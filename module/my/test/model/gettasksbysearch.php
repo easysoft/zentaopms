961d368d333a95ac49c43e9a20744e6e00100dad
liumengyi <liumengyi@easycorp.ltd>
2023-12-02 10:00:46 +0800

    * Modify unit test of myModel::getTasksBySearch.

--- module/my/test/model/gettasksbysearch.php
+++ module/my/test/model/gettasksbysearch.php
@@ -7,7 +7,7 @@ include dirname(__FILE__, 2) . '/my.class.php';
 zdTable('task')->config('task')->gen('40');
 zdTable('story')->gen('40');
 zdTable('project')->config('program')->gen('80');
-zdTable('taskteam')->gen('40');
+zdTable('taskteam')->gen('0');
 zdTable('user')->gen('1');
 
 su('admin');
@@ -23,7 +23,7 @@ pid=1
 */
 
 $my    = new myTest();
-$account  = array('admin', 'user1');
+$account  = array('admin', 'user5');
 $limit    = array(0, 5);
 
 global $tester;
@@ -35,11 +35,11 @@ $tasks2 = $my->getTasksBySearchTest($account[0], $limit[1]);
 $tasks3 = $my->getTasksBySearchTest($account[1], $limit[0]);
 $tasks4 = $my->getTasksBySearchTest($account[1], $limit[1]);
 
-r(implode(',', $tasks1)) && p() && e('37,33,25,21,13,9,1'); // 测试通过搜索获取用户 admin 的任务id
-r(count($tasks1))        && p() && e('7');                  // 测试通过搜索获取用户 admin 的任务数量
-r(implode(',', $tasks2)) && p() && e('37,33,25,21,13');     // 测试通过搜索获取用户 admin 的任务id 限制5
-r(count($tasks2))        && p() && e('5');                  // 测试通过搜索获取用户 admin 的任务数量 限制5
-r(implode(',', $tasks3)) && p() && e('0');                  // 测试通过搜索获取用户 user1 的任务id
-r(count($tasks3))        && p() && e('0');                  // 测试通过搜索获取用户 user1 的任务数量
-r(implode(',', $tasks4)) && p() && e('0');                  // 测试通过搜索获取用户 user1 的任务id 限制5
-r(count($tasks4))        && p() && e('0');                  // 测试通过搜索获取用户 user1 的任务数量 限制5
+r(implode(',', $tasks1)) && p() && e('37,33,29,25,21,17,13,9,5,1'); // 测试通过搜索获取用户 admin 的任务id
+r(count($tasks1))        && p() && e('10');                         // 测试通过搜索获取用户 admin 的任务数量
+r(implode(',', $tasks2)) && p() && e('37,33,29,25,21');             // 测试通过搜索获取用户 admin 的任务id 限制5
+r(count($tasks2))        && p() && e('5');                          // 测试通过搜索获取用户 admin 的任务数量 限制5
+r(implode(',', $tasks3)) && p() && e('0');                          // 测试通过搜索获取用户 user5 的任务id
+r(count($tasks3))        && p() && e('0');                          // 测试通过搜索获取用户 user5 的任务数量
+r(implode(',', $tasks4)) && p() && e('0');                          // 测试通过搜索获取用户 user5 的任务id 限制5
+r(count($tasks4))        && p() && e('0');                          // 测试通过搜索获取用户 user5 的任务数量 限制5
