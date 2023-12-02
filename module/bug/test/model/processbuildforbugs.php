c6f714e223a62c4063de9d646bc58b5655d471c3
liumengyi <liumengyi@easycorp.ltd>
2023-12-02 13:22:17 +0800

    * Modify unit test of bugModel::processBuildForBugs.

--- module/bug/test/model/processbuildforbugs.php
+++ module/bug/test/model/processbuildforbugs.php
@@ -17,11 +17,11 @@ cid=1
 pid=1
 
 - 测试处理bug1 2 3的openedBuild resolvedBuild字段
- - 第1条的openedBuild属性 @项目版本版本1
+ - 第1条的openedBuild属性 @项目11版本1
  - 第1条的resolvedBuild属性 @~~
  - 第2条的openedBuild属性 @0
  - 第2条的resolvedBuild属性 @~~
- - 第3条的openedBuild属性 @项目版本版本1
+ - 第3条的openedBuild属性 @项目11版本1
  - 第3条的resolvedBuild属性 @~~
 
 - 测试处理bug4 5 6的openedBuild resolvedBuild字段
@@ -74,9 +74,9 @@ $bugIDList5 = array('81', '82', '83');
 $bugIDList6 = array('84', '85', '86');
 
 $bug = new bugTest();
-r($bug->processBuildForBugsTest($bugIDList1)) && p('1:openedBuild,resolvedBuild;2:openedBuild,resolvedBuild;3:openedBuild,resolvedBuild')    && e('项目版本版本1,~~,0,~~,项目版本版本1,~~'); // 测试处理bug1 2 3的openedBuild resolvedBuild字段
-r($bug->processBuildForBugsTest($bugIDList2)) && p('4:openedBuild,resolvedBuild;5:openedBuild,resolvedBuild;6:openedBuild,resolvedBuild')    && e('主干,~~,主干,~~,主干,~~');                // 测试处理bug4 5 6的openedBuild resolvedBuild字段
-r($bug->processBuildForBugsTest($bugIDList3)) && p('51:openedBuild,resolvedBuild;52:openedBuild,resolvedBuild;53:openedBuild,resolvedBuild') && e('主干,~~,主干,~~,主干,~~');                // 测试处理bug51 52 53的openedBuild resolvedBuild字段
-r($bug->processBuildForBugsTest($bugIDList4)) && p('54:openedBuild,resolvedBuild;55:openedBuild,resolvedBuild;56:openedBuild,resolvedBuild') && e('主干,~~,主干,~~,主干,主干');              // 测试处理bug54 55 56的openedBuild resolvedBuild字段
-r($bug->processBuildForBugsTest($bugIDList5)) && p('81:openedBuild,resolvedBuild;82:openedBuild,resolvedBuild;83:openedBuild,resolvedBuild') && e('主干,~~,主干,~~,主干,~~');                // 测试处理bug81 82 83的openedBuild resolvedBuild字段
-r($bug->processBuildForBugsTest($bugIDList6)) && p('84:openedBuild,resolvedBuild;85:openedBuild,resolvedBuild;86:openedBuild,resolvedBuild') && e('主干,~~,主干,~~,主干,~~');                // 测试处理bug84 85 86的openedBuild resolvedBuild字段
+r($bug->processBuildForBugsTest($bugIDList1)) && p('1:openedBuild,resolvedBuild;2:openedBuild,resolvedBuild;3:openedBuild,resolvedBuild')    && e('项目11版本1,~~,0,~~,项目11版本1,~~'); // 测试处理bug1 2 3的openedBuild resolvedBuild字段
+r($bug->processBuildForBugsTest($bugIDList2)) && p('4:openedBuild,resolvedBuild;5:openedBuild,resolvedBuild;6:openedBuild,resolvedBuild')    && e('主干,~~,主干,~~,主干,~~');            // 测试处理bug4 5 6的openedBuild resolvedBuild字段
+r($bug->processBuildForBugsTest($bugIDList3)) && p('51:openedBuild,resolvedBuild;52:openedBuild,resolvedBuild;53:openedBuild,resolvedBuild') && e('主干,~~,主干,~~,主干,~~');            // 测试处理bug51 52 53的openedBuild resolvedBuild字段
+r($bug->processBuildForBugsTest($bugIDList4)) && p('54:openedBuild,resolvedBuild;55:openedBuild,resolvedBuild;56:openedBuild,resolvedBuild') && e('主干,~~,主干,~~,主干,主干');          // 测试处理bug54 55 56的openedBuild resolvedBuild字段
+r($bug->processBuildForBugsTest($bugIDList5)) && p('81:openedBuild,resolvedBuild;82:openedBuild,resolvedBuild;83:openedBuild,resolvedBuild') && e('主干,~~,主干,~~,主干,~~');            // 测试处理bug81 82 83的openedBuild resolvedBuild字段
+r($bug->processBuildForBugsTest($bugIDList6)) && p('84:openedBuild,resolvedBuild;85:openedBuild,resolvedBuild;86:openedBuild,resolvedBuild') && e('主干,~~,主干,~~,主干,~~');            // 测试处理bug84 85 86的openedBuild resolvedBuild字段
