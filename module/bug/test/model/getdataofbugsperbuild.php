7f401aecda53e97f3a9359c0c0ef17163eae0eff
liumengyi <liumengyi@easycorp.ltd>
2023-12-02 10:55:38 +0800

    * Modify unit test result of bugModel::getDataOfBugsPerBuild.

--- module/bug/test/model/getdataofbugsperbuild.php
+++ module/bug/test/model/getdataofbugsperbuild.php
@@ -22,11 +22,11 @@ cid=1
  - 第0条的value属性 @1
 
 - 获取项目版本版本1 bug 数
- - 第1条的name属性 @项目版本版本1
+ - 第1条的name属性 @项目11版本1
  - 第1条的value属性 @2
 
 - 获取项目版本版本2 bug 数
- - 第2条的name属性 @项目版本版本2
+ - 第2条的name属性 @项目12版本2
  - 第2条的value属性 @2
 
 */
@@ -34,5 +34,5 @@ cid=1
 $bug = new bugTest();
 r($bug->getDataOfBugsPerBuildTest()) && p('trunk:name,value') && e('主干,6');          // 获取主干 bug 数
 r($bug->getDataOfBugsPerBuildTest()) && p('0:name,value')     && e('未设定,1');        // 获取没关联影响版本的 bug 数
-r($bug->getDataOfBugsPerBuildTest()) && p('1:name,value')     && e('项目版本版本1,2'); // 获取项目版本版本1 bug 数
-r($bug->getDataOfBugsPerBuildTest()) && p('2:name,value')     && e('项目版本版本2,2'); // 获取项目版本版本2 bug 数
+r($bug->getDataOfBugsPerBuildTest()) && p('1:name,value')     && e('项目11版本1,2'); // 获取项目版本版本1 bug 数
+r($bug->getDataOfBugsPerBuildTest()) && p('2:name,value')     && e('项目12版本2,2'); // 获取项目版本版本2 bug 数
