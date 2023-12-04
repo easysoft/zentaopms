55e0e01eaaceb1c69077b2219b36e80b03474217
tianshujie <tianshujie@easycorp.ltd>
2023-12-04 14:22:42 +0800

    + Declare strict type.

--- module/doc/control.php
+++ module/doc/control.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 /**
  * The control file of doc module of ZenTaoPMS.
  *
--- module/doc/model.php
+++ module/doc/model.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 /**
  * The model file of doc module of ZenTaoPMS.
  *
