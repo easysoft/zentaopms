41ac8093974109592aab93f2976173f899f9e3cc
sunguangming <sunguangming@easycorp.ltd>
2023-12-04 13:38:52 +0800

    * Add updateSpace form config.

--- module/kanban/config/form.php
+++ module/kanban/config/form.php
@@ -3,7 +3,9 @@ $config->kanban->form = new stdclass();
 
 $config->kanban->form->createSpace['type']      = array('type' => 'string',  'required' => true,  'default' => '');
 $config->kanban->form->createSpace['name']      = array('type' => 'string',  'required' => true,  'default' => '');
-$config->kanban->form->createSpace['owner']     = array('type' => 'string',  'required' => true,  'default' => '');
+$config->kanban->form->createSpace['owner']     = array('type' => 'string',  'required' => false, 'default' => '');
 $config->kanban->form->createSpace['team']      = array('type' => 'array',   'required' => false, 'default' => '', 'filter'  => 'join');
 $config->kanban->form->createSpace['whitelist'] = array('type' => 'array',   'required' => false, 'default' => '', 'filter'  => 'join');
 $config->kanban->form->createSpace['desc']      = array('type' => 'string',  'required' => false, 'default' => '', 'control' => 'editor');
+
+$config->kanban->form->editSpace = $config->kanban->form->createSpace;
