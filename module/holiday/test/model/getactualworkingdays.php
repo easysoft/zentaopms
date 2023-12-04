046c336bcc053254d57d8e46025efb604d656159
liumengyi <liumengyi@easycorp.ltd>
2023-12-04 14:25:29 +0800

    * Modify unit test to get the actual working day on the same date.

--- module/holiday/test/model/getactualworkingdays.php
+++ module/holiday/test/model/getactualworkingdays.php
@@ -25,7 +25,8 @@ r($holiday->getActualWorkingDaysTest($begin[0], $end[0])) && p() && e('19'); // 
 r($holiday->getActualWorkingDaysTest($begin[0], $end[1])) && p() && e('24'); // 返回处于 14天前 到 14天后 之间的实际工作日。
 r($holiday->getActualWorkingDaysTest($begin[1], $end[0])) && p() && e('13'); // 返回处于 7天前 到 7天后 之间的实际工作日。
 r($holiday->getActualWorkingDaysTest($begin[1], $end[1])) && p() && e('18'); // 返回处于 7天前 到 14天后 之间的实际工作日。
-r($holiday->getActualWorkingDaysTest($begin[2], $end[0])) && p() && e('0');  // 测试开始和结束日期相同的实际工作日。
+r($holiday->getActualWorkingDaysTest($begin[2], $end[0])) && p() && e('1');  // 测试开始和结束日期相同的实际工作日 工作。
+r($holiday->getActualWorkingDaysTest($begin[3], $end[2])) && p() && e('0');  // 测试开始和结束日期相同的实际工作日 节假日。
 r($holiday->getactualworkingdaystest($begin[3], $end[0])) && p() && e('0');  // 测试当结束日期小于开始日期时。
 r($holiday->getactualworkingdaystest($begin[4], $end[0])) && p() && e('0');  // 测试输入开始日期为空。
 r($holiday->getactualworkingdaystest($begin[0], $end[2])) && p() && e('0');  // 测试输入结束日期为空。
