6549ed7c3cec0485c63f8ff995ff41ae30139219
sunguangming <sunguangming@easycorp.ltd>
2023-12-05 10:58:17 +0800

    * Add kanban::create form config.

--- module/kanban/config/form.php
+++ module/kanban/config/form.php
@@ -24,3 +24,20 @@ $config->kanban->form->closeSpace['closedBy']       = array('type' => 'string', 
 $config->kanban->form->closeSpace['closedDate']     = array('type' => 'datetime', 'required' => true,  'default' => $now);
 $config->kanban->form->closeSpace['lastEditedBy']   = array('type' => 'string',   'required' => true,  'default' => $app->user->account);
 $config->kanban->form->closeSpace['lastEditedDate'] = array('type' => 'datetime', 'required' => true,  'default' => $now);
+
+$config->kanban->form->create['name']        = array('type' => 'string',   'required' => true,  'default' => '');
+$config->kanban->form->create['showWIP']     = array('type' => 'string',   'required' => true,  'default' => '');
+$config->kanban->form->create['space']       = array('type' => 'int',      'required' => true,  'default' => '');
+$config->kanban->form->create['owner']       = array('type' => 'string',   'required' => false, 'default' => '');
+$config->kanban->form->create['team']        = array('type' => 'array',    'required' => false, 'default' => '', 'filter'  => 'join');
+$config->kanban->form->create['fluidBoard']  = array('type' => 'string',   'required' => false, 'default' => '');
+$config->kanban->form->create['colWidth']    = array('type' => 'int',      'required' => false, 'default' => '');
+$config->kanban->form->create['minColWidth'] = array('type' => 'int',      'required' => false, 'default' => '');
+$config->kanban->form->create['maxColWidth'] = array('type' => 'int',      'required' => false, 'default' => '');
+$config->kanban->form->create['archived']    = array('type' => 'string',   'required' => false, 'default' => '');
+$config->kanban->form->create['performable'] = array('type' => 'string',   'required' => false, 'default' => '');
+$config->kanban->form->create['alignment']   = array('type' => 'string',   'required' => false, 'default' => '');
+$config->kanban->form->create['desc']        = array('type' => 'string',   'required' => false, 'default' => '', 'control' => 'editor');
+$config->kanban->form->create['whitelist']   = array('type' => 'array',    'required' => false, 'default' => '', 'filter'  => 'join');
+$config->kanban->form->create['createdBy']   = array('type' => 'string',   'required' => true,  'default' => $app->user->account);
+$config->kanban->form->create['createdDate'] = array('type' => 'datetime', 'required' => false, 'default' => $now);
