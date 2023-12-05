d911a1514ee53b6dc3b0b9f173e07a7decb0ce5c
caoyanyi <caoyanyi@easycorp.ltd>
2023-12-05 10:21:31 +0800

    - Remove unused module.

--- module/automation/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$config->automation->ztfSite         = 'https://ztf.im';
-$config->automation->ztfDownload     = 'https://ztf.im/ztf.html';
-$config->automation->ztfManual       = 'https://ztf.im/book/ztf/ztf-about-26.html';
-$config->automation->zendataSite     = 'https://www.zendata.cn';
-$config->automation->zendataDownload = 'https://www.zendata.cn/download.html';
-$config->automation->zendataManual   = 'https://www.zendata.cn/book/zendata/why-zendata-115.html';
