7b58ca9ec71d3d7b40b1a649c19ec15896ea08ad
sunguangming <sunguangming@easycorp.ltd>
2023-12-06 15:00:04 +0800

    * Add unit test.

--- module/kanban/test/model/getkanbandata.php
+++ module/kanban/test/model/getkanbandata.php
@@ -4,28 +4,50 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/kanban.class.php';
 su('admin');
 
+zdTable('kanban')->gen(5);
+zdTable('kanbanregion')->gen(20);
+zdTable('kanbancell')->gen(100);
+zdTable('kanbancolumn')->gen(100);
+zdTable('kanbanlane')->gen(100);
+
 /**
 
 title=测试 kanbanModel->getKanbanData();
+timeout=0
 cid=1
-pid=1
 
-测试获取kanban1的视图 >> columns:4, lanes:1, cards:4
-测试获取kanban2的视图 >> columns:4, lanes:1, cards:4
-测试获取kanban3的视图 >> columns:4, lanes:1, cards:4
-测试获取kanban4的视图 >> columns:4, lanes:1, cards:4
-测试获取kanban5的视图 >> columns:4, lanes:1, cards:4
-测试获取不存在的kanban的视图 >> columns:0, lanes:0, cards:0
+- 查看获取到的kanban1下的列数据
+ - 第0条的title属性 @未开始
+ - 第0条的type属性 @column1
+- 查看获取到的kanban2下的泳道数据
+ - 第0条的title属性 @默认泳道
+ - 第0条的type属性 @common
+- 查看获取到的kanban3下的区域数据 @region3
+- 查看获取到的kanban4下的列数据
+ - 第0条的title属性 @未开始
+ - 第0条的type属性 @column13
+- 查看获取到的kanban5下的泳道数据
+ - 第0条的title属性 @默认泳道
+ - 第0条的type属性 @common
+- 测试获取到的kanban下的数据数量 @159
+- 测试获取到的kanban下的数据数量 @159
+- 测试获取到的kanban下的数据数量 @117
+- 测试获取到的kanban下的数据数量 @117
+- 测试获取到的kanban下的数据数量 @117
 
 */
 
-$kanbanIDList = array('1', '2', '3', '4', '5', '1000001');
+global $tester;
+$tester->loadModel('kanban');
 
-$kanban = new kanbanTest();
+r($tester->kanban->getKanbanData(1)[0]['items'][0]['data']['cols'])  && p('0:title,type') && e('未开始,column1');  // 查看获取到的kanban1下的列数据
+r($tester->kanban->getKanbanData(2)[0]['items'][0]['data']['lanes']) && p('0:title,type') && e('默认泳道,common'); // 查看获取到的kanban2下的泳道数据
+r($tester->kanban->getKanbanData(3)[0]['key'])                       && p('')             && e('region3');         // 查看获取到的kanban3下的区域数据
+r($tester->kanban->getKanbanData(4)[0]['items'][0]['data']['cols'])  && p('0:title,type') && e('未开始,column13'); // 查看获取到的kanban4下的列数据
+r($tester->kanban->getKanbanData(5)[0]['items'][0]['data']['lanes']) && p('0:title,type') && e('默认泳道,common'); // 查看获取到的kanban5下的泳道数据
 
-r($kanban->getKanbanDataTest($kanbanIDList[0])) && p() && e('columns:4, lanes:1, cards:4'); // 测试获取kanban1的视图
-r($kanban->getKanbanDataTest($kanbanIDList[1])) && p() && e('columns:4, lanes:1, cards:4'); // 测试获取kanban2的视图
-r($kanban->getKanbanDataTest($kanbanIDList[2])) && p() && e('columns:4, lanes:1, cards:4'); // 测试获取kanban3的视图
-r($kanban->getKanbanDataTest($kanbanIDList[3])) && p() && e('columns:4, lanes:1, cards:4'); // 测试获取kanban4的视图
-r($kanban->getKanbanDataTest($kanbanIDList[4])) && p() && e('columns:4, lanes:1, cards:4'); // 测试获取kanban5的视图
-r($kanban->getKanbanDataTest($kanbanIDList[5])) && p() && e('columns:0, lanes:0, cards:0'); // 测试获取不存在的kanban的视图
+r(count($tester->kanban->getKanbanData(1), true)) && p() && e('159'); // 测试获取到的kanban下的数据数量
+r(count($tester->kanban->getKanbanData(2), true)) && p() && e('159'); // 测试获取到的kanban下的数据数量
+r(count($tester->kanban->getKanbanData(3), true)) && p() && e('117'); // 测试获取到的kanban下的数据数量
+r(count($tester->kanban->getKanbanData(4), true)) && p() && e('117'); // 测试获取到的kanban下的数据数量
+r(count($tester->kanban->getKanbanData(5), true)) && p() && e('117'); // 测试获取到的kanban下的数据数量
