366df50de8bd51d2a2071ea41625df67ded991dd
liumengyi <liumengyi@easycorp.ltd>
2023-12-06 10:43:06 +0800

    * Optimize unit test code for personnelModel::getProjectTaskInvest.

--- module/personnel/test/model/getprojecttaskinvest.php
+++ module/personnel/test/model/getprojecttaskinvest.php
@@ -16,11 +16,6 @@ title=测试 personnelModel->getProjectTaskInvest();
 cid=1
 pid=1
 
-次方法为页面上的项目信息，人员，创建人物 >> 0
-次方法为页面上的项目信息，人员，已完成任务 >> 0
-次方法为页面上的项目信息，人员，待处理 >> 0
-次方法为页面上的项目信息，人员，任务消耗 >> 0
-
 */
 
 $personnel = new personnelTest('admin');
@@ -32,7 +27,7 @@ $account = array('admin' => 'admin', 'user1' => 'user1');
 $result1 = $personnel->getProjectTaskInvestTest($projectID[0], $account);
 $result2 = $personnel->getProjectTaskInvestTest($projectID[1], $account);
 
-r($result1) && p('admin:createdTask,finishedTask,pendingTask,leftTask,consumedTask')  && e('2,0,0,1,5'); // 测试获取 admin 在项目 11 12 的 创建任务
+r($result1) && p('admin:createdTask,finishedTask,pendingTask,leftTask,consumedTask')  && e('2,0,0,3,4'); // 测试获取 admin 在项目 11 12 的 创建任务
 r($result1) && p('user1:createdTask,finishedTask,pendingTask,leftTask,consumedTask')  && e('0,0,0,0,0'); // 测试获取 user1 在项目 11 12 的 创建任务
 
 r($result2) && p('admin:createdTask,finishedTask,pendingTask,leftTask,consumedTask')  && e('0,0,0,0,0'); // 测试获取 admin 在 不存在的 项目的 创建任务
