4192143c4c13f679d3ce843c2ce05fe6e4decd03
sunguangming <sunguangming@easycorp.ltd>
2023-12-07 13:47:48 +0800

    * Modify comment.

--- module/kanban/test/model/copylanes.php
+++ module/kanban/test/model/copylanes.php
@@ -9,11 +9,12 @@ zdTable('kanbanlane')->gen(1);
 zdTable('kanbanregion')->gen(1);
 
 /**
+
 title=测试 kanbanModel->copyLanes();
 timeout=0
 cid=1
 
-- 正常复制泳道，查看生成的泳道ID数量
+- 正常复制泳道，查看生成的泳道ID
  - 属性1 @2
  - 属性2 @3
  - 属性3 @4
@@ -36,6 +37,6 @@ $copyLane3  = (object)array('id' => 3, 'title' => '复制泳道3');
 $copyLane4  = (object)array('id' => 4, 'title' => '复制泳道4');
 $emptyLane  = (object)array('id' => 5, 'title' => '');
 
-r($tester->kanban->copyLanes($kanban, [$copyLane1, $copyLane2, $copyLane3, $copyLane4], $regionID, $newGroupID)) && p("1,2,3,4") && e('2,3,4,5'); // 正常复制泳道，查看生成的泳道ID数量
+r($tester->kanban->copyLanes($kanban, [$copyLane1, $copyLane2, $copyLane3, $copyLane4], $regionID, $newGroupID)) && p("1,2,3,4") && e('2,3,4,5'); // 正常复制泳道，查看生成的泳道ID
 r($tester->kanban->copyLanes($kanban, [$emptyLane], $regionID, $newGroupID)) && p('') && e('0'); //复制空泳道，没有条目被插入
 r(dao::getError()) && p('name:0') && e('『泳道名称』不能为空。'); //复制空泳道，应该返回错误信息
