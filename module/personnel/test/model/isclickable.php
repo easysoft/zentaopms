3377ab44945d0414d0cef407030bf2c100dc8883
liumengyi <liumengyi@easycorp.ltd>
2023-12-07 13:16:57 +0800

    * Optimize unit test for personnelModel::isClickable.

--- module/personnel/test/model/isclickable.php
+++ module/personnel/test/model/isclickable.php
@@ -1,5 +1,6 @@
 #!/usr/bin/env php
 <?php
+declare(strict_types=1);
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/personnel.class.php';
 
@@ -14,8 +15,9 @@ pid=1
 */
 
 $object     = new stdclass();
-$actionList = array('unbindWhitelist');
+$actionList = array('unbindWhitelist', 'addWhitelist');
 
 $personnel = new personnelTest('admin');
 
 r($personnel->isClickableTest($object, $actionList[0])) && p() && e('1'); // 测试 unbindWhitelist 方法是否可以点击
+r($personnel->isClickableTest($object, $actionList[1])) && p() && e('1'); // 测试 addWhitelist 方法是否可以点击
