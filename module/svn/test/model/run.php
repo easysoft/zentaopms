31243450651da7a8cca9078612a52e5ea8f39883
caoyanyi <caoyanyi@easycorp.ltd>
2023-12-08 11:31:48 +0800

    + Add ztf check rule for svn run.

--- module/svn/test/model/run.php
+++ module/svn/test/model/run.php
@@ -1,5 +1,19 @@
 #!/usr/bin/env php
 <?php
+
+/**
+
+title=svnModel->run();
+timeout=0
+cid=1
+
+- 更新svn提交信息到禅道，检查第二条记录是否正确
+ - 属性commit @2
+ - 属性comment @+ Add secondary file.
+ - 属性committer @user
+
+*/
+
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/svn.class.php';
 
@@ -9,15 +23,6 @@ zdTable('repofiles')->config('repofiles')->gen(1);
 zdTable('repohistory')->config('repohistory')->gen(1);
 su('admin');
 
-/**
-
-title=svnModel->run();
-timeout=0
-cid=1
-
-*/
-
 $svn = new svnTest();
 
-r($svn->runTest()) && p('commit,comment,committer') && e('2,+ Add secondary file.,user');
-
+r($svn->runTest()) && p('commit,comment,committer') && e('2,+ Add secondary file.,user'); // 更新svn提交信息到禅道，检查第二条记录是否正确
