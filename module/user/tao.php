5f7ac33dffcb263d9a1c37a03b9e435b78b4504a
liugang <liugang@easycorp.ltd>
2023-12-08 22:21:04 +0800

    + userTao: add a method to fetch executions that a user joined.

--- module/user/tao.php
+++ module/user/tao.php
@@ -40,4 +40,35 @@ class userTao extends userModel
             ->page($pager)
             ->fetchAll('id');
     }
+
+    /**
+     * 获取某个用户参与的执行和他在执行中的团队信息。
+     * Get the executions that the user joined.j
+     *
+     * @param  string $account
+     * @param  string $status
+     * @param  string $orderBy
+     * @param  object $pager
+     * @access public
+     * @return array
+     */
+    public function fetchExecutions(string $account, string $status = 'all', string $orderBy = 'id_desc', object $pager = null): array
+    {
+        return $this->dao->select('t1.role, t1.join, t1.days, t1.hours, t2.*')->from(TABLE_TEAM)->alias('t1')
+            ->leftJoin(TABLE_PROJECT)->alias('t2')->on('t1.root = t2.id')
+            ->where('t1.type')->eq('execution')
+            ->andWhere('t1.account')->eq($account)
+            ->andWhere('t2.deleted')->eq('0')
+            ->andWhere('t2.multiple')->eq('1')
+            ->andWhere('t2.type')->in('sprint,stage,kanban')
+            ->andWhere('t2.vision')->eq($this->config->vision)
+            ->beginIF(strpos('doing|wait|suspended|closed', $status) !== false)->andWhere('t2.status')->eq($status)->fi()
+            ->beginIF($status == 'done')->andWhere('t2.status')->in('done,closed')->fi()
+            ->beginIF($status == 'undone')->andWhere('t2.status')->notin('done,closed')->fi()
+            ->beginIF($status == 'openedbyme')->andWhere('t2.openedBy')->eq($account)->fi()
+            ->beginIF(!$this->app->user->admin)->andWhere('t2.id')->in($this->app->user->view->sprints)->fi()
+            ->orderBy("t2.$orderBy")
+            ->page($pager)
+            ->fetchAll('id');
+    }
 }
