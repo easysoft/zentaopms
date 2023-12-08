782b28d5ac21b577c571de678b86f4a5c555ce03
zhouxin <zhouxin@easycorp.ltd>
2023-12-08 15:42:32 +0800

    * Add border style to metric's table in screen designer.

--- module/screen/model.php
+++ module/screen/model.php
@@ -1629,8 +1629,10 @@ class screenModel extends model
         if($metric->scope != 'system') $tableOption->objectPairs = $this->loadModel('metric')->getPairsByScope($metric->scope);
         $tableOption->scope   = $metric->scope;
 
-        $tableOption->oddBGC  = '#173250FF';
-        $tableOption->evenBGC = '#0B1727FF';
+        $tableOption->oddBGC        = '#173250FF';
+        $tableOption->evenBGC       = '#0B1727FF';
+        $tableOption->borderBGC     = '#285A8EFF';
+        $tableOption->borderSpacing = 1;
         return $tableOption;
     }
 
