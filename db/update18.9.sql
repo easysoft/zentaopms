850935bac9f96bd4fd45ac6a32e95daa928a2b93
chaideqing <chaideqing@easycorp.ltd>
2023-12-08 05:41:28 +0000

    + Add column: keywords for zt_feedback.

--- db/update18.9.sql
+++ db/update18.9.sql
@@ -1,5 +1,6 @@
 ALTER TABLE `zt_review` MODIFY `doc` varchar(255) DEFAULT '';
 ALTER TABLE `zt_review` MODIFY `docVersion` varchar(255) DEFAULT '';
+ALTER TABLE `zt_feedback` ADD COLUMN `keywords` varchar(255) NOT NULL DEFAULT '';
 
 CREATE INDEX `metricCode` ON zt_metriclib (metricCode) USING BTREE;
 CREATE INDEX `metricID` ON zt_metriclib (metricID) USING BTREE;
