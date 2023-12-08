09f7ce29dc8e5223658483f3b85c67ca7e945c4d
zhouxin <zhouxin@easycorp.ltd>
2023-12-08 13:35:30 +0800

    * Add text css to metric table in screen designer.

---
+++ www/static/js/config-5e7f3b57.js
@@ -0,0 +1,1 @@
+import{al as o}from"./index.js";import{l as i,m as t}from"./useSync.hook-e2095104.js";import"./plugin-1aefaa40.js";import"./icon-b0ca51a3.js";import"./tables_list-38e71280.js";const s={dataset:"",fit:"contain",borderRadius:10};class c extends i{constructor(){super(...arguments),this.key=t.key,this.chartConfig=o(t),this.option=o(s)}}export{c as default,s as option};
