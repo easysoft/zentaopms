f4c85db12cb8c87248214a8c079367651fb4bf21
liugang <liugang@easycorp.ltd>
2023-12-08 22:17:10 +0800

    + userTao: add tao.php.

---
+++ module/user/tao.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+/**
+ * The model file of user module of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Gang Liu <liugang@easycorp.ltd>
+ * @package     user
+ * @link        https://www.zentao.net
+ */
+class userTao extends userModel
+{
+}
