dbff9e494220350f8f58183e645e1604a396740d
songchenxuan <songchenxuan@easycorp.ltd>
2023-12-11 12:24:19 +0800

    * Add title for preview a screen.

---
+++ www/static/js/storage-ce5c85d6.js
@@ -0,0 +1,1 @@
+import{c1 as n,S as s}from"./index.js";const c=()=>{if(window.chartData)return window.chartData;const r=document.location.hash.split("/"),a=r&&r[r.length-1],t=n(s.GO_CHART_STORAGE_LIST);if(!!t){for(let o=0;o<t.length;o++)if(a.toString()===t[o].id)return t[o]}};export{c as g};
