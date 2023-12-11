90758f05ad46d12b17b7f3d150a2812ff4f19c8e
liugang <liugang@easycorp.ltd>
2023-12-11 10:53:06 +0800

    + myConfig: add configuration items of manage contacts form.

--- module/my/config/form.php
+++ module/my/config/form.php
@@ -6,3 +6,8 @@ $config->my->form->changePassword['password1']        = array('required' => true
 $config->my->form->changePassword['password2']        = array('required' => true,  'type' => 'string', 'default' => '');
 $config->my->form->changePassword['passwordLength']   = array('required' => false, 'type' => 'int',    'default' => 0);
 $config->my->form->changePassword['passwordStrength'] = array('required' => false, 'type' => 'int',    'default' => 0);
+
+$config->my->form->manageContacts = array();
+$config->my->form->manageContacts['listName'] = array('required' => true,  'type' => 'string', 'default' => '');
+$config->my->form->manageContacts['userList'] = array('required' => true,  'type' => 'array',  'default' => array(), 'filter' => 'join');
+$config->my->form->manageContacts['public']   = array('required' => false, 'type' => 'int',    'default' => 0);
