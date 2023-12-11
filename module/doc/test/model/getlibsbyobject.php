eb2e125e6b11812f31f7e76610a10dc8f6dcef8f
tianshujie <tianshujie@easycorp.ltd>
2023-12-11 15:32:41 +0800

    * Change the comment of doc::getLibsByObject unit test.

--- module/doc/test/model/getlibsbyobject.php
+++ module/doc/test/model/getlibsbyobject.php
@@ -1,10 +1,41 @@
 #!/usr/bin/env php
 <?php
+
 /**
 
 title=测试 docModel->getLibsByObject();
-timeout=0
 cid=1
+
+- 获取我的文档库
+ - 第11条的type属性 @mine
+ - 第11条的name属性 @我的文档库11
+- 获取项目文档库
+ - 第1条的type属性 @api
+ - 第1条的name属性 @项目接口库1
+- 获取执行文档库
+ - 第20条的type属性 @execution
+ - 第20条的name属性 @执行文档主库20
+- 获取产品文档库
+ - 第4条的type属性 @api
+ - 第4条的name属性 @产品接口库4
+- 获取自定义文档库
+ - 第6条的type属性 @custom
+ - 第6条的name属性 @自定义文档库6
+- 获取我的文档库和id=1的文档库
+ - 第1条的type属性 @api
+ - 第1条的name属性 @项目接口库1
+- 获取项目文档库和id=1的文档库
+ - 第1条的type属性 @api
+ - 第1条的name属性 @项目接口库1
+- 获取执行文档库和id=1的文档库
+ - 第1条的type属性 @api
+ - 第1条的name属性 @项目接口库1
+- 获取产品文档库和id=1的文档库
+ - 第1条的type属性 @api
+ - 第1条的name属性 @项目接口库1
+- 获取自定义文档库和id=1的文档库
+ - 第1条的type属性 @api
+ - 第1条的name属性 @项目接口库1
 
 */
 
