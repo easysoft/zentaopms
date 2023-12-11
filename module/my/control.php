e31b069bbf618fa6e42dc189c5af9cfa26ab50ae
liugang <liugang@easycorp.ltd>
2023-12-11 14:56:56 +0800

    - myControl: remove unused personlData property.

--- module/my/control.php
+++ module/my/control.php
@@ -1394,11 +1394,10 @@ EOF;
 
         $user = $this->user->getById($this->app->user->account);
 
-        $this->view->title        = $this->lang->my->common . $this->lang->colon . $this->lang->my->profile;
-        $this->view->user         = $user;
-        $this->view->groups       = $this->loadModel('group')->getByAccount($this->app->user->account);
-        $this->view->deptPath     = $this->dept->getParents($user->dept);
-        $this->view->personalData = $this->user->getPersonalData();
+        $this->view->title    = $this->lang->my->common . $this->lang->colon . $this->lang->my->profile;
+        $this->view->user     = $user;
+        $this->view->groups   = $this->loadModel('group')->getByAccount($this->app->user->account);
+        $this->view->deptPath = $this->dept->getParents($user->dept);
         $this->display();
     }
 
