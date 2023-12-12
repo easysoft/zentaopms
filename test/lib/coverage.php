11f6dbbb3b205b4bc9e97ed92c85376e31e7301a
liyang <liyang@easycorp.ltd>
2023-12-12 14:59:03 +0800

    * Filter invalid xdebug records.

--- test/lib/coverage.php
+++ test/lib/coverage.php
@@ -33,7 +33,7 @@ class coverage
 
         $this->traceFile = $tracePath . $this->moduleName . '_' . $this->testType . '_' . basename($this->runFile, '.php') . '.json';
         if(!is_dir($tracePath)) mkdir($tracePath, 0777, true);
-        if(!is_file($this->traceFile) && $this->moduleName !== 'app') file_put_contents($this->traceFile, json_encode(array()));
+        if(!is_file($this->traceFile) && !in_array($this->moduleName, array('app', 'www'))) file_put_contents($this->traceFile, json_encode(array()));
 
         return true;
     }
