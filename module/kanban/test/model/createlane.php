921e3eb919ee5ef2ec8e6a6633c7487923ba14fb
sunguangming <sunguangming@easycorp.ltd>
2023-12-12 09:03:30 +0800

    * kanban: add unit test for createLane.

--- module/kanban/test/model/createlane.php
+++ module/kanban/test/model/createlane.php
@@ -1,41 +1,58 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . '/test/lib/init.php';
-include dirname(__FILE__, 2) . '/kanban.class.php';
-su('admin');
 
 /**
 
 title=测试 kanbanModel->createLane();
+timeout=0
 cid=1
-pid=1
 
-测试创建与其他泳道使用相同看板列的泳道 >> 测试创建与其他泳道使用相同看板列的泳道,1,1
-测试创建使用独立看板列的泳道 >> 测试创建使用独立看板列的泳道,1,641
-测试创建不填写名称的泳道 >> 『泳道名称』不能为空。
-测试创建复用泳道1信息的泳道 >> 默认泳道,1,1
-测试创建复用泳道2信息的泳道 >> 默认泳道,2,2
+- 测试创建与其他泳道使用相同看板列的泳道
+ - 属性name @测试创建与其他泳道使用相同看板列的泳道
+ - 属性region @1
+ - 属性group @1
+- 测试创建使用独立看板列的泳道
+ - 属性name @测试创建使用独立看板列的泳道
+ - 属性region @1
+ - 属性group @2
+- 测试创建不填写名称的泳道第name条的0属性 @『泳道名称』不能为空。
+- 测试创建复用泳道1信息的泳道
+ - 属性name @测试创建与其他泳道使用相同看板列的泳道
+ - 属性region @1
+ - 属性group @1
+- 测试创建复用泳道2信息的泳道
+ - 属性name @测试创建使用独立看板列的泳道
+ - 属性region @1
+ - 属性group @2
 
 */
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+include dirname(__FILE__, 2) . '/kanban.class.php';
+su('admin');
 
-$kanbanID = 1;
-$regionID = 1;
+zdTable('kanban')->gen(1);
+zdTable('kanbanregion')->gen(1);
+zdTable('kanbanlane')->gen(1);
+zdTable('kanbangroup')->gen(1);
 
-$lane1 = new stdclass();
-$lane1->name      = '测试创建与其他泳道使用相同看板列的泳道';
-$lane1->mode      = 'sameAsOther';
-$lane1->otherLane = '1';
-$lane1->color     = '#7ec5ff';
+$sameAsOtherLane = new stdclass();
+$sameAsOtherLane->name      = '测试创建与其他泳道使用相同看板列的泳道';
+$sameAsOtherLane->mode      = 'sameAsOther';
+$sameAsOtherLane->otherLane = '1';
+$sameAsOtherLane->region    = 1;
+$sameAsOtherLane->color     = '#7ec5ff';
 
-$lane2 = new stdclass();
-$lane2->name  = '测试创建使用独立看板列的泳道';
-$lane2->mode  = 'independent';
-$lane2->color = '#229f24';
+$independentLane = new stdclass();
+$independentLane->name   = '测试创建使用独立看板列的泳道';
+$independentLane->mode   = 'independent';
+$independentLane->region = 1;
+$independentLane->color  = '#229f24';
 
-$lane3 = new stdclass();
-$lane3->name  = '';
-$lane3->mode  = 'independent';
-$lane3->color = '#229f24';
+$emptyNameLane = new stdclass();
+$emptyNameLane->name   = '';
+$emptyNameLane->mode   = 'independent';
+$emptyNameLane->region = 1;
+$emptyNameLane->color  = '#229f24';
 
 $lane4 = new stdclass();
 $lane4->id = 1;
@@ -45,8 +62,8 @@ $lane5->id = 2;
 
 $kanban = new kanbanTest();
 
-r($kanban->createLaneTest($lane1, $kanbanID, $regionID)) && p('name,region,group') && e('测试创建与其他泳道使用相同看板列的泳道,1,1'); //测试创建与其他泳道使用相同看板列的泳道
-r($kanban->createLaneTest($lane2, $kanbanID, $regionID)) && p('name,region,group') && e('测试创建使用独立看板列的泳道,1,641');         //测试创建使用独立看板列的泳道
-r($kanban->createLaneTest($lane3, $kanbanID, $regionID)) && p('name:0')            && e('『泳道名称』不能为空。');                     //测试创建不填写名称的泳道
-r($kanban->createLaneTest($lane4, $kanbanID, $regionID)) && p('name,region,group') && e('默认泳道,1,1');                               //测试创建复用泳道1信息的泳道
-r($kanban->createLaneTest($lane5, $kanbanID, $regionID)) && p('name,region,group') && e('默认泳道,2,2');                               //测试创建复用泳道2信息的泳道
+r($kanban->createLaneTest(1, 1, $sameAsOtherLane))         && p('name,region,group') && e('测试创建与其他泳道使用相同看板列的泳道,1,1'); //测试创建与其他泳道使用相同看板列的泳道
+r($kanban->createLaneTest(1, 1, $independentLane))         && p('name,region,group') && e('测试创建使用独立看板列的泳道,1,2');           //测试创建使用独立看板列的泳道
+r($kanban->createLaneTest(1, 1, $emptyNameLane))           && p('name:0')            && e('『泳道名称』不能为空。');                     //测试创建不填写名称的泳道
+r($kanban->createLaneTest(1, 1, $sameAsOtherLane, 'copy')) && p('name,region,group') && e('测试创建与其他泳道使用相同看板列的泳道,1,1'); //测试创建复用泳道1信息的泳道
+r($kanban->createLaneTest(1, 1, $independentLane, 'copy')) && p('name,region,group') && e('测试创建使用独立看板列的泳道,1,2');           //测试创建复用泳道2信息的泳道
