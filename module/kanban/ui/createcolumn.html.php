e83ac2af5cbc532b2b3c9cfa450ae5271cee8e0f
sunguangming <sunguangming@easycorp.ltd>
2023-12-12 17:09:18 +0800

    * kanban: rename var.

--- module/kanban/ui/createcolumn.html.php
+++ module/kanban/ui/createcolumn.html.php
@@ -54,8 +54,8 @@ formPanel
                         set::value('-1')
                     )
                 ),
-                input(set::className('hidden'), set::name('group'), set::value($column->group)),
-                input(set::className('hidden'), set::name('parent'), set::value($column->parent > 0 ? $column->parent : 0))
+                input(set::className('hidden'), set::name('group'), set::value($fromColumn->group)),
+                input(set::className('hidden'), set::name('parent'), set::value($fromColumn->parent > 0 ? $fromColumn->parent : 0))
             )
         )
     )
