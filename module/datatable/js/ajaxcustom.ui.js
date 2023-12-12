cd8ee1f0a664cca3c5af37a18e3f5f0bc5a4170f
tianshujie <tianshujie@easycorp.ltd>
2023-12-12 16:11:45 +0800

    * Fix bug #41226.

--- module/datatable/js/ajaxcustom.ui.js
+++ module/datatable/js/ajaxcustom.ui.js
@@ -3,7 +3,7 @@ function handleEditColsSubmit()
     const formData = [];
     let index = 0;
     const types = ['left', 'no', 'right'];
-    const getSelector = (value) => `[data-zin-gid="${formGID}"] .drag-ul.${value}-cols`;
+    const getSelector = (value) => `[data-zin-gid="${formGID}"] ul.${value}-cols`;
     const colsList = types.map(x => document.querySelector(getSelector(x)));
 
     for(let i = 0; i < colsList.length; i++)
@@ -16,11 +16,13 @@ function handleEditColsSubmit()
         {
             const li = children[j];
             const checkbox = li.querySelector('input[type="checkbox"]');
+            if(!checkbox) continue;
+
             const input = li.querySelector('input[type="text"]');
-            const unit = li.querySelector('select');
+            const unit  = li.querySelector('select');
             formData.push({
                 id: li.dataset.key,
-                order: ++index,
+                order: ++ index,
                 show: checkbox.checked,
                 width: unit.value === '%' ? String(input.value / 100) : input.value,
                 fixed: types[i],
