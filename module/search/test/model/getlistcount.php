b0bd12b69118e64f7db05c0c594fedef59528da1
daitingting <daitingting@xirangit.com>
2023-12-13 08:14:28 +0000

    * Remove unset values.

--- module/search/test/model/getlistcount.php
+++ module/search/test/model/getlistcount.php
@@ -3,7 +3,7 @@
 
 /**
 
-title=测试 searchModel->getList();
+title=测试 searchModel->getListCount();
 timeout=0
 cid=1
 
@@ -14,7 +14,7 @@ cid=1
 - 测试索引中指定类型的条数
  - 属性project @4
  - 属性story @3
-- 测试索引中指定类型的条数属性bug @~~
+- 测试索引中指定类型为空的条数属性bug @
 
 */
 
@@ -29,4 +29,3 @@ $typeList = array('all', array('project', 'story'), array('bug'));
 $search = new searchTest();
 r($search->getListCountTest($typeList[0])) && p('project,story,task') && e('4,3,3'); //测试索引中所有类型的条数
 r($search->getListCountTest($typeList[1])) && p('project,story') && e('4,3');        //测试索引中指定类型的条数
-r($search->getListCountTest($typeList[1])) && p('bug') && e('~~');                   //测试索引中指定类型为空的条数
