84dc635c1913133a17a86d070bc93447b655fd88
daitingting <daitingting@xirangit.com>
2023-12-13 08:16:15 +0000

    * Adjust field case.

--- module/search/test/model/saveindex.php
+++ module/search/test/model/saveindex.php
@@ -11,15 +11,15 @@ timeout=0
 cid=1
 
 - 创建ID为10的bug的索引
- - 属性objecttype @bug
- - 属性objectid @10
+ - 属性objectType @bug
+ - 属性objectID @10
 - 创建ID为10的任务的索引
- - 属性objecttype @task
- - 属性objectid @10
+ - 属性objectType @task
+ - 属性objectID @10
 
 */
 
 $search = new searchTest();
 
-r($search->saveIndexTest('bug', 10))  && p('objecttype,objectid') && e('bug,10');  //创建ID为10的bug的索引
-r($search->saveIndexTest('task', 10)) && p('objecttype,objectid') && e('task,10'); //创建ID为10的任务的索引
+r($search->saveIndexTest('bug', 10))  && p('objectType,objectID') && e('bug,10');  //创建ID为10的bug的索引
+r($search->saveIndexTest('task', 10)) && p('objectType,objectID') && e('task,10'); //创建ID为10的任务的索引
