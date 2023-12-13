a083dec81c24c2e3676b5e4edd95ed388461ec57
wangyidong <wangyidong@cnezsoft.com>
2023-12-13 11:23:36 +0800

    * Fix for extension unit test.

--- module/extension/test/model/getdbfile.php
+++ module/extension/test/model/getdbfile.php
@@ -9,21 +9,22 @@ title=测试 extensionModel->getDBFile();
 timeout=0
 cid=1
 
-- 获取代号为code1的插件包安装SQL文件。 @/var/web/ztpms/extension/pkg/code1/db/install.sql
-- 获取代号为code1的插件包升级SQL文件。 @/var/web/ztpms/extension/pkg/code1/db/upgrade.sql
-- 获取代号为code1的插件包其他SQL文件。 @/var/web/ztpms/extension/pkg/code1/db/delete.sql
-- 获取代号为zentaopatch的安装SQL文件。 @/var/web/ztpms/extension/pkg/zentaopatch/db/install.sql
-- 获取代号为zentaopatch的升级SQL文件。 @/var/web/ztpms/extension/pkg/zentaopatch/db/upgrade.sql
-- 获取代号为zentaopatch的其他SQL文件。 @/var/web/ztpms/extension/pkg/zentaopatch/db/delete.sql
+- 获取代号为code1的插件包安装SQL文件。 @/apps/zentao/extension/pkg/code1/db/install.sql
+- 获取代号为code1的插件包升级SQL文件。 @/apps/zentao/extension/pkg/code1/db/upgrade.sql
+- 获取代号为code1的插件包其他SQL文件。 @/apps/zentao/extension/pkg/code1/db/delete.sql
+- 获取代号为zentaopatch的安装SQL文件。 @/apps/zentao/extension/pkg/zentaopatch/db/install.sql
+- 获取代号为zentaopatch的升级SQL文件。 @/apps/zentao/extension/pkg/zentaopatch/db/upgrade.sql
+- 获取代号为zentaopatch的其他SQL文件。 @/apps/zentao/extension/pkg/zentaopatch/db/delete.sql
 
 */
 
 global $tester;
 $tester->loadModel('extension');
+$tester->extension->pkgRoot = '/apps/zentao/extension/pkg/';
 
-r($tester->extension->getDBFile('code1', 'install'))        && p() && e('/var/web/ztpms/extension/pkg/code1/db/install.sql');       // 获取代号为code1的插件包安装SQL文件。
-r($tester->extension->getDBFile('code1', 'upgrade'))        && p() && e('/var/web/ztpms/extension/pkg/code1/db/upgrade.sql');       // 获取代号为code1的插件包升级SQL文件。
-r($tester->extension->getDBFile('code1', 'delete'))         && p() && e('/var/web/ztpms/extension/pkg/code1/db/delete.sql');        // 获取代号为code1的插件包其他SQL文件。
-r($tester->extension->getDBFile('zentaopatch', 'install'))  && p() && e('/var/web/ztpms/extension/pkg/zentaopatch/db/install.sql'); // 获取代号为zentaopatch的安装SQL文件。
-r($tester->extension->getDBFile('zentaopatch', 'upgrade'))  && p() && e('/var/web/ztpms/extension/pkg/zentaopatch/db/upgrade.sql'); // 获取代号为zentaopatch的升级SQL文件。
-r($tester->extension->getDBFile('zentaopatch', 'delete'))   && p() && e('/var/web/ztpms/extension/pkg/zentaopatch/db/delete.sql');  // 获取代号为zentaopatch的其他SQL文件。
+r($tester->extension->getDBFile('code1', 'install'))        && p() && e('/apps/zentao/extension/pkg/code1/db/install.sql');       // 获取代号为code1的插件包安装SQL文件。
+r($tester->extension->getDBFile('code1', 'upgrade'))        && p() && e('/apps/zentao/extension/pkg/code1/db/upgrade.sql');       // 获取代号为code1的插件包升级SQL文件。
+r($tester->extension->getDBFile('code1', 'delete'))         && p() && e('/apps/zentao/extension/pkg/code1/db/delete.sql');        // 获取代号为code1的插件包其他SQL文件。
+r($tester->extension->getDBFile('zentaopatch', 'install'))  && p() && e('/apps/zentao/extension/pkg/zentaopatch/db/install.sql'); // 获取代号为zentaopatch的安装SQL文件。
+r($tester->extension->getDBFile('zentaopatch', 'upgrade'))  && p() && e('/apps/zentao/extension/pkg/zentaopatch/db/upgrade.sql'); // 获取代号为zentaopatch的升级SQL文件。
+r($tester->extension->getDBFile('zentaopatch', 'delete'))   && p() && e('/apps/zentao/extension/pkg/zentaopatch/db/delete.sql');  // 获取代号为zentaopatch的其他SQL文件。
--- module/extension/test/model/getpackagefile.php
+++ module/extension/test/model/getpackagefile.php
@@ -9,13 +9,15 @@ title=测试 extensionModel->getPackageFile();
 timeout=0
 cid=1
 
-- 获取代号为code1的插件包地址。 @/var/web/ztpms/tmp/extension/code1.zip
-- 获取代号为zentaopatch的插件包地址。 @/var/web/ztpms/tmp/extension/zentaopatch.zip
+- 获取代号为code1的插件包地址。 @/apps/zentao/tmp/extension/code1.zip
+- 获取代号为zentaopatch的插件包地址。 @/apps/zentao/tmp/extension/zentaopatch.zip
+
 
 */
 
 global $tester;
 $tester->loadModel('extension');
+$tester->extension->app->tmpRoot = '/apps/zentao/tmp/';
 
-r($tester->extension->getPackageFile('code1'))        && p() && e('/var/web/ztpms/tmp/extension/code1.zip');         // 获取代号为code1的插件包地址。
-r($tester->extension->getPackageFile('zentaopatch'))  && p() && e('/var/web/ztpms/tmp/extension/zentaopatch.zip');   // 获取代号为zentaopatch的插件包地址。
+r($tester->extension->getPackageFile('code1'))        && p() && e('/apps/zentao/tmp/extension/code1.zip');         // 获取代号为code1的插件包地址。
+r($tester->extension->getPackageFile('zentaopatch'))  && p() && e('/apps/zentao/tmp/extension/zentaopatch.zip');   // 获取代号为zentaopatch的插件包地址。
