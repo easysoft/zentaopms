3efd9d350722386297e4858ff1944f89a15fb798
caoyanyi <caoyanyi@easycorp.ltd>
2023-12-13 10:58:34 +0800

    * Adjust no language error.

--- module/cron/control.php
+++ module/cron/control.php
@@ -395,6 +395,7 @@ class cron extends control
                 parse_str($task->command, $params);
                 if(isset($params['moduleName']) and isset($params['methodName']))
                 {
+                    $this->app->loadLang($params['moduleName']);
                     $this->app->loadConfig($params['moduleName']);
                     $output = $this->fetch($params['moduleName'], $params['methodName']);
                 }
