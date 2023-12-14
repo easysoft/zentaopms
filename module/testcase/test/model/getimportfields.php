485baeb8360a76f8cb3d9ce56043646dd3654aa8
wangyuting <wangyuting@easycorp.ltd>
2023-12-14 15:54:47 +0800

    * Fix getimportfields unit test script bug.

--- module/testcase/test/model/getimportfields.php
+++ module/testcase/test/model/getimportfields.php
@@ -1,17 +1,69 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . '/test/lib/init.php';
-include dirname(__FILE__, 2) . '/testcase.class.php';
-
-zdTable('product')->gen('90');
-
 /**
 
 title=测试 testcaseModel->getImportFields();
 cid=1
 pid=1
 
+- 测试获取product 1 可以导出的字段的名称
+ - 属性id @用例编号
+ - 属性product @所属产品
+ - 属性branch @平台/分支
+ - 属性module @所属模块
+ - 属性bugsAB @B
+ - 属性resultsAB @R
+ - 属性stepNumberAB @S
+- 测试获取product 2 可以导出的字段的名称
+ - 属性id @用例编号
+ - 属性product @所属产品
+ - 属性branch @平台/分支
+ - 属性module @所属模块
+ - 属性bugsAB @B
+ - 属性resultsAB @R
+ - 属性stepNumberAB @S
+- 测试获取product 3 可以导出的字段的名称
+ - 属性id @用例编号
+ - 属性product @所属产品
+ - 属性branch @平台/分支
+ - 属性module @所属模块
+ - 属性bugsAB @B
+ - 属性resultsAB @R
+ - 属性stepNumberAB @S
+- 测试获取product 41 可以导出的字段的名称
+ - 属性id @用例编号
+ - 属性product @所属产品
+ - 属性branch @分支
+ - 属性module @所属模块
+ - 属性bugsAB @B
+ - 属性resultsAB @R
+ - 属性stepNumberAB @S
+- 测试获取product 81 可以导出的字段的名称
+ - 属性id @用例编号
+ - 属性product @所属产品
+ - 属性branch @平台
+ - 属性module @所属模块
+ - 属性bugsAB @B
+ - 属性resultsAB @R
+ - 属性stepNumberAB @S
+- 测试获取product 0 可以导出的字段的名称
+ - 属性id @用例编号
+ - 属性product @所属产品
+ - 属性branch @平台
+ - 属性module @所属模块
+ - 属性bugsAB @B
+ - 属性resultsAB @R
+ - 属性stepNumberAB @S
+
 */
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+include dirname(__FILE__, 2) . '/testcase.class.php';
+
+zdTable('product')->gen('90');
+
+global $tester, $config;
+$tester->loadModel('testcase');
+$config->testcase->exportFields .= ',bugsAB,resultsAB,stepNumberAB';
 
 $productIDList = array(1, 2, 3, 41, 81);
 
