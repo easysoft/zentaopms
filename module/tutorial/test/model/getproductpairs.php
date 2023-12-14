64253dbc5a99cad53f347a5f8616f4e422082722
liumengyi <liumengyi@easycorp.ltd>
2023-12-14 16:15:17 +0800

    * Optimize code for tutorialTest::getProductPairs.

--- module/tutorial/test/model/getproductpairs.php
+++ module/tutorial/test/model/getproductpairs.php
@@ -1,22 +1,21 @@
 #!/usr/bin/env php
 <?php
 declare(strict_types=1);
+/**
+
+title=测试 tutorialModel->getProductPairs();
+cid=1
+
+- 测试是否能拿到数据属性1 @Test product
+
+*/
+
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/tutorial.class.php';
 
 zdTable('user')->gen(5);
 
 su('admin');
-
-/**
-
-title=测试 tutorialModel->getProductPairs();
-cid=1
-pid=1
-
-测试是否能拿到数据 >> Test product
-
-*/
 
 $tutorial = new tutorialTest();
 
