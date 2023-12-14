d49308b22ee4a19bb4447518426caeb5310d2c85
liumengyi <liumengyi@easycorp.ltd>
2023-12-14 16:17:01 +0800

    * Optimize code for tutorialTest::getProductStats.

--- module/tutorial/test/model/getproductstats.php
+++ module/tutorial/test/model/getproductstats.php
@@ -1,18 +1,32 @@
 #!/usr/bin/env php
 <?php
 declare(strict_types=1);
+/**
+
+title=测试 tutorialModel->getProductStats();
+cid=1
+
+- 测试是否能拿到 admin 的数据 id program PO createdBy reviewer
+ - 第1条的id属性 @1
+ - 第1条的program属性 @0
+ - 第1条的PO属性 @admin
+ - 第1条的createdBy属性 @admin
+ - 第1条的reviewer属性 @admin
+- 测试是否能拿到 admin 的数据 plans第1[plans]条的1属性 @Test plan
+- 测试是否能拿到 user1 的数据 id program PO createdBy reviewer
+ - 第1条的id属性 @1
+ - 第1条的program属性 @0
+ - 第1条的PO属性 @user1
+ - 第1条的createdBy属性 @user1
+ - 第1条的reviewer属性 @user1
+- 测试是否能拿到 user1 的数据 plans第1[plans]条的1属性 @Test plan
+
+*/
+
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/tutorial.class.php';
 
 zdTable('user')->gen(5);
-
-/**
-
-title=测试 tutorialModel->getProductStats();
-timeout=0
-cid=1
-
-*/
 
 $tutorial = new tutorialTest();
 
