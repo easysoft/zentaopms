fc9a0264439b07de5b3c59d3261583d0005d0872
liumengyi <liumengyi@easycorp.ltd>
2023-12-14 16:10:28 +0800

    * Optimize code for tutorialTest::chekcNovice.

--- module/tutorial/test/model/checknovice.php
+++ module/tutorial/test/model/checknovice.php
@@ -1,24 +1,26 @@
 #!/usr/bin/env php
 <?php
 declare(strict_types=1);
+/**
+
+title=测试 tutorialModel->checkNovice();
+cid=1
+
+- 检查当account是 guest 的时候的返回值 modifyPassword 0。 @0
+- 检查当account是 guest 的时候的返回值 modifyPassword 1。 @0
+- 检查当account是 admin 的时候的返回值 modifyPassword 0 动态数量大于 10。 @0
+- 检查当account是 admin 的时候的返回值 modifyPassword 1 动态数量大于 10。 @0
+- 检查当account是 user1 的时候的返回值 modifyPassword 0 动态数量小于等于 10。 @1
+- 检查当account是 user1 的时候的返回值 modifyPassword 1 动态数量小于等于 10。 @0
+
+*/
+
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/tutorial.class.php';
 
 zdTable('action')->gen(40);
 zdTable('user')->gen(5);
 
-
-/**
-
-title=测试 tutorialModel->checkNovice();
-cid=1
-pid=1
-
-检查当account是admin的时候的返回值。 >> 0
-检查当->app->user->modifyPassword是不为空的时候的返回值。 >> 0
-检查当account是guest的时候的返回值。 >> 0
-
-*/
 
 $tutorial = new tutorialTest();
 
