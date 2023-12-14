c69a8c7e278bd3d17c17cc4e73834e3d341cb372
liumengyi <liumengyi@easycorp.ltd>
2023-12-14 16:31:57 +0800

    * Optimize code for tutorialTest::getProject.

--- module/tutorial/test/model/getproject.php
+++ module/tutorial/test/model/getproject.php
@@ -1,18 +1,33 @@
 #!/usr/bin/env php
 <?php
 declare(strict_types=1);
-include dirname(__FILE__, 5) . '/test/lib/init.php';
-include dirname(__FILE__, 2) . '/tutorial.class.php';
-
-zdTable('user')->gen(5);
 
 /**
 
 title=测试 tutorialModel->getProject();
 cid=1
-pid=1
+
+- 测试是否能拿到 admin 的数据
+ - 属性id @2
+ - 属性type @project
+ - 属性PM @admin
+ - 属性PO @admin
+ - 属性QD @admin
+ - 属性RD @admin
+- 测试是否能拿到 admin 的数据
+ - 属性id @2
+ - 属性type @project
+ - 属性PM @user1
+ - 属性PO @user1
+ - 属性QD @user1
+ - 属性RD @user1
 
 */
+
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+include dirname(__FILE__, 2) . '/tutorial.class.php';
+
+zdTable('user')->gen(5);
 
 $tutorial = new tutorialTest();
 
