219ff9bcb60506183b5a2522276613616f96110d
liumengyi <liumengyi@easycorp.ltd>
2023-12-14 16:14:13 +0800

    * Optimize code for tutorialTest::getProduct.

--- module/tutorial/test/model/getproduct.php
+++ module/tutorial/test/model/getproduct.php
@@ -1,18 +1,32 @@
 #!/usr/bin/env php
 <?php
 declare(strict_types=1);
+/**
+
+title=测试 tutorialModel->getProduct();
+cid=1
+
+- 测试是否能拿到 admin 的数据 id program PO createdBy reviewer
+ - 属性id @1
+ - 属性program @0
+ - 属性PO @admin
+ - 属性createdBy @admin
+ - 属性reviewer @admin
+- 测试是否能拿到 admin 的数据 plans第plans条的1属性 @Test plan
+- 测试是否能拿到 user1 的数据 id program PO createdBy reviewer
+ - 属性id @1
+ - 属性program @0
+ - 属性PO @user1
+ - 属性createdBy @user1
+ - 属性reviewer @user1
+- 测试是否能拿到 user1 的数据 plans第plans条的1属性 @Test plan
+
+*/
+
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/tutorial.class.php';
 
 zdTable('user')->gen(5);
-
-/**
-
-title=测试 tutorialModel->getProduct();
-timeout=0
-cid=1
-
-*/
 
 $tutorial = new tutorialTest();
 
