097426addeca464786dfc52b6d7c8892aaef05e3
liugang <liugang@easycorp.ltd>
2023-12-15 10:32:05 +0800

    - userTest: remove unused code.

--- module/user/test/model/getpairs.php
+++ module/user/test/model/getpairs.php
@@ -2,7 +2,6 @@
 <?php
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/user.class.php';
-su('admin');
 
 $user = zdTable('user');
 $user->id->range('1001-1006');
