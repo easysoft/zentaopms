9abf2bf2300a05b7a024835b1b50fea3d2c1e48f
tanghucheng <tanghucheng@easycorp.ltd>
2023-12-15 02:52:13 -0500

    + Add transfer: tao.php.

---
+++ module/transfer/tao.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+/**
+ * The zen file of transfer module of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Tang Hucheng<tanghucheng@easycorp.ltd>
+ * @package     transfer
+ * @link        https://www.zentao.net
+ */
+
+class transferTao extends transferModel
+{
+}
