191f2f6b79a9a9d2f757f23531e78eacdfbcb407
liumengyi <liumengyi@easycorp.ltd>
2023-12-16 15:31:45 +0800

    * Fix bug #41780, add `members` to `zt_testtask` when installing.

--- db/zentao.sql
+++ db/zentao.sql
@@ -2104,6 +2104,7 @@ CREATE TABLE IF NOT EXISTS `zt_testtask` (
   `createdBy` varchar(30) NOT NULL DEFAULT '',
   `createdDate` datetime NULL,
   `deleted` enum('0','1') NOT NULL default '0',
+  `members` text DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE INDEX `product` ON `zt_testtask` (`product`);
