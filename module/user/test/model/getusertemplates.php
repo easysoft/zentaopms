f957551bb82040cb34d135f9886788ad78daa656
liugang <liugang@easycorp.ltd>
2023-12-16 15:40:36 +0800

    * userTest: refactor script to test the getUserTemplates method.

--- module/user/test/model/getusertemplates.php
+++ module/user/test/model/getusertemplates.php
@@ -1,27 +1,60 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . '/test/lib/init.php';
-include dirname(__FILE__, 2) . '/user.class.php';
-zdTable('usertpl')->gen(10);
-zdTable('user')->gen(1000);
-su('admin');
-
 /**
-
 title=测试 userModel::getUserTemplates();
 cid=1
 pid=1
-
-查询当前用户能够使用的exporttask类型的模板 >> admin
-查询当前用户能够使用的story类型的模板 >> dev10
-查询当前用户能够使用的exportbug类型的模板 >> 0
-查询当前用户能够使用的exportstory类型的模板 >> test10
-
 */
-$typeList = array('exporttask', 'story', 'exportbug', 'exportstory');
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+include dirname(__FILE__, 2) . '/user.class.php';
 
-$user = new userTest();
-r($user->getUserTemplatesTest($typeList[0])) && p('0:account') && e('admin');  //查询当前用户能够使用的exporttask类型的模板
-r($user->getUserTemplatesTest($typeList[1])) && p('0:account') && e('dev10');  //查询当前用户能够使用的story类型的模板
-r($user->getUserTemplatesTest($typeList[2])) && p('0:account') && e('0');      //查询当前用户能够使用的exportbug类型的模板
-r($user->getUserTemplatesTest($typeList[3])) && p('0:account') && e('test10'); //查询当前用户能够使用的exportstory类型的模板
+zdTable('user')->gen(2);
+
+$table = zdTable('usertpl');
+$table->account->range('admin{5},user1{5}');
+$table->gen(10);
+
+su('admin'); // 当前用户切换为 admin。
+
+$userTest = new userTest();
+
+$templates = $userTest->getUserTemplatesTest('exporttask');
+r(count($templates)) && p()               && e(2);         // admin 用户可以查看导出任务的模板有 2 个。
+r($templates)        && p('0:id,account') && e('1,admin'); // 第一个模板的 id 是 1，创建者是 admin。
+r($templates)        && p('1:id,account') && e('5,admin'); // 第二个模板的 id 是 5，创建者是 admin。
+
+$templates = $userTest->getUserTemplatesTest('story');
+r(count($templates)) && p()               && e(3);          // admin 用户可以查看需求的模板有 3 个。
+r($templates)        && p('0:id,account') && e('2,admin');  // 第一个模板的 id 是 2，创建者是 admin。
+r($templates)        && p('1:id,account') && e('6,user1');  // 第二个模板的 id 是 6，创建者是 user1。
+r($templates)        && p('2:id,account') && e('10,user1'); // 第三个模板的 id 是 10，创建者是 user1。
+
+$templates = $userTest->getUserTemplatesTest('exportbug');
+r(count($templates)) && p()               && e(1);         // admin 用户可以查看导出 bug 的模板有 1 个。
+r($templates)        && p('0:id,account') && e('3,admin'); // 第一个模板的 id 是 3，创建者是 admin。
+
+$templates = $userTest->getUserTemplatesTest('exportstory');
+r(count($templates)) && p()               && e(2);         // admin 用户可以查看导出需求的模板有 2 个。
+r($templates)        && p('0:id,account') && e('4,admin'); // 第一个模板的 id 是 4，创建者是 admin。
+r($templates)        && p('1:id,account') && e('8,user1'); // 第二个模板的 id 是 8，创建者是 user1。
+
+su('user1'); // 当前用户切换为 user1。
+
+$templates = $userTest->getUserTemplatesTest('exporttask');
+r(count($templates)) && p()               && e(1);         // user1 用户可以查看导出任务的模板有 1 个。
+r($templates)        && p('0:id,account') && e('9,user1'); // 第一个模板的 id 是 9，创建者是 user1。
+
+$templates = $userTest->getUserTemplatesTest('story');
+r(count($templates)) && p()               && e(3);          // user1 用户可以查看需求的模板有 3 个。
+r($templates)        && p('0:id,account') && e('2,admin');  // 第一个模板的 id 是 2，创建者是 admin。
+r($templates)        && p('1:id,account') && e('6,user1');  // 第二个模板的 id 是 6，创建者是 user1。
+r($templates)        && p('2:id,account') && e('10,user1'); // 第三个模板的 id 是 10，创建者是 user1。
+
+$templates = $userTest->getUserTemplatesTest('exportbug');
+r(count($templates)) && p()               && e(1);         // user1 用户可以查看导出 bug 的模板有 1 个。
+r($templates)        && p('0:id,account') && e('7,user1'); // 第一个模板的 id 是 7，创建者是 user1。
+
+$templates = $userTest->getUserTemplatesTest('exportstory');
+r(count($templates)) && p()               && e(2);         // user1 用户可以查看导出需求的模板有 2 个。
+r($templates)        && p('0:id,account') && e('4,admin'); // 第一个模板的 id 是 4，创建者是 admin。
+r($templates)        && p('1:id,account') && e('8,user1'); // 第二个模板的 id 是 8，创建者是 user1。
