ed50e6a18f97fcf2ded10c8f4f8bf67e18845275
caoyanyi <caoyanyi@easycorp.ltd>
2023-12-18 19:04:22 +0800

    * Adjust user set for install.

--- module/instance/config/form.php
+++ module/instance/config/form.php
@@ -6,7 +6,7 @@ $config->instance->form->create['name']        = array('type' => 'string', 'requ
 $config->instance->form->create['url']         = array('type' => 'string', 'required' => true,  'default' => '', 'filter' => 'trim');
 $config->instance->form->create['account']     = array('type' => 'string', 'required' => true,  'default' => '', 'filter' => 'trim');
 $config->instance->form->create['password']    = array('type' => 'string', 'required' => true,  'default' => '', 'filter' => 'trim');
-$config->instance->form->create['createdBy']   = array('type' => 'string', 'required' => false, 'default' => $app->user->account);
+$config->instance->form->create['createdBy']   = array('type' => 'string', 'required' => false, 'default' => isset($app->user->account) ? $app->user->account : '');
 $config->instance->form->create['createdDate'] = array('type' => 'string', 'required' => false, 'default' => helper::now());
 $config->instance->form->create['private']     = array('type' => 'string', 'required' => false, 'default' => uniqid());
 
@@ -15,5 +15,5 @@ $config->instance->form->edit['name']       = array('type' => 'string', 'require
 $config->instance->form->edit['url']        = array('type' => 'string', 'required' => true,  'default' => '', 'filter' => 'trim');
 $config->instance->form->edit['account']    = array('type' => 'string', 'required' => true,  'default' => '', 'filter' => 'trim');
 $config->instance->form->edit['password']   = array('type' => 'string', 'required' => true,  'default' => '', 'filter' => 'trim');
-$config->instance->form->edit['editedBy']   = array('type' => 'string', 'required' => false, 'default' => $app->user->account);
+$config->instance->form->edit['editedBy']   = array('type' => 'string', 'required' => false, 'default' => isset($app->user->account) ? $app->user->account : '');
 $config->instance->form->edit['editedDate'] = array('type' => 'string', 'required' => false, 'default' => helper::now());
