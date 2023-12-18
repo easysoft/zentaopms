de7a6054e40ca6ddc6bfc50074d359f8807ee8a8
qixinzhi <qixinzhi@easycorp.ltd>
2023-12-18 10:15:30 +0800

    Revert "* Move ajaxGetTreeData to pms."
    This reverts commit d52b6bf4f4e671a24cdad13089e04d601a6758d3.

--- module/screen/control.php
+++ module/screen/control.php
@@ -180,28 +180,4 @@ class screen extends control
             print(json_encode($chartData));
         }
     }
-
-    /**
-     * Ajax get tree data.
-     *
-     * @access public
-     * @return void
-     */
-    public function ajaxGetTreeData($screenID)
-    {
-        $dimensions  = $this->screen->getDimensionPairs();
-        $screen      = $this->screen->getByID($screenID);
-        $dimensionID = $this->loadModel('dimension')->setSwitcherMenu($screen->dimension);
-
-        foreach($dimensions as $dimension) $dimension->value = (int)$dimension->value;
-
-        $data = new stdclass();
-        $data->chartData   = $screen->chartData;
-        $data->treeData    = $this->screen->getTreeData($dimensions);
-        $data->dimensions  = $dimensions;
-        $data->dimension   = $screen->dimension;
-        $data->fieldConfig = $this->screen->getTreeSelectOptions();
-
-        echo(json_encode($data));
-    }
 }
