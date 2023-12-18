c43b04fa628a3d4811b9040570aa2def49ba02b0
qixinzhi <qixinzhi@easycorp.ltd>
2023-12-18 10:13:16 +0800

    Revert "* Fix chart type."
    This reverts commit e0e15473e35a259db80e8960de235a2e01ab5b49.

--- module/screen/model.php
+++ module/screen/model.php
@@ -205,7 +205,7 @@ class screenModel extends model
         $chartID = zget($component->chartConfig, 'sourceID', '');
         if(!$chartID) return $component;
 
-        $type  = $component->chartConfig->package;
+        $type  = zget($component->chartConfig, 'key', '');
         $type  = $this->getChartType($type);
         $table = $this->config->objectTables[$type];
         $chart = $this->dao->select('*')->from($table)->where('id')->eq($chartID)->fetch();
