12d1910ad4a9464cd366ac8a61dd55f469ccbdab
sunguangming <sunguangming@easycorp.ltd>
2023-12-18 17:19:47 +0800

    * kanban: refactor getColumnGroupByRegions().

--- module/kanban/test/model/getcolumngroupbyregions.php
+++ module/kanban/test/model/getcolumngroupbyregions.php
@@ -4,25 +4,27 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/kanban.class.php';
 su('admin');
 
+zdTable('kanbancolumn')->gen(100);
+
 /**
 
 title=测试 kanbanModel->getColumnGroupByRegions();
+timeout=0
 cid=1
-pid=1
 
-测试获取region 1 2 3 的看板列数量 >> 12
-测试获取region 4,5,6的看板列数量 >> 12
-测试获取region 7,8,9的看板列数量 >> 12
-测试获取region 10,11,12的看板列数量 >> 12
-测试获取region 13,14,15的看板列数量 >> 12
+- 测试获取region 1 2 3 的看板列数量 @12
+- 测试获取region 4,5,6的看板列数量 @12
+- 测试获取region 7,8,9的看板列数量 @12
+- 测试获取region 10,11,12的看板列数量 @12
+- 测试获取region 13,14,15的看板列数量 @12
 
 */
 
-$regions1 = '1,2,3';
-$regions2 = '4,5,6';
-$regions3 = '7,8,9';
-$regions4 = '10,11,12';
-$regions5 = '13,14,15';
+$regions1 = array(1,2,3);
+$regions2 = array(4,5,6);
+$regions3 = array(7,8,9);
+$regions4 = array(10,11,12);
+$regions5 = array(13,14,15);
 
 $kanban = new kanbanTest();
 
