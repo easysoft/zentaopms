572bb3f6c8b82f8c3a53fb8c543f97e5a362d208
wangyuting <wangyuting@easycorp.ltd>
2023-12-20 13:38:32 +0800

    * Fix the bug where the run the unit test cannot continue with other unit tests.

--- module/install/test/model/createtable.php
+++ module/install/test/model/createtable.php
@@ -22,3 +22,5 @@ $tester->loadModel('install');
 $_POST['clearDB'] = true;
 r($tester->install->createTable('18.0'))     && p() && e('1'); // 测试是否能正常生成18.0版本的数据库表。
 r($tester->install->fetchByID(1, 'product')) && p() && e('0'); // 测试生成的数据库product表是否含有数据。
+
+zdTable('company')->gen(1); //为了防止后面执行的单测无法运行。
