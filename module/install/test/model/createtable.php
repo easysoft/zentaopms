e08d481c4cce5c472e7974e94e424a6e3c63c28a
wangyuting <wangyuting@easycorp.ltd>
2023-12-20 13:29:36 +0800

    * Finish install::createTable() unit test script.

---
+++ module/install/test/model/createtable.php
@@ -0,0 +1,24 @@
+#!/usr/bin/env php
+<?php
+/**
+
+title=测试 installModel->createTable();
+timeout=0
+cid=1
+
+- 测试是否能正常生成18.0版本的数据库表。 @1
+- 测试生成的数据库product表是否含有数据。 @0
+
+*/
+
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+su('admin');
+
+zdTable('product')->gen(10);
+
+global $tester, $config;
+$tester->loadModel('install');
+
+$_POST['clearDB'] = true;
+r($tester->install->createTable('18.0'))     && p() && e('1'); // 测试是否能正常生成18.0版本的数据库表。
+r($tester->install->fetchByID(1, 'product')) && p() && e('0'); // 测试生成的数据库product表是否含有数据。
