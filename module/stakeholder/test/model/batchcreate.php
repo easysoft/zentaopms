c535eda08a025cb4c6b5e9472080577e784c826d
tianshujie <tianshujie@easycorp.ltd>
2023-12-20 15:48:59 +0800

    * Refactor stakeholder::batchCreate unit test.

--- module/stakeholder/test/model/batchcreate.php
+++ module/stakeholder/test/model/batchcreate.php
@@ -1,31 +1,46 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . '/test/lib/init.php';
-include dirname(__FILE__, 2) . '/stakeholder.class.php';
-su('admin');
-
 /**
 
 title=测试 stakeholderModel->batchCreate();
 cid=1
-pid=1
 
- >> 6
- >> 6
- >> 6
- >> 6
+- 测试项目ID为0、人员为空时，批量创建项目干系人 @0
+- 测试项目ID为0、人员为admin、user1时，批量创建项目干系人 @1
+- 测试项目ID为0、人员为user10、user11时，批量创建项目干系人 @3
+- 测试项目ID为11、人员为空时，批量创建项目干系人 @0
+- 测试项目ID为11、人员为admin、user1时，批量创建项目干系人 @5
+- 测试项目ID为11、人员为user10、user11时，批量创建项目干系人 @7
+- 测试项目ID不存在、人员为空时，批量创建项目干系人 @0
+- 测试项目ID不存在、人员为admin、user1时，批量创建项目干系人 @9
+- 测试项目ID不存在、人员为user10、user11时，批量创建项目干系人 @11
 
 */
-$projectIDList = array('11', '31', '61');
-$realnames     = array('开发38', '开发29', '开发30');
-$accounts      = array('test28', 'test29', 'test30', 'dev1', 'dev13', 'dev12');
 
-$normalStakeholder = array('realnames' => $realnames, 'accounts' => $accounts);
-$noRealnames       = array('accounts' => $accounts);
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+include dirname(__FILE__, 2) . '/stakeholder.class.php';
 
-$stakeholder = new stakeholderTest();
-r(count($stakeholder->batchCreateTest($projectIDList[0], $normalStakeholder))) && p() && e('6');
-r(count($stakeholder->batchCreateTest($projectIDList[1], $normalStakeholder))) && p() && e('6');
-r(count($stakeholder->batchCreateTest($projectIDList[2], $normalStakeholder))) && p() && e('6');
-r(count($stakeholder->batchCreateTest($projectIDList[0], $noRealnames)))       && p() && e('6');
+zdTable('user')->gen(5);
+zdTable('stakeholder')->gen(0);
+zdTable('product')->gen(0);
+zdTable('projectproduct')->gen(0);
+zdTable('group')->gen(0);
+zdTable('acl')->gen(0);
+zdTable('project')->config('project')->gen(15);
+zdTable('team')->config('team')->gen(10);
 
+$projectID   = array(0, 11, 100);
+$accounts[0] = array();
+$accounts[1] = array('admin', 'user1');
+$accounts[2] = array('user10', 'user11');
+
+$stakeholderTester = new stakeholderTest();
+r($stakeholderTester->batchCreateTest($projectID[0], $accounts[0])) && p()    && e('0');  // 测试项目ID为0、人员为空时，批量创建项目干系人
+r($stakeholderTester->batchCreateTest($projectID[0], $accounts[1])) && p('0') && e('1');  // 测试项目ID为0、人员为admin、user1时，批量创建项目干系人
+r($stakeholderTester->batchCreateTest($projectID[0], $accounts[2])) && p('0') && e('3');  // 测试项目ID为0、人员为user10、user11时，批量创建项目干系人
+r($stakeholderTester->batchCreateTest($projectID[1], $accounts[0])) && p()    && e('0');  // 测试项目ID为11、人员为空时，批量创建项目干系人
+r($stakeholderTester->batchCreateTest($projectID[1], $accounts[1])) && p('0') && e('5');  // 测试项目ID为11、人员为admin、user1时，批量创建项目干系人
+r($stakeholderTester->batchCreateTest($projectID[1], $accounts[2])) && p('0') && e('7');  // 测试项目ID为11、人员为user10、user11时，批量创建项目干系人
+r($stakeholderTester->batchCreateTest($projectID[2], $accounts[0])) && p()    && e('0');  // 测试项目ID不存在、人员为空时，批量创建项目干系人
+r($stakeholderTester->batchCreateTest($projectID[2], $accounts[1])) && p('0') && e('9');  // 测试项目ID不存在、人员为admin、user1时，批量创建项目干系人
+r($stakeholderTester->batchCreateTest($projectID[2], $accounts[2])) && p('0') && e('11'); // 测试项目ID不存在、人员为user10、user11时，批量创建项目干系人
