c78cd6282fe573baf97e183afa10086961578e0e
sunguangming <sunguangming@easycorp.ltd>
2023-12-20 18:27:43 +0800

    * Modify unit test.

--- module/kanban/test/model/updatespace.php
+++ module/kanban/test/model/updatespace.php
@@ -15,12 +15,12 @@ cid=1
 - 测试修改空间1
  - 属性name @测试修改名称1
  - 属性owner @user3
- - 属性whitelist @,user3,po15,
+ - 属性whitelist @~~
  - 属性team @,user3,po15,
 - 测试修改空间2
  - 属性name @测试修改名称2
  - 属性owner @po16
- - 属性whitelist @,user4,po16,user3,
+ - 属性whitelist @~~
  - 属性team @,user4,po16,
 - 测试修改空间3
  - 属性name @测试修改名称3
@@ -30,7 +30,7 @@ cid=1
 - 测试修改空间4
  - 属性name @测试修改名称4
  - 属性owner @po18
- - 属性whitelist @,user6,po18,
+ - 属性whitelist @~~
  - 属性team @,user6,po18,
 - 测试修改空间5 不填写名称第name条的0属性 @『空间名称』不能为空。
 - 测试修改空间5 不填写负责人第owner条的0属性 @『负责人』不能为空。
@@ -74,9 +74,9 @@ $param6->owner = '';
 
 $kanban = new kanbanTest();
 
-r($kanban->updateSpaceTest($spaceIDList[0], $param1)) && p('name|owner|whitelist|team', '|') && e('测试修改名称1|user3|,user3,po15,|,user3,po15,');      // 测试修改空间1
-r($kanban->updateSpaceTest($spaceIDList[1], $param2)) && p('name|owner|whitelist|team', '|') && e('测试修改名称2|po16|,user4,po16,user3,|,user4,po16,'); // 测试修改空间2
+r($kanban->updateSpaceTest($spaceIDList[0], $param1)) && p('name|owner|whitelist|team', '|') && e('测试修改名称1|user3|~~|,user3,po15,'); // 测试修改空间1
+r($kanban->updateSpaceTest($spaceIDList[1], $param2)) && p('name|owner|whitelist|team', '|') && e('测试修改名称2|po16|~~|,user4,po16,');  // 测试修改空间2
 r($kanban->updateSpaceTest($spaceIDList[2], $param3)) && p('name|owner|whitelist|team', '|') && e('测试修改名称3|po17|,user5,po17,|,user5,po17,user3,'); // 测试修改空间3
-r($kanban->updateSpaceTest($spaceIDList[3], $param4)) && p('name|owner|whitelist|team', '|') && e('测试修改名称4|po18|,user6,po18,|,user6,po18,');       // 测试修改空间4
-r($kanban->updateSpaceTest($spaceIDList[4], $param5)) && p('name:0')                         && e('『空间名称』不能为空。');                             // 测试修改空间5 不填写名称
-r($kanban->updateSpaceTest($spaceIDList[4], $param6)) && p('owner:0')                        && e('『负责人』不能为空。');                               // 测试修改空间5 不填写负责人
+r($kanban->updateSpaceTest($spaceIDList[3], $param4)) && p('name|owner|whitelist|team', '|') && e('测试修改名称4|po18|~~|,user6,po18,'); // 测试修改空间4
+r($kanban->updateSpaceTest($spaceIDList[4], $param5)) && p('name:0')                         && e('『空间名称』不能为空。'); // 测试修改空间5 不填写名称
+r($kanban->updateSpaceTest($spaceIDList[4], $param6)) && p('owner:0')                        && e('『负责人』不能为空。'); // 测试修改空间5 不填写负责人
