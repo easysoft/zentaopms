95a98ea056883bc90c4e3e60b713a794e1b654f4
caoyanyi <caoyanyi@easycorp.ltd>
2023-12-21 20:00:04 +0800

    + Add unit test case for gitea->apiGetBranches.

---
+++ module/gitea/test/model/apigetbranches.php
@@ -0,0 +1,44 @@
+#!/usr/bin/env php
+<?php
+
+/**
+
+title=测试 giteaModel::apiGetBranches();
+timeout=0
+cid=0
+
+- 错误的服务器ID @0
+- 错误的项目ID @0
+- 正确的项目ID
+ - 属性name @main
+ - 属性user_can_push @1
+- 没有权限的用户 @0
+- 有权限的用户
+ - 属性name @main
+ - 属性user_can_push @1
+
+*/
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+
+zdTable('pipeline')->gen(5);
+zdTable('oauth')->config('oauth')->gen(5);
+su('admin');
+
+global $tester;
+$giteaModel = $tester->loadModel('gitea');
+
+$giteaID = 1;
+$project = '';
+r($giteaModel->apiGetBranches($giteaID, $project)) && p() && e('0'); // 错误的服务器ID
+
+$giteaID = 4;
+r($giteaModel->apiGetBranches($giteaID, $project)) && p() && e('0'); // 错误的项目ID
+
+$project = 'gitea/unittest';
+r(end($giteaModel->apiGetBranches($giteaID, $project))) && p('name,user_can_push') && e('main,1'); // 正确的项目ID
+
+su('user1');
+r($giteaModel->apiGetBranches($giteaID, $project)) && p() && e('0'); // 没有权限的用户
+
+su('user2');
+r(end($giteaModel->apiGetBranches($giteaID, $project))) && p('name,user_can_push') && e('main,1'); // 有权限的用户
