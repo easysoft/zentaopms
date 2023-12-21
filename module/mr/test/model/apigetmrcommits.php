cc4175689c3bebc2e499f811ae21591c7d17fe7b
caoyanyi <caoyanyi@easycorp.ltd>
2023-12-21 15:03:49 +0800

    * Adjust unit test check rule for mr->apiGetMRCommits.

--- module/mr/test/model/apigetmrcommits.php
+++ module/mr/test/model/apigetmrcommits.php
@@ -8,9 +8,8 @@ timeout=0
 cid=0
 
 - 正确的Gitlab合并请求
- - 属性short_id @cedfe9a5
+ - 属性short_id @59b9ec0f
  - 属性author_name @Administrator
- - 属性message @Update README.md
 - 正确的Gitea合并请求
  - 属性sha @d30919bdb9b4cf8e2698f4a6a30e41910427c01c
  - 第committer条的login属性 @gitea
@@ -46,9 +45,9 @@ $mrID = array(
     'gogs'   => 7
 );
 
-r($mrModel->apiGetMRCommitsTester($hostID['gitlab'], $projectID['gitlab'], $mrID['gitlab'])) && p('short_id,author_name,message') && e('cedfe9a5,Administrator,Update README.md');        // 正确的Gitlab合并请求
-r($mrModel->apiGetMRCommitsTester($hostID['gitea'],  $projectID['gitea'],  $mrID['gitea']))  && p('sha;committer:login')          && e('d30919bdb9b4cf8e2698f4a6a30e41910427c01c,gitea'); // 正确的Gitea合并请求
-r($mrModel->apiGetMRCommitsTester($hostID['gogs'],   $projectID['gogs'],   $mrID['gogs']))   && p()                               && e('0');                                              // gogs没有接口，返回0
+r($mrModel->apiGetMRCommitsTester($hostID['gitlab'], $projectID['gitlab'], $mrID['gitlab'])) && p('short_id,author_name') && e('59b9ec0f,Administrator');        // 正确的Gitlab合并请求
+r($mrModel->apiGetMRCommitsTester($hostID['gitea'],  $projectID['gitea'],  $mrID['gitea']))  && p('sha;committer:login')  && e('d30919bdb9b4cf8e2698f4a6a30e41910427c01c,gitea'); // 正确的Gitea合并请求
+r($mrModel->apiGetMRCommitsTester($hostID['gogs'],   $projectID['gogs'],   $mrID['gogs']))   && p()                       && e('0');                                              // gogs没有接口，返回0
 
 r($mrModel->apiGetMRCommitsTester($hostID['gitlab'], 9999, $mrID['gitlab'])) && p('message') && e('404 Project Not Found'); // 错误的Gitlab合并请求
 r($mrModel->apiGetMRCommitsTester($hostID['gitea'],  9999, $mrID['gitea']))  && p()          && e('0');                     // 错误的Gitea合并请求
