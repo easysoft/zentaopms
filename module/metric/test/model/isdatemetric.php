48a70c7825d9dcf9e068841229cb27ec3736e73f
zhouxin <zhouxin@easycorp.ltd>
2023-12-21 09:50:32 +0800

    * Add unit test to metricModel-isDateMetric.

--- module/metric/test/model/isdatemetric.php
+++ module/metric/test/model/isdatemetric.php
@@ -2,15 +2,15 @@
 <?php
 /**
 title=isDateMetric
+timeout=0
 cid=1
-pid=1
 */
+
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/calc.class.php';
+su('admin');
 
 $metric = new metricTest();
-
-$records = array();
 
 $header1   = array();
 $header1[] = array('name' => 'value', 'title' => '数值', 'width' => 96);
@@ -29,7 +29,7 @@ $header3[] = array('name' => 'calcTime', 'title' => '采集时间', 'width' => 1
 
 $header4   = array();
 
-r($metric->isDateMetric($header1))  && p() && e('0'); // 测试header1是否有时间概念
-r($metric->isDateMetric($header2))  && p() && e('1'); // 测试header2是否有时间概念
-r($metric->isDateMetric($header3))  && p() && e('0'); // 测试header3是否有时间概念
-r($metric->isDateMetric($header4))  && p() && e('0'); // 测试header4是否有时间概念
+r($metric->isDateMetric($header1)) && p('') && e('false'); // 测试系统度量项
+r($metric->isDateMetric($header2)) && p('') && e('true');  // 测试有日期的度量项
+r($metric->isDateMetric($header3)) && p('') && e('false'); // 测试有对象但没有日期的度量项
+r($metric->isDateMetric($header4)) && p('') && e('false'); // 测试传入空值
