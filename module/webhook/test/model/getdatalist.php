a51a9fba80f0302a646bb8a8245820dd759fc9df
sunguangming <sunguangming@easycorp.ltd>
2023-12-21 14:28:32 +0800

    * webhook: modify unit test.

--- module/webhook/test/model/getdatalist.php
+++ module/webhook/test/model/getdatalist.php
@@ -4,18 +4,20 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/webhook.class.php';
 su('admin');
 
+zdTable('notify')->gen(50);
+
 /**
 
 title=测试 webhookModel->getDataList();
+timeout=0
 cid=1
-pid=1
 
-不需传任何参数,取出其中一个ID=39的日志内容，这里时间上取当前时间三小时以前的 >> 用户创建了任务39
-统计数量 >> 20
+- 不需传任何参数,取出其中一个ID=40的日志内容，这里时间上取当前时间三小时以前的第40条的data属性 @用户创建了任务40
+- 统计数量 @8
 
 */
 
 $webhook = new webhookTest();
 
-r($webhook->getDataListTest())        && p('39:data') && e('用户创建了任务39'); //不需传任何参数,取出其中一个ID=39的日志内容，这里时间上取当前时间三小时以前的
-r(count($webhook->getDataListTest())) && p()          && e('20');               //统计数量
+r($webhook->getDataListTest())        && p('40:data') && e('用户创建了任务40'); //不需传任何参数,取出其中一个ID=40的日志内容，这里时间上取当前时间三小时以前的
+r(count($webhook->getDataListTest())) && p()          && e('8');                //统计数量
