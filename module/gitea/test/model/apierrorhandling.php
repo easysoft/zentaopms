07861afe726bb4536046bf78e696ccd9a42dc13d
caoyanyi <caoyanyi@easycorp.ltd>
2023-12-21 17:32:02 +0800

    * Adjust unit test case for gitea.

--- module/gitea/test/model/apierrorhandling.php
+++ module/gitea/test/model/apierrorhandling.php
@@ -7,6 +7,15 @@ title=测试 giteaModel::apiErrorHandling();
 timeout=0
 cid=0
 
+- 对象里没有任何属性 @error
+- 对象里有error属性 @some error
+- 对象里有message属性属性name @名称已存在。
+- 对象里有不能解析的message属性 @some message
+- 对象里message属性是数组,且不能解析 @some message
+- 对象里message属性是数组
+ -  @some message
+ - 属性name @名称已存在。
+
 */
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/gitea.class.php';
