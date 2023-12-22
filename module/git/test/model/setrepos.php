d213f5377b72055483a80f31edb1a1ad4238c22b
caoyanyi <caoyanyi@easycorp.ltd>
2023-12-22 18:24:51 +0800

    * Adjust unit test case for git->setRepos.

--- module/git/test/model/setrepos.php
+++ module/git/test/model/setrepos.php
@@ -1,19 +1,36 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . '/test/lib/init.php';
-include dirname(__FILE__, 2) . '/git.class.php';
-su('admin');
 
 /**
 
-title=测试gitModel->setRepos();
+title=gitModel->setRepos();
 timeout=0
 cid=1
 
-- 设置版本库属性id @1
+- 获取不到数据时，提示错误信息 @8
+- 获取第一条git记录的name和SCM属性
+ - 第1条的name属性 @unittest1
+ - 第1条的SCM属性 @Gitlab
+- 获取不到数据时，提示错误信息 @You must set one git repo.
 
 */
 
-$git = new gitTest();
-$tester->dao->update(TABLE_REPO)->set('synced')->eq(1)->where('id')->eq(1)->exec();
-r($git->setRepos()) && p('id') && e(1);     // 设置版本库
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+
+zdTable('repo')->config('repo')->gen(10);
+su('admin');
+
+global $tester;
+$git = $tester->loadModel('git');
+$git->setRepos();
+
+r(count($git->repos)) && p() && e('8'); // 获取不到数据时，提示错误信息
+r($git->repos) && p('1:name,SCM') && e('unittest1,Gitlab'); // 获取第一条git记录的name和SCM属性
+
+zdTable('repo')->gen(0);
+dao::$cache = array();
+
+ob_start();
+$git->setRepos();
+$result = ob_get_clean();
+r($result) && p() && e('You must set one git repo.'); // 获取不到数据时，提示错误信息
