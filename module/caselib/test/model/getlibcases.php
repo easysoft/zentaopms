c301c636545b2612f6eb6df995c3c4d00001c2db
wangyuting <wangyuting@easycorp.ltd>
2023-12-22 16:24:25 +0800

    * Fix caselib module unit test bug.

--- module/caselib/test/model/getlibcases.php
+++ module/caselib/test/model/getlibcases.php
@@ -25,24 +25,24 @@ $caselib = new caselibTest();
 
 r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[0], $moduleIdList[0], $sortList[0])) && p() && e('429,427,425,423,421,419,417,415,413,411,409,407,405,403,401'); // bymodule 状态下 查询用例库 201 模块 0 排序 id_desc 的用例 id
 r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[0], $moduleIdList[1], $sortList[0])) && p() && e('421,401');                                                     // bymodule 状态下 查询用例库 201 模块 1 排序 id_desc 的用例 id
-r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[0], $moduleIdList[2], $sortList[0])) && p() && e('429,427,425,423,421,419,417,415,413,411,409,407,405,403,401'); // bymodule 状态下 查询用例库 201 模块 11 排序 id_desc 的用例 id
+r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[0], $moduleIdList[2], $sortList[0])) && p() && e('0');                                                           // bymodule 状态下 查询用例库 201 模块 11 排序 id_desc 的用例 id
 r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[0], $moduleIdList[0], $sortList[1])) && p() && e('401,403,405,407,409,411,413,415,417,419,421,423,425,427,429'); // bymodule 状态下 查询用例库 201 模块 0 排序 id_asc 的用例 id
 r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[0], $moduleIdList[1], $sortList[1])) && p() && e('401,421');                                                     // bymodule 状态下 查询用例库 201 模块 1 排序 id_asc 的用例 id
-r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[0], $moduleIdList[2], $sortList[1])) && p() && e('401,403,405,407,409,411,413,415,417,419,421,423,425,427,429'); // bymodule 状态下 查询用例库 201 模块 11 排序 id_asc 的用例 id
+r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[0], $moduleIdList[2], $sortList[1])) && p() && e('0');                                                           // bymodule 状态下 查询用例库 201 模块 11 排序 id_asc 的用例 id
 
 r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[1], $moduleIdList[0], $sortList[0])) && p() && e('429,427,425,423,421,419,417,415,413,411,409,407,405,403,401'); // all 状态下 查询用例库 201 模块 0 排序 id_desc 的用例 id
 r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[1], $moduleIdList[1], $sortList[0])) && p() && e('421,401');                                                     // all 状态下 查询用例库 201 模块 1 排序 id_desc 的用例 id
-r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[1], $moduleIdList[2], $sortList[0])) && p() && e('429,427,425,423,421,419,417,415,413,411,409,407,405,403,401'); // all 状态下 查询用例库 201 模块 11 排序 id_desc 的用例 id
+r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[1], $moduleIdList[2], $sortList[0])) && p() && e('0');                                                           // all 状态下 查询用例库 201 模块 11 排序 id_desc 的用例 id
 r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[1], $moduleIdList[0], $sortList[1])) && p() && e('401,403,405,407,409,411,413,415,417,419,421,423,425,427,429'); // all 状态下 查询用例库 201 模块 0 排序 id_asc 的用例 id
 r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[1], $moduleIdList[1], $sortList[1])) && p() && e('401,421');                                                     // all 状态下 查询用例库 201 模块 1 排序 id_asc 的用例 id
-r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[1], $moduleIdList[2], $sortList[1])) && p() && e('401,403,405,407,409,411,413,415,417,419,421,423,425,427,429'); // all 状态下 查询用例库 201 模块 11 排序 id_asc 的用例 id
+r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[1], $moduleIdList[2], $sortList[1])) && p() && e('0');                                                           // all 状态下 查询用例库 201 模块 11 排序 id_asc 的用例 id
 
 r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[2], $moduleIdList[0], $sortList[0])) && p() && e('429,425,421,417,413,409,405,401'); // wait 状态下 查询用例库 201 模块 0 排序 id_desc 的用例 id
 r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[2], $moduleIdList[1], $sortList[0])) && p() && e('421,401');                         // wait 状态下 查询用例库 201 模块 1 排序 id_desc 的用例 id
-r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[2], $moduleIdList[2], $sortList[0])) && p() && e('429,425,421,417,413,409,405,401'); // wait 状态下 查询用例库 201 模块 11 排序 id_desc 的用例 id
+r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[2], $moduleIdList[2], $sortList[0])) && p() && e('0');                               // wait 状态下 查询用例库 201 模块 11 排序 id_desc 的用例 id
 r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[2], $moduleIdList[0], $sortList[1])) && p() && e('401,405,409,413,417,421,425,429'); // wait 状态下 查询用例库 201 模块 0 排序 id_asc 的用例 id
 r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[2], $moduleIdList[1], $sortList[1])) && p() && e('401,421');                         // wait 状态下 查询用例库 201 模块 1 排序 id_asc 的用例 id
-r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[2], $moduleIdList[2], $sortList[1])) && p() && e('401,405,409,413,417,421,425,429'); // wait 状态下 查询用例库 201 模块 11 排序 id_asc 的用例 id
+r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[2], $moduleIdList[2], $sortList[1])) && p() && e('0');                               // wait 状态下 查询用例库 201 模块 11 排序 id_asc 的用例 id
 
 r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[3], $moduleIdList[0], $sortList[0])) && p() && e('429,427,425,423,421,419,417,415,413,411,409,407,405,403,401'); // bysearch 状态下 查询用例库 201 模块 0 排序 id_desc 的用例 id
 r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[3], $moduleIdList[1], $sortList[0])) && p() && e('429,427,425,423,421,419,417,415,413,411,409,407,405,403,401'); // bysearch 状态下 查询用例库 201 模块 1 排序 id_desc 的用例 id
@@ -61,10 +61,10 @@ r($caselib->getLibCasesTest($libIdList[0], $browseTypeList[4], $moduleIdList[2],
 
 r($caselib->getLibCasesTest($libIdList[1], $browseTypeList[0], $moduleIdList[0], $sortList[0])) && p() && e('430,428,426,424,422,420,418,416,414,412,410,408,406,404,402'); // bymodule 状态下 查询用例库 402 模块 0 排序 id_desc 的用例 id
 r($caselib->getLibCasesTest($libIdList[1], $browseTypeList[0], $moduleIdList[1], $sortList[0])) && p() && e('0');                                                           // bymodule 状态下 查询用例库 402 模块 1 排序 id_desc 的用例 id
-r($caselib->getLibCasesTest($libIdList[1], $browseTypeList[0], $moduleIdList[2], $sortList[0])) && p() && e('430,428,426,424,422,420,418,416,414,412,410,408,406,404,402'); // bymodule 状态下 查询用例库 402 模块 11 排序 id_desc 的用例 id
+r($caselib->getLibCasesTest($libIdList[1], $browseTypeList[0], $moduleIdList[2], $sortList[0])) && p() && e('0');                                                           // bymodule 状态下 查询用例库 402 模块 11 排序 id_desc 的用例 id
 r($caselib->getLibCasesTest($libIdList[1], $browseTypeList[0], $moduleIdList[0], $sortList[1])) && p() && e('402,404,406,408,410,412,414,416,418,420,422,424,426,428,430'); // bymodule 状态下 查询用例库 402 模块 0 排序 id_asc 的用例 id
 r($caselib->getLibCasesTest($libIdList[1], $browseTypeList[0], $moduleIdList[1], $sortList[1])) && p() && e('0');                                                           // bymodule 状态下 查询用例库 402 模块 1 排序 id_asc 的用例 id
-r($caselib->getLibCasesTest($libIdList[1], $browseTypeList[0], $moduleIdList[2], $sortList[1])) && p() && e('402,404,406,408,410,412,414,416,418,420,422,424,426,428,430'); // bymodule 状态下 查询用例库 402 模块 11 排序 id_asc 的用例 id
+r($caselib->getLibCasesTest($libIdList[1], $browseTypeList[0], $moduleIdList[2], $sortList[1])) && p() && e('0');                                                           // bymodule 状态下 查询用例库 402 模块 11 排序 id_asc 的用例 id
 
 r($caselib->getLibCasesTest($libIdList[1], $browseTypeList[2], $moduleIdList[0], $sortList[0])) && p() && e('0'); // wait 状态下 查询用例库 402 模块 0 排序 id_desc 的用例 id
 r($caselib->getLibCasesTest($libIdList[1], $browseTypeList[2], $moduleIdList[1], $sortList[0])) && p() && e('0'); // wait 状态下 查询用例库 402 模块 1 排序 id_desc 的用例 id
--- module/caselib/test/model/update.php
+++ module/caselib/test/model/update.php
@@ -33,5 +33,5 @@ $lib2->desc = '测试修改描述';
 $lib2->uid  = '';
 
 $caselib = new caselibTest();
-r($caselib->updateTest($lib1)) && p('name:0')    && e('『name』不能为空。');        //测试更新名称为空时候返回
+r($caselib->updateTest($lib1)) && p('name:0')    && e('『名称』不能为空。');        //测试更新名称为空时候返回
 r($caselib->updateTest($lib2)) && p('name;desc') && e('测试修改名称;测试修改描述'); //测试更新之后名称信息
