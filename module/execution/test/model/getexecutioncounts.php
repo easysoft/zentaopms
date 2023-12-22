62a9f6bb216485753d69ba8312624351c473cfd0
wangyuting <wangyuting@easycorp.ltd>
2023-12-22 18:18:10 +0800

    * Fix getexecutioncounts unit test bug.

--- module/execution/test/model/getexecutioncounts.php
+++ module/execution/test/model/getexecutioncounts.php
@@ -3,6 +3,9 @@
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/execution.class.php';
 
+zdTable('group')->gen(0);
+zdTable('userview')->gen(0);
+
 $execution = zdTable('project');
 $execution->id->range('1-5');
 $execution->name->range('项目1,项目2,迭代1,迭代2,迭代3');
@@ -15,6 +18,7 @@ $execution->path->range('1,2,`1,3`,`1,4`,`2,5`')->prefix(',')->postfix(',');
 $execution->begin->range('20230102 000000:0')->type('timestamp')->format('YY/MM/DD');
 $execution->end->range('20230212 000000:0')->type('timestamp')->format('YY/MM/DD');
 $execution->gen(5);
+
 su('admin');
 
 /**
@@ -27,6 +31,6 @@ cid=1
 
 global $tester;
 $tester->loadModel('execution');
-r($tester->execution->getExecutionCounts(1)) && p() && e('0'); // 根据executionID查找任务详情
+r($tester->execution->getExecutionCounts(1)) && p() && e('1'); // 根据executionID查找任务详情
 r($tester->execution->getExecutionCounts(2)) && p() && e('1'); // 根据executionID查找任务详情
 r($tester->execution->getExecutionCounts(3)) && p() && e('0'); // 根据executionID查找任务详情
