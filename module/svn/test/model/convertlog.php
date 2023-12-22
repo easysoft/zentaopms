cceff62161a6b3792704bfee4ba410c695ebfd44
caoyanyi <caoyanyi@easycorp.ltd>
2023-12-22 16:58:56 +0800

    + Add unit test case for git and svn convertLog.

--- module/svn/test/model/convertlog.php
+++ module/svn/test/model/convertlog.php
@@ -5,7 +5,7 @@ su('admin');
 
 /**
 
-title=测试gitModel->convertLog();
+title=测试svnModel->convertLog();
 timeout=0
 cid=1
 
@@ -15,14 +15,14 @@ cid=1
 */
 
 global $tester;
-$git = $tester->loadModel('git');
+$svn = $tester->loadModel('svn');
 
 $log = array();
 $log[] = "commit e7699d04f1586d337f34496da932dde55db92616";
 $log[] = "Author: zhengrunyu <zhenrunyu@easycorp.ltd>";
 $log[] = "Date:   Thu May 5 16:19:48 2022 +0800";
 $log[] = "  * Fix bug 22061.";
-r($git->convertLog($log)) && p('revision') && e('e7699d04f1586d337f34496da932dde55db92616');    // 使用正确格式的数据
+r($svn->convertLog($log)) && p('revision') && e('e7699d04f1586d337f34496da932dde55db92616');    // 使用正确格式的数据
 
 $log = array();
-r($git->convertLog($log)) && p('revision') && e(0);     // 使用空的数据
+r($svn->convertLog($log)) && p('revision') && e(0);     // 使用空的数据
