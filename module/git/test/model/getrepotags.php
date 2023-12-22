4d71fa90298a648c34bd2e35663f2e364c9e65c1
caoyanyi <caoyanyi@easycorp.ltd>
2023-12-22 18:36:07 +0800

    * Adjust unit test case for git->getRepoTags.

--- module/git/test/model/getrepotags.php
+++ module/git/test/model/getrepotags.php
@@ -1,34 +1,24 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . '/test/lib/init.php';
-include dirname(__FILE__, 2) . '/git.class.php';
-su('admin');
 
 /**
 
-title=测试gitModel->getRepoTags();
+title=gitModel->getRepoTags();
 timeout=0
 cid=1
 
-- 获取版本库的tags @1
-- 使用空数据 @0
-- 使用错误的版本库 @0
+- 查询tag信息 @tag1
 
 */
 
-$git = new gitTest();
+include dirname(__FILE__, 5) . '/test/lib/init.php';
 
-/* Fix error Gitlab url. */
-$tester->dao->update(TABLE_PIPELINE)->set('url')->eq('http://10.0.1.161:51080')->where('id')->eq(1)->exec();
+zdTable('repo')->config('repo')->gen(1);
+su('admin');
 
-$repo = $tester->dao->select('*')->from(TABLE_REPO)->limit(1)->fetch();
-if(strtolower($repo->SCM) == 'gitlab') $repo = $tester->loadModel('repo')->processGitlab($repo);
-r($git->getRepoTags($repo)) && p() && e(1);     // 获取版本库的tags
+global $tester;
+$git = $tester->loadModel('git');
+$git->setRepos();
 
-$repo = new stdclass();
-r($git->getRepoTags($repo)) && p() && e(0);    // 使用空数据
-
-$repo = new stdclass();
-$repo->client = '';
-$repo->path   = '';
-r($git->getRepoTags($repo)) && p() && e(0);    // 使用错误的版本库
+$repo = $git->repos[1];
+r($git->getRepoTags($repo)) && p('0') && e('tag1'); // 查询tag信息
