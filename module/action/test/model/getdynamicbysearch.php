36faf01759e8c8bea41360ba4f192f93412b896f
wangyuting <wangyuting@easycorp.ltd>
2023-12-22 19:59:23 +0800

    * Fix action unit test bug.

--- module/action/test/model/getdynamicbysearch.php
+++ module/action/test/model/getdynamicbysearch.php
@@ -26,7 +26,7 @@ pid=1
 */
 
 $queryID       = array(0, 1);
-$orderByList   = array('date_desc', 'date_asc');
+$orderByList   = array('date_desc,id_desc', 'date_asc,id_desc');
 $limit         = 50;
 $dateList      = array('', 'today');
 $directionList = array('next', 'pre');
@@ -34,7 +34,7 @@ $directionList = array('next', 'pre');
 $action = new actionTest();
 
 r($action->getDynamicBySearchTest($queryID[0], $orderByList[0], $limit, $dateList[0], $directionList[0])) && p('0:id,objectType;1:id,objectType') && e('62,branch;31,bug');       // 获取排序为date倒序的所有动态
-r($action->getDynamicBySearchTest($queryID[0], $orderByList[1], $limit, $dateList[0], $directionList[0])) && p('0:id,objectType;1:id,objectType') && e('1,product;32,testcase');  // 获取排序为date正序的所有动态
+r($action->getDynamicBySearchTest($queryID[0], $orderByList[1], $limit, $dateList[0], $directionList[0])) && p('0:id,objectType;1:id,objectType') && e('63,module;32,testcase');  // 获取排序为date正序的所有动态
 r($action->getDynamicBySearchTest($queryID[0], $orderByList[0], $limit, $dateList[0], $directionList[1])) && p('0:id,objectType;1:id,objectType') && e('62,branch;31,bug');       // 获取排序为date倒序的今年之后的动态
 r($action->getDynamicBySearchTest($queryID[0], $orderByList[0], $limit, $dateList[1], $directionList[0])) && p('0:id,objectType;1:id,objectType') && e('61,todo;30,build');       // 获取排序为date倒序的今天之前的动态
 r($action->getDynamicBySearchTest($queryID[1], $orderByList[0], $limit, $dateList[0], $directionList[1])) && p('0:id,objectType')                 && e('1,product');              // 获取排序为date倒序的今年之后的动态
