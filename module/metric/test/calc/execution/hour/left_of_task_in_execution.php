4147f12c0421885abb91caa6651d571eee995eae
zhouxin <zhouxin@easycorp.ltd>
2023-12-23 14:56:37 +0800

    * Fix unit test of calc in metric module.

--- module/metric/test/calc/execution/hour/left_of_task_in_execution.php
+++ module/metric/test/calc/execution/hour/left_of_task_in_execution.php
@@ -2,7 +2,7 @@
 <?php
 /**
 
-title=left_of_task_in_project
+title=left_of_task_in_execution
 timeout=0
 cid=1
 
@@ -18,7 +18,7 @@ $metric = new metricTest();
 $calc   = $metric->calcMetric(__FILE__);
 
 r(count($calc->getResult())) && p('') && e('10'); // 测试分组数。
-r($calc->getResult(array('execution' => 11))) && p('0:value') && e('126'); // 测试执行11的任务预计工时数。
-r($calc->getResult(array('execution' => 12))) && p('0:value') && e('92');  // 测试执行12的任务预计工时数。
-r($calc->getResult(array('execution' => 13))) && p('0:value') && e('111'); // 测试执行13的任务预计工时数。
+r($calc->getResult(array('execution' => 11))) && p('0:value') && e('108'); // 测试执行11的任务预计工时数。
+r($calc->getResult(array('execution' => 12))) && p('0:value') && e('76');  // 测试执行12的任务预计工时数。
+r($calc->getResult(array('execution' => 13))) && p('0:value') && e('96');  // 测试执行13的任务预计工时数。
 r($calc->getResult(array('execution' => 110))) && p('') && e('0');         // 测试不存在执行的任务预计工时数。
