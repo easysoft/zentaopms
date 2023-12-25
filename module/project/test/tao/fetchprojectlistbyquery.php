376f0eb422fca29f1f77d58875ac6f5cc2ea1b8f
liumengyi <liumengyi@easycorp.ltd>
2023-12-25 08:49:12 +0800

    * Fix bug of get projects, and modify its unit test.

--- module/project/test/tao/fetchprojectlistbyquery.php
+++ module/project/test/tao/fetchprojectlistbyquery.php
@@ -1,5 +1,26 @@
 #!/usr/bin/env php
 <?php
+
+/**
+
+title=测试 projectModel::fetchProjectListByQuery($status, $projectID, $orderBy);
+cid=1
+
+- 获取未开始的项目数量 @3
+- 获取状态不为done和closed的项目数量 @7
+- 根据项目ID获取项目 @1
+- 获取不存在的项目 @0
+- 根据 不匹配的项目ID和状态 获取项目数量 @0
+- 根据 匹配的项目ID和状态 获取项目数量 @1
+- 按照ID正序获取项目列表,查看排第一个的项目详情
+ - 属性id @11
+ - 属性name @项目11
+- 按照项目名称倒序获取项目列表,查看排第一个的项目详情
+ - 属性id @19
+ - 属性name @项目19
+
+*/
+
 include dirname(__FILE__, 5) . "/test/lib/init.php";
 include dirname(__FILE__, 2) . "/project.class.php";
 
@@ -37,24 +58,16 @@ $stakeholder->gen(9);
 
 su('admin');
 
-/**
-
-title=测试 projectModel::fetchProjectListByQuery($status, $projectID, $orderBy);
-timeout=0
-cid=1
-
-*/
-
 $projectTester = new Project();
 
 r(count($projectTester->testFetchProjectListByQuery('wait')))   && p() && e('3'); // 获取未开始的项目数量
 r(count($projectTester->testFetchProjectListByQuery('undone'))) && p() && e('7'); // 获取状态不为done和closed的项目数量
 
-r(count($projectTester->testFetchProjectListByQuery('', 11)))    && p() && e('9'); // 根据项目ID获取项目
-r(count($projectTester->testFetchProjectListByQuery('', 10000))) && p() && e('9'); // 获取不存在的项目
+r(count($projectTester->testFetchProjectListByQuery('', 11)))    && p() && e('1'); // 根据项目ID获取项目
+r(count($projectTester->testFetchProjectListByQuery('', 10000))) && p() && e('0'); // 获取不存在的项目
 
-r(count($projectTester->testFetchProjectListByQuery('doing', 11))) && p() && e('3'); // 根据 不匹配的项目ID和状态 获取项目数量
-r(count($projectTester->testFetchProjectListByQuery('doing', 12))) && p() && e('2'); // 根据 匹配的项目ID和状态 获取项目数量
+r(count($projectTester->testFetchProjectListByQuery('doing', 11))) && p() && e('0'); // 根据 不匹配的项目ID和状态 获取项目数量
+r(count($projectTester->testFetchProjectListByQuery('doing', 12))) && p() && e('1'); // 根据 匹配的项目ID和状态 获取项目数量
 
 r(current($projectTester->testFetchProjectListByQuery('all', 0, 'id_asc')))    && p('id,name') && e('11,项目11'); // 按照ID正序获取项目列表,查看排第一个的项目详情
 r(current($projectTester->testFetchProjectListByQuery('all', 0, 'name_desc'))) && p('id,name') && e('19,项目19'); // 按照项目名称倒序获取项目列表,查看排第一个的项目详情
