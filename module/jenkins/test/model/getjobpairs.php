aae415b8fc2a3a64795e9fb928a3a88f738b2c3f
liumengyi <liumengyi@easycorp.ltd>
2023-12-25 10:55:20 +0800

    + Add unit test result of jenkinsModel::getJobPairs.

--- module/jenkins/test/model/getjobpairs.php
+++ module/jenkins/test/model/getjobpairs.php
@@ -7,6 +7,14 @@ title=测试jenkinsModel->getJobPairs();
 cid=1
 pid=1
 
+- 测试获取 jenkins 1 的构建任务 @1:这是一个Job1,6:这是一个Job6
+
+- 测试获取 jenkins 2 的构建任务 @2:这是一个Job2,7:这是一个Job7
+
+- 测试获取 jenkins 3 的构建任务 @3:这是一个Job3
+- 测试获取 jenkins 0 的构建任务 @0
+- 测试获取 jenkins 111 的构建任务 @0
+
 */
 
 include dirname(__FILE__, 5) . '/test/lib/init.php';
