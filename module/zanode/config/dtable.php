f3557b91844da44b816094eab9dc19ba35e12cb1
xieqiyu <xieqiyu@easycorp.ltd>
2023-12-26 13:50:20 +0800

    * Fix the bug of destroy zanode.

--- module/zanode/config/dtable.php
+++ module/zanode/config/dtable.php
@@ -96,7 +96,7 @@ $config->zanode->dtable->fieldList['actions']['list']['createImage']['data-toggl
 $config->zanode->dtable->fieldList['actions']['list']['destroy']['icon']         = 'trash';
 $config->zanode->dtable->fieldList['actions']['list']['destroy']['hint']         = $lang->zanode->destroy;
 $config->zanode->dtable->fieldList['actions']['list']['destroy']['url']          = array('module' => 'zanode', 'method' => 'destroy', 'params' => 'zanodeID={id}');
-$config->zanode->dtable->fieldList['actions']['list']['destroy']['class']        = 'ajax-submit';
+$config->zanode->dtable->fieldList['actions']['list']['destroy']['innerClass']   = 'ajax-submit';
 $config->zanode->dtable->fieldList['actions']['list']['destroy']['data-confirm'] = $lang->zanode->confirmDelete;
 
 $config->zanode->snapshotDtable = new stdclass();
