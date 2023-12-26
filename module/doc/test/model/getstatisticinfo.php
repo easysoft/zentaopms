b56d6393e959823dc4e5cf377f003464572368c9
wangyuting <wangyuting@easycorp.ltd>
2023-12-26 14:25:23 +0800

    * Adjust doc unit test `` to ~~.

--- module/doc/test/model/getstatisticinfo.php
+++ module/doc/test/model/getstatisticinfo.php
@@ -20,8 +20,8 @@ cid=1
  - 属性myEditedDocs @10
  - 属性myDocs @0
 - 获取登录用户为user1时，用户浏览跟收藏的文档数
- - 第myDoc条的docViews属性 @``
- - 第myDoc条的docCollects属性 @``
+ - 第myDoc条的docViews属性 @~~
+ - 第myDoc条的docCollects属性 @~~
 
 */
 
@@ -42,4 +42,4 @@ r($adminInfo) && p('myDoc:docViews,docCollects')                    && e('0,0');
 
 /* User1 statistic information.*/
 r($user1Info) && p('totalDocs,todayEditedDocs,myEditedDocs,myDocs') && e('50,0,10,0'); // 获取登录用户为user1时，文档总数、今日编辑文档数、用户编辑过的文档数、用户创建的文档数
-r($user1Info) && p('myDoc:docViews,docCollects')                    && e('``,``');   // 获取登录用户为user1时，用户浏览跟收藏的文档数
+r($user1Info) && p('myDoc:docViews,docCollects')                    && e('~~,~~');   // 获取登录用户为user1时，用户浏览跟收藏的文档数
