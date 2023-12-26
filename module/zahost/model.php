fe0ec714730b41132d0bbc651fe56781c9d0adcb
daitingting <daitingting@xirangit.com>
2023-12-26 11:38:19 +0000

    * Refactor getServiceStatus function.

--- module/zahost/model.php
+++ module/zahost/model.php
@@ -449,21 +449,20 @@ class zahostModel extends model
     }
 
     /**
+     * 获取服务状态。
      * Get service status from ZAgent server.
      *
      * @param  object $host
      * @access public
      * @return array
      */
-    public function getServiceStatus($host)
+    public function getServiceStatus(object $host): array
     {
         if(in_array($host->status, array('wait', 'offline'))) return $this->lang->zahost->init->serviceStatus;
-        $result = json_decode(commonModel::http("http://{$host->extranet}:{$host->zap}/api/v1/service/check", json_encode(array("services" => "all")), array(), array("Authorization:$host->tokenSN")));
-        if(empty($result) || $result->code != 'success')
-        {
-            $result = new stdclass;
-            $result->data = $this->lang->zahost->init->serviceStatus;
-        }
+
+        $result = json_decode(commonModel::http("http://{$host->extranet}:{$host->zap}/api/v1/service/check", json_encode(array('services' => 'all')), array(), array("Authorization:$host->tokenSN")));
+
+        if(empty($result) || $result->code != 'success') return $this->lang->zahost->init->serviceStatus;
 
         return $result->data;
     }
