a53eecc1f151316159f8e30bc13f3361aaef6af3
tianshujie <tianshujie@easycorp.ltd>
2023-12-26 15:37:10 +0800

    * Change data of the zt_pipeline table.

--- module/artifactrepo/test/model/getlist.php
+++ module/artifactrepo/test/model/getlist.php
@@ -53,42 +53,42 @@ cid=1
  - 第1条的serverID属性 @1
  - 第1条的repoName属性 @代码库1
  - 第1条的pipelineID属性 @1
- - 第1条的url属性 @https/repository/代码库1/
+ - 第1条的url属性 @https://nexus3dev.qc.oop.cc//repository/代码库1/
 - 按照ID正序，每页10条，取第1页的数据
  - 第1条的name属性 @制品库1
  - 第1条的products属性 @1
  - 第1条的serverID属性 @1
  - 第1条的repoName属性 @代码库1
  - 第1条的pipelineID属性 @1
- - 第1条的url属性 @https/repository/代码库1/
+ - 第1条的url属性 @https://nexus3dev.qc.oop.cc//repository/代码库1/
 - 按照ID正序，每页20条，取第1页的数据
  - 第1条的name属性 @制品库1
  - 第1条的products属性 @1
  - 第1条的serverID属性 @1
  - 第1条的repoName属性 @代码库1
  - 第1条的pipelineID属性 @1
- - 第1条的url属性 @https/repository/代码库1/
+ - 第1条的url属性 @https://nexus3dev.qc.oop.cc//repository/代码库1/
 - 按照ID正序，每页5条，取第2页的数据
  - 第1条的name属性 @制品库1
  - 第1条的products属性 @1
  - 第1条的serverID属性 @1
  - 第1条的repoName属性 @代码库1
  - 第1条的pipelineID属性 @1
- - 第1条的url属性 @https/repository/代码库1/
+ - 第1条的url属性 @https://nexus3dev.qc.oop.cc//repository/代码库1/
 - 按照ID正序，每页10条，取第2页的数据
  - 第1条的name属性 @制品库1
  - 第1条的products属性 @1
  - 第1条的serverID属性 @1
  - 第1条的repoName属性 @代码库1
  - 第1条的pipelineID属性 @1
- - 第1条的url属性 @https/repository/代码库1/
+ - 第1条的url属性 @https://nexus3dev.qc.oop.cc//repository/代码库1/
 - 按照ID正序，每页20条，取第2页的数据
  - 第1条的name属性 @制品库1
  - 第1条的products属性 @1
  - 第1条的serverID属性 @1
  - 第1条的repoName属性 @代码库1
  - 第1条的pipelineID属性 @1
- - 第1条的url属性 @https/repository/代码库1/
+ - 第1条的url属性 @https://nexus3dev.qc.oop.cc//repository/代码库1/
 
 */
 
@@ -110,9 +110,9 @@ r($artifactrepoTester->getListTest($sorts[0], $recPerPages[2], $pageIds[0])) && 
 r($artifactrepoTester->getListTest($sorts[0], $recPerPages[0], $pageIds[1])) && p('20:name,products,serverID,repoName,pipelineID,url') && e('制品库20,5,2,代码库20,2,https://gitlabdev.qc.oop.cc//repository/代码库20/'); // 按照ID倒序，每页5条，取第2页的数据
 r($artifactrepoTester->getListTest($sorts[0], $recPerPages[1], $pageIds[1])) && p('20:name,products,serverID,repoName,pipelineID,url') && e('制品库20,5,2,代码库20,2,https://gitlabdev.qc.oop.cc//repository/代码库20/'); // 按照ID倒序，每页10条，取第2页的数据
 r($artifactrepoTester->getListTest($sorts[0], $recPerPages[2], $pageIds[1])) && p('20:name,products,serverID,repoName,pipelineID,url') && e('制品库20,5,2,代码库20,2,https://gitlabdev.qc.oop.cc//repository/代码库20/'); // 按照ID倒序，每页20条，取第2页的数据
-r($artifactrepoTester->getListTest($sorts[1], $recPerPages[0], $pageIds[0])) && p('1:name,products,serverID,repoName,pipelineID,url')  && e('制品库1,1,1,代码库1,1,https/repository/代码库1/');                           // 按照ID正序，每页5条，取第1页的数据
-r($artifactrepoTester->getListTest($sorts[1], $recPerPages[1], $pageIds[0])) && p('1:name,products,serverID,repoName,pipelineID,url')  && e('制品库1,1,1,代码库1,1,https/repository/代码库1/');                           // 按照ID正序，每页10条，取第1页的数据
-r($artifactrepoTester->getListTest($sorts[1], $recPerPages[2], $pageIds[0])) && p('1:name,products,serverID,repoName,pipelineID,url')  && e('制品库1,1,1,代码库1,1,https/repository/代码库1/');                           // 按照ID正序，每页20条，取第1页的数据
-r($artifactrepoTester->getListTest($sorts[1], $recPerPages[0], $pageIds[1])) && p('1:name,products,serverID,repoName,pipelineID,url')  && e('制品库1,1,1,代码库1,1,https/repository/代码库1/');                           // 按照ID正序，每页5条，取第2页的数据
-r($artifactrepoTester->getListTest($sorts[1], $recPerPages[1], $pageIds[1])) && p('1:name,products,serverID,repoName,pipelineID,url')  && e('制品库1,1,1,代码库1,1,https/repository/代码库1/');                           // 按照ID正序，每页10条，取第2页的数据
-r($artifactrepoTester->getListTest($sorts[1], $recPerPages[2], $pageIds[1])) && p('1:name,products,serverID,repoName,pipelineID,url')  && e('制品库1,1,1,代码库1,1,https/repository/代码库1/');                           // 按照ID正序，每页20条，取第2页的数据
+r($artifactrepoTester->getListTest($sorts[1], $recPerPages[0], $pageIds[0])) && p('1:name,products,serverID,repoName,pipelineID,url')  && e('制品库1,1,1,代码库1,1,https://nexus3dev.qc.oop.cc//repository/代码库1/');    // 按照ID正序，每页5条，取第1页的数据
+r($artifactrepoTester->getListTest($sorts[1], $recPerPages[1], $pageIds[0])) && p('1:name,products,serverID,repoName,pipelineID,url')  && e('制品库1,1,1,代码库1,1,https://nexus3dev.qc.oop.cc//repository/代码库1/');    // 按照ID正序，每页10条，取第1页的数据
+r($artifactrepoTester->getListTest($sorts[1], $recPerPages[2], $pageIds[0])) && p('1:name,products,serverID,repoName,pipelineID,url')  && e('制品库1,1,1,代码库1,1,https://nexus3dev.qc.oop.cc//repository/代码库1/');    // 按照ID正序，每页20条，取第1页的数据
+r($artifactrepoTester->getListTest($sorts[1], $recPerPages[0], $pageIds[1])) && p('1:name,products,serverID,repoName,pipelineID,url')  && e('制品库1,1,1,代码库1,1,https://nexus3dev.qc.oop.cc//repository/代码库1/');    // 按照ID正序，每页5条，取第2页的数据
+r($artifactrepoTester->getListTest($sorts[1], $recPerPages[1], $pageIds[1])) && p('1:name,products,serverID,repoName,pipelineID,url')  && e('制品库1,1,1,代码库1,1,https://nexus3dev.qc.oop.cc//repository/代码库1/');    // 按照ID正序，每页10条，取第2页的数据
+r($artifactrepoTester->getListTest($sorts[1], $recPerPages[2], $pageIds[1])) && p('1:name,products,serverID,repoName,pipelineID,url')  && e('制品库1,1,1,代码库1,1,https://nexus3dev.qc.oop.cc//repository/代码库1/');    // 按照ID正序，每页20条，取第2页的数据
