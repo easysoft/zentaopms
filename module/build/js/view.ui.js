c3595b5caa8e348c61be4710e22dfd4dbfd69910
tianshujie <tianshujie@easycorp.ltd>
2023-12-26 19:55:00 +0800

    * Fix bug #42304.

--- module/build/js/view.ui.js
+++ module/build/js/view.ui.js
@@ -46,7 +46,7 @@ window.onSearchLinks = function(type, result)
     return false;
 };
 
-$(document).on('click', '.linkObjectBtn', function()
+$(document).off('click', '.linkObjectBtn').on('click', '.linkObjectBtn', function()
 {
     const $this       = $(this);
     const type        = $this.data('type');
