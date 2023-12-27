4f371fafe1ca735a324abb1cbb5caf63a62d289a
wangyuting <wangyuting@easycorp.ltd>
2023-12-27 14:25:42 +0800

    * Fix dept updateorder unit test bug.

--- module/dept/test/model/updateorder.php
+++ module/dept/test/model/updateorder.php
@@ -2,6 +2,8 @@
 <?php
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/dept.class.php';
+
+zdTable('dept')->gen(20);
 su('admin');
 
 /**
@@ -10,11 +12,11 @@ title=测试 deptModel->updateOrder();
 cid=1
 pid=1
 
-修改部门排序 >> 17
+修改部门排序 >> 1
 
 */
 
 $orders = array('17');
 
 $dept = new deptTest();
-r($dept->updateOrderTest($orders)) && p('17:order') && e('17'); //修改部门排序
+r($dept->updateOrderTest($orders)) && p('17:order') && e('1'); //修改部门排序
