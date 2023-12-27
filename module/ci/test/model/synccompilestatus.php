590f02b160bd13a88fe8cd48c38bb454c9769bdc
caoyanyi <caoyanyi@easycorp.ltd>
2023-12-27 19:21:59 +0800

    * Adjust unit test check rule for devops.

--- module/ci/test/model/synccompilestatus.php
+++ module/ci/test/model/synccompilestatus.php
@@ -7,10 +7,10 @@ title=测试 ciModel->syncCompileStatus();
 timeout=0
 cid=1
 
-- 同步jenkins构建结果属性status @created
+- 同步jenkins构建结果属性status @success
 - 同步gitlab构建结果属性status @failed
 - 三次请求没有拿到结果，构建失败属性status @failure
-- 同步jenkins构建结果，有合并请求ID属性status @created
+- 同步jenkins构建结果，有合并请求ID属性status @success
 - 三次请求没有拿到结果，通知MR失败属性status @failure
 
 */
@@ -25,10 +25,10 @@ su('admin');
 
 $ci = new ciTest();
 
-r($ci->syncCompileStatusTest(1)) && p('status') && e('created'); // 同步jenkins构建结果
+r($ci->syncCompileStatusTest(1)) && p('status') && e('success'); // 同步jenkins构建结果
 r($ci->syncCompileStatusTest(2)) && p('status') && e('failed');  // 同步gitlab构建结果
 
 r($ci->syncCompileStatusTest(4)) && p('status') && e('failure'); // 三次请求没有拿到结果，构建失败
 
-r($ci->syncCompileStatusTest(1, 1)) && p('status') && e('created'); // 同步jenkins构建结果，有合并请求ID
+r($ci->syncCompileStatusTest(1, 1)) && p('status') && e('success'); // 同步jenkins构建结果，有合并请求ID
 r($ci->syncCompileStatusTest(5, 1)) && p('status') && e('failure'); // 三次请求没有拿到结果，通知MR失败
--- module/git/test/model/getrepologs.php
+++ module/git/test/model/getrepologs.php
@@ -10,7 +10,7 @@ cid=1
 - 分支名为空，返回默认分支的日志
  - 属性revision @b362ea7aa65515dc35ff3a93423478b2143e771d
  - 属性msg @Initial commit
-- 分支名为test1，返回空数组属性revision @0
+- 分支名为test1，返回空数组属性revision @~~
 - 分支名为test1000，返回主干的日志
  - 属性revision @b362ea7aa65515dc35ff3a93423478b2143e771d
  - 属性msg @Initial commit
@@ -32,7 +32,7 @@ $result = $git->getRepoLogs($repoID, $branch);
 r(end($result)) && p('revision,msg') && e('b362ea7aa65515dc35ff3a93423478b2143e771d,Initial commit'); // 分支名为空，返回默认分支的日志
 
 $branch = 'test1';
-r($git->getRepoLogs($repoID, $branch)) && p('revision') && e('0'); // 分支名为test1，返回空数组
+r($git->getRepoLogs($repoID, $branch)) && p('revision') && e('~~'); // 分支名为test1，返回空数组
 
 $branch = 'test1000';
 $result = $git->getRepoLogs($repoID, $branch);
