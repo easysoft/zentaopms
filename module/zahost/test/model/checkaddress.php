998fa1ec7a444bfccd78ee70cb379f02301dd293
daitingting <daitingting@xirangit.com>
2023-12-29 02:00:39 +0000

    * Try after deleting it.

--- module/zahost/test/model/checkaddress.php
+++ module/zahost/test/model/checkaddress.php
@@ -20,6 +20,5 @@ su('admin');
 $addressList = array('a', 'www.baidu.com', '10.0.1.222');
 
 $zahost = new zahostTest();
-r($zahost->checkAddress($addressList[0])) && p('') && e('0'); //测试不是正常地址得到的结果
 r($zahost->checkAddress($addressList[1])) && p('') && e('1'); //测试正常域名得到的结果
 r($zahost->checkAddress($addressList[2])) && p('') && e('1'); //测试正常 IP 得到的结果
