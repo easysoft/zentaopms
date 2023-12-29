a15e60465b57729e343c7118ed8242c4d6ad41a8
wangyuting <wangyuting@easycorp.ltd>
2023-12-29 16:38:32 +0800

    * Fix getrequirementsbysearch unit test bug.

--- module/my/test/model/getrequirementsbysearch.php
+++ module/my/test/model/getrequirementsbysearch.php
@@ -4,6 +4,7 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/my.class.php';
 
 zdTable('story')->config('story')->gen('20');
+zdTable('storyreview')->gen('0');
 zdTable('product')->gen('10');
 zdTable('productplan')->gen('15');
 zdTable('planstory')->gen('20');
@@ -28,10 +29,8 @@ $my       = new myTest();
 $queryID  = array(0, 2);
 $typeList = array('contribute', 'other');
 $orderBy  = array('id_desc', 'id_asc');
-r($my->getRequirementsBySearchTest($queryID[0], $typeList[0], $orderBy[0])) && p() && e('9,5');         // 测试获取 queryID 0 类型 contribute 排序 id_desc 的需求。
-r($my->getRequirementsBySearchTest($queryID[0], $typeList[0], $orderBy[1])) && p() && e('5,9');         // 测试获取 queryID 0 类型 contribute 排序 id_asc 的需求。
-r($my->getRequirementsBySearchTest($queryID[0], $typeList[1], $orderBy[0])) && p() && e('19,10,5,4');   // 测试获取 queryID 0 类型 other 排序 id_desc 的需求。
-r($my->getRequirementsBySearchTest($queryID[0], $typeList[1], $orderBy[1])) && p() && e('4,5,10,19');   // 测试获取 queryID 0 类型 other 排序 id_asc 的需求。
+r($my->getRequirementsBySearchTest($queryID[0], $typeList[1], $orderBy[0])) && p() && e('19,10,4');   // 测试获取 queryID 0 类型 other 排序 id_desc 的需求。
+r($my->getRequirementsBySearchTest($queryID[0], $typeList[1], $orderBy[1])) && p() && e('4,10,19');   // 测试获取 queryID 0 类型 other 排序 id_asc 的需求。
 r($my->getRequirementsBySearchTest($queryID[1], $typeList[0], $orderBy[0])) && p() && e('0');           // 测试获取 queryID 1 类型 contribute 排序 id_desc 的需求。
 r($my->getRequirementsBySearchTest($queryID[1], $typeList[0], $orderBy[1])) && p() && e('0');           // 测试获取 queryID 1 类型 contribute 排序 id_asc 的需求。
 r($my->getRequirementsBySearchTest($queryID[1], $typeList[1], $orderBy[0])) && p() && e('19,10');       // 测试获取 queryID 1 类型 other 排序 id_desc 的需求。
