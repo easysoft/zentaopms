7f450527d26bcf61afae90e414eea38bd916c52c
wangyidong <wangyidong@cnezsoft.com>
2023-12-29 14:35:02 +0800

    * Adjust for editorModel save unit test.

--- module/editor/test/model/save.php
+++ module/editor/test/model/save.php
@@ -1,18 +1,20 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . '/test/lib/init.php';
-include dirname(__FILE__, 2) . '/editor.class.php';
-su('admin');
 
 /**
 
 title=测试 editorModel::save();
-cid=1
-pid=1
+cid=0
 
 */
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+include dirname(__FILE__, 2) . '/editor.class.php';
+su('admin');
 
 $editor = new editorTest();
+
+$oldENV = getenv('IS_CONTAINER');
+putenv('IS_CONTAINER=false');
 
 $filePath = $editor->objectModel->app->getTmpRoot() . 'test.php';
 $editor->saveTest($filePath);
@@ -20,7 +22,6 @@ $editor->saveTest($filePath);
 $errors = dao::getError();
 r((int)str_contains($errors[0], '为了安全起见，系统需要确认您的管理员身份')) && p() && e('1'); //不创建OK文件。
 
-$oldENV = getenv('IS_CONTAINER');
 putenv('IS_CONTAINER=true');
 
 $filePath = '/home/test.php';
