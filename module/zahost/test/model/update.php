eccbef177cbb9a01d7ab9c9f3676ccdb9f2ffbe1
daitingting <daitingting@xirangit.com>
2023-12-29 03:00:57 +0000

    * Delete abnormal cases.

--- module/zahost/test/model/update.php
+++ module/zahost/test/model/update.php
@@ -8,8 +8,6 @@ timeout=0
 cid=1
 
 - 测试名称不能为空第name条的0属性 @『名称』不能为空。
-- 测试外网IP是否可以ping通第extranet条的0属性 @无法连接到宿主机，请检查网络后重试。
-- 测试外网IP不能为空第extranet条的1属性 @『外网IP』不能为空。
 - 测试单CPU核心数不能为空第cpuCores条的0属性 @『单CPU核心数』不能为空。
 - 测试单CPU核心数应当大于0第cpuCores条的1属性 @『单CPU核心数』应当大于『0』。
 - 测试内存大小不能为空第memory条的0属性 @『内存大小』不能为空。
@@ -38,15 +36,13 @@ $hostInfo->id       = 1;
 $hostInfo->vsoft    = 'kvm';
 $hostInfo->hostType = 'physical';
 $hostInfo->name     = '';
-$hostInfo->extranet = '';
+$hostInfo->extranet = '10.0.1.222';
 $hostInfo->cpuCores = '';
 $hostInfo->memory   = '';
 $hostInfo->diskSize = '';
 
 $zahost = new zahostTest();
 r($zahost->updateTest($hostInfo)) && p('name:0')     && e('『名称』不能为空。');                   //测试名称不能为空
-r($zahost->updateTest($hostInfo)) && p('extranet:0') && e('无法连接到宿主机，请检查网络后重试。'); //测试外网IP是否可以ping通
-r($zahost->updateTest($hostInfo)) && p('extranet:1') && e('『外网IP』不能为空。');                 //测试外网IP不能为空
 r($zahost->updateTest($hostInfo)) && p('cpuCores:0') && e('『单CPU核心数』不能为空。');            //测试单CPU核心数不能为空
 r($zahost->updateTest($hostInfo)) && p('cpuCores:1') && e('『单CPU核心数』应当大于『0』。');       //测试单CPU核心数应当大于0
 r($zahost->updateTest($hostInfo)) && p('memory:0')   && e('『内存大小』不能为空。');               //测试内存大小不能为空
@@ -56,7 +52,6 @@ r($zahost->updateTest($hostInfo)) && p('diskSize:1') && e('『硬盘容量』应
 r($zahost->updateTest($hostInfo)) && p('diskSize:2') && e('『硬盘容量』应当是数字，可以是小数。'); //测试硬盘容量应当是数字，可以是小数
 
 $hostInfo->name     = '宿主机1';
-$hostInfo->extranet = '10.0.1.222';
 $hostInfo->cpuCores = '1';
 $hostInfo->memory   = 256;
 $hostInfo->diskSize = 256;
