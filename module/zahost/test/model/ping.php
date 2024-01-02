518b637d4fbdcb9273f79b0a4019fe19d60f9f09
daitingting <daitingting@xirangit.com>
2024-01-02 06:18:45 +0000

    * Adjust codes.

--- module/zahost/test/model/ping.php
+++ module/zahost/test/model/ping.php
@@ -20,6 +20,6 @@ su('admin');
 $addressList = array('a', '10.0.0.222', '10.0.1.222');
 
 $zahost = new zahostTest();
-r($zahost->pingTest($addressList[0])) && p('') && e('no');  //测试不是正常地址得到的结果
-r($zahost->pingTest($addressList[1])) && p('') && e('no');  //测试网络不通的地址得到的结果
-r($zahost->pingTest($addressList[2])) && p('') && e('yes'); //测试网络通的地址得到的结果
+r($zahost->pingTest($addressList[0])) && p() && e('no');  //测试不是正常地址得到的结果
+r($zahost->pingTest($addressList[1])) && p() && e('no');  //测试网络不通的地址得到的结果
+r($zahost->pingTest($addressList[2])) && p() && e('yes'); //测试网络通的地址得到的结果
