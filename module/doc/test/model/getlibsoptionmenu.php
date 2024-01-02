cb129ca29dd7d05aa3babd6ace650152890c167a
tianshujie <tianshujie@easycorp.ltd>
2024-01-02 10:12:25 +0800

    * Change steps of the doc::getLibsOptionMenu unit test.

--- module/doc/test/model/getlibsoptionmenu.php
+++ module/doc/test/model/getlibsoptionmenu.php
@@ -1,6 +1,5 @@
 #!/usr/bin/env php
 <?php
-
 /**
 
 title=测试 docModel->getLibsOptionMenu();
@@ -9,6 +8,9 @@ cid=1
 - 测试libs为空 @0
 - 获取文档库的目录属性6_0 @自定义文档库6/
 - 获取接口库的目录属性1_0 @项目接口库1/
+- 测试libs为空的目录数量 @0
+- 获取文档库的目录数量 @7
+- 获取接口库的目录数量 @10
 
 */
 
@@ -29,3 +31,7 @@ $docTester = new docTest();
 r($docTester->getLibsOptionMenuTest($libs[0], $docTypes[0])) && p()      && e('0');              // 测试libs为空
 r($docTester->getLibsOptionMenuTest($libs[1], $docTypes[0])) && p('6_0') && e('自定义文档库6/'); // 获取文档库的目录
 r($docTester->getLibsOptionMenuTest($libs[2], $docTypes[1])) && p('1_0') && e('项目接口库1/');   // 获取接口库的目录
+
+r(count($docTester->getLibsOptionMenuTest($libs[0], $docTypes[0]))) && p() && e('0');  // 测试libs为空的目录数量
+r(count($docTester->getLibsOptionMenuTest($libs[1], $docTypes[0]))) && p() && e('7');  // 获取文档库的目录数量
+r(count($docTester->getLibsOptionMenuTest($libs[2], $docTypes[1]))) && p() && e('10'); // 获取接口库的目录数量
