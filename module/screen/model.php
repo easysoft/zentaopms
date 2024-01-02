10c35687848389f0e458edc7e9bcfff89685a379
songchenxuan <songchenxuan@easycorp.ltd>
2024-01-02 15:01:00 +0800

    * Delete useless code.

--- module/screen/model.php
+++ module/screen/model.php
@@ -1658,7 +1658,7 @@ class screenModel extends model
     {
         $chartOption = $this->metric->getEchartsOptions($resultHeader, $resultData);
 
-        if(!isset($chartOption['title'])) $chartOption['title'] = array('text' => $metric->name, 'show' => false, 'titleShow' => false, 'textStyle' => array('color' => '#BFBFBF', 'fontSize' => 18));
+        if(!isset($chartOption['title'])) $chartOption['title'] = array('text' => $metric->name, 'textStyle' => array('color' => '#BFBFBF'));
         $chartOption['title']['text'] = $metric->name;
         $chartOption['backgroundColor'] = "#0B1727FF";
 
