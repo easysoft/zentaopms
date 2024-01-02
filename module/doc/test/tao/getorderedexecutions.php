3cc9c8806c79b2ec5ed00a056d2f93d1b49adc51
tianshujie <tianshujie@easycorp.ltd>
2024-01-02 14:05:57 +0800

    * Change steps of the doc::getOrderedExecutions unit test.

--- module/doc/test/tao/getorderedexecutions.php
+++ module/doc/test/tao/getorderedexecutions.php
@@ -3,8 +3,12 @@
 /**
 
 title=测试 docModel->getOrderedExecutions();
-timeout=0
 cid=1
+
+- 获取系统中已排序的执行第0条的102属性 @敏捷项目1 / 迭代6
+- 获取系统中包括ID=101已排序的执行第0条的102属性 @敏捷项目1 / 迭代6
+- 获取系统中已排序的执行数量 @3
+- 获取系统中包括ID=101已排序的执行数量 @3
 
 */
 
@@ -20,3 +24,6 @@ $appends = array(0, 101);
 $docTester = new docTest();
 r($docTester->getOrderedExecutionsTest($appends[0])) && p('0:102') && e('敏捷项目1 / 迭代6'); // 获取系统中已排序的执行
 r($docTester->getOrderedExecutionsTest($appends[1])) && p('0:102') && e('敏捷项目1 / 迭代6'); // 获取系统中包括ID=101已排序的执行
+
+r(count($docTester->getOrderedExecutionsTest($appends[0]))) && p() && e('3'); // 获取系统中已排序的执行数量
+r(count($docTester->getOrderedExecutionsTest($appends[1]))) && p() && e('3'); // 获取系统中包括ID=101已排序的执行数量
