6fc3ba006e0c72988fbb7b109a01953973adce83
liumengyi <liumengyi@easycorp.ltd>
2024-01-02 09:51:07 +0800

    * Modify unit test results in my.

--- module/my/test/model/getstoriesbysearch.php
+++ module/my/test/model/getstoriesbysearch.php
@@ -11,9 +11,9 @@ cid=1
 
 - 测试获取 queryID 0 类型 contribute 排序 id_asc 的需求。 @1,2,3,6,7,8,13,17
 
-- 测试获取 queryID 0 类型 other 排序 id_desc 的需求。 @16,13,7,1
+- 测试获取 queryID 0 类型 other 排序 id_desc 的需求。 @16,13,8,7,1
 
-- 测试获取 queryID 0 类型 other 排序 id_asc 的需求。 @1,7,13,16
+- 测试获取 queryID 0 类型 other 排序 id_asc 的需求。 @1,7,8,13,16
 
 - 测试获取 queryID 1 类型 contribute 排序 id_desc 的需求。 @17,13,1
 
@@ -44,8 +44,8 @@ $orderBy  = array('id_desc', 'id_asc');
 
 r($my->getStoriesBySearchTest($queryID[0], $typeList[0], $orderBy[0])) && p() && e('17,13,8,7,6,3,2,1'); // 测试获取 queryID 0 类型 contribute 排序 id_desc 的需求。
 r($my->getStoriesBySearchTest($queryID[0], $typeList[0], $orderBy[1])) && p() && e('1,2,3,6,7,8,13,17'); // 测试获取 queryID 0 类型 contribute 排序 id_asc 的需求。
-r($my->getStoriesBySearchTest($queryID[0], $typeList[1], $orderBy[0])) && p() && e('16,13,7,1');         // 测试获取 queryID 0 类型 other 排序 id_desc 的需求。
-r($my->getStoriesBySearchTest($queryID[0], $typeList[1], $orderBy[1])) && p() && e('1,7,13,16');         // 测试获取 queryID 0 类型 other 排序 id_asc 的需求。
+r($my->getStoriesBySearchTest($queryID[0], $typeList[1], $orderBy[0])) && p() && e('16,13,8,7,1');         // 测试获取 queryID 0 类型 other 排序 id_desc 的需求。
+r($my->getStoriesBySearchTest($queryID[0], $typeList[1], $orderBy[1])) && p() && e('1,7,8,13,16');         // 测试获取 queryID 0 类型 other 排序 id_asc 的需求。
 r($my->getStoriesBySearchTest($queryID[1], $typeList[0], $orderBy[0])) && p() && e('17,13,1');           // 测试获取 queryID 1 类型 contribute 排序 id_desc 的需求。
 r($my->getStoriesBySearchTest($queryID[1], $typeList[0], $orderBy[1])) && p() && e('1,13,17');           // 测试获取 queryID 1 类型 contribute 排序 id_asc 的需求。
 r($my->getStoriesBySearchTest($queryID[1], $typeList[1], $orderBy[0])) && p() && e('16,13,1');           // 测试获取 queryID 1 类型 other 排序 id_desc 的需求。
--- module/my/test/model/setmenu.php
+++ module/my/test/model/setmenu.php
@@ -7,13 +7,13 @@ declare(strict_types=1);
 title=测试 myModel->getActions();
 cid=1
 
-- 获取 admin 角色 空 的地盘菜单 @5:index,10:calendar,15:work,20:audit,25:project,30:execution,35:contribute,40:dynamic,45:score,50:contacts
+- 获取 admin 角色 空 的地盘菜单 @5:index,10:calendar,15:work,20:audit,25:project,30:execution,35:contribute,40:dynamic,50:contacts
 
-- 获取 user1 角色 qa 的地盘菜单 @5:index,10:calendar,20:audit,25:project,30:execution,35:contribute,40:dynamic,45:score,50:contacts,32:task
+- 获取 user1 角色 qa 的地盘菜单 @5:index,10:calendar,20:audit,25:project,30:execution,35:contribute,40:dynamic,50:contacts,32:task
 
-- 获取 user2 po 的地盘菜单 @5:index,10:calendar,20:audit,25:project,30:task,35:contribute,40:dynamic,45:score,50:contacts,32:task,15:story,16:requirement
+- 获取 user2 po 的地盘菜单 @5:index,10:calendar,20:audit,25:project,30:task,35:contribute,40:dynamic,50:contacts,32:task,15:story,16:requirement
 
-- 获取 user3 pm 的地盘菜单 @5:index,10:calendar,20:audit,25:project,30:task,40:dynamic,45:score,50:contacts,32:task,15:story,16:requirement,17:myProject
+- 获取 user3 pm 的地盘菜单 @5:index,10:calendar,20:audit,25:project,30:task,40:dynamic,50:contacts,32:task,15:story,16:requirement,17:myProject
 
 */
 
@@ -25,7 +25,7 @@ $my = new myTest();
 
 $accountList = array('admin', 'user1', 'user2', 'user3');
 
-r($my->setMenuTest($accountList[0])) && p() && e('5:index,10:calendar,15:work,20:audit,25:project,30:execution,35:contribute,40:dynamic,45:score,50:contacts');                    // 获取 admin 角色 空 的地盘菜单
-r($my->setMenuTest($accountList[1])) && p() && e('5:index,10:calendar,20:audit,25:project,30:execution,35:contribute,40:dynamic,45:score,50:contacts,32:task');                    // 获取 user1 角色 qa 的地盘菜单
-r($my->setMenuTest($accountList[2])) && p() && e('5:index,10:calendar,20:audit,25:project,30:task,35:contribute,40:dynamic,45:score,50:contacts,32:task,15:story,16:requirement'); // 获取 user2 po 的地盘菜单
-r($my->setMenuTest($accountList[3])) && p() && e('5:index,10:calendar,20:audit,25:project,30:task,40:dynamic,45:score,50:contacts,32:task,15:story,16:requirement,17:myProject');  // 获取 user3 pm 的地盘菜单
+r($my->setMenuTest($accountList[0])) && p() && e('5:index,10:calendar,15:work,20:audit,25:project,30:execution,35:contribute,40:dynamic,50:contacts');                    // 获取 admin 角色 空 的地盘菜单
+r($my->setMenuTest($accountList[1])) && p() && e('5:index,10:calendar,20:audit,25:project,30:execution,35:contribute,40:dynamic,50:contacts,32:task');                    // 获取 user1 角色 qa 的地盘菜单
+r($my->setMenuTest($accountList[2])) && p() && e('5:index,10:calendar,20:audit,25:project,30:task,35:contribute,40:dynamic,50:contacts,32:task,15:story,16:requirement'); // 获取 user2 po 的地盘菜单
+r($my->setMenuTest($accountList[3])) && p() && e('5:index,10:calendar,20:audit,25:project,30:task,40:dynamic,50:contacts,32:task,15:story,16:requirement,17:myProject');  // 获取 user3 pm 的地盘菜单
