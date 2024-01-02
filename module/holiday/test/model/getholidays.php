41dfabd792554c6f2d95b8c49000ba811f6dae42
xieqiyu <xieqiyu@easycorp.ltd>
2024-01-02 10:10:25 +0800

    * Modify the test file of getHolidays.

--- module/holiday/test/model/getholidays.php
+++ module/holiday/test/model/getholidays.php
@@ -1,5 +1,18 @@
 #!/usr/bin/env php
 <?php
+/**
+
+title=测试 holidayModel->getHolidays();
+cid=1
+
+- 返回2024年1月1日 14天前 到 7天后 之间的节假日。 @16
+- 返回2024年1月1日 14天前 到 14天后 之间的节假日。 @16
+- 返回 2024年1月1日 7天前 到 7天后 之间的节假日。 @8
+- 返回 2024年1月1日 7天前 到 14天后 之间的节假日。 @8
+- 测试当结束日期小于开始日期时。 @0
+- 测试当数据库没有节假日记录时。 @0
+
+*/
 declare(strict_types=1);
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/holiday.class.php';
@@ -9,21 +22,13 @@ zdTable('user')->gen(1);
 
 su('admin');
 
-/**
-
-title=测试 holidayModel->getHolidays();
-cid=1
-pid=1
-
-*/
-
 $holiday = new holidayTest();
-$begin   = array('-14 day', '-7 day', '+4 day');
-$end     = array('+7 day', '+14 day', '+6day');
+$begin   = array('2023-12-18', '2023-12-25', '2024-01-05');
+$end     = array('2024-01-08', '2024-01-15', '2024-01-07');
 
-r($holiday->getHolidaysTest($begin[0], $end[0])) && p() && e('15'); //返回处于 14天前 到 7天后 之间的节假日。
-r($holiday->getHolidaysTest($begin[0], $end[1])) && p() && e('15'); //返回处于 14天前 到 14天后 之间的节假日。
-r($holiday->getHolidaysTest($begin[1], $end[0])) && p() && e('7'); //返回处于 7天前 到 7天后 之间的节假日。
-r($holiday->getHolidaysTest($begin[1], $end[1])) && p() && e('7'); //返回处于 7天前 到 14天后 之间的节假日。
+r($holiday->getHolidaysTest($begin[0], $end[0])) && p() && e('16'); //返回2024年1月1日 14天前 到 7天后 之间的节假日。
+r($holiday->getHolidaysTest($begin[0], $end[1])) && p() && e('16'); //返回2024年1月1日 14天前 到 14天后 之间的节假日。
+r($holiday->getHolidaysTest($begin[1], $end[0])) && p() && e('8'); //返回 2024年1月1日 7天前 到 7天后 之间的节假日。
+r($holiday->getHolidaysTest($begin[1], $end[1])) && p() && e('8'); //返回 2024年1月1日 7天前 到 14天后 之间的节假日。
 r($holiday->getHolidaysTest($begin[2], $end[0])) && p() && e('0'); //测试当结束日期小于开始日期时。
 r($holiday->getHolidaysTest($begin[2], $end[2])) && p() && e('0'); //测试当数据库没有节假日记录时。
