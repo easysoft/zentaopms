a024c78b90c239509814c9b3ea3999a78ecf51ab
tianshujie <tianshujie@easycorp.ltd>
2024-01-02 10:06:47 +0800

    * Change steps of the doc::getExecutionModulePairs unit test.

--- module/doc/test/model/getexecutionmodulepairs.php
+++ module/doc/test/model/getexecutionmodulepairs.php
@@ -1,13 +1,15 @@
 #!/usr/bin/env php
 <?php
-
 /**
 
 title=测试 docModel->getExecutionModulePairs();
 cid=1
 
 - 测试系统里有执行文档库数据和模块数据的情况属性1 @这是一个模块1
+- 测试系统里有执行文档库数据和模块数据的情况属性29 @这是一个模块29
+- 测试系统里有执行文档库数据和模块数量 @8
 - 测试系统里没有执行库文档数据的情况 @0
+- 测试系统里没有执行库文档数量 @0
 
 */
 
@@ -23,5 +25,9 @@ $moduleTable->type->range('doc,task,bug,story');
 $moduleTable->gen(30);
 
 $docTester = new docTest();
-r($docTester->getExecutionModulePairsTest('normal')) && p('1') && e('这是一个模块1'); // 测试系统里有执行文档库数据和模块数据的情况
-r($docTester->getExecutionModulePairsTest('noData')) && p()    && e('0');             // 测试系统里没有执行库文档数据的情况
+r($docTester->getExecutionModulePairsTest('normal'))        && p('1')  && e('这是一个模块1');  // 测试系统里有执行文档库数据和模块数据的情况
+r($docTester->getExecutionModulePairsTest('normal'))        && p('29') && e('这是一个模块29'); // 测试系统里有执行文档库数据和模块数据的情况
+r(count($docTester->getExecutionModulePairsTest('normal'))) && p()     && e('8');              // 测试系统里有执行文档库数据和模块数量
+
+r($docTester->getExecutionModulePairsTest('noData'))        && p() && e('0'); // 测试系统里没有执行库文档数据的情况
+r(count($docTester->getExecutionModulePairsTest('noData'))) && p() && e('0'); // 测试系统里没有执行库文档数量
