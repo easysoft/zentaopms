cbe5112167c84b4841641f364ff0af1b174b645d
tianshujie <tianshujie@easycorp.ltd>
2024-01-02 09:25:48 +0800

    * Change steps of the doc::deleteFiles unit test.

--- module/doc/test/model/deletefiles.php
+++ module/doc/test/model/deletefiles.php
@@ -1,6 +1,5 @@
 #!/usr/bin/env php
 <?php
-
 /**
 
 title=测试 docModel->deleteFiles();
@@ -8,6 +7,8 @@ cid=1
 
 - 测试空数据 @0
 - 测试正常数据属性1 @1
+- 测试有不存在的数据属性1 @1
+- 测试不存在的数据 @0
 
 */
 
@@ -24,7 +25,11 @@ su('admin');
 
 $docIdList[] = array();
 $docIdList[] = range(1, 5);
+$docIdList[] = array(1, 2, 100, 101);
+$docIdList[] = range(100, 105);
 
 $docTester = new docTest();
 r($docTester->deleteFilesTest($docIdList[0])) && p()    && e('0'); // 测试空数据
 r($docTester->deleteFilesTest($docIdList[1])) && p('1') && e('1'); // 测试正常数据
+r($docTester->deleteFilesTest($docIdList[2])) && p('1') && e('1'); // 测试有不存在的数据
+r($docTester->deleteFilesTest($docIdList[3])) && p()    && e('0'); // 测试不存在的数据
