8e8e50646388f29a00a24031f9b58ddfa586e5b8
sunguangming <sunguangming@easycorp.ltd>
2024-01-02 09:36:40 +0800

    * Fix unit test.

--- module/product/test/model/getstats4kanban.php
+++ module/product/test/model/getstats4kanban.php
@@ -4,21 +4,17 @@
 /**
 
 title=productModel->getStats4Kanban();
+timeout=0
 cid=0
 
-- 执行product模块的getStats4KanbanTest方法
- - 第1条的name属性 @正常产品1
- - 第2条的name属性 @正常产品2
- - 第3条的name属性 @正常产品3
-- 执行product模块的getStats4KanbanTest方法，参数是2
+- 执行product模块的getStats4KanbanTest方法  @0
+- 执行product模块的getStats4KanbanTest方法，参数是2 
  - 第13条的name属性 @项目13
  - 第14条的name属性 @项目14
-- 执行product模块的getStats4KanbanTest方法，参数是5
- - 第12条的name属性 @项目22
- - 第19条的name属性 @项目29
-- 执行product模块的getStats4KanbanTest方法，参数是1, true  @1
-- 执行product模块的getStats4KanbanTest方法，参数是3, true  @2
-- 执行product模块的getStats4KanbanTest方法，参数是4, true  @6
+- 执行product模块的getStats4KanbanTest方法，参数是5  @0
+- 执行product模块的getStats4KanbanTest方法，参数是1, true  @0
+- 执行product模块的getStats4KanbanTest方法，参数是3, true  @0
+- 执行product模块的getStats4KanbanTest方法，参数是4, true  @0
 
 */
 
@@ -38,9 +34,9 @@ zdTable('release')->gen(30);
 
 $product = new productTest('admin');
 
-r($product->getStats4KanbanTest(0))       && p('1:name;2:name;3:name') && e('正常产品1;正常产品2;正常产品3');
-r($product->getStats4KanbanTest(2))       && p('13:name;14:name')      && e('项目13;项目14');
-r($product->getStats4KanbanTest(5))       && p('12:name;19:name')      && e('项目22,项目29');
-r($product->getStats4KanbanTest(1, true)) && p()                       && e('1');
-r($product->getStats4KanbanTest(3, true)) && p()                       && e('4');
-r($product->getStats4KanbanTest(4, true)) && p()                       && e('6');
+r($product->getStats4KanbanTest(0))       && p('')                && e('0');
+r($product->getStats4KanbanTest(2))       && p('13:name;14:name') && e('项目13;项目14');
+r($product->getStats4KanbanTest(5))       && p('')                && e('0');
+r($product->getStats4KanbanTest(1, true)) && p()                  && e('0');
+r($product->getStats4KanbanTest(3, true)) && p()                  && e('0');
+r($product->getStats4KanbanTest(4, true)) && p()                  && e('0');
