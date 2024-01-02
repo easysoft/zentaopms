bbc39a7762be15c3d6a9e5832bc2c3a5074ab3dc
wangyidong <wangyidong@cnezsoft.com>
2024-01-02 11:15:33 +0800

    * Adjust getlinkedproducts unit test.

--- module/sonarqube/test/model/getlinkedproducts.php
+++ module/sonarqube/test/model/getlinkedproducts.php
@@ -1,20 +1,20 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . '/test/lib/init.php';
 
 /**
 
 title=测试 sonarqubeModel::getLinkedProducts();
-timeout=0
-cid=1
+cid=0
 
 - 通过错误的sonarqubeID和projectKey获取关联产品ID @return false
 - 通过正确的sonarqubeID和projectKey获取关联产品ID @1
 
 */
+include dirname(__FILE__, 5) . '/test/lib/init.php';
 
 zdTable('pipeline')->config('pipeline')->gen(5);
 zdTable('job')->gen(5);
+zdTable('repo')->gen(5);
 
 $sonarqube = $tester->loadModel('sonarqube');
 
