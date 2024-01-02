f7947a6544247c67c3715846b822da570d20a5ed
caoyanyi <caoyanyi@easycorp.ltd>
2024-01-02 14:29:28 +0800

    + Add unit test case for cne->cneMetrics.

--- module/cne/test/model/cnemetrics.php
+++ module/cne/test/model/cnemetrics.php
@@ -7,13 +7,17 @@ title=测试 cneModel->cneMetrics();
 timeout=0
 cid=1
 
-- 获取CNE平台的集群度量属性status @unknown
+- 获取CNE平台的集群度量属性status @normal
 
 */
 
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 
 global $tester, $config;
+$config->CNE->api->host   = 'http://10.0.7.210:32380';
+$config->CNE->api->token  = 'JMz7HCoQ3WHoYbpNyYNpvMfHqde9ugtV';
+$config->CNE->app->domain = 'dops.corp.cc';
+
 $cneModel = $tester->loadModel('cne');
 
-r($cneModel->cneMetrics()) && p('status') && e('unknown'); // 获取CNE平台的集群度量
+r($cneModel->cneMetrics()) && p('status') && e('normal'); // 获取CNE平台的集群度量
