7e2ff602799fcbe0e7f0627f63b1d949fdf08d44
caoyanyi <caoyanyi@easycorp.ltd>
2024-01-02 17:21:03 +0800

    * Adjust unit test check rule.

--- module/cne/test/model/getapplogs.php
+++ module/cne/test/model/getapplogs.php
@@ -8,9 +8,7 @@ timeout=0
 cid=1
 
 - 获取日志条目数 @32
-- 获取第一条和最后一条日志
- - 第0条的content属性 @+ CONFIG_DIR=/data/config
- - 第31条的content属性 @[Tue Jan 02 14:50:46.867606 2024] [core:notice] [pid 247] AH00094: Command line: '/usr/sbin/httpd -D FOREGROUND'
+- 获取第一条日志第0条的content属性 @+ CONFIG_DIR=/data/config
 
 */
 
@@ -25,4 +23,4 @@ $cneModel = new cneTest();
 
 $logs = $cneModel->getAppLogsTest();
 r(count($logs->data)) && p() && e('32'); // 获取日志条目数
-r($logs->data) && p('0:content;31:content') && e("+ CONFIG_DIR=/data/config,[Tue Jan 02 14:50:46.867606 2024] [core:notice] [pid 247] AH00094: Command line: '/usr/sbin/httpd -D FOREGROUND'"); // 获取第一条和最后一条日志
+r($logs->data) && p('0:content') && e("+ CONFIG_DIR=/data/config"); // 获取第一条日志
