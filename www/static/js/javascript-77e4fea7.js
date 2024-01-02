4a0748d7fc405dffb78df85e81fe22733811cec2
zhouxin <zhouxin@easycorp.ltd>
2024-01-02 16:53:30 +0800

    * Update style of screen designer.

---
+++ www/static/js/javascript-77e4fea7.js
@@ -0,0 +1,6 @@
+import{conf as t,language as e}from"./typescript-7e0119c9.js";import"./index-e9aeeebe.js";import"./index.js";import"./editorWorker-a0599278.js";import"./storage-d71b517e.js";import"./icon-3654dfb2.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var l=t,c={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{l as conf,c as language};
