3615b10e26522c31f99bb9677d2309b3e1c6053a
liumengyi <liumengyi@easycorp.ltd>
2024-01-02 09:16:41 +0800

    * Modify unit test result of myModel::getActions.

--- module/my/test/model/getactions.php
+++ module/my/test/model/getactions.php
@@ -1,6 +1,18 @@
 #!/usr/bin/env php
 <?php
 declare(strict_types=1);
+
+/**
+
+title=测试 myModel->getActions();
+cid=1
+
+- 正常查询action @96,64,32,95,63
+
+- 正常查询action统计 @5
+
+*/
+
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/my.class.php';
 
@@ -9,17 +21,8 @@ zdTable('user')->gen('1');
 
 su('admin');
 
-/**
-
-title=测试 myModel->getActions();
-cid=1
-pid=1
-
-*/
-
 $my = new myTest();
 
-
 $actions = $my->getActionsTest();
-r(implode(',', $actions)) && p() && e('93,62,31,92,61'); // 正常查询action
+r(implode(',', $actions)) && p() && e('96,64,32,95,63'); // 正常查询action
 r(count($actions))        && p() && e('5');              // 正常查询action统计
