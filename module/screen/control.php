683adba762f0196f4a719a7e6f3bca269912a9f7
zhouxin <zhouxin@easycorp.ltd>
2024-01-02 13:45:17 +0800

    * Delete duplicate ajaxgettreedata method.

--- module/screen/control.php
+++ module/screen/control.php
@@ -201,32 +201,6 @@ class screen extends control
     }
 
     /**
-     * Ajax get tree data.
-     *
-     * @access public
-     * @return void
-     */
-    public function ajaxGetTreeData($screenID)
-    {
-        $dimensions  = $this->screen->getDimensionPairs();
-        $screen      = $this->screen->getByID($screenID);
-        $dimensionID = $this->loadModel('dimension')->setSwitcherMenu($screen->dimension);
-
-        foreach($dimensions as $dimension) $dimension->value = (int)$dimension->value;
-
-        $data = new stdclass();
-        $data->chartData   = $screen->chartData;
-        $data->treeData    = $this->screen->getTreeData($dimensions);
-        $data->dimensions  = $dimensions;
-        $data->dimension   = $screen->dimension;
-        $data->scopeList   = $this->loadModel('metric')->getScopePairs(false);
-        $data->scope       = 'system';
-        $data->fieldConfig = $this->screen->getTreeSelectOptions();
-
-        echo(json_encode($data));
-    }
-
-    /**
      * 获取度量数据。
      * Ajax get metric data.
      *
