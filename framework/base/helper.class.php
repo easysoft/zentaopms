db4629560def479483daf3a59cf7bb9089288de0
liugang <liugang@easycorp.ltd>
2024-01-03 19:53:44 +0800

    * baseHelper: convert the variable to string before passing it to the htmlspecialchars function.

--- framework/base/helper.class.php
+++ framework/base/helper.class.php
@@ -1230,7 +1230,7 @@ function isHttps()
 function htmlSpecialString($string, $flags = '', $encoding = 'UTF-8')
 {
     if(!$flags) $flags = defined('ENT_SUBSTITUTE') ? ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML401 : ENT_QUOTES;
-    return htmlspecialchars($string, $flags, $encoding);
+    return htmlspecialchars((string)$string, $flags, $encoding);
 }
 
 if(!function_exists('array_column'))
