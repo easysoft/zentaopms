c4a4d70e5aa41b667291544e3a463775a5117844
wangyuting <wangyuting@easycorp.ltd>
2024-01-03 13:39:32 +0800

    * Fix todo getlist unit test bug.

--- module/todo/test/model/getlist.php
+++ module/todo/test/model/getlist.php
@@ -17,66 +17,39 @@ $typeList = array('today', 'yesterday', 'thisweek', 'lastweek', 'thismonth', 'la
 
 $todo = new todoTest();
 
-zdTable('todo')->config('getlist')->gen(9);
+$thisYearDay = date('L') ? 366 : 365;
+$lastYearDay = date('L', strtotime("-1 years")) ? 366 : 365;
+zdTable('todo')->config('getlist')->gen($thisYearDay + $lastYearDay);
 
 global $tester;
 $tester->loadModel('todo');
 
-$day         = date('Y-m-d'); //今天
-$lastday     = date('Y-m-d', strtotime('-1 day')); //昨天
-$week        = date('Y-m-d', (time() - ((date('w') == 0 ? 7 : date('w')) - 1) * 24 * 3600)); //本周一
-$month       = date('Y-m-01'); //本月1日
-$lastweekday = date('Y-m-d', strtotime('-7 day')); //上周的今天
+$month = date('m');
+$year  = date('Y');
+$thisMonthDay = cal_days_in_month(CAL_GREGORIAN, (int)$month, (int)$year);
 
-$season = ceil((date('n')) / 3); //当月是第几季度
-$season = date('Y-m-d', mktime(0, 0, 0, (int)$season * 3 - 3 + 1, 1, (int)date('Y'))); //本季度第一天
+$month = date('m', strtotime("-1 months"));
+$year  = date('Y', strtotime("-1 months"));
+$lastMonthDay = cal_days_in_month(CAL_GREGORIAN, (int)$month, (int)$year);
 
-$defaultNum  = '0'; //默认天数
-$dayNum      = '1'; //修改天数为今日的数量
-$lastDayNum  = '2'; //修改天数为昨天的数量
-$last7DayNum = '1'; //修改天数为7天前的数量
+$startMonth = ceil(date('n') / 3) * 3 - 2;
+$start      = date("Y-$startMonth-1");
+$end        = date("Y-m-d", strtotime("+3 months", strtotime($start)));
+$thisSseasonDay = (strtotime($end) - strtotime($start)) / (60 * 60 * 24);
 
-r($tester->todo->editDate(array(1),   date('Y-m-d'))) && p() && e('1');  // 修改id为1的待办的日期
-r($tester->todo->editDate(array(2, 3), date('Y-m-d', strtotime('-1 day')))) && p() && e('1');  // 修改id为2,3的待办的日期
-r($tester->todo->editDate(array(4),   $lastweekday)) && p() && e('1');  // 修改id为2,3的待办的日期
+$lastYearToday = strtotime('last year');
+$thisYearToday = strtotime('this year');
 
-/* 本周上周实际待办数量 */
-$thisWeekNum = $dayNum;
-$lastWeekNum = $lastDayNum;
+$beforeDay = (time() - strtotime('-1 years')) / (60 * 60 * 24) + 1;
 
-if(date('w') != 1){
-    $thisWeekNum = $dayNum + $lastDayNum;
-    $lastWeekNum = $defaultNum;
-}
-if(strtotime($week)- 7*86400 <= strtotime($lastweekday)) $lastWeekNum += $last7DayNum;
-
-/* 本月上月实际待办数量 */
-$thisMonthNum = $dayNum;
-$lastMonthNum = strtotime($lastweekday) >= strtotime($month) ? $defaultNum : $last7DayNum;
-if(strtotime($month) <= strtotime($lastday))     $thisMonthNum += $lastDayNum;
-if(strtotime($month) <= strtotime($lastweekday)) $thisMonthNum += $last7DayNum;
-if($month == $day) $lastMonthNum += $lastDayNum;
-
-/* 本季度实际待办数量 */
-$thisSeasonNum = $dayNum;
-if(strtotime($season) <= strtotime($lastday)) $thisSeasonNum += $lastDayNum;
-if(strtotime($season) <= strtotime($lastweekday)) $thisSeasonNum += $lastWeekNum;
-
-$thisweek   = $todo->getListTest($typeList[2]) == $thisWeekNum   ? '1' : '0';
-$lastweek   = $todo->getListTest($typeList[3]) == $lastWeekNum   ? '1' : '0';
-$thismonth  = $todo->getListTest($typeList[4]) == $thisMonthNum  ? '1' : '0';
-$lastmonth  = $todo->getListTest($typeList[5]) == $lastMonthNum  ? '1' : '0';
-$thisseason = $todo->getListTest($typeList[6]) == $thisSeasonNum ? '1' : '0';
-
-r($thisweek)   && p() && e('1'); // 获取type为thisweek   当前用户的待办数量
-r($lastweek)   && p() && e('1'); // 获取type为lastweek   当前用户的待办数量
-r($thismonth)  && p() && e('1'); // 获取type为thismonth  当前用户的待办数量
-r($lastmonth)  && p() && e('1'); // 获取type为lastmonth  当前用户的待办数量
-r($thisseason) && p() && e('1'); // 获取type为thisseason 当前用户的待办数量
-
-r($todo->getListTest($typeList[0]))  && p() && e('1'); // 获取type为today 当前用户的待办数量
-r($todo->getListTest($typeList[1]))  && p() && e('2'); // 获取type为yesterday 当前用户的待办数量
-r($todo->getListTest($typeList[7]))  && p() && e('9'); // 获取type为thisyear 当前用户的待办数量
-r($todo->getListTest($typeList[8]))  && p() && e('0'); // 获取type为future 当前用户的待办数量
-r($todo->getListTest($typeList[9]))  && p() && e('9'); // 获取type为before 当前用户的待办数量
-r($todo->getListTest($typeList[10])) && p() && e('0'); // 获取type为cycle 当前用户的待办数量
+r($todo->getListTest($typeList[0]))                    && p() && e('1'); // 获取当前用户今天的待办数量
+r($todo->getListTest($typeList[1]))                    && p() && e('1'); // 获取当前用户昨天的待办数量
+r($todo->getListTest($typeList[2]))                    && p() && e('7'); // 获取当前用户本周的待办数量
+r($todo->getListTest($typeList[3]))                    && p() && e('7'); // 获取当前用户上周的待办数量
+r($todo->getListTest($typeList[4]) == $thisMonthDay)   && p() && e('1'); // 获取当前用户本月的待办数量
+r($todo->getListTest($typeList[5]) == $lastMonthDay)   && p() && e('1'); // 获取当前用户上月的待办数量
+r($todo->getListTest($typeList[6]) == $thisSseasonDay) && p() && e('1'); // 获取当前用户本季度的待办数量
+r($todo->getListTest($typeList[7]) == $thisYearDay)    && p() && e('1'); // 获取当前用户本年的待办数量
+r($todo->getListTest($typeList[8]))                    && p() && e('0'); // 获取当前用户待定的待办数量
+r($todo->getListTest($typeList[9]) == $beforeDay)      && p() && e('1'); // 获取当前用户今天之前的待办数量
+r($todo->getListTest($typeList[10]))                   && p() && e('0'); // 获取当前用户周期性待办数量
