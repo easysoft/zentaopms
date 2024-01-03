02843ee44600cc4cd2b9809de80c388ddd4d79bc
chaideqing <chaideqing@easycorp.ltd>
2024-01-03 17:21:50 +0800

    * Fix bug #43058.

--- module/execution/control.php
+++ module/execution/control.php
@@ -3541,7 +3541,7 @@ class execution extends control
         }
         else
         {
-            $status = $storyType == 'story' ? 'active' : 'launched';
+            $status     = $storyType == 'story' ? 'active' : ($model == 'ipd' ? 'launched' : 'active,launched');
             $allStories = $this->story->getProductStories(array_keys($products), $branchIDList, $moduleID = '0', $status, $storyType, 'id_desc', $hasParent = false, '', $pager = null);
         }
 
