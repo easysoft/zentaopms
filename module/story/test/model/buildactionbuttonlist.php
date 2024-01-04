2655f871309a9e357c483f8872837ae9069e8093
xieqiyu <xieqiyu@easycorp.ltd>
2024-01-04 20:39:35 +0800

    * Modify the test file of story.

--- module/story/test/model/buildactionbuttonlist.php
+++ module/story/test/model/buildactionbuttonlist.php
@@ -39,13 +39,13 @@ $execution->multiple = 0;
 $tester->story->app->tab = 'execution';
 $storyOperateMenu2 = $tester->story->buildactionbuttonlist($story, 'browse', $execution, 'story');
 
-r($requirementOperateMenu1) && p('0:name,hint,disabled')   && e('change,只有激活状态的研发需求，才能进行变更,1');             //查看用户需求列表操作菜单第一个链接
+r($requirementOperateMenu1) && p('0:name,hint,disabled')   && e('change,只有激活状态的用户需求，才能进行变更,1');             //查看用户需求列表操作菜单第一个链接
 r($requirementOperateMenu1) && p('1:name,data-toggle,url') && e('submitreview,modal,/story-submitReview-1-requirement.html'); //查看用户需求列表操作菜单第二个链接
 r($requirementOperateMenu1) && p('3:name,data-toggle,url') && e('close,modal,/story-close-1--requirement.html');              //查看用户需求列表操作菜单第四个链接
 r($requirementOperateMenu1) && p('5:name,url')             && e('edit,/story-edit-1-default-requirement.html');               //查看用户需求列表操作菜单第六个链接
 
 r($storyOperateMenu1)       && p('0:name,hint,url')         && e('change,变更,/story-change-2--story.html');                  //查看软件需求详情操作菜单第一个链接
-r($storyOperateMenu1)       && p('1:name,hint,disabled')    && e('review,该研发需求已是激活状态，无需评审,1');                //查看软件需求详情操作菜单第二个链接
+r($storyOperateMenu1)       && p('1:name,hint,disabled')    && e('review,该用户需求已是激活状态，无需评审,1');                //查看软件需求详情操作菜单第二个链接
 r($storyOperateMenu1)       && p('3:name,data-toggle,url')  && e('close,modal,/story-close-2--story.html');                   //查看软件需求详情操作菜单第四个链接
 r($storyOperateMenu1)       && p('5:name,url')              && e('edit,/story-edit-2-default-story.html');                    //查看软件需求详情操作菜单第六个链接
 r($storyOperateMenu1)       && p('6:name,url')              && e('testcase,/testcase-create-1-0-0--0-2.html');                //查看软件需求详情操作菜单第七个链接
