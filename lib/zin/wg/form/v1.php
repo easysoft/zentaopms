fbe87ef23ff81d7aa5bf31a1e94c4c9344c285bb
sunhao <sunhao@easycorp.ltd>
2024-01-05 18:21:00 +0800

    * zin: support to use grid layout in form.

--- lib/zin/wg/form/v1.php
+++ lib/zin/wg/form/v1.php
@@ -7,38 +7,71 @@ require_once dirname(__DIR__) . DS . 'formrow' . DS . 'v1.php';
 require_once dirname(__DIR__) . DS . 'formbase' . DS . 'v1.php';
 
 /**
- * 通用表单（form）部件类，支持 Ajax 提交
- * The common form widget class
+ * 通用表单（form）部件类，支持 Ajax 提交。
+ * The common form widget class.
  */
 class form extends formBase
 {
-    protected static array $defineProps = array(
-        'items?: array',    // 使用一个列定义对象数组来定义表单项。
-        'grid?: bool=true', // 是否启用网格部件，禅道中所有表单都是网格布局，除非有特殊目的，无需设置此项。
-        'labelWidth?: int', // 标签宽度，单位为像素。
-        'actionsClass?: string="form-group no-label"', // 操作按钮栏的 CSS 类。
-        'submitBtnText?: string' // 提交按钮文本。
+    protected static array $defineProps = array
+    (
+        'items?: array',            // 使用一个列定义对象数组来定义表单项。
+        'layout?: string="horz"',   // 表单布局，可选值为：'horz'、'grid' 和 `normal`。
+        'labelWidth?: int',         // 标签宽度，单位为像素。
+        'submitBtnText?: string',   // 提交按钮文本。
+        'requiredFields?: string',  // 必填项定义。
+        'data?: array|object',      // 表单项值默认数据。
+        'labelData?: array|object', // 表单项标签默认数据。
+        'actionsClass?: string="form-group no-label"' // 操作按钮栏的 CSS 类。
     );
+
+    protected $layout = null;
+
+    protected function isLayout($layout)
+    {
+        if(is_null($this->layout)) $this->layout = $this->prop('layout');
+        return $this->layout == $layout;
+    }
 
     protected function created()
     {
         parent::created();
 
-        if(!isAjaxRequest('modal')) return;
-
         global $app, $lang;
         $module = $app->getModuleName();
         $method = $app->getMethodName();
-        $text   = !empty($lang->$module->$method) ? $lang->$module->$method : zget($lang, $method, '');
+        if(isAjaxRequest('modal'))
+        {
+            $text   = !empty($lang->$module->$method) ? $lang->$module->$method : zget($lang, $method, '');
 
-        $defaultProps = array();
-        $defaultProps['submitBtnText'] = $text;
-        $defaultProps['class']         = 'px-3 pb-4';
+            $defaultProps = array();
+            $defaultProps['submitBtnText'] = $text;
+            $defaultProps['class']         = 'px-3 pb-4';
+            $this->setDefaultProps($defaultProps);
+        }
 
-        $this->setDefaultProps($defaultProps);
+        if(!$this->hasProp('data'))      $this->setProp('data',      data($module));
+        if(!$this->hasProp('labelData')) $this->setProp('labelData', $lang->$module);
     }
 
-    public function onBuildItem(item|array $item): wg
+    protected function getItemLabel(string $name): ?string
+    {
+        $labelData = $this->prop('labelData');
+        $lblName   = 'lbl' . ucfirst($name);
+
+        if(is_array($labelData))  return isset($labelData[$lblName]) ? $labelData[$lblName] : (isset($labelData[$name]) ? $labelData[$name] : null);
+        if(is_object($labelData)) return isset($labelData->$lblName) ? $labelData->$lblName : (isset($labelData->$name) ? $labelData->$name : null);
+        return null;
+    }
+
+    protected function getItemValue(string $name): ?string
+    {
+        $data = $this->prop('data');
+        if(is_array($data))  return isset($data[$name]) ? strval($data[$name]) : null;
+        if(is_object($data)) return isset($data->$name) ? strval($data->$name) : null;
+        return null;
+    }
+
+    public function onBuildItem(item|array $item, string|int $key): wg
     {
         if(!($item instanceof item))
         {
@@ -46,7 +79,40 @@ class form extends formBase
             $item = item(set($item));
         }
 
-        if($this->prop('grid')) return new formRow(inherit($item));
+        if(is_string($key) && !$item->hasProp('name')) $item->setProp('name', $key);
+        $name = $item->prop('name');
+        if(is_string($name))
+        {
+            if(!$item->hasProp('value'))
+            {
+                $value = $this->getItemValue($name);
+                if(!is_null($value)) $item->setProp('value', $value);
+            }
+            if(!$item->hasProp('label'))
+            {
+                $label = $this->getItemLabel($name);
+                if(!is_null($label)) $item->setProp('label', $label);
+            }
+        }
+        if($this->hasProp('requiredFields') && !$item->hasProp('requiredFields')) $item->setProp('requiredFields', $this->prop('requiredFields'));
+
+        if($this->isLayout('horz')) return new formRow(inherit($item));
+
+        if($this->isLayout('grid'))
+        {
+            if(!$item->hasProp('width')) $item->setProp('width', '1/2');
+
+            if(is_string($name))
+            {
+                $control = $item->prop('control');
+
+                if(is_string($control)) $control = array('type' => $control, 'name' => $name);
+                if(empty($control))     $control = array('name' => $name);
+
+                if(!isset($control['id'])) $control['id'] = '';
+                $item->setProp('control', $control);
+            }
+        }
 
         return new formGroup(inherit($item));
     }
@@ -54,29 +120,34 @@ class form extends formBase
     protected function buildActions(): wg|null
     {
         $actions = parent::buildActions();
-        if($this->prop('grid') && !empty($actions)) $actions = div(setClass('form-row'), $actions);
+        if($this->isLayout('horz') && !empty($actions)) $actions = div(setClass('form-row'), $actions);
         return $actions;
     }
 
     protected function buildProps(): array
     {
-        list($grid, $labelWidth) = $this->prop(array('grid', 'labelWidth'));
         $props = parent::buildProps();
-        if($grid)               $props[] = setClass('form-horz');
-        if(!empty($labelWidth)) $props[] = setCssVar('form-horz-label-width', $labelWidth);
+        $layout = $this->prop('layout');
+        $props[] = setClass("form-$layout");
+
+        if($layout == 'horz')
+        {
+            $labelWidth = $this->prop('labelWidth');
+            if(!empty($labelWidth)) $props[] = setCssVar('form-horz-label-width', $labelWidth);
+        }
 
         return $props;
     }
 
     protected function buildContent(): array
     {
-        list($items, $grid) = $this->prop(array('items', 'grid'));
+        list($items) = $this->prop(array('items'));
 
-        $list     = is_array($items) ? array_map(array($this, 'onBuildItem'), $items) : array();
+        $list     = is_array($items) ? array_map(array($this, 'onBuildItem'), $items, array_keys($items)) : array();
         $children = $this->children();
         if(!empty($children)) $list = array_merge($list, $children);
 
-        if($grid)
+        if($this->isLayout('horz'))
         {
             foreach($list as $key => $item)
             {
