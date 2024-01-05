1f935f96bf3d4d33a8d3dee5797426f7791ec9b8
tianshujie <tianshujie@easycorp.ltd>
2024-01-05 14:47:57 +0800

    * Change the zt_task and zt_holiday data of the holiday updateTaskRealDuration unit test.

--- module/holiday/test/model/updatetaskrealduration.php
+++ module/holiday/test/model/updatetaskrealduration.php
@@ -7,20 +7,20 @@ cid=1
 
 - 测试插入id为 10 的节假日时任务 1 任务的计划工期 @1
 - 测试插入id为 10 的节假日时任务 2 任务的计划工期 @1
-- 测试插入id为 10 的节假日时任务 3 任务的计划工期 @6
+- 测试插入id为 10 的节假日时任务 3 任务的计划工期 @1
 - 测试插入id为 5 的节假日时任务 1 任务的计划工期 @1
-- 测试插入id为 5 的节假日时任务 2 任务的计划工期 @5
-- 测试插入id为 5 的节假日时任务 3 任务的计划工期 @6
-- 测试插入id为 5 的节假日时任务 1 任务的计划工期 @6
-- 测试插入id为 5 的节假日时任务 2 任务的计划工期 @5
-- 测试插入id为 5 的节假日时任务 3 任务的计划工期 @6
+- 测试插入id为 5 的节假日时任务 2 任务的计划工期 @1
+- 测试插入id为 5 的节假日时任务 3 任务的计划工期 @1
+- 测试插入id为 5 的节假日时任务 1 任务的计划工期 @1
+- 测试插入id为 5 的节假日时任务 2 任务的计划工期 @1
+- 测试插入id为 5 的节假日时任务 3 任务的计划工期 @1
 
 */
 declare(strict_types=1);
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/holiday.class.php';
 
-zdTable('holiday')->gen(10);
+zdTable('holiday')->config('holiday')->gen(24);
 zdTable('task')->config('task')->gen(10);
 zdTable('user')->gen(1);
 
@@ -36,12 +36,12 @@ $holiday = new holidayTest();
 
 r($holiday->updateTaskRealDurationTest($taskIdList[0], $holidayIdList[0], $updateDuration[0])) && p() && e('1'); //测试插入id为 10 的节假日时任务 1 任务的计划工期
 r($holiday->updateTaskRealDurationTest($taskIdList[1], $holidayIdList[0], $updateDuration[1])) && p() && e('1'); //测试插入id为 10 的节假日时任务 2 任务的计划工期
-r($holiday->updateTaskRealDurationTest($taskIdList[2], $holidayIdList[0], $updateDuration[1])) && p() && e('6'); //测试插入id为 10 的节假日时任务 3 任务的计划工期
+r($holiday->updateTaskRealDurationTest($taskIdList[2], $holidayIdList[0], $updateDuration[1])) && p() && e('1'); //测试插入id为 10 的节假日时任务 3 任务的计划工期
 
 r($holiday->updateTaskRealDurationTest($taskIdList[0], $holidayIdList[1], $updateDuration[0])) && p() && e('1'); //测试插入id为 5 的节假日时任务 1 任务的计划工期
-r($holiday->updateTaskRealDurationTest($taskIdList[1], $holidayIdList[1], $updateDuration[1])) && p() && e('5'); //测试插入id为 5 的节假日时任务 2 任务的计划工期
-r($holiday->updateTaskRealDurationTest($taskIdList[2], $holidayIdList[1], $updateDuration[1])) && p() && e('6'); //测试插入id为 5 的节假日时任务 3 任务的计划工期
+r($holiday->updateTaskRealDurationTest($taskIdList[1], $holidayIdList[1], $updateDuration[1])) && p() && e('1'); //测试插入id为 5 的节假日时任务 2 任务的计划工期
+r($holiday->updateTaskRealDurationTest($taskIdList[2], $holidayIdList[1], $updateDuration[1])) && p() && e('1'); //测试插入id为 5 的节假日时任务 3 任务的计划工期
 
-r($holiday->updateTaskRealDurationTest($taskIdList[0], $holidayIdList[2], $updateDuration[0])) && p() && e('6'); //测试插入id为 5 的节假日时任务 1 任务的计划工期
-r($holiday->updateTaskRealDurationTest($taskIdList[1], $holidayIdList[2], $updateDuration[1])) && p() && e('5'); //测试插入id为 5 的节假日时任务 2 任务的计划工期
-r($holiday->updateTaskRealDurationTest($taskIdList[2], $holidayIdList[2], $updateDuration[1])) && p() && e('6'); //测试插入id为 5 的节假日时任务 3 任务的计划工期
+r($holiday->updateTaskRealDurationTest($taskIdList[0], $holidayIdList[2], $updateDuration[0])) && p() && e('1'); //测试插入id为 5 的节假日时任务 1 任务的计划工期
+r($holiday->updateTaskRealDurationTest($taskIdList[1], $holidayIdList[2], $updateDuration[1])) && p() && e('1'); //测试插入id为 5 的节假日时任务 2 任务的计划工期
+r($holiday->updateTaskRealDurationTest($taskIdList[2], $holidayIdList[2], $updateDuration[1])) && p() && e('1'); //测试插入id为 5 的节假日时任务 3 任务的计划工期
