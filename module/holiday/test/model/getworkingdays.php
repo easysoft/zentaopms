32085c1621b788329c6962ecec9334fbc8db996b
tianshujie <tianshujie@easycorp.ltd>
2024-01-05 14:44:38 +0800

    * Modify the step description of the holiday::getWorkingDays unit test.

--- module/holiday/test/model/getworkingdays.php
+++ module/holiday/test/model/getworkingdays.php
@@ -5,10 +5,10 @@
 title=测试 holidayModel->getWorkingDays();
 cid=1
 
-- 返回2024年1月1日 14天前 到 7天后 之间的工作日。 @7
-- 返回2024年1月1日 14天前 到 14天后 之间的工作日。 @7
-- 返回2024年1月1日 7天前 到 7天后 之间的工作日。 @0
-- 返回2024年1月1日 7天前 到 14天后 之间的工作日。 @0
+- 返回 2023-01-28 ~ 2024-01-08 的工作日。 @7
+- 返回 2023-01-28 ~ 2024-01-15 的工作日。 @7
+- 返回 2023-12-25 ~ 2024-01-08 的工作日。 @0
+- 返回 2023-12-25 ~ 2024-01-15 的工作日。 @0
 - 测试当结束日期小于开始日期时。 @0
 - 测试当数据库没有工作日记录时。 @0
 
@@ -26,9 +26,9 @@ $holiday = new holidayTest();
 $begin   = array('2023-01-28', '2023-12-25', '2024-01-05');
 $end     = array('2024-01-08', '2024-01-15', '2024-01-07');
 
-r($holiday->getWorkingDaysTest($begin[0], $end[0])) && p() && e('7'); //返回2024年1月1日 14天前 到 7天后 之间的工作日。
-r($holiday->getWorkingDaysTest($begin[0], $end[1])) && p() && e('7'); //返回2024年1月1日 14天前 到 14天后 之间的工作日。
-r($holiday->getWorkingDaysTest($begin[1], $end[0])) && p() && e('0'); //返回2024年1月1日 7天前 到 7天后 之间的工作日。
-r($holiday->getWorkingDaysTest($begin[1], $end[1])) && p() && e('0'); //返回2024年1月1日 7天前 到 14天后 之间的工作日。
+r($holiday->getWorkingDaysTest($begin[0], $end[0])) && p() && e('7'); //返回 2023-01-28 ~ 2024-01-08 的工作日。
+r($holiday->getWorkingDaysTest($begin[0], $end[1])) && p() && e('7'); //返回 2023-01-28 ~ 2024-01-15 的工作日。
+r($holiday->getWorkingDaysTest($begin[1], $end[0])) && p() && e('0'); //返回 2023-12-25 ~ 2024-01-08 的工作日。
+r($holiday->getWorkingDaysTest($begin[1], $end[1])) && p() && e('0'); //返回 2023-12-25 ~ 2024-01-15 的工作日。
 r($holiday->getWorkingDaysTest($begin[2], $end[0])) && p() && e('0'); //测试当结束日期小于开始日期时。
 r($holiday->getWorkingDaysTest($begin[2], $end[2])) && p() && e('0'); //测试当数据库没有工作日记录时。
