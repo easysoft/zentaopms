28c7cc9fa429288f372c7917b8a744e8f8ce9382
dingyongliang <dingyongliang@easycorp.ltd>
2024-01-05 16:06:26 +0800

    * hidden ai search preview.

--- module/search/view/index.html.php
+++ module/search/view/index.html.php
@@ -35,7 +35,9 @@ adjustMenuWidth();
         <div class='item'>
           <div class='item-heading'>
             <div class="pull-right">
+              <?php if($object->objectType !== 'ai'): ?>
               <span><?php echo html::a($object->url . (strpos($object->url, '?') !== false ? '&onlybody=yes' : '?onlybody=yes'), $lang->search->preview, '', "class='iframe' data-width='90%'")?></span>
+              <?php endif; ?>
             </div>
             <h4>
               <?php
