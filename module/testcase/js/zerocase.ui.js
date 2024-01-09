b01defe267ad2d2c9481262c830de8fcf3c726aa
wangxuepeng <wangxuepeng@easycorp.ltd>
2024-01-09 17:26:45 +0800

    * Fix bug #43472.

---
+++ module/testcase/js/zerocase.ui.js
@@ -0,0 +1,22 @@
+$(document).off('click', '.batch-btn').on('click', '.batch-btn', function()
+{
+    const dtable = zui.DTable.query($(this).target);
+    const checkedList = dtable.$.getChecks();
+    if(!checkedList.length) return;
+
+    const url  = $(this).data('url');
+    const form = new FormData();
+
+    checkedList.forEach((id) => {
+        form.append('storyIdList[]', id);
+    });
+
+    if($(this).hasClass('ajax-btn'))
+    {
+        $.ajaxSubmit({url, data:form});
+    }
+    else
+    {
+        postAndLoadPage(url, form);
+    }
+});
