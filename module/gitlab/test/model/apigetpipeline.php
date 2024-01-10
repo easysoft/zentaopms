71b0beff126febc04c23f8c64529167a6d0c8e67
zenggang <zenggang@easycorp.ltd>
2024-01-10 09:25:12 +0000

    *Adjust apigetpipeline unit test.

--- module/gitlab/test/model/apigetpipeline.php
+++ module/gitlab/test/model/apigetpipeline.php
@@ -10,9 +10,7 @@ title=测试gitlabModel->apiGetPipeline();
 timeout=0
 cid=1
 
-- 查询正确的pipeline信息
- - 属性iid @60
- - 属性project_id @2
+- 查询正确的pipeline信息 @1
 - 使用不存在的gitlabID查询pipeline信息 @0
 - 使用不存在的projectID查询pipeline信息属性message @404 Project Not Found
 - 使用不存在的branch查询pipeline信息 @0
@@ -32,7 +30,7 @@ $pipeline2 = $gitlab->apiGetPipeline(0, $projectID, $branch);
 $pipeline3 = $gitlab->apiGetPipeline($gitlabID, 0, $branch);
 $pipeline4 = $gitlab->apiGetPipeline($gitlabID, $projectID, 'branch123');
 
-r(end($pipeline1)) && p('iid,project_id') && e('60,2');                  // 查询正确的pipeline信息
-r($pipeline2)      && p()                 && e('0');                     // 使用不存在的gitlabID查询pipeline信息
-r($pipeline3)      && p('message')        && e('404 Project Not Found'); // 使用不存在的projectID查询pipeline信息
-r($pipeline4)      && p()                 && e('0');                     // 使用不存在的branch查询pipeline信息
+r(!empty($pipeline1[0]->iid)) && p()          && e('1');                  // 查询正确的pipeline信息
+r($pipeline2)                 && p()          && e('0');                     // 使用不存在的gitlabID查询pipeline信息
+r($pipeline3)                 && p('message') && e('404 Project Not Found'); // 使用不存在的projectID查询pipeline信息
+r($pipeline4)                 && p()          && e('0');                     // 使用不存在的branch查询pipeline信息
