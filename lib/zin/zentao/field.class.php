b3e75f9aa91ca2393096000e9f75ce4611e64368
sunhao <sunhao@easycorp.ltd>
2024-01-11 09:39:30 +0800

    + zin: add field and fieldList class.

---
+++ lib/zin/zentao/field.class.php
@@ -0,0 +1,328 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * The field class file of zin lib.
+ *
+ * @copyright   Copyright 2023 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @author      Hao Sun <sunhao@easycorp.ltd>
+ * @package     zin
+ * @version     $Id
+ * @link        https://www.zentao.net
+ */
+
+namespace zin;
+
+require_once dirname(__DIR__) . DS . 'core' . DS . 'setting.class.php';
+require_once dirname(__DIR__) . DS . 'utils' . DS . 'dataset.class.php';
+
+use \zin\fieldList;
+
+class field extends setting
+{
+    public ?fieldList $fieldList;
+
+    public ?field $parent;
+
+    public ?string $dataType;
+
+    public mixed $default;
+
+    public function __construct(?string $name = null, ?fieldList $fieldList = null, ?field $parent = null)
+    {
+        $this->fieldList = $fieldList;
+        $this->parent    = $parent;
+        parent::__construct(array('name' => $name));
+    }
+
+    public function getName(): string
+    {
+        return $this->get('name');
+    }
+
+    function name(?string $name): field
+    {
+        return $this->setVal('name', $name);
+    }
+
+    function type(?string $type): field
+    {
+        return $this->setVal('type', $type);
+    }
+
+    function id(?string $id): field
+    {
+        return $this->setVal('id', $id);
+    }
+
+    function className(mixed ...$classList): field
+    {
+        return $this->setClass('className', ...$classList);
+    }
+
+    function value(string|array|null $value): field
+    {
+        return $this->setVal('value', $value);
+    }
+
+    function width(string|int|null $width): field
+    {
+        return $this->setVal('width', $width);
+    }
+
+    function required(?bool $required = true): field
+    {
+        return $this->setVal('required', $required);
+    }
+
+    function disabled(?bool $disabled = true): field
+    {
+        return $this->setVal('disabled', $disabled);
+    }
+
+    function readonly(?bool $readonly = true): field
+    {
+        return $this->setVal('readonly', $readonly);
+    }
+
+    function placeholder(?string $placeholder): field
+    {
+        return $this->setVal('placeholder', $placeholder);
+    }
+
+    function foldable(?bool $foldable = true): field
+    {
+        return $this->setVal('foldable', $foldable);
+    }
+
+    function pinned(?bool $pinned = true): field
+    {
+        return $this->setVal('pinned', $pinned);
+    }
+
+    function hidden(?bool $hidden = true): field
+    {
+        return $this->setVal('hidden', $hidden);
+    }
+
+    function strong(?bool $strong = true): field
+    {
+        return $this->setVal('strong', $strong);
+    }
+
+    function label(bool|string|null $label, string|object|array $classOrProps = null): field
+    {
+        $this->setVal('label', $label);
+        if (is_string($classOrProps)) $this->labelClass($classOrProps);
+        else if (!is_null($classOrProps)) $this->labelProps($classOrProps);
+        return $this;
+    }
+
+    function labelFor(bool|string|null $for): field
+    {
+        return $this->setVal('labelFor', $for);
+    }
+
+    function labelClass(mixed ...$classList): field
+    {
+        return $this->setClass('labelClass', ...$classList);
+    }
+
+    function labelProps(array|object|null $props): field
+    {
+        return $this->addToMap('labelProps', $props);
+    }
+
+    function labelWidth(int|string|null $width): field
+    {
+        return $this->setVal('labelWidth', $width);
+    }
+
+    function labelHint(?string $hint, string|object|array $classOrProps = null): field
+    {
+        $this->setVal('labelHint', $hint);
+        if (is_string($classOrProps)) $this->labelHintClass($classOrProps);
+        else if (!is_null($classOrProps)) $this->labelHintProps($classOrProps);
+        return $this;
+    }
+
+    function labelHintClass(mixed ...$classList): field
+    {
+        return $this->setClass('labelHintClass', ...$classList);
+    }
+
+    function labelHintProps(array|object|null $props): field
+    {
+        return $this->addToMap('labelHintProps', $props);
+    }
+
+    function checkbox(bool|string|null $checkbox = true, array|object|null $props = null): field
+    {
+        $this->setVal('checkbox', $checkbox);
+        if (!is_null($props)) $this->checkboxProps($props);
+        return $this;
+    }
+
+    function checkboxProps(array|object|null $props): field
+    {
+        return $this->addToMap('checkboxProps', $props);
+    }
+
+    function checked(?bool $checked = true): field
+    {
+        return $this->setVal('checked', $checked);
+    }
+
+    function wrapBefore(?bool $wrapBefore = true): field
+    {
+        return $this->setVal('wrapBefore', $wrapBefore);
+    }
+
+    function wrapAfter(?bool $wrapAfter = true): field
+    {
+        return $this->setVal('wrapAfter', $wrapAfter);
+    }
+
+    function wrap(string $side = 'before', bool $wrap = true): field
+    {
+        return $this->setVal($side == 'before' ? 'wrapBefore' : 'wrapAfter', $wrap);
+    }
+
+    function tip(bool|string|null $tip): field
+    {
+        return $this->setVal('tip', $tip);
+    }
+
+    function tipClass(mixed ...$classList): field
+    {
+        return $this->setClass('tipClass', ...$classList);
+    }
+
+    function tipProps(array|object|null $tipProps): field
+    {
+        return $this->addToMap('tipProps', $tipProps);
+    }
+
+    function multiple(?bool $multiple = true): field
+    {
+        return $this->setVal('multiple', $multiple);
+    }
+
+    function createChild(?string $itemName = null): field
+    {
+        $item = new field($itemName, null, $this);
+        return $item;
+    }
+
+    function control(string|array|object|null $control, array|object|null $props = null): field
+    {
+        if(is_string($control)) $control = array('type' => $control);
+        $this->addToMap('control', $control);
+        if(!is_null($props)) $this->addToMap('control', $props);
+        return $this;
+    }
+
+    function controlBegin(?string $itemName): field
+    {
+        return $this->createChild($itemName);
+    }
+
+    function controlEnd(): field
+    {
+        if(is_null($this->parent))
+        {
+            trigger_error('[ZIN] The field named ' . $this->getName() . ' has no parent, maybe you should call "controlBegin($name)" firstly.', E_USER_ERROR);
+        }
+        return $this->parent->control($this->toArray());
+    }
+
+    function items(array|object|null $items): field
+    {
+        return $this->addToList('items', $items);
+    }
+
+    function item(string|array|object|null $itemOrName, array|object|null $item = null): field
+    {
+        if(is_null($itemOrName)) return $this;
+        if(is_string($itemOrName))
+        {
+            if(is_null($item)) return $this->removeItem($itemOrName);
+            return $this->addToList('items', array($itemOrName => $item));
+        }
+        return $this->addToList('items', array($itemOrName));
+    }
+
+    function itemBegin(?string $itemName = null): field
+    {
+        return $this->createChild($itemName);
+    }
+
+    function itemEnd(): field
+    {
+        if(is_null($this->parent))
+        {
+            trigger_error('[ZIN] The field named ' . $this->getName() . ' has no parent, maybe you should call "itemBegin($name)" firstly.', E_USER_ERROR);
+        }
+        $this->parent->item($this);
+        return $this->parent;
+    }
+
+    function removeItem(string $itemName): field
+    {
+        return $this->removeFromList('items', $itemName);
+    }
+
+    function children(mixed ...$children): field
+    {
+        return $this->addToList('children', $children);
+    }
+
+    function setDefault(string|array|null $default): field
+    {
+        $this->default = $default;
+        return $this;
+    }
+
+    function setDataType(?string $dataType): field
+    {
+        $this->dataType = $dataType;
+        return $this;
+    }
+
+    function moveBefore(string $name): field
+    {
+        if(is_null($this->parent))
+        {
+            trigger_error('[ZIN] The field named ' . $this->getName() . ' has no parent, maybe you should add self to a fieldList firstly.', E_USER_ERROR);
+        }
+        $this->parent->moveBefore($this->getName(), $name);
+        return $this;
+    }
+
+    function moveAfter(string $name): field
+    {
+        if(is_null($this->parent))
+        {
+            trigger_error('[ZIN] The field named ' . $this->getName() . ' has no parent, maybe you should add self to a fieldList firstly.', E_USER_ERROR);
+        }
+        $this->parent->moveAfter($this->getName(), $name);
+        return $this;
+    }
+
+    function toArray(): array
+    {
+        $array = parent::toArray();
+        if(isset($array['items']))
+        {
+            $items = array();
+            foreach($array['items'] as $key => $item)
+            {
+                if($item instanceof \zin\utils\dataset) $item = $item->toArray();
+                elseif(is_object($item))     $item = get_object_vars($item);
+                $items[$key] = $item;
+            }
+            $array['items'] = $items;
+        }
+        return $array;
+    }
+}
---
+++ lib/zin/zentao/fieldlist.class.php
@@ -0,0 +1,258 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * The field list class file of zin lib.
+ *
+ * @copyright   Copyright 2023 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @author      Hao Sun <sunhao@easycorp.ltd>
+ * @package     zin
+ * @version     $Id
+ * @link        https://www.zentao.net
+ */
+
+namespace zin;
+
+require_once __DIR__ . DS . 'field.class.php';
+
+class fieldList
+{
+    public string $name;
+
+    public array $fields = array();
+
+    public ?array $labelData = null;
+
+    public ?array $valueData = null;
+
+    public function __construct(?string $name = null, ?array $fields = null)
+    {
+        if(is_null($name))
+        {
+            $this->name = '';
+        }
+        else
+        {
+            $this->name = $name;
+
+            if(isset(static::$map[$name]))
+            {
+                trigger_error("[ZIN] The fieldList named \"$name\" already exist.", E_USER_ERROR);
+            }
+
+            static::$map[$name] = $this;
+        }
+
+        if(is_array($fields))
+        {
+            foreach($fields as $field)
+            {
+                $this->setField($field);
+            }
+        }
+    }
+
+    /**
+     * Magic method for using field.
+     *
+     * @access public
+     * @param  string $name  - field name.
+     * @return field
+     */
+    public function __call(string $name, array $args): field
+    {
+        return $this->field($name);
+    }
+
+    public function field(string $name): field
+    {
+        $field = $this->getField($name);
+        if(is_null($field))
+        {
+            $field = new field($name, $this);
+            $this->setField($field);
+        }
+        return $field;
+    }
+
+    public function getField(string $name): field|null
+    {
+        return isset($this->fields[$name]) ? $this->fields[$name] : null;
+    }
+
+    public function setField(field $field): fieldList
+    {
+        $this->fields[$field->getName()] = $field;
+        return $this;
+    }
+
+    public function merge(string|array|field|fieldList|null $info): fieldList
+    {
+        if(is_null($info)) return $this;
+
+        if(is_string($info))
+        {
+            $list = array();
+            foreach(explode(',', $info) as $name)
+            {
+                $list[] = static::getByName($name);
+            }
+            $info = $list;
+        }
+
+        if(is_array($info))
+        {
+            foreach($info as $field)
+            {
+                $this->merge($field);
+            }
+            return $this;
+        }
+
+        if($info instanceof field)     return $this->mergeField($info);
+        if($info instanceof fieldList) return $this->mergeList($info);
+        return $this;
+    }
+
+    public function mergeField(field $field): fieldList
+    {
+        $oldField = $this->getField($field->getName());
+        if(is_null($oldField)) return $this->setField($field);
+
+        $oldField->merge($field);
+        return $this;
+    }
+
+    public function mergeList(fieldList $fieldList): fieldList
+    {
+        foreach($fieldList->fields as $field)
+        {
+            if(is_null($field)) continue;
+            $this->mergeField($field);
+        }
+        return $this;
+    }
+
+    public function remove(string|array $names): fieldList
+    {
+        if(is_string($names)) $names = explode(',', $names);
+        foreach($names as $name) unset($this->fields[$name]);
+        return $this;
+    }
+
+    public function moveBefore(string|array $names, string $beforeName): fieldList
+    {
+        return $this;
+    }
+
+    public function moveAfter(string|array $names, string $beforeName): fieldList
+    {
+        return $this;
+    }
+
+    public function insertBefore(string|array $names, string $beforeName): fieldList
+    {
+        return $this;
+    }
+
+    public function insertAfter(string|array $names, string $beforeName): fieldList
+    {
+        return $this;
+    }
+
+    public function sort(string|array $names): fieldList
+    {
+        return $this;
+    }
+
+    public function batchSet(string|array $names, string|array $prop, mixed $value = null): fieldList
+    {
+        return $this;
+    }
+
+    public function setLabelData(array|object $labelData): fieldList
+    {
+        if(is_object($labelData)) $labelData = get_object_vars($labelData);
+        $this->labelData = $labelData;
+        return $this;
+    }
+
+    public function setValueData(array|object $valueData): fieldList
+    {
+        if(is_object($valueData)) $valueData = get_object_vars($valueData);
+        $this->valueData = $valueData;
+        return $this;
+    }
+
+    public function toList(string|array|null $names = null): array
+    {
+        return $this->fields;
+    }
+
+    public function toArray(): array
+    {
+        $list = array();
+        foreach($this->toList() as $field)
+        {
+            $list[$field->getName()] = $field->toArray();
+        }
+        return $list;
+    }
+
+    protected static array $map = array();
+
+    public static ?string $currentName = null;
+
+    public static function define($currentName)
+    {
+        static::$currentName = $currentName;
+        return static::ensure($currentName);
+    }
+
+    public static function get(string $name): ?fieldList
+    {
+        return isset(static::$map[$name]) ? static::$map[$name] : null;
+    }
+
+    public static function getListField(string $listName, string $name = null): ?field
+    {
+        if(is_null($name)) list($listName, $name) = explode('/', $listName);
+        if(is_null($name)) return null;
+
+        $fieldList = static::get($listName);
+        return is_null($fieldList) ? null : $fieldList->getField($name);
+    }
+
+    public static function getByName(string $name): field|fieldList|null
+    {
+        if(str_ends_with($name, '/')) return static::get(substr($name, 0, -1));
+        if(str_contains($name, '/')) return static::getListField($name);
+        return static::get($name);
+    }
+
+    public static function ensure(string $name): fieldList
+    {
+        if(isset(static::$map[$name])) return static::$map[$name];
+        return new fieldList($name);
+    }
+
+    public static function current(): fieldList
+    {
+        if(is_null(static::$currentName))
+        {
+            trigger_error("[ZIN] The current fieldList name is not defined.", E_USER_ERROR);
+        }
+        return static::ensure(static::$currentName);
+    }
+
+    public static function build(string|array|field|fieldList|null ...$args): fieldList
+    {
+        $fieldList = new fieldList();
+        foreach($args as $arg)
+        {
+            if(is_null($arg)) continue;
+            $fieldList->merge($arg);
+        }
+        return $fieldList;
+    }
+}
