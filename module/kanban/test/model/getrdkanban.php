7dfbd93a7004008a51b8cc98563367507b0987d9
wangyuting <wangyuting@easycorp.ltd>
2024-01-11 16:23:48 +0800

    * Fix kanban unit test bug.

--- module/kanban/test/model/getrdkanban.php
+++ module/kanban/test/model/getrdkanban.php
@@ -41,6 +41,6 @@ $kanban = new kanbanTest();
 r($kanban->getRDKanbanTest($executionIDList[0])[0]['items'][0]['data']['lanes'])          && p('0:id,title') && e('1,泳道1');  // 获取执行1的看板信息
 r($kanban->getRDKanbanTest($executionIDList[1])[0]['items'][0]['data']['items'][7][7])    && p('0:id,title') && e('7,任务7');  // 获取执行2的看板信息
 r($kanban->getRDKanbanTest($executionIDList[2])[0]['items'][0]['data']['cols'])           && p('0:id,title') && e('9,未开始'); // 获取执行3的看板信息
-r($kanban->getRDKanbanTest($executionIDList[0], 'story'))                                 && p('')           && e('0');        // 获取执行1的看板信息,传入列类型
+r($kanban->getRDKanbanTest($executionIDList[0], 'story')[0]['items'])                     && p('')           && e('0');        // 获取执行1的看板信息,传入列类型
 r($kanban->getRDKanbanTest($executionIDList[1], 'task')[0]['items'][0]['data']['lanes'])  && p('0:id,title') && e('2,泳道2');  // 获取执行2的看板信息,传入列类型
-r($kanban->getRDKanbanTest($executionIDList[2], 'bug'))                                   && p('')           && e('0');        // 获取执行3的看板信息,传入列类型
+r($kanban->getRDKanbanTest($executionIDList[2], 'bug')[0]['items'])                       && p('')           && e('0');        // 获取执行3的看板信息,传入列类型
