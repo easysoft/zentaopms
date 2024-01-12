5f67ab0749f5bebf68f96d036abf6f7ba76249c6
sunhao <sunhao@easycorp.ltd>
2024-01-12 08:51:38 +0800

    * bug: refactor bug-create fields.

--- module/bug/ui/create.field.php
+++ module/bug/ui/create.field.php
@@ -3,15 +3,15 @@ namespace zin;
 
 global $lang;
 
-$bugFields = defineFieldList('bug.create', 'bug', '!branch,color');
+$fields = defineFieldList('bug.create', 'bug', '!branch,color');
 
-defineField('product')
+$fields->field('product')
     ->hidden(data('product.shadow'))
     ->control('inputGroup')
     ->items(false)
     ->itemBegin('product')->type('picker')->items(data('products'))->value(data('bug.productID'))->itemEnd()
     ->item((data('product.type') !== 'normal' && isset(data('products')[data('bug.productID')])) ? field('branch')->type('picker')->boxClass('flex-none')->width('100px')->name('branch')->items(data('branches'))->value(data('bug.branch')) : null);
 
-defineField('title')
+$fields->field('title')
     ->width('full')
     ->control('colorInput', array('colorValue' => data('bug.color')));
