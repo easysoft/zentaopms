9aeff5c1e19c1eb8fa94314a65a1e351819b29a9
wangyuting <wangyuting@easycorp.ltd>
2024-01-15 10:10:56 +0800

    * Adjust task row css. bug #42068.

--- module/project/js/execution.ui.js
+++ module/project/js/execution.ui.js
@@ -59,6 +59,7 @@ window.onRenderCell = function(result, {col, row})
         result[result.length] = {html: executionName};
         return result;
     }
+    if(col.name == 'rawID' && row.data.parent) result[0] = '';
 
     return result;
 }
