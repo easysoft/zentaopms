e05d1720431ce92abb5c4f9a792665f4956f40f9
zhouxin <zhouxin@easycorp.ltd>
2024-01-15 15:21:05 +0800

    * Add pager to metric in screen designer.

--- www/static/js/javascript-94b4b7bb.js
+++
@@ -1,6 +0,0 @@
-import{conf as t,language as e}from"./typescript-23e49368.js";import"./index-15e6357c.js";import"./index.js";import"./editorWorker-abdb3aef.js";import"./storage-359649f7.js";import"./icon-ee4c7d1a.js";/*!-----------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
- * Released under the MIT license
- * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
- *-----------------------------------------------------------------------------*/var l=t,c={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{l as conf,c as language};
