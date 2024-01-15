6f7cfa8fde1c2d4a7998f5b439e9e0639b74658e
李硕 <lishuo@easysoft.ltd>
2024-01-15 16:38:12 +0800

    * Fix bug #42225: display task estStarted and deadline in project stage list view.

--- module/execution/model.php
+++ module/execution/model.php
@@ -4782,6 +4782,8 @@ class executionModel extends model
             $task->isParent      = ($task->parent < 0);
             $task->parent        = $task->parent <= 0 ? 'pid' . (string)$task->execution : 'tid' . (string)$task->parent;
             $task->progress      = ($task->consumed + $task->left) == 0 ? 0 : round($task->consumed / ($task->consumed + $task->left), 2) * 100;
+            $task->begin         = $task->estStarted;
+            $task->end           = $task->deadline;
 
             $rows[] = $task;
         }
