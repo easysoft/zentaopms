359eaa10ab98494d83821cb9a0f6fa991f0b6bd8
sunhao <sunhao@easycorp.ltd>
2024-01-16 11:17:33 +0800

    * zin: change callable control value only be syntax.

--- lib/zin/wg/formgroup/v1.php
+++ lib/zin/wg/formgroup/v1.php
@@ -91,8 +91,8 @@ class formGroup extends wg
     {
         $control = $this->prop('control');
 
-        if($control instanceof wg) return $control;
-        if(is_callable($control))  return $control($this->props->toJSON());
+        if($control instanceof wg)       return $control;
+        if(is_callable($control, true))  return $control($this->props->toJSON());
 
         if(is_string($control))                             $control = array('control' => $control);
         elseif($control instanceof item)                    $control = $control->props->toJSON();
