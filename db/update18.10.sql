5157a642c766cc2baf75843981fa5ba9537997af
chaideqing <chaideqing@easycorp.ltd>
2024-01-17 08:59:50 +0800

    * Add field to table zt_story.

--- db/update18.10.sql
+++ db/update18.10.sql
@@ -2,5 +2,5 @@ ALTER TABLE `zt_demandpool` ADD COLUMN `products` varchar(255) NOT NULL DEFAULT 
 ALTER TABLE `zt_project` ADD COLUMN `parallel` mediumint(9) NOT NULL DEFAULT '0';
 ALTER TABLE `zt_demand` CHANGE `product` `product` varchar(255) NOT NULL DEFAULT '';
 ALTER TABLE `zt_story` ADD COLUMN `retractedReason` ENUM('', 'omit', 'other') NOT NULL DEFAULT '';
-ALTER TABLE `zt_story` ADD COLUMN `retractedBy` varchar(255) NOT NULL DEFAULT '';
+ALTER TABLE `zt_story` ADD COLUMN `retractedBy` varchar(30) NOT NULL DEFAULT '';
 ALTER TABLE `zt_story` ADD COLUMN `retractedDate` datetime;
