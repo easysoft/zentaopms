18dcba9471d7d3200a72c7d47121ff0f3ad28385
liumengyi <liumengyi@easycorp.ltd>
2024-01-19 11:00:02 +0800

    - Fix bug when changing first product.

--- lib/zin/wg/productsbox/v1.php
+++ lib/zin/wg/productsbox/v1.php
@@ -262,7 +262,6 @@ class productsBox extends wg
                     set::width('1/2'),
                     $i == 0 ? set::label($lang->project->associatePlan) : null,
                     set::className('planBox'),
-                    set::id("plan{$i}"),
                     inputGroup
                     (
                         set::id("plan{$i}"),
