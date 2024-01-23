2de12ed473dc35c676578e6b317563c446bb0513
daitingting <daitingting@xirangit.com>
2024-01-23 02:44:11 +0000

    * Fix bug #44522.

--- module/program/config/form.php
+++ module/program/config/form.php
@@ -18,16 +18,17 @@ $config->program->form->create['acl']        = array('type' => 'string',   'cont
 $config->program->form->create['groups']     = array('type' => 'array',    'control' => 'picker',     'required' => false, 'default' => '', 'filter' => 'join');
 $config->program->form->create['whitelist']  = array('type' => 'array',    'control' => 'picker',     'required' => false, 'default' => '', 'filter' => 'join');
 
-$config->program->form->edit['parent']    = array('type' => 'int',      'control' => 'picker',     'required' => false, 'default' => 0);
-$config->program->form->edit['name']      = array('type' => 'string',   'control' => 'text',       'required' => true,  'default' => '', 'filter' => 'trim');
-$config->program->form->edit['PM']        = array('type' => 'string',   'control' => 'picker',     'required' => false, 'default' => 0);
-$config->program->form->edit['budget']    = array('type' => 'string',   'control' => 'text',       'required' => false, 'default' => '');
-$config->program->form->edit['begin']     = array('type' => 'date',     'control' => 'datepicker', 'required' => true,  'default' => '');
-$config->program->form->edit['end']       = array('type' => 'date',     'control' => 'datepicker', 'required' => false, 'default' => '');
-$config->program->form->edit['desc']      = array('type' => 'string',   'control' => 'editor',     'required' => false, 'default' => '');
-$config->program->form->edit['acl']       = array('type' => 'string',   'control' => 'radio',      'required' => false, 'default' => '');
-$config->program->form->edit['groups']    = array('type' => 'array',    'control' => 'picker',     'required' => false, 'default' => '', 'filter' => 'join');
-$config->program->form->edit['whitelist'] = array('type' => 'array',    'control' => 'picker',     'required' => false, 'default' => '', 'filter' => 'join');
+$config->program->form->edit['parent']     = array('type' => 'int',      'control' => 'picker',     'required' => false, 'default' => 0);
+$config->program->form->edit['name']       = array('type' => 'string',   'control' => 'text',       'required' => true,  'default' => '', 'filter' => 'trim');
+$config->program->form->edit['PM']         = array('type' => 'string',   'control' => 'picker',     'required' => false, 'default' => 0);
+$config->program->form->edit['budget']     = array('type' => 'string',   'control' => 'text',       'required' => false, 'default' => '');
+$config->program->form->edit['budgetUnit'] = array('type' => 'string',   'control' => 'text',       'required' => false, 'default' => 'CNY');
+$config->program->form->edit['begin']      = array('type' => 'date',     'control' => 'datepicker', 'required' => true,  'default' => '');
+$config->program->form->edit['end']        = array('type' => 'date',     'control' => 'datepicker', 'required' => false, 'default' => '');
+$config->program->form->edit['desc']       = array('type' => 'string',   'control' => 'editor',     'required' => false, 'default' => '');
+$config->program->form->edit['acl']        = array('type' => 'string',   'control' => 'radio',      'required' => false, 'default' => '');
+$config->program->form->edit['groups']     = array('type' => 'array',    'control' => 'picker',     'required' => false, 'default' => '', 'filter' => 'join');
+$config->program->form->edit['whitelist']  = array('type' => 'array',    'control' => 'picker',     'required' => false, 'default' => '', 'filter' => 'join');
 
 $config->program->form->close['realEnd']        = array('type' => 'date',     'required' => true,  'default' => $now, 'filter' => 'trim');
 $config->program->form->close['status']         = array('type' => 'string',   'required' => false, 'default' => 'closed');
