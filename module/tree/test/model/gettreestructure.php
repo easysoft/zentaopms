898be3fcc9e30b20d074d8756defc59d5b6d0938
tianshujie <tianshujie@easycorp.ltd>
2024-01-24 10:25:51 +0800

    * Change result of the tree::getTreeStructure unit test.

--- module/tree/test/model/gettreestructure.php
+++ module/tree/test/model/gettreestructure.php
@@ -35,8 +35,8 @@ r($tree->getTreeStructureTest(1, 'case'))  && p() && e('2:1;12:0;5:1;15:0;'); //
 r($tree->getTreeStructureTest(1, 'doc'))   && p() && e('3:1;13:0;');          // 测试获取root 1 doc   的树结构
 r($tree->getTreeStructureTest(1, 'task'))  && p() && e('0');                  // 测试获取root 1 task  的树结构
 
-r($tree->getTreeStructureTest(41, 'story')) && p() && e('16:1;'); // 测试获取root 41 story 的树结构
-r($tree->getTreeStructureTest(41, 'bug'))   && p() && e('17:1;'); // 测试获取root 41 bug   的树结构
-r($tree->getTreeStructureTest(41, 'case'))  && p() && e('0');     // 测试获取root 41 case  的树结构
-r($tree->getTreeStructureTest(41, 'doc'))   && p() && e('0');     // 测试获取root 41 doc   的树结构
-r($tree->getTreeStructureTest(41, 'task'))  && p() && e('0');     // 测试获取root 41 task  的树结构
+r($tree->getTreeStructureTest(41, 'story')) && p() && e('16:1;');      // 测试获取root 41 story 的树结构
+r($tree->getTreeStructureTest(41, 'bug'))   && p() && e('16:1;17:1;'); // 测试获取root 41 bug   的树结构
+r($tree->getTreeStructureTest(41, 'case'))  && p() && e('16:1;');      // 测试获取root 41 case  的树结构
+r($tree->getTreeStructureTest(41, 'doc'))   && p() && e('0');          // 测试获取root 41 doc   的树结构
+r($tree->getTreeStructureTest(41, 'task'))  && p() && e('0');          // 测试获取root 41 task  的树结构
