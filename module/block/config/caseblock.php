06a8f44fdf97cfdbfe047685139b1e9213c4c067
daitingting <daitingting@xirangit.com>
2024-01-25 06:42:59 +0000

    * Fix bug #45010.

--- module/block/config/caseblock.php
+++ module/block/config/caseblock.php
@@ -9,6 +9,7 @@ $config->block->case->dtable->fieldList = array();
 $config->block->case->dtable->fieldList['id']            = array('name' => 'id',    'title' => $lang->idAB,            'type' => 'id', 'fixed' => false, 'sort' => 'number');
 $config->block->case->dtable->fieldList['title']         = array('name' => 'title', 'title' => $lang->testcase->title, 'type' => 'title', 'sort' => true, 'link' => array('module' => 'testcase', 'method' => 'view', 'params' => "caseID={id}"), 'fixed' => false, 'width' => '50%');
 $config->block->case->dtable->fieldList['pri']           = $config->testcase->dtable->fieldList['pri'];
+$config->block->case->dtable->fieldList['type']          = $config->testcase->dtable->fieldList['type'];
 $config->block->case->dtable->fieldList['status']        = $config->testcase->dtable->fieldList['status'];
 $config->block->case->dtable->fieldList['lastRunDate']   = $config->testcase->dtable->fieldList['lastRunDate'];
 $config->block->case->dtable->fieldList['lastRunResult'] = $config->testcase->dtable->fieldList['lastRunResult'];
@@ -16,6 +17,8 @@ $config->block->case->dtable->fieldList['lastRunResult'] = $config->testcase->dt
 $config->block->case->dtable->fieldList['pri']['name']  = 'pri';
 $config->block->case->dtable->fieldList['pri']['title'] = $lang->priAB;
 $config->block->case->dtable->fieldList['pri']['sort']  = true;
+
+$config->block->case->dtable->fieldList['type']['name']  = 'type';
 
 $config->block->case->dtable->fieldList['status']['name']  = 'status';
 $config->block->case->dtable->fieldList['status']['title'] = $lang->statusAB;
