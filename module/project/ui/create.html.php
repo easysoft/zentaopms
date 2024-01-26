8a810164d4b868e0cdab7c23b06ca38c90017664
liumengyi <liumengyi@easycorp.ltd>
2024-01-26 09:30:53 +0800

    * Clear end and days if the long time is checked.

--- module/project/ui/create.html.php
+++ module/project/ui/create.html.php
@@ -65,7 +65,7 @@ formGridPanel
     on::click('[name=name], [name=code], [name=end], [name=days], [data-name="parent"] .pick *', 'removeTips'),
     on::click('[type=submit]', 'removeAllTips'),
     on::change('[name=hasProduct]', 'changeType'),
-    on::change('[name=longTime]')->do('$("[name=end]").zui("datePicker").render({disabled: $(target).prop("checked")}); $(target).prop("checked") ? $("[name=days]").attr("disabled", "disabled") : $("[name=days]").removeAttr("disabled");'),
+    on::change('[name=longTime]')->do('const $endPicker = $("[name=end]").zui("datePicker"); $endPicker.render({disabled: $(target).prop("checked")}); $endPicker.$.setValue(""); $(target).prop("checked") ? $("[name=days]").attr("disabled", "disabled") : $("[name=days]").removeAttr("disabled");'),
     on::change('[name=future]', 'toggleBudget'),
     on::change('[name=begin], [name=end]', 'computeWorkDays'),
     on::change('[name^=products]', 'toggleStageBy'),
--- module/project/ui/edit.html.php
+++ module/project/ui/edit.html.php
@@ -59,7 +59,7 @@ formGridPanel
             )
     ),
     on::change('[name=hasProduct]', 'changeType'),
-    on::change('[name=longTime]')->do('$("[name=end]").zui("datePicker").render({disabled: $(target).prop("checked")}); $(target).prop("checked") ? $("[name=days]").attr("disabled", "disabled") : $("[name=days]").removeAttr("disabled");'),
+    on::change('[name=longTime]')->do('const $endPicker = $("[name=end]").zui("datePicker"); $endPicker.render({disabled: $(target).prop("checked")}); if($(target).prop("checked")){ $endPicker.$.setValue(""); $("[name=days]").attr("disabled", "disabled");} else{ $("[name=days]").removeAttr("disabled");}'),
     on::change('[name=future]', 'toggleBudget'),
     on::change('[name=begin], [name=end]', 'computeWorkDays'),
     on::change('[name=parent], [name=budget]', 'checkBudget'),
