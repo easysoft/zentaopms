f9a7592d033f52d71f06125d0b71b30b025bc414
liumengyi <liumengyi@easycorp.ltd>
2024-01-26 15:35:19 +0800

    * Modify the tip of costs in block.

--- module/block/lang/zh-cn.php
+++ module/block/lang/zh-cn.php
@@ -708,7 +708,7 @@ $lang->block->tooltips['deliveryRate']      = "{$lang->SRCommon}交付率=按{$l
 $lang->block->tooltips['resolvedRate']      = "按{$lang->productCommon}统计的Bug修复率 = 按{$lang->productCommon}统计的修复Bug数 / 按{$lang->productCommon}统计的有效Bug数";
 $lang->block->tooltips['effectiveStory']    = "按{$lang->productCommon}统计的{$lang->SRCommon}总数：{$lang->productCommon}中{$lang->SRCommon}的个数求和，过滤已删除的{$lang->SRCommon}，过滤已删除的{$lang->productCommon}。";
 $lang->block->tooltips['deliveredStory']    = "按{$lang->productCommon}统计的已交付{$lang->SRCommon}数：{$lang->productCommon}中{$lang->SRCommon}个数求和，所处阶段为已发布或关闭原因为已完成，过滤已删除的{$lang->SRCommon}，过滤已删除的{$lang->productCommon}。";
-$lang->block->tooltips['costs']             = "已投入 = 已消耗工时 / 每日可用工时";
+$lang->block->tooltips['costs']             = "已投入 = 已消耗工时 / 后台配置的每日可用工时";
 $lang->block->tooltips['sv']                = "进度偏差率 = (EV - PV) / PV * 100% ";
 $lang->block->tooltips['ev']                = "<strong>实际完成</strong>=按{$lang->projectCommon}统计的任务预计工时数*按{$lang->projectCommon}统计的任务进度，过滤已删除的任务，过滤已取消的任务，过滤已删除{$lang->execution->common}下的任务，过滤已删除的{$lang->projectCommon}。<br/>
 <strong>按{$lang->projectCommon}统计的任务预计工时数</strong>：{$lang->projectCommon}中任务的预计工时数求和，过滤已删除的任务，过滤父任务，过滤已删除{$lang->execution->common}的任务，过滤已删除的{$lang->projectCommon}。";
