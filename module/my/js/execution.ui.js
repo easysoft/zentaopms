e536a1b8130af1fc4603ed956cd60acc1d429f0c
caoyanyi <caoyanyi@easycorp.ltd>
2024-01-26 09:10:11 +0800

    * Fix bug #44693.

--- module/my/js/execution.ui.js
+++ module/my/js/execution.ui.js
@@ -2,6 +2,7 @@ window.onRenderExecutionCell = function(result, info)
 {
     if(info.col.name === 'name' && systemMode == 'ALM')
     {
+        result[0].props.className = 'flex-1 w-0';
         result.push({html: '<span class="label size-sm circle ' + (info.row.data.type == 'stage' ? 'warning' : 'secondary') + '-pale">' + typeList[info.row.data.type] + '</span>'}, {className:'row-reverse'});
     }
 
