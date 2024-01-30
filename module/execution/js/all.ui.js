b5996d9378649c36513dbf8ccab253e81a070606
tianshujie <tianshujie@easycorp.ltd>
2024-01-30 10:46:21 +0800

    * Fix bug #42291.

--- module/execution/js/all.ui.js
+++ module/execution/js/all.ui.js
@@ -33,11 +33,13 @@ window.onRenderCell = function(result, {col, row})
         const executionLink = $.createLink('execution', 'task', `executionID=${row.data.rawID}`);
         const executionType = typeList[row.data.type];
 
-        let executionName   = `<span class='label secondary-pale'>${executionType}</span> `;
+        let executionName   = `<span class='label secondary-pale flex-none'>${executionType}</span> `;
+        executionName      += '<div class="ml-1 clip" style="width: max-content;">';
         executionName      += (!row.data.isParent) ? `<a href="${executionLink}" class="text-primary">${row.data.name}</a>` : row.data.name;
-        executionName      += (!['done', 'closed', 'suspended'].includes(row.data.status) && today > row.data.end) ? `<span class="label danger-pale ml-1">${delayed}</span>` : '';
+        executionName      += '</div>';
+        executionName      += (!['done', 'closed', 'suspended'].includes(row.data.status) && today > row.data.end) ? `<span class="label danger-pale ml-1 flex-none">${delayed}</span>` : '';
 
-        result[result.length] = {html: executionName};
+        result.push({html: executionName, className: 'w-full flex items-center'});
         return result;
     }
     if(['estimate', 'consumed','left'].includes(col.name) && result) result[0] = {html: result[0] + ' h'};
