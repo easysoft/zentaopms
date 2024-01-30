485efd62c51ff6c6e85bc6d17ecd9e96cf6871fe
sunhao <sunhao@easycorp.ltd>
2024-01-30 11:44:44 +0800

    * zin: refactor css in wg "detailSide".

--- lib/zin/wg/detailside/v1.php
+++ lib/zin/wg/detailside/v1.php
@@ -6,7 +6,13 @@ class detailSide extends wg
 {
     public static function getPageCSS(): string|false
     {
-        return file_get_contents(__DIR__ . DS . 'css' . DS . 'v1.css');
+        return <<<CSS
+        .detail-side {width: 370px;}
+        .detail-side .tab-content>.tab-pane {padding-left: 0 !important;}
+        .detail-side .tabs:not(:first-child) {border-top: 1px solid #E6EAF1;}
+        .detail-side .tabs {padding-top: 12px; padding-bottom: 20px;}
+        .detail-side > .table-data {width: 100%;}
+        CSS;
     }
 
     protected function build(): wg
