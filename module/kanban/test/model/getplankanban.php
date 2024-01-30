a1629f22e3d57fc1c7ea0ea9c7833a059c0a5486
wangyuting <wangyuting@easycorp.ltd>
2024-01-30 16:28:00 +0800

    * Fix getplankanban unit test bug.

--- module/kanban/test/model/getplankanban.php
+++ module/kanban/test/model/getplankanban.php
@@ -37,7 +37,7 @@ $branchIDList  = array('0', '1', '3');
 
 $kanban = new kanbanTest();
 
-r($kanban->getPlanKanbanTest($productIDList[0])[0]['items'][0]['data']['lanes'])   && p('0:name,title') && e('all,所有');                   // 测试获取产品1的计划看板
+r($kanban->getPlanKanbanTest($productIDList[0])[0]['items'][0]['data']['lanes'])   && p('0:name,title') && e('laneall,所有');                   // 测试获取产品1的计划看板
 r($kanban->getPlanKanbanTest($productIDList[1])[0]['items'][0]['data']['items']['laneall']['wait']) && p('1:name,title') && e('5,1.1');     // 测试获取产品2的计划看板
 r($kanban->getPlanKanbanTest($productIDList[2])[0]['items'][0]['data']['cols']) && p('0:name,title') && e('wait,未开始');                   // 测试获取产品3的计划看板
 r($kanban->getPlanKanbanTest($productIDList[0], $branchIDList[1])[0]['items'][0]['data']['cols']) && p('0:name,title') && e('wait,未开始'); // 测试获取产品1的计划看板,传入分支
