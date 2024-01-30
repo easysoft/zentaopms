4cc338bb2dc142aa8fb0e910380f1b6d08b4bfb7
tianshujie <tianshujie@easycorp.ltd>
2024-01-30 10:43:20 +0800

    * Adjust execution name style.

--- module/my/js/execution.ui.js
+++ module/my/js/execution.ui.js
@@ -2,10 +2,16 @@ window.onRenderExecutionCell = function(result, info)
 {
     if(info.col.name === 'name' && systemMode == 'ALM')
     {
-        result[0].props.className = 'flex-1 w-0';
+        if(typeof result[0] == 'string')
+        {
+            result[0] = {html: result[0], className: 'flex-1 w-0'};
+        }
+        else
+        {
+            result[0].props.className = 'flex-1 w-0';
+        }
         result.push({html: '<span class="label size-sm circle ' + (info.row.data.type == 'stage' ? 'warning' : 'secondary') + '-pale">' + typeList[info.row.data.type] + '</span>'}, {className:'row-reverse'});
     }
 
     return result;
 }
-
