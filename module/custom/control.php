526a842a7e6246bef743dc627af6e1fc42abef25
liumengyi <liumengyi@easycorp.ltd>
2024-01-31 10:17:55 +0800

    * Fix bug #44633.

--- module/custom/control.php
+++ module/custom/control.php
@@ -398,6 +398,7 @@ class custom extends control
         {
             $fields = $this->post->fields;
             if(is_array($fields)) $fields = implode(',', $fields);
+            if($module == 'execution' && $section == 'custom' && $key == 'createFields' && strpos(",{$fields},", ',team,') !== false) $fields .= ',teams';
             $this->loadModel('setting')->setItem("{$account}.{$module}.{$section}.{$key}", $fields);
             if(in_array($module, array('story', 'task', 'testcase')) && $section == 'custom' && $key == 'batchCreateFields') return;
             if($module == 'bug' && $section == 'custom' && $key == 'batchCreateFields') return;
