b4cb7a6c5abfc7fe41e378ef453e2fc5e2bdea13
caoyanyi <caoyanyi@easycorp.ltd>
2024-02-05 09:19:10 +0800

    * Adjust widget params.

--- module/datatable/ui/ajaxcustom.html.php
+++ module/datatable/ui/ajaxcustom.html.php
@@ -55,7 +55,7 @@ function buildItem(array $item): wg
                 ),
                 select
                 (
-                    set::name(''),
+                    set::name('width'),
                     setClass('size-sm w-12'),
                     set::name('width'),
                     set::required(true),
