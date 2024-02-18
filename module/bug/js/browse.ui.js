ef108d337d171df09fecad8b8fa31635d48ce961
daitingting <daitingting@xirangit.com>
2024-02-18 02:30:14 +0000

    * Fix bug #45862.

--- module/bug/js/browse.ui.js
+++ module/bug/js/browse.ui.js
@@ -31,10 +31,10 @@ window.onRenderCell = function(result, {row, col})
 {
     if(result && col.name == 'title')
     {
-        result[0].props.className = 'w-0 flex-1';
+        result[0].props.className = 'w-0 flex-1 overflow-hidden';
         if(row.data.color) result[0].props.style = 'color: ' + row.data.color;
         const module = this.options.modules[row.data.module];
-        if(module) result.unshift({html: '<span class="label gray-pale rounded-full">' + module + '</span>'}); // 添加模块标签
+        if(module) result.unshift({html: '<span class="label gray-pale rounded-full whitespace-nowrap w-auto">' + module + '</span>'}); // 添加模块标签
 
         if(parseInt(row.data.case))
         {
