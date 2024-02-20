6b45ae443b267a5108bc6f6148d9e50dab3bbdf3
wangyuting <wangyuting@easycorp.ltd>
2024-02-20 17:05:36 +0800

    * Optimize checkcolorgroup css.

--- lib/zin/wg/checkcolorgroup/v1.php
+++ lib/zin/wg/checkcolorgroup/v1.php
@@ -9,10 +9,10 @@ class checkColorGroup extends checkBtnGroup
     public static function getPageCSS(): string|false
     {
         return <<<CSS
-        .check-btn-group {gap: 1px; padding: 0; border-radius: var(--radius)}
+        .check-btn-group {gap: 1px; padding: 0; border-radius: var(--radius); margin-top: 4px;}
         .check-btn-group > .check-btn {flex: auto; position: relative; display: flex; flex-direction: column; align-items: center; justify-content: center;}
         .check-btn-group > .check-btn + .check-btn::before {content: ' '; display: block; position: absolute; left: -1px; top: 8px; bottom: 8px; width: 1px; background: var(--color-border)}
-        .check-btn-group > .check-btn > label {width: 24px; height: 24px; margin-right: 2px; --tw-ring-color: transparent; border-radius: var(--radius-full);}
+        .check-btn-group > .check-btn > label {width: 20px; height: 20px; padding: 0; margin-right: 6px; --tw-ring-color: transparent; border-radius: var(--radius-full);}
         CSS;
     }
 
