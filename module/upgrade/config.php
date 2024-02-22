c00da77e460cc7a0687a14262b78a47c5f47359d
liugang <liugang@easycorp.ltd>
2024-02-22 09:37:33 +0800

    + upgrade: add configuration of files to delete.

--- module/upgrade/config.php
+++ module/upgrade/config.php
@@ -936,6 +936,14 @@ $config->delete['18_9'][] = 'module/automation/lang/en.php';
 $config->delete['18_9'][] = 'module/automation/view/browse.html.php';
 
 $config->delete['20.0.beta1'][] = 'extension/lite/kanban/ext/model/lite.php';
+$config->delete['20.0.beta1'][] = 'extension/biz/my/ext/model/class/effort.class.php';
+$config->delete['20.0.beta1'][] = 'extension/max/my/ext/model/class/effort.class.php';
+$config->delete['20.0.beta1'][] = 'extension/biz/todo/ext/control/batchcreate.php';
+$config->delete['20.0.beta1'][] = 'extension/max/todo/ext/control/batchcreate.php';
+$config->delete['20.0.beta1'][] = 'extension/biz/bug/ext/control/export.php';
+$config->delete['20.0.beta1'][] = 'extension/max/bug/ext/control/export.php';
+$config->delete['20.0.beta1'][] = 'extension/biz/story/ext/control/export.php';
+$config->delete['20.0.beta1'][] = 'extension/max/story/ext/control/export.php';
 
 $config->upgrade->openModules = array('action', 'admin', 'ai', 'api', 'automation', 'backup', 'block', 'branch', 'budget', 'bug', 'build', 'caselib', 'chart', 'ci', 'client', 'common', 'company', 'compile', 'convert', 'cron', 'custom', 'datatable', 'dataview', 'dept', 'design', 'dev', 'dimension', 'doc', 'durationestimation', 'entry', 'execution', 'extension', 'file', 'git', 'gitlab', 'group', 'holiday', 'im', 'index', 'index.html', 'install', 'issue', 'jenkins', 'job', 'kanban', 'license', 'mail', 'message', 'metric', 'misc', 'mr', 'my', 'personnel', 'pipeline', 'product', 'productplan', 'productset', 'program', 'programplan', 'project', 'projectbuild', 'projectplan', 'projectrelease', 'projectstory', 'pivot', 'qa', 'release', 'repo', 'report', 'risk', 'score', 'screen', 'search', 'setting', 'sonarqube', 'sso', 'stage', 'stakeholder', 'story', 'subject', 'svn', 'task', 'testcase', 'testreport', 'testsuite', 'testtask', 'todo', 'tree', 'tutorial', 'upgrade', 'user', 'webhook', 'weekly', 'workestimation', 'gitea', 'gogs', 'transfer', 'zahost', 'zanode', 'editor', 'charter', 'roadmap', 'account', 'artifactrepo', 'cne', 'host', 'instance', 'ops', 'serverroom', 'space', 'store', 'system', 'solution');
 
