eda155ad3c252a7f37a7e1b5748913381f70ea0d
tanghucheng <tanghucheng@easycorp.ltd>
2024-02-26 16:30:05 +0800

    * Fix bug #46083.

--- module/project/model.php
+++ module/project/model.php
@@ -1456,6 +1456,13 @@ class projectModel extends model
                 $this->dao->update(TABLE_PRODUCT)->set('`order`')->eq($productID * 5)->where('id')->eq($productID)->exec();
                 if($product->acl != 'open') $this->loadModel('user')->updateUserView($productID, 'product');
 
+                $productSettingList = isset($this->config->global->productSettingList) ? json_decode($this->config->global->productSettingList, true) : array();
+                if($productSettingList)
+                {
+                    $productSettingList[] = $productID;
+                    $this->loadModel('setting')->setItem('system.common.global.productSettingList', json_encode($productSettingList));
+                }
+
                 $projectProduct = new stdclass();
                 $projectProduct->project = $projectID;
                 $projectProduct->product = $productID;
