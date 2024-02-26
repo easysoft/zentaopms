41ab3ac087987c9cba09382520ea3c15cceb23d2
wangyuting <wangyuting@easycorp.ltd>
2024-02-26 14:07:56 +0800

    * Finish scrum risk block and waterfall risk block. task #112305.

---
+++ module/block/ui/scrumriskblock.html.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+/**
+* The scrum risk block view file of block module of ZenTaoPMS.
+* @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+* @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+* @author      Yuting Wang <wangyuting@easycorp.ltd>
+* @package     block
+* @link        https://www.zentao.net
+*/
+
+namespace zin;
+
+include 'riskblock.html.php';
