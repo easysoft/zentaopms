814f9d1ea685e57a8ff2fc162d3b729de8d75c18
wangyuting <wangyuting@easycorp.ltd>
2024-02-26 14:07:20 +0800

    * Finish scrum issue block and waterfall issue block. task #112304.

---
+++ module/block/ui/issueblock.html.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+/**
+* The issue block view file of block module of ZenTaoPMS.
+* @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+* @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+* @author      Yuting Wang <wangyuting@easycorp.ltd>
+* @package     block
+* @link        https://www.zentao.net
+*/
+
+namespace zin;
+
+$cols = $longBlock ? $config->block->issue->dtable->fieldList : $config->block->issue->dtable->short->fieldList;
+
+blockPanel
+(
+    setClass('issue-block list-block'),
+    dtable
+    (
+        setID('issue'),
+        set::bordered(false),
+        set::horzScrollbarPos('inside'),
+        set::cols(array_values($cols)),
+        set::data(array_values($issues))
+    )
+);
+
+render();
