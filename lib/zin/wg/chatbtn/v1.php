96a6a0040a64b8fd864700f7b1b9a342338dd9f1
liwenrui <liwenrui@easycorp.ltd>
2024-02-29 08:37:10 +0800

    * fix weird behaviors of ai chat.

--- lib/zin/wg/chatbtn/v1.php
+++ lib/zin/wg/chatbtn/v1.php
@@ -7,9 +7,9 @@ class chatBtn extends wg
     public static function getPageCSS(): string|false
     {
         return <<<CSS
-            #chat-container {position: fixed; left: 96px; right: 0; height: calc(100% - 40px); display: none;}
-            #ai-chat-view {position: fixed; right: 0; width: 330px; bottom: 40px; top: 49px; outline: 1px solid; outline-color: rgba(var(--color-border-rgb),var(--tw-border-opacity))}
-            #ai-chat-frame {height: 100%; width: 100%;}
+        #chat-container {position: fixed; left: 96px; right: 0; height: calc(100% - 40px); display: none;}
+        #ai-chat-view {position: fixed; right: 0; width: 330px; bottom: 40px; top: 49px; outline: 1px solid; outline-color: rgba(var(--color-border-rgb),var(--tw-border-opacity))}
+        #ai-chat-frame {height: 100%; width: 100%;}
         CSS;
     }
 
@@ -17,29 +17,29 @@ class chatBtn extends wg
     {
         $aiChatURL = createLink('ai', 'chat');
         $chatContainer = <<<HTML
-            <div id="chat-container">
-                <div id="ai-chat-view">
-                    <iframe id="ai-chat-frame" src="{$aiChatURL}" frameborder="no" allowtransparency="true" scrolling="auto" hidefocus></iframe>
-                </div>
+        <div id="chat-container">
+            <div id="ai-chat-view">
+                <iframe id="ai-chat-frame" src="{$aiChatURL}" frameborder="no" allowtransparency="true" scrolling="auto" hidefocus></iframe>
             </div>
+        </div>
         HTML;
         return <<<JAVASCRIPT
-            window.toggleChatContainer = () => {
-                if(!$('#chat-container').length)
-                {
-                    $('body').append(`{$chatContainer}`);
-                    $('#ai-chat-frame').on('load', () => {window.toggleChatContainer();});
-                    return;
-                }
-                $('#chat-btn').toggleClass('active');
-                $('#chat-container').toggle();
-            }
-            $(document).on('click', e =>
+        window.toggleChatContainer = () => {
+            if(!$('#chat-container').length)
             {
-                if(!$('#chat-container').isVisible()) return;
-                if($(e.target).closest('#ai-chat-view,#chat-btn').length) return;
-                window.toggleChatContainer();
-            });
+                $('body').append(`{$chatContainer}`);
+                $('#ai-chat-frame').on('load', () => {window.toggleChatContainer();});
+                return;
+            }
+            $('#chat-btn').toggleClass('active');
+            $('#chat-container').toggle();
+        }
+        $(document).on('click', e =>
+        {
+            if($('#chat-container').prop('style') && $('#chat-container').prop('style').display !== 'block') return;
+            if($(e.target).closest('#ai-chat-view,#chat-btn').length) return;
+            window.toggleChatContainer();
+        });
         JAVASCRIPT;
     }
 
@@ -54,7 +54,7 @@ class chatBtn extends wg
                 setClass('ghost px-1'),
                 set('square', false),
                 set::icon(img(set('src', 'static/svg/ai-chat.svg'))),
-                on::click('window.toggleChatContainer()')
+                set('onclick', 'window.toggleChatContainer()')
             )
         );
     }
