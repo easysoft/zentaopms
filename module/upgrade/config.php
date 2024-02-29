a5241de8dd0fbcfa26955ae1076d7d5e87bf3ae0
tianshujie <tianshujie@easycorp.ltd>
2024-02-29 09:29:42 +0800

    + Add deleted file config for excel plugin.

--- module/upgrade/config.php
+++ module/upgrade/config.php
@@ -994,6 +994,8 @@ $config->delete['20.0.beta1'][] = 'extension/biz/effort/view/effortsforobject.ht
 $config->delete['20.0.beta1'][] = 'extension/max/effort/view/effortsforobject.html.php';
 $config->delete['20.0.beta1'][] = 'extension/biz/user/ext/view/featurebar.calendar.html.hook.php';
 $config->delete['20.0.beta1'][] = 'extension/max/user/ext/view/featurebar.calendar.html.hook.php';
+$config->delete['20.0.beta1'][] = 'extension/biz/bug/ext/view/import.html.php';
+$config->delete['20.0.beta1'][] = 'extension/max/bug/ext/view/import.html.php';
 
 $config->upgrade->openModules = array('action', 'admin', 'ai', 'api', 'automation', 'backup', 'block', 'branch', 'budget', 'bug', 'build', 'caselib', 'chart', 'ci', 'client', 'common', 'company', 'compile', 'convert', 'cron', 'custom', 'datatable', 'dataview', 'dept', 'design', 'dev', 'dimension', 'doc', 'durationestimation', 'entry', 'execution', 'extension', 'file', 'git', 'gitlab', 'group', 'holiday', 'im', 'index', 'index.html', 'install', 'issue', 'jenkins', 'job', 'kanban', 'license', 'mail', 'message', 'metric', 'misc', 'mr', 'my', 'personnel', 'pipeline', 'product', 'productplan', 'productset', 'program', 'programplan', 'project', 'projectbuild', 'projectplan', 'projectrelease', 'projectstory', 'pivot', 'qa', 'release', 'repo', 'report', 'risk', 'score', 'screen', 'search', 'setting', 'sonarqube', 'sso', 'stage', 'stakeholder', 'story', 'subject', 'svn', 'task', 'testcase', 'testreport', 'testsuite', 'testtask', 'todo', 'tree', 'tutorial', 'upgrade', 'user', 'webhook', 'weekly', 'workestimation', 'gitea', 'gogs', 'transfer', 'zahost', 'zanode', 'editor', 'charter', 'roadmap', 'account', 'artifactrepo', 'cne', 'host', 'instance', 'ops', 'serverroom', 'space', 'store', 'system', 'solution');
 
