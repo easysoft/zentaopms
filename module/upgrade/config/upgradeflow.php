b5aeb0247e39aa877ebee0e5bb68d580838f0c45
liugang <liugang@easycorp.ltd>
2024-02-29 16:36:53 +0800

    + upgrade: add method to check database consistency.

--- module/upgrade/config/upgradeflow.php
+++ module/upgrade/config/upgradeflow.php
@@ -93,7 +93,7 @@ $config->upgrade->execFlow['18_7']        = array('functions' => 'processHistory
 $config->upgrade->execFlow['18_8']        = array('functions' => 'upgradeTesttaskMembers,deleteGeneralReportBlock,stopOldCron');
 $config->upgrade->execFlow['18_9']        = array('functions' => 'addDefaultTraincoursePriv,renameBIModule,migrateXuanClientSettings');
 $config->upgrade->execFlow['18_10_1']     = array('functions' => 'migrateAIModelConfig');
-$config->upgrade->execFlow['20.0.alpha1'] = array('functions' => 'revertStoryCustomFields,hideOA,updateMetricDateType,migrateAIModelConfig');
+$config->upgrade->execFlow['20_0_alpha1'] = array('functions' => 'revertStoryCustomFields,hideOA,updateMetricDateType,migrateAIModelConfig,update1810,update18101');
 
 if(!empty($config->isINT))
 {
