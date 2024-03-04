6360f4914eede3ab571c24ff0d834c648fcc567f
liumengyi <liumengyi@easycorp.ltd>
2024-03-04 15:49:10 +0800

    * Fix bug #46468.

--- module/todo/ui/edit.html.php
+++ module/todo/ui/edit.html.php
@@ -452,6 +452,7 @@ formPanel
                         'id'      => 'private',
                         'name'    => 'private',
                         'text'    => $lang->todo->private,
+                        'value'   => !empty($todo->private) ? 'on' : '0',
                         'checked' => !empty($todo->private)
                     )
                 ),
