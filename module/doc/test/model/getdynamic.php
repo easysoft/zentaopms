5978684c4cebe90fd0c0ad2cc7f3dea01a6e447c
tianshujie <tianshujie@easycorp.ltd>
2024-03-04 13:32:55 +0800

    * Change the result of doc::getdynamic unit test.

--- module/doc/test/model/getdynamic.php
+++ module/doc/test/model/getdynamic.php
@@ -27,9 +27,9 @@ $recPerPages = array(5, 10, 20);
 $pagerIds    = array(1, 2);
 
 $docTester = new docTest();
-r($docTester->getDynamicTest($recPerPages[0], $pagerIds[0])) && p() && e('32;31;30;29;28;');                                              // 获取每页5条，第1页动态数据
-r($docTester->getDynamicTest($recPerPages[0], $pagerIds[1])) && p() && e('27;26;25;24;23;');                                              // 获取每页5条，第2页动态数据
-r($docTester->getDynamicTest($recPerPages[1], $pagerIds[0])) && p() && e('32;31;30;29;28;27;26;25;24;23;');                               // 获取每页10条，第1页动态数据
-r($docTester->getDynamicTest($recPerPages[1], $pagerIds[1])) && p() && e('22;21;20;19;18;50;17;49;16;48;');                               // 获取每页10条，第2页动态数据
-r($docTester->getDynamicTest($recPerPages[2], $pagerIds[0])) && p() && e('32;31;30;29;28;27;26;25;24;23;22;21;20;19;18;50;17;49;16;48;'); // 获取每页20条，第1页动态数据
-r($docTester->getDynamicTest($recPerPages[2], $pagerIds[1])) && p() && e('15;47;14;46;13;45;12;44;11;43;10;42;9;41;8;40;7;39;6;38;');     // 获取每页20条，第2页动态数据
+r($docTester->getDynamicTest($recPerPages[0], $pagerIds[0])) && p() && e('30;29;28;27;26;');                                              // 获取每页5条，第1页动态数据
+r($docTester->getDynamicTest($recPerPages[0], $pagerIds[1])) && p() && e('25;24;23;22;21;');                                              // 获取每页5条，第2页动态数据
+r($docTester->getDynamicTest($recPerPages[1], $pagerIds[0])) && p() && e('30;29;28;27;26;25;24;23;22;21;');                               // 获取每页10条，第1页动态数据
+r($docTester->getDynamicTest($recPerPages[1], $pagerIds[1])) && p() && e('20;50;19;49;18;48;17;47;16;46;');                               // 获取每页10条，第2页动态数据
+r($docTester->getDynamicTest($recPerPages[2], $pagerIds[0])) && p() && e('30;29;28;27;26;25;24;23;22;21;20;50;19;49;18;48;17;47;16;46;'); // 获取每页20条，第1页动态数据
+r($docTester->getDynamicTest($recPerPages[2], $pagerIds[1])) && p() && e('15;45;14;44;13;43;12;42;11;41;10;40;9;39;8;38;7;37;6;36;');     // 获取每页20条，第2页动态数据
