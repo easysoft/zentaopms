6ca4eb28829ce8c5644423bbcecbc1f4b735b6a9
tianshujie <tianshujie@easycorp.ltd>
2024-03-04 13:51:14 +0800

    - Remove unused param of the group::updateprojectadmin unit test.

--- module/group/test/model/updateprojectadmin.php
+++ module/group/test/model/updateprojectadmin.php
@@ -43,17 +43,17 @@ $allProducts   = array('program' => array(),      'project' => array(),      'pr
 $allProjects   = array('program' => array(),      'project' => array('all'), 'product' => array(),      'execution' => array(),      'accounts' => array('user1,user2'));
 $allExecutions = array('program' => array(),      'project' => array(),      'product' => array(),      'execution' => array('all'), 'accounts' => array('user1,user2'));
 
-r($group->updateProjectAdminTest(1, array($programs1)))   && p('user1:programs')     && e('2'); // 测试项目管理员更新项目集
-r($group->updateProjectAdminTest(1, array($products1)))   && p('user1:products')     && e('2'); // 测试项目管理员更新产品
-r($group->updateProjectAdminTest(1, array($projects1)))   && p('user1:projects')     && e('2'); // 测试项目管理员更新项目
-r($group->updateProjectAdminTest(1, array($executions1))) && p('user1:executions')   && e('2'); // 测试项目管理员更新执行
+r($group->updateProjectAdminTest(array($programs1)))   && p('user1:programs')     && e('2'); // 测试项目管理员更新项目集
+r($group->updateProjectAdminTest(array($products1)))   && p('user1:products')     && e('2'); // 测试项目管理员更新产品
+r($group->updateProjectAdminTest(array($projects1)))   && p('user1:projects')     && e('2'); // 测试项目管理员更新项目
+r($group->updateProjectAdminTest(array($executions1))) && p('user1:executions')   && e('2'); // 测试项目管理员更新执行
 
-r($group->updateProjectAdminTest(1, array($programs2)))   && p('user1,user2:programs')   && e('2'); // 测试项目管理员更新多人项目集
-r($group->updateProjectAdminTest(1, array($products2)))   && p('user1,user2:products')   && e('2'); // 测试项目管理员更新多人产品
-r($group->updateProjectAdminTest(1, array($projects2)))   && p('user1,user2:projects')   && e('2'); // 测试项目管理员更新多人项目
-r($group->updateProjectAdminTest(1, array($executions2))) && p('user1,user2:executions') && e('2'); // 测试项目管理员更新多人执行
+r($group->updateProjectAdminTest(array($programs2)))   && p('user1,user2:programs')   && e('2'); // 测试项目管理员更新多人项目集
+r($group->updateProjectAdminTest(array($products2)))   && p('user1,user2:products')   && e('2'); // 测试项目管理员更新多人产品
+r($group->updateProjectAdminTest(array($projects2)))   && p('user1,user2:projects')   && e('2'); // 测试项目管理员更新多人项目
+r($group->updateProjectAdminTest(array($executions2))) && p('user1,user2:executions') && e('2'); // 测试项目管理员更新多人执行
 
-r($group->updateProjectAdminTest(1, array($allPrograms)))   && p('user1,user2:programs')   && e('all'); // 测试项目管理员更新所有项目集
-r($group->updateProjectAdminTest(1, array($allProducts)))   && p('user1,user2:products')   && e('all'); // 测试项目管理员更新所有项目集
-r($group->updateProjectAdminTest(1, array($allProjects)))   && p('user1,user2:projects')   && e('all'); // 测试项目管理员更新所有项目集
-r($group->updateProjectAdminTest(1, array($allExecutions))) && p('user1,user2:executions') && e('all'); // 测试项目管理员更新所有项目集
+r($group->updateProjectAdminTest(array($allPrograms)))   && p('user1,user2:programs')   && e('all'); // 测试项目管理员更新所有项目集
+r($group->updateProjectAdminTest(array($allProducts)))   && p('user1,user2:products')   && e('all'); // 测试项目管理员更新所有项目集
+r($group->updateProjectAdminTest(array($allProjects)))   && p('user1,user2:projects')   && e('all'); // 测试项目管理员更新所有项目集
+r($group->updateProjectAdminTest(array($allExecutions))) && p('user1,user2:executions') && e('all'); // 测试项目管理员更新所有项目集
