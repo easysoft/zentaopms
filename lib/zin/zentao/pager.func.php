288a99cb8890e0bacdeb5f53d665363dbebe044f
qixinzhi <qixinzhi@easycorp.ltd>
2024-03-06 14:41:13 +0800

    * Update usePager of zin.

--- lib/zin/zentao/pager.func.php
+++ lib/zin/zentao/pager.func.php
@@ -10,7 +10,7 @@ namespace zin;
  * @param ?array $userSetting
  * @return array
  */
-function usePager(string|array $pagerName = 'pager', string $extra = '', ?array $userSetting = null): ?array
+function usePager(string|array $pagerName = 'pager', string $extra = '', ?array $userSetting = null, ?array $sizeMenuItems = null): ?array
 {
     if(is_array($pagerName))
     {
@@ -49,7 +49,7 @@ function usePager(string|array $pagerName = 'pager', string $extra = '', ?array 
     }
     elseif($extra == 'shortPageSize')
     {
-        $setting['items'][] = array('type' => 'size-menu', 'text' => str_replace('<strong>', '', str_replace('</strong>', '', $pager->lang->pager->shortPageSize)), 'dropdown' => array('placement' => 'top'), 'itemProps' => array('onClick' => jsRaw("(e, item) => $.cookie.set('$pager->pageCookie', item.text)")));
+        $setting['items'][] = array('type' => 'size-menu', 'text' => str_replace('<strong>', '', str_replace('</strong>', '', $pager->lang->pager->shortPageSize)), 'dropdown' => array('placement' => 'top'), 'itemProps' => array('onClick' => jsRaw("(e, item) => $.cookie.set('$pager->pageCookie', item.key)")));
         $setting['items'][] = array('type' => 'link', 'page' => 'prev', 'hint' => $pager->lang->pager->previousPage, 'icon' => 'icon-angle-left');
         $setting['items'][] = array('type' => 'info', 'text' => '{page}/{pageTotal}');
         $setting['items'][] = array('type' => 'link', 'page' => 'next', 'hint' => $pager->lang->pager->nextPage, 'icon' => 'icon-angle-right');
@@ -57,7 +57,18 @@ function usePager(string|array $pagerName = 'pager', string $extra = '', ?array 
     else
     {
         $setting['items'][] = array('type' => 'info', 'text' => $pager->lang->pager->totalCountAB);
-        if(strpos(',short,', ",{$extra},") === false) $setting['items'][] = array('type' => 'size-menu', 'text' => str_replace('<strong>', '', str_replace('</strong>', '', $pager->lang->pager->pageSize)), 'dropdown' => array('placement' => 'top'), 'itemProps' => array('onClick' => jsRaw("(e, item) => $.cookie.set('$pager->pageCookie', item.text)")));
+        if(strpos(',short,', ",{$extra},") === false)
+        {
+            $sizeMenuSetting = array
+            (
+                'type'      => 'size-menu',
+                'text'      => str_replace('<strong>', '', str_replace('</strong>', '', $pager->lang->pager->pageSize)),
+                'dropdown'  => array('placement' => 'top'),
+                'itemProps' => array('onClick' => jsRaw("(e, item) => $.cookie.set('$pager->pageCookie', item.key)"))
+            );
+            if($sizeMenuItems) $sizeMenuSetting['items'] = $sizeMenuItems;
+            $setting['items'][] = $sizeMenuSetting;
+        }
         $setting['items'][] = array('type' => 'link', 'page' => 'first', 'hint' => $pager->lang->pager->firstPage, 'icon' => 'icon-first-page');
         $setting['items'][] = array('type' => 'link', 'page' => 'prev', 'hint' => $pager->lang->pager->previousPage, 'icon' => 'icon-angle-left');
         $setting['items'][] = array('type' => 'info', 'text' => '{page}/{pageTotal}');
