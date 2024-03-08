53851187bfd8491af3a2973893d82ee173aaf129
liyang <liyang@easycorp.ltd>
2024-03-08 09:41:36 +0800

    * Shield the branch field in the bug list under execution.

--- module/datatable/control.php
+++ module/datatable/control.php
@@ -162,6 +162,7 @@ class datatable extends control
         if($module == 'execution' && $method == 'bug')
         {
             unset($cols['execution']);
+            unset($cols['branch']);
             $execution = $this->loadModel('execution')->getByID($this->session->execution);
             $project   = $this->loadModel('project')->getByID($execution->project);
             if(!$project->hasProduct && $project->model != 'scrum') unset($cols['plan']);
