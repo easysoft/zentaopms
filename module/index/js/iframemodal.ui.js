59be4a259aa1e741a54d4cb98f97170010ea1047
sunhao <sunhao@easycorp.ltd>
2024-03-08 14:20:59 +0800

    * zin: fix iframe modal for old page.

--- module/index/js/iframemodal.ui.js
+++ module/index/js/iframemodal.ui.js
@@ -1,4 +1,5 @@
 $.fn.triggerHandler = $.fn.trigger;
+['registerRender', 'fetchContent', 'loadTable', 'loadPage', 'postAndLoadPage', 'loadCurrentPage', 'parseSelector', 'toggleLoading', 'openUrl', 'openPage', 'goBack', 'registerTimer', 'loadModal', 'loadTarget', 'loadComponent', 'loadPartial', 'reloadPage', 'selectLang', 'selectTheme', 'selectVision', changeAppLang, 'changeAppTheme', 'setImageSize', 'showMoreImage', 'autoLoad', 'loadForm'].forEach(function(name){window[name] = parent.parent[name];});
 
 $(function()
 {
@@ -6,12 +7,9 @@ $(function()
 
     function resizeModal()
     {
-        const $modal = $('.modal-body');
-        const width = $modal.outerWidth();
+        const $modal = $('body>.modal-dialog>.modal-content>.modal-body,.modal-body').first();
         const height = $modal.outerHeight();
         $modal.closest('body').height(height);
-        const $iframe = parent.$('iframe[name="' + window.name + '"]');
-        $iframe.closest('.modal-dialog').css({width: width + 10});
     }
 
     $.ajax(
@@ -23,7 +21,7 @@ $(function()
             const $body = $('body');
             $body.html(data);
             const resizeOb = new ResizeObserver(resizeModal);
-            resizeOb.observe($('.modal-body')[0]);
+            resizeOb.observe($('body>.modal-dialog>.modal-content>.modal-body,.modal-body')[0]);
             resizeModal();
             $body.zuiInit().removeClass('invisible');
         }
