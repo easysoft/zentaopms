8143818491c9da82a71384e9815311935c90e97b
sunhao <sunhao@easycorp.ltd>
2024-03-11 10:18:17 +0800

    * my: refactor js code.

--- module/my/js/common.ui.js
+++ module/my/js/common.ui.js
@@ -2,31 +2,18 @@ $(function()
 {
     if(typeof mode === 'string')
     {
-        $('#mainNavbar li[data-id=' + mode + ']').addClass('active');
+        const $mainNavbar = $('#mainNavbar');
+        $mainNavbar.find('.nav-item > a[data-id=' + mode + ']').addClass('active');
         if(typeof rawMethod === 'string' && rawMethod == 'work')
         {
-            if($("#mainNavbar li.nav-item a[data-id='task'] .label").length > 0) return false;
+            if($mainNavbar.find('.nav-item > a[data-id=task] .label').length) return false;
 
-            $("#mainNavbar li.nav-item a[data-id='task']").append('<span class="label rounded-2xl gray-pale h-4">' + todoCount.task + '</span>');
-            $("#mainNavbar li.nav-item a[data-id='story']").append('<span class="label rounded-2xl gray-pale h-4">' + todoCount.story + '</span>');
-            $("#mainNavbar li.nav-item a[data-id='bug']").append('<span class="label rounded-2xl gray-pale h-4">' + todoCount.bug + '</span>');
-            $("#mainNavbar li.nav-item a[data-id='testcase']").append('<span class="label rounded-2xl gray-pale h-4">' + todoCount.case + '</span>');
-            $("#mainNavbar li.nav-item a[data-id='testtask']").append('<span class="label rounded-2xl gray-pale h-4">' + todoCount.testtask + '</span>');
+            const countMap = {task: 'task', story: 'story', bug: 'bug', testcase: 'case', testtask: 'testtask'};
+            if(isOpenedURAndSR !== 0)      $.extend(countMap, {requirement: 'requirement'});
+            if(isBiz !== 0 || isMax !== 0) $.extend(countMap, {feedback: 'feedback', ticket: 'ticket'});
+            if(isMax !== 0)                $.extend(countMap, {issue: 'issue', risk: 'risk', nc: 'qa', myMeeting: 'meeting'});
 
-            if(isOpenedURAndSR !== 0) $("#mainNavbar li.nav-item a[data-id='requirement']").append('<span class="label rounded-2xl gray-pale h-4">' + todoCount.requirement + '</span>');
-
-            if(isBiz !== 0 || isMax !== 0)
-            {
-                $("#mainNavbar li.nav-item a[data-id='feedback']").append('<span class="label rounded-2xl gray-pale h-4">' + todoCount.feeckback + '</span>');
-                $("#mainNavbar li.nav-item a[data-id='ticket']").append('<span class="label rounded-2xl gray-pale h-4">' + todoCount.ticket + '</span>');
-            }
-            if(isMax !== 0)
-            {
-                $("#mainNavbar li.nav-item a[data-id='issue']").append('<span class="label rounded-2xl gray-pale h-4">' + todoCount.issue + '</span>');
-                $("#mainNavbar li.nav-item a[data-id='risk']").append('<span class="label rounded-2xl gray-pale h-4">' + todoCount.risk + '</span>');
-                $("#mainNavbar li.nav-item a[data-id='nc']").append('<span class="label rounded-2xl gray-pale h-4">' + todoCount.qa + '</span>');
-                $("#mainNavbar li.nav-item a[data-id='myMeeting']").append('<span class="label rounded-2xl gray-pale h-4">' + todoCount.meeting + '</span>');
-            }
+            $.each(countMap, (name, key) => $mainNavbar.find('.nav-item > a[data-id=' + name + ']').append('<span class="label rounded-full gray-pale size-sm">' + todoCount[key] + '</span>'));
         }
     }
 });
