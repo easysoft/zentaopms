e0756c2a3565fd91907199930dface0fc8192557
qixinzhi <qixinzhi@easycorp.ltd>
2024-03-11 15:48:54 +0800

    * Fix can not delist metric with builtin.

--- module/metric/zen.php
+++ module/metric/zen.php
@@ -543,7 +543,7 @@ class metricZen extends metric
         {
             $metric->canEdit      = $metric->stage == 'wait';
             $metric->canImplement = ($metric->stage == 'wait' && !$this->metric->isOldMetric($metric) && $metric->builtin === '0');
-            $metric->canDelist    = $metric->stage == 'released';
+            $metric->canDelist    = $metric->stage == 'released' && $metric->builtin === '0';
         }
         return $metrics;
     }
