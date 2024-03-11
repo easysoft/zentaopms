d0eb29a21c198cefaecf130917639926bcee62a9
qixinzhi <qixinzhi@easycorp.ltd>
2024-03-11 16:44:33 +0800

    * Fix delist metric in view page.

--- module/metric/js/browse.ui.js
+++ module/metric/js/browse.ui.js
@@ -1,19 +1,4 @@
-/**
- * 提示并下架度量项。
- * Delist metric with tips.
- *
- * @param  int    metricID
- * @param  string metricName
- * @access public
- * @return void
- */
-window.confirmDelist = function(metricID, metricName)
-{
-    zui.Modal.confirm(confirmDelist.replace('%s', metricName)).then((res) =>
-    {
-        if(res) $.ajaxSubmit({url: $.createLink('metric', 'delist', 'metricID=' + metricID)});
-    });
-};
+
 
 window.onRenderCell = function(result, {row, col})
 {
