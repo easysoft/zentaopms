bc2e8c07d81320db6de2133ddff5b4775eba831e
sunguangming <sunguangming@easycorp.ltd>
2024-03-11 14:16:27 +0800

    * Add unit test.

--- module/custom/test/model/setstorygrade.php
+++ module/custom/test/model/setstorygrade.php
@@ -7,9 +7,19 @@ su('admin');
 
 /**
 
-title=测试 customModel->setConcept();
+title=测试 customModel->setStoryGrade();
 timeout=0
 cid=1
+
+- 查看插入的等级数量 @5
+- 查看插入的等级1详情
+ - 第0条的grade属性 @1
+ - 第0条的name属性 @一级
+ - 第0条的status属性 @enable
+- 查看插入的等级5详情
+ - 第4条的grade属性 @5
+ - 第4条的name属性 @五级
+ - 第4条的status属性 @enable
 
 */
 
@@ -29,3 +39,10 @@ $data['gradeName'] = array(
     4 => '五级'
 );
 
+global $tester;
+$tester->loadModel('custom')->setStoryGrade('story', $data);
+$gradeSetting = $tester->loadModel('story')->getGradeSetting();
+
+r(count($gradeSetting)) && p()                      && e('5');              // 查看插入的等级数量
+r($gradeSetting)        && p('0:grade,name,status') && e('1,一级,enable');  // 查看插入的等级1详情
+r($gradeSetting)        && p('4:grade,name,status') && e('5,五级,enable');  // 查看插入的等级5详情
