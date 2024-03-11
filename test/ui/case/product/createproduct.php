2acf60efcc181117d0c8be985fc21ea411a2bdcd
liyang <liyang@easycorp.ltd>
2024-03-11 15:59:49 +0800

    * update create product case demo.

--- test/ui/case/product/createproduct.php
+++ test/ui/case/product/createproduct.php
@@ -7,15 +7,14 @@ pid=79
 >>  登录测试 : PASS!
 **/
 
-include dirname(__FILE__, 4) . '/lib/ui.php';
+include dirname(__FILE__, 4) . '/ui/lib/ui.php';
 include dirname(__FILE__) . '/product.class.php';
 
 $tester = new product();
 
 $product = array();
-$result = $tester->createProductTest($product);
-r($result->nameTip->getText()) && p() && e('『产品名称』不能为空。'); // 判断名称是否必输
-
+r($tester->createProduct($product)) && p('nameTip:text') && e('『产品名称』不能为空。'); // 判断名称是否必输
+die;
 $product['name'] = array('setValue' => '产品' . time());
 $result = $tester->createProductTest($product);
 r($result->settings->getText()) && p() && e('设置');                  // 只输入名称创建产品
--- test/ui/case/product/product.class.php
+++ test/ui/case/product/product.class.php
@@ -9,15 +9,31 @@ class product
      * @access public
      * @return object
      */
-    public function createProductTest($formData)
+    public function createProduct($formData)
     {
+        global $lang, $results;
         $productPage = new productPage();
 
         loadModel('user')->login();
         $productPage->go('product', 'mainNav');
 
-        $this->createInFrom('product', $formData);
-        $productPage->driver->wait(1);
+        $productPage->btn($lang->product->addBtn)->click();
+        $productPage->wait(1)->getErrors("appIframe-product");
+
+        foreach($formData as $formName => $value)
+        {
+            if(isset($value['wait']))        $productPage->wait($value['wait']);
+            if(isset($value['picker']))      $productPage->$formName->picker($value['picker']);
+            if(isset($value['multiPicker'])) $productPage->$formName->multiPicker($value['multiPicker']);
+            if(isset($value['datePicker']))  $productPage->$formName->datePicker($value['datePicker']);
+            if(isset($value['setValue']))    $productPage->$formName->setValue($value['setValue']);
+            if(isset($value['click']))       $productPage->$formName->click();
+        }
+
+        $productPage->btn($lang->product->saveBtn)->click();
+        $productPage->wait(1);
+
+        $results->setPage($productPage);
 
         return $productPage;
     }
