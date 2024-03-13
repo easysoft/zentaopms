781a7d76c8c86dbd3f4381a923c9876639076ec0
wangyuting <wangyuting@easycorp.ltd>
2024-03-13 15:26:26 +0800

    * Fix create build link bug. bug #46943.

--- module/testtask/js/common.ui.js
+++ module/testtask/js/common.ui.js
@@ -46,9 +46,9 @@ window.loadExecutionBuilds = function()
         let oldBuild     = $('[name="build"]').val();
         $buildPicker.render({items: data});
         executionID == 0 ? $buildPicker.$.setValue('') : $buildPicker.$.setValue(oldBuild);
-        if(data.length == 0)
+        if(data.length == 0 && executionID != 0)
         {
-            $('[name="build"]').closest('.input-group').find('.input-group-addon').removeClass('hidden');
+            $('[name="build"]').closest('.input-group').find('.input-group-addon').removeClass('hidden').find('a').attr('href', $.createLink('build', 'create', "executionID=" + executionID + "&productID=" + productID + "&projectID=" + projectID));
         }
         else
         {
