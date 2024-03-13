82e2f5c41c08a515d9e5b114a907cb17236d6807
Chenjianyu <chenjianyu@easycorp.ltd>
2024-03-13 10:33:33 +0800

    * refactor ai chat create.

---
+++ xuanxuan/extension/xuan/im/ext/control/getaipermission.php
@@ -0,0 +1,44 @@
+<?php
+helper::import('../../control.php');
+class myIm extends im {
+    public function getAiPermission($userID = 0, $version = '', $device = 'desktop')
+    {
+        $user = $this->im->user->getByID($userID);
+
+        global $app;
+        $app->user = $user;
+
+        $aiChatPermission = commonModel::hasPriv('ai', 'chat');
+        $hasModelsAvailable = $this->loadModel('ai')->hasModelsAvailable();
+
+        $chats = array();
+        if($hasModelsAvailable)
+        {
+
+            $models = $this->ai->getLanguageModels('', true);
+            foreach($models as $model)
+            {
+                $chat = $this->im->chat->getByGid("$userID&ai-{$model->id}");
+                if($chat) continue;
+
+                $chat = new stdclass();
+                $chat->gid = "$userID&ai-{$model->id}";
+                $chat->name = $model->name;
+
+                $chats[] = $chat;
+            }
+        }
+
+        $output = new stdclass();
+        $output->module = 'im';
+        $output->method = 'getaipermission';
+        $output->result = 'success';
+
+        $output->data = new stdclass();
+        $output->data->hasAiChatPermission = $aiChatPermission;
+        $output->data->hasModelsAvailable  = $hasModelsAvailable;
+        $output->data->needToCreateChat    = $chats;
+
+        return $this->im->sendOutput($output, 'getaipermissionResponse');
+    }
+}
