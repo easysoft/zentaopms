ce54ba6535399ca03d9e12b8f300ee563670863f
caoyanyi <caoyanyi@easycorp.ltd>
2024-03-14 15:56:06 +0800

    * Update version detail.

--- module/misc/lang/en.php
+++ module/misc/lang/en.php
@@ -113,7 +113,7 @@ $lang->misc->feature->promptExecImage   = 'theme/default/images/main/prompt_exec
 $lang->misc->feature->promptLearnMore   = 'https://www.zentao.net/book/zentaopms/1097.html';
 
 /* Release Date. */
-$lang->misc->releaseDate['20.0.beta2']  = '2024-03-07';
+$lang->misc->releaseDate['20.0.beta2']  = '2024-03-15';
 $lang->misc->releaseDate['18.11']       = '2024-02-28';
 $lang->misc->releaseDate['18.10.1']     = '2024-01-17';
 $lang->misc->releaseDate['20.0.beta1']  = '2024-01-26';
@@ -224,7 +224,7 @@ $lang->misc->releaseDate['7.1.stable']  = '2015-03-07';
 $lang->misc->releaseDate['6.3.stable']  = '2014-11-07';
 
 /* Release Detail. */
-$lang->misc->feature->all['20.0.beta2'][]  = array('title' => '', 'desc' => '');
+$lang->misc->feature->all['20.0.beta2'][]  = array('title' => 'Enhance the functionality and fine-tune the details, while addressing any known bugs.', 'desc' => '');
 $lang->misc->feature->all['18.11'][]       = array('title' => "We have successfully implemented AI mini-program functionality, integrating measurement metrics and global filtering capabilities into the large-screen display. Additionally, we have added feedback functionality to the OR interface. Furthermore, we have enabled the inclusion of keywords in the Requirement Management Hub, allowing for the redistribution of requirements in the Requirement Management Hub once a user's requirement has been withdrawn.", 'desc' => '');
 $lang->misc->feature->all['18.10.1'][]     = array('title' => 'The Requirement Management Hub (RM Hub) now includes notification functionality, supports product line features, and allows for distributing a single requirement to multiple products.', 'desc' => '');
 $lang->misc->feature->all['20.0.beta1'][]  = array('title' => 'This release mainly involves a large-scale refactoring of the code and a brand new upgrade to the user interface (UI), bringing better product performance, stronger security, and a more user-friendly experience.', 'desc' => '');
