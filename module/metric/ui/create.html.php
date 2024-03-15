9531a253bf14e3015d4c51b38df688661d593f70
zhouxin <zhouxin@easycorp.ltd>
2024-03-15 14:41:59 +0800

    * Change title's style to text-xl,font-black in metric.

--- module/metric/ui/create.html.php
+++ module/metric/ui/create.html.php
@@ -36,7 +36,7 @@ formPanel
         (
             entityLabel
             (
-                setClass('text-lg'),
+                setClass('text-xl font-black'),
                 set::level(1),
                 set::text($lang->metric->create)
             )
