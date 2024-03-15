5614a2c67a8352c6d170cf7494e062be2a94e174
liumengyi <liumengyi@easycorp.ltd>
2024-03-15 13:59:59 +0800

    * Fix bug #47012 to get current version steps.

--- module/testcase/tao.php
+++ module/testcase/tao.php
@@ -321,7 +321,12 @@ class testcaseTao extends testcaseModel
      */
     protected function getRelatedSteps(array $caseIdList): array
     {
-        return $this->dao->select('id, parent, `case`, version, type, `desc`, expect')->from(TABLE_CASESTEP)->where('`case`')->in($caseIdList)->orderBy('version desc,id')->fetchGroup('case', 'id');
+        return $this->dao->select('DISTINCT t2.id, t2.parent, t2.`case`, t2.version, t2.type, t2.`desc`, t2.expect')
+            ->from(TABLE_CASE)->alias('t1')
+            ->leftJoin(TABLE_CASESTEP)->alias('t2')->on('t1.version = t2.version')
+            ->where('t1.id')->in($caseIdList)
+            ->orderBy('t2.version,t2.desc,t2.id')
+            ->fetchGroup('case', 'id');
     }
 
     /**
