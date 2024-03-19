6f24d692f9d763e571feb6a1940817e86841db50
liumengyi <liumengyi@easycorp.ltd>
2024-03-19 17:13:53 +0800

    * Modify unit test result.

--- module/my/test/model/getoverview.php
+++ module/my/test/model/getoverview.php
@@ -23,9 +23,9 @@ title=测试 myModel->getOverview();
 cid=1
 pid=1
 
-projectTotal数据获取 >> 110
-allConsumed数据获取 >> 6750
-thisYearConsumed数据获取 >> 0
+projectTotal数据获取 >> 4
+allConsumed数据获取 >> 33
+thisYearConsumed数据获取 >> 1
 
 */
 
@@ -35,6 +35,6 @@ $projectTotal     = $my->getOverviewTest()->projectTotal;
 $allConsumed      = $my->getOverviewTest()->allConsumed;
 $thisYearConsumed = $my->getOverviewTest()->thisYearConsumed;
 
-r($projectTotal)     && p() && e('4'); // projectTotal数据获取
-r($allConsumed)      && p() && e('3'); // allConsumed数据获取
-r($thisYearConsumed) && p() && e('1'); // thisYearConsumed数据获取
+r($projectTotal)     && p() && e('4');  // projectTotal数据获取
+r($allConsumed)      && p() && e('33'); // allConsumed数据获取
+r($thisYearConsumed) && p() && e('1');  // thisYearConsumed数据获取
