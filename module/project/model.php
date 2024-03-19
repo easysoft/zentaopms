f4d3a504bc9cb56e6a5591041573acf37a656577
tianshujie <tianshujie@easycorp.ltd>
2024-03-19 15:30:56 +0800

    + Add 1.5 level drop-down menu to old page under project app.

--- module/project/model.php
+++ module/project/model.php
@@ -2076,6 +2076,7 @@ class projectModel extends model
         /* Set secondary menu. */
         $this->projectTao->setMenuByModel($project->model);
         $this->projectTao->setMenuByProduct($projectID, $project->hasProduct, $project->model);
+        $this->lang->switcherMenu = $this->getSwitcher($projectID, $moduleName, $methodName);
 
         /* Replace url params. */
         common::setMenuVars('project', $projectID);
@@ -2487,5 +2488,41 @@ class projectModel extends model
         $project = $this->dao->select('end')->from(TABLE_PROJECT)->where('id')->eq($projectID)->fetch();
         $this->dao->update(TABLE_PROJECT)->set('firstEnd')->eq(helper::isZeroDate($project->end) ? null : $project->end)->where('id')->eq($projectID)->exec();
         return !dao::isError();
+    }
+
+    /*
+     * 获取旧页面1.5级下拉。
+     * Get project swapper.
+     *
+     * @param  int     $projectID
+     * @param  string  $currentModule
+     * @param  string  $currentMethod
+     * @access public
+     * @return string
+     */
+    public function getSwitcher(int $projectID, string $currentModule, string $currentMethod): string
+    {
+        if($currentModule == 'project' && $currentMethod == 'browse') return '';
+
+        $currentProjectName = $this->lang->project->common;
+        if($projectID)
+        {
+            $currentProject     = $this->getById($projectID);
+            $currentProjectName = $currentProject->name;
+        }
+
+        if($this->app->viewType == 'mhtml' && $projectID)
+        {
+            $output  = $this->lang->project->common . $this->lang->colon;
+            $output .= "<a id='currentItem' href=\"javascript:showSearchMenu('project', '$projectID', '$currentModule', '$currentMethod', '')\">{$currentProjectName} <span class='icon-caret-down'></span></a><div id='currentItemDropMenu' class='hidden affix enter-from-bottom layer'></div>";
+            return $output;
+        }
+
+        $dropMenuLink = helper::createLink('project', 'ajaxGetDropMenu', "objectID=$projectID&module=$currentModule&method=$currentMethod");
+        $output  = "<div class='btn-group header-btn' id='swapper'><button data-toggle='dropdown' type='button' class='btn' id='currentItem' title='{$currentProjectName}'><span class='text'>{$currentProjectName}</span> <span class='caret' style='margin-bottom: -1px'></span></button><div id='dropMenu' class='dropdown-menu search-list' data-ride='searchList' data-url='$dropMenuLink'>";
+        $output .= '<div class="input-control search-box has-icon-left has-icon-right search-example"><input type="search" class="form-control search-input" /><label class="input-control-icon-left search-icon"><i class="icon icon-search"></i></label><a class="input-control-icon-right search-clear-btn"><i class="icon icon-close icon-sm"></i></a></div>';
+        $output .= "</div></div>";
+
+        return $output;
     }
 }
---
+++ module/project/test/model/getswitcher.php
@@ -0,0 +1,38 @@
+#!/usr/bin/env php
+<?php
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+include dirname(__FILE__, 2) . '/project.class.php';
+su('admin');
+zdTable('project')->config('project')->gen(3);
+
+/**
+
+title=测试 projectModel->getSwitcher();
+cid=1
+
+- 测试设置敏捷项目列表的1.5级下拉菜单 @0
+- 测试设置瀑布项目列表的1.5级下拉菜单 @0
+- 测试设置看板项目列表的1.5级下拉菜单 @0
+- 测试设置敏捷项目文档空间的1.5级下拉菜单 @1
+- 测试设置瀑布项目文档空间的1.5级下拉菜单 @1
+- 测试设置看板项目文档空间的1.5级下拉菜单 @1
+- 测试设置创建敏捷项目的1.5级下拉菜单 @1
+- 测试设置创建瀑布项目的1.5级下拉菜单 @1
+- 测试设置创建看板项目的1.5级下拉菜单 @1
+
+*/
+
+$projectIdList = range(1, 3);
+$moduleList    = array('project', 'doc');
+$methodList    = array('browse', 'projectSpace', 'create');
+
+$projectTester = new Project();
+r($projectTester->getSwitcherTest($projectIdList[0], $moduleList[0], $methodList[0])) && p() && e('0'); // 测试设置敏捷项目列表的1.5级下拉菜单
+r($projectTester->getSwitcherTest($projectIdList[1], $moduleList[0], $methodList[0])) && p() && e('0'); // 测试设置瀑布项目列表的1.5级下拉菜单
+r($projectTester->getSwitcherTest($projectIdList[2], $moduleList[0], $methodList[0])) && p() && e('0'); // 测试设置看板项目列表的1.5级下拉菜单
+r($projectTester->getSwitcherTest($projectIdList[0], $moduleList[1], $methodList[1])) && p() && e('1'); // 测试设置敏捷项目文档空间的1.5级下拉菜单
+r($projectTester->getSwitcherTest($projectIdList[1], $moduleList[1], $methodList[1])) && p() && e('1'); // 测试设置瀑布项目文档空间的1.5级下拉菜单
+r($projectTester->getSwitcherTest($projectIdList[2], $moduleList[1], $methodList[1])) && p() && e('1'); // 测试设置看板项目文档空间的1.5级下拉菜单
+r($projectTester->getSwitcherTest($projectIdList[0], $moduleList[1], $methodList[2])) && p() && e('1'); // 测试设置创建敏捷项目的1.5级下拉菜单
+r($projectTester->getSwitcherTest($projectIdList[1], $moduleList[1], $methodList[2])) && p() && e('1'); // 测试设置创建瀑布项目的1.5级下拉菜单
+r($projectTester->getSwitcherTest($projectIdList[2], $moduleList[1], $methodList[2])) && p() && e('1'); // 测试设置创建看板项目的1.5级下拉菜单
