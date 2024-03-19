e906ac039c6262c872a60172ae0dbf2e9b972ff0
songchenxuan <songchenxuan@easycorp.ltd>
2024-03-19 17:25:40 +0800

    * Move ajaxGetChart to 20 from 18.

--- module/pivot/control.php
+++ module/pivot/control.php
@@ -63,4 +63,66 @@ class pivot extends control
         $this->view->params      = $params;
         $this->display();
     }
+
+    /**
+     * Ajax get sys options.
+     *
+     * @param  string $type
+     * @param  string $object
+     * @param  string $field
+     * @param  string $saveAs
+     * @access public
+     * @return string
+     */
+    public function ajaxGetSysOptions($type, $object = '', $field = '', $saveAs = '')
+    {
+        $sql     = isset($_POST['sql'])     ? $_POST['sql']     : '';
+        $filters = isset($_POST['filters']) ? $_POST['filters'] : '';
+
+        $sql     = $this->loadModel('chart')->parseSqlVars($sql, $filters);
+        $options = $this->pivot->getSysOptions($type, $object, $field, $sql, $saveAs);
+        return print(html::select('default[]', array('' => '') + $options, '', "class='form-control form-select picker-select' multiple"));
+    }
+
+    /**
+     * Ajax get pivot table.
+     *
+     * @access public
+     * @return void
+     */
+    public function ajaxGetPivot()
+    {
+        $post = fixer::input('post')->skipSpecial('settings,filters,sql,langs')->get();
+
+        $pivotID    = $post->id;
+        $settings   = $post->settings;
+        $filters    = isset($_POST['searchFilters']) ? $_POST['searchFilters'] : (isset($_POST['filters']) ? $post->filters : array());
+        $page       = isset($_POST['page']) ? $_POST['page'] : 0;
+        $filterType = 'result';
+
+        $pivot = $this->pivot->getById($pivotID);
+
+        list($sql, $filterFormat) = $this->pivot->getFilterFormat($post->sql, $filters);
+        $post->sql = $sql;
+
+        $settings['filterType'] = $filterType;
+
+        $sql    = str_replace(';', '', "$post->sql");
+        $fields = $post->fieldSettings;
+        $langs  = isset($post->langs) ? (is_array($post->langs) ? $post->langs : json_decode($post->langs, true)) : array();
+
+        $processSqlData = $this->loadModel('chart')->getTables($sql);
+        $sql = $processSqlData['sql'];
+
+        if(isset($settings['summary']) and $settings['summary'] == 'notuse')
+        {
+            list($data, $configs) = $this->pivot->genOriginSheet($fields, $settings, $sql, $filterFormat, $langs);
+        }
+        else
+        {
+            list($data, $configs) = $this->pivot->genSheet($fields, $settings, $sql, $filterFormat, $langs);
+        }
+
+        $this->pivot->buildPivotTable($data, $configs, $page);
+    }
 }
