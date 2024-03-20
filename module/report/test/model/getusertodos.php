ea2449bf6cb6e31f7f09f6f1decede0a4a68bd8d
王怡栋 <wangyidong@cnezsoft.com>
2024-03-20 11:08:07 +0800

    * Adjust for report getusertodos unit test.

--- module/report/test/model/getusertodos.php
+++ module/report/test/model/getusertodos.php
@@ -1,5 +1,19 @@
 #!/usr/bin/env php
 <?php
+
+/**
+
+title=测试 reportModel->getUserTodos();
+cid=0
+
+- 测试获取用户待办数 @admin:3;
+- 测试获取用户待办数 @dev2:1;dev3:2;
+- 测试获取用户待办数 @test1:1;test2:2;test3:1;
+- 测试获取用户待办数 @user1:1;user2:1;
+- 测试获取用户待办数 @pm3:1;pm1:1;
+- 测试获取用户待办数 @po1:2;po2:1;
+
+*/
 declare(strict_types=1);
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/report.class.php';
@@ -11,21 +25,13 @@ zdTable('user')->config('user')->gen(31);
 
 su('admin');
 
-/**
-
-title=测试 reportModel->getUserTodos();
-cid=1
-pid=1
-
-*/
-
 $userType = array('admin', 'dev', 'test', 'user', 'pm', 'po');
 
 $report = new reportTest();
 
-r($report->getUserTodosTest($userType[0])) && p() && e('admin:1;');                 // 测试获取用户待办数
+r($report->getUserTodosTest($userType[0])) && p() && e('admin:3;');                 // 测试获取用户待办数
 r($report->getUserTodosTest($userType[1])) && p() && e('dev2:1;dev3:2;');           // 测试获取用户待办数
 r($report->getUserTodosTest($userType[2])) && p() && e('test1:1;test2:2;test3:1;'); // 测试获取用户待办数
-r($report->getUserTodosTest($userType[3])) && p() && e('user1:2;user2:1;');         // 测试获取用户待办数
+r($report->getUserTodosTest($userType[3])) && p() && e('user1:1;user2:1;');         // 测试获取用户待办数
 r($report->getUserTodosTest($userType[4])) && p() && e('pm3:1;pm1:1;');             // 测试获取用户待办数
 r($report->getUserTodosTest($userType[5])) && p() && e('po1:2;po2:1;');             // 测试获取用户待办数
