c08c427f72f2b8ca9d81fa0fdbf21c907671e46e
liumengyi <liumengyi@easycorp.ltd>
2024-03-21 10:47:48 +0800

    * Add testcase-zerocase to story's back url.

--- lib/zin/wg/backbtn/v1.php
+++ lib/zin/wg/backbtn/v1.php
@@ -44,7 +44,7 @@ class backBtn extends btn
 
         $backs = array(
             'task'           => 'execution-task,my-work,my-contribute,execution-tree,execution-grouptask,project-execution,product-track,repo-view,story-change',
-            'story'          => 'product-browse,projectstory-story,execution-story,my-work,my-contribute,productplan-view,build-view,projectbuild-view,product-track,repo-view',
+            'story'          => 'product-browse,projectstory-story,execution-story,my-work,my-contribute,productplan-view,build-view,projectbuild-view,product-track,repo-view,testcase-zerocase',
             'bug'            => 'bug-browse,project-bug,my-work,my-contribute,execution-bug,bug-view,qa-index,execution-task,product-track,execution-task,task-view,repo-view,story-change',
             'testcase'       => 'testcase-browse,project-testcase,my-work,my-contribute,execution-testcase,testtask-cases,testsuite-view,product-browse,testcase-view,qa-index,caselib-browse,product-track,story-change',
             'testsuite'      => 'testsuite-browse,testsuite-view,',
