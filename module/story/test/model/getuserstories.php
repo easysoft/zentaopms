9e0a3d7e804d0f9f36847f9f17952acde973cf74
tianshujie <tianshujie@easycorp.ltd>
2024-03-21 08:47:04 +0800

    * Change the result for the story::getuserstories unit test.

--- module/story/test/model/getuserstories.php
+++ module/story/test/model/getuserstories.php
@@ -8,34 +8,30 @@ cid=0
 
 - 获取指派给User2的所有需求数量 @103
 - 获取指派给User2的ID为6的需求详情
- - 第6条的title属性 @软件需求6
- - 第6条的type属性 @story
- - 第6条的assignedTo属性 @user2
- - 第6条的stage属性 @projected
+- 第6条的title属性 @软件需求6
+- 第6条的type属性 @story
+- 第6条的assignedTo属性 @user2
+- 第6条的stage属性 @projected
 - 获取由User2创建的需求数量，每页10条 @10
 - 获取由User2创建的ID为38的需求详情
- - 第38条的title属性 @软件需求38
- - 第38条的type属性 @story
- - 第38条的openedBy属性 @user2
- - 第38条的stage属性 @released
+- 第38条的title属性 @软件需求38
+- 第38条的type属性 @story
+- 第38条的openedBy属性 @user2
+- 第38条的stage属性 @released
 - 获取由Test3关闭的所有需求数量 @41
 - 获取由Test3关闭的ID为203的需求详情
- - 第203条的title属性 @用户需求203
- - 第203条的type属性 @requirement
- - 第203条的closedBy属性 @test3
- - 第203条的stage属性 @~~
+- 第203条的title属性 @用户需求203
+- 第203条的type属性 @requirement
+- 第203条的closedBy属性 @test3
+- 第203条的stage属性 @~~
 - 获取无评审人的需求数量 @6
 - 获取无评审人的ID为402的需求详情
- - 第402条的title属性 @软件需求402
- - 第402条的type属性 @story
- - 第402条的reviewedBy属性 @~~
- - 第402条的stage属性 @wait
-- 获取admin评审人的需求数量 @12
-- 获取admin评审人的ID为2的需求详情
- - 第4条的title属性 @软件需求4
- - 第4条的type属性 @story
- - 第4条的reviewedBy属性 @admin
- - 第4条的stage属性 @planned
+- 第402条的title属性 @软件需求402
+- 第402条的type属性 @story
+- 第402条的reviewedBy属性 @~~
+- 第402条的stage属性 @wait
+- 获取admin评审人的需求数量 @0
+- 获取admin评审人的ID为2的需求详情 @0
 
 */
 include dirname(__FILE__, 5) . '/test/lib/init.php';
@@ -70,5 +66,5 @@ r(count($closedByTest3Stories))   && p()                                  && e('
 r($closedByTest3Stories)          && p('203:title,type,closedBy,stage')   && e('用户需求203,requirement,test3,~~'); //获取由Test3关闭的ID为203的需求详情
 r(count($emptyReviewedByStories)) && p()                                  && e('6');                                //获取无评审人的需求数量
 r($emptyReviewedByStories)        && p('402:title,type,reviewedBy,stage') && e('软件需求402,story,~~,wait');        //获取无评审人的ID为402的需求详情
-r(count($emptyReviewByStories))   && p()                                  && e('12');                               //获取admin评审人的需求数量
-r($emptyReviewByStories)          && p('4:title,type,reviewedBy,stage')   && e('软件需求4,story,admin,planned');    //获取admin评审人的ID为2的需求详情
+r(count($emptyReviewByStories))   && p()                                  && e('0');                                //获取admin评审人的需求数量
+r($emptyReviewByStories)          && p()                                  && e('0');                                //获取admin评审人的ID为2的需求详情
