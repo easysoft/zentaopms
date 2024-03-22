727adc95a2abf8615bdcc4b6e2fb314b8b1a73a5
liumengyi <liumengyi@easycorp.ltd>
2024-03-22 13:56:37 +0800

    * Fix bug #47180, unset roadmap from the search fields of story if the edition isn't ipd.

--- module/my/model.php
+++ module/my/model.php
@@ -666,6 +666,7 @@ class myModel extends model
         $this->config->product->search['params']['plan']['values']    = $this->loadModel('productplan')->getPairs($productIdList, $branchParam);
         $this->config->product->search['fields']['title']             = $this->lang->story->title;
         unset($this->config->product->search['fields']['module'], $this->config->product->search['fields']['branch']);
+        if($this->config->edition != 'ipd') unset($this->config->product->search['fields']['roadmap']);
 
         $this->loadModel('search')->setSearchParams($this->config->product->search);
     }
