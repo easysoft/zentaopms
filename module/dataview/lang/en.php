77f5e0b83412daddfaf988ea3f0edd5b0d4681a1
songchenxuan <songchenxuan@easycorp.ltd>
2024-03-22 14:51:14 +0800

    * Add lang for dataview.

--- module/dataview/lang/en.php
+++ module/dataview/lang/en.php
@@ -59,6 +59,7 @@ $lang->dataview->relatedField   = 'Field';
 $lang->dataview->multilingual   = 'After switching the system language, the corresponding name will be displayed';
 $lang->dataview->duplicateField = 'Duplicate field names exist: <strong>%s</strong>. You are advised to: (1) Modify the <strong>*</strong> query to a specific field. (2) Use <strong>as</strong> to alias the field.';
 $lang->dataview->errorField     = 'The current field %s is an invalid character. Please [as] it in English or English underscore format.';
+$lang->dataview->queryFilterTip = 'Query filter is a way to implement dynamic query filtering by inserting variables into SQL. The result filter configured in the third step is to further filter the SQL query results.';
 
 $lang->dataview->varFilter = new stdclass();
 $lang->dataview->varFilter->varCode     = 'Var Code';
@@ -92,12 +93,14 @@ $lang->dataview->objects['bug']         = $lang->bug->common;
 $lang->dataview->objects['project']     = $lang->project->common;
 $lang->dataview->objects['task']        = $lang->task->common;
 $lang->dataview->objects['team']        = 'team';
+$lang->dataview->objects['user']        = 'user';
 $lang->dataview->objects['execution']   = $lang->execution->common;
 $lang->dataview->objects['testtask']    = 'testtask';
 $lang->dataview->objects['testrun']     = 'testrun';
 $lang->dataview->objects['testcase']    = 'testcase';
 $lang->dataview->objects['testresult']  = 'result';
 $lang->dataview->objects['casemodule']  = 'case module';
+$lang->dataview->objects['feedback']    = 'Feedback';
 $lang->dataview->objects['action']      = 'Action';
 $lang->dataview->objects['effort']      = 'Effort';
 
