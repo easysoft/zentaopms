4685bc068fa97228c5ca1ab6dc318597ee727ce8
wangyuting <wangyuting@easycorp.ltd>
2024-03-25 16:41:10 +0800

    * Add workflow extra field to batch form.

--- module/bug/config/form.php
+++ module/bug/config/form.php
@@ -167,7 +167,7 @@ $config->bug->form->batchCreate['os']          = array('required' => false, 'typ
 $config->bug->form->batchCreate['browser']     = array('required' => false, 'type' => 'array',  'default' => '', 'filter' => 'join');
 $config->bug->form->batchCreate['keywords']    = array('required' => false, 'type' => 'string', 'default' => '');
 
-$config->bug->form->batchEdit = common::formConfig('bug', 'batchEdit');
+$config->bug->form->batchEdit = array();
 $config->bug->form->batchEdit['id']             = array('required' => false, 'type' => 'int',    'base' => true);
 $config->bug->form->batchEdit['type']           = array('required' => false, 'type' => 'string', 'default' => '');
 $config->bug->form->batchEdit['severity']       = array('required' => false, 'type' => 'int',    'default' => 0);
