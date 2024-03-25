48992dea39f49121c5ca471a7b16c26ec2c2dab3
sunhao <sunhao@easycorp.ltd>
2024-03-25 11:36:05 +0800

    * zin: set nav in feature bar to compact mode.

--- lib/zin/wg/nav/v1.php
+++ lib/zin/wg/nav/v1.php
@@ -9,6 +9,7 @@ class nav extends wg
     protected static array $defineProps = array(
         'stacked?: bool',    // 是否为垂直模式。
         'justified?: bool',  // 是否为自适应模式。
+        'compact?: bool',    // 是否为紧凑模式。
         'type?: string',     // 导航类型，包括 primary, tabs, secondary，pills
         'items?:array'       // 使用数组指定导航中的每一项。
     );
@@ -25,10 +26,10 @@ class nav extends wg
 
     protected function build()
     {
-        list($items, $type, $stacked, $justified) = $this->prop(array('items', 'type', 'stacked', 'justified'));
+        list($items, $type, $stacked, $justified, $compact) = $this->prop(array('items', 'type', 'stacked', 'justified', 'compact'));
         return h::menu
         (
-            setClass('nav', $type ? "nav-$type" : null, $stacked ? 'nav-stacked' : '', $justified ? 'nav-justified' : ''),
+            setClass('nav', $type ? "nav-$type" : null, $stacked ? 'nav-stacked' : '', $justified ? 'nav-justified' : '', $compact ? 'nav-compact' : ''),
             set($this->getRestProps()),
             is_array($items) ? array_map(array($this, 'onBuildItem'), $items) : null,
             $this->children()
