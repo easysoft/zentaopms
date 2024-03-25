05c8d03bf7db621d9e9cc07dcc9ef9e9317b65ac
liyang <liyang@easycorp.ltd>
2024-03-25 11:26:55 +0800

    - Delete lang dir.

--- test/lang/zh-cn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$lang = new stdClass();
-$lang->product = new stdClass();
-$lang->product->addBtn  = '添加产品';
-$lang->product->saveBtn = '保存';
-
-$lang->program = new stdClass();
-$lang->program->addBtn  = '添加项目集';
-$lang->program->saveBtn = '保存';
