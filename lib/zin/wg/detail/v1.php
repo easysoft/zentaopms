f8561dd0b5a6e1a5fc40d1852007d444dc420d61
sunhao <sunhao@easycorp.ltd>
2024-03-27 12:33:24 +0800

    * zin: fix side style in detail.

--- lib/zin/wg/detail/v1.php
+++ lib/zin/wg/detail/v1.php
@@ -109,7 +109,7 @@ class detail extends wg
         .detail-section-title, .detail-section.panel .panel-heading {background: var(--color-canvas); position: sticky; top: 0; z-index: 1}
         .detail-side > * + * {margin-top: 4px}
         .detail-side .tabs {padding: 12px 8px 12px 16px}
-        .detail-side .tabs-header {position: sticky; top: 0; background: var(--color-surface-light);}
+        .detail-side .tabs-header {position: sticky; top: 0;}
         .detail-side .tab-pane {padding: 0}
 CSS;
     }
