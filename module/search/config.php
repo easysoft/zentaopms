23cc0bccd23a7c8b93cf11dff3ef080054c8409e
tanghucheng <tanghucheng@easycorp.ltd>
2024-03-28 13:23:54 +0800

    * Add oldQuery config.

--- module/search/config.php
+++ module/search/config.php
@@ -159,7 +159,7 @@ $config->search->recPerPage = 10;
 $config->search->summaryLength = 120;
 $config->search->maxFileSize   = 1024;
 
-$config->search->oldQuery = array('workFeedback', 'workTicket', 'issue', 'risk', 'traincourse', 'ticket', 'activity', 'zoutput', 'trainplan', 'reviewissue', 'researchreport', 'researchplan', 'process', 'opportunity', 'nc', 'meetingroom', 'meeting', 'measurement', 'gapanalysis', 'auditplan', 'auditcl', 'assetlib', 'testcase', 'domain');
+$config->search->oldQuery = array('workFeedback', 'workTicket', 'issue', 'risk', 'traincourse', 'ticket', 'activity', 'zoutput', 'trainplan', 'reviewissue', 'researchreport', 'researchplan', 'process', 'opportunity', 'nc', 'meetingroom', 'meeting', 'measurement', 'gapanalysis', 'auditplan', 'auditcl', 'assetlib', 'testcase', 'domain', 'demandpool', 'demand');
 
 if($config->vision == 'rnd')
 {
