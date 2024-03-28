470a6dc7c01f399a8a4f4b181cdee3eec77ec3ed
liyang <liyang@easycorp.ltd>
2024-03-28 09:27:42 +0800

    * Adjust unit test of the mr module.

--- module/mr/test/model/apiupdatemr.php
+++ module/mr/test/model/apiupdatemr.php
@@ -51,21 +51,21 @@ $oldMR['gitlab']->title         = 'test-merge（不要关闭或删除）';
 $oldMR['gitlab']->hostID        = 1;
 $oldMR['gitlab']->sourceProject = 3;
 $oldMR['gitlab']->targetBranch  = 'master';
-$oldMR['gitlab']->id            = 36;
+$oldMR['gitlab']->mriid         = 36;
 
 $oldMR['gitea'] = new stdclass();
 $oldMR['gitea']->title         = '更新 LICENSE（不要删除）';
 $oldMR['gitea']->hostID        = 4;
 $oldMR['gitea']->sourceProject = 'gitea/unittest';
 $oldMR['gitea']->targetBranch  = 'test1';
-$oldMR['gitea']->id            = 11;
+$oldMR['gitea']->mriid         = 11;
 
 $oldMR['gogs'] = new stdclass();
 $oldMR['gogs']->title         = 'test（不要删除）';
 $oldMR['gogs']->hostID        = 5;
 $oldMR['gogs']->sourceProject = 'easycorp/unittest';
 $oldMR['gogs']->targetBranch  = 'master';
-$oldMR['gogs']->id            = 7;
+$oldMR['gogs']->mriid         = 7;
 
 r($mrModel->apiUpdateMrTester($oldMR['gitlab'], $newMR)) && p('oldTitle,title') && e('test-merge（不要关闭或删除）,test'); // 正确的数据更新Gitlab合并请求
 r($mrModel->apiUpdateMrTester($oldMR['gitea'],  $newMR)) && p('oldTitle,title') && e('更新 LICENSE（不要删除）,test');     // 正确的数据更新Gitea合并请求
