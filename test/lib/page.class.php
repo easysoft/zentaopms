58b50f7dfe306cdfa8b5088ee01c8387312730e7
liyang <liyang@easycorp.ltd>
2024-04-01 15:15:07 +0800

    * Adjust page class.

--- test/lib/page.class.php
+++ test/lib/page.class.php
@@ -1,17 +1,12 @@
 <?php
-class page
+class page extends webDriver
 {
-    public $webDriver;
-    public $lang;
-    public $xpathName;
     public $doms;
+    public $xpathName;
     public $timeout = 5;
 
-    public function __construct($webDriver)
+    public function __construct()
     {
-        global $lang;
-        $this->webDriver = $webDriver;
-        $this->lang      = $lang;
         $this->doms      = array(
             'menuMoreNav' => '//*[@id="menuMoreNav"]/li[2]/a'
         );
@@ -27,8 +22,8 @@ class page
     public function openAppTab($appTab)
     {
         $xpath = "//li[@data-app='$appTab']/a";
-        $this->webDriver->waitElement($xpath, $this->timeout)->getElement($xpath)->click();
-        $this->webDriver->wait(1)->switchToIframe("appIframe-{$appTab}");
+        $this->waitElement($xpath, $this->timeout)->getElement($xpath)->click();
+        $this->wait(1)->switchToIframe("appIframe-{$appTab}");
 
         return $this;
     }
@@ -43,7 +38,7 @@ class page
     public function openNavbar($nav)
     {
         $xpath = "//a[@data-id='$nav']";
-        $this->webDriver->waitElement($xpath, $this->timeout)->getElement($xpath)->click();
+        $this->waitElement($xpath, $this->timeout)->getElement($xpath)->click();
 
         return $this;
     }
@@ -59,7 +54,7 @@ class page
     public function btn($value, $type = 'text')
     {
         $xpath = $type == 'text' ? "//*[text()='$value']" : $value;
-        $this->webDriver->waitElement($xpath, $this->timeout)->getElement($xpath);
+        $this->waitElement($xpath, $this->timeout)->getElement($xpath);
 
         return $this;
     }
@@ -85,7 +80,7 @@ class page
             $xpath = $this->generateXPath($name);
         }
 
-        $this->webDriver->waitElement($xpath, $this->timeout)->getElement($xpath);
+        $this->waitElement($xpath, $this->timeout)->getElement($xpath);
         return $this;
     }
 
@@ -102,21 +97,21 @@ class page
 
         try
         {
-            $this->webDriver->getElement($element);
+            $this->getElement($element);
         }
         catch (Exception $e)
         {
             try
             {
                 $element = '//*[@name="' . $name . '[]' . '"]';
-                $this->webDriver->getElement($element);
+                $this->getElement($element);
             }
             catch (Exception $e)
             {
                 $element = '//*[@id="' . $name . '"]';
                 try
                 {
-                    $this->webDriver->getElement($element);
+                    $this->getElement($element);
                 }
                 catch (Exception $e)
                 {
@@ -136,7 +131,7 @@ class page
      */
     public function getFormTips()
     {
-        $elements = $this->webDriver->getElementList('xpath://div[contains(@class, "form-tip")]');
+        $elements = $this->getElementListInPage('xpath://div[contains(@class, "form-tip")]');
 
         $tips = array();
         foreach($elements as $element)
@@ -146,20 +141,5 @@ class page
         }
 
         return $tips;
-    }
-
-    /**
-     * Methods in the public webdriver class.
-     *
-     * @param  string $method
-     * @param  array  $params
-     * @access public
-     * @return void
-     */
-    public function __call($method, $params = array())
-    {
-        if(method_exists($this->webDriver, $method)) return call_user_func_array(array($this->webDriver, $method), $params);
-
-        return false;
     }
 }
