182dacdf4d6dbdafa14ac459a48d5975a429cca8
hufangzhou <hufangzhou@easycorp.ltd>
2024-04-02 13:26:16 +0800

    * Add enabled fields for customizing stages and review points.

--- db/update20.0.beta2.sql
+++ db/update20.0.beta2.sql
@@ -1,2 +1,5 @@
 UPDATE `zt_workflowfield` SET `default` = 0 WHERE `field` = 'approva' AND `role` = 'approval' AND `default` = '';
 UPDATE `zt_workflowfield` SET `default` = 'wait' WHERE `field` = 'reviewStatus' AND `role` = 'approval' AND `default` = '';
+
+ALTER TABLE `zt_project` ADD COLUMN `enabled` enum('0','1') NOT NULL DEFAULT '1' AFTER `parallel`;
+ALTER TABLE `zt_object`  ADD COLUMN `enabled` enum('0','1') NOT NULL DEFAULT '1' AFTER `type`;
