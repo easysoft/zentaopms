079b4f9681e4ae004c57ef591a85efa775960377
liugang <liugang@easycorp.ltd>
2024-04-03 15:42:50 +0800

    + control: validate the view file before include it.

--- framework/control.class.php
+++ framework/control.class.php
@@ -514,6 +514,9 @@ class control extends baseControl
     public function printViewFile($viewFile)
     {
         if(!file_exists($viewFile)) return false;
+        if(strpos($viewFile, '..') !== false) return false;
+        if(strpos($viewFile, '/view/') === false) return false;
+        if(strpos($viewFile, $this->app->getModuleRoot()) !== 0 && strpos($viewFile, $this->app->getExtensionRoot()) !== 0) return false;
 
         $currentPWD = getcwd();
         chdir(dirname($viewFile));
