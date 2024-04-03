46773b6902176a43ac86765decc36d6400426e71
Chenjianyu <chenjianyu@easycorp.ltd>
2024-04-03 09:59:18 +0800

    * fix AI assistants view sorts.

--- module/ai/config/dtable.php
+++ module/ai/config/dtable.php
@@ -14,8 +14,8 @@ $config->ai->dtable->models['actions']     = array('title' => $lang->actions, 't
 
 $config->ai->dtable->assistants                  = array();
 $config->ai->dtable->assistants['id']            = array('name' => 'id', 'title' => $lang->idAB, 'type' => 'id', 'sortType' => true);
-$config->ai->dtable->assistants['name']          = array('name' => 'name', 'title' => $lang->ai->assistants->name, 'flex' => 1, 'type' => 'link', 'link' => array('module' => 'ai', 'method' => 'assistantview', 'params' => 'assistantID={id}'));
-$config->ai->dtable->assistants['model']         = array('name' => 'model', 'title' => $lang->ai->assistants->refModel, 'flex' => 'auto', 'type' => 'text', 'sortType' => true, 'minWidth' => 120,);
+$config->ai->dtable->assistants['name']          = array('name' => 'name', 'title' => $lang->ai->assistants->name, 'flex' => 1, 'sortType' => true, 'type' => 'link', 'link' => array('module' => 'ai', 'method' => 'assistantview', 'params' => 'assistantID={id}'));
+$config->ai->dtable->assistants['model']         = array('name' => 'modelId', 'title' => $lang->ai->assistants->refModel, 'flex' => 'auto', 'type' => 'text', 'sortType' => true, 'minWidth' => 120,);
 $config->ai->dtable->assistants['status']        = array('name' => 'enabled', 'title' => $lang->statusAB, 'flex' => 'auto', 'type' => 'status', 'statusMap' => $lang->ai->assistants->statusList, 'minWidth' => 80);
 $config->ai->dtable->assistants['createdDate']   = array('name' => 'createdDate', 'title' => $lang->ai->assistants->createdDate, 'flex' => 'auto', 'type' => 'datetime', 'minWidth' => 120);
 $config->ai->dtable->assistants['publishedDate'] = array('name' => 'publishedDate', 'title' => $lang->ai->assistants->publishedDate, 'flex' => 'auto', 'type' => 'datetime', 'minWidth' => 120,);
