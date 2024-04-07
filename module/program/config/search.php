10d3f435bd04e26152873ca7bfb258dd72a7da44
tianshujie <tianshujie@easycorp.ltd>
2024-04-07 15:27:41 +0800

    * Fix bug #45006.

--- module/program/config/search.php
+++ module/program/config/search.php
@@ -2,9 +2,9 @@
 global $lang;
 $config->program->search['module']                   = 'program';
 $config->program->search['fields']['name']           = $lang->program->name;
-$config->program->search['fields']['status']         = $lang->program->status;
 $config->program->search['fields']['PM']             = $lang->program->PM;
 $config->program->search['fields']['openedDate']     = $lang->program->openedDate;
+$config->program->search['fields']['status']         = $lang->program->status;
 $config->program->search['fields']['openedBy']       = $lang->program->openedBy;
 $config->program->search['fields']['begin']          = $lang->program->begin;
 $config->program->search['fields']['end']            = $lang->program->end;
@@ -16,9 +16,9 @@ $config->program->search['fields']['desc']           = $lang->program->desc;
 
 /* This is an ordered array. */
 $config->program->search['params']['name']           = array('operator' => 'include', 'control' => 'input',  'values' => '');
+$config->program->search['params']['status']         = array('operator' => '=',       'control' => 'select', 'values' => $lang->program->statusList);
 $config->program->search['params']['PM']             = array('operator' => '=',       'control' => 'select', 'values' => 'users');
 $config->program->search['params']['openedDate']     = array('operator' => '=',       'control' => 'date',   'values' => '');
-$config->program->search['params']['status']         = array('operator' => '=',       'control' => 'select', 'values' => $lang->program->statusList);
 $config->program->search['params']['openedBy']       = array('operator' => '=',       'control' => 'select', 'values' => 'users');
 $config->program->search['params']['begin']          = array('operator' => '=',       'control' => 'date',   'values' => '');
 $config->program->search['params']['end']            = array('operator' => '=',       'control' => 'date',   'values' => '');
