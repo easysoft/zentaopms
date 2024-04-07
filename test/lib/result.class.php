d40f5c174c085c0ae02e184a707a7a6cfa67070a
liyang <liyang@easycorp.ltd>
2024-04-07 13:31:25 +0800

    + Add parse url method in result class.

--- test/lib/result.class.php
+++ test/lib/result.class.php
@@ -39,6 +39,39 @@ class result
     }
 
     /**
+     * Parsing the current page's URL.
+     *
+     * @access public
+     * @return bool
+     */
+    public function parseCurrentUrl()
+    {
+        if(empty($this->pageObject)) return false;
+
+        $url = $this->pageObject->getPageUrl();
+        $this->url = $url;
+
+        $url = parse_url($url);
+        if(isset($url['query']))
+        {
+            $query = $url['query'];
+            parse_str($query, $queryParams);
+            $this->module = $queryParams['m'];
+            $this->method = $queryParams['f'];
+        }
+        else
+        {
+            $path      = $url['path'];
+            $pathParts = explode('-', trim($path, '/'));
+
+            $this->module = str_replace('.html', '', $pathParts[0]);
+            $this->method = str_replace('.html', '', $pathParts[1]);
+        }
+
+        return true;
+    }
+
+    /**
      * Get response of the test case.
      *
      * @access public
@@ -56,6 +89,8 @@ class result
 
             return $param ? '' : array();
         }
+
+        $this->parseCurrentUrl();
 
         $result = array();
         $result['errors']  = $this->errors;
