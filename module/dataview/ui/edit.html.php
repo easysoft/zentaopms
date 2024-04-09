8d4395627daff0cd81e0b29ebe76980d0552660c
zhouxin <zhouxin@easycorp.ltd>
2024-04-09 15:30:58 +0800

    * Add edit modal of dataview.

---
+++ module/dataview/ui/edit.html.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+/**
+ * The edit view file of dataview module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Xin Zhou <zhouxin@easycorp.ltd>
+ * @package     dataview
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+
+modalHeader();
+
+formPanel
+(
+    formGroup
+    (
+        set::label($lang->dataview->group),
+        set::required(true),
+        select
+        (
+            set::name('group'),
+            set::items($groups)
+        )
+    ),
+    formGroup
+    (
+        set::label($lang->dataview->name),
+        set::required(true),
+        input(set::name('name'))
+    ),
+    set::submitBtnText($lang->save)
+);
+
+render();
