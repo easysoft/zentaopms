97bff9d6a566fc8b7438431fdad9c40bf732f240
liyang <liyang@easycorp.ltd>
2024-04-09 17:36:09 +0800

    + Add hide repo method.

--- module/repo/js/import.ui.js
+++ module/repo/js/import.ui.js
@@ -30,17 +30,56 @@ function selectServer(event)
     if(server) loadPage($.createLink('repo', 'import', 'server=' + server));
 }
 
+window.onRenderRowCol = function(row, cel)
+{
+    if(cel.name == 'actions')
+    {
+       const repoID = $(row).closest('tr').find('input[data-name="serviceProject"]').val();
+       if(hiddenRepos.includes(repoID))
+        {
+            $(row).find('button').attr('title', showLang);
+            $(row).find('button').removeClass('hideRepo').addClass('showRepo');
+            $(row).find('.icon').removeClass('icon-eye-off').addClass('icon-eye');
+        }
+
+    }
+
+}
+
 /**
- * Hidden repo.
+ * Show hidden repos.
  *
  * @access public
  * @return void
  */
-window.hiddenRepo = function(event)
+window.toggleShowRepo = function(event)
 {
-    var repoID = $(event.target).closest('tr').find('input[data-name="serviceProject"]').val();
+    const showHidden = $(event.target).prop('checked') ? 1 : 0;
+    loadPage($.createLink('repo', 'import', 'server=' + serverID + '&showHiddenRepo=' + showHidden), '#repoList');
+}
+
+/**
+ * Hide a repo.
+ *
+ * @access public
+ * @return void
+ */
+window.hideRepo = function(event)
+{
+    const repoID = $(event.target).closest('tr').find('input[data-name="serviceProject"]').val();
     $.post($.createLink('repo', 'ajaxHiddenRepo'), {'serverID': serverID, 'repoID': repoID}, function(response){
         if(response.result == 'fail') return zui.Modal.alert(response.message);
         $(event.target).closest('tr').remove();
+    });
+}
+
+window.showRepo = function(event)
+{
+    const repoID = $(event.target).closest('tr').find('input[data-name="serviceProject"]').val();
+    $.post($.createLink('repo', 'ajaxShowRepo'), {'serverID': serverID, 'repoID': repoID}, function(response){
+        if(response.result == 'fail') return zui.Modal.alert(response.message);
+        $(event.target).find('button').attr('title', hideLang);
+        $(event.target).find('button').removeClass('showRepo').addClass('hideRepo');
+        $(event.target).find('.icon').removeClass('icon-eye').addClass('icon-eye-off');
     });
 }
