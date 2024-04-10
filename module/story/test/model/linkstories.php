b43e465a728cd43410165cb476d006b47dfc97ec
sunguangming <sunguangming@easycorp.ltd>
2024-04-10 09:59:37 +0800

    * Adaptation unit test.

--- module/story/test/model/linkstories.php
+++ module/story/test/model/linkstories.php
@@ -4,7 +4,13 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/story.class.php';
 su('admin');
 
-zdTable('story')->gen(20);
+$story = zdTable('story');
+$story->product->range(1);
+$story->version->range(1);
+$story->parent->range('0{18},`-1`,19');
+$story->type->range('requirement{10},story{10}');
+$story->gen(20);
+
 zdTable('storyspec')->gen(60);
 $relation = zdTable('relation');
 $relation->gen(0);
@@ -12,12 +18,12 @@ $relation->gen(0);
 /**
 
 title=测试 storyModel->linkStories();
+timeout=0
 cid=1
-pid=1
 
-查看关联前的关联关系数量 >> 0
-查看关联后的关联关系数量 >> 4
-查看关联后的需求详情 >> 软件需求300,软件需求302,软件需求304,软件需求306
+- 查看关联前的关联关系数量 @0
+- 查看关联后的关联关系数量 @4
+- 查看关联后的需求详情 @1|3|5|7
 
 */
 
@@ -27,8 +33,7 @@ $tester->loadModel('story');
 $beforeRelations = $tester->story->getRelation(1, 'requirement');
 
 $storyIdList = array(1, 3, 5, 7);
-$_POST['stories'] = $storyIdList;
-$tester->story->linkStories(1);
+$tester->story->linkStories(1, $storyIdList);
 
 $afterRelations = $tester->story->getRelation(1, 'requirement');
 
