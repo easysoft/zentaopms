1a6f076eb6973c7b82b8da3a17b1887aea5f8240
liugang <liugang@easycorp.ltd>
2024-04-10 16:43:33 +0800

    * commonModel: remove unused code.

--- module/common/model.php
+++ module/common/model.php
@@ -425,7 +425,7 @@ class commonModel extends model
         {
             /* Get authorize again. */
             $user = $this->app->user;
-            $user->rights = $this->loadModel('user')->authorize($user->account);
+            $user->rights = $this->user->authorize($user->account);
             $user->groups = $this->user->getGroups($user->account);
             $user->admin  = strpos($this->app->company->admins, ",{$user->account},") !== false;
             $this->session->set('user', $user);
