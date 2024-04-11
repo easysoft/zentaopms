152afb93ed856fda68a14355acffb4a4664055a6
sunguangming <sunguangming@easycorp.ltd>
2024-04-11 09:47:26 +0800

    * Fix upgrade sql.

--- db/update20.0.sql
+++ db/update20.0.sql
@@ -40,7 +40,7 @@ UPDATE `zt_story` SET stage = 'defining'   WHERE stage = 'wait' AND (parent = -1
 UPDATE `zt_story` SET stage = 'planning'   WHERE stage in ('planned', 'projected') AND (parent = -1 OR type != 'story');
 UPDATE `zt_story` SET stage = 'developing' WHERE stage in ('developed', 'testing', 'tested') AND (parent = -1 OR type != 'story');
 UPDATE `zt_story` SET stage = 'delivering' WHERE stage in ('verified', 'released') AND (parent = -1 OR type != 'story');
-UPDATE `zt_story` SET isParent = 1 WHERE parent = -1;
+UPDATE `zt_story` SET isParent = '1' WHERE parent = -1;
 UPDATE `zt_story` SET grade = 1, parent = 0, root = id, path = concat(',', id, ',') WHERE type != 'story';
 UPDATE `zt_story` SET grade = 1, parent = 0, root = id, path = concat(',', id, ',') WHERE type = 'story' AND parent <= 0;
 UPDATE `zt_story` SET grade = 2, root = parent, path = concat(',', parent, ',', id, ',') WHERE type = 'story' AND parent > 0;
