4492d43b285bd147916cfea87fa73e0c4d0f96c8
sunguangming <sunguangming@easycorp.ltd>
2024-04-12 15:25:56 +0800

    * Add unit test.

--- module/story/test/model/getkanbangroupdata.php
+++ module/story/test/model/getkanbangroupdata.php
@@ -4,18 +4,18 @@
 /**
 
 title=测试 storyModel->getKanbanGroupData();
+timeout=0
 cid=0
 
-- 传入stories数据，获取按照stage分组后的结果数量 @2
-- 传入stories数据，获取按照stage分组后的结果数量 @2
-- 传入stories数据，获取按照stage分组后的结果详情
- - 第2条的title属性 @软件需求2
- - 第2条的type属性 @story
- - 第2条的stage属性 @wait
-- 传入stories数据，获取按照stage分组后的结果详情
- - 第6条的title属性 @软件需求6
- - 第6条的type属性 @story
- - 第6条的stage属性 @projected
+- 查看获取到的第一个需求看板分组数据数量 @3
+- 查看获取到的第二个需求看板分组数据数量 @3
+- 查看获取到的第一个需求看板分组数据的第一个列的名称
+ - 第0条的name属性 @1
+ - 第0条的title属性 @已立项
+- 查看获取到的第二个需求看板分组数据的第一个泳道的名称
+ - 第0条的name属性 @1
+ - 第0条的title属性 @~~
+- 查看获取到的第二个需求看板分组数据的所有Items数量 @5
 
 */
 include dirname(__FILE__, 5) . '/test/lib/init.php';
@@ -31,7 +31,8 @@ $stories2 = $tester->story->getProductStories(2);
 $storyGroup1 = $tester->story->getKanbanGroupData($stories1);
 $storyGroup2 = $tester->story->getKanbanGroupData($stories2);
 
-r(count($storyGroup1))        && p()                     && e('2');                          //传入stories数据，获取按照stage分组后的结果数量
-r(count($storyGroup2))        && p()                     && e('2');                          //传入stories数据，获取按照stage分组后的结果数量
-r($storyGroup1['wait'])       && p('2:title,type,stage') && e('软件需求2,story,wait');       //传入stories数据，获取按照stage分组后的结果详情
-r($storyGroup2['projected'])  && p('6:title,type,stage') && e('软件需求6,story,projected');  //传入stories数据，获取按照stage分组后的结果详情
+r(count($storyGroup1))    && p()               && e('3');        // 查看获取到的第一个需求看板分组数据数量
+r(count($storyGroup2))    && p()               && e('3');        // 查看获取到的第二个需求看板分组数据数量
+r($storyGroup1['cols'])   && p('0:name,title') && e('1,已立项'); // 查看获取到的第一个需求看板分组数据的第一个列的名称
+r($storyGroup2['lanes'])  && p('0:name,title') && e('1,~~');     // 查看获取到的第二个需求看板分组数据的第一个泳道的名称
+r(count($storyGroup2['items'], true)) && p() && e('5');          // 查看获取到的第二个需求看板分组数据的所有Items数量
