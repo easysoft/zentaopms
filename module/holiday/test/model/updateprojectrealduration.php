dff511bce83e54051577473ab4db46aaeec42c2d
tianshujie <tianshujie@easycorp.ltd>
2024-04-12 15:08:14 +0800

    * Change the result of the holiday::updateprojectrealduration unit test.

--- module/holiday/test/model/updateprojectrealduration.php
+++ module/holiday/test/model/updateprojectrealduration.php
@@ -7,13 +7,13 @@ cid=1
 
 - 测试插入id为 10 的节假日时迭代 11 项目的实际工期 @250
 - 测试插入id为 10 的节假日时迭代 60 项目的实际工期 @252
-- 测试插入id为 10 的节假日时迭代 101 项目的实际工期 @191
+- 测试插入id为 10 的节假日时迭代 101 项目的实际工期 @168
 - 测试插入id为 5 的节假日时迭代 11 项目的实际工期 @250
 - 测试插入id为 5 的节假日时迭代 60 项目的实际工期 @252
-- 测试插入id为 5 的节假日时迭代 101 项目的实际工期 @191
+- 测试插入id为 5 的节假日时迭代 101 项目的实际工期 @168
 - 测试插入id为 5 的节假日时迭代 11 项目的实际工期 @250
 - 测试插入id为 5 的节假日时迭代 60 项目的实际工期 @252
-- 测试插入id为 5 的节假日时迭代 101 项目的实际工期 @191
+- 测试插入id为 5 的节假日时迭代 101 项目的实际工期 @168
 
 */
 declare(strict_types=1);
@@ -34,12 +34,12 @@ $holiday = new holidayTest();
 
 r($holiday->updateProjectRealDurationTest($projectIdList[0], $holidayIdList[0], $updateDuration[0])) && p() && e('250'); //测试插入id为 10 的节假日时迭代 11 项目的实际工期
 r($holiday->updateProjectRealDurationTest($projectIdList[1], $holidayIdList[0], $updateDuration[1])) && p() && e('252'); //测试插入id为 10 的节假日时迭代 60 项目的实际工期
-r($holiday->updateProjectRealDurationTest($projectIdList[2], $holidayIdList[0], $updateDuration[1])) && p() && e('191'); //测试插入id为 10 的节假日时迭代 101 项目的实际工期
+r($holiday->updateProjectRealDurationTest($projectIdList[2], $holidayIdList[0], $updateDuration[1])) && p() && e('168'); //测试插入id为 10 的节假日时迭代 101 项目的实际工期
 
 r($holiday->updateProjectRealDurationTest($projectIdList[0], $holidayIdList[1], $updateDuration[0])) && p() && e('250'); //测试插入id为 5 的节假日时迭代 11 项目的实际工期
 r($holiday->updateProjectRealDurationTest($projectIdList[1], $holidayIdList[1], $updateDuration[1])) && p() && e('252'); //测试插入id为 5 的节假日时迭代 60 项目的实际工期
-r($holiday->updateProjectRealDurationTest($projectIdList[2], $holidayIdList[1], $updateDuration[1])) && p() && e('191'); //测试插入id为 5 的节假日时迭代 101 项目的实际工期
+r($holiday->updateProjectRealDurationTest($projectIdList[2], $holidayIdList[1], $updateDuration[1])) && p() && e('168'); //测试插入id为 5 的节假日时迭代 101 项目的实际工期
 
 r($holiday->updateProjectRealDurationTest($projectIdList[0], $holidayIdList[2], $updateDuration[0])) && p() && e('250'); //测试插入id为 5 的节假日时迭代 11 项目的实际工期
 r($holiday->updateProjectRealDurationTest($projectIdList[1], $holidayIdList[2], $updateDuration[1])) && p() && e('252'); //测试插入id为 5 的节假日时迭代 60 项目的实际工期
-r($holiday->updateProjectRealDurationTest($projectIdList[2], $holidayIdList[2], $updateDuration[1])) && p() && e('191'); //测试插入id为 5 的节假日时迭代 101 项目的实际工期
+r($holiday->updateProjectRealDurationTest($projectIdList[2], $holidayIdList[2], $updateDuration[1])) && p() && e('168'); //测试插入id为 5 的节假日时迭代 101 项目的实际工期
