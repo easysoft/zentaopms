97fea417bbf6b41a96c9c9cb341a127efc6b6b47
tianshujie <tianshujie@easycorp.ltd>
2024-04-12 14:51:17 +0800

    * Adjust the x-axis label angle.

--- module/block/ui/productstatisticblock.html.php
+++ module/block/ui/productstatisticblock.html.php
@@ -193,9 +193,11 @@ statisticBlock
             echarts
             (
                 set::color(array('#2B80FF', '#17CE97')),
+                set::width('100%'),
+                set::height(170),
                 set::grid(array('left' => '10px', 'top' => '30px', 'right' => '0', 'bottom' => '0',  'containLabel' => true)),
                 set::legend(array('show' => true, 'right' => '0')),
-                set::xAxis(array('type' => 'category', 'data' => array_keys($monthFinish), 'splitLine' => array('show' => false), 'axisTick' => array('alignWithLabel' => true, 'interval' => '0'))),
+                set::xAxis(array('type' => 'category', 'data' => array_keys($monthFinish), 'splitLine' => array('show' => false), 'axisTick' => array('alignWithLabel' => true, 'interval' => '0'), 'axisLabel' => array('rotate' => 45))),
                 set::yAxis(array('type' => 'value', 'name' => $lang->number, 'splitLine' => array('show' => false), 'axisLine' => array('show' => true, 'color' => '#DDD'))),
                 set::series
                 (
@@ -217,7 +219,7 @@ statisticBlock
                         )
                     )
                 )
-            )->size('100%', 170),
+            )
         ),
         ($product->newPlan || $product->newExecution || $product->newRelease) ? col
         (
