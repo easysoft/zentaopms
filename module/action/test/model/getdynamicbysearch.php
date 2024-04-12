0a7cf22ce0cbeae7cb87af3ab7df1acbc41483d0
liumengyi <liumengyi@easycorp.ltd>
2024-04-12 15:30:01 +0800

    * Modify unit test result of actionModel::getDynamicBySearch.

--- module/action/test/model/getdynamicbysearch.php
+++ module/action/test/model/getdynamicbysearch.php
@@ -10,25 +10,25 @@ timeout=0
 cid=1
 
 - 获取排序为date倒序的所有动态
- - 第0条的id属性 @90
- - 第0条的objectType属性 @entry
- - 第1条的id属性 @60
- - 第1条的objectType属性 @doclib
+ - 第0条的id属性 @64
+ - 第0条的objectType属性 @testsuite
+ - 第1条的id属性 @32
+ - 第1条的objectType属性 @testcase
 - 获取排序为date正序的所有动态
- - 第0条的id属性 @61
- - 第0条的objectType属性 @todo
+ - 第0条的id属性 @65
+ - 第0条的objectType属性 @caselib
+ - 第1条的id属性 @33
+ - 第1条的objectType属性 @case
+- 获取排序为date倒序的今年之后的动态
+ - 第0条的id属性 @64
+ - 第0条的objectType属性 @testsuite
+ - 第1条的id属性 @32
+ - 第1条的objectType属性 @testcase
+- 获取排序为date倒序的今天之前的动态
+ - 第0条的id属性 @63
+ - 第0条的objectType属性 @module
  - 第1条的id属性 @31
  - 第1条的objectType属性 @bug
-- 获取排序为date倒序的今年之后的动态
- - 第0条的id属性 @90
- - 第0条的objectType属性 @entry
- - 第1条的id属性 @60
- - 第1条的objectType属性 @doclib
-- 获取排序为date倒序的今天之前的动态
- - 第0条的id属性 @89
- - 第0条的objectType属性 @testreport
- - 第1条的id属性 @59
- - 第1条的objectType属性 @doc
 - 获取排序为date倒序的今年之后的动态
  - 第0条的id属性 @1
  - 第0条的objectType属性 @product
@@ -57,8 +57,8 @@ $directionList = array('next', 'pre');
 
 $action = new actionTest();
 
-r($action->getDynamicBySearchTest($queryID[0], $orderByList[0], $limit, $dateList[0], $directionList[0])) && p('0:id,objectType;1:id,objectType') && e('90,entry;60,doclib');   // 获取排序为date倒序的所有动态
-r($action->getDynamicBySearchTest($queryID[0], $orderByList[1], $limit, $dateList[0], $directionList[0])) && p('0:id,objectType;1:id,objectType') && e('61,todo;31,bug');       // 获取排序为date正序的所有动态
-r($action->getDynamicBySearchTest($queryID[0], $orderByList[0], $limit, $dateList[0], $directionList[1])) && p('0:id,objectType;1:id,objectType') && e('90,entry;60,doclib');   // 获取排序为date倒序的今年之后的动态
-r($action->getDynamicBySearchTest($queryID[0], $orderByList[0], $limit, $dateList[1], $directionList[0])) && p('0:id,objectType;1:id,objectType') && e('89,testreport;59,doc'); // 获取排序为date倒序的今天之前的动态
+r($action->getDynamicBySearchTest($queryID[0], $orderByList[0], $limit, $dateList[0], $directionList[0])) && p('0:id,objectType;1:id,objectType') && e('64,testsuite;32,testcase');   // 获取排序为date倒序的所有动态
+r($action->getDynamicBySearchTest($queryID[0], $orderByList[1], $limit, $dateList[0], $directionList[0])) && p('0:id,objectType;1:id,objectType') && e('65,caselib;33,case');       // 获取排序为date正序的所有动态
+r($action->getDynamicBySearchTest($queryID[0], $orderByList[0], $limit, $dateList[0], $directionList[1])) && p('0:id,objectType;1:id,objectType') && e('64,testsuite;32,testcase');   // 获取排序为date倒序的今年之后的动态
+r($action->getDynamicBySearchTest($queryID[0], $orderByList[0], $limit, $dateList[1], $directionList[0])) && p('0:id,objectType;1:id,objectType') && e('63,module;31,bug'); // 获取排序为date倒序的今天之前的动态
 r($action->getDynamicBySearchTest($queryID[1], $orderByList[0], $limit, $dateList[0], $directionList[1])) && p('0:id,objectType')                 && e('1,product');            // 获取排序为date倒序的今年之后的动态
