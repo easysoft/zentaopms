4c91760c1e7a30142f3bdc09c4df539bc44aa0a4
liyang <liyang@easycorp.ltd>
2024-04-12 10:46:38 +0800

    * Adjust test case use new load page method.

--- module/product/test/lib/createproduct.ui.class.php
+++ module/product/test/lib/createproduct.ui.class.php
@@ -1,4 +1,5 @@
 <?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class createProductTester extends tester
 {
     /**
@@ -8,11 +9,11 @@ class createProductTester extends tester
      * @access public
      * @return object
      */
-    public function checkLocatePage($name)
+    public function checkLocating($productName)
     {
         $this->login();
         $form = $this->initForm('product', 'create');
-        $form->dom->name->setValue($name);
+        $form->dom->name->setValue($productName);
         $form->dom->btn($this->lang->save)->click();
         $form->wait(1);
         return $this->response();
@@ -25,24 +26,24 @@ class createProductTester extends tester
      * @access public
      * @return object
      */
-    public function createDefault($name)
+    public function createDefault($productName)
     {
-        /* 提交表单。 */
         $this->login();
+
+        /* 提交表单。 */
         $form = $this->initForm('product', 'create');
-        $form->dom->name->setValue($name);
+        $form->dom->name->setValue($productName);
         $form->dom->btn($this->lang->save)->click();
         $form->wait(1);
-
         if($this->response('method') != 'browse') return $this->failed($form->dom->getFormTips());
 
         /* 跳转到产品需求页面，点击设置菜单查看产品设置页面。 */
-        $browsePage = $this->initPage('product', 'browse');
+        $browsePage = $this->loadPage('product', 'browse');
         $browsePage->dom->settings->click();
 
-        $viewPage = $this->initPage('product', 'view');
+        $viewPage = $this->loadPage('product', 'view');
         $this->app->loadLang('product');
-        if($viewPage->dom->productName->getText() != $name) return $this->failed('名称错误');
+        if($viewPage->dom->productName->getText() != $productName) return $this->failed('名称错误');
         if($viewPage->dom->type->getText() != $this->lang->product->typeList['normal']) return $this->failed('类型错误');
         if($viewPage->dom->acl->getText()  != $this->lang->product->abbr->aclList['open']) return $this->failed('权限错误');
 
@@ -69,10 +70,10 @@ class createProductTester extends tester
         if($this->response('method') != 'browse') return $this->failed($form->dom->getFormTips());
 
         /* 跳转到产品需求页面，点击设置菜单查看产品设置页面。 */
-        $browsePage = $this->initPage('product', 'browse');
+        $browsePage = $this->loadPage('product', 'browse');
         $browsePage->dom->settings->click();
 
-        $viewPage = $this->initPage('product', 'view');
+        $viewPage = $this->loadPage('product', 'view');
         $this->app->loadLang('product');
         if($viewPage->dom->productName->getText() != $name) return $this->failed('名称错误');
         if($viewPage->dom->type->getText() != $this->lang->product->typeList['branch']) return $this->failed('类型错误');
--- module/product/test/ui/createproduct.php
+++ module/product/test/ui/createproduct.php
@@ -1,8 +1,6 @@
 #!/usr/bin/env php
 <?php
-
 /**
-
 title=用户登录
 timeout=0
 cid=10001
@@ -13,26 +11,24 @@ SUCCESS
 product
 browse
 SUCCESS
-
-
 */
-
-include dirname(__FILE__, 5) . '/test/lib/ui.php';
-include dirname(__FILE__, 2) . '/lib/createproduct.ui.class.php';
+chdir(__DIR__);
+include '../lib/createproduct.ui.class.php';
 
 zdTable('product')->config('product', false, 2)->gen(10);
-$createTester = new createProductTester();
+//zendata::loadYaml('product', false, 2)->createFor('product', 10);
+$tester = new createProductTester();
 
-$nameList = array();
-$nameList['null']    = '';
-$nameList['default'] = '默认产品';
-$nameList['normal']  = '正常产品';
-$nameList['branch']  = '多分支产品产品';
+$products = array();
+$products['null']    = '';
+$products['default'] = '默认产品';
+$products['normal']  = '正常产品';
+$products['branch']  = '多分支产品产品';
 
-r($createTester->createDefault($nameList['null']))       && p('message:nameTip')   && e('『产品名称』不能为空。');                     // 缺少产品名称，创建失败
-r($createTester->createDefault($nameList['default']))    && p('status')            && e('SUCCESS');                                    // 使用默认选项创建产品
-r($createTester->createDefault($nameList['default']))    && p('message:nameTip')   && e('『产品名称』已经有『默认产品』这条记录了。'); // 创建重复名称的产品
-r($createTester->checkLocatePage($nameList['normal']))   && p('module,method')     && e('product,browse');                             // 创建正常产品后的跳转链接检查
-r($createTester->createMultiBranch($nameList['branch'])) && p('status')            && e('SUCCESS');                                    // 创建正常产品成功
+r($tester->createDefault($products['null']))       && p('message:nameTip') && e('『产品名称』不能为空。');                     // 缺少产品名称，创建失败
+r($tester->createDefault($products['default']))    && p('status')          && e('SUCCESS');                                    // 使用默认选项创建产品
+r($tester->createDefault($products['default']))    && p('message:nameTip') && e('『产品名称』已经有『默认产品』这条记录了。'); // 创建重复名称的产品
+r($tester->checkLocating($products['normal']))     && p('module,method')   && e('product,browse');                             // 创建正常产品后的跳转链接检查
+r($tester->createMultiBranch($products['branch'])) && p('status')          && e('SUCCESS');                                    // 创建正常产品成功
 
-$createTester->closeBrowser();
+$tester->closeBrowser();
