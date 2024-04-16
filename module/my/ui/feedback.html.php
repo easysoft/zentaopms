a4ff1a27754a71b77c77e75d21d7b15c5b58af21
wangyuting <wangyuting@easycorp.ltd>
2024-04-16 10:12:04 +0800

    * Finish task #116393.

--- module/my/ui/feedback.html.php
+++ module/my/ui/feedback.html.php
@@ -19,18 +19,60 @@ featureBar
     li(searchToggle(set::module($this->app->rawMethod . 'Feedback'), set::open($type == 'bysearch')))
 );
 
-$feedbacks = initTableData($feedbacks, $config->feedback->dtable->fieldList, $this->feedback);
-$cols      = array_values($config->feedback->dtable->fieldList);
-$data      = array_values($feedbacks);
+foreach($feedbacks as $feedback)
+{
+    $feedback->solution = zget($lang->feedback->solutionList, $feedback->solution, '');
+}
+
+$cols      = $this->loadModel('datatable')->getSetting($this->moduleName);
+$feedbacks = initTableData($feedbacks, $cols, $this->feedback);
+
+if(!empty($cols['product'])) $cols['product']['map'] = $allProducts;
+if(!empty($cols['module']))  $cols['module']['map']  = $modules;
+if(!empty($cols['dept']))    $cols['dept']['map']    = $depts;
+
+$canBatchEdit     = common::hasPriv('feedback', 'batchEdit');
+$canBatchClose    = common::hasPriv('feedback', 'batchClose');
+$canBatchAssignTo = common::hasPriv('feedback', 'batchAssignTo');
+$canBatchAction   = $canBatchEdit || $canBatchClose || $canBatchAssignTo;
+
+$footToolbar     = array();
+$assignedToItems = array();
+foreach($users as $key => $value)
+{
+    if($value) $assignedToItems[] = array('text' => $value, 'innerClass' => 'batch-btn ajax-btn not-open-url', 'data-url' => helper::createLink('feedback', 'batchAssignTo', "assignedTo=$key"));
+}
+
+if($canBatchAction)
+{
+    $footToolbar['items'] = array();
+    if($canBatchEdit)
+    {
+        $footToolbar['items'][] = array('text' => $lang->edit, 'className' => 'primary batch-btn not-open-url', 'data-url' => createLink('feedback', 'batchEdit', "browseType=$type"));
+    }
+    if($canBatchClose)
+    {
+        $footToolbar['items'][] = array('text' => $lang->close, 'className' => 'primary batch-btn not-open-url', 'data-url' => createLink('feedback', 'batchClose'));
+    }
+    if($canBatchAssignTo)
+    {
+        $footToolbar['items'][] = array('caret' => 'up', 'text' => $lang->feedback->assignedTo, 'type' => 'dropdown', 'data-placement' => 'top-start', 'items' => $assignedToItems, 'data-menu' => array('searchBox' => true));
+    }
+    $footToolbar['btnProps'] = array('size' => 'sm', 'btnType' => 'secondary');
+}
+
 dtable
 (
-    set::cols($cols),
-    set::data($data),
+    set::cols(array_values($cols)),
+    set::data(array_values($feedbacks)),
+    set::checkable($canBatchAction),
     set::userMap($users),
     set::fixedLeftWidth('44%'),
     set::orderBy($orderBy),
+    set::customCols(true),
     set::sortLink(createLink('my', $app->rawMethod, "mode={$mode}&type={$type}&param={$param}&orderBy={name}_{sortType}&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}&pageID={$pager->pageID}")),
     set::footPager(usePager()),
+    set::footToolbar($footToolbar),
     set::emptyTip($lang->task->noTask)
 );
 
