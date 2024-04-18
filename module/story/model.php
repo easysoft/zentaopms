571b2c5d0f7be7cb452a53d36d8778069f7b4a69
sunguangming <sunguangming@easycorp.ltd>
2024-04-18 10:56:26 +0800

    * Finish task #116682.

--- module/story/model.php
+++ module/story/model.php
@@ -368,7 +368,6 @@ class storyModel extends model
             ->leftJoin(TABLE_PRODUCT)->alias('t3')->on('t1.product = t3.id')
             ->where('t1.project')->eq($executionID)
             ->andWhere('t2.deleted')->eq('0')
-            ->andWhere('t2.type')->eq('story')
             ->beginIF($productID)->andWhere('t2.product')->eq($productID)->fi()
             ->beginIF($branch !== 'all')->andWhere('t2.branch')->in("0,$branch")->fi()
             ->beginIF($moduleIdList)->andWhere('t2.module')->in($moduleIdList)->fi()
