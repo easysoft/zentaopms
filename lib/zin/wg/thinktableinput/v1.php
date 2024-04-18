98abbfa5deb87e8157518f2d97b9f66768d4964b
Zhangyu <zhangyu@easycorp.ltd>
2024-04-18 07:55:06 +0000

    * zin: modify thinktableinput wg.

--- lib/zin/wg/thinktableinput/v1.php
+++ lib/zin/wg/thinktableinput/v1.php
@@ -12,12 +12,12 @@ class thinkTableInput extends thinkStep
 {
     protected static array $defineProps = array(
         'isRequired?: bool',                        // 是否必填
-        'isRequiredName?: string="isRequired"',     // 是否必填对应的name
+        'isRequiredName?: string="required"',       // 是否必填对应的name
         'requiredRows?: number=1',                  // 必填行数
         'requiredRowsName?: string="requiredRows"', // 必填行数对应的name
         'rowsTitle?: array',                        // 行标题
-        'rowsTitleName: string="rowsTitle"',        // 行标题对应的name
-        'isSupportAdd?: bool',                       // 是否支持用户添加行
+        'rowsTitleName: string="fields"',           // 行标题对应的name
+        'isSupportAdd?: bool',                      // 是否支持用户添加行
         'isSupportAddName: string="isSupportAdd"',  // 是否支持用户添加行的name
         'canAddRows: number=1',                     // 可添加行数
         'canAddRowsName: string="canAddRows"',      // 可添加行数对应的name
@@ -38,12 +38,12 @@ class thinkTableInput extends thinkStep
             formGroup
             (
                 setClass('w-1/2'),
-                set::label($lang->thinkmodel->isRequired),
+                set::label($lang->thinkwizard->step->label->required),
                 radioList
                 (
                     set::name($isRequiredName),
                     set::inline(true),
-                    set::items($lang->thinkmodel->selectList),
+                    set::items($lang->thinkwizard->step->requiredList),
                     set::value($isRequired ? $isRequired : 0),
                     on::change('changeIsRequired')
                 )
@@ -51,13 +51,13 @@ class thinkTableInput extends thinkStep
             formGroup
             (
                 setClass('w-1/2 hidden required-rows'),
-                set::label($lang->thinkmodel->requiredRows),
+                set::label($lang->thinkwizard->step->label->requiredRows),
                 set::labelClass('required'),
                 input
                 (
                     set::name($requiredRowsName),
                     set::value($requiredRows),
-                    set::placeholder($lang->thinkmodel->inputPlaceholder),
+                    set::placeholder($lang->thinkwizard->step->inputContent),
                     set::min(1),
                     on::input('changeInput')
                 )
@@ -75,12 +75,12 @@ class thinkTableInput extends thinkStep
             formGroup
             (
                 setClass('w-1/2'),
-                set::label($lang->thinkmodel->isSupportAdd),
+                set::label($lang->thinkwizard->step->label->isSupportAdd),
                 radioList
                 (
                     set::name($isSupportAddName),
                     set::inline(true),
-                    set::items($lang->thinkmodel->selectList),
+                    set::items($lang->thinkwizard->step->requiredList),
                     set::value($isSupportAdd ? $isSupportAdd : 0),
                     on::change('changeSupportAdd')
                 )
@@ -88,14 +88,14 @@ class thinkTableInput extends thinkStep
             formGroup
             (
                 setClass($isSupportAdd ? 'w-1/2' : 'w-1/2 hidden can-add-rows'),
-                set::label($lang->thinkmodel->canAddRows),
+                set::label($lang->thinkwizard->step->label->canAddRows),
                 set::labelClass('required'),
                 input
                 (
                     set::type('number'),
                     set::name($canAddRowsName),
                     set::value($canAddRows),
-                    set::placeholder($lang->thinkmodel->inputPlaceholder),
+                    set::placeholder($lang->thinkwizard->step->inputContent),
                     set::min(1),
                     on::input('changeInput')
                 )
@@ -112,7 +112,7 @@ class thinkTableInput extends thinkStep
             formGroup
             (
                 set::width('full'),
-                set::label($lang->thinkmodel->rowsTitle),
+                set::label($lang->thinkwizard->step->label->rowsTitle),
                 thinkoptions
                 (
                     set(array(
