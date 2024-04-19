383c6b8ff8fc4a785b30ca78499acef289054315
xieqiyu <xieqiyu@easycorp.ltd>
2024-04-19 10:33:13 +0800

    * Fix bug#47920.

--- module/product/ui/browse.html.php
+++ module/product/ui/browse.html.php
@@ -48,7 +48,7 @@ $fnGenerateSideBar = function() use ($moduleTree, $moduleID, $productID, $branch
             set::activeKey($isProjectStory && empty($param) && !empty($productID) && count($projectProducts) > 1 ? $productID : $moduleID),
             set::closeLink(helper::createLink($app->rawModule, $app->rawMethod, http_build_query($params))),
             $productID ? set::settingLink(helper::createLink('tree', 'browse', "rootID=$productID&view=story&currentModuleID=0&branch=$branchID")) : null,
-            set::settingApp($isProjectStory ? 'project' : 'product')
+            set::settingApp($isProjectStory && !$projectHasProduct ? 'project' : 'product')
         )
     );
 };
