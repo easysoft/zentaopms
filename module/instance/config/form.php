cf70dd5c7d89dd30b0e5406590ab81d0565795b4
caoyanyi <caoyanyi@easycorp.ltd>
2024-04-22 16:21:42 +0800

    * Move user account setting of instance.

--- module/instance/config/form.php
+++ module/instance/config/form.php
@@ -1,5 +1,4 @@
 <?php
-global $app;
 $config->instance->form = new stdclass();
 $config->instance->form->create = array();
 $config->instance->form->create['appType']     = array('type' => 'string',   'required' => true,  'default' => '');
@@ -7,7 +6,6 @@ $config->instance->form->create['name']        = array('type' => 'string', 'requ
 $config->instance->form->create['url']         = array('type' => 'string', 'required' => true,  'default' => '', 'filter' => 'trim');
 $config->instance->form->create['account']     = array('type' => 'string', 'required' => true,  'default' => '', 'filter' => 'trim');
 $config->instance->form->create['password']    = array('type' => 'string', 'required' => true,  'default' => '', 'filter' => 'trim');
-$config->instance->form->create['createdBy']   = array('type' => 'string', 'required' => false, 'default' => isset($app->user->account) ? $app->user->account : '');
 $config->instance->form->create['createdDate'] = array('type' => 'string', 'required' => false, 'default' => helper::now());
 $config->instance->form->create['private']     = array('type' => 'string', 'required' => false, 'default' => uniqid());
 
@@ -16,7 +14,6 @@ $config->instance->form->edit['name']       = array('type' => 'string', 'require
 $config->instance->form->edit['url']        = array('type' => 'string', 'required' => true,  'default' => '', 'filter' => 'trim');
 $config->instance->form->edit['account']    = array('type' => 'string', 'required' => true,  'default' => '', 'filter' => 'trim');
 $config->instance->form->edit['password']   = array('type' => 'string', 'required' => true,  'default' => '', 'filter' => 'trim');
-$config->instance->form->edit['editedBy']   = array('type' => 'string', 'required' => false, 'default' => isset($app->user->account) ? $app->user->account : '');
 $config->instance->form->edit['editedDate'] = array('type' => 'string', 'required' => false, 'default' => helper::now());
 
 $config->instance->form->install = array();
