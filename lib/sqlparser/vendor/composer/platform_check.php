88328f86b2420f6a77d8944e47508372e18ef9e5
zhouxin <zhouxin@easycorp.ltd>
2024-04-22 13:56:36 +0800

    * Fix php version error of sqlparser.

--- lib/sqlparser/vendor/composer/platform_check.php
+++ lib/sqlparser/vendor/composer/platform_check.php
@@ -4,7 +4,7 @@
 
 $issues = array();
 
-if (!(PHP_VERSION_ID >= 70200)) {
+if (!(PHP_VERSION_ID >= 50400)) {
     $issues[] = 'Your Composer dependencies require a PHP version ">= 7.2.0". You are running ' . PHP_VERSION . '.';
 }
 
