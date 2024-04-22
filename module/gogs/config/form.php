47f72739f9bfa03b06e8e9aa2a4fedb8c74d5190
caoyanyi <caoyanyi@easycorp.ltd>
2024-04-22 16:19:51 +0800

    * Move user account setting of gogs.

--- module/gogs/config/form.php
+++ module/gogs/config/form.php
@@ -1,5 +1,4 @@
 <?php
-global $app;
 $config->gogs->form = new stdclass();
 $config->gogs->form->create = array();
 $config->gogs->form->create['appType']     = array('type' => 'string',   'required' => true,  'default' => 'gogs');
@@ -7,7 +6,6 @@ $config->gogs->form->create['type']        = array('type' => 'string',   'requir
 $config->gogs->form->create['name']        = array('type' => 'string',   'required' => true, 'default' => '', 'filter' => 'trim');
 $config->gogs->form->create['url']         = array('type' => 'string',   'required' => true, 'default' => '', 'filter' => 'trim');
 $config->gogs->form->create['token']       = array('type' => 'string',   'required' => true,  'default' => '', 'filter' => 'trim');
-$config->gogs->form->create['createdBy']   = array('type' => 'string',   'required' => false, 'default' => $app->user->account);
 $config->gogs->form->create['createdDate'] = array('type' => 'string', 'required' => false, 'default' => helper::now());
 $config->gogs->form->create['private']     = array('type' => 'string',   'required' => false, 'default' => uniqid());
 
@@ -15,5 +13,4 @@ $config->gogs->form->edit = array();
 $config->gogs->form->edit['name']       = array('type' => 'string', 'required' => true,  'default' => '', 'filter' => 'trim');
 $config->gogs->form->edit['url']        = array('type' => 'string', 'required' => true,  'default' => '', 'filter' => 'trim');
 $config->gogs->form->edit['token']      = array('type' => 'string', 'required' => true,  'default' => '', 'filter' => 'trim');
-$config->gogs->form->edit['editedBy']   = array('type' => 'string', 'required' => false, 'default' => $app->user->account);
 $config->gogs->form->edit['editedDate'] = array('type' => 'string', 'required' => false, 'default' => helper::now());
