f07035a4fc2030d397a7db05d0d245530be7156e
caoyanyi <caoyanyi@easycorp.ltd>
2024-04-22 10:48:53 +0800

    * Adjust unit test case for repo.

--- module/repo/test/model/getgitlabprojects.php
+++ module/repo/test/model/getgitlabprojects.php
@@ -1,7 +1,6 @@
 #!/usr/bin/env php
 <?php
 include dirname(__FILE__, 5) . '/test/lib/init.php';
-su('admin');
 
 /**
 
@@ -9,24 +8,28 @@ title=测试 gitlabModel::getGitlabProjects();
 timeout=0
 cid=1
 
-- 使用正确的gitlabID,空的filter查询群组属性name @testHtml
-- 普通用户使用正确的gitlabID,IS_DEVELOPER filter查询群组属性name @testHtml
-- 普通用户使用正确的gitlabID,filter查询群组属性name @testHtml
+- 使用正确的gitlabID,空的filter查询群组属性name @unittest1
+- 普通用户使用正确的gitlabID,IS_DEVELOPER filter查询群组属性name @privateProject
+- 普通用户使用正确的gitlabID,filter查询群组属性name @unittest1
 
 */
 
 zdTable('pipeline')->gen(5);
 zdTable('user')->gen(10);
+zdTable('repo')->config('repo')->gen(5);
 zdTable('oauth')->config('oauth')->gen(5);
+su('admin');
 $repo = $tester->loadModel('repo');
 
 $gitlabID       = 1;
 $projectFilters = array('IS_DEVELOPER', 'ALL');
 
 $result = $repo->getGitlabProjects($gitlabID, '');
-r(end($result)) && p('name') && e('testHtml'); //使用正确的gitlabID,空的filter查询群组
+r(end($result)) && p('name') && e('unittest1'); //使用正确的gitlabID,空的filter查询群组
+
 su('user6');
 $result = $repo->getGitlabProjects($gitlabID, $projectFilters[0]);
-r(end($result)) && p('name') && e('testHtml'); //普通用户使用正确的gitlabID,IS_DEVELOPER filter查询群组
+r(end($result)) && p('name') && e('privateProject'); //普通用户使用正确的gitlabID,IS_DEVELOPER filter查询群组
+
 $result = $repo->getGitlabProjects($gitlabID, $projectFilters[1]);
-r(end($result)) && p('name') && e('testHtml'); //普通用户使用正确的gitlabID,filter查询群组
+r(end($result)) && p('name') && e('unittest1'); //普通用户使用正确的gitlabID,filter查询群组
--- module/repo/test/model/getimportedprojects.php
+++ module/repo/test/model/getimportedprojects.php
@@ -10,11 +10,14 @@ title=测试 repoModel->getImportedProjects();
 timeout=0
 cid=1
 
+- 查询已关联的版本库服务器ID为1的版本库列表属性1 @1
+- 查询未关联的版本库服务器ID为2的版本库列表，结果应该为空属性1 @0
+
 */
 
 zdTable('repo')->config('repo')->gen(4);
 
 $repo = $tester->loadModel('repo');
 
-r($repo->getImportedProjects(1)) && p('1:serviceProject') && e('1'); // 查询已关联的版本库服务器ID为1的版本库列表
-r($repo->getImportedProjects(2)) && p('1:serviceProject') && e('0'); // 查询未关联的版本库服务器ID为2的版本库列表，结果应该为空
+r($repo->getImportedProjects(1)) && p('1') && e('1'); // 查询已关联的版本库服务器ID为1的版本库列表
+r($repo->getImportedProjects(2)) && p('1') && e('0'); // 查询未关联的版本库服务器ID为2的版本库列表，结果应该为空
