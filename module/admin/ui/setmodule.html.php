fc673b76ce40b98ec5535bf0262e60673ad4b6c1
liumengyi <liumengyi@easycorp.ltd>
2024-04-24 11:26:49 +0800

    * Fix bug #48310.

--- module/admin/ui/setmodule.html.php
+++ module/admin/ui/setmodule.html.php
@@ -50,7 +50,7 @@ foreach($config->featureGroup as $group => $features)
                 setID("module{$code}"),
                 set::type('hidden'),
                 set::name("module[{$code}]"),
-                set::value($value)
+                set::value(1)
             );
         }
         $rows[] = h::tr
