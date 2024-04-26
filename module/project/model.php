82be78538f4be0eec707d23155df1862c6dae733
wangyuting <wangyuting@easycorp.ltd>
2024-04-26 10:43:42 +0800

    * Fix ipd project only one execution bug.

--- module/project/model.php
+++ module/project/model.php
@@ -222,8 +222,8 @@ class projectModel extends model
         $this->app->loadClass('pager', true);
         foreach($projects as $projectID => $project)
         {
-            $orderBy = $project->model == 'waterfall' ? 'id_asc' : 'id_desc';
-            $pager   = $project->model == 'waterfall' ? null : new pager(0, 1, 1);
+            $orderBy = in_array($project->model, array('waterfall', 'ipd')) ? 'id_asc' : 'id_desc';
+            $pager   = in_array($project->model, array('waterfall', 'ipd')) ? null : new pager(0, 1, 1);
             $project->executions = $this->loadModel('execution')->getStatData($projectID, 'undone', 0, 0, false, '', $orderBy, $pager);
             $project->teamCount  = isset($teamCount[$projectID]) ? $teamCount[$projectID] : 0;
             $project->estimate   = isset($estimates[$projectID]) ? round($estimates[$projectID]->estimate, 2) : 0;
