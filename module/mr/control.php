e163380c7cfe2e20503ead6e0f3304572e41da45
dingguodong <dingguodong@easycorp.ltd>
2024-04-26 15:25:12 +0800

    * Use the author instead of committer.

--- module/mr/control.php
+++ module/mr/control.php
@@ -825,9 +825,9 @@ class mr extends control
             if(strtolower($repo->SCM) == 'gitfox')
             {
                 $commitLog->id = $commitLog->sha;
-                $commitLog->committed_date  = $commitLog->committer->when;
-                $commitLog->committer_name  = $commitLog->committer->identity->name;
-                $commitLog->committer_email = $commitLog->committer->identity->email;
+                $commitLog->committed_date  = $commitLog->author->when;
+                $commitLog->committer_name  = $commitLog->author->identity->name;
+                $commitLog->committer_email = $commitLog->author->identity->email;
             }
             elseif(in_array(strtolower($repo->SCM), array('gitea', 'gogs')))
             {
