8d77b127df52bf20d69a789b8217362e95ac4003
liumengyi <liumengyi@easycorp.ltd>
2024-05-06 16:55:25 +0800

    * Modify unit test results.

--- module/product/test/tao/createline.php
+++ module/product/test/tao/createline.php
@@ -9,7 +9,7 @@ cid=0
 - 执行product模块的createLineTest方法，参数是'1', 'test line1'
  - 属性root @1
  - 属性name @test line1
-- 执行product模块的createLineTest方法，参数是'0', 'test line1'  @0
+- 执行product模块的createLineTest方法，参数是'-1', 'test line1'  @0
 - 执行product模块的createLineTest方法，参数是'1', ''  @0
 - 执行product模块的createLineTest方法，参数是'1', "<script>alert
  - 属性root @1
@@ -23,7 +23,7 @@ zenData('module')->gen(1);
 
 $product = new productTest('admin');
 
-r($product->createLineTest('1', 'test line1')) && p('root,name') && e('1,test line1');
-r($product->createLineTest('0', 'test line1')) && p() && e('0');
+r($product->createLineTest('1',  'test line1')) && p('root,name') && e('1,test line1');
+r($product->createLineTest('-1', 'test line1')) && p() && e('0');
 r($product->createLineTest('1', '')) && p() && e('0');
 r($product->createLineTest('1', "<script>alert('test')</script>")) && p('root,name') && e('1,&lt;script&gt;alert(&#039;test&#039;)&lt;/script&gt;');
