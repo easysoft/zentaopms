fa0ec207a260ea715582d86d7abfeed311802956
wangyuting <wangyuting@easycorp.ltd>
2024-05-08 15:04:26 +0800

    * Fix doc edit bug for not text type.

--- module/doc/ui/edit.html.php
+++ module/doc/ui/edit.html.php
@@ -18,7 +18,7 @@ jsVar('libNotEmpty', sprintf($lang->error->notempty, $lang->doc->lib));
 jsVar('keywordsNotEmpty', sprintf($lang->error->notempty, $lang->doc->keywords));
 jsVar('contentNotEmpty', sprintf($lang->error->notempty, $lang->doc->content));
 
-form
+$doc->type == 'text' ? form
 (
     set::actions(''),
     set::ajax(array('beforeSubmit' => jsRaw("clickSubmit"))),
@@ -196,4 +196,104 @@ form
             )
         )
     )
+) : form
+(
+    modalHeader(),
+    on::change('#product',   "loadObjectModules"),
+    on::change('#project',   "loadObjectModules"),
+    on::change('#execution', "loadObjectModules"),
+    (strpos('product|project|execution', $type) !== false) ? formGroup
+    (
+        set::width('1/2'),
+        set::required(true),
+        set::label($lang->doc->{$type}),
+        picker
+        (
+            set::name($type),
+            set::id($type),
+            set::items($objects),
+            set::value($objectID),
+            set::required(true)
+        )
+    ) : null,
+    formGroup
+    (
+        set::width('1/2'),
+        set::required(true),
+        set::label($lang->doc->libAndModule),
+        picker
+        (
+            set::name('module'),
+            set::items($moduleOptionMenu),
+            set::value($doc->lib . '_' . $doc->module),
+            set::required(true)
+        )
+    ),
+    formGroup
+    (
+        set::label($lang->doc->title),
+        set::name('title'),
+        set::value($doc->title),
+        set::required(true)
+    ),
+    formGroup
+    (
+        strpos($config->doc->officeTypes, $doc->type) === false ? setClass('hidden') : null,
+        set::label($lang->doc->keywords),
+        set::control('input'),
+        set::name('keywords'),
+        set::value($doc->keywords)
+    ),
+    formGroup
+    (
+        set::label($lang->doc->files),
+        fileSelector()
+    ),
+    formGroup
+    (
+        set::label($lang->doc->mailto),
+        mailto(set::items($users), set::value($doc->mailto))
+    ),
+    formGroup
+    (
+        set::label($lang->doclib->control),
+        radioList
+        (
+            set::name('acl'),
+            set::items($lang->doc->aclList),
+            set::value($doc->acl),
+            on::change('toggleWhiteList')
+        )
+    ),
+    formGroup
+    (
+        $doc->acl == 'open' ? setClass('hidden') : null,
+        set::label($lang->doc->whiteList),
+        set::id('whitelistBox'),
+        div
+        (
+            setClass('w-full check-list'),
+            inputGroup
+            (
+                setClass('w-full'),
+                $lang->doc->groups,
+                picker
+                (
+                    set::name('groups[]'),
+                    set::items($groups),
+                    set::multiple(true),
+                    set::value($doc->groups)
+                )
+            ),
+            div
+            (
+                setClass('w-full'),
+                userPicker(set::label($lang->doc->users), set::items($users), set::value($doc->users))
+            )
+        )
+    ),
+    formHidden('contentType', $doc->contentType),
+    formHidden('type', $doc->type),
+    formHidden('status', $doc->status),
+    formHidden('parent', $doc->parent)
 );
