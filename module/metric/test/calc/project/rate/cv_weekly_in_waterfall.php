cef93860706c248743cfc03c363526d44a48fe05
caoyanyi <caoyanyi@easycorp.ltd>
2024-05-08 08:49:33 +0800

    * Replace zdTable to zendata.

--- module/metric/test/calc/project/rate/cv_weekly_in_waterfall.php
+++ module/metric/test/calc/project/rate/cv_weekly_in_waterfall.php
@@ -17,7 +17,7 @@ include dirname(__FILE__, 4) . '/lib/calc.unittest.class.php';
 zendata('project')->loadYaml('waterfall', true, 4)->gen(10);
 zendata('project')->loadYaml('stage', true, 4)->gen(40, false);
 zendata('task')->loadYaml('task_waterfall', true, 4)->gen(1000);
-zdTable('effort')->config('effort_ac', true, 4)->gen(1000);
+zendata('effort')->loadYaml('effort_ac', true, 4)->gen(1000);
 
 $metric = new metricTest();
 $calc   = $metric->calcMetric(__FILE__);
