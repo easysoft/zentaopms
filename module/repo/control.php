f4e2947a6c93f57bd32fb6929856680e9ba21b05
caoyanyi <caoyanyi@easycorp.ltd>
2024-05-13 15:51:16 +0800

    * Adjust repo list check for gitfox.

--- module/repo/control.php
+++ module/repo/control.php
@@ -1416,6 +1416,7 @@ class repo extends control
         $getProjectFunc = 'ajaxGet' . $server->type . 'Projects';
 
         $repos = $this->$getProjectFunc($serverID);
+        if(empty($repos)) return print(json_encode(array()));
 
         return print(json_encode($this->repoZen->buildRepoPaths(array_column($repos, 'text', 'value'))));
     }
