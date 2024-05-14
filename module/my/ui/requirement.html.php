48bd212a070d64fd922a8ad662fe52a2ad9b82af
sunguangming <sunguangming@easycorp.ltd>
2024-05-14 09:58:51 +0800

    * Modify method for requirement.

--- module/my/ui/requirement.html.php
+++ module/my/ui/requirement.html.php
@@ -19,10 +19,10 @@ featureBar
     li(searchToggle(set::module($this->app->rawMethod . 'Requirement'), set::open($type == 'bysearch')))
 );
 
-$canBatchEdit     = common::hasPriv('story', 'batchEdit');
-$canBatchReview   = common::hasPriv('story', 'batchReview');
-$canBatchAssignTo = common::hasPriv('story', 'batchAssignTo');
-$canBatchClose    = common::hasPriv('story', 'batchClose');
+$canBatchEdit     = common::hasPriv('requirement', 'batchEdit');
+$canBatchReview   = common::hasPriv('requirement', 'batchReview');
+$canBatchAssignTo = common::hasPriv('requirement', 'batchAssignTo');
+$canBatchClose    = common::hasPriv('requirement', 'batchClose');
 $canBatchAction   = $canBatchEdit || $canBatchReview || $canBatchAssignTo || $canBatchClose;
 
 $reviewItems = array();
@@ -32,7 +32,7 @@ if($canBatchReview)
     foreach($lang->story->reasonList as $key => $reason)
     {
         if(!$key || $key == 'subdivided' || $key == 'duplicate') continue;
-        $rejectItems[] = array('text' => $reason, 'innerClass' => 'batch-btn ajax-btn not-open-url', 'data-url' => helper::createLink('story', 'batchReview', "result=reject&reason={$key}&storyType=requirement"));
+        $rejectItems[] = array('text' => $reason, 'innerClass' => 'batch-btn ajax-btn not-open-url', 'data-url' => helper::createLink('requirement', 'batchReview', "result=reject&reason={$key}&storyType=requirement"));
     }
 
     foreach($lang->story->reviewResultList as $key => $result)
@@ -44,7 +44,7 @@ if($canBatchReview)
         }
         else
         {
-            $reviewItems[] = array('text' => $result, 'innerClass' => 'batch-btn ajax-btn not-open-url', 'data-url' => helper::createLink('story', 'batchReview', "result={$key}&reason=&storyType=requirement"));
+            $reviewItems[] = array('text' => $result, 'innerClass' => 'batch-btn ajax-btn not-open-url', 'data-url' => helper::createLink('requirement', 'batchReview', "result={$key}&reason=&storyType=requirement"));
         }
     }
 }
@@ -55,16 +55,16 @@ if($canBatchAssignTo)
     foreach($users as $key => $value)
     {
         if(empty($key) || $key == 'closed') continue;
-        $assignedToItems[] = array('text' => $value, 'innerClass' => 'batch-btn ajax-btn not-open-url', 'data-url' => helper::createLink('story', 'batchAssignTo', "storyType=requirement&assignedTo={$key}"));
+        $assignedToItems[] = array('text' => $value, 'innerClass' => 'batch-btn ajax-btn not-open-url', 'data-url' => helper::createLink('requirement', 'batchAssignTo', "storyType=requirement&assignedTo={$key}"));
     }
 }
 
 $footToolbar = array('items' => array
 (
-    $canBatchEdit ?     array('text' => $lang->edit, 'className' => 'batch-btn', 'data-url' => helper::createLink('story', 'batchEdit', "productID=0&executionID=0&branch=0&storyType=requirement&from={$app->rawMethod}")) : null,
+    $canBatchEdit ?     array('text' => $lang->edit, 'className' => 'batch-btn', 'data-url' => helper::createLink('requirement', 'batchEdit', "productID=0&executionID=0&branch=0&storyType=requirement&from={$app->rawMethod}")) : null,
     $canBatchReview ?   array('caret' => 'up', 'text' => $lang->story->review, 'type' => 'dropdown', 'items' => $reviewItems, 'data-placement' => 'top-start') : null,
     $canBatchAssignTo ? array('caret' => 'up', 'text' => $lang->story->assignedTo, 'type' => 'dropdown', 'items' => $assignedToItems, 'data-placement' => 'top-start') : null,
-    $canBatchClose ?    array('text' => $lang->story->close, 'className' => 'batch-btn', 'data-url' => helper::createLink('story', 'batchClose', "productID=0&executionID=0&storyType=requirement&from={$app->rawMethod}")) : null
+    $canBatchClose ?    array('text' => $lang->story->close, 'className' => 'batch-btn', 'data-url' => helper::createLink('requirement', 'batchClose', "productID=0&executionID=0&storyType=requirement&from={$app->rawMethod}")) : null
 ), 'btnProps' => array('size' => 'sm', 'btnType' => 'secondary'));
 
 if($canBatchAction) $config->my->requirement->dtable->fieldList['id']['type'] = 'checkID';
