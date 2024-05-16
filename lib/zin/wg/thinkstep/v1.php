610352ac61ca2c1daf1d2c6c44f9dc071fc36ffe
wangzemei <wangzemei@easycorp.ltd>
2024-05-16 14:38:24 +0800

    * zin: modify control of thinkstep wg.

--- lib/zin/wg/thinkstep/v1.php
+++ lib/zin/wg/thinkstep/v1.php
@@ -10,103 +10,49 @@ class thinkStep  extends wg
         'addType?: string',
     );
 
-    protected function buildQuestionBody(): array|wg
-    {
-        $item = $this->prop('item');
-        if(property_exists($item, 'questionType') && $item->questionType === 'tableInput') return thinkTableInputDetail
-        (
-            set::item($item),
-            set::required($item->required),
-            set::rowsTitle(explode(', ', $item->fields)),
-        );
-        return array();
-
-    }
-
     protected function buildBody(): wg|array
     {
         list($item, $action, $addType) = $this->prop(array('item', 'action', 'addType'));
 
-        if($addType === 'input' || !$addType && $item->questionType === 'input') return thinkInput
-        (
-            set::step($addType ? null : $item),
-            set::mode($action),
-        );
+        $step         = $addType ? null : $item;
+        $questionType = $addType ? $addType : $item->options->questionType;
+        if($item->type === 'node') return thinkNodeBase(set::step($step), set::mode($action));
+        if($addType === 'transition' || !$addType && $item->type === 'transition') return thinkTransition(set::step($step), set::mode($action));
+        if($questionType === 'input')    return thinkInput(set::step($step), set::question('input'), set::mode($action));
+        if($questionType === 'radio')    return thinkRadio(set::step($step), set::question('radio'), set::mode($action));
+        if($questionType === 'checkbox') return thinkCheckbox(set::step($step), set::question('checkbox'), set::mode($action));
 
-        if($addType === 'radio' || !$addType && $item->questionType === 'radio') return thinkRadio
-        (
-            set::step($addType ? null : $item),
-            set::mode($action),
-        );
-
-        if($addType === 'checkbox' || !$addType && $item->questionType === 'checkbox') return thinkCheckbox
-        (
-            set::step($addType ? null : $item),
-            set::mode($action),
-        );
-
-        if($action == 'detail')
+        $fields = !empty($item->options->fields) ? explode(', ', $itemi->options->fields) : null;
+        if($action == 'detail' && (property_exists($item->options, 'questionType') && $item->options->questionType === 'tableInput'))
         {
             return thinkStepDetail
             (
                 set::item($item),
-                $this->buildQuestionBody()
+                thinkTableInputDetail
+                (
+                    set::item($item),
+                    set::required($item->required),
+                    set::rowsTitle($fields)
+                )
             );
         }
         else
         {
-            $item->options = null;
             $isEdit = $action === 'edit' ? true : false;
 
-
-            if($addType === 'transition' || $isEdit && $item->type === 'transition') return thinkTransition
-            (
-                set::title($isEdit ? $item->title : ''),
-                set::desc($isEdit ? $item->desc: ''),
-            );
-            if($isEdit && $item->type === 'question' || $addType)
+            if($isEdit && $item->type === 'question' && $item->options->questionType === 'tableInput' || $addType == 'tableInput')
             {
-                if($addType === 'radio' || ($isEdit && $item->questionType === 'radio')) return thinkRadio
-                (
-                    set::data(!$isEdit ? null : explode(', ', $item->fields)),
+                return thinkTableInput(
                     set::title($isEdit ? $item->title : ''),
                     set::desc($isEdit ? $item->desc : ''),
-                    set::required($isEdit ? $item->required : 0),
-                    set::enableOther($isEdit ? $item->enableOther : false),
-                );
-                if($addType === 'checkbox' || ($isEdit && $item->questionType === 'checkbox')) return thinkCheckbox
-                (
-                    set::data(!$isEdit ? null : explode(', ', $item->fields)),
-                    set::title($isEdit ? $item->title : ''),
-                    set::desc($isEdit ? $item->desc : ''),
-                    set::required($isEdit ? $item->required : 0),
-                    set::enableOther($isEdit ? $item->enableOther : false),
-                    set::minCount($isEdit && isset($item->minCount) ? $item->minCount : ''),
-                    set::maxCount($isEdit && isset($item->maxCount) ? $item->maxCount : ''),
-                );
-                if($addType === 'input' || ($isEdit && $item->questionType === 'input')) return thinkInput(
-                    set::title($isEdit ? $item->title : ''),
-                    set::desc($isEdit ? $item->desc : ''),
-                    set::required($isEdit ? $item->required : false),
-                    set::type('question')
-                );
-                if($addType === 'tableInput' || ($isEdit && $item->questionType === 'tableInput'))  return thinkTableInput(
-                    set::title($isEdit ? $item->title : ''),
-                    set::desc($isEdit ? $item->desc : ''),
-                    set::required($isEdit ? $item->required : false),
+                    set::required($isEdit ? $item->options->required : false),
                     set::type('question'),
-                    set::requiredRows($isEdit ? $item->requiredRows : 1),
-                    set::isSupportAdd($isEdit ? $item->isSupportAdd : false),
-                    set::canAddRows($isEdit ? $item->canAddRows : 1),
-                    set::rowsTitle(!$isEdit ? null : explode(', ', $item->fields))
+                    set::requiredRows($isEdit ? $item->options->requiredRows : 1),
+                    set::isSupportAdd($isEdit ? $item->options->isSupportAdd : false),
+                    set::canAddRows($isEdit ? $item->options->canAddRows : 1),
+                    set::rowsTitle(!$isEdit ? null : $fields)
                 );
             }
-            if($isEdit) return thinkNode
-            (
-                set::type($item->type),
-                set::title($item->title),
-                set::desc($item->desc),
-            );
         }
     }
 
@@ -126,11 +72,7 @@ class thinkStep  extends wg
                     div
                     (
                         setClass('flex items-center text-gray-950 h-12 py-0 px-8 mx-4'),
-                        div
-                        (
-                            setClass('font-medium'),
-                            $title
-                        )
+                        div(setClass('font-medium'), $title)
                     ),
                     h::hr()
                 ) : null,
