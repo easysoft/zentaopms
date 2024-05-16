1ac040a9cc10fe3a0995e09b9d1b46325ead7026
sunguangming <sunguangming@easycorp.ltd>
2024-05-16 09:23:49 +0800

    * Adjust code.

--- module/epic/lang/zh-cn.php
+++ module/epic/lang/zh-cn.php
@@ -2,13 +2,14 @@
 global $app;
 $app->loadLang('story');
 $lang->epic = clone $lang->story;
-$lang->epic->common = $lang->ERCommon;
 
 foreach($lang->epic as $key => $value)
 {
     if(!is_string($value)) continue;
     if(strpos($value, $lang->SRCommon) !== false) $lang->epic->$key = str_replace($lang->SRCommon, $lang->ERCommon, $value);
 }
+
+$lang->epic->common = $lang->ERCommon;
 
 $lang->epic->stageList = array();
 $lang->epic->stageList[''] = '';
--- module/requirement/lang/zh-cn.php
+++ module/requirement/lang/zh-cn.php
@@ -2,13 +2,14 @@
 global $app;
 $app->loadLang('story');
 $lang->requirement = clone $lang->story;
-$lang->requirement->common = $lang->URCommon;
 
 foreach($lang->requirement as $key => $value)
 {
     if(!is_string($value)) continue;
     if(strpos($value, $lang->SRCommon) !== false) $lang->requirement->$key = str_replace($lang->SRCommon, $lang->URCommon, $value);
 }
+
+$lang->requirement->common = $lang->URCommon;
 
 $lang->requirement->stageList = array();
 $lang->requirement->stageList[''] = '';
