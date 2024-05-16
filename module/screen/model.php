a77ebc5263e2cfbd9935b48301e18fd39cad984c
宋辰轩 <songchenxuan@easycorp.ltd>
2024-05-16 14:41:34 +0800

    * Fix bug for screen view height bug.

--- module/screen/model.php
+++ module/screen/model.php
@@ -104,10 +104,9 @@ class screenModel extends model
         $this->filter->account = $account;
         $this->filter->charts  = array();
 
-        $scheme = json_decode($screen->scheme);
-
         if(!$screen->builtin or in_array($screen->id, $this->config->screen->builtinScreen))
         {
+            $scheme = json_decode($screen->scheme);
             foreach($scheme->componentList as $component)
             {
                 if(!empty($component->isGroup))
@@ -128,9 +127,24 @@ class screenModel extends model
             return $scheme;
         }
 
+        $editCanvasConfig = $this->config->screen->editCanvasConfig;
+
+        $componentList = json_decode($screen->scheme);
+        if(empty($componentList)) $componentList = array();
+
+        /* Reset height of canvas. */
+        foreach($componentList as $component)
+        {
+            if(!isset($component->attr)) continue;
+
+            $height = $component->attr->y + $component->attr->h;
+            if($height > $editCanvasConfig->height) $editCanvasConfig->height = $height;
+        }
+        $editCanvasConfig->height += 50;
+
         $chartData = new stdclass();
-        $chartData->editCanvasConfig = $this->config->screen->editCanvasConfig;
-        $chartData->componentList    = $this->buildComponentList($scheme);
+        $chartData->editCanvasConfig = $editCanvasConfig;
+        $chartData->componentList    = $this->buildComponentList($componentList);
 
         return $chartData;
     }
