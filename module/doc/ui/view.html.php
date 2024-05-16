6b000b74adf8dd8412e64b5554390289024117f3
wangyidong <wangyidong@cnezsoft.com>
2024-05-16 08:37:58 +0800

    * Adjust export single doc.

--- module/doc/ui/view.html.php
+++ module/doc/ui/view.html.php
@@ -26,11 +26,10 @@ toolbar
     (
         'id'          => $exportMethod,
         'icon'        => 'export',
+        'target'      => '_blank',
         'class'       => 'ghost export',
         'text'        => $lang->export,
-        'url'         => createLink('doc', $exportMethod, "libID={$libID}&moduleID={$moduleID}"),
-        'data-size'   => 'sm',
-        'data-toggle' => 'modal'
+        'url'         => createLink('doc', $exportMethod, "libID={$libID}&moduleID={$moduleID}&docID={$doc->id}")
     ))) : null,
     common::hasPriv('doc', 'createLib') ? item(set(array
     (
