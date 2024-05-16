95f00b45d21b5ef99eef8e54135a1e8f6f944f9b
liumengyi <liumengyi@easycorp.ltd>
2024-05-16 11:19:00 +0800

    * Modify unit test of todo.

--- module/todo/test/tao/buildcycletodo.php
+++ module/todo/test/tao/buildcycletodo.php
@@ -22,4 +22,4 @@ global $tester;
 $tester->loadModel('todo');
 $todo = $tester->todo->getByID(1);
 
-r($tester->todo->buildCycleTodo($todo)) && p('name,type,status') && e('我的待办,cycle,wait'); // 根据待办数据构建一个周期待办数据，验证name,type,status字段的结果为：我的待办,cycle,wait
+r($tester->todo->buildCycleTodo($todo)) && p('name,type,status') && e('我的待办,custom,wait'); // 根据待办数据构建一个周期待办数据，验证name,type,status字段的结果为：我的待办,cycle,wait
