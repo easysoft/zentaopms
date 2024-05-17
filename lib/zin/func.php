5e0f6c2e405e1e34d648fc459d5ea669d19e5f73
xieqiyu <xieqiyu@easycorp.ltd>
2024-05-17 14:33:42 +0800

    * Add affected component.

--- lib/zin/func.php
+++ lib/zin/func.php
@@ -210,6 +210,7 @@ function thinkStepDetail(): thinkStepDetail {return createWg('thinkStepDetail', 
 function thinkTableInputDetail(): thinkTableInputDetail {return createWg('thinkTableInputDetail', func_get_args());}
 function thinkNodeMenu(): thinkNodeMenu {return createWg('thinkNodeMenu', func_get_args());}
 function thinkCover(): thinkCover {return createWg('thinkCover', func_get_args());}
+function affected(): affected {return createWg('affected', func_get_args());}
 
 if(is_dir(__DIR__ . DS . 'wg' . DS . 'boardeditor'))
 {
