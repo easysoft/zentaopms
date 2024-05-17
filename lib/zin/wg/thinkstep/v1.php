0fef936d783e7f7285322a9e77cffc5c78076288
wangzemei <wangzemei@easycorp.ltd>
2024-05-17 09:18:53 +0800

    * zin: add the isRun prop of thinkstep wg.

--- lib/zin/wg/thinkstep/v1.php
+++ lib/zin/wg/thinkstep/v1.php
@@ -8,19 +8,20 @@ class thinkStep  extends wg
         'item: object',
         'action?: string="detail"',
         'addType?: string',
+        'isRun?: bool=false', // 是否是分析活动
     );
 
     protected function buildBody(): wg|array
     {
-        list($item, $action, $addType) = $this->prop(array('item', 'action', 'addType'));
+        list($item, $action, $addType, $isRun) = $this->prop(array('item', 'action', 'addType', 'isRun'));
 
         $step         = $addType ? null : $item;
         $questionType = $addType ? $addType : ($item->options->questionType ?? '');
-        if($addType === 'node' || !$addType && $item->type === 'node') return thinkNodeBase(set::step($step), set::mode($action));
-        if($addType === 'transition' || !$addType && $item->type === 'transition') return thinkTransition(set::step($step), set::mode($action));
-        if($questionType === 'input')    return thinkInput(set::step($step), set::question('input'), set::mode($action));
-        if($questionType === 'radio')    return thinkRadio(set::step($step), set::question('radio'), set::mode($action));
-        if($questionType === 'checkbox') return thinkCheckbox(set::step($step), set::question('checkbox'), set::mode($action));
+        if($addType === 'node' || !$addType && $item->type === 'node') return thinkNodeBase(set::step($step), set::mode($action), set::isRun($isRun));
+        if($addType === 'transition' || !$addType && $item->type === 'transition') return thinkTransition(set::step($step), set::mode($action), set::isRun($isRun));
+        if($questionType === 'input')    return thinkInput(set::step($step), set::question('input'), set::mode($action), set::isRun($isRun));
+        if($questionType === 'radio')    return thinkRadio(set::step($step), set::question('radio'), set::mode($action), set::isRun($isRun));
+        if($questionType === 'checkbox') return thinkCheckbox(set::step($step), set::question('checkbox'), set::mode($action), set::isRun($isRun));
 
         $isEdit = $action === 'edit' ? true : false;
         $fields = !empty($item->options->fields) ? explode(', ', $itemi->options->fields) : array();
