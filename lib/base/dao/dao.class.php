c3f7b9646a5807f2c5e0bc06117db902101ecd2e
liugang <liugang@easycorp.ltd>
2024-05-17 10:26:08 +0800

    * baseDAO: append - in cache key.

--- lib/base/dao/dao.class.php
+++ lib/base/dao/dao.class.php
@@ -194,7 +194,7 @@ class baseDAO
             $app->loadClass('cache', true);
             try
             {
-                $this->cache = new cache($config->cache->dao->driver, 'dao', $config->cache->dao->lifetime);
+                $this->cache = new cache($config->cache->dao->driver, 'dao-', $config->cache->dao->lifetime);
             }
             catch (Exception $e)
             {
