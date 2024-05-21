e66305831d447f0d23fb425a646ee9245aa87b29
sunhao <sunhao@easycorp.ltd>
2024-05-21 13:02:03 +0800

    * zin: change padding of tree in module menu.

--- lib/zin/wg/modulemenu/v1.php
+++ lib/zin/wg/modulemenu/v1.php
@@ -299,7 +299,7 @@ class moduleMenu extends wg
                 zui::tree
                 (
                     set::_tag('menu'),
-                    set::_class('tree tree-lines col flex-auto scrollbar-hover scrollbar-thin overflow-y-auto overflow-x-hidden px-4'),
+                    set::_class('tree tree-lines col flex-auto scrollbar-hover scrollbar-thin overflow-y-auto overflow-x-hidden px-2'),
                     set::defaultNestedShow(true),
                     set::hover(true),
                     set::lines(true),
