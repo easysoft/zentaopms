7335568db1fd98ed31d55e2f77d6c9e4b5a30276
liyang <liyang@easycorp.ltd>
2024-05-22 10:14:39 +0800

    * Modify method name format.

--- module/mr/test/model/apiacceptmr.php
+++ module/mr/test/model/apiacceptmr.php
@@ -26,13 +26,14 @@ $mr->gen(10);
 $mrModel = new mrTest();
 
 $noHost = 6;
-r($mrModel->apiAcceptMRTester($noHost)) && p() && e('0'); // 不存在的主机
+r($mrModel->apiAcceptMrTester($noHost)) && p() && e('0'); // 不存在的主机
 
 $gitlab = 1;
-r($mrModel->apiAcceptMRTester($gitlab)) && p('message') && e('404 Not found'); // 已关闭的gitlab合并请求
+r($mrModel->apiAcceptMrTester($gitlab)) && p('message') && e('404 Not found'); // 已关闭的gitlab合并请求
 
 $gitea = 4;
-r($mrModel->apiAcceptMRTester($gitea)) && p() && e('0'); // 已关闭的gitea合并请求
+sleep(1);
+r($mrModel->apiAcceptMrTester($gitea)) && p() && e('0'); // 已关闭的gitea合并请求
 
 $gogs = 5;
-r($mrModel->apiAcceptMRTester($gogs)) && p() && e('0'); // 已关闭的gogs合并请求
+r($mrModel->apiAcceptMrTester($gogs)) && p() && e('0'); // 已关闭的gogs合并请求
