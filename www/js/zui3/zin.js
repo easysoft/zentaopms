1f5a7ffd3ec1cff6d8bc19458203fc063a4bc69a
sunhao <sunhao@easycorp.ltd>
2024-05-23 11:03:32 +0800

    * zin: only show zinbar on turn on $config->zinTool config.

--- www/js/zui3/zin.js
+++ www/js/zui3/zin.js
@@ -99,7 +99,7 @@
 
     function initZinbar()
     {
-        if(!DEBUG || DEBUG < 3 || isIndexPage) return;
+        if(!DEBUG || !window.zin || !window.zin.zinTool || isIndexPage) return;
         let $bar = $('#zinbar');
         if($bar.length) return;
 
