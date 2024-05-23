409e6406084066ae16c4ec6c7018bc3386394bec
liugang <liugang@easycorp.ltd>
2024-05-23 10:53:41 +0800

    * baseDAO: disable cache when installing or upgrading.

--- lib/base/dao/dao.class.php
+++ lib/base/dao/dao.class.php
@@ -198,7 +198,7 @@ class baseDAO
         $this->dbh       = $dbh;
         $this->slaveDBH  = $slaveDBH ? $slaveDBH : false;
 
-        if($config->cache->dao->enable)
+        if($app->isServing() && $config->cache->dao->enable)
         {
             $app->loadClass('cache', true);
             try
