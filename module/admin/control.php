fb9533782141fd753ffbf14adb67bc4054f30cc1
liugang <liugang@easycorp.ltd>
2024-05-24 15:50:31 +0800

    * admin: call dao method to clear cache.

--- module/admin/control.php
+++ module/admin/control.php
@@ -593,7 +593,7 @@ class admin extends control
             $cache = form::data()->get();
             $this->loadModel('setting')->setItem('system.common.global.cache', json_encode($cache));
 
-            if(empty($cache->dao['enable'])) $this->dao->cache->clear();
+            if(empty($cache->dao['enable'])) $this->dao->clearCache();
 
             return $this->send(array('result' => 'success', 'message' => $this->lang->saveSuccess, 'load' => true));
         }
