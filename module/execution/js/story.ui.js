9823b232ba7ea8a4d80b549059617aa76c9bc52c
daitingting <daitingting@xirangit.com>
2024-05-24 01:13:02 +0000

    * Remove debug codes.

--- module/execution/js/story.ui.js
+++ module/execution/js/story.ui.js
@@ -64,7 +64,6 @@ $(document).off('click','.batch-btn').on('click', '.batch-btn', function()
 
         if(confirm(confirmStoryToTaskTip))
         {
-            console.log('ddd');
             $('#storyIdList').val(checkedIdList);
         }
         else
@@ -78,13 +77,10 @@ $(document).off('click','.batch-btn').on('click', '.batch-btn', function()
             $('#storyIdList').val(unlinkTaskIdList);
         }
     }
-    console.log('ccc');
 
     zui.Modal.hide('#taskModal');
-    console.log('eee');
 
     const formData = new FormData($("#toTaskForm")[0]);
-    console.log(formData);
     postAndLoadPage($('#toTaskForm').attr('action'), formData);
 
     return false;
