f234109bd6ce90681424b2530397eb5aef9861c7
liumengyi <liumengyi@easycorp.ltd>
2024-05-27 16:30:21 +0800

    * Fix bug #50164.

--- module/testtask/js/browse.ui.js
+++ module/testtask/js/browse.ui.js
@@ -5,3 +5,12 @@ function changeBrowseDate()
     const params = condition + '&beginTime=' + begin + '&endTime=' + end;
     loadPage($.createLink('testtask', 'browse', params), '#mainContent');
 };
+
+window.onRenderCell = function(result, {row, col})
+{
+    if(result && col.name == 'buildName' && row.data.execution != 0 && multipleSprints[row.data.execution] == undefined)
+    {
+        if(result[0].props) result[0].props['data-app'] = 'project';
+    }
+    return result;
+}
