30bf5fe013bfcd9bd4496b1bd8f46d6ab837e592
Lufei <lufei@easycorp.ltd>
2024-05-27 17:18:38 +0800

    - remove board createIcons.

--- module/common/lang/common.php
+++ module/common/lang/common.php
@@ -238,7 +238,6 @@ $lang->createIcons['story']       = 'lightbulb';
 $lang->createIcons['task']        = 'check-sign';
 $lang->createIcons['testcase']    = 'sitemap';
 $lang->createIcons['doc']         = 'doc';
-$lang->createIcons['board']       = 'board';
 $lang->createIcons['execution']   = 'run';
 $lang->createIcons['project']     = 'project';
 $lang->createIcons['product']     = 'product';
