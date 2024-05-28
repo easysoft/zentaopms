adc4d45f0e50661901bbfd314e3f1d80341d83fd
zhouxin <zhouxin@easycorp.ltd>
2024-05-28 16:39:06 +0800

    * Fix unit test of metric.

--- module/metric/test/tao/fetchmodules.php
+++ module/metric/test/tao/fetchmodules.php
@@ -7,9 +7,9 @@ title=fetchModules
 timeout=0
 cid=1
 
-- 获取范围为系统的分组个数 @26
-- 获取范围为产品的分组个数 @9
-- 获取范围为项目的分组个数 @10
+- 获取范围为系统的分组个数 @27
+- 获取范围为产品的分组个数 @10
+- 获取范围为项目的分组个数 @12
 - 获取范围为执行的分组个数 @5
 - 获取范围为用户的分组个数 @4
 
@@ -19,8 +19,8 @@ include dirname(__FILE__, 2) . '/lib/calc.unittest.class.php';
 
 $metric = new metricTest();
 
-r(count($metric->fetchModules('system'))) && p() && e(26);   // 获取范围为系统的分组个数
-r(count($metric->fetchModules('product'))) && p() && e(9);   // 获取范围为产品的分组个数
-r(count($metric->fetchModules('project'))) && p() && e(10);  // 获取范围为项目的分组个数
+r(count($metric->fetchModules('system'))) && p() && e(27);   // 获取范围为系统的分组个数
+r(count($metric->fetchModules('product'))) && p() && e(10);  // 获取范围为产品的分组个数
+r(count($metric->fetchModules('project'))) && p() && e(12);  // 获取范围为项目的分组个数
 r(count($metric->fetchModules('execution'))) && p() && e(5); // 获取范围为执行的分组个数
-r(count($metric->fetchModules('user'))) && p() && e(4);      // 获取范围为用户的分组个数
+r(count($metric->fetchModules('user'))) && p() && e(5);      // 获取范围为用户的分组个数
