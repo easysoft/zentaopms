6654fc8f586d374c0c322c305bca19326dbae9cb
caoyanyi <caoyanyi@easycorp.ltd>
2024-05-29 09:45:03 +0800

    * [misc,0.3h] adjust mr linked object open use modal.

--- module/mr/ui/linkbug.html.php
+++ module/mr/ui/linkbug.html.php
@@ -50,8 +50,9 @@ div
 $cols = array();
 foreach($config->release->dtable->defaultFields['linkBug']['bug'] as $field) $cols[$field] = zget($config->bug->dtable->fieldList, $field, array());
 $data = array_values($allBugs);
-$cols['title']['data-toggle'] = '';
-$cols['title']['link']        = array('module' => 'bug', 'method' => 'view', 'params' => 'bugID={id}', 'target' => '_blank');
+$cols['title']['data-toggle'] = 'modal';
+$cols['title']['data-size']   = 'lg';
+$cols['title']['link']        = array('module' => 'bug', 'method' => 'view', 'params' => 'bugID={id}');
 dtable
 (
     set::userMap($users),
