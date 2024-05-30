b426a620b05d3d7c9334e7fea4662291c765b638
liugang <liugang@easycorp.ltd>
2024-05-30 17:16:46 +0800

    * admin: clear dao cache after toggling cache switch.

--- module/admin/control.php
+++ module/admin/control.php
@@ -593,7 +593,7 @@ class admin extends control
             $cache = form::data()->get();
             $this->loadModel('setting')->setItem('system.common.global.cache', json_encode($cache));
 
-            $this->dao->clearCache();
+            if($cache->dao['enable'] != $this->config->cache->dao->enable) $this->dao->clearCache();
 
             return $this->send(array('result' => 'success', 'message' => $this->lang->saveSuccess, 'load' => true));
         }
