6435e57fe1a4af2f6fcbe0cdda1fff8ab1ec0341
caoyanyi <caoyanyi@easycorp.ltd>
2024-06-03 14:41:38 +0800

    * [misc] adjust release info.

--- module/misc/lang/zh-cn.php
+++ module/misc/lang/zh-cn.php
@@ -114,7 +114,7 @@ $lang->misc->feature->promptExecImage   = 'theme/default/images/main/prompt_exec
 $lang->misc->feature->promptLearnMore   = 'https://www.zentao.net/book/zentaopms/1097.html';
 
 /* Release Date. */
-$lang->misc->releaseDate['20.1.0']      = '2024-05-31';
+$lang->misc->releaseDate['20.1.0']      = '2024-06-03';
 $lang->misc->releaseDate['20.0']        = '2024-04-30';
 $lang->misc->releaseDate['18.12']       = '2024-04-12';
 $lang->misc->releaseDate['20.0.beta2']  = '2024-03-15';
