8801cd2e90585f480d499445cb30c07df2d6e982
sunguangming <sunguangming@easycorp.ltd>
2024-06-03 17:24:49 +0800

    * Compatible unit test.

--- module/story/test/model/createtwins.php
+++ module/story/test/model/createtwins.php
@@ -4,6 +4,7 @@
 /**
 
 title=测试 storyModel->createTwins();
+timeout=0
 cid=0
 
 - 检查创建后的数据。
@@ -39,7 +40,7 @@ $data->module      = 0;
 $data->branches    = array(0, 1);
 $data->modules     = array(1, 2);
 $data->plans       = array(0, 0);
-$data->plan        = 1;
+$data->plan        = '1';
 $data->assignedTo  = '';
 $data->source      = '';
 $data->sourceNote  = '';
@@ -61,7 +62,6 @@ $data->openedBy    = 'admin';
 $data->openedDate  = date('Y-m-d H:i:s');
 $data->mailto      = '';
 $data->parent      = 1;
-$data->URS[]       = 1;
 $data->reviewer[]  = 'admin';
 
 $story = new storyTest();
