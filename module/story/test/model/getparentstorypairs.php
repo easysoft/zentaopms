c46a6b3448b704ed62699300a3d24c2fef4f7711
sunguangming <sunguangming@easycorp.ltd>
2024-06-04 09:11:33 +0800

    * Compatible unit test.

--- module/story/test/model/getparentstorypairs.php
+++ module/story/test/model/getparentstorypairs.php
@@ -4,13 +4,12 @@
 /**
 
 title=测试 storyModel->getParentStoryPairs();
+timeout=0
 cid=0
 
-- 第91个元素和第0个元素的值
- - 属性91 @用户需求91
- -  @~~
-- 获取符合条件的需求数 @5
-- 测试附加的需求ID1，需求1以数字1结尾属性1 @~f:1$~
+- 获取ID为3的父需求的标题第3条的keys属性 @用户需求9
+- 获取符合条件的需求数 @3
+- 测试附加的需求ID1，需求1以数字1结尾第0条的keys属性 @软件需求2
 
 */
 
@@ -31,19 +30,24 @@ $story->status->range('active,draft');
 $story->product->range('1{10},2{5},3{5},4{70},5{10}');
 $story->parent->range('0,1');
 $story->plan->range('0,1');
+$story->version->range('1');
+$story->grade->range('1{10},2{10}');
 $story->gen(100);
+
+zenData('storyspec')->gen(100);
+zenData('storygrade')->gen(6);
 
 $product = zenData('product');
 $product->gen(5);
 
 global $tester;
 $tester->loadModel('story');
-$stories = $tester->story->getParentStoryPairs(5);
+$stories = $tester->story->getParentStoryPairs(1);
 
-r($stories) && p('91,0') && e('用户需求91,~~'); // 第91个元素和第0个元素的值
+r($stories) && p('3:keys') && e('用户需求9'); // 获取ID为3的父需求的标题
 
 array_pop($stories); /* remove empty item at array top. */
-r(count($stories)) && p() && e(5);  // 获取符合条件的需求数
+r(count($stories)) && p() && e(3);  // 获取符合条件的需求数
 
-$storiesWithAppended = $tester->story->getParentStoryPairs(5, 1);
-r($storiesWithAppended) && p('1') && e('~f:1$~'); // 测试附加的需求ID1，需求1以数字1结尾
+$storiesWithAppended = $tester->story->getParentStoryPairs(5, 2);
+r($storiesWithAppended) && p('0:keys') && e('软件需求2'); // 测试附加的需求ID1，需求1以数字1结尾
