9408e51f214fa04a0bd7122083535326007d476b
sunguangming <sunguangming@easycorp.ltd>
2024-06-04 10:11:25 +0800

    * Compatible unit test.

--- module/story/test/model/reportcondition.php
+++ module/story/test/model/reportcondition.php
@@ -4,11 +4,12 @@
 /**
 
 title=测试 storyModel->reportCondition();
+timeout=0
 cid=0
 
-- 执行story模块的reportCondition方法  @1=1
-- 执行story模块的reportCondition方法  @`title` like '%aa%'
-- 执行story模块的reportCondition方法  @id in (SELECT t1.id FROM `zt_story` AS t1 WHERE t1.`title` like '%aa%')
+- 执行story模块的reportCondition方法，参数是'story'  @1=1
+- 执行story模块的reportCondition方法，参数是'story'  @`title` like '%aa%'
+- 执行story模块的reportCondition方法，参数是'story'  @id in (SELECT t1.id FROM `zt_story` AS t1 WHERE t1.`title` like '%aa%')
 
 */
 include dirname(__FILE__, 5) . '/test/lib/init.php';
@@ -16,12 +17,12 @@ su('admin');
 global $tester;
 $tester->loadModel('story');
 
-r($tester->story->reportCondition()) && p() && e('1=1');
+r($tester->story->reportCondition('story')) && p() && e('1=1');
 
 $_SESSION['storyOnlyCondition']  = true;
 $_SESSION['storyQueryCondition'] = "`title` like '%aa%'";
-r($tester->story->reportCondition()) && p() && e("`title` like '%aa%'");
+r($tester->story->reportCondition('story')) && p() && e("`title` like '%aa%'");
 
 $_SESSION['storyOnlyCondition']  = false;
 $_SESSION['storyQueryCondition'] = "SELECT * FROM `zt_story` AS t1 WHERE t1.`title` like '%aa%'";
-r($tester->story->reportCondition()) && p() && e("id in (SELECT t1.id FROM `zt_story` AS t1 WHERE t1.`title` like '%aa%')");
+r($tester->story->reportCondition('story')) && p() && e("id in (SELECT t1.id FROM `zt_story` AS t1 WHERE t1.`title` like '%aa%')");
