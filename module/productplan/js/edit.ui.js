3a9287ca72410b305fc8b7087bafbbf2b893abf9
tianshujie <tianshujie@easycorp.ltd>
2024-06-05 16:04:44 +0800

    * [bug#50553,done,0.1h] modify empty branches judgement.

--- module/productplan/js/edit.ui.js
+++ module/productplan/js/edit.ui.js
@@ -23,18 +23,19 @@ $(document).on('change', 'input[name=begin],input[name=end]', function()
 
 $(document).on('click', 'button[type=submit]', function()
 {
-    const parentPlan = $('input[name=parent]').val();
-    const branches    = $('[name^=branch]').val();
-    const title       = $('input[name=title]').val();
-    const begin       = $('input[name=begin]').val();
-    const end         = $('input[name=end]').val();
-    const parentBegin = typeof parentList[parentPlan] !== 'undefined' ? parentList[parentPlan]['begin'] : '';
-    const parentEnd   = typeof parentList[parentPlan] !== 'undefined' ? parentList[parentPlan]['end'] : '';
-    const errorBegin  = parentBegin && begin && begin < parentBegin;
-    const errorEnd    = parentEnd   && end   && end > parentEnd;
-    if(parentPlan > 0 && branches && title && !errorBegin && !errorEnd)
+    const parentPlan   = $('input[name=parent]').val();
+    const branches     = $('[name^=branch]').val();
+    const branchIdList = branches ? branches.toString() : '';
+    const title        = $('input[name=title]').val();
+    const begin        = $('input[name=begin]').val();
+    const end          = $('input[name=end]').val();
+    const parentBegin  = typeof parentList[parentPlan] !== 'undefined' ? parentList[parentPlan]['begin'] : '';
+    const parentEnd    = typeof parentList[parentPlan] !== 'undefined' ? parentList[parentPlan]['end'] : '';
+    const errorBegin   = parentBegin && begin && begin < parentBegin;
+    const errorEnd     = parentEnd   && end   && end > parentEnd;
+    if(parentPlan > 0 && branchIdList !== '' && title && !errorBegin && !errorEnd)
     {
-        const link = $.createLink('productplan', 'ajaxGetDiffBranchesTip', "produtID=" + productID + "&parentID=" + parentPlan + "&branches=" + branches.toString());
+        const link = $.createLink('productplan', 'ajaxGetDiffBranchesTip', "produtID=" + productID + "&parentID=" + parentPlan + "&branches=" + branchIdList);
         $.get(link, function(diffBranchesTip)
         {
             const $form    = $('#editForm').find('form');
