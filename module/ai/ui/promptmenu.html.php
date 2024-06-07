f92ac55f32d4143c092aa5cc00c52550b2165b7f
liumengyi <liumengyi@easycorp.ltd>
2024-06-07 09:43:38 +0800

    + [bug#49415,done,0.3h] set 'z-index' for 'detail-section'.

--- module/ai/ui/promptmenu.html.php
+++ module/ai/ui/promptmenu.html.php
@@ -15,6 +15,11 @@
 
 namespace zin;
 
+h::css("
+.detail-view > .detail-body > .detail-main > .detail-sections:first > .detail-section {position: relative; z-index: 1;}
+.detail-view > .detail-body > .detail-main > .detail-sections:first > .detail-section:first {z-index: 2;}
+");
+
 $promptMenuInject = function()
 {
     if(isInModal()) return;
