449c166a136a5da0aae0db90753e15928f10f4df
liuruogu <liuruogu@easycorp.ltd>
2024-06-07 16:24:07 +0800

    * [story#57185,doing,0.1h,2.0h] Add metric time tips to qastatistic block.

--- module/block/ui/qastatisticblock.html.php
+++ module/block/ui/qastatisticblock.html.php
@@ -125,6 +125,24 @@ $testTasksView = !empty($product) ? $buildTesttasks($product, $longBlock) : null
 
 statisticBlock
 (
+    to::titleSuffix
+    (
+        icon
+        (
+            setClass('text-light text-sm cursor-pointer'),
+            toggle::tooltip
+            (
+                array
+                (
+                    'title'     => sprintf($lang->block->tooltips['metricTime'], $metricTime),
+                    'placement' => 'bottom',
+                    'type'      => 'white',
+                    'className' => 'text-dark border border-light leading-5'
+                )
+            ),
+            'help'
+        )
+    ),
     set::block($block),
     set::active($active),
     set::items($items),
