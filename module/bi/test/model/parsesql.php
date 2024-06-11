d885ebf0753e94400055ff8a49bc8f2ef78aaad7
qixinzhi <qixinzhi@easycorp.ltd>
2024-06-11 06:26:47 +0000

    + [misc] init unit test:parseSqlTest.

---
+++ module/bi/test/model/parsesql.php
@@ -0,0 +1,12 @@
+#!/usr/bin/env php
+<?php
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+include dirname(__FILE__, 2) . '/lib/bi.unittest.class.php';
+
+/**
+
+title=biModel->parseSql();
+timeout=0
+cid=1
+
+*/
