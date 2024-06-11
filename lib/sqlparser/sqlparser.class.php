bfaf6c56496da32b12bac7ce37f82d6ab25a37b5
qixinzhi <qixinzhi@easycorp.ltd>
2024-06-11 02:50:49 +0000

    * [misc] add skipLineBreak function.

--- lib/sqlparser/sqlparser.class.php
+++ lib/sqlparser/sqlparser.class.php
@@ -8,4 +8,23 @@ class sqlparser
         $this->parser = new PhpMyAdmin\SqlParser\Parser($query);
         $this->statements = $this->parser->statements;
     }
+
+    /**
+     * Skip line break in sql.
+     *
+     * @param  string    $sql
+     * @access public
+     * @return string
+     */
+    public function skipLineBreak($sql)
+    {
+        $sql = str_replace("\n\t", " ", $sql);
+        $sql = str_replace("\t\n", " ", $sql);
+        $sql = str_replace("\n\r", " ", $sql);
+        $sql = str_replace("\r\n", " ", $sql);
+        $sql = str_replace("\r", " ", $sql);
+        $sql = str_replace("\n", " ", $sql);
+
+        return $sql;
+    }
 }
