000d6755e479002cfcfa0efb7503bb3db6011e92
qixinzhi <qixinzhi@easycorp.ltd>
2024-06-11 06:31:33 +0000

    + [misc] add unit test case:parseSqlTest.

--- module/bi/test/model/parsesql.php
+++ module/bi/test/model/parsesql.php
@@ -39,6 +39,17 @@ $sqls[1] = <<<EOT
 select id,name,consumed from zt_task
 EOT;
 
+$sqls[2] = <<<EOT
+SELECT DISTINCT id,estimate
+FROM zt_story t1
+WHERE 1 = (
+    SELECT COUNT(DISTINCT id,estimate)
+    FROM zt_story t2
+    WHERE t2.estimate> t1.estimate
+)
+EOT;
+
 $bi=new biTest();
 r($bi->parseSqlTest($sqls[0])) && p('id,name,bugID,type,fixedBugs,caseTitle') && e('zt_product=>id,zt_product=>name,zt_bug=>id,zt_bug=>type,zt_product=>fixedBugs,zt_case=>title'); // 测试第1条sql
 r($bi->parseSqlTest($sqls[1])) && p('id,name,consumed')                       && e('zt_task=>id,zt_task=>name,zt_task=>consumed');                                                  // 测试第2条sql
+r($bi->parseSqlTest($sqls[2])) && p('id,estimate')                            && e('zt_story=>id,zt_story=>estimate');                                                              // 测试第3条sql
