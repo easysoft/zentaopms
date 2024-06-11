3b13de68b2699ba7858dbfc4e779bf0890729794
dingguodong <dingguodong@easycorp.ltd>
2024-06-11 16:30:23 +0800

    * [task#116512] Load model before useing it.

--- module/backup/control.php
+++ module/backup/control.php
@@ -119,7 +119,7 @@ class backup extends control
         $systemInfo->currentVersion = $version;
         $systemInfo->versionHint    = $version;
         $systemInfo->latestVersion  = $latestVersion;
-        $systemInfo->upgradeable    = $version != $latestVersion && $this->system->isUpgradeable();
+        $systemInfo->upgradeable    = $version != $latestVersion && $this->loadModel('system')->isUpgradeable();
         $systemInfo->upgradeHint    = $systemInfo->upgradeable ? $this->lang->system->backup->versionInfo: null;
         $systemInfo->latestURL      = !empty($latestVersionList[$version]->link) ? $latestVersionList[$version]->link : $this->lang->install->officeDomain;
 
