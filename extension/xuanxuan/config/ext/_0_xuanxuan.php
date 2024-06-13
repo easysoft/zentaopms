9c1322ec56ba729a80724cd0f5f4806f758fd2f3
Chenjianyu <chenjianyu@easycorp.ltd>
2024-06-13 05:47:34 +0000

    * [refac] revert zentaoxx makefile changes.

--- extension/xuanxuan/config/ext/_0_xuanxuan.php
+++
@@ -1,6 +0,0 @@
-<?php
-$config->xuanxuan->enabledMethods['im']['getaipermission']    = 'getAiPermission';
-$config->xuanxuan->enabledMethods['im']['chatwithai']         = 'chatWithAi';
-$config->xuanxuan->enabledMethods['im']['clearaichatcontext'] = 'clearAiChatContext';
-$config->xuanxuan->enabledMethods['im']['getaiassistant']     = 'getAiAssistant';
-$config->xuanxuan->enabledMethods['im']['getaichat']          = 'getAiChat';
