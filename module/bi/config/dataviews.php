601fba2b6e40db0364aecbe52c14ed95afed3857
qixinzhi <qixinzhi@easycorp.ltd>
2024-06-13 09:15:43 +0000

    + [bug#51074,doing,0.1h,0.5h] add built-in dataview build field.

--- module/bi/config/dataviews.php
+++ module/bi/config/dataviews.php
@@ -9,6 +9,20 @@ $config->bi->builtin->dataviews[] = array
     'sql'       => <<<EOT
 SELECT product.name AS `product_name`,product.id AS `product_id`,project.name AS `project_name`,project.id AS `project_id`,execution.name AS `execution_name`,execution.id AS `execution_id`,build.name AS `name`,build.builder AS `builder`,build.stories AS `stories`,build.bugs AS `bugs`,build.date AS `date`,build.desc AS `desc` FROM zt_build AS `build`  LEFT JOIN zt_product AS `product` ON product.id   = build.product  LEFT JOIN zt_project AS `project` ON project.id   = build.project  LEFT JOIN zt_project AS `execution` ON execution.id = build.execution where `build`.deleted = '0' LIMIT 100
 EOT,
-    'fields'    => array(),
+    'fields'    => array
+    (
+        'product_id'     => array('name' => '编号', 'field' => 'id', 'object' => 'product', 'type' => 'object'),
+        'product_name'   => array('name' => '所属产品', 'field' => 'name', 'object' => 'product', 'type' => 'object'),
+        'project_id'     => array('name' => '项目ID', 'field' => 'id', 'object' => 'project', 'type' => 'object'),
+        'project_name'   => array('name' => '所属项目', 'field' => 'name', 'object' => 'project', 'type' => 'object'),
+        'execution_id'   => array('name' => '迭代编号', 'field' => 'id', 'object' => 'execution', 'type' => 'object'),
+        'execution_name' => array('name' => '所属迭代', 'field' => 'name', 'object' => 'execution', 'type' => 'object'),
+        'name'           => array('name' => '名称编号', 'field' => 'name', 'object' => 'build', 'type' => 'string'),
+        'builder'        => array('name' => '构建者', 'field' => 'builder', 'object' => 'build', 'type' => 'user'),
+        'stories'        => array('name' => '完成的研发需求', 'field' => 'stories', 'object' => 'build', 'type' => 'string'),
+        'bugs'           => array('name' => '解决的Bug', 'field' => 'bugs', 'object' => 'build', 'type' => 'string'),
+        'date'           => array('name' => '打包日期', 'field' => 'date', 'object' => 'build', 'type' => 'date'),
+        'desc'           => array('name' => '描述', 'field' => 'desc', 'object' => 'build', 'type' => 'string')
+    ),
     'group'     => '101'
 );
