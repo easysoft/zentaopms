5f912cdbe4a4c31b55384c1ae5dc4193fc03d739
tianshujie <tianshujie@easycorp.ltd>
2024-06-14 10:56:32 +0800

    * [bug#51123,done,0.2h] change data-load to data-toggle.

--- module/story/js/view.ui.js
+++ module/story/js/view.ui.js
@@ -19,3 +19,8 @@ window.refreshViewModal = function()
         loadPage($.createLink('story', 'view', 'storyID=' + storyID));
     }
 }
+
+window.waitDom('body.body-modal .toolbar', function()
+{
+    $('.body-modal .toolbar a[data-load="modal"]').attr('data-toggle', 'modal').removeAttr('data-load');
+})
