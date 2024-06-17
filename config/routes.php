8e96e94fad6f79c95b64198402ddcff62b619d1a
caoyanyi <caoyanyi@easycorp.ltd>
2024-06-17 09:43:48 +0800

    - [story#57301] delete gitfox and artifactrepo code.

--- config/routes.php
+++ config/routes.php
@@ -178,7 +178,6 @@ $routes['/z/files/:id']         = 'zfile';
 $routes['/z/files/:id/content'] = 'zfileContent';
 
 $routes['/gitlab/webhook'] = 'gitlabWebhook';
-$routes['/gitfox/webhook'] = 'gitfoxWebhook';
 
 $routes['/ciresults'] = 'ciresults';
 
