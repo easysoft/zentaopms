71848541fee9e5c9e3a64c705c632a5362ce569e
hufangzhou <hufangzhou@easycorp.ltd>
2024-06-17 11:09:49 +0800

    * [bug#50735,done,0.1h] Hide the return button in the create story pop-up.

--- module/story/ui/create.html.php
+++ module/story/ui/create.html.php
@@ -127,7 +127,7 @@ formGridPanel
     (
         array('text' => $lang->save,             'data-status' => 'active', 'class' => 'primary',   'btnType' => 'submit'),
         array('text' => $lang->story->saveDraft, 'data-status' => 'draft',  'class' => 'secondary', 'btnType' => 'submit'),
-        array('text' => $lang->goback,           'back'   => true)
+        !isInModal() ? array('text' => $lang->goback, 'back' => true) : null
     )),
     set::fields($createFields),
     on::click('#loadURS', "loadURS"),
