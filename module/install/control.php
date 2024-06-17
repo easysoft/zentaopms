80e688416cdabc56bf46626df7ecd614db5f9df2
dingguodong <dingguodong@easycorp.ltd>
2024-06-17 18:22:15 +0800

    * [bug#51198,done,0.5h] Remove the DevOps from version name.

--- module/install/control.php
+++ module/install/control.php
@@ -53,7 +53,6 @@ class install extends control
             $editionName = $this->config->edition === 'open' ? $this->lang->pmsName : $this->lang->{$this->config->edition . 'Name'};
             if($this->config->edition === 'max') $editionName = '';
             $this->view->versionName   = $editionName . str_replace(array('max', 'biz', 'ipd'), '', $this->view->versionName);
-            $this->view->versionName   = $this->lang->devopsPrefix . $this->view->versionName;
             $this->lang->install->desc = $this->lang->install->desc . "\n" . $this->lang->install->devopsDesc;
         }
         $this->display();
