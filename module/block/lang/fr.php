06092cd654dc59638d9682162718d76bedc451e6
wangyuting <wangyuting@easycorp.ltd>
2024-06-18 15:14:44 +0800

    * [bug#51197,done,0.3h,0h] Add requirement item to welcome block.

--- module/block/lang/fr.php
+++ module/block/lang/fr.php
@@ -539,10 +539,11 @@ $lang->block->welcome->reviewList['story']      = 'Story';
 $lang->block->welcome->reviewList['reviewByMe'] = 'Review';
 
 $lang->block->welcome->assignList = array();
-$lang->block->welcome->assignList['task']        = 'Task';
+$lang->block->welcome->assignList['task'] = 'Task';
 if($config->vision != 'or') $lang->block->welcome->assignList['bug']   = 'Bug';
 if($config->vision != 'or') $lang->block->welcome->assignList['story'] = 'SRStroy';
-$lang->block->welcome->assignList['testcase']    = 'TestCase';
+$lang->block->welcome->assignList['testcase'] = 'TestCase';
+if($config->vision != 'or') $lang->block->welcome->assignList['requirement'] = 'URStroy';
 
 $lang->block->customModeTip = new stdClass();
 $lang->block->customModeTip->common = 'There are 2 running modes of ZenTao:  Light Mode and ALM Mode.';
