6931137406341c735724bf62dbcc181744fd9615
liwenrui <liwenrui@easycorp.ltd>
2024-06-19 10:55:17 +0800

    * [misc] update zeneditor.

--- www/js/zui3/zen-editor/p-5fc8e56e.js
+++ www/js/zui3/zen-editor/p-5fc8e56e.js
@@ -1,4 +1,4 @@
-import{conf as e,language as t}from"./p-b91c94ed.js";import"./p-72ee5dac.js";import"./p-7900c24a.js";
+import{conf as e,language as t}from"./p-f3476fcb.js";import"./p-0266e7fc.js";import"./p-7900c24a.js";import"./p-986e5fe7.js";
 /*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.45.0(5e5af013f8d295555a7210df0d5f2cea0bf5dd56)
