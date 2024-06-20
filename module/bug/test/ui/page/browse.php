f714f1126f0a5737c07d4fef04703b60534e4859
luxuyang <luxuyang@easycorp.ltd>
2024-06-20 11:34:51 +0800

    * [test,done,0.5h,0] fix confirm bug xpath.

--- module/bug/test/ui/page/browse.php
+++ module/bug/test/ui/page/browse.php
@@ -7,7 +7,7 @@ class browsePage extends page
         $xpath = array(
             'searchButton' => "//*[@id='mainContentCell']//button[text()='搜索']",
             'bugTitle' => '(//div[@id="table-bug-browse"]/div[@class="dtable-block dtable-body"]/div[@class="dtable-cells dtable-fixed-left"]/div//a)[1]',
-            'confirmButton' => '(//a[@title="确认"])[1]',
+            'confirmButton' => '(//a[@title="确认"]/i)[1]',
         );
 
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
