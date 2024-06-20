de815fa8ebce5487b4ba93cc1529f6d61829a583
luxuyang <luxuyang@easycorp.ltd>
2024-06-20 11:32:19 +0800

    * [test,doing,0.5h,0.5h] add confirm bug with any pickers.

--- module/bug/test/lib/confirmbug.ui.class.php
+++ module/bug/test/lib/confirmbug.ui.class.php
@@ -11,7 +11,17 @@ class confirmBugTester extends tester
         $list->dom->field1->picker($bug['search']);
         $list->dom->value1->picker($bug['isConfirm']);
         $list->dom->searchButton->click();
+        $this->webdriver->wait(1);
         $bugTitle = $list->dom->bugTitle->getText();
         $list->dom->confirmButton->click();
+        $this->webdriver->wait(1);
+
+        if(isset($bug['assignedTo'])) $list->dom->assignedTo->picker($bug['assignedTo']);
+        if(isset($bug['type'])) $list->dom->type->picker($bug['type']);
+        if(isset($bug['pri'])) $list->dom->pri->picker($bug['pri']);
+        if(isset($bug['deadline'])) $list->dom->deadline->datePicker($bug['deadline']);
+        if(isset($bug['mailto'])) $list->dom->{'mailto[]'}->multiPicker($bug['mailto']);
+
+        $list->dom->btn($this->lang->bug->confirm)->click();
     }
 }
