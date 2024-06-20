1eb422c6f8959f6ed651e87d2ad1e249dc141e1a
luxuyang <luxuyang@easycorp.ltd>
2024-06-20 19:57:49 +0800

    * [test,done,0.5h,0h] add confirm bug assertion.

--- module/bug/test/lib/confirmbug.ui.class.php
+++ module/bug/test/lib/confirmbug.ui.class.php
@@ -21,7 +21,20 @@ class confirmBugTester extends tester
         if(isset($bug['pri'])) $list->dom->pri->picker($bug['pri']);
         if(isset($bug['deadline'])) $list->dom->deadline->datePicker($bug['deadline']);
         if(isset($bug['mailto'])) $list->dom->{'mailto[]'}->multiPicker($bug['mailto']);
-
         $list->dom->btn($this->lang->bug->confirm)->click();
+        $this->webdriver->wait(1);
+
+        $bugTitleLists = $list->dom->bugTitleList->getElementList($list->dom->page->xpath['bugTitleList']);
+        $bugList = [];
+        foreach($bugTitleLists->element as $bugTitleList)
+        {
+           $bugList[] = $bugTitleList->getText();
+        }
+        if(!in_array($bugTitle, $bugList))
+        {
+            return $this->success('确认bug成功');
+        }
+        else return $this->failed('确认bug失败');
     }
+
 }
