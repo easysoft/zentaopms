e5c148b09ef96b2a18c9d8b2ead93e8cb1326af5
luxuyang <luxuyang@easycorp.ltd>
2024-06-25 16:46:42 +0800

    * [test,doing,0.5h,0.5h] add resolve bug uitest xpath.

--- module/bug/test/ui/page/browse.php
+++ module/bug/test/ui/page/browse.php
@@ -5,10 +5,11 @@ class browsePage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'searchButton' => "//*[@id='mainContentCell']//button[text()='搜索']",
-            'bugTitle' => '(//div[@id="table-bug-browse"]/div[@class="dtable-block dtable-body"]/div[@class="dtable-cells dtable-fixed-left"]/div//a)[1]',
-            'bugTitleList' => '//div[@id="table-bug-browse"]/div[@class="dtable-block dtable-body"]/div[@class="dtable-cells dtable-fixed-left"]/div//a',
+            'searchButton'  => "//*[@id='mainContentCell']//button[text()='搜索']",
+            'bugTitle'      => '(//div[@id="table-bug-browse"]/div[@class="dtable-block dtable-body"]/div[@class="dtable-cells dtable-fixed-left"]/div//a)[1]',
+            'bugTitleList'  => '//div[@id="table-bug-browse"]/div[@class="dtable-block dtable-body"]/div[@class="dtable-cells dtable-fixed-left"]/div//a',
             'confirmButton' => '(//a[@title="确认"]/i)[1]',
+            'resolveButton' => '(//a[@title="解决"]/i)[1]',
         );
 
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
