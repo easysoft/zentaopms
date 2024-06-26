e828a0d99279df41486b23c4c07929884e509b47
sunhao <sunhao@easycorp.ltd>
2024-06-26 12:10:47 +0800

    + [misc] build dropPicker with dropdown.

--- lib/zin/wg/droppicker/v1.php
+++ lib/zin/wg/droppicker/v1.php
@@ -22,4 +22,22 @@ class dropPicker extends wg
         'name'  => 'string',
         'items' => 'array'
     );
+    protected function build()
+    {
+        list($items, $text, $value, $name) = $this->prop(array('items', 'text', 'value', 'name'));
+        $btnID = $this->gid;
+
+        return new dropdown
+        (
+            new btn
+            (
+                $text,
+                setClass('w-full justify-between'),
+                setID($btnID),
+                h::formHidden($name, $value)
+            ),
+            set::items($items),
+            set($this->getRestProps())
+        );
+    }
 }
