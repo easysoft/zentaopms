7ae37e831f85522f5e4905221cbab7a15b93b08c
sunhao <sunhao@easycorp.ltd>
2024-06-26 12:12:24 +0800

    + [misc] add comments.

--- lib/zin/wg/droppicker/v1.php
+++ lib/zin/wg/droppicker/v1.php
@@ -1,4 +1,14 @@
 <?php
+declare(strict_types=1);
+/**
+ * The dropPicker widget class file of zin module of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      sunhao<sunhao@easycorp.ltd>
+ * @package     zin
+ * @link        http://www.zentao.net
+ */
 
 namespace zin;
 /**
@@ -22,6 +32,13 @@ class dropPicker extends wg
         'name'  => 'string',
         'items' => 'array'
     );
+
+    /**
+     * Override the build method.
+     *
+     * @access protected
+     * @return array
+     */
     protected function build()
     {
         list($items, $text, $value, $name) = $this->prop(array('items', 'text', 'value', 'name'));
