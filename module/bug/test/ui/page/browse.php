452735cdc8800b4a9bdca4e2a8938f908a9407d9
luxuyang <luxuyang@easycorp.ltd>
2024-06-26 15:43:20 +0800

    + [test,done,0.2h,0h] add close bug uitest xpath.

--- module/bug/test/ui/page/browse.php
+++ module/bug/test/ui/page/browse.php
@@ -10,6 +10,7 @@ class browsePage extends page
             'bugTitleList'  => '//div[@id="table-bug-browse"]/div[@class="dtable-block dtable-body"]/div[@class="dtable-cells dtable-fixed-left"]/div//a',
             'confirmButton' => '(//a[@title="确认"]/i)[1]',
             'resolveButton' => '(//a[@title="解决"]/i)[1]',
+            'closeButton'   => '(//a[@title="关闭"]/i)[1]',
         );
 
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
