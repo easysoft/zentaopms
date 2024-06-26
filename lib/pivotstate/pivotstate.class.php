c93d21edac6a37dd50c5d6d0180a09749822047f
qixinzhi <qixinzhi@easycorp.ltd>
2024-06-26 05:45:39 +0000

    + [story#54149,doing,0.1h,2h] add pivotstate.class property.

--- lib/pivotstate/pivotstate.class.php
+++ lib/pivotstate/pivotstate.class.php
@@ -1,4 +1,19 @@
 <?php
 class pivotState
 {
+    /**
+     * Pivot state ID.
+     *
+     * @var int
+     * @access public
+     */
+    public $id;
+
+    /**
+     * Pivot state dimension.
+     *
+     * @var string
+     * @access public
+     */
+    public $dimension;
 }
