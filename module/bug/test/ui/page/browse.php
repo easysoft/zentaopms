8a0d59e25872c22b2f3f34a47c91e3cc52caea6b
luxuyang <luxuyang@easycorp.ltd>
2024-06-26 18:22:13 +0800

    + [test,done,0.1h,0h] add more button xpath.

--- module/bug/test/ui/page/browse.php
+++ module/bug/test/ui/page/browse.php
@@ -11,6 +11,7 @@ class browsePage extends page
             'confirmButton' => '(//a[@title="确认"]/i)[1]',
             'resolveButton' => '(//a[@title="解决"]/i)[1]',
             'closeButton'   => '(//a[@title="关闭"]/i)[1]',
+            'more'          => "//*[@id='mainContentCell']//button[text()='保存搜索条件']/following-sibling::button/i",
         );
 
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
