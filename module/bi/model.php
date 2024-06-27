15b913a8e87251b50138e933a38927a4b967aa77
qixinzhi <qixinzhi@easycorp.ltd>
2024-06-27 01:10:16 +0000

    * [story#54149,doing,0.1h,0.5h] keep sql origin.

--- module/bi/model.php
+++ module/bi/model.php
@@ -1001,14 +1001,14 @@ class biModel extends model
     public function query($stateObj, $driver = 'mysql')
     {
         $dbh = $this->app->loadDriver($driver);
-        $stateObj->sql = $this->processVars($stateObj->sql, $stateObj->filters);
+        $sql = $this->processVars($stateObj->sql, $stateObj->filters);
 
         $stateObj->beforeQuerySql();
 
-        $statement = $this->sql2Statement($stateObj->sql);
+        $statement = $this->sql2Statement($sql);
         if(is_string($statement)) return $stateObj->setError($statement);
 
-        $checked = $this->validateSql($stateObj->sql);
+        $checked = $this->validateSql($sql);
         if($checked !== true) return $stateObj->setError($checked);
 
         $recPerPage = $stateObj->pager->recPerPage;
@@ -1016,7 +1016,7 @@ class biModel extends model
         $limitSql   = $this->prepareSqlPager($statement, $recPerPage, $pageID, $driver);
 
         $mysqlCountSql  = "SELECT FOUND_ROWS() AS count";
-        $duckdbCountSql = "SELECT COUNT(1) AS count FROM ($stateObj->sql)";
+        $duckdbCountSql = "SELECT COUNT(1) AS count FROM ($sql)";
 
         try
         {
