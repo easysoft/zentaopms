157402695c5ffb11e83a30c505cb9106258fd42a
luxuyang <luxuyang@easycorp.ltd>
2024-07-01 09:49:55 +0800

    + [test,done,0.5h,0h] add create testcase assertion.

--- module/testcase/test/lib/testcase.ui.class.php
+++ module/testcase/test/lib/testcase.ui.class.php
@@ -11,5 +11,13 @@ class testcase extends tester
         if(isset($testcase['type']))     $form->dom->type->picker($testcase['type']);
         if(isset($testcase['stage']))    $form->dom->{'stage[]'}->multiPicker($testcase['stage']);
         if(isset($testcase['pri']))      $form->dom->pri->picker($testcase['pri']);
+        $form->dom->btn($this->lang->save)->click();
+        $this->webdriver->wait(1);
+
+        $caseLists = $form->dom->caseName->getElementList($form->dom->page->xpath['caseNameList']);
+        $caseList  = array_map(function($element){return $element->getText();}, $caseLists->element);
+        var_dump($caseList);
+        if(in_array($testcase['caseName'], $caseList)) return $this->success('创建测试用例成功');
+        return $this->failed('创建测试用例失败');
     }
 }
