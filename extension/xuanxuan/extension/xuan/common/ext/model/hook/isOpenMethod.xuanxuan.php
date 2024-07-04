5d586d135dd459000c1fd286998a6c898d6958f3
liwenrui <liwenrui@easycorp.ltd>
2024-07-04 14:50:24 +0800

    * [bug#52139] fix xuan methods inaccessible from xxd.

--- extension/xuanxuan/extension/xuan/common/ext/model/hook/isOpenMethod.xuanxuan.php
+++ extension/xuanxuan/extension/xuan/common/ext/model/hook/isOpenMethod.xuanxuan.php
@@ -1,4 +1,6 @@
 <?php
+if(defined('RUN_MODE') && RUN_MODE == 'xuanxuan' && !empty($this->config->xuanxuan->enabledMethods[$module][$method])) return true;
+
 if($module == 'entry' and $method == 'visit')      return true;
 if($module == 'integration' and $method == 'wopi') return true;
 if($module == 'im' and $method == 'authorize')     return true;
