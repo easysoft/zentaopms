294e31948d536c02541c22a796cbf90ba0deca8c
zhouxin <zhouxin@easycorp.ltd>
2024-07-08 17:06:15 +0800

    * [bug#52241,done,1h] add config of field type.

--- module/dataview/config.php
+++ module/dataview/config.php
@@ -12,3 +12,21 @@ $config->dataview->groupChild['company'] = array('user' => 'user,usercontact,use
 
 $config->dataview->multipleMappingFields = array();
 $config->dataview->multipleMappingFields[] = 'testcase-stage';
+
+$config->dataview->fieldTypes = new stdclass();
+$config->dataview->fieldTypes->TINY       = 'number';
+$config->dataview->fieldTypes->SHORT      = 'number';
+$config->dataview->fieldTypes->LONG       = 'number';
+$config->dataview->fieldTypes->FLOAT      = 'number';
+$config->dataview->fieldTypes->DOUBLE     = 'number';
+$config->dataview->fieldTypes->TIMESTAMP  = 'string';
+$config->dataview->fieldTypes->LONGLONG   = 'string';
+$config->dataview->fieldTypes->INT24      = 'number';
+$config->dataview->fieldTypes->DATE       = 'date';
+$config->dataview->fieldTypes->TIME       = 'string';
+$config->dataview->fieldTypes->DATETIME   = 'date';
+$config->dataview->fieldTypes->YEAR       = 'date';
+$config->dataview->fieldTypes->ENUM       = 'string';
+$config->dataview->fieldTypes->SET        = 'string';
+$config->dataview->fieldTypes->TINYBLOB   = 'string';
+$config->dataview->fieldTypes->MEDIUMBLOB = 'string';
