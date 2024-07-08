9d80c1422f8ec9f834fbc58aca15d257376d5b45
sunguangming <sunguangming@easycorp.ltd>
2024-07-08 08:49:45 +0000

    * [bug] split story config.

--- module/epic/config.php
+++ module/epic/config.php
@@ -4,3 +4,20 @@ $app->loadConfig('story');
 $config->epic = clone $config->story;
 
 $config->epic->needReview = 1;
+
+$config->epic->create = new stdclass();
+$config->epic->edit   = new stdclass();
+$config->epic->change = new stdclass();
+$config->epic->close  = new stdclass();
+$config->epic->review = new stdclass();
+
+$config->epic->create->requiredFields = 'title';
+$config->epic->edit->requiredFields = 'title';
+$config->epic->change->requiredFields = 'title';
+$config->epic->close->requiredFields  = 'closedReason';
+$config->epic->review->requiredFields = '';
+
+$config->epic->custom = new stdclass();
+$config->epic->custom->createFields      = '';
+$config->epic->custom->batchCreateFields = 'module,plan,spec,pri,estimate,review,%s';
+$config->epic->custom->batchEditFields   = 'branch,module,plan,estimate,pri,source,stage,closedBy,closedReason';
