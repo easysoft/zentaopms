c364003c99541cb38d9a4f9f298519dbbadea75b
qixinzhi <qixinzhi@easycorp.ltd>
2024-07-08 03:53:25 +0000

    * [misc] add installduckdb.html.php.

---
+++ module/install/ui/installduckdb.html.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+/**
+ * The install duckdb view file of install module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Yanyi Cao <caoyanyi@easycorp.ltd>
+ * @package     install
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+
+$icons = array
+(
+    'loading' => 'spinner-indicator',
+    'ok'      => 'check-circle',
+    'fail'    => 'close'
+);
