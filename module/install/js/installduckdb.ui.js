eb1c49073ab825ca56972b41cb452fff5ea40ee7
qixinzhi <qixinzhi@easycorp.ltd>
2024-07-08 03:22:21 +0000

    * [misc] add ajaxCheckDuckdb.

--- module/install/js/installduckdb.ui.js
+++ module/install/js/installduckdb.ui.js
@@ -9,3 +9,22 @@ function ajaxInstallDuckdb()
   let url = $.createLink('install', 'ajaxInstallDuckdb');
   $.get(url);
 }
+
+function ajaxCheckDuckdb()
+{
+  let url = $.createLink('install', 'ajaxCheckDuckdb');
+  $.get(url, function(resp)
+  {
+    resp = JSON.parse(resp);
+    if(resp.duckdb == 'loading' || resp.extension == 'loading') setTimeout(() => {ajaxCheckDuckdb()}, 500);
+
+    $('#installDuckdb p').addClass('hidden');
+    $('#installDuckdb p.duckdb-' + resp.duckdb).removeClass('hidden');
+    $('#installDuckdb p.extension-' + resp.extension).removeClass('hidden');
+
+    if(resp.duckdb == 'fail' || resp.extension == 'fail')
+    {
+      $('#installDuckdb .help').removeClass('hidden');
+    }
+  });
+}
