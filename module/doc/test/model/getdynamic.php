800907edd04b692891da112464d3bc7de8813a34
xieqiyu <xieqiyu@easycorp.ltd>
2024-07-09 08:46:20 +0800

    * [misc] Delete space.

--- module/doc/test/model/getdynamic.php
+++ module/doc/test/model/getdynamic.php
@@ -30,6 +30,6 @@ $docTester = new docTest();
 r($docTester->getDynamicTest($recPerPages[0], $pagerIds[0])) && p() && e('31;30;29;28;27;');                                              // 获取每页5条，第1页动态数据
 r($docTester->getDynamicTest($recPerPages[0], $pagerIds[1])) && p() && e('26;25;24;23;22;');                                              // 获取每页5条，第2页动态数据
 r($docTester->getDynamicTest($recPerPages[1], $pagerIds[0])) && p() && e('31;30;29;28;27;26;25;24;23;22;');                               // 获取每页10条，第1页动态数据
-r($docTester->getDynamicTest($recPerPages[1], $pagerIds[1])) && p() && e('21;20;19;50;18;49;17;48;16;47;');                                // 获取每页10条，第2页动态数据
+r($docTester->getDynamicTest($recPerPages[1], $pagerIds[1])) && p() && e('21;20;19;50;18;49;17;48;16;47;');                               // 获取每页10条，第2页动态数据
 r($docTester->getDynamicTest($recPerPages[2], $pagerIds[0])) && p() && e('31;30;29;28;27;26;25;24;23;22;21;20;19;50;18;49;17;48;16;47;'); // 获取每页20条，第1页动态数据
 r($docTester->getDynamicTest($recPerPages[2], $pagerIds[1])) && p() && e('15;46;14;45;13;44;12;43;11;42;10;41;9;40;8;39;7;38;6;37;');     // 获取每页20条，第2页动态数据
