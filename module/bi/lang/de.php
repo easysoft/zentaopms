f3e1c7bf86831ed1e77ca1927fb1f7590d5bc19b
qixinzhi <qixinzhi@easycorp.ltd>
2024-07-09 02:49:36 +0000

    * [story#59956,0.5h] add lang.

---
+++ module/bi/lang/de.php
@@ -0,0 +1,3 @@
+<?php
+$lang->bi->binNotExists        = 'The DuckDB binary does not exist.';
+$lang->bi->tmpPermissionDenied = 'The DuckDB tmp directory has no permissions.';
---
+++ module/bi/lang/zh-tw.php
@@ -0,0 +1,3 @@
+<?php
+$lang->bi->binNotExists        = 'DuckDB 二进制文件不存在。';
+$lang->bi->tmpPermissionDenied = 'DuckDB tmp 目录没有权限。';
