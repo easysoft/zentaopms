03e1f5553d6550407a9e3b8e5f83265b643bce02
qixinzhi <qixinzhi@easycorp.ltd>
2024-07-09 02:58:58 +0000

    * [story#59956,0.5h] add lang.

--- module/bi/lang/zh-tw.php
+++ module/bi/lang/zh-tw.php
@@ -1,3 +1,3 @@
 <?php
 $lang->bi->binNotExists        = 'DuckDB 二进制文件不存在。';
-$lang->bi->tmpPermissionDenied = 'DuckDB tmp 目录没有权限。';
+$lang->bi->tmpPermissionDenied = 'DuckDB tmp 目录没有权限, 需要修改目录 "%s" 的权限。<br />命令为：<br />chmod 755 -R %s。';
