f26755d2b623c0fb9b97a8ea9d5e2daa7f12ea2c
sunhao <sunhao@easycorp.ltd>
2024-07-09 16:29:45 +0800

    * [refac] refactor.

--- lib/zin/wg/col/v1.php
+++ lib/zin/wg/col/v1.php
@@ -13,14 +13,14 @@ class col extends wg
     protected function build()
     {
         $classList = 'col';
-        list($justify, $align) = $this->prop(array('justify', 'align'));
+        list($justify, $align, $gap) = $this->prop(array('justify', 'align', 'gap'));
         if(!empty($justify)) $classList .= ' justify-' . $justify;
         if(!empty($align))   $classList .= ' items-' . $align;
 
         return div
         (
             setClass($classList),
-            zui::gap($this->prop('gap')),
+            is_numeric($gap) ? setClass("gap-$gap") : setStyle('gap', $gap),
             set($this->getRestProps()),
             $this->children()
         );
