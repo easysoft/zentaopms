3ac527da5027c516bae500a1b332a11f31b393f7
qixinzhi <qixinzhi@easycorp.ltd>
2024-07-09 10:44:34 +0000

    * [story#52426,0.5h] fix upgrade duckdb install.

--- module/upgrade/js/execute.ui.js
+++ module/upgrade/js/execute.ui.js
@@ -28,7 +28,8 @@ function initStatus()
 {
     $('#installDuckdb p').addClass('hidden');
     $('#installDuckdb p.duckdb-' + duckdb).removeClass('hidden');
-    $('#installDuckdb p.extension-' + extension).removeClass('hidden');
+    $('#installDuckdb p.ext_dm-' + ext_dm).removeClass('hidden');
+    $('#installDuckdb p.ext_mysql-' + ext_mysql).removeClass('hidden');
 }
 
 /**
@@ -53,22 +54,18 @@ function ajaxInstallDuckdb()
  */
 function ajaxCheckDuckdb()
 {
-    let url = $.createLink('upgrade', 'ajaxCheckDuckdb');
+    let url = $.createLink('install', 'ajaxCheckDuckdb');
     $.get(url, function(resp)
     {
         resp = JSON.parse(resp);
-        if(resp.duckdb == 'loading' || resp.extension == 'loading')
-        {
-            setTimeout(() => {ajaxCheckDuckdb()}, 500);
-        }
+        const {loading, ok, fail, duckdb, ext_dm, ext_mysql} = resp;
+        if(loading) setTimeout(() => {ajaxCheckDuckdb()}, 500);
 
         $('#installDuckdb p').addClass('hidden');
-        $('#installDuckdb p.duckdb-' + resp.duckdb).removeClass('hidden');
-        $('#installDuckdb p.extension-' + resp.extension).removeClass('hidden');
+        $('#installDuckdb p.duckdb-' + duckdb).removeClass('hidden');
+        $('#installDuckdb p.ext_dm-' + ext_dm).removeClass('hidden');
+        $('#installDuckdb p.ext_mysql-' + ext_mysql).removeClass('hidden');
 
-        if(resp.duckdb == 'fail' || resp.extension == 'fail')
-        {
-            $('#installDuckdb .help').removeClass('hidden');
-        }
+        if(fail) $('#installDuckdb .help').removeClass('hidden');
     });
 }
