9b46fe539b5b4096c5ae34e7d66e95850fa9903a
lijie <lijie@easycorp.ltd>
2024-07-12 10:07:06 +0800

    *[misc, 0.5h] add createstory.php file.

---
+++ module/story/test/ui/creatstory.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php
+#<?php
+#declare(strict_types=1);
+#/**
+#
+#title=创建研发需求测试
+#timeout=0
+#cid=80
+#
+#- 缺少需求名称，创建失败
+# -  测试结果 @创建产品表单页提示信息正确
+#  -  最终测试状态 @SUCCESS
+#  - 使用默认选项创建研发需求 最终测试状态 @SUCCESS
+#  - 创建正常需求后检查创建需求信息是否正确
+#   - 属性module @story
+#    - 属性method @view
+#    - 创建正常产品成功 最终测试状态 @SUCCESS
+#
+#    */
