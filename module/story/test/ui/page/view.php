d70cfaffdc378b490735a1bcf288d966412214e3
lijie <lijie@easycorp.ltd>
2024-07-12 10:21:26 +0800

    *[misc, 0.5h] add createstory.php file.

---
+++ module/story/test/ui/page/view.php
@@ -0,0 +1,16 @@
+<?php
+class viewPage extends page
+{
+    public function __construct($webdriver)
+    {
+        parent::__construct($webdriver);
+
+        $xpath = array(
+            'storyName' => "//*[@id='mainContent']/div/div[1]/div[1]/span[2]",
+            'status' => "/html/body/div[1]/div/div/div/div[2]/div[2]/div[1]/div[2]/div[1]/div/div[6]/div[2]/span",
+            'historyOpenedBy' => "/html/body/div[1]/div/div/div/div[2]/div[1]/div[2]/div/div[2]/ul/li/div/div[2]/div/div/strong",
+
+        );
+        $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
+    }
+}
