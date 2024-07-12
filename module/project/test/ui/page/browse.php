6d10a01add8b37ec591b7de340eda68eb9897bc5
lihuifang <lihuifang@easycorp.ltd>
2024-07-12 08:59:55 +0800

    [misc] adjust code for ui test.

--- module/project/test/ui/page/browse.php
+++ module/project/test/ui/page/browse.php
@@ -6,10 +6,10 @@ class browsePage extends page
         parent::__construct($webdriver);
 
         $xpath = array(
-            'settings' => "//*[@id='navbar']//a[@data-id='settings']/span",
-            'editBtn'  => "//*[@id='table-project-browse']/div[2]/div[3]/div/div[1]/div/nav/a[2]/i",
+            'settings'    => "//*[@id='navbar']//a[@data-id='settings']/span",
+            'editBtn'     => "//*[@id='table-project-browse']/div[2]/div[3]/div/div[1]/div/nav/a[2]/i",
             'projectName' => "//*[@id='table-project-browse']/div[2]/div[1]/div/div[2]/div/a",
-            'scrumName' => "//*[@id='mainContent']/div[2]/div/div/div[2]/div/div/div[2]/div/a",
+            'scrumName'   => "//*[@id='mainContent']/div[2]/div/div/div[2]/div/div/div[2]/div/a",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
--- module/project/test/ui/page/create.php
+++ module/project/test/ui/page/create.php
@@ -5,8 +5,8 @@ class createPage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'settings' => "//*[@id='navbar']/menu/li[14]/a/span",
-             'noproduct' => "//*[@id='mainContent']/div/div[2]/div/form/div[2]/div/div[2]/label",
+            'settings'  => "//*[@id='navbar']/menu/li[14]/a/span",
+            'noproduct' => "//*[@id='mainContent']/div/div[2]/div/form/div[2]/div/div[2]/label",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
