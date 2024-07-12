ae3a2ea753303c14ece38090466312daa7345040
lijie <lijie@easycorp.ltd>
2024-07-12 10:27:33 +0800

    *[misc, 0.3h] add searchedstoryXPATH

--- module/product/test/ui/page/browse.php
+++ module/product/test/ui/page/browse.php
@@ -6,7 +6,8 @@ class browsePage extends page
         parent::__construct($webdriver);
 
         $xpath = array(
-            'settings' => "//*[@id='navbar']//a[@data-id='settings']/span",
+            'settings'        => "//*[@id='navbar']//a[@data-id='settings']/span",
+            'browseStoryName' => "//*[@id="stories"]/div[2]/div[1]/div/div[2]/div/a",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
