2b5dc59517fd512d95d91a4e3761e324271391f6
wangyidong <wangyidong@cnezsoft.com>
2024-07-13 09:23:33 +0800

    * [unittest] Fix unittest for programplan getpairs method.

--- module/programplan/test/model/getpairs.php
+++ module/programplan/test/model/getpairs.php
@@ -6,8 +6,8 @@
 title=测试 programplanModel->getPairs();
 cid=0
 
-- 测试获取执行11 产品11的计划键值对 @;/项目11
-- 测试获取执行12 产品12的计划键值对 @;/项目12
+- 测试获取执行11 产品11的计划键值对 @/项目11
+- 测试获取执行12 产品12的计划键值对 @/项目12
 
 */
 include dirname(__FILE__, 5) . '/test/lib/init.php';
@@ -22,5 +22,5 @@ $productIDList   = array(11, 12, 13, 14, 15);
 
 $programplan = new programplanTest();
 
-r($programplan->getPairsTest($executionIDList[0], $productIDList[0]))         && p() && e(';/项目11'); // 测试获取执行11 产品11的计划键值对
-r($programplan->getPairsTest($executionIDList[1], $productIDList[1], 'leaf')) && p() && e(';/项目12'); // 测试获取执行12 产品12的计划键值对
+r($programplan->getPairsTest($executionIDList[0], $productIDList[0]))         && p() && e('/项目11'); // 测试获取执行11 产品11的计划键值对
+r($programplan->getPairsTest($executionIDList[1], $productIDList[1], 'leaf')) && p() && e('/项目12'); // 测试获取执行12 产品12的计划键值对
