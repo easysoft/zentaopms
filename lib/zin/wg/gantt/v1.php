79c4e280f6d977085e6bfcc84278ab09fc4ca74c
wangyidong <wangyidong@cnezsoft.com>
2024-07-13 09:38:37 +0800

    * [perf story #59018] Define props for gantt wg.

--- lib/zin/wg/gantt/v1.php
+++ lib/zin/wg/gantt/v1.php
@@ -4,17 +4,19 @@ namespace zin;
 
 class gantt extends wg
 {
-    public static function getPageCSS(): string
-    {
-        return file_get_contents(dirname(__DIR__, 4) . '/www/js/dhtmlxgantt/min.css');
-    }
+    protected static array $defineProps = array(
+        'id:string',
+        'ganttLang:array',
+        'canEdit:bool',
+        'canEditDeadline:bool',
+        'ganttFields:array',
+        'showChart?:bool',
+        'zooming?:string',
+        'options?:array'
+    );
 
-    protected function build()
-    {
-        global $app;
-        $jsFile = $app->getWebRoot() . 'js/dhtmlxgantt/min.js';
-
-        return zui::gantt(inherit($this), set::_call("~((name,selector,options) => $.getLib('$jsFile', {root: false}, () => {gantt.plugins({marker: true, critical_path: true, fullscreen: true, tooltip: true, click_drag: true});zui.create(name,selector,options);}))"));
-
-    }
+    protected static array $defaultProps = array(
+        'showChart' => true,
+        'zooming' => 'day'
+    );
 }
