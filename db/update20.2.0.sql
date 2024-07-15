1f0a62aa0e353aeb50a4b51121ae2717371eee1b
sunguangming <sunguangming@easycorp.ltd>
2024-07-15 01:05:46 +0000

    * [sql] add sql.

--- db/update20.2.0.sql
+++ db/update20.2.0.sql
@@ -1,5 +1,5 @@
 UPDATE zt_demand SET status = 'active' WHERE status in ('launched','distributed','pass');
 UPDATE zt_story  SET status = 'active' WHERE status in ('launched','developing');
 
-ALTER TABLE `zt_approvalnode` ADD `forwardTo` char(30) NOT NULL DEFAULT '' AFTER `extra`;
+ALTER TABLE `zt_approvalnode` ADD `forwardBy` char(30) NOT NULL DEFAULT '' AFTER `extra`;
 ALTER TABLE `zt_approvalnode` ADD `revertTo` char(30) NOT NULL DEFAULT '' AFTER `extra`;
