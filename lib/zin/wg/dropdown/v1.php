89047c8d3b0d1c99aa1a402238802db303568ff1
sunhao <sunhao@easycorp.ltd>
2024-07-15 09:59:21 +0800

    * [refac] merge if statement with the enclosing one.

--- lib/zin/wg/dropdown/v1.php
+++ lib/zin/wg/dropdown/v1.php
@@ -113,10 +113,7 @@ class dropdown extends wg
     {
         $menu = $this->block('menu');
 
-        if(!$menu)
-        {
-            if($this->prop('staticMenu')) $menu = $this->buildStaticMenu();
-        }
+        if(!$menu && $this->prop('staticMenu')) $menu = $this->buildStaticMenu();
 
         if(is_array($menu) && $menu) $menu = $menu[0];
 
