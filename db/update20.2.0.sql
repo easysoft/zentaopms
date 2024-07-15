af119593d9da89f84bee819d79acbe42931bb8b3
tanghucheng <tanghucheng@easycorp.ltd>
2024-07-15 16:42:57 +0800

    * [task#122693,done,1h,0h] When the parent OR demand changes, the direct subordinate demand needs to confirm the parent demand change.

--- db/update20.2.0.sql
+++ db/update20.2.0.sql
@@ -1,5 +1,8 @@
 UPDATE zt_demand SET status = 'active' WHERE status in ('launched','distributed','pass');
 UPDATE zt_story  SET status = 'active' WHERE status in ('launched','developing');
 
+ALTER TABLE `zt_demand` ADD `parentVersion` smallint NOT NULL DEFAULT '0' AFTER `version`;
+UPDATE `zt_demand` AS t1 JOIN `zt_demand` AS t2 ON t1.parent = t2.id SET t1.parentVersion = t2.version WHERE t1.parent > 0;
+
 ALTER TABLE `zt_approvalnode` ADD `forwardBy` char(30) NOT NULL DEFAULT '' AFTER `extra`;
 ALTER TABLE `zt_approvalnode` ADD `revertTo` char(30) NOT NULL DEFAULT '' AFTER `extra`;
