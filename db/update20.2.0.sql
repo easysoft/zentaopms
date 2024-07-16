1bdb6b279bec7ab8a6b55513038cb04fae26db62
sunguangming <sunguangming@easycorp.ltd>
2024-07-16 05:13:33 +0000

    * [task#122765,doing,0.2h] remove conditions in approval.

--- db/update20.2.0.sql
+++ db/update20.2.0.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `zt_approvalnode` ADD `forwardBy` char(30) NOT NULL DEFAULT '' AFTER `extra`;
 ALTER TABLE `zt_approvalnode` ADD `revertTo` char(30) NOT NULL DEFAULT '' AFTER `extra`;
+
+UPDATE `zt_workflowaction` SET conditions = '' WHERE `action` IN ('approvalreview','approvalcancel','approvalsubmit');
