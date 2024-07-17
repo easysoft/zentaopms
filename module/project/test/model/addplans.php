e3c14db9ce3bf3a94755a561b522041a91024eb5
liumengyi <liumengyi@easycorp.ltd>
2024-07-17 10:53:54 +0800

    * [misc] modify unit test.

--- module/project/test/model/addplans.php
+++ module/project/test/model/addplans.php
@@ -6,7 +6,9 @@ include dirname(__FILE__, 2) . '/lib/project.unittest.class.php';
 zenData('project')->loadYaml('project')->gen(100);
 zenData('product')->gen(100);
 zenData('productplan')->gen(100);
-zenData('story')->gen(100);
+$storyTable = zenData('story');
+$storyTable->status->range('active');
+$storyTable->gen(100);
 zenData('planstory')->gen(100);
 zenData('projectstory')->gen(0);
 
@@ -24,10 +26,10 @@ $project = new Project();
 
 $plan = array();
 $plan[3] = array(1, 4, 7);
-r(count($project->addPlansTest(13, $plan))) && p('') && e('4'); // 将计划1，4，7下的需求关联到项目13，查看关联后的需求数
-r($project->addPlansTest(13, $plan)) && p('3:story,product') && e('10,3'); // 将计划1，4，7下的需求关联到项目13，查看关联后的需求ID/产品ID
+r(count($project->addPlansTest(13, $plan))) && p('') && e('16'); // 将计划1，4，7下的需求关联到项目13，查看关联后的需求数
+r($project->addPlansTest(13, $plan)) && p('3:story,product') && e('4,1'); // 将计划1，4，7下的需求关联到项目13，查看关联后的需求ID/产品ID
 
 $plan = array();
 $plan[1] = array(2, 5, 10, 13);
-r(count($project->addPlansTest(11, $plan))) && p('') && e('2'); // 将计划2，5，10，13下的需求关联到项目11，查看关联后的需求数
-r($project->addPlansTest(11, $plan)) && p('0:story,product') && e('14,4'); // 将计划2，5，10，13下的需求关联到项目11，查看关联后的需求ID/产品ID
+r(count($project->addPlansTest(11, $plan))) && p('') && e('8'); // 将计划2，5，10，13下的需求关联到项目11，查看关联后的需求数
+r($project->addPlansTest(11, $plan)) && p('0:story,product') && e('13,4'); // 将计划2，5，10，13下的需求关联到项目11，查看关联后的需求ID/产品ID
