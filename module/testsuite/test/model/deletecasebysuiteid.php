9b2ba1327eabad6fd8255aa7606c5e76aeb941ed
liumengyi <liumengyi@easycorp.ltd>
2024-07-17 15:49:54 +0800

    - [test] remove dao::cache.

--- module/testsuite/test/model/deletecasebysuiteid.php
+++ module/testsuite/test/model/deletecasebysuiteid.php
@@ -28,5 +28,4 @@ $testsuite = new testsuiteTest();
 
 r(count($testsuite->getLinkedCasesTest($suiteID)))       && p() && e('2');        //检查当前关联的用例数量
 r($testsuite->deleteCaseBySuiteIDTest($cases, $suiteID)) && p() && e('2');        //测试suiteID值为1
-unset(dao::$cache[TABLE_CASE]);
 r(count($testsuite->getLinkedCasesTest($suiteID)))       && p() && e('0');        //检查当前关联的用例数量
