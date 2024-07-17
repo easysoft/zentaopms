abb3b2700b2be2e745516a4474c0b750db6b0ef7
liumengyi <liumengyi@easycorp.ltd>
2024-07-17 15:52:24 +0800

    * [test] modify unit test.

--- module/testreport/test/model/getresultsummary.php
+++ module/testreport/test/model/getresultsummary.php
@@ -18,7 +18,7 @@ title=测试 testreportModel->getResultSummary();
 timeout=0
 cid=1
 
-- 正常查询 @共有<strong>4</strong>个用例，共执行<strong>4</strong>个用例，产生了<strong>4</strong>个结果，失败的用例有<strong>2</strong>个。
+- 正常查询 @共有<strong>4</strong>个用例，共执行<strong>4</strong>个用例，产生了<strong>4</strong>个结果，失败的用例有<strong>4</strong>个。
 - taskID为空查询 @共有<strong>0</strong>个用例，共执行<strong>0</strong>个用例，产生了<strong>0</strong>个结果，失败的用例有<strong>0</strong>个。
 
 */
@@ -26,5 +26,5 @@ $taskID   = array(1, 0);
 $reportID = 1;
 
 $testreport = new testreportTest();
-r($testreport->getResultSummaryTest($taskID[0], $reportID)) && p() && e('共有<strong>4</strong>个用例，共执行<strong>4</strong>个用例，产生了<strong>4</strong>个结果，失败的用例有<strong>2</strong>个。'); //正常查询
+r($testreport->getResultSummaryTest($taskID[0], $reportID)) && p() && e('共有<strong>4</strong>个用例，共执行<strong>4</strong>个用例，产生了<strong>4</strong>个结果，失败的用例有<strong>4</strong>个。'); //正常查询
 r($testreport->getResultSummaryTest($taskID[1], $reportID)) && p() && e('共有<strong>0</strong>个用例，共执行<strong>0</strong>个用例，产生了<strong>0</strong>个结果，失败的用例有<strong>0</strong>个。'); //taskID为空查询
