52bc1fe36d10f8c1a79141416a468553e79c66a3
luxuyang <luxuyang@easycorp.ltd>
2024-07-19 16:05:25 +0800

    * [task#12323,doing,1h,3h] add create application process.

--- module/space/test/lib/space.ui.class.php
+++ module/space/test/lib/space.ui.class.php
@@ -6,6 +6,13 @@ class space extends tester
     public function createApplication($url = array(), $application = array())
     {
         $this->login();
-        $form = $this->initForm('space', 'createApplication',$url, 'appIframe-devops');
+        $form = $this->initForm('space', 'browse', '', 'appIframe-devops');
+        $form->dom->btn($this->lang->space->install)->click();
+        $form->dom->typeExternal->click();
+        $form->dom->appType->picker($application['appType']);
+        $form->dom->name->setValue($application['name']);
+        $form->dom->url->setValue($application['url']);
+        $form->dom->token->setValue($application['token']);
+        $form->dom->saveButton->click();
     }
 }
