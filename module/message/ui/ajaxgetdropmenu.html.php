3074467b109e43cd6cf1587a15856845819285c8
wangyidong <wangyidong@cnezsoft.com>
2024-07-23 17:20:26 +0800

    * [perf story #59898] Add buildMessageList function for ajaxgetdropmenu.html.php.

--- module/message/ui/ajaxgetdropmenu.html.php
+++ module/message/ui/ajaxgetdropmenu.html.php
@@ -15,17 +15,36 @@ namespace zin;
 jsVar('unreadLangTempate', $lang->message->unread);
 jsVar('noDataLang', $lang->noData);
 
+$buildMessageList = function($messageGroup) use ($lang)
+{
+    if(empty($messageGroup)) return div(setClass('text-gray text-center'), $lang->noData);
+
+    $dateList = array();
+    foreach($messageGroup as $date => $messages)
+    {
+        $dateList[] = h::li(setClass('message-date font-bold'), $date);
+    }
+    return h::ul(setClass('list-unstyled'), $dateList);
+};
+
 tabs
 (
+    setID('messageTabs'),
+    setClass('text-black pt-1 px-5 pb-5'),
+    set::style(array('width' => '400px', 'background-color' => '#fff')),
+    on::click('.delete-message-btn', 'deleteMessage'),
+    on::click('.message-item', 'markRead'),
     tabPane
     (
         set::key('unread-messages'),
         set::title(sprintf($lang->message->unread, $unreadCount)),
         set::active(true),
+        $buildMessageList($unreadMessages)
     ),
     tabPane
     (
         set::key('all-messages'),
         set::title($lang->message->all),
+        $buildMessageList($allMessages)
     )
 );
