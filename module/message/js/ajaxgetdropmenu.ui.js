c6eac8f2ea60812b13462869e6775b2ac4e16706
wangyidong <wangyidong@cnezsoft.com>
2024-07-23 17:40:32 +0800

    * [perf story #59898] Add delete message function.

--- module/message/js/ajaxgetdropmenu.ui.js
+++ module/message/js/ajaxgetdropmenu.ui.js
@@ -12,11 +12,31 @@ window.markRead = function(e)
     $.get($.createLink('message', 'ajaxMarkRead', "id=" + messageID));
 
     /* Rerender unread count. */
-    let $unreadTab  = $this.closest('#unread-messages.tab-pane');
+    let $unreadTab = $this.closest('#unread-messages.tab-pane');
+    if($unreadTab.hasClass('active')) $this.hide();
+    renderUnreadCount();
+}
+
+window.deleteMessage = function(e)
+{
+    let $this = $(e.target);
+    if(!$this.hasClass('message-item')) $this = $this.closest('.message-item');
+
+    let messageID = $this.data("id");
+    let $messageItem = $('#messageTabs .message-item[data-id="' + messageID + '"]');
+    $messageItem.hide();
+    $.get($.createLink('message', 'ajaxDelete', "id=" + messageID));
+
+    /* Rerender unread count. */
+    renderUnreadCount();
+}
+
+window.renderUnreadCount = function()
+{
+    let $unreadTab = $('#messageTabs #unread-messages.tab-pane');
     let unreadCount = $unreadTab.find('.message-item.unread').length;
     let $messageBarDot = $('#messageBar .label-dot.danger');
     $('[href="#unread-messages"] span').html(unreadLangTempate.replace(/%s/, unreadCount));
-    if($unreadTab.hasClass('active')) $this.hide();
     if($messageBarDot.html()) $messageBarDot.html(unreadCount);
     if(unreadCount == 0)
     {
