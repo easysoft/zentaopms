3815aed2d3e5677ec98b24a9998c9615a0b1fe53
lihuifang <lihuifang@easycorp.ltd>
2024-07-24 16:51:09 +0800

    [misc] Rename the ui files 0f edit project.

---
+++ module/project/test/ui/editproject.php
@@ -0,0 +1,25 @@
+#!/usr/bin/env php
+<?php
+
+/**
+
+title=编辑瀑布项目测试
+timeout=0
+cid=73
+
+- 编辑瀑布项目成功  测试结果 @编辑项目成功
+
+*/
+chdir(__DIR__);
+include '../lib/editproject.ui.class.php';
+
+$tester = new editProjectTester();
+$tester->login();
+
+$project = array(
+    array('name' => '编辑项目'.time()),
+);
+
+r($tester->editProject($project['0'])) && p('message') && e('编辑项目成功');    //编辑项目名称
+
+$tester->closeBrowser();
