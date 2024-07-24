c727e6584d2392634e9ff55b465beef82772694b
qixinzhi <qixinzhi@easycorp.ltd>
2024-07-24 13:12:59 +0000

    * [story#59894,doing,0.5h] add view page.

---
+++ module/screen/ui/view.html.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types = 1);
+/**
+ * The browse view file of screen module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Gang Liu <liugang@easycorp.ltd>
+ * @package     screen
+ * @link        https://www.zentao.net
+ */
+
+namespace zin;
