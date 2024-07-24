7180366cccaf9dfac5948890f45a348394e83001
azalea <azalea@easycorp.ltd>
2024-07-24 16:01:12 +0800

    * [uitest] Adjust createuser ui testcase

--- module/user/test/ui/createuser.php
+++ module/user/test/ui/createuser.php
@@ -22,3 +22,10 @@ $user->confirmPassword = '123456';
 $user->realname        = '李娟娟';
 $user->verifyPassword  = $config->uitest->defaultPassword;
 
+r($tester->createNormalUser($user)) && p('message')  && e('创建用户成功');                   // 创建用户后的跳转链接检查
+r($tester->createNormalUser($user)) && p('message')  && e('创建重名用户提示正确');           // 创建用户后的跳转链接检查
+
+r($tester->createEmptyAccountUser($user))  && p('message')  && e('用户名为空提示正确');      // 创建用户后的跳转链接检查
+r($tester->createEmptyPasswordUser($user)) && p('message')  && e('密码为空提示正确');        // 创建用户后的跳转链接检查
+
+$tester->closeBrowser();
