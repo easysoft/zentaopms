bd8c430048c1794c1c3a5093dc272f829b491b06
lijie <lijie@easycorp.ltd>
2024-07-24 16:36:22 +0800

    [misc, 0.2h] add newxpath

--- module/story/test/ui/page/view.php
+++ module/story/test/ui/page/view.php
@@ -8,7 +8,9 @@ class viewPage extends page
         $xpath = array(
             'storyName'       => "//*[@id='mainContent']/div/div[1]/div[1]/span[2]",
             'status'          => "/html/body/div[1]/div/div/div/div[2]/div[2]/div[1]/div[2]/div[1]/div/div[6]/div[2]/span",
+            'storyFrom'            => "/html/body/div[1]/div/div/div/div[2]/div[2]/div[1]/div[2]/div[1]/div/div[4]/div[1]"
             'historyOpenedBy' => "/html/body/div[1]/div/div/div/div[2]/div[1]/div[2]/div/div[2]/ul/li/div/div[2]/div/div/strong",
+            'hisoryEditedBy'  => "/html/body/div[1]/div/div/div/div[2]/div[1]/div[2]/div/div[2]/ul/li[2]/div/div[2]/div[1]/div/strong"
 
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
