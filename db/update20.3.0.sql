5c95c7ef20a5b1cfc0664a0cacbdb6195c59b09e
tanghucheng <tanghucheng@easycorp.ltd>
2024-07-24 16:07:09 +0800

    + [task#123689,done,4h,0h] add productcharterbox and modyfi sql.

--- db/update20.3.0.sql
+++ db/update20.3.0.sql
@@ -3,3 +3,6 @@ ALTER TABLE `zt_approvalnode` CHANGE `date` `date` datetime NULL;
 INSERT INTO `zt_workflowdatasource` (`type`, `name`, `code`, `datasource`, `view`, `keyField`, `valueField`, `buildin`, `vision`) VALUES
 ('lang', '反馈优先级', 'feedbackPri', 'feedbackPri', '', '', '', 1, 'rnd'),
 ('lang', '反馈优先级', 'litefeedbackclosedPri', 'feedbackPri', '', '', '', 1, 'lite');
+
+ALTER TABLE `zt_charter` MODIFY `product` text NULL;
+ALTER TABLE `zt_charter` MODIFY `roadmap` text NULL;
---
+++ lib/zin/wg/productcharterbox/v1.php
@@ -0,0 +1,114 @@
+<?php
+declare(strict_types=1);
+
+namespace zin;
+
+class productCharterBox extends wg
+{
+    /**
+     * Define widget properties.
+     *
+     * @var    array
+     * @access protected
+     */
+    protected static array $defineProps = array(
+        'products?: array',      // 产品下拉列表。
+    );
+
+    public static function getPageCSS(): ?string
+    {
+        return file_get_contents(__DIR__ . DS . 'css' . DS . 'v1.css');
+    }
+
+    public static function getPageJS(): ?string
+    {
+        return file_get_contents(__DIR__ . DS . 'js' . DS . 'v1.js');
+    }
+
+    protected function build()
+    {
+        $productsBox = $this->initProductsBox();
+
+        return div
+        (
+            setClass('productsBox'),
+            on::click('.productsBox .addLine', 'window.addNewLine'),
+            on::click('.productsBox .removeLine', 'window.removeLine'),
+            on::change('.linkProduct .pick-value', 'window.refreshPicker(e.target)'),
+            $productsBox
+        );
+    }
+
+    protected function initProductsBox(): array
+    {
+        global $lang;
+        $products    = $this->prop('products');
+        $productsBox = array();
+
+        $productsBox[] = div
+        (
+            set::className('productBox flex'),
+            formGroup
+            (
+                set::width('1/2'),
+                setClass('distributeProduct'),
+                set::required(true),
+                set::label($lang->charter->product),
+                inputGroup
+                (
+                    div
+                    (
+                        setClass('grow linkProduct w-1/2'),
+                        picker
+                        (
+                            set::name("product[0]"),
+                            set::items($products),
+                        )
+                    )
+                )
+            ),
+            formGroup
+            (
+                set::width('1/2'),
+                set::label($lang->charter->roadmap),
+                set::className('roadmapBox'),
+                set::required(true),
+                inputGroup
+                (
+                    div
+                    (
+                        setClass('grow linkRoadmap w-1/2'),
+                        picker
+                        (
+                            set::name("roadmap[0]"),
+                            set::multiple(true),
+                            set::required(true),
+                            set::items(array())
+                        )
+                    )
+                )
+            ),
+            count($products) ? formGroup
+            (
+                set::label(''),
+                set::className('actionsBox'),
+                div
+                (
+                    setClass('pl-2 flex self-center line-btn'),
+                    btn
+                    (
+                        setClass('btn btn-link text-gray addLine'),
+                        icon('plus')
+                    ),
+                    btn
+                    (
+                        setClass('btn btn-link text-gray hidden removeLine'),
+                        icon('trash')
+                    )
+                )
+            ) : null
+        );
+
+        return $productsBox;
+    }
+}
