27f32b1219e7d4105a71d942463a154daa73438c
azalea <azalea@easycorp.ltd>
2024-07-24 15:45:59 +0800

    * [uitest] Add note for editUser

--- module/user/test/lib/edituser.ui.class.php
+++ module/user/test/lib/edituser.ui.class.php
@@ -2,6 +2,13 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class editUserTester extends tester
 {
+    /**
+     * Check the page jump after edited a user.
+     *
+     * @param  string    $user
+     * @access public
+     * @return object
+     */
     public function checkLocating($user)
     {
         $form = $this->initForm('user', 'edit', array('userID' => '2', 'from' => 'company'), 'appIframe-admin');
