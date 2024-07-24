88fc82d73cb4d6f69839b6cc41271a0c8bd2c0fd
azalea <azalea@easycorp.ltd>
2024-07-24 15:19:35 +0800

    * [perf] Adjust createNormalUser ui test for create user.

--- module/user/test/lib/createuser.ui.class.php
+++ module/user/test/lib/createuser.ui.class.php
@@ -2,6 +2,13 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class createUserTester extends tester
 {
+    /**
+     * Check the page jump after created a normal user.
+     *
+     * @param  string    $user
+     * @access public
+     * @return object
+     */
     public function createNormalUser($user)
     {
         $form = $this->initForm('user', 'create', array(), 'appIframe-admin');
@@ -12,6 +19,12 @@ class createUserTester extends tester
         $form->dom->verifyPassword->setValue($user->verifyPassword);
         $form->dom->btn($this->lang->save)->click();
         $form->wait(1);
+
+        if($this->response('method') != 'browse')
+        {
+            if($this->checkFormTips('user')) return $this->success('创建重名用户提示正确');
+            return $this->failed('创建重名用户提示错误');
+        }
 
         return $this->success('创建用户成功');
     }
