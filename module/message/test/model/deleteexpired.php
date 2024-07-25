408f7f107a3e5eda22e616a656118b12363415c2
wangyidong <wangyidong@cnezsoft.com>
2024-07-25 14:01:06 +0800

    * [unittest] add check logic for deleteexpired unit test.

--- module/message/test/model/deleteexpired.php
+++ module/message/test/model/deleteexpired.php
@@ -10,3 +10,20 @@ $notify->status->range('wait,sended,read');
 $notify->gen(10);
 
 zenData('user')->gen(1);
+
+global $tester;
+$tester->loadModel('message');
+$tester->message->app->user->account = 'admin';
+
+$createdDate = date('Y-m-d H:i:s', strtotime('-10 day'));
+$tester->message->dao->update(TABLE_NOTIFY)->set('createdDate')->eq($createdDate)->exec();
+
+$tester->message->config->message->browser->maxDays = 10;
+$tester->message->deleteExpired();
+$messages = $tester->message->dao->select('*')->from(TABLE_NOTIFY)->fetchAll();
+r(count($messages)) && p() && e('10'); // 过期时间为10，检查剩余条目数
+
+$tester->message->config->message->browser->maxDays = 5;
+$tester->message->deleteExpired();
+$messages = $tester->message->dao->select('*')->from(TABLE_NOTIFY)->fetchAll();
+r(count($messages)) && p() && e('5'); // 过期时间为5，检查剩余条目数
