e19c427ef3acd66031fadba00b01d8c50279fb64
wangyidong <wangyidong@cnezsoft.com>
2024-07-25 13:59:27 +0800

    * [unittest] add envisage for getunreadcount unit test.

--- module/message/test/model/getunreadcount.php
+++ module/message/test/model/getunreadcount.php
@@ -3,6 +3,15 @@
 declare(strict_types=1);
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 
+/**
+
+title=测试 messageModel->getUnreadCount();
+cid=0
+
+- 测试获取status不为 read 的条目数 @3
+
+*/
+
 $notify = zenData('notify');
 $notify->objectType->range('message');
 $notify->toList->range('`,admin,`,`,user1,`');
