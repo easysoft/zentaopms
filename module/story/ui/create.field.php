8a5bc0997f70de562fe0f329f246d65785dcbcd2
wangyuting <wangyuting@easycorp.ltd>
2024-07-29 09:46:05 +0800

    * [misc] Adjust or code to zentaoipd.

--- module/story/ui/create.field.php
+++ module/story/ui/create.field.php
@@ -67,21 +67,6 @@ if(isset($createFields['URS']))
         ->items($createFields['parent']['options'])
         ->value($createFields['parent']['default']);
 }
-if(isset($createFields['duration']))
-{
-    $fields->field('duration')
-        ->required($createFields['duration']['required'])
-        ->items($createFields['duration']['options'])
-        ->value($createFields['duration']['default']);
-}
-
-if(isset($createFields['BSA']))
-{
-    $fields->field('BSA')
-        ->required($createFields['BSA']['required'])
-        ->items($createFields['BSA']['options'])
-        ->value($createFields['BSA']['default']);
-}
 
 $fields->field('reviewer')
     ->width('full')
