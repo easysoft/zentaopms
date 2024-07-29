a96fdcca8facfd8c2e5cd002112985d324c814e7
lijie <lijie@easycorp.ltd>
2024-07-29 13:47:21 +0800

    [misc, 0.1h] Correct code format.

--- module/release/test/ui/page/create.php
+++ module/release/test/ui/page/create.php
@@ -1,12 +1,4 @@
 <?php
 class createPage extends page
 {
-    public function __construct($webdriver)
-    {
-        parent::__construct($webdriver);
-
-        $xpath = array(
-        );
-        $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
-    }
 }
