17f37040f3cbf9c1e1248b90fc20ec7c95ee670b
sunguangming <sunguangming@easycorp.ltd>
2024-07-30 07:26:17 +0000

    * [bug#53277,done,0.1h] fix bug.

--- module/product/model.php
+++ module/product/model.php
@@ -2017,6 +2017,7 @@ class productModel extends model
         if($module == 'testtask'   && $method == 'unitcases')   return helper::createLink($module, 'browseUnits', "productID=%s&browseType=newest&orderBy=id_desc&recTotal=0&recPerPage=0&pageID=1" . ($this->app->tab == 'project' ? "&projectID={$this->session->project}" : ''));
 
         if(($module == 'story' && in_array($method, array('create', 'batchcreate'))) || ($module == 'product' && $method == 'browse'))  return helper::createLink('product', 'browse', "productID=%s&branch={$branchID}&browseType=" . ($this->config->vision == 'or' ? 'assignedtome' : 'unclosed') . "&param=0&storyType={$params[0]}");
+        if((in_array($module, array('story', 'epic', 'requirement')) && in_array($method, array('create', 'edit', 'batchcreate'))) || ($module == 'product' && $method == 'browse'))  return helper::createLink('product', 'browse', "productID=%s&branch={$branchID}&browseType=" . ($this->config->vision == 'or' ? 'assignedtome' : 'unclosed') . "&param=0&storyType=$module");
         if($module == 'story' && $method == 'view') return helper::createLink('product', 'browse', "productID=%s&branch={$branchID}");
 
         if($module == 'execution'  && in_array($method, array('bug', 'testcase')))        return helper::createLink($module,    $method,  "executionID={$params[0]}&productID=%s{$branchParam}");
