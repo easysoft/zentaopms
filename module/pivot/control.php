876fd6e13da7bad56b634fe4eb14139ed865dd52
daitingting <daitingting@xirangit.com>
2024-08-01 02:33:08 +0000

    * [bug] Just do not set value.

--- module/pivot/control.php
+++ module/pivot/control.php
@@ -90,8 +90,7 @@ class pivot extends control
         $mergeConditions = array();
         foreach($drill->condition as $index => $condition)
         {
-            if(trim($conditions[$index]) === '') continue;
-            $condition['value'] = $conditions[$index];
+            if(isset($conditions[$index]) && $conditions[$index]) $condition['value'] = $conditions[$index];
             $mergeConditions[] = $condition;
         }
 
