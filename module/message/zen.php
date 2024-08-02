586419c795f7c2f7e12b04732c2eb5782d5c7209
wangyidong <wangyidong@cnezsoft.com>
2024-08-02 12:52:48 +0800

    * [perf] Init message zen.php file.

---
+++ module/message/zen.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+/**
+ * The zen file of message module of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(http://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Yidong Wang <yidong@easysoft.ltd>
+ * @package     message
+ * @link        https://www.zentao.net
+ */
+class messageZen extends message
+{
+}
