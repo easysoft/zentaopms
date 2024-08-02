f1f42c3696419e80aeca3ff8711803f06a138d48
wangyidong <wangyidong@cnezsoft.com>
2024-08-02 13:40:21 +0800

    * [perf] Add updateAllDot js function for ajaxgetdropmenuforold

--- module/message/view/ajaxgetdropmenuforold.html.php
+++ module/message/view/ajaxgetdropmenuforold.html.php
@@ -275,4 +275,22 @@ window.renderMessage = function()
 
     updateAllDot(showCount);
 };
+
+window.updateAllDot = function(showCount)
+{
+    showCount      = showCount != '0';
+    let dotStyle   = 'padding: 2px;';
+    let rightStyle = showCount ? 'right: -10px;' : 'right: -2px;';
+    if(!showCount) dotStyle += 'width: 5px; height: 5px;';
+
+    let $unreadTab  = $('#messageTabs #unread-messages.tab-pane');
+    let unreadCount = $unreadTab.find('.message-item.unread').length;
+    if(unreadCount < 10 && showCount) rightStyle = 'right: -5px;';
+    if(unreadCount > 99) unreadCount = '99+';
+
+    dotStyle += showCount ? 'top: -3px; aspect-ratio: 0;' : 'top: -2px; aspect-ratio: 1 / 1;';
+    dotStyle += rightStyle;
+
+    let dotHtml = '<span class="label danger label-dot absolute' + (showCount ? ' rounded-sm' : '') + '" style="' + dotStyle + '">' + (showCount ? unreadCount : '') + '</span>';
+};
 </script>
