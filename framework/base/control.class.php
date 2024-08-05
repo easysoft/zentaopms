c31884b2ae44e10660f3f271cc2f17ec8d0bc776
wangyidong <wangyidong@cnezsoft.com>
2024-08-05 16:09:44 +0800

    * [refac bug #53548] Check referer for 18.x

--- framework/base/control.class.php
+++ framework/base/control.class.php
@@ -1072,6 +1072,7 @@ class baseControl
         {
             if(!empty($data['message'])) echo js::alert($data['message']);
             $locate = isset($data['locate']) ? $data['locate'] : (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '');
+            if($locate && strpos($locate, '(') !== false) $locate = $this->config->webRoot;
             if(!empty($locate)) die(js::locate($locate));
             die(isset($data['message']) ? $data['message'] : 'success');
         }
@@ -1084,6 +1085,7 @@ class baseControl
                 {
                     echo js::alert($data['message']);
                     $locate = isset($data['locate']) ? $data['locate'] : (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '');
+                    if($locate && strpos($locate, '(') !== false) $locate = $this->config->webRoot;
                     if (!empty($locate)) die(js::locate($locate));
                     die(isset($data['message']) ? $data['message'] : 'fail');
                 }
