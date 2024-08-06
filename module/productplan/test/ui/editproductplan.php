c416beb5fd34f93b20fa6bd31b656d64d0fd144c
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-08-06 02:06:29 +0000

    *[task#124934,doing,2h,8h] Add ui test files of edit productplan

---
+++ module/productplan/test/ui/editproductplan.php
@@ -0,0 +1,35 @@
+#!/usr/bin/env php
+<?php
+
+chdir(__DIR__);
+include '../lib/editplan.ui.class.php';
+
+zendata('product')->loadYaml('product', false, 2)->gen(10);
+zendata('productplan')->loadYaml('productplan', false, 2)->gen(10);
+$tester = new editPlanTester();
+$tester->login();
+
+$planurl['planID'] = 6;
+
+$productplan        = new stdClass();
+$waitplan           = new stdClass();
+$productplan->title = '';
+r($tester->editDefault($productplan, $planurl)) && p('message,status') && e('编辑计划提示信息正确,SUCCESS'); // 计划名称必填校验
+
+$productplan->title = '计划_编辑后';
+$productplan->begin = '2024-06-24';
+$productplan->end   = '2024-12-30';
+r($tester->editDefault($productplan, $planurl)) && p('message,status') && e('编辑计划成功,SUCCESS'); //编辑计划
+
+
+$productplan->begin = '2024-06-24';
+$productplan->end   = '2024-06-20';
+r($tester->editDefault($productplan, $planurl)) && p('message,status') && e('日期校验正确,SUCCESS'); // 校验结束日期不小于开始日期
+
+
+$waitplan->title  = '一个待定的计划';
+$waitplan->begin  = '2024-06-24';
+$waitplan->future = 'future';
+r($tester->editDefault($waitplan, $planurl)) && p('message,status') && e('编辑待定计划成功,SUCCESS'); // 将计划结束日期改为待定
+
+$tester->closeBrowser();
---
+++ module/productplan/test/ui/page/edit.php
@@ -0,0 +1,4 @@
+<?php
+class editPage extends page
+{
+}
