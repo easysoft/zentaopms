6ca1735ae282acab8f140744500c3a7bfd9150a4
liyang <liyang@easycorp.ltd>
2024-08-07 09:10:13 +0800

    + [task#125044] Get iframe height on zanode instruction page.

--- module/zanode/js/instruction.ui.js
+++ module/zanode/js/instruction.ui.js
@@ -7,4 +7,19 @@
  */
 window.getIframeHeight = function()
 {
+    var windowHeight   = $(window).height();
+    var headerHeight   = parseInt($('#header').height());
+    var mainNavbar     = parseInt($('#mainNavbar').height());
+    var featureBar     = parseInt($('#featureBar').height());
+    var mainMenuHeight = parseInt($('#mainMenu').css('padding-top')) + parseInt($('#mainMenu').css('padding-bottom'));
+    var appTabsHeight  = parseInt($('#appTabs').height());
+    var appsBarHeight  = parseInt($('#appsBar').height());
+    featureBar         = featureBar ? featureBar : 0;
+    appsBarHeight      = appsBarHeight ? appsBarHeight : 0;
+    appTabsHeight      = appTabsHeight ? appTabsHeight : 0;
+    mainMenuHeight     = mainMenuHeight ? mainMenuHeight : 0;
+    mainNavbar         = mainNavbar ? mainNavbar : 0;
+    iframeHeight       = windowHeight - headerHeight - appsBarHeight - appTabsHeight - mainMenuHeight - mainNavbar - 20 - featureBar;
+
+    $('#iframe-instruction').height(iframeHeight);
 }
