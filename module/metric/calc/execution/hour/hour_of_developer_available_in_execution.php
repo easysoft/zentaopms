27567f3f4bde94e4d291f785a421190b356c8b50
zhouxin <zhouxin@easycorp.ltd>
2024-08-07 14:18:12 +0800

    * [task#124849,doing,0.2h] add metric file.

---
+++ module/metric/calc/execution/hour/hour_of_developer_available_in_execution.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 按执行统计的开发人员可用工时
+ * Hour of developer available in execution
+ *
+ * 范围：execution
+ * 对象：user
+ * 目的：hour
+ * 度量名称：按执行统计的开发人员可用工时
+ * 单位：小时
+ * 描述：按执行统计的开发人员可用工时是指执行团队中角色为研发的可用工时之和。该度量项反映了团队中开发人员能够投入在本迭代的时间，有助于计算执行团队的工作负载。
+ * 定义：执行团队成员每日可用工时*可用工日，人员职位为研发，过滤已删除的执行，过滤已删除的项目。
+ *
+ * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @author    zhouxin <zhouxin@chandao.com>
+ * @package
+ * @uses      func
+ * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @Link      https://www.zentao.net
+ */
