8f639db200b7dcd8b7271601061214a086e5e9a4
zhouxin <zhouxin@easycorp.ltd>
2024-08-07 13:31:45 +0800

    * [task#124865,doing,0.1h] add metric config.

--- module/bi/config/metrics.php
+++ module/bi/config/metrics.php
@@ -4270,3 +4270,17 @@ $config->bi->builtin->metrics[] = array
     'desc'       => '按执行统计的开发任务消耗工时数是指任务类型为开发时已经消耗的工时总和，该度量项反映了开发任务的资源使用情况，可以帮助团队掌握执行的开发成本。',
     'definition' => "执行中满足以下条件的任务消耗工时数求和\n任务类型为开发\n过滤已删除的任务\n过滤父任务\n过滤已删除的执行\n过滤已删除的项目"
 );
+
+$config->bi->builtin->metrics[] = array
+(
+    'name'       => '按执行统计的来源Bug的任务消耗工时数',
+    'alias'      => '来源Bug的任务消耗工时数',
+    'code'       => 'consume_of_frombug_task_in_execution',
+    'purpose'    => 'hour',
+    'scope'      => 'execution',
+    'object'     => 'task',
+    'unit'       => 'hour',
+    'dateType'   => 'nodate',
+    'desc'       => '按执行统计的来源Bug的任务消耗工时数是指执行中Bug转任务消耗的工时总和。该度量项反映了任务来源为Bug的资源使用情况，可以帮助团队识别缺陷管理中存在的问题。',
+    'definition' => "执行中满足以下条件的任务消耗工时数求和\n任务来源为Bug\n过滤已删除的任务\n过滤父任务\n过滤已删除的执行\n过滤已删除的项目"
+);
