1b33a9a5b8d631a1680f0d310fc4bb1b259dd296
liyang <liyang@easycorp.ltd>
2024-08-07 11:08:23 +0800

    * [task#125044 start 0.5h, 0.5h] Adjust instruction page.

--- module/zanode/ui/instruction.html.php
+++ module/zanode/ui/instruction.html.php
@@ -1,11 +1,11 @@
 <?php
 declare(strict_types=1);
 /**
- * The yyy view file of xxx module of ZenTaoPMS.
+ * The instruction view file of zanode module of ZenTaoPMS.
  * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
  * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
  * @author      Yang Li <liyang@chandao.com>
- * @package     xxx
+ * @package     zanode
  * @link        https://www.zentao.net
  */
 namespace zin;
