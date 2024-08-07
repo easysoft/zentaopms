44132ec0f28a07c6cbc687f4f161c51214ff00cb
songchenxuan <songchenxuan@easycorp.ltd>
2024-08-07 10:02:06 +0800

    + [task#124875,done,1h].

--- module/bi/lang/en.php
+++ module/bi/lang/en.php
@@ -1,3 +1,7 @@
 <?php
 $lang->bi->binNotExists        = 'The DuckDB binary does not exist.';
 $lang->bi->tmpPermissionDenied = 'The DuckDB tmp directory has no permissions, you need to change the permissions for the directory "%s". The <br /> command is: <br />chmod 777 -R %s.';
+
+$lang->bi->driver = 'Driver';
+$lang->bi->driverList = array();
+$lang->bi->driverList['mysql'] = 'MySQL';
--- module/bi/lang/zh-cn.php
+++ module/bi/lang/zh-cn.php
@@ -1,3 +1,7 @@
 <?php
 $lang->bi->binNotExists        = 'DuckDB 二进制文件不存在。';
 $lang->bi->tmpPermissionDenied = 'DuckDB tmp 目录没有权限, 需要修改目录 "%s" 的权限。<br />命令为：<br />chmod 755 -R %s。';
+
+$lang->bi->driver = '数据库类型';
+$lang->bi->driverList = array();
+$lang->bi->driverList['mysql'] = 'MySQL';
