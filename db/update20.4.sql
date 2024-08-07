d5cc4b62558cb8f6ea2678e8fb7f16c72f12a25d
caoyanyi <caoyanyi@easycorp.ltd>
2024-08-07 16:02:10 +0800

    * [misc] merge update sql.

--- db/update20.4.sql
+++ db/update20.4.sql
@@ -1,1 +1,10 @@
+ALTER TABLE `zt_doclib` ADD `parent` mediumint(8) NOT NULL DEFAULT '0' AFTER `vision`;
+
+CREATE TABLE IF NOT EXISTS `zt_duckdbqueue` (
+  `object` varchar(255) NOT NULL DEFAULT '',
+  `updatedTime` datetime NULL,
+  `syncTime` datetime NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE UNIQUE INDEX `object` ON `zt_duckdbqueue`(`object`);
+
 ALTER TABLE `zt_metriclib` ADD COLUMN `repo` char(30) NOT NULL DEFAULT '';
