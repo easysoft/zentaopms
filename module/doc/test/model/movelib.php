7ba0e9cb7b0d80e28d41345a950c1f6a35114407
wangyidong <wangyidong@cnezsoft.com>
2024-08-08 15:57:27 +0800

    + [pref story #49397] Add test logic for unit test file for moveLib method.

--- module/doc/test/model/movelib.php
+++ module/doc/test/model/movelib.php
@@ -27,3 +27,19 @@ $errorData  = new stdclass();
 $customData->space = 7;
 $mineData->space   = 'mine';
 $errorData->space  = 'project';
+
+/* Empty module object. */
+r((int)$tester->doc->moveLib(0, clone $emptyData))  && p() && e('0'); // 数据为空，libID=0时，检查返回结果
+r((int)$tester->doc->moveLib(1, clone $emptyData))  && p() && e('0'); // 数据为空，libID=1时，检查返回结果
+r((int)$tester->doc->moveLib(0, clone $customData)) && p() && e('0'); // 数据不为空，libID=0时，检查返回结果
+
+/* Parent module object. */
+r((int)$tester->doc->moveLib($mineLibID, clone $errorData))  && p() && e('0'); // 传入错误数据，检查返回结果
+r((int)$tester->doc->moveLib($mineLibID, clone $mineData))   && p() && e('0'); // 未修改文档空间，检查返回结果
+r((int)$tester->doc->moveLib($mineLibID, clone $customData)) && p() && e('1'); // 传入正确数据，检查返回结果
+r($tester->doc->getLibByID($mineLibID)) && p('type,parent') && e('custom,7');  // 检查移动后的文档数据
+
+r((int)$tester->doc->moveLib($customLibID, clone $customData)) && p() && e('1'); // 传入移动到团队空间的数据，检查返回结果
+r($tester->doc->getLibByID($customLibID)) && p('type,parent') && e('custom,7');  // 检查移动后的文档数据
+r((int)$tester->doc->moveLib($customLibID, clone $mineData)) && p() && e('1');   // 传入移动到我的空间的数据，检查返回结果
+r($tester->doc->getLibByID($customLibID)) && p('type,parent') && e('mine,0');    // 检查移动后的文档数据
