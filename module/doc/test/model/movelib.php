3d79a4634d52201d13109aeb12c90a27a78a8f3c
wangyidong <wangyidong@cnezsoft.com>
2024-08-08 15:57:54 +0800

    + [pref story #49397] Add expect for unit test file for moveLib method.

--- module/doc/test/model/movelib.php
+++ module/doc/test/model/movelib.php
@@ -7,6 +7,24 @@ title=测试 docModel->moveLib();
 timeout=0
 cid=0
 
+- 数据为空，libID=0时，检查返回结果 @0
+- 数据为空，libID=1时，检查返回结果 @0
+- 数据不为空，libID=0时，检查返回结果 @0
+- 传入错误数据，检查返回结果 @0
+- 未修改文档空间，检查返回结果 @0
+- 传入正确数据，检查返回结果 @1
+- 检查移动后的文档数据
+ - 属性type @custom
+ - 属性parent @7
+- 传入移动到团队空间的数据，检查返回结果 @1
+- 检查移动后的文档数据
+ - 属性type @custom
+ - 属性parent @7
+- 传入移动到我的空间的数据，检查返回结果 @1
+- 检查移动后的文档数据
+ - 属性type @mine
+ - 属性parent @0
+
 */
 
 include dirname(__FILE__, 5) . '/test/lib/init.php';
