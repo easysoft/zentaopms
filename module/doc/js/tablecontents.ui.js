cf8fa10125d0b3714832b3615aa8da02dbcea10f
sunguangming <sunguangming@easycorp.ltd>
2024-08-08 05:22:38 +0000

    * [task#124813,doing,1h] add sort js func.

--- module/doc/js/tablecontents.ui.js
+++ module/doc/js/tablecontents.ui.js
@@ -6,3 +6,14 @@ $(document).off('click', '#actionBar #' + exportMethod).on('click', '#actionBar 
     const checkedList = dtable.$.getChecks();
     $.cookie.set('checkedItem', checkedList, {expires:config.cookieLife, path:config.webRoot});
 });
+
+window.onSortEnd = function(from, to, type)
+{
+    if(!from || !to) return false;
+
+    const url  = $.createLink('doc', 'sortDoc');
+    const form = new FormData();
+    form.append('orders', JSON.stringify(this.state.rowOrders));
+    $.ajaxSubmit({url, data: form});
+    return true;
+}
