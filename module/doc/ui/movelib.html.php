4712f30bbc30f3cce78c0832b564394649473f17
wangyidong <wangyidong@cnezsoft.com>
2024-08-08 15:15:29 +0800

    + [pref story #49397] Init movelib.html.php.

---
+++ module/doc/ui/movelib.html.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+/**
+ * The moveLib view file of doc module of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2015 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     ZPL (http://zpl.pub/page/zplv12.html)
+ * @author      Yidong Wang <yidong@chandao.com>
+ * @package     doc
+ * @version     $Id$
+ * @link        http://www.zentao.net
+ */
+namespace zin;
