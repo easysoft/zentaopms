194d072460e304b5c9d2661b741691d2325b417b
wangyidong <wangyidong@cnezsoft.com>
2024-08-08 15:57:01 +0800

    + [pref story #49397] Buile test data for unit test file for moveLib method.

--- module/doc/test/model/movelib.php
+++ module/doc/test/model/movelib.php
@@ -12,3 +12,18 @@ cid=0
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 
 zenData('doclib')->loadYaml('doclib')->gen(30);
+
+global $tester;
+$tester->loadModel('doc');
+
+$mineLibID   = 11;
+$customLibID = 6;
+
+$emptyData  = new stdclass();
+$customData = new stdclass();
+$mineData   = new stdclass();
+$errorData  = new stdclass();
+
+$customData->space = 7;
+$mineData->space   = 'mine';
+$errorData->space  = 'project';
