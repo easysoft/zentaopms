e56d6b778b824814669d32689fd711ba70cb27a5
qixinzhi <qixinzhi@easycorp.ltd>
2024-08-09 03:13:11 +0000

    * [bug#53110,0.5h] fix modal mask style.

--- module/screen/js/view.ui.js
+++ module/screen/js/view.ui.js
@@ -2,5 +2,5 @@ window.drillModalApi = function(pivotID, originField, conditions, filterValues, 
 {
     let drillModalLink = $.createLink('pivot', 'drillModal', `pivotID=${pivotID}&colName=${originField}&status=published&drillFields=${conditions}&filterValues=${filterValues}&value=${value}`);
     drillModalLink = drillModalLink.replace(/\+/g, '%2B');
-    zui.Modal.open({url: drillModalLink, size: 'lg', className: 'dark'});
+    zui.Modal.open({url: drillModalLink, size: 'lg', className: 'dark bg-gray-200 bg-opacity-50'});
 };
