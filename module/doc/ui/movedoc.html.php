4e1f8806a46a1b9982cff99f0cbb6ebdb8346042
wangyidong <wangyidong@cnezsoft.com>
2024-08-09 15:07:04 +0800

    * [perf story #49405] Init movedoc ui page.

---
+++ module/doc/ui/movedoc.html.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+/**
+ * The moveDoc view file of doc module of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2015 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     ZPL (http://zpl.pub/page/zplv12.html)
+ * @author      Yidong Wang <yidong@chandao.com>
+ * @package     doc
+ * @version     $Id$
+ * @link        http://www.zentao.net
+ */
+namespace zin;
+
+modalHeader(set::title($lang->doc->moveDocAction));
