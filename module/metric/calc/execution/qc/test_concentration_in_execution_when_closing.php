4d40fd6266cce551eefc257916bd2951e7f91538
daitingting <daitingting@xirangit.com>
2024-08-09 06:48:21 +0000

    * [task#124859,doing,0.1h] Add test_concentration_in_execution_when_closing metric.

---
+++ module/metric/calc/execution/qc/test_concentration_in_execution_when_closing.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 按执行统计的研发需求计划负载。
+ * Story planning load by execution statistics
+ *
+ * 范围：execution
+ * 对象：execution
+ * 目的：qc
+ * 度量名称：按执行统计的研发需求计划负载
+ * 单位：%
+ * 描述：按执行统计的研发需求计划负载是指执行开始时计划的需求规模数与执行开发人员可用工时数的比率。该度量项反映了团队的工作负载，可以帮助团队进行资源调配和需求规划。
+ * 定义：复用：按执行统计的截止执行开始当天研发需求规模数、按执行统计的开发人员可用工时；公式：按执行统计的截止执行开始当天研发需求规模数/按执行统计的开发人员可用工时
+ *
+ * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @author    Daitingting <daitingting@xirangit.com>
+ * @package
+ * @uses      func
+ * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @Link      https://www.zentao.net
+ */
