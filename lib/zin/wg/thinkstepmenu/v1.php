b576326714497c49ffde6a74e8feba075c77cc8d
Zhangyu <zhangyu@easycorp.ltd>
2024-08-12 07:18:46 +0000

    * [bug#53624] modify delet action hint of thinkstepmenu.

--- lib/zin/wg/thinkstepmenu/v1.php
+++ lib/zin/wg/thinkstepmenu/v1.php
@@ -182,9 +182,7 @@ class thinkStepMenu extends wg
                 'icon'           => 'trash',
                 'text'           => $this->lang->thinkstep->actions['delete'],
                 'innerClass'     => 'text-gray opacity-50',
-                'data-toggle'    => 'tooltip',
-                'data-title'     => $this->lang->thinkstep->cannotDeleteNode,
-                'data-placement' => 'right',
+                'hint'           => $this->lang->thinkstep->cannotDeleteNode,
             )) : null
         ), $transitionAction);
 
