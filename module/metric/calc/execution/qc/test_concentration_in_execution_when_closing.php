85cd366fe981c052e34e540a9622e1c8380f845a
daitingting <daitingting@xirangit.com>
2024-08-12 05:58:15 +0000

    * [misc] Adjut note.

--- module/metric/calc/execution/qc/test_concentration_in_execution_when_closing.php
+++ module/metric/calc/execution/qc/test_concentration_in_execution_when_closing.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * 按执行统计的研发需求计划负载。
- * Story planning load by execution statistics
+ * 按执行统计的执行结束时测试缺陷密度。
+ * Test concentration in executionw when the execution closing.
  *
  * 范围：execution
  * 对象：execution
  * 目的：qc
- * 度量名称：按执行统计的研发需求计划负载
+ * 度量名称：按执行统计的执行结束时测试缺陷密度
  * 单位：%
- * 描述：按执行统计的研发需求计划负载是指执行开始时计划的需求规模数与执行开发人员可用工时数的比率。该度量项反映了团队的工作负载，可以帮助团队进行资源调配和需求规划。
- * 定义：复用：按执行统计的截止执行开始当天研发需求规模数、按执行统计的开发人员可用工时；公式：按执行统计的截止执行开始当天研发需求规模数/按执行统计的开发人员可用工时
+ * 描述：按执行统计的执行测试缺陷密度是指执行产生的有效Bug数与执行交付的研发需求数的比率。该度量项反映了团队交付的研发需求的质量，可以帮助团队识别研发中存在的潜在问题。
+ * 定义：复用：按执行统计的执行结束时已交付的研发需求规模数、按执行统计的新增有效Bug数；公式：按执行统计的新增有效Bug数/按执行统计的执行结束时已交付的研发需求规模数
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
  * @author    Daitingting <daitingting@xirangit.com>
