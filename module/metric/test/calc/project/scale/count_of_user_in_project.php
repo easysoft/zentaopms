3ac19adbb9613d931fbe99565e35148bd5ed6f69
qixinzhi <qixinzhi@easycorp.ltd>
2024-08-12 02:02:29 +0000

    * [bug] fix unit test.

--- module/metric/test/calc/project/scale/count_of_user_in_project.php
+++ module/metric/test/calc/project/scale/count_of_user_in_project.php
@@ -17,10 +17,11 @@ include dirname(__FILE__, 4) . '/lib/calc.unittest.class.php';
 zendata('project')->loadYaml('project_close', true, 4)->gen(10);
 zendata('project')->loadYaml('execution', true, 4)->gen(200, false);
 zendata('team')->loadYaml('team', true, 4)->gen(2000);
+zendata('user')->loadYaml('user', true, 4)->gen(41);
 
 $metric = new metricTest();
 $calc   = $metric->calcMetric(__FILE__);
 
 r(count($calc->getResult())) && p('') && e('6'); // 测试分组数。
 
-r($calc->getResult()) && p('0:value') && e('37'); // 测试分组数。
+r($calc->getResult()) && p('0:value') && e('14'); // 测试分组数。
