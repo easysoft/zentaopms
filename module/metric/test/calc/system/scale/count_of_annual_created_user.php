c4b3b6ef5236daa23919c82833bd0d6a55765773
qixinzhi <qixinzhi@easycorp.ltd>
2024-08-12 02:50:01 +0000

    * [bug] fix unit test.

--- module/metric/test/calc/system/scale/count_of_annual_created_user.php
+++ module/metric/test/calc/system/scale/count_of_annual_created_user.php
@@ -7,21 +7,21 @@ title=count_of_annual_created_user
 timeout=0
 cid=1
 
-- 测试年度新增用户分组数。 @10
-- 测试2017年新增用户数第0条的value属性 @36
-- 测试2018年新增用户数第0条的value属性 @28
-- 测试2023年新增用户数第0条的value属性 @0
+- 测试年度新增用户分组数。 @5
+- 测试2011年新增用户数第0条的value属性 @5
+- 测试2013年新增用户数第0条的value属性 @4
+- 测试2015年新增用户数第0条的value属性 @4
 
 */
 include dirname(__FILE__, 7) . '/test/lib/init.php';
 include dirname(__FILE__, 4) . '/lib/calc.unittest.class.php';
 
-zendata('user')->loadYaml('user', true, 4)->gen(1000);
+zendata('user')->loadYaml('user', true, 4)->gen(41);
 
 $metric = new metricTest();
 $calc   = $metric->calcMetric(__FILE__);
 
-r(count($calc->getResult())) && p('') && e('10');                        // 测试年度新增用户分组数。
-r($calc->getResult(array('year' => '2017'))) && p('0:value') && e('36'); // 测试2017年新增用户数
-r($calc->getResult(array('year' => '2018'))) && p('0:value') && e('28'); // 测试2018年新增用户数
-r($calc->getResult(array('year' => '2023'))) && p('0:value') && e('0');  // 测试2023年新增用户数
+r(count($calc->getResult())) && p('') && e('5');                        // 测试年度新增用户分组数。
+r($calc->getResult(array('year' => '2011'))) && p('0:value') && e('5'); // 测试2011年新增用户数
+r($calc->getResult(array('year' => '2013'))) && p('0:value') && e('4'); // 测试2013年新增用户数
+r($calc->getResult(array('year' => '2015'))) && p('0:value') && e('4'); // 测试2015年新增用户数
