a58b40fd94400b33bfb8704262c5f6a1f6a6d89b
luxuyang <luxuyang@easycorp.ltd>
2024-08-12 15:33:56 +0800

    + [task#125557,doing,0.5h.0.5h] add create maintain UI scripts.

---
+++ module/repo/test/ui/createmaintain.php
@@ -0,0 +1,15 @@
+#!/usr/bin/env php
+<?php
+chdir(__DIR__);
+include '../lib/repo.ui.class.php';
+
+$tester = new repo();
+$maintain = array(
+    'SCM'            => 'GitFox',
+    'serviceHost'    => 'GitFox',
+    'serviceProject' => 'php-demo',
+    'product'        => 'devops专属产品',
+    'desc'           => '一句话描述test',
+    'acl'            => 'aclPrivate'
+);
+r($tester->createMaintain($maintain)) && p('message,status') && e('关联GitFox代码库成功,SUCCESS'); //验证关联GitFox代码库
