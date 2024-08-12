4b31f663ea3aeb28d0785aaa5d8f2e1084c13b0d
qixinzhi <qixinzhi@easycorp.ltd>
2024-08-12 02:22:41 +0000

    * [bug] fix unit test.

--- module/metric/test/calc/system/scale/count_of_user.php
+++ module/metric/test/calc/system/scale/count_of_user.php
@@ -17,10 +17,10 @@ include dirname(__FILE__, 4) . '/lib/calc.unittest.class.php';
 $metric = new metricTest();
 $calc   = $metric->calcMetric(__FILE__);
 
-zendata('user')->loadYaml('user', true, 4)->gen(356, true, false);
+zendata('user')->loadYaml('user', true, 4)->gen(10, true, false);
 $calc = $metric->calcMetric(__FILE__);
-r($calc->getResult()) && p('0:value') && e('119'); // 测试356条数据。
+r($calc->getResult()) && p('0:value') && e('5'); // 测试356条数据。
 
-zendata('user')->loadYaml('user', true, 4)->gen(652, true, false);
+zendata('user')->loadYaml('user', true, 4)->gen(20, true, false);
 $calc = $metric->calcMetric(__FILE__);
-r($calc->getResult()) && p('0:value') && e('218'); // 测试652条数据。
+r($calc->getResult()) && p('0:value') && e('10'); // 测试652条数据。
