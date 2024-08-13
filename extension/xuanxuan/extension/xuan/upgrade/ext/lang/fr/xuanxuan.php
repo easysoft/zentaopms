209d2182502dc5cb5ee2885c7ec5412da5f87ea4
Chenjianyu <chenjianyu@easycorp.ltd>
2024-08-13 13:43:15 +0800

    * [task#124649, done, 1h] alter zentao upgrade message for xuanxuan conference.

--- extension/xuanxuan/extension/xuan/upgrade/ext/lang/fr/xuanxuan.php
+++ extension/xuanxuan/extension/xuan/upgrade/ext/lang/fr/xuanxuan.php
@@ -2,3 +2,9 @@
 $lang->upgrade->upgradeXuanxuan = 'Upgrade Xuanxuan';
 $lang->upgrade->archiveChangeNoticeTitle   = 'Chat group archive change notice';
 $lang->upgrade->archiveChangeNoticeContent = 'Due to the version update, the original archive feature has been changed, and your original archive discussion group has been set as a no-disturb message, please go to https://xuanim.com/book/xuanxuankehuduan/205.html for details.';
+
+$lang->upgrade->jitsiConferenceNotice = new stdClass();
+$lang->upgrade->jitsiConferenceNotice->super = 'The audio and video service of ZenDesktop version 9.0 has been replaced. Please deploy Jitsi service in the background and notify system members to upgrade the client to version 9.0. ';
+$lang->upgrade->jitsiConferenceNotice->user  = 'The system has been upgraded to ZenDesktop version 9.0. For your better user experience, please upgrade the client to version 9.0 in time. ';
+$lang->upgrade->jitsiConferenceNotice->setupLink = 'https://www.zentao.net/book/zentaopms/1362.html';
+$lang->upgrade->jitsiConferenceNotice->manualLink = 'https://www.zentao.net/book/zentaopms/1364.html';
