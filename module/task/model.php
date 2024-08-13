ada30f3cf876f132994eb842b7e62feb50036e22
zhaixiaojian <zhaixiaojian@easycorp.ltd>
2024-08-13 09:05:44 +0800

    * [task#125051,done,4h,3h] format code.

--- module/task/model.php
+++ module/task/model.php
@@ -3327,13 +3327,13 @@ class taskModel extends model
      */
     public function getLinkedCommits(int $repoID, array $revisions): array
     {
-        return $this->dao->select('h.revision,t.id AS id,t.name AS name')
-            ->from(TABLE_REPOHISTORY)->alias('h')
-            ->leftJoin(TABLE_RELATION)->alias('r')->on('r.relation="completedin" and r.BType="commit" and r.BID=h.id')
-            ->leftJoin(TABLE_TASK)->alias('t')->on('r.AType="task" and r.AID=t.id')
-            ->where('h.revision')->in($revisions)
-            ->andWhere('h.repo')->eq($repoID)
-            ->andWhere('t.id')->ne('')
+        return $this->dao->select('t1.revision,t3.id AS id,t3.name AS name')
+            ->from(TABLE_REPOHISTORY)->alias('t1')
+            ->leftJoin(TABLE_RELATION)->alias('t2')->on('t2.relation="completedin" and t2.BType="commit" and t2.BID=t1.id')
+            ->leftJoin(TABLE_TASK)->alias('t3')->on('t2.AType="task" and t2.AID=t3.id')
+            ->where('t1.revision')->in($revisions)
+            ->andWhere('t1.repo')->eq($repoID)
+            ->andWhere('t3.id')->ne('')
             ->fetchGroup('revision', 'id');
     }
 }
