4e4364f245b2b56350cbbc5440cbd0cc95a1bc73
Chenjianyu <chenjianyu@easycorp.ltd>
2024-08-15 14:59:44 +0800

    * [task] adjust xuanxuan upgrade language items.

--- extension/xuanxuan/extension/xuan/upgrade/ext/lang/zh-cn/xuanxuan.php
+++ extension/xuanxuan/extension/xuan/upgrade/ext/lang/zh-cn/xuanxuan.php
@@ -4,7 +4,7 @@ $lang->upgrade->archiveChangeNoticeTitle   = '存档功能变更通知';
 $lang->upgrade->archiveChangeNoticeContent = '由于版本更新，原存档功能发生变更，已将您原存档讨论组状态设为消息免打扰，具体变更请移步 https://www.xuanim.com/book/xuanxuankehuduan/205.html ';
 
 $lang->upgrade->jitsiConferenceNotice = new stdClass();
-$lang->upgrade->jitsiConferenceNotice->super = '大桌面9.0版本音视频服务已更换，请在后台部署Jitsi服务，同时通知系统成员将客户端升级至9.0版本。';
-$lang->upgrade->jitsiConferenceNotice->user  = '系统已升级到大桌面9.0版本，为了您更好的使用体验，请及时将客户端升级至9.0版本。';
+$lang->upgrade->jitsiConferenceNotice->super = '大桌面自9.0版本起已更换音视频服务，请在后台部署Jitsi服务，并通知系统成员及时将大桌面升级至9.0或更高版本。';
+$lang->upgrade->jitsiConferenceNotice->user  = '系统已升级至大桌面9.0或更高版本，为了您更好的使用体验，请及时将大桌面更新至相应版本。';
 $lang->upgrade->jitsiConferenceNotice->setupLink = 'https://www.zentao.net/book/zentaopms/1362.html';
 $lang->upgrade->jitsiConferenceNotice->manualLink = 'https://www.zentao.net/book/zentaopms/1364.html';
