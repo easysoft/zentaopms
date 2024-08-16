57de8a724f020a3e2503179dc5ffba1914474930
wangyuting <wangyuting@easycorp.ltd>
2024-08-16 10:42:47 +0800

    * [bug#54074,done,1h,0h] Fix change product error for create ticker page.

--- extension/lite/ticket/ext/view/create.lite.html.hook.php
+++ extension/lite/ticket/ext/view/create.lite.html.hook.php
@@ -9,12 +9,15 @@
  */
 function loadModules(productID)
 {
-    link = createLink('tree', 'ajaxGetOptionMenu', 'productID=' + productID + '&viewtype=ticket&branch=all&rootModuleID=0&returnType=html&fieldID=&needManage=false');
-    $('#moduleBox').load(link, function(data)
+    link = createLink('tree', 'ajaxGetOptionMenu', 'productID=' + productID + '&viewtype=ticket&branch=all&rootModuleID=0&returnType=json&fieldID=&needManage=false');
+    $.getJSON(link, function(data)
     {
-        var $inputGroup = $(this);
-        $inputGroup.find('select').chosen()
-        $inputGroup.prepend("<span class='input-group-addon'>" + moduleLang + "</span>");
-    });
+        $('#module').empty();
+        $.each(data, function(key, value)
+        {
+            $('#module').append('<option value=' + key + ' title="' + value + '">' + value + '</option>');
+        });
+        $('#module').trigger('chosen:updated');
+    })
 }
 </script>
