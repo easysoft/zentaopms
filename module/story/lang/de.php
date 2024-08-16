14d9ad92e9d715c401e228f58ee9ad476108433d
daitingting <daitingting@xirangit.com>
2024-08-16 06:35:42 +0000

    * [misc] Add language.

--- module/story/lang/de.php
+++ module/story/lang/de.php
@@ -223,6 +223,7 @@ $lang->story->linkMR           = 'Related MRs';
 $lang->story->linkCommit       = 'Related Commits';
 $lang->story->URS              = 'User requirements';
 $lang->story->estimateUnit     = "(Unit: {$lang->story->hour})";
+$lang->story->verifiedDate     = 'Verified Date';
 
 $lang->story->ditto       = 'Dito';
 $lang->story->dittoNotice = "This story is not linked to the same {$lang->productCommon} as the last one is!";
