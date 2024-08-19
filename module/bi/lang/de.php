77648a4cf50ab638b4c3a58614f42ddc1934d859
qixinzhi <qixinzhi@easycorp.ltd>
2024-08-19 08:40:51 +0000

    * [story#63284,0.5h] add lang.

--- module/bi/lang/de.php
+++ module/bi/lang/de.php
@@ -5,3 +5,17 @@ $lang->bi->tmpPermissionDenied = 'The DuckDB tmp directory has no permissions, y
 $lang->bi->driver = 'Driver';
 $lang->bi->driverList = array();
 $lang->bi->driverList['mysql'] = 'MySQL';
+
+$lang->bi->sqlQuery   = 'SQL statements query';
+$lang->bi->sqlBuilder = 'SQL builder';
+
+$lang->bi->toggleSqlText    = 'Write SQL statements by hand';
+$lang->bi->toggleSqlBuilder = 'SQL builder';
+
+$lang->bi->builderStepList = array();
+$lang->bi->builderStepList['table'] = 'Select tables';
+$lang->bi->builderStepList['field'] = 'Select fields';
+$lang->bi->builderStepList['func']  = 'add function field';
+$lang->bi->builderStepList['where'] = 'Add where';
+$lang->bi->builderStepList['query'] = 'Add query filter';
+$lang->bi->builderStepList['group'] = 'set group by';
