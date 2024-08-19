df5ad015fc7b55dea3c221db1aabef4a5408f61e
qixinzhi <qixinzhi@easycorp.ltd>
2024-08-19 05:58:43 +0000

    * [misc] add sqlBuilder unit test.

---
+++ module/bi/test/model/sqlbuilder.php
@@ -0,0 +1,14 @@
+#!/usr/bin/env php
+<?php
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+include dirname(__FILE__, 2) . '/lib/bi.unittest.class.php';
+
+/**
+
+title=biModel->sqlBuilder();
+timeout=0
+cid=1
+
+*/
+
+$bi = new biTest();
