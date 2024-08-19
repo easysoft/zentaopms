d2242083d8ca070facdc316c4b74383ca64b6731
qixinzhi <qixinzhi@easycorp.ltd>
2024-08-19 06:02:04 +0000

    * [misc] add sqlBuilder unit test.

--- module/bi/test/model/sqlbuilder.php
+++ module/bi/test/model/sqlbuilder.php
@@ -36,6 +36,7 @@ $simple = array
 );
 
 r($bi->sqlBuilderTest($simple)) && p('') && e("SELECT `t1`.`id`, `t1`.`name` FROM `zt_task` AS `t1`"); // 测试 简单别名sql
+
 $withJoin = array
 (
     'selects' => array
@@ -56,3 +57,22 @@ $withJoin = array
 );
 
 r($bi->sqlBuilderTest($withJoin)) && p('') && e("SELECT `t1`.`id`, `t1`.`name`, `t2`.`name` AS `execution` FROM `zt_task` AS `t1` LEFT JOIN `zt_project` AS `t2` ON `t1`.`execution` = `t2`.`id`"); // 测试 联表sql
+
+$withGroup = array
+(
+    'selects' => array
+    (
+        array('t1', 'type')
+    ),
+    'functions' => array
+    (
+        array('t1', 'consumed', 'consumed', 'sum')
+    ),
+    'from' => array('zt_task', null, 't1'),
+    'groups' => array
+    (
+        array('t1', 'type')
+    )
+);
+
+r($bi->sqlBuilderTest($withGroup)) && p('') && e("SELECT `t1`.`type`, SUM(`t1`.`consumed`) AS `consumed` FROM `zt_task` AS `t1` GROUP BY `t1`.`type`"); // 测试 分组sql
