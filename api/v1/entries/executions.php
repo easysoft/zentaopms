2f1580ef1736a71a6f80e07e1849404b15f9f308
caoyanyi <caoyanyi@easycorp.ltd>
2024-08-20 13:31:16 +0800

    * [task#125954,done,0.1h] adjust execution dropmenu data for api.

--- api/v1/entries/executions.php
+++ api/v1/entries/executions.php
@@ -127,7 +127,6 @@ class executionsEntry extends entry
         if(isset($data->result) and $data->result == 'fail') return $this->sendError(400, $data->message);
 
         $account  = $this->app->user->account;
-        $projects = $data->data->projects;
         $dropMenu = array('involved' => array(), 'other' => array(), 'closed' => array());
         foreach($data->data->projects as $projectID => $projectName)
         {
@@ -139,8 +138,7 @@ class executionsEntry extends entry
                 $teams     = $execution->teams;
                 $execution = $this->filterFields($execution, 'id,project,model,type,name,code,status,PM,delay');
 
-                $projectName = zget($projects, $execution->project, '');
-                if($projectName) $execution->name = $projectName . '/' . $execution->name;
+                $execution->name = $projectName . '/' . $execution->name;
 
                 if($execution->status == 'closed')
                 {
