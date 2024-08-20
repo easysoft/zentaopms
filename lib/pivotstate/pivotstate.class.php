1c48b9022d4fef6c942c130eeeb97be32bd5f356
qixinzhi <qixinzhi@easycorp.ltd>
2024-08-20 09:06:55 +0000

    * [story#60291,0.5h] add setFrom.

--- lib/pivotstate/pivotstate.class.php
+++ lib/pivotstate/pivotstate.class.php
@@ -423,6 +423,18 @@ class pivotState
      */
 
     /**
+     * Set form.
+     *
+     * @param  string $table
+     * @access public
+     * @return void
+     */
+    public function setFrom($table)
+    {
+        $this->sqlBuilder['from'] = array('table' => $table, 'alias' => 't1');
+    }
+
+    /**
      * Clear fieldSettings.
      *
      * @access public
