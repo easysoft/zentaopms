06fa27964483be4ff590e6d60c5cd5f954d6e809
sunhao <sunhao@easycorp.ltd>
2024-08-20 23:15:17 +0800

    * [misc] export more language texts.

--- module/tutorial/ui/index.html.php
+++ module/tutorial/ui/index.html.php
@@ -160,9 +160,11 @@ $buildTutorialTabPane = function($type) use ($groupedGuides, $lang, $currentType
 jsVar('guides', $guides);
 jsVar('lang', array
 (
-    'nextStep'       => $lang->tutorial->nextStep,
-    'clickTipFormat' => $lang->tutorial->clickTipFormat,
-    'clickAndOpenIt' => $lang->tutorial->clickAndOpenIt
+    'nextTask'         => $lang->tutorial->nextTask,
+    'nextStep'         => $lang->tutorial->nextStep,
+    'clickTipFormat'   => $lang->tutorial->clickTipFormat,
+    'clickAndOpenIt'   => $lang->tutorial->clickAndOpenIt,
+    'congratulateTask' => $lang->tutorial->congratulateTask
 ));
 
 div
