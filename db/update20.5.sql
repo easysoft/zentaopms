acf81098dc90e8e394af89bbc6b7b8664ead9086
liugang <liugang@easycorp.ltd>
2024-08-20 08:59:37 +0800

    + [misc] add table to save workflow ui.

---
+++ db/update20.5.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `zt_workflowui` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `module` varchar(30) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `conditions` text NOT NULL,
+  PRIMARY KEY `id` (`id`),
+  KEY `module` (`module`),
+  KEY `action` (`action`),
+  UNIQUE KEY `unique` (`module`, `action`, `name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ALTER TABLE `zt_workflowlayout` ADD `ui` mediumint(8) unsigned NOT NULL DEFAULT '0' AFTER `action`;
