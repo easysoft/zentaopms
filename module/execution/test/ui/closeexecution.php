467ed1ff83697dd5ca1cad754927907508501ab8
yulujie <yulujie@easycorp.ltd>
2024-08-20 14:32:23 +0800

    * [task#126185,doing,0.5h,1h] Add ui file of ui test to close execution.

---
+++ module/execution/test/ui/closeexecution.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php
+
+/**
+title=关闭执行
+timeout=0
+cid=1
+*/
+
+chdir(__DIR__);
+include '../lib/closeexecution.ui.class.php';
+
+zendata('project')->loadYaml('execution', false, 2)->gen(10);
+$tester = new closeExecutionTester();
+$tester->login();
+
+$realEnd = array(date('Y-m-d', strtotime('+20 days')), date('Y-m-d'));
+
+r($tester->closeWithGreaterDate($realEnd[0])) && p('message') && e('关闭执行表单页提示信息正确');
+$tester->closeBrowser();
