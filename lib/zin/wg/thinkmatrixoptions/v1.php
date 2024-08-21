2dbbb3661131ffa0f1aac0cdc3ddc21eb1c525ed
wangzemei <wangzemei@easycorp.ltd>
2024-08-21 16:16:25 +0800

    + [task#126392] init data and reference files of thinkmatrixoptions widget.

--- lib/zin/wg/thinkmatrixoptions/v1.php
+++ lib/zin/wg/thinkmatrixoptions/v1.php
@@ -10,11 +10,27 @@ declare(strict_types=1);
  * @link        http://www.zentao.net
  */
 namespace zin;
+
+require_once dirname(__DIR__) . DS . 'input' . DS . 'v1.php';
+
 class thinkMatrixOptions extends wg
 {
     protected static array $defineProps = array
     (
         'id?: string="$GID"',     // 组件根元素的 ID。
         'name?: string="fields"', // 输入框作为表单项的名称。
+        'data?: array',           // 默认值。
+        'deleteTip?: string',     // 禁用删除提示。
+        'addColText: string',     // 添加列按钮文字
     );
+
+    public static function getPageJS(): ?string
+    {
+        return file_get_contents(__DIR__ . DS . 'js' . DS . 'v1.js');
+    }
+
+    public static function getPageCSS(): ?string
+    {
+        return file_get_contents(__DIR__ . DS . 'css' . DS . 'v1.css');
+    }
 }
