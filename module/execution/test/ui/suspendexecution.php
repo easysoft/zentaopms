ba23b4daa25d54302d98240046246d43cd103bdf
yulujie <yulujie@easycorp.ltd>
2024-08-22 11:24:02 +0800

    * [task#126834,done,1h,0h] Add file of ui test to suspend execution.

---
+++ module/execution/test/ui/suspendexecution.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php
+<?php
+
+/**
+title=挂起执行
+timeout=0
+cid=1
+*/
+
+chdir(__DIR__);
+include '../lib/suspendexecution.ui.class.php';
+
+zendata('project')->loadYaml('execution', false, 2)->gen(10);
+$tester = new suspendExecutionTester();
+$tester->login();
+
+r($tester->suspend('101')) && p('message') && e('挂起执行成功'); //挂起未开始的执行
+r($tester->suspend('103')) && p('message') && e('挂起执行成功'); //挂起进行中的执行
+$tester->closeBrowser();
