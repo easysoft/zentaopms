90f3385d236ffce779e3eaa62741500322c20a91
huairong <huairong@easycorp.ltd>
2024-08-22 13:55:49 +0800

    + [task#126576] add ui test file of createstage of programplan.

---
+++ module/programplan/test/lib/createstage.ui.class.php
@@ -0,0 +1,17 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+class createStageTester extends tester
+{
+    public function createStage(array $waterfall)
+    {
+        $programplanForm = $this->initForm('programplan', 'create' , array('projectID' => 5), 'appIframe-project');
+
+        //删除除需求外的其他默认阶段行
+        for ($i = 6; $i > 1; $i--)
+        {
+            $btn = "deleteBtn_$i";
+            $programplanForm->dom->$btn->click();
+            $programplanForm->wait(1);
+        }
+    }
+}
