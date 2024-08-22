8b7d0d72591a772fe1b8085badedc799a2bcffe4
azalea <azalea@easycorp.ltd>
2024-08-22 16:56:02 +0800

    * [uitest] Add batchcreateuser ui testcase

--- module/user/test/ui/batchcreateuser.php
+++ module/user/test/ui/batchcreateuser.php
@@ -14,3 +14,15 @@ include '../lib/batchcreateuser.ui.class.php';
 zendata('user')->loadYaml('user', false, 2)->gen(10);
 $tester = new batchCreateUserTester();
 $tester->login();
+
+$user = new stdclass();
+$user->account         = 'ljj';
+$user->realname        = '李娟';
+$user->password        = '123456';
+$user->verifyPassword  = $config->uitest->defaultPassword;
+
+$user1 = new stdclass();
+$user1->account         = '';
+$user1->realname        = '李娟1';
+$user1->password        = '123456';
+$user1->verifyPassword  = $config->uitest->defaultPassword;
