fa0b907341bf397a572d661ea059dc16ef3d9efe
lihuifang <lihuifang@easycorp.ltd>
2024-08-22 17:35:28 +0800

    +[task#126390,doing,3h,2h] add view page file of create projectrelease.

---
+++ module/projectrelease/test/ui/page/view.php
@@ -0,0 +1,14 @@
+<?php
+class viewPage extends page
+{
+    public function __construct($webdriver)
+    {
+        parent::__construct($webdriver);
+        $xpath = array(
+            'basic'            => "//*[@id='mainContent']/div[2]/div/div/div/div/ul/li[4]/a/span",
+            'basicreleasename' => "//*[@id='releaseInfo']/div[2]/div[1]/table/tbody/tr[2]/td",
+            'basicstatus'      => "//*[@id='releaseInfo']/div[2]/div[1]/table/tbody/tr[4]/td",
+        );
+        $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
+    }
+}
