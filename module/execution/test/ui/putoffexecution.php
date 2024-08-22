04295a530f1106d1e06ed13854f440276cb3fc5d
yulujie <yulujie@easycorp.ltd>
2024-08-22 15:48:07 +0800

    * [task#126833,doing,0.5h,1h] Modify file of ui test to putoff execution.

--- module/execution/test/ui/putoffexecution.php
+++ module/execution/test/ui/putoffexecution.php
@@ -9,12 +9,26 @@ cid=1
 chdir(__DIR__);
 include '../lib/putoffexecution.ui.class.php';
 
+zenData('project')->loadYaml('execution', false, 2)->gen(10);
 $tester = new putoffexecutionTester();
 $tester->login();
 
 $execution = array(
-    '0' => array(
-        'begin' => '',
-        'end'   => '',
+    '0' => array(),
+    '1' => array(
+        'begin' => date('Y-m-d', strtotime('-1 days')),
+        'end'   => date('Y-m-d', strtotime('+1 days')),
+    ),
+    '2' => array(
+        'begin' = '',
+    ),
+    '3' =>array(
+        'end' = '',
+    ),
+    '4' => array(
+        'begin' => date('Y-m-d', strtotime('-10 months')),
+    ),
+    '5' => array(
+        'end' => date('Y-m-d', strtotime('+10 months')),
     ),
 );
