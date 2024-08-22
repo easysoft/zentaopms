88556107200b1e2a06eb2f7921ee3657202018d5
huairong <huairong@easycorp.ltd>
2024-08-22 15:16:07 +0800

    + [task#126576] add conditional judgment of createsubstage file.

--- module/programplan/test/lib/createsubstage.ui.class.php
+++ module/programplan/test/lib/createsubstage.ui.class.php
@@ -19,3 +19,41 @@ class createSubStageTester extends tester
         $programplanForm->wait(1);
         $programplanForm->dom->submitBtn->click();
         $programplanForm->wait(1);
+
+        if($this->response('module') != 'project')
+        {
+             if($this->checkFormTips('programplan')) return $this->success('创建子阶段表单页提示信息正确');
+             if($programplanForm->dom->nameTip)
+             {
+                 //检查阶段名称不能为空
+                 $nameTipText = $programplanForm->dom->nameTip->getText();
+                 $nameTip     = sprintf($this->lang->error->notempty, $this->lang->programplan->name);
+                 return ($nameTipText == $nameTip) ? $this->success('创建子阶段表单页提示信息正确') : $this->failed('创建子阶段表单页提示信息不正确');
+             }
+             if($programplanForm->dom->beginTip)
+             {
+                 //检查计划开始日期不能为空
+                 $beginTipText = $programplanForm->dom->beginTip->getText();
+                 $beginTip     = sprintf($this->lang->error->notempty, $this->lang->programplan->begin);
+                 return ($beginTipText == $beginTip) ? $this->success('创建子阶段表单页提示信息正确') : $this->failed('创建子阶段表单页提示信息不正确');
+             }
+             if($programplanForm->dom->endTip)
+             {
+                 //检查计划结束日期不能为空
+                 if($waterfall['end_0'] == '')
+                 {
+                     $endTipText = $programplanForm->dom->endTip->getText();
+                     $endTip     = sprintf($this->lang->error->notempty, $this->lang->programplan->end);
+                     return ($endTipText == $endTip) ? $this->success('创建子阶段表单页提示信息正确') : $this->failed('创建子阶段表单页提示信息不正确');
+                 }
+                 //检查计划开始不能大于计划完成
+                 if($waterfall['begin_0'] > $waterfall['end_0'])
+                 {
+                     $endTipText = $programplanForm->dom->endTip->getText();
+                     $endTip     = sprintf($this->lang->programplan->error->planFinishSmall, '');
+                     return ($endTipText == $endTip) ? $this->success('创建子阶段表单页提示信息正确') : $this->failed('创建子阶段表单页提示信息不正确');
+                 }
+             }
+             return $this->failed('创建子阶段表单页提示信息不正确');
+        }
+}
