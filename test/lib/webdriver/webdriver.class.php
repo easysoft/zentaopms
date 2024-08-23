bf5a59e55417da229df99eed5962930f33eedde3
luxuyang <luxuyang@easycorp.ltd>
2024-08-23 18:25:30 +0800

    *[misc] Fix bug that picker doesn't work.

--- test/lib/webdriver/webdriver.class.php
+++ test/lib/webdriver/webdriver.class.php
@@ -999,8 +999,14 @@ class dom
 
                 if($xpath != 'form')
                 {
-                    $this->driver->findElement(WebDriverBy::xpath("//a[@title='$value']"))->click();
-                    $this->driver->findElement(WebDriverBy::xpath("//*[@id='pick-pop-$pickerID']//span[@class='is-match-keys']"))->click();
+                    try
+                    {
+                        $this->driver->findElement(WebDriverBy::xpath("//a[@title='$value']"))->click();
+                    }
+                    catch(Exception $xpathException)
+                    {
+                        $this->driver->findElement(WebDriverBy::xpath("//*[@id='pick-pop-$pickerID']//span[@class='is-match-keys']"))->click();
+                    }
                 }
                 else
                 {
