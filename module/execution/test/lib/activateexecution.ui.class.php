4869db617de9005895d647327e91d6e802ec2556
yulujie <yulujie@easycorp.ltd>
2024-08-23 14:08:32 +0800

    * [task#126878,doing,0.3h,3h] Add new function of ui test to activate execution.

---
+++ module/execution/test/lib/activateexecution.ui.class.php
@@ -0,0 +1,16 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+Class activateExecutionTester extends tester
+{
+    /**
+     * 输入激活执行表单字段。
+     * Input fields.
+     *
+     * @param string $end
+     * @param string $executionId
+     * @access public
+     */
+    public function inputFields($end, $executionId)
+    {
+    }
+}
