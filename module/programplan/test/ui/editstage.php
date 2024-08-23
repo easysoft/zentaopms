41e8a05c6881888b4639524e65644a263b6664ec
huairong <huairong@easycorp.ltd>
2024-08-23 14:56:05 +0800

    + [task#126575] add comment of ui test files of editstage.

--- module/programplan/test/ui/editstage.php
+++ module/programplan/test/ui/editstage.php
@@ -1,5 +1,13 @@
 #!/usr/bin/env php
 <?php
+
+/**
+
+title=编辑瀑布项目阶段测试
+timeout=0
+cid=3
+
+*/
 chdir(__DIR__);
 include '../lib/editstage.ui.class.php';
 
@@ -13,8 +21,10 @@ $waterfall = array(
     array('name' => '需求的阶段a1', 'begin' => '2020-11-02', 'end' => ''),
     array('name' => '需求的阶段a1', 'begin' => '2020-11-11', 'end' => '2020-11-16'),
 );
+
 r($tester->checkInput($waterfall['0'])) && p('message,status') && e('编辑阶段表单页提示信息正确，success'); //校验阶段名称不能为空
 r($tester->checkInput($waterfall['1'])) && p('message,status') && e('编辑阶段表单页提示信息正确，success'); //校验计划开始必填
 r($tester->checkInput($waterfall['2'])) && p('message,status') && e('编辑阶段表单页提示信息正确，success'); //校验计划完成必填
 r($tester->checkInput($waterfall['3'])) && p('status') && e('success');                                     //编辑需求阶段
+
 $tester->closeBrowser();
