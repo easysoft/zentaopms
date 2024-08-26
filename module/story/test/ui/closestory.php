3718d29336eedf75c77a1304a639fd3f82cfab19
lijie <lijie@easycorp.ltd>
2024-08-26 22:00:58 +0800

    [task#126277]add closestory.php file.

---
+++ module/story/test/ui/closestory.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php
+declare(strict_types=1);
+/**
+
+title=关闭研发需求测试
+timeout=0
+cid=88
+- 关闭没有父需求的研发需求后检查信息正确
+ -  属性module @story
+ -  属性method @view
+- 关闭有父需求的研发需求后检查信息正确
+ -  属性module @story
+ -  属性method @view
+- 关闭需求成功，最终测试状态 @success
+ */
+chdir (__DIR__);
+include '../lib/closestory.ui.class.php';
+
+$tester = new closeStoryTester();
