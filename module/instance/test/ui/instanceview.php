f4df37f76a0e3775395b6132d2d825245c06a6aa
luxuyang <luxuyang@easycorp.ltd>
2024-08-26 16:54:57 +0800

    *[task#126441] Modify comment of instance script.

--- module/instance/test/ui/instanceview.php
+++ module/instance/test/ui/instanceview.php
@@ -4,10 +4,10 @@ include '../lib/instance.ui.class.php';
 $tester = new instance();
 $instance = zenData('pipeline')->loadYaml('pipeline')->gen(7);
 $type = 'GitFox';
-r($tester->instanceView($type)) && p('message,status') && e('检查GitFox应用成功,SUCCESS'); //检查iGitFox应用页面是否可以正常展示
+r($tester->instanceView($type)) && p('message,status') && e('检查GitFox应用成功,SUCCESS'); //检查GitFox应用页面是否可以正常展示
 
 $type = 'GitLab';
-r($tester->instanceView($type)) && p('message,status') && e('检查GitLab应用成功,SUCCESS'); //检查iGitLab应用页面是否可以正常展示
+r($tester->instanceView($type)) && p('message,status') && e('检查GitLab应用成功,SUCCESS'); //检查GitLab应用页面是否可以正常展示
 
 $type = 'Gogs';
 r($tester->instanceView($type)) && p('message,status') && e('检查Gogs应用成功,SUCCESS'); //检查Gogs应用页面是否可以正常展示
