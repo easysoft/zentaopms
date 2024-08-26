608f64900afa05cc4067b9fe202f8a91ee00b189
lihuifang <lihuifang@easycorp.ltd>
2024-08-26 13:18:05 +0800

    +[task#126285,doing,3h,2.5h] add ui test file of create scrumproject sprint.

--- module/project/test/ui/createsprint.php
+++ module/project/test/ui/createsprint.php
@@ -13,3 +13,16 @@ include '../lib/createsprint.ui.class.php';
 zendata('project')->loadYaml('project', false, 1)->gen(1);
 $tester = new createSprintTester();
 $tester->login();
+
+//设置敏捷项目执行数据
+$sprint = array(
+    array('project' => '敏捷项目1', 'name' => '', 'end' => date('Y-m-d', strtotime('+5 days'))),
+    array('project' => '敏捷项目1', 'name' => '一个敏捷迭代'.time(), 'end' => ''),
+    array('project' => '敏捷项目1', 'name' => '一个敏捷迭代'.time(), 'end' => date('Y-m-d', strtotime('+5 days'))),
+);
+
+r($tester->checkInput($sprint['0'])) && p('message') && e('创建执行成功');  //执行名称为空时的提示校验
+r($tester->checkInput($sprint['1'])) && p('message') && e('创建执行成功');  //计划完成时间为空时的提示校验
+r($tester->checkInput($sprint['2'])) && p('status')  && e('SUCCESS');       //添加迭代成功
+
+$tester->closeBrowser();
