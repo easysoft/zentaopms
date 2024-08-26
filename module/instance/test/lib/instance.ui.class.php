0a54ad85cf4fc60f94840f83705c8594af785b5c
luxuyang <luxuyang@easycorp.ltd>
2024-08-26 15:14:52 +0800

    *[task#126441] Modify to distinguish different type of instance in function view.

--- module/instance/test/lib/instance.ui.class.php
+++ module/instance/test/lib/instance.ui.class.php
@@ -7,9 +7,25 @@ class instance extends tester
     public function instanceView($type)
     {
         $this->login();
-        $this->initForm('space', 'browse', '', 'appIframe-devops');
-        $xpathNum = $this->page->webdriver->driver->findElement(WebDriverBy::xpath("(//div[text()='$type']/..)[1]"))->getAttribute('data-row');
-        $this->page->webdriver->driver->findElement(WebDriverBy::xpath("//div[@data-col='name' and @data-row=$xpathNum]//a[text()]"))->click();
+        $form = $this->initForm('space', 'browse', '', 'appIframe-devops');
+        $elementCount = $this->page->webdriver->driver->findElement(WebDriverBy::xpath("(//div[text()='$type']/..)[1]"))->getAttribute('data-row');
+        $this->page->webdriver->driver->findElement(WebDriverBy::xpath("//div[@data-col='name' and @data-row=$elementCount]//a[text()]"))->click();
+        $this->webdriver->wait(1);
+        $newPage = $this->loadPage('instance', 'view');
+        if($type == 'GitLab' || $type == 'SonarQube')
+        {
+            $form->dom->btn($this->lang->instance->management)->click();
+        }
+        elseif($type == 'GitFox')
+        {
+            $newPage->dom->tokenCopy->click();
+        }
+        else
+        {
+            $newPage->dom->passwdCopy->click();
+            $this->webdriver->wait(1);
+            $newPage->dom->tokenCopy->click();
+        }
         $this->webdriver->wait(1);
     }
 }
