26e825f334f553dd0fc1597276b97f90569325ff
luxuyang <luxuyang@easycorp.ltd>
2024-08-26 15:18:03 +0800

    *[task#126441] Code optimization of instance view UI script.

--- module/instance/test/ui/instanceview.php
+++ module/instance/test/ui/instanceview.php
@@ -5,15 +5,21 @@ $tester = new instance();
 $instance = zenData('pipeline')->loadYaml('pipeline')->gen(7);
 $type = 'GitFox';
 r($tester->instanceView($type)) && p('message,status') && e('检查GitFox应用成功,SUCCESS'); //检查iGitFox应用页面是否可以正常展示
+
 $type = 'GitLab';
 r($tester->instanceView($type)) && p('message,status') && e('检查GitLab应用成功,SUCCESS'); //检查iGitLab应用页面是否可以正常展示
+
 $type = 'Gogs';
 r($tester->instanceView($type)) && p('message,status') && e('检查Gogs应用成功,SUCCESS'); //检查Gogs应用页面是否可以正常展示
+
 $type = 'Gitea';
 r($tester->instanceView($type)) && p('message,status') && e('检查Gitea应用成功,SUCCESS'); //检查Gitea应用页面是否可以正常展示
+
 $type = 'Jenkins';
 r($tester->instanceView($type)) && p('message,status') && e('检查Jenkins应用成功,SUCCESS'); //检查Jenkins应用页面是否可以正常展示
+
 $type = 'SonarQube';
 r($tester->instanceView($type)) && p('message,status') && e('检查SonarQube应用成功,SUCCESS'); //检查SonarQube应用页面是否可以正常展示
+
 $type = 'Nexus';
 r($tester->instanceView($type)) && p('message,status') && e('检查Nexus应用成功,SUCCESS'); //检查Nexus应用页面是否可以正常展示
