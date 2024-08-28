1bb4642722c66c1a6c19cc0d537a62180c46097e
caoyanyi <caoyanyi@easycorp.ltd>
2024-08-28 10:35:32 +0800

    - [misc] delete unused language.

--- module/testtask/lang/zh-cn.php
+++ module/testtask/lang/zh-cn.php
@@ -122,8 +122,6 @@ $lang->testtask->allSummary        = '本页共 <strong>%s</strong> 个测试单
 $lang->testtask->checkedAllSummary = '共选中 <strong>%total%</strong> 个测试单，待测试 <strong>%wait%</strong>，测试中 <strong>%testing%</strong>，被阻塞 <strong>%blocked%</strong>，已测试 <strong>%done%</strong>。';
 $lang->testtask->emptyCases        = 'ID %s 的用例没有步骤，过滤显示。';
 
-$lang->story->closedStory      = "{$lang->SRCommon} %s is closed, and this operation has been filtered.";
-
 $lang->testtask->beginAndEnd = '起止时间';
 $lang->testtask->to          = '至';
 
