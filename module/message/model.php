1db6b76f3c8528fdca63c36161e566d7d5809b6b
tianshujie <tianshujie@easycorp.ltd>
2024-08-28 11:10:36 +0800

    * [refac] change ZenTaoCMS to ZenTaoPMS.

--- module/message/model.php
+++ module/message/model.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 /**
- * The model file of message module of ZenTaoCMS.
+ * The model file of message module of ZenTaoPMS.
  *
  * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.cnezsoft.com)
  * @license     ZPL(http://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
