db1c7d516e4ad8e943087876808d1fcc065c9f66
wangyidong <wangyidong@cnezsoft.com>
2024-08-29 09:15:31 +0800

    * [refac bug #54645] use zget to get session.

--- module/datatable/ui/ajaxcustom.html.php
+++ module/datatable/ui/ajaxcustom.html.php
@@ -86,7 +86,7 @@ function buildBody(array $cols): form
 
     foreach($cols as $col)
     {
-        if($_SESSION['currentProductType'] == 'normal' && $col['name'] == 'branch') continue;
+        if(zget($_SESSION, 'currentProductType', '') == 'normal' && $col['name'] == 'branch') continue;
         if(isset($col['type'])) $col = array_merge(getDefaultConfig($col['type']), $col);
         if(!isset($col['fixed']) || empty($col['fixed'])) $col['fixed'] = 'no';
         $itemsList[$col['fixed']][] = array(
