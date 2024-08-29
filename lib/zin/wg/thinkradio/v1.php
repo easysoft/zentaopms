7c7fd5b16f5ddafb00187698fdba9882a723b874
Zhangyu <zhangyu@easycorp.ltd>
2024-08-29 14:27:59 +0000

    * [task#126402] add isResult pros of thinkradio.

--- lib/zin/wg/thinkradio/v1.php
+++ lib/zin/wg/thinkradio/v1.php
@@ -45,6 +45,7 @@ class thinkRadio extends thinkQuestion
         if(!empty($fields)) foreach($fields as $field) $items[] = array('text' => $field, 'value' => $field);
         if(empty($fields) && !$isRun) $items[] = array('text' => $lang->thinkstep->optionReference, 'disabledPrefix' => true);
         if(!empty($step->options->enableOther)) $items[] = array('text' => $lang->other, 'value' => 'other', 'isOther' => '1', 'other' => isset($answer->other) ? $answer->other : '');
+        $isQutoCheckbox = $step->options->questionType == 'checkbox' && !empty($step->options->setOption) && $step->options->setOption == 1;
 
         $detailWg[] = thinkBaseCheckbox
         (
@@ -52,7 +53,7 @@ class thinkRadio extends thinkQuestion
             set::items($items),
             set::name('result[]'),
             set::value($step->options->questionType == 'radio' ? ($result[0] ?? '') : $result),
-            set::disabled(($step->options->questionType == 'checkbox' && !empty($step->options->setOption) && $step->options->setOption == 1 && !$isRun)|| $isResult)
+            set::disabled(($isQutoCheckbox && !$isRun)|| $isResult)
         );
         $quoteItem = $step->options->questionType == 'checkbox' && !empty($step->options->setOption) && $step->options->setOption == 1;
         $quoteName = '';
