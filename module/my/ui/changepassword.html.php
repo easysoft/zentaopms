0135a82f6ec665b6c951bc5218b7b29278689265
sunguangming <sunguangming@easycorp.ltd>
2024-08-29 07:53:01 +0000

    * [bug#54719,done,0.2h] fix bug.

--- module/my/ui/changepassword.html.php
+++ module/my/ui/changepassword.html.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  */
 namespace zin;
 
-$reason = $app->user->modifyPasswordReason;
+$reason = isset($app->user->modifyPasswordReason) ? $app->user->modifyPasswordReason : '';
 
 formPanel
 (
