bda7f33fe90b8ac61d94d97f4e5f6159f3acdae0
azalea <azalea@easycorp.ltd>
2024-08-30 10:45:33 +0800

    * [uitest] Add batchedituser ui testcase

--- module/user/test/ui/batchedituser.php
+++ module/user/test/ui/batchedituser.php
@@ -18,3 +18,17 @@ $tester->login();
 $user = new stdclass();
 $user->realname        = '李娟娟';
 $user->verifyPassword  = $config->uitest->defaultPassword;
+
+$user1 = new stdclass();
+$user1->realname        = '';
+$user1->verifyPassword  = $config->uitest->defaultPassword;
+
+$user2 = new stdclass();
+$user2->realname        = '李娟';
+$user2->verifyPassword  = '';
+
+r($tester->batchEditUser($user))        && p('message') && e('保存成功');                         //批量编辑用户后的跳转链接检查
+r($tester->emptyRealname($user1))       && p('message') && e('姓名不能为空');                             //批量编辑用户后的跳转链接检查
+r($tester->wrongVerifyPassword($user2)) && p('message') && e('验证失败，请检查您的系统登录密码是否正确'); //批量编辑用户后的跳转链接检查
+
+$tester->closeBrowser();
