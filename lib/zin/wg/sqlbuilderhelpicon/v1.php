576cc1cc82cfd924cf92e9330d8ca856331cd7e8
qixinzhi <qixinzhi@easycorp.ltd>
2024-09-02 08:10:47 +0000

    * [misc,0.5h] add sqlbuilderhelpicon.

---
+++ lib/zin/wg/sqlbuilderhelpicon/v1.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+namespace zin;
+
+class sqlBuilderHelpIcon extends wg
+{
+    protected static array $defineProps = array(
+        "text?: string",
+        "placement?: string",
+        "toggle?: bool=true"
+    );
+
+    protected function build()
+    {
+        list($text, $placement, $toggle) = $this->prop(array('text', 'placement', 'toggle'));
+
+        if(empty($text)) return span
+        (
+            setClass('text-warning'),
+            icon('help'),
+        );
+
+        return btn
+        (
+            setClass('inline ghost'),
+            set::size('sm'),
+            setData(array('title' => $text, 'placement' => $placement, 'className' => 'text-gray border border-light', 'type' => 'white', 'hideOthers' => true)),
+            $toggle ? set('data-toggle', 'tooltip') : null,
+            icon('help')
+        );
+    }
+}
