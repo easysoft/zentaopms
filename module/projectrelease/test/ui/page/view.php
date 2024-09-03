953cfdf459abdf989c242c6b03453f5fd33f5523
lihuifang <lihuifang@easycorp.ltd>
2024-09-03 08:55:07 +0800

    *[task#127642,doing,2h,1.5h] modify view page file of edit projectrelease.

--- module/projectrelease/test/ui/page/view.php
+++ module/projectrelease/test/ui/page/view.php
@@ -8,6 +8,8 @@ class viewPage extends page
             'basic'            => "//*[@id='mainContent']/div[2]/div/div/div/div/ul/li[4]/a/span",
             'basicreleasename' => "//*[@id='releaseInfo']/div[2]/div[1]/table/tbody/tr[2]/td",
             'basicstatus'      => "//*[@id='releaseInfo']/div[2]/div[1]/table/tbody/tr[4]/td",
+            'basicplandate'    => "//*[@id='releaseInfo']/div[2]/div[1]/table/tbody/tr[5]/td",
+            'basicreleasedate' => "//*[@id='releaseInfo']/div[2]/div[1]/table/tbody/tr[6]/td",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
