333395edc1fcef7835e0d907d598ca8916170921
lihuifang <lihuifang@easycorp.ltd>
2024-09-03 08:57:47 +0800

    *[task#127642,doing,2h,0.5h] add ui function files of edit projectrelease.

--- module/projectrelease/test/lib/editprojectrelease.ui.class.php
+++ module/projectrelease/test/lib/editprojectrelease.ui.class.php
@@ -17,3 +17,23 @@ class editProjectReleaseTester extends tester
         if(isset($release['name']))   $form->dom->name->setValue($release['name']);
         if(isset($release['status'])) $form->dom->status->picker($release['status']);
         $form->wait(2);
+        if(isset($release['plandate']))    $form->dom->date->datepicker($release['plandate']);
+        if(isset($release['releasedate'])) $form->dom->releasedDate->datepicker($release['releasedate']);
+
+        $form->dom->btn($this->lang->save)->click();
+        $form->wait(5);
+        return $this->checkResult($release);
+    }
+
+    /**
+     * Check the result after editing the project release.
+     * 编辑项目发布后检查结果
+     *
+     * @param  array $release
+     * @access public
+     * @return object
+     */
+    public function checkResult($release)
+    {
+        //检查编辑项目发布页面时的提示信息
+        if($this->response('method') != 'view')
