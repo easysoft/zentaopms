7db250ac36c3d935ce5f0c666e7e44dd8d57feb8
宋辰轩 <songchenxuan@easycorp.ltd>
2024-09-03 16:23:11 +0800

    发起 "main" 强制评审 (#3001)

--- db/update20.6.sql
+++ db/update20.6.sql
@@ -1,1 +1,17 @@
 ALTER TABLE `zt_workflow` ADD `belong` varchar(50) NOT NULL DEFAULT '' AFTER `buildin`;
+
+ALTER TABLE `zt_pivot` ADD `mode` enum('text', 'builder') not NULL default 'builder' AFTER `driver`;
+ALTER TABLE `zt_pivot` ADD `builder` mediumtext AFTER `sql`;
+UPDATE `zt_pivot` SET `mode` = 'text';
+
+ALTER TABLE zt_dimension ADD `acl` enum('open','private') NOT NULL DEFAULT 'open' AFTER `desc`;
+ALTER TABLE zt_dimension ADD `whitelist` text NULL AFTER `acl`;
+
+ALTER TABLE zt_chart ADD `acl` enum('open','private') NOT NULL DEFAULT 'open' AFTER `desc`;
+ALTER TABLE zt_chart ADD `whitelist` text NULL AFTER `acl`;
+
+ALTER TABLE zt_pivot ADD `acl` enum('open','private') NOT NULL DEFAULT 'open' AFTER `desc`;
+ALTER TABLE zt_pivot ADD `whitelist` text NULL AFTER `acl`;
+
+ALTER TABLE zt_screen ADD `acl` enum('open','private') NOT NULL DEFAULT 'open' AFTER `desc`;
+ALTER TABLE zt_screen ADD `whitelist` text NULL AFTER `acl`;
