090b57d0a3ebd16052d541a93d4f26d82e7032df
yulujie <yulujie@easycorp.ltd>
2024-09-03 14:22:08 +0800

    * [task#127262,doing,2h,1h] Add new function of ui test to manage products.

--- module/execution/test/lib/manageproducts.ui.class.php
+++ module/execution/test/lib/manageproducts.ui.class.php
@@ -26,4 +26,20 @@ class manageProductsTester extends tester
         if($form->dom->linckedProducta === true) return $this->failed('取消关联产品失败');
         return $this->success('取消关联产品成功');
     }
+
+    /**
+     * 执行下是否有产品导航。
+     * Product navigation.
+     *
+     * @param  array  $execution
+     * @param  bool   $expect
+     * @access public
+     * @return void
+     */
+    public function checkProductsNev($execution, $expect)
+    {
+        $form = $this->initForm('execution', 'view', array('execution' => $execution['id']), 'appIframe-execution');
+        if(is_object($form->dom->productsNev) === $expect) return $this->success('关联产品导航显示正确');
+        return $this->failed('关联产品导航显示错误');
+    }
 }
