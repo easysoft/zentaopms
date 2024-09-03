f23b3edad856cca7c1b1569e5adb9cb31e8d427a
yulujie <yulujie@easycorp.ltd>
2024-09-03 10:43:18 +0800

    * [task#127262,doing,0.2h,3.5h] Add new xpath of ui test to manage products.

--- module/execution/test/ui/page/view.php
+++ module/execution/test/ui/page/view.php
@@ -6,14 +6,16 @@ class viewPage extends page
         parent::__construct($webdriver);
         $xpath = array(
             /* 执行概况页元素 */
-            'executionName' => "//*[@id='mainContent']/div[1]/div[1]/div[2]/div[1]/div",
-            'status'        => "//*[@id='mainContent']/div[1]/div[1]/div[2]/div[1]/span[2]",
-            'acl'           => "//*[@id='mainContent']/div[1]/div[1]/div[2]/div[1]/span[3]",
-            'plannedBegin'  => "//*[@id='mainContent']/div[2]/div[1]/div/table[3]/tbody/tr/td/div/div[1]/span[2]",
-            'plannedEnd'    => "//*[@id='mainContent']/div[2]/div[1]/div/table[3]/tbody/tr/td/div/div[2]/span[2]",
-            'realBeganView' => "//*[@id='mainContent']/div[2]/div[1]/div/table[3]/tbody/tr/td/div/div[3]/span[2]",
-            'realEndView'   => "//*[@id='mainContent']/div[2]/div[1]/div/table[3]/tbody/tr/td/div/div[4]/span[2]",
-            'edit'          => "//*[@id='mainContent']/div[3]/div/a[last()-1]",
+            'executionName'   => "//*[@id='mainContent']/div[1]/div[1]/div[2]/div[1]/div",
+            'status'          => "//*[@id='mainContent']/div[1]/div[1]/div[2]/div[1]/span[2]",
+            'acl'             => "//*[@id='mainContent']/div[1]/div[1]/div[2]/div[1]/span[3]",
+            'plannedBegin'    => "//*[@id='mainContent']/div[2]/div[1]/div/table[3]/tbody/tr/td/div/div[1]/span[2]",
+            'plannedEnd'      => "//*[@id='mainContent']/div[2]/div[1]/div/table[3]/tbody/tr/td/div/div[2]/span[2]",
+            'realBeganView'   => "//*[@id='mainContent']/div[2]/div[1]/div/table[3]/tbody/tr/td/div/div[3]/span[2]",
+            'realEndView'     => "//*[@id='mainContent']/div[2]/div[1]/div/table[3]/tbody/tr/td/div/div[4]/span[2]",
+            'edit'            => "//*[@id='mainContent']/div[3]/div/a[last()-1]",
+            'linckedProducta' => "//*[@id='mainContent']/div[2]/div[1]/div/table[1]/tbody/tr[1]",
+            'linckedProductb' => "//*[@id='mainContent']/div[2]/div[1]/div/table[1]/tbody/tr[2]",
             /* 编辑执行弹窗中元素 */
             'products'    => "//*[@name='products[0]']",
             'productsTip' => "//*[@id='products[0]Tip']",
