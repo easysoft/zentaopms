69cf82926cd72718b037b11524b6801291918481
yulujie <yulujie@easycorp.ltd>
2024-09-03 10:43:51 +0800

    * [task#127262,doing,0.5h,3.5h] Modify function of ui test to manage products.

--- module/execution/test/lib/manageproducts.ui.class.php
+++ module/execution/test/lib/manageproducts.ui.class.php
@@ -16,5 +16,14 @@ class manageProductsTester extends tester
         if($execution['operate'] == 'link')   $form->dom->productb->click();
         if($execution['operate'] == 'unlink') $form->dom->producta->click();
         $form->dom->btn($this->lang->save)->click();
+
+        $form = $this->initForm('execution', 'view', array('execution' => $execution['id']), 'appIframe-execution');
+        if($execution['operate'] == 'link')
+        {
+            if($form->dom->linckedProductb === false) return $this->failed('关联产品失败');
+            return $this->success('关联产品成功');
+        }
+        if($form->dom->linckedProducta === true) return $this->failed('取消关联产品失败');
+        return $this->success('取消关联产品成功');
     }
 }
