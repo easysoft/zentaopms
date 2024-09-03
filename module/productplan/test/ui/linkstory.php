e3304b98c6033783585413b29910a647b6eb6d72
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-09-03 08:03:29 +0000

    *[task#127573,done,0.5h,4h] Add test case files of linkstory

--- module/productplan/test/ui/linkstory.php
+++ module/productplan/test/ui/linkstory.php
@@ -14,3 +14,19 @@ $story->status->range('active');
 $story->plan->range('0');
 $story->gen(5);
 
+$storyspec = zenData('storyspec');
+$storyspec->story->range('1-10');
+$storyspec->version->range('1');
+$storyspec->spec->range('需求描述');
+$storyspec->title->range('需求1,需求2,需求3,需求4,需求5');
+$storyspec->gen(5);
+
+$tester = new linkStoryTester();
+$tester->login();
+$planID['planID'] = '2';
+
+r($tester->linkStory($planID))      && p('message,status') && e('关联需求成功,SUCCESS');//关联需求
+r($tester->unlinkStory($planID))    && p('message,status') && e('移除单个需求成功,SUCCESS');//移除单个需求
+r($tester->unlinkAllStory($planID)) && p('message,status') && e('移除全部需求成功,SUCCESS');//移除全部需求
+
+$tester->closeBrowser();
