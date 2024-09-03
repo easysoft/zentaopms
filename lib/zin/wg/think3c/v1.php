493c5b752a6bbf1453ad2a78aa443e4482a521ad
wangzemei <wangzemei@easycorp.ltd>
2024-09-03 15:40:21 +0800

    + [task127424] add the buildBody function of think3c widget.

--- lib/zin/wg/think3c/v1.php
+++ lib/zin/wg/think3c/v1.php
@@ -10,4 +10,11 @@ class think3c extends thinkModel
     {
         return file_get_contents(__DIR__ . DS . 'js' . DS . 'v1.js');
     }
+
+    protected function buildBody(): array
+    {
+        jsVar('modelImg', 'data/thinmory/thumbnail/model3c.png');
+
+        return array(h::canvas(setID('canvas')));
+    }
 }
