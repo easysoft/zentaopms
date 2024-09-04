f782278c6831c5967b5eabd375b631f9e80b96dd
yulujie <yulujie@easycorp.ltd>
2024-09-04 09:57:53 +0800

    * [task#127263,doing,0.5h,4h] Add xpath of ui test to manage members.

--- module/execution/test/ui/page/managemembers.php
+++ module/execution/test/ui/page/managemembers.php
@@ -5,8 +5,8 @@ class managemembersPage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            /* 团队管理页面第一个为空的用户input */
-            'account' => "(//div[@id='teamForm']//input[starts-with(@name, 'account[') and not(@value)])[1]",
+            'account' => "(//div[@id='teamForm']//input[starts-with(@name, 'account[') and not(@value)])[1]", //团队管理页面第一个为空的用户
+            'user'    => "(//*[@id='table-execution-team']//div[@data-col='realname'])[last()]/div/a",        //团队列表页面最后一个用户
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
