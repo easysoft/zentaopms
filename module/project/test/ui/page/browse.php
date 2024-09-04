d20291dd0acff9bb8586452c092e0ee6023999b0
huairong <huairong@easycorp.ltd>
2024-09-04 16:41:40 +0800

    + [task#127605] modify ui test file of browse page.

--- module/project/test/ui/page/browse.php
+++ module/project/test/ui/page/browse.php
@@ -21,6 +21,10 @@ class browsePage extends page
             'suspendBtn'     => "//*[@class='popover show fade dropdown in']/menu/menu/li/a",
             'suspendProject' => "//*[@class='modal-content']/div[3]/div/div/form/div[2]/div/button",
             'browseStatus'   => "//*[@id='table-project-browse']/div[2]/div[2]/div/div/div/span",
+            'selectBtn'      => "//*[@id='table-project-browse']/div[2]/div[1]/div/div[1]",
+            'batchEditBtn'   => "//*[@id='table-project-browse']/div[3]/nav[1]/nav/button/span",
+            'begin'          => "//*[@id='table-project-browse']/div[2]/div[2]/div/div[7]/div",
+            'end'            => "//*[@id='table-project-browse']/div[2]/div[2]/div/div[8]/div",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
