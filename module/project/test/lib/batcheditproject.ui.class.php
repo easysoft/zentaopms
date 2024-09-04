f634b81cef94b788d995eb559974eca9f53676a3
huairong <huairong@easycorp.ltd>
2024-09-04 16:34:53 +0800

    + [task#127605] add ui test file of checkBatchEdit function.

--- module/project/test/lib/batcheditproject.ui.class.php
+++ module/project/test/lib/batcheditproject.ui.class.php
@@ -26,4 +26,34 @@ class batchEditProjectTester extends tester
         $form->wait(1);
 
         return $this->checkBatchEdit($form, $firstID, $project);
+    }
+    public function checkBatchEdit(object $form, string $firstID, array $project)
+    {
+        if($this->response('method') != 'browse')
+        {
+            $firstBeginTip  = "begin[{$firstID}]Tip";
+            $firstEndTip    = "end[{$firstID}]Tip";
+            $firstNameTip   = "name[{$firstID}]Tip";
+            if($form->dom->$firstNameTip)
+            {
+                //检查项目名称不能为空
+                $nameTipText = $form->dom->$firstNameTip->getText();
+                $nameTip     = sprintf($this->lang->error->notempty, $this->lang->project->name);
+                return ($nameTipText == $nameTip) ? $this->success('批量编辑项目表单页提示信息正确') : $this->failed('批量编辑项目表单页提示信息不正确');
+            }
+            if($form->dom->$firstBeginTip)
+            {
+                //检查计划开始不能为空
+                $beginTipText = $form->dom->$firstBeginTip->getText();
+                $beginTip     = sprintf($this->lang->error->notempty, $this->lang->project->begin);
+                return ($beginTipText == $beginTip) ? $this->success('批量编辑项目表单页提示信息正确') : $this->failed('批量编辑项目表单页提示信息不正确');
+            }
+            if($form->dom->$firstEndTip)
+            {
+                //检查计划完成不能为空
+                $endTipText = $form->dom->$firstEndTip->getText();
+                $endTip     = sprintf($this->lang->project->copyProject->endTips,'');
+                return ($endTipText == $endTip) ? $this->success('批量编辑项目表单页提示信息正确') : $this->failed('批量编辑项目表单页提示信息不正确');
+            }
+        }
 }
