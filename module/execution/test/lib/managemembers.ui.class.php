d237402bc17093da77df09108283fb7d43838f61
yulujie <yulujie@easycorp.ltd>
2024-09-04 14:36:28 +0800

    * [task#127263,doing,1h,4h] Add function of ui test to manage members.

--- module/execution/test/lib/managemembers.ui.class.php
+++ module/execution/test/lib/managemembers.ui.class.php
@@ -16,7 +16,7 @@ class manageMembersTester extends tester
         $form->dom->firstNullAccount->picker($execution['account']);
         $form->dom->btn($this->lang->save)->click();
 
-        $form = $this->loadPage();
+        $form = $this->loadPage('execution', 'team');
         if($form->dom->lastUser->getText() != $execution['account']) return $this->failed('添加团队成员失败');
         return $this->success('添加团队成员成功');
     }
@@ -36,9 +36,28 @@ class manageMembersTester extends tester
         $form->dom->firstDelBtn->click();
         $form->dom->btn($this->lang->save)->click();
 
-        $form = $this->loadPage();
+        $form = $this->loadPage('execution', 'team');
         if($form->dom->firstUser->getText() != $firstAccount) return $this->failed('删除团队成员成功');
         return $this->success('删除团队成员失败');
 
+    }
+    /**
+     * 移除团队成员。
+     * Remove team members.
+     *
+     * @param  array  $execution
+     * @access public
+     * @return object
+     */
+    public function remove($execution)
+    {
+        $form = $this->initForm('execution', 'team', array('execution' => $execution['id']), 'appIframe-execution');
+        $numBefore = $form->dom->num->getText();
+        $form->dom->firstRemoveBtn->click();
+        $form->dom->alertModal();
+        $form->wait(1);
+        $numAfter = $form->dom->num->getText();
+        if($numBefore == $numAfter + 1) return $this->success('移除团队成员成功');
+        return $this->failed('移除团队成员失败');
     }
 }
