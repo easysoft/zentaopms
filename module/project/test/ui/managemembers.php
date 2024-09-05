9d1e384eaa0f839f01be93d3dfe91874e1ba1c68
lihuifang <lihuifang@easycorp.ltd>
2024-09-05 16:30:12 +0800

    *[task#127645,doing,5h,4h] Add ui test file of project managemembers.

--- module/project/test/ui/managemembers.php
+++ module/project/test/ui/managemembers.php
@@ -15,3 +15,14 @@ zendata('user')->loadYaml('account', false, 1)->gen(5);
 zendata('dept')->loadYaml('dept', false, 1)->gen(1);
 $tester = new manageMembersTester();
 $tester->login();
+
+//设置敏捷项目执行数据
+$members = array(
+    array('account' => '用户1', 'role' => '开发人员', 'day' => '7', 'hours' => '3'),
+);
+
+r($tester->addMembers($members['0'])) && p('message') && e('项目团队成员添加成功');  //添加项目团队成员
+r($tester->deleteMembers())           && p('status')  && e('SUCCESS');               //删除项目已有的团队成员
+r($tester->copyDeptMembers())         && p('status')  && e('SUCCESS');               //复制部门成员
+
+$tester->closeBrowser();
