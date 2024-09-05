065d4de66cc8308f986478d60348f57c79a6fe22
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-09-05 07:07:20 +0000

    *[task#127261,done,1h,3h] Add uitest case files of batch edit  product

--- module/product/test/ui/batcheditproduct.php
+++ module/product/test/ui/batcheditproduct.php
@@ -13,3 +13,20 @@ $product->gen(5);
 $tester = new batchEditProduct();
 $tester->login();
 
+$product1 = new stdClass();
+$product1->name = '产品名称2024';
+r($tester->batchEditProduct($product1)) && p('message,status') && e('产品名称修改成功,SUCCESS');//批量修改产品名称
+
+$product2 = new stdClass();
+$product2->type = '多分支';
+r($tester->batchEditProduct($product2)) && p('message,status') && e('产品类型修改成功,SUCCESS');//批量修改产品类型
+
+$product3 = new stdClass();
+$product3->status = '结束';
+r($tester->batchEditProduct($product3)) && p('message,status') && e('产品状态修改成功,SUCCESS');//批量修改产品状态为结束
+
+$product4 = new stdClass();
+$product4->acl = 'private';
+r($tester->batchEditProduct($product4)) && p('message,status') && e('产品访问控制修改为私有成功,SUCCESS');//批量修改产品访问控制为私有
+
+$tester->closeBrowser();
