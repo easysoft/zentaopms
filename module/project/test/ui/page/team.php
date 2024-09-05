51ac34742166d7ca9082809c114c5ec34021817f
lihuifang <lihuifang@easycorp.ltd>
2024-09-05 16:34:48 +0800

    +[task#127645,doing,5h,4h] Add team page file of project managemembers.

---
+++ module/project/test/ui/page/team.php
@@ -0,0 +1,19 @@
+<?php
+class teamPage extends page
+{
+    public function __construct($webdriver)
+    {
+        parent::__construct($webdriver);
+
+        $xpath = array(
+            'browseFirAccount' => "//*[@id='table-project-team']/div[2]/div[1]/div/div/div/a",
+            'browseSecAccount' => "//*[@id='table-project-team']/div[2]/div[1]/div/div[8]/div/a",
+            'browseSecRole'    => "//*[@id='table-project-team']/div[2]/div/div/div[9]/div",
+            'browseSecDay'     => "//*[@id='table-project-team']/div[2]/div/div/div[11]/div",
+            'browseSecHours'   => "//*[@id='table-project-team']/div[2]/div/div/div[12]/div",
+            'amount'           => "//*[@id='featureBar']/menu/li/a/span[2]",
+            'teamBtn'          => "//*[@id='actionBar']/a/span",
+        );
+        $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
+    }
+}
