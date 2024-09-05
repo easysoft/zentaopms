cb588965a4219b8fc1fae2b3c064cb8f2c814b42
lijie <lijie@easycorp.ltd>
2024-09-05 16:58:30 +0800

    [task#12803,doing,1h] add a class.ui.php file that activates story function.

--- module/story/test/lib/activatestory.ui.class.php
+++ module/story/test/lib/activatestory.ui.class.php
@@ -17,3 +17,22 @@ class activateStoryTester extends tester
      * @param string $storyID
      * @access public
      * @return object
+     */
+    public function activateStory($storyID, $status)
+    {
+        $form = $this->openURL('story', 'view', array('id' => $storyID), 'appIframe-product');  //进入研发需求详情页
+        $form = $this->loadPage('story', 'view');
+        $form->dom->btn($this->lang->story->activate)->click();  //点击激活需求按钮
+
+        $form->wait(1);
+
+        $form->dom->assignedTo->picker('admin'); //选择指派人
+        $form->dom->activate->click();           //点击激活按钮
+        $form->wait(1);
+
+        $viewPage = $this->loadPage('story', 'view');   //进入需求详情页查看状态是否与关闭前一致
+        if($viewPage->dom->status->getText() != $status) return $this->failed('激活需求后状态不正确');
+
+        return $this->success('激活需求成功');
+    }
+}
