96d535bf59452965ac4296ea1fad23233f5cb82e
yulujie <yulujie@easycorp.ltd>
2024-09-05 10:30:02 +0800

    * [task#127261,doing,0.5h,5h] Modify function of ui test to batchedit execution.

--- module/execution/test/lib/batchedit.ui.class.php
+++ module/execution/test/lib/batchedit.ui.class.php
@@ -9,17 +9,18 @@ class batchEditTester extends tester
      * @param array $execution
      * @access public
      */
-    public function inputForm($execution)
+    public function inputFields($execution)
     {
         $form = $this->initForm('execution', 'all', '', 'appIframe-execution');
         $form->dom->firstCheckbox->click();
         $form->dom->btn($this->lang->edit)->click();
 
-        $form = $this->loadPage('execution', 'batchEdit');
-        $id = $form->dom->id_static_0->getValue();
-        if(isset($execution['name']))  $form->dom->name_0->setValue($execution['name']);
-        if(isset($execution['begin'])) $form->dom->begin[{$id}]->datePicker($execution['begin']);
-        if(isset($execution['end']))   $form->dom->end[{$id}]->datePicker($execution['end']);
+        $batchEditForm = $this->loadPage('execution', 'batchEdit');
+        $id = $batchEditForm->dom->id_static_0->getText();
+        if(isset($execution['name']))  $batchEditForm->dom->name_0->setValue($execution['name']);
+        if(isset($execution['begin'])) $batchEditForm->dom->begin[$id]->datePicker($execution['begin']);
+        if(isset($execution['end']))   $batchEditForm->dom->end[$id]->datePicker($execution['end']);
+        $batchEditForm->wait(1);
         $form->dom->btn($this->lang->save)->click();
     }
 }
