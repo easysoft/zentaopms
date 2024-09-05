5b04d6e99a7381e3e94060dceaa909161dd051b8
lijie <lijie@easycorp.ltd>
2024-09-05 17:05:18 +0800

    [task#127803, doing, 1h] add the data by zendata crate.

--- module/story/test/ui/activatestory.php
+++ module/story/test/ui/activatestory.php
@@ -18,3 +18,23 @@ chdir (__DIR__);
 include '../lib/activatestory.ui.class.php';
 
 $product = zenData('product');
+$product->id->range('1');
+$product->program->range('0');
+$product->name->range('产品1');
+$product->shadow->range('0');
+$product->bind->range('0');
+$product->acl->range('open');
+$product->createdBy->range('admin');
+$product->vision->range('rnd');
+$product->gen(1);
+
+$story = zenData('story');
+$story->id->range('1-2');
+$story->root->range('1-2');
+$story->path->range('`,1,`, `,2,`');
+$story->grade->range('1');
+$story->product->range('1');
+$story->module->range('0');
+$story->title->range('激活研发需求, 草稿研发需求');
+$story->type->range('story');
+$story->stage->range('closed');
