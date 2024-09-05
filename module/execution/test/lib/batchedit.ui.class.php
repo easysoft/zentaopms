9355caa24ee049cb6079e34354d168c655057111
yulujie <yulujie@easycorp.ltd>
2024-09-05 15:07:02 +0800

    * [task#127261,doing,1h,5h] Add function of ui test to batchedit execution.

--- module/execution/test/lib/batchedit.ui.class.php
+++ module/execution/test/lib/batchedit.ui.class.php
@@ -6,8 +6,9 @@ class batchEditTester extends tester
      * 批量编辑页面表单。
      * Batch edit page form.
      *
-     * @param array $execution
+     * @param  array  $execution
      * @access public
+     * @return string
      */
     public function inputFields($execution)
     {
@@ -17,10 +18,38 @@ class batchEditTester extends tester
 
         $batchEditForm = $this->loadPage('execution', 'batchEdit');
         $id = $batchEditForm->dom->id_static_0->getText();
+        $beginDom = "begin[{$id}]";
+        $endDom   = "end[{$id}]";
         if(isset($execution['name']))  $batchEditForm->dom->name_0->setValue($execution['name']);
-        if(isset($execution['begin'])) $batchEditForm->dom->begin[$id]->datePicker($execution['begin']);
-        if(isset($execution['end']))   $batchEditForm->dom->end[$id]->datePicker($execution['end']);
-        $batchEditForm->dom->btn($this->lang->save)->click();
+        if(isset($execution['begin'])) $batchEditForm->dom->$beginDom->datePicker($execution['begin']);
+        if(isset($execution['end']))   $batchEditForm->dom->$endDom->datePicker($execution['end']);
+        $batchEditForm->dom->submitBtn->click();
         $batchEditForm->wait(1);
+        return $id;
+    }
+
+    /**
+     * 检查执行名称字段。
+     * Check execution name field.
+     *
+     * @param  array  $execution
+     * @access public
+     * @return object
+     */
+    public function checkName($execution)
+    {
+        $this->inputFields($execution);
+        $form = $this->loadPage();
+
+        $text = $form->dom->alertModal('text');
+        if($execution['name'] =='')
+        {
+            $info = sprintf($this->lang->error->notempty, $this->lang->execution->name);
+            if($text == $info) return $this->success('执行名称为空提示信息正确');
+            return $this->failed('执行名称为空提示信息不正确');
+        }
+        $info = sprintf($this->lang->error->repeat, $this->lang->execution->name, $execution['name']);
+        if($text == $info) return $this->success('执行名称重复提示信息正确');
+        return $this->failed('执行名称重复提示信息不正确');
     }
 }
