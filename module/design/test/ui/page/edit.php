9610e46d233a44e99830a177b0c214cc7e052bdb
huairong <huairong@easycorp.ltd>
2024-09-06 09:28:38 +0800

    + [task#127604] Add ui test file of edit page.

---
+++ module/design/test/ui/page/edit.php
@@ -0,0 +1,13 @@
+<?php
+class editPage extends page
+{
+    public function __construct($webdriver)
+    {
+        parent::__construct($webdriver);
+        $xpath = array(
+            'type'    => "//*[@id='zin_design_edit_1_form']/div[4]/div/div/input",
+            'product' => "//*[@id='zin_design_edit_1_form']/div[2]/div/div/input",
+        );
+        $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
+    }
+}
