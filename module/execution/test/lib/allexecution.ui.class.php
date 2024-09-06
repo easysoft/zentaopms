804a77348b81826384d162da2c2016c09c659aa2
yulujie <yulujie@easycorp.ltd>
2024-09-06 14:53:50 +0800

    * [task#128088,doing,1h,2h] Add function.

--- module/execution/test/lib/allexecution.ui.class.php
+++ module/execution/test/lib/allexecution.ui.class.php
@@ -20,4 +20,26 @@ class allExecutionTester extends tester
         if($form->dom->num->getText() == $expectNum) return $this->success($tab . '标签下显示条数正确');
         return $this->failed($tab . '标签下显示条数不正确');
     }
+
+    /**
+     * 批量操作执行状态。
+     * Batch operation execution status.
+     *
+     * @param  string $status wait|doing|suspended|closed
+     * @access public
+     * @return object
+     */
+    public function changeStatus($status)
+    {
+        $form = $this->initForm('execution', 'all', '', 'appIframe-execution');
+        $firstId = $form->dom->firstId->getText();
+        $form->dom->firstCheckbox->click();
+        $form->dom->statusBtn->click();
+        $form->wait(1);
+        $form->dom->$status->click();
+
+        $viewForm = $this->initForm('execution', 'view', array('execution' => $firstId), 'appIframe-execution');
+        if($viewForm->dom->status->getText() == $this->lang->execution->statusList->$status) return $this->success("批量操作执行状态为{$status}成功");
+        return $this->failed("批量操作执行状态为{$status}失败");
+    }
 }
