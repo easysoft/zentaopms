0636fc58aa49242a15796d183416e00e48ff1133
daitingting <daitingting@xirangit.com>
2024-09-06 08:18:35 +0000

    * [task#127492,done,0.5h] Adjust sql.

--- module/bi/config/pivots.php
+++ module/bi/config/pivots.php
@@ -2164,9 +2164,12 @@ $config->bi->builtin->pivots[] = array
 select t1.actor,LEFT(t1.`date`,10) as `day` from zt_action t1
 left join zt_user as t2 on t1.actor = t2.account
 where t1.`action`='login'
-and LEFT(t1.`date`, 10)>=cast(\$startDate as date)
-and LEFT(t1.`date`, 10)<=cast(\$endDate as date)
-and t2.`dept` = \$dept
+and if
+(
+    \$startDate = '' && \$endDate = '' && \$dept = '',
+    1=2,
+    (LEFT(t1.`date`, 10)>=cast(\$startDate as date) and LEFT(t1.`date`, 10)<=cast(\$endDate as date) and t2.`dept` = \$dept)
+)
 order by t1.`date` asc, t1.actor asc
 EOT,
     'settings'  => array
