09419f91e97e915e7aae15c26ef22ed0ad8a6618
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-09-06 09:04:23 +0000

    *[task#128109 ,doing,0.5h,2h] Add  uitest function files of delete productline

--- module/product/test/lib/manageline.ui.class.php
+++ module/product/test/lib/manageline.ui.class.php
@@ -2,6 +2,13 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class batchEditProduct extends tester
 {
+    /*
+     * 创建产品线
+     * create product line
+     *
+     * @param  $line
+     * @return mixed
+     */
     public function createLine($line)
     {
         $form = $this->initForm('product', 'all', array(), 'appIframe-product');
@@ -13,5 +20,13 @@ class batchEditProduct extends tester
         $form->dom->manageLineBtn->click();
         return ($form->dom->lineName->getValue() == $line->name) ? $this->success('产品线名称正确') : $this->failed('产品线名称不正确');
         return ($form->dom->lineprogram->getValue() == $line->program) ? $this->success('所属项目集正确') : $this->failed('所属项目集不正确');
+    }
+
+    public function delLine()
+    {
+        $form = $this->initForm('product', 'all', array(), 'appIframe-product');
+        $form->dom->manageLineBtn->click();
+        $form->dom->delLineBtn->click();
+        return ($form->dom->modules[0]->getValue() == '') ? $this->success('删除成功') : $this->failed('删除失败');
     }
 }
