5a2d359564809709c869b3c4bcf6bc13949a5bbe
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-09-06 09:03:43 +0000

    *[task#128109 ,doing,1h,3h] Add dom of create manageline

--- module/product/test/ui/page/all.php
+++ module/product/test/ui/page/all.php
@@ -9,6 +9,12 @@ class allPage extends page
             'batchEditBtn'  => "//*[@id='products']/div[3]/nav[1]/a",
             'allProductTab' => "//*[@id='featureBar']/menu/li[1]/a/span[1]",
             'saveBtn'       => "//*[@id='zin_product_batchedit_formBatch']/div[2]/button[1]",
+            /*维护产线*/
+            'manageLineBtn' => "//*[@id='actionBar']/button[2]",
+            'lineName'      => "//*[@id='zin_product_manageline_form']/div[2]/div/div/input",
+            'lineprogram'   => "//*[@id='zin_product_manageline_form']/div[2]/div/div[2]/div/div/input",
+            'delLineBtn'    => "//*[@id='treeEditor-product-manageline']/li[1]/div/nav/a",
+            'confirm'       => "//button[@z-key='confirm']",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
