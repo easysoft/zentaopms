3b1b89b60c415aa5a82862fc4c3e8e830f43320d
lihuifang <lihuifang@easycorp.ltd>
2024-09-06 10:00:46 +0800

    +[task#127644,doing,3h,0h] Add ui function files of project team.

--- module/project/test/lib/team.ui.class.php
+++ module/project/test/lib/team.ui.class.php
@@ -13,3 +13,11 @@ class teamTester extends tester
         $form              = $this->initForm('project', 'team', array('projectID' => '1'), 'appIframe-project');
         $browseFirAccount1 = $form->dom->browseFirAccount->getText();
         $form->dom->unlinkBtn->click();
+        $form->dom->confirmBtn->click();
+        $form->wait(2);
+
+        //添加断言，判断是否移除团队成员成功
+        if($form->dom->browseFirAccount->getText() == $browseFirAccount1) return $this->failed('项目团队成员移除失败');
+        return $this->success('项目团队成员移除成功');
+    }
+}
