1356e40879e5bdfc60936a6864aac4d5dd6843ed
huairong <huairong@easycorp.ltd>
2024-09-06 09:24:58 +0800

    + [task#127604] Add ui test file of checkResult function.

--- module/design/test/lib/editdesign.ui.class.php
+++ module/design/test/lib/editdesign.ui.class.php
@@ -23,3 +23,22 @@ class editDesignTester extends tester
 
         return $this->checkResult($design);
     }
+
+    public function checkResult(array $design)
+    {
+        if($this->response('method') != 'view')
+        {
+            if($this->checkFormTips('design')) return $this->success('编辑设计表单页提示信息正确');
+            return $this->failed('编辑设计表单页提示信息不正确');
+
+        }
+
+        /* 跳转到设计详情页，检查设计字段信息。 */
+        $viewPage = $this->loadPage('design', 'view');
+        $viewPage->wait(2);
+        if($viewPage->dom->designName->getText()    != $design['name']) return $this->failed('设计名称错误');
+        if($viewPage->dom->linkedProduct->getText() != $design['product']) return $this->failed('所属产品错误');
+
+        return $this->success();
+    }
+}
