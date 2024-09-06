1288024cad69f8c5654933d05f4351573b241e3c
yulujie <yulujie@easycorp.ltd>
2024-09-06 14:54:16 +0800

    * [task#128088,done,2h] Add cases.

--- module/execution/test/ui/allexecution.php
+++ module/execution/test/ui/allexecution.php
@@ -43,10 +43,16 @@ $execution->gen(5, false);
 $tester = new allExecutionTester();
 $tester->login();
 
+/* 检查标签下显示条数 */
 r($tester->checkTab('all', '5'))       && p('message') && e('all标签下显示条数正确');
 r($tester->checkTab('undone', '4'))    && p('message') && e('undone标签下显示条数正确');
 r($tester->checkTab('wait', '2'))      && p('message') && e('wait标签下显示条数正确');
 r($tester->checkTab('doing', '1'))     && p('message') && e('doing标签下显示条数正确');
 r($tester->checkTab('suspended', '1')) && p('message') && e('suspended标签下显示条数正确');
 r($tester->checkTab('closed', '1'))    && p('message') && e('closed标签下显示条数正确');
+/* 批量操作执行状态 */
+r($tester->changeStatus('wait'))      && p('message') && e('批量操作执行状态为wait成功');
+r($tester->changeStatus('doing'))     && p('message') && e('批量操作执行状态为doing成功');
+r($tester->changeStatus('suspended')) && p('message') && e('批量操作执行状态为suspended成功');
+r($tester->changeStatus('closed'))    && p('message') && e('批量操作执行状态为closed成功');
 $tester->closeBrowser();
--- module/execution/test/ui/page/all.php
+++ module/execution/test/ui/page/all.php
@@ -7,8 +7,8 @@ class allPage extends page
         $xpath = array(
             /* 列表字段 */
             'firstCheckbox' => "//*[@id='table-execution-all']/div[2]/div[1]/div/div[1]/div/div",
+            'firstId'       => "//*[@id='table-execution-all']/div[2]/div[1]/div/div[1]/div",
             'firstName'     => "//*[@id='table-execution-all']/div[2]/div[1]/div/div[2]/div/div/div/a",
-            'firstStatus'   => "//*[@id='table-execution-all']/div[2]/div[2]/div/div[2]/div",
             'firstBegin'    => "//*[@id='table-execution-all']/div[2]/div[2]/div/div[4]/div",
             'firstEnd'      => "//*[@id='table-execution-all']/div[2]/div[2]/div/div[5]/div",
             /* 标签 */
