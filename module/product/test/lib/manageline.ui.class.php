d78df0b4ebfea06ed656f1bff3b6e90318215a39
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-09-09 01:26:39 +0000

    *[task#128109 ,doing,1h,2h] Add  uitest files of manage productline

--- module/product/test/lib/manageline.ui.class.php
+++ module/product/test/lib/manageline.ui.class.php
@@ -1,8 +1,8 @@
 <?php
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
-class batchEditProduct extends tester
+class manageLineTester extends tester
 {
-    /*
+    /**
      * 创建产品线
      * create product line
      *
@@ -17,11 +17,24 @@ class batchEditProduct extends tester
         if (isset($line->name))    $form->dom->lineName->setValue($line->name);
         if (isset($line->program)) $form->dom->lineprogram->setValue($line->program);
         $form->dom->btn($this->lang->save)->click();
-        $form->dom->manageLineBtn->click();
-        return ($form->dom->lineName->getValue() == $line->name) ? $this->success('产品线名称正确') : $this->failed('产品线名称不正确');
-        return ($form->dom->lineprogram->getValue() == $line->program) ? $this->success('所属项目集正确') : $this->failed('所属项目集不正确');
+        if ($form->dom->lineDialog === false)
+        {
+            if ($this->checkFormTips('product')) return $this->success('维护产品线表单页提示信息正确');
+        }
+        else
+        {
+            $form->dom->manageLineBtn->click();
+            return ($form->dom->lineName->getValue() == $line->name) ? $this->success('产品线名称正确') : $this->failed('产品线名称不正确');
+            return ($form->dom->lineprogram->getValue() == $line->program) ? $this->success('所属项目集正确') : $this->failed('所属项目集不正确');
+        }
     }
 
+    /**
+     * 删除产品线
+     * delete product line
+     *
+     * @return mixed
+     */
     public function delLine()
     {
         $form = $this->initForm('product', 'all', array(), 'appIframe-product');
