6197c52416b8929ffe4597c9a4d1132315b1e589
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-09-09 14:31:45 +0000

    *[task#127579,doing,0.3h] Add ui test files of edit a doclib

--- module/doc/test/lib/editdoclib.ui.class.php
+++ module/doc/test/lib/editdoclib.ui.class.php
@@ -16,5 +16,21 @@ class createDocTester extends tester
         $this->openUrl('doc', 'mySpace', array('type' => 'mine'));
         $form = $this->loadPage('doc', 'mySpace', array('type' => 'mine'));
         $form->dom->createLibBtn->click();
+        $form->wait(1);
+        $form->dom->name->setValue($libName->libName);
+        $form->dom->btn($this->lang->save)->click();
+        $form->wait(1);
+
+        /*编辑文档库*/
+        $this->openUrl('doc', 'mySpace', array('type' => 'mine'));
+        $form = $this->loadPage('doc', 'mySpace', array('type' => 'mine'));
+        $form->dom->fstMoreBtn->click();
+        $form->dom->editLib->click();
+        $form->wait(1);
+        $form->dom->name->setValue($editLibName->editName);
+        $form->dom->btn($this->lang->save)->click();
+        if($form->dom->fstDocLib->getText() != $editLibName->editName) return $this->failed('编辑文档库失败。');
+        $this->openUrl('doc', 'mySpace');
+        return $this->success('编辑文档库成功。');
     }
 }
