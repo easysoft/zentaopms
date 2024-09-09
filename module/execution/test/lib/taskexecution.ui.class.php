59bdbd958919f76e84cbd5b35ffa529be3f26643
yulujie <yulujie@easycorp.ltd>
2024-09-09 17:20:44 +0800

    * [task#128116,1h,4h] Add function.

---
+++ module/execution/test/lib/taskexecution.ui.class.php
@@ -0,0 +1,25 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+class taskExecutionTester extends tester
+{
+    /**
+     * 检查Tab标签下的数据。
+     * Check the data of the Tab tag.
+     *
+     * @param  string $tab
+     * @param  string $expectNum
+     * @access public
+     * @return object
+     */
+    public function checkTab($tab, $expectNum)
+    {
+        $form = $this->initForm('execution', 'task', array('execution' => '2'), 'appIframe-execution');
+        $params = array('allTab', 'unclosedTab', 'myTab', 'involvedTab','assignedByMeTab', 'changedByMeTab');
+        if(!in_array($tab, $params)) $form->dom->MoreTab->click();
+        $form->wait(1);
+        $form->dom->$tab->click();
+        $form->wait(1);
+        if($form->dom->num->getText() == $expectNum) return $this->success($tab . '下显示条数正确');
+        return $this->failed($tab . '下显示条数不正确');
+    }
+}
