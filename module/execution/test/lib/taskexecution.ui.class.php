8dee45f96c7f57f7f9138235fe5146f3b7f4a337
yulujie <yulujie@easycorp.ltd>
2024-09-10 13:44:48 +0800

    * [tsk#128116,2h,2h] Add new function.

--- module/execution/test/lib/taskexecution.ui.class.php
+++ module/execution/test/lib/taskexecution.ui.class.php
@@ -22,4 +22,37 @@ class taskExecutionTester extends tester
         if($form->dom->num->getText() == $expectNum) return $this->success($tab . '下显示条数正确');
         return $this->failed($tab . '下显示条数不正确');
     }
+
+    /**
+     * 批量编辑状态
+     * Batch edit status
+     *
+     * @param  string $status closed|cancel
+     * @access public
+     * @return object
+     */
+    public function batchEditStatus($status)
+    {
+        $form = $this->initForm('execution', 'task', array('execution' => '2'), 'appIframe-execution');
+        $name = $form->dom->firstName->getText();
+        $btn  = $status . 'Btn';
+        $form->dom->firstCheckbox->click();
+        $form->dom->statusBtn->click();
+        $form->wait(1);
+        $form->dom->$btn->click();
+        $form->wait(1);
+        try
+        {
+            $form->dom->alertModal();
+        }
+        catch(Exception $e)
+        {
+        }
+        $form->wait(1);
+        $form->dom->search(array("{$this->lang->task->name},=,{$name}"));
+        $form->wait(1);
+        $statusAfter = $form->dom->firstStatus->getText();
+        if($this->lang->task->statusList->$status == $statusAfter) return $this->success("批量修改状态为{$status}成功");
+        return $this->failed("批量修改状态为{$status}失败");
+    }
 }
