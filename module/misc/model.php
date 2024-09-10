e0cef7385956b3c3d9da45b4c740aea2fb460543
zhaixiaojian <zhaixiaojian@easycorp.ltd>
2024-09-10 18:05:40 +0800

    * [task#128241,done,1h,0h] Fill in the SN statistical fields.

--- module/misc/model.php
+++ module/misc/model.php
@@ -153,4 +153,134 @@ class miscModel extends model
         }
         return $remind;
     }
+
+
+    /**
+     * 获取最近版本列表.
+     * Get the latest version list
+     * @param  string       $url
+     * @param  string|array $data
+     * @param  array        $options   This is option and value pair, like CURLOPT_HEADER => true. Use curl_setopt function to set options.
+     * @param  array        $headers   Set request headers.
+     * @param  string       $dataType
+     * @param  string       $method    POST|PATCH|PUT
+     * @param  int          $timeout
+     * @param  bool         $httpCode
+     * @param  bool         $log
+     * @static
+     * @access public
+     * @return string
+     */
+    public function getLatestVersionList($url, $data = null, $options = array(), $headers = array(), $dataType = 'data', $method = 'POST', $timeout = 30, $httpCode = false, $log = true)
+    {
+        /* Module detection. */
+        global $lang, $app;
+        if(!extension_loaded('curl'))
+        {
+            if($dataType == 'json') return print($lang->error->noCurlExt);
+            return json_encode(array('result' => 'fail', 'message' => $lang->error->noCurlExt));
+        }
+
+
+        /* Set HTTP request header. */
+        commonModel::$requestErrors = array();
+        $requestType = 'GET';
+        if(!is_array($headers)) $headers = (array)$headers;
+
+        $headers[] = 'API-RemoteIP: ' . helper::getRemoteIp(); /* Real IP of real user. */
+        $headers[] = 'API-LocalIP: ' . zget($_SERVER, 'SERVER_ADDR', ''); /* Server IP of self. */
+
+        if($dataType == 'json')
+        {
+            $headers[] = 'Content-Type: application/json;charset=utf-8';
+            if(!empty($data)) $data = json_encode($data);
+        }
+
+        /* Set curl configuration options. */
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'ZenTao PMS ' . $app->config->version);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_ENCODING,'');
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+        curl_setopt($curl, CURLOPT_HEADER, $httpCode);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 2);
+        curl_setopt($curl, CURLINFO_HEADER_OUT, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_REFERER, $_SERVER['HTTP_REFERER']);
+        curl_setopt($curl, CURLOPT_COOKIE, $_SERVER['HTTP_COOKIE']);
+
+        if(!empty($data))
+        {
+            if(is_object($data)) $data = (array) $data;
+            if($method == 'POST') curl_setopt($curl, CURLOPT_POST, true);
+            if(in_array($method, array('PATCH', 'PUT'))) curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+            $requestType = $method;
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        }
+
+        /* Send request. */
+        if($options) curl_setopt_array($curl, $options);
+        $response = curl_exec($curl);
+
+        $errno  = curl_errno($curl);
+        $errors = empty($errno)  ? 0 : curl_error($curl);
+        $info   = curl_getinfo($curl);
+
+        /* Processing response data. */
+        if($httpCode)
+        {
+            $httpCode     = $info['http_code'] ?? curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            $headerSize   = $info['header_size'] ?? curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+            $headerString = substr($response, 0, $headerSize);
+            $body         = substr($response, $headerSize);
+
+            /* Parse header. */
+            $header    = explode("\n", $headerString);
+            $newHeader = array();
+            foreach($header as $item)
+            {
+                $field = explode(':', $item);
+                if(count($field) < 2) continue;
+                $headerKey = array_shift($field);
+                $newHeader[$headerKey] = implode('', $field);
+            }
+        }
+
+        curl_close($curl);
+
+        if($log or $app->config->debug)
+        {
+            $runMode = PHP_SAPI == 'cli' ? '_cli' : '';
+            $logFile = $app->getLogRoot() . 'saas' . $runMode . '.' . date('Ymd') . '.log.php';
+            if(!file_exists($logFile)) file_put_contents($logFile, '<?php die(); ?' . '>');
+
+            $fh = fopen($logFile, 'a');
+            if($fh)
+            {
+                fwrite($fh, date('Ymd H:i:s') . ': ' . $app->getURI() . "\n");
+                fwrite($fh, "{$requestType} url:    " . $url . "\n");
+                if(!empty($data)) fwrite($fh, 'data:   ' . print_r($data, true) . "\n");
+                fwrite($fh, 'results:' . print_r($response, true) . "\n");
+
+                if(!empty($errors))
+                {
+                    fwrite($fh, 'errno: ' . $errno . "\n");
+                    fwrite($fh, 'errors: ' . $errors . "\n");
+                    fwrite($fh, 'info: ' . print_r($info, true) . "\n");
+                }
+
+                fclose($fh);
+            }
+        }
+
+        if($errors) commonModel::$requestErrors[] = $errors;
+
+        return $httpCode ? array($response, $httpCode, 'body' => $body, 'header' => $newHeader, 'errno' => $errno, 'info' => $info, 'response' => $response) : $response;
+    }
 }
