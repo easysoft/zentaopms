0bd32bd509de1b8e1349478892f51b86f5212668
lijie <lijie@easycorp.ltd>
2024-09-10 08:48:28 +0800

    [task#127804, 1h] add assigntostory function.

--- module/story/test/lib/assigntostory.ui.class.php
+++ module/story/test/lib/assigntostory.ui.class.php
@@ -17,4 +17,21 @@ class assignToStoryTester extends tester
      * @access public
      * @return object
      */
+    public function assignToStory()
+    {
+        $form = $this->openURL('story', 'view', array('id' => 1), 'appIframe-product');  //进入研发需求列表
+        $form = $this->loadPage('story', 'view');
+        $form->dom->btn($this->lang->story->assignTo)->click();  //点击指派按钮
+        $form->wait(1);
+
+        $form->dom->assignedTo->picker('admin'); //指派人选择admin
+        $form->dom->assignToBtn->click();
+        $form->wait(1);
+
+        $viewPage = $this->loadPage('story', 'view');
+        $viewPage->dom->btn($this->lang->story->legendLifeTime)->click();
+        if(strpos($viewPage->dom->assignToONE->getText(), 'admin') === false)  return $this->failed('指派人不正确'); //检查需求详情页指派人是否正确
+
+        return $this->success('指派需求成功');
+    }
 }
