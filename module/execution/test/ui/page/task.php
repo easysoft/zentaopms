90f0b57f110624705fda629a3f85d6d70f39553a
yulujie <yulujie@easycorp.ltd>
2024-09-10 13:45:14 +0800

    * [tsk#128116,0.5h,2h] Add new xpath.

--- module/execution/test/ui/page/task.php
+++ module/execution/test/ui/page/task.php
@@ -24,8 +24,13 @@ class taskPage extends page
             'num'             => "//*[@id='table-execution-task']/div[3]/div[2]/strong[1]",
             /* 列表 */
             'firstCheckbox' => "//*[@id='table-execution-task']/div[2]/div[1]/div/div[1]/div/div",
-            'firstId'       => "//*[@id='table-execution-task']/div[2]/div[1]/div/div[1]/div",
+            'firstName'     => "//*[@id='table-execution-task']/div[2]/div[1]/div/div[2]/div/a",
             'firstStatus'   => "//*[@id='table-execution-task']/div[2]/div[2]/div/div[2]/div/span",
+            'statusBtn'     => "//*[@id='table-execution-task']/div[3]/nav[1]/nav/button[2]",
+            'closedBtn'     => "(//*[@data-page='execution-task']/div[2]/menu/menu//div[@class='item-title'])[1]",
+            'cancelBtn'     => "(//*[@data-page='execution-task']/div[2]/menu/menu//div[@class='item-title'])[2]",
+            'assignedToBtn' => "//*[@id='table-execution-task']/div[3]/nav[1]/button[2]",
+            'users'         => "//*[@data-page='execution-task']/div[2]/menu/menu//div[@class='item-title' and text()='admin']",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
