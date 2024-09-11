2f6302fa65485e575331ff5f5b556100dc4e00dd
hufangzhou <hufangzhou@easycorp.ltd>
2024-09-11 08:52:46 +0800

    * [bug#55141,done,0.3h] Cannot perform other operations after deletion.

--- module/design/ui/view.html.php
+++ module/design/ui/view.html.php
@@ -13,14 +13,14 @@ namespace zin;
 jsVar('type', strtolower($design->type));
 detailHeader
 (
-    to::title(entityLabel(set(array('entityID' => $design->id, 'level' => 1, 'text' => $design->name))))
+    to::title(entityLabel(set(array('entityID' => $design->id, 'level' => 1, 'text' => $design->name))), $design->deleted ? span(setClass('label danger'), $lang->deleted) : null),
 );
 
 /* Construct suitable actions for the current task. */
 $operateMenus = array();
 foreach($config->design->view->operateList['main'] as $operate)
 {
-    if(!common::hasPriv('design', $operate)) continue;
+    if(!common::hasPriv('design', $operate) || $design->deleted) continue;
 
     if(empty($repos) && $operate == 'linkCommit')
     {
@@ -35,7 +35,7 @@ foreach($config->design->view->operateList['main'] as $operate)
 $commonActions = array();
 foreach($config->design->view->operateList['common'] as $operate)
 {
-    if(!common::hasPriv('design', $operate)) continue;
+    if(!common::hasPriv('design', $operate) || $design->deleted) continue;
     if($operate == 'delete') $config->design->actionList['delete']['class'] = 'ajax-submit';
     $commonActions[] = $config->design->actionList[$operate];
 }
