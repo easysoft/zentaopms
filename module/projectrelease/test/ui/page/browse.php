19672110f5f85bab185bead8c25662aec7da5d36
lihuifang <lihuifang@easycorp.ltd>
2024-09-11 08:47:34 +0800

    *[task#127641,doing,2h,1h] Add browse page file of project release view.

--- module/projectrelease/test/ui/page/browse.php
+++ module/projectrelease/test/ui/page/browse.php
@@ -5,17 +5,20 @@ class browsePage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'editBtn'          => "//*[@id='table-projectrelease-browse']/div[2]/div[3]/div/div/div/nav/a[4]/i",
-            'waitTab'          => "//*[@id='featureBar']/menu/li[2]/a/span",
-            'releasedTab'      => "//*[@id='featureBar']/menu/li[3]/a/span[1]",
-            'terminatedTab'    => "//*[@id='featureBar']/menu/li[4]/a/span",
-            'releaseBtn'       => "//*[@id='table-projectrelease-browse']/div[2]/div[3]/div/div/div/nav/a[3]/i",
-            'releaseSubmit'    => "//*[@id='zin_projectrelease_publish_form']/div[4]/div/button/span",
-            'terminateBtn'     => "//*[@class='dtable-cell-content']/nav/a[3]/i",
-            'terminateConfirm' => "//*[@class='modal-footer']/nav/button/span",
-            'activeBtn'        => "//*[@class='toolbar']/a[3]/i",
-            'activeConfirm'    => "//*[@class='modal-footer']/nav/button/span",
-            'releaseName'      => "//*[@class='dtable-cells-container']/div[2]/div/a",
+            'editBtn'             => "//*[@id='table-projectrelease-browse']/div[2]/div[3]/div/div/div/nav/a[4]/i",
+            'waitTab'             => "//*[@id='featureBar']/menu/li[2]/a/span",
+            'releasedTab'         => "//*[@id='featureBar']/menu/li[3]/a/span[1]",
+            'terminatedTab'       => "//*[@id='featureBar']/menu/li[4]/a/span",
+            'releaseBtn'          => "//*[@id='table-projectrelease-browse']/div[2]/div[3]/div/div/div/nav/a[3]/i",
+            'releaseSubmit'       => "//*[@id='zin_projectrelease_publish_form']/div[4]/div/button/span",
+            'terminateBtn'        => "//*[@class='dtable-cell-content']/nav/a[3]/i",
+            'terminateConfirm'    => "//*[@class='modal-footer']/nav/button/span",
+            'activeBtn'           => "//*[@class='toolbar']/a[3]/i",
+            'activeConfirm'       => "//*[@class='modal-footer']/nav/button/span",
+            'releaseName'         => "//*[@class='dtable-cells-container']/div[2]/div/a",
+            'releaseNameBrowse'   => "//*[@id='table-projectrelease-browse']/div[2]/div/div/div[2]/div/a",
+            'releaseStatusBrowse' => "//*[@id='table-projectrelease-browse']/div[2]/div[2]/div/div[3]/div/span",
+            'planDateBrowse'      => "//*[@id='table-projectrelease-browse']/div[2]/div[2]/div/div[4]/div",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
