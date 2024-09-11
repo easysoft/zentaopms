23ba454fc2b4e36825d285e00d1d7637df0121c8
lihuifang <lihuifang@easycorp.ltd>
2024-09-11 08:49:51 +0800

    +[task#127641,done,2h,0h] Add ui function files of project release view.

--- module/projectrelease/test/lib/view.ui.class.php
+++ module/projectrelease/test/lib/view.ui.class.php
@@ -16,3 +16,17 @@ class viewTester extends tester
          $releaseStatusBrowse = $browsePage->dom->releaseStatusBrowse->getText();
          $planDateBrowse      = $browsePage->dom->planDateBrowse->getText();
          $browsePage->dom->releaseNameBrowse->click();
+         //在详情页面，检查字段信息显示是否正确
+         $viewPage = $this->initForm('projectrelease', 'view', array('projectID' => 1), 'appIframe-project');
+         $viewPage->dom->basic->click();
+         $basicReleaseName = $viewPage->dom->basicreleasename->getText();
+         $basicStatus      = $viewPage->dom->basicstatus->getText();
+         $basicPlanDate    = $viewPage->dom->basicplandate->getText();
+
+         //断言检查发布是否发布成功
+         if($releaseNameBrowse != $basicReleaseName) return $this->failed('项目发布详情名称显示不正确');
+         if($releaseStatusBrowse != $basicStatus)    return $this->failed('项目发布详情状态显示不正确');
+         if($planDateBrowse != $basicPlanDate)       return $this->failed('项目发布详情计划时间显示不正确');
+         return $this->success('项目发布详情查看成功');
+     }
+}
