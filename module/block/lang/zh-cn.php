3256393502034f276387399e26fb972b3c99c904
xieqiyu <xieqiyu@easycorp.ltd>
2024-09-11 17:35:21 +0800

    * [bug#55147] Change the language for tutorial in block.

--- module/block/lang/zh-cn.php
+++ module/block/lang/zh-cn.php
@@ -104,7 +104,7 @@ $lang->block->dynamicInfo        = "<span class='timeline-tag'>%s</span> <span c
 $lang->block->noLinkDynamic      = "<span class='timeline-tag'>%s</span> <span class='timeline-text' title='%s'>%s<span class='label-action'>%s</span>%s<span class='label-name'>%s</span></span>";
 $lang->block->cannotPlaceInLeft  = '此区块无法放置在左侧。';
 $lang->block->cannotPlaceInRight = '此区块无法放置在右侧。';
-$lang->block->tutorial           = '进入新手教程';
+$lang->block->tutorial           = '进入使用教程';
 $lang->block->filterProject      = "{$lang->projectCommon}筛选";
 
 $lang->block->productName   = $lang->productCommon . '名称';
