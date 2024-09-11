82396ba151a1ff52a39f56ce248174b3d8f94fe7
liumengyi <liumengyi@easycorp.ltd>
2024-09-11 11:08:50 +0800

    * [bug#54362,done,0.3h] process dropdown value.

--- module/transfer/model.php
+++ module/transfer/model.php
@@ -730,7 +730,8 @@ class transferModel extends model
 
                 /* 如果字段是下拉字段并且在excel里不是下拉框的形式时，根据fieldList->value查找value。*/
                 /* If the field is a dropdown field and the value in excel is not a dropdown box, the value is found by fieldList->value. */
-                if(!in_array($control, array('select', 'multiple', 'picker'))) continue;
+                if(!isset($control['control']) && !in_array($control, array('select', 'multiple', 'picker'))) continue;
+                if(isset($control['control']) && !in_array($control['control'], array('select', 'multiple', 'picker'))) continue;
                 $rows[$key]->$field = $this->transferTao->extractElements((string) $cellValue, $field, $fieldList[$field]['items']);
             }
         }
