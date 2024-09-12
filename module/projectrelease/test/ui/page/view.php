b50f0d02b4881f0af49f615d931dbf7cf719712a
lihuifang <lihuifang@easycorp.ltd>
2024-09-12 14:17:04 +0800

    +[task#127640,doing,5h,3h] Add view page of projectrelease link story.

--- module/projectrelease/test/ui/page/view.php
+++ module/projectrelease/test/ui/page/view.php
@@ -5,11 +5,20 @@ class viewPage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'basic'            => "//*[@id='mainContent']/div[2]/div/div/div/div/ul/li[4]/a/span",
-            'basicreleasename' => "//*[@id='releaseInfo']/div[2]/div[1]/table/tbody/tr[2]/td",
-            'basicstatus'      => "//*[@id='releaseInfo']/div[2]/div[1]/table/tbody/tr[4]/td",
-            'basicplandate'    => "//*[@id='releaseInfo']/div[2]/div[1]/table/tbody/tr[5]/td",
-            'basicreleasedate' => "//*[@id='releaseInfo']/div[2]/div[1]/table/tbody/tr[6]/td",
+            'basic'               => "//*[@id='mainContent']/div[2]/div/div/div/div/ul/li[4]/a/span",
+            'basicreleasename'    => "//*[@id='releaseInfo']/div[2]/div[1]/table/tbody/tr[2]/td",
+            'basicstatus'         => "//*[@id='releaseInfo']/div[2]/div[1]/table/tbody/tr[4]/td",
+            'basicplandate'       => "//*[@id='releaseInfo']/div[2]/div[1]/table/tbody/tr[5]/td",
+            'linkStoryBtn'        => "//*[@id='mainContent']/div[2]/div/div/div/div[2]/div/div/button/span",
+            'linkStoryBtnBottom'  => "//*[@id='unlinkStoryList']/div[3]/nav/button/span",
+            'confirm'             => "//div[@class='modal']//button[text()='确定']",
+            'basicreleasedate'    => "//*[@id='releaseInfo']/div[2]/div[1]/table/tbody/tr[6]/td",
+            'searchBtn'           => "//*[@class='btn primary']",
+            'selectAllStory'      => "//*[@id='unlinkStoryList']/div[3]/div/div/label",
+            'finishedStoryNum'    => "//*[@id='finishedStoryDTable']/div[3]/div[2]/strong[1]",
+            'unlinkFirBtn'        => "//*[@id='finishedStoryDTable']/div[2]/div[3]/div/div/div/nav/a/i",
+            'allFinishedStoryBtn' => "//*[@id='finishedStoryDTable']/div[3]/div/div/label",
+            'batchUnlinkBtn'      => "//*[@id='finishedStoryDTable']/div[3]/nav[1]/button[1]/span",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
