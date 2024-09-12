db4a25e77ad2cdc2b00e6b0c81522fcf4868d521
lihuifang <lihuifang@easycorp.ltd>
2024-09-12 14:14:49 +0800

    *[task#127640,doing,5h,4h] Add ui function files of projectrelease link story.

--- module/projectrelease/test/lib/releaselinkstory.ui.class.php
+++ module/projectrelease/test/lib/releaselinkstory.ui.class.php
@@ -18,3 +18,23 @@ class releaseLinkStoryTester extends tester
         $form->dom->selectAllStory->click();//点击全选按钮
         $form->dom->linkStoryBtnBottom->click();
         //断言检查发布关联需求数量是否成功
+        $viewPage = $this->initForm('projectrelease', 'view', array('projectID' => 1), 'appIframe-project');
+        return ($viewPage->dom->finishedStoryNum === '0') ? $this->failed('发布关联需求失败') : $this->success('发布关联需求成功');
+    }
+
+    /**
+     * Unlink a story of project release.
+     * 发布单个移除需求
+     *
+     * @access public
+     * @return object
+     */
+    public function unlinkStory()
+    {
+        $form = $this->initForm('projectrelease', 'view', array('releaseID' => 1), 'appIframe-project');
+        $linkNumBefore = $form->dom->finishedStoryNum->getText();//记录移除需求前发布关联的需求数量
+        $form->dom->unlinkFirBtn->click();//点击第一行的单个移除按钮
+        $form->wait(1);
+        $form->dom->alertModal();//模态框中点击确定
+        $form->wait(2);
+        $linkNumAfter = $form->dom->finishedStoryNum->getText();//记录移除需求后发布关联的需求数量
