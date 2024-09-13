565129f6c1fad7b0c0f8f38c15505336d1acebed
lijie <lijie@easycorp.ltd>
2024-09-13 09:19:08 +0800

    [task#127805, 0.5h] add uifunction file of review story.

--- module/story/test/lib/reviewstory.ui.class.php
+++ module/story/test/lib/reviewstory.ui.class.php
@@ -17,4 +17,23 @@ class reviewStoryTester extends tester
      * @access public
      * @return object
      */
+    public function reviewStory($result, $status)
+    {
+        $form = $this->openURL('story', 'review', array('id' => 1), 'appIframe-product');  //进入研发评审页面
+        $form = $this->loadPage('story', 'review');
+
+        $form->dom->result->picker($result); //选择研发评审结果
+        $form->dom->assignedTo->picker('admin'); //指派人选择admin
+        $form->dom->btn($this->lang->save)->click();
+        $form->wait(1);
+
+        $viewPage = $this->loadPage('story', 'view');
+        $viewPage->wait(1);
+
+        if($viewPage->dom->status->getText() != $status) return $this->fail('需求状态错误');
+        $viewPage->dom->btn($this->lang->story->legendLifeTime)->click();
+        if($viewPage->dom->reviewer->getText() != 'admin') return $this->fail('需求评审人错误');
+
+        return $this->success('评审需求成功');
+    }
 }
