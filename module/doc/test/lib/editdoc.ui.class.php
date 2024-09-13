658f098f1a4caadb167cfc1beb9d6f9a5a7d1ea3
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-09-13 09:56:35 +0000

    *[task#127577,doing,0.3h] Add ui test files of doclist

--- module/doc/test/lib/editdoc.ui.class.php
+++ module/doc/test/lib/editdoc.ui.class.php
@@ -58,5 +58,18 @@ class createDocTester extends tester
         $form->dom->name->setValue($libName->myDocLib);
         $form->dom->btn($this->lang->save)->click();
         $form->wait(1);
+
+        /*移动文档*/
+        $this->openUrl('doc', 'mySpace', array('type' => 'mine'));
+        $form = $this->loadPage('doc', 'mySpace', array('type' => 'mine'));
+        $form->dom->fstMoveBtn->click();
+        $form->wait(1);
+        $form->dom->lib->picker($libName->myDocLib);
+        $form->dom->saveMoveBtn->click();
+        $form->wait(1);
+
+        if($form->dom->leftListHeader->getText() != $llibName->myDocLib) return $this->failed('文档移动失败');
+        $this->openUrl('doc', 'mySpace');
+        return $this->success('文档移动失败');
     }
 }
