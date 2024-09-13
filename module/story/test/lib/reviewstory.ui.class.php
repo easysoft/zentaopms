a62bca85dd7795d73eee70af6cf18881cf7b3dd0
lijie <lijie@easycorp.ltd>
2024-09-13 09:17:49 +0800

    [task#127805, 0.5h] add reviewstory ui function file.

---
+++ module/story/test/lib/reviewstory.ui.class.php
@@ -0,0 +1,20 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+/**
+    * The control file of example module of ZenTaoPMS.
+    *
+    * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+    * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+    * @author      lijie
+    * @package     story
+    * @link        http://www.zentao.net
+    */
+class reviewStoryTester extends tester
+{
+    /**
+     * check the reviewer and status after review story.
+     * @param string
+     * @access public
+     * @return object
+     */
+}
