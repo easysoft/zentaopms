c2e3415da730981f2c89a022cfbc7ba420ad385a
xieqiyu <xieqiyu@easycorp.ltd>
2024-09-18 16:33:46 +0800

    * [bug#48650] Set dropdown url for product in epic view or requirement view.

--- module/product/model.php
+++ module/product/model.php
@@ -2019,9 +2019,8 @@ class productModel extends model
         if($module == 'testtask'   && $method == 'browseunits') return helper::createLink($module, 'browseUnits', "productID=%s&browseType=newest&orderBy=id_desc&recTotal=0&recPerPage=0&pageID=1" . ($this->app->tab == 'project' ? "&projectID={$this->session->project}" : ''));
         if($module == 'testtask'   && $method == 'unitcases')   return helper::createLink($module, 'browseUnits', "productID=%s&browseType=newest&orderBy=id_desc&recTotal=0&recPerPage=0&pageID=1" . ($this->app->tab == 'project' ? "&projectID={$this->session->project}" : ''));
 
-        if(in_array($module, array('story', 'epic', 'requirement')) && in_array($method, array('create', 'edit', 'batchcreate'))) return helper::createLink('product', 'browse', "productID=%s&branch={$branchID}&browseType=" . ($this->config->vision == 'or' ? 'assignedtome' : 'unclosed') . "&param=0&storyType=$module");
+        if(in_array($module, array('story', 'epic', 'requirement')) && in_array($method, array('create', 'edit', 'batchcreate', 'view'))) return helper::createLink('product', 'browse', "productID=%s&branch={$branchID}&browseType=" . ($this->config->vision == 'or' ? 'assignedtome' : 'unclosed') . "&param=0&storyType=$module");
         if($module == 'product' && $method == 'browse')    return helper::createLink('product', 'browse', "productID=%s&branch={$branchID}&browseType=" . ($this->config->vision == 'or' ? 'assignedtome' : 'unclosed') . "&param=0&storyType=$extra");
-        if($module == 'story'   && $method == 'view')      return helper::createLink('product', 'browse', "productID=%s&branch={$branchID}");
         if($module == 'story'   && $method == 'storyview') return helper::createLink('product', 'browse', "productID=%s&branch={$branchID}");
         if($this->config->edition != 'open')
         {
