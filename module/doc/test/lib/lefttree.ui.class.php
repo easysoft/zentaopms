85a232ed7202c20339b8fbb28813e0594b2f754b
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-09-19 15:05:40 +0000

    *[task#128887,doing,0.2h] Add ui test files of check lefttree

--- module/doc/test/lib/lefttree.ui.class.php
+++ module/doc/test/lib/lefttree.ui.class.php
@@ -14,5 +14,19 @@ class createDocTester extends tester
     {
         /*进入我的空间创建并收藏一个文档*/
         $this->openUrl('doc', 'mySpace', array('type' => 'mine'));
+        $form = $this->loadPage('doc', 'mySpace', array('type' => 'mine'));
+        $form->dom->createDocBtn->click();
+        $form->wait(1);
+        $form->dom->showTitle->setValue($docName->dcName);
+        $form->dom->saveBtn->click();
+        $form->wait(1);
+        $form->dom->releaseBtn->click();
+
+        /*收藏文档*/
+        $this->openUrl('doc', 'mySpace', array('type' => 'mine'));
+        $form = $this->loadPage('doc', 'mySpace', array('type' => 'mine'));
+        $form->dom->fstSaveBtn->click();
+        $form->wait(1);
+        $form->dom->myFavorites->click();
     }
 }
