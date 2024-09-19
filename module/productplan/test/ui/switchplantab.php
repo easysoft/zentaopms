ba019bc82d283263e47a3187645fced1ae767de4
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-09-19 13:47:46 +0800

    *[task#128807,doing,0.1h,1h] add uitest case of switch planTab

--- module/productplan/test/ui/switchplantab.php
+++ module/productplan/test/ui/switchplantab.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 chdir(__DIR__);
-include '../lib/switchtab.ui.class.php';
+include '../lib/browse.ui.class.php';
 
 zendata('product')->loadYaml('product', false, 2)->gen(10);
 $productplan = zenData('productplan');
@@ -13,8 +13,33 @@ $productplan->begin->range('`2024-09-19`,`2024-09-20`,`2024-09-21`,`2024-09-22`,
 $productplan->end->range('`2024-10-19`,`2024-10-20`,`2024-10-21`,`2024-10-22`,`2024-10-23`,`2024-10-24`,`2024-10-25`');
 $productplan->status->range('wait{3},doing{2},done{1},closed{1}');
 $productplan->gen(7);
-$tester = new createPlanTester();
+
+$tester = new browseTester();
 $tester->login();
+$tabNum = new stdClass();
+
 $planurl['productID'] = 1;
-r($tester->switchTab()) && p('status') && e('SUCCESS');
+$tabName = 'all';
+$tabNum  = '7';
+r($tester->switchTab($planurl,$tabName,$tabNum)) && p('message,status') && e('切换成功,SUCCESS');
+
+$tabName = 'undone';
+$tabNum  = '5';
+r($tester->switchTab($planurl,$tabName,$tabNum)) && p('message,status') && e('切换成功,SUCCESS');
+
+$tabName = 'waiting';
+$tabNum  = '3';
+r($tester->switchTab($planurl,$tabName,$tabNum)) && p('message,status') && e('切换成功,SUCCESS');
+
+$tabName = 'doing';
+$tabNum  = '2';
+r($tester->switchTab($planurl,$tabName,$tabNum)) && p('message,status') && e('切换成功,SUCCESS');
+
+$tabName = 'done';
+$tabNum  = '1';
+r($tester->switchTab($planurl,$tabName,$tabNum)) && p('message,status') && e('切换成功,SUCCESS');
+
+$tabName = 'closed';
+$tabNum  = '1';
+r($tester->switchTab($planurl,$tabName,$tabNum)) && p('message,status') && e('切换成功,SUCCESS');
 $tester->closeBrowser();
