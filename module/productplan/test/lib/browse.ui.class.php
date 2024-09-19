2ccf12c48b5d0baea9102223b7c1e6c39d872143
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-09-19 14:04:47 +0800

    *[task#128807,doing,0.1h,2h] Add comments

--- module/productplan/test/lib/browse.ui.class.php
+++ module/productplan/test/lib/browse.ui.class.php
@@ -2,13 +2,23 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class browseTester extends tester
 {
+    /**
+     * 切换tab
+     * switch tab
+     *
+     * @param $planurl 产品ID
+     * @param $tabName tab名称
+     * @param $tabNum tab下计划数量
+     *
+     * @return mixed
+     */
     public function switchTab($planurl,$tabName,$tabNum)
     {
-        $tabDom  = $tabName.'Tab';
+        $tabDom     = $tabName.'Tab';
         $tabNumDom  = $tabName.'Num';
         $browsePage = $this->initForm('productplan', 'browse', $planurl, 'appIframe-product');
-        $browsePage->dom->$tabDom->click();
-        $num = $browsePage->dom->$tabNumDom->getText();
-        return ($num == $tabNum) ? $this->success("切换至{$tab}Tab成功") : $this->failed("切换至{$tab}Tab失败");
+        $browsePage->dom->$tabDom->click();//点击对应的tab
+        $num = $browsePage->dom->$tabNumDom->getText();//获取对应tab下计划数量
+        return ($num == $tabNum) ? $this->success("切换至{$tabName}Tab成功") : $this->failed("切换至{$tabName}Tab失败");
     }
 }
