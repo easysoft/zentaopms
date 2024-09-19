bf7a90a61fa59fceebfd1e5bf70e3e143e26a61e
yulujie <yulujie@easycorp.ltd>
2024-09-19 14:27:48 +0800

    * [task#128684,0.1h,4.5h] Add xpath.

---
+++ module/execution/test/ui/page/story.php
@@ -0,0 +1,11 @@
+<?php
+class storyPage extends page
+{
+    public function __construct($webdriver)
+    {
+        parent::__construct($webdriver);
+        $xpath = array(
+        );
+        $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
+    }
+}
