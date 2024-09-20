00d6ba805127c8458100f5cb3e0b7b0cb1962097
yulujie <yulujie@easycorp.ltd>
2024-09-20 11:17:26 +0800

    * [task#128685,doing,0.5h,4.5h] Add xpath.

--- module/execution/test/ui/page/story.php
+++ module/execution/test/ui/page/story.php
@@ -5,6 +5,13 @@ class storyPage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
+            /* 标签 */
+            'allTab'       => "//*[@data-page='execution-story']//div[@id='featureBar']/menu/li[1]/a",
+            'unclosedTab'  => "//*[@data-page='execution-story']//div[@id='featureBar']/menu/li[2]/a",
+            'draftTab'     => "//*[@data-page='execution-story']//div[@id='featureBar']/menu/li[3]/a",
+            'reviewingTab' => "//*[@data-page='execution-story']//div[@id='featureBar']/menu/li[3]/a",
+            'num'          => "//*[@id='table-execution-story']/div[3]/div[2]/strong[1]",
+            /* 列表 */
             'firstName' => "//*[@id='table-execution-story']/div[2]/div[1]/div/div[2]/div/a",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
