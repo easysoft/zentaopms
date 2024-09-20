ad2ed1465a45d6abf4c42e276c1208be842a5c06
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-09-20 17:27:59 +0800

    *[task#128808,doing,0.3h,2h] add uitest case

--- module/productplan/test/ui/sortinkanban.php
+++ module/productplan/test/ui/sortinkanban.php
@@ -1,5 +1,6 @@
 #!/usr/bin/env php
 <?php
+
 /**
 title=计划看板中排序测试
 timeout=0
@@ -18,3 +19,14 @@ $productplan->begin->range('`2024-09-19`,`2024-09-20`,`2024-09-21`,`2024-09-22`,
 $productplan->end->range('`2024-10-19`,`2024-10-20`,`2024-10-21`,`2024-10-22`,`2024-10-23`,`2024-10-24`,`2024-10-25`');
 $productplan->status->range('wait{3},doing{2},done{1},closed{1}');
 $productplan->gen(7);
+
+$tester = new browseTester();
+$tester->login();
+$planurl['productID'] = 1;
+
+$orderBy  = 'begin_desc';
+r($tester->sortInKanban($planurl, $orderBy)) && p('message,status') && e('按计划开始时间倒序排序成功,SUCCESS');//按计划开始时间倒序进行排序
+
+$orderBy  = 'begin_asc';
+r($tester->sortInKanban($planurl, $orderBy)) && p('message,status') && e('按计划开始时间正序排序成功,SUCCESS');//按计划开始时间正序进行排序
+$tester->closeBrowser();
