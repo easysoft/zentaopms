9523cb6ff70c3e4ba3cc41afc2c736fcf1e7bb0a
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-09-20 17:26:12 +0800

    *[task#128808,doing,1h,2h] add uitest case of sortInKanban

---
+++ module/productplan/test/ui/sortinkanban.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php
+/**
+title=计划看板中排序测试
+timeout=0
+cid=11
+*/
+chdir(__DIR__);
+include '../lib/browse.ui.class.php';
+
+zendata('product')->loadYaml('product', false, 2)->gen(10);
+$productplan = zenData('productplan');
+$productplan->id->range('1-7');
+$productplan->product->range(1);
+$productplan->title->range('计划1,计划2,计划3,计划4,计划5,计划6,计划7');
+$productplan->parent->range(0);
+$productplan->begin->range('`2024-09-19`,`2024-09-20`,`2024-09-21`,`2024-09-22`,`2024-09-23`,`2024-09-24`,`2024-09-25`');
+$productplan->end->range('`2024-10-19`,`2024-10-20`,`2024-10-21`,`2024-10-22`,`2024-10-23`,`2024-10-24`,`2024-10-25`');
+$productplan->status->range('wait{3},doing{2},done{1},closed{1}');
+$productplan->gen(7);
