202ca7c7bc4e8be2040279d38d19f34b2e5cde03
lijie <lijie@easycorp.ltd>
2024-09-20 15:22:17 +0800

    [misc, 1h] create zendata config in the createstory.php.

--- module/story/test/ui/creatstory.php
+++ module/story/test/ui/creatstory.php
@@ -23,6 +23,26 @@ chdir(__DIR__);
 include '../lib/createstory.ui.class.php';
 include 'page/create.php';
 
+$product->name->range('产品1');
+$product->shadow->range('0');
+$product->bind->range('0');
+$product->acl->range('open');
+$product->createdBy->range('admin');
+$product->vision->range('rnd');
+$product->gen(1);
+
+$story = zenData('story');
+$story->id->range('1-3');
+$story->root->range('1-3');
+$story->path->range('`,1,`, `,2,`, `,3,`');
+$story->grade->range('1');
+$story->product->range('1');
+$story->module->range('0');
+$story->title->range('激活研发需求,激活用户需求,激活业务需求');
+$story->type->range('story,requirement,epic');
+$story->stage->range('wait');
+$story->status->range('active');
+$story->openedBy->range('admin');
 $story->version->range('1');
 $story->assignedTo->range('[]');
 $story->reviewedBy->range('[]');
