b64029358742db6ae686e228ada26953e4d344b6
yulujie <yulujie@easycorp.ltd>
2024-09-20 11:17:51 +0800

    * [task#128685,doing,0.5h,4h] Add function.

---
+++ module/execution/test/lib/story.ui.class.php
@@ -0,0 +1,22 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+class storyTester extends tester
+{
+    /**
+     * 检查Tab标签下的数据。
+     * Check the data of the Tab tag.
+     *
+     * @param  string $tab       allTab|unclosedTab|draftTab|reviewingTab
+     * @param  string $expectNum
+     * @access public
+     * @return object
+     */
+    public function checkTab($tab, $expectNum)
+    {
+        $form = $this->initForm('execution', 'story', array('execution' => '2'), 'appIframe-execution');
+        $form->dom->$tab->click();
+        $form->wait(1);
+        if($form->dom->num->getText() == $expectNum) return $this->success($tab . '下显示条数正确');
+        return $this->failed($tab . '下显示条数不正确');
+    }
+}
