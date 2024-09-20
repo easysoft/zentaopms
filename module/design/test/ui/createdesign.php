44c9f5247ac873d857697477497ac8ea2dfe1b76
huairong <huairong@easycorp.ltd>
2024-09-20 10:40:13 +0800

    + [test] modify ui test files of createdesign.

--- module/design/test/ui/createdesign.php
+++ module/design/test/ui/createdesign.php
@@ -7,6 +7,15 @@ title=创建设计测试
 timeout=0
 cid=1
 
+- 校验设计类型不能为空
+ - 测试结果 @创建设计表单页提示信息正确
+ - 最终测试状态 @SUCCESS
+- 校验设计名称不能为空
+ - 测试结果 @创建设计表单页提示信息正确
+ - 最终测试状态 @SUCCESS
+- 创建概要设计类型的设计 最终测试状态 @SUCCESS
+- 创建详细设计类型的设计 最终测试状态 @SUCCESS
+
 */
 chdir(__DIR__);
 include '../lib/createdesign.ui.class.php';
@@ -22,9 +31,9 @@ $design = array(
     array('product' => '所有产品', 'type' => '详细设计', 'name' => '详细设计1'),
 );
 
-r($tester->createDesign($design['0'])) && p('message,status') && e('创建设计表单页提示信息正确, success'); //校验设计类型不能为空
-r($tester->createDesign($design['1'])) && p('message,status') && e('创建设计表单页提示信息正确, success'); //校验设计名称不能为空
-r($tester->createDesign($design['2'])) && p('status') && e('success');                                     //创建概要设计类型的设计
-r($tester->createDesign($design['3'])) && p('status') && e('success');                                     //创建详细设计类型的设计
+r($tester->createDesign($design['0'])) && p('message,status') && e('创建设计表单页提示信息正确, SUCCESS'); //校验设计类型不能为空
+r($tester->createDesign($design['1'])) && p('message,status') && e('创建设计表单页提示信息正确, SUCCESS'); //校验设计名称不能为空
+r($tester->createDesign($design['2'])) && p('status') && e('SUCCESS');                                     //创建概要设计类型的设计
+r($tester->createDesign($design['3'])) && p('status') && e('SUCCESS');                                     //创建详细设计类型的设计
 
 $tester->closeBrowser();
