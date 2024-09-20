ab1fc609588a22de51916906015fde09a4c54625
lijie <lijie@easycorp.ltd>
2024-09-20 15:19:16 +0800

    [misc, 1h] create zendata config in the createstory.php.

--- module/story/test/ui/creatstory.php
+++ module/story/test/ui/creatstory.php
@@ -11,24 +11,42 @@ cid=80
  -  测试结果 @创建产品表单页提示信息正确
  -  最终测试状态 @SUCCESS
 - 使用默认选项创建研发需求 最终测试状态 @SUCCESS
+- 使用默认选项创建用户需求 最终测试状态 @SUCCESS
+- 使用默认选项创建业务需求 最终测试状态 @SUCCESS
 - 创建正常需求后检查创建需求信息是否正确
  - 属性module @story
  - 属性method @view
-- 创建正常产品成功 最终测试状态 @SUCCESS
+- 创建需求成功 最终测试状态 @SUCCESS
 
 */
 chdir(__DIR__);
 include '../lib/createstory.ui.class.php';
+include 'page/create.php';
+
+$storyspec = zenData('storyspec');
+$storyspec->story->range('1-3');
+$storyspec->version->range('1');
+$storyspec->title->range('激活研发需求,激活用户需求,激活业务需求');
+$storyspec->gen(3);
 
 $tester = new createStoryTester();
 $tester->login();
 
 $storys = array();
-$storys['null']       = '';
-$storys['default']    = '默认需求';
-$storys['childStory'] = '子需求';
+$storys['null']        = '';
+$storys['story']       = '研发需求';
+$storys['requirement'] = '用户需求';
+$storys['epic']        = '业务需求';
+$storys['childStory']  = '子需求';
 
-r($tester->createDefault($storys['null']))    && p('message,status') && e('创建需求页面名称为空提示正确,SUCCESS'); // 缺少需求名称，创建失败
-r($tester->createDefault($storys['default'])) && p('status')         && e('SUCCESS');                              // 使用默认选项创建需求,搜索后详情页信息对应
+$storyType = array();
+$storyType['epic']        = 'epic';
+$storyType['requirement'] = 'requirement';
+$storyType['story']       = 'story';
+
+r($tester->createDefault($storyType['story'], $storys['null']))              && p('message,status') && e('创建需求页面名称为空提示正确,SUCCESS'); // 缺少需求名称，创建失败
+r($tester->createDefault($storyType['story'], $storys['story']))             && p('message,status') && e('创建研发需求成功,SUCCESS'); // 使用默认选项创建需求,搜索后详情页信息对应
+r($tester->createDefault($storyType['requirement'], $storys['requirement'])) && p('message,status') && e('创建用户需求成功,SUCCESS'); // 使用默认选项创建用户需求，搜索后详情页信息对应
+r($tester->createDefault($storyType['epic'], $storys['epic']))               && p('message,status') && e('创建业务需求成功,SUCCESS'); // 使用默认选项创建业务需求，搜索后详情页信息对应
 
 $tester->closeBrowser();
