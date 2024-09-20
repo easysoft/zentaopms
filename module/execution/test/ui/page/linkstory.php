3fd837872e784a5adb967be5e6eeebe4141d5b05
yulujie <yulujie@easycorp.ltd>
2024-09-20 09:47:59 +0800

    * [task#128684,doing,0.5h,0.5h] Add xpath.

--- module/execution/test/ui/page/linkstory.php
+++ module/execution/test/ui/page/linkstory.php
@@ -6,6 +6,7 @@ class linkStoryPage extends page
         parent::__construct($webdriver);
         $xpath = array(
         'firstCheckbox' => "//*[@id='table-execution-linkstory']/div[2]/div/div/div[1]/div/div",
+        'firstName'     => "//*[@id='table-execution-linkstory']/div[2]/div[1]/div/div[2]/div/a",
         'saveBtn'       => "//*[@id='table-execution-linkstory']/div/nav[1]/button",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
