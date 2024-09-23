45dc65822303b535b7779b6e1125737eb3eb7b96
zhouxin <zhouxin@easycorp.ltd>
2024-09-23 15:31:54 +0800

    * [misc] add missing comma.

--- db/zentao.sql
+++ db/zentao.sql
@@ -1236,7 +1236,7 @@ CREATE TABLE IF NOT EXISTS `zt_mr` (
   `compileStatus` char(30) NOT NULL DEFAULT '',
   `removeSourceBranch` enum('0','1') NOT NULL DEFAULT '0',
   `squash` enum('0','1') NOT NULL DEFAULT '0',
-  `isFlow` ENUM('0', '1') NOT NULL DEFAULT '0'
+  `isFlow` ENUM('0', '1') NOT NULL DEFAULT '0',
   `synced` enum('0','1') COLLATE 'utf8_general_ci' NOT NULL DEFAULT '1',
   `syncError` varchar(255) NOT NULL DEFAULT '',
   `hasNoConflict` enum('0','1') COLLATE 'utf8_general_ci' NOT NULL DEFAULT '0',
