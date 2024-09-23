5dc8d435eb72addf0f56963efd8d3acd8b000092
lihuifang <lihuifang@easycorp.ltd>
2024-09-23 09:46:43 +0800

    *[task#129038,doing,5h,1h] Add ui function files of projectrelease link bug.

--- module/projectrelease/test/lib/releaselinkbug.ui.class.php
+++ module/projectrelease/test/lib/releaselinkbug.ui.class.php
@@ -18,3 +18,23 @@ class releaseLinkBugTester extends tester
         $form->dom->searchBtn->click();
         $form->dom->selectAllBug->click(); // 点击全选按钮
         $form->dom->linkBugBtnBottom->click();
+        // 断言检查发布关联bug数量是否成功
+        $viewPage = $this->initForm('projectrelease', 'view', array('projectID' => 1), 'appIframe-project');
+        $form->dom->resolvedBugTab->click();
+        $form->wait(2);
+        return ($viewPage->dom->resolvedBugNum === '0') ? $this->failed('发布关联bug失败') : $this->success('发布关联bug成功');
+    }
+
+    /**
+     * Unlink a bug of project release.
+     * 移除单个bug
+     *
+     * @access public
+     * @return object
+     */
+    public function unlinkBug()
+    {
+        $form = $this->initForm('projectrelease', 'view', array('releaseID' => 1), 'appIframe-project');
+        $form->dom->resolvedBugTab->click();
+        $form->wait(2);
+        $linkNumBefore = $form->dom->resolvedBugNum->getText(); // 记录移除bug前发布关联的bug数量
