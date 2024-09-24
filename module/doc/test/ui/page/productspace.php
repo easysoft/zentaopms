fcaee81ba303a7400a5bb22bfd8a15f691534a1d
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-09-24 14:01:08 +0000

    *[task#129119,done,0.1h] Add ui test files of create a product doc

--- module/doc/test/ui/page/productspace.php
+++ module/doc/test/ui/page/productspace.php
@@ -5,9 +5,10 @@ class productspacePage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'createDocBtn' => '/html/body/div[1]/div/div[1]/div[2]/div/a',
-            'saveBtn'      => '/html/body/div[1]/div/div/div[2]/form/div[1]/div[1]/div/div/a',
-            'releaseBtn'   => '/html/body/div[1]/div/div/div[2]/form/div[2]/div/div/div[3]/div[10]/div/button',
+            'createDocBtn'   => '/html/body/div[1]/div/div[1]/div[2]/div/a',
+            'saveBtn'        => '/html/body/div[1]/div/div/div[2]/form/div[1]/div[1]/div/div/a',
+            'releaseBtn'     => '/html/body/div[1]/div/div/div[2]/form/div[2]/div/div/div[3]/div[10]/div/button',
+            'leftListHeader' => '/html/body/div[1]/div/div/div[1]/div[1]/div[1]/button/span',
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
