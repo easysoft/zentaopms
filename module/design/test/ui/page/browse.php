4222763332accad7eb8c2d5e7b4d6b37ae9e07fe
huairong <huairong@easycorp.ltd>
2024-09-24 11:10:38 +0800

    + [task#128889] modify ui test files of browse page.

--- module/design/test/ui/page/browse.php
+++ module/design/test/ui/page/browse.php
@@ -9,6 +9,9 @@ class browsePage extends page
             'designName'    => "//*[@id='table-design-browse']/div[2]/div[1]/div/div[2]/div/a",
             'linkedProduct' => "//*[@id='table-design-browse']/div[2]/div[2]/div/div[1]/div",
             'designType'    => "//*[@id='table-design-browse']/div[2]/div[2]/div/div[2]/div/span",
+            'assignedTo'    => "//*[@name='assignedTo']",
+            'assignedToBtn' => "//*[@class='form load-indicator form-ajax no-morph form-horz']/div[3]/div/button/span",
+            'assigned'      => "//*[@id='table-design-browse']/div[2]/div[2]/div/div[3]/div/a/span",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
