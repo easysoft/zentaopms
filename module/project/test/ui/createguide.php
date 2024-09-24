636c8882f22830de8a6d0af91407537879073d18
lihuifang <lihuifang@easycorp.ltd>
2024-09-24 16:05:58 +0800

    +[task#129208,doing,3h,2.5h] Add ui test file of create project guide .

---
+++ module/project/test/ui/createguide.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+
+/**
+
+title=创建项目向导
+timeout=0
+cid=1
+
+- 检查scrum向导跳转测试结果 @scrum向导跳转正确
+- 检查waterfall向导跳转测试结果 @waterfall向导跳转正确
+- 检查kanban向导跳转测试结果 @kanban向导跳转正确
+- 检查agileplus向导跳转测试结果 @agileplus向导跳转正确
+- 检查waterfallplus向导跳转测试结果 @waterfallplus向导跳转正确
+
+*/
+
+chdir(__DIR__);
