b6fe164568053501ea801a4d686af778c27e3587
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-09-24 13:13:33 +0000

    *[task#129119,doing,0.1h] Add ui test files of create a product doc

---
+++ module/doc/test/ui/page/productspace.php
@@ -0,0 +1,14 @@
+<?php
+class productspacePage extends page
+{
+    public function __construct($webdriver)
+    {
+        parent::__construct($webdriver);
+        $xpath = array(
+            'createDocBtn' => '/html/body/div[1]/div/div[1]/div[2]/div/a',
+            'saveBtn'      => '/html/body/div[1]/div/div/div[2]/form/div[1]/div[1]/div/div/a',
+            'releaseBtn'   => '/html/body/div[1]/div/div/div[2]/form/div[2]/div/div/div[3]/div[10]/div/button',
+        );
+        $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
+    }
+}
