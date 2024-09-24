28f57884d97199854a068f8e4489ffc0f8e6e12c
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-09-24 11:13:57 +0800

    *[task#129121,doing,1h,2h] add function of create branch

--- module/branch/test/lib/createbranch.ui.class.php
+++ module/branch/test/lib/createbranch.ui.class.php
@@ -17,42 +17,32 @@ class createBranchTester extends tester
         $form->dom->btn($this->lang->branch->createAction)->click();
         $form->wait(2);
         //设置表单字段
-        //$form = $this->loadPage();
-        //var_dump($form->dom->branchName);
-        if ($form->dom->branchName === false)
+        /*if ($form->dom->branchName === false)
         {
             var_dump('没找到分支名称输入框');
         }
         else
         {
             var_dump('找到了');
-        }
-        $form->wait(3);
+        }*/
         $form->dom->branchName->click();
-        try{
-        if (isset($branch->name))
-        {
-            $form->dom->branchName->setValue($branch->name);
-        }
-        }
-        catch (Exception $e){
-            echo 'Error: ' . $e->getMessage();
-        }
-      //  $form->dom->branchName->setValue('fenzhi');
-        //if (isset($branch->desc)) $form->dom->desc->setValue($branch->desc);
-        //$form->dom->save111->click();
+        if (isset($branch->name)) $form->dom->branchName->setValue($branch->name);
+        if (isset($branch->desc)) $form->dom->desc->setValue($branch->desc);
         $form->wait(2);
-/*
+        $form->dom->save->click();
+        $form->wait(2);
         if ($form->dom->createBranchForm)
         {
             var_dump($this->page->dom->getFormTips());
-            if ($this->checkFormTips('branch')) return $this->success('创建分支表单页提示信息正确');
+            if ($this->checkFormTips('branch'))
+            {
+                return $this->success('创建分支表单页提示信息正确');
+            }
             return $this->failed('创建分支表单页提示信息不正确');
         }
         else
         {
             return $this->success('创建分支成功');
         }
-*/
     }
 }
--- module/branch/test/ui/page/manage.php
+++ module/branch/test/ui/page/manage.php
@@ -5,8 +5,7 @@ class managePage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'branchName' => "//*[@id='createForm']/div/input",
-            'save111' => "//*[@id='zin_branch_create_form']/div[3]/div/button",
+            'save' => "//*[@id='zin_branch_create_form']/div[3]/div/button",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
