15c2a42d75f8038d2514447edfaf426e6c74a642
qixinzhi <qixinzhi@easycorp.ltd>
2024-09-24 02:24:02 +0000

    * [story#60827,0.5h] add exclude table.

--- module/bi/tao.php
+++ module/bi/tao.php
@@ -10,10 +10,15 @@ class biTao extends biModel
      */
     protected function fetchAllTables()
     {
+        $prefix   = $this->config->db->prefix;
+        $excludes = $this->config->bi->duckdbExcludeTables;
+        foreach($excludes as $index => $table) $excludes[$index] = $prefix . $table;
+
         return $this->dao->select("table_name as 'table'")
             ->from('information_schema.tables')
             ->where('table_type')->eq('BASE TABLE')
             ->andWhere('table_schema')->eq($this->config->db->name)
+            ->andWhere('table_name')->notin($excludes)
             ->fetchPairs();
     }
 
@@ -25,9 +30,15 @@ class biTao extends biModel
      */
     protected function fetchTableQueue()
     {
+        $prefix   = $this->config->db->prefix;
+        $excludes = $this->config->bi->duckdbExcludeTables;
+        foreach($excludes as $index => $table) $excludes[$index] = $prefix . $table;
+
         return $this->dao->select('object')->from(TABLE_DUCKDBQUEUE)
-            ->where('updatedTime >= syncTime')
+            ->where('updatedTime >= syncTime', true)
             ->orWhere('syncTime IS NULL')
+            ->markRight(1)
+            ->andWhere('object')->notin($excludes)
             ->fetchPairs();
     }
 
