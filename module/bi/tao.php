4eba621e69682f64f4a4cb679b0a38a5b7255a4a
qixinzhi <qixinzhi@easycorp.ltd>
2024-09-24 01:40:19 +0000

    * [story#60827,0.5h] add fetchTableQueue.

--- module/bi/tao.php
+++ module/bi/tao.php
@@ -6,14 +6,28 @@ class biTao extends biModel
      * Fetch tables.
      *
      * @access protected
-     * @return void
+     * @return array
      */
-    protected function fetchTables()
+    protected function fetchAllTables()
     {
         return $this->dao->select("table_name as 'table'")
             ->from('information_schema.tables')
             ->where('table_type')->eq('BASE TABLE')
             ->andWhere('table_schema')->eq($this->config->db->name)
+            ->fetchPairs();
+    }
+
+    /**
+     * Fetch table queue.
+     *
+     * @access protected
+     * @return array
+     */
+    protected function fetchTableQueue()
+    {
+        return $this->dao->select('object')->from(TABLE_DUCKDBQUEUE)
+            ->where('updatedTime >= syncTime')
+            ->orWhere('syncTime IS NULL')
             ->fetchPairs();
     }
 
