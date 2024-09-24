21dc073dafb54387cf7ec0cc78412f12c678f8a5
lihuifang <lihuifang@easycorp.ltd>
2024-09-24 16:06:16 +0800

    *[task#129208,doing,3h,2h] Add ui test file of create project guide .

--- module/project/test/ui/createguide.php
+++ module/project/test/ui/createguide.php
@@ -16,3 +16,15 @@ cid=1
 */
 
 chdir(__DIR__);
+include '../lib/createguide.ui.class.php';
+
+$tester = new createGuideTester();
+$tester->login();
+
+r($tester->createGuide('scrum'))         && p('message') && e('scrum向导跳转正确');         // 检查scrum向导跳转
+r($tester->createGuide('waterfall'))     && p('message') && e('waterfall向导跳转正确');     // 检查waterfall向导跳转
+r($tester->createGuide('kanban'))        && p('message') && e('kanban向导跳转正确');        // 检查kanban向导跳转
+r($tester->createGuide('agileplus'))     && p('message') && e('agileplus向导跳转正确');     // 检查agileplus向导跳转
+r($tester->createGuide('waterfallplus')) && p('message') && e('waterfallplus向导跳转正确'); // 检查waterfallplus向导跳转
+
+$tester->closeBrowser();
