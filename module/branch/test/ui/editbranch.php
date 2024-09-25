1fe5e1ee776427d9ebf669ef14c939ad2d6ee147
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-09-25 14:01:11 +0800

    *[task#129120,doing,0.5h,1h] add zendata of edit branch

--- module/branch/test/ui/editbranch.php
+++ module/branch/test/ui/editbranch.php
@@ -17,3 +17,22 @@ cid=13
  -最终测试状态 @SUCCESS
 
  */
+chdir(__DIR__);
+include '../lib/editbranch.ui.class.php';
+
+$product = zenData('product');
+$product->id->range('11');
+$product->name->range('多分支产品');
+$product->status->range('normal');
+$product->type->range('branch');
+$product->gen(1);
+
+$branch = zenData('branch');
+$branch->id->range('1-2');
+$branch->product->range('11');
+$branch->name->range('分支01,分支02');
+$branch->desc->range('分支描述01,分支描述02');
+$branch->gen(2);
+
+$tester = new editBranchTester();
+$tester->login();
