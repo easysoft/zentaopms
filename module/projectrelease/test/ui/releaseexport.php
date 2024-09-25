cdc03b08bfc13aa92060b2c1fb99fdd49468e5f3
lihuifang <lihuifang@easycorp.ltd>
2024-09-25 13:45:10 +0800

    *[task#129229,doing,3h,2.5h] Add ui test file of export project release.

--- module/projectrelease/test/ui/releaseexport.php
+++ module/projectrelease/test/ui/releaseexport.php
@@ -18,3 +18,21 @@ cid=73
  - 最终测试状态 @SUCCESS
 
 */
+chdir(__DIR__);
+include '../lib/releaseexport.ui.class.php';
+
+zendata('release')->loadYaml('projectrelease', false, 1)->gen(1);
+zendata('project')->loadYaml('project', false, 1)->gen(1);
+zendata('projectproduct')->loadYaml('projectproduct', false, 1)->gen(1);
+
+$tester = new releaseExportTester();
+$tester->login();
+
+//设置导出发布数据
+$release = array(
+    array('filename' => ''),
+    array('filename' => '导出文件1'),
+    array('filename' => '导出文件2', 'exportdata' => '解决的Bug'),
+);
+
+r($tester->exportWithNoFilename($release['0'])) && p('message,status') && e('项目发布导出必填提示信息正确,SUCCESS');   // 项目发布导出时文件名必填项检查
