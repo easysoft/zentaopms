9bdb1599a38e5cc88c90a25acf94dbbe961fe7f6
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-09-25 14:18:33 +0000

    *[task#129223,doing,0.2h] Add ui test files of create a project doc

--- module/doc/test/ui/createdoc.php
+++ module/doc/test/ui/createdoc.php
@@ -15,6 +15,9 @@ timeout=0
 - 创建产品空间下的文档，创建成功
  - 测试结果 @产品空间文档创建成功
  - 最终测试结果 @SUCCESS
+- 创建项目空间下的文档，创建成功
+ - 测试结果 @项目空间文档创建成功
+ - 最终测试结果 @SUCCESS
 
  */
 chdir(__DIR__);
@@ -25,15 +28,26 @@ $tester->login();
 
 $draftName = new stdClass();
 $draftName->nullName = '';
-$draftName->dftName  = '我的草稿文档1';
+$draftName->dftName  = '草稿文档1';
 
 $docName = new stdClass();
-$docName->dcName = '我的文档1';
+$docName->dcName = '文档1';
 
 $productName = new stdClass();
 $productName->fstProduct = '产品1';
 $productName->secProduct = '产品2';
 
+$projectName = new stdClass();
+$projectName->fstProject = '项目1';
+
+$executionName = new stdClass();
+$executionName->fstExecution = '执行1';
+
+$plan = new stdClass();
+$plan->begin = '2024-09-01';
+$plan->end = '2024-11-01';
+
 r($tester->createDraft($draftName))                  && p('message,status') && e('创建草稿成功,SUCCESS');     //创建草稿文档成功
 r($tester->createDoc($docName))                      && p('message,status') && e('创建文档成功,SUCCESS');     //创建文档成功
 r($tester->createProductDoc($productName, $docName)) && p('message,status') && e('创建产品文档成功,SUCCESS'); //创建产品空间下的文档成功
+r($tester->createProjectDoc($projectName, $executionName, $plan, $docName)) && p('message,status') && e('创建项目文档成功,SUCCESS'); //创建项目空间下的文档成功
