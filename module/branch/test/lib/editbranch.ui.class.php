48971fae607559e080a99c0dc2910ccd8e5e014c
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-09-25 14:13:12 +0800

    *[task#129120,done,0.2h,0h] modify code

--- module/branch/test/lib/editbranch.ui.class.php
+++ module/branch/test/lib/editbranch.ui.class.php
@@ -6,25 +6,25 @@ class editBranchTester extends tester
      * 编辑分支
      * edit  branch
      *
-     * @param $branch    分支数据
-     * @param $productID 产品ID
+     * @param $editBranch 分支数据
+     * @param $productID  产品ID
      *
      * @return mixed
      */
-    public function editBranch($branch, $productID)
+    public function editBranch($editBranch, $productID)
     {
         $form = $this->initForm('branch', 'manage', $productID, 'appIframe-product');
         $form->dom->allTab->click();
         $form->dom->editBtn->click();
         //设置表单字段值
-        if (isset($branch->name)) $form->dom->editName->setValue($branch->name);
-        if (isset($branch->desc)) $form->dom->editDesc->setValue($branch->desc);
+        if (isset($editBranch->name)) $form->dom->editName->setValue($editBranch->name);
+        if (isset($editBranch->desc)) $form->dom->editDesc->setValue($editBranch->desc);
         $form->dom->editSave->click();
         $form->wait(2);
         if ($form->dom->zin_branch_edit_1_form)
         {
             if ($this->checkFormTips('branch')) return $this->success('分支名称必填提示信息正确');
-            if (isset($branch->name) && ($form->dom->nameTip))
+            if (isset($editBranch->name) && ($form->dom->nameTip))
             {
                 //分支已存在
                 $nameTip = str_replace('@branch@', $this->lang->branch->common, $this->lang->branch->existName);
@@ -34,7 +34,7 @@ class editBranchTester extends tester
             }
             return $this->failed('分支名称必填提示信息不正确');
         }
-        return ($form->dom->secName->getText() == $branch->name)
+        return ($form->dom->secName->getText() == $editBranch->name)
             ? $this->success('编辑分支成功')
             : $this->failed('编辑分支失败');
     }
