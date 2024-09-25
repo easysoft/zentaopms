8ee6e579243f5803880565269d42a6e4af238537
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-09-25 15:08:40 +0000

    *[task#128887,done,0.1h] Add ui test files of fix the error code

--- module/doc/test/ui/lefttree.php
+++ module/doc/test/ui/lefttree.php
@@ -9,9 +9,6 @@ timeout=0
 - 收藏文档，收藏成功
  - 测试结果 @检查收藏文档成功
  - 最终测试状态 @SUCCESS
-- 添加我的库的目录，添加成功
- - 测试结果 @添加我的库目录添加成功
- - 最终测试状态 @SUCCESS
 
  */
 chdir(__DIR__);
@@ -24,4 +21,3 @@ $docName = new stdClass();
 $docName->dcName = '我的文档A';
 
 r($tester->myFavorites($docName)) && p('message,status') && e('收藏文档成功,SUCCESS'); //收藏文档成功
-r($tester->addDirectory())        && p('message,status') && e('添加目录成功,SUCCESS'); //添加目录成功
