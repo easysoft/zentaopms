4f97998f94657c92de6d18d029998558704d7fef
huairong <huairong@easycorp.ltd>
2024-09-25 15:36:02 +0800

    + [task#128891] modify ui test files of view page.

--- module/project/test/ui/page/view.php
+++ module/project/test/ui/page/view.php
@@ -15,6 +15,7 @@ class viewPage extends page
             'projectName'           => "//*[@id='mainContent']/div[1]/div[1]/div[1]/div[2]/div[1]/span[2]",
             'acl'                   => "//*[@id='mainContent']/div[1]/div[1]/div[1]/div[2]/div[1]/span[5]",
             'end'                   => "//*[@id='mainContent']/div[1]/div[1]/div[2]/div/table[3]/tbody/tr/td/div/div[2]/span[2]",
+            'linkedProduct'         => "//*[@id='mainContent']/div[1]/div[1]/div[2]/div/table[1]/tbody/tr[3]/td[1]/div/div/a/span",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
