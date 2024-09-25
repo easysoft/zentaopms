f7e10d89f40e5967ee61c5cf729bd8272eecf4c4
lihuifang <lihuifang@easycorp.ltd>
2024-09-25 13:48:19 +0800

    *[task#129229,doing,3h,1h] Add ui function files of export project release.

--- module/projectrelease/test/lib/releaseexport.ui.class.php
+++ module/projectrelease/test/lib/releaseexport.ui.class.php
@@ -13,3 +13,23 @@ class releaseExportTester extends tester
     {
         $form = $this->initForm('projectrelease', 'view', array('releaseID' => 1), 'appIframe-project');
         $form->dom->exportBtn->click();
+        if(isset($release['filename']))   $form->dom->fileName->setValue($release['filename']);
+        if(isset($release['exportdata'])) $form->dom->exportData->picker($release['exportdata']);
+
+        $form->dom->exportBtnAlert->click();
+        $form->wait(2);
+
+        if($form->dom->exportBtnAlert) return $this->failed('项目发布导出失败');
+        return $this->success('项目发布导出成功');
+    }
+
+    /**
+     * Check required tips of projectrelease export.
+     * 项目发布导出时文件名为空时的必填校验检查
+     *
+     * @access public
+     * @return object
+     */
+    public function exportWithNoFilename()
+    {
+        $form = $this->initForm('projectrelease', 'view', array('releaseID' => 1), 'appIframe-project');
