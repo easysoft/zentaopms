1e9a4147dacb322978513c70610b1c3eacedc2dd
huairong <huairong@easycorp.ltd>
2024-09-25 15:43:39 +0800

    + [task#128891] Add ui test files of linkProducts function.

--- module/project/test/lib/manageproducts.ui.class.php
+++ module/project/test/lib/manageproducts.ui.class.php
@@ -19,4 +19,24 @@ class manageProductsTester extends tester
         if($form->dom->alertModal('text') === '『产品』不能为空。') return $this->success('关联其他产品必填提示信息正确');
         return $this->failed('关联其他产品必填提示信息不正确');
     }
+    /**
+     * 关联产品。
+     * Link Products.
+     *
+     * @param  array  $project
+     * @access public
+     * @return void
+     */
+    public function linkProducts($project)
+    {
+        $form = $this->initForm('project', 'manageproducts', array('project' => 11), 'appIframe-project');
+        $form->dom->linkBtn->click(); //点击关联其他产品
+        if(isset($project['otherProducts'])) $form->dom->{'otherProducts[]'}->multiPicker($project['otherProducts']);
+        $form->dom->btn($this->lang->save)->click();
+
+        //跳转到项目概况页检查已关联产品
+        $form = $this->initForm('project', 'view', array('project' => 11), 'appIframe-project');
+        if($form->dom->linkedProduct->getText() != $project['otherProducts']['multiPicker']) return $this->failed('关联产品失败');
+        return $this->success('关联产品成功');
+    }
 }
