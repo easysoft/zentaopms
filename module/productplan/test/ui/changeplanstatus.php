35efb9f644f2c1bddb3a6286175e8d8ebfa00354
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-09-26 17:18:39 +0800

    *[task#129339,doing,0.3h,3h] add ztf comments

--- module/productplan/test/ui/changeplanstatus.php
+++ module/productplan/test/ui/changeplanstatus.php
@@ -1,6 +1,26 @@
 #!/usr/bin/env php
 <?php
+/**
+title=修改计划状态
+timeout=0
+cid=0
 
+- 开始计划
+ - 测试结果 @开始计划成功
+ - 最终测试状态 @SUCCESS
+- 完成计划
+ - 测试结果 @完成计划成功
+ - 最终测试状态 @SUCCESS
+- 关闭计划
+ - 测试结果 @关闭计划成功
+ - 最终测试状态 @SUCCESS
+- 激活计划
+ - 测试结果 @激活计划成功
+ - 最终测试状态 @SUCCESS
+- 删除计划
+ - 测试结果 @删除计划成功
+ - 最终测试状态 @SUCCESS
+*/
 chdir(__DIR__);
 include '../lib/changeplanstatus.ui.class.php';
 
