883c45cdeea55292e9bb935f96f0a7ced1005686
lijie <lijie@easycorp.ltd>
2024-09-26 09:20:37 +0800

    [misc, 0.5h] modify code style.

--- module/story/test/ui/page/batchedit.php
+++ module/story/test/ui/page/batchedit.php
@@ -5,8 +5,8 @@ class batchEditPage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'batchSource' => "/html/body/div[1]/div/div/div/div[2]/form/div[1]/table/tbody/tr/td[10]/div/div",
-            'batchEditSave'   => "//*[@id='zin_story_batchedit_formBatch']/div[2]/button[1]"
+            'batchSource'   => "/html/body/div[1]/div/div/div/div[2]/form/div[1]/table/tbody/tr/td[10]/div/div",
+            'batchEditSave' => "//*[@id='zin_story_batchedit_formBatch']/div[2]/button[1]"
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
