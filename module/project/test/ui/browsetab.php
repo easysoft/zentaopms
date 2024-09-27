75edd1056b9b4432c5d62638fa194c78673cf017
lihuifang <lihuifang@easycorp.ltd>
2024-09-27 15:19:31 +0800

    +[task#129333,doing,4h,3.5h] Add ui test file of project browse tab.

---
+++ module/project/test/ui/browsetab.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php
+
+/**
+
+title=项目列表页tab标签检查
+timeout=0
+cid=73
+
+- 检查all标签数量
+ - 测试结果 @all标签下条数显示正确
+ - 最终测试状态 @SUCCESS
+- 检查undone标签数量
+ - 测试结果 @undone标签下条数显示正确
+ - 最终测试状态 @SUCCESS
+- 检查wait标签数量
+ - 测试结果 @wait标签下条数显示正确
+ - 最终测试状态 @SUCCESS
+- 检查doing标签数量
+ - 测试结果 @doing标签下条数显示正确
