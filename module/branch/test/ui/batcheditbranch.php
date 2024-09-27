f24c6946d016bc085aa5c0b1d3fbd8523a8e50f2
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-09-27 09:39:31 +0800

    *[task#129339,doing,0.5h,3h] add ztf comments

--- module/branch/test/ui/batcheditbranch.php
+++ module/branch/test/ui/batcheditbranch.php
@@ -6,16 +6,6 @@ title=批量编辑分支测试
 timeout=0
 cid=0
 
--校验分支名称必填
- -测试结果 @分支名称必填提示信息正确
- -最终测试状态 @SUCCESS
--校验分支重复
- -测试结果 @分支已存在提示信息正确
- -最终测试状态 @SUCCESS
--校验分支正常编辑
- -测试结果 @批量编辑分支成功
- -最终测试状态 @SUCCESS
-
 */
 chdir(__DIR__);
 include '../lib/batcheditbranch.ui.class.php';
@@ -40,13 +30,13 @@ $tester->login();
 $productID['productID'] = 11;
 $editBranch = new stdClass();
 $editBranch->name = '';
-r($tester->batchEditBranch($editBranch, $productID)) && p('message,status') && e('分支名称必填提示信息正确,SUCCESS');
+r($tester->batchEditBranch($editBranch, $productID)) && p('message,status') && e('分支名称必填提示信息正确,SUCCESS');//分支名称必填校验
 
 $editBranch->name = '分支02';
-r($tester->batchEditBranch($editBranch, $productID)) && p('message,status') && e('分支已存在提示信息正确,SUCCESS');
+r($tester->batchEditBranch($editBranch, $productID)) && p('message,status') && e('分支已存在提示信息正确,SUCCESS');//分支名称重复校验
 
 $editBranch->name = '分支test编辑';
 $editBranch->desc = '这是一行分支的描述';
-r($tester->batchEditBranch($editBranch, $productID)) && p('message,status') && e('批量编辑分支成功,SUCCESS');
+r($tester->batchEditBranch($editBranch, $productID)) && p('message,status') && e('批量编辑分支成功,SUCCESS');//批量编辑分支
 
 $tester->closeBrowser();
