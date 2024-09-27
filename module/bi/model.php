4417f64c9668ef86d43366a6e9af768534c9d250
qixinzhi <qixinzhi@easycorp.ltd>
2024-09-27 08:00:24 +0000

    [bug] fix special.

--- module/bi/model.php
+++ module/bi/model.php
@@ -541,6 +541,8 @@ class biModel extends model
 
             $useField = in_array($useField, $fieldList) ? $useField : 'id';
             $options = $this->dao->select("id, {$useField}")->from($table)->fetchPairs();
+            // htmlspecialchars values
+            foreach($options as $key => $value) $options[$key] = htmlspecialchars($value);
         }
 
         return $options;
