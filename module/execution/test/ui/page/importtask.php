860453380fe5387a0e0cb785e5f2fcfe09029872
yulujie <yulujie@easycorp.ltd>
2024-09-27 11:51:29 +0800

    * [task#128682,doing,1h,2h] Add xpath.

--- module/execution/test/ui/page/importtask.php
+++ module/execution/test/ui/page/importtask.php
@@ -5,8 +5,6 @@ class importTaskPage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'pickInput'     => "//*[@data-page='execution-importtask']//div[@id='featureBar']/menu/div/div/div",
-            'executionList' => "//*[@data-page='execution-importtask']/div[@class='pick-container']/div/menu/menu/li" ,
             'firstCheckbox' => "//*[@id='table-execution-importtask']/div[2]/div[1]/div/div[1]/div/div",
             'firstName'     => "//*[@id='table-execution-importtask']/div[2]/div[1]/div/div[2]/div/a",
             'num'           => "//*[@data-page='execution-importtask']/div[1]//li/a/span[2]",
