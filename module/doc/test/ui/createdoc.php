702579b4dfbbcbe5932c2e883672a943cee237ff
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-09-29 09:38:05 +0000

    *[task#129224,doing,0.2h] Add ui test files of create a team doc

--- module/doc/test/ui/createdoc.php
+++ module/doc/test/ui/createdoc.php
@@ -24,7 +24,7 @@ cid=0
 chdir(__DIR__);
 include '../lib/createdoc.ui.class.php';
 
-zendata('doclib')->loadYaml('custom', false, 2)->gen(5);
+zendata('doclib')->loadYaml('custom', false, 2)->gen(1);
 $tester = new createDocTester();
 $tester->login();
 
@@ -48,9 +48,8 @@ $executionName->fstExecution = '执行1';
 $teamSpace = new stdClass();
 $teamSpace->spaceName = '团队空间1';
 
-$teamLib = array();
-$teamLib['null']    = '';
-$teamLib['default'] = '团队文档库A';
+$teamLib = new stdClass();
+$teamLib->libName = '团队文档库A';
 
 $plan = new stdClass();
 $plan->begin = '2024-09-01';
@@ -60,5 +59,4 @@ r($tester->createDraft($draftName))                  && p('message,status') && e
 r($tester->createDoc($docName))                      && p('message,status') && e('创建文档成功,SUCCESS');     //创建文档成功
 r($tester->createProductDoc($productName, $docName)) && p('message,status') && e('创建产品文档成功,SUCCESS'); //创建产品空间下的文档成功
 r($tester->createProjectDoc($projectName, $executionName, $plan, $docName)) && p('message,status') && e('创建项目文档成功,SUCCESS'); //创建项目空间下的文档成功
-r($tester->createTeamDoc($teamSpace, $teamLib['null'], $docName))    && p('message,status') && e('创建团队文档成功,SUCCESS'); //团队文档库名称非空校验成功
-r($tester->createTeamDoc($teamSpace, $teamLib['default'], $docName)) && p('message,status') && e('创建团队文档成功,SUCCESS'); //创建团队空间下的文档成功
+r($tester->createTeamDoc($teamSpace, $teamLib, $docName)) && p('message,status') && e('创建团队文档成功,SUCCESS'); //创建团队空间文档成功
