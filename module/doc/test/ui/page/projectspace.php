b2afb0cacf35e66124adc473130b77eddc5b1fc2
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-09-29 09:57:19 +0000

    *[task#129224,done,0.1h] Add ui test files of modify the error code format

--- module/doc/test/ui/page/projectspace.php
+++ module/doc/test/ui/page/projectspace.php
@@ -8,7 +8,7 @@ class projectspacePage extends page
             'createDocBtn' => '/html/body/div[1]/div/div[1]/div[2]/div/a',
             'saveBtn'      => '/html/body/div[1]/div/div/div[2]/form/div[1]/div[1]/div/div/a',
             'releaseBtn'   => '/html/body/div[1]/div/div/div[2]/form/div[2]/div/div/div[3]/div[10]/div/button',
-            'fstDocName'   => '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div[2]/div[1]/div/div[2]/div/a',
+            'fstDocName'   => '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div[2]/div[1]/div/div[2]/div/a'
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
