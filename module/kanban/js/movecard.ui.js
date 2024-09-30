bdf7e26c3c03d8c194318368a5b734682c710a99
liumengyi <liumengyi@easycorp.ltd>
2024-09-30 15:22:40 +0800

    + [task#129614,doing,0.2h] add move card js.

--- module/kanban/js/movecard.ui.js
+++ module/kanban/js/movecard.ui.js
@@ -7,5 +7,25 @@ function changeRegion(event)
         $lanePicker.render({items: [], disabled: true});
         $lanePicker.$.setValue('');
         const $columnPicker = $('#moveCardForm').find('[name=column]').zui('picker');
+        $columnPicker.render({items: [], disabled: true});
+        $columnPicker.$.setValue('');
+        return;
     }
+
+    const laneLink = $.createLink('kanban', 'ajaxGetLanes', 'regionID=' + regionID);
+    $.getJSON(laneLink, function(data)
+    {
+        if(!data || !data.items) return;
+
+        const $lanePicker = $('#moveCardForm').find('[name=lane]').zui('picker');
+        const oldLane     = $('#moveCardForm').find('[name=lane]').val();
+        $lanePicker.render({items: data.items, disabled: false});
+        if(oldLane) $lanePicker.$.setValue(oldLane);
+    });
+}
+
+function changeLane(event)
+{
+    const laneID = $(event.target).val();
+    const columnLink = $.createLink('kanban', 'ajaxGetColumns', 'laneID=' + laneID);
 }
