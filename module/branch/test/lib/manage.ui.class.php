3f09250f2f6a0a44c88a1efdbd651d5c97eb58b9
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-10-08 16:05:48 +0800

    *[task#129871,doing,0.3h,4h]close branch

--- module/branch/test/lib/manage.ui.class.php
+++ module/branch/test/lib/manage.ui.class.php
@@ -22,4 +22,24 @@ class manageTester extends tester
         $num = $managePage->dom->$tabNumDom->getText();//对应tab下分支数量
         return ($num == $tabNum) ? $this->success("切换至{$tabName}Tab成功") : $this->failed("切换至{$tabName}Tab失败");
     }
+
+    /**
+     * 关闭分支
+     * close branch
+     *
+     * @param $productID 产品ID
+     *
+     * @return mixed
+    */
+    public function closeBranch($productID)
+    {
+        $managePage = $this->initForm('branch', 'manage', $productID, 'appIframe-product');
+        $managePage->dom->allTab->click();
+        $managePage->dom->closeBtn->click();
+        $managePage->wait(1);
+        $managePage->dom->confirmBtn->click();
+        $managePage->wait(1);
+        $status = $managePage->dom->secStatus->getText();
+        return ($status == $this->lang->branch->closed) ? $this->success("关闭分支成功") : $this->failed("关闭分支失败");
+    }
 }
