37b22bc8fdb8be108ae41b8090dc1e987efadbb8
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-10-08 15:12:19 +0800

    *[task#129871,doing,0.1h,4h]code style

--- module/branch/test/lib/manage.ui.class.php
+++ module/branch/test/lib/manage.ui.class.php
@@ -14,12 +14,12 @@ class manageTester extends tester
      */
     public function switchTab($productID, $tabName, $tabNum)
     {
-        $tabDom    = $tabName . 'Tab';
-        $tabNumDom = $tabName . 'Num';
+        $tabDom     = $tabName . 'Tab';
+        $tabNumDom  = $tabName . 'Num';
         $managePage = $this->initForm('branch', 'manage', $productID, 'appIframe-product');
         $managePage->dom->$tabDom->click();//点击对应的tab
         $managePage->wait(1);
-        $num = $managePage->dom->$tabNumDom->getText();//获取对应tab下分支数量
+        $num = $managePage->dom->$tabNumDom->getText();//对应tab下分支数量
         return ($num == $tabNum) ? $this->success("切换至{$tabName}Tab成功") : $this->failed("切换至{$tabName}Tab失败");
     }
 }
