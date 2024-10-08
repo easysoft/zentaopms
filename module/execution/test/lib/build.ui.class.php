15abe45bafd95694b35739e8e41df25fe45e7a0a
yulujie <yulujie@easycorp.ltd>
2024-10-08 15:09:40 +0800

    * [task#129846,doing,1h,1.5h] Add function.

--- module/execution/test/lib/build.ui.class.php
+++ module/execution/test/lib/build.ui.class.php
@@ -2,4 +2,22 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class buildTester extends tester
 {
+    /**
+     * 切换产品查看版本。
+     * Switch products to view builds.
+     *
+     * @param  string $product
+     * @param  string $expectNum
+     * @access public
+     * @return void
+     */
+    public function switchProduct($product, $expectNum)
+    {
+        $form = $this->initForm('execution', 'build', array('execution' => $executionId), 'appIframe-execution');
+        if($product != '') $form->dom->product->picker($product);
+        $form->wait(1);
+        $num = explode(' ', $form->dom->num->getText())[1];
+        if($num == $expectNum) return $this->success('版本显示正确');
+        return $this->failed('版本显示错误');
+    }
 }
