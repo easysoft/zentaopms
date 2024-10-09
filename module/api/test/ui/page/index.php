35f6c910d4ee2bf3af4da74facd43378dcaf9ccb
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-09 17:13:58 +0000

    *[task#129851,doing,0.3h] Add ui test files of create an api doc

---
+++ module/api/test/ui/page/index.php
@@ -0,0 +1,14 @@
+<?php
+class indexPage extends page
+{
+    public function __construct($webdriver)
+    {
+        parent::__construct($webdriver);
+        $xpath = array(
+            'createLibBtn' => '/html/body/div/div/div[1]/div[2]/a',
+            'createApiBtn' => '/html/body/div[1]/div/div[1]/div[2]/a[5]',
+            'fstDocPath'   => '/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/ul/li/div/a/span[2]',
+        );
+        $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
+    }
+}
