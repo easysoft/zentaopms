93427b30774c806b548bcc1fb6e41bd9152911d6
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-09 10:34:06 +0000

    *[task#127576,done,0.1h] Add ui test files of add ztf comments

--- module/doc/test/ui/createdoclib.php
+++ module/doc/test/ui/createdoclib.php
@@ -1,6 +1,21 @@
 #!/usr/bin/env php
 <?php
 
+/**
+
+title=创建文档库测试
+timeout=0
+cid=0
+
+- 库名称为空，创建失败
+ - 测试结果 @库名称非空校验成功
+ - 最终测试状态 @SUCCESS
+- 创建我的文档库成功
+ - 测试结果 @创建我的文档库成功
+ - 最终测试状态 @SUCCESS
+
+*/
+
 chdir(__DIR__);
 include '../lib/createdoclib.ui.class.php';
 
@@ -11,5 +26,5 @@ $libName = array();
 $libName['null']     = '';
 $libName['myDocLib'] = '我的文档库1';
 
-r($tester->createDocLib($libName['null']))     && p('message,status') && e('创建文档库表单页提示信息正确,SUCCESS'); // 缺少库名称，创建失败
-r($tester->createDocLib($libName['myDocLib'])) && p('message,status') && e('创建文档库表单页提示信息正确,SUCCESS'); // 创建我的文档库，创建成功
+r($tester->createDocLib($libName['null']))     && p('message,status') && e('库名称非空校验成功,SUCCESS'); //库名称为空，创建失败
+r($tester->createDocLib($libName['myDocLib'])) && p('message,status') && e('创建我的文档库成功,SUCCESS'); //创建我的文档库成功
