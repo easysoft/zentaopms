8c3a0041fbc46300ed7615da27d4a4d2b0ae2e63
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-09 17:10:43 +0000

    *[task#129851,done,0.3h] Add ui test files of create an api doc

---
+++ module/api/test/lib/createapi.ui.class.php
@@ -0,0 +1,19 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+class createDocTester extends tester
+{
+    /*
+     * 创建接口文档。
+     * Create a api.
+     *
+     * @param  string $apiLib
+     * @param  string $apiDoc
+     * @param  string $apiPath
+     * @access public
+     * @return void
+     */
+    public function createApiDoc($apiLib, $apiDoc, $apiPath)
+    {
+        /*进入接口空间创建独立接口库*/
+    }
+}
