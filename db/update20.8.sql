b2570b1d677d287880708ba00c99892f25ce2c80
sunguangming <sunguangming@easycorp.ltd>
2024-10-09 07:13:29 +0000

    * [misc] adjust code style.

--- db/update20.8.sql
+++ db/update20.8.sql
@@ -37,8 +37,8 @@ ALTER TABLE `zt_workflowgroup` ADD `code` varchar(30) NOT NULL DEFAULT '' AFTER 
 
 DELETE FROM `zt_workflowgroup` WHERE `main` = '1';
 INSERT INTO `zt_workflowgroup` (`type`, `projectModel`, `projectType`, `name`, `code`, `status`, `vision`, `main`) VALUES
-('product',	'',          'project',	'默认流程',            'productproject',	'normal', 'rnd', '1'),
-('project',	'scrum',     'product',	'敏捷-产品型默认流程', 'scrumproduct',	    'normal', 'rnd', '1'),
-('project',	'scrum',     'project',	'敏捷-项目型默认流程', 'scrumproject',	    'normal', 'rnd', '1'),
-('project',	'waterfall', 'product',	'瀑布-产品型默认流程', 'waterfallproduct',	'normal', 'rnd', '1'),
-('project',	'waterfall', 'project',	'瀑布-项目型默认流程', 'waterfallproject',	'normal', 'rnd', '1');
+('product', '',          'project',    '默认流程',            'productproject',   'normal', 'rnd', '1'),
+('project', 'scrum',     'product',    '敏捷-产品型默认流程', 'scrumproduct',     'normal', 'rnd', '1'),
+('project', 'scrum',     'project',    '敏捷-项目型默认流程', 'scrumproject',     'normal', 'rnd', '1'),
+('project', 'waterfall', 'product',    '瀑布-产品型默认流程', 'waterfallproduct', 'normal', 'rnd', '1'),
+('project', 'waterfall', 'project',    '瀑布-项目型默认流程', 'waterfallproject', 'normal', 'rnd', '1');
