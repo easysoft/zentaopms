8461863662851442e3fa7799297a60514c6aed02
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-09 17:11:02 +0000

    *[task#129851,done,0.1h] Add ui test files of create an api doc

--- module/api/test/lib/createapi.ui.class.php
+++ module/api/test/lib/createapi.ui.class.php
@@ -15,5 +15,22 @@ class createDocTester extends tester
     public function createApiDoc($apiLib, $apiDoc, $apiPath)
     {
         /*进入接口空间创建独立接口库*/
+        $form = $this->initForm('api', 'index', array(), 'appIframe-doc');
+        $form->wait(1);
+        $form->dom->createLibBtn->click();
+        $form->wait(1);
+        $form->dom->name->setValue($apiLib);
+        $form->dom->btn($this->lang->save)->click();
+        /*创建接口文档*/
+        $form->dom->createApiBtn->click();
+        $form->dom->title->setValue($apiDoc->docA);
+        $form->dom->path->setValue($apiPath->pathA);
+        $form->dom->btn($this->lang->save)->click();
+        $form->wait(1);
+        $this->openUrl('api', 'index');
+        $form->dom->search(array("接口名称,=,{$apiDoc->docA}"));
+        $form->wait(1);
+        if($form->dom->fstDocPath->getText() != $apiPath->pathA) return $this->failed('创建接口文档失败');
+        return $this->success('创建接口文档成功');
     }
 }
