ff9c1d0f6cb91c3dcec075a22e07ce24ea9f6b4d
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-09 08:51:42 +0000

    *[task#127578,done,0.3h] Add ui test files of add ztf comments

--- module/doc/test/ui/editdoc.php
+++ module/doc/test/ui/editdoc.php
@@ -1,6 +1,23 @@
 #!/usr/bin/env php
 <?php
 
+/**
+
+title=编辑文档测试
+timeout=0
+cid=0
+
+- 编辑文档成功
+ - 测试结果 @编辑文档成功
+ - 最终测试状态 @SUCCESS
+- 移动文档成功
+ - 测试结果 @移动文档成功
+ - 最终测试状态 @SUCCESS
+- 删除文档成功
+ - 测试结果 @删除文档成功
+ - 最终测试状态 @SUCCESS
+
+*/
 chdir(__DIR__);
 include '../lib/editdoc.ui.class.php';
 
@@ -16,6 +33,6 @@ $editDocName->editName = '编辑文档1';
 $libName = new stdClass();
 $libName->myDocLib = '我的文档库1';
 
-r($tester->editDoc($docName, $editDocName)) && p('message,status') && e('编辑文档表单页提示信息正确,SUCCESS');
-r($tester->moveDoc($libName))               && p('message,status') && e('移动文档成功，SUCCESS');
-r($tester->deleteDoc())                     && p('message,status') && e('删除文档成功，SUCCESS');
+r($tester->editDoc($docName, $editDocName)) && p('message,status') && e('编辑文档成功,SUCCESS'); //编辑文档成功
+r($tester->moveDoc($libName))               && p('message,status') && e('移动文档成功,SUCCESS'); //移动文档成功
+r($tester->deleteDoc())                     && p('message,status') && e('删除文档成功,SUCCESS'); //删除文档成功
