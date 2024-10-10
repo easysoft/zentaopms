19c7d86a6a652ac2e211991fa6013f003ea4b099
azalea <azalea@easycorp.ltd>
2024-10-10 14:10:31 +0800

    * [uitest] Add comment.

--- module/user/test/ui/createuser.php
+++ module/user/test/ui/createuser.php
@@ -7,7 +7,16 @@ title=创建用户
 timeout=0
 cid=1
 
-*/
+- 正常创建用户，创建成功
+ - 测试结果 @成功创建用户
+- 创建重名用户
+ - 测试结果 @创建重名用户提示正确
+-创建用户名为空的用户
+ - 测试结果 @用户名为空提示正确
+- 创建密码为空的用户
+ - 测试结果 @密码为空提示正确
+
+ */
 chdir(__DIR__);
 include '../lib/createuser.ui.class.php';
 
@@ -22,10 +31,10 @@ $user->confirmPassword = '123456';
 $user->realname        = '李娟娟';
 $user->verifyPassword  = $config->uitest->defaultPassword;
 
-r($tester->createNormalUser($user)) && p('message')  && e('创建用户成功');                   // 创建用户后的跳转链接检查
-r($tester->createNormalUser($user)) && p('message')  && e('创建重名用户提示正确');           // 创建用户后的跳转链接检查
+r($tester->createNormalUser($user)) && p('message')  && e('创建用户成功');                   // 创建用户成功
+r($tester->createNormalUser($user)) && p('message')  && e('创建重名用户提示正确');           // 创建重名用户提示正确
 
-r($tester->createEmptyAccountUser($user))  && p('message')  && e('用户名为空提示正确');      // 创建用户后的跳转链接检查
-r($tester->createEmptyPasswordUser($user)) && p('message')  && e('密码为空提示正确');        // 创建用户后的跳转链接检查
+r($tester->createEmptyAccountUser($user))  && p('message')  && e('用户名为空提示正确');      // 用户名为空的提示正确
+r($tester->createEmptyPasswordUser($user)) && p('message')  && e('密码为空提示正确');        // 密码为空的提示正确
 
 $tester->closeBrowser();
