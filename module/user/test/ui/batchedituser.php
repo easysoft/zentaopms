a1f8ba5867b0116093ab2b7c7e6ecd5a54baf1e3
azalea <azalea@easycorp.ltd>
2024-10-10 14:37:28 +0800

    * [uitest] Add comment.

--- module/user/test/ui/batchedituser.php
+++ module/user/test/ui/batchedituser.php
@@ -7,6 +7,13 @@ title=批量编辑用户
 timeout=0
 cid=2
 
+- 正常批量编辑用户
+ - 测试结果 @保存成功
+- 批量编辑用户，姓名字段置空
+ - 测试结果 @姓名不能为空
+- 批量编辑用户，验证密码置空
+ - 测试结果 @验证失败，请检查您的系统登录密码是否正确
+
 */
 chdir(__DIR__);
 include '../lib/batchedituser.ui.class.php';
@@ -27,8 +34,8 @@ $user2 = new stdclass();
 $user2->realname        = '李娟';
 $user2->verifyPassword  = '';
 
-r($tester->batchEditUser($user))        && p('message') && e('保存成功');                         //批量编辑用户后的跳转链接检查
-r($tester->emptyRealname($user1))       && p('message') && e('姓名不能为空');                             //批量编辑用户后的跳转链接检查
-r($tester->wrongVerifyPassword($user2)) && p('message') && e('验证失败，请检查您的系统登录密码是否正确'); //批量编辑用户后的跳转链接检查
+r($tester->batchEditUser($user))        && p('message') && e('保存成功');                                 // 正常批量编辑用户
+r($tester->emptyRealname($user1))       && p('message') && e('姓名不能为空');                             // 批量编辑用户，姓名字段置空
+r($tester->wrongVerifyPassword($user2)) && p('message') && e('验证失败，请检查您的系统登录密码是否正确'); // 批量编辑用户，验证密码置空
 
 $tester->closeBrowser();
