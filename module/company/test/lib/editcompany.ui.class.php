c1d274f22d50045ef7bb064864b45227de0885de
azalea <azalea@easycorp.ltd>
2024-10-10 11:25:54 +0800

    * [uitest] Adjust editcompany for edit company information.

--- module/company/test/lib/editcompany.ui.class.php
+++ module/company/test/lib/editcompany.ui.class.php
@@ -15,3 +15,15 @@ class editCompanyTester extends tester
         $this->openURL('company', 'view',array(),'appIframe-system');
         $form = $this->loadPage('company', 'edit');
         $form->dom->editbtn->click();
+
+        $form->dom->name->setValue($company->name);
+        $form->dom->phone->setValue($company->phone);
+        $form->dom->address->setValue($company->address);
+        $form->dom->zipcode->setValue($company->zipcode);
+        $form->dom->primary->click();
+        $form->wait(1);
+        $form->dom->savebtn->click();
+
+        return $this->success('编辑公司信息成功');
+    }
+}
