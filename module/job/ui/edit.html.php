cd4273d3493e875c69dcee330cde8d2e70261107
caoyanyi <caoyanyi@easycorp.ltd>
2024-10-10 09:19:19 +0800

    - [task#129928,doing,1.5h] delete trigger form for job.

--- module/job/ui/edit.html.php
+++ module/job/ui/edit.html.php
@@ -12,88 +12,11 @@ namespace zin;
 
 jsVar('engine', $job->engine);
 jsVar('job', $job);
-jsVar('dirChange', $lang->job->dirChange);
-jsVar('buildTag', $lang->job->buildTag);
 jsVar('repoList', $repoList);
 jsVar('triggerList', $lang->job->triggerTypeList);
 jsVar('dirs', !empty($dirs) ? $dirs : '');
 
 if($job->engine != 'jenkins') unset($lang->job->frameList['sonarqube']);
-
-if($job->customParam)
-{
-    $customParam = array();
-    $i = 1;
-    foreach(json_decode($job->customParam) as $paramName => $paramValue)
-    {
-        $isCustom = zget($lang->job->paramValueList, $paramValue, '') ? false : true;
-        $customParam[] =  inputGroup
-        (
-            $lang->job->paramName,
-            input
-            (
-                setStyle('width', '50%'),
-                setClass('form-control'),
-                set::id("paramName{$i}"),
-                set::name('paramName[]'),
-                set::value($paramName)
-            ),
-            $lang->job->paramValue,
-            select
-            (
-                setStyle('width', '25%'),
-                setClass('paramValue ' . ($isCustom ? 'hidden' : '')),
-                set::id("paramValue{$i}"),
-                set::name('paramValue[]'),
-                !$isCustom ? set::value($paramValue) : null,
-                set::items($lang->job->paramValueList),
-                $isCustom ? set::disabled(true) : null
-            ),
-            input
-            (
-                setStyle('width', '25%'),
-                setClass('form-control paramValue ' . (!$isCustom ? 'hidden' : '')),
-                set::id("paramValueInput{$i}"),
-                set::name('paramValue[]'),
-                $isCustom ? set::value($paramValue) : null,
-                !$isCustom ? set::disabled(true) : null
-            ),
-            span
-            (
-                setClass('input-group-addon'),
-                checkbox
-                (
-                    setClass('custom'),
-                    set::id("custom{$i}"),
-                    set::name('custom'),
-                    set::text($lang->job->custom),
-                    set::checked($isCustom)
-                )
-            ),
-            span
-            (
-                setClass('input-group-addon'),
-                h::a
-                (
-                    setClass('add-param'),
-                    set::href('javascript:void(0)'),
-                    icon('plus')
-                )
-            ),
-            span
-            (
-                setClass('input-group-addon'),
-                a
-                (
-                    setClass('delete-param'),
-                    set::href('javascript:void(0)'),
-                    icon('close')
-                )
-            )
-        );
-        $i++;
-    }
-}
 
 if($this->session->repoID)
 {
@@ -118,10 +41,6 @@ formPanel
     set::title($lang->job->edit),
     setClass('job-form'),
     set::labelWidth('10em'),
-    on::click('.add-param', 'addItem'),
-    on::click('.delete-param', 'deleteItem'),
-    on::click('.custom', 'setValueInput'),
-    on::click('select.paramValue', 'changeCustomField'),
     on::click('.dropmenu-list li.tree-item', 'setJenkinsJob'),
     set::actionsClass('w-2/3'),
     formGroup
@@ -225,41 +144,6 @@ formPanel
             )
         )
     ) : null,
-    formGroup
-    (
-        set::name('useZentao'),
-        set::label($lang->job->useZentao),
-        set::control('radioListInline'),
-        set::items($lang->job->zentaoTrigger),
-        set::value($job->triggerType ? '1' : '0'),
-        set::width('1/2'),
-        on::change('changeTrigger')
-    ),
-    formRow
-    (
-        formGroup
-        (
-            set::name('triggerType'),
-            set::width('1/2'),
-            set::required(true),
-            set::label($lang->job->triggerType),
-            set::items($lang->job->triggerTypeList),
-            set::value($job->triggerType),
-            on::change('changeTriggerType')
-        )
-    ),
-    formRow
-    (
-        setClass('svn-fields hidden'),
-        formGroup
-        (
-            set::name('svnDir[]'),
-            set::width('1/2'),
-            set::label($lang->job->svnDir),
-            set::items(!empty($dirs) ? $dirs : array()),
-            set::value($job->svnDir)
-        )
-    ),
     formRow
     (
         setClass('sonarqube hidden'),
@@ -277,7 +161,7 @@ formPanel
     formRow
     (
         set::id('sonarProject'),
-        setClass('sonarqube', $job->projectKey ? '' : 'hidden'),
+        setClass('sonarqube', $job->frame == 'sonarqube' && $job->projectKey ? '' : 'hidden'),
         formGroup
         (
             set::name('projectKey'),
@@ -287,120 +171,5 @@ formPanel
             set::value($job->projectKey),
             set::required(true)
         )
-    ),
-    formRow
-    (
-        setClass('comment-fields hidden'),
-        formGroup
-        (
-            set::name('comment'),
-            set::label($lang->job->comment),
-            set::value($job->comment),
-            set::width('1/2'),
-            set::required(true)
-        ),
-        h::span
-        (
-            setClass('leading-8 ml-2'),
-            html($lang->job->commitEx)
-        )
-    ),
-    formRow
-    (
-        setClass('custom-fields hidden'),
-        formGroup
-        (
-            set::label(''),
-            set::name('atDay[]'),
-            set::control('checkListInline'),
-            set::items($lang->datepicker->dayNames),
-            set::value($job->atDay),
-            set::width('1/2')
-        )
-    ),
-    formRow
-    (
-        setClass('custom-fields hidden'),
-        formGroup
-        (
-            set::label(''),
-            set::width('1/2'),
-            inputGroup
-            (
-                $lang->job->atTime,
-                timePicker
-                (
-                    set::name('atTime'),
-                    set::value($job->atTime)
-                )
-            )
-        )
-    ),
-    formRow
-    (
-        set::id('paramDiv'),
-        formGroup
-        (
-            set::label($lang->job->customParam),
-            !empty($job->customParam) ? $customParam : null,
-            set::width('2/3'),
-            inputGroup
-            (
-                $lang->job->paramName,
-                input
-                (
-                    setStyle('width', '50%'),
-                    setClass('form-control paramName'),
-                    set::name('paramName[]')
-                ),
-                $lang->job->paramValue,
-                select
-                (
-                    setStyle('width', '25%'),
-                    setClass('paramValue'),
-                    set::name('paramValue[]'),
-                    set::items($lang->job->paramValueList)
-                ),
-                input
-                (
-                    setStyle('width', '25%'),
-                    setClass('form-control hidden paramValue'),
-                    set::name('paramValue[]'),
-                    set::disabled(true)
-                ),
-                span
-                (
-                    setClass('input-group-addon'),
-                    checkbox
-                    (
-                        setClass('custom'),
-                        set::name('custom'),
-                        set::text($lang->job->custom)
-                    )
-                ),
-                span
-                (
-                    setClass('input-group-addon'),
-                    h::a
-                    (
-                        setClass('add-param'),
-                        set::href('javascript:void(0)'),
-                        icon('plus')
-                    )
-                ),
-                span
-                (
-                    setClass('input-group-addon'),
-                    a
-                    (
-                        setClass('delete-param'),
-                        set::href('javascript:void(0)'),
-                        icon('close')
-                    )
-                )
-            )
-        )
     )
 );
-
-render();
