77c306c8a42b6bfb7e1b02747fd67249d0cecee0
caoyanyi <caoyanyi@easycorp.ltd>
2024-10-10 16:05:26 +0800

    + [task#129928,doing,0.5h] add js param for job trigger.

---
+++ module/job/ui/trigger.html.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+/**
+ * The trigger view file of job module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2024 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Yanyi Cao <caoyanyi@chandao.com>
+ * @package     job
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+jsVar('repo', $repo);
+jsVar('dirs', !empty($dirs) ? $dirs : '');
+jsVar('buildTag', $lang->job->buildTag);
+jsVar('dirChange', $lang->job->dirChange);
+jsVar('triggerTypeList', $lang->job->triggerTypeList);
