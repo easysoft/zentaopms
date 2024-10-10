c26d1c08c90c814be7edb44d3f2bdc9646c6e31a
caoyanyi <caoyanyi@easycorp.ltd>
2024-10-10 16:06:39 +0800

    + [task#129928,doing,1.5h] add js param for job trigger.

--- module/job/ui/trigger.html.php
+++ module/job/ui/trigger.html.php
@@ -92,3 +92,89 @@ jsVar('triggerField', div
         div(setID('triggerPicker%s'))
     )
 )->render());
+
+$triggers = array();
+if(empty($job->triggerType)) $job->triggerType = key($lang->job->triggerTypeList);
+foreach(explode(',', $job->triggerType) as $index => $trigger)
+{
+    $triggers[] = div
+    (
+        setClass('trigger-box border py-4 bg-gray-100'),
+        icon
+        (
+            'trash pull-right mr-4 delete-trigger cursor-pointer',
+            !strpos($job->triggerType, ',') ? setClass('hidden') : null
+        ),
+        formGroup
+        (
+            set::width('1/2'),
+            set::name("triggerType[$index]"),
+            set::required(true),
+            set::label($lang->job->triggerType),
+            set::items($lang->job->triggerTypeList),
+            set::value($trigger)
+        ),
+        $trigger == 'tag' && $repo->SCM == 'Subversion' ? formRow
+        (
+            setClass('svn-fields hidden'),
+            formGroup
+            (
+                set::name('svnDir[]'),
+                set::width('1/2'),
+                set::label($lang->job->svnDir),
+                set::items(!empty($dirs) ? $dirs : array()),
+                set::value($job->svnDir)
+            )
+        ) : null,
+        $trigger == 'commit' ? formRow
+        (
+            setClass('comment-fields hidden mt-4'),
+            formGroup
+            (
+                set::name('comment'),
+                set::label($lang->job->comment),
+                set::value($job->comment),
+                set::width('1/2'),
+                set::required(true)
+            ),
+            h::span
+            (
+                setClass('leading-8 ml-2'),
+                html($lang->job->commitEx)
+            )
+        ) : null,
+        $trigger == 'schedule' ? div
+        (
+            formRow
+            (
+                setClass('custom-fields hidden mt-2'),
+                formGroup
+                (
+                    set::label(''),
+                    set::name('atDay[]'),
+                    set::control('checkListInline'),
+                    set::items($lang->datepicker->dayNames),
+                    set::value($job->atDay)
+                )
+            ),
+            formRow
+            (
+                setClass('custom-fields hidden'),
+                formGroup
+                (
+                    set::label(''),
+                    set::width('1/2'),
+                    inputGroup
+                    (
+                        $lang->job->atTime,
+                        timePicker
+                        (
+                            set::name('atTime'),
+                            set::value($job->atTime)
+                        )
+                    )
+                )
+            )
+        ) : null,
+    );
+}
