5ee8e006c757b7f240e9bd9cc8a1a9d8cc32a554
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-10-11 13:22:33 +0800

    *[task#129870,doing,0.5h,1h]add zendata

---
+++ module/product/test/ui/switchtab.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php
+<?php
+
+/**
+
+title=切换产品列表tab
+timeout=0
+cid=0
+
+*/
+chdir(__DIR__);
+include '../lib/all.ui.class.php';
+
+$product = zenData('product');
+$product->id->range('1-3');
+$product->name->range('产品1,产品2,产品3');
+$product->status->range('normal{2},closed{1}');
+$product->type->range('normal');
+$product->gen(3);
