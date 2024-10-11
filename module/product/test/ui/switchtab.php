a571919397c7a29390baf4d908ae1c135b1ad7f2
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-10-11 13:24:30 +0800

    *[task#129870,doing,0.5h,1h]uitest case of switch product tab

--- module/product/test/ui/switchtab.php
+++ module/product/test/ui/switchtab.php
@@ -17,3 +17,12 @@ $product->name->range('产品1,产品2,产品3');
 $product->status->range('normal{2},closed{1}');
 $product->type->range('normal');
 $product->gen(3);
+
+$tester = new allTester();
+$tester->login();
+
+r($tester->switchTab('all', '3'))    && p('message,status') && e('切换至allTab成功,SUCCESS');//切换至全部产品Tab
+r($tester->switchTab('open', '2'))   && p('message,status') && e('切换至openTab成功,SUCCESS');//切换至未关闭Tab
+r($tester->switchTab('closed', '1')) && p('message,status') && e('切换至closedTab成功,SUCCESS');//切换至结束Tab
+
+$tester->closeBrowser();
