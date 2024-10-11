2f951c1cccb52ca332bfe662edb2542089aaeb67
huairong <huairong@easycorp.ltd>
2024-10-11 11:16:12 +0800

    + [task#130151] Add project data of ui test file.

---
+++ module/design/test/ui/browse.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php
+<?php
+chdir(__DIR__);
+include '../lib/browse.ui.class.php';
+
+$project = zendata('project');
+$project->id->range('1');
+$project->project->range('0');
+$project->model->range('waterfall');
+$project->type->range('project');
+$project->parent->range('0');
+$project->auth->range('extend');
+$project->grade->range('1');
+$project->name->range('瀑布项目1');
+$project->path->range('`,1,`');
+$project->begin->range('(-2M)-(-M):1D')->type('timestamp')->format('YY/MM/DD');
+$project->end->range('(+2M)-(+3M):1D')->type('timestamp')->format('YY/MM/DD');
+$project->acl->range('open');
+$project->gen(1);
