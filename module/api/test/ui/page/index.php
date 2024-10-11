528e51153805d67f007954a84075c269303a9d27
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-11 10:44:16 +0000

    *[task#130296,doing,0.1h] Add ui test files of edit an apilib

--- module/api/test/ui/page/index.php
+++ module/api/test/ui/page/index.php
@@ -8,6 +8,9 @@ class indexPage extends page
             'createLibBtn' => '/html/body/div[1]/div/div[1]/div[2]/a[4]',
             'createApiBtn' => '/html/body/div[1]/div/div[1]/div[2]/a[5]',
             'fstDocPath'   => '/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/ul/li/div/a/span[2]',
+            'fstMoreBtn'   => '/html/body/div/div/div[2]/div[1]/div[1]/div[2]/main/menu/li[1]/div/nav/button',
+            'fstEditBtn'   => '//*[@class="popover show fade dropdown in"]/menu/menu/li[2]/a[1]',
+            'fstLibTitle'  => '/html/body/div[1]/div/div[2]/div[1]/div[1]/div[2]/main/menu/li[1]/div/div/a'
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
