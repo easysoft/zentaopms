e8e451be117578175b3e1dbffc449fb2d27b57a0
huairong <huairong@easycorp.ltd>
2024-10-11 11:19:25 +0800

    + [task#130151] Add testcase of browse ui test file.

--- module/design/test/ui/browse.php
+++ module/design/test/ui/browse.php
@@ -36,3 +36,19 @@ $design->product->range('1{2}, 2{2}');
 $design->name->range('概要设计1, 详细设计1, 数据库设计1, 接口设计1');
 $design->type->range('HLDS, DDS, DBDS, ADS');
 $design->gen(4);
+
+$tester = new browseTester();
+$tester->login();
+
+/* 检查各个菜单下显示数据 */
+r($tester->checkMenu('hlds', '1')) && p('message') && e('hlds菜单下显示数据正确');
+r($tester->checkMenu('dds', '1'))  && p('message') && e('dds菜单下显示数据正确');
+r($tester->checkMenu('dbds', '1')) && p('message') && e('dbds菜单下显示数据正确');
+r($tester->checkMenu('ads', '1'))  && p('message') && e('ads菜单下显示数据正确');
+r($tester->checkMenu('all', '4'))  && p('message') && e('all菜单下显示数据正确');
+
+/* 检查不同产品下显示数据 */
+r($tester->switchProduct('firstProduct', '2'))  && p('message') && e('切换firstProduct查看设计数据成功');
+r($tester->switchProduct('secondProduct', '2')) && p('message') && e('切换secondProduct查看设计数据成功');
+
+$tester->closeBrowser();
