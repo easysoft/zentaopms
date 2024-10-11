65686d491248ca838edfc61fea7c7445eb692f39
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-10-11 10:54:39 +0800

    *[task#129870,doing,0.5h,2h]modify code

--- module/product/test/ui/page/all.php
+++ module/product/test/ui/page/all.php
@@ -5,10 +5,16 @@ class allPage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'selectAllBtn'  => "//*[@id='products']/div[1]/div[1]/div/div/div[1]/div/label",
-            'batchEditBtn'  => "//*[@id='products']/div[3]/nav[1]/a",
-            'allProductTab' => "//*[@id='featureBar']/menu/li[1]/a/span[1]",
-            'saveBtn'       => "//*[@id='zin_product_batchedit_formBatch']/div[2]/button[1]",
+            'selectAllBtn' => "//*[@id='products']/div[1]/div[1]/div/div/div[1]/div/label",
+            'batchEditBtn' => "//*[@id='products']/div[3]/nav[1]/a",
+            'saveBtn'      => "//*[@id='zin_product_batchedit_formBatch']/div[2]/button[1]",
+            /*tab标签*/
+            'allTab'    => "//*[@id='featureBar']/menu/li[1]/a/span[1]",
+            'openTab'   => "//*[@id='featureBar']/menu/li[2]/a/span[1]",
+            'closedTab' => "//*[@id='featureBar']/menu/li[3]/a/span[1]",
+            'allNum'    => "//*[@id='featureBar']/menu/li[1]/a/span[2]",
+            'openNum'   => "//*[@id='featureBar']/menu/li[2]/a/span[2]",
+            'closedNum' => "//*[@id='featureBar']/menu/li[3]/a/span[2]",
             /*维护产线*/
             'lineDialog'    => "//*[@id='manageLineModal']/div",
             'manageLineBtn' => "//*[@id='actionBar']/button[2]",
