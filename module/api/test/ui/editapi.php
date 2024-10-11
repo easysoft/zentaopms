d4eed55a57ae943a34d54ffe4560ec1d1fe2fb18
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-11 10:46:40 +0000

    *[task#130296,doing,0.2h] Add ui test files of edit an apilib

--- module/api/test/ui/editapi.php
+++ module/api/test/ui/editapi.php
@@ -18,3 +18,18 @@ $doclib->product->range('0');
 $doclib->name->range('独立接口库1,独立接口库2,独立接口库3,独立接口库4,独立接口库5');
 $doclib->acl->range('open');
 $doclib->order->range('0');
+$doclib->gen(5);
+
+$tester = new createDocTester();
+$tester->login();
+
+$editLib = new stdClass();
+$editLib->title = '编辑接口库';
+
+$apiDoc = new stdClass();
+$apiDoc->docB = 'apiDocB';
+
+$apiPath = new stdClass();
+$apiPath->pathB = 'apipathB';
+
+r($tester->editApiLib($editLib, $apiDoc, $apiPath)) && p('message,status') && e('编辑接口文档成功,SUCCESS'); //编辑接口文档成功
