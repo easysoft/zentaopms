3dc0f6de219096db9c76aabd79b01f01f3f2869e
huairong <huairong@easycorp.ltd>
2024-10-11 11:17:40 +0800

    + [task#130151] Add product and design data of ui test file.

--- module/design/test/ui/browse.php
+++ module/design/test/ui/browse.php
@@ -17,3 +17,22 @@ $project->begin->range('(-2M)-(-M):1D')->type('timestamp')->format('YY/MM/DD');
 $project->end->range('(+2M)-(+3M):1D')->type('timestamp')->format('YY/MM/DD');
 $project->acl->range('open');
 $project->gen(1);
+
+$product = zenData('product');
+$product->id->range('1-2');
+$product->name->range('产品1, 产品2');
+$product->type->range('normal');
+$product->gen(2);
+
+$projectProduct = zenData('projectproduct');
+$projectProduct->project->range('1');
+$projectProduct->product->range('1{1}, 2{1}');
+$projectProduct->gen(2);
+
+$design = zendata('design');
+$design->id->range('1-4');
+$design->project->range('1{4}');
+$design->product->range('1{2}, 2{2}');
+$design->name->range('概要设计1, 详细设计1, 数据库设计1, 接口设计1');
+$design->type->range('HLDS, DDS, DBDS, ADS');
+$design->gen(4);
