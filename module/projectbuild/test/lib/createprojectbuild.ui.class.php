6a46670bb47c94fa91c18fceffc94871aed80227
lihuifang <lihuifang@easycorp.ltd>
2024-10-12 11:02:02 +0800

    *[task#129800,doing,3h,0.5h] Add ui function files of create project build.

--- module/projectbuild/test/lib/createprojectbuild.ui.class.php
+++ module/projectbuild/test/lib/createprojectbuild.ui.class.php
@@ -18,3 +18,22 @@ class createProjectBuildTester extends tester
         /*断言检查提示信息*/
         if($this->response('method') != 'view')
         {
+            if($this->checkFormTips('projectbuild')) return $this->success('创建项目版本表单页提示信息正确');
+            return $this->failed('创建项目版本表单页提示信息不正确');
+        }
+    }
+
+    /**
+     * 创建项目版本。
+     * Creat the project build.
+     *
+     * @param  array $build
+     * @access public
+     */
+    public function createProjectBuild($build)
+    {
+        $form = $this->initForm('projectbuild', 'create', array('projectID' => 1), 'appIframe-project');
+
+        if(isset($build['execution'])) $form->dom->execution->picker($build['execution']);
+        if(isset($build['name']))      $form->dom->name->setValue($build['name']);
+        $form->dom->btn($this->lang->save)->click();
