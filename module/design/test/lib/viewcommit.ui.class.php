da529eb28600b7a3d6ed2030d49745951a6e3aea
huairong <huairong@easycorp.ltd>
2024-10-12 15:04:11 +0800

    + [task#130368] Add comment of viewcommit function.

--- module/design/test/lib/viewcommit.ui.class.php
+++ module/design/test/lib/viewcommit.ui.class.php
@@ -2,6 +2,15 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class viewCommitTester extends tester
 {
+    /**
+     * 检查查看提交页面数据。
+     * Check the data of viewcommit page.
+     *
+     * @param  string
+     * @param  string $expectNum
+     * @access public
+     * @return object
+     */
     public function viewCommit($expectNum)
     {
         $form = $this->initForm('design', 'browse', array('projecID' => 1), 'appIframe-project');
