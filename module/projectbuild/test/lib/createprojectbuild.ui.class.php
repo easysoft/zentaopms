72609abf388af896678bc8ff78048ec1c31d9200
lihuifang <lihuifang@easycorp.ltd>
2024-10-12 11:01:36 +0800

    +[task#129800,doing,3h,1h] Add ui function files of create project build.

---
+++ module/projectbuild/test/lib/createprojectbuild.ui.class.php
@@ -0,0 +1,20 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+class createProjectBuildTester extends tester
+{
+    /**
+     * 创建项目版本时名称必填提示检查。
+     * Check for no name input when creating the project build.
+     *
+     * @param  array $build
+     * @access public
+     */
+    public function checkNoNameInfo($build)
+    {
+        $form = $this->initForm('projectbuild', 'create', array('projectID' => 1), 'appIframe-project');
+        $form->dom->btn($this->lang->save)->click();
+        $form->wait(2);
+
+        /*断言检查提示信息*/
+        if($this->response('method') != 'view')
+        {
