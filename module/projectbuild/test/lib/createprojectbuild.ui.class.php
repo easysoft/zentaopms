24f2db0c741bce2a226c36652888aee2f36582b6
lihuifang <lihuifang@easycorp.ltd>
2024-10-12 11:02:22 +0800

    *[task#129800,done,3h,0h] Add ui function files of create project build.

--- module/projectbuild/test/lib/createprojectbuild.ui.class.php
+++ module/projectbuild/test/lib/createprojectbuild.ui.class.php
@@ -37,3 +37,17 @@ class createProjectBuildTester extends tester
         if(isset($build['execution'])) $form->dom->execution->picker($build['execution']);
         if(isset($build['name']))      $form->dom->name->setValue($build['name']);
         $form->dom->btn($this->lang->save)->click();
+        $form->wait(2);
+
+        /* 跳转到版本概况页面，点击基本信息标签，查看信息是否正确 */
+        $viewPage = $this->loadPage('projectbuild', 'view');
+        $viewPage->dom->basic->click();
+        $viewPage->wait(5);
+
+        //断言检查版本名称、所属执行是否正确
+        if($viewPage->dom->basicBuildName->getText() != $build['name'])      return $this->failed('项目版本名称错误');
+        if($viewPage->dom->basicExecution->getText() != $build['execution']) return $this->failed('项目版本所属执行错误');
+
+        return $this->success('项目版本创建成功');
+    }
+}
