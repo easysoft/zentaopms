a62a5698f8f43bf75bf19ef8048d30ce1a32ec29
huairong <huairong@easycorp.ltd>
2024-10-12 15:03:38 +0800

    + [task#130368] Add viewcommit function of ui test file.

---
+++ module/design/test/lib/viewcommit.ui.class.php
@@ -0,0 +1,15 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+class viewCommitTester extends tester
+{
+    public function viewCommit($expectNum)
+    {
+        $form = $this->initForm('design', 'browse', array('projecID' => 1), 'appIframe-project');
+        $form->dom->viewCommitBtn->click();
+        $form->wait(1);
+        $string    = $form->dom->commitNum->getText();
+        $commitNum = preg_replace('/\D/', '', $string); //从字符串中提取数字部分
+        if($commitNum == $expectNum) return $this->success('查看提交页显示数据正确');
+        return $this->failed('查看提交页显示数据不正确');
+    }
+}
