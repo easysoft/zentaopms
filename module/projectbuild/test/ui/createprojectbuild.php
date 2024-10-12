926c3dc11f83f24472636168a70dc1e0dc4bdf4e
lihuifang <lihuifang@easycorp.ltd>
2024-10-12 10:58:03 +0800

    *[task#129800,doing,3h,2.5h] Add ui test file of create project build.

--- module/projectbuild/test/ui/createprojectbuild.php
+++ module/projectbuild/test/ui/createprojectbuild.php
@@ -16,3 +16,23 @@ cid=73
 
 */
 chdir(__DIR__);
+include '../lib/createprojectbuild.ui.class.php';
+
+$product = zenData('product');
+$product->id->range('1');
+$product->name->range('产品1');
+$product->type->range('normal');
+$product->gen(1);
+
+$project = zenData('project');
+$project->id->range('1');
+$project->project->range('0');
+$project->model->range('scrum');
+$project->type->range('project');
+$project->attribute->range('[]');
+$project->auth->range('[]');
+$project->parent->range('0');
+$project->grade->range('1');
+$project->name->range('敏捷项目1');
+$project->path->range('`,1,`');
+$project->begin->range('(-3w)-(-2w):1D')->type('timestamp')->format('YY/MM/DD');
