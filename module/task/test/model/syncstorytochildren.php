398bf93703e5358d2d6fae52fa8d08c678195778
liumengyi <liumengyi@easycorp.ltd>
2024-10-12 17:34:02 +0800

    + [task#130251,doing,0.5h] add unit test of taskModel::syncStoryToChildren.

--- module/task/test/model/syncstorytochildren.php
+++ module/task/test/model/syncstorytochildren.php
@@ -18,3 +18,23 @@ zenData('story')->gen(50);
 
 $task1 = new stdclass();
 $task1->id    = 1;
+$task1->story = 2;
+
+$task2 = new stdclass();
+$task2->id    = 2;
+$task2->story = 3;
+
+$task3 = new stdclass();
+$task3->id    = 3;
+$task3->story = 4;
+
+$task4 = new stdclass();
+$task4->id    = 11;
+$task4->story = 25;
+
+$task = new taskTest();
+
+r($task->syncStoryToChildrenTest($task1)) && p() && e('6:1;7:2'); // 同步任务 id 1 的需求 2 到其未关联需求的子任务
+r($task->syncStoryToChildrenTest($task2)) && p() && e('8:2;9:2'); // 同步任务 id 2 的需求 3 到其未关联需求的子任务
+r($task->syncStoryToChildrenTest($task3)) && p() && e('10:2');    // 同步任务 id 3 的需求 4 到其未关联需求的子任务
+r($task->syncStoryToChildrenTest($task4)) && p() && e('0');       // 同步任务 id 11 的需求 25 到其未关联需求的子任务
