1e4a92239ee5a6461a4caac308b42e3a82649c59
sunhao <sunhao@easycorp.ltd>
2024-10-14 17:05:51 +0800

    * [refac] remove unused code.

--- module/doc/js/app.ui.js
+++ module/doc/js/app.ui.js
@@ -599,15 +599,3 @@ window.beforeRequestContent = function(options)
 
     return false;
 };
-
-window.goToOldDocPage = function()
-{
-    const docApp = getDocApp();
-    if(!docApp) return;
-
-    zui.store.set('docAppEnabled', false);
-    const spaceType = docApp.signals.spaceType.value
-    const map       = {mine: 'myspace', custom: 'teamspace', product: 'productspace', project: 'projectspace'};
-    const method    = map[spaceType];
-    $.apps.openUrl($.createLink('doc', method));
-};
