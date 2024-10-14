ebf314275078d03294cfb345aabcd638713d7018
lihuifang <lihuifang@easycorp.ltd>
2024-10-14 10:33:39 +0800

    *[task#129801,doing,2h,0.5h] Add ui function files of edit project build.

--- module/projectbuild/test/lib/editprojectbuild.ui.class.php
+++ module/projectbuild/test/lib/editprojectbuild.ui.class.php
@@ -18,3 +18,21 @@ class editProjectBuildTester extends tester
 
         /* 断言检查提示信息 */
         if($this->response('method') != 'view')
+        {
+            if($this->checkFormTips('projectbuild')) return $this->success('编辑项目版本表单页提示信息正确');
+            return $this->failed('编辑项目版本表单页提示信息不正确');
+        }
+    }
+
+    /**
+     * 编辑项目版本。
+     * Edit the project build.
+     *
+     * @param  array $build
+     * @access public
+     */
+    public function editProjectBuild($build)
+    {
+        $form = $this->initForm('projectbuild', 'edit', array('projectID' => 1), 'appIframe-project');
+
+        if(isset($build['name'])) $form->dom->name->setValue($build['name']);
