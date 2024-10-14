effc3f2cad160c66a744bb61b437c244e72832fd
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-14 16:30:07 +0000

    *[task#130411,doing,0.2h] Add ui test files of release api

--- module/api/test/lib/releaseapi.ui.class.php
+++ module/api/test/lib/releaseapi.ui.class.php
@@ -14,5 +14,13 @@ class createDocTester extends tester
     {
         $form = $this->initForm('api', 'index', array(), 'appIframe-doc');
         $form->dom->publishBtn->click();
+        $form->wait(1);
+        $form->dom->version->setValue($apiVersion->fstVersion);
+        $form->dom->btn($this->lang->save)->click();
+        $form->wait(1);
+        $form->dom->releaseBtn->click();
+        $form->wait(1);
+        if($form->dom->fstVersion->getText() != $apiVersion->fstVersion) return $this->failed('发布接口失败');
+        return $this->success('发布接口成功');
     }
 }
