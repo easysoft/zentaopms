650d52fae54ac32b2d8de24e11c8c66048490db5
caoyanyi <caoyanyi@easycorp.ltd>
2024-10-14 10:49:26 +0800

    * [task#129926,doing,0.1h] add zen file for compile.

---
+++ module/compile/zen.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+/**
+ * The zen file of compile module of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2024 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Yanyi Cao <caoyanyi@chandao.com>
+ * @package     compile
+ * @link        https://www.zentao.net
+ */
+class compileZen extends compile
+{
+}
