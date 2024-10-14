cfdd470e9a4a7d541132b35692dca0e39ec1e478
daitingting <daitingting@xirangit.com>
2024-10-14 05:28:58 +0000

    * [task#129946,done,0.5h] Add batch move file.

---
+++ module/doc/ui/batchmovedoc.html.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+/**
+ * The batchMoveDoc view file of doc module of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2015 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     ZPL (http://zpl.pub/page/zplv12.html)
+ * @author      Tingting Dai <daitingting@xirangit.com>
+ * @package     doc
+ * @version     $Id$
+ * @link        http://www.zentao.net
+ */
+namespace zin;
+
+modalHeader(set::title($lang->doc->batchMove));
+
+formPanel
+(
+);
