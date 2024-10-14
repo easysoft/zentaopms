f985cee208f137b75b069cdb5fc66b05348292a6
luxuyang <luxuyang@easycorp.ltd>
2024-10-14 18:34:37 +0800

    * [task#127586,doing,2h,1h] finish multi-level testcase.

--- module/testcase/test/ui/createtestcase.php
+++ module/testcase/test/ui/createtestcase.php
@@ -12,9 +12,18 @@ $testcase = array(
     'precondition'   => '前置条件测试',
     'steps'          => array(
         'step1'  => 'expect1',
-        'group2' => array('step2.1' => 'expect2.1', 'step2.2' => 'expect2.2'),
-        'group3' => array('group3.1' => array('step3.3.1' => 'expect3.3.1', 'step3.3.2' => 'expect3.3.2'), 'step3.2' => 'expect3.2')
-    ),
+        'step2'  => 'expect2',
+        'step3'  => 'expect3',
+        'step4'  => 'expect4',
+        'step5'  => 'expect5',
+        'step6'  => 'expect6',
+        'group7' => array('step7.1' => 'expect7.1', 'step7.2' => 'expect7.2', 'step7.3' => 'expect7.3', 'step7.4' => 'expect7.4', 'step7.5' => 'expect7.5'),
+        'group8' => array('step8.1' => 'expect8.1', 'step8.2' => 'expect8.2', 'step8.3' => 'expect8.3', 'step8.4' => 'expect8.4', 'step8.5' => 'expect8.5'),
+        'group9' => array('step9.1' => 'expect9.1', 'step9.2' => 'expect9.2', 'step9.3' => 'expect9.3', 'step9.4' => 'expect9.4', 'step9.5' => 'expect9.5'),
+        'group10' => array('group10.1' => array('step10.1.1' => 'expect10.1.1', 'step10.1.2' => 'expect10.1.2', 'step10.1.3' => 'expect10.1.3', 'step10.1.4' => 'expect10.1.4'), 'step10.2' => 'expect10.2'),
+        'step11'  => 'expect11',
+        'group12' => array('group12.1' => array('step12.1.1' => 'expect12.1.1', 'step12.1.2' => 'expect12.1.2', 'step12.1.3' => 'expect12.1.3', 'step12.1.4' => 'expect12.1.4'), 'step12.2' => 'expect12.2'),
+    )
 );
 
 $project  = array(
