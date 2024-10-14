b2fda46e8287a63922736de2bc9b9a8522e168df
caoyanyi <caoyanyi@easycorp.ltd>
2024-10-14 10:45:52 +0800

    + [task#129926,doing,0.3h] add search block for compile page.

--- module/compile/ui/browse.html.php
+++ module/compile/ui/browse.html.php
@@ -19,11 +19,14 @@ if($repoID)
 }
 
 /* zin: Define the set::module('compile') feature bar on main menu. */
+$queryMenuLink = createLink('compile', 'browse', "repoID={$repoID}&jobID={$jobID}&browseType=bySearch&param={queryID}");
 featureBar
 (
     set::current('compile'),
     set::link(createLink('{key}', 'browse', "repoID=$repoID")),
-    set::itemLink(array('compile' => createLink('compile', 'browse', "repoID=$repoID&jobID=$jobID")))
+    set::itemLink(array('compile' => createLink('compile', 'browse', "repoID=$repoID&jobID=$jobID"))),
+    set::queryMenuLinkCallback(array(fn($key) => str_replace('{queryID}', (string)$key, $queryMenuLink))),
+    li(searchToggle(set::module('compile'), set::open($browseType == 'bySearch')))
 );
 
 /* zin: Define the toolbar on main menu. */
@@ -37,7 +40,7 @@ dtable
 (
     set::cols($config->compile->dtable->fieldList),
     set::data($tableData),
-    set::sortLink(createLink('compile', 'browse', "repoID=$repoID&jobID={$jobID}&orderBy={name}_{sortType}&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}&pageID={$pager->pageID}")),
+    set::sortLink(createLink('compile', 'browse', "repoID=$repoID&jobID={$jobID}&browseType={$browseType}&param={$param}&orderBy={name}_{sortType}&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}&pageID={$pager->pageID}")),
     set::orderBy($orderBy),
     set::onRenderCell(jsRaw('window.renderCell')),
     set::footPager(usePager())
