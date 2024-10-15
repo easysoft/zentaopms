a2bfd9c20db45b85b0945c2997d926955523c81d
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-15 10:41:10 +0000

    *[task#130410,doing,0.2h] Add ui test files of create struct

---
+++ module/api/test/ui/createstruct.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+
+/**
+
+title=创建数据结构测试
+timeout=0
+cid=0
+
+- 创建数据结构成功
+ - 测试结果 @创建数据结构成功
+ - 最终测试状态 @SUCCESS
+
+*/
+chdir(__DIR__);
+include '../lib/createstruct.ui.class.php';
+
+$doclib = zenData('doclib');
