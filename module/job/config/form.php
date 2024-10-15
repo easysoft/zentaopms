e20d4f58dc5a539cc3d6bfc6004ea43a750800e0
liyang <liyang@easycorp.ltd>
2024-10-15 16:58:25 +0800

    + [task#129957] Adjust create job config file.

--- module/job/config/form.php
+++ module/job/config/form.php
@@ -9,18 +9,11 @@ $config->job->form->create['engine']          = array('type' => 'string', 'requi
 $config->job->form->create['repo']            = array('type' => 'int',    'required' => true);
 $config->job->form->create['reference']       = array('type' => 'string', 'required' => false, 'default' => '');
 $config->job->form->create['frame']           = array('type' => 'string', 'required' => false, 'default' => '');
-$config->job->form->create['triggerType']     = array('type' => 'string', 'required' => false, 'default' => '');
-$config->job->form->create['svnDir']          = array('type' => 'array',  'required' => false, 'default' => array());
 $config->job->form->create['product']         = array('type' => 'int',    'required' => false, 'default' => 0);
 $config->job->form->create['sonarqubeServer'] = array('type' => 'int',    'required' => false, 'default' => 0);
 $config->job->form->create['projectKey']      = array('type' => 'string', 'required' => false, 'default' => '');
-$config->job->form->create['comment']         = array('type' => 'string', 'required' => false, 'default' => '');
-$config->job->form->create['atDay']           = array('type' => 'string', 'required' => false, 'default' => '');
-$config->job->form->create['atTime']          = array('type' => 'string', 'required' => false, 'default' => '');
 $config->job->form->create['jkServer']        = array('type' => 'int',    'required' => false, 'default' => 0);
 $config->job->form->create['jkTask']          = array('type' => 'string', 'required' => false, 'default' => '');
-$config->job->form->create['paramName']       = array('type' => 'array',  'required' => false, 'default' => array());
-$config->job->form->create['paramValue']      = array('type' => 'array',  'required' => false, 'default' => array());
 $config->job->form->create['createdDate']     = array('type' => 'string', 'required' => false, 'default' => $now);
 
 $config->job->form->edit = array();
