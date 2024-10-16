7df002e23da5e8522f385551212d43cb7d7a2fc5
liugang <liugang@easycorp.ltd>
2024-10-16 14:44:54 +0800

    - [perf] remove unused code of redis.

--- framework/model.class.php
+++ framework/model.class.php
@@ -20,15 +20,6 @@
 include __DIR__ . '/base/model.class.php';
 class model extends baseModel
 {
-    public $redis = false;
-
-    public function __construct($appName = '')
-    {
-        parent::__construct($appName);
-
-        if($this->config->redis) $this->redis = $this->app->redis;
-    }
-
     /**
      * 企业版部分功能是从然之合并过来的。ZDOO代码中调用loadModel方法时传递了一个非空的appName，在禅道中会导致错误。
      * 调用父类的loadModel方法来避免这个错误。
