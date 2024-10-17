50ea0ce72e886b94675107b47a05a0df74c1cbdf
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-17 17:19:00 +0000

    *[task#130266,done,0.1h] Add ui test files of fix the error code

--- module/api/test/lib/editapi.ui.class.php
+++ module/api/test/lib/editapi.ui.class.php
@@ -22,18 +22,4 @@ class createDocTester extends tester
         if($form->dom->fstLibTitle->getText() != $editLib->title) return $thhis->failed('编辑接口库失败');
         return $this->success('编辑接口库成功');
     }
-
-    /*
-     * 编辑接口文档。
-     * Edit a apiDoc.
-     *
-     * @param  string $apiDoc
-     * @param  string $apiPath
-     * @access public
-     * @return void
-     */
-    public function editApiDoc($apiDoc, $apiPath)
-    {
-        /*创建一个接口文档*/
-    }
 }
