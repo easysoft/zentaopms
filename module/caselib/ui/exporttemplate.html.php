def54497ee473844f356284eecb5e25b22702d17
sunguangming <sunguangming@easycorp.ltd>
2024-10-17 03:01:10 +0000

    * [bug#56203,done,0.2h] fix bug.

--- module/caselib/ui/exporttemplate.html.php
+++ module/caselib/ui/exporttemplate.html.php
@@ -35,30 +35,4 @@ formPanel
     set::submitBtnText($lang->export)
 );
 
-js
-(
-    <<<JAVASCRIPT
-    function setDownloading()
-    {
-        if(navigator.userAgent.toLowerCase().indexOf("opera") > -1) return true; // Opera don't support, omit it.
-
-        $.cookie.set('downloading', 0, {expires:config.cookieLife, path:config.webRoot});
-
-        time = setInterval(function()
-        {
-            if($.cookie.get('downloading') == 1)
-            {
-                $('.modal .modal-actions .close')[0].click();
-
-                $.cookie.set('downloading', null, {expires:config.cookieLife, path:config.webRoot});
-
-                clearInterval(time);
-            }
-        }, 300);
-
-        return true;
-    }
-    JAVASCRIPT
-);
-
 render('modalDialog');
