ece76b95d5c3572b6b6dfad6ef77162d5768d896
azalea <azalea@easycorp.ltd>
2024-10-17 15:07:55 +0800

    * [uitest] Init company data.

--- module/company/test/ui/edit.php
+++ module/company/test/ui/edit.php
@@ -10,6 +10,9 @@ cid=2
 - 编辑修改公司信息
  - 测试结果 @编辑公司信息成功
  - 最终测试状态 @SUCCESS
+-重置匿名登录为不允许
+ - 测试结果 @匿名登录修改为不允许
+ - 最终测试状态 @SUCCESS
 
 */
 
@@ -25,6 +28,7 @@ $company->phone   = '0532-86893032';
 $company->address = '青铁广场18楼';
 $company->zipcode = '266520';
 
-r($tester->editCompany($company)) && p('message') && e('编辑公司信息成功');   //编辑修改公司信息
+r($tester->editCompany($company)) && p('message') && e('编辑公司信息成功');       //编辑修改公司信息
+r($tester->initPrimary($company)) && p('message') && e('匿名登录修改为不允许');   //重置匿名登录为不允许
 
 $tester->closeBrowser();
