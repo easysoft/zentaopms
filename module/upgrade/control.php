3b1118810bffc90577b85cb263bd055c735c3a66
sunguangming <sunguangming@easycorp.ltd>
2024-10-18 07:50:24 +0000

    * [bug#56298,done,0.2h] fix bug.

--- module/upgrade/control.php
+++ module/upgrade/control.php
@@ -545,7 +545,7 @@ class upgrade extends control
     {
         /* 如果数据库有冲突，显示更改的 sql。*/
         /* If there is a conflict with the standard database, display the changed sql. */
-        $alterSQL = $this->upgrade->checkConsistency($this->config->version);
+        $alterSQL = $this->config->db->driver == 'mysql' ? $this->upgrade->checkConsistency() : array();
         if(!empty($alterSQL)) return $this->displayConsistency($alterSQL);
 
         /**
@@ -592,7 +592,7 @@ class upgrade extends control
         $hasError = $this->upgrade->hasConsistencyError();
         if(file_exists($logFile)) unlink($logFile);
 
-        $alterSQL = $this->upgrade->checkConsistency();
+        $alterSQL = $this->config->db->driver == 'mysql' ? $this->upgrade->checkConsistency() : array();
         if(empty($alterSQL))
         {
             /* 能访问禅道官网插件接口跳转到检查插件页面，否则跳转到选择版本页面。*/
