0b139336bbded7ce53f5f6ea57854b8d64b89203
lihuifang <lihuifang@easycorp.ltd>
2024-10-21 17:07:27 +0800

    +[task#130687,doing,6h,5h] modify zendata notes.

--- module/projectrelease/test/ui/createprojectrelease.php
+++ module/projectrelease/test/ui/createprojectrelease.php
@@ -2,9 +2,15 @@
 <?php
 
 /**
+
 title=创建项目发布
 timeout=0
 cid=73
+
+- 缺少发布名称，检查提示信息测试结果 @创建项目发布表单页提示信息正确
+- 创建状态为未开始的发布最终测试状态 @SUCCESS
+- 创建状态为已发布的发布最终测试状态 @SUCCESS
+
 */
 chdir(__DIR__);
 include '../lib/createprojectrelease.ui.class.php';
@@ -19,7 +25,7 @@ $release = array(
     array('name' => '一个已发布的项目发布'.time(), 'status' => '已发布', 'plandate' => date('Y-m-d', strtotime('+1 day')), 'releasedate' => date('Y-m-d', strtotime('+1 month'))),
 );
 
-r($tester->checkInput($release['0'],$project)) && p('message') && e('创建敏捷项目表单页提示信息正确');   // 缺少发布名称，检查提示信息
+r($tester->checkInput($release['0'],$project)) && p('message') && e('创建项目发布表单页提示信息正确');   // 缺少发布名称，检查提示信息
 r($tester->checkInput($release['1'],$project)) && p('status')  && e('SUCCESS');                          // 创建状态为未开始的发布
 r($tester->checkInput($release['2'],$project)) && p('status')  && e('SUCCESS');                          // 创建状态为已发布的发布
 
