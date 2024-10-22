d3ca3648b732ccc8b7ec508afc51c6fb87e2cfba
lihuifang <lihuifang@easycorp.ltd>
2024-10-22 11:16:45 +0800

    [task#130687,doing,6h,4h] Modify notes of create sprint.

--- module/project/test/ui/createsprint.php
+++ module/project/test/ui/createsprint.php
@@ -2,9 +2,19 @@
 <?php
 
 /**
+
 title=敏捷项目下创建迭代
 timeout=0
 cid=1
+
+- 执行名称为空时的提示校验
+ - 测试结果 @创建迭代表单页提示信息正确
+ - 最终测试状态 @SUCCESS
+- 计划完成时间为空时的提示校验
+ - 测试结果 @创建迭代表单页提示信息正确
+ - 最终测试状态 @SUCCESS
+- 添加迭代成功最终测试状态 @SUCCESS
+
  */
 
 chdir(__DIR__);
@@ -21,8 +31,8 @@ $sprint = array(
     array('project' => '敏捷项目1', 'name' => '一个敏捷迭代' . time(), 'end' => date('Y-m-d', strtotime('+5 days'))),
 );
 
-r($tester->checkInput($sprint['0'])) && p('message') && e('创建执行成功');  //执行名称为空时的提示校验
-r($tester->checkInput($sprint['1'])) && p('message') && e('创建执行成功');  //计划完成时间为空时的提示校验
-r($tester->checkInput($sprint['2'])) && p('status')  && e('SUCCESS');       //添加迭代成功
+r($tester->checkInput($sprint['0'])) && p('message,status') && e('创建迭代表单页提示信息正确,SUCCESS');  //执行名称为空时的提示校验
+r($tester->checkInput($sprint['1'])) && p('message,status') && e('创建迭代表单页提示信息正确,SUCCESS');  //计划完成时间为空时的提示校验
+r($tester->checkInput($sprint['2'])) && p('status')  && e('SUCCESS');                                    //添加迭代成功
 
 $tester->closeBrowser();
