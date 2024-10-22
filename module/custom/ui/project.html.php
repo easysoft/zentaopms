ad9f98d095a2541fc69fb2e9b4d3aca4d823e73d
liumengyi <liumengyi@easycorp.ltd>
2024-10-22 13:28:53 +0800

    + [task#130244,doing,0.1h] add ui of custom::project.

---
+++ module/custom/ui/project.html.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+/**
+ * The project view file of custom module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2024 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Mengyi Liu
+ * @package     custom
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+include 'sidebar.html.php';
+
+div
+(
+    setClass('flex'),
+    $sidebarMenu,
+);
+
+render();
