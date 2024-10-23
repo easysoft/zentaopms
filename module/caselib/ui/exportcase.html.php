82e717cdf48066633cf1440e6254eb374c9e1d90
tianshujie <tianshujie@easycorp.ltd>
2024-10-23 16:06:30 +0800

    * [refac,0.1h] modify comment.

--- module/caselib/ui/exportcase.html.php
+++ module/caselib/ui/exportcase.html.php
@@ -1,11 +1,11 @@
 <?php
 declare(strict_types=1);
 /**
- * The export view file of testcase module of ZenTaoPMS.
- * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * The exportCase view file of caselib module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2024 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
  * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
- * @author      Tingting Dai <daitingting@easycorp.ltd>
- * @package     testcase
+ * @author      Shujie Tian <tianshujie@easycorp.ltd>
+ * @package     caselib
  * @link        https://www.zentao.net
  */
 
