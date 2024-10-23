204f6168db01c7e65580268381f1d6f6a7a09be3
tianshujie <tianshujie@easycorp.ltd>
2024-10-23 14:00:41 +0800

    * [task#131112,doing,0.2h] add export case page.

---
+++ module/caselib/ui/exportcase.html.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+/**
+ * The export view file of testcase module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Tingting Dai <daitingting@easycorp.ltd>
+ * @package     testcase
+ * @link        https://www.zentao.net
+ */
+
+include '../../file/ui/export.html.php';
