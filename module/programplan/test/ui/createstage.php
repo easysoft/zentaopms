5fc5becc0c9fba49c618919086198a1e532a0b00
huairong <huairong@easycorp.ltd>
2024-10-23 16:21:26 +0800

    + [task#131084] Modify project test data.

--- module/programplan/test/ui/createstage.php
+++ module/programplan/test/ui/createstage.php
@@ -25,8 +25,25 @@ cid=1
 chdir(__DIR__);
 include '../lib/createstage.ui.class.php';
 
-zendata('project')->loadYaml('project', false, 2)->gen(10);
+$project = zenData('project');
+$project->id->range('1');
+$project->project->range('0');
+$project->model->range('waterfall');
+$project->type->range('project');
+$project->auth->range('extend');
+$project->storytype->range('`story,epic,requirement`');
+$project->path->range('`,1,`');
+$project->grade->range('1');
+$project->name->range('瀑布项目1');
+$project->hasProduct->range('1');
+$project->status->range('wait');
+$project->begin->range('(-72w)-(-71w):1D')->type('timestamp')->format('YY/MM/DD');
+$project->end->range('(+72w)-(+73w):1D')->type('timestamp')->format('YY/MM/DD');
+$project->acl->range('open');
+$project->gen(1);
+
 zendata('projectspec')->gen(0);
+
 $tester = new createstageTester();
 $tester->login();
 
@@ -34,14 +51,14 @@ $waterfall = array(
     array('name_0' => '', 'begin_0' => date('Y-m-d'), 'end_0' => date('Y-m-d', strtotime('+30 days'))),
     array('begin_0' => '', 'end_0' => date('Y-m-d', strtotime('+30 days'))),
     array('begin_0' => date('Y-m-d'), 'end_0' => ''),
-    array('begin_0' => '2024-08-02', 'end_0' => '2024-08-01'),
-    array('name_0' => '需求阶段', 'begin_0' => '2024-08-01', 'end_0' => '2024-09-30'),
+    array('begin_0' => '2024-10-24', 'end_0' => '2024-10-23'),
+    array('name_0' => '需求阶段', 'begin_0' => '2024-10-24', 'end_0' => '2024-10-30'),
 );
 
-r($tester->createStage($waterfall['0'])) && p('message,status') && e('创建阶段表单页提示信息正确, SUCCESS');                //校验阶段名称不能为空
-r($tester->createStage($waterfall['1'])) && p('message,status') && e('创建阶段表单页提示信息正确, SUCCESS');                //校验计划开始必填
-r($tester->createStage($waterfall['2'])) && p('message,status') && e('创建阶段表单页提示信息正确, SUCCESS');                //校验计划完成必填
-r($tester->createStage($waterfall['3'])) && p('message,status') && e('创建阶段表单页提示信息正确, SUCCESS');                //校验计划完成必须大于计划开始
-r($tester->createStage($waterfall['4'])) && p('status') && e('SUCCESS');                                                    //创建需求阶段
+r($tester->createStage($waterfall['0'])) && p('message,status') && e('创建阶段表单页提示信息正确,SUCCESS');                //校验阶段名称不能为空
+r($tester->createStage($waterfall['1'])) && p('message,status') && e('创建阶段表单页提示信息正确,SUCCESS');                //校验计划开始必填
+r($tester->createStage($waterfall['2'])) && p('message,status') && e('创建阶段表单页提示信息正确,SUCCESS');                //校验计划完成必填
+r($tester->createStage($waterfall['3'])) && p('message,status') && e('创建阶段表单页提示信息正确,SUCCESS');                //校验计划完成必须大于计划开始
+r($tester->createStage($waterfall['4'])) && p('status') && e('SUCCESS');                                                   //创建需求阶段
 
 $tester->closeBrowser();
