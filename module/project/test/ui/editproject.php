b354cda4e40cd00e42e24ef3de925a7abbae3c59
huairong <huairong@easycorp.ltd>
2024-10-23 16:23:19 +0800

    + [task#131084] Modify project test data.

--- module/project/test/ui/editproject.php
+++ module/project/test/ui/editproject.php
@@ -13,6 +13,23 @@ cid=73
 chdir(__DIR__);
 include '../lib/editproject.ui.class.php';
 
+$project = zenData('project');
+$project->id->range('1');
+$project->project->range('0');
+$project->model->range('waterfall');
+$project->type->range('project');
+$project->auth->range('extend');
+$project->storytype->range('`story,epic,requirement`');
+$project->path->range('`,1,`');
+$project->grade->range('1');
+$project->name->range('瀑布项目1');
+$project->hasProduct->range('1');
+$project->status->range('wait');
+$project->begin->range('(-72w)-(-71w):1D')->type('timestamp')->format('YY/MM/DD');
+$project->end->range('(+72w)-(+73w):1D')->type('timestamp')->format('YY/MM/DD');
+$project->acl->range('open');
+$project->gen(1);
+
 $tester = new editProjectTester();
 $tester->login();
 
