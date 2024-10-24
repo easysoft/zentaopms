ccd93cfdb7c666a0d5dd4c9b5b26cd9dfb0d0b09
yulujie <yulujie@easycorp.ltd>
2024-10-24 09:37:45 +0800

    * [task#131144,doing,0.7h,3h] Add xpath.

---
+++ module/execution/test/ui/page/executionkanban.php
@@ -0,0 +1,15 @@
+<?php
+class executionkanbanPage  extends page
+{
+    public function __construct($webdriver)
+    {
+        parent::__construct($webdriver);
+        $xpath = array(
+            'wait'    => '(//div[@class='kanban-header-title'])[1]/span[2]/div/span',
+            'doing'   => '(//div[@class='kanban-header-title'])[2]/span[2]/div/span',
+            'suspend' => '(//div[@class='kanban-header-title'])[3]/span[2]/div/span',
+            'closed'  => '(//div[@class='kanban-header-title'])[4]/span[2]/div/span'
+        );
+        $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
+    }
+}
