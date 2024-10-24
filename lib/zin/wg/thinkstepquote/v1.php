7878ecb21ad707b5177daf5ffc07233dd821f54f
Zhangyu <zhangyu@easycorp.ltd>
2024-10-24 06:40:55 +0000

    * [task#131064] initial build function of thinkstepquote.

--- lib/zin/wg/thinkstepquote/v1.php
+++ lib/zin/wg/thinkstepquote/v1.php
@@ -16,4 +16,24 @@ class thinkStepQuote extends wg
         'citation?: int=1',        // 引用方式
         'selectColumn?: string',   // 选择列
     );
+
+    public static function getPageJS(): string
+    {
+        return file_get_contents(__DIR__ . DS . 'js' . DS . 'v1.js');
+    }
+    protected function build(): array
+    {
+        global $lang, $app;
+        $app->loadLang('thinkstep');
+        list($step, $setOption, $quoteTitle, $quoteQuestions, $citation, $selectColumn, $questionType) = $this->prop(array('step', 'setOption', 'quoteTitle', 'quoteQuestions', 'citation', 'selectColumn', 'questionType'));
+        if($step)
+        {
+            $enableOther  = $step->options->enableOther ?? 0;
+            $setOption    = isset($step->options->setOption) ? $step->options->setOption : false;
+            $defaultQuote = !empty($quoteQuestions) ? $quoteQuestions[0]->id : null;
+            $quoteTitle   = isset($step->options->quoteTitle) ? $step->options->quoteTitle : $defaultQuote;
+            $citation     = isset($step->options->citation) ? $step->options->citation : 1;
+            $selectColumn = isset($step->options->selectColumn) ? $step->options->selectColumn : null;
+        }
+    }
 }
