80979c1085b523c2d2c779e91bdf9fbd9e330db4
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-25 13:28:46 +0000

    *[task#130733,doing,0.2h] Add ui test files of edit a program

--- module/program/test/ui/editprogram.php
+++ module/program/test/ui/editprogram.php
@@ -17,3 +17,15 @@ include '../lib/editprogram.ui.class.php';
 $program = zenData('project');
 $program->id->range('1-2');
 $program->project->range('0');
+$program->type->range('program');
+$program->name->range('项目集1,项目集2');
+$program->acl->range('open');
+$program->gen(2);
+
+$tester = new createProgramTester();
+$tester->login();
+
+$editName = new stdClass();
+$editName->name = '编辑过的项目集';
+
+r($tester->editProgram($editName)) && p('message,status') && e ('编辑项目集成功，SUCCESS');
