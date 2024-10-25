bd5923d57a3db7b315f86bc1f9f9a4a4a7bbfd99
huairong <huairong@easycorp.ltd>
2024-10-25 14:05:03 +0800

    + [task#131108] Add comment of linkCommit ui test file.

---
+++ module/design/test/ui/linkcommit.php
@@ -0,0 +1,15 @@
+#!/usr/bin/env php
+<?php
+
+/**
+
+title=关联设计检查测试
+timeout=0
+cid=9
+
+- 执行tester模块的linkCommit方法，参数是'3' 测试结果 @关联提交成功
+
+*/
+
+chdir(__DIR__);
+include '../lib/linkcommit.ui.class.php';
