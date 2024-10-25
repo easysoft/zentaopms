05e95ad9eb785a9dd5b644e383a7b8ec4da242ee
huairong <huairong@easycorp.ltd>
2024-10-25 14:06:40 +0800

    + [task#131108] Add test data of project.

--- module/design/test/ui/linkcommit.php
+++ module/design/test/ui/linkcommit.php
@@ -13,3 +13,18 @@ cid=9
 
 chdir(__DIR__);
 include '../lib/linkcommit.ui.class.php';
+
+$project = zendata('project');
+$project->id->range('1');
+$project->project->range('0');
+$project->model->range('waterfall');
+$project->type->range('project');
+$project->parent->range('0');
+$project->auth->range('extend');
+$project->grade->range('1');
+$project->name->range('瀑布项目1');
+$project->path->range('`,1,`');
+$project->begin->range('(-2M)-(-M):1D')->type('timestamp')->format('YY/MM/DD');
+$project->end->range('(+2M)-(+3M):1D')->type('timestamp')->format('YY/MM/DD');
+$project->acl->range('open');
+$project->gen(1);
