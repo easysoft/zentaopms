61ed66f984fe4635037dba13996b4f8b0fd2b846
lihuifang <lihuifang@easycorp.ltd>
2024-10-26 14:42:21 +0800

    +[task#120690,doing,3h,2.8h] Add zendata notes of projectbuild link bug.

---
+++ module/projectbuild/test/ui/buildlinkbug.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php
+
+/**
+
+title=项目版本关联和移除Bug
+timeout=0
+cid=73
+
+- 项目版本关联bug
+ - 测试结果 @版本关联bug成功
+ - 最终测试状态 @SUCCESS
+- 单个移除bug
+ - 测试结果 @单个移除bug成功
+ - 最终测试状态 @SUCCESS
+- 移除全部bug
+ - 测试结果 @移除全部bug成功
+ - 最终测试状态 @SUCCESS
+
+*/
