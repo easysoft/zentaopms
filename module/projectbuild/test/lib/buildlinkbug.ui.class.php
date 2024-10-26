549c9b2cab95d313daf62984334bef70004ce5b9
lihuifang <lihuifang@easycorp.ltd>
2024-10-26 14:48:05 +0800

    +[task#120690,doing,3h,1.5h] Add ui test file of projectbuild link bug.

--- module/projectbuild/test/lib/buildlinkbug.ui.class.php
+++ module/projectbuild/test/lib/buildlinkbug.ui.class.php
@@ -18,3 +18,22 @@ class buildLinkBugTester extends tester
         $form->dom->searchBtn->click();
         $form->wait(2);
         $form->dom->selectAllBug->click(); // 点击全选按钮
+        $form->dom->linkBugBtnBottom->click();
+        // 断言检查版本关联bug数量是否成功
+        $viewPage = $this->initForm('projectbuild', 'view', array('projectID' => 1), 'appIframe-project');
+        $form->dom->resolvedBugTab->click();
+        $form->wait(2);
+        return ($viewPage->dom->resolvedBugNum === '0') ? $this->failed('版本关联bug失败') : $this->success('版本关联bug成功');
+    }
+
+    /**
+     * Unlink a bug of project build.
+     * 移除单个bug
+     *
+     * @access public
+     * @return object
+     */
+    public function unlinkBug()
+    {
+        $form = $this->initForm('projectbuild', 'view', array('buildID' => 1), 'appIframe-project');
+        $form->dom->resolvedBugTab->click();
