aa0e0bbe9187355ab337cf10a190624d4ee9db1e
lihuifang <lihuifang@easycorp.ltd>
2024-10-26 14:44:27 +0800

    *[task#120690,doing,3h,2.6h] Add zendata code of projectbuild link bug.

--- module/projectbuild/test/ui/buildlinkbug.php
+++ module/projectbuild/test/ui/buildlinkbug.php
@@ -18,3 +18,23 @@ cid=73
  - 最终测试状态 @SUCCESS
 
 */
+chdir(__DIR__);
+include '../lib/buildlinkbug.ui.class.php';
+
+$product = zenData('product');
+$product->id->range('1');
+$product->name->range('产品1');
+$product->type->range('normal');
+$product->gen(1);
+
+$project = zenData('project');
+$project->id->range('1');
+$project->project->range('0');
+$project->model->range('scrum');
+$project->type->range('project');
+$project->attribute->range('[]');
+$project->auth->range('[]');
+$project->parent->range('0');
+$project->grade->range('1');
+$project->name->range('敏捷项目1');
+$project->path->range('`,1,`');
