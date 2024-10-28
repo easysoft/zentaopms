2fc43f324484950a8a08c43d86714348a4a5b8a7
yulujie <yulujie@easycorp.ltd>
2024-10-28 09:56:30 +0800

    * [task#131194,doing,1.5h,3h] Add xpath.

---
+++ module/execution/test/ui/page/grouptask.php
@@ -0,0 +1,34 @@
+<?php
+class managemembersPage extends page
+{
+    public function __construct($webdriver)
+    {
+        parent::__construct($webdriver);
+        $xpath = array(
+            /* 分组 */
+            'dropdownBtn' => "//*[@id='tasksTable']/table/thead/tr/th[1]/button",
+            'story'       => "//*[@data-id='execution-grouptask']/../div[3]//li[1]",
+            'status'      => "//*[@data-id='execution-grouptask']/../div[3]//li[2]",
+            'priority'    => "//*[@data-id='execution-grouptask']/../div[3]//li[3]",
+            'assignedTo'  => "//*[@data-id='execution-grouptask']/../div[3]//li[4]",
+            'finishedBy'  => "//*[@data-id='execution-grouptask']/../div[3]//li[5]",
+            'closedBy'    => "//*[@data-id='execution-grouptask']/../div[3]//li[6]",
+            'type'        => "//*[@data-id='execution-grouptask']/../div[3]//li[7]"
+            /* 全部展开时左侧区块统计 */
+            'tasks'     => "//div[@id='tasksTable']//tbody/tr[1]/td/div/div/div/strong",
+            'waiting'   => "//*[@id='tasksTable']/table/tbody/tr[1]/td[1]/div/div/div[1]/text()[2]",
+            'doing'     => "//*[@id='tasksTable']/table/tbody/tr[1]/td[1]/div/div/div[1]/text()[3]",
+            'estimates' => "//*[@id='tasksTable']/table/tbody/tr[1]/td[1]/div/div/div[2]/strong",
+            'cost'      => "//*[@id='tasksTable']/table/tbody/tr[1]/td[1]/div/div/div[1]/text()[2]",
+            'left'      => "//*[@id='tasksTable']/table/tbody/tr[1]/td[1]/div/div/div[2]/text()[3]"
+            /* 全部收起时右侧区块统计 */
+            'rtasks'     => "//tr[@class='group-summary group-toggle']/td[2]/div[1]/div[1]/div[1]/div[1]/div[2]",
+            'rdoing'     => "//tr[@class='group-summary group-toggle']/td[2]/div[1]/div[1]/div[1]/div[2]/div[2]/span",
+            'rwaiting'   => "//tr[@class='group-summary group-toggle']/td[2]/div[1]/div[1]/div[1]/div[3]/div[2]/span",
+            'restimates' => "//tr[@class='group-summary group-toggle']/td[2]/div[1]/div[2]/div[1]/div[1]/div[2]",
+            'rcost'      => "//tr[@class='group-summary group-toggle']/td[2]/div[1]/div[2]/div[1]/div[2]/div[2]",
+            'rleft'      => "//tr[@class='group-summary group-toggle']/td[2]/div[1]/div[2]/div[1]/div[3]/div[2]"
+        );
+        $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
+    }
+}
