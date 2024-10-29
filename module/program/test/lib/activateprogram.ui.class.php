82f1da703bbfd5ee7932b0635476bc10626c2dd6
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-29 09:11:26 +0000

    *[task#130730,doing,0.3h] Add ui test files of activate a program

--- module/program/test/lib/activateprogram.ui.class.php
+++ module/program/test/lib/activateprogram.ui.class.php
@@ -16,3 +16,19 @@ class createProgramTester extends tester
         $browsePage = $this->loadPage('program', 'browse');
         $browsePage->dom->fstStartBtn->click();
         $browsePage->wait(1);
+        $browsePage->dom->startProgramBtn->click();
+        $browsePage->wait(1);
+        $browsePage->dom->fstStartBtn->click();
+        $browsePage->wait(1);
+        $browsePage->dom->startProgramBtn->click();
+        $browsePage->dom->search(array("项目集名称,=,{$programName->name}"));
+        $browsePage->wait(1);
+        $browsePage->dom->fstStartBtn->click();
+        $browsePage->wait(1);
+        $browsePage->dom->startProgramBtn->click();
+        $browsePage->wait(1);
+
+        if($browsePage->dom->fstStatus->getText() != '进行中') return $this->failed('激活项目集失败');
+        return $this->success('激活项目集成功');
+    }
+}
