a79535130332b9b45f6926ed9ec4b6604f06090c
huairong <huairong@easycorp.ltd>
2024-10-29 14:00:49 +0800

    + [task#131110] Add test data of product .

--- module/project/test/ui/bug.php
+++ module/project/test/ui/bug.php
@@ -19,3 +19,14 @@ cid=1
 - 执行tester模块的switchProduct方法，参数是'secondProduct', '5'
  - 最终测试状态 @SUCCESS
  - 测试结果 @切换secondProduct查看数据成功
+
+ */
+
+chdir(__DIR__);
+include '../lib/bug.ui.class.php';
+
+$product = zenData('product');
+$product->id->range('1-2');
+$product->name->range('产品1, 产品2');
+$product->type->range('normal');
+$product->gen(2);
