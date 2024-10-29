32803ba0e4edeea29612081e11ad2deb6b412ae5
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-29 09:13:41 +0000

    *[task#130730,doing,0.2h] Add ui test files of activate a program

--- module/program/test/ui/activateprogram.php
+++ module/program/test/ui/activateprogram.php
@@ -17,3 +17,16 @@ include '../lib/activateprogram.ui.class.php';
 
 $program = zenData('project');
 $program->id->range('1-2');
+$program->project->range('0');
+$program->type->range('program');
+$program->name->range('项目集1,项目集2');
+$program->acl->range('open');
+$program->gen(2);
+
+$tester = new createProgramTester();
+$tester->login();
+
+$programName = new stdClass();
+$programName->name = '项目集1';
+
+r($tester->activateProgram($programName)) && p('message,status') && e ('激活项目集成功，SUCCESS'); //激活项目集成功
