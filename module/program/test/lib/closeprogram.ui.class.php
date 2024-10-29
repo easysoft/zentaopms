25196f0fe53735a6652d73be47cb1203181a4fbc
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-29 09:08:29 +0000

    *[task#130731,doing,0.2h] Add ui test files of close a program

--- module/program/test/lib/closeprogram.ui.class.php
+++ module/program/test/lib/closeprogram.ui.class.php
@@ -16,3 +16,16 @@ class createProgramTester extends tester
         $browsePage = $this->loadpage('program', 'browse');
         $browsePage->dom->fstStartBtn->click();
         $browsePage->wait(1);
+        $browsePage->dom->startProgramBtn->click();
+        $browsePage->wait(1);
+        $browsePage->dom->fstStartBtn->click();
+        $browsePage->wait(2);
+        $browsePage->dom->startProgramBtn->click();
+        $browsePage->wait(1);
+        $browsePage->dom->search(array("项目集名称,=,{$programName->name}"));
+        $browsePage->wait(1);
+
+        if($browsePage->dom->fstStatus->getText() != '已关闭') return $this->failed('关闭项目集失败');
+        return $this->success('关闭项目集成功');
+    }
+}
