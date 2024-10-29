8404166f6dafd5330bf87d04853b20ab794a993e
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-29 08:55:59 +0000

    *[task#130731,doing,0.2h] Add ui test files of close a program

--- module/program/test/ui/closeprogram.php
+++ module/program/test/ui/closeprogram.php
@@ -17,3 +17,16 @@ include '../lib/closeprogram.ui.class.php';
 
 $program = zenData('project');
 $program->id->range('1-2');
+$program->project->range('0');
+$program->type->range('program');
+$program->name->range('项目集1,项目集2');
+$program->acl->range('open');
+$program->gen(2);
+
+$tester = new createProgramTester();
+$tester->login();
+
+$programName = new stdClass();
+$programName->name = '项目集1';
+
+r($tester->closeProgram($programName)) && p('message,status') && e ('关闭项目集成功，SUCCESS'); //关闭项目集成功
