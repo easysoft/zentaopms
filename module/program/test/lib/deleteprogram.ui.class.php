5ba80e205212896a3f57efdccac547faadfb0ff3
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-29 13:07:59 +0000

    *[task#131359,doing,0.2h] Add ui test files of delete a program

--- module/program/test/lib/deleteprogram.ui.class.php
+++ module/program/test/lib/deleteprogram.ui.class.php
@@ -16,3 +16,14 @@ class createProgramTester extends tester
         $this->openUrl('program', 'browse');
         $browsePage = $this->loadPage('program', 'browse');
         $browsePage->dom->fstdeleteBtn->click();
+        $browsePage->dom->undeleteConfirm->click();
+        $browsePage->dom->fstdeleteBtn->click();
+        $browsePage->dom->deleteConfirm->click();
+        $browsePage->wait(1);
+        $browsePage->dom->search(array("项目集名称,=,{$programName->name}"));
+        $browsePage->wait(1);
+
+        if($browsePage->dom->formText->getText() != '暂时没有项目集') return $this->failed('删除项目集失败');
+        return $this->success('删除项目集成功');
+    }
+}
