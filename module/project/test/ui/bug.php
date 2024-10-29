e8101f0b472ed62fd5b5fb7bfc569ca94df8cce1
huairong <huairong@easycorp.ltd>
2024-10-29 14:03:01 +0800

    + [task#131110] Add test data of project .

--- module/project/test/ui/bug.php
+++ module/project/test/ui/bug.php
@@ -30,3 +30,18 @@ $product->id->range('1-2');
 $product->name->range('产品1, 产品2');
 $product->type->range('normal');
 $product->gen(2);
+
+$project = zenData('project');
+$project->id->range('1');
+$project->project->range('0');
+$project->model->range('scrum');
+$project->type->range('project');
+$project->auth->range('extend');
+$project->storytype->range('`story,epic,requirement`');
+$project->path->range('`,1,`');
+$project->grade->range('1');
+$project->name->range('敏捷项目1');
+$project->hasProduct->range('1');
+$project->status->range('wait');
+$project->acl->range('open');
+$project->gen(1);
