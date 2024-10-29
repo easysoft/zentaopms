a8c55c3085dd8d80642d73cfb5c769dce7cde58c
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-29 08:54:44 +0000

    *[task#130688,done,0.2h] Add ui test files of start a program

--- module/program/test/ui/startprogram.php
+++ module/program/test/ui/startprogram.php
@@ -17,3 +17,15 @@ include '../lib/startprogram.ui.class.php';
 $program = zenData('project');
 $program->id->range('1-2');
 $program->project->range('0');
+$program->type->range('program');
+$program->name->range('项目集1,项目集2');
+$program->acl->range('open');
+$program->gen(2);
+
+$tester = new createProgramTester();
+$tester->login();
+
+$programName = new stdClass();
+$programName->name = '项目集1';
+
+r($tester->startProgram($programName)) && p('message,status') && e ('开始项目集成功，SUCCESS'); //开始项目集成功
