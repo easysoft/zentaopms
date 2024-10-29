21aba7280bc7561ba2931be252dfd30a516bd8c4
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-29 13:49:07 +0000

    *[task#131359,done,0.1h] Add ui test files of fix error code

--- module/program/test/ui/page/edit.php
+++ module/program/test/ui/page/edit.php
@@ -1,11 +1,4 @@
 <?php
 class editPage extends page
 {
-    public function __construct($webdriver)
-    {
-        parent::__construct($webdriver);
-        $xpath = array(
-        );
-        $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
-    }
 }
