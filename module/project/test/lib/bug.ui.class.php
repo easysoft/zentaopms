c502da72d4a6291c34daca22ce439795fb222315
huairong <huairong@easycorp.ltd>
2024-10-29 13:50:43 +0800

    + [task#131110] Add comment of checkTab function.

--- module/project/test/lib/bug.ui.class.php
+++ module/project/test/lib/bug.ui.class.php
@@ -2,6 +2,15 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class bugTester extends tester
 {
+    /**
+     * 检查Tab标签下的数据。
+     * Check the data of Tab tag.
+     *
+     * @param  string $tab allTab|unresolvedTab
+     * @param  string $expectNum
+     * @access public
+     * @return object
+     */
     public function checkTab($tab, $expectNum)
     {
         $form = $this->initForm('project', 'bug', array('project' => 1), 'appIframe-project');
