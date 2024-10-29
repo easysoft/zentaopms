2abaec54996b00d9dad777d38f58ff50a90cea9d
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-29 08:53:07 +0000

    *[task#130688,doing,0.3h] Add ui test files of start a program

--- module/program/test/lib/startprogram.ui.class.php
+++ module/program/test/lib/startprogram.ui.class.php
@@ -14,16 +14,6 @@ class createProgramTester extends tester
     {
         $this->openUrl('program', 'browse');
         $browsePage = $this->loadPage('program', 'browse');
-        $browsePage->dom->fstStartBtn->click();
-        $browsePage->wait(1);
-        $browsePage->dom->startProgramBtn->click();
-        $browsePage->wait(1);
-
-        $this->openUrl('program', 'browse');
-        $browsePage = $this->loadPage('program', 'browse');
-        $browsePage->dom->search(array("项目集名称,=,{$programName->name}"));
-        $browsePage->wait(1);
-        if($browsePage->dom->fstStatus->getText() != '进行中') return $thia->failed('开始项目集失败');
         return $this->success('开始项目集成功');
     }
 }
