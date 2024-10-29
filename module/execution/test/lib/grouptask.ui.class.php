8a5ac57aaeca12932a1e264d55442532b39d4e4a
yulujie <yulujie@easycorp.ltd>
2024-10-29 11:02:27 +0800

    * [task#131194,doing,3h,2h] Add function.

--- module/execution/test/lib/grouptask.ui.class.php
+++ module/execution/test/lib/grouptask.ui.class.php
@@ -6,25 +6,21 @@ class grouptaskTester extends tester
      * 检查全部展开时分组下数据。
      * Check the data of the group.、
      *
-     * @param  string $groupData story|status|priority|assignedTo|finishedBy|closedBy|type
+     * @param  string $groupData story|status|pri|assignedTo|finishedBy|closedBy|type
      * @param  array  $nums
      * @access public
      * @return void
      */
     public function checkGroupData($groupData, $nums)
     {
-        $form = $this->initForm('execution', 'grouptask', array('execution' => '2' ), 'appIframe-execution');
-        $form->dom->dropdownBtn->click();
-        $form->wait(1);
-        $form->dom->$groupData->click();
-        $form->wait(1);
-        if($form->dom->group->getText() != $this->lang->task->noStory) return $this->falied('研发需求分类错误');
-        if($form->dom->tasks->getText() != $nums['tasks'])             return $this->falied('总任务数目错误');
-        if($form->dom->waiting->getText() != $nums['waiting'])         return $this->falied('未开始任务数目错误');
-        if($form->dom->doing->getText() != $nums['doing'])             return $this->falied('进行中任务数目错误');
-        if($form->dom->estimates->getText() != $nums['estimates'])     return $this->falied('总预计工时错误');
-        if($form->dom->cost->getText() != $nums['cost'])               return $this->falied('已消耗工时错误');
-        if($form->dom->left->getText() != $nums['left'])               return $this->falied('剩余工时错误');
+        $form = $this->initForm('execution', 'grouptask', array('execution' => '2', 'groupBy' => $groupData ), 'appIframe-execution');
+        if($form->dom->group->getText() != $this->lang->task->noStory)                                     return $this->failed('研发需求分类错误');
+        if(filter_var($form->dom->tasks->getText(), FILTER_SANITIZE_NUMBER_INT) != $nums['tasks'])         return $this->failed('总任务数目错误');
+        if(filter_var($form->dom->waiting->getText(), FILTER_SANITIZE_NUMBER_INT) != $nums['waiting'])     return $this->failed('未开始任务数目错误');
+        if(filter_var($form->dom->doing->getText(), FILTER_SANITIZE_NUMBER_INT) != $nums['doing'])         return $this->failed('进行中任务数目错误');
+        if(filter_var($form->dom->estimates->getText(), FILTER_SANITIZE_NUMBER_INT) != $nums['estimates']) return $this->failed('总预计工时错误');
+        if(filter_var($form->dom->cost->getText(), FILTER_SANITIZE_NUMBER_INT) != $nums['cost'])           return $this->failed('已消耗工时错误');
+        if(filter_var($form->dom->left->getText(), FILTER_SANITIZE_NUMBER_INT) != $nums['left'])           return $this->failed('剩余工时错误');
         $this->success('数据正确');
     }
 
@@ -38,16 +34,40 @@ class grouptaskTester extends tester
      */
     public function checkTaskLinkedStory($nums)
     {
-        $form = $this->initForm('execution', 'grouptask', array('execution' => '2' ), 'appIframe-execution');
+        $form = $this->initForm('execution', 'grouptask', array('execution' => '2', 'groupBy' => 'story' ), 'appIframe-execution');
         $form->dom->taskLinkedStoryBtn->click();
         $form->wait(1);
-        if($form->dom->group->getText() != $this->lang->task->noStory) return $this->falied('研发需求分类错误');
-        if($form->dom->tasks->getText() != $nums['tasks'])             return $this->falied('总任务数目错误');
-        if($form->dom->waiting->getText() != $nums['waiting'])         return $this->falied('未开始任务数目错误');
-        if($form->dom->doing->getText() != $nums['doing'])             return $this->falied('进行中任务数目错误');
-        if($form->dom->estimates->getText() != $nums['estimates'])     return $this->falied('总预计工时错误');
-        if($form->dom->cost->getText() != $nums['cost'])               return $this->falied('已消耗工时错误');
-        if($form->dom->left->getText() != $nums['left'])               return $this->falied('剩余工时错误');
+        if(filter_var($form->dom->group->getText(), FILTER_SANITIZE_NUMBER_INT) != $this->lang->task->noStory) return $this->failed('研发需求分类错误');
+        if(filter_var($form->dom->tasks->getText(), FILTER_SANITIZE_NUMBER_INT) != $nums['tasks'])             return $this->failed('总任务数目错误');
+        if(filter_var($form->dom->waiting->getText(), FILTER_SANITIZE_NUMBER_INT) != $nums['waiting'])         return $this->failed('未开始任务数目错误');
+        if(filter_var($form->dom->doing->getText(), FILTER_SANITIZE_NUMBER_INT) != $nums['doing'])             return $this->failed('进行中任务数目错误');
+        if(filter_var($form->dom->estimates->getText(), FILTER_SANITIZE_NUMBER_INT) != $nums['estimates'])     return $this->failed('总预计工时错误');
+        if(filter_var($form->dom->cost->getText(), FILTER_SANITIZE_NUMBER_INT) != $nums['cost'])               return $this->failed('已消耗工时错误');
+        if(filter_var($form->dom->left->getText(), FILTER_SANITIZE_NUMBER_INT) != $nums['left'])               return $this->failed('剩余工时错误');
+        $this->success('数据正确');
+    }
+
+    /**
+     * 检查折叠后的数据统计。
+     * Check the data of the group.
+     *
+     * @param  array  $nums
+     * @access public
+     * @return void
+     */
+    public function checkCollapse($nums)
+    {
+        $form = $this->initForm('execution', 'grouptask', array('execution' => '2', 'groupBy' => 'story' ), 'appIframe-execution');
+        $form->dom->collapseBtn->click();
+        $form->wait(1);
+        if($form->dom->rtasks->getText() != $nums['tasks'])             return $this->failed('总任务数目错误');
+        var_dump($form->dom->rdoing->getText());
+        var_dump($nums['doing']);
+        if($form->dom->rdoing->getText() != $nums['doing'])             return $this->failed('进行中任务数目错误');
+        if($form->dom->rwaiting->getText() != $nums['waiting'])         return $this->failed('未开始任务数目错误');
+        if($form->dom->restimates->getText() != $nums['estimates'])     return $this->failed('总预计工时错误');
+        if($form->dom->rcost->getText() != $nums['cost'])               return $this->failed('已消耗工时错误');
+        if($form->dom->rleft->getText() != $nums['left'])               return $this->failed('剩余工时错误');
         $this->success('数据正确');
     }
 }
