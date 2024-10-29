1ecb7f8f4677c69e2754ce814f7cf107ea27c68e
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-10-29 13:27:41 +0000

    *[task#131359,done,0.1h] Add ui test files of fix error code

--- module/program/test/ui/createprogram.php
+++ module/program/test/ui/createprogram.php
@@ -18,6 +18,9 @@ timeout=0
 - 创建一个私有项目集，创建成功
  - 测试结果 @创建私有项目集成功
  - 最终测试状态 @SUCCESS
+- 添加一个子项目集，添加成功
+ - 测试结果 @添加子项目集成功
+ - 最终测试状态 @SUCCESS
 
 */
 chdir(__DIR__);
@@ -44,9 +47,4 @@ r($tester->createDefault($programs['repeat']))              && p('message,status
 r($tester->createDefault($programs['open']))                && p('message,status') && e('创建项目集成功,SUCCESS');               // 创建一个公开项目集，创建成功
 r($tester->createPrivate($programs['private'], $whitelist)) && p('message,status') && e('创建私有项目集成功,SUCCESS');           // 创建一个私有项目集，创建成功
 
-#r($tester->editProgram($programs['null']))   && p('message,status') && e('编辑项目集表单提示信息正确，SUCCESS');  //项目集名称为空，>编辑失败
-#r($tester->editProgram($programs['edited'])) && p('message,status') && e('编辑项目集成功，SUCCESS');     //编辑项目集，编辑成功
-#r($tester->closeProgram())                   && p('message,status') && e('关闭项目集成功，SUCCESS');     //关闭项目集，关闭成功
-#r($tester->activateProgram())                && p('message,status') && e('激活项目集成功，SUCCESS');     //激活项目集，激活成功
 r($tester->addChildProgram($programs['open'], $childProgram))                && p('message,status') && e('添加子项目集成功，SUCCESS');   //添加子项目集，添加成功
-#r($tester->deleteProgram())                  && p('message,status') && e('删除项目集校验成功，SUCCESS'); //删除项目集，删除成功
