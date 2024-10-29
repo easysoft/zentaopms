bce212f50c5beb6f20acb87288e8849051a90c6f
huairong <huairong@easycorp.ltd>
2024-10-29 13:52:10 +0800

    + [task#131110] Add switchProduct function.

--- module/project/test/lib/bug.ui.class.php
+++ module/project/test/lib/bug.ui.class.php
@@ -19,4 +19,13 @@ class bugTester extends tester
         if($form->dom->bugNum->getText() == $expectNum) return $this->success($tab . '下显示条数正确');
         return $this->failed($tab . '下显示条数不正确');
     }
+    public function switchProduct($product, $expectNum)
+    {
+        $form = $this->initForm('project', 'bug', array('project' => 1), 'appIframe-project');
+        $form->dom->dropMenu->click();
+        $form->dom->$product->click();
+        $form->wait(1);
+        if($form->dom->bugNum->getText() == $expectNum) return $this->success('切换' . $product . '查看数据成功');
+        return $this->failed('切换' . $product . '查看数据失败');
+    }
 }
