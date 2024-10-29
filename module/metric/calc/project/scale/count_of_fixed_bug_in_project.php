97697919c456bf7426563d423fb322ff870d3939
songchenxuan <songchenxuan@easycorp.ltd>
2024-10-29 16:56:58 +0800

    *[misc] add count_of_fixed_bug_in_project file.

---
+++ module/metric/calc/project/scale/count_of_fixed_bug_in_project.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 按项目统计的已修复Bug数。
+ * Count of fixed bug in project.
+ *
+ * 范围：project
+ * 对象：bug
+ * 目的：scale
+ * 度量名称：按项目统计的已修复Bug数
+ * 单位：个
+ * 描述：按项目统计的已修复Bug数是指解决方案为已解决并且状态为已关闭的Bug数量。这个度量项反映了项目解决的问题数量。已修复Bug数的可以评估开发团队在Bug解决方面的工作效率。
+ * 定义：项目中Bug的个数求和;解决方案为已解决;状态为已关闭;过滤已删除的Bug;过滤已删除的项目;
+ *
+ * @copyright Copyright 2009-2024 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @author    songchenxuan <songchenxuan@easycorp.ltd>
+ * @package
+ * @uses      func
+ * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @Link      https://www.zentao.net
+ */
