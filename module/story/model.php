3941570add6f8401c8fe204ae86136677f55fdd7
liumengyi <liumengyi@easycorp.ltd>
2024-10-29 15:11:05 +0800

    * [task#130461,start,1h] move js.

--- module/story/model.php
+++ module/story/model.php
@@ -571,7 +571,8 @@ class storyModel extends model
         $this->loadModel('score')->create('story', 'create',$storyID);
 
         /* Create actions. Record submit review action. */
-        $action = $bugID == 0 ? (empty($storyFrom) ? 'Opened' : 'From' . ucfirst($storyFrom)) : 'Frombug';
+        $bugAction = empty($storyFrom) ? 'Opened' : 'From' . ucfirst($storyFrom);
+        $action    = $bugID == 0 ? $bugAction : 'Frombug';
         $this->action->create('story', $storyID, $action, '', $extra);
         if($story->status == 'reviewing') $this->action->create('story', $storyID, 'submitReview');
         if(!empty($story->assignedTo)) $this->action->create('story', $storyID, 'Assigned', '', $story->assignedTo);
