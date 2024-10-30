8bd733586561d07e7b417eaf669e39db22d88361
songchenxuan <songchenxuan@easycorp.ltd>
2024-10-30 15:39:03 +0800

    +[misc] add rate_of_fixed_bug_in_project file.

---
+++ module/metric/calc/project/rate/rate_of_fixed_bug_in_project.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 按项目统计的Bug修复率。
+ * Rate of fixed bug in project.
+ *
+ * 范围：project
+ * 对象：bug
+ * 目的：rate
+ * 度量名称：按项目统计的Bug修复率
+ * 单位：%
+ * 描述：按项目统计的Bug修复率是指按项目统计的修复Bug数相对于按项目统计的有效Bug数的比例。该度量项可以帮助我们了解开发团队对Bug修复的效率和质量，高的修复率可能说明Bug得到及时解决，项目质量得到有效保障。
+ * 定义：复用： 按项目统计的修复Bug数、按项目统计的有效Bug数。公式： 按项目统计的Bug修复率=按项目统计的修复Bug数/按项目统计的有效Bug数。
+ *
+ * @copyright Copyright 2009-2024 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @author    songchenxuan <songchenxuan@easycorp.ltd>
+ * @package
+ * @uses      func
+ * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @Link      https://www.zentao.net
+ */
