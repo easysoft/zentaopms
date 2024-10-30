cb6724f9415c2f307d9dde6320a1116d07b9fcd3
songchenxuan <songchenxuan@easycorp.ltd>
2024-10-30 16:40:05 +0800

    *[misc] objectID default is 0.

--- module/doc/control.php
+++ module/doc/control.php
@@ -61,8 +61,7 @@ class doc extends control
      */
     public function mySpace(int $objectID = 0, int $libID = 0, int $moduleID = 0, string $browseType = 'all', int $param = 0, string $orderBy = 'order_asc', int $recTotal = 0, int $recPerPage = 20, int $pageID = 1, string $mode = '', int $docID = 0, string $search = '')
     {
-        if(empty($objectID)) $this->docZen->initLibForMySpace();
-
+        $this->docZen->initLibForMySpace();
         echo $this->fetch('doc', 'app', "type=mine&spaceID=$objectID&libID=$libID&moduleID=$moduleID&docID=$docID&mode=$mode&orderBy=$orderBy&recTotal=$recTotal&recPerPage=$recPerPage&pageID=$pageID&filterType=$browseType&search=$search");
     }
 
@@ -930,13 +929,7 @@ class doc extends control
      */
     public function teamSpace(int $objectID = 0, int $libID = 0, int $moduleID = 0, string $browseType = 'all', string $orderBy = '', int $param = 0, int $recTotal = 0, int $recPerPage = 20, int $pageID = 1, string $mode = '', int $docID = 0, string $search = '')
     {
-        if(empty($objectID))
-        {
-            $this->docZen->initLibForTeamSpace();
-            $spaces = $this->doc->getSubSpacesByType('custom');
-            $objectID = key($spaces);
-        }
-
+        $this->docZen->initLibForTeamSpace();
         echo $this->fetch('doc', 'app', "type=custom&spaceID=$objectID&libID=$libID&moduleID=$moduleID&docID=$docID&mode=$mode&orderBy=$orderBy&recTotal=$recTotal&recPerPage=$recPerPage&pageID=$pageID&filterType=$browseType&search=$search");
     }
 
