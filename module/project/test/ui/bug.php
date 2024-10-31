fd0139592237cc56f3e01608ff173589ad3ecedd
huairong <huairong@easycorp.ltd>
2024-10-31 09:39:52 +0800

    + [task#131109] Add testcase of assignBug.

--- module/project/test/ui/bug.php
+++ module/project/test/ui/bug.php
@@ -19,6 +19,9 @@ cid=1
 - 执行tester模块的switchProduct方法，参数是'secondProduct', '5'
  - 最终测试状态 @SUCCESS
  - 测试结果 @切换secondProduct查看数据成功
+- 执行tester模块的assignBug方法，参数是'admin'
+ - 最终测试状态 @SUCCESS
+ - 测试结果 @单个指派Bug成功
 
  */
 
@@ -78,5 +81,7 @@ r($tester->checkTab('unresolvedTab', '4')) && p('status,message') && e('SUCCESS,
 /* 切换1.5级导航产品 */
 r($tester->switchProduct('firstProduct', '5')) && p('status,message') && e('SUCCESS,切换firstProduct查看数据成功');
 r($tester->switchProduct('secondProduct', '5')) && p('status,message') && e('SUCCESS,切换secondProduct查看数据成功');
+/* 单个指派Bug */
+r($tester->assignBug('admin')) && p('status,message') && e('SUCCESS,单个指派Bug成功');
 
 $tester->closeBrowser();
