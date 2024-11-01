6e96f6b9b0eedbcafc050833f0d31ea3e3820512
lijie <lijie@easycorp.ltd>
2024-11-01 09:52:23 +0800

    [task#131146, 0.5h] modify zendata date.

--- module/story/test/ui/assigntostory.php
+++ module/story/test/ui/assigntostory.php
@@ -23,29 +23,49 @@ $product->vision->range('rnd');
 $product->gen(1);
 
 $story = zenData('story');
-$story->id->range('1');
-$story->root->range('1');
-$story->path->range('`,1,`');
+$story->id->range('1-3');
+$story->root->range('1-3');
+$story->path->range('`,1,`, `,2,`, `,3,`');
 $story->grade->range('1');
 $story->product->range('1');
 $story->module->range('0');
-$story->title->range('研发需求');
-$story->type->range('story');
-$story->stage->range('developing');
+$story->title->range('激活研发需求,激活用户需求,激活业务需求');
+$story->type->range('story,requirement,epic');
+$story->stage->range('wait');
 $story->status->range('active');
 $story->openedBy->range('admin');
 $story->version->range('1');
 $story->assignedTo->range('[]');
-$story->gen(1);
+$story->reviewedBy->range('[]');
+$story->reviewedDate->range('`NULL`');
+$story->closedBy->range('[]');
+$story->closedReason->range('[]');
+$story->gen(3);
 
 $storyspec = zenData('storyspec');
-$storyspec->story->range('1');
+$storyspec->story->range('1-3');
 $storyspec->version->range('1');
-$storyspec->title->range('研发需求');
-$storyspec->gen(1);
+$storyspec->title->range('激活研发需求,激活用户需求,激活业务需求');
+$storyspec->gen(3);
+
+$storyreview = zenData('storyreview');
+$storyreview->gen(0);
+
+$action = zenData('action');
+$action->id->range('1-4');
+$action->objectType->range('product,story,story,story');
+$action->objectID->range('1,1,2,3');
+$action->product->range('`,1,`');
+$action->project->range('0');
+$action->execution->range('0');
+$action->actor->range('admin');
+$action->action->range('opened');
+$action->read->range('0');
+$action->vision->range('rnd');
+$action->gen(4);
 
 $tester = new assignToStoryTester();
 $tester->login();
 
-r($tester->assignToStory()) && p('message') && e('指派需求成功');
+r($tester->assignToStory()) && p('message') && e('指派研发需求成功');
 $tester->closeBrowser();
