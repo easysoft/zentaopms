1476d57429ecde07c0027e5358545ee4e9f08596
songchenxuan <songchenxuan@easycorp.ltd>
2024-11-04 17:07:16 +0800

    [misc] add count_of_monthly_fixed_bug_in_project class.

---
+++ module/metric/calc/project/scale/count_of_monthly_fixed_bug_in_project.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 按项目统计的月度解决Bug数。
+ * Count of monthly fixed bug in project.
+ *
+ * 范围：project
+ * 对象：bug
+ * 目的：scale
+ * 度量名称：按项目统计的月度解决Bug数
+ * 单位：个
+ * 描述：按产品统计的月度解决Bug数是指每天在产品开发过程中被解决并关闭的Bug的数量。该度量项可以帮助我们了解开发团队解决Bug的速度和效率。
+ * 定义：产品中创建时间在某年某月的Bug个数求和;过滤已删除的Bug;过滤已删除的产品;
+ *
+ * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @author    qixinzhi <qixinzhi@easycorp.ltd>
+ * @package
+ * @uses      func
+ * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @Link      https://www.zentao.net
+ */
