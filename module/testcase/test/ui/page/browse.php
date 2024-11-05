6a4626c7e526adf2c978aac67a62bcebb162be27
luxuyang <luxuyang@easycorp.ltd>
2024-11-05 17:40:58 +0800

    * [task#132044,doing,1h,2h] add review testcase ui script.

--- module/testcase/test/ui/page/browse.php
+++ module/testcase/test/ui/page/browse.php
@@ -8,6 +8,8 @@ class browsePage extends page
             'caseListID'   => "//a[text()='ID']",
             'caseName'     => "(//div[@data-col='title']//a[@data-app='qa'])[1]",
             'caseNameView' => "(//div[@id='mainContent']//span[text()])[3]",
+            'review'       => "(//div[@data-row='case_1']//a[@data-toggle='modal'])[1]",
+            'comment'      => "//zen-editor[@menubar-mode='compact']"
         );
 
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
