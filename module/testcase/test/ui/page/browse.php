de9b2dc39cbac7bb9160f05e0fba4efcfe8c2246
luxuyang <luxuyang@easycorp.ltd>
2024-11-05 09:56:41 +0800

    * [task132043,done,1h,0h] add testcase view ui script.

--- module/testcase/test/ui/page/browse.php
+++ module/testcase/test/ui/page/browse.php
@@ -5,7 +5,9 @@ class browsePage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'caseListID' => "//a[text()='ID']"
+            'caseListID'   => "//a[text()='ID']",
+            'caseName'     => "(//div[@data-col='title']//a[@data-app='qa'])[1]",
+            'caseNameView' => "(//div[@id='mainContent']//span[text()])[3]",
         );
 
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
