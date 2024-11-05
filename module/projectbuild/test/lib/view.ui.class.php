1e69c82bbea51157c2fd1c0767649794198e5f69
lihuifang <lihuifang@easycorp.ltd>
2024-11-05 15:47:47 +0800

    *[task#131957,done,3h,0h] Add ui function file of projectbuild view.

--- module/projectbuild/test/lib/view.ui.class.php
+++ module/projectbuild/test/lib/view.ui.class.php
@@ -17,3 +17,18 @@ class buildViewTester extends tester
          $browsePage->dom->buildNameBrowse->click();
 
          //在详情页面，获取项目版本字段信息
+         $viewPage = $this->loadPage('projectbuild', 'view');
+         $viewPage->wait(2);
+         $viewPage->dom->basic->click();
+         $viewPage->wait(2);
+         $basicbuildName = $viewPage->dom->basicBuildName->getText();
+         $basicProduct   = $viewPage->dom->basicProduct->getText();
+         $basicExecution = $viewPage->dom->basicExecution->getText();
+
+         //断言检查版本详情页显示是否正确
+         if($buildNameBrowse != $basicbuildName) return $this->failed('项目版本详情页名称显示不正确');
+         if($productBrowse != $basicProduct)     return $this->failed('项目版本详情页所属产品显示不正确');
+         if($executionBrowse != $basicExecution) return $this->failed('项目版本详情页所属执行显示不正确');
+         return $this->success('项目版本详情查看成功');
+     }
+}
