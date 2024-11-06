c9372babf88da0aa299ead58a570ca1deb4d6f88
yulujie <yulujie@easycorp.ltd>
2024-11-06 13:35:02 +0800

    * [task#131847,doing] Add function.

--- module/execution/test/lib/tree.ui.class.php
+++ module/execution/test/lib/tree.ui.class.php
@@ -3,6 +3,25 @@ include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class treeTester extends tester
 {
     /**
+     * 检查树状图数据。
+     * Check tree data.
+     *
+     * @param  string $level
+     * @param  string $num
+     * @param  bool   $click
+     * @access public
+     * @return object
+     */
+    public function checkTreeData($level, $num, $click = false)
+    {
+        $form = $this->initForm('execution', 'tree', array('execution' => '2'), 'appIframe-execution');
+        if($click) $form->dom->onlyStoryBtn->click();
+        $form->wait(1);
+        if(count($form->dom->getElementList($form->dom->xpath[$level])->element) != $num) return $this->failed('数据错误');
+        return $this->success('数据正确');
+    }
+
+    /**
      * 检查树状图中的任务、需求详情显示。
      * Check detail data.
      *
