ba4601f34bc06b65360f28caf26bef191091aca9
songchenxuan <songchenxuan@easycorp.ltd>
2024-11-06 09:57:45 +0800

    *[misc] add count_of_annual_created_effective_bug_in_project metric config.

--- module/bi/config/metrics.php
+++ module/bi/config/metrics.php
@@ -4690,3 +4690,17 @@ $config->bi->builtin->metrics[] = array
     'desc'       => '按项目统计的年度修复Bug数是指在某年度解决并关闭的Bug数量。这个度量项反映了项目在某年度解决的问题数量。年度修复Bug数越多可能说明开发团队在Bug解决方面的工作效率较高。',
     'definition' => "项目中Bug的个数求和\n关闭时间为某年\n解决方案为已解决\n过滤已删除的Bug\n过滤已删除的项目\n",
 );
+
+$config->bi->builtin->metrics[] = array
+(
+    'name'       => '按项目统计的年度新增有效Bug数',
+    'alias'      => '新增有效Bug数',
+    'code'       => 'count_of_annual_created_effective_bug_in_project',
+    'purpose'    => 'scale',
+    'scope'      => 'project',
+    'object'     => 'bug',
+    'unit'       => 'count',
+    'dateType'   => 'year',
+    'desc'       => '按项目统计的年度新增有效Bug数是指项目在某年度新发现的真正具有影响和价值的Bug数量。有效Bug通常是指导致项目不正常运行或影响用户体验的Bug。统计有效Bug数可以帮助评估项目的稳定性和质量也可以评估测试人员之前的协作或对项目的了解程度。',
+    'definition' => "项目中Bug个数求和\n创建时间为某年\n解决方案为已解决和延期处理或者状态为激活\n过滤已删除的Bug\n过滤已删除的项目\n",
+);
