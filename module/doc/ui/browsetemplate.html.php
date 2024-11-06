a0d9418662ce019985fa3f08a9aaf3103d463997
daitingting <daitingting@xirangit.com>
2024-11-06 07:41:08 +0000

    * [misc] Set template list view.

--- module/doc/ui/browsetemplate.html.php
+++ module/doc/ui/browsetemplate.html.php
@@ -10,4 +10,30 @@ declare(strict_types=1);
  */
 namespace zin;
 
+$spaceID = 1;
+$libID   = $menu['id'];
+$data    = array('spaceID' => $spaceID, 'docs' => array_values($templateList));
+$data['spaces'][] = array('name' => $lang->doc->template, 'id' => $spaceID);
+foreach($config->doc->templateMenu as $item) $data['libs'][] = $item + array('space' => $spaceID);
+
+$privs = array();
+$langData = array();
+
+docApp
+(
+    set::data($data),
+    set::spaceID($spaceID),
+    set::libID($libID),
+    set::docID($docID),
+    set::noSpace(),
+    set::noModule(),
+    set::homeName(false),
+    set::mode($docID ? 'view' : 'list'),
+    set::privs($privs),
+    set::userMap($users),
+    set::spaceIcon('home'),
+    set::langData($langData),
+    set::pager(array('recTotal' => count($templateList), 'recPerPage' => $recPerPage, 'page' => $pageID))
+);
+
 render();
