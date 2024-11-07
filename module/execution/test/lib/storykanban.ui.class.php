8e96e558ce4de23cd79dc5a6af5fe473b47bc4d3
yulujie <yulujie@easycorp.ltd>
2024-11-07 09:40:54 +0800

    * [task#131846,doing,0.5h,2h] Add function of storykanban.

--- module/execution/test/lib/storykanban.ui.class.php
+++ module/execution/test/lib/storykanban.ui.class.php
@@ -2,4 +2,22 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class storykanbanTester extends tester
 {
+    /**
+     * 检查需求看板数据。
+     * Check storykanban data.
+     *
+     * @param  string $stage
+     * @param  string $num
+     * @access public
+     * @return void
+     */
+    public function check($stage, $num)
+    {
+        $form = $this->initForm('execution', 'storykanban', array('execution' => '2'), 'appIframe-execution');
+        $form->dom->kanbanBtn->click();
+        $form->wait(1);
+        preg_match_all('/\d+(?:\.\d+)?/', $form->dom->$stage->getText(), $matches);
+        if($matches[0][0] != $num) return $this->failed("$stage列数据有误");
+        return $this->success("$stage列数据正确");
+    }
 }
