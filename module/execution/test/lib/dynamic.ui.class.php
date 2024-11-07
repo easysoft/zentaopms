e970a801179e4d262dbf6b9521ab2ea6cd68c63b
yulujie <yulujie@easycorp.ltd>
2024-11-07 17:03:36 +0800

    * [task#131845,doing,0.2h,3h] Add function.

--- module/execution/test/lib/dynamic.ui.class.php
+++ module/execution/test/lib/dynamic.ui.class.php
@@ -2,4 +2,17 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class dynamicTester extends tester
 {
-}
+    /**
+     * 检查执行动态。
+     * Check the dynamic of execution.
+     *
+     * @param  string $num
+     * @access public
+     * @return void
+     */
+    public function checkTotalNum($num)
+    {
+        $form = $this->initForm('execution', 'dynamic', array('execution' => '3'), 'appIframe-execution');
+        if($form->dom->num->getText !== $num)  return $this->failed('执行动态数量不正确');
+        return $this->success('执行动态数量正确');
+    }
