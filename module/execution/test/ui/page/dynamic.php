66260c34923f74bbe13a96cedb0f0f47c0788069
yulujie <yulujie@easycorp.ltd>
2024-11-07 15:16:40 +0800

    * [task#131845,doing,0.2h,3h] Add xpath.

---
+++ module/execution/test/ui/page/dynamic.php
@@ -0,0 +1,13 @@
+<?php
+class dynamicPage extends page
+{
+    public function __construct($webdriver)
+    {
+        parent::__construct($webdriver);
+        $xpath = array(
+            'num' => "//*[@id='featureBar']/menu/li[2]/a/span[2]",
+            'detailNum' => "//*[@id='mainContent']/div/div/ul/li/div[2]/div/ul/li"
+        );
+        $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
+    }
+}
