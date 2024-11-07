675d535aef2a012e86ef75a57d280f0975e298a9
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-11-07 16:57:55 +0800

    *[task#131965,doing,0.5h,4h] add project data of product track

--- module/product/test/ui/checktrack.php
+++ module/product/test/ui/checktrack.php
@@ -18,3 +18,23 @@ $product->PO->range('admin');
 $product->status->range('normal');
 $product->type->range('normal');
 $product->gen(1);
+
+$project = zenData('project');
+$project->id->range('1-2');
+$project->project->range('0,1');
+$project->model->range('scrum,[]');
+$project->type->range('project,sprint');
+$project->parent->range('0,1');
+$project->auth->range('extend');
+$project->storytype->range('story');
+$project->path->range('`,1,`,`,1,2,`');
+$project->grade->range('1');
+$project->name->range('项目1,迭代1');
+$project->hasProduct->range('1');
+$project->status->range('doing');
+$project->gen(2);
+
+$projectProduct = zenData('projectproduct');
+$projectProduct->project->range('1-6');
+$projectProduct->product->range('1');
+$projectProduct->gen(6);
