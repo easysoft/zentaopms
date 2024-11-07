4ef08bd98c96da482b35245880743ed35e1d8acd
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-11-07 09:26:34 +0000

    * [task#131888,doing,0.2h] Add ui test files of add a todo

--- module/my/test/ui/addtodo.php
+++ module/my/test/ui/addtodo.php
@@ -18,3 +18,16 @@ $todo->name->range('待办A,待办B,待办C,待办D,待办E');
 $todo->status->range('wait,doing,done,closed,wait');
 $todo->assignedTo->range('admin');
 $todo->assignedBy->range('admin');
+$todo->gen(5);
+
+$tester = new addTodoTester();
+$tester->login();
+
+$todoTitle = new stdClass();
+$todoTitle->name = '待办test01';
+
+$todoStatus = new stdClass();
+$todoStatus->doing = '进行中';
+$todoStatus->done  = '已完成';
+
+r($tester->addTodo($todoTitle, $todoStatus)) && p('message,status') && e('添加待办成功，SUCCESS');   //添加待办，添加成功
