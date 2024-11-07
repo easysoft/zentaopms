65ec63213d3901305df654b7bbed0cc1aac64b3b
huairong <huairong@easycorp.ltd>
2024-11-07 13:34:27 +0800

    + [task#132217,doing,2.4h,0.6h] Add comment of resolvebug file.

---
+++ module/project/test/ui/resolvebug.php
@@ -0,0 +1,17 @@
+#!/usr/bin/env php
+<?php
+
+/**
+
+title=项目下解决Bug操作检查
+timeout=0
+cid=1
+
+- 执行tester模块的resolveBug方法，参数是$bug[0]
+ - 最终测试状态 @SUCCESS
+ - 测试结果 @解决Bug表单页提示信息正确
+- 执行tester模块的resolveBug方法，参数是$bug[1]
+ - 最终测试状态 @SUCCESS
+ - 测试结果 @解决Bug成功
+
+*/
