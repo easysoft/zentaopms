692b79e6f82bbb21309043085acfc298a18ed315
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-11-07 10:37:18 +0800

    *[task#131966,doing,0.5h,3h] add function of check data of product kanban

--- module/product/test/lib/kanban.ui.class.php
+++ module/product/test/lib/kanban.ui.class.php
@@ -18,3 +18,16 @@ class kanbanTester extends tester
         $num     = $form->dom->$numDom->getText();
         $message = [
             'plan'      => '未过期计划数',
+            'project'   => '进行中的项目数',
+            'execution' => '进行中的执行数',
+            'release'   => '正常发布数'
+        ];
+        if (isset($message[$type]))
+        {
+            return ($num == $expected)
+                ? $this->success($message[$type] . '正确')
+                : $this->failed($message[$type] . '不正确');
+        }
+        return $this->failed('页面中无该类型的数据');
+    }
+}
