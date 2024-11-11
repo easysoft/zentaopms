0e65b06b953e9bd244a6744ef7f5a1d697c18695
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-11-11 15:16:40 +0800

    *[task#131967,doing,0.4h,2h] add project data of link project

--- module/product/test/ui/linkproject.php
+++ module/product/test/ui/linkproject.php
@@ -18,3 +18,22 @@ $product->PO->range('admin');
 $product->status->range('normal');
 $product->type->range('normal');
 $product->gen(2);
+
+$project = zenData('project');
+$project->id->range('1');
+$project->project->range('0');
+$project->model->range('scrum');
+$project->type->range('project');
+$project->parent->range('0');
+$project->auth->range('extend');
+$project->path->range('`,1,`');
+$project->grade->range('1');
+$project->name->range('项目1');
+$project->hasProduct->range('1');
+$project->status->range('wait');
+$project->gen(1);
+
+$projectProduct = zenData('projectproduct');
+$projectProduct->project->range('1');
+$projectProduct->product->range('2');
+$projectProduct->gen(1);
