66cbacbb1ebb6ab3dcbc0ea5f2e57e47e6b37900
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-11-11 15:36:46 +0800

    *[task#131967,doing,0.3h,2h] add product data project list

---
+++ module/product/test/ui/projectlist.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php
+
+/**
+title=检查产品下项目列表
+timeout=0
+cid=0
+
+*/
+chdir(__DIR__);
+include '../lib/project.ui.class.php';
+
+$product = zenData('product');
+$product->id->range('1-2');
+$product->program->range('0');
+$product->name->range('产品1,产品2');
+$product->PO->range('admin');
+$product->status->range('normal');
+$product->type->range('normal');
+$product->gen(2);
