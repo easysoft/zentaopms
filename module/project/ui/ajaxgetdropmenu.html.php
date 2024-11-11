4ef78250c05b0bd3f9fc8da20755fc397f369f81
dingyongliang <dingyongliang@easycorp.ltd>
2024-11-11 16:23:49 +0800

    * [task#132244,132245] update product and project home of doc module.

--- module/project/ui/ajaxgetdropmenu.html.php
+++ module/project/ui/ajaxgetdropmenu.html.php
@@ -61,11 +61,12 @@ foreach($projects as $programID => $programProjects)
         $group = $getProjectGroup($project);
 
         $item = array();
-        $item['id']   = $project->id;
-        $item['text'] = $project->name;
-        $item['icon'] = $project->model == 'scrum' ? 'sprint' : $project->model;
-        $item['keys'] = zget(common::convert2Pinyin(array($project->name)), $project->name, '');
-        $item['url']  = sprintf($link, $project->id);
+        $item['id']       = $project->id;
+        $item['text']     = $project->name;
+        $item['icon']     = $project->model == 'scrum' ? 'sprint' : $project->model;
+        $item['keys']     = zget(common::convert2Pinyin(array($project->name)), $project->name, '');
+        $item['url']      = sprintf($link, $project->id);
+        $item['involved'] = isset($involvedProjects[$project->id]);
 
         if((empty($project->multiple) || $project->type == 'kanban' || $project->model == 'kanban') && strpos($link, 'ajaxSwitchBelong') === false) $item['url'] = helper::createLink('project', 'index', "projectID={$project->id}");
 
