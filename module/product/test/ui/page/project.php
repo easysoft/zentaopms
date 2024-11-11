b6de05a091a1404429b1d427875f20e2a99557b1
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-11-11 15:09:59 +0800

    *[task#131967,doing,0.2h,3.5h] add xpath of projectlist

--- module/product/test/ui/page/project.php
+++ module/product/test/ui/page/project.php
@@ -13,6 +13,14 @@ class projectPage extends page
             'doingTab'      => '//*[@id="featureBar"]/menu/li[4]/a',
             'suspendedTab'  => '//*[@id="featureBar"]/menu/li[5]/a',
             'closedTab'     => '//*[@id="featureBar"]/menu/li[6]/a',
+            /*tab标签下项目数*/
+            'allNum'        => '//*[@id="featureBar"]/menu/li[1]/a/span[2]',
+            'unfinishedNum' => '//*[@id="featureBar"]/menu/li[2]/a/span[2]',
+            'waitingNum'    => '//*[@id="featureBar"]/menu/li[3]/a/span[2]',
+            'doingNum'      => '//*[@id="featureBar"]/menu/li[4]/a/span[2]',
+            'suspendedNum'  => '//*[@id="featureBar"]/menu/li[5]/a/span[2]',
+            'closedNum'     => '//*[@id="featureBar"]/menu/li[6]/a/span[2]',
+            'firstProject'  => '//*[@id="table-product-project"]/div[2]/div[1]/div/div[3]/div/a'
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
