498a7e06995c2db9bce6a3e88fc10fb6835ff7cf
lijie <lijie@easycorp.ltd>
2024-11-11 16:24:12 +0800

    [misc, 0.1h] modify code style.

--- module/story/test/ui/changestory.php
+++ module/story/test/ui/changestory.php
@@ -74,10 +74,10 @@ $storys = array();
 $storys['null']    = '';
 $storys['default'] = '变更后需求';
 
-r($tester->changeStory($storys['null']))    && p('message,status')  && e('变更需求表单页面提示信息正确,SUCCESS'); // 缺少需求名称，变更失败
+r($tester->changeStory($storys['null']))    && p('message,status') && e('变更需求表单页面提示信息正确,SUCCESS'); // 缺少需求名称，变更失败
 r($tester->changeStory($storys['default'])) && p('message,status') && e('变更需求成功,SUCCESS');                 // 使用默认选项变更需求,详情页信息对应
 
-r($tester->changeEpic($storys['null']))    && p('message,status')  && e('变更需求表单页面提示信息正确,SUCCESS'); // 缺少需求名称，变更失败
+r($tester->changeEpic($storys['null']))    && p('message,status') && e('变更需求表单页面提示信息正确,SUCCESS'); // 缺少需求名称，变更失败
 r($tester->changeEpic($storys['default'])) && p('message,status') && e('变更需求成功,SUCCESS');                 // 使用默认选项变更需求,详情页信息对应
 
 $tester->closeBrowser();
