72e59bd2ce7a6e4b21ca8ed26ffe11ce8816c503
lihuifang <lihuifang@easycorp.ltd>
2024-11-12 15:48:34 +0800

    *[task#131956,doing,1h,2h] Add ui test file of create kanban project.

--- module/project/test/ui/createkanban.php
+++ module/project/test/ui/createkanban.php
@@ -18,3 +18,22 @@ cid=73
 - 创建有日期的项目型看板项目最终测试状态 @SUCCESS
 
 */
+chdir(__DIR__);
+include '../lib/createkanban.ui.class.php';
+
+$tester = new createKanbanTester();
+$tester->login();
+
+//设置看板项目数据
+$kanban = array(
+    array('name' => '', 'end' => date('Y-m-d', strtotime('+30 days'))),
+    array('name' => '一个产品型看板项目'.time(), 'end' => ''),
+    array('type' => 1, 'name' => '一个产品型看板项目'.time(), 'end' => date('Y-m-d', strtotime('+1 month')), 'PM' => 'admin'),
+    array('type' => 1, 'name' => '一个长期的产品型看板项目'.time(), 'PM' => 'admin'),
+    array('type' => 0, 'name' => '一个项目型看板项目'.time(), 'end' => date('Y-m-d', strtotime('+1 month')), 'PM' => 'admin'),
+);
+
+r($tester->checkInput($kanban['0'])) && p('message,status') && e('创建看板项目表单页提示信息正确,SUCCESS');   // 创建看板项目缺少项目名称
+r($tester->checkInput($kanban['1'])) && p('message,status') && e('创建看板项目表单页提示信息正确,SUCCESS');   // 创建看板项目计划完成时间置空
+r($tester->checkInput($kanban['2'])) && p('status')  && e('SUCCESS');                                         // 创建有日期的产品型看板项目
+r($tester->checkInput($kanban['3'])) && p('status')  && e('SUCCESS');                                         // 创建一个长期的产品型看板项目
