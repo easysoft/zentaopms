ee27256ce6b6a3e8580404ede5afea81a8b9bcdf
liumengyi <liumengyi@easycorp.ltd>
2024-11-12 14:33:43 +0800

    + [task#132175,0.5h,doing] add unit test of programplamModel::getGanttTasks.

--- module/programplan/test/model/getgantttasks.php
+++ module/programplan/test/model/getgantttasks.php
@@ -18,3 +18,24 @@ cid=0
 
 */
 include dirname(__FILE__, 5) . '/test/lib/init.php';
+include dirname(__FILE__, 2) . '/lib/programplan.unittest.class.php';
+su('admin');
+
+zenData('task')->gen(30);
+zenData('userquery')->gen(1);
+
+$projectID  = array(11, 12, 101);
+$planIdList = array(array(101), array(102));
+$browseType = array('', 'bysearch');
+$queryID    = array(0, 1);
+
+$programplan = new programplanTest();
+r($programplan->getGanttTasksTest($projectID[0], $planIdList[0], $browseType[0], $queryID[0])) && p() && e('1'); // 测试查询项目 11 执行 101 browseType 空 queryID 0 的甘特图任务
+r($programplan->getGanttTasksTest($projectID[0], $planIdList[0], $browseType[0], $queryID[1])) && p() && e('1'); // 测试查询项目 11 执行 101 browseType 空 queryID 1 的甘特图任务
+r($programplan->getGanttTasksTest($projectID[0], $planIdList[0], $browseType[1], $queryID[0])) && p() && e('1'); // 测试查询项目 11 执行 101 browseType bysearch queryID 0 的甘特图任务
+r($programplan->getGanttTasksTest($projectID[0], $planIdList[0], $browseType[1], $queryID[1])) && p() && e('0'); // 测试查询项目 11 执行 101 browseType bysearch queryID 1 的甘特图任务
+r($programplan->getGanttTasksTest($projectID[1], $planIdList[1], $browseType[0], $queryID[0])) && p() && e('2'); // 测试查询项目 12 执行 102 browseType 空 queryID 0 的甘特图任务
+r($programplan->getGanttTasksTest($projectID[1], $planIdList[1], $browseType[0], $queryID[1])) && p() && e('2'); // 测试查询项目 12 执行 102 browseType 空 queryID 1 的甘特图任务
+r($programplan->getGanttTasksTest($projectID[1], $planIdList[1], $browseType[1], $queryID[0])) && p() && e('0'); // 测试查询项目 12 执行 102 browseType bysearch queryID 0 的甘特图任务
+r($programplan->getGanttTasksTest($projectID[1], $planIdList[1], $browseType[1], $queryID[1])) && p() && e('0'); // 测试查询项目 12 执行 102 browseType bysearch queryID 1 的甘特图任务
+r($programplan->getGanttTasksTest($projectID[2], $planIdList[0], $browseType[0], $queryID[0])) && p() && e('0'); // 测试查询不存在的项目 101 执行 101 browseType 空 queryID 0 的甘特图任务
