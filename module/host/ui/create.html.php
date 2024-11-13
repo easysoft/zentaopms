5a9540f72946f17d961b1d7e3e2a2e38adda945a
liyang <liyang@easycorp.ltd>
2024-11-13 16:20:49 +0800

    * [bug#56989] Fix bug for create host.

--- module/host/ui/create.html.php
+++ module/host/ui/create.html.php
@@ -50,7 +50,7 @@ formPanel
             set::label($lang->host->osName),
             set::control('picker'),
             set::name('osName'),
-            set::items(array()),
+            set::items($lang->host->osNameList),
             on::change('osChange')
         )
     ),
@@ -68,7 +68,7 @@ formPanel
             set::label($lang->host->osVersion),
             set::control('picker'),
             set::name('osVersion'),
-            set::items($lang->host->{"{$osName}List"})
+            set::items(array())
         )
     ),
     formRow
