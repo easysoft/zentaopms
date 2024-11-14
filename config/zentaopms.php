773bb45c3000fcb755c00f5bb713f65804755ac8
zhangxiquan <zhangxiquan@chandao.com>
2024-11-14 05:56:59 +0000

    * [bug#57020,done,0.2h] fix bug.

--- config/zentaopms.php
+++ config/zentaopms.php
@@ -694,7 +694,6 @@ $config->objectTables['privmanager']    = TABLE_PRIVMANAGER;
 $config->objectTables['privrelation']   = TABLE_PRIVRELATION;
 $config->objectTables['scene']          = TABLE_SCENE;
 $config->objectTables['pivot']          = TABLE_PIVOT;
-$config->objectTables['account']        = TABLE_ACCOUNT;
 $config->objectTables['serverroom']     = TABLE_SERVERROOM;
 $config->objectTables['host']           = TABLE_ZAHOST;
 $config->objectTables['instance']       = TABLE_INSTANCE;
