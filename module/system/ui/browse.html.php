d610d39af2d6a3b0960a839d796fb6d90714b568
liyang <liyang@easycorp.ltd>
2024-11-19 17:32:52 +0800

    * [task#132856] add system browse page.

---
+++ module/system/ui/browse.html.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+/**
+ * The browse view file of system module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Yang Li <liyang@chandao.com>
+ * @package     system
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+
+$canCreate  = hasPriv('system', 'create');
+$createLink = $this->createLink('system', 'create');
+$createItem = array('text' => $lang->system->create, 'url' => $createLink, 'class' => 'primary', 'icon' => 'plus', 'data-toggle' => 'modal');
+
+$tableData = initTableData($appList, $config->system->dtable->fieldList, $this->system);
