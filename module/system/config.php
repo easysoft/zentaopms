5e23d16db45e7368642362a4dc90baed6e7a2de3
liyang <liyang@easycorp.ltd>
2024-11-19 17:31:34 +0800

    * [task#132856] add action config.

--- module/system/config.php
+++ module/system/config.php
@@ -23,3 +23,18 @@ $config->system->actionList['inactive']['showText']     = true;
 $config->system->actionList['inactive']['ajaxSubmit']   = true;
 $config->system->actionList['inactive']['data-confirm'] = array('message' => $lang->system->confirmInactive, 'icon' => 'icon-exclamation-sign', 'iconClass' => 'warning-pale rounded-full icon-2x');
 $config->system->actionList['inactive']['url']          = array('module' => 'system', 'method' => 'inactive', 'params' => 'id={id}');
+
+$config->system->actionList['edit']['icon']        = 'edit';
+$config->system->actionList['edit']['text']        = $lang->edit;
+$config->system->actionList['edit']['hint']        = $lang->edit;
+$config->system->actionList['edit']['showText']    = true;
+$config->system->actionList['edit']['url']         = array('module' => 'system', 'method' => 'edit', 'params' => 'id={id}');
+$config->system->actionList['edit']['data-toggle'] = 'modal';
+
+$config->system->actionList['delete']['icon']         = 'trash';
+$config->system->actionList['delete']['text']         = $lang->delete;
+$config->system->actionList['delete']['hint']         = $lang->delete;
+$config->system->actionList['delete']['showText']     = true;
+$config->system->actionList['delete']['ajaxSubmit']   = true;
+$config->system->actionList['delete']['data-confirm'] = array('message' => $lang->system->confirmDelete, 'icon' => 'icon-exclamation-sign', 'iconClass' => 'warning-pale rounded-full icon-2x');
+$config->system->actionList['delete']['url']          = array('module' => 'system', 'method' => 'delete', 'params' => 'id={id}');
