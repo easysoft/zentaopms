2a2b6b0404e6c2c5cb4e51442e1347333ba2dcbc
liumengyi <liumengyi@easycorp.ltd>
2024-11-19 09:25:57 +0800

    * [task#132183,done,0.1h] optimize code.

--- module/my/config/table.php
+++ module/my/config/table.php
@@ -1012,10 +1012,10 @@ $config->my->audit->dtable->fieldList['type']['title']    = $lang->my->auditFiel
 $config->my->audit->dtable->fieldList['type']['type']     = 'catetory';
 $config->my->audit->dtable->fieldList['type']['sortType'] = true;
 
-$config->my->audit->dtable->fieldList['project']['name']   = 'project';
-$config->my->audit->dtable->fieldList['project']['title']  = $lang->execution->project;
-$config->my->audit->dtable->fieldList['project']['type']   = 'text';
-$config->my->audit->dtable->fieldList['project']['show']   = false;
+$config->my->audit->dtable->fieldList['project']['name']     = 'project';
+$config->my->audit->dtable->fieldList['project']['title']    = $lang->execution->project;
+$config->my->audit->dtable->fieldList['project']['type']     = 'text';
+$config->my->audit->dtable->fieldList['project']['sortType'] = true;
 
 $config->my->audit->dtable->fieldList['product']['name']     = 'product';
 $config->my->audit->dtable->fieldList['product']['title']    = $lang->story->product;
