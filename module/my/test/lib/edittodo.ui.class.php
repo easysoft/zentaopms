e83a25da2685748248ab01499b575714b1735d17
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-11-20 15:04:33 +0800

    * [task#133024,doing,0.2h] Add ui test files of edit a todo

--- module/my/test/lib/edittodo.ui.class.php
+++ module/my/test/lib/edittodo.ui.class.php
@@ -17,3 +17,19 @@ class addTodoTester extends tester
         $todoList = $this->loadPage('my', 'todo', array('type' => 'all'));
         $todoList->dom->fstTodoEdit->click();
         $todoList->wait(1);
+        $todoList->dom->name->setValue($todoTitle->name);
+        $todoList->wait(1);
+        $todoList->dom->status->picker($todoStatus->done);
+        $todoList->wait(1);
+        $todoList->dom->switchTime->click();
+        $todoList->dom->editTodoBtn->click();
+
+        $this->openUrl('my', 'todo', array('type' => 'all'));
+        $todoList = $this->loadPage('my', 'todo', array('type' => 'all'));
+        if($todoList->dom->fstTodoTitle->getText() != $todoTitle->name)
+        {
+            if($todoList->dom->fstTodoStatus->getText() != $todoStatus->done) return $this->success('添加待办成功');
+        }
+        return $this->failed('添加待办失败');
+    }
+}
