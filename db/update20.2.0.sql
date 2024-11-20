aff4b3f88fe8fb5f4248b86f456f1a7103771df2
daitingting <daitingting@xirangit.com>
2024-11-20 02:24:35 +0000

    * [task#133009,doint,1h] Add pivotSpec table, adjust fields.

--- db/update20.2.0.sql
+++ db/update20.2.0.sql
@@ -18,3 +18,31 @@ CREATE INDEX `idx_repo_deleted` ON `zt_job` (`repo`,`deleted`);
 UPDATE `zt_workflowfield` SET options = '{"wait":"\\u5f85\\u5ba1\\u6279","doing":"\\u5ba1\\u6279\\u4e2d","pass":"\\u901a\\u8fc7","reject":"\\u4e0d\\u901a\\u8fc7","reverting":"\\u56de\\u9000\\u4e2d"}' WHERE field = 'reviewStatus';
 
 UPDATE `zt_workflowaction` SET `role` = 'buildin' WHERE `buildin` = '1' AND `module` IN ('requirement', 'epic');
+
+CREATE TABLE `zt_pivotSpec` (
+  `pivot` mediumint(8) NOT NULL,
+  `version` varchar(10) NOT NULL,
+  `driver` enum('mysql', 'duckdb') NOT NULL default 'mysql',
+  `mode` varchar(10) NOT NULL default 'builder',
+  `name` text NULL,
+  `desc` text NULL,
+  `sql` text NULL,
+  `fields` text NULL,
+  `langs` text NULL,
+  `vars` text NULL,
+  `objects` text NULL,
+  `settings` text NULL,
+  `filters` text NULL,
+  `createdDate` datetime NULL,
+  UNIQUE KEY `pivot` (`pivot`, `version`)
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `zt_pivot` ADD `version` varchar(10) NOT NULL AFTER `builtin`;
+ALTER TABLE `zt_pivot` CHANGE `mode` `mode` varch(10) NOT NULL DEFAULT 'builder';
+ALTER TABLE `zt_pivot` CHANGE `sql` `sql` text NULL;
+ALTER TABLE `zt_pivot` CHANGE `fields` `fields` text NULL;
+ALTER TABLE `zt_pivot` CHANGE `langs` `langs` text NULL;
+ALTER TABLE `zt_pivot` CHANGE `vars` `vars` text NULL;
+ALTER TABLE `zt_pivot` CHANGE `objects` `objects` text NULL;
+ALTER TABLE `zt_pivot` CHANGE `settings` `settings` text NULL;
+ALTER TABLE `zt_pivot` CHANGE `filters` `filters` text NULL;
