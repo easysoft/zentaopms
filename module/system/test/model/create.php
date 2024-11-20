b737db46353783e8fb62a4a97b41890d95c2be93
liyang <liyang@easycorp.ltd>
2024-11-20 15:19:15 +0800

    * [task#132857] add create method unit test.

--- module/system/test/model/create.php
+++ module/system/test/model/create.php
@@ -14,7 +14,23 @@ cid=1
 */
 global $tester;
 $system = $tester->loadModel('system');
+
 $default = new stdclass();
 $default->name        = '应用10';
 $default->product     = 1;
 $default->integrated  = 0;
+$default->children    = '1';
+$default->status      = 'active';
+$default->desc        = '应用描述';
+$default->createdBy   = 'admin';
+$default->createdDate = '2024-01-01 00:00:00';
+
+r($system->create($default)) && p() && e('1'); // 查询默认排序应用
+$system->create($default);
+r(dao::getError()) && p('name:0') && e('『应用名称』已经有『应用10』这条记录了。如果您确定该记录已删除，请到后台-系统设置-回收站还原。'); // 查询默认排序应用
+$default->name = '';
+$system->create($default);
+r(dao::getError()) && p('name:0') && e('『应用名称』不能为空。');
+$default->name    = '应用11';
+$default->product = '字符串';
+$system->create($default);
