a0765ffc8c0908b76473fd984b7c9218f2eef51f
yulujie <yulujie@easycorp.ltd>
2024-11-20 09:23:57 +0800

    * [task#133020,doing,0.2h,4.5h] Add story data.

--- module/execution/test/ui/view.php
+++ module/execution/test/ui/view.php
@@ -12,9 +12,9 @@ include '../lib/view.ui.class.php';
 
 $product = zenData('product');
 $product->id->range('1-100');
-$product->name->range('产品1');
+$product->name->range('产品1,产品2');
 $product->type->range('normal');
-$product->gen(1);
+$product->gen(2);
 
 $project = zenData('project');
 $project->id->range('1-100');
@@ -35,6 +35,24 @@ $project->status->range('doing');
 $project->gen(3);
 
 $projectproduct = zenData('projectproduct');
-$projectproduct->project->range('2, 3');
-$projectproduct->product->range('1');
-$projectproduct->gen(2);
+$projectproduct->project->range('2{2}, 3');
+$projectproduct->product->range('1, 2, 1');
+$projectproduct->gen(3);
+
+$story->id->range('1-100');
+$story->parent->range('0');
+$story->isParent->range('0');
+$story->root->range('1-100');
+$story->path->range('`,1,`, `,2,`, `,3,`');
+$story->grade->range('1');
+$story->product->range('1');
+$story->module->range('0');
+$story->plan->range('0');
+$story->title->range('1-100');
+$story->type->range('story');
+$story->estimate->range('0');
+$story->status->range('active');
+$story->stage->range('projected');
+$story->assignedTo->range('[]');
+$story->version->range('1');
+$story->gen(3);
