6bf3e1720a33eda36a627ab0120b3a18e8dad6a7
lihuifang <lihuifang@easycorp.ltd>
2024-11-20 16:37:26 +0800

    +[task#133129,doing,0.5h,3.5h] Add ztf notes.

---
+++ module/projectstory/test/ui/checktab.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php
+
+/**
+
+title=项目下需求列表标签切换检查
+timeout=0
+cid=1
+
+- 检查全部标签下显示条数
+ - 测试结果 @allTab下显示条数正确
+ - 最终测试状态 @SUCCESS
+- 检查未关闭标签下显示条数
+ - 测试结果 @unclosedTab下显示条数正确
+ - 最终测试状态 @SUCCESS
+- 检查草稿标签下显示条数
+ - 测试结果 @draftTab下显示条数正确
+ - 最终测试状态 @SUCCESS
+- 检查评审中标签下显示条数
+ - 测试结果 @reviewingTab下显示条数正确
