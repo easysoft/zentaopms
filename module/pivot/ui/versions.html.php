c3142854e247f4fb1ab2d4b76f45daa625ed428d
zhouxin <zhouxin@easycorp.ltd>
2024-11-21 13:31:56 +0800

    * [task#133002,doing,0.1h] add ui page.

---
+++ module/pivot/ui/versions.html.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types = 1);
+/**
+ * The versions view file of pivot module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Xin Zhou <zhouxin@chandao.net>
+ * @package     pivot
+ * @link        https://www.zentao.net
+ */
+namespace zin;
