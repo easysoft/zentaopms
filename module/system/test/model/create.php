d360f499b357dfb47e37cca0366462982b8c99ed
liyang <liyang@easycorp.ltd>
2024-11-21 10:34:21 +0800

    * [task#132858] update create unit test.

--- module/system/test/model/create.php
+++ module/system/test/model/create.php
@@ -10,11 +10,11 @@ title=测试 systemModel::create();
 timeout=0
 cid=1
 
-- 查询默认排序应用 @1
-- 查询默认排序应用第name条的0属性 @『应用名称』已经有『应用10』这条记录了。如果您确定该记录已删除，请到后台-系统设置-回收站还原。
-- 执行第name条的0属性 @『应用名称』不能为空。
-- 执行第product条的0属性 @『product』应当是数字。
-- 执行第status条的0属性 @『状态』不符合格式，应当为:『/active|inactive/』。
+- 默认创建 @1
+- 重复创建第name条的0属性 @『应用名称』已经有『应用10』这条记录了。如果您确定该记录已删除，请到后台-系统设置-回收站还原。
+- 创建空名称失败第name条的0属性 @『应用名称』不能为空。
+- 创建非数字失败第product条的0属性 @『product』应当是数字。
+- 创建非法状态失败第status条的0属性 @『状态』不符合格式，应当为:『/active|inactive/』。
 */
 global $tester;
 $system = $tester->loadModel('system');
@@ -29,17 +29,17 @@ $default->desc        = '应用描述';
 $default->createdBy   = 'admin';
 $default->createdDate = '2024-01-01 00:00:00';
 
-r($system->create($default)) && p() && e('1'); // 查询默认排序应用
+r($system->create($default)) && p() && e('1'); // 默认创建
 $system->create($default);
-r(dao::getError()) && p('name:0') && e('『应用名称』已经有『应用10』这条记录了。如果您确定该记录已删除，请到后台-系统设置-回收站还原。'); // 查询默认排序应用
+r(dao::getError()) && p('name:0') && e('『应用名称』已经有『应用10』这条记录了。如果您确定该记录已删除，请到后台-系统设置-回收站还原。'); // 重复创建
 $default->name = '';
 $system->create($default);
-r(dao::getError()) && p('name:0') && e('『应用名称』不能为空。');
+r(dao::getError()) && p('name:0') && e('『应用名称』不能为空。'); // 创建空名称失败
 $default->name    = '应用11';
 $default->product = '字符串';
 $system->create($default);
-r(dao::getError()) && p('product:0') && e('『product』应当是数字。');
+r(dao::getError()) && p('product:0') && e('『product』应当是数字。'); // 创建非数字失败
 $default->product = 1;
 $default->status  = 'test';
 $system->create($default);
-r(dao::getError()) && p('status:0') && e('『状态』不符合格式，应当为:『/active|inactive/』。');
+r(dao::getError()) && p('status:0') && e('『状态』不符合格式，应当为:『/active|inactive/』。'); // 创建非法状态失败
