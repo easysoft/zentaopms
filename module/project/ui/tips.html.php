890c45d3426d54028b22b7476cbf41c0c5baf1ae
liumengyi <liumengyi@easycorp.ltd>
2024-11-21 17:02:04 +0800

    + [task#132763,doing,0.2h] add ui of project::tips.

--- module/project/ui/tips.html.php
+++ module/project/ui/tips.html.php
@@ -16,3 +16,24 @@ $objectID  = !empty($executionID) ? $executionID : $projectID;
 $executionLang = $lang->execution->typeList['sprint'];
 if($project->model == 'kanban') $executionLang = $lang->execution->typeList['kanban'];
 if($project->model == 'waterfall' || $project->model == 'waterfallplus') $executionLang = $lang->execution->typeList['stage'];
+
+$isKanbanProject         = $project->model == 'kanban';
+$hasProduct              = !empty($project->hasProduct);
+$multiple                = !empty($project->multiple);
+$showLinkStory           = !$isKanbanProject && $hasProduct;
+$showCreateStory         = !$isKanbanProject && !$hasProduct;
+$showCreateTask          = !$multiple && strpos(',scrum,agileplus,', ",{$project->model},") !== false;
+$showCreateExecution     = $multiple && strpos(',scrum,agileplus,kanban,', ",{$project->model},") !== false;
+$showSetDoc              = !$isKanbanProject;
+$showBackToTaskList      = !$isKanbanProject && !$multiple;
+$showBackToExecutionList = $multiple;
+
+$backUrl = createLink('project', 'execution', "status=undone&projectID={$projectID}");
+if(!$isKanbanProject && !$multiple) $backUrl = createLink('execution', 'task', "executionID={$executionID}");
+if($isKanbanProject) $backUrl = $multiple ? createLink('project', 'index', "projectID={$projectID}") : createLink('execution', 'kanban', "executionID={$executionID}");
+
+panel
+(
+    setID('tipsModal'),
+    set::title($lang->project->tips),
+);
