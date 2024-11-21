3043f6958d1a857f0430b26e6ca445b0bd211cfe
liyang <liyang@easycorp.ltd>
2024-11-21 10:33:00 +0800

    * [task#132858] add update method unit test.

---
+++ module/system/test/model/update.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+su('admin');
+zendata('system')->gen(10);
+
+/**
+
+title=测试 systemModel::create();
+timeout=0
+cid=1
+
+- 默认修改应用 @1
+- 修改重名提示第name条的0属性 @『应用名称』已经有『应用2』这条记录了。如果您确定该记录已删除，请到后台-系统设置-回收站还原。
+- 修改空提示第name条的0属性 @『应用名称』不能为空。
+- 修改非数字第product条的0属性 @『product』应当是数字。
+- 修改非法状态第status条的0属性 @『状态』不符合格式，应当为:『/active|inactive/』。
+*/
+global $tester;
+$system = $tester->loadModel('system');
