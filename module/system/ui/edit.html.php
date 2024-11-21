44ec2c143303787be11acdc3e1d233c70174504f
liyang <liyang@easycorp.ltd>
2024-11-21 10:37:00 +0800

    * [task#132858] add edit page.

---
+++ module/system/ui/edit.html.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+/**
+ * The edit view file of system module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Yang Li <liyang@chandao.com>
+ * @package     system
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+
+formPanel
+(
+    set::id('systemEditForm'),
+    set::title($lang->system->edit),
+    set::submitBtnText($lang->save),
+);
