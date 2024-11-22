7448150cee86dce940086e40e7643c401703bba6
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-11-22 09:32:00 +0800

    *[task#133115,doing,2h,7h] add data of contribute

---
+++ module/my/test/ui/contribute.php
@@ -0,0 +1,50 @@
+#!/usr/bin/env php
+<?php
+
+/**
+title=检查地盘贡献数据
+timeout=0
+cid=0
+*/
+chdir(__DIR__);
+include '../lib/contribute.ui.class.php';
+
+$product = zenData('product');
+$product->id->range('1');
+$product->program->range('0');
+$product->name->range('产品1');
+$product->PO->range('admin');
+$product->status->range('normal');
+$product->type->range('normal');
+$product->gen(1);
+
+$project = zenData('project');
+$project->id->range('1-2');
+$project->project->range('0,1');
+$project->model->range('scrum,[]');
+$project->type->range('project,sprint');
+$project->parent->range('0,1');
+$project->auth->range('extend');
+$project->storytype->range('story');
+$project->path->range('`,1,`,`,1,2,`');
+$project->grade->range('1');
+$project->name->range('项目1,迭代1');
+$project->hasProduct->range('1');
+$project->status->range('doing');
+$project->acl->range('open');
+$project->gen(2);
+
+$projectProduct = zenData('projectproduct');
+$projectProduct->project->range('1-2');
+$projectProduct->product->range('1');
+$projectProduct->gen(2);
+
+$task = zenData('task');
+$task->id->range('1-10');
+$task->parent->range('0');
+$task->project->range('1');
+$task->execution->range('2');
+$task->story->range('0');
+$task->name->range('任务1,任务2,任务3,任务4,任务5,任务6,任务7,任务8,任务9,任务10');
+$task->status->range('closed{2},cancel{1},wait{5},done{2}');
+$task->gen(10);
