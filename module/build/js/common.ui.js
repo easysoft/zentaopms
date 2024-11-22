4b2f4d85b8a98f73e52a1e4dd77bf71eacbc2b89
liyang <liyang@easycorp.ltd>
2024-11-22 14:04:11 +0800

    * [task#132870] add js.

--- module/build/js/common.ui.js
+++ module/build/js/common.ui.js
@@ -30,3 +30,19 @@ window.loadBranches = function(productID)
         }
     }, 'json');
 }
+
+window.setSystemBox = function(e)
+{
+    const newSystem = $(e.target).is(':checked') ? 1 : 0;
+    $('#systemBox #systemName').addClass('hidden');
+    $('#systemBox .picker-box').addClass('hidden');
+    if(newSystem == 1)
+    {
+        $('#systemBox #systemName').removeClass('hidden');
+    }
+    else
+    {
+        $('#systemBox #systemName').val('');
+        $('#systemBox .picker-box').removeClass('hidden');
+    }
+}
