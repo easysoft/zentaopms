24b4608d7395a2819f61b2e6112256a9ed68ce93
wangzemei <wangzemei@easycorp.ltd>
2024-11-22 13:42:15 +0800

    + [task#132561 doing 0.2h 7h] add the getIndicator function for thinkappeals widget.

--- lib/zin/wg/thinkappeals/v1.php
+++ lib/zin/wg/thinkappeals/v1.php
@@ -14,4 +14,18 @@ class thinkAppeals extends thinkModel
     {
         return file_get_contents(__DIR__ . DS . 'js' . DS . 'v1.js');
     }
+
+    protected function getIndicator(): array
+    {
+        global $app;
+
+        $blocks    = $this->prop('blocks');
+        $indicator = array();
+
+        foreach($blocks['steps'] as $key => $step)
+        {
+            $indicator[] = array('name' => $step->title, 'color'=> '#64758B', 'axisLabel' => $key == 0 ? array('show' => true) : null);
+        }
+        return $indicator;
+    }
 }
