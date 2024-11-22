bde82f64edcab892addde087b39e8f7d60bc4e64
lihuifang <lihuifang@easycorp.ltd>
2024-11-22 15:00:08 +0800

    +[task#133057,doing,2h,0.5h] Add element of export projectstory.

--- module/projectstory/test/ui/page/story.php
+++ module/projectstory/test/ui/page/story.php
@@ -15,7 +15,12 @@ class storyPage extends page
             'closedTab'            => "//*[@id='more']/menu/menu/li[1]/a/div/div",
             'linkedExecutionTab'   => "//*[@id='more']/menu/menu/li[2]/a/div/div",
             'unlinkedExecutionTab' => "//*[@id='more']/menu/menu/li[3]/a/div/div",
-            'num'                  => "//*[@id='stories_table']/div/div[3]/div[2]/strong[1]"
+            'num'                  => "//*[@id='stories_table']/div/div[3]/div[2]/strong[1]",
+            /* 导出 */
+            'format'         => "//*[@data-name='fileType']/div/div/span[1]",
+            'encoding'       => "//*[@data-name='encode']/div/div/span[1]",
+            'data'           => "//*[@data-name='exportType']/div/div/span[1]",
+            'exportBtnAlert' => "//*[@class='modal-content']/div[3]/div/div/form/div[8]/div/div/button/span"
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
