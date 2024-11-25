1119cd30b404aad22e2a33dbd3cab42878a4fa06
huairong <huairong@easycorp.ltd>
2024-11-25 15:54:32 +0800

    + [task#133030,doing,0.2,0.6h] Add comment of export bug ui test file.

---
+++ module/project/test/ui/exportbug.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php
+
+/**
+
+title=项目下导出Bug操作检查
+timeout=0
+cid=1
+
+- 按照默认设置导出
+ - 最终测试状态 @SUCCESS
+ - 测试结果 @导出Bug成功
+- 导出xml选中记录
+ - 最终测试状态 @SUCCESS
+ - 测试结果 @导出Bug成功
+- 导出html全部记录
+ - 最终测试状态 @SUCCESS
+ - 测试结果 @导出Bug成功
+
+*/
