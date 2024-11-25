07d78f99085cba6a8e74321e00e2178f46c0d06d
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-11-25 15:19:36 +0800

    *[task#133114,doing,1h,4h]review in review list

--- module/my/test/lib/audit.ui.class.php
+++ module/my/test/lib/audit.ui.class.php
@@ -24,4 +24,29 @@ class auditTester extends tester
                 ? $this->success("{$tabMessage[$tabName]}tab下数据显示正确")
                 : $this->failed("{$tabMessage[$tabName]}tab下数据显示不正确");
         }
+    }
+    public function review($type, $expectNum)
+    {
+        $form = $this->initForm('my', 'audit', array(), 'appIframe-my');
+        $form->wait(2);
+        $typeDom = $type . 'Tab';
+        $numDom  = $type . 'Num';
+        $typeMessage = [
+            'SR'  => '研发需求',
+            'UR'  => '用户需求',
+            'ER'  => '业务需求'
+        ];
+        $form->dom->$typeDom->click();
+        $form->wait(2);
+        $form->dom->reviewBtn->click();
+        $form->wait(2);
+        $form->dom->btn($this->lang->story->review)->click();
+        $form->wait(2);
+        $form->dom->$typeDom->click();
+        $form->wait(1);
+        $num = $form->dom->$numDom->getText();
+        return ($num == $expectNum)
+            ? $this->success("{$typeMessage[$type]}评审成功")
+            : $this->failed("{$typeMessage[$type]}评审不成功");
+    }
 }
