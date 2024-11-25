8e3493d9bf5b2e21a6c79772fa122eab3d627fd8
liugang <liugang@easycorp.ltd>
2024-11-25 13:41:39 +0800

    - [refac] remove unused code of cache files.

--- config/cache.php
+++ config/cache.php
@@ -42,32 +42,6 @@ foreach($config->cache->res as $table => $caches)
     }
 }
 
-$config->cache->enableFullPage   = false; // 是否开启整页缓存。Enable full page cache or not.
-$config->cache->fullPageLifetime = 5 * 60;
-$config->cache->fullPageDriver   = 'File';
-
-$config->cache->fullPages = array();      // 需要整页缓存的页面，格式为：模块名|方法名。模块和方法名都是小写。The pages need to be cached, format: module|method. Both module and method are lowercase.
-$config->cache->fullPages[] = 'program|browse';
-$config->cache->fullPages[] = 'product|all';
-$config->cache->fullPages[] = 'project|browse';
-$config->cache->fullPages[] = 'block|printblock';
-$config->cache->fullPages[] = 'screen|view';
-
-$config->cacheKeys = new stdclass();
-
-$config->cacheKeys->block = new stdclass();
-$config->cacheKeys->block->welcome          = 'welcome';
-$config->cacheKeys->block->contribute       = 'contribute';
-$config->cacheKeys->block->projectStatistic = 'projectStatistic%s%s%s';
-$config->cacheKeys->block->recentProject    = 'recentProject';
-
-$config->cacheKeys->execution = new stdclass();
-$config->cacheKeys->execution->ajaxGetDropMenuProjects   = 'ajaxDropMenuProjects';
-$config->cacheKeys->execution->ajaxGetDropMenuExecutions = 'ajaxDropMenuExecutions';
-
-$config->cacheKeys->bug = new stdclass();
-$config->cacheKeys->bug->browse = 'bugBrowse%s';
-
 $config->redis = new stdClass();
 $config->redis->host       = '';
 $config->redis->port       = '';
