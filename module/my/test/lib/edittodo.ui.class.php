abe1fd39e41eda3a997f6ba8d32d267eaa4d9ffb
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-11-25 16:21:34 +0800

    * [task#133028,done,0.5h] Add ui test files of activate a todo

--- module/my/test/lib/edittodo.ui.class.php
+++ module/my/test/lib/edittodo.ui.class.php
@@ -62,3 +62,21 @@ class addTodoTester extends tester
         if($todoList->dom->fstTodoStatus->getText() != $todoStatus->done) return $this->failed('完成待办失败');
         return $this->success('完成待办成功');
     }
+
+    /**
+     * 激活待办。
+     * Activate a todo.
+     *
+     * @param  string $todoStatus
+     * @access public
+     * @return void
+     */
+    public function activateTodo($todoStatus)
+    {
+        $this->openUrl('my', 'todo', array('type' => 'all'));
+        $todoList = $this->loadPage('my', 'todo', array('type' => 'all'));
+        $todoList->dom->fstTodoActivate->click();
+        $todoList->wait(1);
+        if($todoList->dom->fstTodoStatus->getText() != $todoStatus->waiting) return $this->failed('激活待办失败');
+        return $this->success('激活待办成功');
+    }
