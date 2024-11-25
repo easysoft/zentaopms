47ec0314acc73512c1bcd08cd8561be411aae0c2
lihuifang <lihuifang@easycorp.ltd>
2024-11-25 10:41:23 +0800

    +[task#133058,doing,1h,1.5h] Add project testdata.

--- module/projectstory/test/ui/linkstory.php
+++ module/projectstory/test/ui/linkstory.php
@@ -18,3 +18,23 @@ include '../lib/linkstory.ui.class.php';
 
 $product = zenData('product');
 $product->id->range('1');
+$product->name->range('产品1');
+$product->type->range('normal');
+$product->gen(1);
+
+$project = zenData('project');
+$project->id->range('1');
+$project->project->range('0');
+$project->model->range('scrum');
+$project->type->range('project');
+$project->auth->range('extend');
+$project->storytype->range('`story,epic,requirement`');
+$project->path->range('`,1,`');
+$project->grade->range('1');
+$project->name->range('项目1');
+$project->hasProduct->range('1');
+$project->status->range('wait');
+$project->acl->range('open');
+$project->gen(1);
+
+$projectProduct = zenData('projectproduct');
