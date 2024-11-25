84f63926befab8b08dfc05152297e5085598bfdf
liumengyi <liumengyi@easycorp.ltd>
2024-11-25 13:41:55 +0800

    * [misc] check execution if execution exists.

--- module/build/ui/view.html.php
+++ module/build/ui/view.html.php
@@ -11,8 +11,9 @@ declare(strict_types=1);
 namespace zin;
 
 $buildModule  = $app->tab == 'project' ? 'projectbuild' : 'build';
-$canBeChanged = common::canModify('execution', $execution) && common::canBeChanged($buildModule, $build);
 $decodeParam  = helper::safe64Decode($param);
+$canBeChanged = common::canBeChanged($buildModule, $build);
+if($canBeChanged && $execution) $canBeChanged = common::canModify('execution', $execution);
 
 $buildItems = array();
 foreach($buildPairs as $id => $name)
