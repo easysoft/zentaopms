a39c647632589bdd95c1c124b2b604a772994a60
lihuifang <lihuifang@easycorp.ltd>
2024-11-25 14:12:16 +0800

    +[task#133060,doing,2.5h,0.5h] Add testui function of unlinkstory.

--- module/projectstory/test/lib/unlinkstory.ui.class.php
+++ module/projectstory/test/lib/unlinkstory.ui.class.php
@@ -18,3 +18,23 @@ class unLinkStoryTester extends tester
         $form->wait(1);
         $form->dom->alertModal(); // 模态框中点击确定
         $form->wait(2);
+        $unlinkNumAfter = $form->dom->allTabNum->getText(); // 记录移除需求后版本关联的需求数量
+        // 断言检查单个移除需求是否成功
+        return ($unlinkNumAfter == $unlinkNumBefore - 1) ? $this->success('单个移除需求成功') : $this->failed('单个移除需求失败');
+    }
+
+    /**
+     * unlink all story of project.
+     * 移除全部需求
+     *
+     * @return object
+     */
+    public function batchUnlinkStory()
+    {
+        $form = $this->initForm('projectstory', 'story', array('projectID' => 1), 'appIframe-project');
+        $form->dom->allTab->click();
+        $form->dom->selectAllBtn->click(); // 全选需求
+        $form->wait(2);
+        $form->dom->batchUnlinkBtn->click(); // 点击批量移除按钮
+        $form->wait(2);
+        // 断言检查移除全部需求是否成功
