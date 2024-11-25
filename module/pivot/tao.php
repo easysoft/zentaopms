46c379a52c9687542736a04a3a25ceb9371e9795
zhouxin <zhouxin@easycorp.ltd>
2024-11-25 10:30:35 +0800

    Revert " * [task#133009,done,1h] Optimize codes."
    This reverts commit 191e3758957a48607c5b1aaa6d6c4182a34cf2eb.

--- module/pivot/tao.php
+++ module/pivot/tao.php
@@ -16,15 +16,43 @@ class pivotTao extends pivotModel
         $pivot = $this->dao->select('*')->from(TABLE_PIVOT)->where('id')->eq($id)->andWhere('deleted')->eq('0')->fetch();
         if(!$pivot) return false;
 
-        $specData = $this->dao->select('*')->from(TABLE_PIVOTSPEC)
-            ->where('pivot')->eq($id)
-            ->beginIF(is_null($version))->andWhere('version')->eq($pivot->version)->fi()
-            ->beginIF(!is_null($version))->andWhere('version')->eq($version)->fi()
-            ->fetch();
+        if(is_null($version)) return $this->mergePivotSpecData($pivot);
+
+        $specData = $this->dao->select('*')->from(TABLE_PIVOTSPEC)->where('pivot')->eq($id)->andWhere('version')->eq($version)->fetch();
         if(!$specData) return $pivot;
 
         foreach($specData as $specKey => $specValue) $pivot->$specKey = $specValue;
         return $pivot;
+    }
+
+    /**
+     * 合并 pivotSpec 的数据。
+     * Merge pivotSpec data to pivot.
+     *
+     * @param int     $id
+     * @param bool    $isObject
+     * @access public
+     * @return object|bool
+     */
+    protected function mergePivotSpecData($pivots, $isObject = true)
+    {
+        if($isObject) $pivots = array($pivots);
+        $pivotIDList = array_column($pivots, 'id');
+
+        $pivotSpecs = $this->dao->select('t2.*')->from(TABLE_PIVOT)->alias('t1')
+            ->leftJoin(TABLE_PIVOTSPEC)->alias('t2')->on('t1.id = t2.pivot and t1.version = t2.version')
+            ->where('t1.id')->in($pivotIDList)
+            ->fetchAll('pivot');
+
+        foreach($pivots as $index => $pivot)
+        {
+            if(!isset($pivotSpecs[$pivot->id])) continue;
+
+            foreach($pivotSpecs[$pivot->id] as $specKey => $specValue) $pivot->$specKey = $specValue;
+            $pivots[$index] = $pivot;
+        }
+
+        return $isObject ? current($pivots) : $pivots;
     }
 
     /**
@@ -452,13 +480,14 @@ EOT)->from(TABLE_TASK)->alias('t1')
      */
     protected function getAllPivotByGroupID(int $groupID): array
     {
-        return $this->dao->select('t1.*, t2.*')->from(TABLE_PIVOT)->alias('t1')
-            ->leftJoin(TABLE_PIVOTSPEC)->alias('t2')->on('t1.id=t2.pivot and t1.version=t2.version')
-            ->where("FIND_IN_SET({$groupID}, t1.`group`)")
-            ->andWhere('t1.stage')->ne('draft')
-            ->andWhere('t1.deleted')->eq('0')
+        $pivots = $this->dao->select('*')->from(TABLE_PIVOT)
+            ->where("FIND_IN_SET({$groupID}, `group`)")
+            ->andWhere('stage')->ne('draft')
+            ->andWhere('deleted')->eq('0')
             ->orderBy('id_desc')
             ->fetchAll();
+
+        return $this->mergePivotSpecData($pivots);
     }
 
     /**
