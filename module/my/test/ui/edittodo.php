ab54b040538ed08ab46e78aa3c97fd05c83339fe
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-11-25 10:49:07 +0800

    * [task#133024,done,0.2h] Add ui test files of edit a todo

--- module/my/test/ui/edittodo.php
+++ module/my/test/ui/edittodo.php
@@ -13,3 +13,23 @@ timeout=0
 */
 chdir(__DIR__);
 include '../lib/edittodo.ui.class.php';
+
+$todo = zenData('todo');
+$todo->id->range('1-5');
+$todo->account->range('admin');
+$todo->type->range('custom');
+$todo->name->range('待办A,待办B,待办C,待办D,待办E');
+$todo->status->range('wait,doing,done,closed,wait');
+$todo->assignedTo->range('admin');
+$todo->assignedBy->range('admin');
+$todo->gen(5);
+
+$tester = new addTodoTester();
+$tester->login();
+
+$todoTitle = new stdClass();
+$todoTitle->name = '待办test01';
+
+$todoStatus = new stdClass();
+$todoStatus->doing = '进行中';
+$todoStatus->done  = '已完成';
