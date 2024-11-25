a6b5099193a65267dd45c9a5743d583ff05a1910
caoyanyi <caoyanyi@easycorp.ltd>
2024-11-25 14:34:27 +0800

    + [task#132861,doing,0.3h] add system page for release.

--- module/release/ui/ajaxloadsystemblock.html.php
+++ module/release/ui/ajaxloadsystemblock.html.php
@@ -15,3 +15,42 @@ jsVar('appList',  $appList);
 
 $systemTR = array();
 $i        = 0;
+foreach($appList as $system)
+{
+    $systemTR[] = h::tr
+    (
+        setClass('form-row'),
+        h::td
+        (
+            picker
+            (
+                set::id("apps{$i}"),
+                set::name("apps[$i]"),
+                set::items($appList),
+                set('onchange', "setRelease(event, '{$i}')")
+            )
+        ),
+        h::td
+        (
+            picker
+            (
+                set::id("releases{$i}"),
+                set::name("releases[$i]"),
+                set::items(array())
+            )
+        ),
+        h::td
+        (
+            set::className('actions-list'),
+            btnGroup
+            (
+                set::items(array(
+                    array('class' => 'btn btn-link text-gray', 'icon' => 'plus', 'onclick' => 'addItem(this)'),
+                    array('class' => 'btn btn-link text-gray', 'icon' => 'trash', 'onclick' => 'deleteItem(this)')
+                ))
+            )
+        )
+    );
+
+    $i ++;
+}
