cb161f9798ba33a328893eb3026ed9b3a8f34c43
lihuifang <lihuifang@easycorp.ltd>
2024-11-25 10:44:32 +0800

    +[task#133058,doing,2h,0.5h] Add element of linkstory.

--- module/projectstory/test/ui/page/story.php
+++ module/projectstory/test/ui/page/story.php
@@ -16,11 +16,18 @@ class storyPage extends page
             'linkedExecutionTab'   => "//*[@id='more']/menu/menu/li[2]/a/div/div",
             'unlinkedExecutionTab' => "//*[@id='more']/menu/menu/li[3]/a/div/div",
             'num'                  => "//*[@id='stories_table']/div/div[3]/div[2]/strong[1]",
+            'allTabNum'            => "//*[@id='main']/div/div[1]/div[1]/menu/li[1]/a/span[2]",
             /* 导出 */
             'format'         => "//*[@data-name='fileType']/div/div/span[1]",
             'encoding'       => "//*[@data-name='encode']/div/div/span[1]",
             'data'           => "//*[@data-name='exportType']/div/div/span[1]",
-            'exportBtnAlert' => "//*[@class='modal-content']/div[3]/div/div/form/div[8]/div/div/button/span"
+            'exportBtnAlert' => "//*[@class='modal-content']/div[3]/div/div/form/div[8]/div/div/button/span",
+            /* 关联需求 */
+            'allTabNum'      => "//*[@id='main']/div/div[1]/div[1]/menu/li[1]/a/span[2]",
+            'linkStoryBtn'   => "//*[@id='actionBar']/div[3]/a/span",
+            'searchBtn'      => "//*[@class='btn primary']",
+            'selectAllStory' => "//*[@id='table-projectstory-linkstory']/div[3]/div/div/label",
+            'saveBtn'        => "//*[@id='table-projectstory-linkstory']/div[3]/nav/button/span"
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
