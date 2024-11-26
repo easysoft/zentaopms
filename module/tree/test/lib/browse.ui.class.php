05be060851f55ab04166b8381eab395316f67b22
yulujie <yulujie@easycorp.ltd>
2024-11-26 10:45:33 +0800

    * [task#133022,doing,0.6h,4.5h] Add create function

--- module/tree/test/lib/browse.ui.class.php
+++ module/tree/test/lib/browse.ui.class.php
@@ -2,4 +2,22 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class browseTester extends tester
 {
+    /**
+     * 检查产品下创建模板。
+     * Check the creation of modules under the product.
+     *
+     * @param  string $moduleName
+     * @access public
+     * @return void
+     */
+    public function createModule($moduleName)
+    {
+        $form = $this->initForm('tree', 'browse', array('product' => '1', 'view' => 'story'), 'appIframe-product');
+        $form->dom->firsrNullModule->setValue($moduleName);
+        $form->dom->submitBtn->click();
+        $form->wait(1);
+
+        if($form->dom->firstModule->getText() == $moduleName) return $this->success('创建模块成功');
+        return $this->failed('创建模块失败');
+    }
 }
