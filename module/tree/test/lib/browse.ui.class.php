19f6648219909586e7127948052998d7db086484
yulujie <yulujie@easycorp.ltd>
2024-11-26 15:01:53 +0800

    * [task#133022,doing,0.6h,3.5h] Add new function.

--- module/tree/test/lib/browse.ui.class.php
+++ module/tree/test/lib/browse.ui.class.php
@@ -3,12 +3,12 @@ include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class browseTester extends tester
 {
     /**
-     * 检查产品下创建模板。
+     * 检查产品下创建模块。
      * Check the creation of modules under the product.
      *
      * @param  string $moduleName
      * @access public
-     * @return void
+     * @return object
      */
     public function createModule($moduleName)
     {
@@ -19,5 +19,33 @@ class browseTester extends tester
 
         if($form->dom->firstModule->getText() == $moduleName) return $this->success('创建模块成功');
         return $this->failed('创建模块失败');
+    }
+
+    /**
+     * 检查产品下创建子模块。
+     * Check the creation of child modules under the product.
+     *
+     * @param  string $childModuleName
+     * @access public
+     * @return object
+     */
+    public function createChildModule($childModuleName)
+    {
+        $form = $this->initForm('tree', 'browse', array('product' => '1', 'view' => 'story'), 'appIframe-product');
+        if(!is_object($form->dom->firstViewBtn)) return $this->failed('不能创建子模块');
+        $form->dom->firstViewBtn->click();
+        $form->wait(1);
+        $form->dom->firsrNullModule->setValue($childModuleName);
+        $form->dom->submitBtn->click();
+        $form->wait(1);
+
+        $form = $this->loadPage();
+        $form->wait(1);
+        if($form->dom->firstCaret->attr('class') == 'caret-right') $form->dom->firstCaret->click();
+        $form->wait(1);
+        if($form->dom->firstChildModule->getText() == $childModuleName) return $this->success('创建模块成功');
+        return $this->failed('创建模块失败');
+
+
     }
 }
