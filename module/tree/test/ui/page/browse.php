afef545875dc7a84b0e2d4f7e2ac94ca1be749f1
yulujie <yulujie@easycorp.ltd>
2024-11-26 18:00:39 +0800

    * [task#133022,doing,0.2h,2.0h] Add xpath.

--- module/tree/test/ui/page/browse.php
+++ module/tree/test/ui/page/browse.php
@@ -12,11 +12,17 @@ class browsePage extends page
             'submitBtn'        => "//*[@id='zin_tree_browse_form']//button[@type='submit']",
 
             'firstModule'      => "//*[@id='treeEditor-tree-browse']/li[1]/div/div/a",
+            'lastModule'       => "(//*[@id='treeEditor-tree-browse']/li)[last()]/div/div/a",
             'firstChildModule' => "//*[@id='treeEditor-tree-browse']/li/menu/li[1]/div/div/a",
+            'lastChildModule'  => "(//*[@id='treeEditor-tree-browse']/li/menu/li)[last()]/div/div/a",
             'firstEditBtn'     => "//*[@id='treeEditor-tree-browse']/li[1]/div/nav/a[1]",
             'firstDelBtn'      => "//*[@id='treeEditor-tree-browse']/li[1]/div/nav/a[2]",
             'firstViewBtn'     => "//*[@id='treeEditor-tree-browse']/li[1]/div/nav/a[3]",
             'firstCaret'       => "//*[@id='treeEditor-tree-browse']/li/div/span/span",
+
+            'modalText' => "//div[@class='modal-body']/div",
+            /* 编辑弹窗中元素 */
+            'editSubmitBtn' => "//*[@id='zin_tree_edit_form']//button[@type='submit']"
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
