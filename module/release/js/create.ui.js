c9c93525106a840cab74e896dce933c1be2c4adc
caoyanyi <caoyanyi@easycorp.ltd>
2024-11-26 10:58:05 +0800

    * [task#132861,doing,0.2h] format code for release js.

--- module/release/js/create.ui.js
+++ module/release/js/create.ui.js
@@ -1,3 +1,22 @@
+window.loadSystemBlock = function()
+{
+    const systemID = $('[name=system]').val();
+    if(!systemID || !appList[systemID]) return;
+
+    $('#systemBlock, #buildBox').addClass('hidden');
+    if(appList[systemID].integrated == 1)
+    {
+        if(typeof linkedRelease == 'undefined') linkedRelease = '';
+
+        $('#systemBlock').removeClass('hidden');
+        loadTarget($.createLink('release', 'ajaxLoadSystemBlock', `systemID=${systemID}&release=${linkedRelease}`), 'systemItems');
+    }
+    else
+    {
+        $('#buildBox').removeClass('hidden');
+    }
+}
+
 window.changeProduct = function(event)
 {
     let productID = $(event.target).val();
