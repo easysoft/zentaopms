09ff594a23d015af0a19a7c260e1ae3bcf9439a6
lijie <lijie@easycorp.ltd>
2024-11-26 14:49:32 +0800

    [task#133132, 1h] add close epic ui function.

--- module/epic/test/lib/closeepic.ui.class.php
+++ module/epic/test/lib/closeepic.ui.class.php
@@ -17,3 +17,23 @@ class closeEpicTester extends tester
      * @param string closeReason
      * @access public
      * @return object
+     */
+    public function closeEpic($storyID, $closeReason)
+    {
+        $form = $this->initForm('epic', 'view', array('id' => $storyID), 'appIframe-product');  //进入业务需求详情页
+        $form->dom->btn($this->lang->story->close)->click();  //点击关闭需求按钮
+
+        $form->wait(1);
+        $form->dom->closedReason->picker($closeReason); //选择关闭原因
+        $form->dom->closedButton->click();
+        $form->wait(1);
+
+        $viewPage = $this->loadPage();
+        if($viewPage->dom->status->getText() != '已关闭') return $this->failed('需求状态不正确');
+        $viewPage->dom->btn($this->lang->story->legendLifeTime)->click();
+        if($viewPage->dom->closeReason->getText() != $closeReason) return $this->failed('需求关闭原因不正确');
+
+        return $this->success('关闭业务需求成功');
+    }
+
+    /**
