279ce944348cba9b6105d9ca8c74a8af1a3bbc9f
yulujie <yulujie@easycorp.ltd>
2024-11-26 17:58:27 +0800

    * [task#133022,doing,1.5h,2.0h] Add function.

--- module/tree/test/lib/browse.ui.class.php
+++ module/tree/test/lib/browse.ui.class.php
@@ -7,16 +7,28 @@ class browseTester extends tester
      * Check the creation of modules under the product.
      *
      * @param  string $moduleName
+     * @param  bool   $checkRepeat
      * @access public
      * @return object
      */
-    public function createModule($moduleName)
+    public function createModule($moduleName, $checkRepeat = false)
     {
         $form = $this->initForm('tree', 'browse', array('product' => '1', 'view' => 'story'), 'appIframe-product');
+        if($checkRepeat) $moduleName = $form->dom->firstModule->getText();
         $form->dom->firsrNullModule->setValue($moduleName);
         $form->dom->submitBtn->click();
         $form->wait(1);
 
+        if($checkRepeat)
+        {
+            if($form->dom->modalText->getText() == sprintf($this->lang->tree->repeatName, $moduleName)) return $this->success('创建模块时模块已存在，提示正确');
+            return $this->failed('创建模块时模块已存在，提示错误');
+        }
+        if(preg_match('/\s/', $moduleName))
+        {
+            if($form->dom->modalText->getText() == $this->lang->tree->shouldNotBlank) return $this->success('创建模块时模块名包含空格，提示正确');
+            return $this->failed('创建模块时模块名包含空格，提示错误');
+        }
         if($form->dom->lastModule->getText() == $moduleName) return $this->success('创建模块成功');
         return $this->failed('创建模块失败');
     }
@@ -26,25 +38,38 @@ class browseTester extends tester
      * Check the creation of child modules under the product.
      *
      * @param  string $childModuleName
+     * @param  bool   $checkRepeat
      * @access public
      * @return object
      */
-    public function createChildModule($childModuleName)
+    public function createChildModule($childModuleName, $checkRepeat = false)
     {
         $form = $this->initForm('tree', 'browse', array('product' => '1', 'view' => 'story'), 'appIframe-product');
         if(!is_object($form->dom->firstViewBtn)) return $this->failed('不能创建子模块');
         $form->dom->firstViewBtn->click();
         $form->wait(1);
+        if($checkRepeat) $childModuleName = $form->dom->firstChildModule->getText();
         $form->dom->firsrNullModule->setValue($childModuleName);
         $form->dom->submitBtn->click();
         $form->wait(1);
 
         $form = $this->loadPage();
         $form->wait(1);
+
+        if($checkRepeat)
+        {
+            if($form->dom->modalText->getText() == sprintf($this->lang->tree->repeatName, $childModuleName)) return $this->success('创建子模块时子模块已存在，提示正确');
+            return $this->failed('创建子模块时子模块已存在，提示错误');
+        }
+        if(preg_match('/\s/', $childModuleName))
+        {
+            if($form->dom->modalText->getText() == $this->lang->tree->shouldNotBlank) return $this->success('创建子模块时子模块名包含空格，提示正确');
+            return $this->failed('创建子模块时子模块名包含空格，提示错误');
+        }
         if($form->dom->firstCaret->attr('class') == 'caret-right') $form->dom->firstCaret->click();
         $form->wait(1);
-        if($form->dom->firstChildModule->getText() == $childModuleName) return $this->success('创建模块成功');
-        return $this->failed('创建模块失败');
+        if($form->dom->lastChildModule->getText() == $childModuleName) return $this->success('创建子模块成功');
+        return $this->failed('创建子模块失败');
     }
 
     /**
@@ -69,6 +94,17 @@ class browseTester extends tester
         {
             if($form->dom->nameTip->getText() == sprintf($this->lang->error->notempty, $this->lang->tree->name)) return $this->success('编辑模块时模块为空，提示正确');
             return $this->failed('编辑模块时模块为空，提示错误');
+        }
+        if($form->dom->lastModule->getText() == $newName)
+        {
+            if($form->dom->nameTip->getText() == sprintf($this->lang->tree->repeatName, $newName)) return $this->success('编辑模块时模块已存在，提示正确');
+            return $this->failed('编辑模块时模块已存在，提示错误');
+        }
+        if(preg_match('/\s/', $newName))
+        {
+            if(!is_object($form->dom->modalText)) return $this->failed('编辑模块时模块名包含空格，没有提示');
+            if($form->dom->modalText->getText() == $this->lang->tree->shouldNotBlank) return $this->success('编辑模块时模块名包含空格，提示正确');
+            return $this->failed('编辑模块时模块名包含空格，提示错误');
         }
         if($form->dom->firstModule->getText() == $newName) return $this->success('编辑模块成功');
         return $this->failed('编辑模块失败');
