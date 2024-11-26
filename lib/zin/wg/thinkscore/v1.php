f7308e7a3968f24f1f8c097e5cac180c61fcf5a4
zhangyu <zhangyu@easycorp.ltd>
2024-11-26 16:06:21 +0800

    * [task#132545 doing 0.2h 2h] modify select items criterions of thinkscore.

--- lib/zin/wg/thinkscore/v1.php
+++ lib/zin/wg/thinkscore/v1.php
@@ -15,6 +15,8 @@ class thinkScore extends thinkQuestion
         'citation?: int=1',        // 引用方式
         'selectColumn?: string',   // 选择列
         'scoreSetting?: string=0', // 分制设置
+        'criterions5?: array',     // 5分制的评分标准
+        'criterions10?: array',    // 10分制的评分标准
     );
 
     public static function getPageCSS(): ?string
@@ -27,20 +29,23 @@ class thinkScore extends thinkQuestion
     {
         global $lang;
         $detailWg = parent::buildDetail();
-        list($step, $scoreSetting) = $this->prop(array('step','scoreSetting'));
+        list($step, $scoreSetting, $criterions5, $criterions10) = $this->prop(array('step', 'scoreSetting', 'criterions5', 'criterions10'));
         if($step)
         {
             $fields         = !empty($step->options->fields) ? $step->options->fields : array();
             $scoreSetting   = isset($step->options->scoreSetting) ? $step->options->scoreSetting : '0';
             $quoteTitleList = !empty($step->options->quoteTitle) ? explode(", ", $step->options->quoteTitle) : array();
+            $criterions5    = isset($step->options->criterions5) ? $step->options->criterions5 : $lang->thinkwizard->criterion->defaultCriteria5;
+            $criterions10   = isset($step->options->criterions10) ? $step->options->criterions10 : $lang->thinkwizard->criterion->defaultCriteria10;
         }
         if(empty($fields) && !empty($quoteTitleList)) $fields = array($lang->thinkwizard->previewSteps->objectReference, $lang->thinkwizard->previewSteps->objectReference,$lang->thinkwizard->previewSteps->objectReference);
 
         $scoreCount = $scoreSetting == '0' ? 5 : 10;
         $items      = array();
         $radioItems = array();
+        $criterions = $scoreSetting == '0' ? $criterions5 : $criterions10;
 
-        for($i=1; $i<=$scoreCount; $i++){$items[] = array('text' => $i, 'value' => $i, 'disabledPrefix' => true);}
+        for($i=1; $i<=$scoreCount; $i++){$items[] = array('text' => $i, 'value' => $i, 'disabledPrefix' => true, 'title' => $criterions[$i - 1]);}
 
         foreach ($fields as $field)
         {
