055a82a071e50711fe8739ceebfbd7cc3b80a4e9
yulujie <yulujie@easycorp.ltd>
2024-11-26 18:00:18 +0800

    * [task#133022,doing,0.2h,2.0h] Add cases.

--- module/tree/test/ui/browse.php
+++ module/tree/test/ui/browse.php
@@ -22,5 +22,17 @@ $module->gen(0, true);
 $tester = new browseTester();
 $tester->login();
 
-r($tester->createModule('模块1'))   && p('status,message') && e('SUCCESS,创建模块成功');
-r($tester->createChildModule('子模块1')) && p('status,message') && e('SUCCESS,创建模块成功');
+r($tester->createModule('模块 1'))             && p('status,message') && e('SUCCESS,创建模块时模块名包含空格空，提示正确');
+r($tester->createModule('模块1'))              && p('status,message') && e('SUCCESS,创建模块成功');
+r($tester->createModule('模块1', true))        && p('status,message') && e('SUCCESS,创建模块时模块已存在，提示正确');
+r($tester->createModule('模块2'))              && p('status,message') && e('SUCCESS,创建模块成功');
+r($tester->createChildModule('子模块 1'))      && p('status,message') && e('SUCCESS,创建子模块时子模块名包含空格，提示正确');
+r($tester->createChildModule('子模块1'))       && p('status,message') && e('SUCCESS,创建子模块成功');
+r($tester->createChildModule('模块2'))         && p('status,message') && e('SUCCESS,创建子模块成功');
+r($tester->createChildModule('子模块1', true)) && p('status,message') && e('SUCCESS,创建子模块时子模块已存在，提示正确');
+
+r($tester->editModule(''))          && p('status,message') && e('SUCCESS,编辑模块时模块为空，提示正确');
+r($tester->editModule('模块 2'))    && p('status,message') && e('SUCCESS,编辑模块时模块名包含空格，提示正确');
+r($tester->editModule('模块2'))     && p('status,message') && e('SUCCESS,编辑模块时模块已存在，提示正确');
+r($tester->editModule('编辑模块1')) && p('status,message') && e('SUCCESS,编辑模块成功');
+$tester->closeBrowser();
