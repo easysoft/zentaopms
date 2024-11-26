9515ac43ccf54179a7637fdabb7d1fc7ff982dc4
zhangyu <zhangyu@easycorp.ltd>
2024-11-26 14:16:07 +0800

    * [task#132545 doing 0.2h 2h] initial buildDetail function of thinkscore.

--- lib/zin/wg/thinkscore/v1.php
+++ lib/zin/wg/thinkscore/v1.php
@@ -14,5 +14,20 @@ class thinkScore extends thinkQuestion
         'quoteQuestions?: array',  // 引用问题
         'citation?: int=1',        // 引用方式
         'selectColumn?: string',   // 选择列
+        'scoreSetting?: string=0', // 分制设置
     );
+
+    public static function getPageCSS(): ?string
+    {
+        $baseCss = file_get_contents(dirname(__FILE__, 2) . DS . 'thinkstepbase' . DS . 'css' . DS . 'v1.css');
+        return file_get_contents(__DIR__ . DS . 'css' . DS . 'v1.css') . $baseCss;
+    }
+
+    protected function buildDetail(): array
+    {
+        global $lang;
+        $detailWg = parent::buildDetail();
+        list($step, $quoteQuestions, $preViewModel, $isRun, $scoreSetting) = $this->prop(array('step', 'quoteQuestions', 'preViewModel', 'isRun', 'scoreSetting'));
+        return $detailWg;
+    }
 }
