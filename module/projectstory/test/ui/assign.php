aba7617e8c66d1cb077b6a3dc3610f4b12292eb6
lihuifang <lihuifang@easycorp.ltd>
2024-11-27 11:19:24 +0800

    *[task#133128,doing,0.8h,1.7h] Add story testdata.

--- module/projectstory/test/ui/assign.php
+++ module/projectstory/test/ui/assign.php
@@ -38,3 +38,23 @@ $project->grade->range('1');
 $project->name->range('项目1');
 $project->hasProduct->range('1');
 $project->status->range('wait');
+$project->acl->range('open');
+$project->gen(1);
+
+$projectProduct = zenData('projectproduct');
+$projectProduct->project->range('1');
+$projectProduct->product->range('1');
+$projectProduct->gen(1);
+
+$story = zenData('story');
+$story->id->range('1-5');
+$story->parent->range('0');
+$story->isParent->range('0');
+$story->root->range('1-5');
+$story->path->range('`,1,`, `,2,`, `,3,`, `,4,`, `,5,`');
+$story->grade->range('1');
+$story->product->range('1');
+$story->module->range('0');
+$story->plan->range('0');
+$story->title->range('需求001,需求002,需求003,需求004,需求005');
+$story->type->range('story');
