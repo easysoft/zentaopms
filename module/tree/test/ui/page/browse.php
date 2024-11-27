d393f4beec1c40f966920a619614b3d7a1e60ce4
yulujie <yulujie@easycorp.ltd>
2024-11-27 16:01:55 +0800

    * [task#133022,doing,0.3h,2h] Add xpath.

--- module/tree/test/ui/page/browse.php
+++ module/tree/test/ui/page/browse.php
@@ -16,11 +16,12 @@ class browsePage extends page
             'firstChildModule' => "//*[@id='treeEditor-tree-browse']/li/menu/li[1]/div/div/a",
             'lastChildModule'  => "(//*[@id='treeEditor-tree-browse']/li/menu/li)[last()]/div/div/a",
             'firstEditBtn'     => "//*[@id='treeEditor-tree-browse']/li[1]/div/nav/a[1]",
-            'firstDelBtn'      => "//*[@id='treeEditor-tree-browse']/li[1]/div/nav/a[2]",
+            'firstChildDelBtn' => "//*[@id='treeEditor-tree-browse']/li[1]/menu/li[1]/div/nav/a[2]",
             'firstViewBtn'     => "//*[@id='treeEditor-tree-browse']/li[1]/div/nav/a[3]",
             'firstCaret'       => "//*[@id='treeEditor-tree-browse']/li/div/span/span",
 
-            'modalText' => "//div[@class='modal-body']/div",
+            'modalText' => "//div[contains(@class,'modal-body')]//div[text()!='']",
+            'modalConfirm' => "//button[@z-key='confirm']",
             /* 编辑弹窗中元素 */
             'editSubmitBtn' => "//*[@id='zin_tree_edit_form']//button[@type='submit']"
         );
