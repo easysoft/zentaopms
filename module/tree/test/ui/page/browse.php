37567890e2b0e917aca968a36b250c851bf62422
yulujie <yulujie@easycorp.ltd>
2024-11-27 16:39:42 +0800

    * [task#133022,done,0.5h] Add xpath.

--- module/tree/test/ui/page/browse.php
+++ module/tree/test/ui/page/browse.php
@@ -5,12 +5,11 @@ class browsePage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'firsrNullModule'  => "(//*[@name='modules[]'])[1]",
-            'secondNullModule' => "(//*[@name='modules[]'])[2]",
-            'firstAddBtn'      => "//*[@id='zin_tree_browse_form']/div[2]/div/div[2]/div/button[1]",
-            'firstRemoveBtn'   => "//*[@id='zin_tree_browse_form']/div[2]/div/div[2]/div/button[2]",
-            'submitBtn'        => "//*[@id='zin_tree_browse_form']//button[@type='submit']",
-
+            /* 左侧区块中元素 */
+            'firsrNullModule' => "(//*[@name='modules[]'])[1]",
+            'copyIcon'        => "//button[@id='copyModule']",
+            'submitBtn'       => "//*[@id='zin_tree_browse_form']//button[@type='submit']",
+            /* 右侧区块中元素 */
             'firstModule'      => "//*[@id='treeEditor-tree-browse']/li[1]/div/div/a",
             'lastModule'       => "(//*[@id='treeEditor-tree-browse']/li)[last()]/div/div/a",
             'firstChildModule' => "//*[@id='treeEditor-tree-browse']/li/menu/li[1]/div/div/a",
@@ -19,8 +18,8 @@ class browsePage extends page
             'firstChildDelBtn' => "//*[@id='treeEditor-tree-browse']/li[1]/menu/li[1]/div/nav/a[2]",
             'firstViewBtn'     => "//*[@id='treeEditor-tree-browse']/li[1]/div/nav/a[3]",
             'firstCaret'       => "//*[@id='treeEditor-tree-browse']/li/div/span/span",
-
-            'modalText' => "//div[contains(@class,'modal-body')]//div[text()!='']",
+            /* 确认弹窗中元素 */
+            'modalText'    => "//div[contains(@class,'modal-body')]//div[text()!='']",
             'modalConfirm' => "//button[@z-key='confirm']",
             /* 编辑弹窗中元素 */
             'editSubmitBtn' => "//*[@id='zin_tree_edit_form']//button[@type='submit']"
