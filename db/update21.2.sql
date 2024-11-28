b458139ce4a0157f1cdfbf94668b849515bee663
liumengyi <liumengyi@easycorp.ltd>
2024-11-28 16:28:39 +0800

    * [story#69524,1h] add sql.

--- db/update21.2.sql
+++ db/update21.2.sql
@@ -1,1 +1,6 @@
 ALTER TABLE `zt_workflowfield` MODIFY `placeholder` VARCHAR(255) NOT NULL DEFAULT '';
+
+ALTER TABLE zt_dataview MODIFY `fields` text NULL;
+ALTER TABLE zt_dataview MODIFY `objects` text NULL;
+ALTER TABLE zt_dataview MODIFY `mode` varchar(50) NOT NULL DEFAULT 'builder';
+ALTER TABLE zt_dataview ADD `driver` enum('mysql','duckdb') NOT NULL DEFAULT 'mysql' AFTER `code`;
