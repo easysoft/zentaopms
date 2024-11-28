3ba9cf04ade7527522fcc39f78885ef74345c4c5
liyang <liyang@easycorp.ltd>
2024-11-28 14:51:09 +0800

    * [bug#57552] fix bug for edit system.

--- module/system/model.php
+++ module/system/model.php
@@ -116,7 +116,6 @@ class systemModel extends model
      */
     public function update(int $id, object $formData, bool $checkRequired = true): bool
     {
-        $oldSystem = $this->fetchById($id);
         $this->dao->update(TABLE_SYSTEM)->data($formData)
             ->check('name', 'unique', '`id` != ' . $id)
             ->autoCheck()
@@ -124,9 +123,8 @@ class systemModel extends model
             ->where('id')->eq($id)
             ->exec();
         if(dao::isError()) return false;
-        $change = common::createChanges($oldSystem, $formData);
 
-        if(!empty($change))$this->loadModel('action')->create('system', $id, 'edited');
+        $this->loadModel('action')->create('system', $id, 'edited');
         return !dao::isError();
     }
 
