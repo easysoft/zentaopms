de8c995293a4ea9b49b64c86f7f612499cdff767
dingyongliang <dingyongliang@easycorp.ltd>
2024-11-29 10:39:47 +0800

    * [bug#57406] fix cant get project element.

--- module/doc/js/setdocbasic.ui.js
+++ module/doc/js/setdocbasic.ui.js
@@ -1,32 +1,30 @@
-window.loadExecutions = function(e)
+window.loadExecutions = function()
 {
-    const officeTypes      = $('#setDocBasicForm').data('officeTypes');
-    const docType          = $('#setDocBasicForm').data('docType');
-    const isOfficeType     = officeTypes.includes(docType);
-    const projectElement   = isOfficeType ? '.projectBox input[name="project"]': '#setDocBasicForm input[name="project"]';
-    const executionElement = isOfficeType ? '.executionBox input[name="execution"]': '#setDocBasicForm input[name="execution"]';
-    const projectID        = $(projectElement).val();
-    if($(executionElement))
+    const $project   = $('#setDocBasicForm input[name="project"]');
+    const $execution = $('#setDocBasicForm input[name="execution"]');
+    const projectID  = $project.val();
+
+    if($execution.length)
     {
-        const executionID = $(executionElement).val();
-        const link        = $.createLink('project', 'ajaxGetExecutions', "projectID=" + projectID + "&mode=multiple,leaf,noprefix");
+        const executionID = $execution.val();
+        const link        = $.createLink('project', 'ajaxGetExecutions', `projectID=${projectID}&mode=multiple,leaf,noprefix`);
         $.getJSON(link, function(data)
         {
-            let $picker = $(executionElement).zui('picker');
-            $picker.render({items: data.items, disabled: !data.multiple});
-            $picker.$.setValue(executionID);
+            const picker = $(executionElement).zui('picker');
+            picker.render({items: data.items, disabled: !data.multiple});
+            picker.$.setValue(executionID);
         });
     }
 
-    const link = $.createLink('doc', 'ajaxGetModules', 'objectType=project&objectID=' + projectID + '&type=doc');
+    const link = $.createLink('doc', 'ajaxGetModules', `objectType=project&objectID=${projectID}&type=doc`);
     $.getJSON(link, function(data)
     {
-        const $libPicker = $(`${isOfficeType ? '' : '#setDocBasicForm'} [name='lib']`).zui('picker');
-        $libPicker.render({items: data.libs});
-        $libPicker.$.setValue('');
+        const libPicker = $(`#setDocBasicForm [name="lib"]`).zui('picker');
+        libPicker.render({items: data.libs});
+        libPicker.$.setValue('');
 
-        const $modulePicker = $(`${isOfficeType ? '' : '#setDocBasicForm'} [name='module']`).zui('picker');
-        $modulePicker.render({items: data.modules});
-        $modulePicker.$.setValue('');
+        const modulePicker = $(`#setDocBasicForm [name="module"]`).zui('picker');
+        modulePicker.render({items: data.modules});
+        modulePicker.$.setValue('');
     });
 }
