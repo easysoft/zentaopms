9142a8e43bbe92d7e342c841461ea8f3cbed18a7
zhangxiquan <zhangxiquan@chandao.com>
2024-11-30 03:45:42 +0000

    * [misc] Common checkiframe unit testing.

--- module/common/test/model/checkiframe.php
+++ module/common/test/model/checkiframe.php
@@ -8,9 +8,11 @@ title=测试 commonModel::checkIframe();
 timeout=0
 cid=1
 
-- 查看任务一和任务二的描述差异长度 @77
-- 查看返回的差异中<ins>标签的位置 @44
-- 查看返回的差异中<del>标签的位置 @5
+- 查看任务一和任务二的描述差异长度 @1
+- 查看返回的差异中<ins>标签的位置 查看返回的差异中<del>标签的位置 @1
+- 查看实例的终端页面是否要在iframe中打开 @1
+- 查看实例的日志页面是否要在iframe中打开 @1
+- 查看实例的事件页面是否要在iframe中打开 @1
 
 */
 
@@ -31,4 +33,8 @@ r($tester->common->checkIframe()) && p() && e('1');
 
 $app->moduleName = 'instance';
 $app->methodName = 'logs';
+r($tester->common->checkIframe()) && p() && e('1');
+
+$app->moduleName = 'instance';
+$app->methodName = 'events';
 r($tester->common->checkIframe()) && p() && e('1');
