ce0c9dcaa8b21bf0be2762fbbf5fdb44181cb77a
tianshujie <tianshujie@easycorp.ltd>
2024-12-03 14:41:33 +0800

    * [task#133225,doing,1h] add taskassignedto control.

--- lib/zin/wg/taskassignedto/v1.php
+++ lib/zin/wg/taskassignedto/v1.php
@@ -26,4 +26,34 @@ class taskAssignedTo extends wg
     {
         return file_get_contents(__DIR__ . DS . 'css' . DS . 'v1.css');
     }
+
+    protected function build()
+    {
+        global $app, $lang;
+        $app->loadLang('execution');
+
+        $pickerToolbar = false;
+        $manageLink    = $this->prop('manageLink');
+        if($this->prop('multiple') || $manageLink)
+        {
+            $pickerToolbar = array();
+            if($this->prop('multiple'))
+            {
+                $pickerToolbar[] = array('key' => 'selectAll', 'text' => $lang->selectAll);
+                $pickerToolbar[] = array('key' => 'cancelSelect', 'text' => $lang->cancelSelect);
+
+                $this->setProp('menu', array('checkbox' => true));
+            }
+
+            if($manageLink) $pickerToolbar[] = array('type' => 'button', 'className' => 'text-primary manageTeamBtn', 'key' => 'manageTeam', 'text' => $lang->execution->manageTeamMember, 'icon' => 'plus-solid-circle', 'url' => $manageLink, 'data-toggle' => 'modal', 'data-size' => 'lg', 'data-dismiss' => 'pick');
+        }
+
+        $this->setProp('toolbar', $pickerToolbar);
+
+        return picker
+        (
+            setClass('taskAssignedToBox'),
+            set($this->props->pick(array('id', 'name', 'value', 'required', 'items', 'toolbar', 'menu', 'multiple')))
+        );
+    }
 }
