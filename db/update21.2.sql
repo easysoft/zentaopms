70723162c77fdbee4b27db9acaaa31376210d269
liumengyi <liumengyi@easycorp.ltd>
2024-12-03 13:15:14 +0800

    * [refac] change docContent->html to docContent->content.

--- db/update21.2.sql
+++ db/update21.2.sql
@@ -4,3 +4,6 @@ ALTER TABLE zt_dataview MODIFY `fields` text NULL;
 ALTER TABLE zt_dataview MODIFY `objects` text NULL;
 ALTER TABLE zt_dataview MODIFY `mode` varchar(50) NOT NULL DEFAULT 'builder';
 ALTER TABLE zt_dataview ADD `driver` enum('mysql','duckdb') NOT NULL DEFAULT 'mysql' AFTER `code`;
+
+ALTER TABLE `zt_doccontent` ADD `html` longtext DEFAULT NULL AFTER `content`;
+ALTER TABLE `zt_doccontent` ADD `rawContent` longtext DEFAULT NULL AFTER `content`;
