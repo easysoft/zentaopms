18e8985bb0f546320589d373837ffc18e9f87e28
sunhao <sunhao@easycorp.ltd>
2024-12-04 16:08:13 +0800

    * [bug #57643] fix image not show in pageEditor.

--- lib/zin/wg/pageeditor/v1.php
+++ lib/zin/wg/pageeditor/v1.php
@@ -14,6 +14,7 @@ class pageEditor extends wg
         'readonly?: bool=false',                // 是否只读
         'locale?: string',                      // 语言，可选值 'zh', 'en'，默认跟随浏览器，也可以是自定义的语言项 JSON，详见 ZenEditor 文档
         'value?: string',                       // 初始内容
+        'downloadUrl?: string',                 // 文件下载链接
         'uid?: string'                          // 图片上传 uid
     );
 
@@ -76,6 +77,9 @@ class pageEditor extends wg
             );
         }
 
+        $downloadUrl = $this->prop('downloadUrl');
+        if(is_null($downloadUrl)) $downloadUrl = createLink('file', 'ajaxQuery', 'fileID=0&objectType=doc&objectID=0&title={title}&extra={extra}&stream=0');
+
         return div
         (
             setClass('editor-container p-px mt-px rounded relative w-full no-morph', $readonly ? 'is-readonly' : ''),
@@ -87,6 +91,7 @@ class pageEditor extends wg
                 set::name($name),
                 set::content($value),
                 set::readonly($readonly),
+                set::downloadUrl($downloadUrl),
                 set($this->getRestProps())
             ),
             input
