7e3b066cf26e6878b0b32bb7cf03fe7272690efb
yulujie <yulujie@easycorp.ltd>
2024-12-04 17:06:29 +0800

    * [task#134046,doing,0.5h,4,5h] Add create function.

--- module/tree/test/lib/browsetask.ui.class.php
+++ module/tree/test/lib/browsetask.ui.class.php
@@ -2,4 +2,34 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class browsetaskTester extends tester
 {
+    /**
+     * 检查执行下创建模块。
+     * Check the creation of modules under the execution.
+     *
+     * @param  string $moduleName
+     * @param  bool   $checkRepeat
+     * @access public
+     * @return object
+     */
+    public function createModule($moduleName, $checkRepeat = false)
+    {
+        $form = $this->initForm('tree', 'browsetask', array('rootID' => '2'), 'appIframe-execution');
+        if($checkRepeat) $moduleName = $form->dom->firstModule->getText();
+        $form->dom->firsrNullModule->setValue($moduleName);
+        $form->dom->submitBtn->click();
+        $form->wait(1);
+
+        if($checkRepeat)
+        {
+            if($form->dom->modalText->getText() == sprintf($this->lang->tree->repeatName, $moduleName)) return $this->success('创建模块时模块已存在，提示正确');
+            return $this->failed('创建模块时模块已存在，提示错误');
+        }
+        if(preg_match('/\s/', $moduleName))
+        {
+            if($form->dom->modalText->getText() == $this->lang->tree->shouldNotBlank) return $this->success('创建模块时模块名包含空格，提示正确');
+            return $this->failed('创建模块时模块名包含空格，提示错误');
+        }
+        if($form->dom->lastModule->getText() == $moduleName) return $this->success('创建模块成功');
+        return $this->failed('创建模块失败');
+    }
 }
