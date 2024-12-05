ce26d04f248f2027e00673e966ffc39ffb04a231
huairong <huairong@easycorp.ltd>
2024-12-05 10:16:31 +0800

    + [task#133982,doing,0.5h,2h] Add switchProduct function.

--- module/project/test/lib/testcase.ui.class.php
+++ module/project/test/lib/testcase.ui.class.php
@@ -21,4 +21,25 @@ class testcaseTester extends tester
         if ($actualNum == $expectNum) return $this->success($tab . '下显示用例数正确');
         return $this->failed($tab . '下显示用例数不正确');
     }
+
+    /**
+     * 切换产品查看用例数据。
+     * Switch product to view testcase data.
+     *
+     * @param  string $product firstProduct|secondProduct
+     * @param  string $expectNum
+     * @access public
+     * @return object
+     */
+    public function switchProduct($product, $expectNum)
+    {
+        $form = $this->initForm('project', 'testcase', array('project' => 1), 'appIframe-project');
+        $form->dom->dropMenu->click();
+        $form->dom->$product->click();
+        $form->wait(1);
+        $string      = $form->dom->testcaseNum->getText();
+        $testCaseNum = preg_replace('/\D/', '', $string); //从字符串中提取数字部分
+        if($testCaseNum == $expectNum) return $this->success('切换' . $product . '查看用例数据成功');
+        return $this->failed('切换' . $product . '查看用例数据失败');
+    }
 }
