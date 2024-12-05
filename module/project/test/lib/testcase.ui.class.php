79577f6f778389950bc2e19f039cc6556e3d56d4
huairong <huairong@easycorp.ltd>
2024-12-05 10:15:31 +0800

    + [task#133982,doing,0.5h,2.5h] Add checkTab function.

--- module/project/test/lib/testcase.ui.class.php
+++ module/project/test/lib/testcase.ui.class.php
@@ -11,4 +11,14 @@ class testcaseTester extends tester
      * @access public
      * @return object
      */
+    public function checkTab($tab, $expectNum)
+    {
+        $form = $this->initForm('project', 'testcase', array('project' => 1), 'appIframe-project');
+        $form->dom->$tab->click();
+        $form->wait(1);
+        $target = ($tab == "storyNoCaseTab") ? $form->dom->zerocaseNum : $form->dom->testcaseNum;
+        $actualNum = preg_replace('/\D/', '', $target->getText());
+        if ($actualNum == $expectNum) return $this->success($tab . '下显示用例数正确');
+        return $this->failed($tab . '下显示用例数不正确');
+    }
 }
