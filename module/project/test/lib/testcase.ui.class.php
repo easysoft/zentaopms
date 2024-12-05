ee06e8bc18dda65995095f2824095920998ba714
huairong <huairong@easycorp.ltd>
2024-12-05 10:14:35 +0800

    + [task#133982,doing,0.5h,2.5h] Add comment of checkTab function.

---
+++ module/project/test/lib/testcase.ui.class.php
@@ -0,0 +1,14 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+class testcaseTester extends tester
+{
+    /**
+     * 检查Tab标签下的数据。
+     * Check the data of Tab tag.
+     *
+     * @param  string $tab allTab|waitingTab|storyChangedTab|storyNoCaseTab
+     * @param  string $expectNum
+     * @access public
+     * @return object
+     */
+}
