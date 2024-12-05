fb92804f5e1dc7e72fcb6cd4585fe6355748639f
lihuifang <lihuifang@easycorp.ltd>
2024-12-05 08:56:52 +0800

    +[task#133875,doing,0.5h,2h] Add testdata.

--- module/projectrelease/test/ui/createprojectrelease.php
+++ module/projectrelease/test/ui/createprojectrelease.php
@@ -17,3 +17,20 @@ include '../lib/createprojectrelease.ui.class.php';
 
 zendata('project')->loadYaml('project', false, 1)->gen(1);
 zendata('product')->loadYaml('product', false, 1)->gen(1);
+zendata('projectproduct')->loadYaml('projectproduct', false, 1)->gen(1);
+
+$project = zenData('system');
+$project->id->range('1');
+$project->product->range('1');
+$project->name->range('应用AAA');
+$project->status->range('active');
+$project->createdBy->range('admin');
+$project->gen(1);
+
+$tester = new createProjectReleaseTester();
+$tester->login();
+
+//设置项目发布数据
+$release = array(
+    array('name' => '一个已发布的项目发布'.time(), 'status' => '已发布', 'plandate' => date('Y-m-d', strtotime('+1 day')), 'releasedate' => date('Y-m-d', strtotime('+1 month'))),
+    array('systemname' => '新建应用1', 'name' => '一个未开始的项目发布'.time(), 'status' => '未开始', 'plandate' => date('Y-m-d', strtotime('+1 day'))),
