54948be2fc9e4c25b4f5d792ca66bdac1b126b63
lihuifang <lihuifang@easycorp.ltd>
2024-12-05 09:11:31 +0800

    +[task#133875,doing,1.5h,1h] Add testui function.

--- module/projectrelease/test/lib/createprojectrelease.ui.class.php
+++ module/projectrelease/test/lib/createprojectrelease.ui.class.php
@@ -3,51 +3,38 @@ include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class createProjectReleaseTester extends tester
 {
     /**
-     * Check the page input when creating the project release.
-     * 创建项目发布时检查页面输入
+     * Check required input when creating the project release.
+     * 创建项目发布时检查必填项
      *
-     * @param  array $release
      * @access public
      */
-    public function checkInput($release)
+    public function checkRequired()
     {
         $form = $this->initForm('projectrelease', 'create', array('projectID' => 1), 'appIframe-project');
-        if(isset($release['name']))   $form->dom->name->setValue($release['name']);
-        if(isset($release['status'])) $form->dom->status->picker($release['status']);
-        $form->wait(2);
-        if(isset($release['plandate']))    $form->dom->date->datepicker($release['plandate']);
-        if(isset($release['releasedate'])) $form->dom->releasedDate->datepicker($release['releasedate']);
-
+        $form->dom->newSystem->click();
         $form->dom->btn($this->lang->save)->click();
-        $form->wait(5);
-        return $this->checkResult($release);
+        $form->wait(2);
+
+        /* 创建发布页面应用不能为空和应用版本号不能为空的提示 */
+        $systemTipForm = $form->dom->systemNameTip->getText();
+        $nameTipForm   = $form->dom->nameTip->getText();
+        $systemTip     = sprintf($this->lang->error->notempty, $this->lang->product->system);
+        $nameTip       = sprintf($this->lang->error->notempty, $this->lang->product->system . '版本号');
+
+        /* 断言检查必填提示信息 */
+        if($systemTipForm == $systemTip and $nameTipForm == $nameTip) return $this->success('创建项目发布表单页必填提示信息正确');
+        return $this->failed('创建项目发布表单页必填提示信息不正确');
     }
 
     /**
-     * Check the result after creating the project release.
-     * 创建项目发布后检查结果
+     * Create the project release.
+     * 创建项目发布
      *
      * @param  array $release
      * @access public
-     * @return object
      */
-    public function checkResult($release)
+    public function createProjectRelease($release)
     {
-        //检查创建页面时的提示信息
-        if($this->response('method') != 'view')
-        {
-            if($this->checkFormTips('projectrelease')) return $this->success('创建项目发布表单页提示信息正确');
-            return $this->failed('创建项目发布表单页提示信息不正确');
-        }
-        /* 跳转到发布概况页面，点击基本信息标签，查看信息是否正确 */
-        $viewPage = $this->loadPage('projectrelease', 'view');
-        $viewPage->dom->basic->click();
-        $viewPage->wait(5);
+        $form = $this->initForm('projectrelease', 'create', array('projectID' => 1), 'appIframe-project');
 
-        //断言检查发布名称、状态是否正确
-        if($viewPage->dom->basicreleasename->getText() != $release['name']) return $this->failed('项目发布名称错误');
-        if($viewPage->dom->basicstatus->getText() != $release['status'])    return $this->failed('项目发布状态错误');
-
-        return $this->success();
-    }
-}
+        /* 如果用例中设置了应用名称，就勾选新建应用，填写已设置的应用名称。没有设置应用名称，就自动使用数据库中的应用 */
