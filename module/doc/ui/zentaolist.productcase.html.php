a0bc62f42c4d0aa6a9ed856f2eab101db12bd6d2
qixinzhi <qixinzhi@easycorp.ltd>
2024-12-05 05:17:56 +0000

    * [story#69800,0.5h] add previewProductCase.

---
+++ module/doc/ui/zentaolist.productcase.html.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace zin;
+
+$fnGenerateFormRows = function () use ($lang)
+{
+    $productList = $this->loadModel('product')->getPairs();
+    $this->app->loadLang('testcase');
+    $conditions = array_filter($lang->testcase->featureBar['browse'], fn($value) => $value !== '-');
+    $conditions['customSearch'] = $lang->doc->customSearch;
+    return array
+    (
+        formRow
+        (
+            formGroup
+            (
+                set::width('1/2'),
+                set::name('product'),
+                set::label($lang->doc->product),
+                set::required(),
+                set::control(array('contorl' => 'picker', 'required' => false, 'maxItemsCount' => 50)),
+                set::items($productList),
+                span
+                (
+                    setClass('error-tip text-danger hidden'),
+                    $lang->doc->emptyError
+                )
+            )
+        ),
+        formRow
+        (
+            formGroup
+            (
+                set::width('1/2'),
+                set::name('condition'),
+                set::label($lang->doc->searchCondition),
+                set::required(),
+                set::control(array('contorl' => 'picker', 'required' => false, 'maxItemsCount' => 50)),
+                set::items($conditions),
+                span
+                (
+                    setClass('error-tip text-danger hidden'),
+                    $lang->doc->emptyError
+                )
+            )
+        )
+    );
+};
