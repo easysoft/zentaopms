b93816c22e0218046f32ef2e921e9e57aa68538b
huairong <huairong@easycorp.ltd>
2024-12-05 10:19:34 +0800

    + [task#133982,doing,0.5h,1.5h] Add element of testcase browse.

--- module/project/test/ui/page/testcase.php
+++ module/project/test/ui/page/testcase.php
@@ -12,6 +12,10 @@ class testcasePage extends page
             'storyNoCaseTab'  => "//*[@id='featureBar']/menu/li[5]/a",
             'testcaseNum'     => "//*[@id='table-project-testcase']/div[3]/nav/div[1]",
             'zerocaseNum'     => "//*[@id='table-testcase-zerocase']/div[3]/nav/div[1]",
+            /* 1.5级产品导航 */
+            'dropMenu'        => "//*[@id='pick-project-menu']",
+            'firstProduct'    => "//*[@id='pick-pop-project-menu']/div[2]/div/div[1]/menu/li[2]/div",
+            'secondProduct'   => "//*[@id='pick-pop-project-menu']/div[2]/div/div[1]/menu/li[3]/div",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
