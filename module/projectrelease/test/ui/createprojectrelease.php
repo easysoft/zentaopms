aa5590d60b2355cd44a1f170391f1d2238bf8211
lihuifang <lihuifang@easycorp.ltd>
2024-12-05 08:55:51 +0800

    +[task#133875,doing,0.3h,2.2h] Add ztf notes.

--- module/projectrelease/test/ui/createprojectrelease.php
+++ module/projectrelease/test/ui/createprojectrelease.php
@@ -7,9 +7,9 @@ title=创建项目发布
 timeout=0
 cid=73
 
-- 缺少发布名称，检查提示信息测试结果 @创建项目发布表单页提示信息正确
-- 创建状态为未开始的发布最终测试状态 @SUCCESS
-- 创建状态为已发布的发布最终测试状态 @SUCCESS
+- 创建项目发布时检查必填校验测试结果 @创建项目发布表单页必填提示信息正确
+- 创建一个已发布的发布，且选择已有应用测试结果 @创建项目发布成功
+- 创建一个未开始的发布，且勾选新建应用测试结果 @创建项目发布成功
 
 */
 chdir(__DIR__);
@@ -17,20 +17,3 @@ include '../lib/createprojectrelease.ui.class.php';
 
 zendata('project')->loadYaml('project', false, 1)->gen(1);
 zendata('product')->loadYaml('product', false, 1)->gen(1);
-zendata('projectproduct')->loadYaml('projectproduct', false, 1)->gen(1);
-
-$tester = new createProjectReleaseTester();
-$tester->login();
-
-//设置项目发布数据
-$release = array(
-    array('name' => ''),
-    array('name' => '一个未开始的项目发布'.time(), 'status' => '未开始', 'plandate' => date('Y-m-d', strtotime('+1 day'))),
-    array('name' => '一个已发布的项目发布'.time(), 'status' => '已发布', 'plandate' => date('Y-m-d', strtotime('+1 day')), 'releasedate' => date('Y-m-d', strtotime('+1 month'))),
-);
-
-r($tester->checkInput($release['0'],$project)) && p('message') && e('创建项目发布表单页提示信息正确');   // 缺少发布名称，检查提示信息
-r($tester->checkInput($release['1'],$project)) && p('status')  && e('SUCCESS');                          // 创建状态为未开始的发布
-r($tester->checkInput($release['2'],$project)) && p('status')  && e('SUCCESS');                          // 创建状态为已发布的发布
-
-$tester->closeBrowser();
