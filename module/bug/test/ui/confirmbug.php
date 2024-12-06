ece7fd2165db01af3cd67cf42bc5443f6e652177
luxuyang <luxuyang@easycorp.ltd>
2024-12-06 17:04:57 +0800

    * [task#134061] modify confirm bug ui test script.

--- module/bug/test/ui/confirmbug.php
+++ module/bug/test/ui/confirmbug.php
@@ -2,6 +2,33 @@
 <?php
 chdir(__DIR__);
 include '../lib/confirmbug.ui.class.php';
+
+/**
+
+title=确认bug
+timeout=0
+cid=1
+
+- 验证确认bug测试结果 @确认bug成功
+最终测试状态 @确认bug成功
+
+*/
+
+zenData('product')->loadYaml('product')->gen(1);
+$story = zenData('story');
+$story->id->setFields(array(array('range' => '2')));
+$story->version->setFields(array(array('range' => '1')));
+$story->gen(1);
+$bug = zenData('bug');
+$bug->status->setFields(array(array('range' => 'active')));
+$bug->module->setFields(array(array('range' => '0')));
+$bug->execution->setFields(array(array('range' => '0')));
+$bug->plan->setFields(array(array('range' => '0')));
+$bug->story->setFields(array(array('range' => '0')));
+$bug->storyVersion->setFields(array(array('range' => '0')));
+$bug->resolvedBuild->setFields(array(array('range' => 'trunk')));
+$bug->testtask->setFields(array(array('range' => '0')));
+$bug->gen(1);
 $tester = new confirmBugTester();
 
 $bug = array();
@@ -11,7 +38,7 @@ $bug['type']       = '其他';
 $bug['pri']        = '2';
 $bug['deadline']   = '2027-02-15';
 $bug['mailto']     = array('multiPicker' => 'admin');
-$project = array();
-$project['productID'] = 1;
+$product = array();
+$product['productID'] = 1;
 
-r($tester->confirmBug($project, $bug)) && p('message,status') && e('确认bug成功'); //验证bug表单页必填项校验
+r($tester->confirmBug($product, $bug)) && p('message,status') && e('确认bug成功'); //验证确认bug
