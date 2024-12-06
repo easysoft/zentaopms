9a012aedd48e312ca1499e5ca64d3f53c27a1d04
daitingting <daitingting@xirangit.com>
2024-12-06 08:39:10 +0000

    * [task#133869,doing,0.5h] Use common story ui.

--- module/doc/ui/zentaolist.productstory.html.php
+++ module/doc/ui/zentaolist.productstory.html.php
@@ -1,27 +1,2 @@
 <?php
-declare(strict_types=1);
-
-namespace zin;
-
-$fnGenerateFormRows = function () use ($lang, $settings)
-{
-    $products = $this->loadModel('product')->getPairs();
-    $searchConditions = array();
-    foreach($lang->product->featureBar['browse'] as $key => $label)
-    {
-        if(strpos($key, 'byme') !== false || strpos($key, 'tome') !== false) continue;
-        if($key == 'more')
-        {
-            foreach($lang->product->moreSelects['browse']['more'] as $moreKey => $moreLabel)
-            {
-                if(strpos($moreKey, 'byme') !== false || strpos($moreKey, 'tome') !== false) continue;
-
-                $searchConditions[$moreKey] = $moreLabel;
-            }
-        }
-        else
-        {
-            $searchConditions[$key] = $label;
-        }
-    }
-};
+include 'zentaolist.story.html.php';
