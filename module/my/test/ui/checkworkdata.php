07bd53129f0b41f8ea1a7fdb21d30e2da8c4bbd1
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-12-06 16:13:55 +0800

    *[task#133114,doing,0.3h,3h] add project data of work list

--- module/my/test/ui/checkworkdata.php
+++ module/my/test/ui/checkworkdata.php
@@ -17,4 +17,25 @@ $product->PO->range('admin');
 $product->status->range('normal');
 $product->type->range('normal');
 $product->gen(1);
+
+$project = zenData('project');
+$project->id->range('1-2');
+$project->project->range('0,1');
+$project->model->range('scrum,[]');
+$project->type->range('project,sprint');
+$project->parent->range('0,1');
+$project->auth->range('extend');
+$project->storytype->range('story');
+$project->path->range('`,1,`,`,1,2,`');
+$project->grade->range('1');
+$project->name->range('项目1,迭代1');
+$project->hasProduct->range('1');
+$project->status->range('doing');
+$project->acl->range('open');
+$project->gen(2);
+
+$projectProduct = zenData('projectproduct');
+$projectProduct->project->range('1-2');
+$projectProduct->product->range('1');
+$projectProduct->gen(2);
 $tester->closeBrowser();
