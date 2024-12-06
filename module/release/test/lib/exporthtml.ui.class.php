27eff6640ab639aef74493bfe244b9e6a3619f2d
lihuifang <lihuifang@easycorp.ltd>
2024-12-06 16:35:04 +0800

    *[task#133877,doing,1.7h,0.2h] Add testui function.

--- module/release/test/lib/exporthtml.ui.class.php
+++ module/release/test/lib/exporthtml.ui.class.php
@@ -17,3 +17,20 @@ class exportHtmlTester extends tester
         if(isset($release['exportdata'])) $form->dom->exportData->picker($release['exportdata']);
 
         $form->dom->exportBtnAlert->click();
+        $form->wait(2);
+
+        if($form->dom->exportBtnAlert) return $this->failed('发布导出失败');
+        return $this->success('发布导出成功');
+    }
+
+    /**
+     * 发布导出时文件名为空时的必填校验检查。
+     * Check required tips of release export.
+     *
+     * @access public
+     * @return object
+     */
+    public function exportWithNoFilename()
+    {
+        $form = $this->initForm('release', 'view', array('releaseID' => 1), 'appIframe-product');
+        $form->dom->exportBtn->click();
