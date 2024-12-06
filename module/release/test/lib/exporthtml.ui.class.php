3003c28e9bb3d8ba379f0638454d4c8f2626759e
lihuifang <lihuifang@easycorp.ltd>
2024-12-06 16:35:22 +0800

    *[task#133877,done,2h,0h] Add testui function.

--- module/release/test/lib/exporthtml.ui.class.php
+++ module/release/test/lib/exporthtml.ui.class.php
@@ -34,3 +34,11 @@ class exportHtmlTester extends tester
     {
         $form = $this->initForm('release', 'view', array('releaseID' => 1), 'appIframe-product');
         $form->dom->exportBtn->click();
+        $form->dom->exportBtnAlert->click();
+        $form->wait(2);
+
+        /*点击导出按钮后，检查必填校验*/
+        if($form->dom->alertModal('text') === '『文件名』不能为空。') return $this->success('发布导出必填提示信息正确');
+        return $this->failed('发布导出必填提示信息不正确');
+    }
+}
