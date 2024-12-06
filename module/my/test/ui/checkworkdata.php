866a41563670b437ee06a331cfb1b8b09a4a9477
lixiaoyu <lixiaoyu@easycorp.ltd>
2024-12-06 16:15:29 +0800

    *[task#133114,doing,0.3h,3h] add task data of work list

--- module/my/test/ui/checkworkdata.php
+++ module/my/test/ui/checkworkdata.php
@@ -38,4 +38,19 @@ $projectProduct = zenData('projectproduct');
 $projectProduct->project->range('1-2');
 $projectProduct->product->range('1');
 $projectProduct->gen(2);
+
+$task = zenData('task');
+$task->id->range('1-10');
+$task->parent->range('0');
+$task->project->range('1');
+$task->execution->range('2');
+$task->story->range('0');
+$task->name->range('任务1,任务2,任务3,任务4,任务5,任务6,任务7,任务8,任务9,任务10');
+$task->status->range('closed{2},cancel{1},wait{5},done{2}');
+$task->closedBy->range('admin{2},{8}');
+$task->canceledBy->range('null{2},admin{1},{7}');
+$task->assignedTo->range('closed{2},admin{4}');
+$task->finishedBy->range('admin{2},{8}');
+$task->gen(10);
+
 $tester->closeBrowser();
