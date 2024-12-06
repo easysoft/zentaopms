895e369bbae22b95c6186401643741930ac20d34
luxuyang <luxuyang@easycorp.ltd>
2024-12-06 15:03:29 +0800

    * [task#134062] modify close bug ui test script.

--- module/bug/test/ui/page/browse.php
+++ module/bug/test/ui/page/browse.php
@@ -19,7 +19,9 @@ class browsePage extends page
             'bugCount'      => "//div[@id='mainContainer']//tbody/tr",
             'save'          => "//button[@type='button']/span[text()]/../preceding-sibling::button[@type='submit']/span[text()]",
             'bugID'         => "//div[@data-col='id' and @data-type='checkID']/div[text()]",
-            'bugTitle'      => "//div[@data-col='title' and @data-type='title']/div/a[text() and @data-app]"
+            'bugTitle'      => "//div[@data-col='title' and @data-type='title']/div/a[text() and @data-app]",
+            'bugStatus'     => "//div[@data-col='status' and @data-type='status']/div/span[text()]",
+            'closeComment'  => "//zen-editor[@id='comment']",
         );
 
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
