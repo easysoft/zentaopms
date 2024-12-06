4027beded5c6d454ddc423585aed8aff1a353f0d
qixinzhi <qixinzhi@easycorp.ltd>
2024-12-06 05:22:50 +0000

    * [story#69800,0.5h] add value.

--- module/doc/ui/zentaolist.planstory.html.php
+++ module/doc/ui/zentaolist.planstory.html.php
@@ -3,9 +3,10 @@ declare(strict_types=1);
 
 namespace zin;
 
-$fnGenerateFormRows = function () use ($lang)
+$fnGenerateFormRows = function () use ($lang, $settings)
 {
     $productList = $this->loadModel('product')->getPairs();
+    $planList    = isset($settings['product']) ? $this->loadModel('productplan')->getPairs((int)$settings['product']) : array();
     return array
     (
         formRow
@@ -18,6 +19,7 @@ $fnGenerateFormRows = function () use ($lang)
                 set::required(),
                 set::control(array('contorl' => 'picker', 'required' => false, 'maxItemsCount' => 50)),
                 set::items($productList),
+                set::value(isset($settings['product']) ? $settings['product'] : ''),
                 span
                 (
                     setClass('error-tip text-danger hidden'),
@@ -34,7 +36,8 @@ $fnGenerateFormRows = function () use ($lang)
                 set::label($lang->doc->plan),
                 set::required(),
                 set::control(array('contorl' => 'picker', 'required' => false, 'maxItemsCount' => 50)),
-                set::items(array()),
+                set::items($planList),
+                set::value(isset($settings['plan']) ? $settings['plan'] : ''),
                 span
                 (
                     setClass('error-tip text-danger hidden'),
