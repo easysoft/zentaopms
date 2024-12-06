fb7f996eff34cfafaca99072c315ca24dce8224e
caoyanyi <caoyanyi@easycorp.ltd>
2024-12-06 11:14:19 +0800

    + [task#134002,doing,0.3h] add config for release tutorial.

--- module/tutorial/config/productManage.php
+++ module/tutorial/config/productManage.php
@@ -368,6 +368,31 @@ $productManage->basic->tasks['releaseManage']['steps'][] = array(
 
 $productManage->basic->tasks['releaseManage']['steps'][] = array(
     'type'   => 'click',
+    'target' => '#actionBar a:first-child',
+    'page'   => 'release-browse',
+    'url'    => array('release', 'browse', 'productID=1'),
+    'title'  => $lang->tutorial->productManage->releaseManage->step20->name,
+    'desc'   => $lang->tutorial->productManage->releaseManage->step20->desc
+);
+
+$productManage->basic->tasks['releaseManage']['steps'][] = array(
+    'type'   => 'click',
+    'target' => '#actionBar a',
+    'page'   => 'system-browse',
+    'url'    => array('system', 'browse', 'productID=1'),
+    'title'  => $lang->tutorial->productManage->releaseManage->step21->name,
+    'desc'   => $lang->tutorial->productManage->releaseManage->step21->desc
+);
+
+$productManage->basic->tasks['releaseManage']['steps'][] = array(
+    'type'   => 'form',
+    'target' => '#systemCreateForm',
+    'page'   => 'system-browse',
+    'title'  => $lang->tutorial->productManage->releaseManage->step22->name
+);
+
+$productManage->basic->tasks['releaseManage']['steps'][] = array(
+    'type'   => 'click',
     'target' => '#actionBar a:last-child',
     'page'   => 'release-browse',
     'url'    => array('release', 'browse', 'productID=1'),
