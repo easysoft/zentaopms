be55ec2aa8760f97e0dab1b6d09d1e2540097f3c
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2024-12-09 13:16:05 +0800

    * [task#134113,doing,0.1h] Add ui test files of check todo tabs

--- module/my/test/ui/page/todo.php
+++ module/my/test/ui/page/todo.php
@@ -8,6 +8,7 @@ class todoPage extends page
             'addTodo'         => '/html/body/div/div/div[1]/div[2]/div/a',
             'addTodoBtn'      => '/html/body/div[2]/div/div/div[2]/div/div[2]/form/div[15]/div/button',
             'fstTodoTitle'    => '/html/body/div/div/div[2]/div/div/div[2]/div[1]/div/div[2]/div/a',
+            'secTodoTitle'    => '/html/body/div/div/div[2]/div/div/div[2]/div[1]/div/div[4]/div/a',
             'editTodoBtn'     => '/html/body/div[3]/div/div/div[2]/div/div/form/div[9]/div/button',
             'fstTodoStatus'   => '/html/body/div[1]/div/div[2]/div/div/div[2]/div[2]/div/div[5]/div/span',
             'fstTodoEdit'     => '/html/body/div[1]/div/div[2]/div/div/div[2]/div[3]/div/div[1]/div/nav/a[3]/i',
