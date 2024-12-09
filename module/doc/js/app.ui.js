31e3efdd9f4961f822c91f2b690dfd7b36076d61
zhouxin <zhouxin@easycorp.ltd>
2024-12-09 14:24:56 +0800

    * [task#133857,done,0.5h] add btn to apply template.

--- module/doc/js/app.ui.js
+++ module/doc/js/app.ui.js
@@ -1,5 +1,19 @@
 $.extend(window.docAppActions,
 {
+    'doc-edit': function(info)
+    {
+        const doc = info.data;
+        if(!doc) return;
+
+        const lang = getLang();
+        return [
+            {text: lang.template, size: 'md', className: 'btn-wide', type: 'primary-outline', icon: 'save', command: 'selectTemplate'},
+            doc.status === 'draft' ? {text: lang.saveDraft, size: 'md', className: 'btn-wide', type: 'secondary', command: 'saveDoc/draft'} : null,
+            {text: lang.release, size: 'md', className: 'btn-wide', type: 'primary', command: 'saveDoc'},
+            {text: lang.cancel, size: 'md', className: 'btn-wide', type: 'primary-outline', command: 'cancelEditDoc'},
+            {text: lang.settings, size: 'md', type: 'ghost', command: `showDocSettingModal/${doc.id}/${doc.contentType}/1`, icon: 'cog-outline'},
+        ];
+    },
     'doc-create': function(info)
     {
         const doc = info.data;
@@ -7,7 +21,7 @@ $.extend(window.docAppActions,
 
         const lang = getLang();
         return [
-            {text: lang.template, size: 'md', className: 'btn-wide', type: 'secondary', command: 'selectTemplate'},
+            {text: lang.template, size: 'md', className: 'btn-wide', type: 'primary-outline', icon: 'save', command: 'selectTemplate'},
             {text: lang.saveDraft, size: 'md', className: 'btn-wide', type: 'secondary', command: 'saveNewDoc/draft'},
             {text: lang.release, size: 'md', className: 'btn-wide', type: 'primary', command: 'saveNewDoc'},
             {text: lang.cancel, size: 'md', className: 'btn-wide', type: 'primary-outline', command: 'cancelCreateDoc'},
