0c0d7b6138cebd01ec493193e0bcea57cb689c3f
qixinzhi <qixinzhi@easycorp.ltd>
2024-12-09 08:55:39 +0000

    * [story#69800,0.5h] finish search form.

--- module/doc/ui/zentaolist.customsearch.html.php
+++ module/doc/ui/zentaolist.customsearch.html.php
@@ -2,19 +2,36 @@
 declare(strict_types=1);
 namespace zin;
 
-$fnGenerateCustomSearchItem = function ($index, $form) use ($lang, $config)
+$fnGenerateCustomSearchItem = function ($index, $searchConfig, $form) use ($lang, $config)
 {
     list($andor, $field, $operator, $value) = $form;
     $isFirst = $index === 0;
+
+    $searchFields = $searchConfig['fields'];
+    $searchParams = $searchConfig['params'];
+
+    $fields     = array_keys($searchFields);
+    $field      = empty($field) ? reset($fields) : $field;
+    $operator   = empty($operator) ? $searchParams[$field]['operator'] : $operator;
+    $control    = $searchParams[$field]['control'];
+    $valueItems = $searchParams[$field]['values'];
+
+    $isPicker = false;
+    if($control === 'select')
+    {
+        $isPicker = true;
+        $control  = 'picker';
+    }
+
     return formRow
     (
         setClass('flex gap-x-2'),
         setID('row' . $index),
         formGroup
         (
-            setClass(array('hidden' => $isFirst)),
-            set::width('22'),
             setID('andor' . $index),
+            setClass(array('search-andor', 'hidden' => $isFirst)),
+            set::width('22'),
             set::name('andor[]'),
             set::control(array('required' => true)),
             set::items($lang->search->andor),
@@ -22,17 +39,20 @@ $fnGenerateCustomSearchItem = function ($index, $form) use ($lang, $config)
         ),
         formGroup
         (
+            setID('field' . $index),
+            setClass('search-field'),
             set::width($isFirst ? '56' : '32'),
             set::label($isFirst ? $lang->doc->customSearch : null),
-            setID('field' . $index),
             set::name("field[]"),
-            set::items($config->product->search['fields']),
+            set::control(array('required' => true)),
+            set::items($searchFields),
             set::value($field)
         ),
         formGroup
         (
-            set::width('20'),
             setID('operator' . $index),
+            setClass('search-operator'),
+            set::width('20'),
             set::name("operator[]"),
             set::control(array('required' => true)),
             set::items($lang->search->operators),
@@ -40,10 +60,13 @@ $fnGenerateCustomSearchItem = function ($index, $form) use ($lang, $config)
         ),
         formGroup
         (
+            setID('value' . $index . $control),
+            setClass('search-value'),
             set::width('60'),
-            setID('value' . $index),
             set::name("value[]"),
-            set::value($value)
+            set::control(array('type' => $control)),
+            set::value($value),
+            $isPicker ? set::items($valueItems) : null
         ),
         btnGroup
         (
@@ -53,7 +76,7 @@ $fnGenerateCustomSearchItem = function ($index, $form) use ($lang, $config)
     );
 };
 
-$fnGenerateCustomSearch = function () use ($lang, $config, $settings, $fnGenerateCustomSearchItem)
+$fnGenerateCustomSearch = function ($searchConfig) use ($lang, $config, $settings, $fnGenerateCustomSearchItem)
 {
     $this->loadModel('product');
     $this->loadModel('search');
@@ -66,7 +89,7 @@ $fnGenerateCustomSearch = function () use ($lang, $config, $settings, $fnGenerat
         $andor    = $settings['andor'][$index];
         $operator = $settings['operator'][$index];
         $value    = $settings['value'][$index];
-        $items[]  = $fnGenerateCustomSearchItem($index, array($andor, $field, $operator, $value));
+        $items[]  = $fnGenerateCustomSearchItem($index, $searchConfig, array($andor, $field, $operator, $value));
     }
 
     return div
@@ -75,6 +98,7 @@ $fnGenerateCustomSearch = function () use ($lang, $config, $settings, $fnGenerat
         setClass('flex col gap-y-2', array('hidden' => !$isCustom)),
         $items,
         on::click('#customSearchContent .search-add')->do("updateCustomSearchItem(\$this, 'add')"),
-        on::click('#customSearchContent .search-remove')->do("updateCustomSearchItem(\$this, 'remove')")
+        on::click('#customSearchContent .search-remove')->do("updateCustomSearchItem(\$this, 'remove')"),
+        on::change('#customSearchContent .search-field')->do("updateCustomSearch(\$this)")
     );
 };
