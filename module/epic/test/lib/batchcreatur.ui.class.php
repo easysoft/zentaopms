9c26e79df47ea867285269765dbbdee7bcfd4faf
lijie <lijie@chandao.com>
2024-12-10 16:34:48 +0800

    [task#133979, 1h] add ER split UR uitest steps.

--- module/epic/test/lib/batchcreatur.ui.class.php
+++ module/epic/test/lib/batchcreatur.ui.class.php
@@ -18,3 +18,23 @@ class createChildStoryTester extends tester
             'productID'  => '1',
             'branch'     => '',
             'browseType' => 'allstory',
+            'param'      => '0',
+            'storyType'  => 'epic'
+        );
+        $form = $this->initForm('product', 'browse', $browseStoryParam, 'appIframe-product');
+        $form->dom->search($searchList = array("需求名称,包含,$storyName"));
+        if($form->dom->{$this->lang->story->subdivide}->getText != '评审中和已关闭的需求，无法进行拆分操作') return $this->failed('细分按钮高亮不正确');
+        return $this->success('细分按钮高亮正确');
+    }
+    /**
+     * Batchcreate requirement.
+     *
+     * @param  string $type
+     * @param  string $storyName
+     * @access public
+     * @return object
+     */
+    public function batchCreateDefault($storyName, $childName)
+    {
+        $browseStoryParam = array(
+            'productID'  => '1',
