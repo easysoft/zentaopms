bddbdde4e642132ae637a0816e263570cba8bf53
lijie <lijie@chandao.com>
2024-12-10 15:49:22 +0800

    [task#133979, 1h] add create child requirment function.

--- module/epic/test/ui/page/view.php
+++ module/epic/test/ui/page/view.php
@@ -7,8 +7,11 @@ class viewPage extends page
 
         $xpath = array(
             /*需求详情页*/
-            'status'      => "//*[@class='tab-content']/div/div/div[6]/div[2]/span",
-            'closeReason' => "/html/body/div[1]/div/div/div/div[2]/div[2]/div[1]/div[2]/div[2]/div/div[6]/div[2]",
+            'status'           => "//*[@class='tab-content']/div/div/div[6]/div[2]/span",
+            'closeReason'      => "/html/body/div[1]/div/div/div/div[2]/div[2]/div[1]/div[2]/div[2]/div/div[6]/div[2]",
+            'childStoryName'   =>"//*[@id='table-story-children']/div[2]/div[1]/div/div[2]/div/a",
+            'childStoryStatus' =>"//*[@id='table-story-children']/div[2]/div[2]/div/div[3]/div/span",
+            'parentStoryName'  =>"//*[@id='zin_requirement_view_tabPane']/div/div[3]/div[2]/div/div/a",
             /*激活弹窗的激活按钮*/
             'activate'    => "//*[@type='submit']",
             /*关闭弹窗中的关闭按钮*/
