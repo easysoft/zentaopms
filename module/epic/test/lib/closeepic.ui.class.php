a940fef52df8a78c472b807f0c57cfcbe5abc855
lijie <lijie@chandao.com>
2024-12-10 16:32:00 +0800

    [task#133979, 1h] add ER split UR uitest function.

--- module/epic/test/lib/closeepic.ui.class.php
+++ module/epic/test/lib/closeepic.ui.class.php
@@ -58,20 +58,20 @@ class closeEpicTester extends tester
         $browsePage->dom->firstSelect->click();
         $browsePage->dom->batchMore->click();
         sleep(1);
-        $browsepage->dom->getelement("/html/body/div[2]/menu/menu/li[1]/a/div/div")->click();
+        $browsePage->dom->getelement("/html/body/div[2]/menu/menu/li[1]/a/div/div")->click();
         sleep(1);
 
-        $batchclose = $this->loadpage($storytype, 'batchclose');
-        $batchclose->dom->batchclosedreason->picker($closereason);
+        $batchclose = $this->loadpage($storyType, 'batchclose');
+        $batchclose->dom->batchclosedreason->picker($closeReason);
         $batchclose->dom->batchclosedsave->click();
         $batchclose->wait(1);
 
         /*检查需求详情页需求状态和关闭原因*/
-        $viewpage = $this->initform($storytype, 'view', array('id' => $storyid), 'appiframe-product');
+        $viewpage = $this->initform($storyType, 'view', array('id' => $storyID), 'appiframe-product');
         if($viewpage->dom->status->gettext() != '已关闭') return $this->failed('需求状态不正确');
 
         $viewpage->dom->btn($this->lang->story->legendlifetime)->click();
-        if($viewpage->dom->closereason->gettext() != $closereason) return $this->failed('需求关闭原因不正确');
+        if($viewpage->dom->closereason->gettext() != $closeReason) return $this->failed('需求关闭原因不正确');
 
         return $this->success('批量关闭业务需求成功');
     }
