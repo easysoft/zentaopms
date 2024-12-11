1a089b7c2dac8b4611aa9eba2de738cde9b8059d
huairong <huairong@easycorp.ltd>
2024-12-11 08:49:42 +0800

    + [task#133981,doing,0.5h,2.5h] Add comment of checkNum function.

---
+++ module/project/test/lib/testtask.ui.class.php
@@ -0,0 +1,14 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+class testtaskTester extends tester
+{
+    /**
+     * 检查测试单列表统计数据。
+     * Check the data of testtask list.
+     *
+     * @param  string $select
+     * @param  array  $number
+     * @access public
+     * @return object
+     */
+}
