00771dddda1d951c241ca77cd7813b63710c9f5b
sunguangming <sunguangming@chandao.com>
2024-12-11 09:26:25 +0000

    * [misc] Move sql.

--- db/update21.2.sql
+++ db/update21.2.sql
@@ -1,3 +1,12 @@
+ALTER TABLE `zt_task` ADD `isParent` tinyint(1) NOT NULL DEFAULT 0 after `parent`;
+ALTER TABLE `zt_task` ADD `path` text NULL after `isParent`;
+
+UPDATE `zt_task` SET `path` = concat(',', id, ',') WHERE `parent` <= 0;
+UPDATE `zt_task` SET `path` = concat(',', parent, ',', id, ',') WHERE `parent` > 0;
+
+UPDATE `zt_task` SET `isParent` = 1 WHERE `parent` = -1;
+UPDATE `zt_task` SET `parent`   = 0 WHERE `parent` = -1;
+
 ALTER TABLE `zt_workflowfield` MODIFY `placeholder` VARCHAR(255) NOT NULL DEFAULT '';
 
 ALTER TABLE zt_dataview MODIFY `fields` text NULL;
