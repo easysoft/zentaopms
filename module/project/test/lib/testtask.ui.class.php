b7fbeab0031373c051021365df1865589aeb55ee
huairong <huairong@easycorp.ltd>
2024-12-11 08:50:30 +0800

    + [task#133981,doing,0.5h,2.5h] Add checkNum function.

--- module/project/test/lib/testtask.ui.class.php
+++ module/project/test/lib/testtask.ui.class.php
@@ -11,4 +11,17 @@ class testtaskTester extends tester
      * @access public
      * @return object
      */
+    public function checkNum($select, $number)
+    {
+        $form = $this->initForm('project', 'testtask', array('project' => 1), 'appIframe-project');
+        if($select) $form->dom->totalCheckbox->click();
+        $form->wait(1);
+
+        if($form->dom->total->getText()   != $number[0]) return $this->failed('测试单总数统计错误');
+        if($form->dom->waiting->getText() != $number[1]) return $this->failed('待测试的测试单数据统计错误');
+        if($form->dom->doing->getText()   != $number[2]) return $this->failed('测试中的测试单数据统计错误');
+        if($form->dom->blocked->getText() != $number[3]) return $this->failed('被阻塞的测试单数据统计错误');
+        if($form->dom->done->getText()    != $number[4]) return $this->failed('已测试的测试单数据统计错误');
+        return $this->success('测试单列表统计数据正确');
+    }
 }
