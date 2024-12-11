78da89ac2566999f4622e657a6cdfe7dc7b73dfe
huairong <huairong@easycorp.ltd>
2024-12-11 08:58:47 +0800

    + [task#133981,doing,0.1h,1.7h] Add comment of testtask ui file.

---
+++ module/project/test/ui/testtask.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php
+
+/**
+
+title=项目下测试单列表数据检查
+timeout=0
+cid=1
+
+- 执行tester模块的checkNum方法，参数是false, array
+ - 最终测试状态 @SUCCESS
+ - 测试结果 @测试单列表统计数据正确
+- 执行tester模块的checkNum方法，参数是true, array
+ - 最终测试状态 @SUCCESS
+ - 测试结果 @测试单列表统计数据正确
+- 执行tester模块的createReport方法▫
+ - 最终测试状态 @SUCCESS
+ - 测试结果 @不同产品测试单生成测试报告提示信息正确
+
+ */
