8a136e9184ff094aa089f0d1662181fc7d2bce8d
huairong <huairong@easycorp.ltd>
2024-12-12 16:31:56 +0800

    + [task#133983,doing,0.5h,2.5h] Add createStakeholder function.

--- module/stakeholder/test/lib/createstakeholder.ui.class.php
+++ module/stakeholder/test/lib/createstakeholder.ui.class.php
@@ -20,5 +20,17 @@ class createStakeholderTester extends tester
             $form->dom->$from->click();
             $form->wait(1);
         }
+        /* 根据传的值来判断是否点击“是”单选按钮 */
+        if(isset($stakeholder['key']))
+        {
+            $key = $stakeholder['key'];
+            $form->dom->$key->click();
+            $form->wait(1);
+        }
+        if(isset($stakeholder['user'])) $form->dom->user->setvalue($stakeholder['user']);
+        $form->wait(1);
+        $form->dom->btn($this->lang->save)->click();
+        $form->wait(1);
+        return $this->checkResult($stakeholder);
     }
 }
