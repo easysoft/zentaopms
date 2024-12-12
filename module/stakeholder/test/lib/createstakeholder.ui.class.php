2a1c599c684dea425d094930d730d80ee2d998ce
huairong <huairong@easycorp.ltd>
2024-12-12 16:29:55 +0800

    + [task#133983,doing,0.1h,3h] Add comment of createStakeholder function.

---
+++ module/stakeholder/test/lib/createstakeholder.ui.class.php
@@ -0,0 +1,13 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+class createStakeholderTester extends tester
+{
+    /**
+     * Create a stakeholder.
+     * 创建干系人表单。
+     *
+     * @param  array  $stakeholder
+     * @access public
+     * @return object
+     */
+}
