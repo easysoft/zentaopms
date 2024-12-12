2614e5184ecf37a2b3d856c4e8590d04932e448a
songchenxuan <songchenxuan@easycorp.ltd>
2024-12-12 10:05:10 +0800

    + [task#133861,doing,0.5h].

--- module/doc/ui/zentaolist.productbug.html.php
+++ module/doc/ui/zentaolist.productbug.html.php
@@ -3,10 +3,17 @@ declare(strict_types=1);
 
 namespace zin;
 
-$fnGenerateFormRows = function () use ($lang, $settings)
+$fnGenerateFormRows = function () use ($lang, $settings, $fnGenerateCustomSearch)
 {
     $productList = $this->loadModel('product')->getPairs();
     $planList    = isset($settings['product']) ? $this->loadModel('productplan')->getPairs((int)$settings['product'], '', '', true) : array();
+    $productID   = isset($settings['product']) ? (int)$settings['product'] : 0;
+
+    $this->loadModel('bug');
+    $conditions = array_filter($lang->bug->featureBar['browse'], fn($value) => $value !== '-');
+    $conditions['customSearch'] = $lang->doc->customSearch;
+
+    $searchConfig = $this->bug->buildSearchConfig($productID);
     return array
     (
         formRow
@@ -26,6 +33,26 @@ $fnGenerateFormRows = function () use ($lang, $settings)
                     $lang->doc->emptyError
                 )
             )
-        )
+        ),
+        formRow
+        (
+            formGroup
+            (
+                set::width('1/2'),
+                set::name('condition'),
+                set::label($lang->doc->searchCondition),
+                set::required(),
+                set::control(array('contorl' => 'picker', 'required' => false, 'maxItemsCount' => 50)),
+                set::items($conditions),
+                set::value(isset($settings['condition']) ? $settings['condition'] : ''),
+                span
+                (
+                    setClass('error-tip text-danger hidden'),
+                    $lang->doc->emptyError
+                )
+            )
+        ),
+        $fnGenerateCustomSearch($searchConfig)
+
     );
 };
