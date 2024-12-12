f5d8f492c7b72c87650b2c9f2150c8723434752c
hufangzhou <hufangzhou@chandao.com>
2024-12-12 16:55:48 +0800

    * [task#134226,done,0.1h,0h] Modify the configuration of the charter return button.

--- lib/zin/wg/backbtn/v1.php
+++ lib/zin/wg/backbtn/v1.php
@@ -90,7 +90,7 @@ class backBtn extends btn
             'domain'         => 'domain-browse,domain-view',
             'researchtask'   => 'marketresearch-task,my-index,product-dynamic,project-dynamic,project-view,my-contribute',
             'review'         => 'review-browse,my-index,product-dynamic,project-dynamic,project-view,programplan-browse,project-execution,my-contribute',
-            'charter'        => 'my-index,charter-browse,charter-view,my-audit,program-browse',
+            'charter'        => 'my-index,charter-browse,charter-view,my-audit,program-browse,project-view',
             'roadmap'        => 'charter-view',
             'approvalflow'   => 'approvalflow-browse',
             'host'           => 'host-browse,my-index',
