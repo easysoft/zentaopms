fc578543bb956b15d6bee86cc26069cffcecfecc
huairong <huairong@easycorp.ltd>
2024-12-13 08:53:56 +0800

    + [task#133983,done,0.1h] modify format.

--- module/stakeholder/test/lib/createstakeholder.ui.class.php
+++ module/stakeholder/test/lib/createstakeholder.ui.class.php
@@ -10,9 +10,9 @@ class createStakeholderTester extends tester
      * @access public
      * @return object
      */
-    public function createstakeholder(array $stakeholder)
+    public function createStakeholder(array $stakeholder)
     {
-        $form = $this->initform('stakeholder', 'create', array('projecid' => 1), 'appiframe-project');
+        $form = $this->initForm('stakeholder', 'create', array('projecID' => 1), 'appIframe-project');
         /* 根据传的值来判断是否点击公司同事/外部人员单选按钮 */
         if(isset($stakeholder['type']))
         {
