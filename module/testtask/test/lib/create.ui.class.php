249d1adc76a36709205d355fb4e7a77f89a97712
yulujie <yulujie@easycorp.ltd>
2024-12-13 17:21:25 +0800

    * [task#134548,doing,0.5h,2.5h] Add function

--- module/testtask/test/lib/create.ui.class.php
+++ module/testtask/test/lib/create.ui.class.php
@@ -17,9 +17,29 @@ class createTester extends tester
         $form->dom->product->picker($product);
         $form->wait(1);
         $form->dom->execution->picker($execution);
-        $form->wait(2);
+        $form->wait(1);
         if(!is_object($form->dom->createBuildBtn)) return $this->failed('没有显示创建构建按钮');
         if(!is_object($form->dom->refreshBtn))     return $this->failed('没有显示刷新按钮');
         return $this->success('正确显示创建构建和刷新按钮');
+    }
+
+    /**
+     * 创建构建。
+     * Create build.
+     *
+     * @param  array $build
+     * @access public
+     * @return void
+     */
+    public function create($build)
+    {
+        $form = $this->initForm('build', 'create', $build, 'appIframe-qa');
+        if(isset($build['product']))   $form->dom->product->picker($build['product']);
+        if(isset($build['execution'])) $form->dom->execution->picker($build['execution']);
+        if(isset($build['build']))     $form->dom->build->picker($build['build']);
+        if(isset($build['begin']))     $form->dom->begin->setValue($build['begin']);
+        if(isset($build['end']))       $form->dom->end->setValue($build['end']);
+        if(isset($build['name']))      $form->dom->name->setValue($build['name']);
+        $form->dom->submitBtn->click();
     }
 }
