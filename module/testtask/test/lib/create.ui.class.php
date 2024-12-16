e730a2db9bda91fbf8673dc3320022c7655ff849
yulujie <yulujie@easycorp.ltd>
2024-12-16 13:53:42 +0800

    * [task#134548,doing,2h,1h] Add function.

--- module/testtask/test/lib/create.ui.class.php
+++ module/testtask/test/lib/create.ui.class.php
@@ -41,5 +41,32 @@ class createTester extends tester
         if(isset($build['end']))       $form->dom->end->setValue($build['end']);
         if(isset($build['name']))      $form->dom->name->setValue($build['name']);
         $form->dom->submitBtn->click();
+        $form->wait(1);
+        if(isset($testtask['build']) && $testtask['build'] == '')
+        {
+            if($form->dom->buildTip->getText() != sprintf($this->lang->error->notempty, $this->lang->testtask->build)) return $this->failed('提测构建为空时提示信息错误');
+            return $this->success('提测构建为空时提示信息正确');
+        }
+        if(isset($testtask['begin']) && $testtask['begin'] == '')
+        {
+            if($form->dom->beginTip->getText() != sprintf($this->lang->error->notempty, $this->lang->testtask->begin)) return $this->failed('开始日期为空时提示信息错误');
+            return $this->success('开始日期为空时提示信息正确');
+        }
+        if(isset($testtask['end']) && $testtask['end'] == '')
+        {
+            if($form->dom->endTip->getText() != sprintf($this->lang->error->notempty, $this->lang->testtask->end)) return $this->failed('结束日期为空时提示信息错误');
+            return $this->success('结束日期为空时提示信息正确');
+        }
+        if(isset($testtask['name']) && $testtask['name'] == '')
+        {
+            if($form->dom->nameTip->getText() != sprintf($this->lang->error->notempty, $this->lang->testtask->name)) return $this->failed('测试单名称为空时提示信息错误');
+            return $this->success('测试单名称为空时提示信息正确');
+        }
+        if(isset($testtask['begin']) && isset($testtask['end']) && $testtask['begin'] > $testtask['end'])
+        {
+            if($form->dom->endTip->getText() != sprintf($this->lang->error->ge, $this->lang->testtask->end, $testtask['begin'])) return $this->failed('开始日期大于结束日期时提示信息错误');
+            return $this->success('开始日期大于结束日期时提示信息正确');
+        }
+        return $this->success('创建测试单成功');
     }
 }
