228b682611c08ce4e18275edb4490727cb078e73
qixinzhi <qixinzhi@easycorp.ltd>
2024-12-17 01:54:54 +0000

    * [story#69837,0.1h] add simple.

--- lib/zin/wg/searchtoggle/v1.php
+++ lib/zin/wg/searchtoggle/v1.php
@@ -10,7 +10,8 @@ class searchToggle extends wg
         'module?: string',
         'url?: string',
         'searchUrl?: string',
-        'text?: string'
+        'text?: string',
+        'simple?: boolean' // 是否为简单模式，不包含保存搜索条件和已保存的查询条件侧边栏。
     );
 
     public static function getPageCSS(): ?string
@@ -32,7 +33,7 @@ class searchToggle extends wg
         if(!common::hasPriv('search', 'buildForm')) return;
 
         global $lang, $app, $config;
-        list($target, $module, $open, $url, $searchUrl, $text) = $this->prop(array('target', 'module', 'open', 'url', 'searchUrl', 'text'));
+        list($target, $module, $open, $url, $searchUrl, $text, $simple) = $this->prop(array('target', 'module', 'open', 'url', 'searchUrl', 'text', 'simple'));
 
         if(is_null($open) && !empty($_GET['browseType'])) $open = $_GET['browseType'] === 'bySearch';
         if(is_null($module)) $module = $app->rawModule;
@@ -50,7 +51,7 @@ class searchToggle extends wg
             set::icon('search'),
             set::active($open),
             set::text($text),
-            toggle::searchform(array('module' => $module, 'target' => $target, 'url' => $url, 'searchUrl' => $searchUrl)),
+            toggle::searchform(array('module' => $module, 'target' => $target, 'url' => $url, 'searchUrl' => $searchUrl, 'simple' => $simple)),
             $open ? on::init()->call('zui.toggleSearchForm', array('module' => $module, 'target' => $target, 'show' => $open, 'url' => $url, 'searchUrl' => $searchUrl)) : on::init()->do('$element.closest(".show-search-form").removeClass("show-search-form").find(".search-form[data-module=' . $module . ']").remove()')
         );
     }
