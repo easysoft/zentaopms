efe10dec91d741cee42166caf2139acbce6cb662
wangzemei <wangzemei@easycorp.ltd>
2024-12-18 11:03:25 +0800

    + [task#135043 0.2h 2h] add content for the thinkBcg widget.

--- lib/zin/wg/thinkbcg/v1.php
+++ lib/zin/wg/thinkbcg/v1.php
@@ -10,4 +10,17 @@ requireWg('thinkModel');
  */
 class thinkBcg extends thinkModel
 {
+    protected function buildBody(): node
+    {
+        global $app, $lang;
+        $app->loadLang('thinkwizard');
+        list($mode, $wizard, $previewKey) = $this->prop(array('mode', 'wizard', 'previewKey'));
+
+        return div();
+    }
+
+    protected function build(): node
+    {
+        return div(setClass('model-appeals my-1 flex col flex-wrap justify-between'), $this->buildBody());
+    }
 }
