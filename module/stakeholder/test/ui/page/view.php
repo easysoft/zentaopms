1cd34947ed3c1ee6c2ecac8dc0401ef2501ab1f5
huairong <huairong@easycorp.ltd>
2024-12-19 09:21:38 +0800

    + [task#135060,doing,0.2h,2.6h] add element of view page.

--- module/stakeholder/test/ui/page/view.php
+++ module/stakeholder/test/ui/page/view.php
@@ -5,10 +5,13 @@ class viewPage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'name' => "//*[@id='basicInfo']/table/tbody/tr[1]/td",
-            'type' => "//*[@id='basicInfo']/table/tbody/tr[9]/td",
-            'key'  => "//*[@id='basicInfo']/table/tbody/tr[8]/td",
-
+            'name'           => "//*[@id='basicInfo']/table/tbody/tr[1]/td",
+            'type'           => "//*[@id='basicInfo']/table/tbody/tr[9]/td",
+            'key'            => "//*[@id='basicInfo']/table/tbody/tr[8]/td",
+            'personality'    => "//*[@id='mainContent']/div[2]/div[1]/div[1]/div[1]/div[2]/p/span",
+            'impactAnalysis' => "//*[@id='mainContent']/div[2]/div[1]/div[1]/div[2]/div[2]/p/span",
+            'response'       => "//*[@id='mainContent']/div[2]/div[1]/div[1]/div[3]/div[2]/p/span",
+            'communication'  => "//*[@id='mainContent']/div[2]/div[1]/div[2]/div[2]/ul/li/div[2]/div/div/p/span",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
