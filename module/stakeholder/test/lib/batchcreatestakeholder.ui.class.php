dc8b7be4f85b8c59384f6c80a2991af2bf608653
huairong <huairong@easycorp.ltd>
2024-12-19 09:24:05 +0800

    + [task#135060,doing,0.5h,2.1h] add comment copyFromDept function.

---
+++ module/stakeholder/test/lib/batchcreatestakeholder.ui.class.php
@@ -0,0 +1,13 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+class batchCreateStakeholderTester extends tester
+{
+    /**
+     * Batch create stakeholder by copy from department.
+     * 通过复制部门人员来批量创建干系人。
+     *
+     * @param  string  $expectNum
+     * @access public
+     * @return object
+     */
+}
