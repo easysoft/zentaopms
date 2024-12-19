998ab203ad0950d705a0e42154b56d0c51397630
huairong <huairong@easycorp.ltd>
2024-12-19 09:24:51 +0800

    + [task#135060,doing,0.5h,2.1h] add copyFromDept function.

--- module/stakeholder/test/lib/batchcreatestakeholder.ui.class.php
+++ module/stakeholder/test/lib/batchcreatestakeholder.ui.class.php
@@ -10,4 +10,19 @@ class batchCreateStakeholderTester extends tester
      * @access public
      * @return object
      */
+    public function copyFromDept($expectNum)
+    {
+        $form = $this->initForm('stakeholder', 'batchCreate', array('projecID' => 2), 'appIframe-project');
+        $form->wait(1);
+        $form->dom->selectDept->picker('研发部');
+        $form->wait(1);
+        $form->dom->btn($this->lang->save)->click();
+        $form->wait(1);
+        /* 干系人列表检查干系人数 */
+        $browsePage     = $this->loadPage('stakeholder', 'browse');
+        $string         = $browsePage->dom->number->getText();
+        $stakeholderNum = preg_replace('/\D/', '', $string); //从字符串中提取数字部分
+        if($stakeholderNum == $expectNum) return $this->success('批量创建干系人成功');
+        return $this->failed('批量创建干系人失败');
+    }
 }
