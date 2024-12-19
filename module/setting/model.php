ede683ad0e6a1f1ef47edb8fac5897067847b436
liugang <liugang@chandao.com>
2024-12-19 09:59:54 +0800

    * [refac] replace mao with dao to improve effeciency.

--- module/setting/model.php
+++ module/setting/model.php
@@ -243,8 +243,7 @@ class settingModel extends model
         $params['section'] = isset($params['section']) ? $params['section'] : '';
         $params['key']     = isset($params['key'])     ? $params['key']     : '';
 
-        $access = $method == 'select' ? $this->mao : $this->dao;
-        return $access->$method('*')->from(TABLE_CONFIG)->where('1 = 1')
+        return $this->dao->$method('*')->from(TABLE_CONFIG)->where('1 = 1')
             ->beginIF($params['vision'])->andWhere('vision')->in($params['vision'])->fi()
             ->beginIF($params['owner'])->andWhere('owner')->in($params['owner'])->fi()
             ->beginIF($params['module'])->andWhere('module')->in($params['module'])->fi()
@@ -263,7 +262,7 @@ class settingModel extends model
     public function getSysAndPersonalConfig(string $account = ''): array
     {
         $owner = 'system,' . ($account ? $account : '');
-        $records = $this->mao->select('*')->from(TABLE_CONFIG)
+        $records = $this->dao->select('*')->from(TABLE_CONFIG)
             ->where('owner')->in($owner)
             ->beginIF(!$this->app->upgrading)->andWhere('vision')->in(array('', $this->config->vision))->fi()
             ->orderBy('id')
