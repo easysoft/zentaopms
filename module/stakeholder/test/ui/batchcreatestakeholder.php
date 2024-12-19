2aa4a904c063a7861912acbb1414fbb111b809c2
huairong <huairong@easycorp.ltd>
2024-12-19 09:32:17 +0800

    + [task#135060,doing,0.3h,0.7h] add stakeholder and dept test data.

--- module/stakeholder/test/ui/batchcreatestakeholder.php
+++ module/stakeholder/test/ui/batchcreatestakeholder.php
@@ -18,3 +18,24 @@ cid=2
  - 最终测试状态 @ SUCCESS
 
 */
+
+chdir(__DIR__);
+include '../lib/batchcreatestakeholder.ui.class.php';
+global $config;
+
+$stakeholder = zenData('stakeholder');
+$stakeholder->id->range('1');
+$stakeholder->objectID->range('1');
+$stakeholder->objectType->range('program');
+$stakeholder->user->range('user3');
+$stakeholder->type->range('inside');
+$stakeholder->from->range('company');
+$stakeholder->gen(1);
+
+$dept =zenData('dept');
+$dept->id->range('1');
+$dept->name->range('研发部');
+$dept->parent->range('0');
+$dept->path->range('`,1,`');
+$dept->grade->range('1');
+$dept->gen(1);
