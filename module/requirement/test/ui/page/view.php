420a39bee2ecd5ea37e3fd64267b9bce395f13dc
lijie <lijie@chandao.com>
2024-12-20 14:47:44 +0800

    [task#133977, 0.2h] add change requirement xpath.

--- module/requirement/test/ui/page/view.php
+++ module/requirement/test/ui/page/view.php
@@ -6,6 +6,8 @@ class viewPage extends page
         parent::__construct($webdriver);
 
         $xpath = array(
+            'storyName'       => "//*[@id='mainContent']/div/div/div/span[2]",
+            'status'          => "//*[@id='zin_requirement_view_tabPane']/div/div[6]/div[2]/span",
             'parentStoryName' => "//*[@id='zin_requirement_view_tabPane']/div/div[3]/div[2]/div/div/a"
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
