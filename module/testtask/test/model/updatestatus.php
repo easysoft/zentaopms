538b0c39135575957f32e7ca6898a1155b428065
liumengyi <liumengyi@easycorp.ltd>
2024-12-21 13:04:59 +0800

    + [task#134170,done,0.1h] add unittest of testtaskModel::updateStatus.

--- module/testtask/test/model/updatestatus.php
+++ module/testtask/test/model/updatestatus.php
@@ -13,8 +13,26 @@ title=测试 testtaskModel->updateStatus();
 cid=1
 pid=1
 
+- 测试更新测试单 ID 为 1 的状态到doing属性status @doing
+- 测试更新测试单 ID 为 2 的状态到doing属性status @doing
+- 测试更新测试单 ID 为 3 的状态到doing属性status @doing
+- 测试更新测试单 ID 为 4 的状态到doing属性status @doing
+- 测试更新测试单 ID 为 5 的状态到doing属性status @doing
+- 测试更新测试单 ID 不存在的 10001 的状态到wait属性status @0
+- 测试更新测试单 ID 不存在的 0 的状态到wait属性status @0
+
 */
 
 $uid = uniqid();
 
 $taskIdList = array(1, 2, 3, 4, 5, 10001, 0);
+
+$testtask = new testtaskTest();
+
+r($testtask->updateStatusTest($taskIdList[0])) && p('status') && e('doing'); // 测试更新测试单 ID 为 1 的状态到doing
+r($testtask->updateStatusTest($taskIdList[1])) && p('status') && e('doing'); // 测试更新测试单 ID 为 2 的状态到doing
+r($testtask->updateStatusTest($taskIdList[2])) && p('status') && e('doing'); // 测试更新测试单 ID 为 3 的状态到doing
+r($testtask->updateStatusTest($taskIdList[3])) && p('status') && e('doing'); // 测试更新测试单 ID 为 4 的状态到doing
+r($testtask->updateStatusTest($taskIdList[4])) && p('status') && e('doing'); // 测试更新测试单 ID 为 5 的状态到doing
+r($testtask->updateStatusTest($taskIdList[5])) && p('status') && e('0');     // 测试更新测试单 ID 不存在的 10001 的状态到wait
+r($testtask->updateStatusTest($taskIdList[6])) && p('status') && e('0');     // 测试更新测试单 ID 不存在的 0 的状态到wait
