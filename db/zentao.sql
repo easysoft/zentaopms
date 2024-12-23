bdf881bb01a525b152e83a94c10086f08306a024
qixinzhi <qixinzhi@easycorp.ltd>
2024-12-23 07:50:53 +0000

    * [story#69412,0.5h] fix sql.

--- db/zentao.sql
+++ db/zentao.sql
@@ -851,12 +851,12 @@ CREATE TABLE IF NOT EXISTS `zt_docblock` (
   `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `doc` mediumint(8) unsigned NOT NULL DEFAULT 0,
   `type` varchar(50) NOT NULL DEFAULT '',
-  `settings` text NOT NULL DEFAULT '',
-  `content` text NOT NULL DEFAULT '',
-  `extra` varchar(255) NOT NULL DEFAULT '',
+  `settings` text NULL,
+  `content` text NULL,
+  `extra` varchar(255) NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE INDEX `idx_doc` ON `zt_docblock`(`doc`);
+CREATE INDEX `idx_doc` ON `zt_docblock` (`doc`);
 
 -- DROP TABLE IF EXISTS `zt_effort`;
 CREATE TABLE IF NOT EXISTS `zt_effort` (
