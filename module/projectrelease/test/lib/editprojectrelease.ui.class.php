6bbe95ad46b46cbd432f5daee12b6eb660acf0ef
lihuifang <lihuifang@easycorp.ltd>
2024-12-24 14:38:08 +0800

    *[task#135220,done,0.5h,0h] Modify function.

--- module/projectrelease/test/lib/editprojectrelease.ui.class.php
+++ module/projectrelease/test/lib/editprojectrelease.ui.class.php
@@ -15,7 +15,7 @@ class editProjectReleaseTester extends tester
         $browseForm->dom->editBtn->click();
         $form = $this->initForm('projectrelease', 'edit', array('releaseID' => 1), 'appIframe-project');
 
-        if(isset($release['systemname'])) $form->dom->systemName->picker($release['systemname']);
+        if(isset($release['systemname'])) $form->dom->system->picker($release['systemname']);
         if(isset($release['name']))       $form->dom->name->setValue($release['name']);
         if(isset($release['status']))     $form->dom->status->picker($release['status']);
         $form->wait(2);
@@ -25,16 +25,22 @@ class editProjectReleaseTester extends tester
         $form->dom->btn($this->lang->save)->click();
         $form->wait(2);
 
+        /* 断言检查必填提示信息 */
+        if($this->response('method') == 'edit') return $this->success('编辑项目发布表单页必填提示信息正确');
+
         /* 跳转到发布概况页面，点击基本信息标签，查看信息是否正确 */
-        $viewPage = $this->loadPage('projectrelease', 'view');
-        $viewPage->dom->basic->click();
-        $viewPage->wait(2);
+        else
+        {
+            $viewPage = $this->loadPage('projectrelease', 'view');
+            $viewPage->dom->basic->click();
+            $viewPage->wait(2);
 
-        //断言检查应用名称、发布名称、状态是否正确
-        if($viewPage->dom->basicSystemName->getText() != $release['systemname']) return $this->failed('应用名称错误');
-        if($viewPage->dom->basicreleasename->getText() != $release['name'])      return $this->failed('项目发布名称错误');
-        if($viewPage->dom->basicstatus->getText() != $release['status'])         return $this->failed('项目发布状态错误');
+            //断言检查应用名称、发布名称、状态是否正确
+            if(isset($release['systemname']) && $viewPage->dom->basicSystemName->getText() != $release['systemname']) return $this->failed('应用名称错误');
+            if(isset($release['name']) &&$viewPage->dom->basicreleasename->getText() != $release['name'])             return $this->failed('项目发布名称错误');
+            if(isset($release['status']) &&$viewPage->dom->basicstatus->getText() != $release['status'])              return $this->failed('项目发布状态错误');
 
-        return $this->success('编辑项目发布成功');
+            return $this->success('编辑项目发布成功');
+        }
     }
 }
