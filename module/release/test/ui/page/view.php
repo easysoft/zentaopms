bcc44952bc09153a3966d69bcbfdd322ca365443
lihuifang <lihuifang@easycorp.ltd>
2024-12-24 15:31:01 +0800

    *[task#134824,doing,1.5h,1h] Add element

--- module/release/test/ui/page/view.php
+++ module/release/test/ui/page/view.php
@@ -13,7 +13,16 @@ class viewPage extends page
             /*导出HTML*/
             'exportBtn'      => "//*[@id='mainContent']/div[2]/div/div/div/div[2]/div[2]/div/a/span",
             'exportData'     => "//*[@class='panel-body']/form/div[2]/div/div/span",
-            'exportBtnAlert' => "//*[@class='form-row']/div/button"
+            'exportBtnAlert' => "//*[@class='form-row']/div/button",
+            /*发布关联需求*/
+            'linkStoryBtn'        => "//*[@id='mainContent']/div[2]/div/div/div/div[2]/div/div/button/span",
+            'linkStoryBtnBottom'  => "//*[@id='unlinkStoryList']/div[3]/nav/button/span",
+            'searchBtn'           => "//*[@class='btn primary']",
+            'selectAllStory'      => "//*[@id='unlinkStoryList']/div[3]/div/div/label",
+            'finishedStoryNum'    => "//*[@id='finishedStoryDTable']/div[3]/div[2]/strong[1]",
+            'unlinkFirBtn'        => "//*[@id='finishedStoryDTable']/div[2]/div[3]/div/div/div/nav/a/i",
+            'allFinishedStoryBtn' => "//*[@id='finishedStoryDTable']/div[3]/div/div/label",
+            'batchUnlinkBtn'      => "//*[@id='finishedStoryDTable']/div[3]/nav[1]/button[1]/span"
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
