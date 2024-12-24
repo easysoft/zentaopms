ca5333a0b08f63f78d7294e8d1b5c843293fedbf
huairong <huairong@easycorp.ltd>
2024-12-24 13:27:10 +0800

    + [task#135061,doing,0.1h,2.4h] add comment of editStakeholder function.

---
+++ module/stakeholder/test/lib/editstakeholder.ui.class.php
@@ -0,0 +1,13 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+class editStakeholderTester extends tester
+{
+    /**
+     * Edit a stakeholder.
+     * 编辑干系人表单。
+     *
+     * @param  array  $stakeholder
+     * @access public
+     * @return object
+     */
+}
