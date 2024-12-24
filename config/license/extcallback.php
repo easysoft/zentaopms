9a559e5ccfa6df2da9152a2df14561d20935e8e8
zhaixiaojian <zhaixiaojian@easycorp.ltd>
2024-12-24 16:44:39 +0800

    * [task#135155,done,1h,0h] Adjust plugin expire notice.

--- config/license/extcallback.php
+++ config/license/extcallback.php
@@ -5,12 +5,24 @@ function ioncube_event_handler($err_code, $params)
     $extensionLink = helper::createLink('extension', 'browse');
     $homePageLink  = helper::createLink('my', 'index');
     $pluginNotice  = '';
+    $deleteBtnZh   = '';
+    $deleteBtnEn   = '';
+
+    /* Display plugin information and permission check */
     if(!empty($params['license_file']) && preg_match('/([a-zA-Z]+)(\d+\.\d+)/', basename($params['license_file']), $matches))
     {
         $extensionInfo = !empty($matches[1]) ? $app->dao->select('*')->from(TABLE_EXTENSION)->where('code')->eq($matches[1])->fetch() : [];
         $pluginName    = !empty($extensionInfo->name) ? $extensionInfo->name : '';
         $pluginNotice  = !empty($pluginName) ? "<h3 style='margin: 30px 30px 0px;'><span class='icon icon-exclamation-sign warning-pale rounded-full icon-2x' style='margin-right:10px;'></span>“{$pluginName}”插件暂无授权</h3>" : '';
+
+        $isPlugin = commonModel::hasPriv('extension', 'uninstall');
+        if($isPlugin)
+        {
+            $deleteBtnZh = "<a href='{$extensionLink}' target='_top' class='btn btn-wide btn-default'>卸载插件</a>";
+            $deleteBtnEn = "<a href='{$extensionLink}' target='_top' class='btn item btn-wide btn-default'>Delete Plugin</a>";
+        }
     }
+
     $email   = 'co@zentao.net';
     $mobile  = '4006-8899-23';
     $expired = "
@@ -33,8 +45,8 @@ function ioncube_event_handler($err_code, $params)
         </div>
         <div style='text-align:center; margin: 13px 0px;'>
             <a href='https://www.zentao.net/extension-browse.html' target='_blank' class='btn btn-wide primary' style='margin-right: 20px;'>下载更新</a>
-            <a href='{$extensionLink}' target='_top' class='btn btn-wide btn-default'>卸载插件</a>
-            <a href='{$homePageLink}' target='_blank' class='btn btn-wide primary' style='margin-left: 20px;'>返回首页</a>
+            {$deleteBtnZh}
+            <a href='{$homePageLink}' target='_blank' class='btn btn-wide primary' style='margin-left: 20px;'>我的地盘</a>
         </div>
         <br /> <br /> <br />
         <h2 style='color:red;text-align:center'>This version is not licensed</h2>
@@ -45,8 +57,8 @@ function ioncube_event_handler($err_code, $params)
         </div>
          <div style='text-align:center; margin: 13px 0px;'>
             <a href='https://www.zentao.net/extension-browse.html' target='_blank' class='btn btn-wide primary' style='margin-right: 20px;'>Download Update</a>
-            <a href='{$extensionLink}' target='_top' class='btn item btn-wide btn-default'>Delete Plugin</a>
-            <a href='{$homePageLink}' target='_blank' class='btn btn-wide primary' style='margin-left: 20px;'>Home Page</a>
+            {$deleteBtnEn}
+            <a href='{$homePageLink}' target='_blank' class='btn btn-wide primary' style='margin-left: 20px;'>Dashboard</a>
         </div>
         </body>
         </html>
@@ -63,14 +75,14 @@ function ioncube_event_handler($err_code, $params)
         <p style='text-align:left; margin: 0px 20%;'>软件授权的IP地址或MAC地址和当前系统的IP地址或MAC地址不一致，请使用最初授权的服务器。或你访问的域名与绑定的域名不一致。</p>
         <p style='text-align:center; margin: 13px 0px;'>
         <a href='https://www.zentao.net/extension-browse.html' target='_blank' class='btn btn-wide btn-primary' style='margin-right: 20px;'>下载更新</a>
-        <a href='{$extensionLink}' target='_top' class='btn btn-wide extension-btn' style='background-color: #fff; border-color: #d6dae3; color: #3c4353;'>卸载插件</a>
+        {$deleteBtnZh}
         </p>
         <br /> <br /> <br />
         <h2 style='color:red;text-align:center'>Wrong IP, MAC address, or domains!</h2>
         <p style='text-align:left; margin: 0px 20%;'>The IP, MAC address, or the domains of your server is not the same one in your license.</p>
         <p style='text-align:center; margin: 13px 0px;'>
         <a href='https://www.zentao.net/extension-browse.html' target='_blank' class='btn btn-wide primary' style='margin-right: 20px;'>Download Update</a>
-        <a href='{$extensionLink}' target='_top' class='btn item btn-wide btn-default'>Delete Plugin</a>
+        {$deleteBtnEn}
         <a href='{$homePageLink}' target='_blank' class='btn btn-wide primary' style='margin-left: 20px;'>Home Page</a>
         </p>
         </body>
