48298ac2901ae91f78e92755cc8e20e7d1bbc924
huairong <huairong@easycorp.ltd>
2024-12-25 14:29:14 +0800

    + [task#135062,doing,0.1h,1.8h] add comment of communicate function.

---
+++ module/stakeholder/test/lib/communicate.ui.class.php
@@ -0,0 +1,13 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+class communicateTester extends tester
+{
+    /**
+     * Stakeholder communication records.
+     * 干系人沟通记录。
+     *
+     * @param  array  $stakeholder
+     * @access public
+     * @return object
+     */
+}
