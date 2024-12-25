f8fc4d29fcfd7f25a47d2d8827a0d06a8845078a
lihuifang <lihuifang@easycorp.ltd>
2024-12-25 16:00:05 +0800

    +[task#134825,doing,0.1h,1.4h] Add ztf notes.

---
+++ module/release/test/ui/releaselinkbug.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php
+
+/**
+
+title=项目发布关联和移除Bug
+timeout=0
+cid=73
+
+- 项目发布关联bug
+ - 测试结果 @发布关联bug成功
+ - 最终测试状态 @SUCCESS
+- 单个移除bug
+ - 测试结果 @单个移除bug成功
+ - 最终测试状态 @SUCCESS
+- 移除全部bug
+ - 测试结果 @移除全部bug成功
+ - 最终测试状态 @SUCCESS
+
+*/
