25b123321378c92cde793bd9f90b7e8e48fbd4ca
lijie <lijie@chandao.com>
2024-12-25 15:26:19 +0800

    [task#135094, 0.5h] modify function for assigned to story, requirement, epic.

--- module/story/test/lib/assigntostory.ui.class.php
+++ module/story/test/lib/assigntostory.ui.class.php
@@ -13,14 +13,15 @@ class assignToStoryTester extends tester
 {
     /**
      * check the assignedTo after assignto story.
-     * @param string
+     * @param string $storyType
+     * @param int $storyID
      * @access public
      * @return object
      */
-    public function assignToStory()
+    public function assignToStory($storyType, $storyID)
     {
-        $form = $this->openURL('story', 'view', array('id' => 1), 'appIframe-product');  //进入研发需求列表
-        $form = $this->loadPage('story', 'view');
+        $form = $this->openURL($storyType, 'view', array('id' => $storyID), 'appIframe-product');  //进入需求详情页
+        $form = $this->loadPage($storyType, 'view');
         $form->dom->btn($this->lang->story->assignTo)->click();  //点击指派按钮
         $form->wait(1);
 
@@ -28,34 +29,19 @@ class assignToStoryTester extends tester
         $form->dom->assignToBtn->click();
         $form->wait(1);
 
-        $viewPage = $this->loadPage('story', 'view');
+        $viewPage = $this->loadPage($storyType, 'view');
         $viewPage->dom->btn($this->lang->story->legendLifeTime)->click();
         if(strpos($viewPage->dom->assignToONE->getText(), 'admin') === false)  return $this->failed('指派人不正确'); //检查需求详情页指派人是否正确
 
-        return $this->success('指派研发需求成功');
-    }
-
-    /**
-     * check the assignedTo after assignto epic.
-     * @param string
-     * @access public
-     * @return object
-     */
-    public function assignToEpic()
-    {
-        $form = $this->openURL('epic', 'view', array('id' => 3), 'appIframe-product');  //进入业务需求详情页
-        $form = $this->loadPage('epic', 'view');
-        $form->dom->btn($this->lang->story->assignTo)->click();  //点击指派按钮
-        $form->wait(1);
-
-        $form->dom->assignedTo->picker('admin'); //指派人选择admin
-        $form->dom->assignToBtn->click();
-        $form->wait(1);
-
-        $viewPage = $this->loadPage('epic', 'view');
-        $viewPage->dom->btn($this->lang->story->legendLifeTime)->click();
-        if(strpos($viewPage->dom->assignToONE->getText(), 'admin') === false)  return $this->failed('指派人不正确'); //检查需求详情页指派人是否正确
-
-        return $this->success('指派业务需求成功');
+        if($storyType == 'story')
+        {
+            return $this->success('指派研发需求成功');
+        }elseif($storyType == 'requirement')
+        {
+            return $this->success('指派用户需求成功');
+        }else
+        {
+            return $this->success('指派业务需求成功');
+        }
     }
 }
