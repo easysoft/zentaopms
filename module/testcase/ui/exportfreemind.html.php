4cd5406629c0d21c15b89a0f2483c60efaa29e17
liumengyi <liumengyi@easycorp.ltd>
2024-12-26 10:48:14 +0800

    + [task#134149,doing,0.1h] add ui of testcase::exportFreeMind.

---
+++ module/testcase/ui/exportfreemind.html.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+/**
+ * The exportxmid view file of testcase module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Tingting Dai <daitingting@easycorp.ltd>
+ * @package     testcase
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+
+set::title($lang->testcase->exportXmind);
+
+formPanel
+(
+    set::target('_self'),
+);
+
+render('modalDialog');
