e57b05f908e0c4635b88d218890622e4d6314b33
liumengyi <liumengyi@easycorp.ltd>
2024-12-27 14:46:49 +0800

    + [task#134151,doing,0.2h] add xmind class.

---
+++ lib/xmind/xmind.class.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The xmind library of zentaopms, can be used to bakup and restore a database.
+ *
+ * @copyright   Copyright 2009-2015 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.cnezsoft.com)
+ * @license     ZPL(http://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Mengyi Liu <liumengyi@cnezsoft.com>
+ * @package     Xmind
+ * @version     $Id$
+ * @link        http://www.zentao.net
+ */
