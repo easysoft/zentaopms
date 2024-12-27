d4e20c34e4d9b1ba0d3edc8aab67fca77a3782d9
liumengyi <liumengyi@easycorp.ltd>
2024-12-27 14:50:11 +0800

    + [task#134151,doing,0.2h] add xmind class.

--- lib/xmind/xmind.class.php
+++ lib/xmind/xmind.class.php
@@ -2,9 +2,9 @@
 /**
  * The xmind library of zentaopms, can be used to bakup and restore a database.
  *
- * @copyright   Copyright 2009-2015 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.cnezsoft.com)
+ * @copyright   Copyright 2009-2024 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.cnezsoft.com)
  * @license     ZPL(http://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
- * @author      Mengyi Liu <liumengyi@cnezsoft.com>
+ * @author      Mengyi Liu <liumengyi@easycorp.ltd>
  * @package     Xmind
  * @version     $Id$
  * @link        http://www.zentao.net
@@ -23,5 +23,24 @@ class xmind
     {
         if(empty($suffix)) return $str;
         return $str . '['.$suffix.']';
+    }
+
+    /**
+     * Create module node.
+     *
+     * @param  DOMDocument $xmlDoc
+     * @param  array       $context
+     * @param  DOMElement  $productTopics
+     * @param  array       $moduleTopics
+     * @access public
+     * @return void
+     */
+    function createModuleTopic($xmlDoc, $context, $productTopics, &$moduleTopics)
+    {
+        $config     = $context['config'];
+        $moduleList = $context['moduleList'];
+        $caseModules = array_column($context['caseList'], 'moduleID');
+        $caseModules = array_filter($caseModules);
+        $caseModules = array_combine($caseModules, $caseModules);
     }
 }
