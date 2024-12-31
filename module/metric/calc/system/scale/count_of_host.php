e8b5881aaf5a43eb1561ffba1a5c537b7d1fcd73
liyang <liyang@easycorp.ltd>
2024-12-31 08:36:50 +0800

    + [task#125723] add count of host method.

---
+++ module/metric/calc/system/scale/count_of_host.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 按系统统计主机总数。
+ * Count of host.
+ *
+ * 范围：system
+ * 对象：host
+ * 目的：scale
+ * 度量名称：按系统统计主机总数
+ * 单位：个
+ * 描述：按系统统计的主机总数是指在禅道中的全部主机总数
+ * 定义：所有主机的个数求和
+ *
+ * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @author    liyang <liyang@easycorp.ltd>
+ * @package
+ * @uses      func
+ * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @Link      https://www.zentao.net
+ */
