e6cc490ad1beb1a8ab6c57a021b5a17be2f64a05
songchenxuan <songchenxuan@easycorp.ltd>
2025-01-02 13:30:08 +0800

    * [task#135777,doing,1h].

--- module/execution/js/task.ui.js
+++ module/execution/js/task.ui.js
@@ -193,3 +193,38 @@ $(document).off('click', '.switchButton').on('click', '.switchButton', function(
     $.cookie.set('taskViewType', taskViewType, {expires:config.cookieLife, path:config.webRoot});
     loadCurrentPage();
 });
+
+window.insertListToDoc = function()
+{
+    const dtable      = zui.DTable.query($('#tasks'));
+    const checkedList = dtable.$.getChecks();
+    if(!checkedList.length) return;
+
+    let {cols, data} = dtable.options;
+    data = data.filter((item) => checkedList.includes(item.id + ''));
+    const docID = getDocApp()?.docID;
+
+    const url = $.createLink('doc', 'buildZentaoList', `docID=${docID}&type=task&blockID=${blockID}`);
+    const formData = new FormData();
+    formData.append('cols', JSON.stringify(cols));
+    formData.append('data', JSON.stringify(data));
+    formData.append('idList', checkedList.join(','));
+    formData.append('url', insertListLink);
+    $.post(url, formData, function(resp)
+    {
+        resp = JSON.parse(resp);
+        if(resp.result == 'success')
+        {
+            const blockID = resp.blockID;
+            zui.Modal.hide();
+            window.insertZentaoList && window.insertZentaoList('task', blockID, null, true);
+        }
+    });
+}
+
+window.toggleCheckRows = function(idList)
+{
+    if(!idList?.length) return;
+    const dtable = zui.DTable.query($('#tasks'));
+    dtable.$.toggleCheckRows(idList.split(','), true);
+}
