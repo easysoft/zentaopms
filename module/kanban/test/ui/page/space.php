a93b54597b26e1b2e0f0c5c6ff1bd325b094deb5
lixiaoyu <lixiaoyu@easycorp.ltd>
2025-01-03 16:06:57 +0800

    *[task#136202,doing,0.5h,3h] add xpath of create kanban space

--- module/kanban/test/ui/page/space.php
+++ module/kanban/test/ui/page/space.php
@@ -1,4 +1,12 @@
 <?php
 class spacePage extends page
 {
+    public function __construct($webdriver)
+    {
+        parent::__construct($webdriver);
+        $xpath = array(
+            'spaceName' => '//*[@id="mainContent"]/div[1]/div[1]/div[1]'
+        );
+        $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
+    }
 }
