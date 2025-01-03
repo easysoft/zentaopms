2d0b67f81ada8910295924d73adb3df0e3925dce
yulujie <yulujie@easycorp.ltd>
2025-01-03 14:51:38 +0800

    * [task#136195,doing,0.1h,3.7h] Add project data.

--- module/testtask/test/ui/linkcase.php
+++ module/testtask/test/ui/linkcase.php
@@ -15,3 +15,18 @@ $product->id->range('1-100');
 $product->name->range('产品1, 产品2');
 $product->type->range('normal');
 $product->gen(1);
+
+$project = zenData('project');
+$project->id->range('1-100');
+$project->project->range('0, 1{3}');
+$project->model->range('scrum, []{3}');
+$project->type->range('project, sprint{3}');
+$project->auth->range('extend, []{3}');
+$project->storytype->range('`story,epic,requirement`');
+$project->path->range('`,1,`, `,1,2,`, `,1,3,`, `,1,4,`');
+$project->grade->range('1');
+$project->name->range('项目1, 项目1执行1, 项目1执行2, 项目1执行3');
+$project->hasProduct->range('1');
+$project->status->range('wait');
+$project->acl->range('open');
+$project->gen(2);
