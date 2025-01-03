e396c8f05861e7a816cd836abef9bc3028cfd3c9
yulujie <yulujie@easycorp.ltd>
2025-01-03 13:49:58 +0800

    * [task#136196,doing,0.1h,2.9h] Add product data.

---
+++ module/testtask/test/ui/start.php
@@ -0,0 +1,17 @@
+#!/usr/bin/env php
+<?php
+
+/**
+title=开始测试单
+timeout=0
+cid=1
+ */
+
+chdir(__DIR__);
+include '../lib/start.ui.class.php';
+
+$product = zenData('product');
+$product->id->range('1-100');
+$product->name->range('产品1, 产品2');
+$product->type->range('normal');
+$product->gen(2);
