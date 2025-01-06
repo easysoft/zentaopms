638872890f23ea8a50da505de2f4f99d43f9be10
lihuifang <lihuifang@easycorp.ltd>
2025-01-06 16:59:31 +0800

    *[task#136313,doing,0.3h,1.7h] Add product zendata.

--- module/release/test/ui/editrelease.php
+++ module/release/test/ui/editrelease.php
@@ -18,3 +18,23 @@ cid=73
  - 最终测试状态 @SUCCESS
 - 编辑发布，修改名称、状态改为停止维护
  - 测试结果 @编辑发布成功
+ - 最终测试状态 @SUCCESS
+- 编辑发布，修改名称、状态改为停止维护
+ - 测试结果 @编辑发布成功
+ - 最终测试状态 @SUCCESS
+
+*/
+chdir(__DIR__);
+include '../lib/editrelease.ui.class.php';
+
+$product = zenData('product');
+$product->id->range('1');
+$product->name->range('产品1');
+$product->type->range('normal');
+$product->gen(1);
+
+$system = zenData('system');
+$system->id->range('1,2');
+$system->product->range('1');
+$system->name->range('应用AAA, 应用BBB');
+$system->status->range('active');
