a58e3c16e095e4e54f4d90acc39221389554bf05
liyang <liyang@easycorp.ltd>
2025-01-06 16:55:55 +0800

    * [task#135727] add count of yearly delete rows in user metric.

--- module/metric/calc/user/rate/count_of_yearly_delete_rows_in_user.php
+++ module/metric/calc/user/rate/count_of_yearly_delete_rows_in_user.php
@@ -35,3 +35,16 @@ class count_of_yearly_delete_rows_in_user extends baseCalc
         if(!$row->email) $row->email = $row->account . '@gitfox.io';
         $this->rows[] = $row;
     }
+
+    /**
+     * 通过API获取提交次数。
+     * Get commits by API.
+     *
+     * @param  object $repo
+     * @param  string $begin
+     * @param  string $end
+     * @access public
+     * @return object
+     */
+    public function getUserCommitCount($account, $begin, $end)
+    {
