78ebf13af909c99938ae8759253a39ee48417e5c
yulujie <yulujie@easycorp.ltd>
2025-01-06 16:35:31 +0800

    * [task#136194,doing,0.1h,1.6h] Add xpath.

--- module/testtask/test/ui/page/cases.php
+++ module/testtask/test/ui/page/cases.php
@@ -10,9 +10,12 @@ class casesPage extends page
             'num'         => "//*[@id='table-testtask-cases']/div[3]/div[2]/strong",
             'firstSuite'  => "//menu/menu/li[1]/a/div/div",
             /* 列表 */
-            'firstCheckbox'  => "//*[@id='table-testtask-cases']/div[2]/div[1]/div/div[1]/div/div",
-            'dropDownBtn'    => "//*[@id='table-testtask-cases']/div[3]/nav[1]/nav/button[2]",
-            'batchUnlinkBtn' => "//menu/menu/li[1]/a/div/div"
+            'firstCheckbox'      => "//*[@id='table-testtask-cases']/div[2]/div[1]/div/div[1]/div/div",
+            'firstAssignedTo'    => "//*[@id='table-testtask-cases']/div[2]/div[2]/div/div[2]/div/a/span",
+            'dropDownBtn'        => "//*[@id='table-testtask-cases']/div[3]/nav[1]/nav/button[2]",
+            'batchUnlinkBtn'     => "//menu/menu/li[1]/a/div/div",
+            'batchAssignedToBtn' => "//*[@id='table-testtask-cases']/div[3]/nav[1]/button[1]",
+            'secondUser'         => "//menu/menu/li[2]/a/div/div"
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
