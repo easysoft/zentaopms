63f913c0fd39dd11fbfdbe344481fe8f21503905
daitingting <daitingting@xirangit.com>
2025-01-06 08:17:32 +0000

    * [task#135776,done,1h] Set bug list block events.

---
+++ module/productplan/js/bug.ui.js
@@ -0,0 +1,35 @@
+window.insertListToDoc = function()
+{
+    const dtable      = zui.DTable.query($('#bugs'));
+    const checkedList = dtable.$.getChecks();
+    if(!checkedList.length) return;
+
+    let {cols, data} = dtable.options;
+    data = data.filter((item) => checkedList.includes(item.id + ''));
+    const docID = getDocApp()?.docID;
+
+    let blockType = 'planBug';
+    const url = $.createLink('doc', 'buildZentaoList', `docID=${docID}&type=${blockType}&blockID=${blockID}`);
+    const formData = new FormData();
+    formData.append('cols', JSON.stringify(cols));
+    formData.append('data', JSON.stringify(data));
+    formData.append('idList', checkedList.join(','));
+    formData.append('url', insertListLink);
+    $.post(url, formData, function(resp)
+    {
+        resp = JSON.parse(resp);
+        if(resp.result == 'success')
+        {
+            const blockID = resp.blockID;
+            zui.Modal.hide();
+            window.insertZentaoList && window.insertZentaoList(blockType, blockID, null, true);
+        }
+    });
+}
+
+window.toggleCheckRows = function(idList)
+{
+    if(!idList?.length) return;
+    const dtable = zui.DTable.query($('#bugs'));
+    dtable.$.toggleCheckRows(idList.split(','), true);
+}
