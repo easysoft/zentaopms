13551730d3109be79843cc4783033eb3f2e22c84
liyang <liyang@easycorp.ltd>
2025-01-06 11:14:56 +0800

    * [task#135728] add count of yearly delete rows in codebase metric.

--- module/metric/calc/repo/rate/count_of_yearly_delete_rows_in_codebase.php
+++ module/metric/calc/repo/rate/count_of_yearly_delete_rows_in_codebase.php
@@ -18,3 +18,23 @@
  * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
  * @Link      https://www.zentao.net
  */
+class count_of_yearly_delete_rows_in_codebase extends baseCalc
+{
+    public $useSCM = true;
+
+    public $result = array();
+
+    /**
+     * 通过API获取提交次数。
+     * Get commits by API.
+     *
+     * @param  object $repo
+     * @param  string $begin
+     * @param  string $end
+     * @access public
+     * @return object
+     */
+    public function getCommitCount($repo, $begin, $end)
+    {
+    }
+}
