7f6bf3280052f6c4a998700925659c1c42330efa
yulujie <yulujie@easycorp.ltd>
2025-01-06 14:34:28 +0800

    * [task#136194,doing,0.2h,2.7h] Add function.

--- module/testtask/test/lib/cases.ui.class.php
+++ module/testtask/test/lib/cases.ui.class.php
@@ -2,4 +2,23 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class casesTester extends tester
 {
+    /**
+     * 检查测试单下不同标签下的数据。
+     * Check the data of testtasks under different tags.
+     *
+     * @param  string $tags
+     * @param  string $num
+     * @access public
+     * @return void
+     */
+    public function checkTagData($tags, $num)
+    {
+        $form = $this->initForm('testtask', 'cases', array('taskID' => '1'), 'appIframe-qa');
+        if($tags == 'all')
+        {
+            $form->dom->btn($this->lang->testtask->allCases)->click();
+            if($form->dom->num->getText() == $num) $this->success('标签下数据统计正确');
+            return $this->failed('标签下数据统计错误');
+        }
+    }
 }
