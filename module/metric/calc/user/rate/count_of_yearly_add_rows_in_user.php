440d5dea7b07216cadda7a3103f6234ead7459bd
liyang <liyang@easycorp.ltd>
2025-01-06 16:31:51 +0800

    * [task#135720] add count of yearly add rows in user metric.

---
+++ module/metric/calc/user/rate/count_of_yearly_add_rows_in_user.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 按人员统计的年度代码新增行数。
+ * Count of year commits in user.
+ *
+ * 范围：user
+ * 对象：commit
+ * 目的：rate
+ * 度量名称：按人员统计的年度代码新增行数
+ * 单位：行
+ * 描述：按人员统计的年度代码新增行数是指单个提交人每年的全部代码提交新增的代码行数量。代码提交次数反映了提交人的年开发活动频率和代码更新情况。
+ * 定义：所有代码提交新增行数求和 提交人为某人  时间为某年。
+ *
+ * @copyright Copyright 2009-2024 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @author    Yang Li <liyang@chandao.com>
+ * @package
+ * @uses      func
+ * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @Link      https://www.zentao.net
+ */
