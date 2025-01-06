7f0756cd53ffa813d08eee6f07a0e551a9287ee6
liumengyi <liumengyi@easycorp.ltd>
2025-01-06 15:39:33 +0800

    * [bug#57250,start,2h] reload with fromID and fromType.

--- module/bug/js/create.ui.js
+++ module/bug/js/create.ui.js
@@ -1,4 +1,7 @@
 window.reloadByProduct = function(e)
 {
-    loadPage($.createLink('bug', 'create', 'productID=' + $(e.target).val() + '&branch=0&extra=projectID=' + projectID + ',executionID=' + executionID));
+    let extra = 'projectID=' + projectID + ',executionID=' + executionID;
+    if(typeof fromID != 'undefined') extra += ',fromID=' + fromID;
+    if(typeof fromType != 'undefined') extra += ',fromType=' + fromType;
+    loadPage($.createLink('bug', 'create', 'productID=' + $(e.target).val() + '&branch=0&extra=' + extra));
 }
