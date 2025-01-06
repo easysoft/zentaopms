41730c93d67e5ccc53f056817e61f73b4d4c2741
yulujie <yulujie@easycorp.ltd>
2025-01-06 14:57:17 +0800

    * [task#136194,doing,0.2h,2.7h] Add xpath.

--- module/testtask/test/ui/page/cases.php
+++ module/testtask/test/ui/page/cases.php
@@ -6,7 +6,8 @@ class casesPage extends page
         parent::__construct($webdriver);
         $xpath = array(
             'allCasesNum' => "//*[@id='featureBar']/menu/li[1]/a/span[2]",
-            'num'         => "//*[@id='table-testtask-cases']/div[3]/div[2]/strong"
+            'num'         => "//*[@id='table-testtask-cases']/div[3]/div[2]/strong",
+            'firstSuite'  => "//menu/menu/li[1]/a/div/div"
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
