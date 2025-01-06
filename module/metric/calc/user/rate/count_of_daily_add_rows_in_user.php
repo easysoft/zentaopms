f89dc1fce1b52d9c19ffcc993777d8dd6c3a9f5b
liyang <liyang@easycorp.ltd>
2025-01-06 16:17:25 +0800

    * [task#135717] add count of daily add rows in user metric.

--- module/metric/calc/user/rate/count_of_daily_add_rows_in_user.php
+++ module/metric/calc/user/rate/count_of_daily_add_rows_in_user.php
@@ -64,3 +64,23 @@ class count_of_daily_add_rows_in_user extends baseCalc
 
         $result = $this->scm->engine->getCodeFrequencyByUser($account->email, 'day', $begin, $end);
         if(empty($result)) return false;
+        foreach($result->stats as $stats)
+        {
+            $account->time      = $stats->key;
+            $account->additions = $stats->additions;
+            $this->setResult($account);
+        }
+
+        $this->setResult($repo);
+    }
+
+    /**
+     * 设置结果集。
+     * Set result set.
+     *
+     * @param  object $row
+     * @access public
+     * @return void
+     */
+    public function setResult($row)
+    {
