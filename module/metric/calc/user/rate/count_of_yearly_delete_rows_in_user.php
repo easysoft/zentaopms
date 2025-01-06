eb4fb5a7b6998ccf36ab83be6c09b70d9ed9b39a
liyang <liyang@easycorp.ltd>
2025-01-06 16:55:32 +0800

    * [task#135727] add count of yearly delete rows in user metric.

---
+++ module/metric/calc/user/rate/count_of_yearly_delete_rows_in_user.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 按人员统计的年度代码删除行数。
+ * Count of year deletions in user.
+ *
+ * 范围：user
+ * 对象：commit
+ * 目的：rate
+ * 度量名称：按人员统计的年度代码删除行数
+ * 单位：行
+ * 描述：按人员统计的年度代码删除行数是指单个提交人每年的全部代码提交删除的代码行数量。代码提交次数反映了提交人的年开发活动频率和代码更新情况。
+ * 定义：所有代码删除新增行数求和 提交人为某人  时间为某年。
+ *
+ * @copyright Copyright 2009-2024 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @author    Yang Li <liyang@chandao.com>
+ * @package
+ * @uses      func
+ * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @Link      https://www.zentao.net
+ */
