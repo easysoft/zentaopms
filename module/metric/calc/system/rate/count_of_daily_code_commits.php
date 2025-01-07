f8edf74f41087623a90e0690dfd769e231f80bb8
liyang <liyang@easycorp.ltd>
2025-01-07 09:22:48 +0800

    * [task#135715] add count of daily code commits metric.

---
+++ module/metric/calc/system/rate/count_of_daily_code_commits.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 按系统统计的每日代码提交次数
+ * Count of daily commits.
+ *
+ * 范围：repo
+ * 对象：commit
+ * 目的：rate
+ * 度量名称：按系统统计的每日代码提交次数
+ * 单位：次
+ * 描述：按系统统计的每日代码提交次数是指本系统每日的全部代码提交的数量，这个度量项可以反映组织的每日开发活动频率和代码更新情况。
+ * 定义：所有代码库的代码提交次数求和，提交时间为某日。
+ *
+ * @copyright Copyright 2009-2024 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @author    Yang Li <liyang@chandao.com>
+ * @package
+ * @uses      func
+ * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @Link      https://www.zentao.net
+ */
