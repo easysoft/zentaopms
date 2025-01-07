4f1e6834bd1cf18e0690ecb51cfdc3e096f50c97
sunxiaoqi <sunxiaoqi@easycorp.ltd>
2025-01-07 10:10:13 +0800

    * [task#136330,doing,0.1h] Add ui test files of mode management

--- module/admin/test/ui/modemanagement.php
+++ module/admin/test/ui/modemanagement.php
@@ -9,3 +9,21 @@ timeout=0
 - 切换轻量级模式，切换成功
  - 测试结果 @切换轻量级模式成功
  - 最终测试状态 @SUCCESS
+
+*/
+chdir(__DIR__);
+include '../lib/modemanagement.ui.class.php';
+
+$program = zenData('project');
+$program->id->range('1-3');
+$program->type->range('program');
+$program->name->range('项目集A,项目集B,项目集C');
+$program->begin->range('2025-01-01');
+$program->end->range('2025-12-31');
+$program->status->range('doing');
+$program->gen(3);
+
+$tester = new modemanagementTester();
+$tester->login();
+
+$programName = new stdClass();
