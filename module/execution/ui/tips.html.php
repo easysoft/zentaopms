864f9f8455e164e74b949cd5fdabde2bbbdeaa52
liumengyi <liumengyi@easycorp.ltd>
2025-01-09 18:29:11 +0800

    * [task#136279,done,0.3h] set target in execution::tips.

--- module/execution/ui/tips.html.php
+++ module/execution/ui/tips.html.php
@@ -36,31 +36,36 @@ panel
         (
             set::className('mr-2 tipBtn ml-1'),
             $lang->execution->setTeam,
-            set('data-url', createLink('execution', 'team', "executionID={$executionID}"))
+            set::target('_blank'),
+            set::url(createLink('execution', 'team', "executionID={$executionID}"))
         ),
         $execution->lifetime != 'ops' ? btn
         (
             set::className('mr-2 tipBtn linkstory-btn'),
             $lang->execution->linkStory,
-            set('data-url', createLink('execution', 'linkstory', "executionID=$executionID"))
+            set::target('_blank'),
+            set::url(createLink('execution', 'linkstory', "executionID=$executionID"))
         ) : null,
         btn
         (
             set::className('mr-2 tipBtn'),
             $lang->execution->createTask,
-            set('data-url', createLink('task', 'create', "execution=$executionID"))
+            set::target('_blank'),
+            set::url(createLink('task', 'create', "execution=$executionID"))
         ),
         btn
         (
             set::className('mr-2 tipBtn'),
             $lang->execution->goback,
-            set('data-url', createLink('execution', 'task', "executionID={$executionID}"))
+            set::target('_blank'),
+            set::url(createLink('execution', 'task', "executionID={$executionID}"))
         ),
         btn
         (
             set::className('tipBtn'),
             $lang->execution->gobackExecution,
-            set('data-url', createLink('execution', 'all'))
+            set::target('_blank'),
+            set::url(createLink('execution', 'all'))
         )
     )
 );
