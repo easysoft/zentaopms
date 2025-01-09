4d7629ed9e9092d1a4a4bee06acf5a0019fadda5
liumengyi <liumengyi@easycorp.ltd>
2025-01-09 09:35:50 +0800

    * [bug#59063,done,0.5h] check post value of precondition.

--- module/testcase/zen.php
+++ module/testcase/zen.php
@@ -3255,7 +3255,7 @@ class testcaseZen extends testcase
         if(!$this->testcase->forceNotReview() && $stepChanged) $status = 'wait';
 
         if($this->post->title && $case->title != $this->post->title) $stepChanged = true;
-        if($this->post->precondition && $case->precondition != $this->post->precondition) $stepChanged = true;
+        if(isset($_POST['precondition']) && $case->precondition != $this->post->precondition) $stepChanged = true;
         if(!empty($_FILES['files']['name'][0])) $stepChanged = true;
         if(!empty($_POST['deleteFiles'])) $stepChanged = true;
 
