6e863d8e211bbd98e329bf819550031fb4aee600
liumengyi <liumengyi@easycorp.ltd>
2025-01-15 14:15:18 +0800

    + [task#40664,doing,0.2h] add unittest of execution::getToAndCcList

--- module/execution/test/model/gettoandcclist.php
+++ module/execution/test/model/gettoandcclist.php
@@ -9,6 +9,22 @@ title=测试 executionModel::getToAndCcList();
 timeout=0
 cid=1
 
+- 获取执行 101 的通知人员
+ -  @user4
+ - 属性1 @~~
+- 获取执行 102 的通知人员
+ -  @user5
+ - 属性1 @~~
+- 获取执行 103 的通知人员
+ -  @user6
+ - 属性1 @~~
+- 获取执行 104 的通知人员
+ -  @user7
+ - 属性1 @~~
+- 获取执行 105 的通知人员
+ -  @user8
+ - 属性1 @~~
+
 */
 
 su('admin');
@@ -17,3 +33,11 @@ zenData('team')->loadYaml('team')->gen(10);
 zenData('user')->gen(110);
 
 $executionIdList = array(101, 102, 103, 104, 105);
+
+$executionTester = new executionTest();
+
+r($executionTester->getToAndCcListTest($executionIdList[0])) && p('0|1', '|') && e('user4|~~'); // 获取执行 101 的通知人员
+r($executionTester->getToAndCcListTest($executionIdList[1])) && p('0|1', '|') && e('user5|~~'); // 获取执行 102 的通知人员
+r($executionTester->getToAndCcListTest($executionIdList[2])) && p('0|1', '|') && e('user6|~~'); // 获取执行 103 的通知人员
+r($executionTester->getToAndCcListTest($executionIdList[3])) && p('0|1', '|') && e('user7|~~'); // 获取执行 104 的通知人员
+r($executionTester->getToAndCcListTest($executionIdList[4])) && p('0|1', '|') && e('user8|~~'); // 获取执行 105 的通知人员
