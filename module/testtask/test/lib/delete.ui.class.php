38bf073089b470d0e91ef9c723fe5c6eddb07566
yulujie <yulujie@easycorp.ltd>
2025-01-16 14:35:42 +0800

    * [task#136820,doing,0.6h,0.8h] Add function.

--- module/testtask/test/lib/delete.ui.class.php
+++ module/testtask/test/lib/delete.ui.class.php
@@ -2,4 +2,24 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class deleteTester extends tester
 {
+    /**
+     * 删除测试单。
+     * Delete testtask.
+     *
+     * @access public
+     * @return void
+     */
+    public function deleteTest()
+    {
+        $form = $this->initForm('testtask', 'browse', array('productID' => '1'), 'appIframe-qa');
+        $num  = $form->dom->totalNum->getText();
+        $id   = $form->dom->firstID->getText();
+        $form->dom->firstDeleteBtn->click();
+        $form->wait(1);
+        if($$form->dom->totalNum->getText() != $num - 1) return $this->failed('删除测试单失败');
+
+        $form = $this->initForm('testtask', 'view', array('taskID' => $id), 'appIframe-qa');
+        if(is_object($form->dom->deletedLabel) && $form->dom->deletedLabel->getText() == $this->lang->testtask->deleted) return $this->success('删除测试单成功');
+        return $this->failed('删除测试单后概况页没有显示已删除标签');
+    }
 }
