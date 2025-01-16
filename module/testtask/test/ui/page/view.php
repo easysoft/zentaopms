dc64bb47598e4eac89d027327b1809e56ca42bb6
yulujie <yulujie@easycorp.ltd>
2025-01-16 13:16:48 +0800

    * [task#136813,doing,0.1h,2.0h] Add xpath.

--- module/testtask/test/ui/page/view.php
+++ module/testtask/test/ui/page/view.php
@@ -7,7 +7,8 @@ class viewPage extends page
         $xpath = array(
             'begin'     => "//*[@id='basicInfo']/table/tbody/tr[7]/td",
             'status'    => "//*[@id='basicInfo']/table/tbody/tr[10]/td",
-            'submitBtn' => "//button[@type='submit']"
+            'submitBtn' => "//button[@type='submit']",
+            'buttons'   => "//*[@id='mainContent']/div[2]/div[1]/div[3]/div"
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
