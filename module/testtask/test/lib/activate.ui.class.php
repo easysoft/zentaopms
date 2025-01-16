d6d53ecbac82272081a377af6788763b25069440
yulujie <yulujie@easycorp.ltd>
2025-01-16 10:33:15 +0800

    * [task#136813,doing,0.3h,2.3h] Add activate function of uitest.

--- module/testtask/test/lib/activate.ui.class.php
+++ module/testtask/test/lib/activate.ui.class.php
@@ -2,4 +2,21 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class activateTester extends tester
 {
+    /**
+     * 激活测试单
+     *
+     * @param  string $testtaskID
+     * @access public
+     * @return void
+     */
+    public function activateTest($testtaskID)
+    {
+        $form   = $this->initForm('testtask', 'view', array('taskID' => $testtaskID), 'appIframe-qa');
+        $status = $form->dom->status->getText();
+        if($status == $this->lang->testtask->statusList->wait || $status == $this->lang->testtask->statusList->doing)
+        {
+            if(!is_object($form->dom->btn($this->lang->testtask->activate))) return $this->success('未开始和进行中的测试单不显示激活按钮');
+            return $this->failed('未开始和进行中的测试单错误的显示了激活按钮');
+        }
+    }
 }
