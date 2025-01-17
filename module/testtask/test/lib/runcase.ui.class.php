b5a83fa0b144cf1c0be8d2ad729a670fa6f7e91f
yulujie <yulujie@easycorp.ltd>
2025-01-17 17:18:01 +0800

    * [task#136865,doing,1h,1.0h] Add function.

--- module/testtask/test/lib/runcase.ui.class.php
+++ module/testtask/test/lib/runcase.ui.class.php
@@ -2,4 +2,28 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class runCaseTester extends tester
 {
+    /**
+     * 单个执行测试用例.
+     * Run testcase.
+     *
+     * @param  bool   $hasStep
+     * @param  string $expectRusult
+     * @access public
+     * @return void
+     */
+    public function runCase($hasStep, $expectRusult)
+    {
+        $form = $this->initForm('testtask', 'cases', array('taskID' => '1'), 'appIframe-qa');
+        if($hasStep)  $form->dom->lastRunBtn->click();
+        if(!$hasStep) $form->dom->firstRunBtn->click();
+        $form->wait(1);
+        $form->dom->rusult->picker($this->lang->testcase->resultList->$expectRusult);
+        $form->dom->submitBtn->click();
+        $form->wait(1);
+
+        if($hasStep)  $result = $form->dom->lastResult->getText();
+        if(!$hasStep) $result = $form->dom->firstResult->getText();
+        if($result == $this->lang->testcase->resultList->$expectRusult) return $this->success('用例执行成功');
+        return $this->failed('用例执行失败');
+    }
 }
