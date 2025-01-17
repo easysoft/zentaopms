9e25565381986874bba9ba7c5f5f4b6bdd12437a
yulujie <yulujie@easycorp.ltd>
2025-01-17 10:58:12 +0800

    * [task#136856,doing,0.3h,2.5h] Add xpath.

--- module/testtask/test/ui/page/browse.php
+++ module/testtask/test/ui/page/browse.php
@@ -8,13 +8,18 @@ class browsePage extends page
             'dropMenu'   => "//*[@id='mainMenu']/button",
             'allProduct' => "//li[@class='menu-item item'][1]",
             /* 标签 */
-            'totalNum'   => "//*[@data-id='totalStatus']/span[2]",
-            'myinvolved' => "//*[@data-id='myinvolved']/span[2]",
-            'wait'       => "//*[@data-id='wait']/span[2]",
-            'doing'      => "//*[@data-id='doing']/span[2]",
-            'blocked'    => "//*[@data-id='blocked']/span[2]",
-            'done'       => "//*[@data-id='done']/span[2]",
-            'startTime'  =>
+            'myinvolved' => "//*[@data-id='myinvolved']",
+            'wait'       => "//*[@data-id='wait']",
+            'doing'      => "//*[@data-id='doing']",
+            'blocked'    => "//*[@data-id='blocked']",
+            'done'       => "//*[@data-id='done']",
+            /* 标签后数量 */
+            'totalNum'      => "//*[@data-id='totalStatus']/span[2]",
+            'myinvolvedNum' => "//*[@data-id='myinvolved']/span[2]",
+            'waitNum'       => "//*[@data-id='wait']/span[2]",
+            'doingNum'      => "//*[@data-id='doing']/span[2]",
+            'blockedNum'    => "//*[@data-id='blocked']/span[2]",
+            'doneNum'       => "//*[@data-id='done']/span[2]",
             /* 列表 */
             'firstID'        => "//*[@id='table-testtask-browse']/div[2]/div[1]/div/div[1]/div",
             'firstName'      => "//*[@id='table-testtask-browse']/div[2]/div[1]/div/div[2]/div/a",
