0cf07beb65f9bcdea4a99864fcb4886299cd7aa2
yulujie <yulujie@easycorp.ltd>
2025-01-17 15:07:36 +0800

    * [task#136856,done,0.7h] Add function.

--- module/testtask/test/lib/browse.ui.class.php
+++ module/testtask/test/lib/browse.ui.class.php
@@ -14,7 +14,7 @@ class browseTester extends tester
      * @access public
      * @return void
      */
-    public function checkTestTaskNum($tab, $num, $startTime = '', $endTime = '', $allProduct = false)
+    public function checkNum($tab, $num, $allProduct = false, $startTime = '', $endTime = '')
     {
         $form = $this->initForm('testtask', 'browse', array('productID' => '1'), 'appIframe-qa');
         if($allProduct)
@@ -23,16 +23,16 @@ class browseTester extends tester
             $form->dom->allProduct->click();
             $form->wait(1);
         }
+        $form->dom->$tab->click();
+        $form->wait(1);
         if(!empty($startTime) || !empty($endTime))
         {
-            $form->dom->begin->setValue($startTime);
-            $form->dom->end->setValue($endTime);
+            $form->dom->begin->datePicker($startTime);
+            $form->dom->end->datePicker($endTime);
             $form->wait(1);
         }
-        $form->dom->${tab}->click();
-        $form->wait(1);
-        $tabNum = $tab . 'Num';
-        if($form->dom->$tabNum->getText() == $num) return $this->success('标签下的测试单数量正确');
+        if(!is_object($form->dom->num)) return $this->success('测试单数量为0');
+        if($form->dom->num->getText() == $num) return $this->success('标签下的测试单数量正确');
         return $this->failed('标签下的测试单数量不正确');
     }
 }
