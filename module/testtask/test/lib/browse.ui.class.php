74abe46e0c21bd82ade197e0ddf85a7ba3c8da9f
yulujie <yulujie@easycorp.ltd>
2025-01-17 10:37:07 +0800

    * [task#136856,doing,0.5h,2.8h] Add function.

--- module/testtask/test/lib/browse.ui.class.php
+++ module/testtask/test/lib/browse.ui.class.php
@@ -2,4 +2,26 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class browseTester extends tester
 {
+    /**
+     * 检查产品下不同标签下的测试单数量。
+     * Check the number of testtask under different tags of a product.
+     *
+     * @param  bool   $allProduct
+     * @param  string $startTime
+     * @param  string $endTime
+     * @param  string $tab
+     * @param  int    $num
+     * @access public
+     * @return void
+     */
+    public function checkTestTaskNum($tab, $num, $startTime = '', $endTime = '', $allProduct = false)
+    {
+        $form = $this->initForm('testtask', 'browse', array('productID' => '1'), 'appIframe-qa');
+        if($allProduct)
+        {
+            $form->dom->dropMenu->click();
+            $form->dom->allProduct->click();
+            $form->wait(1);
+        }
+    }
 }
