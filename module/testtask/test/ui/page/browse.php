b8d47ffe380c89634563d4050ead34f63957560c
yulujie <yulujie@easycorp.ltd>
2025-01-17 10:36:36 +0800

    * [task#136856,doing,0.3h,3.3h] Add xpath.

--- module/testtask/test/ui/page/browse.php
+++ module/testtask/test/ui/page/browse.php
@@ -5,7 +5,17 @@ class browsePage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'totalNum'       => "//*[@data-id='totalStatus']/span[2]",
+            'dropMenu'   => "//*[@id='mainMenu']/button",
+            'allProduct' => "//li[@class='menu-item item'][1]",
+            /* 标签 */
+            'totalNum'   => "//*[@data-id='totalStatus']/span[2]",
+            'myinvolved' => "//*[@data-id='myinvolved']/span[2]",
+            'wait'       => "//*[@data-id='wait']/span[2]",
+            'doing'      => "//*[@data-id='doing']/span[2]",
+            'blocked'    => "//*[@data-id='blocked']/span[2]",
+            'done'       => "//*[@data-id='done']/span[2]",
+            'startTime'  =>
+            /* 列表 */
             'firstID'        => "//*[@id='table-testtask-browse']/div[2]/div[1]/div/div[1]/div",
             'firstName'      => "//*[@id='table-testtask-browse']/div[2]/div[1]/div/div[2]/div/a",
             'firstDeleteBtn' => "//*[@id='table-testtask-browse']/div[2]/div[3]/div/div[1]/div/nav/a[last()]"
