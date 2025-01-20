767514f8764bd42f6919282397a600c6e7b3a683
yulujie <yulujie@easycorp.ltd>
2025-01-20 13:13:20 +0800

    * [task#136865,doing,0.7h,1.8h] Modify xpath of runcase.

--- module/testtask/test/ui/page/cases.php
+++ module/testtask/test/ui/page/cases.php
@@ -13,16 +13,17 @@ class casesPage extends page
             'firstCheckbox'      => "//*[@id='table-testtask-cases']/div[2]/div[1]/div/div[1]/div/div",
             'firstAssignedTo'    => "//*[@id='table-testtask-cases']/div[2]/div[2]/div/div[2]/div/a/span",
             'firstResult'        => "(//div[@data-type='status'])[2]",
-            'firstRunBtn'        => "//*[@id='table-testtask-cases']/div[2]/div[3]/div/div[1]//a[1]",
+            'firstRunBtn'        => "//*[@id='table-testtask-cases']/div[2]/div[3]/div/div[1]/div/nav//a[contains(@href, 'index.php?m=testtask&f=runCase')]",
             'dropDownBtn'        => "//*[@id='table-testtask-cases']/div[3]/nav[1]/nav/button[2]",
             'batchUnlinkBtn'     => "//menu/menu/li[1]/a/div/div",
             'batchAssignedToBtn' => "//*[@id='table-testtask-cases']/div[3]/nav[1]/button[1]",
             'secondUser'         => "//menu/menu/li[2]/a/div/div",
             'lastResult'         => "(//div[@data-type='status'])[last()]",
-            'lastRunBtn'         => "//*[@id='table-testtask-cases']/div[2]/div[3]/div/div[last()]//a[1]",
+            'lastRunBtn'         => "//*[@id='table-testtask-cases']/div[2]/div[3]/div/div[5]/div/nav//a[contains(@href, 'index.php?m=testtask&f=runCase')]",
             /* 执行弹窗 */
-            'rusult'    => "//*[@name='result[0]']",
-            'submitBtn' => "//button[@type='submit']"
+            'result'    => "//*[@id='caseStepForm']/table/tbody/tr[1]/td[3]/div/div/input",
+            'submitBtn' => "//*[@id='caseStepForm']//button[@type='submit']",
+            'close'     => "//*[@id='runCaseModal']/div/div[2]/button/span"
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
