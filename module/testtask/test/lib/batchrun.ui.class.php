a7d148e88092a7790d228d5a1f776818cb17b5c5
yulujie <yulujie@easycorp.ltd>
2025-01-20 13:59:07 +0800

    * [task#136887,doing,0.1h,2.5h] Add class of batch run cases.

---
+++ module/testtask/test/lib/batchrun.ui.class.php
@@ -0,0 +1,17 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+class batchRunTester extends tester
+{
+    /**
+     * 批量执行测试用例.
+     * Batch run testcase.
+     *
+     * @param  bool   $hasStep
+     * @param  string $expectRusult
+     * @access public
+     * @return void
+     */
+    public function batchRun($hasStep, $expectRusult)
+    {
+    }
+}
