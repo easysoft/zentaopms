d8e04215f916b3f01a2b2c530dfac03e0f49f5a2
huairong <huairong@easycorp.ltd>
2025-01-21 15:50:29 +0800

    + [task#136907,doing,0.1h,2.4h] add element of browse page.

--- module/stakeholder/test/ui/page/browse.php
+++ module/stakeholder/test/ui/page/browse.php
@@ -7,8 +7,10 @@ class browsePage extends page
 
         $xpath = array(
             'title'       => "//*[@id='table-stakeholder-browse']/div[2]/div[1]/div/div[2]/div/a",
+            'type'        => "//*[@id='table-stakeholder-browse']/div[2]/div[2]/div/div[1]/div",
             'number'      => "//*[@id='table-stakeholder-browse']/div[3]/nav/div[1]",
             'communicate' => "//*[@id='table-stakeholder-browse']/div[2]/div[3]/div/div/div/nav/a[1]/i",
+            'expect'      => "//*[@id='table-stakeholder-browse']/div[2]/div[3]/div/div/div/nav/a[2]/i",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
