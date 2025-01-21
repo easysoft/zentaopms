2814ccaeb9f54f4a1f2f9040604d686d5deed5b4
huairong <huairong@easycorp.ltd>
2025-01-21 15:58:24 +0800

    + [task#136907,doing,0.3h,1.9h] add expect function.

---
+++ module/stakeholder/test/lib/expect.ui.class.php
@@ -0,0 +1,18 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+class expectTester extends tester
+{
+    public function expect($stakeholder)
+    {
+        $form = $this->initForm('stakeholder', 'browse', array('project' => 1), 'appIframe-project');
+        $form->dom->expect->click();
+        $expectForm = $this->loadPage('stakeholder', 'expect');
+        $form->wait(1);
+        if(isset($stakeholder['expectComment'])) $expectForm->dom->expectComment->setValueInZenEditor($stakeholder['expectComment']);
+        if(isset($stakeholder['progress']))      $expectForm->dom->progress->setValueInZenEditor($stakeholder['progress']);
+        $form->wait(1);
+        $form->dom->btn($this->lang->save)->click();
+        $form->wait(1);
+        return $this->checkResult($expectForm, $stakeholder);
+    }
+}
