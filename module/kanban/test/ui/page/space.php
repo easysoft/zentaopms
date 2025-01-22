b8d560aa78c76f3a1246a36e1f43366a58436596
lixiaoyu <lixiaoyu@easycorp.ltd>
2025-01-22 13:10:53 +0800

    *[task#136927,doing,0.5h,3h] xpath of kanban space

--- module/kanban/test/ui/page/space.php
+++ module/kanban/test/ui/page/space.php
@@ -9,7 +9,14 @@ class spacePage extends page
             'closed'    => '//*[@id="mainContent"]/div[1]/div[1]/div[1]/span',
             'confirm'   => '//button[@z-key="confirm"]',
             /*空间tab*/
-            'involvedNum' => '//*[@id="featureBar"]/menu/li[1]/a/span[2]'
+            'involvedTab'    => '//*[@id="featureBar"]/menu/li[1]/a',
+            'involvedNum'    => '//*[@id="featureBar"]/menu/li[1]/a/span[2]',
+            'cooperationTab' => '//*[@id="featureBar"]/menu/li[2]/a',
+            'cooperationNum' => '//*[@id="featureBar"]/menu/li[2]/a/span[2]',
+            'publicTab'      => '//*[@id="featureBar"]/menu/li[3]/a',
+            'publicNum'      => '//*[@id="featureBar"]/menu/li[3]/a/span[2]',
+            'privateTab'     => '//*[@id="featureBar"]/menu/li[4]/a',
+            'privateNum'     => '//*[@id="featureBar"]/menu/li[4]/a/span[2]'
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
