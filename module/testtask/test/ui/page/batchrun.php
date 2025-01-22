4096f17e68a529fc27e3f42bfa7b37b3bb5367db
yulujie <yulujie@easycorp.ltd>
2025-01-22 10:00:00 +0800

    * [task#136887,doing,0.5h,3h] Add xpath of batchrun.

--- module/testtask/test/ui/page/batchrun.php
+++ module/testtask/test/ui/page/batchrun.php
@@ -6,6 +6,10 @@ class batchRunPage extends page
         parent::__construct($webdriver);
         $xpath = array(
             'modalText' => "//*[@class='modal-content']/div[2]/text()",
+            'pass'      => "//*[@id='zin_testtask_batchrun_form']/table/tbody/tr[1]/td[5]/div/div[1]/label",
+            'fail'      => "//*[@id='zin_testtask_batchrun_form']/table/tbody/tr[1]/td[5]/div/div[2]/label",
+            'blocked'   => "//*[@id='zin_testtask_batchrun_form']/table/tbody/tr[1]/td[5]/div/div[3]/label",
+            'submitBtn' => "//*[@id='zin_testtask_batchrun_form']/div/div/button[1]"
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
