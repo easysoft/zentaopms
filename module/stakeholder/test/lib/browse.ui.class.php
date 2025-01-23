31dd5e5e7eb1bb50307a6ff5f45f2a281cfc565f
huairong <huairong@easycorp.ltd>
2025-01-23 09:07:39 +0800

    + [task#136934,doing,0.3h,1.1h] add assertion of deleteStakeholder function.

--- module/stakeholder/test/lib/browse.ui.class.php
+++ module/stakeholder/test/lib/browse.ui.class.php
@@ -15,5 +15,13 @@ class browseTester extends tester
         $form->dom->deleteBtn->click();
         $form->dom->confirmBtn->click();
         $form->wait(1);
+
+        /* 跳转到干系人详情页，检查是否有已删除标识。 */
+        $viewPage = $this->initForm('stakeholder', 'view', array('id' => 1), 'appIframe-project');
+        $form->wait(1);
+        $deleteFlag = $viewPage->dom->deleteFlag->getText();
+        $form->wait(1);
+        if($deleteFlag != $this->lang->stakeholder->deleted) return $this->failed('删除干系人失败');
+        return $this->success('删除干系人成功');
     }
 }
