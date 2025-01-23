59c9770eba4d1a7969e134c42e6c68a254c645af
huairong <huairong@easycorp.ltd>
2025-01-23 09:03:45 +0800

    + [task#136934,doing,0.1h,1.4h] add element of browse page.

--- module/stakeholder/test/ui/page/browse.php
+++ module/stakeholder/test/ui/page/browse.php
@@ -11,6 +11,8 @@ class browsePage extends page
             'number'      => "//*[@id='table-stakeholder-browse']/div[3]/nav/div[1]",
             'communicate' => "//*[@id='table-stakeholder-browse']/div[2]/div[3]/div/div/div/nav/a[1]/i",
             'expect'      => "//*[@id='table-stakeholder-browse']/div[2]/div[3]/div/div/div/nav/a[2]/i",
+            'deleteBtn'   => "//*[@id='table-stakeholder-browse']/div[2]/div[3]/div/div/div/nav/a[4]/i",
+            'confirmBtn'  => "//*[@class='modal modal-async load-indicator modal-alert modal-trans show in']/div/div/div[3]/nav/button[1]/span",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
