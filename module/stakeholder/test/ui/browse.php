8632da5199a1194e183eb9917b05d827b16dcb0e
huairong <huairong@easycorp.ltd>
2025-01-23 09:13:00 +0800

    + [task#136934,doing,0.2h,0.6h] add comment of browse ui test file.

---
+++ module/stakeholder/test/ui/browse.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+
+/**
+
+title=删除干系人测试
+timeout=0
+cid=6
+
+- 删除干系人成功
+ - 测试结果 @删除干系人成功
+ - 最终测试状态 @ SUCCESS
+
+*/
+
+chdir(__DIR__);
+include '../lib/browse.ui.class.php';
+global $config;
