e13f7a875b7bf0ffcb26e6b1f9848d266424fbbb
luxuyang <luxuyang@easycorp.ltd>
2025-01-26 09:20:06 +0800

    * [task#136929,done,2h,0h] add export testcase ui script.

--- module/testcase/test/ui/page/browse.php
+++ module/testcase/test/ui/page/browse.php
@@ -5,14 +5,17 @@ class browsePage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'caseListID'   => "//a[text()='ID']",
-            'caseName'     => "(//div[@data-col='title']//a[@data-app='qa'])[1]",
-            'caseNameView' => "(//div[@id='mainContent']//span[text()])[3]",
-            'review'       => "(//div[@data-row='case_1']//a[@data-toggle='modal'])[1]",
-            'comment'      => "//zen-editor[@menubar-mode='compact']",
-            'needReview'   => "(//form/div//button[@type='submit'])[2]",
-            'caseAllLabel' => "(//div[@id='mainContent']//div[@id='testcases_table']//label)[last()]",
-            'save'         => "//button[@type='submit']"
+            'caseListID'       => "//a[text()='ID']",
+            'caseName'         => "(//div[@data-col='title']//a[@data-app='qa'])[1]",
+            'caseNameView'     => "(//div[@id='mainContent']//span[text()])[3]",
+            'review'           => "(//div[@data-row='case_1']//a[@data-toggle='modal'])[1]",
+            'comment'          => "//zen-editor[@menubar-mode='compact']",
+            'needReview'       => "(//form/div//button[@type='submit'])[2]",
+            'caseAllLabel'     => "(//div[@id='mainContent']//div[@id='testcases_table']//label)[last()]",
+            'save'             => "//button[@type='submit']",
+            'exportMenu'       => "//button[@type='button' and contains(@zui-toggle-dropdown,'export')]/i",
+            'exportCaseButton' => "//menu/li/a[contains(@href,'=export&')]",
+            'export'           => "//span[text()='导出']"
         );
 
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
