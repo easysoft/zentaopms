fbce9dbaf3f8ff045ed3cf0557278604b92d723c
liyang <liyang@easycorp.ltd>
2025-01-27 09:37:39 +0800

    * [misc] add get app menu method in spider script.

--- test/spider.php
+++ test/spider.php
@@ -4,3 +4,23 @@ include dirname(__FILE__) . '/lib/ui.php';
 $tester = new tester();
 $tester->login();
 sleep(2);
+
+/**
+ * Get all xpath of main menu.
+ *
+ * @param  int    $count
+ * @access public
+ * @return mixed
+ */
+function getAppMenu($mainCount = 15)
+{
+    $xpath = '//*[@id="menuMainNav"]/li[%s]';
+
+    $mainMenu = array();
+    for($i = 1; $i <= $mainCount; $i ++)
+    {
+        $mainMenu[] = sprintf($xpath, $i);
+    }
+
+    return $mainMenu;
+}
