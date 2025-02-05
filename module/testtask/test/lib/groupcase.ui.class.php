68378b4a6d6ac0918a8b493241f2b0cd23d68d22
yulujie <yulujie@easycorp.ltd>
2025-02-05 14:33:58 +0800

    * [story#62102,doing,1h,3.3h] Add function of groupcase function.

--- module/testtask/test/lib/groupcase.ui.class.php
+++ module/testtask/test/lib/groupcase.ui.class.php
@@ -2,4 +2,21 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class groupCaseTester extends tester
 {
+    /**
+     * 检查分组视图下用例数量。
+     * Check the number of cases in the group view.
+     *
+     * @param  string $tab
+     * @param  string $story
+     * @param  string $num
+     * @access public
+     * @return void
+     */
+    public function checkNum($story, $num, $tab = 'all')
+    {
+        $form = $this->initForm('testtask', 'groupCase', array('taskID' => '1', 'browseType' => $tab), 'appIframe-qa');
+        $form->dom->xpath['case'] = "//div[@data-col='storyTitle']/div[text()={$story}]";
+        if(count($form->dom->xpath['case']->element) == $num) return $this->success('用例数量正确');
+        return $this->failed('用例数量不正确');
+    }
 }
