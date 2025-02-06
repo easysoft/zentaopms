d99e175844626c7c6573bc8dba23b186fdacec79
lihuifang <lihuifang@easycorp.ltd>
2025-02-06 15:33:15 +0800

    *[story#61317,doing,0.5h,2h] Add ui test file.

--- module/caselib/test/ui/createcaselib.php
+++ module/caselib/test/ui/createcaselib.php
@@ -18,3 +18,21 @@ cid=73
  - 最终测试状态 @SUCCESS
 
 */
+chdir(__DIR__);
+include '../lib/createcaselib.ui.class.php';
+
+$tester = new createCaselibTester();
+$tester->login();
+
+//设置数据
+$caselib = array(
+    array('name' => ''),
+    array('name' => '用例库1'),
+    array('name' => '用例库2'),
+);
+
+r($tester->createCaselib($caselib['0']))    && p('message,status') && e('创建用例库表单页必填提示信息正确,SUCCESS'); // 用例库名称置空保存，检查提示信息
+r($tester->createCaselib($caselib['1']))    && p('message,status') && e('用例库创建成功,SUCCESS'); // 无用例库时创建用例库
+r($tester->createCaselib($caselib['2'], 0)) && p('message,status') && e('用例库创建成功,SUCCESS'); // 有用例库时创建用例库
+
+$tester->closeBrowser();
