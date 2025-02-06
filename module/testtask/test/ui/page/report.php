ce880cf30d1daa2008728a10b237ca6f24fe7bf4
yulujie <yulujie@easycorp.ltd>
2025-02-06 14:34:24 +0800

    * [story#62105,doing,0.7h,3.5h] Add xpath to test report function.

--- module/testtask/test/ui/page/report.php
+++ module/testtask/test/ui/page/report.php
@@ -5,10 +5,23 @@ class reportPage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'charta' => "//*[@value='testTaskPerRunResult']",
-            'chartb' => "//*[@value='testTaskPerType']",
-            'chartc' => "//*[@value='testTaskPerModule']",
-            'chartd' => "//*[@value='testTaskPerRunner']",
+            /* 左侧报表 */
+            'testTaskPerRunResult' => "//*[@value='testTaskPerRunResult']",
+            'testTaskPerType'      => "//*[@value='testTaskPerType']",
+            'testTaskPerModule'    => "//*[@value='testTaskPerModule']",
+            'testTaskPerRunner'    => "//*[@value='testTaskPerRunner']",
+            'createBtn'            => "//*[@id='mainContent']/div[2]/div[1]/button[2]",
+            /* 数据 */
+            'title'    => "//div[@class='tab-pane active']/div[2]/div[1]/div[1]/div[1]",
+            'itema'    => "//div[@class='tab-pane active']//tbody/tr[2]/td[1]",
+            'valuea'   => "//div[@class='tab-pane active']//tbody/tr[2]/td[2]",
+            'percenta' => "//div[@class='tab-pane active']//tbody/tr[2]/td[3]",
+            'itemb'    => "//div[@class='tab-pane active']//tbody/tr[3]/td[1]",
+            'valueb'   => "//div[@class='tab-pane active']//tbody/tr[3]/td[2]",
+            'percentb' => "//div[@class='tab-pane active']//tbody/tr[3]/td[3]",
+            'itemc'    => "//div[@class='tab-pane active']//tbody/tr[4]/td[1]",
+            'valuec'   => "//div[@class='tab-pane active']//tbody/tr[4]/td[2]",
+            'percentc' => "//div[@class='tab-pane active']//tbody/tr[4]/td[3]",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
