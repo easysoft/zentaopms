e9c8760b8073ddf9fc042b56a9053f0232bba203
yulujie <yulujie@easycorp.ltd>
2025-02-07 15:49:39 +0800

    * [task#137029,doing,1h,3h] Modify xpath for uitest of report function.

--- module/testtask/test/ui/page/report.php
+++ module/testtask/test/ui/page/report.php
@@ -15,20 +15,8 @@ class reportPage extends page
             'pie'  => "//*[@data-key='pie']/a",
             'bar'  => "//*[@data-key='bar']/a",
             'line' => "//*[@data-key='line']/a",
-            /* 数据 */
-            'title'    => "//div[@class='tab-pane active']/div[2]/div[1]/div[1]/div[1]",
-            'itema'    => "//div[@class='tab-pane active']//tbody/tr[2]/td[1]",
-            'valuea'   => "//div[@class='tab-pane active']//tbody/tr[2]/td[2]",
-            'percenta' => "//div[@class='tab-pane active']//tbody/tr[2]/td[3]",
-            'itemb'    => "//div[@class='tab-pane active']//tbody/tr[3]/td[1]",
-            'valueb'   => "//div[@class='tab-pane active']//tbody/tr[3]/td[2]",
-            'percentb' => "//div[@class='tab-pane active']//tbody/tr[3]/td[3]",
-            'itemc'    => "//div[@class='tab-pane active']//tbody/tr[4]/td[1]",
-            'valuec'   => "//div[@class='tab-pane active']//tbody/tr[4]/td[2]",
-            'percentc' => "//div[@class='tab-pane active']//tbody/tr[4]/td[3]",
-            'itemd'    => "//div[@class='tab-pane active']//tbody/tr[5]/td[1]",
-            'valued'   => "//div[@class='tab-pane active']//tbody/tr[5]/td[2]",
-            'percentd' => "//div[@class='tab-pane active']//tbody/tr[5]/td[3]",
+            /* 报表标题 */
+            'title' => "//div[@class='tab-pane active']/div[2]/div[1]/div[1]/div[1]",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
