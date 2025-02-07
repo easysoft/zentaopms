e0613791e739e08e1c90f0c47aa1568efe70ebb7
lixiaoyu <lixiaoyu@easycorp.ltd>
2025-02-07 07:23:41 +0000

    *[task#137030,doing,0.5h,2h] add space data

---
+++ module/kanban/test/ui/editkanban.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+
+chdir(__DIR__);
+include '../lib/kanban.ui.class.php';
+
+$kanbanspace = zenData('kanbanspace');
+$kanbanspace->id->range('1');
+$kanbanspace->name->range('协作空间-A');
+$kanbanspace->type->range('cooperation');
+$kanbanspace->owner->range('admin');
+$kanbanspace->team->range(',admin');
+$kanbanspace->acl->range('open');
+$kanbanspace->status->range('active');
+$kanbanspace->createdBy->range('admin');
+$kanbanspace->gen(1);
+
+$tester->closeBrowser();
