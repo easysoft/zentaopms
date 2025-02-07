e99edcd3f42a7fbf04b8441a16d6924a0451d778
lixiaoyu <lixiaoyu@easycorp.ltd>
2025-02-07 05:12:56 +0000

    *[task#137028,done,1h,0h]add uitest case of create kanban

--- module/kanban/test/ui/createkanban.php
+++ module/kanban/test/ui/createkanban.php
@@ -18,3 +18,15 @@ $kanbanspace->acl->range('open');
 $kanbanspace->status->range('active');
 $kanbanspace->createdBy->range('admin');
 $kanbanspace->gen(1);
+
+$tester = new kanbanTester();
+$tester->login();
+$kanban = new stdClass();
+
+$kanban->name = '';
+r($tester->createKanban($kanban)) && p('message,status') && e('看板名称必填提示信息正确,SUCCESS');//看板名必填校验
+
+$kanban->name = '第一个看板';
+r($tester->createKanban($kanban)) && p('message,status') && e('创建看板成功,SUCCESS');//创建看板
+
+$tester->closeBrowser();
