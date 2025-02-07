f1387ac5fce28b6a64b2987c9d3454f8b645dd58
lixiaoyu <lixiaoyu@easycorp.ltd>
2025-02-07 07:16:53 +0000

    *[task#137030,doing,1h,2h] function of edit kanban

--- module/kanban/test/lib/kanban.ui.class.php
+++ module/kanban/test/lib/kanban.ui.class.php
@@ -28,4 +28,22 @@ class kanbanTester extends tester
             ? $this->success('创建看板成功')
             : $this->failed('创建看板失败');
     }
+
+    public function editKanban($kanban)
+    {
+        $form = $this->initForm('kanban', 'space', ['browseType' => 'cooperation'], 'appIframe-kanban');
+        $form->dom->moreBtn->click();
+        $form->dom->btn($this->lang->kanban->edit)->click();
+        if (isset($kanban->name)) $form->dom->name->setValue($kanban->name);
+        $form->dom->saveEditBtn->click();
+        //校验编辑结果
+        if($form->dom->zin_kanban_edit_1_formPanel)
+        {
+            if($this->checkFormTips('kanban')) return $this->success('看板名称必填提示信息正确');
+            return $this->failed('看板名称必填提示信息不正确');
+        }
+        return ($form->dom->kanbanName->getText() == $kanban->name)
+            ? $this->success('编辑看板成功')
+            : $this->failed('编辑看板失败');
+    }
 }
