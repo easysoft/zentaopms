b69f4f18c0504a3312fd9a1736d8088d0a1bd944
lixiaoyu <lixiaoyu@easycorp.ltd>
2025-02-07 07:24:19 +0000

    *[task#137030,doing,0.5h,2h] add kanban data

--- module/kanban/test/ui/editkanban.php
+++ module/kanban/test/ui/editkanban.php
@@ -1,6 +1,11 @@
 #!/usr/bin/env php
 <?php
 
+/**
+title=编辑看板
+timeout=0
+cid=0
+*/
 chdir(__DIR__);
 include '../lib/kanban.ui.class.php';
 
@@ -14,5 +19,18 @@ $kanbanspace->acl->range('open');
 $kanbanspace->status->range('active');
 $kanbanspace->createdBy->range('admin');
 $kanbanspace->gen(1);
+
+$kanbanData = zenData('kanban');
+$kanbanData->id->range('1');
+$kanbanData->space->range('1');
+$kanbanData->name->range('看板-01');
+$kanbanData->owner->range('admin');
+$kanbanData->team->range(',admin');
+$kanbanData->acl->range('open');
+$kanbanData->status->range('active');
+$kanbanData->order->range('1');
+$kanbanData->object->range('plans,releases,builds,executions,cards');
+$kanbanData->gen(1);
+
 
 $tester->closeBrowser();
