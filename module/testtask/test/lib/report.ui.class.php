363220bbe9b7669fac4f8353d20a5057a5eb7266
yulujie <yulujie@easycorp.ltd>
2025-02-07 15:50:15 +0800

    * [task#137029,doing,3h,2h] Modify function for uitest of report function.

--- module/testtask/test/lib/report.ui.class.php
+++ module/testtask/test/lib/report.ui.class.php
@@ -6,59 +6,66 @@ class reportTester extends tester
      * 检查报表数据。
      * Check report data.
      *
-     * @param  string $report  报表名称
-     * @param  array  $itemNum 查看的报表条目
-     * @param  array  $data    报表条目、值、百分比
+     * @param  string $report 报表名称
+     * @param  array  $data   报表条目、值、百分比
      * @access public
      * @return void
      */
-    public function check($report, $itemNum = 'a', $data)
+    public function check($report, $data)
     {
         $form = $this->initForm('testtask', 'report', array('productID' => '1', 'taskID' => '1', 'browseType' => 'all', 'chartType' => 'pie'), 'appIframe-qa');
         $form->dom->$report->click();
         $form->dom->createBtn->click();
         $form->wait(1);
-        $types = array('pie', 'bar', 'line');
+        /* 获取报表中条目数 */
+        $itemXpath = "//div[@class='tab-pane active']//tbody/tr";
+        $itemNum   = count($form->dom->getElementList($itemXpath)->element);
+        $types     = array('pie', 'bar', 'line');
         foreach($types as $type)
         {
             $form->dom->$type->click();
-            $message = $this->checkReport($report, $type, $itemNum, $data);
-            if($message == 'titleError')   return $this->failed($type . '报表' . $itemNum . '行显示名称错误');
-            if($message == 'itemError')    return $this->failed($type . '报表' . $itemNum . '显示条目错误');
-            if($message == 'valueError')   return $this->failed($type . '报表' . $itemNum . '显示值错误');
-            if($message == 'percentError') return $this->failed($type . '报表' . $itemNum . '显示百分比错误');
-            $form->wait(2);
+            for($num =2; $num <= $itemNum; $num++)
+            {
+                $message = $this->checkReport($report, $type, $num, $data);
+                if($message == 'titleError')   return $this->failed($type . '报表第' . $num . '行显示名称错误');
+                if($message == 'itemError')    return $this->failed($type . '报表第' . $num . '行显示条目错误');
+                if($message == 'valueError')   return $this->failed($type . '报表第' . $num . '行显示值错误');
+                if($message == 'percentError') return $this->failed($type . '报表第' . $num . '行显示百分比错误');
+                $form->wait(1);
+            }
         }
-        if($message == 'success')  return $this->success($report . '报表' . $itemNum . '行数据正确');
+        if($message == 'success') return $this->success($report . '报表数据正确');
     }
 
     /**
      * 检查不同报表数据。
      * Check different report data.
      *
-     * @param  string $report  报表名称
-     * @param  string $type    报表类型
-     * @param  array  $itemNum 查看的报表条目
-     * @param  array  $data    报表条目、值、百分比
+     * @param  string $report 报表名称
+     * @param  string $type   报表类型
+     * @param  array  $num    查看的报表条目
+     * @param  array  $data   报表条目、值、百分比
      * @access public
      * @return void
      */
-    public function checkReport($report, $type, $itemNum, $data)
+    public function checkReport($report, $type, $num, $data)
     {
         $form = $this->loadPage();
-        if($form->dom->title->getText() != $this->lang->testtask->report->charts->$report) return 'titleError';
+        $form->wait(2);
 
-        $item    = 'item' . $itemNum;
-        $value   = 'value' . $itemNum;
-        $percent = 'percent' . $itemNum;
+        if($form->dom->title->getText() != $this->lang->testtask->report->charts->$report) return 'titleError';
+        /* 各行的Xpath */
+        $form->dom->xpath['item']    = "//div[@class='tab-pane active']//tbody/tr[$num]/td[1]";
+        $form->dom->xpath['value']   = "//div[@class='tab-pane active']//tbody/tr[$num]/td[2]";
+        $form->dom->xpath['percent'] = "//div[@class='tab-pane active']//tbody/tr[$num]/td[3]";
         /* 从测试数据中提取对应的子数组，子数组中为此行的条目、值、百分比 */
-        $key = array_search($form->dom->$item->getText(), array_column($data, $this->config->default->lang));
+        $form->wait(1);
+        $key = array_search($form->dom->item->getText(), array_column($data, $this->config->default->lang));
         if($key === false) return 'itemError';
         $result = $data[$key];
 
-        if($form->dom->$value->getText() != $result['value'])     return 'valueError';
-        if($form->dom->$percent->getText() != $result['percent']) return 'percentError';
+        if($form->dom->value->getText() != $result['value'])     return 'valueError';
+        if($form->dom->percent->getText() != $result['percent']) return 'percentError';
         return 'success';
-        $form->wait(2);
     }
 }
