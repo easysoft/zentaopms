5b294cc4fd5a97129f6911ed23e978ce9c035ec4
lixiaoyu <lixiaoyu@easycorp.ltd>
2025-02-07 05:08:19 +0000

    *[task#137028,doing,0.5h,1h]add comments

--- module/kanban/test/lib/kanban.ui.class.php
+++ module/kanban/test/lib/kanban.ui.class.php
@@ -2,12 +2,23 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class kanbanTester extends tester
 {
+    /*
+     * 创建看板
+     * Create kanban
+     *
+     * @param  $kanban 看板
+     * @return mixed
+     */
     public function createKanban($kanban)
     {
         $form = $this->initForm('kanban', 'space', ['browseType' => 'cooperation'], 'appIframe-kanban');
         $form->dom->createKanbanBtn->click();
+        $form->wait(2);
         if (isset($kanban->name)) $form->dom->name->setValue($kanban->name);
         $form->dom->saveKanbanBtn->click();
+        $form->wait(2);
+
+        //校验创建结果
         if ($form->dom->zin_kanban_create_formPanel)
         {
             if($this->checkFormTips('kanban')) return $this->success('看板名称必填提示信息正确');
