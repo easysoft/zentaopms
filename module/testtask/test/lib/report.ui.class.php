3569f1f6e6421477026be089492277e7a2c03b6f
yulujie <yulujie@easycorp.ltd>
2025-02-07 08:53:47 +0800

    * [task#137029,doing,2h,1.5h] Add function for uitest of report function.

--- module/testtask/test/lib/report.ui.class.php
+++ module/testtask/test/lib/report.ui.class.php
@@ -6,19 +6,34 @@ class reportTester extends tester
      * 检查报表数据。
      * Check report data.
      *
-     * @param  string $report 报表名称
-     * @param  string $type   报表类型
-     * @param  array  $data   报表条目、值、百分比
+     * @param  string $report  报表名称
+     * @param  string $type    报表类型
+     * @param  array  $itemNum 查看的报表条目
+     * @param  array  $data    报表条目、值、百分比
      * @access public
      * @return void
      */
-    public function checkReport($report, $type, $data)
+    public function checkReport($report, $type, $itemNum = 'a', $data)
     {
-        $form = $this->initForm('testtask', 'report', array('productID' => '1', 'taskID' => '1', 'browseType' => 'all', 'chartType' => $type), 'appIframe-qa');
+        $form = $this->initForm('testtask', 'report', array('productID' => '1', 'taskID' => '1', 'browseType' => 'all', 'chartType' => 'pie'), 'appIframe-qa');
         $form->dom->$report->click();
         $form->wait(1);
         $form->dom->createBtn->click();
         $form->wait(1);
+        $form->dom->$type->click();
+        $form->wait(1);
         if($form->dom->title->getText() != $this->lang->testtask->report->charts->$report) return $this->failed('显示报表错误');
+
+        $item    = 'item' . $itemNum;
+        $value   = 'value' . $itemNum;
+        $percent = 'percent' . $itemNum;
+        /* 从测试数据中提取对应的子数组，子数组中为此行的条目、值、百分比 */
+        $key = array_search($form->dom->$item->getText(), array_column($data, $this->config->default->lang));
+        if($key === false) return $this->failed('报表中无此行数据');
+        $result = $data[$key];
+
+        if($form->dom->$value->getText() != $result['value'])     return $this->failed('显示报表值错误');
+        if($form->dom->$percent->getText() != $result['percent']) return $this->failed('显示报表百分比错误');
+        return $this->success('报表数据正确');
     }
 }
