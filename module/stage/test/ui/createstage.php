03f8880a15d62e617ad5be3cfbf2ee763eb9874a
huairong <huairong@easycorp.ltd>
2025-02-08 14:36:57 +0800

    + [story#61952,doing,0.2h,1.2h] add stage testdata of createstage ui file.

--- module/stage/test/ui/createstage.php
+++ module/stage/test/ui/createstage.php
@@ -5,6 +5,7 @@
 title=创建阶段测试
 timeout=0
 cid=1
+
 - 瀑布模型下校验阶段名称不能为空
  - 测试结果 @新建阶段表单页提示信息正确
  - 最终测试状态 @ SUCCESS
@@ -18,3 +19,19 @@ cid=1
 - 测试结果 @新建阶段成功
 
 */
+chdir(__DIR__);
+include '../lib/createstage.ui.class.php';
+
+$stage = zendata('stage');
+$stage->id->range('1-12');
+$stage->name->range('需求, 设计, 开发, 测试, 发布, 总结评审');
+$stage->percent->range('10,10,40,15,10,5');
+$stage->type->range('request,design,dev,qa,release,review');
+$stage->projectType->range('waterfall{6},waterfallplus{6}');
+$stage->createdBy->range('admin');
+$stage->createdDate->range('(-2M)-(-M):1D')->type('timestamp')->format('YY/MM/DD');
+$stage->deleted->range('0');
+$stage->gen(12);
+
+$tester = new createStageTester();
+$tester->login();
