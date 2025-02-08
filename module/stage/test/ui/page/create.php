3efb284387917f764c243ba05f41ab291444c3b3
huairong <huairong@easycorp.ltd>
2025-02-08 14:28:11 +0800

    + [story#61952,doing,0.1h,1.9h] add xpath of create page element.

---
+++ module/stage/test/ui/page/create.php
@@ -0,0 +1,13 @@
+<?php
+class createPage extends page
+{
+    public function __construct($webdriver)
+    {
+        parent::__construct($webdriver);
+        $xpath = array(
+            'submitBtn'      => "//*[@id='zin_stage_create_form']/div[4]/div/button",
+            'percentOverTip' => "//*[@id='percentTip']",
+        );
+        $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
+    }
+}
