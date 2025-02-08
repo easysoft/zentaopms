9a8516947e72cee493ff99eed409f5fe83b36074
huairong <huairong@easycorp.ltd>
2025-02-08 09:12:11 +0800

    + [task#137033,doing,0.1h,0.6h] add comment of view ui test file.

---
+++ module/stakeholder/test/ui/view.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+
+/**
+
+title=检查干系人信息测试
+timeout=0
+cid=7
+
+- 检查干系人信息
+ - 测试结果 @干系人信息正确
+ - 最终测试状态 @ SUCCESS
+
+*/
+
+chdir(__DIR__);
+include '../lib/view.ui.class.php';
+global $config;
