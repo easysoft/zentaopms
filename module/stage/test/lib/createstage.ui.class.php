f0a49bbeb87cc884010448f9cdb5a7c820253d9d
huairong <huairong@easycorp.ltd>
2025-02-08 14:20:41 +0800

    + [story#61952,doing,0.3h,2.6h] add create stage form of createstage function.

--- module/stage/test/lib/createstage.ui.class.php
+++ module/stage/test/lib/createstage.ui.class.php
@@ -11,4 +11,22 @@ class createStageTester extends tester
      * @access public
      * @return object
      */
+    public function createStage(array $stage, string $type = '')
+    {
+        if($type == 'waterfall')
+        {
+            $form = $this->initForm('stage', 'browse', array(), 'appIframe-admin');
+            $form->dom->createBtn->click();
+        }
+        if($type == 'waterfallplus')
+        {
+            $form = $this->initForm('stage', 'plusbrowse', array(), 'appIframe-admin');
+            $form->dom->createBtn->click();
+        }
+        $createForm = $this->loadPage('stage', 'create');
+        if(isset($stage['name']))    $createForm->dom->name->setValue($stage['name']);
+        if(isset($stage['percent'])) $createForm->dom->percent->setValue($stage['percent']);
+        if(isset($stage['type']))    $createForm->dom->type->picker($stage['type']);
+        $createForm->dom->submitBtn->click();
+        $createForm->wait(1);
 }
