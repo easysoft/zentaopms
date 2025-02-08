b96755ebc1440dcc37c6a24f8d89ce3c142c3b3e
yulujie <yulujie@easycorp.ltd>
2025-02-08 14:53:48 +0800

    * [task#137056,doing,0.1h,2.5h] Add xpath for uitest of view function.

--- module/testtask/test/ui/page/view.php
+++ module/testtask/test/ui/page/view.php
@@ -5,10 +5,14 @@ class viewPage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'begin'        => "//*[@id='basicInfo']/table/tbody/tr[7]/td",
-            'status'       => "//*[@id='basicInfo']/table/tbody/tr[10]/td",
-            'submitBtn'    => "//button[@type='submit']",
-            'buttons'      => "//*[@id='mainContent']/div[2]/div[1]/div[3]/div",
+            'begin'     => "//*[@id='basicInfo']/table/tbody/tr[7]/td",
+            'status'    => "//*[@id='basicInfo']/table/tbody/tr[10]/td",
+            'submitBtn' => "//button[@type='submit']",
+            'buttons'   => "//*[@id='mainContent']/div[2]/div[1]/div[3]/div",
+            /* 测试单ID及测试单名称 */
+            'id'   => "//*[@id='mainContent']/div[1]/div/div/span[1]",
+            'name' => "//*[@id='mainContent']/div[1]/div/div/span[2]",
+            /* 标题旁的已删除标签 */
             'deletedLabel' => "//*[@id='mainContent']/div[1]/div/span"
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
