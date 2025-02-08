73c3025661a4ad9fc0ebf62b0421ed2aa2a939f7
huairong <huairong@easycorp.ltd>
2025-02-08 14:23:34 +0800

    + [story#61952,doing,0.3h,2.3h] add form field validation of create stage.

--- module/stage/test/lib/createstage.ui.class.php
+++ module/stage/test/lib/createstage.ui.class.php
@@ -29,4 +29,21 @@ class createStageTester extends tester
         if(isset($stage['type']))    $createForm->dom->type->picker($stage['type']);
         $createForm->dom->submitBtn->click();
         $createForm->wait(1);
+
+        /* 检查阶段名称和工作量占比不能为空 */
+        if($createForm->dom->nameTip && $createForm->dom->percentTip)
+        {
+            $nameTipText    = $createForm->dom->nameTip->getText();
+            $nameTip        = sprintf($this->lang->error->notempty, $this->lang->stage->name);
+            $percentTipText = $createForm->dom->percentTip->getText();
+            $percentTip     = sprintf($this->lang->error->notempty, $this->lang->stage->percent);
+            return ($nameTipText == $nameTip && $percentTipText == $percentTip) ? $this->success('新建阶段表单页提示信息正确') : $this->failed('新建阶段表单页提示信息不正确');
+        }
+        /* 检查工作量占比累计不能超过100% */
+        if($createForm->dom->percentOverTip)
+        {
+            $percentOverText = $createForm->dom->percentOverTip->getText();
+            $percentOverTip  = $this->lang->stage->error->percentOver;
+            return ($percentOverText == $percentOverTip) ? $this->success('工作量占比累计超出100%时提示信息正确') : $this->failed('工作量占比累计超出100%时提示信息不正确');
+        }
 }
