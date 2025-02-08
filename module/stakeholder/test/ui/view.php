bb703d000ab3ace35ff15b62cd4abd24327f64e4
huairong <huairong@easycorp.ltd>
2025-02-08 09:13:57 +0800

    + [task#137033,doing,0.2h,0.4h] add stakeholder and user test data.

--- module/stakeholder/test/ui/view.php
+++ module/stakeholder/test/ui/view.php
@@ -16,3 +16,23 @@ cid=7
 chdir(__DIR__);
 include '../lib/view.ui.class.php';
 global $config;
+
+$stakeholder = zenData('stakeholder');
+$stakeholder->id->range('1');
+$stakeholder->objectID->range('1');
+$stakeholder->objectType->range('project');
+$stakeholder->user->range('user1');
+$stakeholder->type->range('inside');
+$stakeholder->key->range('0');
+$stakeholder->from->range('company');
+$stakeholder->gen(1);
+
+$user = zenData('user');
+$user->id->range('1-5');
+$user->type->range('inside{4}, outside{1}');
+$user->dept->range('1');
+$user->account->range('admin, user1, user2, user3, user4');
+$user->realname->range('admin, user1, user2, user3, user4');
+$user->password->range($config->uitest->defaultPassword)->format('md5');
+$user->visions->range('rnd');
+$user->gen(5);
