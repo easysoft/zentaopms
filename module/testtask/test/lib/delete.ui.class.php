bfdefa92f8a2ad518abc17b6648a20b792f43312
yulujie <yulujie@easycorp.ltd>
2025-02-08 14:47:45 +0800

    * [uitest] Modify function..

--- module/testtask/test/lib/delete.ui.class.php
+++ module/testtask/test/lib/delete.ui.class.php
@@ -12,13 +12,13 @@ class deleteTester extends tester
     public function deleteTest()
     {
         $form = $this->initForm('testtask', 'browse', array('productID' => '1'), 'appIframe-qa');
-        $num  = $form->dom->totalNum->getText();
+        $num  = $form->dom->num->getText();
         $id   = $form->dom->firstID->getText();
         $form->dom->firstDeleteBtn->click();
         $form->wait(1);
         $form->dom->alertModal();
         $form->wait(1);
-        if($form->dom->totalNum->getText() != $num - 1) return $this->failed('删除测试单失败');
+        if($form->dom->num->getText() != $num - 1) return $this->failed('删除测试单失败');
 
         $form = $this->initForm('testtask', 'view', array('taskID' => $id), 'appIframe-qa');
         if(is_object($form->dom->deletedLabel) && $form->dom->deletedLabel->getText() == $this->lang->testtask->deleted) return $this->success('删除测试单成功');
