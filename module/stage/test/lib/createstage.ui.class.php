9d9b3583fe08c40eb2567f8b1f5cf95a48c90045
huairong <huairong@easycorp.ltd>
2025-02-08 14:17:51 +0800

    + [story#61952,doing,0.1h,2.9h] add comment of createstage function.

---
+++ module/stage/test/lib/createstage.ui.class.php
@@ -0,0 +1,14 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+class createStageTester extends tester
+{
+    /**
+     * Create a stage.
+     * 创建一个阶段。
+     *
+     * @param  array  $stage
+     * @param  string $type  waterfall|waterfallplus
+     * @access public
+     * @return object
+     */
+}
