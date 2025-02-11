afe1e44b3d4ca540b31fdb81973c022f1c163837
lixiaoyu <lixiaoyu@easycorp.ltd>
2025-02-11 07:55:37 +0000

    *[task#137157,doing,1h,3h] function of create card

--- module/kanban/test/lib/card.ui.class.php
+++ module/kanban/test/lib/card.ui.class.php
@@ -2,10 +2,29 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class cardTester extends tester
 {
+    /*
+     * 创建卡片
+     * Create card
+     *
+     * @param array  $kanbanurl
+     * @param object $card
+     *
+     * @return mixed
+     */
     public function createCard($kanbanurl, $card)
     {
         $form = $this->initForm('kanban', 'view', $kanbanurl, 'appIframe-kanban');
+        $form->dom->createBtn->click();
+        $form->wait(1);
+        $form->dom->btn($this->lang->kanban->createCard)->click();
+        $form->wait(1);
+
+        //填写卡片信息
         if (isset($card->name)) $form->dom->name->setValue($card->name);
+        $form->dom->btn($this->lang->save)->click();
+        $form->wait(2);
+
+        //校验创建结果
         if ($form->dom->zin_kanban_createcard_form)
         {
             $nameTip = sprintf($this->lang->error->notempty, $this->lang->kanbancard->name);
