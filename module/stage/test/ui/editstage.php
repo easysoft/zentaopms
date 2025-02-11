2f97442cbb0a7213507c9510aa83f095c483a65c
huairong <huairong@easycorp.ltd>
2025-02-11 16:47:29 +0800

    + [task#137219,doing,0.3h,0.5h] add stage test data and testcase.

--- module/stage/test/ui/editstage.php
+++ module/stage/test/ui/editstage.php
@@ -38,3 +38,24 @@ $stage->type->range('request,design,dev,qa,release,review');
 $stage->projectType->range('waterfall{6},waterfallplus{6}');
 $stage->createdBy->range('admin');
 $stage->createdDate->range('(-2M)-(-M):1D')->type('timestamp')->format('YY/MM/DD');
+$stage->deleted->range('0');
+$stage->gen(12);
+
+$tester = new editStageTester();
+$tester->login();
+
+$stage = array(
+    array('name' => '', 'percent' => ''),
+    array('name' => '瀑布需求阶段', 'percent' => '101', 'type' => '需求'),
+    array('name' => '瀑布需求阶段', 'percent' => '10', 'type' => '需求'),
+    array('name' => '', 'percent' => ''),
+    array('name' => '融合瀑布设计阶段', 'percent' => '101', 'type' => '设计'),
+    array('name' => '融合瀑布设计阶段', 'percent' => '10', 'type' => '设计'),
+);
+
+r($tester->editstage($stage['0'], 'waterfall'))     && p('message,status') && e('编辑阶段表单页提示信息正确, SUCCESS');           //瀑布模型下校验阶段名称不能为空
+r($tester->editstage($stage['1'], 'waterfall'))     && p('message,status') && e('工作量占比累计超出100%时提示信息正确, SUCCESS'); //瀑布模型下工作量占比累计不能超出100%
+r($tester->editstage($stage['2'], 'waterfall'))     && p('message,status') && e('编辑阶段成功, SUCCESS');                         //瀑布模型下编辑需求类型阶段
+r($tester->editstage($stage['3'], 'waterfallplus')) && p('message,status') && e('编辑阶段表单页提示信息正确, SUCCESS');           //融合瀑布模型下校验阶段名称不能为空
+r($tester->editstage($stage['4'], 'waterfallplus')) && p('message,status') && e('工作量占比累计超出100%时提示信息正确, SUCCESS'); //融合瀑布模型下工作量占比累计不能超出100%
+r($tester->editstage($stage['5'], 'waterfallplus')) && p('message,status') && e('编辑阶段成功, SUCCESS');                         //融合瀑布模型下编辑设计类型阶段
