fc0edeb901b0f74440b7c538d9a0d402d0ad5fe8
lihuifang <lihuifang@easycorp.ltd>
2025-02-11 15:31:28 +0800

    *[task#137311,done,1.5h,0h] Add testui function.

--- module/caselib/test/lib/editcaselib.ui.class.php
+++ module/caselib/test/lib/editcaselib.ui.class.php
@@ -17,3 +17,23 @@ class editCaselibTester extends tester
         if(isset($caselib['name'])) $form->dom->name->setValue($caselib['name']);
 
         $form->dom->btn($this->lang->save)->click();
+        $form->wait(1);
+
+        /* 检查创建用例库页面必填提示信息 */
+        if(is_object($form->dom->nameTip))
+        {
+            $nameTipForm = $form->dom->nameTip->getText();
+            $nameTip     = sprintf($this->lang->error->notempty, $this->lang->caselib->name);
+            return ($nameTipForm == $nameTip) ? $this->success('编辑用例库表单页必填提示信息正确') : $this->failed('编辑用例库表单页必填提示信息不正确');
+        }
+
+        /* 点击查看库概况 */
+        $browsePage = $this->loadPage('caselib', 'browse');
+        $browsePage->dom->btn($this->lang->caselib->view)->click();
+        $browsePage->wait(1);
+
+        //断言检查用例库名称是否正确
+        if($browsePage->dom->viewName->getText() == $caselib['name']) return $this->success('用例库编辑成功');
+        return $this->failed('用例库编辑失败');
+    }
+}
