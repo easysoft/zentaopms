cfb6559e13b60429cfd2dcab50ade05af72022c9
lixiaoyu <lixiaoyu@easycorp.ltd>
2025-02-11 08:09:57 +0000

    *[task#137157,doing,0.3h,3h]kanban data

--- module/kanban/test/ui/createcard.php
+++ module/kanban/test/ui/createcard.php
@@ -1,5 +1,6 @@
 #!/usr/bin/env php
 <?php
+
 /**
 title=创建卡片
 timeout=0
@@ -18,3 +19,20 @@ $kanbanspace->acl->range('open');
 $kanbanspace->status->range('active');
 $kanbanspace->createdBy->range('admin');
 $kanbanspace->gen(1);
+
+$kanban = zenData('kanban');
+$kanban->id->range('1');
+$kanban->space->range('1');
+$kanban->name->range('看板-01');
+$kanban->owner->range('admin');
+$kanban->team->range(',admin');
+$kanban->acl->range('open');
+$kanban->archived->range('1');
+$kanban->performable->range('1');
+$kanban->status->range('active');
+$kanban->order->range('1');
+$kanban->object->range('`plans,releases,builds,executions,cards`');
+$kanban->gen(1);
+
+
+$tester->closeBrowser();
