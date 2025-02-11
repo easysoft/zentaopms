4f6ea740ceb993f0737c5acf9a7f1e0dba07cf1b
yulujie <yulujie@easycorp.ltd>
2025-02-11 16:13:57 +0800

    * [task#137312,doing,0.1h,5.9h] Add product data for uitest of results function.

---
+++ module/testtask/test/ui/results.php
@@ -0,0 +1,17 @@
+#!/usr/bin/env php
+<?php
+
+/**
+title=检查测试单下的分组视图
+timeout=0
+cid=1
+ */
+
+chdir(__DIR__);
+include '../lib/results.ui.class.php';
+
+$product = zenData('product');
+$product->id->range('1-100');
+$product->name->range('产品1, 产品2');
+$product->type->range('normal');
+$product->gen(2);
