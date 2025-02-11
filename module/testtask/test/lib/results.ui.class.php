4ba1f1c63b398cc314017b3f9dd3c403cc9f51a8
yulujie <yulujie@easycorp.ltd>
2025-02-11 16:19:26 +0800

    * [task#137312,doing,0.1h,4.9h] Add class for uitest of results function.

---
+++ module/testtask/test/lib/results.ui.class.php
@@ -0,0 +1,14 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+class resultsTester extends tester
+{
+    /**
+     * 检查测试单下测试用例结果。
+     * Check results of testcase.
+     *
+     * @param  int    $num  第几个结果
+     * @Param  array  $task 测试单相关信息
+     * @access public
+     * @return void
+     */
+}
