a1a3af077b18134edf95d6e0417d33d89f6475d3
lixiaoyu <lixiaoyu@easycorp.ltd>
2025-02-12 02:33:17 +0000

    *[task#137157,done,0.1h,0h]modify code

--- module/kanban/test/ui/page/view.php
+++ module/kanban/test/ui/page/view.php
@@ -1,12 +1,12 @@
 <?php
-class spacePage extends page
+class viewPage extends page
 {
     public function __construct($webdriver)
     {
         parent::__construct($webdriver);
         $xpath = array(
             'createBtn'   => '//*[@id="kanbanList"]/div/div/div/div[2]/div[1]/div[2]/div[1]/div/nav/button[1]',
-            'firCardName' => '//*[@id="kanbanList"]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div/div/div[1]/a',
+            'firCardName' => '//*[@id="kanbanList"]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div/div/div[1]/a'
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
