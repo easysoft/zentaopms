5ce8486f678ff4cd013f45ac2adeb7a67a7e9c34
sunguangming <sunguangming@chandao.com>
2025-02-13 01:58:25 +0000

    * [misc] add db field.

--- db/update22.0.sql
+++ db/update22.0.sql
@@ -26,6 +26,7 @@ CREATE TABLE `zt_rulequeue` (
   `rule` int(8) NOT NULL,
   `actions` longtext DEFAULT NULL,
   `status` char(30) NOT NULL DEFAULT 'wait',
+  `log` text DEFAULT NULL,
   `triggeredBy` varchar(30) DEFAULT NULL,
   `triggeredDate` date DEFAULT NULL,
   `result` varchar(30) DEFAULT NULL,
