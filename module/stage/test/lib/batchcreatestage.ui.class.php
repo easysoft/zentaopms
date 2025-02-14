d87b6ec32d5edf84493739267e3e5357de694d2c
huairong <huairong@easycorp.ltd>
2025-02-14 09:10:00 +0800

    + [task#137220,doing,0.1h,2.7h] add comment of batchcreatestage function.

---
+++ module/stage/test/lib/batchcreatestage.ui.class.php
@@ -0,0 +1,14 @@
+<?php
+include dirname(__FILE__, 5) . '/test/lib/ui.php';
+class batchCreateStageTester extends tester
+{
+    /**
+     * Batch create a stage.
+     * 批量创建一个阶段。
+     *
+     * @param  array  $stage
+     * @param  string $type  waterfall|waterfallplus
+     * @access public
+     * @return object
+     */
+}
