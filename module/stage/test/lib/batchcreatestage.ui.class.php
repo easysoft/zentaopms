d9066d2cc6dcaaa44678f570c27931885dcb21b0
huairong <huairong@easycorp.ltd>
2025-02-14 09:13:34 +0800

    + [task#137220,doing,0.3h,2.4h] add form fields of batchcreatestage.

--- module/stage/test/lib/batchcreatestage.ui.class.php
+++ module/stage/test/lib/batchcreatestage.ui.class.php
@@ -11,4 +11,22 @@ class batchCreateStageTester extends tester
      * @access public
      * @return object
      */
+    public function batchCreateStage(array $stage, string $type = '')
+    {
+        if($type == 'waterfall')
+        {
+            $form = $this->initForm('stage', 'browse', array(), 'appIframe-admin');
+            $form->dom->batchCreateBtn->click();
+        }
+        if($type == 'waterfallplus')
+        {
+            $form = $this->initform('stage', 'plusbrowse', array(), 'appiframe-admin');
+            $form->dom->batchcreatebtn->click();
+        }
+        $batchcreateform = $this->loadpage('stage', 'batchcreate');
+        if(isset($stage['name']))    $batchcreateform->dom->name->setvalue($stage['name']);
+        if(isset($stage['percent'])) $batchcreateform->dom->percent->setvalue($stage['percent']);
+        if(isset($stage['type']))    $batchcreateform->dom->type->select('text', $stage['type']);
+        $batchcreateform->dom->submitbtn->click();
+        $batchcreateform->wait(1);
 }
