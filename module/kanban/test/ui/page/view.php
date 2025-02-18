940c53f57ecf16fa66dd582787e080dd034accea
lixiaoyu <lixiaoyu@easycorp.ltd>
2025-02-18 08:20:25 +0000

    *[task#137817,doing,0.3h,3h] add xpath of card

--- module/kanban/test/ui/page/view.php
+++ module/kanban/test/ui/page/view.php
@@ -6,7 +6,8 @@ class viewPage extends page
         parent::__construct($webdriver);
         $xpath = array(
             'createBtn'   => '//*[@id="kanbanList"]/div/div/div/div[2]/div[1]/div[2]/div[1]/div/nav/button[1]',
-            'firCardName' => '//*[@id="kanbanList"]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div/div/div[1]/a'
+            'firCardName' => '//*[@id="kanbanList"]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div/div/div[1]/a',
+            'moreBtn'     => '//*[@id="kanbanList"]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div[1]/div/nav/button'
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
