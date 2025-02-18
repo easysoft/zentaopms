23c8f67e17e77fe75d1eb11fd379d81f6ab3c1fa
lixiaoyu <lixiaoyu@easycorp.ltd>
2025-02-18 08:26:29 +0000

    *[task#137817,doing,0.2h,2h] add data of edit card

--- module/kanban/test/ui/editcard.php
+++ module/kanban/test/ui/editcard.php
@@ -1,5 +1,6 @@
 #!/usr/bin/env php
 <?php
+
 /**
 title=编辑卡片
 timeout=0
@@ -18,3 +19,22 @@ $kanbanspace->acl->range('open');
 $kanbanspace->status->range('active');
 $kanbanspace->createdBy->range('admin');
 $kanbanspace->gen(1);
+$kanban = zenData('kanban');
+$kanban->id->range('1');
+$kanban->space->range('1');
+$kanban->name->range('看板-01');
+$kanban->owner->range('admin');
+$kanban->team->range(',admin');
+$kanban->acl->range('open');
+$kanban->archived->range('1');
+$kanban->performable->range('1');
+$kanban->status->range('active');
+$kanban->order->range('1');
+$kanban->object->range('`plans,releases,builds,executions,cards`');
+$kanban->gen(1);
+$kanbanregion = zenData('kanbanregion');
+$kanbanregion->id->range('1');
+$kanbanregion->space->range('1');
+$kanbanregion->name->range('区域AAA');
+$kanbanregion->order->range('1');
+$kanbanregion->gen(1);
