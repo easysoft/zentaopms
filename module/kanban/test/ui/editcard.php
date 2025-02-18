8f00c12134391fdee6944e48671411f3fdb1d4d8
lixiaoyu <lixiaoyu@easycorp.ltd>
2025-02-18 08:25:41 +0000

    *[task#137817,doing,0.2h,2h] add data of edit card

---
+++ module/kanban/test/ui/editcard.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php
+/**
+title=编辑卡片
+timeout=0
+cid=0
+*/
+chdir(__DIR__);
+include '../lib/card.ui.class.php';
+
+$kanbanspace = zenData('kanbanspace');
+$kanbanspace->id->range('1');
+$kanbanspace->name->range('协作空间-A');
+$kanbanspace->type->range('cooperation');
+$kanbanspace->owner->range('admin');
+$kanbanspace->team->range(',admin');
+$kanbanspace->acl->range('open');
+$kanbanspace->status->range('active');
+$kanbanspace->createdBy->range('admin');
+$kanbanspace->gen(1);
