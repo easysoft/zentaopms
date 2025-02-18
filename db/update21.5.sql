1ef4cea72294ecbba98c70bef48c326fa273139b
tianshujie <tianshujie@easycorp.ltd>
2025-02-18 15:36:41 +0800

    * [task#137483,doing,0.1h] change field name.

--- db/update21.5.sql
+++ db/update21.5.sql
@@ -6,4 +6,4 @@ ALTER TABLE `zt_doc` ADD COLUMN `editUsers` text NULL AFTER `editGroups`;
 ALTER TABLE `zt_doc` ADD COLUMN `readGroups` varchar(255) NOT NULL DEFAULT '' AFTER `users`;
 ALTER TABLE `zt_doc` ADD COLUMN `readUsers` text NULL AFTER `readGroups`;
 
-ALTER TABLE `zt_feedback` ADD COLUMN `preStatus` varchar(30) NOT NULL DEFAULT '' AFTER `subStatus`;
+ALTER TABLE `zt_feedback` ADD COLUMN `prevStatus` varchar(30) NOT NULL DEFAULT '' AFTER `subStatus`;
