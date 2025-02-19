cce28d452befb293464424effc062b59a97e9f8e
lihuifang <lihuifang@easycorp.ltd>
2025-02-19 14:13:13 +0800

    +[task#137795,doing,0.2h,3.5h] Modify zendata.

--- module/projectrelease/test/ui/releaseexport.php
+++ module/projectrelease/test/ui/releaseexport.php
@@ -21,23 +21,39 @@ cid=73
 chdir(__DIR__);
 include '../lib/releaseexport.ui.class.php';
 
-zendata('release')->loadYaml('projectrelease', false, 1)->gen(1);
-zendata('project')->loadYaml('project', false, 1)->gen(1);
-zendata('product')->loadYaml('product', false, 1)->gen(1);
-zendata('projectproduct')->loadYaml('projectproduct', false, 1)->gen(1);
+$product = zenData('product');
+$product->id->range('1');
+$product->name->range('产品1');
+$product->type->range('normal');
+$product->gen(1);
+
+$system = zenData('system');
+$system->id->range('1');
+$system->product->range('1');
+$system->name->range('应用AAA');
+$system->status->range('active');
+$system->integrated->range('0');
+$system->createdBy->range('admin');
+$system->gen(1);
+
+$project = zenData('project');
+$project->id->range('1');
+$project->project->range('0');
+$project->model->range('scrum');
+$project->type->range('project');
+$project->attribute->range('[]');
 
 $tester = new releaseExportTester();
 $tester->login();
 
 //设置导出发布数据
 $release = array(
-    array('filename' => ''),
     array('filename' => '导出文件1'),
     array('filename' => '导出文件2', 'exportdata' => '解决的Bug'),
 );
 
-r($tester->exportWithNoFilename($release['0'])) && p('message,status') && e('项目发布导出必填提示信息正确,SUCCESS');   // 项目发布导出时文件名必填项检查
-r($tester->releaseExport($release['1']))        && p('message,status') && e('项目发布导出成功,SUCCESS');               // 项目发布导出所有数据
-r($tester->releaseExport($release['2']))        && p('message,status') && e('项目发布导出成功,SUCCESS');               // 项目发布导出指定数据
+r($tester->exportWithNoFilename())       && p('message,status') && e('项目发布导出必填提示信息正确,SUCCESS');   // 项目发布导出时文件名必填项检查
+r($tester->releaseExport($release['0'])) && p('message,status') && e('项目发布导出成功,SUCCESS');               // 项目发布导出所有数据
+r($tester->releaseExport($release['1'])) && p('message,status') && e('项目发布导出成功,SUCCESS');               // 项目发布导出指定数据
 
 $tester->closeBrowser();
