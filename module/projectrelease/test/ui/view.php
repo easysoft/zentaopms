424192ef5c2fd43dd1f077fce3f151d0618de1e9
lihuifang <lihuifang@easycorp.ltd>
2025-02-19 16:39:16 +0800

    +[task#137795,doing,0.2h,2.3h] Modify testui case.

--- module/projectrelease/test/ui/view.php
+++ module/projectrelease/test/ui/view.php
@@ -2,9 +2,15 @@
 <?php
 
 /**
+
 title=项目发布详情
 timeout=0
 cid=73
+
+- 项目发布详情检查
+ - 测试结果 @项目发布详情查看成功
+ - 最终测试状态 @SUCCESS
+
 */
 chdir(__DIR__);
 include '../lib/view.ui.class.php';
@@ -13,6 +19,6 @@ zendata('release')->loadYaml('projectrelease', false, 1)->gen(1);
 $tester = new viewTester();
 $tester->login();
 
-r($tester->checkReleaseView()) && p('status') && e('SUCCESS');    //项目发布详情检查
+r($tester->checkReleaseView()) && p('message,status') && e('项目发布详情查看成功,SUCCESS');    //项目发布详情检查
 
 $tester->closeBrowser();
