7611203c79bab3238ba85bd61e6151480f96fae5
lihuifang <lihuifang@easycorp.ltd>
2025-02-19 16:28:53 +0800

    +[task#137795,doing,0.2h,2.5h] Modify testui case.

--- module/projectrelease/test/ui/releaselinkbug.php
+++ module/projectrelease/test/ui/releaselinkbug.php
@@ -6,9 +6,15 @@ title=项目发布关联和移除Bug
 timeout=0
 cid=73
 
-- 项目发布关联bug最终测试状态 @SUCCESS
-- 单个移除bug最终测试状态 @SUCCESS
-- 移除全部bug测试结果 @移除全部bug成功
+- 项目发布关联bug
+ - 测试结果 @发布关联bug成功
+ - 最终测试状态 @SUCCESS
+- 单个移除bug
+ - 测试结果 @单个移除bug成功
+ - 最终测试状态 @SUCCESS
+- 移除全部bug
+ - 测试结果 @移除全部bug成功
+ - 最终测试状态 @SUCCESS
 
 */
 chdir(__DIR__);
@@ -22,8 +28,8 @@ zendata('projectproduct')->loadYaml('projectproduct', false, 1)->gen(1);
 $tester = new releaseLinkBugTester();
 $tester->login();
 
-r($tester->linkBug())        && p('status')  && e('SUCCESS');         // 项目发布关联bug
-r($tester->unlinkBug())      && p('status')  && e('SUCCESS');         // 单个移除bug
-r($tester->batchUnlinkBug()) && p('message') && e('移除全部bug成功'); // 移除全部bug
+r($tester->linkBug())        && p('message,status') && e('发布关联bug成功,SUCCESS'); // 项目发布关联bug
+r($tester->unlinkBug())      && p('message,status') && e('单个移除bug成功,SUCCESS'); // 单个移除bug
+r($tester->batchUnlinkBug()) && p('message,status') && e('移除全部bug成功,SUCCESS'); // 移除全部bug
 
 $tester->closeBrowser();
