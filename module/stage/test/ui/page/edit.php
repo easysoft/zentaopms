8b42fdadc9392059c2b1ac1921b4a60470a46d3e
huairong <huairong@easycorp.ltd>
2025-02-19 10:18:07 +0800

    + [task#137816,doing,0.1h,3.0h] Modify xpath of edit stage page.

--- module/stage/test/ui/page/edit.php
+++ module/stage/test/ui/page/edit.php
@@ -5,8 +5,8 @@ class editPage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'submitBtn'     => "//*[@id='zin_stage_edit_1_form']/div[4]/div/button/span",
-            'plusSubmitBtn' => "//*[@id='zin_stage_edit_7_form']/div[4]/div/button",
+            'submitBtn'     => "//*[@id='zin_stage_edit_1_form']/div[3]/div/button/span",
+            'plusSubmitBtn' => "//*[@id='zin_stage_edit_7_form']/div[3]/div/button",
             'percentOverTip' => "//*[@id='percentTip']",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
