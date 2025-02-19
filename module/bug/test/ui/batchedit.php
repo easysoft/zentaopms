555c89d3df526d2b8bf04983aac7a1190bebb085
luxuyang <luxuyang@easycorp.ltd>
2025-02-19 16:02:31 +0800

    * [misc,done,0.5h,0h] fix UI script error.

--- module/bug/test/ui/batchedit.php
+++ module/bug/test/ui/batchedit.php
@@ -16,19 +16,24 @@ cid=1
 */
 $tester = new createBugTester();
 zenData('product')->loadYaml('product')->gen(1);
+zenData('project')->loadYaml('execution')->gen(1);
 $story = zenData('story');
-$story->id->setFields(array(array('range' => '2')));
-$story->version->setFields(array(array('range' => '1')));
+$story->id->range(2);
+$story->version->range('1');
 $story->gen(1);
 $bug = zenData('bug');
+$bug->project->range('1');
+$bug->execution->range('1');
+$bug->openedBuild->range('trunk');
 $bug->gen(3);
 
 $bugs = array();
-$bugs[0] = array('title' => 'bug' . time(), 'deadline' => '2028-06-06', 'type' => '安装部署', 'pri' => '1', 'severity' => '1');
-$bugs[1] = array('title' => 'bug' . time(), 'deadline' => '2027-06-06', 'type' => '配置相关', 'pri' => '2', 'severity' => '2');
-$bugs[2] = array('title' => 'bug' . time(), 'deadline' => '2026-06-06', 'type' => '安全相关', 'pri' => '3', 'severity' => '3');
+$bugs[0] = array('title' => 'bug' . time(), 'deadline' => '2028-06-06');
+$bugs[1] = array('title' => 'bug' . time(), 'deadline' => '2027-06-06');
+$bugs[2] = array('title' => 'bug' . time(), 'deadline' => '2026-06-06');
 
 $product = array();
 $product['productID'] = 1;
 
 r($tester->batchEdit($product, $bugs)) && p('message,status') && e('批量编辑bug成功,SUCCESS'); //批量编辑bug
+$tester->closeBrowser();
