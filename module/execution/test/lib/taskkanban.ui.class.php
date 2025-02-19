aaa00bf0d4ca3b43fbf1fc6c6f47b8ca99371990
yulujie <yulujie@easycorp.ltd>
2025-02-19 14:35:40 +0800

    * [task#137796,doing,0.5h,9h] Modify uitest of taskkanban function.

--- module/execution/test/lib/taskkanban.ui.class.php
+++ module/execution/test/lib/taskkanban.ui.class.php
@@ -17,6 +17,7 @@ class taskkanbanTester extends tester
     {
         $form = $this->initForm('execution', 'taskkanban', array('execution' => '2'), 'appIframe-execution');
 
+        /* 选择看板 */
         if(!empty($nameId))
         {
             $form->dom->xpath['kanbanName'] = "(//menu/menu/li)[{$nameId}]";
@@ -24,6 +25,7 @@ class taskkanbanTester extends tester
             $form->dom->kanbanName->click();
             $form->wait(1);
         }
+        /* 选择泳道分组 */
         if(!empty($groupId))
         {
             $form->dom->xpath['kanbanGroup'] = "(//menu/menu/li)[{$groupId}]";
@@ -32,6 +34,7 @@ class taskkanbanTester extends tester
             $form->wait(1);
         }
 
+        /* 检查对应泳道下的卡片数量 */
         $form->dom->xpath['col'] = "//div[@z-col='{$col}']//div[@class='card']";
         if(count($form->dom->getElementList($form->dom->xpath['col'])->element) != $num) return $this->failed("数据错误");
         return $this->success("数据正确");
