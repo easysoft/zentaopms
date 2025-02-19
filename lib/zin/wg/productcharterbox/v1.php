1f6e3512e12df576ab68d1f5b8190141870403dc
liumengyi <liumengyi@easycorp.ltd>
2025-02-19 09:25:50 +0800

    Revert "+ [task#137378,start,0.2h] add branch picker."
    This reverts commit 348853d97ac0319e3ff6496bd99aab407ca27f21.

--- lib/zin/wg/productcharterbox/v1.php
+++ lib/zin/wg/productcharterbox/v1.php
@@ -35,7 +35,6 @@ class productCharterBox extends wg
         (
             setClass('productsBox'),
             setData(array('objecttype' => $this->prop('objectType'))),
-            jsVar('multiBranchProducts', data('multiBranchProducts')),
             $productsBox
         );
     }
@@ -86,22 +85,6 @@ class productCharterBox extends wg
                                         set::items($products),
                                         set::value($productID)
                                     )
-                                )
-                            )
-                        ),
-                        formGroup
-                        (
-                            set::width('1/4'),
-                            setClass('hidden linkBranch ml-px'),
-                            set::label(''),
-                            inputGroup
-                            (
-                                setClass('branchBox'),
-                                picker
-                                (
-                                    set::name('branch[0]'),
-                                    set::items(array()),
-                                    setData(array('on' => 'change', 'call' => 'branchChange', 'params' => 'event'))
                                 )
                             )
                         ),
