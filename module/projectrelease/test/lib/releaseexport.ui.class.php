18e45f40e9d8199134da6fce89b309ec471b59a9
lihuifang <lihuifang@easycorp.ltd>
2025-02-19 14:15:19 +0800

    +[task#137795,doing,0.2h,2.8h] Modify function.

--- module/projectrelease/test/lib/releaseexport.ui.class.php
+++ module/projectrelease/test/lib/releaseexport.ui.class.php
@@ -9,17 +9,18 @@ class releaseExportTester extends tester
      * @param  array $release
      * @access public
      */
-    public function releaseExport($release = array())
+    public function releaseExport($release)
     {
-        $form = $this->initForm('projectrelease', 'view', array('releaseID' => 1), 'appIframe-project');
-        $form->dom->exportBtn->click();
+        $form = $this->initForm('projectrelease', 'browse', array('project' => 1), 'appIframe-project');
+        $form->dom->releaseNameBrowse->click();
+        $form->dom->btn($this->lang->projectrelease->export)->click();
         if(isset($release['filename']))   $form->dom->fileName->setValue($release['filename']);
-        if(isset($release['exportdata'])) $form->dom->exportData->picker($release['exportdata']);
+        if(isset($release['exportdata'])) $form->dom->type->picker($release['exportdata']);
 
-        $form->dom->exportBtnAlert->click();
+        $form->dom->btn($this->lang->project->export)->click();
         $form->wait(2);
 
-        if($form->dom->exportBtnAlert) return $this->failed('项目发布导出失败');
+        if(is_object($this->lang->project->export)) return $this->failed('项目发布导出失败');
         return $this->success('项目发布导出成功');
     }
 
@@ -32,9 +33,10 @@ class releaseExportTester extends tester
      */
     public function exportWithNoFilename()
     {
-        $form = $this->initForm('projectrelease', 'view', array('releaseID' => 1), 'appIframe-project');
-        $form->dom->exportBtn->click();
-        $form->dom->exportBtnAlert->click();
+        $form = $this->initForm('projectrelease', 'browse', array('project' => 1), 'appIframe-project');
+        $form->dom->releaseNameBrowse->click();
+        $form->dom->btn($this->lang->projectrelease->export)->click();
+        $form->dom->btn($this->lang->project->export)->click();
         $form->wait(2);
 
         /*点击导出按钮后，检查必填校验*/
