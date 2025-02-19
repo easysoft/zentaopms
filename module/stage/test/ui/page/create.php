0eaa86af708b50ec4364aef2e20ed69def6a6515
huairong <huairong@easycorp.ltd>
2025-02-19 10:17:38 +0800

    + [task#137816,doing,0.1h,3.1h] Modify xpath of create stage page.

--- module/stage/test/ui/page/create.php
+++ module/stage/test/ui/page/create.php
@@ -5,7 +5,7 @@ class createPage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'submitBtn'      => "//*[@id='zin_stage_create_form']/div[4]/div/button",
+            'submitBtn'      => "//*[@id='zin_stage_create_form']/div[3]/div/button",
             'percentOverTip' => "//*[@id='percentTip']",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
