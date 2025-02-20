17e3a0b799c74475d2a72e393f1723107586d99d
huairong <huairong@easycorp.ltd>
2025-02-20 13:14:36 +0800

    + [task#137816,done,0.1h] Modify xpath of project testcase page.

--- module/project/test/ui/page/testcase.php
+++ module/project/test/ui/page/testcase.php
@@ -10,7 +10,7 @@ class testcasePage extends page
             'waitingTab'      => "//*[@id='featureBar']/menu/li[2]/a",
             'storyChangedTab' => "//*[@id='featureBar']/menu/li[4]/a",
             'storyNoCaseTab'  => "//*[@id='featureBar']/menu/li[5]/a",
-            'testcaseNum'     => "//*[@id='table-project-testcase']/div[3]/nav/div[1]",
+            'testcaseNum'     => "//*[@id='testcases']/div[3]/nav/div[1]",
             'zerocaseNum'     => "//*[@id='table-testcase-zerocase']/div[3]/nav/div[1]",
             /* 1.5级产品导航 */
             'dropMenu'        => "//*[@id='pick-project-menu']",
