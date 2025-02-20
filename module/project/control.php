5ba94ef156ad3b9155cbf498921ff2478a03df63
tianshujie <tianshujie@easycorp.ltd>
2025-02-20 13:30:02 +0800

    * [bug#59890,done,0.2h] fix an error when removing a member.

--- module/project/control.php
+++ module/project/control.php
@@ -1172,7 +1172,7 @@ class project extends control
         /* if ajax request, send result. */
         if(dao::isError()) return $this->sendError(dao::getError());
 
-        $this->execution->getLimitedExecution();
+        $this->loadModel('execution')->getLimitedExecution();
 
         return $this->send(array('result' => 'success', 'load' => $this->createLink($this->config->vision == 'or' ? 'marketresearch' : 'project', 'team', "projectID={$projectID}")));
     }
