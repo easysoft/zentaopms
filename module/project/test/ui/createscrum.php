106b593f1911fb5337de4f18cbf678109f1c21f8
lihuifang <lihuifang@easycorp.ltd>
2025-02-20 14:46:12 +0800

    +[task#137795,doing,0.2h,1.5h] Modify testui case.

--- module/project/test/ui/createscrum.php
+++ module/project/test/ui/createscrum.php
@@ -3,7 +3,7 @@
 
 /**
 
-title=创建敏捷项目测试
+title=创建敏捷项目
 timeout=0
 cid=73
 
@@ -13,9 +13,15 @@ cid=73
 - 计划完成时间置空，检查提示信息
  - 测试结果 @创建敏捷项目表单页提示信息正确
  - 最终测试状态 @SUCCESS
-- 创建有日期的产品型项目最终测试状态 @SUCCESS
-- 创建一个长期的产品型项目，检查提示信息最终测试状态 @SUCCESS
-- 创建有日期的项目型项目最终测试状态 @SUCCESS
+- 创建有日期的产品型项目
+ - 测试结果 @创建敏捷项目成功
+ - 最终测试状态 @SUCCESS
+- 创建一个长期的产品型项目，检查提示信息
+ - 测试结果 @创建敏捷项目成功
+ - 最终测试状态 @SUCCESS
+- 创建有日期的项目型项目
+ - 测试结果 @创建敏捷项目成功
+ - 最终测试状态 @SUCCESS
 
 */
 chdir(__DIR__);
@@ -36,8 +42,8 @@ $scrum = array(
 
 r($tester->checkInput($scrum['0'])) && p('message,status') && e('创建敏捷项目表单页提示信息正确,SUCCESS');   // 缺少项目名称，检查提示信息
 r($tester->checkInput($scrum['1'])) && p('message,status') && e('创建敏捷项目表单页提示信息正确,SUCCESS');   // 计划完成时间置空，检查提示信息
-r($tester->checkInput($scrum['2'])) && p('status')  && e('SUCCESS');                                         // 创建有日期的产品型项目
-r($tester->checkInput($scrum['3'])) && p('status')  && e('SUCCESS');                                         // 创建一个长期的产品型项目，检查提示信息
-r($tester->checkInput($scrum['4'])) && p('status')  && e('SUCCESS');                                         // 创建有日期的项目型项目
+r($tester->checkInput($scrum['2'])) && p('message,status') && e('创建敏捷项目成功,SUCCESS');                 // 创建有日期的产品型项目
+r($tester->checkInput($scrum['3'])) && p('message,status') && e('创建敏捷项目成功,SUCCESS');                 // 创建一个长期的产品型项目，检查提示信息
+r($tester->checkInput($scrum['4'])) && p('message,status') && e('创建敏捷项目成功,SUCCESS');                 // 创建有日期的项目型项目
 
 $tester->closeBrowser();
