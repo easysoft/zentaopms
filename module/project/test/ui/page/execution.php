0a508fd1b4927a682c5e09fd6b6264da57ab587a
lihuifang <lihuifang@easycorp.ltd>
2025-02-21 09:26:16 +0800

    +[task#137795,doing,0.2h,2h] Add element.

--- module/project/test/ui/page/execution.php
+++ module/project/test/ui/page/execution.php
@@ -14,7 +14,8 @@ class executionPage extends page
             'editBtn'    => "//*[@id='table-project-execution']/div[2]/div[3]/div/div/div/nav/a[3]/i",
             'submitBtn'  => "//*[@id='zin_programplan_edit_form']/div[9]/div/button/span",
             'addSprint'  => "//*[@id='actionBar']/a[2]/span",
-            'sprintName' => "//*[@id='mainContent']/div/div/div[2]/div/div/div[2]/div/div/div/a",
+            'sprintName' => "//*[@id='table-project-execution']/div[2]/div[1]/div/div[2]/div/a",
+            'planEnd'    => "//*[@id='table-project-execution']/div[2]/div[2]/div/div[6]/div",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
