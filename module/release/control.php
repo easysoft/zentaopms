e067c02df73249440afbfb864dec4f5d45413ffd
daitingting <daitingting@xirangit.com>
2025-02-25 05:58:19 +0000

    * [bug#59484,done,1h] Exclude shadow product.

--- module/release/control.php
+++ module/release/control.php
@@ -59,7 +59,7 @@ class release extends control
         {
             $this->app->loadLang('doc');
 
-            $products = $this->loadModel('product')->getPairs('nodeleted', 0, '', 'all');
+            $products = $this->loadModel('product')->getPairs('nodeleted', 0, '');
             if(empty($products)) return $this->send(array('result' => 'fail', 'message' => $this->lang->doc->tips->noProduct));
 
             if(!$productID) $productID = key($products);
