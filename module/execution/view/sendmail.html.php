3aa048d7b78328d16e9601ac513770c416af2589
liumengyi <liumengyi@easycorp.ltd>
2025-02-26 16:21:39 +0800

    + [bug#59384,done,0.5h] add mail content.

--- module/execution/view/sendmail.html.php
+++ module/execution/view/sendmail.html.php
@@ -1,3 +1,15 @@
+<?php
+/**
+ * The mail file of execution module of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2025 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.cnezsoft.com)
+ * @license     ZPL(http://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Mengyi Liu
+ * @package     execution
+ * @version     $Id: sendmail.html.php
+ * @link        https://www.zentao.net
+ */
+?>
 <?php $mailTitle = 'EXECUTION #' . $object->id . ' ' . $object->name;?>
 <?php include $this->app->getModuleRoot() . 'common/view/mail.header.html.php';?>
 <tr>
