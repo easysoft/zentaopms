4ff53f58b36c4cc7f6c87cdb6e1dd3e5eb916c82
liyang <liyang@easycorp.ltd>
2025-02-28 11:08:38 +0800

    * [uitest] add link html.

--- test/list.html.php
+++ test/list.html.php
@@ -9,3 +9,18 @@ foreach($link as $module => $group)
         $links[$module][$method][$name] = $url;
     }
 }
+?>
+<script src="https://cdn.staticfile.net/jquery/1.10.2/jquery.min.js">
+</script>
+<style>
+h1 {text-align: center;}
+ul {padding: 0;}
+ul li {list-style: none; padding: 4px 0px; margin: 0px;}
+.row {width: 100%; margin: 20px; display: flex; flex-flow: row wrap;}
+.col {flex: 0 0 19.5%;}
+a {color: #0c64eb;}
+.moduleName {color: #000;}
+.methodName {color: #000;}
+.methodList {border: 1px solid #000; margin: 10px; margin-left: 0px; padding: 10px;}
+body {overflow-x: hidden;}
+</style>
