81f80baf4345d574187e5b72f5bd240adc97af58
liumengyi <liumengyi@easycorp.ltd>
2025-03-02 09:47:25 +0800

    + [bug#60056,done,1h] set history file actions for docapp.

--- lib/zin/wg/docapp/v1.php
+++ lib/zin/wg/docapp/v1.php
@@ -256,6 +256,28 @@ class docApp extends wg
                 ->const('downloadLink', $fileUrl)
                 ->const('libreOfficeTurnon', isset($config->file->libreOfficeTurnon) && $config->file->libreOfficeTurnon == 1)
                 ->do("
+                let fileActions = [];
+
+                let canPreview       = false;
+                let officeTypes      = 'doc|docx|xls|xlsx|ppt|pptx|pdf';
+                let isOfficeFile     = officeTypes.includes(file.extension);
+                let previewExtension = 'txt|jpg|jpeg|gif|png|bmp|mp4';
+                if(previewExtension.includes(file.extension)) canPreview = true;
+                if(libreOfficeTurnon && isOfficeFile)         canPreview = true;
+                if(canPreview)
+                {
+                    let previewAction = {icon: 'eye', title: previewLang, url: previewLink.replace('{id}', file.id).replace('\\', ''), className: 'text-primary', target: '_blank'};
+                    if(!isOfficeFile)
+                    {
+                        previewAction['data-toggle'] = 'modal';
+                        previewAction['data-size'] = 'lg';
+                        delete previewAction.target;
+                    }
+                    fileActions.push(previewAction);
+                }
+
+                fileActions.push({icon: 'download', title: downloadLang, url: downloadLink.replace('{id}', file.id).replace('\\', ''), className: 'text-primary', target: '_blank'});
+                return fileActions;
             ");
         }
         else
