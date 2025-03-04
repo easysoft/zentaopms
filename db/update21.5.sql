11792c617b37b97ea96609e767702a4664021c5a
qixinzhi <qixinzhi@easycorp.ltd>
2025-03-04 06:15:49 +0000

    * [story#72123] mv update sql.

--- db/update21.5.sql
+++ db/update21.5.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `zt_feedback` ADD COLUMN `prevStatus` varchar(30) NOT NULL DEFAULT '' AFTER `subStatus`;
 ALTER TABLE `zt_feedback` ADD COLUMN `prevAssignedTo` varchar(255) NOT NULL DEFAULT '' AFTER `assignedTo`;
+
+ALTER TABLE `zt_doc` ADD COLUMN `editGroups` varchar(255) NOT NULL DEFAULT '' AFTER `users`;
+ALTER TABLE `zt_doc` ADD COLUMN `editUsers` varchar(255) NULL AFTER `editGroups`;
