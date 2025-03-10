84041dbe222747fb8a9ee17a0379acc7899d81e7
qixinzhi <qixinzhi@easycorp.ltd>
2025-03-10 02:03:35 +0000

    * [misc] fix sql.

--- db/update21.5.sql
+++ db/update21.5.sql
@@ -1,10 +1,5 @@
 ALTER TABLE `zt_feedback` ADD COLUMN `prevStatus` varchar(30) NOT NULL DEFAULT '' AFTER `subStatus`;
 ALTER TABLE `zt_feedback` ADD COLUMN `prevAssignedTo` varchar(255) NOT NULL DEFAULT '' AFTER `assignedTo`;
 
-ALTER TABLE `zt_doc` ADD COLUMN `editGroups` varchar(255) NOT NULL DEFAULT '' AFTER `users`;
-ALTER TABLE `zt_doc` ADD COLUMN `editUsers` text NULL AFTER `editGroups`;
 ALTER TABLE `zt_doc` ADD COLUMN `readGroups` varchar(255) NOT NULL DEFAULT '' AFTER `users`;
 ALTER TABLE `zt_doc` ADD COLUMN `readUsers` text NULL AFTER `readGroups`;
-
-ALTER TABLE `zt_feedback` ADD COLUMN `prevStatus` varchar(30) NOT NULL DEFAULT '' AFTER `subStatus`;
-ALTER TABLE `zt_feedback` ADD COLUMN `prevAssignedTo` varchar(255) NOT NULL DEFAULT '' AFTER `assignedTo`;
