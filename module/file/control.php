137917e05ba29a4307782bda2d17ac8c2b1bd187
sunhao <sunhao@chandao.com>
2025-03-12 19:25:09 +0800

    * [misc] change base64 encoding of title param of file-ajaxQuery.

--- module/file/control.php
+++ module/file/control.php
@@ -577,7 +577,6 @@ class file extends control
             $this->sendError(404, '404 Not found');
         }
 
-        if(!empty($title)) $title = base64_decode($title);
         $file = $this->file->query($objectType, $objectID, $title, $extra);
         if(empty($file)) return $this->send(array('result' => 'fail', 'message' => $this->lang->file->fileNotFound, 'load' => helper::createLink('my', 'index'), 'closeModal' => true));
         $fileID = $file->id;
