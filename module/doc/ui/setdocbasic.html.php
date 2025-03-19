181444e5274e3b4fe09839b614a2fb3ec7c37622
daitingting <daitingting@xirangit.com>
2025-03-19 08:16:58 +0000

    * [bug#60673,done,0.5h] Adjust aclList for chapter.

--- module/doc/ui/setdocbasic.html.php
+++ module/doc/ui/setdocbasic.html.php
@@ -10,6 +10,8 @@ declare(strict_types=1);
  */
 namespace zin;
 
+if($modalType == 'chapter') $lang->doc->aclList['private'] = $lang->doclib->aclList['private'];
+
 formPanel
 (
     setID('setDocBasicForm'),
