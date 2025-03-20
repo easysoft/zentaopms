b24c482f1e457df06ff27da9818f7bc922957609
daitingting <daitingting@xirangit.com>
2025-03-20 01:42:59 +0000

    * [misc] Set default value.

--- module/doc/ui/setdocbasic.html.php
+++ module/doc/ui/setdocbasic.html.php
@@ -90,7 +90,7 @@ formPanel
         (
             set::name('parent'),
             set::items($modalType != 'chapter' ? array(0 => '/') + $chapterAndDocs : $chapterAndDocs),
-            set::value(!empty($doc->parent) ? $doc->parent : "m_{$doc->module}"),
+            !$isCreate ? set::value(!empty($doc->parent) ? $doc->parent : "m_{$doc->module}") : null,
             set::required(true)
         ),
     ) : null,
