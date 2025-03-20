98345c23af556f902ad9540b07638578cbb418ea
sunguangming <sunguangming@chandao.com>
2025-03-20 01:19:36 +0000

    * [misc] remove emoji.

--- module/convert/tao.php
+++ module/convert/tao.php
@@ -995,7 +995,7 @@ class convertTao extends convertModel
             $objectID   = zget($issueList[$issueID], 'BID',   '');
 
             if(empty($objectID)) continue;
-            $comment = preg_replace('/[\x{1F600}-\x{1F64F}\x{1F300}-\x{1F5FF}\x{1F680}-\x{1F6FF}\x{2600}-\x{26FF}\x{2700}-\x{27BF}]/u', '', $comment);
+            $comment = preg_replace('/[\x{10000}-\x{10FFFF}]/u', '', $comment);
 
             $action = new stdclass();
             $action->objectType = substr($objectType, 1);
