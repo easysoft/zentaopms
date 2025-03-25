f82798c4aa5cfdb3e9584c57d0c351be06e51f73
wangyidong <yidong@chandao.com>
2025-03-25 17:23:12 +0800

    * [pref story #72218] delete useless field for workflow.

--- db/update21.6.beta.sql
+++ db/update21.6.beta.sql
@@ -4,6 +4,7 @@ REPLACE INTO `zt_workflowaction` (`module`, `action`, `method`, `name`, `type`, 
 ('feedback', 'import', 'import', '导入', 'single', 'different', 'none', 'modal', 'browse', 'normal', 'direct', 0, 1, 'buildin', 0, NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, 'enable', 'rnd', 'admin', '2025-03-24 14:50:45', '', NULL),
 ('feedback', 'showimport', 'showimport', '显示导入内容', 'single', 'different', 'none', 'modal', 'browse', 'normal', 'direct', 0, 1, 'buildin', 0, NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, 'enable', 'rnd', 'admin', '2025-03-24 14:50:45', '', NULL);
 
+DELETE FROM `zt_workflowdatasource` WHERE `code` IN ('color', 'litecolor');
 INSERT INTO `zt_workflowdatasource` (`type`, `name`, `code`, `buildin`, `vision`, `createdBy`, `createdDate`, `datasource`, `view`, `keyField`, `valueField`) VALUES
 ('option', '颜色', 'color',     '1', 'rnd',  'admin', '1970-01-01 00:00:01', '{"#ef4444":"#ef4444","#f97316":"#f97316","#eab308":"#eab308","#84cc16":"#84cc16","#22c55e":"#22c55e","#14b8a6":"#14b8a6","#0ea5e9":"#0ea5e9","#6366f1":"#6366f1","#a855f7":"#a855f7","#d946ef":"#d946ef","#ec4899":"#ec4899"}', '', '', ''),
 ('option', '颜色', 'litecolor', '1', 'lite', 'admin', '1970-01-01 00:00:01', '{"#ef4444":"#ef4444","#f97316":"#f97316","#eab308":"#eab308","#84cc16":"#84cc16","#22c55e":"#22c55e","#14b8a6":"#14b8a6","#0ea5e9":"#0ea5e9","#6366f1":"#6366f1","#a855f7":"#a855f7","#d946ef":"#d946ef","#ec4899":"#ec4899"}', '', '', '');
@@ -12,3 +13,5 @@ UPDATE `zt_workflowfield` SET `control` = 'select', `options` = (SELECT id FROM 
 UPDATE `zt_workflowfield` SET `control` = 'select', `options` = (SELECT id FROM `zt_workflowdatasource` WHERE `code` = 'bugs'  limit 1) WHERE `module` = 'testcase' AND `field` = 'fromBug';
 DELETE FROM `zt_workflowfield` WHERE `module` = 'testcase' AND `field` = 'order';
 DELETE FROM `zt_workflowfield` WHERE `module` = 'testcase' AND `field` = 'frequency';
+DELETE FROM `zt_workflowfield` WHERE `module` = 'product' AND `field` = 'order';
+DELETE FROM `zt_workflowfield` WHERE `module` = 'product' AND `field` = 'createdVersion';
