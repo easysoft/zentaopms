06a3a7562da3d9781470811b2608f469ef42671d
liyang <liyang@easycorp.ltd>
2025-03-31 15:15:05 +0800

    + [task#140380] add job search config.

--- module/job/config.php
+++ module/job/config.php
@@ -19,3 +19,13 @@ $config->job->search['fields']['engine']      = $lang->job->engine;
 $config->job->search['fields']['frame']       = $lang->job->frame;
 $config->job->search['fields']['triggerType'] = $lang->job->triggerType;
 $config->job->search['fields']['lastExec']    = $lang->job->lastExec;
+
+$config->job->search['params']['id']          = array('operator' => '=', 'control' => 'input', 'values' => '');
+$config->job->search['params']['name']        = array('operator' => '=', 'control' => 'input', 'values' => '');
+$config->job->search['params']['lastStatus']  = array('operator' => '=', 'control' => 'select', 'values' => $lang->compile->statusList);
+$config->job->search['params']['product']     = array('operator' => '=', 'control' => 'select', 'values' => array());
+$config->job->search['params']['repo']        = array('operator' => '=', 'control' => 'select', 'values' => array());
+$config->job->search['params']['engine']      = array('operator' => '=', 'control' => 'select', 'values' => $lang->job->engineList);
+$config->job->search['params']['frame']       = array('operator' => '=', 'control' => 'select', 'values' => $lang->job->frameList);
+$config->job->search['params']['triggerType'] = array('operator' => '=', 'control' => 'select', 'values' => $lang->job->triggerTypeList);
+$config->job->search['params']['lastExec']    = array('operator' => '=', 'control' => 'date', 'values' => '');
