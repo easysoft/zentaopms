2f30ea4312e968a66195464d50322de179052b1b
liyang <liyang@easycorp.ltd>
2025-03-31 15:14:56 +0800

    + [task#140380] add job search config.

--- module/job/config.php
+++ module/job/config.php
@@ -1,6 +1,21 @@
 <?php
+global $lang, $app;
+$app->loadLang('compile');
+
 $config->job = new stdclass();
 $config->job->create = new stdclass();
 $config->job->edit   = new stdclass();
 $config->job->create->requiredFields = 'name,repo,engine,server,pipeline';
 $config->job->edit->requiredFields   = 'name,repo,server,pipeline';
+
+/* Search config. */
+$config->job->search['module']                = 'job';
+$config->job->search['fields']['id']          = $lang->idAB;
+$config->job->search['fields']['name']        = $lang->job->name;
+$config->job->search['fields']['lastStatus']  = $lang->job->lastStatus;
+$config->job->search['fields']['product']     = $lang->job->product;
+$config->job->search['fields']['repo']        = $lang->job->repo;
+$config->job->search['fields']['engine']      = $lang->job->engine;
+$config->job->search['fields']['frame']       = $lang->job->frame;
+$config->job->search['fields']['triggerType'] = $lang->job->triggerType;
+$config->job->search['fields']['lastExec']    = $lang->job->lastExec;
