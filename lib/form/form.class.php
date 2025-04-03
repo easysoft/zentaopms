facb305cfefe4c47ac4ef073e65d52c930ae9014
wangyuting <wangyuting@chandao.com>
2025-04-03 14:03:09 +0800

    * [bug#61097,done,0.5h,0h] Get target module language to rawModule.

--- lib/form/form.class.php
+++ lib/form/form.class.php
@@ -210,7 +210,7 @@ class form extends fixer
      */
     private function processRequiredFields(array $configObject): array
     {
-        global $app, $config;
+        global $app, $config, $lang;
 
         $module = $app->getModuleName();
         $method = $app->getMethodName();
@@ -233,7 +233,11 @@ class form extends fixer
             $field = trim($field);
             if(!isset($configObject[$field])) continue;
 
-            if(!isset($configObject[$field]['skipRequired'])) $configObject[$field]['required'] = true;
+            if(!isset($configObject[$field]['skipRequired']))
+            {
+                $configObject[$field]['required'] = true;
+                if(empty($lang->{$app->rawModule}->$field)) $lang->{$app->rawModule}->$field = $lang->$module->$field;
+            }
         }
 
         return $configObject;
