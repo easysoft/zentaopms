7737204e00d342e88bf6d0e25618efb5061f84c4
wangyuting <wangyuting@chandao.com>
2025-04-09 13:43:31 +0800

    * [task#140893,doing,0.2h,0h] Add check btn priv.

--- module/programplan/ui/gantt.html.php
+++ module/programplan/ui/gantt.html.php
@@ -54,7 +54,7 @@ if($app->rawModule == 'programplan')
             ))
         ),
         btn(set::url($this->createLink('programplan', 'ajaxcustom')), set::icon('cog-outline'), $lang->settings, setClass('no-underline'), set::type('link'), set('data-toggle', 'modal'), set('data-size', 'sm')),
-        btn(set::url($this->createLink('programplan', 'relation', "projectID={$projectID}")), set::icon('list-alt'), $lang->programplan->setTaskRelation, setClass('no-underline'), set::type('link')),
+        common::hasPriv('programplan', 'relation') ? btn(set::url($this->createLink('programplan', 'relation', "projectID={$projectID}")), set::icon('list-alt'), $lang->programplan->setTaskRelation, setClass('no-underline'), set::type('link')) : null,
         (common::canModify('project', $project) && common::hasPriv('programplan', 'create') && empty($product->deleted)) ? btn(set::url($this->createLink('programplan', 'create', "projectID=$projectID")), set::icon('plus'), $lang->programplan->create, setClass('primary programplan-create-btn')) : null
     );
 }
