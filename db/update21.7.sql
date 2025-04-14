952a94fedb3aa2ffcd24c2b2f66739091f72726d
sunguangming <sunguangming@chandao.com>
2025-04-14 08:55:01 +0000

    * [task#141578,done,0.5h] add table.

---
+++ db/update21.7.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS `zt_deliverable` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `module` varchar(30) NULL,
+  `method` varchar(30) NULL,
+  `model` text NULL,
+  `type` enum('doc','file') NULL DEFAULT 'file',
+  `desc` text NULL,
+  `files` varchar(255) NULL,
+  `createdBy` varchar(30) NULL,
+  `createdDate` date NULL,
+  `deleted` enum('0','1') NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- db/zentao.sql
+++ db/zentao.sql
@@ -578,6 +578,22 @@ CREATE TABLE IF NOT EXISTS `zt_screen` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- DROP TABLE IF EXISTS `zt_deliverable`;
+CREATE TABLE IF NOT EXISTS `zt_deliverable` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `module` varchar(30) NULL,
+  `method` varchar(30) NULL,
+  `model` text NULL,
+  `type` enum('doc','file') NULL DEFAULT 'file',
+  `desc` text NULL,
+  `files` varchar(255) NULL,
+  `createdBy` varchar(30) NULL,
+  `createdDate` date NULL,
+  `deleted` enum('0','1') NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 -- DROP TABLE IF EXISTS `zt_dimension`;
 CREATE TABLE IF NOT EXISTS `zt_dimension` (
   `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
