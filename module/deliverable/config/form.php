0ba870ae15e638f36a7fb39e78d4fa1e35791ce2
sunguangming <sunguangming@chandao.com>
2025-04-16 07:27:24 +0000

    * [misc] remove code.

--- module/deliverable/config/form.php
+++
@@ -1,8 +0,0 @@
-<?php
-$config->deliverable->form = new stdclass();
-$config->deliverable->form->create = array();
-$config->deliverable->form->create['name']   = array('type' => 'string', 'required' => true);
-$config->deliverable->form->create['module'] = array('type' => 'string', 'required' => true);
-$config->deliverable->form->create['method'] = array('type' => 'string', 'required' => true);
-$config->deliverable->form->create['model']  = array('type' => 'array',  'required' => true, 'filter' => 'join');
-$config->deliverable->form->create['desc']   = array('type' => 'string', 'required' => false, 'control' => 'editor');
