d350da361ca4515fabe6fbe555ef0b8128cc0c76
sunguangming <sunguangming@chandao.com>
2025-04-16 02:08:57 +0000

    * Add required fields for deliverable.

--- module/deliverable/config.php
+++ module/deliverable/config.php
@@ -1,2 +1,6 @@
 <?php
 $config->deliverable = new stdclass();
+$config->deliverable->create = new stdclass();
+$config->deliverable->edit   = new stdclass();
+$config->deliverable->create->requiredFields = 'name,module,method,model';
+$config->deliverable->edit->requiredFields   = 'name,module,method,model';
