04d3f8a8a3bdc382de5a9c298418af8b291c3538
tianshujie <tianshujie@easycorp.ltd>
2025-04-16 14:57:21 +0800

    * [ci,done,0.1] add unit test for getspaces method.

--- module/doc/test/model/getspaces.php
+++ module/doc/test/model/getspaces.php
@@ -28,3 +28,11 @@ su('admin');
 
 $typeList    = array('all', 'mine', 'custom', 'product', 'project', 'execution');
 $spaceIdList = array(0, 1, 2, 3, 4, 101);
+
+$docTester = new docTest();
+r($docTester->objectModel->getSpaces($typeList[0], $spaceIdList[0])[0]) && p('0')      && e('0');       // 测试获取不存在类型的空间
+r($docTester->objectModel->getSpaces($typeList[1], $spaceIdList[1])[0]) && p('0:type') && e('mine');    // 测试获取我的空间数据
+r($docTester->objectModel->getSpaces($typeList[2], $spaceIdList[2])[0]) && p('0:type') && e('custom');  // 测试获取自定义空间数据
+r($docTester->objectModel->getSpaces($typeList[3], $spaceIdList[3])[0]) && p('0:type') && e('product'); // 测试获取产品空间数据
+r($docTester->objectModel->getSpaces($typeList[4], $spaceIdList[4])[0]) && p('0:type') && e('project'); // 测试获取项目空间数据
+r($docTester->objectModel->getSpaces($typeList[5], $spaceIdList[5])[0]) && p('0:name') && e('迭代5');   // 测试获取执行空间数据
