18ea19e38d344f1297c9185d318b302c0fd6b53f
sunguangming <sunguangming@chandao.com>
2025-04-16 07:02:45 +0000

    * [misc] add en lang.

---
+++ module/deliverable/lang/de.php
@@ -0,0 +1,30 @@
+<?php
+$lang->deliverable->name        = 'Name';
+$lang->deliverable->desc        = 'Description';
+$lang->deliverable->module      = 'Applicable Object';
+$lang->deliverable->method      = 'Action';
+$lang->deliverable->model       = 'Applicable Scope';
+$lang->deliverable->createdBy   = 'Created By';
+$lang->deliverable->createdDate = 'Created Date';
+$lang->deliverable->template    = 'Reference Template';
+$lang->deliverable->files       = 'Upload Template';
+$lang->deliverable->or          = 'OR';
+
+$lang->deliverable->create = 'Add Deliverable';
+$lang->deliverable->edit   = 'Edit Deliverable';
+$lang->deliverable->delete = 'Delete Deliverable';
+$lang->deliverable->view   = 'Deliverable Details';
+
+$lang->deliverable->moduleList['project']   = 'Project';
+$lang->deliverable->moduleList['execution'] = 'Execution';
+
+$lang->deliverable->methodList['create'] = 'Create';
+$lang->deliverable->methodList['close']  = 'Close';
+
+$lang->deliverable->modelList['product_waterfall'] = 'Product Waterfall';
+$lang->deliverable->modelList['project_waterfall'] = 'Project Waterfall';
+$lang->deliverable->modelList['product_scrum']     = 'Product Scrum';
+$lang->deliverable->modelList['project_scrum']     = 'Project Scrum';
+
+$lang->deliverable->confirmDelete = 'Are you sure to delete this deliverable?';
+$lang->deliverable->summary       = 'There are %s deliverables on this page';
