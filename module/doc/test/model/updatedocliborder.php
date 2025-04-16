bd6302dc3b0d9a69198b732a8e2f05786db198bf
tianshujie <tianshujie@easycorp.ltd>
2025-04-16 14:58:36 +0800

    * [ci,doing,0.2h] add unit test for updatedocliborder method.

---
+++ module/doc/test/model/updatedocliborder.php
@@ -0,0 +1,27 @@
+#!/usr/bin/env php
+<?php
+/**
+
+title=测试 docModel->updateDocLibOrder();
+cid=1
+
+- 测试更新ID为0的文档库顺序为2 @0
+- 测试更新ID为1的文档库顺序为5 @5
+- 测试更新ID为2的文档库顺序为1 @1
+- 测试更新ID为3的文档库顺序为10 @10
+- 测试更新ID为4的文档库顺序为7 @7
+- 测试更新ID为5的文档库顺序为9 @9
+- 测试更新ID不存在的文档库顺序为8 @0
+
+*/
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+include dirname(__FILE__, 2) . '/lib/doc.unittest.class.php';
+
+$docLibData = zenData('doclib')->loadYaml('doclib');
+$docLibData->order->range('1-5');
+$docLibData->gen(5);
+zenData('user')->gen(5);
+su('admin');
+
+$idList    = array(0, 1, 2, 3, 4, 5, 6);
+$orderList = array(2, 5, 1, 10, 7, 9, 8);
