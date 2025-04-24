16075e0f22a6d0cace5947957e069ff746c77cbd
lihuifang <lihuifang@easycorp.ltd>
2025-04-24 14:29:56 +0800

    +[task#142428,doing,0.2h,0.6h] Add deleteMembers function of manage members for project in lite.

--- module/project/test/lib/managemembersforlite.ui.class.php
+++ module/project/test/lib/managemembersforlite.ui.class.php
@@ -18,3 +18,23 @@ class manageMembersForLiteTester extends tester
         $form->wait(2);
 
         $form->dom->saveBtn->click();
+        $form->wait(2);
+
+        //断言检查团队成员列表页姓名、角色是否正确
+        $teamPage = $this->loadPage('project', 'team');
+        if($teamPage->dom->browseSecAccountLite->getText() != $members['account']) return $this->failed('姓名错误');
+        if($teamPage->dom->browseSecRoleLite->getText()    != $members['role'])    return $this->failed('角色错误');
+
+        return $this->success('项目团队成员添加成功');
+    }
+
+    /**
+     * 删除项目团队成员。
+     * Check delete members.
+     *
+     * @access public
+     */
+    public function deleteMembers()
+    {
+        $browseForm        = $this->initForm('project', 'team', array('projectID' => '1'), 'appIframe-project');
+        $browseFirAccount1 = $browseForm->dom->browseFirAccount->getText();
