5158f2880dcf02460b75cfb677d6aa16d916385f
lihuifang <lihuifang@easycorp.ltd>
2025-04-24 14:31:29 +0800

    *[task#142428,doing,0.1h,0.1h] Add xpath of element.

--- module/project/test/ui/page/managemembers.php
+++ module/project/test/ui/page/managemembers.php
@@ -7,6 +7,8 @@ class manageMembersPage extends page
         $xpath = array(
             'account'        => "//*[@id='account1']/div/span[1]",
             'firstDeleteBtn' => "//*[@id='teamForm']/table/tbody/tr[1]/td[6]/div/button[2]/i",
+            /* 运营界面 */
+            'saveBtn' => "//*[@id='teamForm']/div/div/button[1]",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
