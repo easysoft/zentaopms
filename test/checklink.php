9ccf1bcdfb84e1a777fa1cd953754afbeab49c4a
liyang <liyang@easycorp.ltd>
2025-04-28 16:38:37 +0800

    + [pipeline] add check url method.

--- test/checklink.php
+++ test/checklink.php
@@ -16,3 +16,16 @@ foreach($link as $module => $LinkList)
     {
         $params = parse_url($url);
         if(isset($params['query']) && in_array($module, array('company', 'project', 'product', 'execution', 'bug', 'feedback', 'ticket')))
+        {
+            parse_str($params['query'], $params);
+            if($params['f'] == 'browse' && isset($params['dept']) && $params['dept'] != 1) continue;
+            if($params['f'] == 'browse' && isset($params['programID']) && $params['programID'] != 1) continue;
+            if($params['f'] == 'index' && isset($params['id']) && $params['id'] != 1001) continue;
+            if($params['f'] == 'browse' && isset($params['productID']) && $params['productID'] != 1) continue;
+            if($params['f'] == 'task' && isset($params['executionID']) && $params['executionID'] != 12000) continue;
+            if($params['f'] == 'admin' && isset($params['browseType']) && $params['browseType'] == 'byProduct' && isset($params['param']) && $params['param'] != 1) continue;
+            if($params['f'] == 'manageProduct' && isset($params['product']) && $params['product'] != 1) continue;
+        }
+        $urlList[] = $url;
+    }
+}
