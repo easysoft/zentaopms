d4071fe160cec6e1e98da3c7bfc34af2145ae7d5
caoyanyi <caoyanyi@easycorp.ltd>
2025-04-29 16:59:25 +0800

    * [misc] update release info.

--- module/misc/lang/de.php
+++ module/misc/lang/de.php
@@ -114,6 +114,7 @@ $lang->misc->feature->promptExecImage   = 'theme/default/images/main/prompt_exec
 $lang->misc->feature->promptLearnMore   = 'https://www.zentao.net/book/zentaopms/1097.html';
 
 /* Release Date. */
+$lang->misc->releaseDate['21.6.1']      = '2025-04-30';
 $lang->misc->releaseDate['21.6']        = '2025-04-11';
 $lang->misc->releaseDate['21.6.beta']   = '2025-03-21';
 $lang->misc->releaseDate['21.5']        = '2025-03-06';
