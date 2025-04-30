25b56e2d165f18f52a380918795a9c0e6737133a
wangyuting <wangyuting@chandao.com>
2025-04-30 15:17:25 +0800

    * Adjust update file.

--- db/update21.6.1.sql
+++ db/update21.6.1.sql
@@ -1,3 +1,7 @@
+ALTER TABLE `zt_relationoftasks` ADD COLUMN `project` mediumint(8) unsigned NOT NULL DEFAULT 0 AFTER `id`;
+ALTER TABLE `zt_relationoftasks` MODIFY `execution` char(30) NOT NULL DEFAULT '' AFTER `project`;
+UPDATE `zt_relationoftasks` SET `project` = (SELECT `project` FROM `zt_task` WHERE `id` = `zt_relationoftasks`.`task`);
+
 CREATE TABLE IF NOT EXISTS `zt_deliverable` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
