0dd5a9d42107e83efb562555df5f433c18a2611e
wangyuting <wangyuting@chandao.com>
2025-05-06 11:03:35 +0800

    * [story#73843,done,0.2h,0h] Finish task related docs.

--- module/task/ui/edit.html.php
+++ module/task/ui/edit.html.php
@@ -172,6 +172,7 @@ detailBody
         ) : null,
         section
         (
+            setID('files'),
             set::title($lang->files),
             fileSelector($task->files ? set::defaultFiles(array_values($task->files)) : null)
         ),
