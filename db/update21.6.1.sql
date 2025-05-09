751b88ecb07610bfc8ceae9446085f905022d1e5
wangyuting <wangyuting@chandao.com>
2025-05-09 16:45:11 +0800

    * [bug#62377,done,0.5h,0h] Finish task and design related docs support version management.

--- db/update21.6.1.sql
+++ db/update21.6.1.sql
@@ -1,4 +1,6 @@
 ALTER TABLE `zt_task`   ADD COLUMN `docs` text NULL AFTER `fromIssue`;
+ALTER TABLE `zt_task`   ADD COLUMN `docVersions` text NULL AFTER `docs`;
 ALTER TABLE `zt_storyspec`  ADD COLUMN `docs` text NULL AFTER `files`;
 ALTER TABLE `zt_storyspec`  ADD COLUMN `docVersions` text NULL AFTER `docs`;
 ALTER TABLE `zt_design` ADD COLUMN `docs` text NULL AFTER `storyVersion`;
+ALTER TABLE `zt_design` ADD COLUMN `docVersions` text NULL AFTER `docs`;
