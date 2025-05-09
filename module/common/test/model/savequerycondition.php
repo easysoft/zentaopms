d05e7912f1ae5bc8777e40432883a98917c2d479
sunguangming <sunguangming@chandao.com>
2025-05-09 01:42:39 +0000

    * [unit] add unit test.

--- module/common/test/model/savequerycondition.php
+++ module/common/test/model/savequerycondition.php
@@ -17,6 +17,12 @@ global $tester;
 
 $tester->loadModel('common')->saveQueryCondition('SELECT * FROM zt_task WHERE id = 1', 'task', true);
 $tester->loadModel('common')->saveQueryCondition('SELECT * FROM zt_execution WHERE id = 1', 'execution', false);
+$tester->loadModel('common')->saveQueryCondition('', 'testcase', true);
+$tester->loadModel('common')->saveQueryCondition('SELECT * FROM zt_story WHERE id = 1 and type = "story"', 'story', true);
+$tester->loadModel('common')->saveQueryCondition('SELECT * FROM zt_epic WHERE id = 1 and type = "epic"', 'epic', true);
 
 r($tester->session->taskQueryCondition)      && p('') && e('id = 1'); // 仅保留WHERE条件
 r($tester->session->executionQueryCondition) && p('') && e('SELECT * FROM zt_execution WHERE id = 1'); //  保留完整SQL语句
+r($tester->session->testcaseQueryCondition)  && p('') && e('1=1'); // 仅保留WHERE条件
+r($tester->session->storyQueryCondition)     && p('') && e('id = 1 and type = "story"'); // 仅保留WHERE条件
+r($tester->session->epicQueryCondition)      && p('') && e('id = 1 and type = "epic"'); // 仅保留WHERE条件
