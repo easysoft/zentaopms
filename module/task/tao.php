6649b25cc265b7d032cbffa56c5a13a6ddaed00d
wangyuting <wangyuting@chandao.com>
2025-05-09 16:45:11 +0800

    * [bug#62377,done,0.5h,0h] Finish task and design related docs support version management.

--- module/task/tao.php
+++ module/task/tao.php
@@ -448,7 +448,7 @@ class taskTao extends taskModel
             $task->lastEditedDate = helper::now();
         }
 
-        $this->dao->update(TABLE_TASK)->data($task, 'deleteFiles,renameFiles,files')
+        $this->dao->update(TABLE_TASK)->data($task, 'deleteFiles,renameFiles,files,docVersions,oldDocs,docs')
             ->autoCheck()
             ->batchCheckIF($task->status != 'cancel', $requiredFields, 'notempty')
             ->checkIF(!helper::isZeroDate($task->deadline), 'deadline', 'ge', $task->estStarted)
