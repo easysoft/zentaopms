6a321c91147994a1c0baa0af63f86e30adf431ed
liumengyi <liumengyi@easycorp.ltd>
2025-05-12 14:20:00 +0800

    * [bug#61513,done,0.1h] show storyTitle.

--- module/testcase/model.php
+++ module/testcase/model.php
@@ -2052,7 +2052,7 @@ class testcaseModel extends model
     {
         $browseType = $this->session->caseBrowseType && $this->session->caseBrowseType != 'bysearch' ? $this->session->caseBrowseType : 'all';
 
-        $stmt = $this->dao->select('t1.*,t3.title')->from(TABLE_CASE)->alias('t1');
+        $stmt = $this->dao->select('t1.*,t3.title as storyTitle')->from(TABLE_CASE)->alias('t1');
         if($this->app->tab == 'project') $stmt = $stmt->leftJoin(TABLE_PROJECTCASE)->alias('t2')->on('t1.id=t2.case');
         $stmt->leftJoin(TABLE_STORY)->alias('t3')->on('t1.story = t3.id');
 
