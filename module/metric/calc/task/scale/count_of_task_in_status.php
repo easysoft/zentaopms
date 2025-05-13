32a31ff623535149eb03ea8a3dc91434a54da449
caoyanyi <caoyanyi@easycorp.ltd>
2025-05-13 10:10:28 +0800

    + [task#142891,doing,0.3h] add calc script for task status.

---
+++ module/metric/calc/task/scale/count_of_task_in_status.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * 按状态统计的任务数。
+ * Count of task in status.
+ *
+ * 范围：status
+ * 对象：task
+ * 目的：scale
+ * 度量名称：按状态统计的任务数
+ * 单位：个
+ * 描述：按状态统计的任务数
+ * 定义：按状态统计的任务数
+ *
+ * @copyright Copyright 2009-2025 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @author    Yanyi Cao <caoyanyi@chandao.com>
+ * @package
+ * @uses      func
+ * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @Link      https://www.zentao.net
+ */
+class count_of_task_in_status extends baseCalc
+{
+    public $dataset = '';
+
+    public $fieldList = array();
+
+    public $result = array();
+
+    public function calculate($row)
+    {
+        if(!isset($this->result[$row->status])) $this->result[$row->status] = array();
+        $this->result[$row->status][$row->id] = $row->id;
+    }
+
+    public function getResult($options = array())
+    {
+        foreach($this->result as $status => $tasks)
+        {
+            if(!is_array($tasks)) continue;
+
+            $this->result[$status] = count($tasks);
+        }
+
+        $records = $this->getRecords(array('status', 'value'));
+        return $this->filterByOptions($records, $options);
+    }
+}
