5fa5d3c8d5bc03fc1a45498dd457a08eb94eb896
liyang <liyang@easycorp.ltd>
2025-05-20 11:30:03 +0800

    * [task#143866, doing, 0.5h] add instance unit test.

--- module/instance/test/model/getinstancecount.php
+++ module/instance/test/model/getinstancecount.php
@@ -17,3 +17,12 @@ cid=1
 - 获取异常的instance数量 @1
 - 获取不存在状态的instance数量 @0
 */
+
+global $tester;
+$instance = $tester->loadModel('instance');
+
+r($instance->getInstanceCount())           && p() && e('200'); // 获取所有instance数量
+r($instance->getInstanceCount('running'))  && p() && e('198'); // 获取运行中的instance数量
+r($instance->getInstanceCount('stopped'))  && p() && e('1');   // 获取关闭的instance数量
+r($instance->getInstanceCount('abnormal')) && p() && e('1');   // 获取异常的instance数量
+r($instance->getInstanceCount('test'))     && p() && e('0');   // 获取不存在状态的instance数量
