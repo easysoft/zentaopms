c2005daa86a2fae683cc1e7bfc7ef16f2dc735e3
xieqiyu <xieqiyu@chandao.com>
2025-05-21 16:00:54 +0800

    * [task#143694,doing,0.2h] Add the scope for the page of OR and lite.

---
+++ db/update21.7.1.sql
@@ -0,0 +1,11 @@
+INSERT INTO `zt_doclib`
+(`type`, `vision`, `parent`, `product`, `project`, `execution`, `name`, `baseUrl`, `acl`, `groups`, `users`, `main`, `collector`, `desc`, `order`, `addedBy`, `addedDate`, `deleted`, `archived`, `orderBy`) VALUES
+('template', 'rnd',  0, 0, 0, 0, '产品', '', 'open', '', NULL, '1', NULL, NULL, 0, '', NULL, '0', '0', 'id_asc'),
+('template', 'rnd',  0, 0, 0, 0, '项目', '', 'open', '', NULL, '1', NULL, NULL, 0, '', NULL, '0', '0', 'id_asc'),
+('template', 'rnd',  0, 0, 0, 0, '执行', '', 'open', '', NULL, '1', NULL, NULL, 0, '', NULL, '0', '0', 'id_asc'),
+('template', 'rnd',  0, 0, 0, 0, '个人', '', 'open', '', NULL, '1', NULL, NULL, 0, '', NULL, '0', '0', 'id_asc'),
+('template', 'or',   0, 0, 0, 0, '市场', '', 'open', '', NULL, '1', NULL, NULL, 0, '', NULL, '0', '0', 'id_asc'),
+('template', 'or',   0, 0, 0, 0, '产品', '', 'open', '', NULL, '1', NULL, NULL, 0, '', NULL, '0', '0', 'id_asc'),
+('template', 'or',   0, 0, 0, 0, '个人', '', 'open', '', NULL, '1', NULL, NULL, 0, '', NULL, '0', '0', 'id_asc'),
+('template', 'lite', 0, 0, 0, 0, '项目', '', 'open', '', NULL, '1', NULL, NULL, 0, '', NULL, '0', '0', 'id_asc'),
+('template', 'lite', 0, 0, 0, 0, '个人', '', 'open', '', NULL, '1', NULL, NULL, 0, '', NULL, '0', '0', 'id_asc');
