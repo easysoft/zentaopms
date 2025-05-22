bc4b43d4896d9e4789b8958710bf06c6042f645d
liyang <liyang@easycorp.ltd>
2025-05-22 13:53:34 +0800

    * [uitest] add ui test coverage report page.

--- www/webcoverage.php
+++ www/webcoverage.php
@@ -38,3 +38,23 @@ switch($type)
   <meta charset="UTF-8">
   <title>单元测试行覆盖率报告</title>
 </head>
+<style>
+body {font-family: Arial, sans-serif; font-size: 16px; line-height: 1.5; margin: 0; padding: 20px; }
+table {border-collapse: collapse; max-width: 100%; width: 100%; margin: 20px 0; }
+th {border: 1px solid #ccc; padding: 8px; text-align: center; background-color: #eee; white-space: nowrap; }
+caption{font-weight: bold; margin: 10px 0; font-size: 18px; }
+h2 {margin-top: 20px; font-size: 24px; }
+.red {color: red; }
+.green {color: green; }
+tbody tr:hover {background-color: #f5f5f5; }
+tbody tr:nth-child(even) {background-color: #f9f9f9; }
+h1 {text-align: center; }
+</style>
+<body>
+<h1>单元测试行覆盖率报告</h1>
+<?php
+echo $ztfHtml;
+echo $report;
+echo "<script>var type = '$type';</script>";
+?>
+<script src="./js/jquery/lib.js"></script>
