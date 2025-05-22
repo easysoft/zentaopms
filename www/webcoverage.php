7f547ad7535908ccaf8542800c97ad83e8dc1966
liyang <liyang@easycorp.ltd>
2025-05-22 13:52:50 +0800

    * [uitest] add ui test coverage report page.

---
+++ www/webcoverage.php
@@ -0,0 +1,18 @@
+<?php
+include_once(dirname(__FILE__, 2) . "/test/lib/coverage.php");
+global $zentaoRoot;
+$zentaoRoot = dirname(__FILE__, 2);
+
+$type      = isset($_GET['module']) ? 'module' : 'summary';
+$coverage  = new coverage();
+$report    = '';
+$ztfReport = $coverage->getZtfReport('web');
+if($ztfReport)
+{
+    $ztfHtml = "<div class='report'><strong>%s</strong> 执行 <strong>%s个</strong>用例，耗时 <strong>%s秒</strong>。<strong>%s (%s%%) </strong>通过，<strong>%s (%s%%)</strong> 失败，<strong>%s (%s%%)</strong> 忽略。</div>";
+    $ztfHtml = sprintf($ztfHtml, $ztfReport->time, $ztfReport->total, $ztfReport->duration, $ztfReport->pass, $ztfReport->passPercent, $ztfReport->fail, $ztfReport->failPercent, $ztfReport->skip, $ztfReport->skipPercent);
+}
+else
+{
+    $ztfHtml = "<p>没有找到ZTF测试报告。</p>";
+}
