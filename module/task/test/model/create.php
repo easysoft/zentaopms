a1fdf25fa139b16b055f8b7ec80494008d46e654
wangyuting <wangyuting@chandao.com>
2025-05-30 02:43:46 +0000

    * Add unit test script comment.

--- module/task/test/model/create.php
+++ module/task/test/model/create.php
@@ -55,7 +55,41 @@ cid=2
  - 属性estStarted @2023-04-01
  - 属性deadline @2024-01-01
 - 测试正常的创建讨论任务
-
+ - 属性name @讨论任务一
+ - 属性execution @2
+ - 属性type @discuss
+ - 属性estimate @1
+ - 属性version @1
+ - 属性estStarted @2021-01-10
+ - 属性deadline @2021-03-19
+- 测试正常的创建界面任务
+ - 属性name @界面任务一
+ - 属性execution @2
+ - 属性type @ui
+ - 属性estimate @1
+ - 属性version @1
+ - 属性estStarted @2021-01-10
+ - 属性deadline @2021-03-19
+- 测试正常的创建事务任务
+ - 属性name @事务任务一
+ - 属性execution @2
+ - 属性type @affair
+ - 属性estimate @1
+ - 属性version @1
+ - 属性estStarted @2021-01-10
+ - 属性deadline @2021-03-19
+- 测试正常的创建其他任务
+ - 属性name @其他任务一
+ - 属性execution @2
+ - 属性type @misc
+ - 属性estimate @1
+ - 属性version @1
+ - 属性estStarted @2021-01-10
+ - 属性deadline @2021-03-19
+- 测试不输入执行创建任务第execution条的0属性 @『所属执行』不能为空。
+- 测试不输入名称创建任务第name条的0属性 @『任务名称』不能为空。
+- 测试不输入类型创建任务第type条的0属性 @『任务类型』不能为空。
+- 测试不输入类型创建任务第estimate条的0属性 @『最初预计』应当是数字，可以是小数。
 
 */
 $devel         = array('execution' => 2, 'name' => '开发任务一', 'type' => 'devel', 'estimate' => 1, 'version' => 1,  'estStarted' => null, 'deadline' => null, 'left' => 1);
