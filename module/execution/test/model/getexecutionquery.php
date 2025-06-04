5015926d4c4e3a2afbfe59582ab3d766e3dc0958
tianshujie <tianshujie@easycorp.ltd>
2025-06-04 16:35:36 +0800

    * [ci,done,0.1h] fix getexecutionquery unit test error.

--- module/execution/test/model/getexecutionquery.php
+++ module/execution/test/model/getexecutionquery.php
@@ -2,6 +2,20 @@
 <?php
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/lib/execution.unittest.class.php';
+/**
+
+title=测试 executionModel->getExecutionQuery();
+timeout=0
+cid=1
+
+- 没有queryID时，获取查询执行的SQL @(( 1 ) AND ( 1  AND t1.`status` = 'doing'))
+- 有queryID时，获取查询执行的SQL @(( 1   AND t1.`name`  LIKE '迭代' ) AND ( 1  AND t1.`status` = 'doing'  ))
+- queryID不存在时，获取查询执行的SQL @(( 1   AND t1.`name`  LIKE '迭代' ) AND ( 1  AND t1.`status` = 'doing'  ))
+- queryID不存在时，获取查询执行的SQL @(( 1   AND t1.`name`  LIKE '迭代' ) AND ( 1  AND t1.`status` = 'doing'  ))
+- queryID不存在时，获取查询执行的SQL @(( 1   AND t1.`name`  LIKE '迭代' ) AND ( 1  AND t1.`status` = 'doing'  ))
+
+*/
+
 zenData('user')->gen(5);
 su('admin');
 
@@ -14,15 +28,10 @@ $userquery->sql->range("`(( 1   AND `name`  LIKE '迭代' ) AND ( 1  AND `status
 $userquery->form->range('``');
 $userquery->gen(1);
 
-/**
-
-title=测试 executionModel->getExecutionQuery();
-timeout=0
-cid=1
-
-*/
-
-$queryIdList = array(0, 1);
+$queryIdList = array(0, 1, 2, 3, 4);
 $executionTester = new executionTest();
-r($executionTester->getExecutionQueryTest($queryIdList[0])) && p() && e("(( 1 ) AND ( 1  AND t1.`status` = 'doing'))");                                   // 没有queryID时，获取查询执行的SQL
-r($executionTester->getExecutionQueryTest($queryIdList[1])) && p() && e("(( 1   AND t1.`name`  LIKE '迭代' ) AND ( 1  AND t1.`status` = 'doing'  ))");  // 有queryID时，获取查询执行的SQL
+r($executionTester->getExecutionQueryTest($queryIdList[0])) && p() && e("(( 1 ) AND ( 1  AND t1.`status` = 'doing'))");                                // 没有queryID时，获取查询执行的SQL
+r($executionTester->getExecutionQueryTest($queryIdList[1])) && p() && e("(( 1   AND t1.`name`  LIKE '迭代' ) AND ( 1  AND t1.`status` = 'doing'  ))"); // 有queryID时，获取查询执行的SQL
+r($executionTester->getExecutionQueryTest($queryIdList[2])) && p() && e("(( 1   AND t1.`name`  LIKE '迭代' ) AND ( 1  AND t1.`status` = 'doing'  ))"); // queryID不存在时，获取查询执行的SQL
+r($executionTester->getExecutionQueryTest($queryIdList[3])) && p() && e("(( 1   AND t1.`name`  LIKE '迭代' ) AND ( 1  AND t1.`status` = 'doing'  ))"); // queryID不存在时，获取查询执行的SQL
+r($executionTester->getExecutionQueryTest($queryIdList[4])) && p() && e("(( 1   AND t1.`name`  LIKE '迭代' ) AND ( 1  AND t1.`status` = 'doing'  ))"); // queryID不存在时，获取查询执行的SQL
