92f2e7654f6b407174699d3c4a5933cf8b9a67bb
wangyuting <wangyuting@chandao.com>
2025-06-04 08:16:02 +0000

    * [task#144776,doing,0.1h,6h] Optimize dept getsons unit test script.

--- module/dept/test/model/getsons.php
+++ module/dept/test/model/getsons.php
@@ -9,19 +9,26 @@ zenData('dept')->loadYaml('dept')->gen(13);
 /**
 
 title=测试 deptModel->getSons();
+timeout=0
 cid=1
-pid=1
 
-有子部门查询   >> 一级部门8|2|,8,
-无子部门查询   >> 0
-子部门数量统计 >> 2
+- 有子部门查询
+ - 第0条的name属性 @一级部门8
+ - 第0条的parent属性 @2
+ - 第0条的path属性 @,8,
+- 无子部门查询 @0
+- 子部门数量统计 @2
+- 不存在的子部门数量统计 @0
+- 不存在的子部门数量统计 @0
 
 */
 
-$deptIDList = array('2', '5');
+$deptIDList = array('2', '5', '20');
 $count      = array('0', '1');
 
 $dept = new deptTest();
 r($dept->getSonsTest($deptIDList[0], $count[0])) && p('0:name|parent|path', '|') && e('一级部门8|2|,8,');   //有子部门查询
 r($dept->getSonsTest($deptIDList[1], $count[0])) && p()                          && e('0');                 //无子部门查询
 r($dept->getSonsTest($deptIDList[0], $count[1])) && p()                          && e('2');                 //子部门数量统计
+r($dept->getSonsTest($deptIDList[1], $count[1])) && p()                          && e('0');                 //不存在的子部门数量统计
+r($dept->getSonsTest($deptIDList[2], $count[1])) && p()                          && e('0');                 //不存在的子部门数量统计
