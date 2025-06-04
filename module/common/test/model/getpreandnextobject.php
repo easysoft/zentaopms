129f6e984dbe4042642eef863372dae5f0f9afdf
sunguangming <sunguangming@chandao.com>
2025-06-04 04:03:51 +0000

    * [unittest] modify unit test.

--- module/common/test/model/getpreandnextobject.php
+++ module/common/test/model/getpreandnextobject.php
@@ -11,10 +11,11 @@ title=测试 commonModel->getPreAndNextObject();
 timeout=0
 cid=1
 
-- 执行common模块的getPreAndNextObject方法，参数是'', 3
+- 执行 @1
+- 执行common模块的getPreAndNextObject方法，参数是'', 3 
  - 属性pre @~~
  - 属性next @~~
-- 执行common模块的getPreAndNextObject方法，参数是'story', 3
+- 执行common模块的getPreAndNextObject方法，参数是'story', 3 
  - 属性pre @~~
  - 属性next @~~
 - 执行$preAndNextObject->pre属性id @2
