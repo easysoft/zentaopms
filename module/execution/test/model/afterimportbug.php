61dd12472beada50911ab89986d6f4d9fa8915bd
tianshujie <tianshujie@easycorp.ltd>
2025-06-04 16:18:24 +0800

    * [ci,done,0.1h] fix afterimportbug unit test.

--- module/execution/test/model/afterimportbug.php
+++ module/execution/test/model/afterimportbug.php
@@ -14,13 +14,13 @@ $task->consumed->range('3');
 $task->gen(10);
 
 $bug = zenData('bug');
-$bug->id->range('1-4');
-$bug->title->range('1-4')->prefix('Bug');
+$bug->id->range('1-5');
+$bug->title->range('1-5')->prefix('Bug');
 $bug->project->range('1,2,1');
 $bug->execution->range('3,4,5');
 $bug->task->range('1-10');
 $bug->status->range('active');
-$bug->gen(4);
+$bug->gen(5);
 
 su('admin');
 
@@ -33,13 +33,17 @@ cid=1
 - 测试导入Bug1后的数据处理 @1
 - 测试导入Bug2后的数据处理 @1
 - 测试导入Bug3后的数据处理 @1
+- 测试导入Bug4后的数据处理 @1
+- 测试导入Bug5后的数据处理 @1
 
 */
 
-$taskIdList = array(1, 2, 3);
-$bugIdList  = array(1, 2, 3);
+$taskIdList = array(1, 2, 3, 4, 5);
+$bugIdList  = array(1, 2, 3, 4, 5);
 
 $executionTester = new executionTest();
 r($executionTester->afterImportBugTest($taskIdList[0], $bugIdList[0])) && p() && e('1'); // 测试导入Bug1后的数据处理
 r($executionTester->afterImportBugTest($taskIdList[1], $bugIdList[1])) && p() && e('1'); // 测试导入Bug2后的数据处理
 r($executionTester->afterImportBugTest($taskIdList[2], $bugIdList[2])) && p() && e('1'); // 测试导入Bug3后的数据处理
+r($executionTester->afterImportBugTest($taskIdList[3], $bugIdList[3])) && p() && e('1'); // 测试导入Bug4后的数据处理
+r($executionTester->afterImportBugTest($taskIdList[4], $bugIdList[4])) && p() && e('1'); // 测试导入Bug5后的数据处理
