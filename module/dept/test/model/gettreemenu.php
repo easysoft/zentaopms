4ba7c3883d9ba5c29f90c3cfbec41137ebe97adc
wangyuting <wangyuting@chandao.com>
2025-06-04 08:19:25 +0000

    * [task#144776,doing,0.1h,6h] Optimize dept gettreemenu unit test script.

--- module/dept/test/model/gettreemenu.php
+++ module/dept/test/model/gettreemenu.php
@@ -9,19 +9,23 @@ zenData('dept')->gen(30);
 /**
 
 title=测试 deptModel->getTreeMenu();
+timeout=0
 cid=1
-pid=1
 
-全部部门树结构查询 >> 产品部
-无子部门树结构查询 >> 下级部门
-有子部门树结构查询 >> 删除
+- 全部部门树结构查询第0条的name属性 @产品部1
+- 无子部门树结构查询第0条的name属性 @产品部1
+- 有子部门树结构查询第0条的name属性 @开发部2
+- 有子部门树结构查询第0条的name属性 @测试部3
+- 有子部门树结构查询第0条的name属性 @运营部4
 
 */
 
-$deptIDList = array(0, 1, 2);
+$deptIDList = array(0, 1, 2, 3, 4);
 $userFunc   = array('deptmodel', 'createMemberLink');
 
 $dept = new deptTest();
-r($dept->getTreeMenuTest($deptIDList[0], $userFunc)) && p('0:name') && e('产品部1');    //全部部门树结构查询
-r($dept->getTreeMenuTest($deptIDList[1], $userFunc)) && p('0:name') && e('产品部1');    //无子部门树结构查询
-r($dept->getTreeMenuTest($deptIDList[2], $userFunc)) && p('0:name') && e('开发部2');    //有子部门树结构查询
+r($dept->getTreeMenuTest($deptIDList[0], $userFunc)) && p('0:name') && e('产品部1'); //全部部门树结构查询
+r($dept->getTreeMenuTest($deptIDList[1], $userFunc)) && p('0:name') && e('产品部1'); //无子部门树结构查询
+r($dept->getTreeMenuTest($deptIDList[2], $userFunc)) && p('0:name') && e('开发部2'); //有子部门树结构查询
+r($dept->getTreeMenuTest($deptIDList[3], $userFunc)) && p('0:name') && e('测试部3'); //有子部门树结构查询
+r($dept->getTreeMenuTest($deptIDList[4], $userFunc)) && p('0:name') && e('运营部4'); //有子部门树结构查询
