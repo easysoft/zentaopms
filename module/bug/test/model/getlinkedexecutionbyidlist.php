ec9021a4cb605274426bd6153e871b04f7eed366
wangyuting <wangyuting@chandao.com>
2025-06-04 07:19:58 +0000

    * Optimize bug getlinkedexecutionbyidlist unit test script.

--- module/bug/test/model/getlinkedexecutionbyidlist.php
+++ module/bug/test/model/getlinkedexecutionbyidlist.php
@@ -8,20 +8,16 @@ zenData('build')->loadYaml('build')->gen(100);
 /**
 
 title=bugModel->getLinkedExecutionByIdList();
+timeout=0
 cid=1
-pid=1
 
 - 测试获取版本ID 1 3 5 的执行 @1,3
 
-
 - 测试获取版本ID 2 4 6 的执行 @2,4
-
 
 - 测试获取版本ID 41 51 61 的执行 @39,49,59,2
 
-
 - 测试获取版本ID 10 30 70 的执行 @8,28,68,2
-
 
 - 测试获取版本ID 不存在 的执行 @0
 
