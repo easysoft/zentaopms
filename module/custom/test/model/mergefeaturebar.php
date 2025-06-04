09f73973e3416aab14f5eb9d9f2f0fde1fd5fd5b
sunguangming <sunguangming@chandao.com>
2025-06-04 06:38:31 +0000

    * [unittest] code for unittest.

--- module/custom/test/model/mergefeaturebar.php
+++ module/custom/test/model/mergefeaturebar.php
@@ -1,10 +1,16 @@
 #!/usr/bin/env php
 <?php
+
 /**
 
 title=测试 customModel->mergeFeatureBar();
 timeout=0
 cid=1
+
+- 获取地盘-待处理-任务列表筛选标签 @0
+- 获取产品-需求列表筛选标签属性unclosed @未关闭
+- 获取项目-项目列表筛选标签属性all @全部
+- 获取执行-任务列表筛选标签属性needconfirm @变更
 
 */
 
@@ -20,9 +26,9 @@ $modules = array('my', 'product', 'project', 'execution');
 $methods = array('work', 'browse', 'browse', 'task');
 
 $customTester  = new customTest();
-r($customTester->mergeFeatureBarTest($modules[0], $methods[0])) && p()      && e('0');    // 获取地盘-待处理-任务列表筛选标签
-r($customTester->mergeFeatureBarTest($modules[1], $methods[1])) && p()      && e('0');    // 获取产品-需求列表筛选标签
-r($customTester->mergeFeatureBarTest($modules[2], $methods[2])) && p('all') && e('全部'); // 获取项目-项目列表筛选标签
-r($customTester->mergeFeatureBarTest($modules[3], $methods[3])) && p()      && e('0');    // 获取执行-任务列表筛选标签
+r($customTester->mergeFeatureBarTest($modules[0], $methods[0])) && p()              && e('0');      // 获取地盘-待处理-任务列表筛选标签
+r($customTester->mergeFeatureBarTest($modules[1], $methods[1])) && p('unclosed')    && e('未关闭'); // 获取产品-需求列表筛选标签
+r($customTester->mergeFeatureBarTest($modules[2], $methods[2])) && p('all')         && e('全部');   // 获取项目-项目列表筛选标签
+r($customTester->mergeFeatureBarTest($modules[3], $methods[3])) && p('needconfirm') && e('变更');   // 获取执行-任务列表筛选标签
 
 zenData('lang')->gen(0);
