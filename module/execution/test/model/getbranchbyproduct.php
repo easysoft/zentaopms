452fa3425e8a66c368e2f1294991e2e1b97a3ea6
tianshujie <tianshujie@easycorp.ltd>
2025-06-04 16:28:26 +0800

    * [ci,done,0.1h] add comment for getbranchbyproduct unit test.

--- module/execution/test/model/getbranchbyproduct.php
+++ module/execution/test/model/getbranchbyproduct.php
@@ -2,6 +2,20 @@
 <?php
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/lib/execution.unittest.class.php';
+/**
+
+title=executionModel->getBranchByProduct();
+timeout=0
+cid=1
+
+- 正常产品分支统计 @0
+- 正常产品分支统计 @0
+- 分支产品分支查看第2条的1属性 @分支1
+- 分支产品分支统计 @3
+- 分支产品分支查看第2条的2属性 @分支2
+
+*/
+
 zenData('user')->gen(5);
 su('admin');
 
@@ -20,18 +34,12 @@ $branch->name->range('分支1,分支2');
 $branch->status->range('active');
 $branch->gen(2);
 
-/**
-
-title=executionModel->getBranchByProduct();
-timeout=0
-cid=1
-
-*/
-
 $productIDList = array(1, 2);
 $count         = array(0, 1);
 
 $executionTester = new executionTest();
+r($executionTester->getBranchByProductTest($productIDList[0], $count[0])) && p()      && e('0');     // 正常产品分支统计
 r($executionTester->getBranchByProductTest($productIDList[0], $count[1])) && p()      && e('0');     // 正常产品分支统计
 r($executionTester->getBranchByProductTest($productIDList[1], $count[0])) && p('2:1') && e('分支1'); // 分支产品分支查看
 r($executionTester->getBranchByProductTest($productIDList[1], $count[1])) && p()      && e('3');     // 分支产品分支统计
+r($executionTester->getBranchByProductTest($productIDList[1], $count[0])) && p('2:2') && e('分支2'); // 分支产品分支查看
