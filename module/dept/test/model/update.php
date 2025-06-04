01148148c561bb0e33f394c57a442987cbcd862e
wangyuting <wangyuting@chandao.com>
2025-06-04 08:26:18 +0000

    * [task#144776,doing,0.1h,6h] Optimize dept update unit test script.

--- module/dept/test/model/update.php
+++ module/dept/test/model/update.php
@@ -9,14 +9,29 @@ zenData('dept')->gen(30);
 /**
 
 title=测试 deptModel->update();
+timeout=0
 cid=1
-pid=1
 
-修改父级       >> 修改后部门|0|,16,|dev1
-修改子级       >> 子级部门修改|1|,1,18,|dev2
-不输入上级部门 >> 17,一级部门7
-部门名称为空   >> 19,二级部门9
-无负责人       >> 无负责人部门|1|,1,20,|N/A
+- 修改父级
+ - 第16条的name属性 @修改后部门
+ - 第16条的parent属性 @0
+ - 第16条的path属性 @,16,
+ - 第16条的manager属性 @dev1
+- 修改子级
+ - 第18条的name属性 @子级部门修改
+ - 第18条的parent属性 @1
+ - 第18条的path属性 @,1,18,
+ - 第18条的manager属性 @dev2
+- 不输入上级部门
+ - 第17条的id属性 @17
+ - 第17条的name属性 @无父级部门
+- 部门名称为空
+ - 第19条的id属性 @19
+ - 第19条的name属性 @二级部门9
+- 无负责人
+ - 第20条的name属性 @无负责人部门
+ - 第20条的parent属性 @1
+ - 第20条的path属性 @,1,20,
 
 */
 
@@ -29,6 +44,6 @@ $noManager  = array('id' => 20, 'parent' => '1', 'name' => '无负责人部门')
 $dept = new deptTest();
 r($dept->updateTest((object)$parentDept)) && p('16:name|parent|path|manager', '|')  && e('修改后部门|0|,16,|dev1');     //修改父级
 r($dept->updateTest((object)$childDept))  && p('18:name|parent|path|manager', '|')  && e('子级部门修改|1|,1,18,|dev2'); //修改子级
-r($dept->updateTest((object)$noParent))   && p('17:id,name')                        && e('17,无父级部门');               //不输入上级部门
+r($dept->updateTest((object)$noParent))   && p('17:id,name')                        && e('17,无父级部门');              //不输入上级部门
 r($dept->updateTest((object)$noName))     && p('19:id,name')                        && e('19,二级部门9');               //部门名称为空
-r($dept->updateTest((object)$noManager))  && p('20:name|parent|path', '|')          && e('无负责人部门|1|,1,20,');  //无负责人
+r($dept->updateTest((object)$noManager))  && p('20:name|parent|path', '|')          && e('无负责人部门|1|,1,20,');      //无负责人
