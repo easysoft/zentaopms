9de6eae4d11ed403fa442eb5aecc4c1e7e9ca629
liumengyi <liumengyi@easycorp.ltd>
2025-06-05 11:19:54 +0800

    * [unittest] modify unittest of my::buildTicketSearchForm.

--- module/my/test/model/buildticketsearchform.php
+++ module/my/test/model/buildticketsearchform.php
@@ -16,14 +16,22 @@ cid=1
 
 - 测试获取queryID 1 actionURL actionURL1 的搜索表单 @0
 - 测试获取queryID 0 actionURL actionURL2 的搜索表单 @0
+- 测试获取queryID 0 actionURL actionURL2 的搜索表单 @0
+- 测试获取queryID 0 actionURL actionURL3 的搜索表单 @0
+- 测试获取queryID 1 actionURL actionURL1 的搜索表单 @0
+- 测试获取queryID 1 actionURL actionURL3
 
 */
 
 $my = new myTest();
 
 $queryID   = array(0, 1);
-$actionURL = array('actionURL1', 'actionURL2');
+$actionURL = array('actionURL1', 'actionURL2', 'actionURL3');
 $config1 = $my->buildTicketSearchFormTest($queryID[0], $actionURL[0]);
 $config2 = $my->buildTicketSearchFormTest($queryID[1], $actionURL[1]);
 r($config1) && p() && e('0'); // 测试获取queryID 1 actionURL actionURL1 的搜索表单
 r($config2) && p() && e('0'); // 测试获取queryID 0 actionURL actionURL2 的搜索表单
+r($my->buildTicketSearchFormTest($queryID[0], $actionURL[1])) && p() && e('0'); // 测试获取queryID 0 actionURL actionURL2 的搜索表单
+r($my->buildTicketSearchFormTest($queryID[0], $actionURL[2])) && p() && e('0'); // 测试获取queryID 0 actionURL actionURL3 的搜索表单
+r($my->buildTicketSearchFormTest($queryID[1], $actionURL[0])) && p() && e('0'); // 测试获取queryID 1 actionURL actionURL1 的搜索表单
+r($my->buildTicketSearchFormTest($queryID[1], $actionURL[2])) && p() && e('0'); // 测试获取queryID 1 actionURL actionURL3 的搜索表单
