19cb2fcde36c40e7ccd859b8ea3ea33ac2d139b3
liumengyi <liumengyi@easycorp.ltd>
2025-06-05 08:58:46 +0800

    * [unittest] modify unit test of devModel::getTree.

--- module/dev/test/model/gettree.php
+++ module/dev/test/model/gettree.php
@@ -11,10 +11,17 @@ title=测试 devModel::getTree();
 cid=1
 pid=1
 
-测试传入空值的情况 >> 0
-测试传入错误类型的情况 >> 0
-测试获取type=module模块树，并检查高亮情况 >> my,1
-测试获取type=table模块树，并检查高亮情况 >> my,1
+- 测试传入空值的情况 @0
+- 测试传入错误类型的情况 @0
+- 测试获取type=module，active空
+ - 第0条的key属性 @my
+ - 第0条的active属性 @0
+- 测试获取type=module模块树，并检查高亮情况
+ - 第0条的key属性 @my
+ - 第0条的active属性 @1
+- 测试获取type=table模块树，并检查高亮情况
+ - 第0条的key属性 @my
+ - 第0条的active属性 @1
 
 */
 
@@ -27,5 +34,6 @@ $activeList = array('', 'index', 'zt_todo');
 $typeList   = array('', 'tree', 'module', 'table');
 r($devModel->getTree($activeList[0], $typeList[0])) && p() && e('0');                  // 测试传入空值的情况
 r($devModel->getTree($activeList[0], $typeList[1])) && p() && e('0');                  // 测试传入错误类型的情况
+r($devModel->getTree($activeList[0], $typeList[2])) && p('0:key,active') && e('my,0'); // 测试获取type=module，active空
 r($devModel->getTree($activeList[1], $typeList[2])) && p('0:key,active') && e('my,1'); // 测试获取type=module模块树，并检查高亮情况
 r($devModel->getTree($activeList[2], $typeList[3])) && p('0:key,active') && e('my,1'); // 测试获取type=table模块树，并检查高亮情况
