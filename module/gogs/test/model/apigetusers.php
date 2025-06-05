cce956a3628e11a8a1d551252eb7dc625a27bdd7
caoyanyi <caoyanyi@easycorp.ltd>
2025-06-05 16:47:20 +0800

    * [unittest] adjust gogs unit test case.

--- module/gogs/test/model/apigetusers.php
+++ module/gogs/test/model/apigetusers.php
@@ -11,13 +11,20 @@ cid=0
 - 正确的服务器ID
  - 第0条的realname属性 @gogs-admin
  - 第1条的realname属性 @unittest
-- 正确的服务器ID，只查询绑定过的用户第0条的realname属性 @unittest
+- 正确的服务器ID，查询绑定过的用户
+ - 第0条的realname属性 @unittest
+ - 第1条的realname属性 @unittest1
 
 */
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 
 zenData('pipeline')->gen(5);
+zenData('oauth')->loadYaml('oauth')->gen(5);
 su('admin');
+
+global $app;
+$app->rawModule = 'gogs';
+$app->rawMethod = 'browse';
 
 global $tester;
 $gogsModel = $tester->loadModel('gogs');
@@ -27,4 +34,5 @@ r($gogsModel->apiGetUsers($gogsID)) && p() && e('0'); // 错误的服务器ID
 
 $gogsID = 5;
 r($gogsModel->apiGetUsers($gogsID, false)) && p('0:realname;1:realname') && e('gogs-admin,unittest'); // 正确的服务器ID
-r($gogsModel->apiGetUsers($gogsID, true))  && p('0:realname')            && e('unittest');            // 正确的服务器ID，只查询绑定过的用户
+
+r($gogsModel->apiGetUsers($gogsID, true))  && p('0:realname;1:realname') && e('unittest,unittest1'); // 正确的服务器ID，查询绑定过的用户
