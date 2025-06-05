7530d16ac4e553407a9ccfb22eedc096d19ace3d
xieqiyu <xieqiyu@chandao.com>
2025-06-05 17:53:25 +0800

    * [unittest] Modify the unit test of tutorialModel::getExecutionStories.

--- module/tutorial/test/model/getexecutionstories.php
+++ module/tutorial/test/model/getexecutionstories.php
@@ -6,16 +6,11 @@ declare(strict_types=1);
 title=测试 tutorialModel->getExecutionStories();
 cid=1
 
-- 检查是否获取到 admin 数据
- - 第1条的id属性 @1
- - 第1条的title属性 @Test story
- - 第1条的stage属性 @wait
- - 第1条的openedBy属性 @admin
-- 检查是否获取到 user1 数据
- - 第1条的id属性 @1
- - 第1条的title属性 @Test story
- - 第1条的stage属性 @wait
- - 第1条的openedBy属性 @user1
+- 检查获取需求ID第3条的id属性 @3
+- 检查获取需求名称第3条的title属性 @Test active story
+- 检查获取需求阶段第3条的stage属性 @wait
+- 检查获取需求创建者第3条的openedBy属性 @admin
+- 切换用户后检查创建者第3条的openedBy属性 @user1
 
 */
 
@@ -27,7 +22,10 @@ zenData('user')->gen(5);
 $tutorial = new tutorialTest();
 
 su('admin');
-r($tutorial->getExecutionStoriesTest()) && p('1:id,title,stage,openedBy') && e('1,Test story,wait,admin'); // 检查是否获取到 admin 数据
+r($tutorial->getExecutionStoriesTest()) && p('3:id')       && e('3');                 // 检查获取需求ID
+r($tutorial->getExecutionStoriesTest()) && p('3:title')    && e('Test active story'); // 检查获取需求名称
+r($tutorial->getExecutionStoriesTest()) && p('3:stage')    && e('wait');              // 检查获取需求阶段
+r($tutorial->getExecutionStoriesTest()) && p('3:openedBy') && e('admin');             // 检查获取需求创建者
 
 su('user1');
-r($tutorial->getExecutionStoriesTest()) && p('1:id,title,stage,openedBy') && e('1,Test story,wait,user1'); // 检查是否获取到 user1 数据
+r($tutorial->getExecutionStoriesTest()) && p('3:openedBy') && e('user1'); // 切换用户后检查创建者
