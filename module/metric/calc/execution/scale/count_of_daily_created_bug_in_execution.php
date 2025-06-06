974a8fb9dfe77bb9cfcc33cca9ad2bba378524e1
wangzemei <wangzemei@easycorp.ltd>
2025-06-06 09:10:34 +0800

    + [story#75286] add the calculate for count_of_daily_created_bug_in_execution metric.

--- module/metric/calc/execution/scale/count_of_daily_created_bug_in_execution.php
+++ module/metric/calc/execution/scale/count_of_daily_created_bug_in_execution.php
@@ -7,6 +7,22 @@ class count_of_daily_created_bug_in_execution extends baseCalc
 
     public $result = array();
 
+    public function calculate($row)
+    {
+        $year = $this->getYear($row->openedDate);
+        if(!$year) return false;
+
+        $date = substr($row->openedDate, 0, 10);
+        list($year, $month, $day) = explode('-', $date);
+
+        if(!isset($this->result[$row->execution]))                      $this->result[$row->execution] = array();
+        if(!isset($this->result[$row->execution][$year]))               $this->result[$row->execution][$year] = array();
+        if(!isset($this->result[$row->execution][$year][$month]))       $this->result[$row->execution][$year][$month] = array();
+        if(!isset($this->result[$row->execution][$year][$month][$day])) $this->result[$row->execution][$year][$month][$day] = 0;
+
+        $this->result[$row->execution][$year][$month][$day] ++;
+    }
+
     public function getResult($options = array())
     {
         $records = $this->getRecords(array('execution', 'year', 'month', 'day', 'value'));
