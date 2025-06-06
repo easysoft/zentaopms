4ae4a5d567cfd06ff9a4e9d831a47c62db77f2ef
tianshujie <tianshujie@easycorp.ltd>
2025-06-06 14:42:11 +0800

    * [unittest] fix getcasetreemenu error.

--- module/tree/test/model/getcasetreemenu.php
+++ module/tree/test/model/getcasetreemenu.php
@@ -7,8 +7,10 @@ title=测试 treeModel->getCaseTreeMenu();
 timeout=0
 cid=1
 
-- 测试获取产品1的Case模块 @正常产品1|模块5
+- 测试获取产品1的Case模块 @正常产品1
 - 测试产品10的Case模块 @正常产品10
+- 测试获取产品1的Case模块 @正常产品2
+- 测试获取产品1的Case模块 @正常产品3
 - 测试不存在产品的Case模块 @0
 
 */
@@ -18,25 +20,33 @@ include dirname(__FILE__, 2) . '/lib/tree.unittest.class.php';
 
 su('admin');
 
-zenData('module')->loadYaml('module')->gen(20);
-zenData('product')->gen(20);
+$moduleTable = zenData('module')->loadYaml('module');
+$moduleTable->root->range('1');
+$moduleTable->type->range('case');
+$moduleTable->gen(3);
+zenData('product')->loadYaml('product')->gen(20);
+zenData('branch')->gen(0);
 
 $projectproduct = zenData('projectproduct');
 $projectproduct->project->range('1-100');
 $projectproduct->product->range('1-100');
 $projectproduct->gen(20);
 
-$projectcase = zenData('projectcase');
+$projectcase = zenData('projectcase')->loadYaml('projectcase');
 $projectcase->project->range('1-100');
 $projectcase->case->range('1-100');
 $projectcase->gen(20);
 
-$case = zenData('case');
+$case = zenData('case')->loadYaml('case');
 $case->module->range('5');
+$case->product->range('1');
+$case->project->range('1');
 $case->gen(20);
 
 $tree = new treeTest();
 
 r($tree->getCaseTreeMenuTest(1, 1))   && p() && e('正常产品1');  // 测试获取产品1的Case模块
+r($tree->getCaseTreeMenuTest(2, 2))   && p() && e('正常产品2');  // 测试获取产品1的Case模块
+r($tree->getCaseTreeMenuTest(3, 2))   && p() && e('正常产品3');  // 测试获取产品1的Case模块
 r($tree->getCaseTreeMenuTest(10, 10)) && p() && e('正常产品10'); // 测试产品10的Case模块
 r($tree->getCaseTreeMenuTest(30, 30)) && p() && e('0');          // 测试不存在产品的Case模块
