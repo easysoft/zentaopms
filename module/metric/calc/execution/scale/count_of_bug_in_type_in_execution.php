74773cc488c23f8a3c83e706efac5ebb894552fc
wangzemei <wangzemei@easycorp.ltd>
2025-06-06 09:25:37 +0800

    + [story#75279] init the count_of_bug_in_type_in_execution metric.

---
+++ module/metric/calc/execution/scale/count_of_bug_in_type_in_execution.php
@@ -0,0 +1,15 @@
+<?php
+class count_of_bug_in_type_in_execution extends baseCalc
+{
+    public $dataset = 'getExecutionBugs';
+
+    public $fieldList = array();
+
+    public $result = array();
+
+    public function calculate($row)
+    {
+        if(!isset($this->result[$row->type])) $this->result[$row->type] = array();
+        $this->result[$row->type][$row->id] = $row->id;
+    }
+}
