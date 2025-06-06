b7bbec1e590eaff2c4263c28117aabebf6bc6f7d
wangzemei <wangzemei@easycorp.ltd>
2025-06-06 16:55:26 +0800

    + [task#144626] add the getResult for count_of_created_bug_in_user metrics.

--- module/metric/calc/user/scale/count_of_created_bug_in_user.php
+++ module/metric/calc/user/scale/count_of_created_bug_in_user.php
@@ -12,4 +12,17 @@ class count_of_created_bug_in_user extends baseCalc
         if(!isset($this->result[$row->openedBy])) $this->result[$row->openedBy] = array();
         $this->result[$row->openedBy][$row->id] = $row->id;
     }
+
+    public function getResult($options = array())
+    {
+        foreach($this->result as $openedBy => $bugs)
+        {
+            if(!is_array($bugs)) continue;
+
+            $this->result[$openedBy] = count($bugs);
+        }
+
+        $records = $this->getRecords(array('user', 'value'));
+        return $this->filterByOptions($records, $options);
+    }
 }
