f2afb4eb896af2895d6282008a5bd155ed98661d
tianshujie <tianshujie@easycorp.ltd>
2025-06-06 15:16:41 +0800

    * [unittest] fix updatestats error.

--- module/program/test/tao/updatestats.php
+++ module/program/test/tao/updatestats.php
@@ -6,6 +6,17 @@ title=测试 programTao::updateStats();
 timeout=0
 cid=1
 
+- 获取系统中所有项目的任务统计信息
+ - 第11条的estimate属性 @51
+ - 第11条的left属性 @44
+ - 第11条的consumed属性 @13
+ - 第11条的teamCount属性 @4
+- 获取系统中项目ID=60的任务统计信息
+ - 第60条的estimate属性 @21
+ - 第60条的left属性 @18
+ - 第60条的consumed属性 @7
+ - 第60条的teamCount属性 @0
+
 */
 
 include dirname(__FILE__, 5) . '/test/lib/init.php';
@@ -21,5 +32,5 @@ $projectIdList[] = array();
 $projectIdList[] = array(60);
 
 $programTester = new programTest();
-r($programTester->updateStatsTest($projectIdList[0])) && p('11:estimate,left,consumed,teamCount') && e('46,39,13,4'); // 获取系统中所有项目的任务统计信息
-r($programTester->updateStatsTest($projectIdList[1])) && p('60:estimate,left,consumed,teamCount') && e('18,15,7,0');  // 获取系统中项目ID=60的任务统计信息
+r($programTester->updateStatsTest($projectIdList[0])) && p('11:estimate,left,consumed,teamCount') && e('51,44,13,4'); // 获取系统中所有项目的任务统计信息
+r($programTester->updateStatsTest($projectIdList[1])) && p('60:estimate,left,consumed,teamCount') && e('21,18,7,0');  // 获取系统中项目ID=60的任务统计信息
