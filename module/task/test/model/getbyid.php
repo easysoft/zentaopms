77ab3778c40639c6cfb36b1e1fe1277d95365334
sunguangming <sunguangming@chandao.com>
2025-06-06 01:17:06 +0000

    * [unittest] modify unittest.

--- module/task/test/model/getbyid.php
+++ module/task/test/model/getbyid.php
@@ -14,7 +14,58 @@ title=taskModel->getByID();
 timeout=0
 cid=1
 
-
+- 测试获取taskID=1的任务信息
+ - 属性id @1
+ - 属性name @开发任务11
+ - 属性status @wait
+ - 属性desc @这里是任务描述1
+- 测试获取taskID=2的任务信息
+ - 属性id @2
+ - 属性name @开发任务12
+ - 属性status @doing
+ - 属性desc @这里是任务描述2
+- 测试获取taskID=3的任务信息
+ - 属性id @3
+ - 属性name @开发任务13
+ - 属性status @done
+ - 属性desc @这里是任务描述3
+- 测试获取taskID=4的任务信息
+ - 属性id @4
+ - 属性name @开发任务14
+ - 属性status @cancel
+ - 属性desc @这里是任务描述4
+- 测试获取taskID=5的任务信息
+ - 属性id @5
+ - 属性name @开发任务15
+ - 属性status @closed
+ - 属性desc @这里是任务描述5
+- 测试获取taskID=7的任务信息
+ - 属性id @7
+ - 属性parent @6
+ - 属性parentName @开发任务16
+- 测试获取taskID=8的任务信息
+ - 第team[1]条的account属性 @admin
+ - 第team[1]条的estimate属性 @1.00
+ - 第team[1]条的status属性 @wait
+- 测试获取taskID=9的任务信息
+ - 第team[4]条的account属性 @admin
+ - 第team[4]条的estimate属性 @4.00
+ - 第team[4]条的status属性 @wait
+- 测试获取taskID=1的任务信息
+ - 属性id @1
+ - 属性name @开发任务11
+ - 属性status @wait
+ - 属性desc @这里是任务描述1
+- 测试获取taskID=2的任务信息
+ - 属性id @2
+ - 属性name @开发任务12
+ - 属性status @doing
+ - 属性desc @这里是任务描述2
+- 测试获取taskID=3的任务信息
+ - 属性id @3
+ - 属性name @开发任务13
+ - 属性status @done
+ - 属性desc @这里是任务描述3
 */
 
 $taskIdList = range(1, 9);
@@ -25,7 +76,6 @@ r($taskModel->getByID($taskIdList[1])) && p('id,name,status,desc')             &
 r($taskModel->getByID($taskIdList[2])) && p('id,name,status,desc')             && e('3,开发任务13,done,这里是任务描述3');   // 测试获取taskID=3的任务信息
 r($taskModel->getByID($taskIdList[3])) && p('id,name,status,desc')             && e('4,开发任务14,cancel,这里是任务描述4'); // 测试获取taskID=4的任务信息
 r($taskModel->getByID($taskIdList[4])) && p('id,name,status,desc')             && e('5,开发任务15,closed,这里是任务描述5'); // 测试获取taskID=5的任务信息
-r($taskModel->getByID($taskIdList[5])) && p('children[7]:id,parent')           && e('7,6');                                 // 测试获取taskID=6的任务信息
 r($taskModel->getByID($taskIdList[6])) && p('id,parent,parentName')            && e('7,6,开发任务16');                      // 测试获取taskID=7的任务信息
 r($taskModel->getByID($taskIdList[7])) && p('team[1]:account,estimate,status') && e('admin,1.00,wait');                     // 测试获取taskID=8的任务信息
 r($taskModel->getByID($taskIdList[8])) && p('team[4]:account,estimate,status') && e('admin,4.00,wait');                     // 测试获取taskID=9的任务信息
@@ -35,7 +85,7 @@ r($taskModel->getByID($taskIdList[1], true)) && p('id,name,status,desc')        
 r($taskModel->getByID($taskIdList[2], true)) && p('id,name,status,desc')             && e('3,开发任务13,done,这里是任务描述3');   // 测试获取taskID=3的任务信息
 r($taskModel->getByID($taskIdList[3], true)) && p('id,name,status,desc')             && e('4,开发任务14,cancel,这里是任务描述4'); // 测试获取taskID=4的任务信息
 r($taskModel->getByID($taskIdList[4], true)) && p('id,name,status,desc')             && e('5,开发任务15,closed,这里是任务描述5'); // 测试获取taskID=5的任务信息
-r($taskModel->getByID($taskIdList[5], true)) && p('children[7]:id,parent')           && e('7,6');                                 // 测试获取taskID=6的任务信息
-r($taskModel->getByID($taskIdList[6], true)) && p('id,parent,parentName')            && e('7,6,开发任务16');                      // 测试获取taskID=7的任务信息
-r($taskModel->getByID($taskIdList[7], true)) && p('team[1]:account,estimate,status') && e('admin,1.00,wait');                     // 测试获取taskID=8的任务信息
-r($taskModel->getByID($taskIdList[8], true)) && p('team[4]:account,estimate,status') && e('admin,4.00,wait');                     // 测试获取taskID=9的任务信息
+r($taskModel->getByID($taskIdList[5], true)) && p('id,name,status,desc')             && e('6,开发任务16,wait,这里是任务描述6');   // 测试获取taskID=6的任务信息
+r($taskModel->getByID($taskIdList[6], true)) && p('id,parent,parentName')            && e('7,6,开发任务16');                    // 测试获取taskID=7的任务信息
+r($taskModel->getByID($taskIdList[7], true)) && p('team[1]:account,estimate,status') && e('admin,1.00,wait');                   // 测试获取taskID=8的任务信息
+r($taskModel->getByID($taskIdList[8], true)) && p('team[4]:account,estimate,status') && e('admin,4.00,wait');                  // 测试获取taskID=9的任务信息
