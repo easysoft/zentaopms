e4f78b728c97cda57a90d134763182cbdc1882cf
caoyanyi <caoyanyi@easycorp.ltd>
2025-06-06 10:30:29 +0800

    * [unittest] adjust jenkins getJobPairs test case.

--- module/jenkins/test/model/getjobpairs.php
+++ module/jenkins/test/model/getjobpairs.php
@@ -29,8 +29,9 @@ $jenkins = new jenkinsTest();
 
 $jenkinsID = array(1, 2, 3, 0, 111);
 
-r($jenkins->getJobPairsTest($jenkinsID[0])) && p() && e('1:这是一个Job1,6:这是一个Job6');  // 测试获取 jenkins 1 的构建任务
-r($jenkins->getJobPairsTest($jenkinsID[1])) && p() && e('2:这是一个Job2,7:这是一个Job7');  // 测试获取 jenkins 2 的构建任务
-r($jenkins->getJobPairsTest($jenkinsID[2])) && p() && e('3:这是一个Job3');                 // 测试获取 jenkins 3 的构建任务
-r($jenkins->getJobPairsTest($jenkinsID[3])) && p() && e('0');                              // 测试获取 jenkins 0 的构建任务
-r($jenkins->getJobPairsTest($jenkinsID[4])) && p() && e('0');                              // 测试获取 jenkins 111 的构建任务
+r($jenkins->getJobPairsTest($jenkinsID[0])) && p('', '|') && e('1:这是一个Job1,6:这是一个Job6');  // 测试获取 jenkins 1 的构建任务
+r($jenkins->getJobPairsTest($jenkinsID[1])) && p('', '|') && e('2:这是一个Job2,7:这是一个Job7');  // 测试获取 jenkins 2 的构建任务
+
+r($jenkins->getJobPairsTest($jenkinsID[2])) && p() && e('3:这是一个Job3'); // 测试获取 jenkins 3 的构建任务
+r($jenkins->getJobPairsTest($jenkinsID[3])) && p() && e('0');              // 测试获取 jenkins 0 的构建任务
+r($jenkins->getJobPairsTest($jenkinsID[4])) && p() && e('0');              // 测试获取 jenkins 111 的构建任务
