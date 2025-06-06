c4ac1cab0e98d1462f4eed9b8572fe21d8df7f47
liumengyi <liumengyi@easycorp.ltd>
2025-06-06 11:03:27 +0800

    * [unittest] add unittest results for testtaskModel::createResult.

--- module/testtask/test/model/createresult.php
+++ module/testtask/test/model/createresult.php
@@ -11,8 +11,27 @@ zenData('testresult')->gen(0);
 /**
 
 title=测试 testtaskModel->createResult();
+timeout=0
 cid=1
-pid=1
+
+- 在测试单外执行测试用例 1，测试结果为忽略。
+ - 属性caseResult @pass
+ - 第case条的lastRunner属性 @admin
+ - 第case条的lastRunResult属性 @pass
+ - 第result条的lastRunner属性 @admin
+ - 第result条的run属性 @0
+ - 第result条的case属性 @1
+ - 第result条的version属性 @1
+ - 第result条的caseResult属性 @pass
+- 在测试单外执行测试用例 1，测试结果为通过。
+ - 属性caseResult @pass
+ - 第case条的lastRunner属性 @admin
+ - 第case条的lastRunResult属性 @pass
+ - 第result条的lastRunner属性 @admin
+ - 第result条的run属性 @0
+ - 第result条的case属性 @1
+ - 第result条的version属性 @1
+ - 第result条的caseResult属性 @pass
 
 */
 
