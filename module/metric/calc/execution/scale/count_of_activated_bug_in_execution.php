bbea45768165dab75e1b3eb26158f0b626bdae54
wangzemei <wangzemei@easycorp.ltd>
2025-06-06 09:17:59 +0800

    + [story#75286] add the calculate for count_of_activated_bug_in_execution metric.

--- module/metric/calc/execution/scale/count_of_activated_bug_in_execution.php
+++ module/metric/calc/execution/scale/count_of_activated_bug_in_execution.php
@@ -7,6 +7,16 @@ class count_of_activated_bug_in_execution extends baseCalc
 
     public $result = array();
 
+    public function calculate($row)
+    {
+        $execution = $row->execution;
+        $status    = $row->status;
+
+        if(!isset($this->result[$execution])) $this->result[$execution] = 0;
+
+        if($status == 'active') $this->result[$execution] += 1;
+    }
+
     public function getResult($options = array())
     {
         $records = $this->getRecords(array('execution', 'value'));
