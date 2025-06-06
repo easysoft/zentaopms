bc417466c4e56729799885ceff714235ba36dd07
wangzemei <wangzemei@easycorp.ltd>
2025-06-06 09:25:55 +0800

    + [story#75279] add the getResult for the count_of_bug_in_type_in_execution metric.

--- module/metric/calc/execution/scale/count_of_bug_in_type_in_execution.php
+++ module/metric/calc/execution/scale/count_of_bug_in_type_in_execution.php
@@ -12,4 +12,17 @@ class count_of_bug_in_type_in_execution extends baseCalc
         if(!isset($this->result[$row->type])) $this->result[$row->type] = array();
         $this->result[$row->type][$row->id] = $row->id;
     }
+
+    public function getResult($options = array())
+    {
+        foreach($this->result as $type => $bugs)
+        {
+            if(!is_array($bugs)) continue;
+
+            $this->result[$type] = count($bugs);
+        }
+
+        $records = $this->getRecords(array('type', 'value'));
+        return $this->filterByOptions($records, $options);
+    }
 }
