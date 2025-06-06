1edbcdf01ac1b906f65bd27d612757038b00ce97
wangzemei <wangzemei@easycorp.ltd>
2025-06-06 09:22:44 +0800

    + [story#75279] add the getResult for the count_of_bug_in_resolution_in_execution metric.

--- module/metric/calc/execution/scale/count_of_bug_in_resolution_in_execution.php
+++ module/metric/calc/execution/scale/count_of_bug_in_resolution_in_execution.php
@@ -12,4 +12,17 @@ class count_of_bug_in_resolution_in_execution extends baseCalc
         if(!isset($this->result[$row->resolution])) $this->result[$row->resolution] = array();
         $this->result[$row->resolution][$row->id] = $row->id;
     }
+
+    public function getResult($options = array())
+    {
+        foreach($this->result as $resolution => $bugs)
+        {
+            if(!is_array($bugs)) continue;
+
+            $this->result[$resolution] = count($bugs);
+        }
+
+        $records = $this->getRecords(array('resolution', 'value'));
+        return $this->filterByOptions($records, $options);
+    }
 }
