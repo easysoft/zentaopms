c47582d3f13086884f2cbcb3fe4834b393584262
wangyuting <wangyuting@chandao.com>
2025-06-06 07:27:40 +0000

    * [task#144843,doing,0.1h,2h] Optimize testtask getbylist unit test script.

--- module/testtask/test/model/getbylist.php
+++ module/testtask/test/model/getbylist.php
@@ -8,8 +8,51 @@ zenData('testtask')->gen(5);
 /**
 
 title=测试 testtaskModel->getByList();
+timeout=0
 cid=1
-pid=1
+
+- idList 参数为空，返回 false。 @0
+- idList 参数包含 10 个 ID，数据库里只有 5 条数据，只返回 5 条数据。 @5
+- 获取 ID 为 1 的测试单的详细信息。
+ - 第1条的project属性 @11
+ - 第1条的name属性 @测试单1
+ - 第1条的product属性 @1
+ - 第1条的execution属性 @101
+ - 第1条的build属性 @11
+ - 第1条的owner属性 @user3
+ - 第1条的desc属性 @这是测试单描述1
+ - 第1条的status属性 @wait
+ - 第1条的auto属性 @no
+- 获取 ID 为 2 的测试单的详细信息。
+ - 第2条的project属性 @12
+ - 第2条的name属性 @测试单2
+ - 第2条的product属性 @2
+ - 第2条的execution属性 @102
+ - 第2条的build属性 @12
+ - 第2条的owner属性 @user4
+ - 第2条的desc属性 @这是测试单描述2
+ - 第2条的status属性 @doing
+ - 第2条的auto属性 @no
+- 获取 ID 为 3 的测试单的详细信息。
+ - 第3条的project属性 @13
+ - 第3条的name属性 @测试单3
+ - 第3条的product属性 @3
+ - 第3条的execution属性 @103
+ - 第3条的build属性 @13
+ - 第3条的owner属性 @user5
+ - 第3条的desc属性 @这是测试单描述3
+ - 第3条的status属性 @done
+ - 第3条的auto属性 @no
+- 获取 ID 为 4 的测试单的详细信息。
+ - 第4条的project属性 @14
+ - 第4条的name属性 @测试单4
+ - 第4条的product属性 @4
+ - 第4条的execution属性 @104
+ - 第4条的build属性 @14
+ - 第4条的owner属性 @user6
+ - 第4条的desc属性 @这是测试单描述4
+ - 第4条的status属性 @blocked
+ - 第4条的auto属性 @no
 
 */
 
