6924b5e21283d47e00420bd314d8427ef2f14024
wangzemei <wangzemei@easycorp.ltd>
2025-06-06 09:11:45 +0800

    + [story#75286] add the calculate for count_of_daily_resolved_bug_in_execution metric.

--- module/metric/calc/execution/scale/count_of_daily_resolved_bug_in_execution.php
+++ module/metric/calc/execution/scale/count_of_daily_resolved_bug_in_execution.php
@@ -7,6 +7,24 @@ class count_of_daily_resolved_bug_in_execution extends baseCalc
 
     public $result = array();
 
+    public function calculate($row)
+    {
+        if($row->status == 'active') return false;
+
+        $year = $this->getYear($row->resolvedDate);
+        if(!$year) return false;
+
+        $date = substr($row->resolvedDate, 0, 10);
+        list($year, $month, $day) = explode('-', $date);
+
+        if(!isset($this->result[$row->execution]))                      $this->result[$row->execution] = array();
+        if(!isset($this->result[$row->execution][$year]))               $this->result[$row->execution][$year] = array();
+        if(!isset($this->result[$row->execution][$year][$month]))       $this->result[$row->execution][$year][$month] = array();
+        if(!isset($this->result[$row->execution][$year][$month][$day])) $this->result[$row->execution][$year][$month][$day] = 0;
+
+        $this->result[$row->execution][$year][$month][$day] ++;
+    }
+
     public function getResult($options = array())
     {
         $records = $this->getRecords(array('execution', 'year', 'month', 'day', 'value'));
