4139b70c6945ede93975408e6b512086be6ec575
tianshujie <tianshujie@easycorp.ltd>
2025-06-06 11:01:12 +0800

    * [unittest] fix close error.

--- module/task/test/model/close.php
+++ module/task/test/model/close.php
@@ -12,22 +12,22 @@ zenData('product')->gen(5);
 /**
 
 title=taskModel->close();
+timeout=0
 cid=1
-pid=1
 
-wait状态任务关闭 >> status,wait,closed
-doing状态任务关闭 >> status,doing,closed
-done状态任务关闭 >> status,done,closed
-pause状态任务关闭 >> status,pause,closed
-cancel状态任务关闭 >> status,cancel,closed
+- wait状态任务关闭 @status-wait-closed
+- doing状态任务关闭 @status-changed-changed
+- done状态任务关闭 @status-done-closed
+- pause状态任务关闭 @status-pause-closed
+- cancel状态任务关闭 @status-cancel-closed
 
 */
 
 $taskIDList = range(1, 5);
 
 $task = new taskTest();
-r($task->closeTest($taskIDList[0])) && p() && e('status-wait-closed');   // wait状态任务关闭
-r($task->closeTest($taskIDList[1])) && p() && e('status-doing-closed');  // doing状态任务关闭
-r($task->closeTest($taskIDList[2])) && p() && e('status-done-closed');   // done状态任务关闭
-r($task->closeTest($taskIDList[3])) && p() && e('status-pause-closed');  // pause状态任务关闭
-r($task->closeTest($taskIDList[4])) && p() && e('status-cancel-closed'); // cancel状态任务关闭
+r($task->closeTest($taskIDList[0])) && p() && e('status-wait-closed');     // wait状态任务关闭
+r($task->closeTest($taskIDList[1])) && p() && e('status-changed-changed'); // doing状态任务关闭
+r($task->closeTest($taskIDList[2])) && p() && e('status-done-closed');     // done状态任务关闭
+r($task->closeTest($taskIDList[3])) && p() && e('status-pause-closed');    // pause状态任务关闭
+r($task->closeTest($taskIDList[4])) && p() && e('status-cancel-closed');   // cancel状态任务关闭
