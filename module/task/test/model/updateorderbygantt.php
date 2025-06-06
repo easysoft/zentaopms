54c3a57155eb452d7df849ee2099561fdc906a01
tianshujie <tianshujie@easycorp.ltd>
2025-06-06 14:06:08 +0800

    * [unittest] fix updateorderbygantt error.

--- module/task/test/model/updateorderbygantt.php
+++ module/task/test/model/updateorderbygantt.php
@@ -12,6 +12,18 @@ title=taskModel->updateOrderByGantt();
 timeout=0
 cid=1
 
+- 测试更新开发任务11的顺序 @1,2,3,4,5
+
+- 测试更新开发任务11的顺序 @3,1,2,4,5
+
+- 测试更新开发任务11的顺序 @4,2,3,1,5
+
+- 测试更新开发任务14的顺序 @1,2,3,4,5
+
+- 测试更新开发任务14的顺序 @3,1,2,4,5
+
+- 测试更新开发任务14的顺序 @4,2,3,1,5
+
 */
 
 $taskIdList[0] = array(1, 2, 3, 4, 5);
@@ -22,4 +34,7 @@ $taskTester = new taskTest();
 
 r($taskTester->updateOrderByGanttTest(3, 1, $taskIdList[0])) && p() && e('1,2,3,4,5'); // 测试更新开发任务11的顺序
 r($taskTester->updateOrderByGanttTest(3, 1, $taskIdList[1])) && p() && e('3,1,2,4,5'); // 测试更新开发任务11的顺序
+r($taskTester->updateOrderByGanttTest(3, 1, $taskIdList[2])) && p() && e('4,2,3,1,5'); // 测试更新开发任务11的顺序
+r($taskTester->updateOrderByGanttTest(3, 4, $taskIdList[0])) && p() && e('1,2,3,4,5'); // 测试更新开发任务14的顺序
+r($taskTester->updateOrderByGanttTest(3, 4, $taskIdList[1])) && p() && e('3,1,2,4,5'); // 测试更新开发任务14的顺序
 r($taskTester->updateOrderByGanttTest(3, 4, $taskIdList[2])) && p() && e('4,2,3,1,5'); // 测试更新开发任务14的顺序
