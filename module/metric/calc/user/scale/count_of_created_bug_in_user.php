27cb44d4d11620a7579622762a7bd784dc9e1526
wangzemei <wangzemei@easycorp.ltd>
2025-06-06 16:55:47 +0800

    + [task#144626] add the comments for count_of_created_bug_in_user metrics.

--- module/metric/calc/user/scale/count_of_created_bug_in_user.php
+++ module/metric/calc/user/scale/count_of_created_bug_in_user.php
@@ -1,4 +1,25 @@
 <?php
+/**
+ * 按人员统计的创建Bug数。
+ * Count of created bug in user.
+ *
+ * 范围：user
+ * 对象：bug
+ * 目的：scale
+ * 度量名称：按人员统计的创建Bug数
+ * 单位：个
+ * 描述：按人员统计的创建Bug数是指每个人创建修复Bug总量。该度量项可以帮助我们了解每个人对已解决的Bug进行确认与关闭的速度和效率。
+ * 定义：截止当前时间;统计每个人创建Bug数的求和;过滤已删除的Bug;过滤已删除的产品;
+ * 度量库：
+ * 收集方式：realtime
+ *
+ * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @author    Zemei Wang <wangzemei@easycorp.ltd>
+ * @package
+ * @uses      func
+ * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @Link      https://www.zentao.net
+ */
 class count_of_created_bug_in_user extends baseCalc
 {
     public $dataset = '';
