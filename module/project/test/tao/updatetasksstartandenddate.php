2afe3103f849f01970af4ff75985a005eddeb134
tianshujie <tianshujie@easycorp.ltd>
2025-06-06 15:23:58 +0800

    * [unittest] add comment for updatetasksstartandenddate.

--- module/project/test/tao/updatetasksstartandenddate.php
+++ module/project/test/tao/updatetasksstartandenddate.php
@@ -13,6 +13,25 @@ title=测试 projectModel->updateTasksStartAndEndDate();
 timeout=0
 cid=1
 
+- 测试更新敏捷项目下的任务起止日期
+ - 第0条的field属性 @estStarted
+ - 第0条的old属性 @2020-11-01
+ - 第0条的new属性 @2020-01-01
+- 测试更新瀑布项目下的任务起止日期
+ - 第0条的field属性 @deadline
+ - 第0条的old属性 @2021-01-13
+ - 第0条的new属性 @2020-03-13
+- 测试更新看板项目下的任务起止日期 @0
+- 测试更新敏捷项目下的任务起止日期
+ - 第0条的field属性 @estStarted
+ - 第0条的old属性 @2020-01-01
+ - 第0条的new属性 @2019-03-02
+- 测试更新瀑布项目下的任务起止日期
+ - 第0条的field属性 @deadline
+ - 第0条的old属性 @2020-03-13
+ - 第0条的new属性 @2019-05-12
+- 测试更新看板项目下的任务起止日期 @0
+
 */
 
 $projectIdList = array(11, 60 ,100);
