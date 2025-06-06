a6802d6442f2b2c9bcce48dcf9ad2b76e2a920d1
wangzemei <wangzemei@easycorp.ltd>
2025-06-06 16:56:42 +0800

    + [task#144627] add the getResult for count_of_resolved_bug_in_user metrics.

--- module/metric/calc/user/scale/count_of_resolved_bug_in_user.php
+++ module/metric/calc/user/scale/count_of_resolved_bug_in_user.php
@@ -12,4 +12,17 @@ class count_of_resolved_bug_in_user extends baseCalc
         if(!isset($this->result[$row->resolvedBy])) $this->result[$row->resolvedBy] = array();
         $this->result[$row->resolvedBy][$row->id] = $row->id;
     }
+
+    public function getResult($options = array())
+    {
+        foreach($this->result as $resolvedBy => $bugs)
+        {
+            if(!is_array($bugs)) continue;
+
+            $this->result[$resolvedBy] = count($bugs);
+        }
+
+        $records = $this->getRecords(array('user', 'value'));
+        return $this->filterByOptions($records, $options);
+    }
 }
