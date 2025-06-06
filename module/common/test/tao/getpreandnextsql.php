e6823c7eebe284f548cd5464a85a357818731cd7
tianshujie <tianshujie@easycorp.ltd>
2025-06-06 16:32:33 +0800

    * [unittest] fix getpreandnextsql error.

--- module/common/test/tao/getpreandnextsql.php
+++ module/common/test/tao/getpreandnextsql.php
@@ -6,8 +6,15 @@ su('admin');
 /**
 
 title=测试 commonTao->getPreAndNextSQL();
+timeout=0
 cid=1
-pid=1
+
+- 执行common模块的getPreAndNextSQL方法，参数是'story'  @SELECT * FROM `zt_story` WHERE id < 40
+- 执行common模块的getPreAndNextSQL方法，参数是'story'  @SELECT *, IF(`pri` = 0, 256, `pri`) as priOrder, IF(`severity` = 0, 256, `severity`) as severityOrder FROM `zt_story` WHERE id < 40  ORDER BY `id` desc,`priOrder`,`severityOrder`
+
+- 执行common模块的getPreAndNextSQL方法，参数是'story'  @SELECT t1.* FROM `zt_story` AS t1 LEFT JOIN `zt_storyspec` AS t2 on t1.id=t2.story WHERE t1.id < 40 AND t1.version = t2.version
+- 执行common模块的getPreAndNextSQL方法，参数是'story'  @SELECT t1.* FROM `zt_story` AS t1 LEFT JOIN `zt_storyspec` AS t2 on t1.id=t2.story WHERE t1.id < 40 AND t1.version = t2.version ORDER BY t1.id desc
+- 执行common模块的getPreAndNextSQL方法，参数是'story'  @SELECT t1.* FROM `zt_story` AS t1 LEFT JOIN `zt_storyspec` AS t2 on t1.id=t2.story WHERE t1.id < 40 AND t1.version = t2.version ORDER BY t1.title asc
 
 */
 
@@ -21,7 +28,6 @@ r(trim($tester->common->getPreAndNextSQL('story'))) && p() && e('SELECT * FROM `
 $_SESSION['storyOrderBy'] = 'id desc, priOrder, severityOrder';
 r(trim($tester->common->getPreAndNextSQL('story'))) && p() && e('SELECT *, IF(`pri` = 0, 256, `pri`) as priOrder, IF(`severity` = 0, 256, `severity`) as severityOrder FROM `zt_story` WHERE id < 40  ORDER BY `id` desc,`priOrder`,`severityOrder`');
 
-
 $_SESSION['storyQueryCondition'] = 'SELECT t1.* FROM `zt_story` AS t1 LEFT JOIN `zt_storyspec` AS t2 on t1.id=t2.story WHERE t1.id < 40 AND t1.version = t2.version';
 $_SESSION['storyOnlyCondition']  = false;
 unset($_SESSION['storyOrderBy']);
@@ -29,3 +35,6 @@ r(trim($tester->common->getPreAndNextSQL('story'))) && p() && e('SELECT t1.* FRO
 
 $_SESSION['storyOrderBy'] = 't1.id desc';
 r(trim($tester->common->getPreAndNextSQL('story'))) && p() && e('SELECT t1.* FROM `zt_story` AS t1 LEFT JOIN `zt_storyspec` AS t2 on t1.id=t2.story WHERE t1.id < 40 AND t1.version = t2.version ORDER BY t1.id desc');
+
+$_SESSION['storyOrderBy'] = 't1.title asc';
+r(trim($tester->common->getPreAndNextSQL('story'))) && p() && e('SELECT t1.* FROM `zt_story` AS t1 LEFT JOIN `zt_storyspec` AS t2 on t1.id=t2.story WHERE t1.id < 40 AND t1.version = t2.version ORDER BY t1.title asc');
