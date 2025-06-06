c6380ed77fd4d7fcb7ef9606eae4488356dddfac
tianshujie <tianshujie@easycorp.ltd>
2025-06-06 13:58:08 +0800

    * [unittest] fix processdata4report error

--- module/task/test/model/processdata4report.php
+++ module/task/test/model/processdata4report.php
@@ -36,6 +36,35 @@ cid=1
 - 计算executionID为3的执行下按预计剩余统计的图表数据
  - 第1条的name属性 @1
  - 第1条的value属性 @6
+- 计算executionID为3的执行下按已消耗统计的图表数据
+ - 第3条的name属性 @3
+ - 第3条的value属性 @9
+- 计算executionID为3的执行下按由谁完成统计的图表数据属性:name @~~
+- 计算executionID为3的执行下按关闭原因统计的图表数据属性:name @~~
+- 计算executionID为3的执行下按任务状态统计的图表数据
+ - 第wait条的name属性 @wait
+ - 第wait条的value属性 @35
+- 计算executionID为3的执行下按每日完成统计的图表数据
+ - 第2021-03-21条的name属性 @2021-03-21
+ - 第2021-03-21条的value属性 @1
+- 计算executionID为3的执行下非父任务按迭代任务数统计的图表数据
+ - 第3条的name属性 @3
+ - 第3条的value属性 @75
+- 计算executionID为3的执行下非父任务按模块统计的图表数据
+ - 第2条的name属性 @2
+ - 第2条的value属性 @24
+- 计算executionID为3的执行下非父任务按指派给统计的图表数据
+ - 第admin条的name属性 @admin
+ - 第admin条的value属性 @14
+- 计算executionID为3的执行下非父任务按任务类型统计的图表数据
+ - 第ui条的name属性 @ui
+ - 第ui条的value属性 @7
+- 计算executionID为3的执行下非父任务按优先级统计的图表数据
+ - 第2条的name属性 @2
+ - 第2条的value属性 @16
+- 计算executionID为3的执行下非父任务按截止时间统计的图表数据
+ - 第2021-01-01条的name属性 @2021-01-01
+ - 第2021-01-01条的value属性 @1
 
 */
 
