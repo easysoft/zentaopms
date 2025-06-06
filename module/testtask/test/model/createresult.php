e9973345a91b5eded61b29be37ddd29ad305094f
liumengyi <liumengyi@easycorp.ltd>
2025-06-06 11:03:54 +0800

    + [unittest] add unittest results for testtaskModel::createResult.

--- module/testtask/test/model/createresult.php
+++ module/testtask/test/model/createresult.php
@@ -68,6 +68,24 @@ cid=1
  - 第result条的case属性 @2
  - 第result条的version属性 @1
  - 第result条的caseResult属性 @pass
+- 在测试单外执行测试用例 2，测试结果为失败。
+ - 属性caseResult @fail
+ - 第case条的lastRunner属性 @admin
+ - 第case条的lastRunResult属性 @fail
+ - 第result条的lastRunner属性 @admin
+ - 第result条的run属性 @0
+ - 第result条的case属性 @2
+ - 第result条的version属性 @1
+ - 第result条的caseResult属性 @fail
+- 在测试单外执行测试用例 2，测试结果为阻塞。
+ - 属性caseResult @blocked
+ - 第case条的lastRunner属性 @admin
+ - 第case条的lastRunResult属性 @blocked
+ - 第result条的lastRunner属性 @admin
+ - 第result条的run属性 @0
+ - 第result条的case属性 @2
+ - 第result条的version属性 @1
+ - 第result条的caseResult属性 @blocked
 
 */
 
