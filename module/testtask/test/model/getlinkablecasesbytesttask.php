d757c6e8a25246c1d7d9f725fc47aead49ecab51
wangyuting <wangyuting@chandao.com>
2025-06-06 09:04:43 +0000

    * [task#144843,doing,0.1h,2h] Optimize testtask getlinkablecasesbytesttask unit test script.

--- module/testtask/test/model/getlinkablecasesbytesttask.php
+++ module/testtask/test/model/getlinkablecasesbytesttask.php
@@ -73,6 +73,46 @@ cid=1
  - 第4条的lastRunResult属性 @fail
 - 测试单 1 查询 id < 10 的用例并排除用例 2 后可关联的用例数为 5。 @5
 - 查看测试单 1 可关联的用例 3 的详细信息。
+ - 第3条的id属性 @3
+ - 第3条的title属性 @这个是测试用例3
+ - 第3条的pri属性 @3
+ - 第3条的type属性 @config
+ - 第3条的auto属性 @no
+ - 第3条的status属性 @blocked
+ - 第3条的lastRunner属性 @user1
+ - 第3条的lastRunResult属性 @pass
+- 查看测试单 1 可关联的用例 4 的详细信息。
+ - 第4条的id属性 @4
+ - 第4条的title属性 @这个是测试用例4
+ - 第4条的pri属性 @4
+ - 第4条的type属性 @install
+ - 第4条的auto属性 @no
+ - 第4条的status属性 @investigate
+ - 第4条的lastRunner属性 @user1
+ - 第4条的lastRunResult属性 @fail
+- 查看测试单 1 可关联的用例 6 的详细信息。
+ - 第6条的id属性 @6
+ - 第6条的title属性 @这个是测试用例6
+ - 第6条的pri属性 @2
+ - 第6条的type属性 @interface
+ - 第6条的auto属性 @no
+ - 第6条的status属性 @normal
+ - 第6条的lastRunner属性 @user1
+ - 第6条的lastRunResult属性 @fail
+- 测试单 1 查询 id < 10 并限制每页查询 5 条的用例后可关联的用例数为 5。 @5
+- 查看测试单 1 可关联的用例 2 的详细信息。
+ - 第2条的id属性 @2
+ - 第2条的title属性 @这个是测试用例2
+ - 第2条的pri属性 @2
+ - 第2条的type属性 @performance
+ - 第2条的auto属性 @no
+ - 第2条的status属性 @normal
+ - 第2条的lastRunner属性 @user1
+ - 第2条的lastRunResult属性 @fail
+- 查看测试单 1 可关联的用例 3 的详细信息。
+ - 第3条的id属性 @3
+ - 第3条的title属性 @这个是测试用例3
+
 
 */
 
