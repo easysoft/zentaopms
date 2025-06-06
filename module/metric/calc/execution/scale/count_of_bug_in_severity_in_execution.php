68855ced294a95c52a956ec78fa80e0cbd0a9631
wangzemei <wangzemei@easycorp.ltd>
2025-06-06 09:25:11 +0800

    + [story#75279] add the comments for the count_of_bug_in_severity_in_execution metric.

--- module/metric/calc/execution/scale/count_of_bug_in_severity_in_execution.php
+++ module/metric/calc/execution/scale/count_of_bug_in_severity_in_execution.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * 按严重程度统计的执行下的Bug数。
+ * Count of bug in severity in execution.
+ *
+ * 范围：severity
+ * 对象：bug
+ * 目的：scale
+ * 度量名称：按严重程度统计的执行下的Bug数
+ * 单位：个
+ * 描述：按严重程度统计的执行下的Bug数
+ * 定义：按严重程度统计的执行下的Bug数
+ *
+ * @copyright Copyright 2009-2025 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @author    Zemei Wang <wangzemei@easycorp.ltd>
+ * @package
+ * @uses      func
+ * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @Link      https://www.zentao.net
+ */
 class count_of_bug_in_severity_in_execution extends baseCalc
 {
     public $dataset = 'getExecutionBugs';
