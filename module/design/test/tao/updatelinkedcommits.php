c69e02df3a2452c2e702b5dcfdc6be3749a4c699
tianshujie <tianshujie@easycorp.ltd>
2025-06-06 14:53:07 +0800

    * [unittest] fix updatelinkedcommits error.

--- module/design/test/tao/updatelinkedcommits.php
+++ module/design/test/tao/updatelinkedcommits.php
@@ -1,8 +1,10 @@
 #!/usr/bin/env php
 <?php
+
 /**
 
 title=测试 designModel->updateLinkedCommits();
+timeout=0
 cid=1
 
 - 测试空数据 @0
@@ -10,7 +12,7 @@ cid=1
 - 测试repoID为0的数据 @0
 - 测试提交记录为空的数据 @0
 - 测试正常数据
- - 第5条的project属性 @11
+ - 第5条的project属性 @60
  - 第5条的product属性 @0
  - 第5条的AType属性 @commit
  - 第5条的AID属性 @3
@@ -37,5 +39,5 @@ r($designTester->updateLinkedCommitsTest($designs[0], $repos[0], $revisions[0]))
 r($designTester->updateLinkedCommitsTest($designs[0], $repos[1], $revisions[1])) && p()                                                 && e('0');                                    // 测试designID为0的数据
 r($designTester->updateLinkedCommitsTest($designs[1], $repos[0], $revisions[1])) && p()                                                 && e('0');                                    // 测试repoID为0的数据
 r($designTester->updateLinkedCommitsTest($designs[1], $repos[1], $revisions[0])) && p()                                                 && e('0');                                    // 测试提交记录为空的数据
-r($designTester->updateLinkedCommitsTest($designs[1], $repos[1], $revisions[1])) && p('5:project,product,AType,AID,BType,BID,relation') && e('11,0,commit,3,design,1,completedfrom'); // 测试正常数据
+r($designTester->updateLinkedCommitsTest($designs[1], $repos[1], $revisions[1])) && p('5:project,product,AType,AID,BType,BID,relation') && e('60,0,commit,3,design,1,completedfrom'); // 测试正常数据
 r($designTester->updateLinkedCommitsTest($designs[2], $repos[1], $revisions[1])) && p()                                                 && e('0');                                    // 测试designID不存在的数据
