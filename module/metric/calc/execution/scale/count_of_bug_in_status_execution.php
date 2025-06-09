93e900ed97c65bdf64832f0002c58e2137f79cf7
wangzemei <wangzemei@easycorp.ltd>
2025-06-09 16:29:39 +0800

    + [task#144617] add the count_of_bug_in_status_execution metric.

--- module/metric/calc/execution/scale/count_of_bug_in_status_execution.php
+++ module/metric/calc/execution/scale/count_of_bug_in_status_execution.php
@@ -12,4 +12,21 @@ class count_of_bug_in_status_execution extends baseCalc
         if(!isset($this->result[$row->status])) $this->result[$row->status] = array();
         $this->result[$row->status][$row->id] = $row->id;
     }
+
+    public function getResult($options = array())
+    {
+        foreach($this->result as $status => $tasks)
+        {
+            if(!is_array($tasks))
+            {
+                unset($this->result[$status]);
+                continue;
+            }
+
+            $this->result[$status] = count($tasks);
+        }
+
+        $records = $this->getRecords(array('status', 'value'));
+        return $this->filterByOptions($records, $options);
+    }
 }
