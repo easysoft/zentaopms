67bdfdf4eb0ec04a1c4f8205dbf3be95cc6b188c
sunguangming <sunguangming@chandao.com>
2025-06-09 00:29:49 +0000

    * [unittest] modify unittest.

--- module/tutorial/test/model/getexecutionstats.php
+++ module/tutorial/test/model/getexecutionstats.php
@@ -31,19 +31,19 @@ cid=0
  - 第0条的name属性 @Test execution
  - 第0条的left属性 @0
  - 第0条的teamCount属性 @1
-- 测试 user1 登录时是否能拿到 browseType 空 数据 teamMembers第0[teamMembers]条的0属性 @admin
+- 测试 user1 登录时是否能拿到 browseType 空 数据 teamMembers第0[teamMembers]条的0属性 @user1
 - 测试 user1 登录时是否能拿到 browseType all 数据
  - 第0条的id属性 @3
  - 第0条的name属性 @Test execution
  - 第0条的left属性 @0
  - 第0条的teamCount属性 @1
-- 测试 user1 登录时是否能拿到 browseType all 数据 teamMembers第0[teamMembers]条的0属性 @admin
+- 测试 user1 登录时是否能拿到 browseType all 数据 teamMembers第0[teamMembers]条的0属性 @user1
 - 测试 user1 登录时是否能拿到 browseType noclosed 数据
  - 第0条的id属性 @3
  - 第0条的name属性 @Test execution-noclosed
  - 第0条的left属性 @0
  - 第0条的teamCount属性 @1
-- 测试 user1 登录时是否能拿到 browseType noclosed 数据 teamMembers第0[teamMembers]条的0属性 @admin
+- 测试 user1 登录时是否能拿到 browseType noclosed 数据 teamMembers第0[teamMembers]条的0属性 @user1
 
 */
 
@@ -64,8 +64,8 @@ r($tutorial->getExecutionStatsTest($browseType['2'])) && p('0[teamMembers]:0')  
 
 su('user1');
 r($tutorial->getExecutionStatsTest($browseType['0'])) && p('0:id,name,left,teamCount') && e('3,Test execution,0,1');          // 测试 user1 登录时是否能拿到 browseType 空 数据
-r($tutorial->getExecutionStatsTest($browseType['0'])) && p('0[teamMembers]:0')         && e('admin');                         // 测试 user1 登录时是否能拿到 browseType 空 数据 teamMembers
+r($tutorial->getExecutionStatsTest($browseType['0'])) && p('0[teamMembers]:0')         && e('user1');                         // 测试 user1 登录时是否能拿到 browseType 空 数据 teamMembers
 r($tutorial->getExecutionStatsTest($browseType['1'])) && p('0:id,name,left,teamCount') && e('3,Test execution,0,1');          // 测试 user1 登录时是否能拿到 browseType all 数据
-r($tutorial->getExecutionStatsTest($browseType['1'])) && p('0[teamMembers]:0')         && e('admin');                         // 测试 user1 登录时是否能拿到 browseType all 数据 teamMembers
+r($tutorial->getExecutionStatsTest($browseType['1'])) && p('0[teamMembers]:0')         && e('user1');                         // 测试 user1 登录时是否能拿到 browseType all 数据 teamMembers
 r($tutorial->getExecutionStatsTest($browseType['2'])) && p('0:id,name,left,teamCount') && e('3,Test execution-noclosed,0,1'); // 测试 user1 登录时是否能拿到 browseType noclosed 数据
-r($tutorial->getExecutionStatsTest($browseType['2'])) && p('0[teamMembers]:0')         && e('admin');                         // 测试 user1 登录时是否能拿到 browseType noclosed 数据 teamMembers
+r($tutorial->getExecutionStatsTest($browseType['2'])) && p('0[teamMembers]:0')         && e('user1');                         // 测试 user1 登录时是否能拿到 browseType noclosed 数据 teamMembers
