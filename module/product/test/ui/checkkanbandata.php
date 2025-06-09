bf14761cd101520e2ed977012ec3d51f77f786a8
lixiaoyu <lixiaoyu@easycorp.ltd>
2025-06-09 07:19:17 +0000

    *[task#144951,doing,0.5h,3h] modify code

--- module/product/test/ui/checkkanbandata.php
+++ module/product/test/ui/checkkanbandata.php
@@ -7,6 +7,19 @@ title=检查产品看板数据准确性
 timeout=0
 cid=0
 
+- 检查未过期计划数
+ - 测试结果 @未过期计划数正确
+ - 最终测试状态 @SUCCESS
+- 检查进行中的项目数
+ - 测试结果 @进行中的项目数正确
+ - 最终测试状态 @SUCCESS
+- 检查进行中的执行数
+ - 测试结果 @进行中的执行数正确
+ - 最终测试状态 @SUCCESS
+- 检查正常发布数
+ - 测试结果 @正常发布数正确
+ - 最终测试状态 @SUCCESS
+
 */
 chdir(__DIR__);
 include '../lib/kanban.ui.class.php';
@@ -28,7 +41,7 @@ $project->type->range('project{3},sprint{3}');
 $project->parent->range('0{3},1{3}');
 $project->auth->range('extend');
 $project->storytype->range('story');
-$project->path->range('`,1,`','`,2,`','`,3,`,`,1,4,`','`,1,5,`','`,1,6,`');
+$project->path->range('`,1,`','`,2,`','`,3,`','`,1,4,`','`,1,5,`','`,1,6,`');
 $project->grade->range('1');
 $project->name->range('项目1,项目2,项目3,迭代1,迭代2,迭代3');
 $project->hasProduct->range('1');
