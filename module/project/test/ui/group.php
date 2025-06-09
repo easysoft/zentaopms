fd68eb7f303f0d441b6af15301545658fc3f25ee
lihuifang <lihuifang@easycorp.ltd>
2025-06-09 16:23:36 +0800

    *[task#144962,doing,0.1h,2h] Modify uitest case of group

--- module/project/test/ui/group.php
+++ module/project/test/ui/group.php
@@ -7,8 +7,15 @@ title=创建项目权限分组
 timeout=0
 cid=1
 
-- 创建项目分组提示信息检查测试结果 @项目创建分组提示信息正确
-- 创建项目分组成功测试结果 @项目分组创建成功
+- 项目创建分组必填提示信息检查
+ - 测试结果 @项目创建分组必填提示信息正确
+ - 最终测试状态 @SUCCESS
+- 创建项目分组成功
+ - 测试结果 @项目分组创建成功
+ - 最终测试状态 @SUCCESS
+- 项目创建分组名称重复时提示信息检查
+ - 测试结果 @项目创建分组名称重复时提示信息正确
+ - 最终测试状态 @SUCCESS
 
 */
 
@@ -16,6 +23,7 @@ chdir(__DIR__);
 include '../lib/group.ui.class.php';
 
 zendata('project')->loadYaml('project', false, 1)->gen(1);
+
 $tester = new groupTester();
 $tester->login();
 
@@ -23,9 +31,11 @@ $tester->login();
 $project = array(
     array('groupname' => '', 'groupdesc' => ''),
     array('groupname' => '项目分组1', 'groupdesc' => '一个分组描述哈哈哈222'),
+    array('groupname' => '项目分组1', 'groupdesc' => '一个分组描述哈哈哈222'),
 );
 
-r($tester->createGroup($project['0'])) && p('message') && e('项目创建分组提示信息正确');  // 创建项目分组提示信息检查
-r($tester->createGroup($project['1'])) && p('message') && e('项目分组创建成功');          // 创建项目分组成功
+r($tester->createGroup($project['0'])) && p('message,status') && e('项目创建分组必填提示信息正确,SUCCESS');       // 项目创建分组必填提示信息检查
+r($tester->createGroup($project['1'])) && p('message,status') && e('项目分组创建成功,SUCCESS');                   // 创建项目分组成功
+r($tester->createGroup($project['2'])) && p('message,status') && e('项目创建分组名称重复时提示信息正确,SUCCESS'); // 项目创建分组名称重复时提示信息检查
 
 $tester->closeBrowser();
