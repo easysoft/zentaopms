37254cf0169abd2a964c51c779e73a8e0a263611
lijie <lijie@chandao.com>
2025-06-09 15:16:54 +0800

    [misc, 0.1h] modify failed uitest.

--- module/requirement/test/ui/closerequirement.php
+++ module/requirement/test/ui/closerequirement.php
@@ -40,7 +40,6 @@ $story->openedBy->range('admin');
 $story->version->range('1');
 $story->assignedTo->range('[]');
 $story->reviewedBy->range('[]');
-$story->reviewedDate->range('`NULL`');
 $story->closedBy->range('[]');
 $story->closedReason->range('[]');
 $story->gen(6);
