efc4dad5bd1ffc87cd74a15f26b63164407f3cf2
liumengyi <liumengyi@easycorp.ltd>
2025-06-09 09:12:50 +0800

    * [unittest] modify unittest of getBugInfoForResult.

--- module/bug/test/model/getbuginfofromresult.php
+++ module/bug/test/model/getbuginfofromresult.php
@@ -8,6 +8,7 @@ zenData('case')->gen(10);
 zenData('testsuite')->gen(10);
 zenData('testrun')->gen(10);
 zenData('casestep')->gen(10);
+zenData('casespec')->gen(0);
 zenData('story')->gen(10);
 
 $result = zenData('testresult');
@@ -23,17 +24,17 @@ cid=1
 - 测试获取runID为0 caseID为6的bug @0
 - 测试获取runID为0 caseID为10的bug @0
 - 测试获取runID为2 caseID为2的bug @这个是测试用例2
-- 测试获取runID为2 caseID为6的bug @1111111111
-- 测试获取runID为2 caseID为10的bug @22
+- 测试获取runID为2 caseID为6的bug @这个是测试用例6
+- 测试获取runID为2 caseID为10的bug @这个是测试用例10
 - 测试获取runID为6 caseID为2的bug @这个是测试用例2
-- 测试获取runID为6 caseID为6的bug @1111111111
-- 测试获取runID为6 caseID为10的bug @22
+- 测试获取runID为6 caseID为6的bug @这个是测试用例6
+- 测试获取runID为6 caseID为10的bug @这个是测试用例10
 - 测试获取runID为10 caseID为2的bug @这个是测试用例2
-- 测试获取runID为10 caseID为6的bug @1111111111
-- 测试获取runID为10 caseID为10的bug @22
+- 测试获取runID为10 caseID为6的bug @这个是测试用例6
+- 测试获取runID为10 caseID为10的bug @这个是测试用例10
 - 测试获取runID为2 caseID为2 stepIdList为2 的bug @这个是测试用例2
-- 测试获取runID为2 caseID为6 stepIdList为6 的bug @1111111111
-- 测试获取runID为2 caseID为10 stepIdList为10 的bug @22
+- 测试获取runID为2 caseID为6 stepIdList为6 的bug @这个是测试用例6
+- 测试获取runID为2 caseID为10 stepIdList为10 的bug @这个是测试用例10
 
 */
 
@@ -43,18 +44,18 @@ $stepIdList = array('2', '6', '10');
 
 $bug = new bugTest();
 
-r($bug->getBugInfoFromResultTest($runIDList[0], $caseIDList[0]))                 && p() && e('0');               // 测试获取runID为0 caseID为2的bug
-r($bug->getBugInfoFromResultTest($runIDList[0], $caseIDList[1]))                 && p() && e('0');               // 测试获取runID为0 caseID为6的bug
-r($bug->getBugInfoFromResultTest($runIDList[0], $caseIDList[2]))                 && p() && e('0');               // 测试获取runID为0 caseID为10的bug
-r($bug->getBugInfoFromResultTest($runIDList[1], $caseIDList[0]))                 && p() && e('这个是测试用例2'); // 测试获取runID为2 caseID为2的bug
-r($bug->getBugInfoFromResultTest($runIDList[1], $caseIDList[1]))                 && p() && e('1111111111');      // 测试获取runID为2 caseID为6的bug
-r($bug->getBugInfoFromResultTest($runIDList[1], $caseIDList[2]))                 && p() && e('22');              // 测试获取runID为2 caseID为10的bug
-r($bug->getBugInfoFromResultTest($runIDList[2], $caseIDList[0]))                 && p() && e('这个是测试用例2'); // 测试获取runID为6 caseID为2的bug
-r($bug->getBugInfoFromResultTest($runIDList[2], $caseIDList[1]))                 && p() && e('1111111111');      // 测试获取runID为6 caseID为6的bug
-r($bug->getBugInfoFromResultTest($runIDList[2], $caseIDList[2]))                 && p() && e('22');              // 测试获取runID为6 caseID为10的bug
-r($bug->getBugInfoFromResultTest($runIDList[3], $caseIDList[0]))                 && p() && e('这个是测试用例2'); // 测试获取runID为10 caseID为2的bug
-r($bug->getBugInfoFromResultTest($runIDList[3], $caseIDList[1]))                 && p() && e('1111111111');      // 测试获取runID为10 caseID为6的bug
-r($bug->getBugInfoFromResultTest($runIDList[3], $caseIDList[2]))                 && p() && e('22');              // 测试获取runID为10 caseID为10的bug
-r($bug->getBugInfoFromResultTest($runIDList[1], $caseIDList[0], $stepIdList[0])) && p() && e('这个是测试用例2'); // 测试获取runID为2 caseID为2 stepIdList为2 的bug
-r($bug->getBugInfoFromResultTest($runIDList[1], $caseIDList[1], $stepIdList[1])) && p() && e('1111111111');      // 测试获取runID为2 caseID为6 stepIdList为6 的bug
-r($bug->getBugInfoFromResultTest($runIDList[1], $caseIDList[2], $stepIdList[2])) && p() && e('22');              // 测试获取runID为2 caseID为10 stepIdList为10 的bug
+r($bug->getBugInfoFromResultTest($runIDList[0], $caseIDList[0]))                 && p() && e('0');                // 测试获取runID为0 caseID为2的bug
+r($bug->getBugInfoFromResultTest($runIDList[0], $caseIDList[1]))                 && p() && e('0');                // 测试获取runID为0 caseID为6的bug
+r($bug->getBugInfoFromResultTest($runIDList[0], $caseIDList[2]))                 && p() && e('0');                // 测试获取runID为0 caseID为10的bug
+r($bug->getBugInfoFromResultTest($runIDList[1], $caseIDList[0]))                 && p() && e('这个是测试用例2');  // 测试获取runID为2 caseID为2的bug
+r($bug->getBugInfoFromResultTest($runIDList[1], $caseIDList[1]))                 && p() && e('这个是测试用例6');  // 测试获取runID为2 caseID为6的bug
+r($bug->getBugInfoFromResultTest($runIDList[1], $caseIDList[2]))                 && p() && e('这个是测试用例10'); // 测试获取runID为2 caseID为10的bug
+r($bug->getBugInfoFromResultTest($runIDList[2], $caseIDList[0]))                 && p() && e('这个是测试用例2');  // 测试获取runID为6 caseID为2的bug
+r($bug->getBugInfoFromResultTest($runIDList[2], $caseIDList[1]))                 && p() && e('这个是测试用例6');  // 测试获取runID为6 caseID为6的bug
+r($bug->getBugInfoFromResultTest($runIDList[2], $caseIDList[2]))                 && p() && e('这个是测试用例10'); // 测试获取runID为6 caseID为10的bug
+r($bug->getBugInfoFromResultTest($runIDList[3], $caseIDList[0]))                 && p() && e('这个是测试用例2');  // 测试获取runID为10 caseID为2的bug
+r($bug->getBugInfoFromResultTest($runIDList[3], $caseIDList[1]))                 && p() && e('这个是测试用例6');  // 测试获取runID为10 caseID为6的bug
+r($bug->getBugInfoFromResultTest($runIDList[3], $caseIDList[2]))                 && p() && e('这个是测试用例10'); // 测试获取runID为10 caseID为10的bug
+r($bug->getBugInfoFromResultTest($runIDList[1], $caseIDList[0], $stepIdList[0])) && p() && e('这个是测试用例2');  // 测试获取runID为2 caseID为2 stepIdList为2 的bug
+r($bug->getBugInfoFromResultTest($runIDList[1], $caseIDList[1], $stepIdList[1])) && p() && e('这个是测试用例6');  // 测试获取runID为2 caseID为6 stepIdList为6 的bug
+r($bug->getBugInfoFromResultTest($runIDList[1], $caseIDList[2], $stepIdList[2])) && p() && e('这个是测试用例10'); // 测试获取runID为2 caseID为10 stepIdList为10 的bug
