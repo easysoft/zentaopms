6d69e51d7f77381672c0575ee8ce552b47d33b7b
huairong <huairong@easycorp.ltd>
2025-06-09 15:51:03 +0800

    + [task#144954,doing,0.1h,0.5h] Modify element xpath of view page.

--- module/project/test/ui/page/view.php
+++ module/project/test/ui/page/view.php
@@ -10,12 +10,14 @@ class viewPage extends page
             'acl'                   => "//*[@id='mainContent']/div[1]/div[1]/div[1]/div[2]/div[1]/span[5]",
             'hasproductend'         => "//*[@id='mainContent']/div/div/div[2]/div/table[3]/tbody/tr/td/div/div[2]/span[2]",
             'noproductend'          => "//*[@id='mainContent']/div/div/div[2]/div/table[2]/tbody/tr/td/div/div[2]/span[2]",
-            'waterfallend'          => "//*[@id='mainContent']/div[1]/div[1]/div[2]/div/table[3]/tbody/tr/td/div/div[2]/span[2]",
-            'waterfallnoproductend' => "//*[@id='mainContent']/div[1]/div[1]/div[2]/div/table[2]/tbody/tr/td/div/div[2]/span[2]",
-            'projectName'           => "//*[@id='mainContent']/div[1]/div[1]/div[1]/div[2]/div[1]/span[2]",
+            'waterfallEnd'          => "//*[@id='mainContent']/div[1]/div[1]/div[2]/div/table[3]/tbody/tr/td/div/div[2]/span[2]",
+            'waterfallNoProductEnd' => "//*[@id='mainContent']/div[1]/div[1]/div[2]/div/table[2]/tbody/tr/td/div/div[2]/span[2]",
             'acl'                   => "//*[@id='mainContent']/div[1]/div[1]/div[1]/div[2]/div[1]/span[5]",
             'end'                   => "//*[@id='mainContent']/div[1]/div[1]/div[2]/div/table[3]/tbody/tr/td/div/div[2]/span[2]",
             'linkedProduct'         => "//*[@id='mainContent']/div[1]/div[1]/div[2]/div/table[1]/tbody/tr[3]/td[1]/div/div/a/span",
+            /* 运营界面项目概况页 */
+            'projectNameLite' => "//*[@id='mainContent']/div[1]/div[1]/div[1]/div[2]/div[1]/span[2]",
+            'aclLite'         => "//*[@id='mainContent']/div[1]/div[1]/div[1]/div[2]/div[1]/span[5]",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
