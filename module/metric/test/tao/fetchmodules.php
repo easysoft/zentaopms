caf1633e4f832c67c56526d2d9b1562507bc32ff
zhouxin <zhouxin@easycorp.ltd>
2025-06-09 13:11:58 +0800

    * [unittest] Modify the unit test of metricTao::fetchmodules.

--- module/metric/test/tao/fetchmodules.php
+++ module/metric/test/tao/fetchmodules.php
@@ -7,11 +7,21 @@ title=fetchModules
 timeout=0
 cid=1
 
-- 获取范围为系统的分组个数 @26
-- 获取范围为产品的分组个数 @9
-- 获取范围为项目的分组个数 @12
-- 获取范围为执行的分组个数 @10
-- 获取范围为用户的分组个数 @7
+- 获取范围为系统的分组
+ - 第0条的object属性 @application
+ - 第0条的purpose属性 @scale
+- 获取范围为产品的分组
+ - 第0条的object属性 @bug
+ - 第0条的purpose属性 @rate
+- 获取范围为项目的分组
+ - 第0条的object属性 @bug
+ - 第0条的purpose属性 @scale
+- 获取范围为执行的分组
+ - 第0条的object属性 @bug
+ - 第0条的purpose属性 @scale
+- 获取范围为用户的分组
+ - 第0条的object属性 @bug
+ - 第0条的purpose属性 @scale
 
 */
 include dirname(__FILE__, 5) . '/test/lib/init.php';
@@ -19,8 +29,8 @@ include dirname(__FILE__, 2) . '/lib/calc.unittest.class.php';
 
 $metric = new metricTest();
 
-r(count($metric->fetchModules('system'))) && p() && e(26);    // 获取范围为系统的分组个数
-r(count($metric->fetchModules('product'))) && p() && e(9);    // 获取范围为产品的分组个数
-r(count($metric->fetchModules('project'))) && p() && e(12);   // 获取范围为项目的分组个数
-r(count($metric->fetchModules('execution'))) && p() && e(10); // 获取范围为执行的分组个数
-r(count($metric->fetchModules('user'))) && p() && e(7);       // 获取范围为用户的分组个数
+r($metric->fetchModules('system'))    && p('0:object,purpose') && e('application,scale'); // 获取范围为系统的分组
+r($metric->fetchModules('product'))   && p('0:object,purpose') && e('bug,rate');          // 获取范围为产品的分组
+r($metric->fetchModules('project'))   && p('0:object,purpose') && e('bug,scale');         // 获取范围为项目的分组
+r($metric->fetchModules('execution')) && p('0:object,purpose') && e('bug,scale');         // 获取范围为执行的分组
+r($metric->fetchModules('user'))      && p('0:object,purpose') && e('bug,scale');         // 获取范围为用户的分组
