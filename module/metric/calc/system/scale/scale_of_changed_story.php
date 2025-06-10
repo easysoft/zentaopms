2da9eaa597c5aad6bcea84efa6ce12b4862643c8
caoyanyi <caoyanyi@easycorp.ltd>
2025-06-10 14:32:07 +0800

    + [task#144594,doing,0.4h] add changed story metric.

---
+++ module/metric/calc/system/scale/scale_of_changed_story.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * 按系统统计的发生变更研发需求规模数。
+ * Scale of closed story.
+ *
+ * 范围：system
+ * 对象：story
+ * 目的：scale
+ * 度量名称：按系统统计的发生变更研发需求规模数
+ * 单位：个
+ * 描述：按系统统计的发生变更研发需求规模数反映了组织在发生变更的研发需求上的规模总数，用于评估组织对于研发进展评估、质量控制、绩效评估和持续改进具有重要意义。
+ * 定义：所有的研发需求规模数求和;过滤父研发需求;过滤已删除的研发需求;
+ *
+ * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @author    Yanyi Cao <caoyanyi@chandao.com>
+ * @package
+ * @uses      func
+ * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @Link      https://www.zentao.net
+ */
+class scale_of_changed_story extends baseCalc
+{
+    public $dataset = '';
+
+    public $fieldList = array();
+
+    public $result = 0;
+
+    public function calculate($row)
+    {
+        if($row->isParent == '1') return false;
+        if(empty($row->estimate)) return false;
+
+        if(empty($row->changedDate) || $row->status == 'changing' || $row->status == 'reviewing') return false;
+        if(!empty($row->executionStartDate) && !empty($row->executionEndDate) && $row->changedDate < $row->executionStartDate || $row->changedDate > $row->executionEndDate) return false;
+
+        $this->result += $row->estimate;
+    }
+
+    public function getResult($options = array())
+    {
+        $records = $this->getRecords(array('value'));
+        return $this->filterByOptions($records, $options);
+    }
+}
