9e991d545fd8d8e0a6941c74840ca28fb55cf407
huairong <huairong@easycorp.ltd>
2025-06-10 13:09:02 +0800

    + [task#144954,doing,0.1h,0.2h] Add element xpath of communicate page.

--- module/stakeholder/test/ui/page/communicate.php
+++ module/stakeholder/test/ui/page/communicate.php
@@ -6,6 +6,7 @@ class communicatePage extends page
         parent::__construct($webdriver);
         $xpath = array(
             'communication' => "//*[@id='comment']",
+            'submitBtn'     => "//*[@id='zin_stakeholder_communicate_form']/div[2]/div/button",
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
