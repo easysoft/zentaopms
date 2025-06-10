88297bd5dc1332515489af950fa844a166881574
lihuifang <lihuifang@easycorp.ltd>
2025-06-10 10:48:40 +0800

    *[task#144962,doing,0.2h,0.8h] Modify uitest function of editcaselib.

--- module/caselib/test/lib/editcaselib.ui.class.php
+++ module/caselib/test/lib/editcaselib.ui.class.php
@@ -12,24 +12,24 @@ class editCaselibTester extends tester
     public function editCaselib($caselib)
     {
         $form = $this->initForm('caselib', 'browse', array('libID' => 1), 'appIframe-qa');
-        $form->dom->btn($this->lang->caselib->view)->click();
+        $form->wait(1);
+        $form->dom->viewBtn->click();
         $form->dom->editBtn->click();
         if(isset($caselib['name'])) $form->dom->name->setValue($caselib['name']);
 
-        $form->dom->btn($this->lang->save)->click();
+        $form->dom->saveBtn->click();
         $form->wait(1);
 
         /* 检查创建用例库页面必填提示信息 */
         if(is_object($form->dom->nameTip))
         {
             $nameTipForm = $form->dom->nameTip->getText();
-            $nameTip     = sprintf($this->lang->error->notempty, $this->lang->caselib->name);
-            return ($nameTipForm == $nameTip) ? $this->success('编辑用例库表单页必填提示信息正确') : $this->failed('编辑用例库表单页必填提示信息不正确');
+            return ($nameTipForm == '『名称』不能为空。') ? $this->success('编辑用例库表单页必填提示信息正确') : $this->failed('编辑用例库表单页必填提示信息不正确');
         }
 
         /* 点击查看库概况 */
         $browsePage = $this->loadPage('caselib', 'browse');
-        $browsePage->dom->btn($this->lang->caselib->view)->click();
+        $browsePage->dom->viewBtn->click();
         $browsePage->wait(1);
 
         //断言检查用例库名称是否正确
