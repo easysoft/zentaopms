0d9e66c0a246812ec7c0555d00b00a9963f0faa4
lihuifang <lihuifang@easycorp.ltd>
2025-06-10 10:49:12 +0800

    *[task#144962,doing,0.1h,0.6h] Add element.

--- module/caselib/test/ui/page/browse.php
+++ module/caselib/test/ui/page/browse.php
@@ -7,7 +7,9 @@ class browsePage extends page
         $xpath = array(
             'viewName'     => "//*[@class='modal-content']/div[2]/div/div/div/span[2]",
             'viewLibModal' => "//*[@id='actionBar']/a[1]",
-            'editBtn'      => "//*[@id='viewLibModal']/div/div/div[2]/div[2]/div/div[3]/div/a[1]"
+            'editBtn'      => "//*[@id='viewLibModal']/div/div/div[2]/div[2]/div/div[3]/div/a[1]",
+            'viewBtn'      => "//*[@id='actionBar']/a[1]",
+            'saveBtn'      => "//*[@id='zin_caselib_edit_form']/div[3]/div/button"
         );
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
     }
