3e7e31dbce82001c10e5f5c460eaf76f8a5afd84
yulujie <yulujie@easycorp.ltd>
2025-06-10 13:19:30 +0800

    *[task#144987,doing,0.5h,1.5h] Fix bug.

--- module/tree/test/ui/browsetask.php
+++ module/tree/test/ui/browsetask.php
@@ -28,6 +28,9 @@ $project->acl->range('open');
 $project->status->range('doing');
 $project->gen(4);
 
+$projectproduct = zenData('projectproduct');
+$projectproduct->gen(0);
+
 $module = zenData('module');
 $module->id->range('1-100');
 $module->root->range('3');
@@ -41,17 +44,17 @@ $module->gen(4);
 $tester = new browsetaskTester();
 $tester->login();
 
-r($tester->createModule('模块 1'))             && p('status,message') && e('SUCCESS,创建模块时模块名包含空格空，提示正确');
+r($tester->createModule('     '))              && p('status,message') && e('SUCCESS,创建模块时模块名包含空格，提示正确');
 r($tester->createModule('模块1'))              && p('status,message') && e('SUCCESS,创建模块成功');
 r($tester->createModule('模块1', true))        && p('status,message') && e('SUCCESS,创建模块时模块已存在，提示正确');
 r($tester->createModule('模块2'))              && p('status,message') && e('SUCCESS,创建模块成功');
-r($tester->createChildModule('子模块 1'))      && p('status,message') && e('SUCCESS,创建子模块时子模块名包含空格，提示正确');
+r($tester->createChildModule(' '))             && p('status,message') && e('SUCCESS,创建子模块时子模块名包含空格，提示正确');
 r($tester->createChildModule('子模块1'))       && p('status,message') && e('SUCCESS,创建子模块成功');
 r($tester->createChildModule('模块2'))         && p('status,message') && e('SUCCESS,创建子模块成功');
 r($tester->createChildModule('子模块1', true)) && p('status,message') && e('SUCCESS,创建子模块时子模块已存在，提示正确');
 
 r($tester->editModule(''))          && p('status,message') && e('SUCCESS,编辑模块时模块为空，提示正确');
-r($tester->editModule('模块 2'))    && p('status,message') && e('SUCCESS,编辑模块时模块名包含空格，提示正确');
+r($tester->editModule('  '))        && p('status,message') && e('SUCCESS,编辑模块时模块名包含空格，提示正确');
 r($tester->editModule('模块2'))     && p('status,message') && e('SUCCESS,编辑模块时模块已存在，提示正确');
 r($tester->editModule('编辑模块1')) && p('status,message') && e('SUCCESS,编辑模块成功');
 
