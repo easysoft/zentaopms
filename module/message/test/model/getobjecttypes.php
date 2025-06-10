769e1bf7e277fabcf5c0b0e16d6ec200f7855f93
wangyidong <yidong@chandao.com>
2025-06-10 10:05:05 +0800

    * [unittest] Fix message getobjecttypes method.

--- module/message/test/model/getobjecttypes.php
+++ module/message/test/model/getobjecttypes.php
@@ -15,10 +15,25 @@ title=测试 messageModel->getObjectTypes();
 cid=1
 pid=1
 
+- 查询objectType为product的objectTypes属性product @产品
+- 查询objectType为story的objectTypes属性story @研发需求
+- 查询objectType为productplan的objectTypes属性productplan @计划
+- 查询objectType为project的objectTypes属性project @项目
+- 查询objectType为task的objectTypes属性task @任务
+- 查询objectType为bug的objectTypes属性bug @Bug
+- 查询objectType为case的objectTypes属性case @用例
+- 查询objectType为testcase的objectTypes属性testtask @测试单
+- 查询objectType为todo的objectTypes属性todo @待办
+- 查询objectType为doc的objectTypes属性doc @文档
+- 查询objectTypes的key值 @product,epic,requirement,story,productplan,release,project,execution,task,bug,case,testtask,todo,doc,kanbancard
+- 查询objectTypes的value值 @产品,业务需求,用户需求,研发需求,计划,发布,项目,执行,任务,Bug,用例,测试单,待办,文档,看板卡片
+
 */
 
 global $lang, $app, $conifg;
 $lang->SRCommon = '研发需求';
+$lang->URCommon = '用户需求';
+$lang->ERCommon = '业务需求';
 $app::$loadedLangs = array();
 $app->loadLang('action');
 
@@ -36,5 +51,5 @@ r($objectTypes) && p('testtask')    && e('测试单');   //查询objectType为te
 r($objectTypes) && p('todo')        && e('待办');     //查询objectType为todo的objectTypes
 r($objectTypes) && p('doc')         && e('文档');     //查询objectType为doc的objectTypes
 
-r(implode(',', array_keys($objectTypes)))   && p() && e('product,story,productplan,project,task,bug,case,testtask,todo,doc,kanbancard'); //查询objectTypes的key值
-r(implode(',', array_values($objectTypes))) && p() && e('产品,研发需求,计划,项目,任务,Bug,用例,测试单,待办,文档,看板卡片');          //查询objectTypes的value值
+r(implode(',', array_keys($objectTypes)))   && p() && e('product,epic,requirement,story,productplan,release,project,execution,task,bug,case,testtask,todo,doc,kanbancard'); //查询objectTypes的key值
+r(implode(',', array_values($objectTypes))) && p() && e('产品,业务需求,用户需求,研发需求,计划,发布,项目,执行,任务,Bug,用例,测试单,待办,文档,看板卡片');                     //查询objectTypes的value值
