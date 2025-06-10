b463eec358b07a2591819cb1e9b550bfc7692f98
liumengyi <liumengyi@easycorp.ltd>
2025-06-10 10:09:20 +0800

    + [unittest] add unittest for biModel::processVars.

--- module/bi/test/model/processvars.php
+++ module/bi/test/model/processvars.php
@@ -26,3 +26,25 @@ EOT;
 $sqls[] = <<<EOT
 SELECT t1.name, t1.PM, t1.begin, t1.realBegan, t3.realname FROM zt_project AS t1 LEFT JOIN zt_team AS t2 ON t1.id = t2.root AND t2.type = 'execution' LEFT JOIN zt_user AS t3 ON t2.account = t3.account WHERE t1.name LIKE '%zentaopms%' AND t1.realBegan > '2022-01-01' order by t1.id
 EOT;
+
+$filters = $filter = array();
+$filter[] = array('field' => '', 'default' => '', 'type' => 'date');
+$filter[] = array('field' => '', 'default' => '', 'type' => 'datetime');
+$filter[] = array('field' => '', 'default' => '', 'type' => 'multipleselect');
+$filter[] = array('field' => '', 'default' => array(1,2), 'type' => 'multipleselect');
+$filters[] = $filter;
+
+$filter[] = array('field' => '', 'from' => '', 'default' => '', 'type' => 'date');
+$filter[] = array('field' => '', 'from' => '', 'default' => '', 'type' => 'datetime');
+$filter[] = array('field' => '', 'from' => '', 'default' => '', 'type' => 'multipleselect');
+$filter[] = array('field' => '', 'from' => '', 'default' => array(1,2), 'type' => 'multipleselect');
+$filters[] = $filter;
+
+$filter = array();
+$filter[] = array('field' => '', 'from' => 'query', 'default' => '', 'type' => 'date');
+$filter[] = array('field' => '', 'from' => 'query', 'default' => '', 'type' => 'datetime');
+$filter[] = array('field' => '', 'from' => 'query', 'default' => '', 'type' => 'multipleselect');
+$filter[] = array('field' => '', 'from' => 'query', 'default' => array(1,2), 'type' => 'multipleselect');
+$filters[] = $filter;
+
+$emptyValue = array(true, false);
