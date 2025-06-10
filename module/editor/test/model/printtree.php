9dc138c93c5265dfc80ebcf5df947340e7c08f2f
wangyidong <yidong@chandao.com>
2025-06-10 09:06:16 +0800

    * [unittest] Fix editor printtree method.

--- module/editor/test/model/printtree.php
+++ module/editor/test/model/printtree.php
@@ -7,13 +7,16 @@ su('admin');
 /**
 
 title=测试 editorModel::printTree();
-cid=1
-pid=1
+cid=0
+
+- 检查文件树的title字段。 @待办
+- 检查文件树items字段第一项的title字段 @model
+- 检查文件数items字段第一项的items字段的name字段 @create
 
 */
 
 $editor = new editorTest();
 $tree   = $editor->printTreeTest();
-r($tree[0]->title) && p() && e('todo');                      //检查文件树的title字段。
-r($tree[0]->items[0]->title) && p() && e('model.php');       //检查文件树items字段第一项的title字段
+r($tree[0]->text) && p() && e('待办');                       //检查文件树的title字段。
+r($tree[0]->items[0]->text) && p() && e('model');            //检查文件树items字段第一项的title字段
 r($tree[0]->items[0]->items[0]->name) && p() && e('create'); //检查文件数items字段第一项的items字段的name字段
