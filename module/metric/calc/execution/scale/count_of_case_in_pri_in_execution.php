d29c9661d446c655870ed9aa5fbc19ec98bd1ddc
wangzemei <wangzemei@easycorp.ltd>
2025-06-11 10:21:57 +0800

    + [task#144636] add the count_of_case_in_pri_in_execution func.

--- module/metric/calc/execution/scale/count_of_case_in_pri_in_execution.php
+++ module/metric/calc/execution/scale/count_of_case_in_pri_in_execution.php
@@ -12,4 +12,20 @@ class count_of_case_in_pri_in_execution extends baseCalc
         if(!isset($this->result[$row->pri])) $this->result[$row->pri] = array();
         $this->result[$row->pri][$row->id] = $row->id;
     }
+
+    public function getResult($options = array())
+    {
+        foreach($this->result as $pri => $cases)
+        {
+            if(!is_array($cases))
+            {
+                unset($this->result[$pri]);
+                continue;
+            }
+            $this->result[$pri] = count($cases);
+        }
+
+        $records = $this->getRecords(array('pri', 'value'));
+        return $this->filterByOptions($records, $options);
+    }
 }
