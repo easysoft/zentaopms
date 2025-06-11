386407cb869abc65fc9707c1d51163c42d287feb
liugang <liugang@chandao.com>
2025-06-11 16:43:54 +0800

    * [bug#63289] fix sql error the max key length is too long.

--- db/update21.7.1.sql
+++ db/update21.7.1.sql
@@ -4,6 +4,11 @@ ADD `isTpl` tinyint(1) unsigned NOT NULL DEFAULT '0' AFTER `project`;
 ALTER TABLE `zt_task`
 ADD `isTpl` tinyint(1) unsigned NOT NULL DEFAULT '0' AFTER `isParent`;
 
+ALTER TABLE `zt_oauth` MODIFY `openID` varchar(100) NOT NULL DEFAULT '';
+
+ALTER TABLE `zt_repobranch` DROP INDEX `repo_revision_branch`;
+CREATE UNIQUE INDEX `repo_revision` ON `zt_repobranch`(`repo`, `revision`);
+
 UPDATE `zt_workflowgroup` SET `name` = '瀑布式产品研发' WHERE `code` = 'waterfallproduct';
 UPDATE `zt_workflowgroup` SET `name` = '瀑布式项目研发' WHERE `code` = 'waterfallproject';
 UPDATE `zt_workflowgroup` SET `name` = '敏捷式产品研发' WHERE `code` = 'scrumproduct';
