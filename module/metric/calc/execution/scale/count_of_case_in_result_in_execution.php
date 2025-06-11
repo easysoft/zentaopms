3457db7d1ba072ea8a84b9990dc136730ca83881
wangzemei <wangzemei@easycorp.ltd>
2025-06-11 10:23:17 +0800

    + [task#144636] add the count_of_case_in_result_in_execution func.

--- module/metric/calc/execution/scale/count_of_case_in_result_in_execution.php
+++ module/metric/calc/execution/scale/count_of_case_in_result_in_execution.php
@@ -12,4 +12,20 @@ class count_of_case_in_result_in_execution extends baseCalc
         if(!isset($this->result[$row->lastRunResult])) $this->result[$row->lastRunResult] = array();
         $this->result[$row->lastRunResult][$row->id] = $row->id;
     }
+
+    public function getResult($options = array())
+    {
+        foreach($this->result as $lastRunResult => $cases)
+        {
+            if(!is_array($cases))
+            {
+                unset($this->result[$lastRunResult]);
+                continue;
+            }
+            $this->result[$lastRunResult] = count($cases);
+        }
+
+        $records = $this->getRecords(array('result', 'value'));
+        return $this->filterByOptions($records, $options);
+    }
 }
