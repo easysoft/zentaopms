ff5c821afba10ba719e2020bc3d55fd582695038
zhangjialiang <zhangjialiang@chandao.com>
2025-06-11 15:39:54 +0800

    * [story#75371,doing,0.1h] Add official website index page, add get sms captcha js.

--- module/officialwebsite/js/index.ui.js
+++ module/officialwebsite/js/index.ui.js
@@ -16,3 +16,50 @@ function getCaptchaContent($ele)
         if(response.result == 'success') $ele.html(response.captchaContent);
     });
 }
+
+$('#captcha-btn').on('click', function(e)
+{
+    $('.captcha-mobile-sender').show();
+});
+
+$('#checkMobileSender').on('click', function(e)
+{
+    e.preventDefault();
+    $('#captchaImageError').html('');
+    $('#captchaMobileError').html('');
+    $.post($.createLink('officialwebsite', 'sendcode'),{mobile:$('#mobile-captcha').val(), captchaContent: $('#captchaImage').val()},function(response)
+    {
+        response = JSON.parse(response);
+        if(response.result == 'success')
+        {
+            countdown = 60;
+            setSmsSenderTime();
+            $('.captcha-mobile-sender').hide();
+            $('#captchaMobileError').html(response.message);
+        }
+        else
+        {
+            $('#captchaImageError').html(response.message);
+            if(response.captchaContent) $('.captch-box .image-box').html(response.captchaContent);
+        }
+    });
+});
+
+function setSmsSenderTime()
+{
+    $('#captcha-btn').html(countdown + 's').off('click').removeClass('captcha-btn-class');
+    smsSenderTimer = setInterval(function(){
+        countdown -= 1;
+        if(countdown > 0){
+            $('#captcha-btn').html(countdown + 's').off('click').removeClass('captcha-btn-class');
+        }else{
+            window.clearInterval(smsSenderTimer);
+            $('#captchaMobileError').html('');
+            $('#captcha-btn').html('重新发送').on('click', function(e)
+            {
+                $('#captchaMobileError').html('');
+                $('.captcha-mobile-sender').show();
+            }).addClass('captcha-btn-class');
+        }
+    },1000);
+}
