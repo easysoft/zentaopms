be2326cd5cc39d5d40adfe5313adec2439f5d02d
zhangjialiang <zhangjialiang@chandao.com>
2025-06-11 09:05:59 +0800

    * [story#75371,doing,0.2h] Add official website module.

---
+++ module/officialwebsite/control.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The control file of officialwebsite currentModule of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.cnezsoft.com)
+ * @license     ZPL(http://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
+ * @package     officialwebsite
+ * @version     $Id: control.php 4297 2013-01-27 07:51:45Z wwccss $
+ * @link        https://www.zentao.net
+ */
+
+class officialwebsite extends control
+{
+    /**
+     * 加入社区。
+     * Join community.
+     *
+     * @access public
+     * @return void
+     */
+    public function index()
+    {
+        $this->display();
+    }
+}
---
+++ module/officialwebsite/model.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The model file of officialwebsite module of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.cnezsoft.com)
+ * @license     ZPL(http://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
+ * @package     officialwebsite
+ * @version     $Id: model.php 5006 2013-07-03 08:52:21Z wyd621@gmail.com $
+ * @link        https://www.zentao.net
+ */
+
+class officialwebsiteModel extends model
+{
+
+}
