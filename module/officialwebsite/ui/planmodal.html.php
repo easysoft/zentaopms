acb59ddf49a472cc95149328ba03e40b060cb8e3
zhangjialiang <zhangjialiang@chandao.com>
2025-06-11 17:13:41 +0800

    * [story#75371,doing,0.5h] Add user Experience Plan Pop up.

---
+++ module/officialwebsite/ui/planmodal.html.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+/**
+ * The create view file of group module of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Yanyi Cao<caoyanyi@easycorp.ltd>
+ * @package     group
+ * @link        https://www.zentao.net
+ */
+
+namespace zin;
+
+modalHeader(set::title('用户体验计划'));
+panel
+(
+    '用户体验计划详情'
+);
