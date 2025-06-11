8ad30e27650a0e5b7f116aa8610f63faf3beeb0b
wangyidong <yidong@chandao.com>
2025-06-11 09:43:40 +0800

    * [unittest] Fix weekly gettasksofnextweek model method test.

--- module/weekly/test/model/gettasksofnextweek.php
+++ module/weekly/test/model/gettasksofnextweek.php
@@ -8,24 +8,32 @@ su('admin');
 
 title=测试 weeklyModel->getTasksOfNextWeek();
 cid=1
-pid=1
 
-测试project值为0，date值为2022-07-30 >> 子任务6
-测试project值为0，date值为空 >> 0
-测试project值为1，date值为2022-07-30 >> 0
-测试project值为1，date值为空 >> 0
-测试project值为11，date值为2022-07-30 >> 开发任务371
-测试project值为11，date值为空 >> 0
+- 测试project值为0，date值为2022-07-30第48条的name属性 @开发任务58
+- 测试project值为0，date值为空 @0
+- 测试project值为1，date值为2022-07-30第48条的name属性 @开发任务58
+- 测试project值为1，date值为空 @0
+- 测试project值为11，date值为2022-07-30第32条的name属性 @开发任务42
+- 测试project值为11，date值为空 @0
 
 */
+$date = date('Y-m-d', time() + 5 * 24 * 3600);
+$project = zenData('project');
+$project->type->range('stage');
+$project->project->range('0,1,11');
+$project->gen(20);
+$task = zenData('task');
+$task->execution->range('1-20');
+$task->deadline->range("`2022-07-25`,`2022-07-31`,`{$date}`,`2022-08-02`");
+$task->gen(50);
 
 $projectList = array(0, 1, 11);
 $dateList    = array('2022-07-30', '');
 
 $weekly = new weeklyTest();
-r($weekly->getTasksOfNextWeekTest($projectList[0], $dateList[0])) && p('906:name') && e('子任务6');     //测试project值为0，date值为2022-07-30
-r($weekly->getTasksOfNextWeekTest($projectList[0], $dateList[1])) && p()           && e('0');          //测试project值为0，date值为空
-r($weekly->getTasksOfNextWeekTest($projectList[1], $dateList[0])) && p()           && e('0');          //测试project值为1，date值为2022-07-30
-r($weekly->getTasksOfNextWeekTest($projectList[1], $dateList[1])) && p()           && e('0');          //测试project值为1，date值为空
-r($weekly->getTasksOfNextWeekTest($projectList[2], $dateList[0])) && p('361:name') && e('开发任务371'); //测试project值为11，date值为2022-07-30
-r($weekly->getTasksOfNextWeekTest($projectList[2], $dateList[1])) && p()           && e('0');          //测试project值为11，date值为空
+r($weekly->getTasksOfNextWeekTest($projectList[0], $dateList[0])) && p('48:name') && e('开发任务58'); //测试project值为0，date值为2022-07-30
+r($weekly->getTasksOfNextWeekTest($projectList[0], $dateList[1])) && p()          && e('0');          //测试project值为0，date值为空
+r($weekly->getTasksOfNextWeekTest($projectList[1], $dateList[0])) && p('48:name') && e('开发任务58'); //测试project值为1，date值为2022-07-30
+r($weekly->getTasksOfNextWeekTest($projectList[1], $dateList[1])) && p()          && e('0');          //测试project值为1，date值为空
+r($weekly->getTasksOfNextWeekTest($projectList[2], $dateList[0])) && p('32:name') && e('开发任务42'); //测试project值为11，date值为2022-07-30
+r($weekly->getTasksOfNextWeekTest($projectList[2], $dateList[1])) && p()          && e('0');          //测试project值为11，date值为空
