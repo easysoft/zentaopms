38393a0378772bd5ff12212bb06c945c393917c7
wangzemei <wangzemei@easycorp.ltd>
2025-06-11 10:05:32 +0800

    + [task#144636] add the count_of_case_in_status_in_execution func.

--- module/metric/calc/execution/scale/count_of_case_in_status_in_execution.php
+++ module/metric/calc/execution/scale/count_of_case_in_status_in_execution.php
@@ -12,4 +12,17 @@ class count_of_case_in_status_in_execution extends baseCalc
         if(!isset($this->result[$row->status])) $this->result[$row->status] = array();
         $this->result[$row->status][$row->id] = $row->id;
     }
+
+    public function getResult($options = array())
+    {
+        foreach($this->result as $status => $cases)
+        {
+            if(!is_array($cases)) continue;
+
+            $this->result[$status] = count($cases);
+        }
+
+        $records = $this->getRecords(array('status', 'value'));
+        return $this->filterByOptions($records, $options);
+    }
 }
