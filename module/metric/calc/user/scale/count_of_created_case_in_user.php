6ce63c1bb75e479c2b4a5aaa1a0d7cc709ef7351
wangzemei <wangzemei@easycorp.ltd>
2025-06-11 09:54:56 +0800

    + [task#144633] add the commnets for count_of_created_case_in_user function.

--- module/metric/calc/user/scale/count_of_created_case_in_user.php
+++ module/metric/calc/user/scale/count_of_created_case_in_user.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * 按人员统计的创建用例数。
+ * Count of created case in user.
+ *
+ * 范围：user
+ * 对象：case
+ * 目的：scale
+ * 度量名称：按人员统计的创建用例数
+ * 单位：个
+ * 描述：按人员统计的创建用例数是指每个人创建修复用例总量。该度量项可以帮助我们了解每个人对已解决的用例进行确认与关闭的速度和效率。
+ * 定义：截止当前时间;统计每个人创建用例数的求和;过滤已删除的用例;过滤已删除的产品;
+ * 度量库：
+ * 收集方式：realtime
+ *
+ */
 class count_of_created_case_in_user extends baseCalc
 {
     public $dataset = '';
