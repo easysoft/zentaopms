17fe44d55737d8523a4259b4eee6ab4c049249fe
sunguangming <sunguangming@chandao.com>
2025-06-11 05:36:06 +0000

    * [unittest] modify unittest.

--- module/action/test/tao/getundeleteparamsbyobjecttype.php
+++ module/action/test/tao/getundeleteparamsbyobjecttype.php
@@ -8,11 +8,11 @@ title=测试 actionTao->getUndeleteParamsByObjectType().
 timeout=0
 cid=1
 
-- 执行actionTest模块的getUndeleteParamsByObjectType方法，参数是'project'  @`zt_project`
-- 执行actionTest模块的getUndeleteParamsByObjectType方法，参数是'project' 属性3 @id
-- 执行actionTest模块的getUndeleteParamsByObjectType方法，参数是'product' 属性2 @id, name, code, acl
-- 执行actionTest模块的getUndeleteParamsByObjectType方法，参数是'doc' 属性1 @version desc
-- 执行actionTest模块的getUndeleteParamsByObjectType方法，参数是''  @~~
+- 查看通过项目获取的字段 @`zt_project`
+- 查看通过项目获取的字段属性3 @id
+- 查看通过产品获取的字段属性2 @id, name, code, acl
+- 查看通过产品获取的字段属性3 @id
+- 查看通过文档获取的字段属性1 @version desc
 
 */
 
@@ -25,8 +25,8 @@ zenData('doc')->gen(5);
 
 $actionTest = new actionTest();
 
-r($actionTest->getUndeleteParamsByObjectType('project')) && p('0')      && e('`zt_project`');
-r($actionTest->getUndeleteParamsByObjectType('project')) && p('3')      && e('id');
-r($actionTest->getUndeleteParamsByObjectType('product')) && p('2', '|') && e('id, name, code, acl');
-r($actionTest->getUndeleteParamsByObjectType('doc'))     && p('1')      && e('version desc');
-r($actionTest->getUndeleteParamsByObjectType(''))        && p('0')      && e('~~');
+r($actionTest->getUndeleteParamsByObjectType('project')) && p('0')      && e('`zt_project`');         // 查看通过项目获取的字段
+r($actionTest->getUndeleteParamsByObjectType('project')) && p('3')      && e('id');                   // 查看通过项目获取的字段
+r($actionTest->getUndeleteParamsByObjectType('product')) && p('2', '|') && e('id, name, code, acl');  // 查看通过产品获取的字段
+r($actionTest->getUndeleteParamsByObjectType('product')) && p('3')      && e('id');                   // 查看通过产品获取的字段
+r($actionTest->getUndeleteParamsByObjectType('doc'))     && p('1')      && e('version desc');         // 查看通过文档获取的字段
