a095b8203e9ab29a43c9a28afaae93b21194b3ba
wangzemei <wangzemei@easycorp.ltd>
2025-06-11 10:24:11 +0800

    + [task#144636] add the count_of_case_in_type_in_execution func.

--- module/metric/calc/execution/scale/count_of_case_in_type_in_execution.php
+++ module/metric/calc/execution/scale/count_of_case_in_type_in_execution.php
@@ -12,4 +12,20 @@ class count_of_case_in_type_in_execution extends baseCalc
         if(!isset($this->result[$row->type])) $this->result[$row->type] = array();
         $this->result[$row->type][$row->id] = $row->id;
     }
+
+    public function getResult($options = array())
+    {
+        foreach($this->result as $type => $cases)
+        {
+            if(!is_array($cases))
+            {
+                unset($this->result[$type]);
+                continue;
+            }
+            $this->result[$type] = count($cases);
+        }
+
+        $records = $this->getRecords(array('type', 'value'));
+        return $this->filterByOptions($records, $options);
+    }
 }
