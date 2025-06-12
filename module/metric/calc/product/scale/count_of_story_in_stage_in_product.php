5bbf7d731387cc055ec4927050bdfbb07de13e42
wangzemei <wangzemei@easycorp.ltd>
2025-06-12 15:45:12 +0800

    + [task#144596] add the comments for count_of_story_in_stage_in_product metric.

--- module/metric/calc/product/scale/count_of_story_in_stage_in_product.php
+++ module/metric/calc/product/scale/count_of_story_in_stage_in_product.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * 按阶段统计的需求数。
+ * Count of story in stage
+ *
+ * 范围：stage
+ * 对象：story
+ * 目的：scale
+ * 度量名称：按阶段统计的需求数
+ * 单位：个
+ * 描述：按阶段统计的需求数
+ * 定义：按阶段统计的需求数
+ *
+ * @copyright Copyright 2009-2025 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @author    Zemei Wang <wangzemei@easycorp.ltd>
+ * @package
+ * @uses      func
+ * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @Link      https://www.zentao.net
+ */
 class count_of_story_in_stage_in_product extends baseCalc
 {
     public $dataset = '';
