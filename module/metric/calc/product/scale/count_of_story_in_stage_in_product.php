e6f3ee5f1805cf3611e31d7e8a9a572db5691fcb
wangzemei <wangzemei@easycorp.ltd>
2025-06-12 15:44:52 +0800

    + [task#144596] add the count_of_story_in_stage_in_product metric.

--- module/metric/calc/product/scale/count_of_story_in_stage_in_product.php
+++ module/metric/calc/product/scale/count_of_story_in_stage_in_product.php
@@ -12,4 +12,20 @@ class count_of_story_in_stage_in_product extends baseCalc
         if(!isset($this->result[$row->stage])) $this->result[$row->stage] = array();
         $this->result[$row->stage][$row->id] = $row->id;
     }
+
+    public function getResult($options = array())
+    {
+        foreach($this->result as $stage => $stories)
+        {
+            if(!is_array($stories))
+            {
+                unset($this->result[$stage]);
+                continue;
+            }
+            $this->result[$stage] = count($stories);
+        }
+
+        $records = $this->getRecords(array('stage', 'value'));
+        return $this->filterByOptions($records, $options);
+    }
 }
