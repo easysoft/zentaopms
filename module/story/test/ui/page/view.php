7c174eaba9080627df93aef8fa68c91e76b7caf2
lijie <lijie@chandao.com>
2025-06-12 10:59:40 +0800

    [task#144951, 2h] modify duplicate xpath in ui-case.

--- module/story/test/ui/page/view.php
+++ module/story/test/ui/page/view.php
@@ -17,7 +17,7 @@ class viewPage extends page
             'assignToBtn'      => "//*[@type='submit']",
             /*提交评审弹窗*/
             'submitReviewSave' => "//*[@type='submit']",
-            'subReviewer'      => "//*[@id='reviewer']",
+            'subReviewer'      => "//*[@id='reviewer[]']",
             /*关联的业需和用需*/
             'firLinkStories'   => "//*[@id='mainContainer']/div/div/div[2]/div[2]/div[2]/div[2]/div/ul/li[2]/div/div",
             'secLinkStories'   => "//*[@id='mainContainer']/div/div/div[2]/div[2]/div[2]/div[2]/div/ul/li[4]/div/div",
