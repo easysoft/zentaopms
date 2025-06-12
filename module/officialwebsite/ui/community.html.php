6f2f91dd9e733522d6f108f29fea9bca2928282c
zhangjialiang <zhangjialiang@chandao.com>
2025-06-12 13:59:24 +0800

    * [story#75371,doing,0.2h] Bound page.

--- module/officialwebsite/ui/community.html.php
+++ module/officialwebsite/ui/community.html.php
@@ -12,8 +12,45 @@ declare(strict_types=1);
 
 namespace zin;
 
+set::zui(true);
+
 modalHeader(set::title('加入禅道社区'));
+
+$checked = $agreeUX ? 'checked' : '';
+
 panel
 (
-
+    setID('main'),
+    setClass('flex justify-center'),
+    div
+    (
+        setID('mainContent'),
+        setClass('px-1 mt-2 w-full max-w-7xl'),
+        div(
+            setClass('max-w-7xl h-40'),
+            '禅道官网 ',
+            br(),
+            span(
+                $bindCommunityMobile
+            ),
+            div(
+                html("<button class='btn btn-wide btn-primary' id='unBind'>解绑</button>")
+            )
+        ),
+        div(
+            '加入',
+            a
+            (
+                setID('experience-plan-show'),
+                set('data-size', 'sm'),
+                '《用户体验计划》',
+                set::href(createLink('officialwebsite', 'planModal')),
+                set('data-toggle', 'modal')
+            ),
+            '情况',
+        ),
+        div(
+            html("<input type='checkbox' id='agreeUX' " . $checked . "/>")
+        )
+    )
 );
