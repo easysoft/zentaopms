b519f85a9ee142ffb2c5493057648c3059d012ca
caoyanyi <caoyanyi@easycorp.ltd>
2025-06-12 10:11:09 +0800

    * [misc] update release info.

--- module/misc/lang/en.php
+++ module/misc/lang/en.php
@@ -250,6 +250,7 @@ $lang->misc->releaseDate['7.1.stable']  = '2015-03-07';
 $lang->misc->releaseDate['6.3.stable']  = '2014-11-07';
 
 /* Release Detail. */
+$lang->misc->feature->all['21.7.2'][]      = array('title' => 'Optimized the document functionality. Project template functionality is available.', 'desc' => '');
 $lang->misc->feature->all['21.7.1'][]      = array('title' => 'Implement enhanced project management features, including adding a top toolbar to the document editor, configuring single project workflow management, optimizing requirements, and adding deliverable management functionality.', 'desc' => '');
 $lang->misc->feature->all['21.7'][]        = array('title' => 'Now, ZenTao supports bulk editing of parent-child stages across both Waterfall and Waterfall Plus methodologies, enables dependencies between cross-execution tasks, and facilitates batch stage editing within Integrated Product Development (IPD) projects, along with bug fixes.', 'desc' => '');
 $lang->misc->feature->all['21.6.1'][]      = array('title' => 'Documentation Bug Resolution.', 'desc' => '');
