e56c00bf248754c0a6f95503a099e9837dec04e0
liugang <liugang@chandao.com>
2025-06-13 13:37:37 +0800

    * [misc] fix unittest error of the testcaseModel::batchConfirmStoryChange method.

--- module/testcase/test/model/batchconfirmstorychange.php
+++ module/testcase/test/model/batchconfirmstorychange.php
@@ -38,7 +38,7 @@ $tester->dao->update(TABLE_STORY)->set('version')->eq(4)->where('id')->eq(2)->ex
 r($testcase->batchConfirmStoryChangeTest($caseIdList[1])) && p() && e('1');                                    // 批量确认需求变动成功，返回 true。
 r($testcase->objectModel->getByList($caseIdList[1])) && p('1:story,storyVersion;2:story,storyVersion') && e('2,4,2,4'); // 批量确认需求变动后需求 2 版本为 4。
 
-$actions = $testcase->objectModel->dao->select('*')->from(TABLE_ACTION)->orderBy('id_desc')->limit(6)->fetchAll();
+$actions = $testcase->objectModel->dao->select('objectType,objectID,action,extra')->from(TABLE_ACTION)->orderBy('id_desc')->limit(6)->fetchAll();
 r($actions) && p('0:objectType,objectID,action,extra;1:objectType,objectID,action,extra') && e('case,2,confirmed,4,case,1,confirmed,4'); // 批量修改用例类型后记录日志。
 r($actions) && p('2:objectType,objectID,action,extra;3:objectType,objectID,action,extra') && e('case,2,confirmed,3,case,1,confirmed,3'); // 批量修改用例类型后记录日志。
 r($actions) && p('4:objectType,objectID,action,extra;5:objectType,objectID,action,extra') && e('case,2,confirmed,2,case,1,confirmed,2'); // 批量修改用例类型后记录日志。
