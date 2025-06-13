d868d1f681968bf016ed6fef86bccd116c9964f2
xieqiyu <xieqiyu@chandao.com>
2025-06-13 05:54:04 +0000

    * [unittest] Modify the unit test file of docModel::upgradeTemplateLibAndModule.

--- module/doc/test/model/upgradetemplatelibandmodule.php
+++ module/doc/test/model/upgradetemplatelibandmodule.php
@@ -10,15 +10,15 @@ cid=1
 - 更新模板1的字段值
  - 属性lib @1
  - 属性module @1
- - 属性templateType @softwareProductPlan
+ - 属性templateType @PP
 - 更新模板1的字段值
  - 属性lib @1
  - 属性module @2
- - 属性templateType @hardwareProductPlan
+ - 属性templateType @QAP
 - 更新模板1的字段值
  - 属性lib @1
  - 属性module @3
- - 属性templateType @qualityAssurancePlan
+ - 属性templateType @CMP
 - 更新模板4的字段值
  - 属性lib @1
  - 属性module @4
@@ -30,19 +30,19 @@ cid=1
 - 更新模板6的字段值
  - 属性lib @2
  - 属性module @5
- - 属性templateType @projectUserRequirementsSpecifi
+ - 属性templateType @ITP
 - 更新模板7的字段值
  - 属性lib @2
  - 属性module @6
- - 属性templateType @projectSoftwareRequirementsSpe
+ - 属性templateType @ERS
 - 更新模板8的字段值
  - 属性lib @2
  - 属性module @7
- - 属性templateType @projectSummaryDesignSpecificat
+ - 属性templateType @URS
 - 更新模板9的字段值
  - 属性lib @2
  - 属性module @8
- - 属性templateType @projectDetailedDesignSpecifica
+ - 属性templateType @SRS
 - 更新模板10的字段值
  - 属性lib @2
  - 属性module @9
@@ -58,13 +58,13 @@ zenData('user')->gen(5);
 su('admin');
 
 $docTester = new docTest();
-r($docTester->upgradeTemplateLibAndModuleTest(1))  && p('lib,module,templateType') && e('1,1,softwareProductPlan');            // 更新模板1的字段值
-r($docTester->upgradeTemplateLibAndModuleTest(2))  && p('lib,module,templateType') && e('1,2,hardwareProductPlan');            // 更新模板1的字段值
-r($docTester->upgradeTemplateLibAndModuleTest(3))  && p('lib,module,templateType') && e('1,3,qualityAssurancePlan');           // 更新模板1的字段值
-r($docTester->upgradeTemplateLibAndModuleTest(4))  && p('lib,module,templateType') && e('1,4,custom1');                        // 更新模板4的字段值
-r($docTester->upgradeTemplateLibAndModuleTest(5))  && p('lib,module,templateType') && e('1,4,custom1');                        // 更新模板5的字段值
-r($docTester->upgradeTemplateLibAndModuleTest(6))  && p('lib,module,templateType') && e('2,5,projectUserRequirementsSpecifi'); // 更新模板6的字段值
-r($docTester->upgradeTemplateLibAndModuleTest(7))  && p('lib,module,templateType') && e('2,6,projectSoftwareRequirementsSpe'); // 更新模板7的字段值
-r($docTester->upgradeTemplateLibAndModuleTest(8))  && p('lib,module,templateType') && e('2,7,projectSummaryDesignSpecificat'); // 更新模板8的字段值
-r($docTester->upgradeTemplateLibAndModuleTest(9))  && p('lib,module,templateType') && e('2,8,projectDetailedDesignSpecifica'); // 更新模板9的字段值
-r($docTester->upgradeTemplateLibAndModuleTest(10)) && p('lib,module,templateType') && e('2,9,custom2');                        // 更新模板10的字段值
+r($docTester->upgradeTemplateLibAndModuleTest(1))  && p('lib,module,templateType') && e('1,1,PP');      // 更新模板1的字段值
+r($docTester->upgradeTemplateLibAndModuleTest(2))  && p('lib,module,templateType') && e('1,2,QAP');     // 更新模板1的字段值
+r($docTester->upgradeTemplateLibAndModuleTest(3))  && p('lib,module,templateType') && e('1,3,CMP');     // 更新模板1的字段值
+r($docTester->upgradeTemplateLibAndModuleTest(4))  && p('lib,module,templateType') && e('1,4,custom1'); // 更新模板4的字段值
+r($docTester->upgradeTemplateLibAndModuleTest(5))  && p('lib,module,templateType') && e('1,4,custom1'); // 更新模板5的字段值
+r($docTester->upgradeTemplateLibAndModuleTest(6))  && p('lib,module,templateType') && e('2,5,ITP');     // 更新模板6的字段值
+r($docTester->upgradeTemplateLibAndModuleTest(7))  && p('lib,module,templateType') && e('2,6,ERS');     // 更新模板7的字段值
+r($docTester->upgradeTemplateLibAndModuleTest(8))  && p('lib,module,templateType') && e('2,7,URS');     // 更新模板8的字段值
+r($docTester->upgradeTemplateLibAndModuleTest(9))  && p('lib,module,templateType') && e('2,8,SRS');     // 更新模板9的字段值
+r($docTester->upgradeTemplateLibAndModuleTest(10)) && p('lib,module,templateType') && e('2,9,custom2'); // 更新模板10的字段值
