280a293bca00c74b3855a886a4de2ef6a619e97c
wangzemei <wangzemei@easycorp.ltd>
2025-06-17 10:01:36 +0800

    + [task#145330] add the count_of_verified_story_in_execution metric.

--- module/metric/calc/execution/scale/count_of_verified_story_in_execution.php
+++ module/metric/calc/execution/scale/count_of_verified_story_in_execution.php
@@ -7,6 +7,18 @@ class count_of_verified_story_in_execution extends baseCalc
 
     public $result = array();
 
+    public function calculate($row)
+    {
+        $execution    = $row->project;
+        $stage        = $row->stage;
+        $closedReason = $row->closedReason;
+
+        if(!isset($this->result[$execution])) $this->result[$execution] = 0;
+
+        if(in_array($stage, array('verified', 'delivering', 'delivered', 'released'))) $this->result[$execution] += 1;
+        if($closedReason == 'done') $this->result[$execution] += 1;
+    }
+
     public function getResult($options = array())
     {
         $records = $this->getRecords(array('execution', 'value'));
