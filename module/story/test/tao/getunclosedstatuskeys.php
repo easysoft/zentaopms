e3f72e602bd2c45f3c6685dde4d28e83a61829be
xieqiyu <xieqiyu@chandao.com>
2025-06-17 15:12:19 +0800

    * [unittest] Modify the unit test file of storyModel::getUnclosedStatusKeys.

--- module/story/test/tao/getunclosedstatuskeys.php
+++ module/story/test/tao/getunclosedstatuskeys.php
@@ -6,7 +6,11 @@
 title=测试 storyModel->getUnclosedStatusKeys();
 cid=0
 
-- 执行storyModel模块的getUnclosedStatusKeys方法  @|draft|reviewing|active|changing
+- 获取草稿状态属性1 @draft
+- 获取评审中状态属性2 @reviewing
+- 获取激活状态属性3 @active
+- 获取变更中状态属性4 @changing
+- 获取空值 @~~
 
 */
 include dirname(__FILE__, 5) . "/test/lib/init.php";
@@ -14,4 +18,8 @@ include dirname(__FILE__, 5) . "/test/lib/init.php";
 global $tester;
 $storyModel = $tester->loadModel('story');
 
-r(implode('|', $storyModel->getUnclosedStatusKeys())) && p() && e('|draft|reviewing|active|changing');
+r($storyModel->getUnclosedStatusKeys()) && p('1') && e('draft');     //获取草稿状态
+r($storyModel->getUnclosedStatusKeys()) && p('2') && e('reviewing'); //获取评审中状态
+r($storyModel->getUnclosedStatusKeys()) && p('3') && e('active');    //获取激活状态
+r($storyModel->getUnclosedStatusKeys()) && p('4') && e('changing');  //获取变更中状态
+r($storyModel->getUnclosedStatusKeys()) && p('0') && e('~~');        //获取空值
