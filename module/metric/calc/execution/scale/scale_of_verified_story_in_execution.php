af1cee1c94b262872172b778ca30c1e5af89756b
wangzemei <wangzemei@easycorp.ltd>
2025-06-17 10:03:07 +0800

    + [task#145330] add the scale_of_verified_story_in_execution metric.

--- module/metric/calc/execution/scale/scale_of_verified_story_in_execution.php
+++ module/metric/calc/execution/scale/scale_of_verified_story_in_execution.php
@@ -7,6 +7,15 @@ class scale_of_verified_story_in_execution extends baseCalc
 
      public $result = 0;
 
+    public function calculate($row)
+    {
+        if($row->isParent == '1') return false;
+        if(empty($row->estimate)) return null;
+
+        if(in_array($row->stage, array('verified', 'delivering', 'delivered', 'released'))) $this->result += $row->estimate;
+        if($row->closedReason == 'done') $this->result += $row->estimate;
+    }
+
     public function getResult($options = array())
     {
         $records = $this->getRecords(array('value'));
