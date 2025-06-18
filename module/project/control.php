35fb9ed9873a56970444b167aee9955e87f73057
wangyidong <yidong@chandao.com>
2025-06-18 14:47:02 +0800

    * [perf story #75385] Adjust project dynamic page.

--- module/project/control.php
+++ module/project/control.php
@@ -768,9 +768,9 @@ class project extends control
 
         $period     = $type == 'account' ? 'all'  : $type;
         $date       = empty($date) ? '' : date('Y-m-d', $date);
-        $actions    = $this->loadModel('action')->getDynamic($account, $period, $orderBy, 50, 'all', $projectID, 'all', $date, $direction);
+        $actions    = $this->loadModel('action')->getDynamicByProject($projectID, $account, $period, $orderBy, 50, $date, $direction);
         $dateGroups = $this->action->buildDateGroup($actions, $direction);
-        if(empty($recTotal)) $recTotal = count($dateGroups) < 2 ? count($dateGroups, 1) - count($dateGroups) : $this->action->getDynamicCount();
+        if(empty($recTotal) && $dateGroups) $recTotal = count($dateGroups) < 2 ? count($dateGroups, 1) - count($dateGroups) : $this->action->getDynamicCount();
 
         /* The header and position. */
         $project = $this->project->getByID($projectID);
