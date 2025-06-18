5535141a3d6dde68f2bada136e9c853e787b47a5
qixinzhi <qixinzhi@easycorp.ltd>
2025-06-18 09:12:50 +0000

    * [unittest] fix gensheet.

--- module/pivot/test/model/gensheet.php
+++ module/pivot/test/model/gensheet.php
@@ -40,15 +40,16 @@ $pivotIDList = array(1002, 1000, 1001, 1025);
 $pivot = $pivotTest->getByID($pivotIDList[0]);
 
 list($sql, $filterFormat) = $pivotTest->getFilterFormat($pivot->sql, $pivot->filters);
+
 $fields = json_decode(json_encode($pivot->fieldSettings), true);
 $langs  = json_decode($pivot->langs, true) ?? array();
 
 list($data, $configs) = $pivotTest->genSheet($fields, $pivot->settings, $sql, $filterFormat, $langs);
 
-r($data->groups) && p('0,1,2') && e('一级项目集,产品线,产品');                  //查看id为1002的透视表生成的分组信息是否正确。
-r($data->columnTotal) && p('') && e('noShow');                                  //查看id为1002的透视表是否显示列总计。
-r(count($data->array)) && p('') && e('10');                                     //查看id为1002的透视表生成的列数量是否正确
-r(array_keys($data->array[0])) && p('0,13') && e('一级项目集,Bug修复率10');     //查看id为1002的透视表生成的列名称是否正确。
+r($data->groups) && p('0,1,2') && e('一级项目集,产品线,产品');              //查看id为1002的透视表生成的分组信息是否正确。
+r($data->showAllTotal) && p('') && e('0');                                 //查看id为1002的透视表是否显示列总计。
+r(count($data->array)) && p('') && e('10');                                //查看id为1002的透视表生成的列数量是否正确
+r(array_keys($data->array[0])) && p('0,13') && e('一级项目集,Bug修复率10'); //查看id为1002的透视表生成的列名称是否正确。
 
 $pivot = $pivotTest->getByID($pivotIDList[1]);
 
@@ -57,10 +58,10 @@ $fields = json_decode(json_encode($pivot->fieldSettings), true);
 $langs  = json_decode($pivot->langs, true) ?? array();
 
 list($data, $configs) = $pivotTest->genSheet($fields, $pivot->settings, $sql, $filterFormat, $langs);
-r($data->groups) && p('0,1') && e('program1,name');                                                     //查看id为1000的透视表生成的分组信息是否正确。
-r($data->cols[0]) && p('0:name,label;9:name,label') && e('program1,一级项目集;rate,工期偏差率的求和');  //当lang存在时，label选用lang中对应的值。
-r($data->array) && p('0:name,rate7;9:name,rate7') && e('项目11,-1;项目20,-1');                  //查看id为1000的透视表生成的列数据是否正确。
-r($configs) && p('0:0,1;1:1') && e('10,1;1');                                                           //查看id为1000的透视表生成的合并单元格数据是否正确。
+r($data->groups) && p('0,1') && e('program1,name');                                                  //查看id为1000的透视表生成的分组信息是否正确。
+r($data->cols[0]) && p('0:name,label;9:name,label') && e('program1,一级项目集;rate,工期偏差率的求和'); //当lang存在时，label选用lang中对应的值。
+r($data->array) && p('0:name,rate7;9:name,rate7') && e('项目11,-1;项目20,-1');                        //查看id为1000的透视表生成的列数据是否正确。
+r($configs) && p('0:0,1;1:1') && e('10,1;1');                                                        //查看id为1000的透视表生成的合并单元格数据是否正确。
 
 $pivot = $pivotTest->getByID($pivotIDList[2]);
 
@@ -68,8 +69,8 @@ list($sql, $filterFormat) = $pivotTest->getFilterFormat($pivot->sql, $pivot->fil
 $fields = json_decode(json_encode($pivot->fieldSettings), true);
 $langs  = json_decode($pivot->langs, true) ?? array();
 
-list($data, $configs) = $pivotTest->genSheet($fields, $pivot->settings, $sql, $filterFormat, $langs);
-r($data->groups) && p('0,1') && e('一级项目集,项目名称');                                                                               //查看id为1001的透视表生成的分组信息是否正确。
-r($data->cols[0]) && p('0:name,label;8:name,label') && e('一级项目集,一级项目集;单位时间交付需求规模数,单位时间交付需求规模数的求和');  //查看id为1001的透视表生成的标签是否正确。
-r($data->array) && p('0:项目名称,消耗工时1;9:项目名称,消耗工时1') && e('项目11,3;项目20,12');                                           //查看id为1001的透视表生成的列数据是否正确。
-r($configs) && p('0:0,1') && e('10,1');                                                                                        //查看id为1001的透视表生成的合并单元格数据是否正确。
+list($data, $configs) = $pivotTest->genSheet($fields, $pivot->settings, $sql, false, $langs);
+r($data->groups) && p('0,1') && e('一级项目集,项目名称');                                                                           //查看id为1001的透视表生成的分组信息是否正确。
+r($data->cols[0]) && p('0:name,label;8:name,label') && e('一级项目集,一级项目集;单位时间交付需求规模数,单位时间交付需求规模数的求和'); //查看id为1001的透视表生成的标签是否正确。
+r($data->array) && p('0:项目名称,消耗工时1;9:项目名称,消耗工时1') && e('项目11,3;项目20,12');                                        //查看id为1001的透视表生成的列数据是否正确。
+r($configs) && p('0:0,1') && e('10,1');                                                                                           //查看id为1001的透视表生成的合并单元格数据是否正确。
