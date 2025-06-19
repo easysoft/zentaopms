ec05d451e7fc5a81f32d569b8e5cc4ab2f3d6f3d
xieqiyu <xieqiyu@chandao.com>
2025-06-19 00:10:12 +0000

    * [unittest] Modify the unit test file of docModel::getTemplateType.

--- module/doc/test/model/gettemplatesbytype.php
+++ module/doc/test/model/gettemplatesbytype.php
@@ -10,27 +10,27 @@ cid=1
 - 获取所有模板
  - 第1条的id属性 @1
  - 第1条的title属性 @产品模板1
- - 第1条的templateType属性 @softwareProductPlan
+ - 第1条的templateType属性 @PP
 - 获取所有已发布模板
  - 第2条的id属性 @2
  - 第2条的title属性 @产品模板2
- - 第2条的templateType属性 @hardwareProductPlan
+ - 第2条的templateType属性 @QAP
 - 获取所有草稿模板
  - 第3条的id属性 @3
  - 第3条的title属性 @产品模板3
- - 第3条的templateType属性 @qualityAssurancePlan
+ - 第3条的templateType属性 @CMP
 - 获取类型1下的所有模板
  - 第1条的id属性 @1
  - 第1条的title属性 @产品模板1
- - 第1条的templateType属性 @softwareProductPlan
+ - 第1条的templateType属性 @PP
 - 获取类型1下的已发布模板
  - 第2条的id属性 @2
  - 第2条的title属性 @产品模板2
- - 第2条的templateType属性 @hardwareProductPlan
+ - 第2条的templateType属性 @QAP
 - 获取类型1下的草稿模板
  - 第3条的id属性 @3
  - 第3条的title属性 @产品模板3
- - 第3条的templateType属性 @qualityAssurancePlan
+ - 第3条的templateType属性 @CMP
 
 */
 
@@ -46,9 +46,9 @@ $types  = array(null, 1);
 $status = array('all', 'normal', 'draft');
 
 $docTester = new docTest();
-r($docTester->getTemplatesByTypeTest($types[0], $status[0])) && p('1:id,title,templateType') && e('1,产品模板1,softwareProductPlan');  // 获取所有模板
-r($docTester->getTemplatesByTypeTest($types[0], $status[1])) && p('2:id,title,templateType') && e('2,产品模板2,hardwareProductPlan');  // 获取所有已发布模板
-r($docTester->getTemplatesByTypeTest($types[0], $status[2])) && p('3:id,title,templateType') && e('3,产品模板3,qualityAssurancePlan'); // 获取所有草稿模板
-r($docTester->getTemplatesByTypeTest($types[1], $status[0])) && p('1:id,title,templateType') && e('1,产品模板1,softwareProductPlan');  // 获取类型1下的所有模板
-r($docTester->getTemplatesByTypeTest($types[1], $status[1])) && p('2:id,title,templateType') && e('2,产品模板2,hardwareProductPlan');  // 获取类型1下的已发布模板
-r($docTester->getTemplatesByTypeTest($types[1], $status[2])) && p('3:id,title,templateType') && e('3,产品模板3,qualityAssurancePlan'); // 获取类型1下的草稿模板
+r($docTester->getTemplatesByTypeTest($types[0], $status[0])) && p('1:id,title,templateType') && e('1,产品模板1,PP');  // 获取所有模板
+r($docTester->getTemplatesByTypeTest($types[0], $status[1])) && p('2:id,title,templateType') && e('2,产品模板2,QAP'); // 获取所有已发布模板
+r($docTester->getTemplatesByTypeTest($types[0], $status[2])) && p('3:id,title,templateType') && e('3,产品模板3,CMP'); // 获取所有草稿模板
+r($docTester->getTemplatesByTypeTest($types[1], $status[0])) && p('1:id,title,templateType') && e('1,产品模板1,PP');  // 获取类型1下的所有模板
+r($docTester->getTemplatesByTypeTest($types[1], $status[1])) && p('2:id,title,templateType') && e('2,产品模板2,QAP'); // 获取类型1下的已发布模板
+r($docTester->getTemplatesByTypeTest($types[1], $status[2])) && p('3:id,title,templateType') && e('3,产品模板3,CMP'); // 获取类型1下的草稿模板
