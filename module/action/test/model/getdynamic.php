79bea7083beb02ea7994c731f9eb459cf1abf993
wangyidong <yidong@chandao.com>
2025-06-19 13:27:21 +0800

    * [perf story #75387] Adjust for getDynamic unit test.

--- module/action/test/model/getdynamic.php
+++ module/action/test/model/getdynamic.php
@@ -5,6 +5,7 @@ include dirname(__FILE__, 2) . '/lib/action.unittest.class.php';
 
 zenData('action')->loadYaml('action_year')->gen(35);
 zenData('actionrecent')->loadYaml('action_year')->gen(35);
+zenData('actionproduct')->loadYaml('actionproduct')->gen(35);
 zenData('doclib')->loadYaml('doclib')->gen(15);
 zenData('doc')->loadYaml('doc')->gen(5);
 zenData('product')->gen(5);
@@ -18,16 +19,16 @@ title=测试 actionModel->getDynamic();
 timeout=0
 cid=1
 
-- 查找全部动态 @36
-- 查找用户admin动态 @13
+- 查找全部动态 @35
+- 查找用户admin动态 @12
 - 查找用户dev17动态 @12
 - 查找用户test18动态 @11
 - 查找今天的动态 @1
 - 查找昨天的动态 @1
 - 查找上周的动态 @7
-- 查找产品1的动态 @1
-- 查找产品2的动态 @1
-- 查找产品3的动态 @1
+- 查找产品1的动态 @7
+- 查找产品2的动态 @7
+- 查找产品3的动态 @6
 - 查找项目1的动态 @1
 - 查找项目2的动态 @1
 - 查找项目3的动态 @1
@@ -42,9 +43,9 @@ cid=1
 - 查找今天的动态 @5
 - 查找昨天的动态 @0
 - 查找上周的动态 @0
-- 查找产品1的动态 @1
-- 查找产品2的动态 @1
-- 查找产品3的动态 @1
+- 查找产品1的动态 @7
+- 查找产品2的动态 @7
+- 查找产品3的动态 @6
 - 查找项目1的动态 @1
 - 查找项目2的动态 @1
 - 查找项目3的动态 @1
@@ -64,9 +65,11 @@ $dateList        = array('', 'today');
 
 $action = new actionTest();
 
+global $app;
 su('admin');
-r($action->getDynamicTest())                                                                                                       && p() && e('36');  // 查找全部动态
-r($action->getDynamicTest($accountList[1]))                                                                                        && p() && e('13');  // 查找用户admin动态
+$app->user->rights['acls'] = array();
+r($action->getDynamicTest())                                                                                                       && p() && e('35');  // 查找全部动态
+r($action->getDynamicTest($accountList[1]))                                                                                        && p() && e('12');  // 查找用户admin动态
 r($action->getDynamicTest($accountList[2]))                                                                                        && p() && e('12');  // 查找用户dev17动态
 r($action->getDynamicTest($accountList[3]))                                                                                        && p() && e('11');  // 查找用户test18动态
 
@@ -78,20 +81,21 @@ r($action->getDynamicTest($accountList[0], $typeList[3]))                       
 
 zenData('action')->loadYaml('action_year')->gen(35, true, false);
 zenData('actionrecent')->loadYaml('action_year')->gen(35, true, false);
-r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[1]))                                                       && p() && e('1');   // 查找产品1的动态
-r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[2]))                                                       && p() && e('1');   // 查找产品2的动态
-r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[3]))                                                       && p() && e('1');   // 查找产品3的动态
+r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[1]))                                                       && p() && e('7');   // 查找产品1的动态
+r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[2]))                                                       && p() && e('7');   // 查找产品2的动态
+r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[3]))                                                       && p() && e('6');   // 查找产品3的动态
 r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[0], $projectIDList[1]))                                    && p() && e('1');   // 查找项目1的动态
 r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[0], $projectIDList[2]))                                    && p() && e('1');   // 查找项目2的动态
 r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[0], $projectIDList[3]))                                    && p() && e('1');   // 查找项目3的动态
 r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[0], $projectIDList[0], $executionIDList[1]))               && p() && e('0');   // 查找执行1的动态
 r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[0], $projectIDList[0], $executionIDList[2]))               && p() && e('0');   // 查找执行2的动态
 r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[0], $projectIDList[0], $executionIDList[3]))               && p() && e('0');   // 查找执行3的动态
-r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[0], $projectIDList[0], $executionIDList[0], $dateList[1])) && p() && e('0');  // 查找今天的动态
+r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[0], $projectIDList[0], $executionIDList[0], $dateList[1])) && p() && e('0');   // 查找今天的动态
 
 zenData('action')->loadYaml('action_year')->gen(35, true, false);
 zenData('actionrecent')->loadYaml('action_year')->gen(35, true, false);
 su('dev17');
+$app->user->rights['acls'] = array();
 r($action->getDynamicTest())                                                                                                       && p() && e('35');  // 查找全部动态
 r($action->getDynamicTest($accountList[1]))                                                                                        && p() && e('11');  // 查找用户admin动态
 r($action->getDynamicTest($accountList[2]))                                                                                        && p() && e('13');  // 查找用户dev17动态
@@ -102,13 +106,13 @@ zenData('actionrecent')->loadYaml('action_year')->gen(35, true, false);
 r($action->getDynamicTest($accountList[0], $typeList[1]))                                                                          && p() && e('5');   // 查找今天的动态
 r($action->getDynamicTest($accountList[0], $typeList[2]))                                                                          && p() && e('0');   // 查找昨天的动态
 r($action->getDynamicTest($accountList[0], $typeList[3]))                                                                          && p() && e('0');   // 查找上周的动态
-r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[1]))                                                       && p() && e('1');   // 查找产品1的动态
-r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[2]))                                                       && p() && e('1');   // 查找产品2的动态
-r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[3]))                                                       && p() && e('1');   // 查找产品3的动态
+r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[1]))                                                       && p() && e('7');   // 查找产品1的动态
+r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[2]))                                                       && p() && e('7');   // 查找产品2的动态
+r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[3]))                                                       && p() && e('6');   // 查找产品3的动态
 r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[0], $projectIDList[1]))                                    && p() && e('1');   // 查找项目1的动态
 r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[0], $projectIDList[2]))                                    && p() && e('1');   // 查找项目2的动态
 r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[0], $projectIDList[3]))                                    && p() && e('1');   // 查找项目3的动态
 r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[0], $projectIDList[0], $executionIDList[1]))               && p() && e('0');   // 查找执行1的动态
 r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[0], $projectIDList[0], $executionIDList[2]))               && p() && e('0');   // 查找执行2的动态
 r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[0], $projectIDList[0], $executionIDList[3]))               && p() && e('0');   // 查找执行3的动态
-r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[0], $projectIDList[0], $executionIDList[0], $dateList[1])) && p() && e('0');  // 查找今天的动态
+r($action->getDynamicTest($accountList[0], $typeList[0], $productIDList[0], $projectIDList[0], $executionIDList[0], $dateList[1])) && p() && e('0');   // 查找今天的动态
