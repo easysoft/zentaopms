eba1b0117e5cb263fba44921e0014fcc3fe7d6c4
liugang <liugang@chandao.com>
2025-06-20 16:03:42 +0800

    * [misc] decrease length of the branch field and add it into the unique index.

--- db/update21.7.1.sql
+++ db/update21.7.1.sql
@@ -5,8 +5,7 @@ ALTER TABLE `zt_oauth` MODIFY `openID` varchar(100) NOT NULL DEFAULT '';
 ALTER TABLE `zt_doclib` MODIFY `id` mediumint(8) unsigned NOT NULL auto_increment;
 ALTER TABLE `zt_doclib` MODIFY `order` mediumint(8) unsigned NOT NULL DEFAULT '0';
 
-ALTER TABLE `zt_repobranch` DROP INDEX `repo_revision_branch`;
-CREATE UNIQUE INDEX `repo_revision` ON `zt_repobranch`(`repo`, `revision`);
+ALTER TABLE `zt_repobranch` MODIFY `branch` varchar(100) NOT NULL DEFAULT '';
 
 UPDATE `zt_workflowgroup` SET `name` = '瀑布式产品研发' WHERE `code` = 'waterfallproduct';
 UPDATE `zt_workflowgroup` SET `name` = '瀑布式项目研发' WHERE `code` = 'waterfallproject';
