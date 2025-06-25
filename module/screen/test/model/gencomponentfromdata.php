fe3233015dbd819467f7f6b38773fd4932ce5057
wangzemei <wangzemei@easycorp.ltd>
2025-06-25 13:51:22 +0800

    + [unittest] add the unittest for genComponentFromData.

---
+++ module/screen/test/model/gencomponentfromdata.php
@@ -0,0 +1,14 @@
+#!/usr/bin/env php
+<?php
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+
+/**
+
+title=测试 screenModel->genComponentFromData();
+timeout=0
+cid=1
+
+*/
+
+global $tester;
+$tester->loadModel('screen');
