fbc1dd70c005371036a981bbbff2ee617d6236b7
wangzemei <wangzemei@easycorp.ltd>
2025-06-25 14:27:39 +0800

    + [unittest] add the unittest for prepareSunburstDataset.

--- module/screen/test/model/preparesunburstdataset.php
+++ module/screen/test/model/preparesunburstdataset.php
@@ -18,3 +18,15 @@ $component1->option->series = new stdclass();
 
 $component2 = clone $component1;
 $component2->styles = 1;
+
+$component3 = clone $component1;
+$component3->status = 1;
+
+$component4 = clone $component1;
+$component4->request = 1;
+
+r($screen->prepareSunburstDataset($component1, array()))            && p('styles:hueRotate') && e(0); // 测试data属性为空的情况下，生成的默认值是否正确;
+r($screen->prepareSunburstDataset($component1, array(0,1,2,3,4,5))) && p('styles:hueRotate') && e(0); // 测试传入data属性的情况下，生成的值是否正确;
+r($screen->prepareSunburstDataset($component2, array(0,1,2,3,4,5))) && p('styles')           && e(1); // 测试传入data属性，styles有值的情况下，是否被修改。
+r($screen->prepareSunburstDataset($component3, array()))            && p('status')           && e(1); // 测试status有值的情况下，是否被修改。
+r($screen->prepareSunburstDataset($component4, array()))            && p('request')          && e(1); // 测试request有值的情况下，是否被修改。
