5dca73074f49181528672dcf6b5b765f13596e64
wangzemei <wangzemei@easycorp.ltd>
2025-06-25 13:51:35 +0800

    + [unittest] add the unittest for genComponentFromData.

--- module/screen/test/model/gencomponentfromdata.php
+++ module/screen/test/model/gencomponentfromdata.php
@@ -8,6 +8,22 @@ title=测试 screenModel->genComponentFromData();
 timeout=0
 cid=1
 
+- 测试类型是text,标题是Text1生成的组件
+ - 属性title @Text1
+ - 属性type @text
+- 测试类型是text,标题是Text2生成的组件
+ - 属性title @Text2
+ - 属性type @text
+- 测试类型是text,标题是Text3生成的组件
+ - 属性title @Text3
+ - 属性type @text
+- 测试类型是waterpolo,标题是Waterpolo1生成的组件
+ - 属性title @Waterpolo1
+ - 属性type @waterpolo
+- 测试类型是table,标题是Table1生成的组件
+ - 属性title @Table1
+ - 属性type @table
+
 */
 
 global $tester;
