275d05728c7b3348fbcba2454e24a7eab7da4314
liyang <liyang@easycorp.ltd>
2025-06-25 15:04:53 +0800

    + [task#144533] add get backup detial page.

---
+++ module/system/ui/backupview.html.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+/**
+ * The backupview view file of system module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Li Yang<liyang@chandao.com>
+ * @package     system
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+$backupDetail = zget($backup, 'backup_details', array());
+
+detailHeader
+(
+    to::prefix(''),
+    to::title
+    (
+        entityLabel
+        (
+            set::level(1),
+            set::text($title)
+        )
+    )
+);
