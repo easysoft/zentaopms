10f7199a4241fc5765a9835b68af9151c35e6e2b
wangzemei <wangzemei@easycorp.ltd>
2025-06-26 11:08:36 +0800

    + [unittest] add the unittest for buildMetricFilters.

--- module/screen/test/model/buildmetricfilters.php
+++ module/screen/test/model/buildmetricfilters.php
@@ -17,3 +17,12 @@ $screen = new screenTest();
 
 $tester->loadModel('bi');
 $metric1 = $tester->loadModel('metric')->getByID(1);
+$metric1 = array_merge((array)$metric1, $config->bi->builtin->metrics[0]);
+
+r($screen->buildMetricFilters((object)$metric1, false, false)) && p() && e(0);
+r($screen->buildMetricFilters((object)$metric1, false, true))  && p('0:field') && e('date');
+
+$metric1 = array_merge((array)$metric1, $config->bi->builtin->metrics[30]);
+r($screen->buildMetricFilters((object)$metric1, false, true)) && p('0:field') && e('date');
+r($screen->buildMetricFilters((object)$metric1, true, true))  && p('0:field') && e('system');
+r($screen->buildMetricFilters((object)$metric1, true, true))  && p('1:field') && e('date');
