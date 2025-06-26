24604006403602ae5192677a0f47ed7f24ff23a8
wangzemei <wangzemei@easycorp.ltd>
2025-06-26 08:56:50 +0800

    + [unittest] add the unittest for preparegroupdataset.

--- module/screen/test/model/preparegroupdataset.php
+++ module/screen/test/model/preparegroupdataset.php
@@ -16,3 +16,19 @@ $component1 = new stdclass();
 $component1->type      = 'group';
 $component1->isGroup   = false;
 $component1->groupList = array();
+
+$component2 = clone $component1;
+$component2->styles = 1;
+
+$component3 = clone $component1;
+$component3->status = 1;
+
+$attr         = array('w' => 900, 'h' => 300, 'x' => 20, 'y' => 100);
+$waterOptions = array(
+    'title.show'                                 => false,
+    'series.0.outline.show'                      => false,
+    'series.0.label.normal.textStyle.fontSize'   => 13,
+    'series.0.label.normal.textStyle.fontWeight' => 'normal',
+    'series.0.label.normal.textStyle.round'      => 2,
+    'series.0.color.0.type'                      => 'linear',
+);
