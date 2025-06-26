10946a13b8d3fdfd1f4cba9916c706ec437d5a31
wangzemei <wangzemei@easycorp.ltd>
2025-06-26 13:33:02 +0800

    + [unittest] add the unittest comments for prepareCardDataset.

--- module/screen/test/model/preparecarddataset.php
+++ module/screen/test/model/preparecarddataset.php
@@ -18,3 +18,15 @@ $component1->option = new stdclass();
 
 $component2 = clone $component1;
 $component2->styles = 1;
+
+$component3 = clone $component1;
+$component3->status = 1;
+
+$component4 = clone $component1;
+$component4->request = 1;
+
+r($screen->prepareCardDataset($component1, '0')) && p('type')    && e('card'); // 测试value属性为空的情况下，生成的默认值是否正确;
+r($screen->prepareCardDataset($component1, '1')) && p('type')    && e('card'); // 测试传入value属性的情况下，生成的值是否正确;
+r($screen->prepareCardDataset($component2, '1')) && p('styles')  && e(1);      // 测试传入value属性，styles有值的情况下，是否被修改。
+r($screen->prepareCardDataset($component3, '0')) && p('status')  && e(1);      // 测试status有值的情况下，是否被修改。
+r($screen->prepareCardDataset($component4, '0')) && p('request') && e(1);      // 测试request有值的情况下，是否被修改。
