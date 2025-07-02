926800fc8ec4e037fdca836e5241d7b5c1bb6166
huairong <huairong@easycorp.ltd>
2025-07-02 09:39:30 +0800

    + [task#146228,doing,2h,1h] Add function of edit project in lite.

--- module/project/test/lib/editprojectlite.ui.class.php
+++ module/project/test/lib/editprojectlite.ui.class.php
@@ -38,3 +38,23 @@ class editProjectLiteTester extends tester
         /* 检查编辑页面提示信息 */
         $form = $this->loadPage('project', 'edit');
         if($this->response('method') != 'view')
+        {
+            if($form->dom->nameTip)
+            {
+                /* 检查项目名称不能为空 */
+                if($project['name'] == '')
+                {
+                    $nameTipText = $form->dom->nameTip->getText();
+                    $nameTip     = sprintf($this->lang->error->notempty, $this->lang->project->name);
+                    return ($nameTipText == $nameTip) ? $this->success('编辑项目表单页提示信息正确') : $this->failed('编辑项目表单页提示信息不正确');
+                }
+                /* 检查项目名称唯一 */
+                else
+                {
+                    $existName = '运营界面项目2';
+                    $nameTipText = $form->dom->nameTip->getText();
+                    $nameTip     = sprintf($this->lang->error->unique, $this->lang->project->name, $existName);
+                    return ($nameTipText == $nameTip) ? $this->success('编辑项目表单页提示信息正确') : $this->failed('编辑项目表单页提示信息不正确');
+                }
+            }
+            if($form->dom->endTip)
