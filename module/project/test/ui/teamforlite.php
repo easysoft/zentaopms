d67550fb716fd03dce3962e48768218b21fcb6d6
lihuifang <lihuifang@chandao.com>
2025-07-02 11:13:51 +0800

    *[task#146230,doing,0.2h,0.8h] Add zendata.

--- module/project/test/ui/teamforlite.php
+++ module/project/test/ui/teamforlite.php
@@ -29,3 +29,27 @@ $project->status->range('wait');
 $project->begin->range('(-72w)-(-71w):1D')->type('timestamp')->format('YY/MM/DD');
 $project->end->range('(+72w)-(+73w):1D')->type('timestamp')->format('YY/MM/DD');
 $project->acl->range('open');
+$project->gen(1);
+
+$user = zenData('user');
+$user->id->range('1-5');
+$user->dept->range('1');
+$user->account->range('admin, user1, user2, user3, user4');
+$user->realname->range('admin, 用户1, 用户2, 用户3, 用户4');
+$user->password->range($config->uitest->defaultPassword)->format('md5');
+$user->gen(5);
+
+$team = zenData('team');
+$team->id->range('1-3');
+$team->root->range('1');
+$team->type->range('project');
+$team->account->range('admin, user1, user2, user3, user4');
+$team->days->range('7');
+$team->hours->range('4');
+$team->gen(3);
+
+$tester = new teamTesterForLite();
+$tester->login();
+
+r($tester->removeMembers()) && p('message') && e('项目团队成员移除成功');   //移除项目已有的团队成员
+$tester->closeBrowser();
