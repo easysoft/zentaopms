09dd0039053716e8c4c5581186ed90b5c3879a59
lixiaoyu <lixiaoyu@easycorp.ltd>
2025-07-02 15:58:21 +0800

    *[task#146146,doing,0.2h,4h]comments

--- module/story/test/lib/batchcreateinlite.ui.class.php
+++ module/story/test/lib/batchcreateinlite.ui.class.php
@@ -2,6 +2,14 @@
 include dirname(__FILE__, 5) . '/test/lib/ui.php';
 class batchCreateStory extends tester
 {
+    /**
+     * 运营界面批量创建目标
+     * batch create story in lite
+     * @param array  $project
+     * @param array  $storyUrl
+     * @param object $story
+     * @return mixed
+     */
     public function batchCreateStory($project, $storyUrl, $story)
     {
         $this->switchVision('lite');
@@ -16,4 +24,14 @@ class batchCreateStory extends tester
         $form->wait(2);
         return $this->checkBatchCreate($project, $storyUrl, $story, $form);
     }
+    /**
+     * 检查批量创建目标的结果
+     * Check the result of batch create story
+     *
+     * @param array  $project
+     * @param array  $storyUrl
+     * @param object $story
+     * @param object $form
+     * @return mixed
+     */
 }
