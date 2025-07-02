95834eb772062e7a37c3eb1a29bc0fc7110ddde4
huairong <huairong@easycorp.ltd>
2025-07-02 09:46:48 +0800

    + [task#146228,doing,1h,1h] Add test data and xpath of edit project in lite.

--- module/project/test/ui/editprojectlite.php
+++ module/project/test/ui/editprojectlite.php
@@ -40,3 +40,20 @@ $project->end->range('(+72w)-(+73w):1D')->type('timestamp')->format('YY/MM/DD');
 $project->acl->range('open');
 $project->vision->range('lite');
 $project->gen(2);
+
+$tester = new editProjectLiteTester();
+$tester->login();
+
+$project = array(
+    array('name' => '', 'end' => date('Y-m-d', strtotime('+30 days'))),
+    array('name' => '运营界面项目'.time(), 'begin' => date('Y-m-d'), 'end' => date('Y-m-d', strtotime('-1 day'))),
+    array('name' => '运营界面项目2', 'end' => date('Y-m-d', strtotime('+1 month'))),
+    array('name' => '运营界面项目a'.time(), 'end' => date('Y-m-d', strtotime('+1 month')), 'PM' => 'admin'),
+);
+
+r($tester->checkInput($project['0'])) && p('message,status') && e('编辑项目表单页提示信息正确,SUCCESS'); // 编辑项目缺少项目名称
+r($tester->checkInput($project['1'])) && p('message,status') && e('编辑项目表单页提示信息正确,SUCCESS'); // 编辑项目计划完成时间小于计划开始时间
+r($tester->checkInput($project['2'])) && p('message,status') && e('编辑项目表单页提示信息正确,SUCCESS'); // 编辑项目名称为已有名称
+r($tester->checkInput($project['3'])) && p('message,status') && e('编辑项目成功,SUCCESS');               // 编辑项目名称
+
+$tester->closeBrowser();
