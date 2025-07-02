951063fe62f0565f3c42e5d5aa373b84bc1b35c0
yulujie <yulujie@chandao.com>
2025-07-02 10:29:45 +0800

    *[task#146066,doing,2h,5h] Add data and case.

---
+++ module/execution/test/ui/editinlite.php
@@ -0,0 +1,81 @@
+#!/usr/bin/env php
+<?php
+
+/**
+
+title=运营界面编辑看板
+timeout=0
+cid=1
+ */
+
+chdir(__DIR__);
+include '../lib/editinlite.ui.class.php';
+
+$project = zenData('project');
+$project->id->range('1-100');
+$project->project->range('0, 1{2}');
+$project->model->range('kanban, []{2}');
+$project->type->range('project, kanban{2}');
+$project->auth->range('[]');
+$project->storyType->range('[]');
+$project->parent->range('0, 1{2}');
+$project->path->range('`,1,`, `,1,2,`, `,1,3,`');
+$project->grade->range('1');
+$project->name->range('项目, 看板1, 看板2');
+$project->begin->range('(-2M)-(-M):1D')->type('timestamp')->format('YY/MM/DD');
+$project->end->range('(+2M)-(+3M):1D')->type('timestamp')->format('YY/MM/DD');
+$project->openedBy->range('user1');
+$project->acl->range('open');
+$project->status->range('wait');
+$project->vision->range('lite');
+$project->gen(3);
+
+$tester = new editExecutionTester();
+$tester->login();
+
+$execution = array(
+    '0' => array(
+        'name'  => '编辑看板1',
+        'begin' => date('Y-m-d'),
+        'end'   => date('Y-m-d', strtotime('+2 days')),
+    ),
+    '1' => array(
+        'name'  => '',
+        'begin' => date('Y-m-d'),
+        'end'   => date('Y-m-d', strtotime('+2 days')),
+    ),
+    '2' => array(
+        'name'  => '编辑看板2',
+        'begin' => '',
+        'end'   => date('Y-m-d', strtotime('+2 days')),
+    ),
+    '3' => array(
+        'name'  => '编辑看板3',
+        'begin' => date('Y-m-d'),
+        'end'   => '',
+    ),
+    '4' => array(
+        'name'  => '看板2',
+        'begin' => date('Y-m-d'),
+        'end'   => date('Y-m-d', strtotime('+2 days')),
+    ),
+    '5' => array(
+        'name'   => '编辑看板5',
+        'begin'  => date('Y-m-d', strtotime('-2 years')),
+        'end'    => date('Y-m-d', strtotime('+2 days')),
+    ),
+    '6' => array(
+        'name'   => '测试看板9',
+        'begin'  => date('Y-m-d'),
+        'end'    => date('Y-m-d', strtotime('+2 years')),
+    ),
+);
+
+r($tester->edit($execution['0']))                       && p('status,message') && e('SUCCESS,编辑看板成功');               //编辑看板成功
+r($tester->editWithEmptyName($execution['1']))          && p('status,message') && e('SUCCESS,编辑看板表单页提示信息正确'); //看板名称为空，编辑失败
+r($tester->edit($execution['2']))                       && p('status,message') && e('SUCCESS,编辑看板表单页提示信息正确'); //计划开始时间为空，编辑失败
+r($tester->edit($execution['3']))                       && p('status,message') && e('SUCCESS,编辑看板表单页提示信息正确'); //计划结束时间为空，编辑失败
+r($tester->editWithRepeatName($execution['4']))         && p('status,message') && e('SUCCESS,编辑看板表单页提示信息正确'); //看板名称重复，编辑失败
+r($tester->editWithDateError($execution['5'], 'begin')) && p('status,message') && e('SUCCESS,编辑看板表单页提示信息正确'); //看板的计划开始时间早于项目的计划开始时间，编辑失败
+r($tester->editWithDateError($execution['6'], 'end'))   && p('status,message') && e('SUCCESS,编辑看板表单页提示信息正确'); //看板的计划结束时间晚于项目的计划结束时间，编辑失败
+$tester->closeBrowser();
