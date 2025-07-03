0d66a4225d9cf6a62d76ef603aeede3a6bff2b97
wangyidong <yidong@chandao.com>
2025-07-03 13:05:27 +0800

    * [unittest] Fix unittest.

--- module/action/test/model/getdynamicbyaccount.php
+++ module/action/test/model/getdynamicbyaccount.php
@@ -24,7 +24,7 @@ cid=1
 - 查找用户test18动态 @11
 - 查找今天的动态 @1
 - 查找昨天的动态 @0
-- 查找上周的动态 @3
+- 查找上周的动态 @2
 - 查找今天的动态 @0
 - 查找用户admin动态 @12
 - 查找用户dev17动态 @12
@@ -52,7 +52,7 @@ zenData('action')->loadYaml('action_week')->gen(35, true, false);
 zenData('actionrecent')->loadYaml('action_week')->gen(35, true, false);
 r($action->getDynamicByAccountTest($accountList[1], $typeList[1])) && p() && e('1');   // 查找今天的动态
 r($action->getDynamicByAccountTest($accountList[1], $typeList[2])) && p() && e('0');   // 查找昨天的动态
-r($action->getDynamicByAccountTest($accountList[1], $typeList[3])) && p() && e('3');   // 查找上周的动态
+r($action->getDynamicByAccountTest($accountList[1], $typeList[3])) && p() && e('2');   // 查找上周的动态
 
 zenData('action')->loadYaml('action_year')->gen(35, true, false);
 zenData('actionrecent')->loadYaml('action_year')->gen(35, true, false);
