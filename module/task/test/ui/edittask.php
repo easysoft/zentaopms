188e8e993357052d5a8ca362cf2209c87179f7fd
azalea <azalea@cnezsoft.com>
2025-07-03 14:02:04 +0800

    * [uitest] Add the case for editing tasks.

--- module/task/test/ui/edittask.php
+++ module/task/test/ui/edittask.php
@@ -48,3 +48,40 @@ $task->execution->range('2');
 $task->name->range('李娟的开发任务1, 李娟的开发任务2');
 $task->type->range('devel');
 $task->story->range('0');
+$task->storyVersion->range('0');
+$task->module->range('0');
+$task->estimate->range('3');
+$task->consumed->range('2');
+$task->left->range('1');
+$task->status->range('doing');
+$task->assignedTo->range('用户1, 用户2');
+$task->gen(2);
+
+$taskSpec = zenData('taskspec');
+$taskSpec->task->range('1, 2');
+$taskSpec->version->range('1');
+$taskSpec->name->range('李娟的开发任务1, 李娟的开发任务2');
+$taskSpec->gen(2);
+
+$team = zenData('team');
+$team->id->range('1,2,3');
+$team->root->range('2');
+$team->type->range('execution');
+$team->account->range('admin, user1, user2');
+$team->gen(3);
+
+$tester = new editTaskTester();
+$tester->login();
+
+$task = new stdclass();
+$task->name       = '开发任务';
+$task->type       = '设计';
+$task->assignedTo = '用户2';
+
+$task2 = new stdclass();
+$task2->name = '';
+
+r($tester->editDevTask($task)) && p('message') && e('成功编辑开发任务');           // 编辑开发任务
+r($tester->editNameBlankTask($task2)) && p('message') && e('任务名称不能为空');    // 编辑任务设置名称为空
+
+$tester->closeBrowser();
