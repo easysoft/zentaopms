71e3582e4af69cfd057d52e2201d9d4ab039ae9f
sunguangming <sunguangming@chandao.com>
2025-07-04 03:07:32 +0000

    * [sql] add build-in sql.

--- db/zentao.sql
+++ db/zentao.sql
@@ -13565,17 +13565,22 @@ REPLACE INTO `zt_workflowrule`(`type`, `name`, `rule`, `createdBy`, `createdDate
 ('system','IP','ip','admin','2020-10-14 14:06:14');
 
 INSERT INTO `zt_workflowgroup` (`type`, `projectModel`, `projectType`, `name`, `code`, `status`, `vision`, `main`) VALUES
-('product', '',              'project', '默认流程',          'productproject',  'normal', 'rnd', '1'),
-('project', 'scrum',         'product', '敏捷式产品研发',     'scrumproduct',    'normal', 'rnd', '1'),
-('project', 'scrum',         'project', '敏捷式项目研发',     'scrumproject',    'normal', 'rnd', '1'),
-('project', 'waterfall',     'product', '瀑布式产品研发',     'waterfallproduct','normal', 'rnd', '1'),
-('project', 'waterfall',     'project', '瀑布式项目研发',     'waterfallproject','normal', 'rnd', '1'),
-('project',	'agileplus',	   'product',	'融合敏捷式产品研发',	'agileplusproduct',	    'normal',	'rnd',	'1'),
-('project',	'agileplus',	   'project',	'融合敏捷式项目研发',	'agileplusproject',	    'normal',	'rnd',	'1'),
-('project',	'waterfallplus', 'product',	'融合瀑布式产品研发',	'waterfallplusproduct',	'normal',	'rnd',	'1'),
-('project',	'waterfallplus', 'project',	'融合瀑布式项目研发',	'waterfallplusproject',	'normal',	'rnd',	'1'),
-('project',	'kanban',	       'product',	'看板式产品研发',	    'kanbanproduct',	    'normal',	'rnd',	'1'),
-('project',	'kanban',	       'project',	'看板式项目研发',	    'kanbanproject',	    'normal',	'rnd',	'1');
+('product', '',              'project',     '默认流程',            'productproject',       'normal', 'rnd', '1'),
+('project', 'scrum',         'product',     '敏捷式产品研发',       'scrumproduct',         'normal', 'rnd', '1'),
+('project', 'scrum',         'project',     '敏捷式项目研发',       'scrumproject',         'normal', 'rnd', '1'),
+('project', 'waterfall',     'product',     '瀑布式产品研发',       'waterfallproduct',     'normal', 'rnd', '1'),
+('project', 'waterfall',     'project',     '瀑布式项目研发',       'waterfallproject',     'normal', 'rnd', '1'),
+('project',	'agileplus',	   'product',	    '融合敏捷式产品研发',	  'agileplusproduct',	    'normal',	'rnd',	'1'),
+('project',	'agileplus',	   'project',	    '融合敏捷式项目研发',	  'agileplusproject',	    'normal',	'rnd',	'1'),
+('project',	'waterfallplus', 'product',    	'融合瀑布式产品研发',	  'waterfallplusproduct',	'normal',	'rnd',	'1'),
+('project',	'waterfallplus', 'project',    	'融合瀑布式项目研发',	  'waterfallplusproject',	'normal',	'rnd',	'1'),
+('project',	'kanban',	       'product',    	'看板式产品研发',	      'kanbanproduct',	      'normal',	'rnd',	'1'),
+('project',	'kanban',	       'project',    	'看板式项目研发',	      'kanbanproject',	      'normal',	'rnd',	'1'),
+('project',	'ipd',	         'ipd',	        'IPD集成产品研发',	    'ipdproduct',	          'normal',	'rnd',	'1'),
+('project',	'ipd',	         'tpd',	        'IPD预研产品研发',	    'tpdproduct',	          'normal',	'rnd',	'1'),
+('project',	'ipd',	         'cbb',	        'IPD平台产品研发',	    'cbbproduct',	          'normal',	'rnd',	'1'),
+('project',	'ipd',	         'cpdproduct',	'IPD定制产品研发',	    'cpdproduct',	          'normal',	'rnd',	'1'),
+('project',	'ipd',	         'cpdproject',	'IPD定制项目研发',	    'cpdproject',	          'normal',	'rnd',	'1');
 
 INSERT INTO `zt_workflowdatasource` (`type`, `name`, `code`, `buildin`, `vision`, `createdBy`, `createdDate`, `datasource`, `view`, `keyField`, `valueField`) VALUES
 ('system',      '产品',           'products',                 '1', 'rnd', 'admin', '1970-01-01 00:00:01', '{\"app\":\"system\",\"module\":\"product\",\"method\":\"getPairs\",\"methodDesc\":\"Get product pairs.\",\"params\":[{\"name\":\"mode\",\"type\":\"string\",\"desc\":\"\",\"value\":\"all\"}]}',       '',     '',     ''),
