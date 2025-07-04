ba9336f4ea19a52c750bf24712008419ef15e7d6
tianshujie <tianshujie@easycorp.ltd>
2025-07-04 09:13:40 +0800

    * [rafac] add comment.

--- extension/lite/story/ext/control/close.php
+++ extension/lite/story/ext/control/close.php
@@ -2,6 +2,16 @@
 helper::importControl('story');
 class myStory extends story
 {
+    /**
+     * 关闭需求。
+     * Close the story.
+     *
+     * @param  int    $storyID
+     * @param  string $from
+     * @param  string $storyType
+     * @access public
+     * @return void
+     */
     public function close(int $storyID, string $from = '', string $storyType = 'story')
     {
         foreach($this->lang->{$storyType}->reasonList as $key => $value)
