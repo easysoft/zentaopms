259a601ac16a6c3c0313cd1542017bd421be6d73
qixinzhi <qixinzhi@easycorp.ltd>
2025-07-08 05:57:34 +0000

    * [unittest] fix unittest.

--- module/story/test/model/change.php
+++ module/story/test/model/change.php
@@ -12,6 +12,9 @@ cid=0
  - 属性spec @测试需求1的变更描述
  - 属性version @4
 - 变更时不填写需求名称，给出提示 @1
+- 测试修改名称为空。第title条的0属性 @『研发需求名称』不能为空。
+- 测试存在relievedTwins。属性title @测试需求1变更标题
+- 测试变成需求。属性title @名称修改
 
 */
 include dirname(__FILE__, 5) . '/test/lib/init.php';
@@ -40,5 +43,8 @@ $story2->version  = 5;
 $result1 = $story->changeTest(1,  $story1);
 $result2 = $story->changeTest(2,  $story2);
 
-r($result1) && p('title,spec,version') && e('测试需求1变更标题,测试需求1的变更描述,4'); // 查看变更后需求数据。
-r((int)strpos($result2['title'][0], '名称』不能为空') !== false) && p() && e('1');      // 变更时不填写需求名称，给出提示
+r($story->changeTest(1, $story1))                               && p('title,spec,version') && e('测试需求1变更标题,测试需求1的变更描述,4');  // 查看变更后需求数据。
+r((int)strpos($result['title'][0], '名称』不能为空') !== false) && p()                     && e('1');                                       // 变更时不填写需求名称，给出提示
+r($story->changeTest(2, $story3))                               && p('title:0')            && e('『研发需求名称』不能为空。');               // 测试修改名称为空。
+r($story->changeTest(3, $story4))                               && p('title')              && e('测试需求1变更标题');                       // 测试存在relievedTwins。
+r($story->changeTest(4, $story5))                               && p('title')              && e('名称修改');                                // 测试变成需求。
