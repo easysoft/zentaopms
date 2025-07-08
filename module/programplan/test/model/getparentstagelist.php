492f882e5cff0b784fe67494cd3a29d660cb3722
qixinzhi <qixinzhi@easycorp.ltd>
2025-07-08 00:57:10 +0000

    * [unittest] fix getparentstagelist.

--- module/programplan/test/model/getparentstagelist.php
+++ module/programplan/test/model/getparentstagelist.php
@@ -9,6 +9,8 @@ cid=0
 - 测试查询项目1产品2阶段8父阶段信息 @执行1-1
 - 测试查询项目1产品3阶段8父阶段信息 @无
 - 测试查询项目4产品6阶段8父阶段信息 @执行2-1-1
+- 测试查询项目10产品6阶段8父阶段信息 @无
+- 测试查询项目20产品6阶段8父阶段信息 @无
 
 */
 include dirname(__FILE__, 5) . '/test/lib/init.php';
@@ -21,6 +23,8 @@ global $tester;
 $tester->loadModel('programplan');
 $tester->programplan->app->user->admin = true;
 
-r($tester->programplan->getParentStageList(1, 8, 2)[2]) && p() && e('执行1-1');   // 测试查询项目1产品2阶段8父阶段信息
-r($tester->programplan->getParentStageList(1, 8, 3)[0]) && p() && e('无');        // 测试查询项目1产品3阶段8父阶段信息
-r($tester->programplan->getParentStageList(4, 8, 6)[6]) && p() && e('执行2-1-1'); // 测试查询项目4产品6阶段8父阶段信息
+r($tester->programplan->getParentStageList(1, 8, 2)[2]) && p()    && e('执行1-1');   // 测试查询项目1产品2阶段8父阶段信息。
+r($tester->programplan->getParentStageList(1, 8, 3)[0]) && p()    && e('无');        // 测试查询项目1产品3阶段8父阶段信息。
+r($tester->programplan->getParentStageList(4, 8, 6)[6]) && p()    && e('执行2-1-1'); // 测试查询项目4产品6阶段8父阶段信息。
+r($tester->programplan->getParentStageList(10, 8, 6))   && p('0') && e('无');        // 测试查询项目10产品父阶段信息。
+r($tester->programplan->getParentStageList(20, 8, 6))   && p('0') && e('无');        // 测试查询项目20产品父阶段信息。
