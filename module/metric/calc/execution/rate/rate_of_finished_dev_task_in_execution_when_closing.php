0a8ba6c21c29662f73e0625bc71c77efcb422f19
qixinzhi <qixinzhi@easycorp.ltd>
2025-07-09 06:45:53 +0000

    * [story#76132] finish metric:rate_of_finished_dev_task_in_execution_when_closing.

--- module/metric/calc/execution/rate/rate_of_finished_dev_task_in_execution_when_closing.php
+++ module/metric/calc/execution/rate/rate_of_finished_dev_task_in_execution_when_closing.php
@@ -12,7 +12,7 @@
  * 复用：按执行统计的执行关闭时已完成的开发任务数、按执行统计的开发任务数，公式：按执行统计的执行关闭时已完成的开发任务数÷按执行统计的开发任务数。
  *
  * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
- * @author    songchenxuan <songchenxuan@chandao.com>
+ * @author    qixinzhi <qixinzhi@zentao.com>
  * @package
  * @uses      func
  * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
@@ -20,18 +20,53 @@
  */
 class rate_of_finished_dev_task_in_execution_when_closing extends baseCalc
 {
-    public $dataset = '';
+    public $initRecord = false;
 
-    public $fieldList = array();
+    public $reuse = true;
+
+    public $reuseMetrics = array('allTask' => 'count_of_dev_task_in_execution', 'finishedTask' => 'count_of_finished_dev_task_in_execution_when_closing');
+
+    public $reuseRule = '{finishedTask} / {allTask}';
 
     public $result = array();
 
-    public function calculate($row)
+    public function calculate($metrics)
     {
+        $allTask      = $metrics['allTask'];
+        $finishedTask = $metrics['finishedTask'];
+        if(empty($allTask) || empty($finishedTask)) return false;
+
+        $all = array_merge($allTask, $finishedTask);
+        $allTask      = $this->generateUniqueKey($allTask);
+        $finishedTask = $this->generateUniqueKey($finishedTask);
+
+        $executions = array_column($all, 'execution', 'execution');
+        foreach($executions as $execution)
+        {
+            if(!isset($allTask[$execution])) continue;
+
+            $allTaskItem      = $allTask[$execution];
+            $finishedTaskItem = isset($finishedTask[$execution]) ? $finishedTask[$execution] : 0;
+            $this->result[$execution] = $allTaskItem == 0 ? 0 : round($finishedTaskItem / $allTaskItem, 4);
+        }
     }
 
     public function getResult($options = array())
     {
-        return $this->filterByOptions($this->result, $options);
+        $records = array();
+        foreach($this->result as $execution => $value) $records[] = array('execution' => $execution, 'value' => $value);
+        return $this->filterByOptions($records, $options);
+    }
+
+    public function generateUniqueKey($records)
+    {
+        $uniqueKeyRecords = array();
+        foreach($records as $record)
+        {
+            $key = $record['execution'];
+            $uniqueKeyRecords[$key] = $record['value'];
+        }
+
+        return $uniqueKeyRecords;
     }
 }
