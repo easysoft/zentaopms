ba4599bc78c273e3472ed8117687b33a75c66ada
wangyuting <wangyuting@chandao.com>
2025-07-09 10:59:26 +0800

    * Optimize unit test script.

--- module/instance/test/model/autobackup.php
+++ module/instance/test/model/autobackup.php
@@ -4,8 +4,8 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 
 $cron = zenData('cron');
 $cron->command->range('1-5')->prefix('moduleName=instance&methodName=cronBackup&instanceID=');
-$cron->gen(1);
-zenData('instance')->loadYaml('instance')->gen(5);
+$cron->gen(2);
+zenData('instance')->loadYaml('instance')->gen(2);
 zenData('space')->loadYaml('space')->gen(1);
 zenData('user')->gen(5);
 
@@ -36,16 +36,7 @@ $user = new stdClass();
 $user->account = 'admin';
 
 $instance = $tester->instance->getByID(1);
-r(!$tester->instance->autoBackup($instance, $user)) && p(0) && e(1);
+r(!$tester->instance->autoBackup($instance, $user)) && p(0) && e(0);
 
 $instance = $tester->instance->getByID(2);
-r(!$tester->instance->autoBackup($instance, $user)) && p(0) && e(1);
-
-$instance = $tester->instance->getByID(3);
-r(!$tester->instance->autoBackup($instance, $user)) && p(0) && e(1);
-
-$instance = $tester->instance->getByID(4);
-r(!$tester->instance->autoBackup($instance, $user)) && p(0) && e(1);
-
-$instance = $tester->instance->getByID(5);
-r(!$tester->instance->autoBackup($instance, $user)) && p(0) && e(1);
+r(!$tester->instance->autoBackup($instance, $user)) && p(0) && e(0);
