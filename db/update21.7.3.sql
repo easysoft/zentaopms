d1ab9ade50d27bb6ba7d239aaa0d35d31aac2aa1
tianshujie <tianshujie@easycorp.ltd>
2025-07-14 14:56:07 +0800

    * [task#146562,doing,0.1h] add isReport column for zt_doc.

--- db/update21.7.3.sql
+++ db/update21.7.3.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `zt_doc` ADD `cycle` char(10) COLLATE 'utf8mb4_general_ci' NOT NULL AFTER `chapterType`;
 ALTER TABLE `zt_doc` ADD `objects` text COLLATE 'utf8mb4_general_ci' NULL AFTER `templateDesc`;
+ALTER TABLE `zt_doc` ADD `isReport` enum('0', '1') COLLATE 'utf8mb4_general_ci' NOT NULL DEFAULT '0' AFTER `templateDesc`;
