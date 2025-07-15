e69fd3400a7b86f6dcafc5d3f5f1688aafe2972c
liugang <liugang@chandao.com>
2025-07-15 14:23:52 +0800

    + [perf] Define userview related tables.

---
+++ config/userview.php
@@ -0,0 +1,3 @@
+<?php
+$config->userview = new stdClass();
+$config->userview->relatedTables = [TABLE_PRODUCT, TABLE_PROJECT, TABLE_TEAM, TABLE_ACL, TABLE_STAKEHOLDER, TABLE_PROJECTADMIN, TABLE_PROJECTPRODUCT]; // userview 相关表。userview related tables.
