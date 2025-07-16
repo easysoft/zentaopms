10a21032a3831d01818f0c1d3d68d851b2b3d4da
wangyuting <wangyuting@chandao.com>
2025-07-16 13:41:12 +0800

    * Optimize setmenu unit test script.

--- module/admin/test/model/setmenu.php
+++ module/admin/test/model/setmenu.php
@@ -11,20 +11,20 @@ cid=0
  - 第system条的name属性 @系统设置
  - 第system条的order属性 @5
 - 执行admin模块的menuList方法
+ - 第switch条的name属性 @功能开关
+ - 第switch条的order属性 @10
+- 执行admin模块的menuList方法
+ - 第company条的name属性 @人员管理
+ - 第company条的order属性 @15
+- 执行admin模块的menuList方法
+ - 第feature条的name属性 @功能配置
+ - 第feature条的order属性 @25
+- 执行admin模块的menuList方法
+ - 第message条的name属性 @通知设置
+ - 第message条的order属性 @35
+- 执行admin模块的menuList方法
  - 第dev条的name属性 @二次开发
  - 第dev条的order属性 @45
-- 执行admin模块的menuList方法
- - 第approvalflow条的name属性 @审批流
- - 第approvalflow条的order属性 @73
-- 执行admin模块的menuList方法
- - 第productflow条的name属性 @产品流程
- - 第productflow条的order属性 @70
-- 执行admin模块的menuList方法
- - 第projectflow条的name属性 @项目流程
- - 第projectflow条的order属性 @71
-- 执行admin模块的menuList方法
- - 第workflow条的name属性 @工作流
- - 第workflow条的order属性 @72
 
 */
 include dirname(__FILE__, 5) . '/test/lib/init.php';
@@ -32,9 +32,9 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 global $tester,$lang;
 $tester->loadModel('admin');
 r($lang->admin->menuList) && p('system:name,order') && e('系统设置,5');
-r($lang->admin->menuList) && p('dev:name,order') && e('二次开发,45');
-r($lang->admin->menuList) && p('approvalflow:name,order') && e('审批流,73');
+r($lang->admin->menuList) && p('switch:name,order') && e('功能开关,10');
+r($lang->admin->menuList) && p('company:name,order') && e('人员管理,15');
 $tester->admin->setMenu();
-r($lang->admin->menuList) && p('productflow:name,order') && e('产品流程,70');
-r($lang->admin->menuList) && p('projectflow:name,order') && e('项目流程,71');
-r($lang->admin->menuList) && p('workflow:name,order') && e('工作流,72');
+r($lang->admin->menuList) && p('feature:name,order') && e('功能配置,25');
+r($lang->admin->menuList) && p('message:name,order') && e('通知设置,35');
+r($lang->admin->menuList) && p('dev:name,order') && e('二次开发,45');
