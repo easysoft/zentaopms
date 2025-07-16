6dda2da090e5bc0fc4378aed2c6dcd6df7eea735
yulujie <yulujie@chandao.com>
2025-07-16 08:55:23 +0800

    *[uitest] Fix bug.

--- module/execution/test/lib/activateinlite.ui.class.php
+++ module/execution/test/lib/activateinlite.ui.class.php
@@ -12,8 +12,6 @@ class activateExecutionTester extends tester
      */
     public function inputFields($end, $executionId)
     {
-        $this->switchVision('lite');
-        $this->page->wait(5);
         $form = $this->initForm('execution', 'kanban', array('execution' => $executionId), 'appIframe-project');
         $form->wait(1);
         $form->dom->kanbanSettingInLite->click();
@@ -62,6 +60,8 @@ class activateExecutionTester extends tester
      */
     public function activateWithLessEnd($end, $executionId)
     {
+        $this->switchVision('lite');
+        $this->page->wait(5)->refresh();
         $this->inputFields($end, $executionId);
         $form = $this->loadPage();
         $form->wait(1);
