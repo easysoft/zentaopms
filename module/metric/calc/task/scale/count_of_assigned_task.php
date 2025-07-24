ca5dad06a434252aa7ef1f21924d115bd62884a2
wangzemei <wangzemei@easycorp.ltd>
2025-07-24 14:51:04 +0800

    * [bug#64287] add the commnets for count_of_assigned_task metric.

--- module/metric/calc/task/scale/count_of_assigned_task.php
+++ module/metric/calc/task/scale/count_of_assigned_task.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * 按人员统计的任务数。
+ * Count of assigned task in user.
+ *
+ * 范围：user
+ * 对象：task
+ * 目的：scale
+ * 度量名称：按人员统计的任务数
+ * 单位：个
+ * 描述：按人员统计的任务数表示每个人的任务数量之和。反映了每个人在需要处理的任务数量上的规模。该数值越大，说明需要投入越多的时间处理任务。
+ * 定义：所有任务个数求和;指派给为某人;过滤已删除的任务;过滤已删除项目的任务;过滤已删除执行的任务;
+ *
+ * @copyright Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @author    Zemei Wang <wangzemei@easycorp.ltd>
+ * @package
+ * @uses      func
+ * @license   ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @Link      https://www.zentao.net
+ */
 class count_of_assigned_task extends baseCalc
 {
     public $dataset = '';
