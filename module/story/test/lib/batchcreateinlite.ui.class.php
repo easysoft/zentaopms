b3306cbc066b4dbd024be856b247435ed8ca0137
lixiaoyu <lixiaoyu@easycorp.ltd>
2025-07-28 16:22:46 +0800

    *[task#146965,doing,0.5h,3h]

--- module/story/test/lib/batchcreateinlite.ui.class.php
+++ module/story/test/lib/batchcreateinlite.ui.class.php
@@ -15,9 +15,9 @@ class batchCreateStory extends tester
     public function batchCreateStory($project, $storyUrl, $story)
     {
         $this->switchVision('lite', 5);
-        $this->page->wait(2);
+        $this->page->waitElement('//*[@id="app-project"]', 5);
         $this->openURL('projectstory', 'story', $project, 'appIframe-project');
-        $this->page->wait(2);
+        $this->page->waitElement('//*[@id="app-project"]', 5);
         $form = $this->initForm('story', 'batchCreate', $storyUrl, 'appIframe-project');
         $form->wait(2);
         //设置表单内容
