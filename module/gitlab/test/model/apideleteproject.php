a8e89c7bfb35e34ed734b2259b9eb77df52bfbfe
liyang <liyang@easycorp.ltd>
2025-08-06 09:33:40 +0800

    * [unittest] fix unittest.

--- module/gitlab/test/model/apideleteproject.php
+++ module/gitlab/test/model/apideleteproject.php
@@ -32,13 +32,11 @@ $gitlab->apiCreateProject($gitlabID, $project);
 
 /* Get projectID. */
 $gitlabProjects = $gitlab->apiGetProjects($gitlabID);
+$unitTestList   = array();
 foreach($gitlabProjects as $gitlabProject)
 {
-    if($gitlabProject->name == $projectName)
-    {
-        $projectID = $gitlabProject->id;
-        break;
-    }
+    if($gitlabProject->name == $projectName) $projectID = $gitlabProject->id;
+    if(strpos($gitlabProject->name, 'unitTest') === 0 || strpos($gitlabProject->name, 'unitest') === 0) $unitTestList[] = $gitlabProject;
 }
 
 $project = new stdclass();
@@ -46,5 +44,15 @@ $project->description = 'apiUpdatedProject';
 
 r($gitlab->apiDeleteProject($gitlabID, 0))  && p() && e('0'); //使用空的projectID删除项目
 r($gitlab->apiDeleteProject(0, $projectID)) && p() && e('0'); //使用错误gitlabID删除项目
+r($gitlab->apiDeleteProject(0, 0))          && p() && e('0'); //使用空的GitlabID和空的projectID删除项目
+r($gitlab->apiDeleteProject(0, 99999))      && p() && e('0'); //使用错误projectID删除项目
 
 r($gitlab->apiDeleteProject($gitlabID, $projectID)) && p('message') && e('202 Accepted');         //通过gitlabID,项目id正确删除项目
+//删除测试数据
+if(!empty($unitTestList))
+{
+    foreach($unitTestList as $unitTestProject)
+    {
+        $gitlab->apiDeleteProject($gitlabID, $unitTestProject->id);
+    }
+}
