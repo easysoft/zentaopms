9d971c8455e9e5c337c1a15f41b636116f52a35e
liyang <liyang@easycorp.ltd>
2025-08-08 14:00:13 +0800

    * [task#146612] hide import job btn.

--- module/job/control.php
+++ module/job/control.php
@@ -83,18 +83,8 @@ class job extends control
         $this->jobZen->buildSearchForm($this->config->job->search, $queryID, $actionURL);
         $jobQuery = $type == 'bySearch' ? $this->jobZen->getJobSearchQuery((int)$queryID) : '';
 
-        $jobList  = $this->jobZen->getJobList($repoID, $jobQuery, $orderBy, $pager);
-
-        $hasJobServer = false;
-        $pipelines    = $this->loadModel('pipeline')->getList();
-        foreach($pipelines as $pipeline)
-        {
-            if(in_array($pipeline->type, array('jenkins', 'gitlab')))
-            {
-                $hasJobServer = true;
-                break;
-            }
-        }
+        $jobList   = $this->jobZen->getJobList($repoID, $jobQuery, $orderBy, $pager);
+        $pipelines = $this->loadModel('pipeline')->getPairs('jenkins,gitlab');
 
         $this->view->title   = $this->lang->ci->job . $this->lang->hyphen . $this->lang->job->browse;
         $this->view->repoID  = $repoID;
@@ -104,7 +94,7 @@ class job extends control
         $this->view->queryID = $queryID;
         $this->view->pager   = $pager;
 
-        $this->view->hasJobServer = $hasJobServer;
+        $this->view->hasJobServer = !empty($pipelines);
 
         $this->display();
     }
