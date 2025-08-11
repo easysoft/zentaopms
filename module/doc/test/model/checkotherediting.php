28dbec7d6f0984bd41d7eea276dd10654dca68f2
tianshujie <tianshujie@easycorp.ltd>
2025-08-11 10:09:49 +0800

    * [task#148023,done,0.2h] add step for checkOtherEditing method.

--- module/doc/test/model/checkotherediting.php
+++ module/doc/test/model/checkotherediting.php
@@ -3,11 +3,13 @@
 /**
 
 title=测试 docModel->checkOtherEditing();
+timeout=0
 cid=1
 
 - 测试空数据 @0
 - 测试文档1是否有在编辑的人 @1
 - 测试文档2是否有在编辑的人 @0
+- 测试文档3是否有在编辑的人 @1
 - 测试不存在的文档是否有在编辑的人 @0
 
 */
@@ -18,13 +20,14 @@ include dirname(__FILE__, 2) . '/lib/doc.unittest.class.php';
 $timestamp = time();
 $docTable = zenData('doc')->loadYaml('doc');
 $docTable->editingDate->range("`{\"user1\": $timestamp}`,[]");
-$docTable->gen(2);
+$docTable->gen(3);
 zenData('user')->gen(5);
 
-$docIds = array(0, 1, 2, 3);
+$docIds = array(0, 1, 2, 3, 100);
 
 $docTester = new docTest();
 r($docTester->checkOtherEditingTest($docIds[0])) && p() && e('0'); // 测试空数据
 r($docTester->checkOtherEditingTest($docIds[1])) && p() && e('1'); // 测试文档1是否有在编辑的人
 r($docTester->checkOtherEditingTest($docIds[2])) && p() && e('0'); // 测试文档2是否有在编辑的人
-r($docTester->checkOtherEditingTest($docIds[3])) && p() && e('0'); // 测试不存在的文档是否有在编辑的人
+r($docTester->checkOtherEditingTest($docIds[3])) && p() && e('1'); // 测试文档3是否有在编辑的人
+r($docTester->checkOtherEditingTest($docIds[4])) && p() && e('0'); // 测试不存在的文档是否有在编辑的人
