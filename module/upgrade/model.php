cd457f7b9a5a12690f0dae4b0a3028414b858a37
sunguangming <sunguangming@chandao.com>
2025-08-11 06:04:11 +0000

    * [task#148112,done,1h] add upgrade method.

--- module/upgrade/model.php
+++ module/upgrade/model.php
@@ -11513,4 +11513,48 @@ class upgradeModel extends model
             $this->dao->update(TABLE_PROJECT)->set('deliverable')->eq($projectDeliverable)->where('id')->eq($project->id)->exec();
         }
     }
+
+    /**
+     * 升级流程和活动, 将流程和活动中的数据迁移到流程中。
+     * Upgrade process and activity.
+     *
+     * @access public
+     * @return void
+     */
+    public function upgradeProcessAndActivity()
+    {
+        $groupList = $this->dao->select('id,projectModel,projectType')->from(TABLE_WORKFLOWGROUP)
+            ->where('projectModel')->in('scrum,waterfall,agileplus,waterfallplus')
+            ->andWhere('main')->eq('1')
+            ->fetchAll('id');
+
+        foreach($groupList as $groupID => $group)
+        {
+            if($group->projectType == 'product')
+            {
+                $this->dao->update(TABLE_PROCESS)->set('workflowGroup')->eq($groupID)->where('model')->eq($group->projectModel)->exec();
+            }
+            else
+            {
+                /* 复制一份到新的模型中。*/
+                $processList   = $this->dao->select('*')->from(TABLE_PROCESS)->where('model')->eq($group->projectModel)->andWhere('deleted')->eq('0')->fetchAll('id');
+                $activityGroup = $this->dao->select('*')->from(TABLE_ACTIVITY)->where('process')->in(array_keys($processList))->andWhere('deleted')->eq('0')->fetchGroup('process', 'id');
+                foreach($processList as $process)
+                {
+                    $activityList = zget($activityGroup, $process->id, array());
+                    unset($process->id);
+                    $process->workflowGroup = $groupID;
+                    $this->dao->insert(TABLE_PROCESS)->data($process)->exec();
+                    $processID = $this->dao->lastInsertID();
+
+                    foreach($activityList as $activity)
+                    {
+                        unset($activity->id);
+                        $activity->process = $processID;
+                        $this->dao->insert(TABLE_ACTIVITY)->data($activity)->exec();
+                    }
+                }
+            }
+        }
+    }
 }
