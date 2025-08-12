6afed2e8b07769a6aa8a9779023e02aa3ef50996
tianshujie <tianshujie@easycorp.ltd>
2025-08-12 09:12:40 +0800

    * [task#148026,done,0.1h] add step for getOrderedProducts method.

--- module/doc/test/model/getorderedproducts.php
+++ module/doc/test/model/getorderedproducts.php
@@ -7,8 +7,10 @@ cid=1
 
 - 获取系统中已排序的产品第0条的1属性 @产品1
 - 获取系统中包括ID=1已排序的产品第0条的1属性 @产品1
+- 获取系统中包括不存在ID已排序的产品第0条的1属性 @产品1
 - 获取系统中已排序的产品数量 @3
 - 获取系统中包括ID=1已排序的产品数量 @3
+- 获取系统中包括不存在ID已排序的产品数量 @3
 
 */
 
@@ -19,11 +21,13 @@ zenData('product')->loadYaml('product')->gen(20);
 zenData('user')->gen(5);
 su('admin');
 
-$appends = array(0, 1);
+$appends = array(0, 1, 100);
 
 $docTester = new docTest();
 r($docTester->getOrderedProductsTest($appends[0])) && p('0:1') && e('产品1'); // 获取系统中已排序的产品
 r($docTester->getOrderedProductsTest($appends[1])) && p('0:1') && e('产品1'); // 获取系统中包括ID=1已排序的产品
+r($docTester->getOrderedProductsTest($appends[2])) && p('0:1') && e('产品1'); // 获取系统中包括不存在ID已排序的产品
 
 r(count($docTester->getOrderedProductsTest($appends[0]))) && p() && e('3'); // 获取系统中已排序的产品数量
 r(count($docTester->getOrderedProductsTest($appends[1]))) && p() && e('3'); // 获取系统中包括ID=1已排序的产品数量
+r(count($docTester->getOrderedProductsTest($appends[2]))) && p() && e('3'); // 获取系统中包括不存在ID已排序的产品数量
