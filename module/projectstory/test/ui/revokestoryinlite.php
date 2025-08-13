08fa88e8521a25de3faebf2ac2a98aa5fbeee109
lixiaoyu <lixiaoyu@easycorp.ltd>
2025-08-13 10:41:38 +0800

    *[task#147928,doing,1h,3h]zendata of revokestory

---
+++ module/projectstory/test/ui/revokestoryinlite.php
@@ -0,0 +1,44 @@
+#!/usr/bin/env php
+<?php
+
+/**
+title=运营界面撤销评审目标
+timeout=0
+cid=0
+*/
+chdir(__DIR__);
+include '../lib/changestatusinlite.ui.class.php';
+
+$project = zenData('project');
+$project->id->range('1');
+$project->model->range('kanban');
+$project->type->range('project');
+$project->name->range('运营项目');
+$project->hasProduct->range('0');
+$project->acl->range('open');
+$project->vision->range('lite');
+$project->gen(1);
+
+$product = zenData('product');
+$product->id->range('1');
+$product->program->range('0');
+$product->name->range('运营项目');
+$product->shadow->range('1');
+$product->type->range('normal');
+$product->acl->range('open');
+$product->vision->range('lite');
+$product->gen(1);
+
+$projectproduct = zenData('projectproduct');
+$projectproduct->project->range('1');
+$projectproduct->product->range('1');
+$projectproduct->branch->range('0');
+$projectproduct->plan->range('0');
+$projectproduct->gen(1);
+
+$team = ZenData('team');
+$team->id->range('1');
+$team->root->range('1');
+$team->type->range('project');
+$team->account->range('admin');
+$team->gen(1);
