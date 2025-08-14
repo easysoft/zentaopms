f88be7e0f71469effa64bcba290a43014db8323a
lixiaoyu <lixiaoyu@easycorp.ltd>
2025-08-14 09:48:32 +0800

    *[task#147928,done,1h,0h]submit review story in lite

--- module/projectstory/test/ui/submitreviewinlite.php
+++ module/projectstory/test/ui/submitreviewinlite.php
@@ -48,3 +48,51 @@ $team->root->range('1');
 $team->type->range('project');
 $team->account->range('admin');
 $team->gen(1);
+
+$story = ZenData('story');
+$story->id->range('1');
+$story->vision->range('lite');
+$story->parent->range('0');
+$story->isparent->range('0');
+$story->root->range('1');
+$story->path->range(',1,');
+$story->product->range('1');
+$story->title->range('目标01');
+$story->type->range('story');
+$story->status->range('draft');
+$story->stage->range('projected');
+$story->version->range('1');
+$story->openedBy->range('admin');
+$story->gen(1);
+
+$storyspec = ZenData('storyspec');
+$storyspec->story->range('1');
+$storyspec->version->range('1');
+$storyspec->title->range('目标01');
+$storyspec->gen(1);
+
+$storyreview = ZenData('storyreview');
+$storyreview->story->range('1');
+$storyreview->version->range('1');
+$storyreview->reviewer->range('admin');
+$storyreview->result->range('');
+$storyreview->gen(1);
+
+$projectstory = ZenData('projectstory');
+$projectstory->project->range('1');
+$projectstory->product->range('1');
+$projectstory->branch->range('0');
+$projectstory->story->range('1');
+$projectstory->version->range('1');
+$projectstory->order->range('1');
+$projectstory->gen(1);
+
+$storyUrl = array(
+    'storyID'   => '1',
+    'projectID' => '1'
+);
+$tester = new changeStatus();
+$tester->login();
+
+r($tester->submitReview($storyUrl)) && p('message,status') && e('目标提交评审成功,SUCCESS');//提交评审
+$tester->closeBrowser();
