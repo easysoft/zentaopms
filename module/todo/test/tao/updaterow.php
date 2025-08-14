4c444bd9bcb21edf2b84388a9a3421137015cf9c
sunguangming <sunguangming@chandao.com>
2025-08-14 06:26:58 +0000

    * [unittest] modify unittest.

--- module/todo/test/tao/updaterow.php
+++ module/todo/test/tao/updaterow.php
@@ -15,12 +15,10 @@ timeout=0
 cid=1
 
 - 正常更新待办数据 @1
-
 - 正常更新task类型待办数据 @1
-
-- 更新没有名称的待办数据 @0
-
-- 更新bug类型待办，没有bugID的情况 @0
+- 更新没有名称的待办数据 @『待办名称』不能为空。
+- 更新bug类型待办，没有bugID的情况 @1
+- 更新story类型待办，没有storyID的情况 @1
 
 */
 
@@ -52,7 +50,18 @@ $todoTask->type     = 'task';
 $todoTask->name     = 'TASK待办';
 $todoTask->objectID = 1;
 
+$todoStory = clone $todo;
+$todoStory->type     = 'story';
+$todoStory->name     = 'STORY待办';
+$todoStory->objectID = 10000;
+
 r($tester->todo->updateRow(1, $todo))       && p() && e('1'); // 正常更新待办数据
 r($tester->todo->updateRow(2, $todoTask))   && p() && e('1'); // 正常更新task类型待办数据
-r($tester->todo->updateRow(3, $todoNoName)) && p() && e('0'); // 更新没有名称的待办数据
-r($tester->todo->updateRow(4, $todoBug))    && p() && e('0'); // 更新bug类型待办，没有bugID的情况
+
+$tester->todo->updateRow(3, $todoNoName);
+r(dao::getError()['name']) && p('0') && e('『待办名称』不能为空。'); // 更新没有名称的待办数据
+
+dao::$errors = array();
+
+r($tester->todo->updateRow(4, $todoBug))    && p() && e('1'); // 更新bug类型待办，没有bugID的情况
+r($tester->todo->updateRow(5, $todoStory))  && p() && e('1'); // 更新story类型待办，没有storyID的情况
