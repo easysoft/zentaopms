18a66afb397d7f21ce489c859ddfb4bd4824abea
wangyidong <yidong@chandao.com>
2025-08-15 12:07:07 +0800

    * [unit] Add unit test step.

--- module/project/test/tao/addprojectadmin.php
+++ module/project/test/tao/addprojectadmin.php
@@ -2,10 +2,9 @@
 <?php
 include dirname(__FILE__, 5) . "/test/lib/init.php";
 include dirname(__FILE__, 2) . '/lib/project.unittest.class.php';
-su('admin');
 
 zenData('projectadmin')->gen(0);
-
+zenData('user')->gen(5);
 zenData('project')->gen(15);
 $group = zenData('group');
 $group->role->range('projectAdmin');
@@ -17,10 +16,23 @@ title=测试 projectModel->create();
 timeout=0
 cid=1
 
+- 测试将项目ID为1添加admin至分组属性projects @1
+- 测试将项目ID为2添加admin至分组属性projects @1,2
+- 测试将项目ID为3添加admin至分组属性projects @1,2,3
+- 测试将项目ID为1添加user1至分组属性projects @1
+- 测试将项目ID为2添加user1至分组属性projects @1,2
+- 测试将项目ID为3添加user1至分组属性projects @1,2,3
+
 */
 
 $projectClass = new project();
 
-r($projectClass->addProjectAdminTest(1)) && p('projects', '|') && e('1');     // 测试将项目ID为1添加管理员至分组
-r($projectClass->addProjectAdminTest(2)) && p('projects', '|') && e('1,2');   // 测试将项目ID为2添加管理员至分组
-r($projectClass->addProjectAdminTest(3)) && p('projects', '|') && e('1,2,3'); // 测试将项目ID为3添加管理员至分组
+su('admin');
+r($projectClass->addProjectAdminTest(1)) && p('projects', '|') && e('1');     // 测试将项目ID为1添加admin至分组
+r($projectClass->addProjectAdminTest(2)) && p('projects', '|') && e('1,2');   // 测试将项目ID为2添加admin至分组
+r($projectClass->addProjectAdminTest(3)) && p('projects', '|') && e('1,2,3'); // 测试将项目ID为3添加admin至分组
+
+su('user1');
+r($projectClass->addProjectAdminTest(1)) && p('projects', '|') && e('1');     // 测试将项目ID为1添加user1至分组
+r($projectClass->addProjectAdminTest(2)) && p('projects', '|') && e('1,2');   // 测试将项目ID为2添加user1至分组
+r($projectClass->addProjectAdminTest(3)) && p('projects', '|') && e('1,2,3'); // 测试将项目ID为3添加user1至分组
