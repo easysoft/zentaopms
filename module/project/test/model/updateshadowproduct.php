16af3af178ff0b2bc509e49fa4c6793e1a22cc65
wangyidong <yidong@chandao.com>
2025-08-15 12:07:07 +0800

    * [unit] Add unit test step.

--- module/project/test/model/updateshadowproduct.php
+++ module/project/test/model/updateshadowproduct.php
@@ -14,12 +14,25 @@ title=测试projectModel->updateShadowProduct();
 timeout=0
 cid=1
 
+- 测试更新项目型敏捷项目的产品信息
+ - 第0条的id属性 @1
+ - 第0条的name属性 @更新敏捷项目11
+- 测试更新产品型瀑布项目的产品信息
+ - 第0条的id属性 @2
+ - 第0条的name属性 @产品2
+- 测试更新项目型瀑布项目的产品信息
+ - 第0条的id属性 @3
+ - 第0条的name属性 @更新瀑布项目13
+- 测试更新产品型看板项目的产品信息
+ - 第0条的id属性 @4
+ - 第0条的name属性 @产品4
+
 */
 
 $projectIdList = array(11, 60, 61, 100);
 $projectTester = new project();
 
-r($projectTester->updateShadowProductTest($projectIdList[0])) && p('0:name') && e('更新敏捷项目11'); // 测试更新项目型敏捷项目的产品信息
-r($projectTester->updateShadowProductTest($projectIdList[1])) && p('0:name') && e('产品2');          // 测试更新产品型瀑布项目的产品信息
-r($projectTester->updateShadowProductTest($projectIdList[2])) && p('0:name') && e('更新瀑布项目13'); // 测试更新项目型瀑布项目的产品信息
-r($projectTester->updateShadowProductTest($projectIdList[3])) && p('0:name') && e('产品4');          // 测试更新产品型看板项目的产品信息
+r($projectTester->updateShadowProductTest($projectIdList[0])) && p('0:id,name') && e('1,更新敏捷项目11'); // 测试更新项目型敏捷项目的产品信息
+r($projectTester->updateShadowProductTest($projectIdList[1])) && p('0:id,name') && e('2,产品2');         // 测试更新产品型瀑布项目的产品信息
+r($projectTester->updateShadowProductTest($projectIdList[2])) && p('0:id,name') && e('3,更新瀑布项目13'); // 测试更新项目型瀑布项目的产品信息
+r($projectTester->updateShadowProductTest($projectIdList[3])) && p('0:id,name') && e('4,产品4');         // 测试更新产品型看板项目的产品信息
