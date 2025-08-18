a25002db50622529f991712b3018f1c01c9fc382
yulujie <yulujie@chandao.com>
2025-08-18 14:00:42 +0800

    *[task#148341,doing,2h,2h] Modify function.

--- module/doc/test/lib/createspace.ui.class.php
+++ module/doc/test/lib/createspace.ui.class.php
@@ -20,19 +20,21 @@ class createSpaceTester extends tester
         $form->dom->name->setValue($name);
         $form->dom->xpath['saveBtn'] = "//button[@type='submit']";
         $form->dom->saveBtn->click();
+        $form->wait(1);
 
         if(empty($name))
         {
             $form = $this->loadPage();
             if(!is_object($form->dom->nameTip)) return $this->failed('空间名称为空时没有提示');
-            var_dump($form->dom->nameTip->getValue());
-            var_dump(sprintf($this->lang->error->notempty, $this->lang->doclib->spaceName));
-            if($form->dom->nameTip->getValue() != sprintf($this->lang->error->notempty, $this->lang->doclib->spaceName)) return $this->failed('空间名称为空时提示正确');
+            if($form->dom->nameTip->getText() != sprintf($this->lang->error->notempty, $this->lang->doclib->spaceName)) return $this->failed('空间名称为空时提示正确');
             return $this->success('空间名称为空时提示正确');
         }
 
+        $form = $this->initForm('doc', $spaceType, '', 'appIframe-doc');
+        $form->wait(1);
         $form->dom->xpath['newSpaceName'] = "//*[@id='mainContent']/div/div/div[2]/div[last()]//strong";
-        if(is_object($form->dom->newSpaceName) && $form->dom->newSpaceName->getValue() == $name) return $this->success('空间创建成功');
+
+        if(is_object($form->dom->newSpaceName) && $form->dom->newSpaceName->getText() == $name) return $this->success('空间创建成功');
         return $this->failed('空间创建失败');
     }
 }
