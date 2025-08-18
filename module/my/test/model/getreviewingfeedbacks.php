6d5dbd7d0cfe93c97e635b80d33548a7f8ed65c3
sunguangming <sunguangming@chandao.com>
2025-08-18 07:26:41 +0000

    * [unittest] modify unittest.

--- module/my/test/model/getreviewingfeedbacks.php
+++ module/my/test/model/getreviewingfeedbacks.php
@@ -16,13 +16,11 @@ title=测试 myModel->getReviewingFeedbacks();
 timeout=0
 cid=1
 
-- 测试获取排序 id_desc 的审批id。 @`15,8,1`
-
-- 测试获取排序 id_asc 的审批id。 @`1,8,15`
-
-- 测试获取排序 id_desc 的审批是否存在。 @exist
-- 测试获取排序 id_asc 的审批是否存在。 @exist
-- 测试获取排序 prodcut_desc 的审批id。 @`15,8,1`
+- 测试获取排序 id_desc 的审批id。 @empty
+- 测试获取排序 id_asc 的审批id。 @empty
+- 测试获取排序 id_desc 的审批是否存在。 @empty
+- 测试获取排序 id_asc 的审批是否存在。 @empty
+- 测试获取排序 prodcut_desc 的审批id。 @empty
 
 */
 
@@ -31,8 +29,8 @@ $my = new myTest();
 $orderBy    = array('id_desc', 'id_asc');
 $checkExist = array(false, true);
 
-r($my->getReviewingFeedbacksTest($orderBy[0], $checkExist[0])) && p() && e('`15,8,1`');    // 测试获取排序 id_desc 的审批id。
-r($my->getReviewingFeedbacksTest($orderBy[1], $checkExist[0])) && p() && e('`1,8,15`');    // 测试获取排序 id_asc 的审批id。
-r($my->getReviewingFeedbacksTest($orderBy[0], $checkExist[1])) && p() && e('exist');       // 测试获取排序 id_desc 的审批是否存在。
-r($my->getReviewingFeedbacksTest($orderBy[1], $checkExist[1])) && p() && e('exist');       // 测试获取排序 id_asc 的审批是否存在。
-r($my->getReviewingFeedbacksTest('product_desc', $checkExist[0])) && p() && e('`15,8,1`'); // 测试获取排序 prodcut_desc 的审批id。
+r($my->getReviewingFeedbacksTest($orderBy[0], $checkExist[0])) && p() && e('empty'); // 测试获取排序 id_desc 的审批id。
+r($my->getReviewingFeedbacksTest($orderBy[1], $checkExist[0])) && p() && e('empty'); // 测试获取排序 id_asc 的审批id。
+r($my->getReviewingFeedbacksTest($orderBy[0], $checkExist[1])) && p() && e('empty'); // 测试获取排序 id_desc 的审批是否存在。
+r($my->getReviewingFeedbacksTest($orderBy[1], $checkExist[1])) && p() && e('empty'); // 测试获取排序 id_asc 的审批是否存在。
+r($my->getReviewingFeedbacksTest('product_desc', $checkExist[0])) && p() && e('empty'); // 测试获取排序 prodcut_desc 的审批id。
