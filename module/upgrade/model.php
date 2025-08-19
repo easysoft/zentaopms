0e35463be9421a5ba6906ad4dcf13af54b343b32
wangyuting <wangyuting@chandao.com>
2025-08-19 08:47:26 +0800

    * Fix lang error.

--- module/upgrade/model.php
+++ module/upgrade/model.php
@@ -11495,7 +11495,7 @@ class upgradeModel extends model
         /* 创建其他过程分类。 */
         $module = new stdclass();
         $module->root    = $workflowGroupID;
-        $module->name    = $this->upgrade->projectManage;
+        $module->name    = $this->lang->upgrade->projectManage;
         $module->parent  = '0';
         $module->grade   = '1';
         $module->type    = 'process';
