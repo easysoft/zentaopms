16e818b3cd826c8fa2f33155dc017dc2c6a67b28
sunguangming <sunguangming@chandao.com>
2025-08-19 07:30:59 +0000

    * [unittest] modify unittest.

--- module/webhook/test/model/getfeishudata.php
+++ module/webhook/test/model/getfeishudata.php
@@ -10,26 +10,30 @@ title=测试 webhookModel->getFeishuData();
 timeout=0
 cid=1
 
-- 测试正常传入的情况第title条的content属性 @denghongtao
+- 测试正常传入的情况第title条的content属性 @test1
 - 测试不传title的情况第title条的content属性 @~~
-- 测试不传text的情况第title条的content属性 @denghongtao
+- 测试不传text的情况第title条的content属性 @test1
+- 查看tag第title条的tag属性 @plain_text
+- 查看template属性template @blue
 
 */
 
 $webhook = new webhookTest();
 
 $title = array();
-$title[0] = 'denghongtao';
+$title[0] = 'test1';
 $title[1] = '';
 
 $text  = array();
-$text[0] = 'wenbenxiaoxiang';
+$text[0] = 'test2';
 $text[1] = '';
 
 $result1 = $webhook->getFeishuDataTest($title[0], $text[0]);
 $result2 = $webhook->getFeishuDataTest($title[1], $text[0]);
 $result3 = $webhook->getFeishuDataTest($title[0], $text[1]);
 
-r($result1->card['header']) && p('title:content') && e('denghongtao'); //测试正常传入的情况
-r($result2->card['header']) && p('title:content') && e('~~');          //测试不传title的情况
-r($result3->card['header']) && p('title:content') && e('denghongtao'); //测试不传text的情况
+r($result1->card['header']) && p('title:content') && e('test1'); //测试正常传入的情况
+r($result2->card['header']) && p('title:content') && e('~~');    //测试不传title的情况
+r($result3->card['header']) && p('title:content') && e('test1'); //测试不传text的情况
+r($result3->card['header']) && p('title:tag')     && e('plain_text'); //查看tag
+r($result3->card['header']) && p('template')      && e('blue'); //查看template
