21b8affdb43a1666b7e394c9094ca1a1d9d771d2
tianshujie <tianshujie@easycorp.ltd>
2025-08-19 15:16:14 +0800

    * [task#148038,done,0.1h] add step for getprivsbymodel unittest.

--- module/project/test/model/getprivsbymodel.php
+++ module/project/test/model/getprivsbymodel.php
@@ -1,19 +1,26 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . '/test/lib/init.php';
-su('admin');
-
 /**
 
 title=测试 projectModel->getPrivsByModel();
 timeout=0
 cid=1
 
+- 传递空类型的情况 @0
+- 传递错误的类型的情况 @0
+- 获取敏捷项目的权限第bug条的delete属性 @deleteAction
+- 获取瀑布项目的权限第story条的create属性 @create
+- 获取看板项目的权限 @0
+
 */
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+su('admin');
 
 global $tester;
 $projectTester = $tester->loadModel('project');
 
-r($projectTester->getPrivsByModel(''))         && p()             && e('0');            // 传递空类型的情况
-r($projectTester->getPrivsByModel('test'))     && p()             && e('0');            // 传递错误的类型的情况
-r($projectTester->getPrivsByModel('scrum'))    && p('bug:delete') && e('deleteAction'); // 传递正确类型的情况
+r($projectTester->getPrivsByModel(''))          && p()               && e('0');            // 传递空类型的情况
+r($projectTester->getPrivsByModel('test'))      && p()               && e('0');            // 传递错误的类型的情况
+r($projectTester->getPrivsByModel('scrum'))     && p('bug:delete')   && e('deleteAction'); // 获取敏捷项目的权限
+r($projectTester->getPrivsByModel('waterfall')) && p('story:create') && e('create');       // 获取瀑布项目的权限
+r($projectTester->getPrivsByModel('kanban'))    && p()               && e('0');            // 获取看板项目的权限
