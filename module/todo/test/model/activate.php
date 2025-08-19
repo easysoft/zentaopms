ecef41066a77071fa3d3688465ce675a33f11f65
sunguangming <sunguangming@chandao.com>
2025-08-19 06:55:18 +0000

    * [unittest] modify unittest.

--- module/todo/test/model/activate.php
+++ module/todo/test/model/activate.php
@@ -22,15 +22,17 @@ cid=1
 
 function initData()
 {
-    zenData('todo')->loadYaml('activate')->gen(3);
+    zenData('todo')->loadYaml('activate')->gen(5);
 }
 
 initData();
 
-$todoIDList = array('1', '2', '3');
+$todoIDList = range(1,5);
 
 $todo = new todoTest();
 
 r($todo->activateTest($todoIDList[0])) && p('status') && e('wait'); // 激活一个状态为wait的todo
 r($todo->activateTest($todoIDList[1])) && p('status') && e('wait'); // 激活一个状态为doing的todo
 r($todo->activateTest($todoIDList[2])) && p('status') && e('wait'); // 激活一个状态为done的todo
+r($todo->activateTest($todoIDList[3])) && p('status') && e('wait'); // 激活一个状态为wait的todo
+r($todo->activateTest($todoIDList[4])) && p('status') && e('wait'); // 激活一个状态为doing的todo
