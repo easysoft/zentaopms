b736a0026143562e979bc3be50d3974c27c69326
mazhiyuan <mazhiyuan@chandao.com>
2025-08-19 13:22:36 +0800

    * [bug#61890,done,3h] fix cne->getBackupStatus param instance uninstantiated error

--- module/system/control.php
+++ module/system/control.php
@@ -362,7 +362,9 @@ class system extends control
 
         $this->loadModel('cne');
         if(strpos($backupName, '_') !== false) $backupName = str_replace('_', '-', $backupName);
-        $result = $this->cne->getBackupStatus($this->config->instance->zentaopaas, $backupName);
+        $this->loadModel('instance');
+        $instance = $this->config->instance->zentaopaas;
+        $result = $this->cne->getBackupStatus($instance, $backupName);
         if($result && $result->code == 200)
         {
             $status = strtolower($result->data->status);
