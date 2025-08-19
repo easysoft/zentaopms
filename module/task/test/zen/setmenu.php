9253bd284656a146c7632fdaef7c6235738b71d2
daitingting <daitingting@xirangit.com>
2025-08-19 11:13:06 +0800

    * [unittest,done,0.5h] Adjust setmenu test.

--- module/task/test/zen/setmenu.php
+++ module/task/test/zen/setmenu.php
@@ -10,7 +10,7 @@ $execution->status->range('doing{3},closed,doing');
 $execution->project->range('0,0,1,1,2');
 $execution->parent->range('0,0,1,1,2');
 $execution->multiple->range('1,0,1,1,0');
-$execution->grade->range('2{2},1{3}');
+$execution->grade->range('1{2},2{3}');
 $execution->path->range('1,2,`1,3`,`1,4`,`2,5`')->prefix(',')->postfix(',');
 $execution->begin->range('20230102 000000:0')->type('timestamp')->format('YY/MM/DD');
 $execution->end->range('20230212 000000:0')->type('timestamp')->format('YY/MM/DD');
@@ -23,7 +23,7 @@ title=测试 taskZen::setMenu();
 timeout=0
 cid=1
 
-- 启用迭代的项目，跳转到可访问的第一个执行下 @5
+- 启用迭代的项目，跳转到可访问的第一个执行下 @3
 - 不启用迭代的项目，直接返回当前项目的导航 @2
 - 进行中的迭代，直接返回当前迭代的导航 @3
 - 不可以修改关闭的迭代，关闭的迭代，跳转到可访问的第一个执行下 @5
@@ -40,10 +40,9 @@ $app->rawMethod   = 'task';
 $zen  = initReference('task');
 $func = $zen->getMethod('setMenu');
 
-r($func->invokeArgs($zen->newInstance(), [$executionIDList[0]])) && p() && e('5'); //启用迭代的项目，跳转到可访问的第一个执行下
+r($func->invokeArgs($zen->newInstance(), [$executionIDList[0]])) && p() && e('3'); //启用迭代的项目，跳转到可访问的第一个执行下
 r($func->invokeArgs($zen->newInstance(), [$executionIDList[1]])) && p() && e('2'); //不启用迭代的项目，直接返回当前项目的导航
 r($func->invokeArgs($zen->newInstance(), [$executionIDList[2]])) && p() && e('3'); //进行中的迭代，直接返回当前迭代的导航
-$config->CRExecution = 0;
 r($func->invokeArgs($zen->newInstance(), [$executionIDList[3]])) && p() && e('5'); //不可以修改关闭的迭代，关闭的迭代，跳转到可访问的第一个执行下
 $config->CRExecution = 1;
 r($func->invokeArgs($zen->newInstance(), [$executionIDList[3]])) && p() && e('4'); //可以修改关闭的迭代，关闭的迭代，直接返回当前迭代的导航
