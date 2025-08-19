015a45c2df29fdf92fe47c4edb45606da0c9148a
tianshujie <tianshujie@easycorp.ltd>
2025-08-19 13:24:47 +0800

    * [task#148035,done,0.2h] add step for getExecutionMembers method.

--- module/project/test/model/getexecutionmembers.php
+++ module/project/test/model/getexecutionmembers.php
@@ -1,5 +1,18 @@
 #!/usr/bin/env php
 <?php
+/**
+
+title=测试 projectModel->getExecutionMembers();
+timeout=0
+cid=1
+
+- 获取admin的执行键值对总数 @2
+- 获取admin的执行键值对 @迭代1-2
+- 获取admin的执行键值对 @迭代6-7
+- 获取user1的执行键值对总数 @1
+- 获取user1的执行键值对 @迭代2-4
+
+*/
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 su('admin');
 
@@ -7,19 +20,16 @@ zenData('project')->loadYaml('project')->gen(7);
 zenData('team')->loadYaml('team')->gen(4);
 zenData('user')->loadYaml('user')->gen(3);
 
-/**
-
-title=测试 projectModel->getExecutionMembers();
-timeout=0
-cid=1
-
-*/
-
 global $tester;
 $tester->loadModel('project');
 
-$executionIdList  = array(2, 4, 5, 7);
-$executionMembers = $tester->project->getExecutionMembers('admin', $executionIdList);
+$executionIdList = array(2, 4, 5, 7);
+$adminExecutions = $tester->project->getExecutionMembers('admin', $executionIdList);
+$user1Executions = $tester->project->getExecutionMembers('user1', $executionIdList);
 
-r(count($executionMembers)) && p() && e('2');       // 获取admin的执行键值对总数
-r($executionMembers[2])     && p() && e('迭代1-2'); // 获取admin的执行键值对
+r(count($adminExecutions)) && p() && e('2');       // 获取admin的执行键值对总数
+r($adminExecutions[2])     && p() && e('迭代1-2'); // 获取admin的执行键值对
+r($adminExecutions[7])     && p() && e('迭代6-7'); // 获取admin的执行键值对
+
+r(count($user1Executions)) && p() && e('1');       // 获取user1的执行键值对总数
+r($user1Executions[4])     && p() && e('迭代2-4'); // 获取user1的执行键值对
