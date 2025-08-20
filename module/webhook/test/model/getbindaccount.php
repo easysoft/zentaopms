89e818cb9ea5dc32daede3bc9d255a695e05be59
sunguangming <sunguangming@chandao.com>
2025-08-20 00:52:07 +0000

    * [unittest] modify unittest.

--- module/webhook/test/model/getbindaccount.php
+++ module/webhook/test/model/getbindaccount.php
@@ -12,8 +12,11 @@ title=测试 webhookModel->getBindAccount();
 timeout=0
 cid=1
 
-- 按条件查出id=1的关联用户 @user3
-- 按条件查出id=空时，关联的用户 @0
+- 按条件查出openID=1的关联用户 @user3
+- 按条件查出openID=空时，关联的用户 @0
+- 按条件查出openID=8的关联用户 @user10
+- 按条件查出openID=10的关联用户 @user12
+- 按条件查出openID=3的关联用户 @user5
 
 */
 
@@ -32,8 +35,14 @@ $openID = array();
 $openID[0] = 1;
 $openID[1] = '';
 
-$result1 = $webhook->getBindAccountTest($ID[0], $type[0], $openID[0]);
-$result2 = $webhook->getBindAccountTest($ID[1], $type[1], $openID[1]);
+$result1 = $webhook->getBindAccountTest(1, 'gitlab', 1);
+$result2 = $webhook->getBindAccountTest(1, '', 1);
+$result3 = $webhook->getBindAccountTest(1, 'gogs', 8);
+$result4 = $webhook->getBindAccountTest(1, 'webhook', 10);
+$result5 = $webhook->getBindAccountTest(1, 'gitea', 3);
 
-r($result1) && p() && e('user3'); //按条件查出id=1的关联用户
-r($result2) && p() && e('0');     //按条件查出id=空时，关联的用户
+r($result1) && p() && e('user3'); //按条件查出openID=1的关联用户
+r($result2) && p() && e('0');     //按条件查出openID=空时，关联的用户
+r($result3) && p() && e('user10'); //按条件查出openID=8的关联用户
+r($result4) && p() && e('user12'); //按条件查出openID=10的关联用户
+r($result5) && p() && e('user5'); //按条件查出openID=3的关联用户
