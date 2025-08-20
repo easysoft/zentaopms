277c3eb9afba40d7ef5558a08384f23ad312e6d7
sunguangming <sunguangming@chandao.com>
2025-08-20 07:14:05 +0000

    * [unittest] modify unittest.

--- module/tree/test/model/updateorder.php
+++ module/tree/test/model/updateorder.php
@@ -26,6 +26,7 @@ $orders1 = array('12' => 1, '16' => 4, '32' => 2);
 $orders2 = array('11' => 7, '18' => 2, '31' => 5);
 $orders3 = array('12' => 7, '2' => 6,  '32' => 5); // 2是branch0， 12 32是分支1
 $orders4 = array('11' => 7, '1' => 6,  '31' => 5); // 1是branch0， 11 31是分支1
+$orders5 = array('13' => 8, '14' => 9, '15' => 10);
 
 $tree = new treeTest();
 
@@ -33,3 +34,4 @@ r($tree->updateOrderTest($orders1)) && p() && e(',12,32,16'); // 测试更新sto
 r($tree->updateOrderTest($orders2)) && p() && e(',18,31,11'); // 测试更新task模块顺序
 r($tree->updateOrderTest($orders3)) && p() && e(',2,32,12');  // 测试更新story不同分支模块顺序
 r($tree->updateOrderTest($orders4)) && p() && e(',1,31,11');  // 测试更新task不同分支模块顺序
+r($tree->updateOrderTest($orders5)) && p() && e(',13,14,15');  // 测试更新模块顺序
