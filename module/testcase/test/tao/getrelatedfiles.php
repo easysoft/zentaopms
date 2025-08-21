c8a3c2e50513648c9423b7cb95ef38cb4c3ac176
sunguangming <sunguangming@chandao.com>
2025-08-21 07:18:46 +0000

    * [unittest] modify unittest.

--- module/testcase/test/tao/getrelatedfiles.php
+++ module/testcase/test/tao/getrelatedfiles.php
@@ -27,6 +27,10 @@ timeout=0
 cid=1
 
 - 测试用例1的附件数 @3
+- 测试用例1的附件数
+ - 第1条的id属性 @1
+ - 第1条的title属性 @文件标题1
+ - 第1条的extension属性 @txt
 - 测试用例2的附件数 @7
 
 */
@@ -37,4 +41,5 @@ global $tester;
 $tester->loadModel('testcase');
 
 r(count($tester->testcase->getRelatedFiles($cases)[1])) && p('') && e('3'); // 测试用例1的附件数
+r($tester->testcase->getRelatedFiles($cases)[1])        && p('1:id,title,extension') && e('1,文件标题1,txt'); // 测试用例1的附件数
 r(count($tester->testcase->getRelatedFiles($cases)[2])) && p('') && e('7'); // 测试用例2的附件数
