6a9b21319857d886d2618d25723700530b169ccd
wangyuting <wangyuting@chandao.com>
2025-08-21 15:48:37 +0800

    * Optimize getplanlist unit test script.

--- module/product/test/tao/getplanlist.php
+++ module/product/test/tao/getplanlist.php
@@ -4,9 +4,19 @@
 /**
 
 title=productTao->getPlanList();
+timeout=0
 cid=0
 
 - 测试传入空的产品ID列表 @0
+- 测试传入产品ID列表
+ - 第5条的product属性 @2
+ - 第5条的title属性 @计划5
+- 测试传入产品ID列表
+ - 第10条的product属性 @4
+ - 第10条的title属性 @计划10
+- 测试传入产品ID列表
+ - 第15条的product属性 @5
+ - 第15条的title属性 @计划15
 - 测试传入产品ID列表
  - 第30条的product属性 @10
  - 第30条的title属性 @计划30
@@ -26,5 +36,8 @@ $productIdList[2] = range(11, 20);
 global $tester;
 $tester->loadModel('product');
 r($tester->product->getPlanList($productIdList[0]))     && p()                   && e('0');         // 测试传入空的产品ID列表
+r($tester->product->getPlanList($productIdList[1])[2])  && p('5:product,title')  && e('2,计划5');   // 测试传入产品ID列表
+r($tester->product->getPlanList($productIdList[1])[4])  && p('10:product,title') && e('4,计划10');  // 测试传入产品ID列表
+r($tester->product->getPlanList($productIdList[1])[5])  && p('15:product,title') && e('5,计划15');  // 测试传入产品ID列表
 r($tester->product->getPlanList($productIdList[1])[10]) && p('30:product,title') && e('10,计划30'); // 测试传入产品ID列表
 r($tester->product->getPlanList($productIdList[2]))     && p()                   && e('0');         // 测试传入不存在产品ID列表
