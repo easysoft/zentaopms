e215aea57e897e373c24ae03744ecf4e4f554e87
sunguangming <sunguangming@chandao.com>
2025-08-21 06:36:04 +0000

    * [unittest] modify unittest.

--- module/testsuite/test/model/deletecasebysuiteid.php
+++ module/testsuite/test/model/deletecasebysuiteid.php
@@ -5,7 +5,7 @@ include dirname(__FILE__, 2) . '/lib/testsuite.unittest.class.php';
 
 zenData('case')->gen(20);
 zenData('suitecase')->gen(3);
-zenData('testsuite')->gen(1);
+zenData('testsuite')->gen(2);
 zenData('user')->gen(1);
 
 su('admin');
@@ -13,19 +13,28 @@ su('admin');
 /**
 
 title=测试 testsuiteModel->deleteCaseBySuiteIDTest();
+timeout=0
 cid=1
-pid=1
 
-检查当前套件的关联用例                >> 2
-测试suiteiD值为1,cases值为array(1, 2) >> 2
-检查当前套件的关联用例数量            >> 0
+- 检查当前关联的用例数量 @2
+- 测试suiteID值为1 @2
+- 检查当前关联的用例数量 @0
+- 检查当前关联的用例数量 @1
+- 测试suiteID值为2 @1
+- 检查当前关联的用例数量 @0
 
 */
-$suiteID    = 1;
-$cases = array(1, 2);
+$suiteID = 1;
+$cases   = array(1, 2);
 
 $testsuite = new testsuiteTest();
 
-r(count($testsuite->getLinkedCasesTest($suiteID)))       && p() && e('2');        //检查当前关联的用例数量
-r($testsuite->deleteCaseBySuiteIDTest($cases, $suiteID)) && p() && e('2');        //测试suiteID值为1
-r(count($testsuite->getLinkedCasesTest($suiteID)))       && p() && e('0');        //检查当前关联的用例数量
+r(count($testsuite->getLinkedCasesTest($suiteID)))       && p() && e('2'); //检查当前关联的用例数量
+r($testsuite->deleteCaseBySuiteIDTest($cases, $suiteID)) && p() && e('2'); //测试suiteID值为1
+r(count($testsuite->getLinkedCasesTest($suiteID)))       && p() && e('0'); //检查当前关联的用例数量
+
+$suiteID = 2;
+$cases   = array(3);
+r(count($testsuite->getLinkedCasesTest($suiteID)))       && p() && e('1'); //检查当前关联的用例数量
+r($testsuite->deleteCaseBySuiteIDTest($cases, $suiteID)) && p() && e('1'); //测试suiteID值为2
+r(count($testsuite->getLinkedCasesTest($suiteID)))       && p() && e('0'); //检查当前关联的用例数量
