15957f812073064542ef8bc1ca6319e668146258
sunguangming <sunguangming@chandao.com>
2025-08-21 05:17:44 +0000

    * [unittest] modify unittest.

--- module/tutorial/test/model/gettutorialed.php
+++ module/tutorial/test/model/gettutorialed.php
@@ -1,13 +1,18 @@
 #!/usr/bin/env php
 <?php
 declare(strict_types=1);
+
 /**
 
 title=测试 tutorialModel->getTutorialed();
+timeout=0
 cid=1
 
 - 测试是否能拿到 admin 的数据 @3
 - 测试是否能拿到 user1 的数据 @4
+- 测试是否能拿到 user2 的数据 @0
+- 测试是否能拿到 user3 的数据 @0
+- 测试是否能拿到 user4 的数据 @0
 
 */
 
@@ -24,3 +29,12 @@ r($tutorial->getTutorialedTest()) && p() && e('3'); // 测试是否能拿到 adm
 
 su('user1');
 r($tutorial->getTutorialedTest()) && p() && e('4'); // 测试是否能拿到 user1 的数据
+
+su('user2');
+r($tutorial->getTutorialedTest()) && p() && e('0'); // 测试是否能拿到 user2 的数据
+
+su('user3');
+r($tutorial->getTutorialedTest()) && p() && e('0'); // 测试是否能拿到 user3 的数据
+
+su('user4');
+r($tutorial->getTutorialedTest()) && p() && e('0'); // 测试是否能拿到 user4 的数据
