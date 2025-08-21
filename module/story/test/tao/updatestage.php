1618f3e388c093845e3ceac6c4c97ba546bb9f28
sunguangming <sunguangming@chandao.com>
2025-08-21 08:57:06 +0000

    * [unittest] modify unittest.

--- module/story/test/tao/updatestage.php
+++ module/story/test/tao/updatestage.php
@@ -8,6 +8,28 @@ timeout=0
 cid=0
 
 - 不传入任何数据。 @0
+- 传入不是多分支产品的需求，检查需求的阶段。属性stage @developed
+- 传入从属多分支的需求，检查需求的阶段。属性stage @developed
+- 传入属于主干需求，检查需求的阶段。属性stage @developed
+- 传入属于主干需求，检查 storystage 记录数。 @2
+- 传入属于主干需求，检查分支为 0 的 storystage 记录。
+ - 属性story @2
+ - 属性branch @0
+ - 属性stage @developed
+- 传入属于主干需求，检查分支为 1 的 storystage 记录。
+ - 属性story @2
+ - 属性branch @1
+ - 属性stage @testing
+- 传入属于主干需求，并传入 oldStages 数据，检查需求的阶段。属性stage @developing
+- 传入属于主干需求，并传入 oldStages 数据，检查 storystage 记录数。 @2
+- 传入属于主干需求，并传入 oldStages 数据，检查分支为 0 的 storystage 记录。
+ - 属性story @2
+ - 属性branch @0
+ - 属性stage @developed
+- 传入属于主干需求，并传入 oldStages 数据，检查分支为 1 的 storystage 记录。
+ - 属性story @2
+ - 属性branch @1
+ - 属性stage @developing
 
 */
 include dirname(__FILE__, 5) . '/test/lib/init.php';
@@ -31,14 +53,14 @@ $stages[1] = 'testing';
 r($storyModel->updateStage(0, $stages)) && p() && e('0'); //不传入任何数据。
 
 $storyTest = new storyTest();
-r($storyTest->updateStageTest(1, $stages)) && p('stage') && r('developed'); //传入不是多分支产品的需求，检查需求的阶段。
-r($storyTest->updateStageTest(3, $stages)) && p('stage') && r('developed'); //传入从属多分支的需求，检查需求的阶段。
+r($storyTest->updateStageTest(1, $stages)) && p('stage') && e('developed'); //传入不是多分支产品的需求，检查需求的阶段。
+r($storyTest->updateStageTest(3, $stages)) && p('stage') && e('developed'); //传入从属多分支的需求，检查需求的阶段。
 
 $story = $storyTest->updateStageTest(2, $stages);
-r($story) && p('stage') && r('developed');                             //传入属于主干需求，检查需求的阶段。
-r(count($story->stages)) && p() && r('2');                             //传入属于主干需求，检查 storystage 记录数。
-r($story->stages[0]) && p('story,branch,stage') && r('2,0,developed'); //传入属于主干需求，检查分支为 0 的 storystage 记录。
-r($story->stages[1]) && p('story,branch,stage') && r('2,1,testing');   //传入属于主干需求，检查分支为 1 的 storystage 记录。
+r($story) && p('stage') && e('developed');                             //传入属于主干需求，检查需求的阶段。
+r(count($story->stages)) && p() && e('2');                             //传入属于主干需求，检查 storystage 记录数。
+r($story->stages[0]) && p('story,branch,stage') && e('2,0,developed'); //传入属于主干需求，检查分支为 0 的 storystage 记录。
+r($story->stages[1]) && p('story,branch,stage') && e('2,1,testing');   //传入属于主干需求，检查分支为 1 的 storystage 记录。
 
 $oldStages = array();
 $oldStages[1] = new stdclass();
@@ -48,7 +70,7 @@ $oldStages[1]->stage    = 'developing';
 $oldStages[1]->stagedBy = 'admin';
 
 $story = $storyTest->updateStageTest(2, $stages, $oldStages);
-r($story) && p('stage') && r('developing');                             //传入属于主干需求，并传入 oldStages 数据，检查需求的阶段。
-r(count($story->stages)) && p() && r('2');                              //传入属于主干需求，并传入 oldStages 数据，检查 storystage 记录数。
-r($story->stages[0]) && p('story,branch,stage') && r('2,0,developed');  //传入属于主干需求，并传入 oldStages 数据，检查分支为 0 的 storystage 记录。
-r($story->stages[1]) && p('story,branch,stage') && r('2,1,developing'); //传入属于主干需求，并传入 oldStages 数据，检查分支为 1 的 storystage 记录。
+r($story) && p('stage') && e('developing');                             //传入属于主干需求，并传入 oldStages 数据，检查需求的阶段。
+r(count($story->stages)) && p() && e('2');                              //传入属于主干需求，并传入 oldStages 数据，检查 storystage 记录数。
+r($story->stages[0]) && p('story,branch,stage') && e('2,0,developed');  //传入属于主干需求，并传入 oldStages 数据，检查分支为 0 的 storystage 记录。
+r($story->stages[1]) && p('story,branch,stage') && e('2,1,developing'); //传入属于主干需求，并传入 oldStages 数据，检查分支为 1 的 storystage 记录。
