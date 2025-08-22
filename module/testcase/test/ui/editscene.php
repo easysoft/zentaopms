a21a4543be9864c9d28f5fe36bd52e9351596768
lihuifang <lihuifang@chandao.com>
2025-08-22 13:55:06 +0800

    *[task#62057,doing,0.5h,2h] Add uitest of create scene.

--- module/testcase/test/ui/editscene.php
+++ module/testcase/test/ui/editscene.php
@@ -46,3 +46,38 @@ $scene->sort->range('1,2');
 $scene->openedBy->range('admin');
 $scene->grade->range('1,1');
 $scene->path->range(',1,',',2,');
+$scene->gen(2);
+
+$module = zenData('module');
+$module->id->range('1,2');
+$module->root->range('1,1');
+$module->name->range('模块1,模块2');
+$module->parent->range('0');
+$module->path->range(',1,',',2,');
+$module->grade->range('1,1');
+$module->type->range('case');
+$module->order->range('10,20');
+$module->gen(2);
+
+$tester = new editSceneTester();
+$tester->login();
+
+//设置数据
+$scene = array(
+    '0' => array(
+        'scene'   => '',
+    ),
+    '1' => array(
+        'scene'   => '场景2',
+    ),
+    '2' => array(
+        'module'  => '模块2',
+        'scene'   => '场景--编辑',
+    ),
+);
+
+r($tester->editScene($scene['0'])) && p('message,status') && e('编辑场景必填提示信息正确,SUCCESS');       // 编辑场景必填信息检查
+r($tester->editScene($scene['1'])) && p('message,status') && e('编辑场景名称重复时提示信息正确,SUCCESS'); // 编辑场景名称重复时提示信息检查
+r($tester->editScene($scene['2'])) && p('message,status') && e('场景编辑成功,SUCCESS');                   // 编辑场景
+
+$tester->closeBrowser();
