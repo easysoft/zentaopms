9150308f75bc1d5d9611ebe75901c3d9a64d6992
sunguangming <sunguangming@chandao.com>
2025-08-22 08:16:44 +0000

    * [unittest] modify unittest.

--- module/story/test/model/getstoryspecs.php
+++ module/story/test/model/getstoryspecs.php
@@ -4,10 +4,15 @@
 /**
 
 title=测试 storyModel->getStorySpecs();
+timeout=0
 cid=0
 
-- 根据传入的需求ID列表，获取最新版本的需求描述信息第1条的spec属性 @这是一个软件需求描述1
-- 根据传入的需求ID列表，获取最新版本的验收标准信息第15条的verify属性 @这是一个需求验收15
+- 根据传入的需求ID列表，获取最新版本的需求描述信息
+ - 第1条的spec属性 @这是一个软件需求描述1
+ - 第1条的story属性 @1
+- 根据传入的需求ID列表，获取最新版本的验收标准信息
+ - 第15条的verify属性 @这是一个需求验收15
+ - 第15条的story属性 @15
 - 传入三个需求ID，判断获取到的需求描述信息数量 @2
 
 */
@@ -22,6 +27,6 @@ $storyIdList = array(1, 15, 100);
 
 $storyspecs = $story->getStorySpecsTest($storyIdList);
 
-r($storyspecs)        && p('1:spec')     && e('这是一个软件需求描述1'); //根据传入的需求ID列表，获取最新版本的需求描述信息
-r($storyspecs)        && p('15:verify')  && e('这是一个需求验收15');    //根据传入的需求ID列表，获取最新版本的验收标准信息
-r(count($storyspecs)) && p()             && e('2');                     //传入三个需求ID，判断获取到的需求描述信息数量
+r($storyspecs)        && p('1:spec,story')    && e('这是一个软件需求描述1,1'); //根据传入的需求ID列表，获取最新版本的需求描述信息
+r($storyspecs)        && p('15:verify,story') && e('这是一个需求验收15,15');  //根据传入的需求ID列表，获取最新版本的验收标准信息
+r(count($storyspecs)) && p()                  && e('2');                     //传入三个需求ID，判断获取到的需求描述信息数量
