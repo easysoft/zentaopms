abe539946f90b8abb41f39bd98c3dcd09f1dc1e0
wangyuting <wangyuting@chandao.com>
2025-08-22 15:52:00 +0800

    * Optimize update unit test script.

--- module/program/test/model/update.php
+++ module/program/test/model/update.php
@@ -1,5 +1,20 @@
 #!/usr/bin/env php
 <?php
+
+/**
+
+title=测试 programModel::update();
+timeout=0
+cid=1
+
+- 正常更新项目集的情况属性name @测试更新项目集
+- 正常更新项目集的情况属性status @doing
+- 更新项目集名称为空时第message[name]条的0属性 @『项目集名称』不能为空。
+- 当计划开始为空时更新项目集信息第message[begin]条的0属性 @『计划开始』不能为空。
+- 当计划完成为空时更新项目集信息第message[end]条的0属性 @『计划完成』不能为空。
+
+*/
+
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/lib/program.unittest.class.php';
 su('admin');
@@ -13,19 +28,6 @@ $program->begin->range('20220112 000000:0')->type('timestamp')->format('YYYY-MM-
 $program->end->range('20220212 000000:0')->type('timestamp')->format('YYYY-MM-DD');
 $program->status->range('wait');
 $program->gen(1);
-
-/**
-
-title=测试 programModel::update();
-cid=1
-pid=1
-
-正常更新项目集的情况 >> 测试更新项目集
-更新项目集名称为空时 >> 『项目集名称』不能为空。
-当计划开始为空时更新项目集信息 >> 『计划开始』不能为空。
-当计划完成为空时更新项目集信息 >> 『计划完成』不能为空。
-
-*/
 
 $programTester = new programTest();
 
@@ -46,6 +48,9 @@ $data = array(
 
 $normalProgram = $data;
 
+$doingProgram = $data;
+$doingProgram['status'] = 'doing';
+
 $emptyTitleProgram = $data;
 $emptyTitleProgram['name'] = '';
 
@@ -56,6 +61,7 @@ $emptyEndProgram = $data;
 $emptyEndProgram['end'] = '';
 
 r($programTester->updateTest(1, $normalProgram))      && p('name')              && e('测试更新项目集');           // 正常更新项目集的情况
+r($programTester->updateTest(1, $doingProgram))       && p('status')            && e('doing');                    // 正常更新项目集的情况
 r($programTester->updateTest(1, $emptyTitleProgram))  && p('message[name]:0')   && e('『项目集名称』不能为空。'); // 更新项目集名称为空时
 r($programTester->updateTest(1, $emptyBeginProgram))  && p('message[begin]:0')  && e('『计划开始』不能为空。');   // 当计划开始为空时更新项目集信息
 r($programTester->updateTest(1, $emptyEndProgram))    && p('message[end]:0')    && e('『计划完成』不能为空。');   // 当计划完成为空时更新项目集信息
