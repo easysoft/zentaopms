0e4d337f126231cb095ad8461f63765a4fe0c906
wangyuting <wangyuting@chandao.com>
2025-08-22 16:17:37 +0800

    * Optimize buildprojectactionsmap unit test script.

--- module/program/test/tao/buildprojectactionsmap.php
+++ module/program/test/tao/buildprojectactionsmap.php
@@ -1,11 +1,5 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . '/test/lib/init.php';
-include dirname(__FILE__, 2) . '/lib/program.unittest.class.php';
-
-zenData('user')->gen(5);
-zenData('project')->loadYaml('program')->gen(40);
-su('admin');
 
 /**
 
@@ -13,11 +7,24 @@ title=测试 programTao::buildProjectActionsMap();
 timeout=0
 cid=1
 
+- 测试生成项目集id为1的操作按钮数据。第0条的name属性 @team
+- 测试生成项目集id为1的操作按钮数据。第0条的name属性 @team
+- 测试生成项目集id为2的操作按钮数据。第0条的name属性 @team
+- 测试生成敏捷项目的操作按钮数据。第0条的name属性 @team
+- 测试生成敏捷项目的操作按钮数据。第0条的name属性 @team
+
 */
 
-$projectIdList = array(11, 60, 100);
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+include dirname(__FILE__, 2) . '/lib/program.unittest.class.php';
+
+zenData('user')->gen(5);
+zenData('project')->loadYaml('program')->gen(40);
+su('admin');
 
 $programTester = new programTest();
-r($programTester->buildProjectActionsMapTest($projectIdList[0])) && p('0:name') && e('team'); // 测试生成项目集id为1的操作按钮数据。
-r($programTester->buildProjectActionsMapTest($projectIdList[1])) && p('0:name') && e('team'); // 测试生成项目集id为2的操作按钮数据。
-r($programTester->buildProjectActionsMapTest($projectIdList[2])) && p('0:name') && e('team'); // 测试生成敏捷项目的操作按钮数据。
+r($programTester->buildProjectActionsMapTest(11))  && p('0:name') && e('team'); // 测试生成项目集id为1的操作按钮数据。
+r($programTester->buildProjectActionsMapTest(61))  && p('0:name') && e('team'); // 测试生成项目集id为1的操作按钮数据。
+r($programTester->buildProjectActionsMapTest(60))  && p('0:name') && e('team'); // 测试生成项目集id为2的操作按钮数据。
+r($programTester->buildProjectActionsMapTest(100)) && p('0:name') && e('team'); // 测试生成敏捷项目的操作按钮数据。
+r($programTester->buildProjectActionsMapTest(101)) && p('0:name') && e('team'); // 测试生成敏捷项目的操作按钮数据。
