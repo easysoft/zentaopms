f8aefa853f25749d7022a593532bc9c650eea0de
sunguangming <sunguangming@chandao.com>
2025-08-22 01:09:14 +0000

    * [unittest] modify unittest.

--- module/story/test/tao/getaffectedcases.php
+++ module/story/test/tao/getaffectedcases.php
@@ -4,10 +4,19 @@
 /**
 
 title=测试 storyModel->getAffectedCases();
+timeout=0
 cid=0
 
 - 获取需求2关联用例数 @3
+- 获取需求2关联的第1条用例
+ - 第0条的id属性 @1
+ - 第0条的title属性 @这个是测试用例1
+ - 第0条的pri属性 @1
 - 获取需求28关联用例数，包含孪生需求 @4
+- 获取需求2关联的第2条用例
+ - 第1条的id属性 @29
+ - 第1条的title属性 @这个是测试用例29
+ - 第1条的pri属性 @1
 
 */
 include dirname(__FILE__, 5) . "/test/lib/init.php";
@@ -38,4 +47,6 @@ $affectedStory2  = $story->getAffectedCasesTest(2);
 $affectedStory28 = $story->getAffectedCasesTest(28);
 
 r(count($affectedStory2->cases))  && p() && e('3');  //获取需求2关联用例数
+r($affectedStory2->cases) && p('0:id,title,pri') && e('1,这个是测试用例1,1'); //获取需求2关联的第1条用例
 r(count($affectedStory28->cases)) && p() && e('4');  //获取需求28关联用例数，包含孪生需求
+r($affectedStory28->cases) && p('1:id,title,pri') && e('29,这个是测试用例29,1'); //获取需求2关联的第2条用例
