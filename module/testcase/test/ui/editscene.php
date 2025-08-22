3855b9571f0a9e328eb952f36e1241d3922e88a8
lihuifang <lihuifang@chandao.com>
2025-08-22 13:54:37 +0800

    *[task#62057,doing,0.5h,2.5h] Add zendata.

---
+++ module/testcase/test/ui/editscene.php
@@ -0,0 +1,48 @@
+#!/usr/bin/env php
+<?php
+
+/**
+
+title=建场景
+timeout=0
+cid=73
+
+- 编辑场景必填信息检查
+ - 测试结果 @编辑场景必填提示信息正确
+ - 最终测试状态 @SUCCESS
+- 编辑场景名称重复时提示信息检查
+ - 测试结果 @编辑场景名称重复时提示信息正确
+ - 最终测试状态 @SUCCESS
+- 编辑场景
+ - 测试结果 @场景编辑成功
+ - 最终测试状态 @SUCCESS
+
+*/
+chdir(__DIR__);
+include '../lib/editscene.ui.class.php';
+
+$module = zenData('module');
+$module->gen(0);
+
+$scene = zenData('scene');
+$scene->gen(0);
+
+$product = zenData('product');
+$product->id->range('1');
+$product->program->range('0');
+$product->name->range('产品1');
+$product->shadow->range('0');
+$product->bind->range('0');
+$product->acl->range('open');
+$product->createdBy->range('admin');
+$product->vision->range('rnd');
+$product->gen(1);
+
+$scene = zenData('scene');
+$scene->id->range('1,2');
+$scene->module->range('1');
+$scene->title->range('场景1,场景2');
+$scene->sort->range('1,2');
+$scene->openedBy->range('admin');
+$scene->grade->range('1,1');
+$scene->path->range(',1,',',2,');
