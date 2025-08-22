57808596df22dce93b75da25e162e5aea681e230
wangyuting <wangyuting@chandao.com>
2025-08-22 16:13:38 +0800

    * Optimize updateorder unit test script.

--- module/program/test/model/updateorder.php
+++ module/program/test/model/updateorder.php
@@ -1,10 +1,5 @@
 #!/usr/bin/env php
 <?php
-include dirname(__FILE__, 5) . '/test/lib/init.php';
-include dirname(__FILE__, 2) . '/lib/program.unittest.class.php';
-su('admin');
-
-zenData('project')->gen(10);
 
 /**
 
@@ -12,13 +7,24 @@ title=测试 programModel::updateOrder();
 timeout=0
 cid=1
 
+- 设置排序是1属性order @1
+- 设置排序是5属性order @5
+- 设置排序是10属性order @10
+- 设置空的项目集属性order @0
+- 设置不存在的项目集属性order @0
+
 */
+
+include dirname(__FILE__, 5) . '/test/lib/init.php';
+include dirname(__FILE__, 2) . '/lib/program.unittest.class.php';
+su('admin');
+
+zenData('project')->gen(10);
 
 $programTester = new programTest();
 
-r($programTester->updateOrderTest(10, 1)) && p('order') && e('1'); // 设置排序是1
-r($programTester->updateOrderTest(10, 5)) && p('order') && e('5'); // 设置排序是5
-
-
-r($programTester->updateOrderTest(0, 1))  && p('order') && e('0'); // 设置空的项目集
-r($programTester->updateOrderTest(11, 1)) && p('order') && e('0'); // 设置不存在的项目集
+r($programTester->updateOrderTest(10, 1))  && p('order') && e('1');  // 设置排序是1
+r($programTester->updateOrderTest(10, 5))  && p('order') && e('5');  // 设置排序是5
+r($programTester->updateOrderTest(10, 10)) && p('order') && e('10'); // 设置排序是10
+r($programTester->updateOrderTest(0, 1))   && p('order') && e('0');  // 设置空的项目集
+r($programTester->updateOrderTest(11, 1))  && p('order') && e('0');  // 设置不存在的项目集
