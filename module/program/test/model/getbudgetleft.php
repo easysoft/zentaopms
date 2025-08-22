b8baa34a0599df04f22c5629da14f77994ed9a01
wangyuting <wangyuting@chandao.com>
2025-08-22 14:58:32 +0800

    * Optimize getbudgetleft unit test script.

--- module/program/test/model/getbudgetleft.php
+++ module/program/test/model/getbudgetleft.php
@@ -1,33 +1,33 @@
 #!/usr/bin/env php
 <?php
+
+/**
+
+title=测试 programModel::getBudgetLeft();
+timeout=0
+cid=1
+
+- 查看项目集1的所有父项目集的预算剩余 @1000
+- 查看项目集2的所有父项目集的预算剩余 @2000
+- 查看项目集3的所有父项目集的预算剩余 @3000
+- 查看项目集4的所有父项目集的预算剩余 @4000
+- 查看项目集5的所有父项目集的预算剩余 @5000
+
+*/
+
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/lib/program.unittest.class.php';
 su('admin');
 
 $program = zenData('project');
-$program->id->range('1,2');
-$program->name->range('父项目集1,父项目集2');
+$program->id->range('1,2,3,4,5');
 $program->type->range('program');
-$program->budget->range('900000,899900');
-$program->path->range('1,2')->prefix(',')->postfix(',');
-$program->begin->range('20220112 000000:0')->type('timestamp')->format('YYYY-MM-DD');
-$program->end->range('20220212 000000:0')->type('timestamp')->format('YYYY-MM-DD');
-$program->gen(2);
-
-/**
-
-title=测试 programModel::getBudgetLeft();
-cid=1
-pid=1
-
-查看项目集1的所有父项目集的预算剩余 >> 900000
-查看项目集2的所有父项目集的预算剩余 >> 899900
-
-*/
+$program->budget->range('1000,2000,3000,4000,5000');
+$program->gen(5);
 
 $programTester = new programTest();
-$result1       = $programTester->getBudgetLeftTest(1);
-$result2       = $programTester->getBudgetLeftTest(2);
-
-r($result1) && p() && e('900000');  // 查看项目集1的所有父项目集的预算剩余
-r($result2) && p() && e('899900');  // 查看项目集2的所有父项目集的预算剩余
+r($programTester->getBudgetLeftTest(1)) && p() && e('1000');  // 查看项目集1的所有父项目集的预算剩余
+r($programTester->getBudgetLeftTest(2)) && p() && e('2000');  // 查看项目集2的所有父项目集的预算剩余
+r($programTester->getBudgetLeftTest(3)) && p() && e('3000');  // 查看项目集3的所有父项目集的预算剩余
+r($programTester->getBudgetLeftTest(4)) && p() && e('4000');  // 查看项目集4的所有父项目集的预算剩余
+r($programTester->getBudgetLeftTest(5)) && p() && e('5000');  // 查看项目集5的所有父项目集的预算剩余
