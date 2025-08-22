44f1f7b856d5a34e2bc277a8576385d7999c59a8
lihuifang <lihuifang@chandao.com>
2025-08-22 13:58:18 +0800

    *[task#62057,doing,0.2h,0.2h] Add xpath of browseScene.

--- module/testcase/test/ui/page/browsescene.php
+++ module/testcase/test/ui/page/browsescene.php
@@ -5,6 +5,7 @@ class browseScenePage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
+            'editBtn'      => "//*[@id='scenes']/div[2]/div[3]/div/div/div/nav/a[1]",
         );
 
         $this->dom->xpath = array_merge($this->dom->xpath, $xpath);
