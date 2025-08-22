6691d88b0394ec0252fed8c564297906b1f64de0
sunguangming <sunguangming@chandao.com>
2025-08-22 00:53:12 +0000

    * [misc] add release info.

--- module/upgrade/config.php
+++ module/upgrade/config.php
@@ -71,7 +71,8 @@ $config->upgrade->maxVersion['max6_7']         = '21_7';
 $config->upgrade->maxVersion['max7_0']         = '21_7_1';
 $config->upgrade->maxVersion['max7_1']         = '21_7_2';
 $config->upgrade->maxVersion['max7_2']         = '21_7_3';
-$config->upgrade->maxVersion['max7_3']         = '21_7_4'; // max insert position.
+$config->upgrade->maxVersion['max7_3']         = '21_7_4';
+$config->upgrade->maxVersion['max7_4']         = '21_7_5'; // max insert position.
 
 $config->upgrade->bizVersion = array();
 $config->upgrade->bizVersion['biz1_0']          = '9_5_1';
@@ -181,7 +182,8 @@ $config->upgrade->bizVersion['biz11_7']         = '21_7';
 $config->upgrade->bizVersion['biz12_0']         = '21_7_1';
 $config->upgrade->bizVersion['biz12_1']         = '21_7_2';
 $config->upgrade->bizVersion['biz12_2']         = '21_7_3';
-$config->upgrade->bizVersion['biz12_3']         = '21_7_4'; // biz insert position.
+$config->upgrade->bizVersion['biz12_3']         = '21_7_4';
+$config->upgrade->bizVersion['biz12_4']         = '21_7_5'; // biz insert position.
 
 $config->upgrade->proVersion = array();
 $config->upgrade->proVersion['pro1_0']        = '3_1';
@@ -328,7 +330,8 @@ $config->upgrade->ipdVersion['ipd3_7']         = '21_7';
 $config->upgrade->ipdVersion['ipd4_0']         = '21_7_1';
 $config->upgrade->ipdVersion['ipd4_1']         = '21_7_2';
 $config->upgrade->ipdVersion['ipd4_2']         = '21_7_3';
-$config->upgrade->ipdVersion['ipd4_3']         = '21_7_4'; // ipd insert position.
+$config->upgrade->ipdVersion['ipd4_3']         = '21_7_4';
+$config->upgrade->ipdVersion['ipd4_4']         = '21_7_5'; // ipd insert position.
 
 $config->upgrade->lowerTables = array();
 $config->upgrade->lowerTables[$config->db->prefix . 'caseStep']       = $config->db->prefix . 'casestep';
