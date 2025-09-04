fa9e87d2dbc0c87b8c0b667a4110ce2ddf9607ef
mazhiyuan <mazhiyuan@gitfox.io>
2025-09-04 14:57:51 +0800

    * [story#76601,done,3h] Add get zen space instance unittest.

--- module/space/test/zen/getspaceinstances.php
+++ module/space/test/zen/getspaceinstances.php
@@ -7,6 +7,10 @@ title=测试 spaceZen->getSpaceInstances();
 cid=1
 
 - 测试 browseType='' 时的返回结果 @1
+- 测试 browseType='all' 时的返回结果 @1
+- 测试 browseType='running' 时的返回结果 @1
+- 测试 browseType='aksjdfj' 时的返回结果 @1
+- 测试 browseType='abc' 时的返回结果 @1
 
 */
 
@@ -20,3 +24,7 @@ zenData('pipeline')->loadYaml('pipeline')->gen(5);
 
 $spaceZenTester = new spaceZenTest();
 r($spaceZenTester->getSpaceInstancesZenTest()) && p() && e('1');
+r($spaceZenTester->getSpaceInstancesZenTest('all')) && p() && e('1');
+r($spaceZenTester->getSpaceInstancesZenTest('running')) && p() && e('1');
+r($spaceZenTester->getSpaceInstancesZenTest('aksjdfj')) && p() && e('1');
+r($spaceZenTester->getSpaceInstancesZenTest('abc')) && p() && e('1');
