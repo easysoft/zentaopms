5771a277cb31fd469f42cb7fae83b04b0e626cae
wangyidong <yidong@chandao.com>
2025-09-08 15:04:08 +0800

    * [refac bug #64780] Adjust upload file for import.

--- module/transfer/control.php
+++ module/transfer/control.php
@@ -97,9 +97,11 @@ class transfer extends control
     public function import(string $module, string $locate = '')
     {
         $locate = $locate ? $locate : $this->session->showImportURL;
-        if($_FILES)
+        if($this->server->request_method == 'POST')
         {
             $file = $this->loadModel('file')->getUpload('file');
+            if(empty($_FILES))  return $this->send(array('result' => 'fail', 'message' => $this->lang->file->errorFileFormat));
+            if(empty($file[0])) return $this->send(array('result' => 'fail', 'message' => $this->lang->testcase->errorFileNotEmpty));
             if(!empty($file['error'])) return $this->send(array('result' => 'fail', 'message' => $this->lang->file->uploadError[$file['error']]));
 
             $file = $file[0];
