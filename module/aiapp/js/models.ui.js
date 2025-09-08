2cf50877d01d2a9490824fcb705706cf80fdf784
wangzemei <wangzemei@easycorp.ltd>
2025-09-08 16:44:46 +0800

    + [task#149196] render the models table.

--- module/aiapp/js/models.ui.js
+++ module/aiapp/js/models.ui.js
@@ -9,4 +9,22 @@ window.initModelList = async function()
 {
     const isOK = await zui.AIPanel?.shared?.store.isOK();
     if(!isOK) return;
+
+    $('#modelsList').addClass('loading');
+    const models = await zui.AIPanel.shared.store.getLlmModels();
+    models?.forEach((model, index)=> {
+        model.index = index + 1;
+    });
+
+    const {modelLang, actionLang, converseLang} = $('.models-view').data();
+    const cols = [
+        {name: 'index', title: 'ID', type: 'id', sortType: false},
+        {name: 'id', title: modelLang},
+        {name: 'actions', title: actionLang, width: 90, type: 'actions', actions: ['converse'], actionsMap: {
+            // TODO: The conversation page has not been developed yet.
+            converse: {text: converseLang},
+        }},
+    ];
+    $('#modelsList').zui('dtable').render({cols, data: models});
+    $('#modelsList').removeClass('loading');
 }
