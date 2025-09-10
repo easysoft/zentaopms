e12ebdccc28bc61ac832c630394fc006abe4ef9d
sunhao <sunhao@cnezsoft.com>
2025-09-10 15:52:35 +0800

    * [test] update unit tests for zai model to reflect changes in sync type retrieval.

--- module/zai/test/model/getnextsynctype.php
+++ module/zai/test/model/getnextsynctype.php
@@ -8,7 +8,7 @@ timeout=0
 cid=0
 
 - 测试无参数时获取第一个同步类型 @story
-- 测试从story获取下一个同步类型 @bug
+- 测试从story获取下一个同步类型 @demand
 - 测试从bug获取下一个同步类型 @doc
 - 测试从doc获取下一个同步类型 @design
 - 测试从最后一个类型获取下一个 @0
@@ -24,12 +24,13 @@ su('admin');
 
 global $tester;
 $zai = new zaiTest();
+$tester->config->edition = 'ipd';
 
 /* 测试无参数时获取第一个同步类型 */
 r($zai->getNextSyncTypeTest()) && p() && e('story'); // 测试无参数时获取第一个同步类型
 
 /* 测试从story获取下一个同步类型 */
-r($zai->getNextSyncTypeTest('story')) && p() && e('bug'); // 测试从story获取下一个同步类型
+r($zai->getNextSyncTypeTest('story')) && p() && e('demand'); // 测试从story获取下一个同步类型
 
 /* 测试从bug获取下一个同步类型 */
 r($zai->getNextSyncTypeTest('bug')) && p() && e('doc'); // 测试从bug获取下一个同步类型
@@ -37,5 +38,5 @@ r($zai->getNextSyncTypeTest('bug')) && p() && e('doc'); // 测试从bug获取下
 /* 测试从doc获取下一个同步类型 */
 r($zai->getNextSyncTypeTest('doc')) && p() && e('design'); // 测试从doc获取下一个同步类型
 
-/* 测试从design获取下一个同步类型 */
-r($zai->getNextSyncTypeTest('design')) && p() && e('0'); // 测试从最后一个类型获取下一个
+/* 测试从feedback获取下一个同步类型 */
+r($zai->getNextSyncTypeTest('feedback')) && p() && e('0'); // 测试从最后一个类型获取下一个
