143ad2d8813c417d303ee7357a5230ad2273006d
wangzemei <wangzemei@easycorp.ltd>
2025-09-22 14:32:42 +0800

    + [task#149860] add the config of ai miniprogram.

--- module/ai/config/actions.php
+++ module/ai/config/actions.php
@@ -8,6 +8,7 @@ $config->ai->actions->assistants    = array('assistantpublish|assistantwithdraw'
 $config->ai->actions->assistantview = array('mainActions' => array('assistantpublish', 'assistantwithdraw'), 'suffixActions' => array('assistantedit', 'assistantdelete'));
 $config->ai->actions->prompts       = $config->edition != 'open' ? array('promptassignrole', 'promptaudit', 'promptedit', 'promptpublish', 'promptunpublish') : array('promptpublish', 'promptunpublish');
 $config->ai->actions->promptview    = array('mainActions' => array('promptassignrole', 'promptaudit', 'promptpublish', 'promptunpublish'), 'suffixActions' => array('promptedit', 'promptdelete'));
+$config->ai->actions->miniPrograms  = $config->edition == 'open' ?  array('publishminiprogram', 'unpublishminiprogram') : array('editminiprogram', 'testminiprogram', 'publishminiprogram', 'unpublishminiprogram', 'exportminiprogram');
 
 $config->ai->actionList = array();
 $config->ai->actionList['modelenable']['icon']             = 'magic';
@@ -85,3 +86,36 @@ $config->ai->actionList['promptdelete']['hint']         = $lang->ai->prompts->ac
 $config->ai->actionList['promptdelete']['url']          = array('module' => 'ai', 'method' => 'promptdelete', 'params' => 'prompt={id}');
 $config->ai->actionList['promptdelete']['className']    = 'ajax-submit';
 $config->ai->actionList['promptdelete']['data-confirm'] = array('message' => $lang->ai->prompts->action->deleteConfirm, 'icon' => 'icon-exclamation-sign', 'iconClass' => 'warning-pale rounded-full icon-2x');
+
+$config->ai->actionList['editminiprogram']['icon'] = 'edit';
+$config->ai->actionList['editminiprogram']['text'] = $lang->ai->prompts->action->edit;
+$config->ai->actionList['editminiprogram']['hint'] = $lang->ai->prompts->action->edit;
+$config->ai->actionList['editminiprogram']['url']  = array('module' => 'ai', 'method' => 'editminiprogram', 'params' => 'appID={id}');
+
+$config->ai->actionList['testminiprogram']['icon']        = 'menu-backend';
+$config->ai->actionList['testminiprogram']['text']        = $lang->ai->prompts->action->test;
+$config->ai->actionList['testminiprogram']['hint']        = $lang->ai->prompts->action->test;
+$config->ai->actionList['testminiprogram']['url']         = array('module' => 'ai', 'method' => 'testminiprogram', 'params' => 'appID={id}');
+$config->ai->actionList['testminiprogram']['data-toggle'] = 'modal';
+$config->ai->actionList['testminiprogram']['data-size']   = 'sm';
+
+$config->ai->actionList['publishminiprogram']['icon']         = 'publish';
+$config->ai->actionList['publishminiprogram']['text']         = $lang->ai->prompts->action->publish;
+$config->ai->actionList['publishminiprogram']['hint']         = $lang->ai->prompts->action->publish;
+$config->ai->actionList['publishminiprogram']['url']          = array('module' => 'ai', 'method' => 'publishminiprogram', 'params' => 'prompt={id}');
+$config->ai->actionList['publishminiprogram']['className']    = 'ajax-submit';
+$config->ai->actionList['publishminiprogram']['data-confirm'] = array('message' => $lang->ai->prompts->action->draftConfirm, 'icon' => 'icon-exclamation-sign', 'iconClass' => 'warning-pale rounded-full icon-2x');
+
+$config->ai->actionList['unpublishminiprogram']['icon']         = 'ban-circle';
+$config->ai->actionList['unpublishminiprogram']['text']         = $lang->ai->prompts->action->unpublish;
+$config->ai->actionList['unpublishminiprogram']['hint']         = $lang->ai->prompts->action->unpublish;
+$config->ai->actionList['unpublishminiprogram']['url']          = array('module' => 'ai', 'method' => 'unpublishminiprogram', 'params' => 'prompt={id}');
+$config->ai->actionList['unpublishminiprogram']['className']    = 'ajax-submit';
+$config->ai->actionList['unpublishminiprogram']['data-confirm'] = array('message' => $lang->ai->prompts->action->draftConfirm, 'icon' => 'icon-exclamation-sign', 'iconClass' => 'warning-pale rounded-full icon-2x');
+
+$config->ai->actionList['exportminiprogram']['icon']         = 'export';
+$config->ai->actionList['exportminiprogram']['text']         = $lang->ai->export;
+$config->ai->actionList['exportminiprogram']['hint']         = $lang->ai->export;
+$config->ai->actionList['exportminiprogram']['url']          = array('module' => 'ai', 'method' => 'exportminiprogram', 'params' => 'prompt={id}');
+$config->ai->actionList['exportminiprogram']['className']    = 'ajax-submit';
+$config->ai->actionList['exportminiprogram']['data-confirm'] = array('message' => $lang->ai->prompts->action->draftConfirm, 'icon' => 'icon-exclamation-sign', 'iconClass' => 'warning-pale rounded-full icon-2x');
