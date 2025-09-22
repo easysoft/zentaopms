6e785877336d5c9661691dbdb98f001577446945
wangzemei <wangzemei@easycorp.ltd>
2025-09-22 17:05:14 +0800

    * [task#149860] modify the config of miniPrograms.

--- module/ai/config/dtable.php
+++ module/ai/config/dtable.php
@@ -60,28 +60,27 @@ $config->ai->dtable->miniPrograms['id']['sortType'] = true;
 $config->ai->dtable->miniPrograms['id']['required'] = true;
 
 $config->ai->dtable->miniPrograms['name']['title']    = $lang->prompt->name;
-$config->ai->dtable->miniPrograms['name']['sortType'] = true;
+$config->ai->dtable->miniPrograms['name']['width']    = '180';
 $config->ai->dtable->miniPrograms['name']['required'] = true;
 if($config->edition != 'open' && common::hasPriv('ai', 'miniProgramView')) $config->ai->dtable->miniPrograms['name']['link'] = array('module' => 'ai', 'method' => 'miniprogramview', 'params' => "id={id}");
 
 $config->ai->dtable->miniPrograms['publishedLabel']['title']    = $lang->prompt->status;
-$config->ai->dtable->miniPrograms['publishedLabel']['sortType'] = true;
 $config->ai->dtable->miniPrograms['publishedLabel']['required'] = true;
 
 $config->ai->dtable->miniPrograms['category']['title']    = $lang->prompt->module;
-$config->ai->dtable->miniPrograms['category']['sortType'] = true;
 $config->ai->dtable->miniPrograms['category']['required'] = true;
 $config->ai->dtable->miniPrograms['category']['map']      = $lang->ai->miniPrograms->categoryList;
 
 $config->ai->dtable->miniPrograms['createdByLabel']['title']    = $lang->prompt->createdBy;
-$config->ai->dtable->miniPrograms['createdByLabel']['sortType'] = true;
 $config->ai->dtable->miniPrograms['createdByLabel']['required'] = true;
 
 $config->ai->dtable->miniPrograms['createdDate']['title']    = $lang->prompt->createdDate;
+$config->ai->dtable->miniPrograms['createdDate']['width']    = '130';
 $config->ai->dtable->miniPrograms['createdDate']['sortType'] = true;
 $config->ai->dtable->miniPrograms['createdDate']['required'] = true;
 
 $config->ai->dtable->miniPrograms['publishedDate']['title']    = $lang->ai->miniPrograms->latestPublishedDate;
+$config->ai->dtable->miniPrograms['publishedDate']['width']    = '130';
 $config->ai->dtable->miniPrograms['publishedDate']['sortType'] = true;
 $config->ai->dtable->miniPrograms['publishedDate']['required'] = true;
 
