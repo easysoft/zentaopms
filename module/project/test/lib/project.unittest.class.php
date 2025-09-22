71ef311a6173e1927fd4d595ce7933167db35b39
tianshujie <tianshujie@easycorp.ltd>
2025-09-22 14:06:20 +0800

    * [unittest] fix linkproducts file error.

--- module/project/test/lib/project.unittest.class.php
+++ module/project/test/lib/project.unittest.class.php
@@ -796,4 +796,27 @@ class projectTest
         global $lang;
         return $lang->executionCommon;
     }
+
+    /**
+     * 关联项目所属项目集下的产品。
+     * Link products of current program of the project.
+     *
+     * @param  int    $projectID
+     * @param  array  $products
+     * @param  array  $branches
+     * @param  array  $plans
+     * @access public
+     * @return array
+     */
+    public function linkProductsTest(int $projectID, array $products = array(), array $branches = array(), array $plans = array()): array
+    {
+        $members            = $this->objectModel->getTeamMembers($projectID);
+        $oldProjectProducts = $this->objectModel->dao->select('*')->from(TABLE_PROJECTPRODUCT)->where('project')->eq($projectID)->fetchGroup('product', 'branch');
+        if(!empty($branches)) $_POST['branch'] = $branches;
+        if(!empty($plans)) $_POST['branch'] = $plans;
+
+        $this->objectModel->linkProducts($projectID, $products, $oldProjectProducts, $members);
+
+        return $this->objectModel->dao->select('*')->from(TABLE_PROJECTPRODUCT)->where('project')->eq($projectID)->fetchAll();
+    }
 }
