3be4902c8fba94698127b369ab64b598267b086a
tianshujie <tianshujie@easycorp.ltd>
2025-09-22 15:17:33 +0800

    * [unittest] fix updatetasksstartandenddate file error.

--- module/project/test/tao/updatetasksstartandenddate.php
+++ module/project/test/tao/updatetasksstartandenddate.php
@@ -36,7 +36,7 @@ cid=1
 
 $projectIdList = array(11, 60 ,100);
 
-$projectTester = new project();
+$projectTester = new projectTest();
 r($projectTester->updateTasksStartAndEndDateTest($projectIdList[0], 'expend')) && p('0:field,old,new') && e('estStarted,2020-11-01,2020-01-01'); // 测试更新敏捷项目下的任务起止日期
 r($projectTester->updateTasksStartAndEndDateTest($projectIdList[1], 'expend')) && p('0:field,old,new') && e('deadline,2021-01-13,2020-03-13'); // 测试更新瀑布项目下的任务起止日期
 r($projectTester->updateTasksStartAndEndDateTest($projectIdList[2], 'expend')) && p()                  && e('0');                                // 测试更新看板项目下的任务起止日期
