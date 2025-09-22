78180f97c516157d160d3347a2a1cd11d2ea7b35
tianshujie <tianshujie@easycorp.ltd>
2025-09-22 14:37:36 +0800

    * [unittest] fix updateproducts file error.

--- module/project/test/model/updateproducts.php
+++ module/project/test/model/updateproducts.php
@@ -52,7 +52,7 @@ $projectIdList      = array(11, 60, 100);
 $productIdList      = array(0, 1, 2, 3);
 $otherProductIdList = array('4', '5', '6', '7');
 
-$projectTester = new Project();
+$projectTester = new projectTest();
 $noLinkProduct[0] = $projectTester->updateProductsTest($projectIdList[0]);
 $noLinkProduct[1] = $projectTester->updateProductsTest($projectIdList[1]);
 $noLinkProduct[2] = $projectTester->updateProductsTest($projectIdList[2]);
