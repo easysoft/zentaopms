6d2800d3d1006fc0a77233a7136606a6e82f4582
tianshujie <tianshujie@easycorp.ltd>
2025-09-22 14:19:30 +0800

    * [unittest] fix unlinkstorybytype error.

--- module/project/test/model/unlinkstorybytype.php
+++ module/project/test/model/unlinkstorybytype.php
@@ -7,11 +7,11 @@ title=测试 projectModel::unlinkStoryByType();
 timeout=0
 cid=0
 
-- 执行projectTest模块的unlinkStoryByTypeTest方法，参数是1, 'story'  @~~
-- 执行projectTest模块的unlinkStoryByTypeTest方法，参数是999, 'story'  @~~
-- 执行projectTest模块的unlinkStoryByTypeTest方法，参数是1, ''  @~~
-- 执行projectTest模块的unlinkStoryByTypeTest方法，参数是1, 'story, requirement'  @~~
-- 执行projectTest模块的unlinkStoryByTypeTest方法，参数是0, 'story'  @~~
+- 执行projectTest模块的unlinkStoryByTypeTest方法，参数是1, 'story'  @0
+- 执行projectTest模块的unlinkStoryByTypeTest方法，参数是999, 'story'  @0
+- 执行projectTest模块的unlinkStoryByTypeTest方法，参数是1, ''  @0
+- 执行projectTest模块的unlinkStoryByTypeTest方法，参数是1, 'story, requirement'  @0
+- 执行projectTest模块的unlinkStoryByTypeTest方法，参数是0, 'story'  @0
 
 */
 
@@ -24,10 +24,10 @@ zenData('projectstory')->loadYaml('projectstory_unlinkstorybytype', false, 2)->g
 
 su('admin');
 
-$projectTest = new Project();
+$projectTest = new projectTest();
 
-r($projectTest->unlinkStoryByTypeTest(1, 'story')) && p() && e('~~');
-r($projectTest->unlinkStoryByTypeTest(999, 'story')) && p() && e('~~');
-r($projectTest->unlinkStoryByTypeTest(1, '')) && p() && e('~~');
-r($projectTest->unlinkStoryByTypeTest(1, 'story,requirement')) && p() && e('~~');
-r($projectTest->unlinkStoryByTypeTest(0, 'story')) && p() && e('~~');
+r($projectTest->unlinkStoryByTypeTest(1, 'story'))             && p() && e('0');
+r($projectTest->unlinkStoryByTypeTest(999, 'story'))           && p() && e('0');
+r($projectTest->unlinkStoryByTypeTest(1, ''))                  && p() && e('0');
+r($projectTest->unlinkStoryByTypeTest(1, 'story,requirement')) && p() && e('0');
+r($projectTest->unlinkStoryByTypeTest(0, 'story'))             && p() && e('0');
