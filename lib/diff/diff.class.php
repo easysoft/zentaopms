4200a92f70390a71fff4769c458de527697a8d4c
liugang <liugang@chandao.com>
2025-09-23 14:37:51 +0800

    + [refac] Add the diff class to delegate method calls of the php-fine-diff library.

---
+++ lib/diff/diff.class.php
@@ -0,0 +1,17 @@
+<?php
+require_once dirname(__FILE__, 2) . '/base/delegate/delegate.class.php';
+
+class diff extends baseDelegate
+{
+    protected static $className = 'GorHill\FineDiff\FineDiff';
+
+	public function __construct($from_text = '', $to_text = '', $granularityStack = null)
+    {
+        $this->instance = new self::$className($from_text, $to_text, $granularityStack);
+    }
+
+    public static function getSentenceGranularity()
+    {
+        return self::$className::$sentenceGranularity;
+    }
+}
