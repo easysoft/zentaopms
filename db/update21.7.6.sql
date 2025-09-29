2b0c243620c86e459e7c213a916be8f32d117da5
wangzemei <wangzemei@easycorp.ltd>
2025-09-29 13:42:39 +0800

    * [task#150347] modify the sql of ai model.

--- db/update21.7.6.sql
+++ db/update21.7.6.sql
@@ -2,10 +2,10 @@ UPDATE `zt_ai_miniprogram` SET `model` = 0 WHERE `model` IS NULL;
 UPDATE `zt_ai_prompt` SET `model` = 0 WHERE `model` IS NULL;
 UPDATE `zt_ai_promptrole` SET `model` = 0 WHERE `model` IS NULL;
 
-UPDATE `zt_ai_miniprogram` SET `model` = '' WHERE `model` = '0' OR `model` = 'default';
-UPDATE `zt_ai_prompt` SET `model` = '' WHERE `model` = '0' OR `model` = 'default';
-UPDATE `zt_ai_promptrole` SET `model` = '' WHERE `model` = '0' OR `model` = 'default';
+ALTER TABLE `zt_ai_miniprogram` MODIFY `model` varchar(255) NOT NULL DEFAULT '';
+ALTER TABLE `zt_ai_prompt` MODIFY `model` varchar(255) NOT NULL DEFAULT '';
+ALTER TABLE `zt_ai_promptrole` MODIFY `model` varchar(255) NOT NULL DEFAULT '';
 
-ALTER TABLE `zt_ai_miniprogram` MODIFY `model` varchar(255) COLLATE 'utf8mb4_general_ci' NOT NULL DEFAULT '';
-ALTER TABLE `zt_ai_prompt` MODIFY `model` varchar(255) COLLATE 'utf8mb4_general_ci' NOT NULL DEFAULT '';
-ALTER TABLE `zt_ai_promptrole` MODIFY `model` varchar(255) COLLATE 'utf8mb4_general_ci' NOT NULL DEFAULT '';
+UPDATE `zt_ai_miniprogram` SET `model` = '' WHERE `model` = '0';
+UPDATE `zt_ai_prompt` SET `model` = '' WHERE `model` = '0';
+UPDATE `zt_ai_promptrole` SET `model` = '' WHERE `model` = '0';
