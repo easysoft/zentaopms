4db0d53a4b25db68e69a61e3f2bea886c54dcc7c
wangzemei <wangzemei@easycorp.ltd>
2025-09-30 16:37:50 +0800

    * [misc] move the sql from 21.7.6 to 21.7.5.

--- db/update21.7.5.sql
+++ db/update21.7.5.sql
@@ -8,3 +8,15 @@ CREATE INDEX `templateType` ON `zt_doc`(`templateType`);
 DELETE FROM `zt_cron` WHERE `command`='moduleName=weekly&methodName=computeWeekly' AND `type`='zentao';
 DELETE FROM `zt_cron` WHERE `command`='moduleName=weekly&methodName=createCycleReport' AND `type`='zentao';
 INSERT INTO `zt_cron` (`m`, `h`, `dom`, `mon`, `dow`, `command`, `remark`, `type`, `buildin`, `status`) VALUES('1','0','*','*','*','moduleName=weekly&methodName=createCycleReport','定时生成报告','zentao',1,'normal');
+
+UPDATE `zt_ai_miniprogram` SET `model` = 0 WHERE `model` IS NULL;
+UPDATE `zt_ai_prompt` SET `model` = 0 WHERE `model` IS NULL;
+UPDATE `zt_ai_promptrole` SET `model` = 0 WHERE `model` IS NULL;
+
+ALTER TABLE `zt_ai_miniprogram` MODIFY `model` varchar(255) COLLATE 'utf8mb4_general_ci' NOT NULL DEFAULT '';
+ALTER TABLE `zt_ai_prompt` MODIFY `model` varchar(255) COLLATE 'utf8mb4_general_ci' NOT NULL DEFAULT '';
+ALTER TABLE `zt_ai_promptrole` MODIFY `model` varchar(255) COLLATE 'utf8mb4_general_ci' NOT NULL DEFAULT '';
+
+UPDATE `zt_ai_miniprogram` SET `model` = '' WHERE `model` = '0';
+UPDATE `zt_ai_prompt` SET `model` = '' WHERE `model` = '0';
+UPDATE `zt_ai_promptrole` SET `model` = '' WHERE `model` = '0';
