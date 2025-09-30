1abfa1755e0eb8a721effd7021e49356da05512e
tianshujie <tianshujie@easycorp.ltd>
2025-09-30 09:15:07 +0800

    * [task#150364,done,0.1h] modify milestone language.

--- module/weekly/lang/zh-cn.php
+++ module/weekly/lang/zh-cn.php
@@ -51,7 +51,7 @@ $lang->weekly->builtInScopes['rnd']['project'] = '项目';
 $lang->weekly->builtInCategoryList['month']     = '月报';
 $lang->weekly->builtInCategoryList['week']      = '周报';
 $lang->weekly->builtInCategoryList['day']       = '日报';
-$lang->weekly->builtInCategoryList['milestone'] = '里程碑';
+$lang->weekly->builtInCategoryList['milestone'] = '里程碑报告';
 
 $lang->weekly->reportHelpNotice = <<<EOD
 <h2>PV 计划完成的工作</h2>
