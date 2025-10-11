2a86d48587ad001994b4900c8e5d8ed32c23ab2e
朱金勇 <zhujinyong@cnezsoft.com>
2025-10-11 09:22:53 +0800

    * [highgo] typo

--- module/story/tao.php
+++ module/story/tao.php
@@ -1154,7 +1154,7 @@ class storyTao extends storyModel
     {
         $projects = $this->dao->select('t2.id,t2.model,t2.type,t3.branch')->from(TABLE_PROJECTSTORY)->alias('t1')
             ->leftJoin(TABLE_PROJECT)->alias('t2')->on('t1.project = t2.id')
-\            ->leftJoin(TABLE_PROJECTPRODUCT)->alias('t3')->on('t1.project = t3.project AND t1.product=t3.product')
+            ->leftJoin(TABLE_PROJECTPRODUCT)->alias('t3')->on('t1.project = t3.project AND t1.product=t3.product')
             ->where('t1.story')->eq($storyID)
             ->andWhere('t2.deleted')->eq(0)
             ->fetchAll();
