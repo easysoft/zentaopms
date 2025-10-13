99cadd5336e19fa3679bb4749e14c28d5519a64b
liyang <liyang@easycorp.ltd>
2025-10-13 15:37:16 +0800

    + [task#150708] add devops space config.

--- config/filter.php
+++ config/filter.php
@@ -35,6 +35,7 @@ $filter->default->cookie['maxImport']   = 'reg::any';
 $filter->default->cookie['za']          = 'reg::any';
 $filter->default->cookie['zp']          = 'reg::any';
 $filter->default->cookie['vision']      = 'reg::word';
+$filter->default->cookie['devopsSpace'] = 'reg::word';
 
 $filter->index        = new stdclass();
 $filter->my           = new stdclass();
