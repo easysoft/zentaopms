6eaf9046c81e56e9848bfacf5fb89def3022a19c
liyang <liyang@easycorp.ltd>
2025-10-15 09:52:38 +0800

    + [task#150711,115072,115073,115074,115075,115076,115077,150718] add devops vision menu icon.

---
+++ extension/devops/common/ext/lang/devopsmenu.php
@@ -0,0 +1,9 @@
+<?php
+global $config;
+$lang->navIcons['repo']         = "<i class='icon icon-code-block'></i>";
+$lang->navIcons['compile']      = "<i class='icon icon-inherit-space'></i>";
+$lang->navIcons['deploy']       = "<i class='icon icon-doclib'></i>";
+$lang->navIcons['space']        = "<i class='icon icon-layout'></i>";
+$lang->navIcons['dynamic']      = "<i class='icon icon-gantt-alt'></i>";
+$lang->navIcons['artifactrepo'] = "<i class='icon icon-folder'></i>";
+$lang->navIcons['env']          = "<i class='icon icon-lang'></i>";
