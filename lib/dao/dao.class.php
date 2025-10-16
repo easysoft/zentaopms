ecf173e129a1b8b364961d1c41cff952e7550a39
tianshujie <tianshujie@easycorp.ltd>
2025-10-16 16:36:19 +0800

    * [refac] optimize code.

--- lib/dao/dao.class.php
+++ lib/dao/dao.class.php
@@ -246,14 +246,14 @@ class dao extends baseDAO
                 if(!empty($result->shadow))
                 {
                     $result  = $this->dbh->query("SELECT t2.`workflowGroup` FROM " . TABLE_PROJECTPRODUCT . " AS t1 LEFT JOIN " . TABLE_PROJECT . " AS t2 ON t1.project = t2.id WHERE t1.product = '{$productID}'")->fetch(PDO::FETCH_OBJ);
-                    $groupID = $result->workflowGroup;
+                    $groupID = !empty($result->workflowGroup) ? $result->workflowGroup : 0;
                 }
             }
         }
         elseif(!empty($_SESSION['project']) && in_array($module, $linkProjectModules))
         {
             $result  = $this->dbh->query('SELECT `workflowGroup` FROM ' . TABLE_PROJECT . " WHERE `id` = '" . $_SESSION['project'] . "'")->fetch(PDO::FETCH_OBJ);
-            $groupID = $result->workflowGroup;
+            $groupID = !empty($result->workflowGroup) ? $result->workflowGroup : 0;
         }
 
         $flowAction = $this->dbh->query("SELECT * FROM " . TABLE_WORKFLOWACTION . " WHERE `module` = '{$module}' AND `action` = '{$method}' AND `buildin` = '1' AND `extensionType` = 'extend' AND `vision` = '{$this->config->vision}' AND `group` = '{$groupID}'")->fetch(PDO::FETCH_OBJ);
