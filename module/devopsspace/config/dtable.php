0bca448cc7e575f9f8e7ab08d0e28443f5497d8d
liyang <liyang@easycorp.ltd>
2025-10-22 14:33:10 +0800

    + [task#151417] add space list field.

--- module/devopsspace/config/dtable.php
+++ module/devopsspace/config/dtable.php
@@ -14,3 +14,31 @@ $config->devopsspace->dtable->fieldList['name']['hint']     = true;
 $config->devopsspace->dtable->fieldList['name']['sortType'] = false;
 $config->devopsspace->dtable->fieldList['name']['url']      = helper::createLink('devopsspace', 'view', 'id={id}');;
 $config->devopsspace->dtable->fieldList['name']['group']    = 1;
+
+$config->devopsspace->dtable->fieldList['owner']['title']    = $lang->devopsspace->owner;
+$config->devopsspace->dtable->fieldList['owner']['name']     = 'owner';
+$config->devopsspace->dtable->fieldList['owner']['type']     = 'user';
+$config->devopsspace->dtable->fieldList['owner']['sortType'] = false;
+$config->devopsspace->dtable->fieldList['owner']['group']    = 2;
+
+$config->devopsspace->dtable->fieldList['desc']['title'] = $lang->devopsspace->desc;
+$config->devopsspace->dtable->fieldList['desc']['name']  = 'desc';
+$config->devopsspace->dtable->fieldList['desc']['type']  = 'desc';
+$config->devopsspace->dtable->fieldList['desc']['width'] = '200';
+$config->devopsspace->dtable->fieldList['desc']['group'] = 3;
+
+$config->devopsspace->dtable->fieldList['createdDate']['title']      = $lang->devopsspace->createdDate;
+$config->devopsspace->dtable->fieldList['createdDate']['name']       = 'createdDate';
+$config->devopsspace->dtable->fieldList['createdDate']['type']       = 'datetime';
+$config->devopsspace->dtable->fieldList['createdDate']['formatDate'] = 'YYYY-MM-dd hh:mm';
+$config->devopsspace->dtable->fieldList['createdDate']['sortType']   = false;
+$config->devopsspace->dtable->fieldList['createdDate']['group']      = 4;
+
+$config->devopsspace->dtable->fieldList['actions']['name']     = 'actions';
+$config->devopsspace->dtable->fieldList['actions']['title']    = $lang->actions;
+$config->devopsspace->dtable->fieldList['actions']['type']     = 'actions';
+$config->devopsspace->dtable->fieldList['actions']['sortType'] = false;
+$config->devopsspace->dtable->fieldList['actions']['fixed']    = 'right';
+$config->devopsspace->dtable->fieldList['actions']['menu']     = array('repo', 'artifactrepo', 'edit');
+$config->devopsspace->dtable->fieldList['actions']['list']     = $config->devopsspace->actionList;
+$config->devopsspace->dtable->fieldList['actions']['width']    = 100;
