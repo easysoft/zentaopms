9d46ea83bb3fca463df5188631750dcf2207738d
liyang <liyang@easycorp.ltd>
2025-10-22 14:29:38 +0800

    + [task#151417] add devops space lang.

--- module/devopsspace/lang/de.php
+++ module/devopsspace/lang/de.php
@@ -4,3 +4,13 @@ $lang->devopsspace->create = 'Create Space';
 $lang->devopsspace->edit   = 'Edit Space';
 $lang->devopsspace->view   = 'Space Detail';
 $lang->devopsspace->delete = 'Delete Space';
+
+$lang->devopsspace->name         = 'Name';
+$lang->devopsspace->owner        = 'Owner';
+$lang->devopsspace->createdDate  = 'Created Date';
+$lang->devopsspace->desc         = 'Description';
+$lang->devopsspace->repo         = 'Repository';
+$lang->devopsspace->artifactrepo = 'Artifact Repository';
+
+$lang->devopsspace->notice = new stdclass();
+$lang->devopsspace->notice->confirmDelete = 'Are you sure to delete this space?';
