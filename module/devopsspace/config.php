50759b5140a5096b62f2a4c08733e9c8514848cb
liyang <liyang@easycorp.ltd>
2025-10-22 14:33:45 +0800

    + [task#151417] add space list action config.

--- module/devopsspace/config.php
+++ module/devopsspace/config.php
@@ -12,3 +12,18 @@ $config->devopsspace->actionList['artifactrepo']['text']     = $lang->devopsspac
 $config->devopsspace->actionList['artifactrepo']['hint']     = $lang->devopsspace->artifactrepo;
 $config->devopsspace->actionList['artifactrepo']['showText'] = true;
 $config->devopsspace->actionList['artifactrepo']['url']      = array('module' => 'artifactrepo', 'method' => 'browse', 'params' => 'id={id}');
+
+$config->devopsspace->actionList['edit']['icon']        = 'edit';
+$config->devopsspace->actionList['edit']['text']        = $lang->edit;
+$config->devopsspace->actionList['edit']['hint']        = $lang->edit;
+$config->devopsspace->actionList['edit']['showText']    = true;
+$config->devopsspace->actionList['edit']['url']         = array('module' => 'devopsspace', 'method' => 'edit', 'params' => 'id={id}');
+$config->devopsspace->actionList['edit']['data-toggle'] = 'modal';
+
+$config->devopsspace->actionList['delete']['icon']         = 'trash';
+$config->devopsspace->actionList['delete']['text']         = $lang->delete;
+$config->devopsspace->actionList['delete']['hint']         = $lang->delete;
+$config->devopsspace->actionList['delete']['showText']     = true;
+$config->devopsspace->actionList['delete']['ajaxSubmit']   = true;
+$config->devopsspace->actionList['delete']['data-confirm'] = array('message' => $lang->devopsspace->notice->confirmDelete, 'icon' => 'icon-exclamation-sign', 'iconClass' => 'warning-pale rounded-full icon-2x');
+$config->devopsspace->actionList['delete']['url']          = array('module' => 'devopsspace', 'method' => 'delete', 'params' => 'id={id}');
