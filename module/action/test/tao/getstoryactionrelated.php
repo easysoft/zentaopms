5e2d66c7699bd83aee74fa9ea1548d5c2e3ef4b9
sunguangming <sunguangming@chandao.com>
2025-10-23 05:28:22 +0000

    * [misc] add default product when create story action.

--- module/action/test/tao/getstoryactionrelated.php
+++ module/action/test/tao/getstoryactionrelated.php
@@ -35,13 +35,13 @@ zenData('project')->loadYaml('execution')->gen(1);
 zenData('project')->gen(11, false, false);
 zenData('projectstory')->gen(5);
 
-$actionTest = new actionTest();
+global $tester;
 
 $actionTypeList = array('linked2build', 'unlinkedfrombuild', 'estimated', 'created');
 $objectIDList   = array(1, 2);
 $extraList      = array(1, 101);
 
-r($actionTest->getStoryActionRelated($actionTypeList[0], $objectIDList[0], $extraList[0])) && p('0:0;1;2') && e('0,11,101');    //获取操作类型为linked2build,extra为1的用户故事相关产品，项目和执行信息。
-r($actionTest->getStoryActionRelated($actionTypeList[1], $objectIDList[0], $extraList[0])) && p('0:0;1;2') && e('0,11,101');    //获取操作类型为unlinkedfrombuild,extra为1的用户故事相关产品，项目和执行信息。
-r($actionTest->getStoryActionRelated($actionTypeList[2], $objectIDList[0], $extraList[1])) && p('0:0;1;2') && e('0,11,101');    //获取操作类型为estimated,extra为1的用户故事相关产品，项目和执行信息。
-r($actionTest->getStoryActionRelated($actionTypeList[3], $objectIDList[1], $extraList[1])) && p('0:0;1;2') && e('0,11,0');      //获取操作类型为created,extra为1的用户故事相关产品，项目和执行信息。
+r($tester->loadModel('action')->getStoryActionRelated('story', $actionTypeList[0], $objectIDList[0], $extraList[0])) && p('0:0;1;2') && e('1,11,101');    //获取操作类型为linked2build,extra为1的用户故事相关产品，项目和执行信息。
+r($tester->loadModel('action')->getStoryActionRelated('story', $actionTypeList[1], $objectIDList[0], $extraList[0])) && p('0:0;1;2') && e('1,11,101');    //获取操作类型为unlinkedfrombuild,extra为1的用户故事相关产品，项目和执行信息。
+r($tester->loadModel('action')->getStoryActionRelated('story', $actionTypeList[2], $objectIDList[0], $extraList[1])) && p('0:0;1;2') && e('1,11,101');    //获取操作类型为estimated,extra为1的用户故事相关产品，项目和执行信息。
+r($tester->loadModel('action')->getStoryActionRelated('story', $actionTypeList[3], $objectIDList[1], $extraList[1])) && p('0:0;1;2') && e('1,11,0');      //获取操作类型为created,extra为1的用户故事相关产品，项目和执行信息。
