146c90fce0e0524324c3793760a00c18508f5b32
sunguangming <sunguangming@chandao.com>
2025-10-23 05:34:07 +0000

    * [misc] modify unittest.

--- module/action/test/tao/getstoryactionrelated.php
+++ module/action/test/tao/getstoryactionrelated.php
@@ -44,4 +44,4 @@ $extraList      = array(1, 101);
 r($tester->loadModel('action')->getStoryActionRelated('story', $actionTypeList[0], $objectIDList[0], $extraList[0])) && p('0:0;1;2') && e('1,11,101');    //获取操作类型为linked2build,extra为1的用户故事相关产品，项目和执行信息。
 r($tester->loadModel('action')->getStoryActionRelated('story', $actionTypeList[1], $objectIDList[0], $extraList[0])) && p('0:0;1;2') && e('1,11,101');    //获取操作类型为unlinkedfrombuild,extra为1的用户故事相关产品，项目和执行信息。
 r($tester->loadModel('action')->getStoryActionRelated('story', $actionTypeList[2], $objectIDList[0], $extraList[1])) && p('0:0;1;2') && e('1,11,101');    //获取操作类型为estimated,extra为1的用户故事相关产品，项目和执行信息。
-r($tester->loadModel('action')->getStoryActionRelated('story', $actionTypeList[3], $objectIDList[1], $extraList[1])) && p('0:0;1;2') && e('1,11,0');      //获取操作类型为created,extra为1的用户故事相关产品，项目和执行信息。
+r($tester->loadModel('action')->getStoryActionRelated('story', $actionTypeList[3], $objectIDList[1], $extraList[1])) && p('0:0;1;2') && e('2,11,0');      //获取操作类型为created,extra为1的用户故事相关产品，项目和执行信息。
