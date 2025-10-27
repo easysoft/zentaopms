0f08c0cedb662d91f41c6f7a76028a1e42971435
sunguangming <sunguangming@chandao.com>
2025-10-27 06:44:40 +0000

    * [misc] code for review.

--- db/update22.0.sql
+++ db/update22.0.sql
@@ -132,4 +132,4 @@ UPDATE zt_activity SET optional = CASE
     ELSE optional
 END;
 
-ALTER TABLE `zt_process` CHANGE `model` `model` char(30) NOT NULL DEFAULT '';
+ALTER TABLE `zt_process` CHANGE `model` `model` varchar(30) NOT NULL DEFAULT '';
