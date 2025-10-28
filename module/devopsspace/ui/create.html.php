48efc5e05230ed8ad3edc746e3daa73ed7e3c9e9
liyang <liyang@easycorp.ltd>
2025-10-28 13:05:31 +0800

    + [task#151421] fix acl lang.

--- module/devopsspace/ui/create.html.php
+++ module/devopsspace/ui/create.html.php
@@ -52,7 +52,7 @@ formPanel
         set::name('acl'),
         set::label($lang->devopsspace->acl),
         set::control('radioList'),
-        set::items($lang->devopsspace->aclList),
+        set::items($lang->devopsspace->aclNoticeList),
         set::value('open')
     )
 );
