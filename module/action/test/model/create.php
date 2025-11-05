a164dc88d1edd222f4fddd5737dc0b1a850137e9
liugang <liugang@chandao.com>
2025-11-05 09:31:49 +0800

    * [misc] Change test data from string to int.

--- module/action/test/model/create.php
+++ module/action/test/model/create.php
@@ -62,7 +62,7 @@ cid=1
 
 */
 $objectTypeList      = array('task', 'project', 'user', 'bug', 'story');
-$objectIDList        = array('1', '11');
+$objectIDList        = array(1, 11);
 $actionTypeList      = array('commented', 'edited', 'editestimate', 'login', 'closed', 'comments', 'logout');
 $storyActionTypeList = array('reviewpassed', 'reviewrejected', 'reviewclarified', 'reviewreverted', 'synctwins');
 $actor               = 'guest';
