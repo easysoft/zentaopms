89bf728c334df49ababae0e59d1059ab78e23728
sunguangming <sunguangming@chandao.com>
2025-11-05 15:21:05 +0800

    * [sql] modify sql.

--- db/update21.7.6.sql
+++ db/update21.7.6.sql
@@ -1,1 +1,5 @@
-ALTER TABLE `zt_demand` CHANGE `version` `version` smallint NOT NULL DEFAULT '1';
+ALTER TABLE `zt_demand` CHANGE `version` `version` smallint unsigned NOT NULL DEFAULT '1';
+ALTER TABLE `zt_demand` CHANGE `pri` `pri` tinyint unsigned NOT NULL DEFAULT 3;
+ALTER TABLE `zt_issue` CHANGE `pri` `pri` tinyint unsigned NOT NULL DEFAULT 3;
+ALTER TABLE `zt_risk` CHANGE `pri` `pri` tinyint unsigned NOT NULL DEFAULT 3;
+ALTER TABLE `zt_opportunity` CHANGE `pri` `pri` tinyint unsigned NOT NULL DEFAULT 3;
