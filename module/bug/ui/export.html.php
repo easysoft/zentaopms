8a93c36a30afcb1f911d183fe630239373bf2827
wangyuting <wangyuting@chandao.com>
2025-11-10 15:18:13 +0800

    * Optimize code.

--- module/bug/ui/export.html.php
+++ module/bug/ui/export.html.php
@@ -1,11 +1,11 @@
 <?php
 declare(strict_types=1);
 /**
- * The yyy view file of xxx module of ZenTaoPMS.
+ * The export view file of bug module of ZenTaoPMS.
  * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
  * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
  * @author      Tingting Dai <daitingting@easycorp.ltd>
- * @package     xxx
+ * @package     bug
  * @link        https://www.zentao.net
  */
 include '../../file/ui/export.html.php';
