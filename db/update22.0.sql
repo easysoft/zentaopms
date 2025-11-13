f1195c522f4f1f6f2717f0215e8b29f45057324c
wangyuting <wangyuting@chandao.com>
2025-11-13 16:25:40 +0800

    * Adjust cycleType default value.

--- db/update22.0.sql
+++ db/update22.0.sql
@@ -148,7 +148,7 @@ END;
 
 ALTER TABLE `zt_process` CHANGE `model` `model` varchar(30) NOT NULL DEFAULT '';
 
-ALTER TABLE `zt_auditplan` ADD `cycleType` varchar(10) NOT NULL DEFAULT '' COMMENT '周期类型' AFTER `status`;
+ALTER TABLE `zt_auditplan` ADD `cycleType` varchar(10) NOT NULL DEFAULT 'noCycle' COMMENT '周期类型' AFTER `status`;
 ALTER TABLE `zt_auditplan` ADD `cycleConfig` varchar(100) NOT NULL DEFAULT '[]' COMMENT '周期设置' AFTER `cycleType`;
 ALTER TABLE `zt_auditplan` ADD `cyclePlan` smallint unsigned  NOT NULL DEFAULT 0 COMMENT '生成计划(提前的天数)' AFTER `cycleConfig`;
 ALTER TABLE `zt_auditplan` ADD `deadline` date NULL COMMENT '过期时间' AFTER `cyclePlan`;
