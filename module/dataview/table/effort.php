3315f361471e02efbf9724c57ed1c8cc4be0fa81
tianshujie <tianshujie@easycorp.ltd>
2025-11-28 14:10:04 +0800

    * [bug#67567,done,0.2h] fix effort summary table error.

--- module/dataview/table/effort.php
+++ module/dataview/table/effort.php
@@ -1,5 +1,6 @@
 <?php
 $this->app->loadLang('user');
+$this->app->loadLang('dataview');
 
 $schema = new stdclass();
 
@@ -9,6 +10,6 @@ $schema->tables = array();
 $schema->tables['effort'] = 'zt_effort';
 
 $schema->fields = array();
-$schema->fields['id']      = array('type' => 'number', 'name' => $this->lang->user->id);
-$schema->fields['account'] = array('type' => 'user',   'name' => $this->lang->user->realname);
-if(isset($this->lang->dataview->consumed)) $schema->fields['consumed'] = array('type' => 'number', 'name' => $this->lang->dataview->consumed);
+$schema->fields['id']       = array('type' => 'number', 'name' => $this->lang->user->id);
+$schema->fields['account']  = array('type' => 'user',   'name' => $this->lang->user->realname);
+$schema->fields['consumed'] = array('type' => 'number', 'name' => $this->lang->dataview->consumed);
