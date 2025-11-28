21d3147a27e725929d197364311e71e012552a38
tianshujie <tianshujie@easycorp.ltd>
2025-11-28 13:33:11 +0800

    * [refac] fix effort summary table error.

--- module/dataview/table/effort.php
+++ module/dataview/table/effort.php
@@ -9,7 +9,6 @@ $schema->tables = array();
 $schema->tables['effort'] = 'zt_effort';
 
 $schema->fields = array();
-$schema->fields['id']       = array('type' => 'number', 'name' => $this->lang->user->id);
-$schema->fields['account']  = array('type' => 'user',   'name' => $this->lang->user->realname);
-$schema->fields['consumed'] = array('type' => 'number', 'name' => $this->lang->dataview->consumed);
-
+$schema->fields['id']      = array('type' => 'number', 'name' => $this->lang->user->id);
+$schema->fields['account'] = array('type' => 'user',   'name' => $this->lang->user->realname);
+if(isset($this->lang->dataview->consumed)) $schema->fields['consumed'] = array('type' => 'number', 'name' => $this->lang->dataview->consumed);
