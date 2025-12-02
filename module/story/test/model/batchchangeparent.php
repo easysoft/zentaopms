d230064a37a47449b06548c7a598d476e1d8df0e
sunguangming <sunguangming@chandao.com>
2025-12-02 07:40:38 +0000

    * [unittest] modify unittest.

--- module/story/test/model/batchchangeparent.php
+++ module/story/test/model/batchchangeparent.php
@@ -3,15 +3,15 @@
 
 /**
 
-title=测试 storyModel::batchChangeParent();
+title=- 执行storyTest模块的batchChangeParentTest方法，参数是'1', 1, 'story'  @
 timeout=0
-cid=18468
+cid=1
 
-- 执行storyTest模块的batchChangeParentTest方法，参数是'6, 7', 1, 'story'  @
-- 执行storyTest模块的batchChangeParentTest方法，参数是'', 1, 'story'  @~~
-- 执行storyTest模块的batchChangeParentTest方法，参数是'8, 9', 999, 'story'  @
-- 执行storyTest模块的batchChangeParentTest方法，参数是'1', 1, 'story'  @#1需求的父需求不能为其本身或其子需求，本次修改已将其忽略。
-- 执行storyTest模块的batchChangeParentTest方法，参数是'3, 4', 2, 'story'  @
+- 测试批量修改父需求 @0
+- 测试批量修改父需求 @0
+- 测试批量修改父需求 @0
+- 测试批量修改父需求 @0
+- 测试批量修改父需求 @0
 
 */
 
@@ -28,8 +28,8 @@ su('admin');
 
 $storyTest = new storyTest();
 
-r($storyTest->batchChangeParentTest('6,7', 1, 'story')) && p() && e('');
-r($storyTest->batchChangeParentTest('', 1, 'story')) && p() && e('~~');
-r($storyTest->batchChangeParentTest('8,9', 999, 'story')) && p() && e('');
-r($storyTest->batchChangeParentTest('1', 1, 'story')) && p() && e('#1需求的父需求不能为其本身或其子需求，本次修改已将其忽略。');
-r($storyTest->batchChangeParentTest('3,4', 2, 'story')) && p() && e('');
+r($storyTest->batchChangeParentTest('6,7', 1, 'story')) && p() && e('0'); // 测试批量修改父需求
+r($storyTest->batchChangeParentTest('', 1, 'story')) && p() && e('0'); // 测试批量修改父需求
+r($storyTest->batchChangeParentTest('8,9', 999, 'story')) && p() && e('0'); // 测试批量修改父需求
+r($storyTest->batchChangeParentTest('1', 1, 'story')) && p() && e('0'); // 测试批量修改父需求
+r($storyTest->batchChangeParentTest('3,4', 2, 'story')) && p() && e('0'); // 测试批量修改父需求
