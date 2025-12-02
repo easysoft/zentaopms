3cd6f30da9691cd2668eb2610689ffe581228859
mazhiyuan <mazhiyuan@gitfox.io>
2025-12-02 14:44:39 +0800

    * [task#151891,done,6h,0h] create tag connect debug.

--- module/repo/model.php
+++ module/repo/model.php
@@ -1782,8 +1782,6 @@ class repoModel extends model
      */
     public function processGitService(object $repo, bool $getCodePath = false): object
     {
-        $service = $this->loadModel('pipeline')->getByID((int)$repo->serviceHost);
-        if(!$service) return $repo;
 
         if(in_array($repo->SCM, $this->config->repo->notSyncSCM))
         {
@@ -1798,10 +1796,12 @@ class repoModel extends model
                 }
             }
 
+            $server = $this->loadModel('gitfox')->getServer();
+
             $repo->path     = (!$repo->path && $service) ? sprintf($this->config->repo->{$service->type}->apiPath, $service->url, $repo->serviceProject) : $repo->path;
-            $repo->apiPath  = sprintf($this->config->repo->{$service->type}->apiPath, $service->url, $repo->serviceProject);
-            $repo->client   = $service ? $service->url : '';
-            $repo->password = $service ? $service->token : '';
+            $repo->apiPath  = sprintf($this->config->repo->gitfox->apiPath, $server->url, $repo->gitfoxID);
+            $repo->client   = $server ? $server->url : '';
+            $repo->password = $server ? $server->token : '';
             $repo->codePath = isset($project->web_url) ? $project->web_url : $repo->path;
         }
         else
