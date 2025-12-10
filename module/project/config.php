fdd0e5aa2c3336ae2c8fe8b710a7777a57893ced
xieqiyu <xieqiyu@chandao.com>
2025-12-10 03:05:59 +0000

    * [bug#67885] Delete the projectType.

--- module/project/config.php
+++ module/project/config.php
@@ -12,7 +12,7 @@ $config->project->editor->activate = array('id' => 'comment', 'tools' => 'simple
 $config->project->editor->view     = array('id' => 'lastComment', 'tools' => 'simpleTools');
 
 $config->project->list = new stdclass();
-$config->project->list->exportFields = 'id,name,code,status,hasProduct,PM,storyPoints,storyCount,executionCount,budget,teamCount,invested,begin,end,realBegan,realEnd,estimate,consumed,progress';
+$config->project->list->exportFields = 'id,name,code,status,PM,storyPoints,storyCount,executionCount,budget,teamCount,invested,begin,end,realBegan,realEnd,estimate,consumed,progress';
 if($config->systemMode == 'ALM') $config->project->list->exportFields = substr_replace($config->project->list->exportFields, ',parent', 2, 0);
 $config->project->list->customCreateFields = 'budget,acl,auth';
 
@@ -68,7 +68,6 @@ $config->project->search['fields']['name']           = $lang->project->name;
 $config->project->search['fields']['code']           = $lang->project->code;
 $config->project->search['fields']['id']             = $lang->project->id;
 $config->project->search['fields']['model']          = $lang->project->model;
-$config->project->search['fields']['hasProduct']     = $lang->project->type;
 $config->project->search['fields']['parent']         = $lang->project->parent;
 $config->project->search['fields']['status']         = $lang->project->status;
 $config->project->search['fields']['desc']           = $lang->project->desc;
@@ -87,7 +86,6 @@ $config->project->search['params']['name']           = array('operator' => 'incl
 $config->project->search['params']['code']           = array('operator' => '='      , 'control' => 'input' , 'values' => '');
 $config->project->search['params']['id']             = array('operator' => '='      , 'control' => 'input' , 'values' => '');
 $config->project->search['params']['model']          = array('operator' => '='      , 'control' => 'select', 'values' => $lang->project->modelList);
-$config->project->search['params']['hasProduct']     = array('operator' => '='      , 'control' => 'select', 'values' => $lang->project->projectTypeList);
 $config->project->search['params']['parent']         = array('operator' => '='      , 'control' => 'select', 'values' => '');
 $config->project->search['params']['status']         = array('operator' => '='      , 'control' => 'select', 'values' => $lang->project->statusList);
 $config->project->search['params']['desc']           = array('operator' => 'include', 'control' => 'input' , 'values' => '');
