adb74fa2378d71ee9a235cca5c6b4276097eda2a
liyang <liyang@easycorp.ltd>
2025-12-18 14:04:25 +0800

    + [task#154264] add ai review in create review flow page.

--- module/repo/ui/createreviewflow.html.php
+++ module/repo/ui/createreviewflow.html.php
@@ -13,6 +13,7 @@ namespace zin;
 formPanel
 (
     set::title($title),
+    set::labelWidth('240px'),
     formGroup
     (
         set::name(''),
@@ -22,14 +23,14 @@ formPanel
     ),
     formGroup
     (
-        set::width('1/2'),
+        set::width('2/3'),
         set::label($lang->repo->name),
         set::name('name'),
         set::required(true)
     ),
     formGroup
     (
-        set::width('1/2'),
+        set::width('2/3'),
         set::label($lang->repo->applicableBranchTypes),
         set::required(true),
         inputGroup
@@ -59,9 +60,38 @@ formPanel
     ),
     formGroup
     (
-        set::width('1/2'),
+        set::width('2/3'),
         set::label($lang->repo->desc),
         set::name('desc'),
         set::control(array('type' => 'textarea', 'rows' => 5)),
+    ),
+    formGroup
+    (
+        set::name(''),
+        set::control('static'),
+        set::label($lang->repo->aiReview),
+        set::labelClass('font-black')
+    ),
+    formGroup
+    (
+        set::id('aiReview'),
+        set::width('2/3'),
+        set::name('aiReview'),
+        set::required(true),
+        set::label($lang->repo->aiAssistedReview),
+        set::control(array('type' => 'radioList', 'inline' => true)),
+        set::items($lang->repo->aiReviewList),
+        set::value('disable'),
+        on::change('loadAiReviewScores')
+    ),
+    formGroup
+    (
+        set::id('aiReviewScores'),
+        set::width('2/3'),
+        set::required(true),
+        set::name('aiReviewScores'),
+        set::label($lang->repo->aiReviewScores),
+        set::control(array('type' => 'number', 'min' => 0, 'max' => 10)),
+        set::value(0)
     )
 );
