c739479fdb7f4c094e37767b8e3b76fcafdbfc49
mazhiyuan <mazhiyuan@gitfox.io>
2025-12-18 14:38:31 +0800

    * [task#154244,doing,5h,0h] add branch rule model func and unittest

--- module/repo/test/lib/repo.unittest.class.php
+++ module/repo/test/lib/repo.unittest.class.php
@@ -2116,4 +2116,91 @@ class repoTest
 
         return $result;
     }
+
+    /**
+     * Test getBranchRule method.
+     *
+     * @param  int    $typeID
+     * @param  int    $repoID
+     * @param  string $branchName
+     * @access public
+     * @return mixed
+     */
+    public function getBranchRuleTest(int $typeID = 0, int $repoID = 0, string $branchName = '')
+    {
+        $result = $this->objectModel->getBranchRule($typeID, $repoID, $branchName);
+        if(dao::isError()) return dao::getError();
+
+        return $result;
+    }
+
+    /**
+     * Test createBranchRule method.
+     *
+     * @param  object $rule
+     * @access public
+     * @return mixed
+     */
+    public function createBranchRuleTest(object $rule)
+    {
+        $result = $this->objectModel->createBranchRule($rule);
+        if(dao::isError()) return dao::getError();
+
+        if($result)
+        {
+            $lastID = $this->objectModel->dao->lastInsertID();
+            return $this->objectModel->dao->select('*')->from(TABLE_BRANCHRULESET)->where('id')->eq($lastID)->fetch();
+        }
+
+        return false;
+    }
+
+    /**
+     * Test updateBranchRule method.
+     *
+     * @param  int    $id
+     * @param  object $rule
+     * @access public
+     * @return mixed
+     */
+    public function updateBranchRuleTest(int $id, object $rule)
+    {
+        $beforeUpdate = $this->objectModel->dao->select('*')->from(TABLE_BRANCHRULESET)->where('id')->eq($id)->fetch();
+        if(!$beforeUpdate) return '0';
+
+        $result = $this->objectModel->updateBranchRule($id, $rule);
+        if(dao::isError()) return dao::getError();
+
+        if($result)
+        {
+            $afterUpdate = $this->objectModel->dao->select('*')->from(TABLE_BRANCHRULESET)->where('id')->eq($id)->fetch();
+            return $afterUpdate ? $afterUpdate : '0';
+        }
+
+        return '0';
+    }
+
+    /**
+     * Test deleteBranchRule method.
+     *
+     * @param  int $id
+     * @access public
+     * @return mixed
+     */
+    public function deleteBranchRuleTest(int $id)
+    {
+        $beforeDelete = $this->objectModel->dao->select('*')->from(TABLE_BRANCHRULESET)->where('id')->eq($id)->fetch();
+        if(!$beforeDelete) return '0';
+
+        $result = $this->objectModel->deleteBranchRule($id);
+        if(dao::isError()) return dao::getError();
+
+        if($result)
+        {
+            $afterDelete = $this->objectModel->dao->select('*')->from(TABLE_BRANCHRULESET)->where('id')->eq($id)->fetch();
+            return $afterDelete ? '0' : '1';
+        }
+
+        return '0';
+    }
 }
