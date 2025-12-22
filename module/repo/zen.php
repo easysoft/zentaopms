fc57e60d4379bbd8c65ee2a94b98c5bfab427493
liyang <liyang@easycorp.ltd>
2025-12-22 08:36:51 +0800

    + [task#154240] add build definition method.

--- module/repo/zen.php
+++ module/repo/zen.php
@@ -135,6 +135,44 @@ class repoZen extends repo
     }
 
     /**
+     * 构建审批流程定义。
+     * Build review flow definition.
+     *
+     * @param  object $formData
+     * @access protected
+     * @return object
+     */
+    protected function buildDefinition(object $formData): object
+    {
+        $definition = new stdClass();
+        $definition->ai = new stdClass();
+        $definition->ai->enable    = zget($formData, 'aiReview') == 'enable';
+        $definition->ai->approvals = new stdClass();
+        $definition->ai->approvals->score = zget($formData, 'aiReviewScores', 0);
+
+        $definition->reviewFlow = new stdClass();
+        $definition->reviewFlow->approvals = new stdClass();
+        $definition->reviewFlow->approvals->defaultReviewers   = explode(',', zget($formData, 'defaultReviewers', ''));
+        $definition->reviewFlow->approvals->specifiedReviewers = explode(',', zget($formData, 'specifiedReviewers', ''));
+        $definition->reviewFlow->approvals->minReviewers       = zget($formData, 'minReviewers', 0);
+        $definition->reviewFlow->approvals->approvalID         = zget($formData, 'approvalID', 0);
+
+        $definition->reviewFlow->issues = new stdClass();
+        $definition->reviewFlow->issues->addressOption = zget($formData, 'addressOption', '');
+        $definition->reviewFlow->issues->mandatoryType = zget($formData, 'addressOption') == 'specificMustBeSolved' ? explode(',', zget($formData, 'issueType', array())) : array();
+        $definition->reviewFlow->issues->mergeOptions  = zget($formData, 'mergeOptions', '');
+
+        $definition->reviewFlow->newCommits = new stdClass();
+        $definition->reviewFlow->newCommits->addressOption = zget($formData, 'newCommits', '');
+
+        $definition->reviewFlow->merge = new stdClass();
+        $definition->reviewFlow->merge->options     = explode(',', zget($formData, 'mergeOptions'));
+        $definition->reviewFlow->merge->autoArchive = zget($formData, 'autoArchive') == 'enable';
+
+        return $definition;
+    }
+
+    /**
      * 准备编辑版本库的数据。
      * Prepare edit repo data.
      *
