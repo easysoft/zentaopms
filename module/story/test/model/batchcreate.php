4b142b1c183080d66a4c572bc0b1157641ebfcfb
tianshujie <tianshujie@easycorp.ltd>
2025-12-24 15:39:35 +0800

    * [unittest] fix batchcreate error.

--- module/story/test/model/batchcreate.php
+++ module/story/test/model/batchcreate.php
@@ -12,7 +12,7 @@ cid=18472
 - 插入两条软件需求，判断返回的title、type等信息
  - 第1条的title属性 @测试需求1
  - 第1条的type属性 @story
- - 第1条的pri属性 @1
+ - 第1条的pri属性 @1.00
  - 第1条的spec属性 @测试需求描述1
  - 第1条的estimate属性 @1
  - 第1条的stage属性 @wait
@@ -22,7 +22,7 @@ cid=18472
  - 第6条的type属性 @requirement
  - 第6条的pri属性 @3
  - 第6条的spec属性 @测试需求描述3
- - 第6条的estimate属性 @3
+ - 第6条的estimate属性 @3.00
  - 第6条的stage属性 @wait
  - 第6条的module属性 @2223
 
@@ -62,5 +62,5 @@ $result2 = $story->batchCreateTest(2, 0, 'requirement', $stories);
 
 r(count($result1)) && p() && e('3'); // 插入两条软件需求，判断返回的需求总量
 r(count($result2)) && p() && e('3'); // 插入两条用户需求，判断返回的需求总量
-r($result1) && p('1:title,type,pri,spec,estimate,stage,module') && e('测试需求1,story,1,测试需求描述1,1,wait,2221');       // 插入两条软件需求，判断返回的title、type等信息
-r($result2) && p('6:title,type,pri,spec,estimate,stage,module') && e('测试需求3,requirement,3,测试需求描述3,3,wait,2223'); // 插入两条用户需求，判断返回的title、type等信息
+r($result1) && p('1:title,type,pri,spec,estimate,stage,module') && e('测试需求1,story,1,测试需求描述1,1.00,wait,2221');       // 插入两条软件需求，判断返回的title、type等信息
+r($result2) && p('6:title,type,pri,spec,estimate,stage,module') && e('测试需求3,requirement,3,测试需求描述3,3.00,wait,2223'); // 插入两条用户需求，判断返回的title、type等信息
