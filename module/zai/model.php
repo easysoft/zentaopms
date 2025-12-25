0a07787623828d6d0abe9bd5b2904e05c33f7bc7
sunhao <sunhao@cnezsoft.com>
2025-12-25 13:41:15 +0800

    * [misc] add admin bypass in canViewObject method to allow unrestricted access for admin users.

--- module/zai/model.php
+++ module/zai/model.php
@@ -616,6 +616,8 @@ class zaiModel extends model
      */
     public function canViewObject(string $objectType, int $objectID, ?array $attrs = null): bool
     {
+        if($this->app->user->admin) return true;
+
         if(isset(static::$objectViews[$objectType][$objectID])) return static::$objectViews[$objectType][$objectID];
 
         if(!isset(static::$objectViews[$objectType])) static::$objectViews[$objectType] = array();
