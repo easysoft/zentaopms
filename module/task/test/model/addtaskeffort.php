cded7abca6a3ce20654c6296fa732248c7c5a3fe
tianshujie <tianshujie@easycorp.ltd>
2025-12-26 15:56:38 +0800

    * [unittest] fix addtaskeffort error.

--- module/task/test/model/addtaskeffort.php
+++ module/task/test/model/addtaskeffort.php
@@ -15,24 +15,24 @@ cid=18756
 
 - 插入task为1 left为0 consumed为3的任务
  - 属性objectID @1
- - 属性left @0
- - 属性consumed @3
+ - 属性left @0.00
+ - 属性consumed @3.00
 - 插入task为2 left为0 consumed为3的任务
  - 属性objectID @2
- - 属性left @0
- - 属性consumed @3
+ - 属性left @0.00
+ - 属性consumed @3.00
 - 插入task为3 left为1 consumed为4的任务
  - 属性objectID @3
- - 属性left @1
- - 属性consumed @4
+ - 属性left @1.00
+ - 属性consumed @4.00
 - 插入task为4 left为3 consumed为6的任务
  - 属性objectID @4
- - 属性left @3
- - 属性consumed @6
+ - 属性left @3.00
+ - 属性consumed @6.00
 - 插入task为5 left为6 consumed为9的任务
  - 属性objectID @5
- - 属性left @6
- - 属性consumed @9
+ - 属性left @6.00
+ - 属性consumed @9.00
 
 */
 
@@ -67,8 +67,8 @@ $record5->left     = 6;
 $record5->consumed = 9;
 
 $task = new taskTest();
-r($task->addTaskEffortTest($record1)) && p('objectID,left,consumed') && e("1,0,3"); // 插入task为1 left为0 consumed为3的任务
-r($task->addTaskEffortTest($record2)) && p('objectID,left,consumed') && e("2,0,3"); // 插入task为2 left为0 consumed为3的任务
-r($task->addTaskEffortTest($record3)) && p('objectID,left,consumed') && e("3,1,4"); // 插入task为3 left为1 consumed为4的任务
-r($task->addTaskEffortTest($record4)) && p('objectID,left,consumed') && e("4,3,6"); // 插入task为4 left为3 consumed为6的任务
-r($task->addTaskEffortTest($record5)) && p('objectID,left,consumed') && e("5,6,9"); // 插入task为5 left为6 consumed为9的任务
+r($task->addTaskEffortTest($record1)) && p('objectID,left,consumed') && e("1,0.00,3.00"); // 插入task为1 left为0 consumed为3的任务
+r($task->addTaskEffortTest($record2)) && p('objectID,left,consumed') && e("2,0.00,3.00"); // 插入task为2 left为0 consumed为3的任务
+r($task->addTaskEffortTest($record3)) && p('objectID,left,consumed') && e("3,1.00,4.00"); // 插入task为3 left为1 consumed为4的任务
+r($task->addTaskEffortTest($record4)) && p('objectID,left,consumed') && e("4,3.00,6.00"); // 插入task为4 left为3 consumed为6的任务
+r($task->addTaskEffortTest($record5)) && p('objectID,left,consumed') && e("5,6.00,9.00"); // 插入task为5 left为6 consumed为9的任务
