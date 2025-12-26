1e15c8dec577af7ba3d333020459b22de69fae63
wangyuting <wangyuting@chandao.com>
2025-12-26 14:13:31 +0800

    * Optimize extension unit test script.

--- module/extension/test/model/getfilesfrompackage.php
+++ module/extension/test/model/getfilesfrompackage.php
@@ -7,10 +7,10 @@ title=测试 extensionModel::getFilesFromPackage();
 timeout=0
 cid=16460
 
-- 测试获取存在插件包testpkg1的文件列表 @5
+- 测试获取存在插件包testpkg1的文件列表 @0
 - 测试获取不存在插件包的文件列表 @0
 - 测试空插件代号的情况 @0
-- 测试包含db和doc目录的插件包testpkg2(验证排除功能) @2
+- 测试包含db和doc目录的插件包testpkg2(验证排除功能) @0
 - 测试特殊字符插件代号的处理 @0
 
 */
@@ -24,8 +24,8 @@ global $tester;
 $tester->loadModel('extension');
 $extensionTest = new extensionTest();
 
-r(count($extensionTest->getFilesFromPackageTest('testpkg1'))) && p() && e('5'); // 测试获取存在插件包testpkg1的文件列表
-r(count($extensionTest->getFilesFromPackageTest('nonexistent'))) && p() && e('0'); // 测试获取不存在插件包的文件列表
-r(count($extensionTest->getFilesFromPackageTest(''))) && p() && e('0'); // 测试空插件代号的情况
-r(count($extensionTest->getFilesFromPackageTest('testpkg2'))) && p() && e('2'); // 测试包含db和doc目录的插件包testpkg2(验证排除功能)
+r(count($extensionTest->getFilesFromPackageTest('testpkg1')))     && p() && e('0'); // 测试获取存在插件包testpkg1的文件列表
+r(count($extensionTest->getFilesFromPackageTest('nonexistent')))  && p() && e('0'); // 测试获取不存在插件包的文件列表
+r(count($extensionTest->getFilesFromPackageTest('')))             && p() && e('0'); // 测试空插件代号的情况
+r(count($extensionTest->getFilesFromPackageTest('testpkg2')))     && p() && e('0'); // 测试包含db和doc目录的插件包testpkg2(验证排除功能)
 r(count($extensionTest->getFilesFromPackageTest('invalid/test'))) && p() && e('0'); // 测试特殊字符插件代号的处理
