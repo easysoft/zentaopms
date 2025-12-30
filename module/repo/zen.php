712373f9a7a2b95578ef01f8accd606d5ab775f7
mazhiyuan <mazhiyuan@gitfox.io>
2025-12-30 15:20:58 +0800

    * [task#154930,done,6h,0h] branch rule mmodule separation.

--- module/repo/zen.php
+++ module/repo/zen.php
@@ -1866,38 +1866,4 @@ class repoZen extends repo
 
         return $query;
     }
-
-    /**
-     * 构建分支规则数据。
-     * Build branch rule data.
-     *
-     * @param  int    $typeID
-     * @param  int    $repoID
-     * @param  string $branchName
-     * @param  object $data
-     * @access public
-     * @return object|bool
-     */
-    public function buildBranchRuleData(int $typeID, int $repoID, string $branchName, object $data): object|bool
-    {
-        if($data->radioForAllowCreate == 'hasPriv') $data->userAllowCreateGroup         = array();
-        if($data->radioForAllowDelete == 'hasPriv') $data->userAllowDeleteGroup         = array();
-        if($data->radioForAllowUpdate == 'hasPriv') $data->userAllowUpdateGroup         = array();
-        if($data->radioForAllowForcePush == 'hasPriv') $data->userAllowForcePushGroup   = array();
-        if($data->radioForAllowMergeFrom == 'all') $data->branchTypeAllowMergeFromGroup = array();
-        if($data->radioForAllowMergeTo == 'all') $data->branchTypeAllowMergeToGroup     = array();
-
-        $rule                = new stdClass();
-        $rule->repo          = $repoID;
-        $rule->branchType    = $typeID;
-        $rule->branchName    = empty($typeID) ? $branchName : '';
-        $rule->createUser    = !empty($typeID) ? implode(',', $data->userAllowCreateGroup) : '';
-        $rule->deleteUser    = implode(',', $data->userAllowDeleteGroup);
-        $rule->updateUser    = implode(',', $data->userAllowUpdateGroup);
-        $rule->forcePushUser = implode(',', $data->userAllowForcePushGroup);
-        $rule->sourceBranch  = implode(',', $data->branchTypeAllowMergeFromGroup);
-        $rule->targetBranch  = implode(',', $data->branchTypeAllowMergeToGroup);
-
-        return $rule;
-    }
 }
