006a45a58504d7c3ddb165829455fbb400fe075f
sunguangming <sunguangming@chandao.com>
2026-01-16 01:02:51 +0000

    * [misc] modify unittest.

--- module/story/test/model/update.php
+++ module/story/test/model/update.php
@@ -1,5 +1,6 @@
 #!/usr/bin/env php
 <?php
+
 /**
 
 title=测试 storyModel->update();
@@ -10,12 +11,12 @@ cid=18593
  - 属性title @编辑后的名称1
  - 属性pri @1
  - 属性sourceNote @来源备注1
- - 属性estimate @1
+ - 属性estimate @1.00
 - 编辑需求，判断返回的信息
  - 属性title @编辑后的名称2
  - 属性pri @2
  - 属性sourceNote @来源备注2
- - 属性estimate @2
+ - 属性estimate @2.00
 
 */
 include dirname(__FILE__, 5) . '/test/lib/init.php';
@@ -36,5 +37,5 @@ $story   = new storyModelTest();
 $result1 = $story->updateTest(1, $params1);
 $result2 = $story->updateTest(2, $params2);
 
-r($result1) && p('title,pri,sourceNote,estimate') && e('编辑后的名称1,1,来源备注1,1'); // 编辑需求，判断返回的信息
-r($result2) && p('title,pri,sourceNote,estimate') && e('编辑后的名称2,2,来源备注2,2'); // 编辑需求，判断返回的信息
+r($result1) && p('title,pri,sourceNote,estimate') && e('编辑后的名称1,1,来源备注1,1.00'); // 编辑需求，判断返回的信息
+r($result2) && p('title,pri,sourceNote,estimate') && e('编辑后的名称2,2,来源备注2,2.00'); // 编辑需求，判断返回的信息
