9cd115aa193af0a7db5cf520c51a95807dcc7486
liyang <liyang@easycorp.ltd>
2026-01-26 15:55:00 +0800

    + [task#156100] add execution page.

---
+++ module/pipeline/ui/execution.html.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+/**
+ * The execution view file of pipeline module of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2023 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Ke Zhao<zhaoke@easycorp.ltd>
+ * @package     pipeline
+ * @link        https://www.zentao.net
+ */
+
+namespace zin;
+
+jsVar('repoID', $repoID);
+
+if($repoID)
+{
+    dropmenu(set::objectID($repoID), set::text($repo->name), set::tab('repo'));
+    unset($lang->pipeline->featureBar['execution']);
+    unset($config->pipeline->execution->dtable->fieldList['repo']);
+    featureBar(div(searchToggle(set::module('pipelineexec'), set::open($type == 'bySearch'))));
+}
+else
+{
+    /* zin: Define the set::module('pipeline') feature bar on main menu. */
+    featureBar
+    (
+        set::current($type),
+        set::link($this->createLink('pipeline', 'execution', "spaceID={$spaceID}&repoID={$repoID}&type={key}")),
+        div(searchToggle(set::module('pipelineexec'), set::open($type == 'bySearch')))
+    );
+}
+$config->pipeline->execution->dtable->fieldList['repo']['map'] = $repos;
+$tableData = initTableData($executionList, $config->pipeline->execution->dtable->fieldList, $this->pipeline);
+dtable
+(
+    set::cols($config->pipeline->execution->dtable->fieldList),
+    set::data($tableData),
+    set::userMap($users),
+    set::sortLink(createLink('pipeline', 'execution', "space={$spaceID}&repoID={$repoID}&type={$type}&queryID={$queryID}&orderBy={name}_{sortType}&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}&pageID={$pager->pageID}")),
+    set::orderBy($orderBy),
+    set::footPager(usePager())
+);
