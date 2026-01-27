2df7d919875d71af8ee3eac24337be3eca96405d
tianshujie <tianshujie@easycorp.ltd>
2026-01-27 16:03:01 +0800

    * [unittest] fix afterSplitTask error.

--- module/task/test/model/aftersplittask.php
+++ module/task/test/model/aftersplittask.php
@@ -8,7 +8,7 @@ cid=18762
 
 - 测试如果父任务有工时消耗时是否成功克隆子任务。属性9 @9
 - 测试父任务状态是否改变。属性status @wait
-- 测试父任务动态是否创建成功。属性actionID @4
+- 测试父任务动态是否创建成功。属性actionID @6
 - 测试父任务预计开始时间是否更新。 @2021-01-09
 - 测试父任务截止时间是否更新。 @2021-01-23
 
@@ -27,6 +27,6 @@ $task = new taskModelTest();
 
 r($task->afterSplitTaskTest($parentIdList[0], array(2), 'children'))         && p('9')        && e('9');          // 测试如果父任务有工时消耗时是否成功克隆子任务。
 r($task->afterSplitTaskTest($parentIdList[0], array(2), 'parent'))           && p('status')   && e('wait');       // 测试父任务状态是否改变。
-r($task->afterSplitTaskTest($parentIdList[1], array(4), 'parentAction'))     && p('actionID') && e('4');          // 测试父任务动态是否创建成功。
+r($task->afterSplitTaskTest($parentIdList[1], array(4), 'parentAction'))     && p('actionID') && e('6');          // 测试父任务动态是否创建成功。
 r($task->afterSplitTaskTest($parentIdList[1], array(4), 'parentEstStarted')) && p()           && e('2021-01-09'); // 测试父任务预计开始时间是否更新。
 r($task->afterSplitTaskTest($parentIdList[1], array(4), 'parentDeadline'))   && p()           && e('2021-01-23'); // 测试父任务截止时间是否更新。
