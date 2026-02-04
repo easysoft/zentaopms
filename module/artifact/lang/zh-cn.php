f0b393243157f80244debbedd2ad34ef179957e4
liyang <liyang@easycorp.ltd>
2026-02-04 16:38:54 +0800

    + [task#156859] add lang.

---
+++ module/artifact/lang/zh-cn.php
@@ -0,0 +1,10 @@
+<?php
+$lang->artifact->browse = '代码库制品库';
+$lang->artifact->create = '创建制品库';
+
+$lang->artifact->name = '名称';
+
+$lang->artifact->okBtn = '确定';
+
+$lang->artifact->placeholder = new stdclass();
+$lang->artifact->placeholder->name = '请输入制品库名称';
