a86a3a8f31247564c1b4084904078ea236932ede
liyang <liyang@easycorp.ltd>
2026-02-05 11:35:36 +0800

    + [task#156862] add edit config.

--- module/artifact/config.php
+++ module/artifact/config.php
@@ -1,3 +1,6 @@
 <?php
 $this->config->artifact->create = new stdClass();
 $this->config->artifact->create->requiredFields = 'name';
+
+$this->config->artifact->edit = new stdClass();
+$this->config->artifact->edit->requiredFields = 'name';
