10acf84249da57546078a170cdadc2a245cfcfcc
liyang <liyang@easycorp.ltd>
2026-02-05 10:22:15 +0800

    + [task#156860] add artifact lang.

--- module/artifact/lang/zh-cn.php
+++ module/artifact/lang/zh-cn.php
@@ -1,10 +1,17 @@
 <?php
 $lang->artifact->browse = '代码库制品库';
 $lang->artifact->create = '创建制品库';
+$lang->artifact->edit   = '编辑制品库';
+$lang->artifact->delete = '删除制品库';
 
 $lang->artifact->name = '名称';
 
 $lang->artifact->okBtn = '确定';
 
+$lang->artifact->countArtifact = '共%s个制品';
+
 $lang->artifact->placeholder = new stdclass();
 $lang->artifact->placeholder->name = '请输入制品库名称';
+
+$lang->artifact->notice = new stdclass();
+$lang->artifact->notice->deleteConfirm = '您确定要删除该制品库吗？';
