83adfaa9f1b455e78a53d056d6b62fd3c6518968
liyang <liyang@easycorp.ltd>
2026-03-04 10:18:44 +0800

    + [task#157438] add ssh table config.

--- module/my/config/table.php
+++ module/my/config/table.php
@@ -1253,3 +1253,41 @@ unset($config->my->team->dtable->fieldList['actions']);
 
 $config->my->project = new stdclass();
 $config->my->project->dtable = $config->project->dtable;
+
+$config->my->ssh = new stdclass();
+$config->my->ssh->actionList = array();
+$config->my->ssh->actionList['edit']['icon'] = 'edit';
+$config->my->ssh->actionList['edit']['text'] = $lang->edit;
+$config->my->ssh->actionList['edit']['hint'] = $lang->edit;
+$config->my->ssh->actionList['edit']['url']  = array('module' => 'my', 'method' => 'editSSH', 'params' => "sshID={id}");
+
+$config->my->ssh->actionList['delete']['icon']         = 'trash';
+$config->my->ssh->actionList['delete']['text']         = $lang->delete;
+$config->my->ssh->actionList['delete']['hint']         = $lang->delete;
+$config->my->ssh->actionList['delete']['url']          = array('module' => 'my', 'method' => 'deleteSSH', 'params' => "sshID={id}");
+$config->my->ssh->actionList['delete']['className']    = 'ajax-submit';
+$config->my->ssh->actionList['delete']['data-confirm'] = array('message' => $lang->my->confirmDeleteSSH);
+
+$config->my->ssh->dtable = new stdclass();
+$config->my->ssh->dtable->fieldList['id']['name']  = 'id';
+$config->my->ssh->dtable->fieldList['id']['title'] = $lang->idAB;
+$config->my->ssh->dtable->fieldList['id']['type']  = 'id';
+
+$config->my->ssh->dtable->fieldList['name']['name']  = 'title';
+$config->my->ssh->dtable->fieldList['name']['title'] = $lang->my->name;
+$config->my->ssh->dtable->fieldList['name']['type']  = 'title';
+
+$config->my->ssh->dtable->fieldList['createdDate']['name']  = 'createdDate';
+$config->my->ssh->dtable->fieldList['createdDate']['title'] = $lang->my->createdDate;
+$config->my->ssh->dtable->fieldList['createdDate']['type']  = 'datetime';
+
+$config->my->ssh->dtable->fieldList['lastUsed']['name']  = 'lastUsed';
+$config->my->ssh->dtable->fieldList['lastUsed']['title'] = $lang->my->lastUsed;
+$config->my->ssh->dtable->fieldList['lastUsed']['type']  = 'text';
+
+$config->my->doc->dtable->fieldList['actions']['name']     = 'actions';
+$config->my->doc->dtable->fieldList['actions']['title']    = $lang->actions;
+$config->my->doc->dtable->fieldList['actions']['type']     = 'actions';
+$config->my->doc->dtable->fieldList['actions']['sortType'] = false;
+$config->my->doc->dtable->fieldList['actions']['list']     = $config->my->ssh->actionList;
+$config->my->doc->dtable->fieldList['actions']['menu']     = array('edit', 'delete');
