62646a39fee0b43494bdde1e2b1e574b08419187
wangyuting <wangyuting@chandao.com>
2026-03-09 10:02:45 +0800

    * [bug#69747,done,1h,0h] Fix hide program chart error.

--- module/chart/model.php
+++ module/chart/model.php
@@ -93,9 +93,12 @@ class chartModel extends model
                 ->andWhere("FIND_IN_SET({$groupID}, `group`)")
                 ->andWhere('stage')->eq('published')
                 ->andWhere('id')->in($viewableObjects)
+                ->beginIF(!helper::hasFeature('program'))->andWhere('code')->notLike("%program%")->fi()
+                ->beginIF(!helper::hasFeature('devops'))->andWhere('code')->notLike("%devops%")->fi()
                 ->orderBy('id_desc')
                 ->limit(1)
                 ->fetch();
+
             if($chart)
             {
                 $chart = $this->processChart($chart);
@@ -181,7 +184,7 @@ class chartModel extends model
         $chartGroups = array();
         foreach($groups as $group)
         {
-            $chartGroups[$group->id] = $this->dao->select('id, name, builtin')->from(TABLE_CHART)
+            $chartGroups[$group->id] = $this->dao->select('id, name, builtin, code')->from(TABLE_CHART)
                 ->where('deleted')->eq('0')
                 ->andWhere('builtin', true)->eq('0')
                 ->orWhere('id')->in($this->config->screen->builtinChart)
@@ -198,8 +201,6 @@ class chartModel extends model
         foreach($groups as $group)
         {
             if(empty($chartGroups[$group->id])) continue;
-            if(!helper::hasFeature('program') && strpos($group->name, $this->lang->program->common) !== false) continue;
-            if(!helper::hasFeature('devops')  && strpos($group->name, $this->lang->devops->common)  !== false) continue;
 
             /* 菜单树中只显示二级分组名称。*/
             /* Only the name of the second-level group is displayed in the menu tree. */
@@ -207,7 +208,7 @@ class chartModel extends model
 
             foreach($chartGroups[$group->id] as $chart)
             {
-                if(!$this->isClickable($chart, 'design') && !helper::hasFeature('program') && strpos($chart->name, $this->lang->program->common) !== false) continue;
+                if(!$this->isClickable($chart, 'design') && !helper::hasFeature('program') && strpos(strtolower($chart->code), 'program') !== false) continue;
                 $treeMenu[] = (object)array('id' => $group->id . '_' . $chart->id, 'parent' => $group->id, 'name' => $chart->name);
             }
         }
