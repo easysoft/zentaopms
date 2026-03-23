cdc16aebd9c30931c09def7cb1a9963e244512f2
wangyuting <wangyuting@chandao.com>
2026-03-23 15:04:16 +0800

    * Adjust jirauser icon.

--- module/company/js/browse.ui.js
+++ module/company/js/browse.ui.js
@@ -2,7 +2,7 @@ window.onRenderCell = function(result, {row, col})
 {
     if(result && col.name == 'realname' && row.data.jira)
     {
-        result[0] = {html: '<icon class="icon icon-link mr-1"></icon>' + result[0]};
+        result[0] = {html: '<div class="flex items-center"><img src="static/png/jirauser.png" class="mr-1"></img>' + result[0] + '</div>'};
     }
 
     return result;
