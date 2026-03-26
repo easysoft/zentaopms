6e5e2d351dfbc9104f5385f2bd0ec28979d1fddf
sunhao <sunhao@cnezsoft.com>
2026-03-26 13:30:47 +0800

    * [bug #69958] adjust scrollbar color in menu popover.

--- module/index/ui/index.html.php
+++ module/index/ui/index.html.php
@@ -164,7 +164,7 @@ div
                     span(setClass('text'), $lang->more),
                     toggle::dropdown(array('placement' => 'right-end', 'offset' => 12))
                 ),
-                ul(setClass('dropdown-menu menu'), setID('menuMoreList'))
+                ul(setClass('dropdown-menu menu scrollbar-light'), setID('menuMoreList'))
             )
         ),
         ul
