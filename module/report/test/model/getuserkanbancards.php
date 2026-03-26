ceeef38033014f99631deaae3477105909988943
sunguangming <sunguangming@chandao.com>
2026-03-26 06:04:07 +0000

    * [misc] modify unittest.

--- module/report/test/model/getuserkanbancards.php
+++ module/report/test/model/getuserkanbancards.php
@@ -10,13 +10,14 @@ $card = zenData('kanbancard');
 $card->id->range('1-8');
 $card->kanban->range('1');
 $card->region->range('1');
+$card->progress->range('1');
 $card->group->range('1');
 $card->name->range('卡片1,卡片2,卡片3,卡片4,卡片5,卡片6,卡片7,卡片8');
 $card->assignedTo->range('admin,admin,user1,user2,,user3,user1,user2');
 $card->status->range('doing,doing,doing,doing,doing,doing,done,doing');
 $card->archived->range('0,0,0,0,0,1,0,0');
 $card->deleted->range('0,0,0,0,0,0,0,1');
-$card->end->range('`' . date('Y-m-d', strtotime('+1 day')) . '`,`' . date('Y-m-d', strtotime('+2 day')) . '`,`' . date('Y-m-d', strtotime('+3 day')) . '`,`' . date('Y-m-d', strtotime('+10 day')) . '`,`' . date('Y-m-d', strtotime('+1 day')) . '`,`' . date('Y-m-d', strtotime('+1 day')) . '`,`' . date('Y-m-d', strtotime('+1 day')) . '`,`' . date('Y-m-d', strtotime('+1 day')) . '`');
+$card->end->range('`' . date('Y-m-d') . '`,`' . date('Y-m-d') . '`,`' . date('Y-m-d', strtotime('+3 day')) . '`,`' . date('Y-m-d', strtotime('+10 day')) . '`,`' . date('Y-m-d') . '`,`' . date('Y-m-d') . '`,`' . date('Y-m-d') . '`,`' . date('Y-m-d') . '`');
 $card->gen(8);
 
 su('admin');
