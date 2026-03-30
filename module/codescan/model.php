fbc01d7b68ae2d947e80e03e57ccaed3e4cd978f
liuruogu <liuruogu@easycorp.ltd>
2026-03-30 00:56:14 +0000

    * Interface with and confirm the issue API.

--- module/codescan/model.php
+++ module/codescan/model.php
@@ -822,20 +822,13 @@ class codescanModel extends model
      */
     public function changeIssueState(int|array $issueIdList, string $status, string $solution = null, string $solutionDate = null, int $ignoreDate = 0): bool
     {
-        $apiRoot = $this->loadModel('gitfox')->getApiRoot();
         if(is_numeric($issueIdList)) $issueIdList = array($issueIdList);
         foreach($issueIdList as $index => $issueID) $issueIdList[$index] = (int)$issueID;
-
-        $url = sprintf($apiRoot->url, "/scan/issues/status");
 
         $data = new stdclass();
         $data->status = $status;
         $data->ids    = $issueIdList;
-        if(!is_null($solution))     $data->resolution = $solution;
-        if(!is_null($solutionDate)) $data->resolved   = strtotime($solutionDate) * 1000;
-        if(!empty($ignoreDate))     $data->ignored    = $ignoreDate == -1 ? -1 : $ignoreDate * 1000;
-        $result = json_decode(common::http($url, $data, array(CURLOPT_CUSTOMREQUEST => 'POST'), $apiRoot->header, 'json', 'POST'));
-        $this->gitfox->getResponse($result);
+        $result = $this->loadModel('gitfox')->request('/scan/issues/status', 'PUT', $data);
         if(dao::isError()) return false;
 
         $this->loadModel('action');
@@ -844,7 +837,7 @@ class codescanModel extends model
         foreach($issueIdList as $issueID)
         {
             $issue = $this->getScanIssue($issueID, false);
-            $extra = empty($issue) || empty($repoPair[$issue->repo_id]) ? '' : "{$issue->content}|issueID={$issueID}&repoID={$repoPair[$issue->repo_id]}";
+            $extra = empty($issue) || empty($repoPair[$issue->repoID]) ? '' : "{$issue->content}|issueID={$issueID}&repoID={$repoPair[$issue->repoID]}";
             $this->action->create('codescanissue', $issueID, $status . 'ScanIssue', '', $extra);
         }
 
