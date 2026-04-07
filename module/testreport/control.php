2f67fb0054594921a29e80d9744fab77a821618e
wangyidong <yidong@chandao.com>
2026-04-07 17:50:45 +0800

    * [refac feedback #10430] Add for lost code.

--- module/testreport/control.php
+++ module/testreport/control.php
@@ -103,12 +103,11 @@ class testreport extends control
         if(empty($reports) && common::hasPriv('testreport', 'create'))
         {
             $param = '';
-            $taskIdList = empty($_POST['taskIdList']) ? $extra : implode(',', $_POST['taskIdList']);
-            if($objectType == 'product' && $extra) $param = "objectID={$extra}&objectType=testtask";
-            if(in_array($objectType, array('project', 'execution')) && ($extra || !empty($_POST['taskIdList']))) $param = "objectID={$objectID}&objectType={$objectType}&extra={$taskIdList}";
+            if(in_array($objectType, array('project', 'execution')) && ($extra || !empty($_POST['taskIdList']))) $param = "objectID={$objectID}&objectType={$objectType}&extra={$extra}";
             if($param)
             {
                 $url = $this->createLink('testreport', 'create', $param);
+                if(!empty($_POST['taskIdList'])) $url .= (strpos($url, '?') !== false ? '&' : '?') . 'taskIdList=' . implode(',', $_POST['taskIdList']);
                 $this->locate($url);
             }
         }
@@ -163,6 +162,7 @@ class testreport extends control
             return $this->send(array('result' => 'success', 'message' => $this->lang->saveSuccess, 'load' => inlink('view', "reportID={$reportID}"), 'id' => $reportID));
         }
 
+        if($this->get->taskIdList) $extra = $this->get->taskIdList;
         if($objectType == 'testtask') list($objectID, $task, $productID) = $this->testreportZen->assignTaskParisForCreate($objectID, $extra);
 
         if(!$objectID) return $this->send(array('result' => 'fail', 'load' => array('confirm' => $this->lang->testreport->noObjectID, 'confirmed' => inlink('browse', "proudctID={$productID}"), 'canceled' => inlink('browse', "proudctID={$productID}"))));
