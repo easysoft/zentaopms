86b00bec7c19b7f1c7d54a47a6d9d0cd77cd9e5a
wangyidong <yidong@chandao.com>
2026-04-09 10:44:58 +0800

    * [refac bug #71423] Add exportgantt package.

--- module/group/packagemanager.php
+++ module/group/packagemanager.php
@@ -3542,7 +3542,12 @@ $config->group->package->gantt->subset = 'programplan';
 $config->group->package->gantt->privs  = array();
 $config->group->package->gantt->privs['programplan-browse']      = array('edition' => 'max,ipd', 'vision' => 'rnd', 'order' => 0, 'depend' => array('project-index'), 'recommend' => array('programplan-ganttEdit'));
 $config->group->package->gantt->privs['programplan-ganttEdit']   = array('edition' => 'max,ipd', 'vision' => 'rnd', 'order' => 5, 'depend' => array('programplan-browse'), 'recommend' => array());
-$config->group->package->gantt->privs['programplan-ganttExport'] = array('edition' => 'max,ipd', 'vision' => 'rnd', 'order' => 10, 'depend' => array('programplan-browse'), 'recommend' => array());
+
+$config->group->package->exportGantt = new stdclass();
+$config->group->package->exportGantt->order  = 10;
+$config->group->package->exportGantt->subset = 'programplan';
+$config->group->package->exportGantt->privs  = array();
+$config->group->package->exportGantt->privs['programplan-ganttExport'] = array('edition' => 'max,ipd', 'vision' => 'rnd', 'order' => 10, 'depend' => array('programplan-browse'), 'recommend' => array());
 
 $config->group->package->ganttVersion = new stdclass();
 $config->group->package->ganttVersion->order  = 10;
