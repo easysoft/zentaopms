87d1022bafb9e6c759e97e519341ce1b11b23b80
wangyidong <yidong@chandao.com>
2026-04-10 08:57:10 +0800

    * [refac bug #71413] Fix for unit test.

--- module/programplan/test/tao/buildplandataforgantt.php
+++ module/programplan/test/tao/buildplandataforgantt.php
@@ -44,4 +44,4 @@ r(isset($planItem->percent)) && p() && e('0'); //检查是否存在percent字段
 
 $tester->programplan->config->setPercent = true;
 $plan->milestone = true;
-r((array)$tester->programplan->buildPlanDataForGantt($plan)) && p('id,type,text,start_date,percent') && e("1,plan,项目集1<icon class='icon icon-flag icon-sm red'></icon>,28-09-2023,10.00"); //里程碑阶段，并设置显示percent，检查构建数据。
+r((array)$tester->programplan->buildPlanDataForGantt($plan)) && p('id,type,text,start_date,percent') && e("1,plan,项目集1<icon class='icon icon-flag icon-sm red'></icon>,28-09-2023,10"); //里程碑阶段，并设置显示percent，检查构建数据。
