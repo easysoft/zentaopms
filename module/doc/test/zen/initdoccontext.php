8fdd021f8c95617e44bd260ec077976638d2042e
hujunqing <hujunqing@chandao.com>
2026-04-15 03:30:19 +0000

    [story#76760,5.5h,done] add test code.

--- module/doc/test/zen/initdoccontext.php
+++ module/doc/test/zen/initdoccontext.php
@@ -18,24 +18,9 @@ cid=16179
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/lib/doczen.unittest.class.php';
 
-global $tester;
-$tester->dao->delete()->from(TABLE_DOCLIB)->exec();
-$tester->dao->delete()->from(TABLE_DOC)->exec();
-$tester->dao->delete()->from(TABLE_DOCCONTENT)->exec();
-
-$tester->dao->insert(TABLE_DOCLIB)->set('id')->eq(1)->set('name')->eq('产品文档库')->set('type')->eq('product')->set('product')->eq(1)->set('acl')->eq('default')->exec();
-$tester->dao->insert(TABLE_DOCLIB)->set('id')->eq(2)->set('name')->eq('项目文档库')->set('type')->eq('project')->set('project')->eq(11)->set('acl')->eq('default')->exec();
-$tester->dao->insert(TABLE_DOCLIB)->set('id')->eq(3)->set('name')->eq('执行文档库')->set('type')->eq('execution')->set('execution')->eq(101)->set('acl')->eq('default')->exec();
-
-$tester->dao->insert(TABLE_DOC)->set('id')->eq(1)->set('title')->eq('文档1')->set('lib')->eq(1)->set('type')->eq('text')->set('version')->eq(1)->exec();
-$tester->dao->insert(TABLE_DOC)->set('id')->eq(2)->set('title')->eq('文档2')->set('lib')->eq(1)->set('type')->eq('text')->set('version')->eq(1)->exec();
-$tester->dao->insert(TABLE_DOC)->set('id')->eq(3)->set('title')->eq('文档3')->set('lib')->eq(2)->set('type')->eq('text')->set('version')->eq(1)->exec();
-$tester->dao->insert(TABLE_DOC)->set('id')->eq(5)->set('title')->eq('文档5')->set('lib')->eq(3)->set('type')->eq('text')->set('version')->eq(1)->exec();
-
-$tester->dao->insert(TABLE_DOCCONTENT)->set('doc')->eq(1)->set('title')->eq('文档1')->set('content')->eq('内容1')->set('version')->eq(1)->exec();
-$tester->dao->insert(TABLE_DOCCONTENT)->set('doc')->eq(2)->set('title')->eq('文档2')->set('content')->eq('内容2')->set('version')->eq(1)->exec();
-$tester->dao->insert(TABLE_DOCCONTENT)->set('doc')->eq(3)->set('title')->eq('文档3')->set('content')->eq('内容3')->set('version')->eq(1)->exec();
-$tester->dao->insert(TABLE_DOCCONTENT)->set('doc')->eq(5)->set('title')->eq('文档5')->set('content')->eq('内容5')->set('version')->eq(1)->exec();
+zenData('doclib')->loadYaml('doclib', false, 2)->gen(5);
+zenData('doccontent')->gen(0);
+zenData('doc')->loadYaml('doc', false, 2)->gen(5);
 
 su('admin');
 
