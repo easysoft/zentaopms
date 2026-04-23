a3f3867e7b02649500f475be76768986929c1e66
liyang <liyang@easycorp.ltd>
2026-04-23 14:10:58 +0800

    * [bug#72112] fix issue search param.

--- module/codescan/zen.php
+++ module/codescan/zen.php
@@ -168,16 +168,26 @@ class codescanZen extends codescan
                 {
                     if(isset($this->config->codescan->remoteFields[$search['field']])) $search['field'] = $this->config->codescan->remoteFields[$search['field']];
                     if($search['field'] == 'id' && $method == 'task') $search['field'] = 'taskID';
+                    if(in_array($search['field'], array('started', 'finished')))
+                    {
+                        $search['field'] = in_array($search['operator'], array('<', '<=')) ? $search['field'] . 'Lt' : $search['field'] . 'Gt';
+                    }
 
-                    if(strpos($search['field'], 'Date') || in_array($search['field'], array('started', 'finished')))
+                    if(strpos($search['field'], 'Date'))
                     {
                         $fields     = explode('_', $search['field']);
                         $dateFilter = $this->getDateFilter($search['value']);
 
-                        $param["{$fields[0]}Lt"] = $dateFilter['begin'];
-                        $param["{$fields[0]}Gt"] = $dateFilter['end'];
-
-                        $search['value'] = $dateFilter['begin'];
+                        if($search['operator'] == '=')
+                        {
+                            $search['value'] = $dateFilter['begin'];
+                        }
+                        else
+                        {
+                            $param["{$fields[0]}Gt"] = $dateFilter['begin'];
+                            $param["{$fields[0]}Lt"] = $dateFilter['end'];
+                            continue;
+                        }
                     }
                     if($search['field'] == 'lang' && $search['value'] == 'all') $search['value'] = '';
 
