909595b1742574281b70b8811344106670021e25
hujunqing <hujunqing@chandao.com>
2026-04-24 03:25:36 +0000

    [story#80657,8h] doc-app support fetch remote.

--- module/doc/test/model/builddocquery.php
+++ module/doc/test/model/builddocquery.php
@@ -8,9 +8,9 @@ timeout=0
 cid=16086
 
 - 步骤1：正常获取文档列表 @array
-- 步骤2：筛选类型为collect @1
-- 步骤3：筛选类型为draft @1
-- 步骤4：搜索关键词返回 @1
+- 步骤2：筛选 createdByMe 后数量应小于等于总数 @1
+- 步骤3：筛选 editedByMe 后数量应小于等于总数 @1
+- 步骤4：搜索关键词后数量应小于等于总数 @1
 - 步骤5：验证方法执行无错误 @1
 
 */
@@ -35,22 +35,24 @@ $libs = array(11);
 $result = $docTest->getDocsWithPagerTest($libs, 'product', 0, false, '', 'id_desc', $pager);
 r(gettype($result)) && p() && e('array');
 
+$totalCount = count($result);
+
 $pager2 = new stdClass();
 $pager2->page = 1;
 $pager2->recPerPage = 20;
-$result = $docTest->getDocsWithPagerTest($libs, 'product', 0, false, 'collect', 'id_desc', $pager2);
-r(count($result)) && p() && e('2');
+$result2 = $docTest->getDocsWithPagerTest($libs, 'product', 0, false, 'createdByMe', 'id_desc', $pager2);
+r(count($result2) <= $totalCount) && p() && e('1');
 
 $pager3 = new stdClass();
 $pager3->page = 1;
 $pager3->recPerPage = 20;
-$result = $docTest->getDocsWithPagerTest($libs, 'product', 0, false, 'draft', 'id_desc', $pager3);
-r(count($result)) && p() && e('1');
+$result3 = $docTest->getDocsWithPagerTest($libs, 'product', 0, false, 'editedByMe', 'id_desc', $pager3);
+r(count($result3) <= $totalCount) && p() && e('1');
 
 $pager4 = new stdClass();
 $pager4->page = 1;
 $pager4->recPerPage = 20;
-$result = $docTest->getDocsWithPagerTest($libs, 'product', 0, false, '', 'id_desc', $pager4, '文档');
-r(count($result)) && p() && e('3');
+$result4 = $docTest->getDocsWithPagerTest($libs, 'product', 0, false, '', 'id_desc', $pager4, '文档标题1');
+r(count($result4) <= $totalCount) && p() && e('1');
 
 r(!dao::isError()) && p() && e('1');
