844807d6d035b8d3426676dec88839ce5bc994c1
daitingting <daitingting@xirangit.com>
2026-04-28 10:53:19 +0800

    * [task#160195,done,1h] Get workflow group of project.

--- module/datatable/control.php
+++ module/datatable/control.php
@@ -219,7 +219,14 @@ class datatable extends control
 
         if($module == 'deliverable' || ($module == 'project' && $method == 'deliverable'))
         {
-            $hasProcess = $this->loadModel('workflowgroup')->hasFeature((int)$extra, 'process');
+            $workflowGroup = (int)$extra;
+            if($module == 'project')
+            {
+                $project = $this->datatable->fetchByID($this->session->project, 'project');
+                $workflowGroup = $project->workflowGroup;
+            }
+
+            $hasProcess = $this->loadModel('workflowgroup')->hasFeature($workflowGroup, 'process');
             if(!$hasProcess) unset($cols['activity'], $cols['trimmable'], $cols['trimRule']);
         }
 
