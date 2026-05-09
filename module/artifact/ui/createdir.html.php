26d5acbbff17407b4e79a9dbe3aeae89f0ece790
liyang <liyang@easycorp.ltd>
2026-05-09 17:13:27 +0800

    + [task#160326] adjust create dir method.

--- module/artifact/ui/createdir.html.php
+++ module/artifact/ui/createdir.html.php
@@ -15,30 +15,10 @@ formPanel
     set::title($title),
     set::labelWidth('100px'),
     set::submitBtnText($lang->artifact->okBtn),
-    on::init()->call('loadFormat'),
-    on::change('#hasVersion')->call('loadFormat'),
     formGroup
     (
         set::label($lang->artifact->dirName),
         set::name('name'),
         set::required(true),
-        checkBox
-        (
-            setID('hasVersion'),
-            set::rootClass('mt-2'),
-            set::text($lang->artifact->hasVersion),
-            set::inline(true),
-            set::checked(false)
-        )
-    ),
-    formGroup
-    (
-        setID('format'),
-        set::label($lang->artifact->format),
-        set::name('format'),
-        set::required(true),
-        set::control('radioList'),
-        set::items($lang->artifact->formatList),
-        set::value('raw')
-    ),
+    )
 );
