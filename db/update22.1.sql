0f0bdaef7aba566ccb90ab63dcc0f034f88a0187
朱金勇 <zhujinyong@chandao.com>
2026-05-14 15:46:07 +0800

    - [misc] Remove webRoot config.

--- db/update22.1.sql
+++ db/update22.1.sql
@@ -9,3 +9,5 @@ ALTER TABLE `zt_projectdeliverable` ADD COLUMN `submittedBy` varchar(30) NOT NUL
 ALTER TABLE `zt_projectdeliverable` ADD COLUMN `submittedDate` datetime DEFAULT NULL COMMENT '提交时间' AFTER `submittedBy`;
 
 ALTER TABLE `zt_reviewissue` MODIFY COLUMN `resolutionDate` datetime DEFAULT NULL COMMENT '解决时间';
+
+DELETE FROM `zt_config` WHERE owner = 'system' AND module = 'common' AND `key` = 'webRoot';
