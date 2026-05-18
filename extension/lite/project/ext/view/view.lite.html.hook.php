39fffc0fc91f7240649601faa3bb351d3ace98e4
sunguangming <sunguangming@chandao.com>
2026-05-18 02:15:28 +0000

    * [misc] remove unused files.

--- extension/lite/project/ext/view/view.lite.html.hook.php
+++
@@ -1,5 +0,0 @@
-<script>
-$('.side-col .row .cell .detail').addClass('hidden');
-$('.side-col .row .cell .detail:first').removeClass('hidden');
-$('.side-col .row .cell .detail:last').removeClass('hidden');
-</script>
