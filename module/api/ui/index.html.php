2bcbc47341f1b9c4e4e1acf47f9886e9cebaaa95
hujunqing <hujunqing@chandao.com>
2026-05-20 03:04:44 +0000

    [story#160873,15,done] support export api docs to openapi.

--- module/api/ui/index.html.php
+++ module/api/ui/index.html.php
@@ -11,27 +11,27 @@ declare(strict_types=1);
 namespace zin;
 
 $privs = array();
-$privs['collect']      = 'no';
-$privs['createApi']    = hasPriv('api', 'create');
-$privs['exportApi']    = $this->config->edition != 'open' && hasPriv('api', 'export');
-$privs['exportopenapi']= $this->config->edition != 'open' && hasPriv('api', 'exportopenapi');
-$privs['edit']         = hasPriv('api', 'edit');
-$privs['editApi']      = hasPriv('api', 'edit');
-$privs['view']         = hasPriv('api', 'view');
-$privs['delete']       = hasPriv('api', 'delete');
-$privs['createLib']    = hasPriv('api', 'createLib');
-$privs['editLib']      = hasPriv('api', 'editLib');
-$privs['moveLib']      = hasPriv('api', 'moveLib');
-$privs['sortDoclib']   = hasPriv('doc', 'sortDoclib');
-$privs['deleteLib']    = hasPriv('api', 'deleteLib');
-$privs['createStruct'] = hasPriv('api', 'createStruct');
-$privs['createRelease']= hasPriv('api', 'createRelease');
-$privs['addModule']    = hasPriv('doc', 'addCatalog');
-$privs['deleteModule'] = hasPriv('doc', 'deleteCatalog');
-$privs['editModule']   = hasPriv('doc', 'editCatalog');
-$privs['sortModule']   = hasPriv('doc', 'sortCatalog');
-$privs['releases']     = hasPriv('api', 'releases');
-$privs['struct']       = hasPriv('api', 'struct');
+$privs['collect']       = 'no';
+$privs['createApi']     = hasPriv('api', 'create');
+$privs['exportApi']     = $this->config->edition != 'open' && hasPriv('api', 'export');
+$privs['exportopenapi'] = $this->config->edition != 'open' && hasPriv('api', 'exportopenapi');
+$privs['edit']          = hasPriv('api', 'edit');
+$privs['editApi']       = hasPriv('api', 'edit');
+$privs['view']          = hasPriv('api', 'view');
+$privs['delete']        = hasPriv('api', 'delete');
+$privs['createLib']     = hasPriv('api', 'createLib');
+$privs['editLib']       = hasPriv('api', 'editLib');
+$privs['moveLib']       = hasPriv('api', 'moveLib');
+$privs['sortDoclib']    = hasPriv('doc', 'sortDoclib');
+$privs['deleteLib']     = hasPriv('api', 'deleteLib');
+$privs['createStruct']  = hasPriv('api', 'createStruct');
+$privs['createRelease'] = hasPriv('api', 'createRelease');
+$privs['addModule']     = hasPriv('doc', 'addCatalog');
+$privs['deleteModule']  = hasPriv('doc', 'deleteCatalog');
+$privs['editModule']    = hasPriv('doc', 'editCatalog');
+$privs['sortModule']    = hasPriv('doc', 'sortCatalog');
+$privs['releases']      = hasPriv('api', 'releases');
+$privs['struct']        = hasPriv('api', 'struct');
 
 docApp
 (
