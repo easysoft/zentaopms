73b5a11cd3472a25295dd73c1b21c17fffa8fec2
tianshujie <tianshujie@easycorp.ltd>
2026-05-21 15:00:51 +0800

    * [bug#72796,done,0.1h] Disabled gantt button when the type is kanban.

--- module/execution/model.php
+++ module/execution/model.php
@@ -4006,6 +4006,7 @@ class executionModel extends model
         if($action == 'putoff')       return $execution->status == 'wait' || $execution->status == 'doing';
         if($action == 'activate')     return $execution->status == 'suspended' || $execution->status == 'closed';
         if($action == 'delete')       return empty($execution->isParent);
+        if($action == 'gantt')        return $execution->type != 'kanban';
 
         return true;
     }
