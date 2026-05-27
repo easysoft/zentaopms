d46f8dea49d607502bc746aecd30e558d513af64
wangyuting <wangyuting@chandao.com>
2026-05-27 06:46:28 +0000

    * Optimize code.

--- module/caselib/test/zen/buildsearchform.php
+++ module/caselib/test/zen/buildsearchform.php
@@ -7,7 +7,7 @@ title=测试 caselibZen::buildSearchForm();
 timeout=0
 cid=15543
 
-- 步骤1：正常情况属性module @caselib
+- 步骤1：正常情况属性module @testcase
 - 步骤2：lib字段第fields条的lib属性 @所属库
 - 步骤3：标题字段第fields条的title属性 @用例名称
 - 步骤4：其他相关字段
@@ -38,7 +38,7 @@ su('admin');
 $caselibTest = new caselibZenTest();
 
 // 5. 强制要求：必须包含至少5个测试步骤
-r($caselibTest->buildSearchFormTest(1, array(1 => '测试用例库1'), 10, 'http://test.com/search')) && p('module') && e('caselib'); // 步骤1：正常情况
+r($caselibTest->buildSearchFormTest(1, array(1 => '测试用例库1'), 10, 'http://test.com/search')) && p('module') && e('testcase'); // 步骤1：正常情况
 r($caselibTest->buildSearchFormTest(1, array(1 => '测试用例库1'), 0, '')) && p('fields:lib') && e('所属库'); // 步骤2：lib字段
 r($caselibTest->buildSearchFormTest(2, array(2 => '测试用例库2'), 5, 'test.php')) && p('fields:title') && e('用例名称'); // 步骤3：标题字段
 r($caselibTest->buildSearchFormTest(1, array(1 => '库1'), 1, 'search.php')) && p('fields:type,keywords,status') && e('用例类型,关键词,用例状态'); // 步骤4：其他相关字段
