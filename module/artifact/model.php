92142b9608de0ca4f5339fc4cc7cc6cb694491f1
liyang <liyang@easycorp.ltd>
2026-05-28 10:08:44 +0800

    * [bug#72902] add notice on delete repo.

--- module/artifact/model.php
+++ module/artifact/model.php
@@ -340,4 +340,21 @@ class artifactModel extends model
         if($size < 1024 * 1024 * 1024) return round($size / 1024 / 1024, 2) . 'MB';
         return round($size / 1024 / 1024 / 1024, 2) . 'GB';
     }
+
+    /**
+     * 通过repoID获取制品库信息。
+     * Get artifact repo by repo ID.
+     *
+     * @param  int $repoID
+     * @access public
+     * @return array
+     */
+    public function getByRepoID(int $repoID): array
+    {
+        return $this->dao->select('*')->from(TABLE_ARTIFACT)
+            ->where('deleted')->eq(0)
+            ->andWhere('scope')->eq('repo')
+            ->andWhere('repoID')->eq($repoID)
+            ->fetchAll('id');
+    }
 }
