2888003e1dd6cc031a8079249aa994807dfd1858
sunhao <sunhao@cnezsoft.com>
2026-05-29 15:25:04 +0800

    * [refac] add detailed PHPDoc comments for field-related functions in field.func.php.

--- lib/zin/zentao/field.func.php
+++ lib/zin/zentao/field.func.php
@@ -29,6 +29,14 @@ function defineFieldList(string $name, string|array|field|fieldList|null ...$ext
     return fieldList::define($name, ...$extends);
 }
 
+/**
+ * Define a field.
+ *
+ * @param string                               $name     The name of field.
+ * @param string|array|field|fieldList|null ...$extends  The extend fields and field list.
+ *
+ * @return field
+ */
 function defineField(string $name, ?string $listName = null): field
 {
     if(str_contains($name, '/') && is_null($listName))
@@ -41,21 +49,51 @@ function defineField(string $name, ?string $listName = null): field
     return $fieldList->field($name);
 }
 
+/**
+ * Get a field list.
+ *
+ * @param string $name The name of field list.
+ *
+ * @return fieldList
+ */
 function fieldList(string $name)
 {
     return fieldList::ensure($name);
 }
 
+/**
+ * Create a field.
+ *
+ * @param string|object|array|null $nameOrProps The name of field or the props of field.
+ *
+ * @return field
+ */
 function field(string|object|array|null $nameOrProps = null): field
 {
     return new field($nameOrProps);
 }
 
+/**
+ * Use fields.
+ *
+ * @param string|array|field|fieldList|null ...$args The args of fields.
+ *
+ * @return fieldList
+ */
 function useFields(string|array|field|fieldList|null ...$args): fieldList
 {
     return fieldList::build(...$args);
 }
 
+/**
+ * Load fields.
+ *
+ * @param string $moduleName The name of module.
+ * @param string $methodName The name of method.
+ * @param string $viewDir    The directory of view.
+ *
+ * @return void
+ */
 function loadFields(string $moduleName, string $methodName, string $viewDir = 'ui'): void
 {
     global $app;
