f2658a44ae1a804c3c3ca48ae97fb374a0b9f949
daitingting <daitingting@xirangit.com>
2026-06-02 14:59:26 +0800

    * [misc] Adjust unit tests.

--- module/story/test/model/change.php
+++ module/story/test/model/change.php
@@ -14,6 +14,10 @@ cid=18479
 - 变更时不填写需求名称，给出提示 @1
 - 测试存在relievedTwins。属性title @测试需求1变更标题
 - 测试变成需求。属性title @名称修改
+- 测试变更时在 spec 中 @用户发送 mention 通知
+ - 属性notifyCount @1
+ - 属性mentionUser @user1
+- 测试变更时 spec 无 mention 不发送通知 @0
 
 */
 include dirname(__FILE__, 5) . '/test/lib/init.php';
@@ -61,3 +65,30 @@ r($story->changeTest(1, $story1))                               && p('title,spec
 r((int)strpos($result['title'][0], '名称』不能为空') !== false) && p()                     && e('1');                                       // 变更时不填写需求名称，给出提示
 r($story->changeTest(3, $story4))                               && p('title')              && e('测试需求1变更标题');                       // 测试存在relievedTwins。
 r($story->changeTest(4, $story5))                               && p('title')              && e('名称修改');                                // 测试变成需求。
+
+zenData('notify')->gen(0);
+$user = zenData('user');
+$user->account->range('admin,user1');
+$user->realname->range('管理员,用户1');
+$user->gen(2);
+
+$types = array();
+$types['story']       = array('mentioned');
+$types['requirement'] = array('mentioned');
+$tester->config->message->setting = array('message' => array('setting' => $types));
+
+$mentionSpan   = '<span class="mention-label" data-type="mention" data-id="user1">@user1</span>';
+$mentionStory  = clone $story1;
+$mentionStory->title   = 'mention变更标题';
+$mentionStory->spec    = $mentionSpan;
+$mentionStory->version = 5;
+$story->changeTest(5, $mentionStory);
+r($story->getLastMentionNotifyInfo()) && p('notifyCount,mentionUser') && e('1,user1'); // 测试变更时在 spec 中 @用户发送 mention 通知
+
+$notifyCount = $story->getMentionNotifyCount();
+$plainStory  = clone $story1;
+$plainStory->title   = 'plain变更标题';
+$plainStory->spec    = 'plain spec';
+$plainStory->version = 6;
+$story->changeTest(11, $plainStory);
+r($story->getMentionNotifyCount() - $notifyCount) && p() && e('0'); // 测试变更时 spec 无 mention 不发送通知
