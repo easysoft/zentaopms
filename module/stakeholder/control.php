c637ade22b445f16ec16063abcd68314cc2d5a47
wangyuting <wangyuting@chandao.com>
2026-06-03 03:27:48 +0000

    * [bug#72674,done,0.5h,0h] Fix create stakeholder error.

--- module/stakeholder/control.php
+++ module/stakeholder/control.php
@@ -246,15 +246,21 @@ class stakeholder extends control
         }
 
         $users        = $this->loadModel('user')->getPairs('noclosed');
-        $companyUsers = array_diff($users, $members);
         $stakeholders = $this->loadModel('stakeholder')->getStakeHolderPairs($programID ? $programID : $projectID);
-        foreach($companyUsers as $account => $realname)
+        foreach($users as $account => $realname)
         {
-            if(isset($stakeholders[$account])) unset($companyUsers[$account]);
+            if(isset($members[$account]))
+            {
+                unset($users[$account]);
+            }
+            else if(isset($stakeholders[$account]))
+            {
+                unset($users[$account]);
+            }
         }
 
         $userItems = array();
-        foreach($companyUsers as $account => $realname) $userItems[] = array('text' => $realname, 'value' => $account);
+        foreach($users as $account => $realname) $userItems[] = array('text' => $realname, 'value' => $account);
 
         return print(json_encode($userItems));
     }
