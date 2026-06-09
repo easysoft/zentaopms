89e18c112a5f270b4cfbea423ca7d7f6b8e40e44
daitingting <daitingting@xirangit.com>
2026-06-09 08:58:44 +0800

    * [misc] Adjust unit test.

--- module/custom/test/zen/checkinvalidkeys.php
+++ module/custom/test/zen/checkinvalidkeys.php
@@ -14,6 +14,9 @@ cid=0
 - 执行customTest模块的checkInvalidKeysTest方法，参数是'story', 'sourceList', 'zh-cn', array 属性message @键的长度必须小于20个字符！
 - 执行customTest模块的checkInvalidKeysTest方法，参数是'story', 'sourceList', 'zh-cn', array  @1
 - 执行customTest模块的checkInvalidKeysTest方法，参数是'story', 'priList', 'zh-cn', array  @1
+- 执行customTest模块的checkInvalidKeysTest方法，参数是'user', 'outside', 'zh-cn', array  @1
+- 执行customTest模块的checkInvalidKeysTest方法，参数是'user', 'outside', 'zh-cn', array 属性message @键值应当为大小写英文字母、数字或下划线的组合
+- 执行customTest模块的checkInvalidKeysTest方法，参数是'user', 'deleted', 'zh-cn', array  @1
 
 */
 
@@ -32,3 +35,6 @@ r($customTest->checkInvalidKeysTest('todo', 'typeList', 'zh-cn', array('short', 
 r($customTest->checkInvalidKeysTest('story', 'sourceList', 'zh-cn', array('valid_key', 'verylongsourcekey1234'))) && p('message') && e('键的长度必须小于20个字符！');
 r($customTest->checkInvalidKeysTest('story', 'sourceList', 'zh-cn', array('valid_key', 'test123', 'ABC_def'))) && p() && e('1');
 r($customTest->checkInvalidKeysTest('story', 'priList', 'zh-cn', array())) && p() && e('1');
+r($customTest->checkInvalidKeysTest('user', 'outside', 'zh-cn', array('0', '1'))) && p() && e('1');
+r($customTest->checkInvalidKeysTest('user', 'outside', 'zh-cn', array('valid', 'invalid-key'))) && p('message') && e('键值应当为大小写英文字母、数字或下划线的组合');
+r($customTest->checkInvalidKeysTest('user', 'deleted', 'zh-cn', array())) && p() && e('1');
--- module/user/test/model/getpairs.php
+++ module/user/test/model/getpairs.php
@@ -15,6 +15,8 @@ cid=19621
 - 使用limit参数来限制最多获取50个用户键值对 @3
 - 使用pofirst参数来将系统中的产品经理用户放到返回值前列属性account1 @A:用户名1
 - 使用pmfirst参数来将系统中的项目经理内部和外部用户放到返回值前列属性account3 @A:用户名3
+- 开启showOutside查询未删除用户键值对属性account4 @A:用户名4
+- 开启showOutside查询未删除用户数量 @4
 
 */
 include dirname(__FILE__, 5) . '/test/lib/init.php';
@@ -31,6 +33,7 @@ $user->visions->range('rnd');
 $user->gen(5);
 
 $usersToAppended = array('account5', 'account1');
+$showOutside     = '1';
 
 $user = new userModelTest();
 $outsideUsersWithClosed     = $user->getPairsTest('outside');
@@ -54,3 +57,5 @@ r(count($limit2Users))               && p()             && e('2');            //
 r(count($limit50Users))              && p()             && e('3');            //使用limit参数来限制最多获取50个用户键值对
 r($firstPO)                          && p('account1')   && e('A:用户名1');    //使用pofirst参数来将系统中的产品经理用户放到返回值前列
 r($firstPM)                          && p('account3')   && e('A:用户名3');    //使用pmfirst参数来将系统中的项目经理内部和外部用户放到返回值前列
+r($user->getPairsTest('noempty|noclosed', '', 0, array(), $showOutside)) && p('account4') && e('A:用户名4'); //开启showOutside查询未删除用户包含外部用户
+r(count($user->getPairsTest('noempty|noclosed', '', 0, array(), $showOutside))) && p() && e('4'); //开启showOutside查询未删除用户数量
