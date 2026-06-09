d2d7e063f4b766340f9c73cfc4b3e0da13a6547c
tianshujie <tianshujie@easycorp.ltd>
2026-06-09 13:51:51 +0800

    * [ci] Fix buildExecutionPairs error.

--- module/product/test/tao/buildexecutionpairs.php
+++ module/product/test/tao/buildexecutionpairs.php
@@ -12,11 +12,11 @@ cid=17531
 - 开启含有项目名称选项，检查包含子阶段的项目。 @瀑布项目2/阶段10/阶段16
 - 开启含有项目名称选项，检查无子阶段的项目。 @瀑布项目3/阶段24
 - 开启含有项目名称选项，检查看板项目。 @看板项目4/看板28
-- 不开启含有项目名称选项，检查敏捷项目。 @迭代5
+- 不开启含有项目名称选项，检查敏捷项目。 @/迭代5
 - 不开启含有项目名称选项，检查不启用迭代的项目。 @敏捷项目1(不启用迭代的项目)
 - 不开启含有项目名称选项，检查包含子阶段的项目。 @/阶段10/阶段16
 - 不开启含有项目名称选项，检查无子阶段的项目。 @/阶段24
-- 不开启含有项目名称选项，检查看板项目。 @看板28
+- 不开启含有项目名称选项，检查看板项目。 @/看板28
 - 开启stagefliter模式，过滤request,design,review属性的阶段，检查request的阶段是否存在。 @1
 - 开启stagefliter模式，过滤request,design,review属性的阶段，检查dev的阶段是否存在。 @1
 
@@ -31,7 +31,7 @@ $projectProduct->project->range('101-150');
 $projectProduct->product->range('1');
 $projectProduct->gen(28);
 
-$product = new productTest('admin');
+$product = new productTaoTest();
 
 $executions = $product->buildExecutionPairsTest('', true);
 r($executions[101]) && p() && e('敏捷项目1/迭代5');             // 开启含有项目名称选项，检查敏捷项目。
@@ -41,11 +41,11 @@ r($executions[120]) && p() && e('瀑布项目3/阶段24');            // 开启�
 r($executions[124]) && p() && e('看板项目4/看板28');            // 开启含有项目名称选项，检查看板项目。
 
 $executions = $product->buildExecutionPairsTest('', false);
-r($executions[101]) && p() && e('迭代5');                       // 不开启含有项目名称选项，检查敏捷项目。
+r($executions[101]) && p() && e('/迭代5');                      // 不开启含有项目名称选项，检查敏捷项目。
 r($executions[104]) && p() && e('敏捷项目1(不启用迭代的项目)'); // 不开启含有项目名称选项，检查不启用迭代的项目。
 r($executions[112]) && p() && e('/阶段10/阶段16');              // 不开启含有项目名称选项，检查包含子阶段的项目。
 r($executions[120]) && p() && e('/阶段24');                     // 不开启含有项目名称选项，检查无子阶段的项目。
-r($executions[124]) && p() && e('看板28');                      // 不开启含有项目名称选项，检查看板项目。
+r($executions[124]) && p() && e('/看板28');                     // 不开启含有项目名称选项，检查看板项目。
 
 $executions = $product->buildExecutionPairsTest('stagefilter', false);
 r(!isset($executions[112])) && p() && e('1'); // 开启stagefliter模式，过滤request,design,review属性的阶段，检查request的阶段是否存在。
