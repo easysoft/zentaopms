b1b1d7e12d72512e46abfb5666aa44914898268a
liyang <liyang@easycorp.ltd>
2026-06-09 14:54:32 +0800

    + [task#162006] add provider lang.

---
+++ module/provider/lang/fr.php
@@ -0,0 +1,5 @@
+<?php
+$lang->provider->browse = 'Browse Providers';
+$lang->provider->create = 'Create Provider';
+$lang->provider->edit   = 'Edit Provider';
+$lang->provider->delete = 'Delete Provider';
---
+++ module/provider/lang/zh-cn.php
@@ -0,0 +1,5 @@
+<?php
+$lang->provider->browse = '浏览服务';
+$lang->provider->create = '添加服务';
+$lang->provider->edit   = '编辑服务';
+$lang->provider->delete = '删除服务';
