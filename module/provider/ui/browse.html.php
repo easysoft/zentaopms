f696879fdcbffa8035ad4224fc91b8a708e0677a
liyang <liyang@easycorp.ltd>
2026-06-09 14:54:41 +0800

    + [task#162006] add provider browse page.

---
+++ module/provider/ui/browse.html.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+/**
+ * The browse view file of provider module of ZenTaoPMS.
+ * @copyright   Copyright 2009-2025 禅道软件（青岛）有限公司(ZenTao Software (Qingdao) Co., Ltd. www.zentao.net)
+ * @license     ZPL(https://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Yang Li <liyang@chandao.com>
+ * @package     provider
+ * @link        https://www.zentao.net
+ */
+namespace zin;
+
+$createItem = array
+(
+    'text'  => $lang->provider->create,
+    'url'   => createLink('provider', 'create'),
+    'icon'  => 'plus',
+    'class' => 'btn primary'
+);
+
+featureBar();
+toolbar
+(
+    hasPriv('provider', 'create') ? item(set($createItem)) : null
+);
