af5ed8a633da060b07f388ab405431f7681f130e
liyang <liyang@easycorp.ltd>
2026-06-10 11:04:08 +0800

    + [task#162010,162011,162012,162013,162014,162015] add check url method.

--- module/provider/zen.php
+++ module/provider/zen.php
@@ -11,4 +11,43 @@ declare(strict_types=1);
  */
 class providerZen extends provider
 {
+    /**
+     * 检查服务地址。
+     * Check provider url.
+     *
+     * @param  object $provider
+     * @access public
+     * @return bool
+     */
+    public function checkServiceUrl(object $provider): bool
+    {
+        $type = zget($provider, 'type', '');
+        $url  = trim((string)zget($provider, 'url', ''));
+        if(empty($type) || $type == 'Subversion' || empty($url)) return true;
+
+        if(!filter_var($url, FILTER_VALIDATE_URL))
+        {
+            dao::$errors['url'][] = sprintf($this->lang->error->URL, $this->lang->provider->url);
+            return false;
+        }
+
+        $apiUrl = $this->getCheckApiUrl($type, $url);
+        if(empty($apiUrl)) return true;
+
+        $headers  = $this->getCheckHeaders($type, trim((string)zget($provider, 'token', '')));
+        $response = json_decode(common::http($apiUrl, null, array(), $headers, 'JSON'));
+
+        if(empty($response))
+        {
+            $requestError = zget(commonModel::$requestErrors, 0, '');
+            dao::$errors['url'][] = empty($requestError) ? $this->lang->provider->error->api : sprintf($this->lang->provider->error->apiWithMessage, $requestError);
+        }
+        if(!empty($response->message))
+        {
+            $message = $response->message;
+            dao::$errors['url'][] = sprintf($this->lang->provider->error->apiWithMessage, $message);
+        }
+
+        return !dao::isError();
+    }
 }
