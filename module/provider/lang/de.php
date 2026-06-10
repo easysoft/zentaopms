c935262e40d86df1db32bb3344cb8fe37eecdc64
liyang <liyang@easycorp.ltd>
2026-06-10 13:49:27 +0800

    + [task#162010,162011,162012,162013,162014,162015] add lang.

--- module/provider/lang/de.php
+++ module/provider/lang/de.php
@@ -4,11 +4,13 @@ $lang->provider->create = 'Create Provider';
 $lang->provider->edit   = 'Edit Provider';
 $lang->provider->delete = 'Delete Provider';
 
-$lang->provider->name    = 'Name';
-$lang->provider->type    = 'Type';
-$lang->provider->url     = 'URL';
-$lang->provider->token   = 'Token';
-$lang->provider->account = 'Account';
+$lang->provider->name        = 'Name';
+$lang->provider->type        = 'Type';
+$lang->provider->url         = 'URL';
+$lang->provider->token       = 'Token';
+$lang->provider->account     = 'Account';
+$lang->provider->createdBy   = 'Created By';
+$lang->provider->createdDate = 'Created Date';
 
 $lang->provider->error = new stdclass();
 $lang->provider->error->api            = 'The server URL cannot reach the provider API.';
@@ -21,3 +23,7 @@ $lang->provider->typeList['Gitea']      = 'Gitea';
 $lang->provider->typeList['Gogs']       = 'Gogs';
 $lang->provider->typeList['Subversion'] = 'Subversion';
 $lang->provider->typeList['Jenkins']    = 'Jenkins';
+
+$lang->provider->notice = new stdclass();
+$lang->provider->notice->confirmDelete = 'Are you sure to delete this provider?';
+$lang->provider->notice->emptyProvider = 'No providers.';
--- module/provider/lang/zh-cn.php
+++ module/provider/lang/zh-cn.php
@@ -4,11 +4,13 @@ $lang->provider->create = '添加服务';
 $lang->provider->edit   = '编辑服务';
 $lang->provider->delete = '删除服务';
 
-$lang->provider->name    = '服务名称';
-$lang->provider->type    = '服务类型';
-$lang->provider->url     = '服务器地址';
-$lang->provider->token   = '令牌';
-$lang->provider->account = '用户名';
+$lang->provider->name        = '服务名称';
+$lang->provider->type        = '服务类型';
+$lang->provider->url         = '服务器地址';
+$lang->provider->token       = '令牌';
+$lang->provider->account     = '用户名';
+$lang->provider->createdBy   = '创建人';
+$lang->provider->createdDate = '创建时间';
 
 $lang->provider->error = new stdclass();
 $lang->provider->error->api            = '『服务器地址』无法调用通接口。';
@@ -21,3 +23,7 @@ $lang->provider->typeList['Gitea']      = 'Gitea';
 $lang->provider->typeList['Gogs']       = 'Gogs';
 $lang->provider->typeList['Subversion'] = 'Subversion';
 $lang->provider->typeList['Jenkins']    = 'Jenkins';
+
+$lang->provider->notice = new stdclass();
+$lang->provider->notice->confirmDelete = '你确定要删除该服务吗？';
+$lang->provider->notice->emptyProvider = '暂无服务。';
