d98dd03b7c4211980ac636ca7e74665295709e9c
liyang <liyang@easycorp.ltd>
2026-06-10 08:48:03 +0800

    test: fix action module failed cases

--- module/action/test/model/getdynamicbyproject.php
+++ module/action/test/model/getdynamicbyproject.php
@@ -11,7 +11,7 @@ cid=14897
 - 指定用户admin获取项目动态 @10
 - 指定用户user1获取项目动态 @0
 - 无效项目ID（0）获取动态 @30
-- 不存在的项目ID获取动态 @0
+- 项目ID为2获取动态 @5
 
 */
 
@@ -19,11 +19,11 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/lib/model.class.php';
 
 // zendata数据准备
-$action = zenData('actionrecent');
+$action = zenData('action');
 $action->id->range('1-30');
 $action->objectType->range('task{10},story{10},bug{10}');
 $action->objectID->range('1-100');
-$action->project->range('101{20},102{5},103{5}');
+$action->project->range('1{20},2{5},3{5}');
 $action->actor->range('admin{10},dev17{10},test18{10}');
 $action->action->range('opened{15},edited{10},closed{5}');
 $action->date->range('(-1h)-(+1w):1D')->type('timestamp')->format('YY/MM/DD hh:mm:ss');
@@ -38,8 +38,8 @@ su('admin');
 // 创建测试实例
 $actionTest = new actionModelTest();
 
-r($actionTest->getDynamicByProjectTest(101)) && p() && e('20');          // 正常项目ID获取所有用户动态
-r($actionTest->getDynamicByProjectTest(101, 'admin')) && p() && e('10'); // 指定用户admin获取项目动态
-r($actionTest->getDynamicByProjectTest(101, 'user1')) && p() && e('0');  // 指定用户user1获取项目动态
+r($actionTest->getDynamicByProjectTest(1)) && p() && e('20');            // 正常项目ID获取所有用户动态
+r($actionTest->getDynamicByProjectTest(1, 'admin')) && p() && e('10');   // 指定用户admin获取项目动态
+r($actionTest->getDynamicByProjectTest(1, 'user1')) && p() && e('0');    // 指定用户user1获取项目动态
 r($actionTest->getDynamicByProjectTest(0)) && p() && e('30');            // 无效项目ID（0）获取动态
-r($actionTest->getDynamicByProjectTest(999)) && p() && e('0');           // 不存在的项目ID获取动态
+r($actionTest->getDynamicByProjectTest(2)) && p() && e('5');             // 项目ID为2获取动态
