aba7a5c9a9b1ecd649adb522c9641e4a4484c30d
liyang <liyang@easycorp.ltd>
2026-06-10 10:57:33 +0800

    + [task#162010,162011,162012,162013,162014,162015] add provider lang.

--- module/provider/lang/de.php
+++ module/provider/lang/de.php
@@ -3,3 +3,21 @@ $lang->provider->browse = 'Browse Providers';
 $lang->provider->create = 'Create Provider';
 $lang->provider->edit   = 'Edit Provider';
 $lang->provider->delete = 'Delete Provider';
+
+$lang->provider->name    = 'Name';
+$lang->provider->type    = 'Type';
+$lang->provider->url     = 'URL';
+$lang->provider->token   = 'Token';
+$lang->provider->account = 'Account';
+
+$lang->provider->error = new stdclass();
+$lang->provider->error->api            = 'The server URL cannot reach the provider API.';
+$lang->provider->error->apiWithMessage = 'The server URL cannot reach the provider API: %s';
+
+$lang->provider->typeList = array();
+$lang->provider->typeList['GitLab']     = 'GitLab';
+$lang->provider->typeList['GitHub']     = 'Github';
+$lang->provider->typeList['Gitea']      = 'Gitea';
+$lang->provider->typeList['Gogs']       = 'Gogs';
+$lang->provider->typeList['Subversion'] = 'Subversion';
+$lang->provider->typeList['Jenkins']    = 'Jenkins';
--- module/provider/lang/zh-cn.php
+++ module/provider/lang/zh-cn.php
@@ -3,3 +3,21 @@ $lang->provider->browse = '浏览服务';
 $lang->provider->create = '添加服务';
 $lang->provider->edit   = '编辑服务';
 $lang->provider->delete = '删除服务';
+
+$lang->provider->name    = '服务名称';
+$lang->provider->type    = '服务类型';
+$lang->provider->url     = '服务器地址';
+$lang->provider->token   = '令牌';
+$lang->provider->account = '用户名';
+
+$lang->provider->error = new stdclass();
+$lang->provider->error->api            = '『服务器地址』无法调用通接口。';
+$lang->provider->error->apiWithMessage = '『服务器地址』无法调用通接口：%s';
+
+$lang->provider->typeList = array();
+$lang->provider->typeList['GitLab']     = 'GitLab';
+$lang->provider->typeList['GitHub']     = 'Github';
+$lang->provider->typeList['Gitea']      = 'Gitea';
+$lang->provider->typeList['Gogs']       = 'Gogs';
+$lang->provider->typeList['Subversion'] = 'Subversion';
+$lang->provider->typeList['Jenkins']    = 'Jenkins';
