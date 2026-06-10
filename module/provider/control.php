dd68d80bebc6d4d779c69ade24332cfdd020f773
liyang <liyang@easycorp.ltd>
2026-06-10 11:00:22 +0800

    + [task#162010,162011,162012,162013,162014,162015] add create control method.

--- module/provider/control.php
+++ module/provider/control.php
@@ -11,6 +11,12 @@ declare(strict_types=1);
  */
 class provider extends control
 {
+    public function __construct($module = '', $method = '')
+    {
+        parent::__construct($module, $method);
+        $this->loadModel('space')->setMenu();
+    }
+
     /**
      * 浏览服务列表。
      * Browse provider list.
@@ -23,9 +29,45 @@ class provider extends control
      */
     public function browse(string $orderBy = 'id_desc', int $recPerPage = 20, int $pageID = 1)
     {
-        $this->loadModel('space')->setMenu();
-
         $this->view->title = $this->lang->provider->browse;
+        $this->display();
+    }
+
+    /**
+     * 创建服务。
+     * Create provider.
+     *
+     * @param  string $type
+     * @access public
+     * @return void
+     */
+    public function create(string $type = 'GitLab')
+    {
+        if($_POST)
+        {
+            $this->config->provider->form->create['token']['required']   = in_array($type, array('GitLab', 'GitHub', 'Gitea', 'Gogs', 'Jenkins'));
+            $this->config->provider->form->create['account']['required'] = $type == 'Jenkins';
+            $formData = form::data($this->config->provider->form->create)
+                ->add('createdBy', $this->app->user->account)
+                ->skipSpecial('name')
+                ->get();
+
+            if(!empty($formData->account && $type == 'Jenkins'))
+            {
+                $formData->token = base64_encode($formData->account . ':' . $formData->token);
+            }
+            unset($formData->account);
+
+            if(!empty($formData->url) && !$this->providerZen->checkServiceUrl($formData)) return $this->sendError(dao::getError());
+
+            $id = $this->provider->create($formData);
+            if(dao::isError()) return $this->sendError(dao::getError());
+
+            if($id) $this->loadModel('action')->create('provider', $id, 'created');
+            return $this->sendSuccess(array('load' => $this->createLink('provider', 'browse')));
+        }
+        $this->view->title = $this->lang->provider->create;
+        $this->view->type  = $type;
         $this->display();
     }
 }
