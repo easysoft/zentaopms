9abfffa24728ca5c82d31967cbf8215a77ebce26
xieqiyu <xieqiyu@chandao.com>
2026-06-11 02:36:25 +0000

    * [misc] Move the sql to 22.3.

--- db/update22.2.sql
+++ db/update22.2.sql
@@ -4,9 +4,3 @@ ALTER TABLE `zt_workflowlabel`
 ADD `type` enum('data', 'sql') NOT NULL DEFAULT 'data' AFTER `label`,
 ADD `sql` text NULL AFTER `params`;
 ALTER TABLE `zt_workflowlayout` ADD `ditto` tinyint unsigned NOT NULL DEFAULT 0 AFTER `position`;
-
-ALTER TABLE `zt_testrun` ADD COLUMN `caseVersion` smallint unsigned NOT NULL DEFAULT 0 COMMENT '用例版本' AFTER `case`;
-ALTER TABLE `zt_suitecase` ADD COLUMN `caseVersion` smallint unsigned NOT NULL DEFAULT 0 COMMENT '用例版本' AFTER `case`;
-
-INSERT INTO `zt_config`(`vision`, `owner`, `module`, `section`, `key`, `value`) VALUES ('', 'system', 'project',   '', 'ganttVersionSettings', 'deliverable');
-INSERT INTO `zt_config`(`vision`, `owner`, `module`, `section`, `key`, `value`) VALUES ('', 'system', 'execution', '', 'ganttVersionSettings', 'gantt');
