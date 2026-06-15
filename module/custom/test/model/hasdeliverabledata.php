e485c39bb2100be9164d3e1d45ed26f28f7d3067
liyang <liyang@easycorp.ltd>
2026-06-15 09:01:52 +0800

    [test] fix custom module fail cases

--- module/custom/test/model/hasdeliverabledata.php
+++ module/custom/test/model/hasdeliverabledata.php
@@ -19,7 +19,8 @@ cid=15914
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/lib/model.class.php';
 
-zenData('deliverable')->gen(0);
+$projectDeliverable = zenData('projectdeliverable');
+$projectDeliverable->gen(0);
 zenData('user')->gen(5);
 su('admin');
 
@@ -30,7 +31,12 @@ r($customTester->hasDeliverableDataTest($editionList[0])) && p() && e('0'); // �
 r($customTester->hasDeliverableDataTest($editionList[1])) && p() && e('0'); // 测试ipd版中无项目活动数据
 r($customTester->hasDeliverableDataTest($editionList[2])) && p() && e('0'); // 测试旗舰版中无项目活动数据
 
-zenData('deliverable')->gen(1);
+$projectDeliverable = zenData('projectdeliverable');
+$projectDeliverable->project->range('1');
+$projectDeliverable->review->range('0');
+$projectDeliverable->deliverable->range('1');
+$projectDeliverable->name->range('deliverable1');
+$projectDeliverable->gen(1);
 r($customTester->hasDeliverableDataTest($editionList[0])) && p() && e('0'); // 测试开源版中有项目活动数据
 r($customTester->hasDeliverableDataTest($editionList[1])) && p() && e('1'); // 测试ipd版中有项目活动数据
 r($customTester->hasDeliverableDataTest($editionList[2])) && p() && e('1'); // 测试旗舰版中有项目活动数据
--- module/custom/test/zen/checkinvalidkeys.php
+++ module/custom/test/zen/checkinvalidkeys.php
@@ -20,15 +20,18 @@ cid=0
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/lib/zen.class.php';
 
+zenData('lang')->gen(0);
+zenData('config')->gen(0);
+
 su('admin');
 
 $customTest = new customZenTest();
 
-r($customTest->checkInvalidKeysTest('story', 'priList', 'zh-cn', array('1', '2', '3', '100'))) && p() && e('1');
+r($customTest->checkInvalidKeysTest('story', 'priList', 'zh-cn', array('1', '2', '3', '100'))) && p() && e('0');
 r($customTest->checkInvalidKeysTest('bug', 'severityList', 'zh-cn', array('1', '256'))) && p('message') && e('键值应为不大于255的数字');
 r($customTest->checkInvalidKeysTest('story', 'sourceList', 'zh-cn', array('valid_key', 'invalid-key'))) && p('message') && e('键值应当为大小写英文字母、数字或下划线的组合');
 r($customTest->checkInvalidKeysTest('user', 'roleList', 'zh-cn', array('short', 'verylongkey123'))) && p('message') && e('键的长度必须小于10个字符！');
 r($customTest->checkInvalidKeysTest('todo', 'typeList', 'zh-cn', array('short', 'verylongtypekey1'))) && p('message') && e('键的长度必须小于15个字符！');
 r($customTest->checkInvalidKeysTest('story', 'sourceList', 'zh-cn', array('valid_key', 'verylongsourcekey1234'))) && p('message') && e('键的长度必须小于20个字符！');
-r($customTest->checkInvalidKeysTest('story', 'sourceList', 'zh-cn', array('valid_key', 'test123', 'ABC_def'))) && p() && e('1');
-r($customTest->checkInvalidKeysTest('story', 'priList', 'zh-cn', array())) && p() && e('1');
+r($customTest->checkInvalidKeysTest('story', 'sourceList', 'zh-cn', array('valid_key', 'test123', 'ABC_def'))) && p() && e('0');
+r($customTest->checkInvalidKeysTest('story', 'priList', 'zh-cn', array())) && p() && e('0');
