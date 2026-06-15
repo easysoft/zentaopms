9ef23568a5777b9312fcac6429d68a179274cba7
hujunqing <hujunqing@chandao.com>
2026-06-15 11:37:35 +0800

    [bug#73428,0.5h] fixed create api error.

--- module/doc/ui/app.html.php
+++ module/doc/ui/app.html.php
@@ -72,7 +72,6 @@ $privs['struct']       = hasPriv('api', 'struct');
 $privs['createOffice'] = $privs['create'];
 $privs['restoreDoc']   = $privs['edit'];
 
-$privs['exportApi']     = $this->config->edition != 'open' && hasPriv('api', 'export');
 $privs['exportOpenApi'] = $this->config->edition != 'open' && hasPriv('api', 'exportOpenApi');
 $privs['importOpenApi'] = $this->config->edition != 'open' && hasPriv('api', 'importOpenApi');
 $privs['addChapter']    = hasPriv('doc', 'addChapter');
