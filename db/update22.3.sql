4d91dc155468d5f3500a6d0a32c0df8bb73ae16a
liuruogu <liuruogu@easycorp.ltd>
2026-06-15 06:50:15 +0000

    * optimize SQL.

--- db/update22.3.sql
+++ db/update22.3.sql
@@ -2,17 +2,17 @@ ALTER TABLE `zt_ai_agent` ADD COLUMN `displayPosition` varchar(20) NOT NULL DEFA
 ALTER TABLE `zt_ai_agent` ADD COLUMN `actionPurpose` varchar(100) NOT NULL DEFAULT '' COMMENT '操作目的编码' AFTER `displayPosition`;
 
 CREATE TABLE `zt_ai_skill` (
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-    `name` VARCHAR(255) NOT NULL COMMENT 'Skill名称',
-    `type` VARCHAR(255) NOT NULL DEFAULT 'private' COMMENT '类型：个人/公开',
-    `category` INT NOT NULL COMMENT '技能类型(模块ID)',
-    `status` VARCHAR(255) NOT NULL COMMENT '状态',
-    `fromID` INT NOT NULL COMMENT '来源ID（可将公开技能复制成个人技能）',
-    `desc` TEXT NULL COMMENT '描述',
-    `createdBy` VARCHAR(255) NOT NULL COMMENT '创建人',
-    `createdDate` DATETIME NULL DEFAULT NULL COMMENT '创建时间',
-    `editedBy` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '最后修改人',
-    `editedDate` DATETIME NULL DEFAULT NULL COMMENT '最后修改时间',
-    `deleted` TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除',
+    `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+    `name` varchar(255) NOT NULL DEFAULT '' COMMENT 'Skill名称',
+    `type` varchar(30) NOT NULL DEFAULT 'private' COMMENT '类型：private个人，public公开',
+    `category` int unsigned NOT NULL DEFAULT 0 COMMENT '技能类型，关联模块ID',
+    `status` varchar(30) NOT NULL DEFAULT 'draft' COMMENT '状态：draft未发布，published已发布',
+    `fromID` int unsigned NOT NULL DEFAULT 0 COMMENT '来源ID，可将公开技能复制成个人技能',
+    `desc` varchar(500) NOT NULL DEFAULT '' COMMENT '描述',
+    `createdBy` varchar(30) NOT NULL DEFAULT '' COMMENT '创建人',
+    `createdDate` datetime DEFAULT NULL COMMENT '创建时间',
+    `editedBy` varchar(30) NOT NULL DEFAULT '' COMMENT '最后修改人',
+    `editedDate` datetime DEFAULT NULL COMMENT '最后修改时间',
+    `deleted` tinyint unsigned NOT NULL DEFAULT 0 COMMENT '是否删除',
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB COMMENT='AI技能';
