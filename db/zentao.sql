e8a36f1c74c17ed8df62b81e0d965cef04284de1
sunguangming <sunguangming@chandao.com>
2026-06-16 07:32:46 +0000

    * [misc] modify sql.

--- db/zentao.sql
+++ db/zentao.sql
@@ -15166,16 +15166,16 @@ CREATE TABLE IF NOT EXISTS `zt_ai_knowledgeitem` (
 -- DROP TABLE IF EXISTS `zt_ai_skill`;
 CREATE TABLE `zt_ai_skill` (
   `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-  `name` varchar(255) NOT NULL COMMENT 'Skill名称',
+  `name` varchar(255) NOT NULL DEFAULT '' COMMENT 'Skill名称',
   `type` varchar(20) NOT NULL DEFAULT 'private' COMMENT '类型：个人/公开',
   `category` int unsigned NOT NULL DEFAULT 0 COMMENT '技能类型(模块ID)',
   `status` varchar(20) NOT NULL DEFAULT '' COMMENT '状态',
   `fromID` int unsigned NOT NULL DEFAULT 0 COMMENT '来源ID（可将公开技能复制成个人技能）',
   `skillID` varchar(64) NOT NULL DEFAULT '' COMMENT 'ZAI中的skillID',
   `desc` text DEFAULT NULL COMMENT '描述',
-  `createdBy` varchar(64) NOT NULL DEFAULT '' COMMENT '创建人',
+  `createdBy` varchar(30) NOT NULL DEFAULT '' COMMENT '创建人',
   `createdDate` datetime DEFAULT NULL COMMENT '创建时间',
-  `editedBy` varchar(64) NOT NULL DEFAULT '' COMMENT '最后修改人ID',
+  `editedBy` varchar(30) NOT NULL DEFAULT '' COMMENT '最后修改人ID',
   `editedDate` datetime DEFAULT NULL COMMENT '最后修改时间',
   `deleted` tinyint unsigned NOT NULL DEFAULT 0 COMMENT '是否删除',
   PRIMARY KEY (`id`)
