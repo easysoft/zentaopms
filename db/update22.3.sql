ddb13e6a329657b6a6e676f92259b104e356158b
sunguangming <sunguangming@chandao.com>
2026-06-16 06:38:19 +0000

    * [misc] modify sql.

--- db/update22.3.sql
+++ db/update22.3.sql
@@ -2,18 +2,18 @@ ALTER TABLE `zt_ai_agent` ADD COLUMN `displayPosition` varchar(20) NOT NULL DEFA
 ALTER TABLE `zt_ai_agent` ADD COLUMN `actionPurpose` varchar(100) NOT NULL DEFAULT '' COMMENT '操作目的编码' AFTER `displayPosition`;
 
 CREATE TABLE `zt_ai_skill` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
   `name` varchar(255) NOT NULL COMMENT 'Skill名称',
-  `type` varchar(255) NOT NULL DEFAULT 'private' COMMENT '类型：个人/公开',
-  `category` int(11) NOT NULL COMMENT '技能类型(模块ID)',
-  `status` varchar(255) NOT NULL COMMENT '状态',
-  `fromID` int(11) NOT NULL COMMENT '来源ID（可将公开技能复制成个人技能）',
-  `skillID` varchar(255) NOT NULL COMMENT 'ZAI中的skillID',
+  `type` varchar(20) NOT NULL DEFAULT 'private' COMMENT '类型：个人/公开',
+  `category` int unsigned NOT NULL DEFAULT 0 COMMENT '技能类型(模块ID)',
+  `status` varchar(20) NOT NULL DEFAULT '' COMMENT '状态',
+  `fromID` int unsigned NOT NULL DEFAULT 0 COMMENT '来源ID（可将公开技能复制成个人技能）',
+  `skillID` varchar(64) NOT NULL DEFAULT '' COMMENT 'ZAI中的skillID',
   `desc` text DEFAULT NULL COMMENT '描述',
-  `createdBy` varchar(255) NOT NULL COMMENT '创建人',
+  `createdBy` varchar(64) NOT NULL DEFAULT '' COMMENT '创建人',
   `createdDate` datetime DEFAULT NULL COMMENT '创建时间',
-  `editedBy` varchar(255) NOT NULL COMMENT '最后修改人ID',
+  `editedBy` varchar(64) NOT NULL DEFAULT '' COMMENT '最后修改人ID',
   `editedDate` datetime DEFAULT NULL COMMENT '最后修改时间',
-  `deleted` tinyint(4) NOT NULL DEFAULT 0 COMMENT '是否删除',
+  `deleted` tinyint unsigned NOT NULL DEFAULT 0 COMMENT '是否删除',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB;
