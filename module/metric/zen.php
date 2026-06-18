7e8d54cbe7f23d0b66f301b74615d18f068df63f
liugang <liugang@chandao.com>
2026-06-18 10:26:51 +0800

    - [perf] fixed an issue where generated metric data contained closed objects.

--- module/metric/zen.php
+++ module/metric/zen.php
@@ -233,19 +233,16 @@ class metricZen extends metric
         /* To ensure logical concentration, here we directly use sql to get data to ensure query performance. */
         $productList = $this->dao->select('id,closedDate')->from(TABLE_PRODUCT)
             ->where('deleted')->eq(0)
-            ->andWhere('status')->ne('closed')
             ->andWhere('shadow')->eq(0)
             ->fetchPairs('id', 'closedDate');
 
         $projectList = $this->dao->select('id,closedDate')->from(TABLE_PROJECT)
             ->where('deleted')->eq(0)
-            ->andWhere('status')->notIN('closed,done')
             ->andWhere('type')->eq('project')
             ->fetchPairs('id', 'closedDate');
 
         $executionList = $this->dao->select('id,closedDate')->from(TABLE_EXECUTION)
             ->where('deleted')->eq(0)
-            ->andWhere('status')->notIN('closed,done')
             ->andWhere('type')->in('sprint,stage,kanban')
             ->fetchPairs('id', 'closedDate');
 
