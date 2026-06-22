6ba9bb812d4ea6c5df1f32ec1f22ac281373f1bb
xieqiyu <xieqiyu@chandao.com>
2026-06-22 06:28:17 +0000

    * [misc] Modify the unit test file.

--- module/testcase/test/model/getexecutioncases.php
+++ module/testcase/test/model/getexecutioncases.php
@@ -4,7 +4,30 @@ include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/lib/model.class.php';
 su('admin');
 
-zenData('projectcase')->gen(10);
+$case = zenData('case');
+$case->id->range('1-6');
+$case->product->range('1{4},2{2}');
+$case->title->range('测试用例1,测试用例2,测试用例3,测试用例4,测试用例5,测试用例6');
+$case->status->range('wait,normal,blocked,investigate,needconfirm,needconfirm');
+$case->story->range('0,0,0,0,5,6');
+$case->storyVersion->range('1{6}');
+$case->deleted->range('0{6}');
+$case->gen(6);
+
+$story = zenData('story');
+$story->id->range('5,6');
+$story->version->range('2,2');
+$story->status->range('active{2}');
+$story->gen(2);
+
+$projectcase = zenData('projectcase');
+$projectcase->id->range('1-6');
+$projectcase->project->range('101{6}');
+$projectcase->product->range('1{4},2{2}');
+$projectcase->case->range('1-6');
+$projectcase->gen(6);
+
+zenData('module')->gen(0);
 
 /**
 
