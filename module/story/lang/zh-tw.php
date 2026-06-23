447a80d77ef772f9bb0b4622cfe7386c25584fbe
sunjun <sunjun@chandao.com>
2026-06-23 05:11:13 +0000

    * [bug#162524,done,0.1h]Fix bug.

--- module/story/lang/zh-tw.php
+++ module/story/lang/zh-tw.php
@@ -419,5 +419,5 @@ $lang->story->categoryList['experience']  = '體驗';
 $lang->story->categoryList['improve']     = '改進';
 $lang->story->categoryList['other']       = '其他';
 
-$lang->story->batchSubmitReviewStatusTips = "Demand %s cannot be submitted for review as its status is neither Draft nor Under Change!";
+$lang->story->batchSubmitReviewStatusTips = "Story %s cannot be submitted for review as its status is neither Draft nor Under Change!";
 $lang->story->batchSubmitReviewPrivTips   = "You do not have permission to batch submit %s for review. Demands of this type have been filtered.\n";
