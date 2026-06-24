49131e21fed44c57cf2fc8e57d8f57d1adc18dba
xieqiyu <xieqiyu@chandao.com>
2026-06-24 10:01:14 +0000

    * [bug#73082] Change the col of budget for project.

--- db/update22.2.sql
+++ db/update22.2.sql
@@ -7,3 +7,5 @@ ALTER TABLE `zt_workflowlabel`
 ADD `type` enum('data', 'sql') NOT NULL DEFAULT 'data' AFTER `label`,
 ADD `sql` text NULL AFTER `params`;
 ALTER TABLE `zt_workflowlayout` ADD `ditto` tinyint unsigned NOT NULL DEFAULT 0 AFTER `position`;
+
+ALTER TABLE `zt_project` MODIFY COLUMN `budget` decimal(14,2) unsigned NOT NULL DEFAULT 0.00 COMMENT '预算';
