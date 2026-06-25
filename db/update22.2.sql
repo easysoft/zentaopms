b7bbc526feea6f5f1b5f036971475d2815ca8775
wangyuting <wangyuting@chandao.com>
2026-06-25 02:18:28 +0000

    * [bug#73287,done,2h,0h] Add demand datasource.

--- db/update22.2.sql
+++ db/update22.2.sql
@@ -9,3 +9,27 @@ ADD `sql` text NULL AFTER `params`;
 ALTER TABLE `zt_workflowlayout` ADD `ditto` tinyint unsigned NOT NULL DEFAULT 0 AFTER `position`;
 
 ALTER TABLE `zt_project` MODIFY COLUMN `budget` decimal(14,2) unsigned NOT NULL DEFAULT 0.00 COMMENT '预算';
+
+INSERT INTO `zt_workflowdatasource` (`type`, `name`, `code`, `buildin`, `vision`, `createdBy`, `createdDate`, `datasource`, `view`, `keyField`, `valueField`) VALUES
+('sql', '需求池',     'demandPool',  '1', 'or',  'admin', '1970-01-01 00:00:01', 'SELECT `id`,`name` FROM zt_demandpool WHERE `deleted`=\'0\'', 'view_datasource_119', 'id', 'name'),
+('sql', '需求池需求', 'demand',      '1', 'or',  'admin', '1970-01-01 00:00:01', 'SELECT `id`,`title` FROM zt_demand WHERE `deleted`=\'0\'',    'view_datasource_120', 'id', 'title'),
+('sql', '路标',       'roadmap',     '1', 'or',  'admin', '1970-01-01 00:00:01', 'SELECT `id`,`name` FROM zt_roadmap WHERE `deleted`=\'0\'',    'view_datasource_121', 'id', 'name'),
+('sql', '分发需求',   'demandStory', '1', 'or',  'admin', '1970-01-01 00:00:01', 'SELECT `id`,`name` FROM zt_story WHERE `deleted`=\'0\'',      'view_datasource_122', 'id', 'title');
+
+DROP VIEW IF EXISTS `view_datasource_119`;
+DROP VIEW IF EXISTS `view_datasource_120`;
+DROP VIEW IF EXISTS `view_datasource_121`;
+DROP VIEW IF EXISTS `view_datasource_122`;
+
+CREATE VIEW `view_datasource_119` AS SELECT `id`,`name`  FROM `zt_demandpool`  WHERE `deleted` = '0';
+CREATE VIEW `view_datasource_120` AS SELECT `id`,`title` FROM `zt_demand`      WHERE `deleted` = '0';
+CREATE VIEW `view_datasource_121` AS SELECT `id`,`name`  FROM `zt_roadmap`     WHERE `deleted` = '0';
+CREATE VIEW `view_datasource_122` AS SELECT `id`,`title` FROM `zt_story`       WHERE `deleted` = '0';
+
+UPDATE `zt_workflowfield` SET `options` = (SELECT `id` FROM `zt_workflowdatasource` WHERE `code` = 'demandPool')  WHERE `module` = 'demand' AND `field` = 'pool';
+UPDATE `zt_workflowfield` SET `options` = (SELECT `id` FROM `zt_workflowdatasource` WHERE `code` = 'products')    WHERE `module` = 'demand' AND `field` = 'product';
+UPDATE `zt_workflowfield` SET `options` = (SELECT `id` FROM `zt_workflowdatasource` WHERE `code` = 'demand')      WHERE `module` = 'demand' AND `field` = 'parent';
+UPDATE `zt_workflowfield` SET `options` = (SELECT `id` FROM `zt_workflowdatasource` WHERE `code` = 'demandStory') WHERE `module` = 'demand' AND `field` = 'story';
+UPDATE `zt_workflowfield` SET `options` = (SELECT `id` FROM `zt_workflowdatasource` WHERE `code` = 'roadmap')     WHERE `module` = 'demand' AND `field` = 'roadmap';
+UPDATE `zt_workflowfield` SET `options` = (SELECT `id` FROM `zt_workflowdatasource` WHERE `code` = 'demand')      WHERE `module` = 'demand' AND `field` = 'duplicateDemand';
+UPDATE `zt_workflowfield` SET `options` = (SELECT `id` FROM `zt_workflowdatasource` WHERE `code` = 'demand')      WHERE `module` = 'demand' AND `field` = 'childDemands';
