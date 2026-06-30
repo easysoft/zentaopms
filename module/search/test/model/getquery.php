1d0839d8c5fbd4037af1a2737d695fa40726958d
liyang <liyang@easycorp.ltd>
2026-06-30 15:59:51 +0800

    + [test] Fix search unit tests

--- module/search/test/model/getquery.php
+++ module/search/test/model/getquery.php
@@ -15,22 +15,22 @@ cid=18303
 
 - 查询ID为1的搜索条件名称及查询数量
  - 属性title @这是搜索条件名称1
- - 属性queryCount @0
-- 查询ID为2的搜索条件名称及查询数量
+ - 第1条查询条件的field,operator,value属性 @name,include,aa
+- 查询ID为2的搜索条件名称及首条查询条件
  - 属性title @这是搜索条件名称2
- - 属性queryCount @0
-- 查询ID为3的搜索条件名称及查询数量
+ - 第1条查询条件的field,operator,value属性 @name,include,aa
+- 查询ID为3的搜索条件名称及首条查询条件
  - 属性title @这是搜索条件名称3
- - 属性queryCount @0
-- 查询ID为4的搜索条件名称及查询数量
+ - 第1条查询条件的field,operator,value属性 @name,include,aa
+- 查询ID为4的搜索条件名称及首条查询条件
  - 属性title @这是搜索条件名称4
- - 属性queryCount @0
-- 查询ID为5的搜索条件名称及查询数量
+ - 第1条查询条件的field,operator,value属性 @name,include,aa
+- 查询ID为5的搜索条件名称及首条查询条件
  - 属性title @这是搜索条件名称5
- - 属性queryCount @0
-- 查询ID为6的搜索条件名称及查询数量
+ - 第1条查询条件的field,operator,value属性 @name,include,aa
+- 查询ID为6的搜索条件名称及首条查询条件
  - 属性title @这是搜索条件名称6
- - 属性queryCount @0
+ - 第1条查询条件的field,operator,value属性 @name,include,aa
 
 */
 
@@ -38,9 +38,22 @@ $search = new searchModelTest();
 
 $queryIDList = array('1', '2', '3', '4', '5', '6');
 
-r($search->getQueryTest($queryIDList[0])) && p('title,queryCount') && e('这是搜索条件名称1,0');      //查询ID为1的搜索条件名称及查询数量
-r($search->getQueryTest($queryIDList[1])) && p('title,queryCount') && e('这是搜索条件名称2,0');      //查询ID为2的搜索条件名称及查询数量
-r($search->getQueryTest($queryIDList[2])) && p('title,queryCount') && e('这是搜索条件名称3,0');      //查询ID为3的搜索条件名称及查询数量
-r($search->getQueryTest($queryIDList[3])) && p('title,queryCount') && e('这是搜索条件名称4,0');      //查询ID为4的搜索条件名称及查询数量
-r($search->getQueryTest($queryIDList[4])) && p('title,queryCount') && e('这是搜索条件名称5,0');      //查询ID为5的搜索条件名称及查询数量
-r($search->getQueryTest($queryIDList[5])) && p('title,queryCount') && e('这是搜索条件名称6,0');      //查询ID为6的搜索条件名称及查询数量
+$query1 = $search->getQueryTest((int)$queryIDList[0]);
+$query2 = $search->getQueryTest((int)$queryIDList[1]);
+$query3 = $search->getQueryTest((int)$queryIDList[2]);
+$query4 = $search->getQueryTest((int)$queryIDList[3]);
+$query5 = $search->getQueryTest((int)$queryIDList[4]);
+$query6 = $search->getQueryTest((int)$queryIDList[5]);
+
+r($query1)          && p('title')                && e('这是搜索条件名称1'); // 查询ID为1的搜索条件名称
+r($query1->form[0]) && p('field,operator,value') && e('name,include,aa'); // 查询ID为1的首条查询条件
+r($query2)          && p('title')                && e('这是搜索条件名称2'); // 查询ID为2的搜索条件名称
+r($query2->form[0]) && p('field,operator,value') && e('name,include,aa'); // 查询ID为2的首条查询条件
+r($query3)          && p('title')                && e('这是搜索条件名称3'); // 查询ID为3的搜索条件名称
+r($query3->form[0]) && p('field,operator,value') && e('name,include,aa'); // 查询ID为3的首条查询条件
+r($query4)          && p('title')                && e('这是搜索条件名称4'); // 查询ID为4的搜索条件名称
+r($query4->form[0]) && p('field,operator,value') && e('name,include,aa'); // 查询ID为4的首条查询条件
+r($query5)          && p('title')                && e('这是搜索条件名称5'); // 查询ID为5的搜索条件名称
+r($query5->form[0]) && p('field,operator,value') && e('name,include,aa'); // 查询ID为5的首条查询条件
+r($query6)          && p('title')                && e('这是搜索条件名称6'); // 查询ID为6的搜索条件名称
+r($query6->form[0]) && p('field,operator,value') && e('name,include,aa'); // 查询ID为6的首条查询条件
