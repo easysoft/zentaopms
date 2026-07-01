41b9a8e8bd468890a4f2c898881fdf10ff85ec01
mazhiyuan <mazhiyuan@gitfox.io>
2026-07-01 08:55:52 +0800

    fix lint error

--- module/jenkins/view/ajaxgetjenkinstasks.html.php
+++ module/jenkins/view/ajaxgetjenkinstasks.html.php
@@ -9,6 +9,49 @@
  * @version     $Id$
  * @link        https://www.zentao.net
  */
+
+/**
+ * Render task tree HTML recursively.
+ *
+ * Each call handles one level of the tree. Branch nodes (array values)
+ * recurse into children, leaf nodes (string values) render as clickable links.
+ *
+ * @param  array  $tasks
+ * @param  int    $level
+ * @access private
+ * @return string
+ */
+function renderTaskTreeHtml($tasks, $level = 0)
+{
+    $html = '';
+    foreach($tasks as $name => $task)
+    {
+        if(empty($task))
+        {
+            continue;
+        }
+
+        if(is_array($task))
+        {
+            $oneLevel = $level === 0 ? ' one-level' : '';
+            $html .= "<li data-idx='\$groupName' data-id='{$name}' class='has-list open in{$oneLevel}'>";
+            $html .= '<i class="list-toggle icon"></i>';
+            $html .= "<div class='label-type'><a class='text-muted not-list-item'>{$name}</a></div>";
+            $html .= "<ul data-idx='{$name}'>";
+            $html .= renderTaskTreeHtml($task, $level + 1);
+            $html .= '</ul>';
+            $html .= '</li>';
+        }
+        else
+        {
+            $displayId = ($level === 0) ? $task : $name;
+            $html .= "<li data-idx='{$displayId}' data-id='{$displayId}'>";
+            $html .= "<a href='###' id='{$name}' class='text-ellipsis' onclick=\"setJenkinsJob('{$task}','{$name}')\" title='{$task}' data-key='{$task}'>{$task}</a>";
+            $html .= '</li>';
+        }
+    }
+    return $html;
+}
 ?>
 <style>
 #dropMenuTasks .tree li {padding: 3px 0 0 10px;}
@@ -32,61 +75,7 @@
   <div class="table-col col-left">
     <div class="list-group" id="jenkinsTaskList">
       <ul class='tree tree-angles' data-ride='tree' data-idx='0'>
-      <?php foreach($tasks as $groupName => $task):?>
-        <?php if(empty($task)) continue;?>
-        <?php if(is_array($task)):?>
-        <li data-idx='$groupName' data-id='<?php echo $groupName?>' class='has-list open in one-level'>
-          <i class='list-toggle icon'></i>
-          <div class='label-type'>
-            <a class='text-muted not-list-item'><?php echo $groupName;?></a>
-          </div>
-          <ul data-idx='<?php echo $groupName;?>'>
-          <?php foreach($task as $task2name => $task2):?>
-            <?php if(is_array($task2)):?>
-            <li data-idx='$groupName' data-id='<?php echo $task2name?>' class='has-list open in'>
-              <i class='list-toggle icon'></i>
-              <div class='label-type'>
-                <a class='text-muted not-list-item'><?php echo $task2name;?></a>
-              </div>
-              <ul data-idx='<?php echo $task2name;?>'>
-              <?php foreach($task2 as $task3name => $task3):?>
-                <?php if(is_array($task3)):?>
-                <li data-idx='$groupName' data-id='<?php echo $task3name?>' class='has-list open in'>
-                  <i class='list-toggle icon'></i>
-                  <div class='label-type'>
-                    <a class='text-muted not-list-item'><?php echo $task3name;?></a>
-                  </div>
-                  <ul data-idx='<?php echo $task3name;?>'>
-                  <?php foreach($task3 as $task4name => $task4):?>
-                    <?php if(is_array($task4)) continue;?>
-                    <li data-idx='<?php echo $task4name;?>' data-id='<?php echo $task4name;?>'>
-                      <a href='###' id='<?php echo $task4name?>' class='' text-ellipsis' onclick='setJenkinsJob("<?php echo $task4;?>","<?php echo $task4name;?>")' title='<?php echo $task4;?>' data-key='<?php echo $task4;?>'><?php echo $task4;?></a>
-                    </li>
-                  <?php endforeach;?>
-                  </ul>
-                </li>
-                <?php else:?>
-                <li data-idx='<?php echo $task3name;?>' data-id='<?php echo $task3name;?>'>
-                  <a href='###' id='<?php echo $task3name?>' onclick='setJenkinsJob("<?php echo $task3;?>","<?php echo $task3name;?>")' class='' text-ellipsis' title='<?php echo $task3;?>' data-key='<?php echo $task3;?>'><?php echo $task3;?></a>
-                </li>
-                <?php endif;?>
-              <?php endforeach;?>
-              </ul>
-            </li>
-            <?php else:?>
-            <li data-idx='<?php echo $task2name;?>' data-id='<?php echo $task2name;?>'>
-              <a href='###' onclick='setJenkinsJob("<?php echo $task2;?>","<?php echo $task2name;?>")' id='<?php echo $task2name?>' class='' text-ellipsis' title='<?php echo $task2;?>' data-key='<?php echo $task2;?>'><?php echo $task2;?></a>
-            </li>
-            <?php endif;?>
-          <?php endforeach;?>
-          </ul>
-        </li>
-        <?php else:?>
-        <li data-idx='<?php echo $task;?>' data-id='<?php echo $task;?>'>
-          <a href='###' id='<?php echo $groupName;?>' class='text-ellipsis' onclick='setJenkinsJob("<?php echo $task;?>","<?php echo $groupName;?>")' title='<?php echo $task;?>' data-key='<?php echo $task;?>' ><?php echo $task;?></a>
-        </li>
-        <?php endif;?>
-      <?php endforeach;?>
+      <?php echo renderTaskTreeHtml($tasks);?>
       </ul>
     </div>
   </div>
