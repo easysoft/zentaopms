a307d882ff0a7a0ac511f70f975390e0dbb998fa
liyang <liyang@easycorp.ltd>
2026-07-14 10:54:27 +0800

    * [misc] fix repo lang.

--- module/repo/lang/fr.php
+++ module/repo/lang/fr.php
@@ -457,6 +457,7 @@ $lang->repo->defaultBranch       = 'Default Branch';
 $lang->repo->committerTip        = 'Committer has write permission on code repository';
 $lang->repo->commitDetail        = '%s committed at %s by %s';
 $lang->repo->hasNoProduct        = 'The project or execution does not have a product';
+$lang->repo->failCreateWebhook   = 'Create Webhook Failed';
 
 $lang->repo->browseWebhooks     = 'Webhook List';
 $lang->repo->createWebhook      = 'Create Webhook';
