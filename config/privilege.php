a960514c56a8ab7702357be7b36d1d852f7df3b6
liyang <liyang@easycorp.ltd>
2026-07-16 16:40:26 +0800

    + [bug#74030] fix priv config.

--- config/privilege.php
+++ config/privilege.php
@@ -538,5 +538,4 @@ $config->ajaxDependencies['artifact.ajaxgetdirparentitems']                   = 
 $config->ajaxDependencies['artifact.ajaxgetfolders']                          = 'artifact.view';
 $config->ajaxDependencies['artifact.ajaxbatchdeleteartifact']                 = 'artifact.deleteartifact';
 $config->ajaxDependencies['ppm.ajaxgetcreatechecklist']                       = 'ppm.create';
-$config->ajaxDependencies['pipeline.ajaxgetstepgroups']                       = 'ppm.arrange';
 $config->ajaxDependencies['pipeline.ajaxexeclog']                             = 'ppm.execview';
