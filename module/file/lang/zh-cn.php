cbea0382aebd92421801da0041449b842a71dc7f
朱金勇 <zhujinyong@chandao.com>
2026-07-16 14:42:34 +0800

    * [apiv2] Fix bug #70546.

--- module/file/lang/zh-cn.php
+++ module/file/lang/zh-cn.php
@@ -76,7 +76,7 @@ $lang->file->errorNotExists   = "<span class='text-red'>文件夹 '%s' 不存在
 $lang->file->errorCanNotWrite = "<span class='text-red'>文件夹 '%s' 不可写,请改变文件夹的权限。在linux中输入指令: <span class='code'>sudo chmod -R 777 %s</span></span>";
 $lang->file->confirmDelete    = " 您确定删除该附件吗？";
 $lang->file->errorFileSize    = " 文件大小已经超过%s，可能不能成功上传！";
-$lang->file->errorFileUpload  = " 文件上传失败，文件大小可能超出限制";
+$lang->file->errorFileUpload  = " 文件上传失败，文件大小为0或者超出限制";
 $lang->file->errorFileFormat  = " 文件上传失败，文件格式不在规定范围内";
 $lang->file->errorFileMove    = " 文件上传失败，移动文件时出错";
 $lang->file->errorFileCount   = " 只能上传 %s 个文件，超出数量限制的文件无法上传。";
