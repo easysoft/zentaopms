2f47ad7cff079fe95d165e83009044527eaa7ab3
sunguangming <sunguangming@chandao.com>
2026-07-22 02:00:46 +0000

    * [misc] add open methods.

--- config/privilege.php
+++ config/privilege.php
@@ -253,6 +253,7 @@ $config->logonMethods[] = 'artifact.history';
 $config->logonMethods[] = 'artifact.moveartifact';
 $config->logonMethods[] = 'artifact.copycmd';
 $config->logonMethods[] = 'zai.ajaxgettoken';
+$config->logonMethods[] = 'zai.ajaxgetuseragent';
 
 /* Ajax 方法依赖的方法。The methods that Ajax methods depend on. */
 $config->ajaxDependencies['action.ajaxgetlist']                               = ['doc.templatelist', 'reporttemplate.browse', 'doc.edit', 'doc.view'];
