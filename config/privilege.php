511c893fff7010a9e5a824d4916b7f5c1a5b2782
liyang <liyang@easycorp.ltd>
2026-07-28 08:24:42 +0800

    * [bug#74432] add priv config.

--- config/privilege.php
+++ config/privilege.php
@@ -255,6 +255,8 @@ $config->logonMethods[] = 'artifact.copycmd';
 $config->logonMethods[] = 'zai.ajaxgettoken';
 $config->logonMethods[] = 'zai.ajaxgetuseragent';
 $config->logonMethods[] = 'repo.import';
+$config->logonMethods[] = 'deploy.ajaxgetartifact';
+$config->logonMethods[] = 'deploy.ajaxgetnodes';
 
 /* Ajax 方法依赖的方法。The methods that Ajax methods depend on. */
 $config->ajaxDependencies['action.ajaxgetlist']                               = ['doc.templatelist', 'reporttemplate.browse', 'doc.edit', 'doc.view'];
