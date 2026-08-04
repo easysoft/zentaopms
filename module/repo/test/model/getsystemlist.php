548a520226f532245a6ae3d4948f1a6822cc900a
liyang <liyang@easycorp.ltd>
2026-08-04 09:37:41 +0800

    * [unittest] add repo unit test.

--- module/repo/test/model/getsystemlist.php
+++ module/repo/test/model/getsystemlist.php
@@ -3,6 +3,8 @@
 include dirname(__FILE__, 5) . '/test/lib/init.php';
 include dirname(__FILE__, 2) . '/lib/model.class.php';
 
+zenData('system')->gen(0);
+
 /**
 
 title=测试 repoModel->getSystemList();
