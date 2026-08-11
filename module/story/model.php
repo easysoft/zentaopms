5f0b8764694660bc411e521b736a397b91d765e4
daitingting <daitingting@xirangit.com>
2026-08-11 10:42:42 +0800

    * [bug#74753,done,0.5h] Fix bug #74753.

--- module/story/model.php
+++ module/story/model.php
@@ -846,6 +846,8 @@ class storyModel extends model
             $actionID = $this->loadModel('action')->create('story', $storyID, $action, $this->post->comment);
             $this->action->logHistory($actionID, $changes);
 
+            $story->id   = $storyID;
+            $story->type = $oldStory->type;
             if(!isset($story->spec)) $story->spec = $oldStory->spec;
             $this->loadModel('message')->sendMentionNotice($oldStory->type, 'change', $actionID, $story, $oldStory);
 
@@ -999,6 +1001,7 @@ class storyModel extends model
             $actionID = $this->action->create('story', $storyID, $action, $story->comment);
             $this->action->logHistory($actionID, $changes);
 
+            $story->id   = $storyID;
             $story->type = $oldStory->type;
             if(!isset($story->spec)) $story->spec = $oldStory->spec;
             $this->loadModel('message')->sendMentionNotice($oldStory->type, 'edit', $actionID, $story, $oldStory);
